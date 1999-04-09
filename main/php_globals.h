011813cce80f057ae1eb602ba18892a735bbc293
Zeev Suraski <zeev@php.net>
1999-04-09 19:16:46 +0000

    *** empty log message ***

---
+++ main/php_globals.h
@@ -0,0 +1,31 @@
+#ifndef _PHP_GLOBALS_H
+#define _PHP_GLOBALS_H
+
+typedef struct _php_core_globals php_core_globals;
+
+#ifdef ZTS
+# define PLS_D	php_core_globals *core_globals
+# define PLS_DC	, PLS_D
+# define PLS_C	core_globals
+# define PLS_CC , PLS_C
+# define PG(v) (core_globals->v)
+# define PLS_FETCH()	php_core_globals *core_globals = ts_resource(core_globals_id)
+#else
+# define PLS_D
+# define PLS_DC
+# define PLS_C
+# define PLS_CC
+# define PG(v) (core_globals.v)
+# define PLS_FETCH()
+extern ZEND_API php_core_globals core_globals;
+#endif
+
+
+struct _php_core_globals {
+    long magic_quotes_gpc;
+    long magic_quotes_runtime;
+    long magic_quotes_sybase;
+};
+
+
+#endif /* _PHP_GLOBALS_H */
