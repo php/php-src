2127ede122ad920c22fecdc668e864c0210dd146
Stig Bakken <ssb@php.net>
1999-04-16 12:15:38 +0000

    updated GD from php3 repository, using it as a test extension for the new directory structure

---
+++ ext/gd/gdttf.h
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+#ifdef _OSD_POSIX
+#ifndef APACHE
+#error On this EBCDIC platform, PHP3 is only supported as an Apache module.
+#else /*APACHE*/
+#ifndef CHARSET_EBCDIC
+#define CHARSET_EBCDIC /* this machine uses EBCDIC, not ASCII! */
+#endif
+#include "ebcdic.h"
+#endif /*APACHE*/
+#endif /*_OSD_POSIX*/
+
+char * gdttf(gdImage *im, int *brect, int fg, char *fontname,
+    double ptsize, double angle, int x, int y, char *string);
+
