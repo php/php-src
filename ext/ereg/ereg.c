a4ffab5d2617ff03a30c19bd0e59fb0b6b3dae59
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-05-26 18:45:53 +0000

    Fixing a couple of long standing bugs.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@9097 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ereg/ereg.c
+++ ext/ereg/ereg.c
@@ -563,7 +563,7 @@ PHP_FUNCTION(split)
 	}
 
 	/* churn through str, generating array entries as we go */
-	while ((count == -1 || count > 1) && !(err = regexec(&re, strp, 1, subs, 0))) {
+	while ((count == -1 || count > 0) && !(err = regexec(&re, strp, 1, subs, 0))) {
 		if (subs[0].rm_so == 0 && subs[0].rm_eo) {
 			/* match is at start of string, return empty string */
 			add_next_index_stringl(return_value, empty_string, 0, 1);
@@ -604,9 +604,11 @@ PHP_FUNCTION(split)
 	}
 
 	/* otherwise we just have one last element to add to the array */
-	size = endp - strp;
+	if (count == -1) {
+		size = endp - strp;
 	
-	add_next_index_stringl(return_value, strp, size, 1);
+		add_next_index_stringl(return_value, strp, size, 1);
+	}
 
 	regfree(&re);
 
