a3076aef5f750553731b3a5877478394ec776399
ssb <ssb@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-06-18 13:42:21 +0000

    Apply Sascha's fsock fixes to php4.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@9786 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/fsock.h
+++ ext/standard/fsock.h
@@ -29,6 +29,8 @@
 */
 /* $Id$ */
 
+/* Synced with php3 revision 1.24 1999-06-18 [ssb] */
+
 #ifndef _FSOCK_H
 #define _FSOCK_H
 
@@ -58,6 +60,7 @@ extern int _php3_is_persistent_sock(int);
 extern int _php3_sock_set_blocking(int socket, int mode);
 extern int _php3_sock_destroy(int socket);
 extern int _php3_sock_close(int socket);
+size_t _php3_sock_set_def_chunk_size(size_t size);
 
 PHPAPI int connect_nonb(int sockfd, struct sockaddr *addr, int addrlen, struct timeval *timeout);
 
