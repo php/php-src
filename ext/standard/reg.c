2b8c85bd0b2bd89f8004cfc7b53b23d03663aa57
Andrey Hristov <andrey@php.net>
1999-06-21 23:07:46 +0000

    Backing out changes.

--- ext/standard/reg.c
+++ ext/standard/reg.c
@@ -561,7 +561,7 @@ PHP_FUNCTION(split)
 	}
 
 	/* churn through str, generating array entries as we go */
-	while ((count == -1 || count > 0) && !(err = regexec(&re, strp, 1, subs, 0))) {
+	while ((count == -1 || count > 1) && !(err = regexec(&re, strp, 1, subs, 0))) {
 		if (subs[0].rm_so == 0 && subs[0].rm_eo) {
 			/* match is at start of string, return empty string */
 			add_next_index_stringl(return_value, empty_string, 0, 1);
@@ -604,11 +604,9 @@ PHP_FUNCTION(split)
 	}
 
 	/* otherwise we just have one last element to add to the array */
-	if (count == -1 || err==REG_NOMATCH) {
-		size = endp - strp;
+	size = endp - strp;
 	
-		add_next_index_stringl(return_value, strp, size, 1);
-	}
+	add_next_index_stringl(return_value, strp, size, 1);
 
 	regfree(&re);
 
