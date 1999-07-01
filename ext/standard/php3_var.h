9ce37bbda164848e65d38fe0326beae0980254f7
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-01 05:47:06 +0000

    some additional consts
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10221 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php3_var.h
+++ ext/standard/php3_var.h
@@ -37,6 +37,6 @@ PHP_FUNCTION(unserialize);
 
 void php3api_var_dump(pval **struc, int level);
 void php3api_var_serialize(pval *buf, pval **struc);
-int php3api_var_unserialize(pval **rval, char **p, char *max);
+int php3api_var_unserialize(pval **rval, const char **p, const char *max);
 
 #endif /* _PHPVAR_H */
