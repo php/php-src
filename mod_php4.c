80c980974327de663eca305f86eb77d0000571e9
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-14 10:26:43 +0000

    change php4_value and php4_flag to php_value and php_flag
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10622 c90b9560-bf6c-de11-be94-00142212c4b1

--- mod_php4.c
+++ mod_php4.c
@@ -522,8 +522,8 @@ handler_rec php_handlers[] =
 
 command_rec php_commands[] =
 {
-	{"php4_value",		php_apache_value_handler, NULL, OR_OPTIONS, TAKE2, "PHP Value Modifier"},
-	{"php4_flag",			php_apache_flag_handler, NULL, OR_OPTIONS, TAKE2, "PHP Flag Modifier"},
+	{"php_value",		php_apache_value_handler, NULL, OR_OPTIONS, TAKE2, "PHP Value Modifier"},
+	{"php_flag",			php_apache_flag_handler, NULL, OR_OPTIONS, TAKE2, "PHP Flag Modifier"},
 	{NULL}
 };
 
