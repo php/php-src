2a43afd2ced1908de514731eb0cd376d8c59e4c9
Andi Gutmans <andi@php.net>
1999-07-15 18:33:41 +0000

    Sooner than you think, Sascha :)

---
+++ scripts/apache/apconf-conv.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$1" = "" ]; then
+	echo "Usage: $0 /somewhere/httpd.conf"
+	exit 1
+fi
+
+if [ ! -w $1 ]; then
+	echo "You cannot write to $1"
+	exit 1
+fi
+
+TMPFILE=tmpfile.$$
+
+awk -f conffix.awk <$1 >$TMPFILE
+
+if [ "$?" != 0 ]; then
+	exit 1
+fi
+
+mv -f $1 $1.orig
+mv -f $TMPFILE $1
+exit 0
+
