0fdfb380b1a3189f08e13f50020e79f04492aef6
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-16 14:58:16 +0000

    License update
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10761 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_list.c
+++ Zend/zend_list.c
@@ -4,15 +4,19 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1998, 1999 Andi Gutmans, Zeev Suraski                  |
    +----------------------------------------------------------------------+
-   | This source file is subject to the Zend license, that is bundled     |
-   | with this package in the file LICENSE.  If you did not receive a     |
-   | copy of the Zend license, please mail us at zend@zend.com so we can  |
-   | send you a copy immediately.                                         |
+   | This source file is subject to version 0.90 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        | 
+   | available at through the world-wide-web at                           |
+   | http://www.zend.com/license/0_90.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
    +----------------------------------------------------------------------+
    | Authors: Andi Gutmans <andi@zend.com>                                |
    |          Zeev Suraski <zeev@zend.com>                                |
    +----------------------------------------------------------------------+
 */
+
 
 #include "zend.h"
 #include "zend_list.h"
