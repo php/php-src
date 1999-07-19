64061202b0572230ce3fb8f99025b1015a518109
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-19 16:17:40 +0000

    Make error codes PHP 3.0 compatible
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10955 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_errors.h
+++ Zend/zend_errors.h
@@ -21,17 +21,17 @@
 #ifndef _ZEND_ERRORS_H
 #define _ZEND_ERRORS_H
 
-#define E_CORE_ERROR 	(1<<0L)
-#define E_CORE_WARNING	(1<<1L)
-#define E_COMPILE_ERROR	(1<<2L)
-#define E_COMPILE_WARNING	(1<<3L)
-#define E_PARSE			(1<<4L)
-#define E_ERROR			(1<<5L)
-#define E_WARNING		(1<<6L)
-#define E_NOTICE		(1<<7L)
+#define E_ERROR				(1<<0L)
+#define E_WARNING			(1<<1L)
+#define E_PARSE				(1<<2L)
+#define E_NOTICE			(1<<3L)
+#define E_CORE_ERROR		(1<<4L)
+#define E_CORE_WARNING		(1<<5L)
+#define E_COMPILE_ERROR		(1<<6L)
+#define E_COMPILE_WARNING	(1<<7L)
 
 #define E_ALL (E_ERROR | E_WARNING | E_PARSE | E_NOTICE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING)
 #define E_CORE (E_CORE_ERROR | E_CORE_WARNING)
 
-
 #endif /* _ZEND_ERRORS_H */
+
