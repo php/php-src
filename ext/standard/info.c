79a4b181bba365eccad91474f0f4fa9822e5a50e
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-19 13:35:13 +0000

    More credits.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10938 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -268,7 +268,9 @@ void php_print_credits(int flag)
 		CREDIT_LINE("Apache SAPI Module", "Rasmus Lerdorf, Zeev Suraski");
 		CREDIT_LINE("ISAPI SAPI Module", "Andi Gutmans, Zeev Suraski");
 		CREDIT_LINE("CGI SAPI Module", "Rasmus Lerdorf, Stig Sather Bakken");
-		CREDIT_LINE("Overall Work", "Sascha Schumann, Andrey Zmievski, Thies C. Arntzen, and others");
+		CREDIT_LINE("Overall Work", "Thies C. Arntzen, Stig Bakken, Shane Caraveo, \
+					Rasmus Lerdorf, Sascha Schumann, Zeev Suraski, Andrey Zmievski, \
+					and others");
 		PUTS("</table>\n");
 	}
 
