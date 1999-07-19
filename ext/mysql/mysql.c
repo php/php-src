8e6e58d0933047eee93d2aa9ea84a7a47e63cc6c
eschmid <eschmid@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-07-19 20:09:05 +0000

    It's in PHP 3 and 4.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@10987 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysql/mysql.c
+++ ext/mysql/mysql.c
@@ -1472,7 +1472,7 @@ PHP_FUNCTION(mysql_fetch_row)
 /* }}} */
 
 
-/* {{{ proto object mysql_fetch_object(int result)
+/* {{{ proto object mysql_fetch_object(int result [, int result_type])
    Fetch a result row as an object */
 PHP_FUNCTION(mysql_fetch_object)
 {
@@ -1486,7 +1486,7 @@ PHP_FUNCTION(mysql_fetch_object)
 /* }}} */
 
 
-/* {{{ proto array mysql_fetch_array(int result)
+/* {{{ proto array mysql_fetch_array(int result [, int result_type])
    Fetch a result row as an associative array */
 PHP_FUNCTION(mysql_fetch_array)
 {
