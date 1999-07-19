8d1de13aef82b5bed8f0f50737ec1065f94b7660
Zeev Suraski <zeev@php.net>
1999-07-19 20:02:12 +0000

    0.91 update

--- Zend/zend_hash.c
+++ Zend/zend_hash.c
@@ -4,10 +4,10 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1998, 1999 Andi Gutmans, Zeev Suraski                  |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 0.90 of the Zend license,     |
+   | This source file is subject to version 0.91 of the Zend license,     |
    | that is bundled with this package in the file LICENSE, and is        | 
    | available at through the world-wide-web at                           |
-   | http://www.zend.com/license/0_90.txt.                                |
+   | http://www.zend.com/license/0_91.txt.                                |
    | If you did not receive a copy of the Zend license and are unable to  |
    | obtain it through the world-wide-web, please send a note to          |
    | license@zend.com so we can mail you a copy immediately.              |
