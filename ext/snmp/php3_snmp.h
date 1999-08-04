2aa4138668388c3526a4ca3d20a235b051a774fc
Steven Lawrance <slawrance@php.net>
1999-08-04 20:56:33 +0000

    Updated the files so that they have all the functionality of PHP 3.0.12.  These include snmpwalkoid, snmp_get_quick_print, snmp_set_quick_print, and snmpset.

--- ext/snmp/php3_snmp.h
+++ ext/snmp/php3_snmp.h
@@ -1,31 +1,23 @@
 /*
-   +----------------------------------------------------------------------+
-   | PHP HTML Embedded Scripting Language Version 3.0                     |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 1997,1998 PHP Development Team (See Credits file)      |
-   +----------------------------------------------------------------------+
-   | This program is free software; you can redistribute it and/or modify |
-   | it under the terms of one of the following licenses:                 |
-   |                                                                      |
-   |  A) the GNU General Public License as published by the Free Software |
-   |     Foundation; either version 2 of the License, or (at your option) |
-   |     any later version.                                               |
-   |                                                                      |
-   |  B) the PHP License as published by the PHP Development Team and     |
-   |     included in the distribution in the file: LICENSE                |
-   |                                                                      |
-   | This program is distributed in the hope that it will be useful,      |
-   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
-   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
-   | GNU General Public License for more details.                         |
-   |                                                                      |
-   | You should have received a copy of both licenses referred to here.   |
-   | If you did not, or have any questions about PHP licensing, please    |
-   | contact core@php.net.                                                |
-   +----------------------------------------------------------------------+
-   | Authors: Rasmus Lerdorf <rasmus@lerdorf.on.ca>                       |
-   +----------------------------------------------------------------------+
- */
++----------------------------------------------------------------------+
+| PHP version 4.0                                                      |
++----------------------------------------------------------------------+
+| Copyright (c) 1997, 1998, 1999 The PHP Group                         |
++----------------------------------------------------------------------+
+| This source file is subject to version 2.0 of the PHP license,       |
+| that is bundled with this package in the file LICENSE, and is        |
+| available at through the world-wide-web at                           |
+| http://www.php.net/license/2_0.txt.                                  |
+| If you did not receive a copy of the PHP license and are unable to   |
+| obtain it through the world-wide-web, please send a note to          |
+| license@php.net so we can mail you a copy immediately.               |
++----------------------------------------------------------------------+
+| Authors: Rasmus Lerdorf <rasmus@lerdorf.on.ca>                       |
+|          Mike Jackson <mhjack@tscnet.com>                            |
+|          Steven Lawrance <slawrance@technologist.com>                |
++----------------------------------------------------------------------+
+*/
+
 /* $Id$ */
 #ifndef _PHP3_SNMP_H
 #define _PHP3_SNMP_H
@@ -46,6 +38,10 @@ extern int php3i_snmp_init(INIT_FUNC_ARGS);
 PHP_FUNCTION(snmpget);
 PHP_FUNCTION(snmpwalk);
 PHP_FUNCTION(snmprealwalk);
+PHP_FUNCTION(snmpwalkoid);
+PHP_FUNCTION(snmp_get_quick_print);
+PHP_FUNCTION(snmp_set_quick_print);
+PHP_FUNCTION(snmpset);
 void php3_info_snmp(ZEND_MODULE_INFO_FUNC_ARGS);
 #else
 
