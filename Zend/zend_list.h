3feb4b3bdd7d5b662929c7ec7a26507e28335553
Zeev Suraski <zeev@php.net>
1999-09-03 19:12:07 +0000

    Add new API for resources

--- Zend/zend_list.h
+++ Zend/zend_list.h
@@ -62,7 +62,18 @@ ZEND_API int zend_list_delete(int id);
 ZEND_API int zend_plist_delete(int id);
 ZEND_API void *zend_list_find(int id, int *type);
 ZEND_API void *zend_plist_find(int id, int *type);
+ZEND_API void *zend_fetch_resource(zval *passed_id, int default_id, char *resource_type_name, int resource_type);
+ZEND_API void *zend_fetch_resource_ex(zval *passed_id, int default_id, char *resource_type_name, int num_resource_types, ...);
 
 extern ZEND_API int le_index_ptr;  /* list entry type for index pointers */
+
+#define ZEND_VERIFY_RESOURCE(rsrc)		\
+	if (!rsrc) {						\
+		RETURN_FALSE;					\
+	}
+
+#define ZEND_FETCH_RESOURCE(rsrc, rsrc_type, passed_id, default_id, resource_type_name, resource_type)	\
+	rsrc = (rsrc_type) zend_fetch_resource(passed_id, default_id, resource_type_name, resource_type);	\
+	ZEND_VERIFY_RESOURCE(rsrc);
 
 #endif
