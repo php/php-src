a035ad1203ae4426096c4c3a760264c226d0d503
musone <musone@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-09-10 18:15:29 +0000

    ported the php3 stuff to php4. This module should work intechangably between the two versions also
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@12845 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mcal/php3_mcal.h
+++ ext/mcal/php3_mcal.h
@@ -17,16 +17,26 @@
 #include "build-defs.h"
 #endif
 
-extern PHP_MINIT_FUNCTION(mcal);
-PHP_MINFO_FUNCTION(mcal);
-
 /* Functions accessable to PHP */
+//extern php3_module_entry php3_mcal_module_entry;
 extern php3_module_entry php3_mcal_module_entry;
 #define php3_mcal_module_ptr &php3_mcal_module_entry
 #define phpext_mcal_ptr php3_mcal_module_ptr
 
+#ifdef ZEND_VERSION
+extern PHP_MINIT_FUNCTION(mcal);
+PHP_MINFO_FUNCTION(mcal);
+#else
+extern int PHP_MINIT_FUNCTION(INIT_FUNC_ARGS);
+extern void PHP_MINFO_FUNCTION(void);
+#endif
+
+
 extern int mcal_init_request(INIT_FUNC_ARGS);
 extern int mcal_end_request(void);
+void make_event_object();
+void php3_event_init();
+
 void php3_mcal_open(INTERNAL_FUNCTION_PARAMETERS);
 void php3_mcal_popen(INTERNAL_FUNCTION_PARAMETERS);
 void php3_mcal_reopen(INTERNAL_FUNCTION_PARAMETERS);
@@ -40,9 +50,28 @@ void php3_mcal_store_event(INTERNAL_FUNCTION_PARAMETERS);
 void php3_mcal_delete_event(INTERNAL_FUNCTION_PARAMETERS);
 void php3_mcal_snooze(INTERNAL_FUNCTION_PARAMETERS);
 void php3_mcal_list_alarms(INTERNAL_FUNCTION_PARAMETERS);
-
-
-
+void php3_mcal_event_set_category(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_title(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_description(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_start(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_end(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_alarm(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_class(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_is_leap_year(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_days_in_month(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_date_valid(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_time_valid(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_day_of_week(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_day_of_year(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_date_compare(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_init(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_next_recurrence(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_recur_daily(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_recur_weekly(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_recur_monthly_mday(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_recur_monthly_wday(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_event_set_recur_yearly(INTERNAL_FUNCTION_PARAMETERS);
+void php3_mcal_fetch_current_stream_event(INTERNAL_FUNCTION_PARAMETERS);
 
 
 #else
