5a6b8653b9f9fcb11227f0983b37ea140cbd21f4
Zeev Suraski <zeev@php.net>
1999-09-12 03:08:29 +0000

    *** empty log message ***

--- mod_php4.c
+++ mod_php4.c
@@ -488,7 +488,9 @@ void php_init_handler(server_rec *s, pool *p)
 		apache_php_initialized = 1;
 	}
 #if MODULE_MAGIC_NUMBER >= 19980527
-	ap_add_version_component("PHP/" PHP_VERSION);
+	if (INI_INT("expose_php")) {
+		ap_add_version_component("PHP/" PHP_VERSION);
+	}
 #endif
 }
 
