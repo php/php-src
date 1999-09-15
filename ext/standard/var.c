2048e1ece6e707828e8d2bcf12dc772633f780d2
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-09-15 13:37:11 +0000

    Fixed printf format.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@13119 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -75,7 +75,7 @@ void php3api_var_dump(pval **struc, int level)
 
 		case IS_OBJECT:
 			myht = HASH_OF(*struc);
-			i = sprintf(buf, "%*object of %s (%d) {\n", level, ' ', (*struc)->value.obj.ce->name, zend_hash_num_elements(myht));
+			i = sprintf(buf, "%*cobject of %s (%d) {\n", level, ' ', (*struc)->value.obj.ce->name, zend_hash_num_elements(myht));
 			PHPWRITE(&buf[1], i - 1);
 		  head_done:
 
