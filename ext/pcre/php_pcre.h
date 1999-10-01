a92fb5fe3d781b130e54ccc9bb457ffb6f5f8715
Andrei Zmievski <andrei@php.net>
1999-10-01 14:54:55 +0000

    rename.

--- ext/pcre/php_pcre.h
+++ ext/pcre/php_pcre.h
@@ -23,7 +23,7 @@
    | If you did not, or have any questions about PHP licensing, please    |
    | contact core@php.net.                                                |
    +----------------------------------------------------------------------+
-   | Authors: Andrey Zmievski <andrey@ispi.net>                           |
+   | Authors: Andrei Zmievski <andrei@ispi.net>                           |
    +----------------------------------------------------------------------+
  */
 
