cff564b5c239716bfe6787dcbda32fa9a4aed54a
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-10-04 10:39:32 +0000

    i swear, i compiled (at least) before i committed!
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@13938 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -229,7 +229,7 @@ PHP_INI_BEGIN()
 
 #if PHP_TRACK_VARS /* "cc -32" on IRIX 6.4 does not like (PHP_TRACK_VARS?"1":"0") - thies 991004 */
 	STD_PHP_INI_BOOLEAN("track_vars",			"1",			PHP_INI_ALL,		OnUpdateBool,				track_vars,		php_core_globals,	core_globals)
-#ese
+#else
 	STD_PHP_INI_BOOLEAN("track_vars",			"0",			PHP_INI_ALL,		OnUpdateBool,				track_vars,		php_core_globals,	core_globals)
 #endif
 
