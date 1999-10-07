d0e5ad2323df1764bad7c1c9eb19893059b8201b
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-10-07 10:56:04 +0000

    compiles again - and gets compiled
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@14137 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/db/db.c
+++ ext/db/db.c
@@ -20,22 +20,16 @@
 /* $Id$ */
 #define IS_EXT_MODULE
 #if COMPILE_DL
-#ifdef PHP_31
-# include "../phpdl.h"
-#else
-# ifdef THREAD_SAFE
-# undef THREAD_SAFE
-# endif
 # include "dl/phpdl.h"
 #endif
-#endif
 
-#if 0
+#if 1
 
 #include "php.h"
 #include "php_globals.h"
 #include "safe_mode.h"
 #include "fopen-wrappers.h"
+#include "ext/standard/flock_compat.h" 
 
 #include <stdlib.h>
 #include <string.h>
