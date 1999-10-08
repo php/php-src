05df81e5db3909bda6e69a2e90d5288db8b8b1c0
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-10-08 16:29:33 +0000

    Use correct data type for mutex.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@14282 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -90,7 +90,7 @@ PHPAPI int core_globals_id;
 
 /* temporary workaround for thread-safety issues in libzend */
 #if defined(ZTS) && !defined(NO_GLOBAL_LOCK)
-static THREAD_T global_lock;
+static MUTEX_T global_lock;
 #define global_lock() tsrm_mutex_lock(global_lock)
 #define global_unlock() tsrm_mutex_unlock(global_lock);
 #define global_lock_init() global_lock = tsrm_mutex_alloc()
