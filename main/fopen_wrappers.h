3f096439767ea92d6be99906572a29b2f708cb3d
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-10-12 18:50:05 +0000

    starting to clean-up/new API'ize and resourcify the file-stuff - nothing happened yet - just renaming things around.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@14449 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/fopen_wrappers.h
+++ main/fopen_wrappers.h
@@ -66,9 +66,7 @@
 #define IS_SOCKET		1
 #define BAD_URL			2
 
-#ifndef THREAD_SAFE
-extern int wsa_fp; /* a list for open sockets */
-#endif
+extern int le_socket; /* a list for open sockets */
 
 extern PHPAPI FILE *php3_fopen_wrapper(char *filename, char *mode, int options, int *issock, int *socketd);
 
