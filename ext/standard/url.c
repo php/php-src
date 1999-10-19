4b5149bc18a87615a80d0279ea2a823b5af9c110
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-10-19 11:02:05 +0000

    (parse_url) fix for #2569: parse_url('-') crashes @- Fixed parse_url('-') crash. (Thies)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@14621 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/url.c
+++ ext/standard/url.c
@@ -140,7 +140,7 @@ url *url_parse(char *string)
 		}
 		efree(result);
 	}
-	else if (!strcmp(ret->scheme, "http")) {
+	else if (ret->scheme && !strcmp(ret->scheme, "http")) {
 		STR_FREE(ret->scheme);
 		STR_FREE(ret->path);
 		STR_FREE(ret->query);
