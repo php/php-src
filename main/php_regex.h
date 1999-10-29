f160ee878256c37d21bf6de225da5978e6fcadb9
Rasmus Lerdorf <rasmus@php.net>
1999-10-29 17:33:19 +0000

    Kill the regex header file warnings

--- main/php_regex.h
+++ main/php_regex.h
@@ -4,6 +4,7 @@
 #if REGEX
 #include "regex/regex.h"
 #define _REGEX_H 1				/* this should stop Apache from loading the system version of regex.h */
+#define _REGEX_H_ 1
 #define _RX_H 1				  	/* Try defining these for Linux to	*/
 #define __REGEXP_LIBRARY_H__ 1 	/* avoid Apache including regex.h	*/
 #define _H_REGEX 1              /* This one is for AIX */
