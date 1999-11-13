3c1b3622351aef836033a9c9786e57bd9b363724
Andi Gutmans <andi@php.net>
1999-11-13 18:03:07 +0000

    - Make Win32 compile again

--- ext/ereg/regex/regex_extra.h
+++ ext/ereg/regex/regex_extra.h
@@ -6,9 +6,13 @@
 
 #if (defined(REGEX) && REGEX == 1) || (!defined(REGEX))
 
+#if !(WIN32|WINNT)
+
 #define regexec php_regexec
 #define regerror php_regerror
 #define regfree php_regfree
 #define regcomp php_regcomp
+
+#endif
 
 #endif
