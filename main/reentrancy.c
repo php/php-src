7d7552d7c48ea958431be355f6be326e2e1e8a05
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
1999-11-26 17:12:01 +0000

    Add/update copyright headers
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@16003 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/reentrancy.c
+++ main/reentrancy.c
@@ -1,3 +1,20 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999 The PHP Group                         |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.0 of the PHP license,       |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_0.txt.                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Sascha Schumann <sascha@schumann.cx>                        |
+   +----------------------------------------------------------------------+
+ */
 
 #include "php_reentrancy.h"
 
