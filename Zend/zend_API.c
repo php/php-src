3a176301214a0129461c947ace061906f9136aa0
Andi Gutmans <andi@php.net>
1999-11-30 20:37:52 +0000

    - Applied Thies' bug fix. Great work!

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -812,6 +812,7 @@ ZEND_API zend_class_entry *register_internal_class(zend_class_entry *class_entry
 	class_entry->parent = NULL;
 	class_entry->refcount = (int *) malloc(sizeof(int));
 	*class_entry->refcount = 1;
+	class_entry->constants_updated = 0;
 	zend_hash_init(&class_entry->default_properties, 0, NULL, PVAL_PTR_DTOR, 1);
 	zend_hash_init(&class_entry->function_table, 0, NULL, ZEND_FUNCTION_DTOR, 1);
 
