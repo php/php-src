d9a86285f6a64c2298dfcf9ba42a1e72e3ee6caa
Sascha Schumann <sas@php.net>
1999-12-05 16:43:32 +0000

    Fix some warnings

--- main/php_content_types.c
+++ main/php_content_types.c
@@ -2,6 +2,8 @@
 #include "SAPI.h"
 #include "rfc1867.h"
 
+#include "php_content_types.h"
+
 static sapi_post_content_type_reader php_post_content_types[] = {
 	{ MULTIPART_CONTENT_TYPE,		sizeof(MULTIPART_CONTENT_TYPE)-1,		rfc1867_post_reader	},
 	{ NULL, 0, NULL }
@@ -19,7 +21,7 @@ SAPI_POST_READER_FUNC(php_default_post_reader)
 }
 
 
-int php_startup_SAPI_content_types()
+int php_startup_SAPI_content_types(void)
 {
 	sapi_register_post_readers(php_post_content_types);
 	sapi_register_default_post_reader(php_default_post_reader);
--- main/php_content_types.h
+++ main/php_content_types.h
@@ -1,6 +1,7 @@
 #ifndef _PHP_CONTENT_TYPES_H
 #define _PHP_CONTENT_TYPES_H
 
+SAPI_POST_READER_FUNC(php_default_post_reader);
 int php_startup_SAPI_content_types(void);
 
 #endif /* _PHP_CONTENT_TYPES_H */
