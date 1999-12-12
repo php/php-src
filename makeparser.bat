9492ee9beab4bbb8b7d083cb7717cb0161c60a1b
SVN Migration <svn@php.net>
1999-12-12 15:55:58 +0000

    This commit was manufactured by cvs2svn to create branch 'RETURN_REF'.

--- makeparser.bat
+++
@@ -1,4 +0,0 @@
-d:\mingw32\bin\bison -p php -v -d language-parser.y
-flex -Pphp -olanguage-scanner.c -i language-scanner.lex
-d:\mingw32\bin\bison -p cfg -v -d configuration-parser.y
-flex -Pcfg -oconfiguration-scanner.c -i configuration-scanner.lex
--- php_getopt.h
+++
@@ -1,9 +0,0 @@
-/* Borrowed from Apache NT Port */
-#include "php.h"
-
-PHPAPI extern char *optarg;
-PHPAPI extern int optind;
-extern int opterr;
-extern int optopt;
-
-PHPAPI int getopt(int argc, char* const *argv, const char *optstr);
