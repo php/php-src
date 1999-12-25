7f27821c5f3b134dbe2ec40a38b33db70cce8be3
Zeev Suraski <zeev@php.net>
1999-12-25 23:52:00 +0000

    inline functions cannot accept varargs

--- Zend/zend_ptr_stack.h
+++ Zend/zend_ptr_stack.h
@@ -32,9 +32,9 @@ typedef struct _zend_ptr_stack {
 
 ZEND_API void zend_ptr_stack_init(zend_ptr_stack *stack);
 ZEND_API void zend_ptr_stack_push(zend_ptr_stack *stack, void *ptr);
-ZEND_API void zend_ptr_stack_n_push(zend_ptr_stack *stack, int count,...);
+ZEND_API void zend_ptr_stack_n_push(zend_ptr_stack *stack, int count, ...);
 ZEND_API void *zend_ptr_stack_pop(zend_ptr_stack *stack);
-ZEND_API void zend_ptr_stack_n_pop(zend_ptr_stack *stack, int count,...);
+ZEND_API void zend_ptr_stack_n_pop(zend_ptr_stack *stack, int count, ...);
 ZEND_API void zend_ptr_stack_destroy(zend_ptr_stack *stack);
 ZEND_API void zend_ptr_stack_apply(zend_ptr_stack *stack, void (*func)(void *));
 ZEND_API void zend_ptr_stack_clean(zend_ptr_stack *stack, void (*func)(void *));
