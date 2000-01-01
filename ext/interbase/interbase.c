43ae2bffbb876b0ad686e1fbe51712dacc76ac2f
Sascha Schumann <sas@php.net>
2000-01-01 01:32:05 +0000

    Happy Y2K patch! Happy new year (or the new millennium, depending on whether you start counting at 0 or 1).

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | PHP version 4.0                                                      |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997, 1998, 1999 The PHP Group                         |
+   | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
