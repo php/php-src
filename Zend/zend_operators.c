7a89cc82dceef9aec0fc5866357462f141d5eddd
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-01-01 15:31:59 +0000

    Fix buglet
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@18301 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -86,7 +86,7 @@ ZEND_API void convert_scalar_to_number(zval *op)
 		(holder).type = IS_LONG;									\
 		(op) = &(holder);											\
 	} else if ((op)->type==IS_UNSET) {								\
-		(holder).type = IS_UNSET;									\
+		(holder).type = IS_LONG;									\
 		(op) = &(holder);											\
 	}
 
