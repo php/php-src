e88946a748f6164d0d29e71926a83ac149847d55
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-01-18 19:15:45 +0000

    Make the phpinfo() images work properly again
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@18730 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -1191,6 +1191,7 @@ PHPAPI void php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_
 	zend_file_handle prepend_file, append_file;
 	SLS_FETCH();
 
+	zend_activate_modules();
 	if (SG(request_info).query_string && SG(request_info).query_string[0]=='=' 
 		&& PG(expose_php)) {
 		if (!strcmp(SG(request_info).query_string+1, PHP_LOGO_GUID)) {
@@ -1239,7 +1240,6 @@ PHPAPI void php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_
 	if (EG(main_op_array)) {
 		zend_hash_environment(PLS_C ELS_CC SLS_CC);
 		EG(active_op_array) = EG(main_op_array);
-		zend_activate_modules();
 		zend_execute(EG(main_op_array) ELS_CC);
 	}
 }
