26a4e25cb3d9f1ade27b3abfb83d6a5cecca1e29
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-02-02 17:19:36 +0000

    Fixed SAPI modules to properly register their related modules.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@19138 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/aolserver/aolserver.c
+++ sapi/aolserver/aolserver.c
@@ -281,7 +281,7 @@ static int
 php_ns_startup(sapi_module_struct *sapi_module)
 {
 	if(php_module_startup(sapi_module) == FAILURE
-			|| zend_register_module(&php_aolserver_module) == FAILURE) {
+			|| zend_startup_module(&php_aolserver_module) == FAILURE) {
 		return FAILURE;
 	} else {
 		return SUCCESS;
