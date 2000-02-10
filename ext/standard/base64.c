36308c04d35d57afa0cfa39215e6e6c0a263e85e
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-02-10 09:44:22 +0000

    @- base64_decode() will decode POST data correct. (Thies) @  Patch submitted by: Turadg Aleahmad <turadg@wise.berkeley.edu>
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@19406 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/base64.c
+++ ext/standard/base64.c
@@ -96,6 +96,16 @@ unsigned char *php_base64_decode(const unsigned char *str, int length, int *ret_
 	/* run through the whole string, converting as we go */
 	while ((ch = *current++) != '\0') {
 		if (ch == base64_pad) break;
+
+	    /* When Base64 gets POSTed, all pluses are interpreted as spaces.
+		   This line changes them back.  It's not exactly the Base64 spec,
+		   but it is completely compatible with it (the spec says that
+		   spaces are invalid).  This will also save many people considerable
+		   headache.  - Turadg Aleahmad <turadg@wise.berkeley.edu>
+	    */
+
+		if (ch == ' ') ch = '+'; 
+
 		ch = reverse_table[ch];
 		if (ch < 0) continue;
 
