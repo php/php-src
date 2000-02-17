4549b969f91556b15bff058197c0ef48e57503f6
Zeev Suraski <zeev@php.net>
2000-02-17 20:46:13 +0000

    Fix duplicate freeing

--- main/SAPI.c
+++ main/SAPI.c
@@ -97,6 +97,7 @@ SAPI_API void sapi_handle_post(void *arg SLS_DC)
 	if (SG(request_info).post_entry) {
 		SG(request_info).post_entry->post_handler(SG(request_info).content_type_dup, arg SLS_CC);
 		efree(SG(request_info).post_data);
+		SG(request_info).post_data = NULL;
 		efree(SG(request_info).content_type_dup);
 		SG(request_info).content_type_dup = NULL;
 	}
