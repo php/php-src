ca191f9f6741b530dda83ecb88a35db9dd9ac41e
ssb <ssb@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-02-17 20:32:51 +0000

    typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@19866 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -1116,7 +1116,7 @@ PHPAPI void php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_
 		prepend_file_p = NULL;
 	}
 	if (PG(auto_append_file) && PG(auto_append_file)[0] &&
-		strncmp(PG(auto_prepend_file), "none", 4) != 0) {
+		strncmp(PG(auto_append_file), "none", 4) != 0) {
 		append_file.filename = PG(auto_append_file);
 		append_file.free_filename = 0;
 		append_file.type = ZEND_HANDLE_FILENAME;
