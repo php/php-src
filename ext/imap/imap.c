35aa09bf2bf3c7e30de933d0608a2a102d4c5eb7
chagenbu <chagenbu@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-02-18 01:56:01 +0000

    fixing imap_delete. All of the argcount checks were against one integer too high.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@19888 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/imap.c
+++ ext/imap/imap.c
@@ -1544,7 +1544,7 @@ PHP_FUNCTION(imap_delete)
 	pils *imap_le_struct;
 	int myargc=ARG_COUNT(ht);
 
-	if ( myargc < 3 || myargc > 4 || getParameters(ht,myargc,&streamind,&sequence,&flags) == FAILURE) {
+	if (myargc < 2 || myargc > 3 || getParameters(ht,myargc,&streamind,&sequence,&flags) == FAILURE) {
 		WRONG_PARAM_COUNT;
 	}
 
@@ -1559,7 +1559,7 @@ PHP_FUNCTION(imap_delete)
 		RETURN_FALSE;
 	}
 
-	mail_setflag_full(imap_le_struct->imap_stream,sequence->value.str.val,"\\DELETED",myargc == 4 ? flags->value.lval : NIL);
+	mail_setflag_full(imap_le_struct->imap_stream,sequence->value.str.val, "\\DELETED", myargc == 3 ? flags->value.lval : NIL);
 	RETVAL_TRUE;
 }
 /* }}} */
