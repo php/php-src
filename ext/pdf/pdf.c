eb1cc78be873dd2c3b7c74eba96bee9bda441914
Egon Schmid <eschmid@php.net>
2000-02-23 14:50:40 +0000

    Changed here also.

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -607,7 +607,7 @@ PHP_FUNCTION(pdf_show_xy) {
 }
 /* }}} */
 
-/* {{{ proto void pdf_set_font(int pdfdoc, string font, double size, int encoding, [int embed])
+/* {{{ proto void pdf_set_font(int pdfdoc, string font, double size, string encoding, [int embed])
    Select the current font face, size and encoding */
 PHP_FUNCTION(pdf_set_font) {
 	pval *arg1, *arg2, *arg3, *arg4, *arg5;
