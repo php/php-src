5e55e47f7f32c40c446f2543cbc0002b52db47dd
Zeev Suraski <zeev@php.net>
2000-03-06 05:26:39 +0000

    It's official now...

--- Zend/zend_builtin_functions.h
+++ Zend/zend_builtin_functions.h
@@ -2,12 +2,12 @@
    +----------------------------------------------------------------------+
    | Zend Engine                                                          |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1998-2000 Andi Gutmans, Zeev Suraski                   |
+   | Copyright (c) 1998-2000 Zend Technologies Ltd. (http://www.zend.com) |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 0.91 of the Zend license,     |
+   | This source file is subject to version 0.92 of the Zend license,     |
    | that is bundled with this package in the file LICENSE, and is        |
    | available at through the world-wide-web at                           |
-   | http://www.zend.com/license/0_91.txt.                                |
+   | http://www.zend.com/license/0_92.txt.                                |
    | If you did not receive a copy of the Zend license and are unable to  |
    | obtain it through the world-wide-web, please send a note to          |
    | license@zend.com so we can mail you a copy immediately.              |
