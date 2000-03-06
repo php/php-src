23748abf86d5eca0bd5925a83c271e05e5d27131
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-03-06 19:25:08 +0000

    - Bump up Zend's API version
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@21069 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO		19990619
+#define ZEND_EXTENSION_API_NO		20000306
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
