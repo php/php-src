0e9f0c1c10dcabff432efb6dbfb9d7d9309ae1f8
stig <stig@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-03-06 18:46:10 +0000

    Added ZEND_MODULE_INFO_FUNC_ARGS_PASSTHRU.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@21067 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_modules.h
+++ Zend/zend_modules.h
@@ -26,6 +26,7 @@
 #define SHUTDOWN_FUNC_ARGS	int type, int module_number
 #define SHUTDOWN_FUNC_ARGS_PASSTHRU type, module_number
 #define ZEND_MODULE_INFO_FUNC_ARGS zend_module_entry *zend_module
+#define ZEND_MODULE_INFO_FUNC_ARGS_PASSTHRU zend_module
 
 #define STANDARD_MODULE_PROPERTIES_EX 0, 0, NULL, 0
 
