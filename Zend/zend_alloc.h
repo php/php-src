599ef2c3e125242bde2743a579be0d1bc453e3c5
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-03-25 19:23:16 +0000

    *** empty log message ***
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@21903 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_alloc.h
+++ Zend/zend_alloc.h
@@ -72,7 +72,7 @@ ZEND_API void *_ecalloc(size_t nmemb, size_t size ZEND_FILE_LINE_DC ZEND_FILE_LI
 ZEND_API void *_erealloc(void *ptr, size_t size, int allow_failure ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
 ZEND_API char *_estrdup(const char *s ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
 ZEND_API char *_estrndup(const char *s, unsigned int length ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
-ZEND_API void _persist_alloc(void *ptr ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
+ZEND_API int _persist_alloc(void *ptr ZEND_FILE_LINE_DC ZEND_FILE_LINE_ORIG_DC);
 
 /* Standard wrapper macros */
 #define emalloc(size)					_emalloc((size) ZEND_FILE_LINE_CC ZEND_FILE_LINE_EMPTY_CC)
