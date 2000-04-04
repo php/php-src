28d7e0ca40af97d88b65aa346396b4d6eebe4402
chagenbu <chagenbu@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-04-04 18:10:24 +0000

    return false after determining that a message number is bad, instead of going on to crash.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@22415 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -1566,7 +1566,9 @@ PHP_FUNCTION(imap_headerinfo)
 	
 	if (!msgno->value.lval || msgno->value.lval < 1 || (unsigned) msgno->value.lval > imap_le_struct->imap_stream->nmsgs) {
 		php_error(E_WARNING, "Bad message number");
+		RETURN_FALSE;
 	}
+	
 	if (mail_fetchstructure(imap_le_struct->imap_stream, msgno->value.lval, NIL)) {
 		cache = mail_elt(imap_le_struct->imap_stream, msgno->value.lval);
 	} else {
