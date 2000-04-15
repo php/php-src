5f0dab2447ecb3fb52ae52b63a1004456accfd0c
hholzgra <hholzgra@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-04-15 20:35:09 +0000

    made calendar a real extension instead of a dl one only
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@22850 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ ext/calendar/php_calendar.h
@@ -0,0 +1,34 @@
+#ifndef _INCLUDED_CALENDAR_H
+#define _INCLUDED_CALENDAR_H
+
+
+
+extern zend_module_entry calendar_module_entry;
+#define  calendar_module_ptr & calendar_module_entry
+
+
+
+/* Functions */
+
+PHP_MINIT_FUNCTION(calendar);
+PHP_RINIT_FUNCTION(calendar);
+PHP_RSHUTDOWN_FUNCTION(calendar);
+PHP_MINFO_FUNCTION(calendar);
+
+PHP_FUNCTION(jdtogregorian);
+PHP_FUNCTION(gregoriantojd);
+PHP_FUNCTION(jdtojulian);
+PHP_FUNCTION(juliantojd);
+PHP_FUNCTION(jdtojewish);
+PHP_FUNCTION(jewishtojd);
+PHP_FUNCTION(jdtofrench);
+PHP_FUNCTION(frenchtojd);
+PHP_FUNCTION(jddayofweek);
+PHP_FUNCTION(jdmonthname);
+PHP_FUNCTION(easter_days);
+PHP_FUNCTION(easter_date);
+
+
+#define phpext_calendar_ptr calendar_module_ptr
+
+#endif
