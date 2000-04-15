2e2642cfb33ffe308b6acea2bfcf7f37f999226b
Frank M. Kromann <fmk@php.net>
2000-04-15 19:20:43 +0000

    Adding $PHP_SELF variable for compatibility with other platforms

--- sapi/isapi/php4isapi.c
+++ sapi/isapi/php4isapi.c
@@ -322,6 +322,8 @@ static void sapi_isapi_register_server_variables(zval *track_vars_array ELS_DC S
 		if (lpECB->GetServerVariable(lpECB->ConnID, *p, static_variable_buf, &variable_len)
 			&& static_variable_buf[0]) {
 			php_register_variable(*p, static_variable_buf, track_vars_array ELS_CC PLS_CC);
+			if (strcmp(*p, "SCRIPT_NAME") == 0)
+				php_register_variable("PHP_SELF", static_variable_buf, track_vars_array ELS_CC PLS_CC);
 		} else if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
 			variable_buf = (char *) emalloc(variable_len);
 			if (lpECB->GetServerVariable(lpECB->ConnID, *p, variable_buf, &variable_len)
