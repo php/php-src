e04242da7b88b7aab11f9a2f81af3d193ea1212e
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-04-27 14:32:58 +0000

    .
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@23281 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -48,12 +48,6 @@
 
 /* {{{ includes & stuff */
 
-/*
-#if defined(COMPILE_DL)
-# include "dl/phpdl.h"
-#endif
-*/
-
 #include "php.h"
 
 #include "ext/standard/head.h"
@@ -99,7 +93,7 @@ PHP_OCI_API php_oci_globals oci_globals;
 /* }}} */
 /* {{{ dynamically loadable module stuff */
 
-#if COMPILE_DL_OCI8
+#ifdef COMPILE_DL_OCI8
 DLEXPORT zend_module_entry *get_module() { return &oci8_module_entry; };
 #endif /* COMPILE_DL */
 
