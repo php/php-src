822cf7ce94387efa6e1840862b7833d4362587d6
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-05-21 16:29:38 +0000

    - Get ready for release
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@24622 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO		20000510
+#define ZEND_EXTENSION_API_NO		20000521
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
