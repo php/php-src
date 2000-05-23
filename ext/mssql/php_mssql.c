5897c8446421aea89e06eccd1e81c182d9860150
hholzgra <hholzgra@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-05-23 22:19:29 +0000

    some protos where missing proto keyword
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@24877 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -1034,7 +1034,7 @@ static void php_mssql_fetch_hash(INTERNAL_FUNCTION_PARAMETERS)
 	result->cur_row++;
 }
 
-/* {{{ object mssql_fetch_object(int result_id)
+/* {{{ proto object mssql_fetch_object(int result_id)
    Returns a psuedo-object of the current row in the result set specified by result_id */
 PHP_FUNCTION(mssql_fetch_object)
 {
@@ -1046,7 +1046,7 @@ PHP_FUNCTION(mssql_fetch_object)
 
 /* }}} */
 
-/* {{{ array mssql_fetch_array(int result_id)
+/* {{{ proto array mssql_fetch_array(int result_id)
    Returns an associative array of the current row in the result set specified by result_id */
 PHP_FUNCTION(mssql_fetch_array)
 {
