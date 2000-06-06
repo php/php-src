1db00a6fcb01947a0fc69e36cb21c447dbce79b1
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-06 18:54:00 +0000

    - I didn't have anything to do with this buggy code. Just kidding :)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@25659 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/output.c
+++ ext/standard/output.c
@@ -12,8 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Andi Gutmans <andi@zend.com>                                |
-   |          Zeev Suraski <zeev@zend.com>                                |
+   | Authors: Zeev Suraski <zeev@zend.com>                                |
    |          Thies C. Arntzen <thies@digicol.de>                         |
    +----------------------------------------------------------------------+
 */
