bf4103d0b748117ca4b79ba8ca3d7d0eac0f343d
sascha <sascha@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-08 06:12:26 +0000

    Clean up acconfig.h
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@25766 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/acconfig.h
+++ Zend/acconfig.h
@@ -3,36 +3,15 @@
 
 @TOP@
 
-/* This is the default configuration file to read */
-#define USE_CONFIG_FILE 1
-
 /* these are defined by automake */
 #undef PACKAGE
 #undef VERSION
 
-/* define uint by configure if it is missed (QNX and BSD derived) */
 #undef uint
-
-/* define ulong by configure if it is missed (most probably is) */
 #undef ulong
-
-/* Undefine if you want stricter XML/SGML compliance by default */
-/* (this disables "<?expression?>" by default) */
-#define T_DEFAULT_SHORT_OPEN_TAG 1
-
-/* Define both of these if you want the bundled REGEX library */
-#define REGEX 0
-#define HSREGEX 0
-
-#undef ZEND_DEBUG
 
 /* Define if you want to enable memory limit support */
 #define MEMORY_LIMIT 0
-
-/* Define to compile Zend thread safe */
-#undef ZTS
-
-#undef HAVE_LIBDL
 
 @BOTTOM@
 
@@ -49,9 +28,6 @@
 #else
 # include <strings.h>
 #endif
-
-/* Define if you have stdiostream.h */
-#undef HAVE_STDIOSTREAM_H
 
 #if ZEND_BROKEN_SPRINTF
 int zend_sprintf(char *buffer, const char *format, ...);
