499c3df123248d917997731bf5c1560774fb923a
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-08 06:16:22 +0000

    Add PHP_BROKEN_SPRINTF check here.
    Also remove unnecessary #ifdef's for PACKAGE and VERSION.
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@25770 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php.h
+++ main/php.h
@@ -35,15 +35,15 @@
 #include "php_compat.h"
 
 /* automake defines PACKAGE and VERSION for Zend too */
-#ifdef PACKAGE
-# undef PACKAGE
-#endif
-#ifdef VERSION
-# undef VERSION
-#endif
+#undef PACKAGE
+#undef VERSION
 
 #include "zend_API.h"
 
+#if PHP_BROKEN_SPRINTF
+#undef sprintf
+#define sprintf php_sprintf
+#endif
 
 extern unsigned char first_arg_force_ref[];   
 extern unsigned char first_arg_allow_ref[];
