6bcc8ff0c767ad22d43b8e17f1f43f4d6ad3ccfa
Andi Gutmans <andi@php.net>
2000-06-09 14:41:33 +0000

    - Time to change it. We changed register_internal_class() -> - zend_register_internal_class()

--- Zend/zend_modules.h
+++ Zend/zend_modules.h
@@ -37,8 +37,7 @@ extern unsigned char second_arg_allow_ref[];
 
 #include "zend.h"
 
-#define ZEND_MODULE_API_NO 20000401
-
+#define ZEND_MODULE_API_NO 20000609
 #ifdef ZTS
 #define USING_ZTS 1
 #else
