c2794acd154495a3a47cc5a32baf7998bf653bbb
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-09 17:21:40 +0000

    Real fix for std_syslog issue on OpenServer. This is limited to the native development system (/bin/cc and icc) and does not show up with the UDK.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@25899 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_syslog.h
+++ main/php_syslog.h
@@ -7,26 +7,23 @@
 #include <syslog.h>
 #endif
 
-/*
- * SCO OpenServer 5 defines syslog to var_syslog/std_syslog which
- * causes trouble with our use of syslog. We define php_syslog
- * to be the system function syslog.
+/* 
+ * The SCO OpenServer 5 Development System (not the UDK)
+ * defines syslog to std_syslog.
  */
 
 #ifdef syslog
 
-#if defined(var_syslog) && var_syslog == syslog
-#define php_syslog var_syslog
-#elif defined(std_syslog) && std_syslog == syslog
+#ifdef HAVE_STD_SYSLOG
 #define php_syslog std_syslog
 #endif
+
+#undef syslog
 
 #endif
 
 #ifndef php_syslog
 #define php_syslog syslog
-#undef syslog
 #endif
-
 
 #endif
