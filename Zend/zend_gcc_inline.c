35f43ad274c504b68ff17d644fd2d16a3feb4a49
Sascha Schumann <sas@php.net>
2000-06-14 07:06:33 +0000

    Rename C0x-inline to C9x-inline, and frame preprocessor directives in zend_gcc_inline.c with #ifndef C9X_INLINE_SEMANTICS..#endif.

--- Zend/zend_gcc_inline.c
+++ Zend/zend_gcc_inline.c
@@ -1,7 +1,17 @@
 
-#define C0X_INLINE_SEMANTICS
+/*
+ * If C9X_INLINE_SEMANTICS is already defined here,
+ * we assume the user does not want GCC inline semantics,
+ * but compiles this file always.
+ */
+
+#ifndef C9X_INLINE_SEMANTICS
+
+#define C9X_INLINE_SEMANTICS
 
 #include "zend.h"
 
 #include "zend_execute.h"
 #include "zend_operators.h"
+
+#endif
