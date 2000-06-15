f5a92b4b5b714d22062bd4c2a96f8964f519361a
jah <jah@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-15 01:57:23 +0000

    - Fixed incorrect code generated when all parameters are optional. - Fixed handling of grouped optional parameters. - Added an option to generate xml documentation. - Added an option not to be nice and helpful and create all kinds   of comments and testing functions. - Added on option to create function stubs only. - Added options --assing-params and --string-lens that change   the generated code. - Updated documentation.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@26350 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/skeleton/skeleton.c
+++ ext/skeleton/skeleton.c
@@ -1,22 +1,4 @@
-/*
-   +----------------------------------------------------------------------+
-   | PHP version 4.0                                                      |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 2.02 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_02.txt.                                 |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Authors:                                                             |
-   +----------------------------------------------------------------------+
- */
- 
-/* $Id: */
+/* __header_here__ */
 
 #include "php.h"
 #include "php_ini.h"
@@ -102,8 +84,7 @@ PHP_MINFO_FUNCTION(extname)
 
 /* Remove the following function when you have succesfully modified config.m4
    so that your module can be compiled into PHP, it exists only for testing
-   purposes.
-*/
+   purposes. */
 
 /* Every user-visible function in PHP should document itself in the source */
 /* {{{ proto string confirm_extname_compiled(string arg)
@@ -134,9 +115,4 @@ PHP_FUNCTION(confirm_extname_compiled)
 
 #endif	/* HAVE_EXTNAME */
 
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- */
+/* __footer_here__ */
