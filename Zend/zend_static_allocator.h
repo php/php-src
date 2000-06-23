947e17b1dafb4310723e5deae0309d41b2ac7870
Andi Gutmans <andi@php.net>
2000-06-23 00:04:53 +0000

    - Add license

--- Zend/zend_static_allocator.h
+++ Zend/zend_static_allocator.h
@@ -1,3 +1,21 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2000 Zend Technologies Ltd. (http://www.zend.com) |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 0.92 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.zend.com/license/0_92.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+   | Authors: Andi Gutmans <andi@zend.com>                                |
+   +----------------------------------------------------------------------+
+*/
+
 #ifndef _STATIC_ALLOCATOR_H
 #define _STATIC_ALLOCATOR_H
 
