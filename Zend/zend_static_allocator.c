4263514405af9710a6f22daee25657c5e739d559
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-06-23 00:04:53 +0000

    - Add license
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@26805 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_static_allocator.c
+++ Zend/zend_static_allocator.c
@@ -1,3 +1,21 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2000 Zend Technologies Ltd. (http://www.zend.com) |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 0.92 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.zend.com/license/0_92.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+   | Authors: Andi Gutmans <andi@zend.com>                                |
+   +----------------------------------------------------------------------+
+*/
+
 #include <stdlib.h>
 
 #include "zend_static_allocator.h"
