ddec70c3b525ab84bb7bcc73e6e548e5164d18d8
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-07-06 22:38:01 +0000

    - One more...
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@27798 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -142,10 +142,10 @@ static inline zval *_get_object_zval_ptr(znode *node, temp_variable *Ts, int *sh
 			return &Ts[node->u.var].tmp_var;
 			break;
 		case IS_VAR:
-			if (Ts[node->u.var].var.ptr) {
-				PZVAL_UNLOCK(Ts[node->u.var].var.ptr);
+			if (Ts[node->u.var].var.ptr_ptr) {
+				PZVAL_UNLOCK(*Ts[node->u.var].var.ptr_ptr);
 				*should_free = 0;
-				return Ts[node->u.var].var.ptr;
+				return *Ts[node->u.var].var.ptr_ptr;
 			} else {
 				if (Ts[node->u.var].EA.type==IS_STRING_OFFSET) {
 					PZVAL_UNLOCK(Ts[node->u.var].EA.data.str_offset.str);
