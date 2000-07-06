58d1e0a2cc0681ff402d4f5ce9523609b6effbfe
Andi Gutmans <andi@php.net>
2000-07-06 23:00:46 +0000

    - Yet another fix...

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO		20000622
+#define ZEND_EXTENSION_API_NO		20000707
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
