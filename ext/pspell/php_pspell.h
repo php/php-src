7499d1f1d95dd436e36b7a359a3f2296aefd2e5c
vlad <vlad@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-07-10 21:29:19 +0000

    Initial release. Includes: - Functionality of aspell - 3 modes of spellchecking (fast, normal, bad_spellers) - support for run-together words
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@28030 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ ext/pspell/php_pspell.h
@@ -0,0 +1,53 @@
+/* 
+   +----------------------------------------------------------------------+
+   | PHP HTML Embedded Scripting Language Version 3.0                     |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-1999 PHP Development Team (See Credits file)      |
+   +----------------------------------------------------------------------+
+   | This program is free software; you can redistribute it and/or modify |
+   | it under the terms of one of the following licenses:                 |
+   |                                                                      |
+   |  A) the GNU General Public License as published by the Free Software |
+   |     Foundation; either version 2 of the License, or (at your option) |
+   |     any later version.                                               |
+   |                                                                      |
+   |  B) the PHP License as published by the PHP Development Team and     |
+   |     included in the distribution in the file: LICENSE                |
+   |                                                                      |
+   | This program is distributed in the hope that it will be useful,      |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+   | GNU General Public License for more details.                         |
+   |                                                                      |
+   | You should have received a copy of both licenses referred to here.   |
+   | If you did not, or have any questions about PHP licensing, please    |
+   | contact core@php.net.                                                |
+   +----------------------------------------------------------------------+
+   | Authors: Rasmus Lerdorf <rasmus@lerdorf.on.ca>                       |
+   +----------------------------------------------------------------------+
+ */
+
+/* $Id$ */
+
+
+#ifndef _PSPELL_H
+#define _PSPELL_H
+#if HAVE_PSPELL
+extern zend_module_entry pspell_module_entry;
+#define pspell_module_ptr &pspell_module_entry
+
+extern PHP_MINIT_FUNCTION(pspell);
+extern PHP_MINFO_FUNCTION(pspell);
+
+PHP_FUNCTION(pspell_new);
+PHP_FUNCTION(pspell_mode);
+PHP_FUNCTION(pspell_runtogether);
+PHP_FUNCTION(pspell_check);
+PHP_FUNCTION(pspell_suggest);
+#else
+#define pspell_module_ptr NULL
+#endif
+
+#define phpext_pspell_ptr pspell_module_ptr
+
+#endif /* _PSPELL_H */
