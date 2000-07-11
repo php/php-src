7c94448146c6ba68d6209cf4e791b8fbb2165abb
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-07-11 14:27:31 +0000

    Disable the hash_apply() protection on hashes that persist across requests - it's unsafe because we may be aborted at any point
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@28086 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_hash.h
+++ Zend/zend_hash.h
@@ -68,6 +68,7 @@ typedef struct _hashtable {
 	dtor_func_t pDestructor;
 	zend_bool persistent;
 	unsigned char nApplyCount;
+	zend_bool bApplyProtection;
 #if ZEND_DEBUG
 	int inconsistent;
 #endif
@@ -79,6 +80,7 @@ BEGIN_EXTERN_C()
 
 /* startup/shutdown */
 ZEND_API int zend_hash_init(HashTable *ht, uint nSize, hash_func_t pHashFunction, dtor_func_t pDestructor, int persistent);
+ZEND_API int zend_hash_init_ex(HashTable *ht, uint nSize, hash_func_t pHashFunction, dtor_func_t pDestructor, int persistent, zend_bool bApplyProtection);
 ZEND_API void zend_hash_destroy(HashTable *ht);
 
 ZEND_API void zend_hash_clean(HashTable *ht);
