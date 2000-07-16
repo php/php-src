7253e0717cefe7b95aaac5f48d1e8709dbd5e686
sterling <sterling@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-07-16 19:37:33 +0000

    Add CURL support (http://curl.haxx.se).
    # Currently pretty experimental, and labeled so, it
    # only really works with the CGI version.  I wanted
    # to get the sources in CVS, so I could give a
    # reference point for some issues, and get more
    # extensive testing...
    #
    # Does anyone know how long it is going to be till
    # the next minor release?
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@28447 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ ext/curl/php_curl.h
@@ -0,0 +1,45 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Sterling Hughes <sterling@php.net>                           |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef _PHP_CURL_H
+#define _PHP_CURL_H
+
+#ifdef COMPILE_DL_CURL
+#undef HAVE_CURL
+#define HAVE_CURL 1
+#endif
+
+#if HAVE_CURL
+
+extern zend_module_entry curl_module_entry;
+#define curl_module_ptr &curl_module_entry
+
+PHP_MINIT_FUNCTION (curl);
+PHP_MINFO_FUNCTION (curl);
+PHP_FUNCTION (curl_init);
+PHP_FUNCTION (curl_setopt);
+PHP_FUNCTION (curl_exec);
+PHP_FUNCTION (curl_close);
+
+#else
+#define curl_module_ptr NULL
+#endif /* HAVE_CURL */
+#define phpext_curl_ptr curl_module_ptr
+#endif  /* _PHP_CURL_H */
