5390b6a291024f9d74e2f4da39b32872c47149b3
Hartmut Holzgraefe <hholzgra@php.net>
2000-07-20 23:08:26 +0000

    fix for bug #5707

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -552,6 +552,8 @@ PHPAPI void php_dirname(char *str, int len) {
 #endif
 		)
 		*c='\0';
+	else
+		*str='\0';
 }
 
 /* {{{ proto string dirname(string path)
@@ -567,7 +569,11 @@ PHP_FUNCTION(dirname)
 	convert_to_string_ex(str);
 	ret = estrdup((*str)->value.str.val);
 	php_dirname(ret,(*str)->value.str.len);
-	RETVAL_STRING(ret,1);
+	if(*ret) {
+		RETVAL_STRING(ret,1);
+	} else { 
+		RETVAL_FALSE;
+	}
 	efree(ret);
 }
 /* }}} */
