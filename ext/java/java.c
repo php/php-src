0836e10a15900808d6b8b5436a346b1de612f2bd
David Croft <david@php.net>
2000-07-24 05:41:02 +0000

    various version 2.0 and 2.01 licenses -> 2.02

--- ext/java/java.c
+++ ext/java/java.c
@@ -7,14 +7,16 @@
    | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
    | available at through the world-wide-web at                           |
-   | http://www.php.net/license.html.                                     |
+   | http://www.php.net/license/2_02.txt.                                 |
    | If you did not receive a copy of the PHP license and are unable to   |
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
    | Author: Sam Ruby (rubys@us.ibm.com)                                  |
    +----------------------------------------------------------------------+
- */
+*/
+
+/* $Id$ */
 
 /*
  * This module implements Zend OO syntax overloading support for Java
