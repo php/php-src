e815a364ec31dec276edb4001719f0d0a408471b
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-07-29 10:48:57 +0000

    - Update .dsp's - Make non ZTS Win32 build again
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@29315 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_main.h
+++ main/php_main.h
@@ -41,6 +41,8 @@ PHPAPI int php_global_startup_extensions(zend_module_entry **ptr, int count);
 PHPAPI int php_global_shutdown_extensions(zend_module_entry **ptr, int count);
 
 PHPAPI void php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_DC);
+PHPAPI int php_lint_script(zend_file_handle *file CLS_DC ELS_DC PLS_DC);
+
 
 extern void php_call_shutdown_functions(void);
 
