3bc8b9dc9c9bacea79b1c762a670dfff6f272a24
Egon Schmid <eschmid@php.net>
2000-08-08 20:28:19 +0000

    Beautified some protos.

--- ext/ftp/php_ftp.c
+++ ext/ftp/php_ftp.c
@@ -24,8 +24,7 @@
    | If you did not, or have any questions about PHP licensing, please    |
    | contact core@php.net.                                                |
    +----------------------------------------------------------------------+
-   | Authors:                                                             |
-   |          Andrew Skalski      <askalski@chek.com>                     |
+   | Authors: Andrew Skalski      <askalski@chek.com>                     |
    +----------------------------------------------------------------------+
  */
 
@@ -130,7 +129,7 @@ PHP_MINIT_FUNCTION(ftp)
 	}
 
 /* {{{ proto int ftp_connect(string host [, int port])
-   Open a FTP stream */
+   Opens a FTP stream */
 PHP_FUNCTION(ftp_connect)
 {
 	pval		*arg1, *arg2;
