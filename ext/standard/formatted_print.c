f50a32da2d0c42dd6f1f09b59a0e22057f7a86ac
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-08-14 10:02:14 +0000

    Fix non-numbers printing (#6142) # Please, somebody knowing about floats - check if there's more non-numbers # seems we should check for all of them
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@30232 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/formatted_print.c
+++ ext/standard/formatted_print.c
@@ -252,6 +252,39 @@ php_sprintf_appenddouble(char **buffer, int *pos,
 	} else if (precision > MAX_FLOAT_PRECISION) {
 		precision = MAX_FLOAT_PRECISION;
 	}
+	
+	if(
+#if defined(HAVE_ISNAN)
+	isnan(number)
+#elif defined(NAN)
+	number == NAN
+#else 
+	0
+#endif
+	) {
+		sign = (number<0);
+		php_sprintf_appendstring(buffer, pos, size, "NaN", 3, 0, padding,
+								 alignment, precision, sign, 0);
+		return;
+	}
+
+	if(
+#if defined(HAVE_ISISINF)
+	isinf(number)
+#elif defined(HAVE_ISFINITE)
+	!isfinite(number)
+#elif defined(HAVE_FINITE)
+	!finite(number)
+#else
+	0
+#endif
+	) {
+		sign = (number<0);
+		php_sprintf_appendstring(buffer, pos, size, "INF", 3, 0, padding,
+								 alignment, precision, sign, 0);
+		return;
+	}
+
 	cvt = php_convert_to_decimal(number, precision, &decpt, &sign, (fmt == 'e'));
 
 	if (sign) {
