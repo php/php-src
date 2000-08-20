e9b82b6775577f259bca8077e1a48055bbb43c11
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-08-20 19:56:24 +0000

    - Change PHP_SEPARATOR to PHP_DIR_SEPARATOR
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@30660 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -317,8 +317,8 @@ PHPAPI FILE *php_fopen_primary_script(void)
 			if (pw && pw->pw_dir) {
 				filename = emalloc(strlen(PG(user_dir)) + strlen(path_info) + strlen(pw->pw_dir) + 4);
 				if (filename) {
-					sprintf(filename, "%s%c%s%c%s", pw->pw_dir, PHP_SEPARATOR,
-								PG(user_dir), PHP_SEPARATOR, s+1); /* Safe */
+					sprintf(filename, "%s%c%s%c%s", pw->pw_dir, PHP_DIR_SEPARATOR,
+								PG(user_dir), PHP_DIR_SEPARATOR, s+1); /* Safe */
 					STR_FREE(SG(request_info).path_translated);
 					SG(request_info).path_translated = filename;
 				}
@@ -338,7 +338,7 @@ PHPAPI FILE *php_fopen_primary_script(void)
 			if (filename) {
 				memcpy(filename, PG(doc_root), length);
 				if (!IS_SLASH(filename[length - 1])) {	/* length is never 0 */
-					filename[length++] = PHP_SEPARATOR;
+					filename[length++] = PHP_DIR_SEPARATOR;
 				}
 				if (IS_SLASH(path_info[0])) {
 					length--;
