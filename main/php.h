c86ec8b1a95e9c4716bffff816427419f295183c
Sascha Schumann <sas@php.net>
2000-08-20 12:49:56 +0000

    Add virtual_real_chdir_file. Silly name for a useful function.

--- main/php.h
+++ main/php.h
@@ -324,7 +324,7 @@ PHPAPI int cfg_get_string(char *varname, char **result);
 #define V_OPEN(open_args) open open_args
 #define V_CREAT(path, mode) creat(path, mode)
 #define V_CHDIR(path) chdir(path)
-#define V_CHDIR_FILE(path) chdir_file(path)
+#define V_CHDIR_FILE(path) virtual_real_chdir_file(path)
 #define V_GETWD(buf) getwd(buf)
 #define V_STAT(path, buff) stat(path, buff)
 #define V_LSTAT(path, buff) lstat(path, buff)
