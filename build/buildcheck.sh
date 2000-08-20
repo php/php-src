3d8fc790d8d1e8dad6f3a2ef843c06e81d1f5657
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-08-20 05:39:37 +0000

    Add/update license
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@30556 c90b9560-bf6c-de11-be94-00142212c4b1

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -1,4 +1,23 @@
 #! /bin/sh
+#  +----------------------------------------------------------------------+
+#  | PHP version 4.0                                                      |
+#  +----------------------------------------------------------------------+
+#  | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
+#  +----------------------------------------------------------------------+
+#  | This source file is subject to version 2.02 of the PHP license,      |
+#  | that is bundled with this package in the file LICENSE, and is        |
+#  | available at through the world-wide-web at                           |
+#  | http://www.php.net/license/2_02.txt.                                 |
+#  | If you did not receive a copy of the PHP license and are unable to   |
+#  | obtain it through the world-wide-web, please send a note to          |
+#  | license@php.net so we can mail you a copy immediately.               |
+#  +----------------------------------------------------------------------+
+#  | Authors: Stig Bakken <ssb@fast.no>                                   |
+#  |          Sascha Schumann <sascha@schumann.cx>                        |
+#  +----------------------------------------------------------------------+
+#
+# $Id: buildcheck.sh,v 1.3 2000-08-20 05:39:37 sas Exp $ 
+#
 
 echo "buildconf: checking installation..."
 
