c952a8852ffb2980e696cb63435ee82deb694019
SVN Migration <svn@php.net>
2000-08-22 12:10:54 +0000

    This commit was manufactured by cvs2svn to create tag 'PRE_LIBMYSQL_REVERT'.

--- main/fdfdata.h
+++
@@ -1,8 +0,0 @@
-#ifndef FDFDATA_H
-#define FDFDATA_H
-
-#include "SAPI.h"
-
-SAPI_POST_HANDLER_FUNC(fdf_post_handler);
-
-#endif /* FDFDATA_H */
--- main/safe_mode.h
+++
@@ -1,7 +0,0 @@
-#ifndef SAFE_MODE_H
-#define SAFE_MODE_H
-
-extern PHPAPI int php_checkuid(const char *filename, char *fopen_mode, int mode);
-extern PHPAPI char *php_get_current_user(void);
-
-#endif
