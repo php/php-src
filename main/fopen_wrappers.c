94e11b401092cf6ed010e4ad3963a4591bce9ba0
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-08-30 20:32:29 +0000

    - Missed a space
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@31555 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -413,7 +413,7 @@ PHPAPI FILE *php_fopen_with_path(char *filename, char *mode, char *path, char **
 #else
 	if (IS_SLASH(*filename)) {
 #endif
-		if (PG(safe_mode)&& PG(doc_root)) {
+		if (PG(safe_mode) && PG(doc_root)) {
 			snprintf(trypath, MAXPATHLEN, "%s%s", PG(doc_root), filename);
 			if (!php_checkuid(trypath, mode, 0)) {
 				return NULL;
