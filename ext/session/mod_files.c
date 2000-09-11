9e301d3c9f6635f4a16c3d7e724d08a6dcfabfca
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-09-11 15:24:28 +0000

    Libc5-readdir_r workaround
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@32471 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -151,7 +151,7 @@ static int ps_files_cleanup_dir(const char *dirname, int maxlifetime)
 {
 	DIR *dir;
 	char dentry[sizeof(struct dirent) + MAXPATHLEN + 1];
-	struct dirent *entry;
+	struct dirent *entry = (struct dirent *) &dentry;
 	struct stat sbuf;
 	char buf[MAXPATHLEN];
 	time_t now;
