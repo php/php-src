d3b478096fedc9d11c4ed0d8038e1c46a0bddd6c
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-09-13 13:55:35 +0000

    Fixed block overrun.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@32643 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/wddx/wddx.c
+++ ext/wddx/wddx.c
@@ -746,7 +746,6 @@ static void php_wddx_pop_element(void *user_data, const char *name)
 			unsigned char *new_str;
 
 			new_str = php_base64_decode(Z_STRVAL_P(ent1->data), Z_STRLEN_P(ent1->data), &new_len);
-			efree(Z_STRVAL_P(ent1->data));
 			Z_STRVAL_P(ent1->data) = new_str;
 			Z_STRLEN_P(ent1->data) = new_len;
 		}
