e07e19f271bf8a108d37b227ac50ade7236a3257
Sascha Schumann <sas@php.net>
2000-09-19 17:45:44 +0000

    The rewrite was completely authored by Hartmut

--- ext/standard/url_scanner.c
+++ ext/standard/url_scanner.c
@@ -12,8 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Author: Sascha Schumann    <sascha@schumann.cx>                      |
-   |         Hartmut Holzgraefe <hartmut@six.de>                          |
+   | Author: Hartmut Holzgraefe <hartmut@six.de>                          |
    +----------------------------------------------------------------------+
  */
 /* $Id$ */
