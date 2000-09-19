d9d65f18e9eec078f76cbf3bc95833cde055c8a3
Stanislav Malyshev <stas@php.net>
2000-09-19 17:27:56 +0000

    Fix crash on Solaris with function parameter destruction

--- Zend/zend_execute_locks.h
+++ Zend/zend_execute_locks.h
@@ -6,7 +6,6 @@
 							if (!(z)->refcount) {							\
 								(z)->refcount = 1;							\
 								(z)->is_ref = 0;							\
-								EG(garbage)[EG(garbage_ptr)++] = (z);		\
 								if (EG(garbage_ptr) == 4) {					\
 									zval_ptr_dtor(&EG(garbage)[0]);			\
 									zval_ptr_dtor(&EG(garbage)[1]);			\
@@ -14,6 +13,7 @@
 									EG(garbage)[1] = EG(garbage)[3];		\
 									EG(garbage_ptr) -= 2;					\
 								}											\
+								EG(garbage)[EG(garbage_ptr)++] = (z);		\
 							}												\
 						}
 
