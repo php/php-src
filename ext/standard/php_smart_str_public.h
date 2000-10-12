0f1baedf0c3e385b1153e0eb733f86eda1037001
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-10-12 00:48:12 +0000

    Move structure definition into its own file, so that public header files don't need to include the complete source code.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@33745 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ ext/standard/php_smart_str_public.h
@@ -0,0 +1,28 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999, 2000 The PHP Group                   |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Sascha Schumann <sascha@schumann.cx>                        |
+   +----------------------------------------------------------------------+
+ */
+
+#ifndef PHP_SMART_STR_PUBLIC_H
+#define PHP_SMART_STR_PUBLIC_H
+
+typedef struct {
+	char *c;
+	size_t len;
+	size_t a;
+} smart_str;
+
+#endif
