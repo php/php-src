9203201a3a0b1324005dd188788370161a29e3ab
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-10-27 23:02:20 +0000

    POST handler for my ubercool new Apache 2.0 function ap_get_req_body.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@34713 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache2filter/php_apache.h
+++ sapi/apache2filter/php_apache.h
@@ -5,6 +5,7 @@ typedef struct php_struct {
 	int state;
 	ap_bucket_brigade *bb;
 	ap_filter_t *f;
+	int post_index;
 } php_struct;
 
 void *merge_php_config(apr_pool_t *p, void *base_conf, void *new_conf);
