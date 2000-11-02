f1a9920bcfc269936338e8839e931d577d45ab6b
Zeev Suraski <zeev@php.net>
2000-11-02 19:27:55 +0000

    Maintain consistency

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -236,138 +236,138 @@ int zend_get_zendleng(CLS_D);
 
 
 /* parser-driven code generators */
-void do_binary_op(int op, znode *result, znode *op1, znode *op2 CLS_DC);
-void do_unary_op(int op, znode *result, znode *op1 CLS_DC);
-void do_binary_assign_op(int op, znode *result, znode *op1, znode *op2 CLS_DC);
-void do_assign(znode *result, znode *variable, znode *value CLS_DC);
-void do_assign_ref(znode *result, znode *lvar, znode *rvar CLS_DC);
+void zend_do_binary_op(int op, znode *result, znode *op1, znode *op2 CLS_DC);
+void zend_do_unary_op(int op, znode *result, znode *op1 CLS_DC);
+void zend_do_binary_assign_op(int op, znode *result, znode *op1, znode *op2 CLS_DC);
+void zend_do_assign(znode *result, znode *variable, znode *value CLS_DC);
+void zend_do_assign_ref(znode *result, znode *lvar, znode *rvar CLS_DC);
 void fetch_simple_variable(znode *result, znode *varname, int bp CLS_DC);
 void fetch_simple_variable_ex(znode *result, znode *varname, int bp, int op CLS_DC);
-void do_indirect_references(znode *result, znode *num_references, znode *variable CLS_DC);
-void do_fetch_global_or_static_variable(znode *varname, znode *static_assignment, int fetch_type CLS_DC);
-void do_fetch_globals(znode *varname CLS_DC);
+void zend_do_indirect_references(znode *result, znode *num_references, znode *variable CLS_DC);
+void zend_do_fetch_global_or_static_variable(znode *varname, znode *static_assignment, int fetch_type CLS_DC);
+void zend_do_fetch_globals(znode *varname CLS_DC);
 
 void fetch_array_begin(znode *result, znode *varname, znode *first_dim CLS_DC);
 void fetch_array_dim(znode *result, znode *parent, znode *dim CLS_DC);
 void fetch_string_offset(znode *result, znode *parent, znode *offset CLS_DC);
-void do_print(znode *result, znode *arg CLS_DC);
-void do_echo(znode *arg CLS_DC);
+void zend_do_print(znode *result, znode *arg CLS_DC);
+void zend_do_echo(znode *arg CLS_DC);
 typedef int (*unary_op_type)(zval *, zval *);
 ZEND_API unary_op_type get_unary_op(int opcode);
 ZEND_API void *get_binary_op(int opcode);
 
-void do_while_cond(znode *expr, znode *close_bracket_token CLS_DC);
-void do_while_end(znode *while_token, znode *close_bracket_token CLS_DC);
-void do_do_while_begin(CLS_D);
-void do_do_while_end(znode *do_token, znode *expr_open_bracket, znode *expr CLS_DC);
+void zend_do_while_cond(znode *expr, znode *close_bracket_token CLS_DC);
+void zend_do_while_end(znode *while_token, znode *close_bracket_token CLS_DC);
+void zend_do_do_while_begin(CLS_D);
+void zend_do_do_while_end(znode *do_token, znode *expr_open_bracket, znode *expr CLS_DC);
 
 
-void do_if_cond(znode *cond, znode *closing_bracket_token CLS_DC);
-void do_if_after_statement(znode *closing_bracket_token, unsigned char initialize CLS_DC);
-void do_if_end(CLS_D);
+void zend_do_if_cond(znode *cond, znode *closing_bracket_token CLS_DC);
+void zend_do_if_after_statement(znode *closing_bracket_token, unsigned char initialize CLS_DC);
+void zend_do_if_end(CLS_D);
 
-void do_for_cond(znode *expr, znode *second_semicolon_token CLS_DC);
-void do_for_before_statement(znode *cond_start, znode *second_semicolon_token CLS_DC);
-void do_for_end(znode *second_semicolon_token CLS_DC);
+void zend_do_for_cond(znode *expr, znode *second_semicolon_token CLS_DC);
+void zend_do_for_before_statement(znode *cond_start, znode *second_semicolon_token CLS_DC);
+void zend_do_for_end(znode *second_semicolon_token CLS_DC);
 
-void do_pre_incdec(znode *result, znode *op1, int op CLS_DC);
-void do_post_incdec(znode *result, znode *op1, int op CLS_DC);
+void zend_do_pre_incdec(znode *result, znode *op1, int op CLS_DC);
+void zend_do_post_incdec(znode *result, znode *op1, int op CLS_DC);
 
-void do_begin_variable_parse(CLS_D);
-void do_end_variable_parse(int type, int arg_offset CLS_DC);
+void zend_do_begin_variable_parse(CLS_D);
+void zend_do_end_variable_parse(int type, int arg_offset CLS_DC);
 
-void do_free(znode *op1 CLS_DC);
+void zend_do_free(znode *op1 CLS_DC);
 
-void do_init_string(znode *result CLS_DC);
-void do_add_char(znode *result, znode *op1, znode *op2 CLS_DC);
-void do_add_string(znode *result, znode *op1, znode *op2 CLS_DC);
-void do_add_variable(znode *result, znode *op1, znode *op2 CLS_DC);
+void zend_do_init_string(znode *result CLS_DC);
+void zend_do_add_char(znode *result, znode *op1, znode *op2 CLS_DC);
+void zend_do_add_string(znode *result, znode *op1, znode *op2 CLS_DC);
+void zend_do_add_variable(znode *result, znode *op1, znode *op2 CLS_DC);
 
-void do_begin_function_declaration(znode *function_token, znode *function_name, int is_method, int return_reference CLS_DC);
-void do_end_function_declaration(znode *function_token CLS_DC);
-void do_receive_arg(int op, znode *var, znode *offset, znode *initialization, unsigned char pass_type CLS_DC);
-int do_begin_function_call(znode *function_name CLS_DC);
-void do_begin_dynamic_function_call(znode *function_name CLS_DC);
-void do_begin_class_member_function_call(znode *class_name, znode *function_name CLS_DC);
-void do_end_function_call(znode *function_name, znode *result, znode *argument_list, int is_method, int is_dynamic_fcall CLS_DC);
-void do_return(znode *expr, int do_end_vparse CLS_DC);
+void zend_do_begin_function_declaration(znode *function_token, znode *function_name, int is_method, int return_reference CLS_DC);
+void zend_do_end_function_declaration(znode *function_token CLS_DC);
+void zend_do_receive_arg(int op, znode *var, znode *offset, znode *initialization, unsigned char pass_type CLS_DC);
+int zend_do_begin_function_call(znode *function_name CLS_DC);
+void zend_do_begin_dynamic_function_call(znode *function_name CLS_DC);
+void zend_do_begin_class_member_function_call(znode *class_name, znode *function_name CLS_DC);
+void zend_do_end_function_call(znode *function_name, znode *result, znode *argument_list, int is_method, int is_dynamic_fcall CLS_DC);
+void zend_do_return(znode *expr, int do_end_vparse CLS_DC);
 ZEND_API int do_bind_function_or_class(zend_op *opline, HashTable *function_table, HashTable *class_table, int compile_time);
-void do_inheritance(zend_class_entry *ce, zend_class_entry *parent_ce);
-void do_early_binding(CLS_D);
+void zend_do_inheritance(zend_class_entry *ce, zend_class_entry *parent_ce);
+void zend_do_early_binding(CLS_D);
 
-void do_pass_param(znode *param, int op, int offset CLS_DC);
-
-
-void do_boolean_or_begin(znode *expr1, znode *op_token CLS_DC);
-void do_boolean_or_end(znode *result, znode *expr1, znode *expr2, znode *op_token CLS_DC);
-void do_boolean_and_begin(znode *expr1, znode *op_token CLS_DC);               
-void do_boolean_and_end(znode *result, znode *expr1, znode *expr2, znode *op_token CLS_DC);
-
-void do_brk_cont(int op, znode *expr CLS_DC);
-
-void do_switch_cond(znode *cond CLS_DC);
-void do_switch_end(znode *case_list CLS_DC);
-void do_case_before_statement(znode *case_list, znode *case_token, znode *case_expr CLS_DC);
-void do_case_after_statement(znode *result, znode *case_token CLS_DC);
-void do_default_before_statement(znode *case_list, znode *default_token CLS_DC);
-
-void do_begin_class_declaration(znode *class_name, znode *parent_class_name CLS_DC);
-void do_end_class_declaration(CLS_D);
-void do_declare_property(znode *var_name, znode *value CLS_DC);
-
-void do_fetch_property(znode *result, znode *object, znode *property CLS_DC);
+void zend_do_pass_param(znode *param, int op, int offset CLS_DC);
 
 
-void do_push_object(znode *object CLS_DC);
-void do_pop_object(znode *object CLS_DC);
+void zend_do_boolean_or_begin(znode *expr1, znode *op_token CLS_DC);
+void zend_do_boolean_or_end(znode *result, znode *expr1, znode *expr2, znode *op_token CLS_DC);
+void zend_do_boolean_and_begin(znode *expr1, znode *op_token CLS_DC);               
+void zend_do_boolean_and_end(znode *result, znode *expr1, znode *expr2, znode *op_token CLS_DC);
+
+void zend_do_brk_cont(int op, znode *expr CLS_DC);
+
+void zend_do_switch_cond(znode *cond CLS_DC);
+void zend_do_switch_end(znode *case_list CLS_DC);
+void zend_do_case_before_statement(znode *case_list, znode *case_token, znode *case_expr CLS_DC);
+void zend_do_case_after_statement(znode *result, znode *case_token CLS_DC);
+void zend_do_default_before_statement(znode *case_list, znode *default_token CLS_DC);
+
+void zend_do_begin_class_declaration(znode *class_name, znode *parent_class_name CLS_DC);
+void zend_do_end_class_declaration(CLS_D);
+void zend_do_declare_property(znode *var_name, znode *value CLS_DC);
+
+void zend_do_fetch_property(znode *result, znode *object, znode *property CLS_DC);
 
 
-void do_begin_new_object(znode *new_token, znode *class_name CLS_DC);
-void do_end_new_object(znode *result, znode *class_name, znode *new_token, znode *argument_list CLS_DC);
+void zend_do_push_object(znode *object CLS_DC);
+void zend_do_pop_object(znode *object CLS_DC);
 
-void do_fetch_constant(znode *result, znode *constant_name, int mode CLS_DC);
 
-void do_shell_exec(znode *result, znode *cmd CLS_DC);
+void zend_do_begin_new_object(znode *new_token, znode *class_name CLS_DC);
+void zend_do_end_new_object(znode *result, znode *class_name, znode *new_token, znode *argument_list CLS_DC);
 
-void do_init_array(znode *result, znode *expr, znode *offset, int is_ref CLS_DC);
-void do_add_array_element(znode *result, znode *expr, znode *offset, int is_ref CLS_DC);
-void do_add_static_array_element(znode *result, znode *offset, znode *expr);
-void do_list_init(CLS_D);
-void do_list_end(znode *result, znode *expr CLS_DC);
-void do_add_list_element(znode *element CLS_DC);
-void do_new_list_begin(CLS_D);
-void do_new_list_end(CLS_D);
+void zend_do_fetch_constant(znode *result, znode *constant_name, int mode CLS_DC);
 
-void do_cast(znode *result, znode *expr, int type CLS_DC);
-void do_include_or_eval(int type, znode *result, znode *op1 CLS_DC);
+void zend_do_shell_exec(znode *result, znode *cmd CLS_DC);
 
-void do_unset(znode *variable CLS_DC);
-void do_isset_or_isempty(int type, znode *result, znode *variable CLS_DC);
+void zend_do_init_array(znode *result, znode *expr, znode *offset, int is_ref CLS_DC);
+void zend_do_add_array_element(znode *result, znode *expr, znode *offset, int is_ref CLS_DC);
+void zend_do_add_static_array_element(znode *result, znode *offset, znode *expr);
+void zend_do_list_init(CLS_D);
+void zend_do_list_end(znode *result, znode *expr CLS_DC);
+void zend_do_add_list_element(znode *element CLS_DC);
+void zend_do_new_list_begin(CLS_D);
+void zend_do_new_list_end(CLS_D);
 
-void do_foreach_begin(znode *foreach_token, znode *array, znode *open_brackets_token, znode *as_token, int variable CLS_DC);
-void do_foreach_cont(znode *value, znode *key, znode *as_token CLS_DC);
-void do_foreach_end(znode *foreach_token, znode *open_brackets_token CLS_DC);
+void zend_do_cast(znode *result, znode *expr, int type CLS_DC);
+void zend_do_include_or_eval(int type, znode *result, znode *op1 CLS_DC);
 
-void do_declare_begin(CLS_D);
-void do_declare_stmt(znode *var, znode *val CLS_DC);
-void do_declare_end(CLS_D);
+void zend_do_unset(znode *variable CLS_DC);
+void zend_do_isset_or_isempty(int type, znode *result, znode *variable CLS_DC);
 
-void do_end_heredoc(CLS_D);
+void zend_do_foreach_begin(znode *foreach_token, znode *array, znode *open_brackets_token, znode *as_token, int variable CLS_DC);
+void zend_do_foreach_cont(znode *value, znode *key, znode *as_token CLS_DC);
+void zend_do_foreach_end(znode *foreach_token, znode *open_brackets_token CLS_DC);
+
+void zend_do_declare_begin(CLS_D);
+void zend_do_declare_stmt(znode *var, znode *val CLS_DC);
+void zend_do_declare_end(CLS_D);
+
+void zend_do_end_heredoc(CLS_D);
 
 void zend_do_exit(znode *result, znode *message CLS_DC);
 
-void do_begin_silence(znode *strudel_token CLS_DC);
-void do_end_silence(znode *strudel_token CLS_DC);
+void zend_do_begin_silence(znode *strudel_token CLS_DC);
+void zend_do_end_silence(znode *strudel_token CLS_DC);
 
-void do_begin_qm_op(znode *cond, znode *qm_token CLS_DC);
-void do_qm_true(znode *true_value, znode *qm_token, znode *colon_token CLS_DC);
-void do_qm_false(znode *result, znode *false_value, znode *qm_token, znode *colon_token CLS_DC);
+void zend_do_begin_qm_op(znode *cond, znode *qm_token CLS_DC);
+void zend_do_qm_true(znode *true_value, znode *qm_token, znode *colon_token CLS_DC);
+void zend_do_qm_false(znode *result, znode *false_value, znode *qm_token, znode *colon_token CLS_DC);
 
-void do_extended_info(CLS_D);
-void do_extended_fcall_begin(CLS_D);
-void do_extended_fcall_end(CLS_D);
+void zend_do_extended_info(CLS_D);
+void zend_do_extended_fcall_begin(CLS_D);
+void zend_do_extended_fcall_end(CLS_D);
 
-void do_ticks(CLS_D);
+void zend_do_ticks(CLS_D);
 
 ZEND_API void function_add_ref(zend_function *function);
 
