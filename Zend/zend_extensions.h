606a1e3aece293c6ea3b5316526d50e5693b4f5f
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-11-20 12:59:56 +0000

    Update API number
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@36207 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO	20001002	
+#define ZEND_EXTENSION_API_NO	20001120	
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
