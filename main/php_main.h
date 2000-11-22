5a9d4c64eb75a520df8ad4a9953125c7de8fbe05
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-11-22 04:23:26 +0000

    Return the exit status from php_execute_script()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@36359 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_main.h
+++ main/php_main.h
@@ -40,7 +40,7 @@ PHPAPI int php_startup_extensions(zend_module_entry **ptr, int count);
 PHPAPI int php_global_startup_extensions(zend_module_entry **ptr, int count);
 PHPAPI int php_global_shutdown_extensions(zend_module_entry **ptr, int count);
 
-PHPAPI void php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_DC);
+PHPAPI int php_execute_script(zend_file_handle *primary_file CLS_DC ELS_DC PLS_DC);
 PHPAPI int php_handle_special_queries(SLS_D PLS_DC);
 PHPAPI int php_lint_script(zend_file_handle *file CLS_DC ELS_DC PLS_DC);
 
