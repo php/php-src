e5a997ff59e5b769bf3125fce26a545a5a3a2c50
foobar <sniper@php.net>
2000-11-28 09:13:22 +0000

    @- Fixed a bug which made readdir() unusable in some systems. (Jani) # Bug report: #7390

--- main/reentrancy.c
+++ main/reentrancy.c
@@ -104,15 +104,15 @@ PHPAPI int php_readdir_r(DIR *dirp, struct dirent *entry,
 	errno = 0;
 
 	ret = readdir_r(dirp, entry);
-	
-	if (ret == 0)
-		*result = entry;
-	else if (errno == 0) {
+
+	if (!ret || errno != 0) {
 		*result = NULL;
-		ret = 0;
+	} else {
+		*result = entry;
 	}
 
-	return ret;
+	return errno;
+
 #else
 	struct dirent *ptr;
 	int ret = 0;
