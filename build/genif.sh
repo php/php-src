544640e305dff4e31d343a211d7fe3f75dda3e33
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2000-12-20 17:21:37 +0000

    Stop automatically passing vars to AC_SUBST through PHP_SUBST.
    There are some variables which are needed for Makefiles and files
    created by AC_OUTPUT; we use PHP_SUBST_OLD for those now.
    
    genif.sh was moved to an appropiate place.
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@38114 c90b9560-bf6c-de11-be94-00142212c4b1

--- build/genif.sh
+++ build/genif.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: genif.sh,v 1.10 2000-12-20 16:29:07 sas Exp $
+# $Id: genif.sh,v 1.1 2000-12-20 17:21:37 sas Exp $
 # replacement for genif.pl
 
 infile="$1"
@@ -27,7 +27,7 @@ for ext in ${1+"$@"} ; do
 	header_list="$header_list ext/$ext/*.h"
 done
 
-includes=`$awk -f $srcdir/build/print_include.awk $header_list`
+includes=`$awk -f ./build/print_include.awk $header_list`
 
 cd $olddir
 
