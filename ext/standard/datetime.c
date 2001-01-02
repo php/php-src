eb9c1c9dc78bdb350260150d2811e6ccb8a42302
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-01-02 20:26:51 +0000

    Fix date('r') overflow @Fix date('r') overflow
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@38455 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/datetime.c
+++ ext/standard/datetime.c
@@ -214,7 +214,7 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 	time_t the_time;
 	struct tm *ta, tmbuf;
 	int i, size = 0, length, h, beat;
-	char tmp_buff[16];
+	char tmp_buff[32];
 
 	switch(ZEND_NUM_ARGS()) {
 	case 1:
