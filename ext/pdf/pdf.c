833a084f78dfaebaf650104cbb0daf6fe625ae32
dbeu <dbeu@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-01-03 11:41:31 +0000

    fix bug 8341 & 8394
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@38508 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -2339,7 +2339,9 @@ PHP_FUNCTION(pdf_open_gif) {
 		RETURN_FALSE;
 	}
 
+#ifdef VIRTUAL_DIR
 	virtual_filepath((*arg2)->value.str.val, &image);
+#endif
 
 	pdf_image = PDF_open_image_file(pdf, "gif", image, "", 0);
 
@@ -2376,7 +2378,9 @@ PHP_FUNCTION(pdf_open_jpeg) {
 		RETURN_FALSE;
 	}
 
+#ifdef VIRTUAL_DIR
 	virtual_filepath((*arg2)->value.str.val, &image);
+#endif
 
 	pdf_image = PDF_open_image_file(pdf, "jpeg", image, "", 0);
 
@@ -2413,7 +2417,9 @@ PHP_FUNCTION(pdf_open_png) {
 		RETURN_FALSE;
 	}
 
+#ifdef VIRTUAL_DIR
 	virtual_filepath((*arg2)->value.str.val, &image);
+#endif
 
 	pdf_image = PDF_open_image_file(pdf, "png", image, "", 0);
 
@@ -2450,7 +2456,9 @@ PHP_FUNCTION(pdf_open_tiff) {
 		RETURN_FALSE;
 	}
 
+#ifdef VIRTUAL_DIR
 	virtual_filepath((*arg2)->value.str.val, &image);
+#endif
 
 	pdf_image = PDF_open_image_file(pdf, "tiff", image, "", 0);
 
@@ -2488,7 +2496,9 @@ PHP_FUNCTION(pdf_open_image_file) {
 		RETURN_FALSE;
 	}
 
+#ifdef VIRTUAL_DIR
 	virtual_filepath((*arg3)->value.str.val, &image);
+#endif
 
 	pdf_image = PDF_open_image_file(pdf, (*arg2)->value.str.val, image, "", 0);
 
