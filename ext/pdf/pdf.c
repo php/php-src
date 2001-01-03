d21c33f14c78282866e32e25d5454bd2ade13bb4
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-01-03 16:53:00 +0000

    I missed this function..
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@38533 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -600,12 +600,11 @@ PHP_FUNCTION(pdf_get_value)
 	convert_to_string_ex(argv[1]);
 
 	if(0 == (strncmp(Z_STRVAL_PP(argv[1]), "image", 5))) {
-		int pdf_image;
-		if(argc < 3)
-			WRONG_PARAM_COUNT;
+		int *pdf_image;
+		if(argc < 3) WRONG_PARAM_COUNT;
 
-		ZEND_FETCH_RESOURCE(pdf_image, int, argv[0], -1, "pdf image", le_pdf_image);
-		value = PDF_get_value(pdf, Z_STRVAL_PP(argv[1]), pdf_image);
+		ZEND_FETCH_RESOURCE(pdf_image, int *, argv[2], -1, "pdf image", le_pdf_image);
+		value = PDF_get_value(pdf, Z_STRVAL_PP(argv[1]), *pdf_image);
 
 	} else {
 
