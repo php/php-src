43a7643453bf9ef0b32f4505781a45b7b393bf71
Zeev Suraski <zeev@php.net>
2001-01-03 22:45:11 +0000

    Fix last couple of problematic execution paths that could corrupt the INI values

--- sapi/apache/mod_php4.c
+++ sapi/apache/mod_php4.c
@@ -475,13 +475,20 @@ int send_php(request_rec *r, int display_source_mode, char *filename)
 
 	/* Apache 1.2 has a more complex mechanism for reading POST data */
 #if MODULE_MAGIC_NUMBER > 19961007
-	if ((retval = setup_client_block(r, REQUEST_CHUNKED_ERROR)))
+	if ((retval = setup_client_block(r, REQUEST_CHUNKED_ERROR))) {
+		if (setjmp(EG(bailout))==0) {
+			zend_ini_deactivate(ELS_C);
+		}
 		return retval;
+	}
 #endif
 
 	if (AP(last_modified)) {
 #if MODULE_MAGIC_NUMBER < 19970912
 		if ((retval = set_last_modified(r, r->finfo.st_mtime))) {
+			if (setjmp(EG(bailout))==0) {
+				zend_ini_deactivate(ELS_C);
+			}
 			return retval;
 		}
 #else
