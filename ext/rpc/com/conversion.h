dee2fd1ec42046c27bb07481f30d60bca6e3b889
Harald Radi <phanto@php.net>
2001-01-29 21:54:46 +0000

    use *_EXTERN_C() and PHP_WIN32

--- ext/rpc/com/conversion.h
+++ ext/rpc/com/conversion.h
@@ -1,18 +1,14 @@
 #ifndef CONVERSION_H
 #define CONVERSION_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+BEGIN_EXTERN_C()
 
-void php_pval_to_variant(pval *pval_arg, VARIANT *var_arg);
-void php_pval_to_variant_ex(pval *pval_arg, VARIANT *var_arg, pval *pval_type);
-void php_variant_to_pval(VARIANT *var_arg, pval *pval_arg, int persistent);
-OLECHAR *php_char_to_OLECHAR(char *C_str, uint strlen);
-char *php_OLECHAR_to_char(OLECHAR *unicode_str, uint *out_length, int persistent);
+extern void php_pval_to_variant(pval *pval_arg, VARIANT *var_arg);
+extern void php_pval_to_variant_ex(pval *pval_arg, VARIANT *var_arg, pval *pval_type);
+extern void php_variant_to_pval(VARIANT *var_arg, pval *pval_arg, int persistent);
+extern OLECHAR *php_char_to_OLECHAR(char *C_str, uint strlen);
+extern char *php_OLECHAR_to_char(OLECHAR *unicode_str, uint *out_length, int persistent);
 
-#ifdef __cplusplus
-}
-#endif
+END_EXTERN_C()
 
 #endif
