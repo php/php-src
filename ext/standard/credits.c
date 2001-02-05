d37d7a044e3924154850543a821f85291c3bc816
jmoore <jmoore@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-02-05 10:18:14 +0000

    Adding Sebastian Bergmann to QA team list.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@40747 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/credits.c
+++ ext/standard/credits.c
@@ -94,7 +94,7 @@ PHPAPI void php_print_credits(int flag)
 	if (flag & PHP_CREDITS_QA) {
 		php_info_print_table_start();
 		php_info_print_table_header(1, "PHP Quality Assurance Team");
-		php_info_print_table_row(1, "Andre Langhorst, Hellekin O. Wolf, Jalal Pushman, James Moore, Jani Taskinen, Joey Smith, Olivier Cahagne, Phil Driscoll, Zak Greant");
+		php_info_print_table_row(1, "Andre Langhorst, Hellekin O. Wolf, Jalal Pushman, James Moore, Jani Taskinen, Joey Smith, Olivier Cahagne, Phil Driscoll, Sebastian Bergmann, Zak Greant");
 		php_info_print_table_end();
 	}
 
