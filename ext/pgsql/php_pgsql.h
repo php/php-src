abe1411075949d1003ca678d1bb80bf75daf39d9
thies <thies@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-02-13 18:28:24 +0000

    @- PostgreSQL now does a rollback at the end of a request on every @  persistent connection. This is done by doing an "empty" transaction @  on the connection (This was advised by someone from the PostgreSQL @  core-team). If you leave transactions open on your page you will see a @  "NOTICE:  BEGIN: already a transaction in progress" message in your @  apache error_log. This message is created by the PostgreSQL libs - we can @  do nothing about it. (Thies)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@41182 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/php_pgsql.h
+++ ext/pgsql/php_pgsql.h
@@ -48,6 +48,7 @@ extern zend_module_entry pgsql_module_entry;
 PHP_MINIT_FUNCTION(pgsql);
 PHP_MSHUTDOWN_FUNCTION(pgsql);
 PHP_RINIT_FUNCTION(pgsql);
+PHP_RSHUTDOWN_FUNCTION(pgsql);
 PHP_MINFO_FUNCTION(pgsql);
 PHP_FUNCTION(pg_connect);
 PHP_FUNCTION(pg_pconnect);
