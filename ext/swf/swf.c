2f0238dc4e439e10611908957642a09c67c5ac08
Sterling Hughes <sterling@php.net>
2001-02-15 16:20:13 +0000

    Change e-mail address
    # Can I get someone to change my cvsusers e-mail address to
    # sterling@designmultimedia.com?

--- ext/swf/swf.c
+++ ext/swf/swf.c
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Author: Sterling Hughes <Sterling.Hughes@pentap.net>                 |
+   | Author: Sterling Hughes <sterling@php.net>                           |
    +----------------------------------------------------------------------+
 */
 
