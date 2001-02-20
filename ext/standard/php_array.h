06eaca4cb3ae22a17cb9b6b7e3c0cd569a080f70
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-02-20 14:23:03 +0000

    Rename to array_search().
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@41551 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_array.h
+++ ext/standard/php_array.h
@@ -49,7 +49,7 @@ PHP_FUNCTION(key);
 PHP_FUNCTION(min);
 PHP_FUNCTION(max);
 PHP_FUNCTION(in_array);
-PHP_FUNCTION(search_array);
+PHP_FUNCTION(array_search);
 PHP_FUNCTION(extract);
 PHP_FUNCTION(compact);
 PHP_FUNCTION(range);
