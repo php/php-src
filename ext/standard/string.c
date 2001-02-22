890340c5f2937f642dde5e874054d5e27175e6d2
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-02-22 10:36:40 +0000

    Safer bin2hex
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@41719 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -96,7 +96,7 @@ static char *php_bin2hex(const unsigned char *old, const size_t oldlen, size_t *
 	unsigned char *result = NULL;
 	size_t i, j;
 
-	result = (char *) emalloc(oldlen * 2 * sizeof(char));
+	result = (char *) emalloc(oldlen * 2 * sizeof(char) + 1);
 	if(!result) {
 		return result;
 	}
@@ -105,6 +105,7 @@ static char *php_bin2hex(const unsigned char *old, const size_t oldlen, size_t *
 		result[j++] = hexconvtab[old[i] >> 4];
 		result[j++] = hexconvtab[old[i] & 15];
 	}
+	result[j] = '\0';
 
 	if(newlen) *newlen = oldlen * 2 * sizeof(char);
 
