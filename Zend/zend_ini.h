31775e970678662070a46b6075df782219d84be3
Zeev Suraski <zeev@php.net>
2001-03-07 17:05:03 +0000

    Add missing #define's

--- Zend/zend_ini.h
+++ Zend/zend_ini.h
@@ -26,6 +26,36 @@
 
 #define ZEND_INI_ALL (ZEND_INI_USER|ZEND_INI_PERDIR|ZEND_INI_SYSTEM)
 
+#ifndef XtOffsetOf
+# if defined(CRAY) || (defined(__arm) && !defined(LINUX))
+# ifdef __STDC__
+# define XtOffset(p_type,field) _Offsetof(p_type,field)
+# else
+# ifdef CRAY2
+# define XtOffset(p_type,field) \
+    (sizeof(int)*((unsigned int)&(((p_type)NULL)->field)))
+
+# else /* !CRAY2 */
+
+# define XtOffset(p_type,field) ((unsigned int)&(((p_type)NULL)->field))
+
+# endif /* !CRAY2 */
+# endif /* __STDC__ */
+# else /* ! (CRAY || __arm) */
+
+# define XtOffset(p_type,field) \
+    ((long) (((char *) (&(((p_type)NULL)->field))) - ((char *) NULL)))
+
+# endif /* !CRAY */
+
+# ifdef offsetof
+# define XtOffsetOf(s_type,field) offsetof(s_type,field)
+# else
+# define XtOffsetOf(s_type,field) XtOffset(s_type*,field)
+# endif
+
+#endif
+
 typedef struct _zend_ini_entry zend_ini_entry;
 
 #define ZEND_INI_MH(name) int name(zend_ini_entry *entry, char *new_value, uint new_value_length, void *mh_arg1, void *mh_arg2, void *mh_arg3, int stage)
