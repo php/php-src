01008bb03cf3053e91e9377f403fd6ef8fe900d5
Jon Parise <jon@php.net>
2001-03-15 05:07:31 +0000

    Whitespace (spaces to four-column tabs)

--- ext/standard/credits.h
+++ ext/standard/credits.h
@@ -32,7 +32,7 @@
 #define PHP_CREDITS_DOCS			(1<<4)
 #define PHP_CREDITS_FULLPAGE		(1<<5)
 #define PHP_CREDITS_QA				(1<<6)
-#define PHP_CREDITS_WEB             (1<<7)
+#define PHP_CREDITS_WEB				(1<<7)
 #define PHP_CREDITS_ALL				0xFFFFFFFF
 
 #endif /* HAVE_CREDITS_DEFS */
