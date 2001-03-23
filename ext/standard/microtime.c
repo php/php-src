7f48c7adf57635f7c1b29e5d77c60a32569ebef8
Stephen van Egmond <svanegmond@php.net>
2001-03-23 04:36:29 +0000

    Simple patch to deal with lack of fields in the BeOS implementation of rusage.

--- ext/standard/microtime.c
+++ ext/standard/microtime.c
@@ -116,7 +116,7 @@ PHP_FUNCTION(getrusage)
 	array_init(return_value);
 #define PHP_RUSAGE_PARA(a) \
 		add_assoc_long(return_value, #a, usg.a)
-#ifndef _OSD_POSIX /* BS2000 has only a few fields in the rusage struct */
+#if !defined( _OSD_POSIX) && !defined(__BEOS__) /* BS2000 has only a few fields in the rusage struct */
 	PHP_RUSAGE_PARA(ru_oublock);
 	PHP_RUSAGE_PARA(ru_inblock);
 	PHP_RUSAGE_PARA(ru_msgsnd);
