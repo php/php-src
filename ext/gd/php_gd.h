ab4beb82fa12ae0bae0b73a01cfbf6f623fd287d
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-04-06 18:01:52 +0000

    Killed some compile warnings.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@44970 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/php_gd.h
+++ ext/gd/php_gd.h
@@ -143,13 +143,6 @@ PHP_FUNCTION(jpeg2wbmp);
 PHP_FUNCTION(png2wbmp);
 PHP_FUNCTION(image2wbmp);
 
-
-gdImagePtr _php_image_create_from_string ( zval **Data, char *tn, gdImagePtr (*ioctx_func_p)() );
-static void _php_image_create_from(INTERNAL_FUNCTION_PARAMETERS, int image_type, char *tn, gdImagePtr (*func_p)(), gdImagePtr (*ioctx_func_p)());
-static void _php_image_output(INTERNAL_FUNCTION_PARAMETERS, int image_type, char *tn, void (*func_p)());
-static int _php_image_type ( char data[8] );
-static void _php_image_convert(INTERNAL_FUNCTION_PARAMETERS, int image_type);
-
 PHPAPI int phpi_get_le_gd(void);
 
 /* This is missing from gd.h */
