35890db0a1be8c3539cc99b4813cbd98c0f007f7
Sterling Hughes <sterling@php.net>
2001-04-26 00:13:26 +0000

    Add an experimental version of the new XSLT extension.  This is by no means to be used on anything production, if you want to test it, beware, it may or may not work (feel free to complain to me if it doesn't work, or shower praise if it does work)...
    Mainly in PHP's CVS so that work on the other backends can take place.
    The api (function_entry) is what I'm thinking of for all backends.  Every
    backend simply needs to define these functions, I'll send an e-mail describing
    all this in a bit....

---
+++ ext/xslt/config.m4
@@ -0,0 +1,81 @@
+dnl config.m4 for extension xslt
+dnl +------------------------------------------------------------------------------+
+dnl |  This is where the magic of the extension reallly is.  Depending on what     |
+dnl |  backend the user chooses, this script performs the magic                    |
+dnl +------------------------------------------------------------------------------+
+dnl   $Id$
+
+PHP_ARG_ENABLE(xslt, whether to enable xslt support,
+[  --enable-xslt           Enable xslt support])
+
+PHP_ARG_WITH(xslt-sablot, whether to enable the sablotron backend,
+[  --with-xslt-sablot      Enable the sablotron backend])
+
+PHP_ARG_WITH(expat-dir, for Sablotron XSL support,
+[  --with-expat-dir=DIR    Sablotron: libexpat dir for Sablotron 0.50])
+
+if test "$PHP_XSLT" != "no"; then
+	if test "$PHP_XSLT_SABLOT" != "no"; then
+		XSLT_CHECK_DIR=$PHP_XSLT_SABLOT
+		XSLT_TEST_FILE=/include/sablot.h
+		XSLT_BACKEND_NAME=Sablotron
+		XSLT_LIBNAME=sablot
+	fi
+
+	condition="$XSLT_CHECK_DIR$XSLT_TEST_FILE"
+
+	if test -r $condition; then
+		XSLT_DIR=$XSLT_CHECK_DIR
+	else
+		AC_MSG_CHECKING(for $XSLT_BACKEND_NAME libraries in the default path)
+		for i in /usr /usr/local; do
+			condition="$i$XSLT_TEST_FILE"
+			if test -r $condition; then
+				XSLT_DIR=$i
+				AC_MSG_RESULT(found $XSLT_BACKEND_NAME in $i)
+			fi
+		done
+	fi
+
+	if test -z "$XSLT_DIR"; then
+		AC_MSG_RESULT(not found)
+		AC_MSG_ERROR(Please re-install the $XSLT_BACKEND_NAME distribution)
+	fi
+					
+
+	if test -z "$XSLT_DIR"; then
+		AC_MSG_RESULT(not found)
+		AC_MSG_ERROR(Please re-install the $XSLT_BACKEND distribution)
+	fi
+
+	PHP_ADD_INCLUDE($XSLT_DIR/include)
+
+	PHP_SUBST(XSLT_SHARED_LIBADD)
+	PHP_ADD_LIBRARY_WITH_PATH($XSLT_LIBNAME, $XSLT_DIR/lib, XSLT_SHARED_LIBADD)
+
+	if test "$PHP_XSLT_SABLOT" != "no"; then
+		if test -z "$PHP_EXPAT_DIR"; then
+			PHP_EXPAT_DIR=""
+		fi
+
+		found_expat=no
+		for i in $PHP_EXPAT_DIR $XSLT_DIR; do
+			if test -f $i/lib/libexpat.a -o -f $i/lib/libexpat.so; then
+				AC_DEFINE(HAVE_LIBEXPAT2, 1, [ ])
+				PHP_ADD_LIBRARY_WITH_PATH(expat, $i/lib)
+				found_expat=yes
+			fi
+		done
+
+		if test "$found_expat" = "no"; then
+			PHP_ADD_LIBRARY(xmlparse)
+			PHP_ADD_LIBRARY(xmltok)
+		fi
+
+		AC_DEFINE(HAVE_SABLOT, 1, [ ])
+		AC_CHECK_LIB(sablot, SablotSetEncoding, AC_DEFINE(HAVE_SABLOT_SET_ENCODING, 1, [ ]))
+	fi
+
+	AC_DEFINE(HAVE_XSLT, 1, [ ])
+	PHP_EXTENSION(xslt, $ext_shared)
+fi
---
+++ ext/xslt/php_sablot.h
@@ -0,0 +1,123 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999, 2000, 2001 The PHP Group             |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Sterling Hughes <sterling@php.net>                          |
+   +----------------------------------------------------------------------+
+ */
+
+#ifndef PHP_XSLT_H
+#define PHP_XSLT_H
+
+#include "php.h"
+
+#if HAVE_SABLOT
+
+#include "php_xslt.h"
+
+#include <sablot.h>
+
+extern zend_module_entry xslt_module_entry;
+#define phpext_xslt_ptr &xslt_module_entry
+
+#ifdef PHP_WIN32
+#define PHP_XSLT_API __declspec(dllexport)
+#else
+#define PHP_XSLT_API
+#endif
+
+#define XSLT_SCHEME(handle)    ((handle)->handlers->scheme)
+#define XSLT_SAX(handle)       ((handle)->handlers->sax)
+#define XSLT_ERROR(handle)     ((handle)->handlers->error)
+
+#define XSLT_PROCESSOR(handle) ((handle)->processor.ptr)
+
+#define XSLT_ERRNO(handle)     ((handle)->err->no)
+#define XSLT_ERRSTR(handle)    ((handle)->err->str)
+#define XSLT_LOG(handle)       ((handle)->err->log)
+
+PHP_MINIT_FUNCTION(xslt);
+PHP_MINFO_FUNCTION(xslt);
+
+PHP_FUNCTION(xslt_create);
+PHP_FUNCTION(xslt_set_sax_handlers);
+PHP_FUNCTION(xslt_set_scheme_handlers);
+PHP_FUNCTION(xslt_set_error_handler);
+PHP_FUNCTION(xslt_set_base);
+PHP_FUNCTION(xslt_set_encoding);
+PHP_FUNCTION(xslt_set_log);
+PHP_FUNCTION(xslt_process);
+PHP_FUNCTION(xslt_error);
+PHP_FUNCTION(xslt_errno);
+PHP_FUNCTION(xslt_free);
+
+struct scheme_handlers {
+	struct xslt_function *get_all;
+	struct xslt_function *open;
+	struct xslt_function *get;
+	struct xslt_function *put;
+	struct xslt_function *close;
+};
+
+struct sax_handlers {
+	struct xslt_function *doc_start;
+	struct xslt_function *element_start;
+	struct xslt_function *element_end;
+	struct xslt_function *namespace_start;
+	struct xslt_function *namespace_end;
+	struct xslt_function *comment;
+	struct xslt_function *pi;
+	struct xslt_function *characters;
+	struct xslt_function *doc_end;
+};
+
+struct xslt_handlers {
+	struct scheme_handlers   scheme;
+	struct sax_handlers      sax;
+	struct xslt_function    *error;
+};
+
+struct xslt_processor {
+	SablotHandle ptr;
+	long         idx;
+};
+
+struct xslt_log {
+	char *path;
+	int   fd;
+};
+
+struct xslt_error {
+	struct xslt_log  log;
+	char            *str;
+	int              no;
+};
+
+typedef struct {
+	struct xslt_handlers  *handlers;
+	struct xslt_processor  processor;
+	struct xslt_error     *err;
+} php_xslt;
+
+#else
+#define phpext_xslt_ptr NULL
+#endif
+
+#endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ */
