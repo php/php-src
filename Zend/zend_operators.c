ce1a38ab2c02973cd1a4a04ba34651c145d29031
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-04-29 09:21:38 +0000

    - Previous patch for too early freeing of resources seemed to have worked. - Clean it up a bit.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@46062 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -167,12 +167,7 @@ ZEND_API void convert_scalar_to_number(zval *op)
 					break;													\
 				}															\
 			case IS_BOOL:													\
-				(holder).value.lval = (op)->value.lval;						\
-				(holder).type = IS_LONG;									\
-				(op) = &(holder);											\
-				break;														\
 			case IS_RESOURCE:												\
-				/*zend_list_delete((op)->value.lval);*/							\
 				(holder).value.lval = (op)->value.lval;						\
 				(holder).type = IS_LONG;									\
 				(op) = &(holder);											\
@@ -207,11 +202,8 @@ ZEND_API void convert_scalar_to_number(zval *op)
 			case IS_OBJECT:											\
 				(holder).value.lval = (zend_hash_num_elements((op)->value.obj.properties)?1:0);	\
 				break;												\
-			case IS_RESOURCE:										\
-				/*zend_list_delete((op)->value.lval);*/					\
-				(holder).value.lval = (op)->value.lval;				\
-				break;												\
 			case IS_BOOL:											\
+			case IS_RESOURCE:										\
 				(holder).value.lval = (op)->value.lval;				\
 				break;												\
 			default:												\
@@ -233,8 +225,6 @@ ZEND_API void convert_scalar_to_number(zval *op)
 				(holder).value.lval = 0;							\
 				break;												\
 			case IS_RESOURCE:										\
-				/*zend_list_delete((op)->value.lval);*/					\
-				/* break missing intentionally */					\
 			case IS_LONG:											\
 				(holder).value.lval = ((op)->value.lval ? 1 : 0);	\
 				break;												\
