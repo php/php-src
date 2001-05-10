4afa93f716fe12178e676ba3e01e37647c27f4ad
Zeev Suraski <zeev@php.net>
2001-05-10 14:13:18 +0000

    @- Fixed a possible crash in the PHP CGI when no input file is @  specified (Zeev) Fixed bug 7822

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -692,8 +692,7 @@ any .htaccess restrictions anywhere on your site you can leave doc_root undefine
 	}
 
 	if (cgi && !file_handle.handle.fp) {
-		file_handle.handle.fp = VCWD_FOPEN(argv0, "rb");
-		if(!file_handle.handle.fp) {
+		if(!argv0 || !(file_handle.handle.fp = VCWD_FOPEN(argv0, "rb"))) {
 			PUTS("No input file specified.\n");
 			php_request_shutdown((void *) 0);
 			php_module_shutdown();
