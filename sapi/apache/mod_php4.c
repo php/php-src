c93964a289da3eef26f563d0bbab10836370c33c
sterling <sterling@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-05-16 18:28:18 +0000

    # Revert the SLS_FETCH() ;)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@47611 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache/mod_php4.c
+++ sapi/apache/mod_php4.c
@@ -295,11 +295,8 @@ static void php_apache_request_shutdown(void *dummy)
 
 static int php_apache_sapi_activate(SLS_D)
 {
-	request_rec *r; 
-	SLS_FETCH();
+	request_rec *r = (request_rec *) SG(server_context); 
 	APLS_FETCH();
-
-	r = (request_rec *) SG(server_context);
 
 	/*
 	 * For the Apache module version, this bit of code registers a cleanup
