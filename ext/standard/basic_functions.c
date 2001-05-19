16b94858b6acf8e45e35d0e69b12e9ed462d3a42
foobar <sniper@php.net>
2001-05-19 22:21:40 +0000

    Fixed bug #10964

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -237,7 +237,10 @@ function_entry basic_functions[] = {
 	PHP_FE(mt_rand,									NULL)
 	PHP_FE(mt_srand,								NULL)
 	PHP_FE(mt_getrandmax,							NULL)
+
+#if HAVE_GETSERVBYNAME
 	PHP_FE(getservbyname, NULL)
+#endif
 #if HAVE_GETSERVBYPORT
 	PHP_FE(getservbyport, NULL)
 #endif
@@ -247,6 +250,7 @@ function_entry basic_functions[] = {
 #if HAVE_GETPROTOBYNUMBER
 	PHP_FE(getprotobynumber, NULL)
 #endif
+
 	PHP_FE(gethostbyaddr,							NULL)
 	PHP_FE(gethostbyname,							NULL)
 	PHP_FE(gethostbynamel,							NULL)
@@ -2256,6 +2260,7 @@ PHP_FUNCTION(ignore_user_abort)
 
 /* {{{ proto int getservbyname(string service, string protocol)
    Returns port associated with service. Protocol must be "tcp" or "udp". */
+#if HAVE_GETSERVBYNAME
 PHP_FUNCTION(getservbyname)
 {
 	pval **name,**proto;
@@ -2274,6 +2279,7 @@ PHP_FUNCTION(getservbyname)
 
 	RETURN_LONG(ntohs(serv->s_port));
 }
+#endif
 /* }}} */
 
 
