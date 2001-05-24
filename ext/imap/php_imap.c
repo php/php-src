98b2882ce66c508f5906a3f42522a8b1ab163ea0
chagenbu <chagenbu@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-05-24 03:06:10 +0000

    make it possible to set the CHARSET parameter with imap_mail_compose() (Johan Ekenberg <johan@ekenberg.se>)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@48332 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -3266,6 +3266,14 @@ PHP_FUNCTION(imap_mail_compose)
 			convert_to_long_ex(pvalue);
 			bod->encoding = (short) Z_LVAL_PP(pvalue);
 		}
+		if (zend_hash_find(Z_ARRVAL_PP(data), "charset", sizeof("charset"), (void **) &pvalue)== SUCCESS) {
+			convert_to_string_ex(pvalue);
+			tmp_param = mail_newbody_parameter();
+			tmp_param->value = cpystr(Z_STRVAL_PP(pvalue));
+			tmp_param->attribute = "CHARSET";
+			tmp_param->next = bod->parameter;
+			bod->parameter = tmp_param;
+		}
 		if (zend_hash_find(Z_ARRVAL_PP(data), "subtype", sizeof("subtype"), (void **) &pvalue)== SUCCESS) {
 			convert_to_string_ex(pvalue);
 			bod->subtype = cpystr(Z_STRVAL_PP(pvalue));
@@ -3343,6 +3351,14 @@ PHP_FUNCTION(imap_mail_compose)
 			if (zend_hash_find(Z_ARRVAL_PP(data), "encoding", sizeof("encoding"), (void **) &pvalue)== SUCCESS) {
 				convert_to_long_ex(pvalue);
 				bod->encoding = (short) Z_LVAL_PP(pvalue);
+			}
+			if (zend_hash_find(Z_ARRVAL_PP(data), "charset", sizeof("charset"), (void **) &pvalue)== SUCCESS) {
+				convert_to_string_ex(pvalue);
+				tmp_param = mail_newbody_parameter();
+				tmp_param->value = cpystr(Z_STRVAL_PP(pvalue));
+				tmp_param->attribute = "CHARSET";
+				tmp_param->next = bod->parameter;
+				bod->parameter = tmp_param;
 			}
 			if (zend_hash_find(Z_ARRVAL_PP(data), "subtype", sizeof("subtype"), (void **) &pvalue)== SUCCESS) {
 				convert_to_string_ex(pvalue);
