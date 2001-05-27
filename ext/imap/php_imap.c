9e75097f587e4fad76edf159459d7cbb1d52c5b1
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-05-27 18:38:59 +0000

    This is not needed. ssl_onceonlyinit() is enough.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@48628 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -451,10 +451,9 @@ PHP_MINIT_FUNCTION(imap)
 	mail_link(&newsdriver);		/* link in the news driver */
 	mail_link(&philedriver);	/* link in the phile driver */
 	auth_link(&auth_log);		/* link in the log authenticator */
-	auth_link(&auth_md5);           /* link in the cram-md5 authenticator */ 
+	auth_link(&auth_md5);       /* link in the cram-md5 authenticator */ 
 #ifdef  HAVE_IMAP_SSL
 	ssl_onceonlyinit ();
-	auth_link (&auth_ssl);		/* link in the ssl authenticator */
 #endif
 #endif
 	mail_link(&dummydriver);	/* link in the dummy driver */
