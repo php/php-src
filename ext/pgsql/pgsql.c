38c8b0c680611ec704814854968e1b11ff36c85e
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-05-27 22:54:54 +0000

    remove ws, kill one compile warning.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@48661 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -122,13 +122,13 @@ static void php_pgsql_set_default_link(int id)
 {   
 	PGLS_FETCH();
 
-        zend_list_addref(id);
+	zend_list_addref(id);
 
-    if (PGG(default_link) != -1) {
-        zend_list_delete(PGG(default_link));
-    }
+	if (PGG(default_link) != -1) {
+		zend_list_delete(PGG(default_link));
+	}
 
-        PGG(default_link) = id;
+	PGG(default_link) = id;
 }
 
 
@@ -162,7 +162,7 @@ _notice_handler(void *arg, const char *message)
 	PGLS_FETCH();
 
 	if (! PGG(ignore_notices)) {
-		php_log_err(message);
+		php_log_err((char *) message);
 		if (PGG(last_notice) != NULL) {
 			efree(PGG(last_notice));
 		}
