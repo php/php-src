491fcd2ef89b1534e1820aa742bfcce749cab534
Daniel Beulshausen <dbeu@php.net>
2001-06-11 15:18:44 +0000

    used in extension(s) as well

--- ext/standard/php_mail.h
+++ ext/standard/php_mail.h
@@ -26,7 +26,7 @@
 PHP_FUNCTION(mail);
 PHP_FUNCTION(ezmlm_hash);
 PHP_MINFO_FUNCTION(mail);
-extern int php_mail(char *to, char *subject, char *message, char *headers, char *extra_cmd);
+PHPAPI extern int php_mail(char *to, char *subject, char *message, char *headers, char *extra_cmd);
 
 #endif
 
