8b43f3222ec7a2956568cd6da055afac89fd3b87
Jason Greene <jason@php.net>
2001-06-12 08:54:25 +0000

    @Added new experimental module pcntl (Process Control) (Jason) Initial Import

---
+++ ext/pcntl/tests/001.php
@@ -0,0 +1,14 @@
+<?php 
+echo "pcntl extension is available";
+/*
+	you can add regression tests for your extension here
+
+  the output of your test code has to be equal to the
+  text in the --EXPECT-- section below for the tests
+  to pass, differences between the output and the
+  expected text are interpreted as failure
+
+	see php4/tests/README for further information on
+  writing regression tests
+*/
+?>
