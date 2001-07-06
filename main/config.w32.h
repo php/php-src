fb30f1b0676f04be9412ddac59b0d16e8a6556ec
Daniel Beulshausen <dbeu@php.net>
2001-07-06 09:53:39 +0000

    enable get*by* networking functions + remove comments

--- main/config.w32.h
+++ main/config.w32.h
@@ -5,12 +5,13 @@
 /* Define if PHP to setup it's own SIGCHLD handler (not needed on Win32) */
 #define PHP_SIGCHILD 0
 
-/* if you have resolv.lib and lib44bsd95.lib you can compile the extra 
-   dns functions located in dns.c.  Set this to 1.  add resolv.lib and
-   lib33bsd95.lib to the project settings, and add the path to the
-   bind include directory to the preprocessor settings.  These libs
-   are availabe in the ntbind distribution */
+/* dns functions found in resolv.lib */
 #define HAVE_LIBBIND 1
+
+#define HAVE_GETSERVBYNAME 1
+#define HAVE_GETSERVBYPORT 1
+#define HAVE_GETPROTOBYNAME 1
+#define HAVE_GETPROTOBYNUMBER 1
 
 /* set to enable bcmath */
 #define WITH_BCMATH 1
@@ -30,6 +31,7 @@
 
 /* set to enable the crypt command */
 #define HAVE_CRYPT 0
+/* #define HAVE_CRYPT_H 1 */
 
 /* set to enable trans sid */
 #define TRANS_SID 1
@@ -80,31 +82,16 @@
 /* Define if you have the link function.  */
 #undef HAVE_LINK
 
-/* Define if you have the lockf function.  */
-/* #undef HAVE_LOCKF */
-
-/* Define if you have the lrand48 function.  */
-/* #undef HAVE_LRAND48 */
-
-/* Define if you have the srand48 function.  */
-/* #undef HAVE_SRAND48 */
-
 /* Define if you have the symlink function.  */
 #undef HAVE_SYMLINK
 
 /* Define if you have the usleep function.  */
 #undef HAVE_USLEEP
 
-
 #define HAVE_GETCWD 1
 #define HAVE_POSIX_READDIR_R 1
 
 #define NEED_ISBLANK 1
-/* ----------------------------------------------------------------
-   The following may be changed and played with right now, but
-   will move to the "don't touch" list below eventualy.
-   ---------------------------------------------------------------*/
-
 
 /* ----------------------------------------------------------------
    The following should never need to be played with
@@ -129,30 +116,14 @@
 /* Define if you have the flock function.  */
 #undef HAVE_FLOCK
 
-/* Define if using alloca.c.  */
-/* #undef C_ALLOCA */
-
-/* Define to one of _getb67, GETB67, getb67 for Cray-2 and Cray-YMP systems.
-   This function is required for alloca.c support on those systems.  */
-/* #undef CRAY_STACKSEG_END */
-
-/* Define to `int' if <sys/types.h> doesn't define.  */
-/* #undef gid_t */
-
 /* Define if you have alloca, as a function or macro.  */
 #define HAVE_ALLOCA 1
-
-/* Define if you have <alloca.h> and it should be used (not on Ultrix).  */
-/* #undef HAVE_ALLOCA_H */
 
 /* Define if you have <sys/time.h> */
 #undef HAVE_SYS_TIME_H
 
 /* Define if you have <signal.h> */
 #define HAVE_SIGNAL_H 1
-
-/* Define if you don't have vprintf but do have _doprnt.  */
-/* #undef HAVE_DOPRNT */
 
 /* Define if your struct stat has st_blksize.  */
 #undef HAVE_ST_BLKSIZE
@@ -169,26 +140,8 @@
 /* Define if you have the vprintf function.  */
 #define HAVE_VPRINTF 1
 
-/* Define to `unsigned' if <sys/types.h> doesn't define.  */
-/* #undef size_t */
-
-/* If using the C implementation of alloca, define if you know the
-   direction of stack growth for your system; otherwise it will be
-   automatically deduced at run-time.
- STACK_DIRECTION > 0 => grows toward higher addresses
- STACK_DIRECTION < 0 => grows toward lower addresses
- STACK_DIRECTION = 0 => direction of growth unknown
- */
-/* #undef STACK_DIRECTION */
-
 /* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
-
-/* Define if your <sys/time.h> declares struct tm.  */
-/* #undef TM_IN_SYS_TIME */
-
-/* Define to `int' if <sys/types.h> doesn't define.  */
-/* #undef uid_t */
 
 /* Define both of these if you want the bundled REGEX library */
 #define REGEX 1
@@ -207,12 +160,6 @@
 
 /* Define if you have the memcpy function.  */
 #define HAVE_MEMCPY 1
-
-/* Define if you have the strlcat function.  */
-/* #undef HAVE_STRLCAT */
-
-/* Define if you have the strlcpy function.  */
-/* #undef HAVE_STRLCPY */
 
 /* Define if you have the memmove function.  */
 #define HAVE_MEMMOVE 1
@@ -256,9 +203,6 @@
 /* Define if you have the utime function.  */
 #define HAVE_UTIME 1
 
-/* Define if you have the <crypt.h> header file.  */
-/* #undef HAVE_CRYPT_H */
-
 /* Define if you have the <dirent.h> header file.  */
 #undef HAVE_DIRENT_H
 
@@ -268,23 +212,14 @@
 /* Define if you have the <grp.h> header file.  */
 #define HAVE_GRP_H 0
 
-/* Define if you have the <ndir.h> header file.  */
-/* #undef HAVE_NDIR_H */
-
 /* Define if you have the <pwd.h> header file.  */
 #define HAVE_PWD_H 1
 
 /* Define if you have the <string.h> header file.  */
 #define HAVE_STRING_H 1
 
-/* Define if you have the <sys/dir.h> header file.  */
-/* #undef HAVE_SYS_DIR_H */
-
 /* Define if you have the <sys/file.h> header file.  */
 #undef HAVE_SYS_FILE_H
-
-/* Define if you have the <sys/ndir.h> header file.  */
-/* #undef HAVE_SYS_NDIR_H */
 
 /* Define if you have the <sys/socket.h> header file.  */
 #undef HAVE_SYS_SOCKET_H
@@ -298,20 +233,11 @@
 /* Define if you have the <unistd.h> header file.  */
 #undef HAVE_UNISTD_H
 
-/* Define if you have the crypt library (-lcrypt).  */
-/* #undef HAVE_LIBCRYPT */
-
 /* Define if you have the dl library (-ldl).  */
 #define HAVE_LIBDL 1
 
 /* Define if you have the m library (-lm).  */
 #define HAVE_LIBM 1
-
-/* Define if you have the nsl library (-lnsl).  */
-/* #undef HAVE_LIBNSL */
-
-/* Define if you have the socket library (-lsocket).  */
-/* #undef HAVE_LIBSOCKET */
 
 /* Define if you have the cuserid function.  */
 #define HAVE_CUSERID 0
