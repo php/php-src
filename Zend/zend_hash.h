2bc09af6957f395f3216aa6022fbba6fb544f5d2
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-07-11 16:16:08 +0000

    - Move inline_zend_hash_func() to header file
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@51237 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_hash.h
+++ Zend/zend_hash.h
@@ -180,7 +180,18 @@ ZEND_API int zend_hash_num_elements(HashTable *ht);
 
 ZEND_API int zend_hash_rehash(HashTable *ht);
 
-/* ZEND_API ulong hashpjw(char *arKey, uint nKeyLength); */
+static inline ulong zend_inline_hash_func(char *arKey, uint nKeyLength)
+{
+	ulong h = 5381;
+	char *arEnd = arKey + nKeyLength;
+
+	while (arKey < arEnd) {
+		h += (h << 5);
+		h ^= (ulong) *arKey++;
+	}
+	return h;
+}
+
 ZEND_API ulong zend_hash_func(char *arKey, uint nKeyLength);
 
 #if ZEND_DEBUG
