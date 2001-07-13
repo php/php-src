ba17fadbfb9423f53a49b8587e5cdace9d518ba4
Thies C. Arntzen <thies@php.net>
2001-07-13 14:35:05 +0000

    @ - Fixed crash in iptcparse() if the supplied data was bogus. (Thies)

--- ext/standard/iptc.c
+++ ext/standard/iptc.c
@@ -351,7 +351,7 @@ PHP_FUNCTION(iptcparse)
 
 		sprintf(key,"%d#%03d",(unsigned int) dataset,(unsigned int) recnum);
 
-		if ((inx + len) > length)
+		if ((len > length) || (inx + len) > length)
 			break;
 
 		if (tagsfound == 0) { /* found the 1st tag - initialize the return array */
