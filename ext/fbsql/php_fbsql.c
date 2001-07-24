c11a9ee5dc640b0fed9e12a042ec4ee931239058
fmk <fmk@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-07-24 18:24:10 +0000

    Add extra check on Delete statements
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@52138 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/fbsql/php_fbsql.c
+++ ext/fbsql/php_fbsql.c
@@ -1582,7 +1582,7 @@ static void phpfbQuery(INTERNAL_FUNCTION_PARAMETERS, char* sql, PHPFBLink* link)
 			if (sR == 1 && md) fbcmdRelease(md);
 			ZVAL_BOOL(return_value, 1)
 		}
-		else if(tp[0] == 'A') 
+		else if(tp[0] == 'A' || tp[0] == 'D') 
 		{
 			if (sR == 1 && md) fbcmdRelease(md);
 			ZVAL_BOOL(return_value, 1)
