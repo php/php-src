acb992f46b0283948596985389f4087a60983148
hirokawa <hirokawa@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-07-29 22:15:41 +0000

    changed for input encoding conversion in win32.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@52517 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/internal_functions_win32.c
+++ main/internal_functions_win32.c
@@ -60,6 +60,7 @@
 #include "ext/xml/php_xml.h"
 #include "ext/wddx/php_wddx.h"
 #include "ext/mysql/php_mysql.h"
+#include "ext/mysql/mbstring.h"
 /* }}} */
 
 /* {{{ php_builtin_extensions[]
@@ -73,6 +74,9 @@ zend_module_entry *php_builtin_extensions[] = {
 	COM_module_ptr,
 	VARIANT_module_ptr,
 	phpext_ftp_ptr,
+#if defined(MBSTR_ENC_TRANS)
+	phpext_mbstring_ptr,
+#endif
 	phpext_mysql_ptr,
 	phpext_odbc_ptr,
 	phpext_pcre_ptr,
