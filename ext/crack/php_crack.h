23cb032fd9d9c3c3dd8c48cc65a8881d3c12aa98
foobar <sniper@php.net>
2001-07-31 20:24:43 +0000

    Missing header.

--- ext/crack/php_crack.h
+++ ext/crack/php_crack.h
@@ -1,3 +1,23 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999, 2000, 2001 The PHP Group             |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors:                                                             |
+   +----------------------------------------------------------------------+
+ */
+
+/* $Id$ */
+
 #ifndef ZEND_CRACK_H
 #define ZEND_CRACK_H
 
