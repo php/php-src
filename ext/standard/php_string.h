a70a4667bf30f6c6c353d7917d16401e069a84ec
jeroen <jeroen@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-08-03 22:12:46 +0000

    - Added new parameter to [l|r]trim, to specify _what_ to trim @- Added optional second parameter to trim, chop and ltrim. You can @  now specify which characters to trim (jeroen)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@53197 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -112,6 +112,7 @@ PHPAPI char *php_stristr(unsigned char *s, unsigned char *t, size_t s_len, size_
 PHPAPI char *php_str_to_str(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length);
 PHPAPI void php_trim(pval *str, pval *return_value, int mode);
+void php_trim2(zval *str, zval *what, zval *return_value, int mode);
 PHPAPI void php_strip_tags(char *rbuf, int len, int state, char *allow, int allow_len);
 
 PHPAPI void php_char_to_str(char *str, uint len, char from, char *to, int to_len, pval *result);
