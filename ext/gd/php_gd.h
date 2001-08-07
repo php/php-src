27fcd0509a6a06eb64cbce3eb3a9a59f0b3bfd54
foobar <sniper@php.net>
2001-08-07 16:15:45 +0000

    @- Added support for GD2 image type for ImageCreateFromString() (Jani)

--- ext/gd/php_gd.h
+++ ext/gd/php_gd.h
@@ -56,6 +56,7 @@
 PHPAPI extern const char php_sig_gif[3];
 PHPAPI extern const char php_sig_jpg[3];
 PHPAPI extern const char php_sig_png[3];
+PHPAPI const char php_sig_gd2[3] = {'g', 'd', '2'};
 
 extern zend_module_entry gd_module_entry;
 #define phpext_gd_ptr &gd_module_entry
