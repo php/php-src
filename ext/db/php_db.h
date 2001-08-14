a256a5a711d446d5695ca50017b42e849c20148c
dbeu <dbeu@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-08-14 17:46:19 +0000

    depreciated, but should build anyway...
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@54860 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/db/php_db.h
+++ ext/db/php_db.h
@@ -51,16 +51,16 @@ typedef struct dbm_info {
 #define phpext_db_ptr NULL
 #endif
 
-dbm_info *php_find_dbm(pval *id);
+dbm_info *php_find_dbm(pval *id TSRMLS_DC);
 int php_dbm_close(zend_rsrc_list_entry *rsrc TSRMLS_DC);
-dbm_info *php_dbm_open(char *filename, char *mode);
+dbm_info *php_dbm_open(char *filename, char *mode TSRMLS_DC);
 int php_dbm_insert(dbm_info *info, char *key, char *value TSRMLS_DC);
-char *php_dbm_fetch(dbm_info *info, char *key);
-int php_dbm_replace(dbm_info *info, char *key, char *value);
+char *php_dbm_fetch(dbm_info *info, char *key TSRMLS_DC);
+int php_dbm_replace(dbm_info *info, char *key, char *value TSRMLS_DC);
 int php_dbm_exists(dbm_info *info, char *key);
 int php_dbm_delete(dbm_info *info, char *key);
 char *php_dbm_first_key(dbm_info *info);
-char *php_dbm_nextkey(dbm_info *info, char *key);
+char *php_dbm_nextkey(dbm_info *info, char *key TSRMLS_DC);
 
 /* db file functions */
 PHP_MINIT_FUNCTION(db);
