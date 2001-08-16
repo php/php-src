0fd9dcc4bc08aed281c27d9c6cc72cb7fe55db8e
Rasmus Lerdorf <rasmus@php.net>
2001-08-16 20:35:33 +0000

    MFH

--- ext/skeleton/skeleton.php
+++ ext/skeleton/skeleton.php
@@ -1,5 +1,14 @@
 <?
+if(!extension_loaded('extname')) {
+	dl('extname.so');
+}
 $module = 'extname';
+$functions = get_extension_funcs($module);
+echo "Functions available in the test extension:<br>\n";
+foreach($functions as $func) {
+    echo $func."<br>\n";
+}
+echo "<br>\n";
 $function = 'confirm_' . $module . '_compiled';
 if (extension_loaded($module)) {
 	$str = $function($module);
