766de1569401131e869e312da26839a3d0ca7d3a
Sterling Hughes <sterling@php.net>
2001-08-22 04:45:51 +0000

    Add some editor modes (in the rest of the source)

--- sapi/apache2filter/apache_config.c
+++ sapi/apache2filter/apache_config.c
@@ -154,3 +154,11 @@ void *create_php_config(apr_pool_t *p, char *dummy)
 	return (void *) newx;
 }
 
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 tw=78 fdm=marker
+ * vim<600: sw=4 ts=4 tw=78
+ */
--- sapi/apache2filter/php_functions.c
+++ sapi/apache2filter/php_functions.c
@@ -149,3 +149,12 @@ int php_apache_register_module(void)
 {
 	return zend_startup_module(&php_apache_module);
 }
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 tw=78 fdm=marker
+ * vim<600: sw=4 ts=4 tw=78
+ */
