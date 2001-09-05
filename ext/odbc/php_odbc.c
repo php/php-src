24b54b866fc36fed094c1f455e0174c99c24ee40
Dan Kalowsky <kalowsky@php.net>
2001-09-05 16:19:48 +0000

    Patch to stop the crashing behavior exhibited by PHP/ODBC in SOLID/DB2 submitted by Walter Franzini (walter@sys-net.it) @Fix for LONG_VARCHAR type crashing submitted by Walter Franzini (walter@sys-net.it) #Should this also be put into the 4.0.7 RC cycle?

--- ext/odbc/php_odbc.c
+++ ext/odbc/php_odbc.c
@@ -615,6 +615,8 @@ int odbc_bindcols(odbc_result *result TSRMLS_DC)
 			default:
 				rc = SQLColAttributes(result->stmt, (UWORD)(i+1), SQL_COLUMN_DISPLAY_SIZE,
 									NULL, 0, NULL, &displaysize);
+				displaysize = displaysize <= result->longreadlen ? displaysize : 
+								result->longreadlen;
 				result->values[i].value = (char *)emalloc(displaysize + 1);
 				rc = SQLBindCol(result->stmt, (UWORD)(i+1), SQL_C_CHAR, result->values[i].value,
 							displaysize + 1, &result->values[i].vallen);
