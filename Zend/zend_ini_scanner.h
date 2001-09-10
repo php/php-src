9f61e47c24f81db65c921d6fbe498ba36e3f0af0
Zeev Suraski <zeev@php.net>
2001-09-10 00:08:24 +0000

    MFZE1 (nuke cplusplus code)

--- Zend/zend_ini_scanner.h
+++ Zend/zend_ini_scanner.h
@@ -1,18 +1,6 @@
 #ifndef _ZEND_INI_SCANNER_H
 #define _ZEND_INI_SCANNER_H
 
-#if defined(ZTS) && defined(__cplusplus)
-class ZendIniFlexLexer : public yyFlexLexer
-{
-public:
-	virtual ~ZendIniFlexLexer();
-	int lex_scan(zval *zendlval);
-	void BeginState(int state);
-
-	char *filename;
-};
-#endif	/* ZTS && __cplusplus */
-
 BEGIN_EXTERN_C()
 int zend_ini_scanner_get_lineno(TSRMLS_D);
 char *zend_ini_scanner_get_filename(TSRMLS_D);
