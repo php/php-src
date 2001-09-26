47c3bc75f5881aaa84f837e11b21f92aafa444b8
jeroen <jeroen@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-09-26 22:24:44 +0000

    Revert macro-subst, because wasn't necessary here, and those who were substitued weren't zvals
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@58541 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xslt/xslt.c
+++ ext/xslt/xslt.c
@@ -150,12 +150,12 @@ extern xslt_args *xslt_parse_arguments(char  *xml,
 	if (! strncasecmp(xml, "arg:", 4)) {
 		char *key = xml + 5;
 
-		Z_TYPE(return_value->xml) = XSLT_IS_DATA;
+		return_value->xml.type = XSLT_IS_DATA;
 		return_value->xml.ptr  = _find_xslt_argument((const char **) argv, 
 		                                             (const char *)  key);
 	}
 	else {
-		Z_TYPE(return_value->xml) = XSLT_IS_FILE;
+		return_value->xml.type = XSLT_IS_FILE;
 		return_value->xml.ptr  = estrdup(xml);
 	}
 
@@ -163,12 +163,12 @@ extern xslt_args *xslt_parse_arguments(char  *xml,
 	if (! strncasecmp(xsl, "arg:", 4)) {
 		char *key = xsl + 5;
 
-		Z_TYPE(return_value->xsl) = XSLT_IS_DATA;
+		return_value->xsl.type = XSLT_IS_DATA;
 		return_value->xsl.ptr  = _find_xslt_argument((const char **) argv, 
 		                                             (const char *)  key);
 	}
 	else {
-		Z_TYPE(return_value->xsl) = XSLT_IS_FILE;
+		return_value->xsl.type = XSLT_IS_FILE;
 		return_value->xsl.ptr  = estrdup(xsl);
 	}
 
@@ -176,12 +176,12 @@ extern xslt_args *xslt_parse_arguments(char  *xml,
 	if (! strncasecmp(result, "arg:", 4)) {
 		char *key = result + 5;
 
-		Z_TYPE(return_value->result) = XSLT_IS_DATA;
+		return_value->result.type = XSLT_IS_DATA;
 		return_value->result.ptr  = _find_xslt_argument((const char **) argv, 
 		                                                (const char *)  key);
 	}
 	else {
-		Z_TYPE(return_value->result) = XSLT_IS_FILE;
+		return_value->result.type = XSLT_IS_FILE;
 		return_value->result.ptr  = estrdup(result);
 	}
 
