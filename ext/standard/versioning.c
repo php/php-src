3a4b329cc3c1f99e7db5365d375ecd9133b0428a
SVN Migration <svn@php.net>
2001-10-10 10:14:52 +0000

    This commit was manufactured by cvs2svn to create branch 'PHP_4_0_7'.

---
+++ ext/standard/versioning.c
@@ -0,0 +1,253 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997, 1998, 1999, 2000, 2001 The PHP Group             |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Stig Sæther Bakken <ssb@fast.no>                            |
+   |                                                                      |
+   +----------------------------------------------------------------------+
+ */
+
+/* $Id$ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include "php.h"
+#include "php_versioning.h"
+
+#define sign(n) ((n)<0?-1:((n)>0?1:0))
+
+#define PHP_VC_COMPARE	0
+#define PHP_VC_LT		1
+#define PHP_VC_LE		2
+#define PHP_VC_GT		3
+#define PHP_VC_GE		4
+#define PHP_VC_EQ		5
+
+#define PHP_VCFUNC(name,mode) \
+	void name(INTERNAL_FUNCTION_PARAMETERS) { \
+		do_version_compare(INTERNAL_FUNCTION_PARAM_PASSTHRU, mode); \
+	}
+
+/* {{{ php_canonicalize_version() */
+
+PHPAPI char *
+php_canonicalize_version(const char *version)
+{
+    int len = strlen(version), in_number = 0;
+    char *buf = emalloc(len * 2 + 1), *q, lp, lq;
+    const char *p;
+
+    if (len == 0) {
+        *buf = '\0';
+        return buf;
+    }
+
+    p = version;
+    q = buf;
+    *q++ = lp = *p++;
+    lq = '\0';
+    while (*p) {
+/*  s/[-_+]/./g;
+ *  s/([^\d\.])([^\D\.])/$1.$2/g;
+ *  s/([^\D\.])([^\d\.])/$1.$2/g;
+ */
+#define isdigdot(x) (isdigit(x)||(x)=='.')
+#define isspecialver(x) ((x)=='-'||(x)=='_'||(x)=='+')
+
+        lq = *(q - 1);
+        if ((isdigdot(*p) != isdigdot(lp) || isspecialver(*p)) &&
+            (lq != '.' && *p != '.')) {
+            lq = *q;
+            *q++ = '.';
+        }
+        *q++ = lp = *p++;
+    }
+    *q++ = '\0';
+    return buf;
+}
+
+/* }}} */
+/* {{{ compare_special_version_forms() */
+
+static int
+compare_special_version_forms(const char *form1, const char *form2)
+{
+	int i, found1 = -1, found2 = -1;
+	char **pp;
+	static char *special_forms[] = {
+		"dev",
+		"a",
+		"b",
+		"RC",
+		"#N#",
+		"pl",
+		NULL
+	};
+
+	for (pp = special_forms, i = 0; *pp != NULL; pp++, i++) {
+		if (strncmp(form1, *pp, strlen(*pp)) == 0) {
+			found1 = i;
+			break;
+		}
+	}
+	for (pp = special_forms, i = 0; *pp != NULL; pp++, i++) {
+		if (strncmp(form2, *pp, strlen(*pp)) == 0) {
+			found2 = i;
+			break;
+		}
+	}
+	return sign(found1 - found2);
+}
+
+/* }}} */
+/* {{{ php_version_compare() */
+
+PHPAPI int
+php_version_compare(const char *orig_ver1, const char *orig_ver2)
+{
+	char *ver1 = php_canonicalize_version(orig_ver1);
+	char *ver2 = php_canonicalize_version(orig_ver2);
+	char *p1, *p2, *n1, *n2;
+	long l1, l2;
+	int compare = 0;
+
+	p1 = n1 = ver1;
+	p2 = n2 = ver2;
+	while (*p1 && *p2 && n1 && n2) {
+		if ((n1 = strchr(p1, '.')) != NULL) {
+			*n1 = '\0';
+		}
+		if ((n2 = strchr(p2, '.')) != NULL) {
+			*n2 = '\0';
+		}
+		if (isdigit(*p1) && isdigit(*p2)) {
+			/* compare element numerically */
+			l1 = strtol(p1, NULL, 10);
+			l2 = strtol(p2, NULL, 10);
+			compare = sign(l1 - l2);
+		} else if (!isdigit(*p1) && !isdigit(*p2)) {
+			/* compare element names */
+			compare = compare_special_version_forms(p1, p2);
+		} else {
+			/* mix of names and numbers */
+			if (isdigit(*p1)) {
+				compare = compare_special_version_forms("#N#", p2);
+			} else {
+				compare = compare_special_version_forms(p1, "#N#");
+			}
+		}
+		if (compare != 0) {
+			break;
+		}
+		if (n1 != NULL) {
+			p1 = n1 + 1;
+		}
+		if (n2 != NULL) {
+			p2 = n2 + 1;
+		}
+	}
+	if (compare == 0) {
+		if (n1 != NULL) {
+			if (isdigit(*p1)) {
+				compare = 1;
+			} else {
+				compare = php_version_compare(p1, "#N#");
+			}
+		} else if (n2 != NULL) {
+			if (isdigit(*p2)) {
+				compare = -1;
+			} else {
+				compare = php_version_compare("#N#", p2);
+			}
+		}
+	}
+	efree(ver1);
+	efree(ver2);
+	return compare;
+}
+
+/* }}} */
+/* {{{ do_version_compare() */
+
+void do_version_compare(INTERNAL_FUNCTION_PARAMETERS, int mode)
+{
+    zval **v1, **v2;
+	int compare, argc;
+
+	argc = ZEND_NUM_ARGS();
+	if (argc != 2 || zend_get_parameters_ex(argc, &v1, &v2) == FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+	convert_to_string_ex(v1);
+	convert_to_string_ex(v2);
+	compare = php_version_compare(Z_STRVAL_PP(v1), Z_STRVAL_PP(v2));
+	switch (mode) {
+		case PHP_VC_LT:
+			RETURN_LONG(compare == -1);
+		case PHP_VC_LE:
+			RETURN_LONG(compare != 1);
+		case PHP_VC_GT:
+			RETURN_LONG(compare == 1);
+		case PHP_VC_GE:
+			RETURN_LONG(compare != -1);
+		case PHP_VC_EQ:
+			RETURN_LONG(compare == 0);
+		case PHP_VC_COMPARE:
+		default:
+			RETURN_LONG(compare);
+	}
+}
+
+/* }}} */
+
+/* {{{ proto int version_compare(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_compare), PHP_VC_COMPARE);
+
+/* }}} */
+/* {{{ proto int version_lt(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_lt), PHP_VC_LT);
+
+/* }}} */
+/* {{{ proto int version_lt(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_le), PHP_VC_LE);
+
+/* }}} */
+/* {{{ proto int version_gt(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_gt), PHP_VC_GT);
+
+/* }}} */
+/* {{{ proto int version_ge(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_ge), PHP_VC_GE);
+
+/* }}} */
+/* {{{ proto int version_eq(string ver1, string ver2) */
+
+PHP_VCFUNC(PHP_FN(version_eq), PHP_VC_EQ);
+
+/* }}} */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */
