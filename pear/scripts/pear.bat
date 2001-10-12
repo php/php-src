d37016f26dd16f895e484441713ecfe1582e6db8
Martin Jansen <mj@php.net>
2001-10-12 20:39:50 +0000

    * streamlined the copyright header * changed path names in pear.bat to more typical values # The GUI installer on Win32 installs PHP to c:\php by default, so we # should make that the predefined value in the PEAR batch file.

--- pear/scripts/pear.bat
+++ pear/scripts/pear.bat
@@ -1,29 +1,29 @@
 @ECHO OFF
 
-REM ----------------------------------------------------------------------
-REM PHP version 4.0                                                      
-REM ----------------------------------------------------------------------
-REM Copyright (c) 1997-2001 The PHP Group                                
-REM ----------------------------------------------------------------------
-REM  This source file is subject to version 2.02 of the PHP license,     
-REM  that is bundled with this package in the file LICENSE, and is       
-REM  available at through the world-wide-web at                          
-REM  http://www.php.net/license/2_02.txt.                                
-REM  If you did not receive a copy of the PHP license and are unable to  
-REM  obtain it through the world-wide-web, please send a note to         
-REM  license@php.net so we can mail you a copy immediately.              
-REM ----------------------------------------------------------------------
-REM  Authors:     Alexander Merz (alexmerz@php.net)                          
-REM ----------------------------------------------------------------------
-REM 
-REM  $Id: pear.bat,v 1.1 2001/10/12 19:43:58 alexmerz Exp $
+REM +----------------------------------------------------------------------+
+REM | PHP version 4.0                                                      |
+REM +----------------------------------------------------------------------+
+REM | Copyright (c) 1997-2001 The PHP Group                                |
+REM +----------------------------------------------------------------------+
+REM | This source file is subject to version 2.02 of the PHP license,      |
+REM | that is bundled with this package in the file LICENSE, and is        |
+REM | available at through the world-wide-web at                           |
+REM | http://www.php.net/license/2_02.txt.                                 |
+REM | If you did not receive a copy of the PHP license and are unable to   |
+REM | obtain it through the world-wide-web, please send a note to          |
+REM | license@php.net so we can mail you a copy immediately.               |
+REM +----------------------------------------------------------------------+
+REM | Authors: Alexander Merz <alexmerz@php.net>                           |
+REM +----------------------------------------------------------------------+
+REM
+REM  $Id: pear.bat,v 1.2 2001/10/12 19:46:21 alexmerz Exp $
 
 REM change this four lines to match the paths of your system
 REM -------------------
-set PHP_PATH=e:\apache\php
-set PEAR_INSTALL_DIR=e:\apache\php\pear
-set PEAR_EXTENSION_DIR=e:\apache\php\extensions
-set PEAR_DOC_DIR=e:\apache\php\pear
+set PHP_PATH=c:\php
+set PEAR_INSTALL_DIR=c:\php\pear
+set PEAR_EXTENSION_DIR=c:\php\extensions
+set PEAR_DOC_DIR=c:\php\pear
 REM -------------------
 set DIRECTORY_SEPARATOR=\
 
