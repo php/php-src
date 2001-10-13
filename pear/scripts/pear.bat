2130fae7c9a7123687dbf82989d9eb90cb05274b
mj <mj@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-10-13 06:22:09 +0000

    * fixed problem with copyright header on some systems.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@59830 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/scripts/pear.bat
+++ pear/scripts/pear.bat
@@ -1,22 +1,22 @@
 @ECHO OFF
 
-REM +----------------------------------------------------------------------+
-REM | PHP version 4.0                                                      |
-REM +----------------------------------------------------------------------+
-REM | Copyright (c) 1997-2001 The PHP Group                                |
-REM +----------------------------------------------------------------------+
-REM | This source file is subject to version 2.02 of the PHP license,      |
-REM | that is bundled with this package in the file LICENSE, and is        |
-REM | available at through the world-wide-web at                           |
-REM | http://www.php.net/license/2_02.txt.                                 |
-REM | If you did not receive a copy of the PHP license and are unable to   |
-REM | obtain it through the world-wide-web, please send a note to          |
-REM | license@php.net so we can mail you a copy immediately.               |
-REM +----------------------------------------------------------------------+
-REM | Authors: Alexander Merz <alexmerz@php.net>                           |
-REM +----------------------------------------------------------------------+
+REM ----------------------------------------------------------------------
+REM PHP version 4.0                                                     
+REM ----------------------------------------------------------------------
+REM Copyright (c) 1997-2001 The PHP Group                               
+REM ----------------------------------------------------------------------
+REM  This source file is subject to version 2.02 of the PHP license,    
+REM  that is bundled with this package in the file LICENSE, and is      
+REM  available at through the world-wide-web at                         
+REM  http://www.php.net/license/2_02.txt.                               
+REM  If you did not receive a copy of the PHP license and are unable to 
+REM  obtain it through the world-wide-web, please send a note to        
+REM  license@php.net so we can mail you a copy immediately.             
+REM ----------------------------------------------------------------------
+REM  Authors:     Alexander Merz (alexmerz@php.net)                         
+REM ----------------------------------------------------------------------
 REM
-REM  $Id: pear.bat,v 1.2 2001/10/12 19:46:21 alexmerz Exp $
+REM  $Id$
 
 REM change this four lines to match the paths of your system
 REM -------------------
