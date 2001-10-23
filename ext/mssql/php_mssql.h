991e30668f78696c8e47ab808996c26fef7c7402
Frank M. Kromann <fmk@php.net>
2001-10-23 16:34:52 +0000

    Patch for correct handling of DATETIME values. The patch is implemented with a parameter in php.ini controling if datetime values are converted with the new or the old method. Thanks to Michael Bretterklieber <mbretter@jawa.at>

--- ext/mssql/php_mssql.h
+++ ext/mssql/php_mssql.h
@@ -114,8 +114,9 @@ ZEND_BEGIN_MODULE_GLOBALS(mssql)
 	long min_error_severity, min_message_severity;
 	long cfg_min_error_severity, cfg_min_message_severity;
 	long compatability_mode, connect_timeout, timeout;
-	void (*get_column_content)(mssql_link *mssql_ptr,int offset,pval *result,int column_type);
+	void (*get_column_content)(mssql_link *mssql_ptr,int offset,pval *result,int column_type  TSRMLS_DC);
 	long textsize, textlimit, batchsize;
+	long datetimeconvert;
 	HashTable *resource_list, *resource_plist;
 ZEND_END_MODULE_GLOBALS(mssql)
 
