d4cb0aaecf23fb5b6798f6115930e9a5512894ef
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-10-27 18:22:56 +0000

    - fix crash bug
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@60949 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -137,8 +137,7 @@ static PHP_INI_MH(OnUpdate_zlib_output_compression)
 	char *ini_value;
 
 	ini_value = php_ini_string("output_handler", sizeof("output_handler"), 0); 
-
-	if (new_value != NULL && strlen(ini_value) != 0) {
+	if (ini_value != NULL && new_value != NULL && strlen(ini_value) != 0) {
 		php_error(E_CORE_ERROR,"Cannot use both zlib.output_compression and output_handler together!!");
 		return FAILURE;
 	}
