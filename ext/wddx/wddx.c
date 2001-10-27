1057a676ff297ce6c419475a91c084c571b6a1a7
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-10-27 06:26:02 +0000

    Fixed bug: #13844
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@60878 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/wddx/wddx.c
+++ ext/wddx/wddx.c
@@ -211,6 +211,7 @@ static void release_wddx_packet_rsrc(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 
 #include "ext/session/php_session.h"
 
+#if HAVE_PHP_SESSION
 /* {{{ PS_SERIALIZER_ENCODE_FUNC
  */
 PS_SERIALIZER_ENCODE_FUNC(wddx)
@@ -283,16 +284,20 @@ PS_SERIALIZER_DECODE_FUNC(wddx)
 	return ret;
 }
 /* }}} */
+#endif
 
 /* {{{ PHP_MINIT_FUNCTION
  */
 PHP_MINIT_FUNCTION(wddx)
 {
 	le_wddx = zend_register_list_destructors_ex(release_wddx_packet_rsrc, NULL, "wddx", module_number);
+
+#if HAVE_PHP_SESSION
 	php_session_register_serializer("wddx",
 									PS_SERIALIZER_ENCODE_NAME(wddx),
 									PS_SERIALIZER_DECODE_NAME(wddx));
-	
+#endif	
+
 	return SUCCESS;
 }
 /* }}} */
@@ -302,7 +307,10 @@ PHP_MINIT_FUNCTION(wddx)
 PHP_MINFO_FUNCTION(wddx)
 {
 	php_info_print_table_start();
-	php_info_print_table_row(2, "WDDX Support", "enabled" );
+	php_info_print_table_header(2, "WDDX Support", "enabled" );
+#if HAVE_PHP_SESSION
+	php_info_print_table_row(2, "WDDX Session Serializer", "enabled" );
+#endif
 	php_info_print_table_end();
 }
 /* }}} */
