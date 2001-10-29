45344b588bf95d520896f75a05f4e395c5fe5364
ssb <ssb@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-10-29 12:15:53 +0000

    * moved System.php out of Experimental/
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@61070 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -22,15 +22,15 @@
 
 require_once 'PEAR.php';
 require_once 'Archive/Tar.php';
-require_once 'Experimental/System.php';
+require_once 'System.php';
 
 /**
-* TODO:
-*   - check in inforFromDescFile that the minimal data needed is present
-*     (pack name, version, files, others?)
-*   - perhaps use parser folding to be less restrictive with the format
-*     of the package.xml file
-*/
+ * TODO:
+ *   - check in inforFromDescFile that the minimal data needed is present
+ *     (pack name, version, files, others?)
+ *   - perhaps use parser folding to be less restrictive with the format
+ *     of the package.xml file
+ */
 class PEAR_Common extends PEAR
 {
     // {{{ properties
