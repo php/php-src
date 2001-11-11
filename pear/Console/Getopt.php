75b87b8abbf16ab3df83899ed83ccd8ff0ec5b0c
mfischer <mfischer@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-11-11 04:09:08 +0000

    Add missing loop variable $i.
    Suppress more warnings when using the option separator '--'.
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@61945 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/Console/Getopt.php
+++ pear/Console/Getopt.php
@@ -75,7 +75,7 @@ class Console_Getopt {
             sort($long_options);
 
         reset($args);
-        while (list(, $arg) = each($args)) {
+        while (list($i, $arg) = each($args)) {
 
             /* The special element '--' means explicit end of options. Treat the
                rest of the arguments as non-options and end the loop. */
@@ -84,9 +84,9 @@ class Console_Getopt {
                 break;
             }
 
-            if ($arg{0} != '-' || ($arg{1} == '-' && !$long_options)) {
+            if ($arg{0} != '-' || (strlen($arg) > 1 && $arg{1} == '-' && !$long_options)) {
                 $non_opts[] = $arg;
-            } else if ($arg{1} == '-') {
+            } else if (strlen($arg) > 1 && $arg{1} == '-') {
                 $error = Console_Getopt::_parseLongOption(substr($arg, 2), $long_options, $opts, $args);
                 if (PEAR::isError($error))
                     return $error;
