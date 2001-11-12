b73a2081ab059d694c942bef5a52516f3ecc4065
Rui Hirokawa <hirokawa@php.net>
2001-11-12 14:51:39 +0000

    fixed a bug in http input processing patched by Yasuo Ohgaki.

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -811,11 +811,11 @@ php_mbstr_encoding_handler(zval *arg, char *res, char *separator TSRMLS_DC)
 	string.no_encoding = MBSTRG(current_internal_encoding);
 	
 	/* count the variables contained in the query */
-	num = 1;
+	num = 0;
 	var = res;
 	n = strlen(res);
 	while(n > 0) {
-		if (*var == *separator) {
+		if (*var == '=') {
 			num++;
 		}
 		var++;
@@ -939,7 +939,7 @@ SAPI_POST_HANDLER_FUNC(php_mbstr_post_handler)
 /* http input processing */
 void mbstr_treat_data(int arg, char *str, zval* destArray TSRMLS_DC)
 {
-	char *res = NULL, *var, *val, *separator=NULL;
+	char *res = NULL, *separator=NULL;
 	const char *c_var;
 	pval *array_ptr;
 	int free_buffer=0;
