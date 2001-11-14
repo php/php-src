3c06e3c4bb35cd8ddcf076e48821b9fe429a5edd
Frank M. Kromann <fmk@php.net>
2001-11-14 20:42:38 +0000

    Prevent fbsql_num_rows from looping

--- ext/fbsql/php_fbsql.c
+++ ext/fbsql/php_fbsql.c
@@ -639,7 +639,7 @@ int phpfbFetchRow(PHPFBResult* result, int row)
 	for (;;)
 	{
 		void *rawData; 
-		if (row >= result->rowCount && row != 0x7fffffff) return 0;
+		if (row >= result->rowCount && result->rowCount != 0x7fffffff) return 0;
 		if (fbcrhRowCount(result->rowHandler) > (unsigned int)row) return 1;
 		rawData = fbcdcFetch(result->link->connection, result->batchSize, result->fetchHandle);
 		if (!fbcrhAddBatch(result->rowHandler, rawData)) result->rowCount = fbcrhRowCount(result->rowHandler);
