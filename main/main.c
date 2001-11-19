dee465139fefeaf30fac0028dc0b939e5e9b8b47
Zeev Suraski <zeev@php.net>
2001-11-19 15:00:42 +0000

    MFH

--- main/main.c
+++ main/main.c
@@ -506,9 +506,8 @@ PHP_FUNCTION(set_time_limit)
 		WRONG_PARAM_COUNT;
 	}
 
-	convert_to_long_ex(new_timeout);
-	zend_unset_timeout(TSRMLS_C);
-	zend_set_timeout(Z_LVAL_PP(new_timeout));
+	convert_to_string_ex(new_timeout);
+	zend_alter_ini_entry("max_execution_time", sizeof("max_execution_time"), Z_STRVAL_PP(new_timeout), Z_STRLEN_PP(new_timeout), PHP_INI_USER, PHP_INI_STAGE_RUNTIME);
 }
 /* }}} */
 
