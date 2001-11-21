e184ed1ed3c5a8e1fe2c1b856c63fe7e1bb8d041
Frank M. Kromann <fmk@php.net>
2001-11-21 22:47:54 +0000

    Allow php_win_err() to be called from extenstions

--- win32/winutil.c
+++ win32/winutil.c
@@ -1,12 +1,10 @@
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include "winutil.h"
+#include <php.h>
 
 #ifndef THREAD_SAFE
 static char Win_Error_msg[256];
 #endif
 
-char *php_win_err(void)
+PHPAPI char *php_win_err(void)
 {
 	FormatMessage(
 					 FORMAT_MESSAGE_FROM_SYSTEM,
