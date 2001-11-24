a25ccbec2e8c2435ebbba1afd39005316fab3a1b
Zeev Suraski <zeev@php.net>
2001-11-24 16:05:22 +0000

    whitespace

--- main/php_variables.c
+++ main/php_variables.c
@@ -34,6 +34,7 @@ PHPAPI void php_register_variable(char *var, char *strval, zval *track_vars_arra
 	php_register_variable_safe(var, strval, strlen(strval), track_vars_array TSRMLS_CC);
 }
 
+
 /* binary-safe version */
 PHPAPI void php_register_variable_safe(char *var, char *strval, int str_len, zval *track_vars_array TSRMLS_DC)
 {
