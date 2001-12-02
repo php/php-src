ce75d2699637e975f64b755b7308c494331a9b7b
Daniel Beulshausen <dbeu@php.net>
2001-12-02 13:34:40 +0000

    fix php_win_err (wasn't threadsafe)

--- win32/winutil.c
+++ win32/winutil.c
@@ -1,19 +1,31 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP version 4.0                                                      |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2001 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author:                                                              |
+   +----------------------------------------------------------------------+
+ */
+
 #include <php.h>
 
-#ifndef THREAD_SAFE
-static char Win_Error_msg[256];
-#endif
-
-PHPAPI char *php_win_err(void)
+PHPAPI char *php_win_err(int error)
 {
-	FormatMessage(
-					 FORMAT_MESSAGE_FROM_SYSTEM,
-					 NULL,
-					 GetLastError(),
-					 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),	/* Default language */
-					  (LPTSTR) Win_Error_msg,
-					 256,
-					 NULL);
+	char *buf;
 
-	return Win_Error_msg;
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |	FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),	(LPTSTR)&buf, 0, NULL
+	);
+
+	return (buf ? (char *) buf : "");
 }
