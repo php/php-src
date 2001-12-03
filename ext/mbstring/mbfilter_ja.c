f6239b098b5c7e8589215500ff2292aaf6590686
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-12-03 15:32:12 +0000

    Fix declarations of static variables.  Builds on AIX now.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@63791 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mbstring/mbfilter_ja.c
+++ ext/mbstring/mbfilter_ja.c
@@ -83,35 +83,35 @@
 #include "mbfilter_ja.h"
 
 /* cp932 table */
-extern const unsigned short cp932ext3_eucjp_table[];
-extern const int cp932ext3_eucjp_table_size;
+static const unsigned short cp932ext3_eucjp_table[];
+static const int cp932ext3_eucjp_table_size;
 
 /* unicode table */
-extern const unsigned short jisx0208_ucs_table[];
-extern const unsigned short jisx0212_ucs_table[];
-extern const unsigned short cp932ext1_ucs_table[];
-extern const unsigned short cp932ext2_ucs_table[];
-extern const unsigned short cp932ext3_ucs_table[];
-extern const unsigned short ucs_a1_jis_table[];
-extern const unsigned short ucs_a2_jis_table[];
-extern const unsigned short ucs_i_jis_table[];
-extern const unsigned short ucs_r_jis_table[];
-extern const int jisx0208_ucs_table_size;
-extern const int jisx0212_ucs_table_size;
-extern const int cp932ext1_ucs_table_min;
-extern const int cp932ext1_ucs_table_max;
-extern const int cp932ext2_ucs_table_min;
-extern const int cp932ext2_ucs_table_max;
-extern const int cp932ext3_ucs_table_min;
-extern const int cp932ext3_ucs_table_max;
-extern const int ucs_a1_jis_table_min;
-extern const int ucs_a1_jis_table_max;
-extern const int ucs_a2_jis_table_min;
-extern const int ucs_a2_jis_table_max;
-extern const int ucs_i_jis_table_min;
-extern const int ucs_i_jis_table_max;
-extern const int ucs_r_jis_table_min;
-extern const int ucs_r_jis_table_max;
+static const unsigned short jisx0208_ucs_table[];
+static const unsigned short jisx0212_ucs_table[];
+static const unsigned short cp932ext1_ucs_table[];
+static const unsigned short cp932ext2_ucs_table[];
+static const unsigned short cp932ext3_ucs_table[];
+static const unsigned short ucs_a1_jis_table[];
+static const unsigned short ucs_a2_jis_table[];
+static const unsigned short ucs_i_jis_table[];
+static const unsigned short ucs_r_jis_table[];
+static const int jisx0208_ucs_table_size;
+static const int jisx0212_ucs_table_size;
+static const int cp932ext1_ucs_table_min;
+static const int cp932ext1_ucs_table_max;
+static const int cp932ext2_ucs_table_min;
+static const int cp932ext2_ucs_table_max;
+static const int cp932ext3_ucs_table_min;
+static const int cp932ext3_ucs_table_max;
+static const int ucs_a1_jis_table_min;
+static const int ucs_a1_jis_table_max;
+static const int ucs_a2_jis_table_min;
+static const int ucs_a2_jis_table_max;
+static const int ucs_i_jis_table_min;
+static const int ucs_i_jis_table_max;
+static const int ucs_r_jis_table_min;
+static const int ucs_r_jis_table_max;
 
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)
 
