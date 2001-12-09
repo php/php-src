216e81db96b54261a09dcd8cfc8f6c4235d1aa9f
Zeev Suraski <zeev@php.net>
2001-12-09 01:00:57 +0000

    Undo (per Jani's request)

--- TSRM/tsrm_config_common.h
+++ TSRM/tsrm_config_common.h
@@ -16,19 +16,8 @@
 #include <malloc.h>
 #endif
 
-/* AIX requires this to be the first thing in the file.  */
-#ifndef __GNUC__
-# if HAVE_ALLOCA_H
-#  include <alloca.h>
-# else
-#  ifdef _AIX
- #pragma alloca
-#  else
-#   ifndef alloca /* predefined by HP cc +Olibcalls */
-char *alloca ();
-#   endif
-#  endif
-# endif
+#if HAVE_ALLOCA_H
+#include <alloca.h>
 #endif
 
 #if HAVE_UNISTD_H
