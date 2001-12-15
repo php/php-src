052d5fc85d074f82557c8bb538874a9d7ab5f854
Hartmut Holzgraefe <hholzgra@php.net>
2001-12-15 14:23:07 +0000

    proto fix

--- ext/ftp/php_ftp.c
+++ ext/ftp/php_ftp.c
@@ -285,7 +285,7 @@ PHP_FUNCTION(ftp_chdir)
 /* }}} */
 
 /* {{{ proto int ftp_exec(int stream, string command)
-   Changes directories */
+    Request execution of a program on the ftp server */
 PHP_FUNCTION(ftp_exec)
 {
 	pval		*arg1, *arg2;
