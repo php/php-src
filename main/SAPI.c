8022bb9b40f79381dbc6efa8fbbeb16534d4dfc2
Stefan Esser <sesser@php.net>
2001-12-16 21:40:20 +0000

    fixed: WWW-Authenticate -> header_line got only freed when safe_mode was activ fixed: WWW-Authenticate -> content of header_line was changed after it was freed

--- main/SAPI.c
+++ main/SAPI.c
@@ -521,7 +521,7 @@ SAPI_API int sapi_add_header_ex(char *header_line, uint header_line_len, zend_bo
 					efree(result);
 					efree(Z_STRVAL_P(repl_temp));
 					efree(repl_temp);
-				}
+				} else efree(header_line);
 #else
 				if(PG(safe_mode)) {
 					myuid = php_getuid();
@@ -532,10 +532,9 @@ SAPI_API int sapi_add_header_ex(char *header_line, uint header_line_len, zend_bo
 					sapi_header.header = newheader;
 					sapi_header.header_len = newlen;
 					efree(result);
-				}
+				} else efree(header_line);
 #endif
 			}
-			*colon_offset = ':';
 		}
 	}
 
