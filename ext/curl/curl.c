8446abbb5d07a22fce44223d37b6629f4de6d10d
sterling <sterling@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-12-24 13:58:03 +0000

    Avoid a segfault when 0 bytes are returned from a website, and RETURNTRANSFER is enabled.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@66093 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/curl/curl.c
+++ ext/curl/curl.c
@@ -890,13 +890,13 @@ PHP_FUNCTION(curl_exec)
 
 	error = curl_easy_perform(ch->cp);
 	if (error != CURLE_OK) {
-		if (ch->handlers->write->buf.c)
+		if (ch->handlers->write->buf.len > 0)
 			smart_str_free(&ch->handlers->write->buf);
 		SAVE_CURL_ERROR(ch, error);
 		RETURN_FALSE;
 	}
 
-	if (ch->handlers->write->method == PHP_CURL_RETURN) {
+	if (ch->handlers->write->method == PHP_CURL_RETURN && ch->handlers->write->buf.len > 0) {
 		if (ch->handlers->write->type != PHP_CURL_BINARY) 
 			smart_str_0(&ch->handlers->write->buf);
 		RETURN_STRINGL(ch->handlers->write->buf.c, ch->handlers->write->buf.len, 0);
