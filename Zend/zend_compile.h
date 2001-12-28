8773b18b055a81eb7cfe06276b6a14043b3d8c5c
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2001-12-28 16:36:04 +0000

    - Fix some case insensitivity stuff in respect to classes
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@66321 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -275,7 +275,7 @@ int zend_do_begin_function_call(znode *function_name TSRMLS_DC);
 void zend_do_begin_method_call(znode *left_bracket TSRMLS_DC);
 void zend_do_begin_dynamic_function_call(znode *function_name TSRMLS_DC);
 void do_fetch_class(znode *result, znode *class_entry, znode *class_name TSRMLS_DC);
-void do_fetch_class_name(znode *result, znode *class_entry, znode *class_name TSRMLS_DC);
+void do_fetch_class_name(znode *result, znode *class_entry, znode *class_name, zend_bool case_sensitive TSRMLS_DC);
 void zend_do_begin_class_member_function_call(znode *class_name, znode *function_name TSRMLS_DC);
 void zend_do_end_function_call(znode *function_name, znode *result, znode *argument_list, int is_method, int is_dynamic_fcall TSRMLS_DC);
 void zend_do_return(znode *expr, int do_end_vparse TSRMLS_DC);
