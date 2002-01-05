9a171d03e84af05f135a2e2afe8a9008bc79e3a1
jim winstead <jimw@php.net>
2002-01-05 01:59:11 +0000

    Be more aggressive in making sure that substring matches are valid in ereg_replace before trying to use them. # i could have sworn i fixed this in php3. ereg() has similar logic, i # guess i just missed ereg_replace. fixing this lets # ext/standard/tests/reg/012.phpt pass on my debian/unstable box

--- ext/standard/reg.c
+++ ext/standard/reg.c
@@ -369,7 +369,9 @@ PHPAPI char *php_reg_replace(const char *pattern, const char *replace, const cha
 				if ('\\' == *walk
 					&& '0' <= walk[1] && '9' >= walk[1]
 					&& subs[walk[1] - '0'].rm_so > -1
-					&& subs[walk[1] - '0'].rm_eo > -1) {
+					&& subs[walk[1] - '0'].rm_eo > -1
+					/* this next case shouldn't happen. it does. */
+					&& subs[walk[1] - '0'].rm_so < subs[walk[1] - '0'].rm_eo) {
 					tmp = subs[walk[1] - '0'].rm_eo
 						- subs[walk[1] - '0'].rm_so;
 					memcpy (walkbuf,
