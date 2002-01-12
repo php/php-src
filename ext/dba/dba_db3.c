7a9ed7badd5d646a091631c856c8b9c2edaff2e2
swm <swm@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-01-12 07:22:27 +0000

    This fixes the notorious "mode 'c' fails" bug (see bugs - 10380, 10798, 11732). The bug originates from the fact that mode "c" for db3 sets 'type' to DB_UNKNOWN and mode DB_CREATE when the database already exists. The underlying library raises an error at this logical discrepancy: obviously one cannot create a database of unknown type.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@67611 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dba/dba_db3.c
+++ ext/dba/dba_db3.c
@@ -49,13 +49,15 @@ DBA_OPEN_FUNC(db3)
 	int gmode = 0;
 	int filemode = 0644;
 	struct stat check_stat;
+	int s = VCWD_STAT(info->path, &check_stat);
 
 	type =  info->mode == DBA_READER ? DB_UNKNOWN :
 		info->mode == DBA_TRUNC ? DB_BTREE :
-		VCWD_STAT(info->path, &check_stat) ? DB_BTREE : DB_UNKNOWN;
+		s? DB_BTREE : DB_UNKNOWN;
 	  
 	gmode = info->mode == DBA_READER ? DB_RDONLY :
-		info->mode == DBA_CREAT  ? DB_CREATE : 
+		(info->mode == DBA_CREAT && s) ? DB_CREATE : 
+		(info->mode == DBA_CREAT && !s) ? 0 :
 		info->mode == DBA_WRITER ? 0         : 
 		info->mode == DBA_TRUNC ? DB_CREATE | DB_TRUNCATE : -1;
 
