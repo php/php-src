7270b4223d9d51f3b69ee19d30c9259f983cb7e5
Martin Jansen <mj@php.net>
2002-02-07 20:50:08 +0000

    * Better error handling.

--- pear/scripts/pearcmd-remote-list.php
+++ pear/scripts/pearcmd-remote-list.php
@@ -3,6 +3,11 @@
 $remote = new PEAR_Remote($config);
 $result = $remote->call('package.listAll', 1);
 $i = $j = 0;
+
+if (PEAR::isError($result)) {
+    die("\n\n   Error while executing command!\n\n");
+}
+
 heading("Available packages");
 foreach ($result as $package => $info) {
     if ($i++ % 20 == 0) {
