2ca97337a60171464ab4ad7b39fe47411b389b7c
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-02-24 07:54:04 +0000

    Make phpinfo() look nicer.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@70832 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/wddx/wddx.c
+++ ext/wddx/wddx.c
@@ -314,9 +314,11 @@ PHP_MINIT_FUNCTION(wddx)
 PHP_MINFO_FUNCTION(wddx)
 {
 	php_info_print_table_start();
-	php_info_print_table_header(2, "WDDX Support", "enabled" );
 #if HAVE_PHP_SESSION
+	php_info_print_table_header(2, "WDDX Support", "enabled" );
 	php_info_print_table_row(2, "WDDX Session Serializer", "enabled" );
+#else
+	php_info_print_table_row(2, "WDDX Support", "enabled" );
 #endif
 	php_info_print_table_end();
 }
