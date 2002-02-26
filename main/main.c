7e7090af97a2880238a8c28fbc9e24999d314e6e
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-02-26 18:59:27 +0000

    - Pass shutdown_memory_manager the TSRMLS context.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@70972 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -698,9 +698,11 @@ int php_request_startup(TSRMLS_D)
  */
 void php_request_shutdown_for_exec(void *dummy)
 {
+	TSRMLS_FETCH();
+
 	/* used to close fd's in the 3..255 range here, but it's problematic
 	 */
-	shutdown_memory_manager(1, 1);
+	shutdown_memory_manager(1, 1 TSRMLS_CC);
 }
 /* }}} */
 
