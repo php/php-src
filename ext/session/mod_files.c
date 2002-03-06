eceb66ad19f6642f2eb6cb7fafffe3928f14f6a7
yohgaki <yohgaki@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-03-06 07:49:49 +0000

    Fix crash bug #14232
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@72073 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -136,8 +136,10 @@ static void ps_files_open(ps_files *data, const char *key TSRMLS_DC)
 		ps_files_close(data);
 		
 		if (!ps_files_valid_key(key) || 
-				!ps_files_path_create(buf, sizeof(buf), data, key))
+			!ps_files_path_create(buf, sizeof(buf), data, key)) {
+			data->fd = -1;
 			return;
+		}
 		
 		data->lastkey = estrdup(key);
 		
