ff62e746d2883eae4a41b17ca58c12609ef94e60
Harald Radi <phanto@php.net>
2002-03-15 00:00:34 +0000

    rpc apstraction module does only work with ZendEngine2

---
+++ ext/rpc/com/com.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../handler.h"
+
+RPC_REGISTER_HANDLERS(com);
+
+RPC_INIT_FUNCTION(com) {
+}
---
+++ ext/rpc/handler.h
@@ -0,0 +1,25 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include "php.h"
+
+#define RPC_HANDLER(layer)				{#layer, layer##_handler_init, &layer##_object_handlers, &layer##_class_entry}
+#define RPC_DECLARE_HANDLER(layer)		void layer##_handler_init();					\
+										rpc_object_handlers layer##_object_handlers;	\
+										zend_class_entry layer##_class_entry;
+#define RPC_INIT_FUNCTION(layer)		void layer##_handler_init()
+#define RPC_REGISTER_HANDLERS(layer)	zend_class_entry layer##_class_entry;			\
+										rpc_object_handlers layer##object_handlers;
+
+typedef struct _rpc_object_handlers {
+	int i;
+} rpc_object_handlers;
+
+typedef struct _rpc_handler_entry {
+	char				*name;
+	void (*rpc_handler_init)();
+	rpc_object_handlers	*handlers;
+	zend_class_entry	*ce;
+} rpc_handler_entry;
+
+#endif /* HANDLER_H */
