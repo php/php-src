4f04017b27b8fd306fb643167f21895b877a3c3a
mfischer <mfischer@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-03-16 22:31:57 +0000

    - WS and code style fix.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@73677 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pcntl/php_pcntl.h
+++ ext/pcntl/php_pcntl.h
@@ -39,7 +39,7 @@ PHP_RINIT_FUNCTION(pcntl);
 PHP_RSHUTDOWN_FUNCTION(pcntl);
 PHP_MINFO_FUNCTION(pcntl);
 
-PHP_FUNCTION(pcntl_fork);	
+PHP_FUNCTION(pcntl_fork);
 PHP_FUNCTION(pcntl_waitpid);
 PHP_FUNCTION(pcntl_wifexited);
 PHP_FUNCTION(pcntl_wifstopped);
