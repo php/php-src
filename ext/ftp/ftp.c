c26ecc6d4ee7c2287792465124acd87f323c090f
vlad <vlad@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-03-18 22:26:32 +0000

    Fix ftp_size() returning bogus results. # make sure the server is in BINARY mode before requesting file size.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@74000 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ftp/ftp.c
+++ ext/ftp/ftp.c
@@ -685,6 +685,8 @@ ftp_size(ftpbuf_t *ftp, const char *path)
 	if (ftp == NULL)
 		return -1;
 
+	if (!ftp_type(ftp, FTPTYPE_IMAGE))
+		return -1;
 	if (!ftp_putcmd(ftp, "SIZE", path))
 		return -1;
 	if (!ftp_getresp(ftp) || ftp->resp != 213)
