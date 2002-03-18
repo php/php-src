aea697e98fa8d293a509e4ad484a1f2d39e32a2a
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-03-18 08:29:17 +0000

    Rename HAVE_PHP_STREAM to PHP_HAVE_STREAMS, because
    1.) a PHP-specific symbol should be in the php namespace, and
    2.) there are multiple streams and the old configure parameter was plural, too.
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@73883 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -623,7 +623,7 @@ function_entry basic_functions[] = {
 	PHP_FE(set_socket_blocking,												NULL)
 	PHP_FE(socket_set_blocking,												NULL)
 
-#if HAVE_PHP_STREAM
+#if PHP_HAVE_STREAMS
 	PHP_FE(fgetwrapperdata,													NULL)
 #endif
 
