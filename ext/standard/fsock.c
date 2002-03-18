e2ecf549e19c4af152a3a240f02d72321e234860
Derick Rethans <derick@php.net>
2002-03-18 21:40:55 +0000

    - MFH for bugfix for bug #10001

--- ext/standard/fsock.c
+++ ext/standard/fsock.c
@@ -495,6 +495,7 @@ static size_t php_sockread_internal(php_sockbuf *sock)
 
 static void php_sockread_total(php_sockbuf *sock, size_t maxread)
 {
+	sock->timeout_event = 0;
 	while(!sock->eof && TOREAD(sock) < maxread && !sock->timeout_event) {
 		php_sockread_internal(sock);
 	}
@@ -555,6 +556,8 @@ static char * php_sock_fgets_internal(char * buf, size_t maxlen, php_sockbuf * s
 	}
 
 	SEARCHCR();
+
+	sock->timeout_event = 0;
 
 	if(!p) {
 		if(sock->is_blocked) {
