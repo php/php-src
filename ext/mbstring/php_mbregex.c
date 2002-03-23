97d473a84bef5d893854f1a56f666352701b937f
hirokawa <hirokawa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-03-23 07:33:13 +0000

    changed license from GPL2 to PHP License.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@74802 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mbstring/php_mbregex.c
+++ ext/mbstring/php_mbregex.c
@@ -1,33 +1,19 @@
-/* 
- * PHP3 Internationalization support program.
- *
- * Copyright (c) 1999,2000 by the PHP3 internationalization team.
- * All rights reserved.
- *
- * This program is free software. You can use, redistribute and/or modify
- * without fee under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY including implied or express warranty of
- * marchantability or fitness for a particular purpose.
- *
- * Currently, the "PHP3 internationalization team" has no relationship with
- * the "PHP Development Team". But we hope these code will be integrated
- * into the PHP3, and it will be distributed as a part of PHP3.
- *
- * See README_i18n for more detail.
- *
- * Authors:
- *    Hironori Sato <satoh@jpnnet.com>
- *    Shigeru Kanemoto <sgk@happysize.co.jp>
- *    Tsukada Takuya <tsukada@fminn.nagano.nagano.jp>
- */
-
-/* 
- * PHP4 multibyte regular expression module
- * Authors:
- *    Tsukada Takuya <tsukada@fminn.nagano.nagano.jp>
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 4                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2001 The PHP Group                                     |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Tsukada Takuya <tsukada@fminn.nagano.nagano.jp>              |
+   +----------------------------------------------------------------------+
  */
 
 /* $Id$ */
