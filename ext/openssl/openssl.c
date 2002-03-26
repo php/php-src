beb3b6b44abc7e4d84689fe3e190581d389b938a
Derick Rethans <derick@php.net>
2002-03-26 08:47:41 +0000

    - No asserts in the branch

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -140,8 +140,6 @@ static int le_csr;
 static void php_pkey_free(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 {
 	EVP_PKEY *pkey = (EVP_PKEY *)rsrc->ptr;
-
-	assert(pkey != NULL);
 	
 	EVP_PKEY_free(pkey);
 }
