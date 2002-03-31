cac350dc272ada025d81a6937ddbc372b707dbdd
Rui Hirokawa <hirokawa@php.net>
2002-03-31 05:16:27 +0000

    moved static constant definitions for separeted files to fix compilation problem in Tru64.

--- ext/mbstring/mbregex.c
+++ ext/mbstring/mbregex.c
@@ -186,7 +186,9 @@ static void store_jump _((char*, int, char*));
 static void insert_jump _((int, char*, char*, char*));
 static void store_jump_n _((char*, int, char*, unsigned));
 static void insert_jump_n _((int, char*, char*, char*, unsigned));
+#if 0
 static void insert_op _((int, char*, char*));
+#endif
 static void insert_op_2 _((int, char*, char*, int, int));
 static int memcmp_translate _((unsigned char*, unsigned char*, int, const unsigned char*));
 static const unsigned char* re_mbctab_get _((int));
