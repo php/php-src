efa18af7e14f8df146505423e72990aa5231e3be
Stig Bakken <ssb@php.net>
2002-04-09 08:19:53 +0000

    * show full cvstag command

--- pear/PEAR/Packager.php
+++ pear/PEAR/Packager.php
@@ -137,7 +137,7 @@ class PEAR_Packager extends PEAR_Common
         $this->log(1, "Package $dest_package done");
         $cvsversion = preg_replace('/[^a-z0-9]/i', '_', $pkgversion);
         $cvstag = "RELEASE_$cvsversion";
-        $this->log(0, "Tag the released code with `pear cvstag'");
+        $this->log(0, "Tag the released code with `pear cvstag $pkgfile'");
         $this->log(0, "(or set the CVS tag $cvstag by hand)");
         return $dest_package;
     }
