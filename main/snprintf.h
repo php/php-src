0389db062211d1061c257a6136d0f05bffc75d6a
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-04-10 01:07:49 +0000

    -preface for new vpprintf #reordered and renamed some functions and removed some static
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@77455 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/snprintf.h
+++ main/snprintf.h
@@ -34,6 +34,41 @@ int php_sprintf (char* s, const char* format, ...);
 #define sprintf php_sprintf
 #endif
 
+#ifdef HAVE_GCVT
+
+#define ap_php_ecvt ecvt
+#define ap_php_fcvt fcvt
+#define ap_php_gcvt gcvt
+
+#else
+
+extern char * ap_php_cvt(double arg, int ndigits, int *decpt, int *sign, int eflag);
+extern char * ap_php_ecvt(double arg, int ndigits, int *decpt, int *sign);
+extern char * ap_php_fcvt(double arg, int ndigits, int *decpt, int *sign);
+extern char * ap_php_gcvt(double number, int ndigit, char *buf);
+
+#endif
+
+typedef enum {
+	NO = 0, YES = 1
+} boolean_e;
+
+#define WIDE_INT		long
+typedef WIDE_INT wide_int;
+typedef unsigned WIDE_INT u_wide_int;
+
+typedef int bool_int;
+
+extern char * ap_php_conv_10(register wide_int num, register bool_int is_unsigned,
+	   register bool_int * is_negative, char *buf_end, register int *len);
+
+extern char * ap_php_conv_fp(register char format, register double num,
+		 boolean_e add_dp, int precision, bool_int * is_negative, char *buf, int *len);
+
+extern char * ap_php_conv_p2(register u_wide_int num, register int nbits,
+		 char format, char *buf_end, register int *len);
+
+
 #endif /* SNPRINTF_H */
 
 /*
