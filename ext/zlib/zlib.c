e750bc62e06d717d3fbf1c4aee589cfc24aefa13
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-04-12 21:19:59 +0000

    Do NOT use C++ comments in C code.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@77947 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -154,8 +154,6 @@ static PHP_INI_MH(OnUpdate_zlib_output_compression)
 /* {{{ OnUpdate_zlib_output_compression_level */
 static PHP_INI_MH(OnUpdate_zlib_output_compression_level)
 {
-//	char *ini_value;
-
 	OnUpdateInt(entry, new_value, new_value_length, mh_arg1, mh_arg2, mh_arg3, stage TSRMLS_CC);
 
 	return SUCCESS;
