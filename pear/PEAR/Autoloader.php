b339986c121af2f10084ba18ca3bc3246512555d
Tomas V.V.Cox <cox@php.net>
2002-04-16 00:44:03 +0000

    Added standar header

--- pear/PEAR/Autoloader.php
+++ pear/PEAR/Autoloader.php
@@ -1,4 +1,23 @@
 <?php
+// /* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2002 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Stig Bakken <ssb@fast.no>                                    |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+//
+// $Id$
 
 if (!extension_loaded("overload")) {
     // die hard without ext/overload
@@ -74,7 +93,7 @@ class PEAR_Autoloader extends PEAR
      * @return bool TRUE if an entry was removed, FALSE if not
      *
      * @access public
-     */       
+     */
     function removeAutoload($method)
     {
         $ok = isset($this->_autoload_map[$method]);
