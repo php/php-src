3285a3c069b0e7a071068a58d5fa08e33b113659
sander <sander@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-04-16 14:19:09 +0000

    Added missing function to function_entry
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@78397 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/w32api/w32api.c
+++ ext/w32api/w32api.c
@@ -41,7 +41,8 @@ function_entry w32api_functions[] = {
 	PHP_FE(w32api_deftype,							NULL)
 	PHP_FE(w32api_init_dtype,						NULL)
 	PHP_FE(w32api_set_call_method,					NULL)
-    {NULL, NULL, NULL}
+	PHP_FE(w32api_invoke_function,					NULL)
+	{NULL, NULL, NULL}
 };
 /* }}} */
 
