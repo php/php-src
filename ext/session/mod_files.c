86cb47ea9c655c4dc671de3d9e5214d84e6d4238
Sascha Schumann <sas@php.net>
2002-04-23 18:10:49 +0000

    MFH: Rip out use of O_EXCL

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -141,14 +141,7 @@ static void ps_files_open(ps_files *data, const char *key TSRMLS_DC)
 		
 		data->lastkey = estrdup(key);
 		
-#ifdef O_EXCL
-		data->fd = VCWD_OPEN(buf, O_RDWR | O_BINARY);
-		
-		if (data->fd == -1 && errno == ENOENT) 
-			data->fd = VCWD_OPEN_MODE(buf, O_EXCL | O_RDWR | O_CREAT | O_BINARY, 0600);
-#else
 		data->fd = VCWD_OPEN_MODE(buf, O_CREAT | O_RDWR | O_BINARY, 0600);
-#endif
 		if (data->fd != -1) 
 			flock(data->fd, LOCK_EX);
 
