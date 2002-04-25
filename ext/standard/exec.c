8ffd089e02134fe02c396372c8b85a3d6ebe4713
Derick Rethans <derick@php.net>
2002-04-25 06:43:11 +0000

    - Fix for bug #16811

--- ext/standard/exec.c
+++ ext/standard/exec.c
@@ -525,6 +525,7 @@ PHP_FUNCTION(shell_exec)
 	if ((in=VCWD_POPEN(Z_STRVAL_PP(cmd), "r"))==NULL) {
 #endif
 		php_error(E_WARNING, "Unable to execute '%s'", Z_STRVAL_PP(cmd));
+		RETURN_FALSE;
 	}
 	allocated_space = EXEC_INPUT_BUF;
 	ret = (char *) emalloc(allocated_space);
