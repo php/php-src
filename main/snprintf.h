66036eab1003e4c9b77f3f1047ecd022c43360aa
Sascha Schumann <sas@php.net>
2002-05-07 13:24:07 +0000

    use thread-safe functions unconditionally

--- main/snprintf.h
+++ main/snprintf.h
@@ -38,20 +38,10 @@ typedef enum {
 	NO = 0, YES = 1
 } boolean_e;
 
-#ifdef HAVE_GCVT
-
-#define ap_php_ecvt ecvt
-#define ap_php_fcvt fcvt
-#define ap_php_gcvt gcvt
-
-#else
-
 extern char * ap_php_cvt(double arg, int ndigits, int *decpt, int *sign, int eflag, char *buf);
 extern char * ap_php_ecvt(double arg, int ndigits, int *decpt, int *sign, char *buf);
 extern char * ap_php_fcvt(double arg, int ndigits, int *decpt, int *sign, char *buf);
 extern char * ap_php_gcvt(double number, int ndigit, char *buf, boolean_e altform);
-
-#endif
 
 #define WIDE_INT		long
 typedef WIDE_INT wide_int;
