4f3e7253c9c1544b9488db33a68d4dd188354f93
mfischer <mfischer@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-05-10 16:18:00 +0000

    - Prefix the first version line with 'PHP' so it's easier to use shell tools to   parse the version number (both CLI and CGI).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@81695 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -874,7 +874,7 @@ consult the installation file that came with this distribution, or visit \n\
 							SG(headers_sent) = 1;
 							SG(request_info).no_headers = 1;
 						}
-						php_printf("%s (%s)\n%s", PHP_VERSION, sapi_module.name, get_zend_version());
+						php_printf("PHP %s (%s)\n%s", PHP_VERSION, sapi_module.name, get_zend_version());
 						php_end_ob_buffers(1 TSRMLS_CC);
 						exit(1);
 						break;
