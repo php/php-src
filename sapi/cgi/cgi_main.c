33080b3806b812133629adc8760f3cf874ed5330
edink <edink@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-05-11 14:26:30 +0000

    Added copyright notice to php -v output.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@81812 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -876,7 +876,7 @@ consult the installation file that came with this distribution, or visit \n\
 							SG(headers_sent) = 1;
 							SG(request_info).no_headers = 1;
 						}
-						php_printf("PHP %s (%s)\n%s", PHP_VERSION, sapi_module.name, get_zend_version());
+						php_printf("PHP %s (%s), Copyright (c) 1997-2002 The PHP Group\n%s", PHP_VERSION, sapi_module.name, get_zend_version());
 						php_end_ob_buffers(1 TSRMLS_CC);
 						exit(1);
 						break;
