554b7684edae7b1173096a01c83c68998c026c3b
Marko Karppinen <markonen@php.net>
2002-05-12 15:06:04 +0000

    Add an option to calculate easter dates based on the Gregorian calendar during the years 1582-1752. Earlier this was only possible from 1753 onwards. Use the optional parameter CAL_EASTER_ROMAN with easter_days() to enable this. This is a fix for bug #12766. # As you can see, my Sundays are *so* busy...

--- ext/calendar/php_calendar.h
+++ ext/calendar/php_calendar.h
@@ -32,4 +32,19 @@ PHP_FUNCTION(cal_info);
 
 #define phpext_calendar_ptr calendar_module_ptr
 
+/*
+ * Specifying the easter calculation method
+ * 
+ * DEFAULT is Anglican, ie. use Julian calendar before 1753
+ * and Gregorian after that. With ROMAN, the cutoff year is 1582.
+ * ALWAYS_GREGORIAN and ALWAYS_JULIAN force the calendar
+ * regardless of date.
+ *
+ */
+ 
+#define CAL_EASTER_DEFAULT			0
+#define CAL_EASTER_ROMAN			1
+#define CAL_EASTER_ALWAYS_GREGORIAN	2
+#define CAL_EASTER_ALWAYS_JULIAN	3
+
 #endif
