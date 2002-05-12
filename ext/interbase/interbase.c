87148b6eddd4d8a80a9d8cd8e99865309bb0379d
Daniela Mariaschi <daniela@php.net>
2002-05-12 12:18:56 +0000

    fix bug #17040 ibase_close again..... User can't close a connection if there are blobs opened. He must close them before to not loose data....

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -435,7 +435,7 @@ static void _php_ibase_free_query(ibase_query *ib_query)
 		}
 		isc_transaction_info(IB_STATUS, &ib_query->trans,sizeof(tr_items), tr_items, sizeof(tmp), tmp );
 		/* we have the trans still open and a statement to drop? */
-		if ( !(IB_STATUS[0] && IB_STATUS[0])  &&  ib_query->stmt) {
+		if ( !(IB_STATUS[0] && IB_STATUS[1])  &&  ib_query->stmt) {
 			IBDEBUG("Dropping statement handle (free_query)...");
 			if (isc_dsql_free_statement(IB_STATUS, &ib_query->stmt, DSQL_drop)){
 				_php_ibase_error();
@@ -466,7 +466,7 @@ static void _php_ibase_free_blob(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 
 	if (ib_blob->bl_handle != NULL) { /* blob open*/
 		if (isc_cancel_blob(IB_STATUS, &ib_blob->bl_handle)) {
-			_php_ibase_error();
+			php_error(E_ERROR, "You can lose data. Close any blob after reading of writing it. Use ibase_blob_close() before calling ibase_close()");
 		}
 	}
 	efree(ib_blob);
