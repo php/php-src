5d286e21eab0f721892f7bf44439232d5cd0870e
Rasmus Lerdorf <rasmus@php.net>
2002-05-16 14:41:15 +0000

    Fix for #17271 @ Fix crash bug in stripslashes() when working in sybase mode (Rasmus)

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -420,6 +420,7 @@ PHP_FUNCTION(nl_langinfo)
 	}
 }
 #endif
+/* }}} */
 
 #ifdef HAVE_STRCOLL
 /* {{{ proto int strcoll(string str1, string str2)
@@ -2130,7 +2131,7 @@ PHPAPI void php_stripslashes(char *str, int *len TSRMLS_DC)
 	t = str;
 
 	if (PG(magic_quotes_sybase)) {
-		while (l >= 0) {
+		while (l > 0) {
 			if(*t=='\'') {
 				if((l>0) && (t[1]=='\'')) {
 					t++;
@@ -2141,6 +2142,7 @@ PHPAPI void php_stripslashes(char *str, int *len TSRMLS_DC)
 			} 
 			*s++ = *t++;
 			l--;
+			php_printf("%d\n",l);
 		}
 		*s = '\0';
 		
