def8bc92fc8c1d09e6479646a39044f70e68801b
Christian Stocker <chregu@php.net>
2002-05-18 14:38:22 +0000

    mem leak fix for domxml_dump_node

--- ext/domxml/php_domxml.c
+++ ext/domxml/php_domxml.c
@@ -3184,7 +3184,8 @@ PHP_FUNCTION(domxml_dump_mem)
 	if (!size) {
 		RETURN_FALSE;
 	}
-	RETURN_STRINGL(mem, size, 1);
+	RETVAL_STRINGL(mem, size, 1);
+	xmlFree(mem);
 }
 /* }}} */
 
@@ -3253,7 +3254,6 @@ PHP_FUNCTION(domxml_dump_node)
 		RETURN_FALSE;
 	}
 	RETVAL_STRING(mem,  1);
-	
 	xmlBufferFree(buf);
 
 }
