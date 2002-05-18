62210bef1d89f77b083fe12d3b947188e8d5efe7
Christian Stocker <chregu@php.net>
2002-05-18 14:43:14 +0000

    MFH for memleak fixes in domxml_dump_mem and domxml_html_dump_mem

--- ext/domxml/php_domxml.c
+++ ext/domxml/php_domxml.c
@@ -3193,7 +3193,8 @@ PHP_FUNCTION(domxml_dump_mem)
 	if (!size) {
 		RETURN_FALSE;
 	}
-	RETURN_STRINGL(mem, size, 1);
+	RETVAL_STRINGL(mem, size, 1);
+	xmlFree(mem);
 }
 /* }}} */
 
@@ -3417,7 +3418,7 @@ PHP_FUNCTION(domxml_html_dump_mem)
 			xmlFree(mem);
 		RETURN_FALSE;
 	}
-	RETURN_STRINGL(mem, size, 1);
+	RETVAL_STRINGL(mem, size, 1);
 	xmlFree(mem);
 }
 /* }}} */
