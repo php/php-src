8e3385dfbf0868dee02d306d939b68c0e34542e7
ssb <ssb@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-05-20 10:45:23 +0000

    * _infoFromAny goes public!
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@82947 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Common.php
+++ pear/PEAR/Common.php
@@ -888,9 +888,9 @@ class PEAR_Common extends PEAR
     }
 
     // }}}
-    // {{{ _infoFromAny()
+    // {{{ infoFromAny()
 
-    function _infoFromAny($info)
+    function infoFromAny($info)
     {
         if (is_string($info) && file_exists($info)) {
             $tmp = substr($info, -4);
@@ -926,7 +926,7 @@ class PEAR_Common extends PEAR
             $_PEAR_Common_dependency_relations,
             $_PEAR_Common_file_roles,
             $_PEAR_Common_replacement_types;
-        if (PEAR::isError($info = $this->_infoFromAny($info))) {
+        if (PEAR::isError($info = $this->infoFromAny($info))) {
             return $this->raiseError($info);
         }
         if (!is_array($info)) {
@@ -1142,7 +1142,7 @@ class PEAR_Common extends PEAR
         if (!function_exists("token_get_all")) {
             return false;
         }
-        if (PEAR::isError($info = $this->_infoFromAny($any))) {
+        if (PEAR::isError($info = $this->infoFromAny($any))) {
             return $this->raiseError($info);
         }
         if (!is_array($info)) {
