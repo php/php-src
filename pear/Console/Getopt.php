64f6b9bdc0080ea3b70def3fd285283901712f5f
SVN Migration <svn@php.net>
2002-05-26 12:54:20 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_10'.

--- pear/Console/Getopt.php
+++ pear/Console/Getopt.php
@@ -77,9 +77,6 @@ class Console_Getopt {
         if ($long_options)
             sort($long_options);
 
-        if ($args[0]{0} != '-') {
-            array_shift($args);
-        }
         reset($args);
         while (list($i, $arg) = each($args)) {
 
