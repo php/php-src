dd1bf198dcafba0bf261d581680615c4248f636a
Venkat Raghavan S <rvenkat@php.net>
2002-05-28 05:33:08 +0000

    *** empty log message ***

---
+++ netware/php-nw.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM CodeWarrior stuff
+set path=%path%;P:\APPS\SCRIPT\SW\CW71-4\BIN
+
+REM Cygwin stuff
+set path=%path%;R:\SCRIPT\PHP\CYGWIN\BIN
+set CYGWIN=R:\SCRIPT\PHP\CYGWIN
+
+set BUILD=release
+
