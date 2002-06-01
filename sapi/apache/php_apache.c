3393e74a96aaad9b04226e69eeab90f14d3b1c1d
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-06-01 10:27:51 +0000

    - Reapply netware patch
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@84310 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache/php_apache.c
+++ sapi/apache/php_apache.c
@@ -80,7 +80,7 @@ static void php_apache_globals_ctor(php_apache_info_struct *apache_globals TSRML
 static PHP_MINIT_FUNCTION(apache)
 {
 #ifdef ZTS
-	ts_allocate_id(&php_apache_info_id, sizeof(php_apache_info_struct), php_apache_globals_ctor, NULL);
+	ts_allocate_id(&php_apache_info_id, sizeof(php_apache_info_struct), (ts_allocate_ctor) php_apache_globals_ctor, NULL);
 #else
 	php_apache_globals_ctor(&php_apache_info TSRMLS_CC);
 #endif
