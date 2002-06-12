a00ecc75ca0f4d95d55248eace56d22f262d9f4c
cox <cox@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-06-12 16:31:53 +0000

    Attempt to make pear.bat work better under Windows
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@85483 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/scripts/pear.bat
+++ pear/scripts/pear.bat
@@ -16,16 +16,22 @@ REM ----------------------------------------------------------------------
 REM  Authors:     Alexander Merz (alexmerz@php.net)                         
 REM ----------------------------------------------------------------------
 REM
-REM  $Id: pear.bat,v 1.6 2002/04/09 09:28:04 alexmerz Exp $
+REM  $Id: pear.bat,v 1.7 2002/04/09 09:59:17 imajes Exp $
 
-REM change this four lines to match the paths of your system
+REM change this lines to match the paths of your system
 REM -------------------
-set PHP_PATH=c:\php
-set PEAR_INSTALL_DIR=c:\php\pear
-set PEAR_EXTENSION_DIR=c:\php\extensions
-set PEAR_DOC_DIR=c:\php\pear\docs
-REM -------------------
-set DIRECTORY_SEPARATOR=\
 
-%PHP_PATH%\php.exe -q %PEAR_INSTALL_DIR%\scripts\pear.in %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM *** Unused stuff ****
+REM set PHP_PATH=c:\php
+REM set PEAR_INSTALL_DIR=c:\php\pear
+REM set PEAR_EXTENSION_DIR=c:\php\extensions
+REM set PEAR_DOC_DIR=c:\php\pear\docs
+REM -------------------
+REM set DIRECTORY_SEPARATOR=\
+REM %PHP_PATH%\php.exe -q %PEAR_INSTALL_DIR%\scripts\pear.in %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+set PHP_BIN=c:\php\php.exe
+set PEAR_PATH=c:\php\pear
+
+%PHP_BIN% -C -d include_path=%PEAR_PATH% -f %PEAR_PATH%\scripts\pear.in %1 %2 %3 %4 %5 %6 %7 %8 %9
 @ECHO ON
