2fda2eb4a879b52540557954783d003da63e0412
Derick Rethans <derick@php.net>
2002-06-28 14:45:10 +0000

    - YAAC: I hope this is the last one!

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -564,8 +564,8 @@ static void php_register_hook(apr_pool_t *p)
 	ap_hook_post_config(php_apache_server_startup, NULL, NULL, APR_HOOK_MIDDLE);
 	ap_hook_insert_filter(php_insert_filter, NULL, NULL, APR_HOOK_MIDDLE);
 	ap_hook_post_read_request(php_post_read_request, NULL, NULL, APR_HOOK_MIDDLE);
-	ap_register_output_filter("PHP", php_output_filter, AP_FTYPE_RESOURCE);
-	ap_register_input_filter("PHP", php_input_filter, AP_FTYPE_RESOURCE);
+	ap_register_output_filter("PHP", php_output_filter, NULL, AP_FTYPE_RESOURCE);
+	ap_register_input_filter("PHP", php_input_filter, NULL, AP_FTYPE_RESOURCE);
 }
 
 AP_MODULE_DECLARE_DATA module php4_module = {
