e1483035b69bf7d52ea25e17c9b95c4bb4d2dc9b
Christian Stocker <chregu@php.net>
2002-07-01 14:48:38 +0000

    outcomment everything related to domxml_elem_set_attribute_node(). This function is not implemented and shouldn't show up at all for the time being

--- ext/domxml/php_domxml.h
+++ ext/domxml/php_domxml.h
@@ -163,7 +163,9 @@ PHP_FUNCTION(domxml_elem_get_attribute);
 PHP_FUNCTION(domxml_elem_set_attribute);
 PHP_FUNCTION(domxml_elem_remove_attribute);
 PHP_FUNCTION(domxml_elem_get_attribute_node);
+/* since this function is not really implemented, outcomment it for the time beeing
 PHP_FUNCTION(domxml_elem_set_attribute_node);
+*/
 PHP_FUNCTION(domxml_elem_get_elements_by_tagname);
 PHP_FUNCTION(domxml_elem_has_attribute);
 /* Class CData methods */
