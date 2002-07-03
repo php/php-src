7651f66f80dd9a03fe4abac52a7b0abb4a16fbd1
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-07-03 03:18:55 +0000

    Fix the nasty crash. And Sterling..test next time before you commit?
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@87536 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/filestat.c
+++ ext/standard/filestat.c
@@ -806,8 +806,9 @@ FileFunction(PHP_FN(filetype), FS_TYPE)
 PHP_FUNCTION(is_writable)
 {
 	char *filename;
+	int filename_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
 		return;
 	}
 
@@ -820,8 +821,9 @@ PHP_FUNCTION(is_writable)
 PHP_FUNCTION(is_readable)
 {
 	char *filename;
+	int filename_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
 		return;
 	}
 
@@ -834,8 +836,9 @@ PHP_FUNCTION(is_readable)
 PHP_FUNCTION(is_executable) 
 {
 	char *filename;
+	int filename_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
 		return;
 	}
 
@@ -863,8 +866,9 @@ FileFunction(PHP_FN(is_link), FS_IS_LINK)
 PHP_FUNCTION(file_exists)
 {
 	char *filename;
+	int filename_len;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
 		return;
 	}
 	
