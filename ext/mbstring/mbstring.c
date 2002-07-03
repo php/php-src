1c3680113eefa3fec75c4fdd7597d7f5b96dc524
hirokawa <hirokawa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-07-03 13:36:19 +0000

    fixed shift_jis character corruption including 0x5c as second byte on uploaded filename.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@87585 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -1323,6 +1323,27 @@ SAPI_POST_HANDLER_FUNC(php_mbstr_post_handler)
 }
 #endif
 
+
+#define IS_SJIS1(c) ((((c)>=0x81 && (c)<=0x9f) || ((c)>=0xe0 && (c)<=0xf5)) ? 1 : 0)
+#define IS_SJIS2(c) ((((c)>=0x40 && (c)<=0x7e) || ((c)>=0x80 && (c)<=0xfc)) ? 1 : 0)
+
+char *mbstr_strrchr(const char *s, char c){
+	unsigned char *p = (unsigned char *)s, *last = NULL;
+	while(*p++) {
+		if (*p == c) {
+			last = p;
+		}
+		if (*p == '\0'){
+			break;
+		}
+		if (MBSTRG(current_language) == mbfl_no_language_japanese 
+			&& IS_SJIS1(*p) && IS_SJIS2(*(p+1))) {
+			p++;
+		}
+	}
+	return last;
+}
+
 /* http input processing */
 void mbstr_treat_data(int arg, char *str, zval* destArray TSRMLS_DC)
 {
