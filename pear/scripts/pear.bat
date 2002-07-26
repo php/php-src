7bf6b4ccee9f7c185bd06a740371f1d875c86047
Tomas V.V.Cox <cox@php.net>
2002-07-26 10:48:20 +0000

    Use script arg separator '--' Clean up unused stuff

--- pear/scripts/pear.bat
+++ pear/scripts/pear.bat
@@ -16,22 +16,13 @@ REM ----------------------------------------------------------------------
 REM  Authors:     Alexander Merz (alexmerz@php.net)                         
 REM ----------------------------------------------------------------------
 REM
-REM  $Id: pear.bat,v 1.7 2002/04/09 09:59:17 imajes Exp $
+REM  $Id: pear.bat,v 1.8 2002/06/12 16:31:53 cox Exp $
 
 REM change this lines to match the paths of your system
 REM -------------------
 
-REM *** Unused stuff ****
-REM set PHP_PATH=c:\php
-REM set PEAR_INSTALL_DIR=c:\php\pear
-REM set PEAR_EXTENSION_DIR=c:\php\extensions
-REM set PEAR_DOC_DIR=c:\php\pear\docs
-REM -------------------
-REM set DIRECTORY_SEPARATOR=\
-REM %PHP_PATH%\php.exe -q %PEAR_INSTALL_DIR%\scripts\pear.in %1 %2 %3 %4 %5 %6 %7 %8 %9
-
 set PHP_BIN=c:\php\php.exe
 set PEAR_PATH=c:\php\pear
 
-%PHP_BIN% -C -d include_path=%PEAR_PATH% -f %PEAR_PATH%\scripts\pear.in %1 %2 %3 %4 %5 %6 %7 %8 %9
+%PHP_BIN% -C -d include_path=%PEAR_PATH% -f %PEAR_PATH%\scripts\pear.in -- %1 %2 %3 %4 %5 %6 %7 %8 %9
 @ECHO ON
