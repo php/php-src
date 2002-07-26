887eb6de392682f2d0b0d8e299d95b30c91beffa
cox <cox@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-07-26 10:11:01 +0000

    use $GLOBALS['HTTP_SERVER_VARS'] instead
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@89872 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/Console/Getopt.php
+++ pear/Console/Getopt.php
@@ -210,10 +210,10 @@ class Console_Getopt {
         global $argv;
         if (!is_array($argv)) {
             if (!@is_array($_SERVER['argv'])) {
-                if (!@is_array($HTTP_SERVER_VARS['argv'])) {
+                if (!@is_array($GLOBALS['HTTP_SERVER_VARS']['argv'])) {
                     return PEAR::raiseError("Console_Getopt: Could not read cmd args (register_argc_argv=Off?)");
                 }
-                return $HTTP_SERVER_VARS['argv'];
+                return $GLOBALS['HTTP_SERVER_VARS']['argv'];
             }
             return $_SERVER['argv'];
         }
