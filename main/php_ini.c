8ae2d738e75689c083797b78f9a18c828a19ab2f
edink <edink@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-07-29 12:17:34 +0000

    First try to open php-SAPI.ini and if it fails fall back to regular php.ini file. For example cli version of PHP will first try to open php-cli.ini in the usual ini file search path and only if it fails will it try to open php.ini using the same path.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@90306 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_ini.c
+++ main/php_ini.c
@@ -312,7 +312,12 @@ int php_init_config()
 	}
 	/* Search php.ini file in search path */
 	if (!fh.handle.fp) {
-		fh.handle.fp = php_fopen_with_path("php.ini", "r", php_ini_search_path, &php_ini_opened_path TSRMLS_CC);
+		char *ini_fname=emalloc(10+strlen(sapi_module.name));
+		sprintf(ini_fname, "php-%s.ini", sapi_module.name);
+		if (!(fh.handle.fp = php_fopen_with_path(ini_fname, "r", php_ini_search_path, &php_ini_opened_path TSRMLS_CC))) {
+			fh.handle.fp = php_fopen_with_path("php.ini", "r", php_ini_search_path, &php_ini_opened_path TSRMLS_CC);
+		}
+		efree(ini_fname);
 		fh.filename = php_ini_opened_path;
 	}
 	if (free_ini_search_path) {
