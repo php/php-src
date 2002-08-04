b038238f0bfd8d5316ca9beca24ba89c5818251f
shane <shane@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-08-04 23:45:38 +0000

    need to export var_replace and var_destroy for win32
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@90941 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_var.h
+++ ext/standard/php_var.h
@@ -55,8 +55,8 @@ PHPAPI int php_var_unserialize(zval **rval, const char **p, const char *max, php
 #define PHP_VAR_UNSERIALIZE_DESTROY(var_hash) \
 	var_destroy(&(var_hash))
 
-void var_replace(php_unserialize_data_t *var_hash, zval *ozval, zval **nzval);
-void var_destroy(php_unserialize_data_t *var_hash);
+PHPAPI void var_replace(php_unserialize_data_t *var_hash, zval *ozval, zval **nzval);
+PHPAPI void var_destroy(php_unserialize_data_t *var_hash);
 
 #define PHP_VAR_UNSERIALIZE_ZVAL_CHANGED(var_hash, ozval, nzval) \
 	var_replace((var_hash), (ozval), &(nzval))
