811eacb67317962bd799e718119962ed951f71df
kalowsky <kalowsky@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-08-06 03:48:15 +0000

    Keeping function prototype variables the same
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@91054 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -2174,7 +2174,7 @@ static void exif_process_SOFn (uchar *Data, int marker, jpeg_sof_info *result)
 /* }}} */
 
 /* forward declarations */
-static int exif_process_IFD_in_JPEG(image_info_type *ImageInfo, char *dir_entry, char *offset_base, size_t IFDlength, size_t displacement, int section_index TSRMLS_DC);
+static int exif_process_IFD_in_JPEG(image_info_type *ImageInfo, char *dir_start, char *offset_base, size_t IFDlength, size_t displacement, int section_index TSRMLS_DC);
 static int exif_process_IFD_TAG(    image_info_type *ImageInfo, char *dir_entry, char *offset_base, size_t IFDlength, size_t displacement, int section_index, int ReadNextIFD, tag_table_type tag_table TSRMLS_DC);
 
 /* {{{ exif_get_markername
