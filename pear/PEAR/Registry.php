5d4b690bab00d8a98fe8a81eb89dc30aab235620
Tomas V.V.Cox <cox@php.net>
2002-08-06 16:35:19 +0000

    Changed Windows 9x detection code (reported by Alex Merz)

--- pear/PEAR/Registry.php
+++ pear/PEAR/Registry.php
@@ -254,7 +254,7 @@ class PEAR_Registry extends PEAR
      */
     function _lock($mode = LOCK_EX)
     {
-        if (!strstr(php_uname(), 'Windows 95/98')) {
+        if (!eregi('Windows 9', php_uname())) {
             if ($mode != LOCK_UN && is_resource($this->lock_fp)) {
                 // XXX does not check type of lock (LOCK_SH/LOCK_EX)
                 return true;
