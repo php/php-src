3b646f0e5d36b4de79666639c8793746bee11c88
Zeev Suraski <zeev@php.net>
2002-08-17 20:32:26 +0000

    Use mtime instead of atime, as we always update the session file anyway.

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -202,7 +202,7 @@ static int ps_files_cleanup_dir(const char *dirname, int maxlifetime TSRMLS_DC)
 				buf[dirname_len + entry_len + 1] = '\0';
 				/* check whether its last access was more than maxlifet ago */
 				if (VCWD_STAT(buf, &sbuf) == 0 && 
-						(now - sbuf.st_atime) > maxlifetime) {
+						(now - sbuf.st_mtime) > maxlifetime) {
 					VCWD_UNLINK(buf);
 					nrdels++;
 				}
