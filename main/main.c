2a71d7d5bb7f58bf0bf6cc5e56d61ad4469aead5
Wez Furlong <wez@php.net>
2002-08-23 17:39:20 +0000

    Respect safe mode setting when opening scripts and things for the ZE.

--- main/main.c
+++ main/main.c
@@ -711,7 +711,7 @@ static FILE *php_fopen_wrapper_for_zend(const char *filename, char **opened_path
 {
 	TSRMLS_FETCH();
 
-	return php_stream_open_wrapper_as_file((char *)filename, "rb", USE_PATH|IGNORE_URL_WIN|REPORT_ERRORS, opened_path);
+	return php_stream_open_wrapper_as_file((char *)filename, "rb", ENFORCE_SAFE_MODE|USE_PATH|IGNORE_URL_WIN|REPORT_ERRORS, opened_path);
 }
 /* }}} */
 
