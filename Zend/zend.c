bdc35c9f2e3fe22cb88c1997d674ea6f67ad1b09
Thies C. Arntzen <thies@php.net>
2002-08-28 14:43:32 +0000

    clear current_execute_data on bailout as it would point into some freed area on the stack.

--- Zend/zend.c
+++ Zend/zend.c
@@ -549,6 +549,7 @@ ZEND_API void _zend_bailout(char *filename, uint lineno)
 	}
 	CG(unclean_shutdown) = 1;
 	CG(in_compilation) = EG(in_execution) = 0;
+	EG(current_execute_data) = NULL;
 	longjmp(EG(bailout), FAILURE);
 }
 END_EXTERN_C()
