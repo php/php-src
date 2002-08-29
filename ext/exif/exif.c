38247a79937298da0063f21675083a5e7f12b0ff
Marcus Boerger <helly@php.net>
2002-08-29 08:32:41 +0000

    One zero to much

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -118,7 +118,7 @@ PHP_MINFO_FUNCTION(exif)
 	php_info_print_table_start();
 	php_info_print_table_row(2, "EXIF Support", "enabled");
 	php_info_print_table_row(2, "EXIF Version", EXIF_VERSION);
-	php_info_print_table_row(2, "Supported EXIF Version", "02200");
+	php_info_print_table_row(2, "Supported EXIF Version", "0220");
 	php_info_print_table_row(2, "Supported filetypes", "JPEG,TIFF");
 	php_info_print_table_end();
 }
