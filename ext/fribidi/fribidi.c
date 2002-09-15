b5f1426c9ca42e225aa42c9f4b8c0c33b20e577f
tal <tal@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-09-15 09:58:35 +0000

    * More work on returning errors properly and freeing resources
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@95783 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/fribidi/fribidi.c
+++ ext/fribidi/fribidi.c
@@ -12,7 +12,8 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Author: Onn Ben-Zvi <onn@zend.com>, onnb@mercury.co.il               |
+   | Authors: Onn Ben-Zvi <onnb@mercury.co.il>                            |
+   |          Tal Peer <tal@php.net>
    +----------------------------------------------------------------------+
  */
 
@@ -236,7 +237,14 @@ PHP_FUNCTION(fribidi_log2vis)
 			fribidi_unicode_to_isiri_3342(u_visual_str, len , outString);
 			break;
 		default:
-			zend_error(E_ERROR,"unknown character set %d<br />", Z_LVAL_PP(parameter3));
+			php_error_docref(NULL TSRMLS_CC, E_ERROR, "Unknown charset");
+			efree(u_logical_str);
+			efree(u_visual_str);
+			efree(position_L_to_V_list);
+			efree(position_V_to_L_list);
+			efree(embedding_level_list);
+			efree(outString);
+			RETURN_FALSE;
 	}
 	
 
