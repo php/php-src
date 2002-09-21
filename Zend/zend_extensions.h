21e9bc0fb313485c722a10ade72968ea7a1ab261
Andi Gutmans <andi@php.net>
2002-09-21 13:01:08 +0000

    - Someone screwed this up.

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO	220020901
+#define ZEND_EXTENSION_API_NO	20020901
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
