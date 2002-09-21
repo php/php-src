bcadd534ee3d10888a7c95394f0d413ecc050d25
Markus Fischer <mfischer@php.net>
2002-09-21 15:03:24 +0000

    - Forgot to add an extra space.

--- win32/sendmail.c
+++ win32/sendmail.c
@@ -265,7 +265,7 @@ int TSendMail(char *host, int *error, char **error_message,
 			return FAILURE;
 		}
 		snprintf(*error_message, HOST_NAME_LEN + 128,
-			"Failed to connect to mailserver at \"%s\" port %d, verify your \"SMTP\""
+			"Failed to connect to mailserver at \"%s\" port %d, verify your \"SMTP\" "
 			"and \"smtp_port\" setting in php.ini or use ini_set()",
 			MailHost, !INI_INT("smtp_port") ? 25 : INI_INT("smtp_port"));
 		return FAILURE;
