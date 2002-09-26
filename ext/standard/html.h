b5d4b5496d0c61283af576903a47b6cb5d04f1b3
Sebastian Bergmann <sebastian@php.net>
2002-09-26 18:13:32 +0000

    Fix ZTS build.

--- ext/standard/html.h
+++ ext/standard/html.h
@@ -36,6 +36,6 @@ PHP_FUNCTION(htmlentities);
 PHP_FUNCTION(html_entity_decode);
 PHP_FUNCTION(get_html_translation_table);
 
-PHPAPI char *php_escape_html_entities(unsigned char *old, int oldlen, int *newlen, int all, int quote_style, char *hint_charset);
+PHPAPI char *php_escape_html_entities(unsigned char *old, int oldlen, int *newlen, int all, int quote_style, char *hint_charset TSRMLS_DC);
 
 #endif /* HTML_H */
