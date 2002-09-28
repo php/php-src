5a239ab0593f74610a3a554df93dbad3204cef6d
Rasmus Lerdorf <rasmus@php.net>
2002-09-28 16:27:10 +0000

    Uh, this is what I meant to do.  Fix for #19292

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -200,8 +200,8 @@ PHPAPI int php_check_open_basedir(const char *path TSRMLS_DC)
 PHPAPI int php_check_safe_mode_include_dir(char *path TSRMLS_DC)
 {
 	/* Only check when safe_mode or open_basedir is on and safe_mode_include_dir is available */
-	if (((PG(open_basedir) && *PG(open_basedir)) || PG(safe_mode)) && PG(safe_mode_include_dir) &&
-			*PG(safe_mode_include_dir))
+	if (((PG(open_basedir) && *PG(open_basedir)) || PG(safe_mode)) && 
+			PG(safe_mode_include_dir) && *PG(safe_mode_include_dir))
 	{
 		char *pathbuf;
 		char *ptr;
@@ -242,7 +242,7 @@ PHPAPI int php_check_safe_mode_include_dir(char *path TSRMLS_DC)
 	}
 
 	/* Nothing to check... */
-	return -1;
+	return 0;
 }
 /* }}} */
 
