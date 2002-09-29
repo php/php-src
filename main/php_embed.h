b5f37fca9928953b728a353f567f89d9c2398354
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-09-29 10:00:07 +0000

    - Fix Edin's name
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@97339 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_embed.h
+++ main/php_embed.h
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Author: Edin Kadrinasic <edink@php.net>                              |
+   | Author: Edin Kadribasic <edink@php.net>                              |
    +----------------------------------------------------------------------+
 */
 /* $Id$ */
