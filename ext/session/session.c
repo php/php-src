6c9acdbbe236d7d13635adb15b602e1e15448ee1
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-09-29 18:33:14 +0000

    Fixed a crash, which would occur when save_handler is invalid.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@97369 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/session.c
+++ ext/session/session.c
@@ -538,6 +538,11 @@ static void php_session_initialize(TSRMLS_D)
 	char *val;
 	int vallen;
 
+	if (!PS(mod)) {
+		php_error_docref(NULL TSRMLS_CC, E_ERROR, "Failed to initialize session module.");
+		return;
+	}
+
 	/* Open session handler first */
 	if (PS(mod)->open(&PS(mod_data), PS(save_path), PS(session_name) TSRMLS_CC) == FAILURE) {
 		php_error(E_ERROR, "Failed to initialize session module");
