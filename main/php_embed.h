583b015642cfcf4de61be31ace2d88c4cad33100
edink <edink@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-09-29 02:45:25 +0000

    Added php/embed toolkit for embedding PHP engine into C/C++ applications. See my post to php-dev on the subject.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@97330 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ main/php_embed.h
@@ -0,0 +1,60 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 4                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2002 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Edin Kadrinasic <edink@php.net>                              |
+   +----------------------------------------------------------------------+
+*/
+/* $Id$ */
+
+#ifndef _PHP_EMBED_H_
+#define _PHP_EMBED_H_
+
+#include <main/php.h>
+#include <main/SAPI.h>
+#include <main/php_main.h>
+#include <main/php_variables.h>
+#include <main/php_ini.h>
+#include <zend_ini.h>
+
+#ifdef ZTS
+#define PTSRMLS_D        void ****ptsrm_ls
+#define PTSRMLS_DC       , PTSRMLS_D
+#define PTSRMLS_C        &tsrm_ls
+#define PTSRMLS_CC       , PTSRMLS_C
+#else
+#define PTSRMLS_D
+#define PTSRMLS_DC
+#define PTSRMLS_C
+#define PTSRMLS_CC
+#endif
+
+#define PHP_EMBED_START_BLOCK(x,y) { \
+    void ***tsrm_ls; \
+    php_embed_init(x, y PTSRMLS_CC); \
+    zend_first_try {
+
+#define PHP_EMBED_END_BLOCK() \
+  } zend_catch { \
+    /* int exit_status = EG(exit_status); */ \
+  } zend_end_try(); \
+  php_embed_shutdown(TSRMLS_C); \
+}
+
+BEGIN_EXTERN_C() 
+int php_embed_init(int argc, char **argv PTSRMLS_DC);
+void php_embed_shutdown(TSRMLS_D);
+END_EXTERN_C()
+
+
+#endif /* _PHP_EMBED_H_ */
