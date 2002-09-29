3cf681c1d97e6ad161f3a5335a8c5948987c80fb
foobar <sniper@php.net>
2002-09-29 18:53:19 +0000

    Make it compile with the latest _release_ too..

--- ext/fribidi/fribidi.c
+++ ext/fribidi/fribidi.c
@@ -32,8 +32,7 @@
 #include <fribidi/fribidi.h>
 
 /* The fribidi guys dont believe in BC */
-#ifndef FRIBIDI_CHARSET_NOT_FOUND
-
+#ifdef FRIBIDI_CHAR_SET_UTF8
 #define FRIBIDI_CHARSET_UTF8                FRIBIDI_CHAR_SET_UTF8
 #define FRIBIDI_CHARSET_ISO8859_6           FRIBIDI_CHAR_SET_ISO8859_6
 #define FRIBIDI_CHARSET_ISO8859_8           FRIBIDI_CHAR_SET_ISO8859_8
@@ -41,7 +40,6 @@
 #define FRIBIDI_CHARSET_CP1256              FRIBIDI_CHAR_SET_CP1256
 #define FRIBIDI_CHARSET_ISIRI_3342          FRIBIDI_CHAR_SET_ISIRI_3342
 #define FRIBIDI_CHARSET_CAP_RTL             FRIBIDI_CHAR_SET_CAP_RTL
-
 #endif
 
 function_entry fribidi_functions[] = {
