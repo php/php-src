164188d122e4eb517182920bc975b02e8b101ef4
Dan Kalowsky <kalowsky@php.net>
2002-10-07 21:58:56 +0000

    Fix for bug #19798 (submitted by mclap@simpage.mv.ru)

--- ext/standard/quot_print.c
+++ ext/standard/quot_print.c
@@ -80,7 +80,7 @@ PHP_FUNCTION(quoted_printable_decode)
 		case '=':
 			if (str_in[i + 1] && str_in[i + 2] && 
 				isxdigit((int) str_in[i + 1]) && 
-				isxdigit((int) str_in[i + 1]))
+				isxdigit((int) str_in[i + 2]))
 			{
 				str_out[j++] = (php_hex2int((int) str_in[i + 1]) << 4) 
 						+ php_hex2int((int) str_in[i + 2]);
