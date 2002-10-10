bb4aefacd0fff07f602bedd76bec3829e75db069
Andrei Zmievski <andrei@php.net>
2002-10-10 17:34:12 +0000

    Update to 4.3.0-pre1.

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.0-dev"
+#define PHP_EXTRA_VERSION "-pre1"
+#define PHP_VERSION "4.3.0-pre1"
