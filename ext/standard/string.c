524c9ef26ae2cfa6ed9b0be7c4d4a37581132a30
Sander Roobol <sander@php.net>
2002-10-11 12:42:01 +0000

    Fixed problems with the new nl2br() and beautified the code a little. Closes #19858.

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -3061,10 +3061,10 @@ PHP_FUNCTION(nl2br)
 	}
 	
 	if (repl_cnt == 0) {
-		RETURN_STRINGL(str, Z_STRLEN_PP(zstr), 1);
+		RETURN_STRINGL(Z_STRVAL_PP(zstr), Z_STRLEN_PP(zstr), 1);
 	}
 
-	new_length = Z_STRLEN_PP(zstr) + repl_cnt * 6;
+	new_length = Z_STRLEN_PP(zstr) + repl_cnt * (sizeof("<br />") - 1);
 	tmp = target = emalloc(new_length + 1);
 
 	str = Z_STRVAL_PP(zstr);
@@ -3091,7 +3091,7 @@ PHP_FUNCTION(nl2br)
 		str++;
 	}
 	
-	*target = 0;
+	*target = '\0';
 
 	RETURN_STRINGL(tmp, new_length, 0);
 }
