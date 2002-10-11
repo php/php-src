b7917def1346ef0374c551df7e9b056c803db324
Ilia Alshanetsky <iliaa@php.net>
2002-10-11 14:48:25 +0000

    Fixed bug #19865

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -138,7 +138,7 @@ php_memnstr(char *haystack, char *needle, int needle_len, char *end)
 	end -= needle_len;
 
 	while (p <= end) {
-		if ((p = strchr(p, *needle)) &&  ne == p[needle_len-1]) {
+		if ((p = memchr(p, *needle, (end-p+1))) && ne == p[needle_len-1]) {
 			if (!memcmp(needle, p, needle_len-1)) {
 				return p;
 			}
