63212d27ee233ce0c71674ec6a6f579c2ab17c1a
Ilia Alshanetsky <iliaa@php.net>
2002-10-13 15:28:21 +0000

    Increased the API number. (re: floats patch)

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -23,7 +23,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO	90020901
+#define ZEND_EXTENSION_API_NO	90021012
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
