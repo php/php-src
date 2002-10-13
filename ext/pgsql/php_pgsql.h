0a0ac7a1d973318957305891a9ceae09208bbb3d
yohgaki <yohgaki@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-10-13 09:37:27 +0000

    Introduce connect_type option to pg_connect(). pg_connect(conn_str, conn_type) is allowed. @Added PGSQL_CONNECT_FORCE_NEW option to pg_connect() (Yasuo) # If you have better idea about constant name(s), let me know.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@99555 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/php_pgsql.h
+++ ext/pgsql/php_pgsql.h
@@ -140,6 +140,8 @@ PHP_FUNCTION(pg_update);
 PHP_FUNCTION(pg_delete);
 PHP_FUNCTION(pg_select);
 
+/* connection options - ToDo: Add async connection option */
+#define PGSQL_CONNECT_FORCE_NEW     (1<<1)
 /* php_pgsql_convert options */
 #define PGSQL_CONV_IGNORE_DEFAULT   (1<<1)     /* Do not use DEAFULT value by removing field from returned array */
 #define PGSQL_CONV_FORCE_NULL       (1<<2)     /* Convert to NULL if string is null string */
