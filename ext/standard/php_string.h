ad24c5b634c1ca9a1f478cb1f553e84c9c93be54
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-10-17 22:44:44 +0000

    Renamed word_count to str_word_count to comply with naming conventions. Thanks Andi, for catching this oversight.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@100071 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -83,7 +83,7 @@ PHP_FUNCTION(substr_count);
 PHP_FUNCTION(str_pad);
 PHP_FUNCTION(sscanf);
 PHP_FUNCTION(str_shuffle);
-PHP_FUNCTION(word_count);
+PHP_FUNCTION(str_word_count);
 #ifdef HAVE_STRCOLL
 PHP_FUNCTION(strcoll);
 #endif
