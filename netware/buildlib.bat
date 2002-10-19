c326e43a95253d1a99235f04cd323ff729caa17d
SVN Migration <svn@php.net>
2002-10-19 09:52:01 +0000

    This commit was manufactured by cvs2svn to create branch 'PHP_4_2_0'.

---
+++ netware/buildlib.bat
@@ -0,0 +1,18 @@
+@echo off
+
+cd ..
+
+if "%1" == "clean" make -f netware\phplib.mak clean
+if "%1" == "/?" goto USAGE
+
+REM Build command
+make -f netware\phplib.mak
+goto EXIT
+
+:USAGE
+ @echo on
+ @echo Usage: buildlib [clean]
+ @echo clean - delete all object files and binaries before building
+
+:EXIT
+cd netware
