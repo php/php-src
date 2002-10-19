584849eb19fb27621f6fd18a0e373de063c29af3
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-10-19 09:52:00 +0000

    Allow ifd start at offset other than 00000007h and provide testcase. #Now we can read images modified by Exifer.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@100211 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -3078,6 +3078,8 @@ static int exif_process_IFD_in_JPEG(image_info_type *ImageInfo, char *dir_start,
 */
 static void exif_process_TIFF_in_JPEG(image_info_type *ImageInfo, char *CharBuf, size_t length, size_t displacement TSRMLS_DC)
 {
+	unsigned exif_value_2a, offset_of_ifd;
+
 	/* set the thumbnail stuff to nothing so we can test to see if they get set up */
 	if (memcmp(CharBuf, "II", 2) == 0) {
 		ImageInfo->motorola_intel = 0;
@@ -3089,16 +3091,16 @@ static void exif_process_TIFF_in_JPEG(image_info_type *ImageInfo, char *CharBuf,
 	}
 
 	/* Check the next two values for correctness. */
-	if (php_ifd_get16u(CharBuf+2, ImageInfo->motorola_intel) != 0x2a
-	  || php_ifd_get32u(CharBuf+4, ImageInfo->motorola_intel) != 0x08
-	) {
+	exif_value_2a = php_ifd_get16u(CharBuf+2, ImageInfo->motorola_intel);
+	offset_of_ifd = php_ifd_get32u(CharBuf+4, ImageInfo->motorola_intel);
+	if ( exif_value_2a != 0x2a || offset_of_ifd < 0x08) {
 		exif_error_docref(NULL TSRMLS_CC, ImageInfo, E_WARNING, "invalid TIFF start (1)");
 		return;
 	}
 
 	ImageInfo->sections_found |= FOUND_IFD0;
 	/* First directory starts at offset 8. Offsets starts at 0. */
-	exif_process_IFD_in_JPEG(ImageInfo, CharBuf+8, CharBuf, length/*-14*/, displacement, SECTION_IFD0 TSRMLS_CC);
+	exif_process_IFD_in_JPEG(ImageInfo, CharBuf+offset_of_ifd, CharBuf, length/*-14*/, displacement, SECTION_IFD0 TSRMLS_CC);
 
 #ifdef EXIF_DEBUG
 	exif_error_docref(NULL TSRMLS_CC, ImageInfo, E_NOTICE, "process TIFF in JPEG done");
