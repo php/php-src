ae44c4cd9e75035d65cf16065f7c2a64535c475b
moriyoshi <moriyoshi@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-10-25 08:51:59 +0000

    Added two new rows to MINFO that describes iconv library info.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@101239 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -144,11 +144,21 @@ PHP_MSHUTDOWN_FUNCTION(miconv)
 
 PHP_MINFO_FUNCTION(miconv)
 {
+	zval iconv_impl, iconv_ver;
+
+	zend_get_constant("ICONV_IMPL", sizeof("ICONV_IMPL")-1, &iconv_impl);
+	zend_get_constant("ICONV_VERSION", sizeof("ICONV_VERSION")-1, &iconv_ver);
+
 	php_info_print_table_start();
-	php_info_print_table_header(2, "iconv support", "enabled");
+	php_info_print_table_row(2, "iconv support", "enabled");
+	php_info_print_table_row(2, "iconv implementation", Z_STRVAL(iconv_impl));
+	php_info_print_table_row(2, "iconv library version", Z_STRVAL(iconv_ver));
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();
+
+	zval_dtor(&iconv_impl);
+	zval_dtor(&iconv_ver);
 }
 #
 #define PHP_ICONV_CONVERTER			1
