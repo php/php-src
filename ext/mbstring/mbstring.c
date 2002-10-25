36447ebe050ac0891d90d3ebff099b8c960094ac
Moriyoshi Koizumi <moriyoshi@php.net>
2002-10-25 09:36:36 +0000

    Fixed null value check in php_mb_mbchar_bytes_ex() as it should be

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -3451,7 +3451,7 @@ PHPAPI size_t php_mb_mbchar_bytes_ex(const char *s, const mbfl_encoding *enc)
 	if (enc != NULL) {
 		if(enc->flag & MBFL_ENCTYPE_MBCS) {
 			if(enc->mblen_table != NULL) {
-				if (*s != '\0') return enc->mblen_table[*(unsigned char *)s];
+				if (s != NULL) return enc->mblen_table[*(unsigned char *)s];
 			}
 		} else if (enc->flag & (MBFL_ENCTYPE_WCS2BE | MBFL_ENCTYPE_WCS2LE)) {
 			return 2;
