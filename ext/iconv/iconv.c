63a9d4fa2ea7462bf4a2238fc1d308879c10d737
Marcus Boerger <helly@php.net>
2002-10-25 11:50:35 +0000

    fix ZTS build

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -146,8 +146,8 @@ PHP_MINFO_FUNCTION(miconv)
 {
 	zval iconv_impl, iconv_ver;
 
-	zend_get_constant("ICONV_IMPL", sizeof("ICONV_IMPL")-1, &iconv_impl);
-	zend_get_constant("ICONV_VERSION", sizeof("ICONV_VERSION")-1, &iconv_ver);
+	zend_get_constant("ICONV_IMPL", sizeof("ICONV_IMPL")-1, &iconv_impl TSRMLS_CC);
+	zend_get_constant("ICONV_VERSION", sizeof("ICONV_VERSION")-1, &iconv_ver TSRMLS_CC);
 
 	php_info_print_table_start();
 	php_info_print_table_row(2, "iconv support", "enabled");
