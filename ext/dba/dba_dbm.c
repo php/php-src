bea9665ec04226673cb9dc6be595d0fc9b0f54f6
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-06 02:21:49 +0000

    correct file open modes
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@102729 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dba/dba_dbm.c
+++ ext/dba/dba_dbm.c
@@ -73,11 +73,13 @@ DBA_OPEN_FUNC(dbm)
 			fmode = "r";
 			break;
 		case DBA_WRITER:
-			fmode = "a+";
+			fmode = "r+b";
 			break;
 		case DBA_CREAT:
+			fmode = "a+b";
+			break;
 		case DBA_TRUNC:
-			fmode = "w+";
+			fmode = "w+b";
 			break;
 		default:
 			return FAILURE; /* not possible */
