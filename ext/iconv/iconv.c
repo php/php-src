8e1f89bc6f68937515675fecde07de1e2f58e1f5
Moriyoshi Koizumi <moriyoshi@php.net>
2002-11-08 18:40:54 +0000

    Use proper types

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -359,15 +359,16 @@ static void _php_iconv_show_error(php_iconv_err_t err, const char *in_charset, c
 PHP_NAMED_FUNCTION(php_if_iconv)
 {
 	char *in_charset, *out_charset, *in_buffer, *out_buffer;
-	unsigned int out_len;
-	int in_charset_len, out_charset_len, in_buffer_len, err;
+	size_t out_len;
+	int in_charset_len, out_charset_len, in_buffer_len;
+	php_iconv_err_t err;
 	
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sss",
 		&in_charset, &in_charset_len, &out_charset, &out_charset_len, &in_buffer, &in_buffer_len) == FAILURE)
 		return;
 
-	err = php_iconv_string(in_buffer, in_buffer_len,
-		&out_buffer,  &out_len, in_charset, out_charset);
+	err = php_iconv_string(in_buffer, (size_t)in_buffer_len,
+		&out_buffer, &out_len, in_charset, out_charset);
 	_php_iconv_show_error(err, in_charset, out_charset TSRMLS_CC); 
 	if (out_buffer != NULL) {
 		RETVAL_STRINGL(out_buffer, out_len, 0);
