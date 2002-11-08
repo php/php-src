5c40cd59da84a8c52ed6f6d1584fc13308454dc8
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-08 21:20:48 +0000

    Remove unnecessary comments.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@103024 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/tokenizer/php_tokenizer.h
+++ ext/tokenizer/php_tokenizer.h
@@ -42,26 +42,6 @@ PHP_FUNCTION(confirm_tokenizer_compiled);	/* For testing, remove later. */
 PHP_FUNCTION(token_get_all);
 PHP_FUNCTION(token_name);
 
-/* 
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:     
-
-ZEND_BEGIN_MODULE_GLOBALS(tokenizer)
-	int   global_value;
-	char *global_string;
-ZEND_END_MODULE_GLOBALS(tokenizer)
-*/
-
-/* In every utility function you add that needs to use variables 
-   in php_tokenizer_globals, call TSRM_FETCH(); after declaring other 
-   variables used by that function, or better yet, pass in TSRMG_CC
-   after the last function argument and declare your utility function
-   with TSRMG_DC after the last declared argument.  Always refer to
-   the globals in your function as TOKENIZER_G(variable).  You are 
-   encouraged to rename these macros something shorter, see
-   examples in any other php module directory.
-*/
-
 #ifdef ZTS
 #define TOKENIZER_G(v) TSRMG(tokenizer_globals_id, zend_tokenizer_globals *, v)
 #else
