08362f90d9fa692b1047c190f51c789c9c3bade7
Wez Furlong <wez@php.net>
2002-11-14 12:06:01 +0000

    Fix build for unix. Not sure if this needs merging to the branch (haven't got that far yet today).

--- ext/mbstring/mbstring.h
+++ ext/mbstring/mbstring.h
@@ -49,18 +49,18 @@
 #ifdef COMPILE_DL_MBSTRING
 #undef HAVE_MBSTRING
 #define HAVE_MBSTRING 1
-#undef MBSTRING_API
-#define MBSTRING_API
-
 #endif
 
 #ifdef PHP_WIN32
-#undef MBSTRING_API
-#ifdef MBSTRING_EXPORTS
-#define MBSTRING_API __declspec(dllexport)
+# undef MBSTRING_API
+# ifdef MBSTRING_EXPORTS
+#  define MBSTRING_API __declspec(dllexport)
+# else
+#  define MBSTRING_API __declspec(dllimport)
+# endif
 #else
-#define MBSTRING_API __declspec(dllimport)
-#endif
+# undef MBSTRING_API
+# define MBSTRING_API /* nothing special */
 #endif
 
 
