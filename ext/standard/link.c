5f7de8bf5b59087ef9a6f9bd9fe4e236f6282836
Ilia Alshanetsky <iliaa@php.net>
2002-11-14 16:20:23 +0000

    Fixed bug #20423.

--- ext/standard/link.c
+++ ext/standard/link.c
@@ -146,7 +146,7 @@ PHP_FUNCTION(symlink)
 		RETURN_FALSE;
 	}
 
-	ret = symlink(dest_p, source_p);
+	ret = symlink(Z_STRVAL_PP(topath), Z_STRVAL_PP(frompath));
 	if (ret == -1) {
 		php_error(E_WARNING, "Symlink failed (%s)", strerror(errno));
 		RETURN_FALSE;
@@ -197,7 +197,7 @@ PHP_FUNCTION(link)
 		RETURN_FALSE;
 	}
 
-	ret = link(dest_p, source_p);
+	ret = link(Z_STRVAL_PP(topath), Z_STRVAL_PP(frompath));
 	if (ret == -1) {
 		php_error(E_WARNING, "Link failed (%s)", strerror(errno));
 		RETURN_FALSE;
