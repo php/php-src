dd68136bd978a08625d50ebb3071c4ea8a99c63c
Marcus Boerger <helly@php.net>
2002-11-15 14:24:20 +0000

    MFH: Show Zend version

--- run-tests.php
+++ run-tests.php
@@ -105,6 +105,7 @@ $info_file = realpath(dirname(__FILE__)) . '/run-test-info.php';
 $php_info = '<?php echo "
 PHP_SAPI    : " . PHP_SAPI . "
 PHP_VERSION : " . phpversion() . "
+ZEND_VERSION: " . zend_version() . "
 PHP_OS      : " . PHP_OS . "
 INI actual  : " . realpath(get_cfg_var("cfg_file_path")) . "
 More .INIs  : " . (function_exists(\'php_ini_scanned_files\') ? str_replace("\n","", php_ini_scanned_files()) : "** not determined **"); ?>';
@@ -335,17 +336,7 @@ if ($sum_results['FAILED'] && !getenv('NO_INTERACTION')) {
 		$failed_tests_data = '';
 		$sep = "\n" . str_repeat('=', 80) . "\n";
 		
-		$failed_tests_data .= $failed_test_summary . "\n";
-		
-		foreach ($GLOBALS['__PHP_FAILED_TESTS__'] as $test_info) {
-			$failed_tests_data .= $sep . $test_info['name'];
-			$failed_tests_data .= $sep . file_get_contents(realpath($test_info['output']));
-			$failed_tests_data .= $sep . file_get_contents(realpath($test_info['diff']));
-			$failed_tests_data .= $sep . "\n\n";
-		}
-		
-		$failed_tests_data .= "\n" . $sep . 'BUILD ENVIRONMENT' . $sep;
-		$failed_tests_data .= "OS:\n". PHP_OS. "\n\n";
+		$failed_tests_data .= "OS:\n". PHP_OS. "\n";
 		$automake = $autoconf = $libtool = $compiler = 'N/A';
 		if (substr(PHP_OS, 0, 3) != "WIN") {
 			$automake = shell_exec('automake --version');
@@ -368,6 +359,15 @@ if ($sum_results['FAILED'] && !getenv('NO_INTERACTION')) {
 		$failed_tests_data .= "Compiler:\n$compiler\n";
 		$failed_tests_data .= "Bison:\n". @shell_exec('bison --version'). "\n";
 		$failed_tests_data .= "\n\n";
+		
+		$failed_tests_data .= $failed_test_summary . "\n";
+		
+		foreach ($GLOBALS['__PHP_FAILED_TESTS__'] as $test_info) {
+			$failed_tests_data .= $sep . $test_info['name'];
+			$failed_tests_data .= $sep . file_get_contents(realpath($test_info['output']));
+			$failed_tests_data .= $sep . file_get_contents(realpath($test_info['diff']));
+			$failed_tests_data .= $sep . "\n\n";
+		}
 		
 		$failed_tests_data .= $sep . "PHPINFO" . $sep;
 		$failed_tests_data .= shell_exec($php.' -dhtml_errors=0 -i');
