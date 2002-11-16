208a3a371790e646f4d02086711c9220951aebd9
Georg Richter <georg@php.net>
2002-11-16 16:07:54 +0000

    forgot to decrement result_allocated :(

--- ext/mysql/php_mysql.c
+++ ext/mysql/php_mysql.c
@@ -231,6 +231,7 @@ static void _free_mysql_result(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 	MYSQL_RES *mysql_result = (MYSQL_RES *)rsrc->ptr;
 
 	mysql_free_result(mysql_result);
+	MySG(result_allocated)--;
 }
 /* }}} */
 
