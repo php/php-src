1ffc9c569e85969138f442252a4ce0807ea23e35
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-17 14:25:50 +0000

    - Fix compile problems for mips pro
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@104535 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -3597,8 +3597,10 @@ int gdImageMeanRemoval(gdImagePtr im)
 int gdImageSmooth(gdImagePtr im, float weight)
 {
 	float filter[3][3] =	{{1.0,1.0,1.0},
-				{1.0,weight ,1.0},
+				{1.0,0.0,1.0},
 				{1.0,1.0,1.0}};
+	
+	filter[1][1] = weight;
 	
 	return gdImageConvolution(im, filter, weight+8, 0);
 }
