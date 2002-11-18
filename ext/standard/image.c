e93f1c00c61a28e322d69da020c12d8036267848
Derick Rethans <derick@php.net>
2002-11-18 15:45:27 +0000

    - Remove outdated GPL clause; the file was totally rewritten

--- ext/standard/image.c
+++ ext/standard/image.c
@@ -16,26 +16,8 @@
    |          Marcus Boerger <helly@php.net>                              |
    +----------------------------------------------------------------------+
  */
-/* $Id$ */
-/*
- * Based on Daniel Schmitt's imageinfo.c which carried the following
- * Copyright notice.
- */
 
-/*
- * imageinfo.c
- *
- * Simple routines to extract image width/height data from GIF/JPEG files.
- *
- * Copyright (c) 1997 Daniel Schmitt, opal online publishing, Bonn, Germany.
- *
- * Includes code snippets from rdjpgcom.c,
- * Copyright (c) 1994-1995 Thomas G. Lane
- * from release 6a of the Independent JPEG Group's software.
- *
- * Legal status: see GNU General Public License version 2 or later.
- *
- */
+/* $Id$ */
 
 #include "php.h"
 #include <stdio.h>
