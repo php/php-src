72b7ea5d77e2451e73b86a43ed92c201f5ecb7dd
foobar <sniper@php.net>
2002-11-18 03:24:33 +0000

    MFH: Added error message if browscap location is not set in php.ini

--- ext/standard/browscap.c
+++ ext/standard/browscap.c
@@ -220,6 +220,7 @@ PHP_FUNCTION(get_browser)
 	char *lookup_browser_name;
 
 	if (!INI_STR("browscap")) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "browscap ini directive not set.");
 		RETURN_FALSE;
 	}
 	
