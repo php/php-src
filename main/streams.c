f0daa5c6d915ad23c783bc7c13c84155dfeacdba
Marcus Boerger <helly@php.net>
2002-11-18 17:23:21 +0000

    Fix warning as discussed with Wez.

--- main/streams.c
+++ main/streams.c
@@ -1374,6 +1374,8 @@ static int php_stdiop_close(php_stream *stream, int close_handle TSRMLS_DC)
 			} else {
 				ret = fclose(data->file);
 			}
+		} else {
+			return 0;/* everything should be closed already -> success*/
 		}
 		if (data->temp_file_name) {
 			unlink(data->temp_file_name);
