eeedc01aa0214e4f0c54e9814a904895633c5899
dickmann <dickmann@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-24 10:13:57 +0000

    add some checks to increase cache integrity
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@105519 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Remote.php
+++ pear/PEAR/Remote.php
@@ -108,13 +108,15 @@ class PEAR_Remote extends PEAR
         $this->cache = $this->getCache($args);
         $cachettl = $this->config->get('cache_ttl');
         // If cache is newer than $cachettl seconds, we use the cache!
-        if ($this->cache !== null && $this->cache['age'] < $cachettl) {
+        if ($this->cache !== null && $this->cache['age'] < $cachettl && is_array($this->cache['content'])) {
             return $this->cache['content'];
         };
         
         if (extension_loaded("xmlrpc")) {
             $result = call_user_func_array(array(&$this, 'call_epi'), $args);
-            $this->saveCache($_args, $result);
+            if (!PEAR::isError($result)) {
+                $this->saveCache($_args, $result);
+            };
             return $result;
         }
         if (!@include_once("XML/RPC.php")) {
