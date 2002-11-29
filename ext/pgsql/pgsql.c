e4be6b3ac2ede24f81daa0a135f9abb5e0d76d39
Yasuo Ohgaki <yohgaki@php.net>
2002-11-29 17:18:01 +0000

    Fixed problem with PostgreSQL 7.3

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -464,7 +464,7 @@ PHP_MINFO_FUNCTION(pgsql)
 	php_info_print_table_header(2, "PostgreSQL Support", "enabled");
 #if HAVE_PG_CONFIG_H
 	php_info_print_table_row(2, "PostgreSQL(libpq) Version", PG_VERSION);
-#ifdef MULTIBYTE
+#ifdef HAVE_PGSQL_WITH_MULTIBYTE_SUPPORT
 	php_info_print_table_row(2, "Multibyte charater support", "enabled");
 #else
 	php_info_print_table_row(2, "Multibyte charater support", "disabled");
