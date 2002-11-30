d4e23cc8a9471971bb5eef68aa3ad4bc1b9d0ca3
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-11-30 18:36:17 +0000

    Bug #20433
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@106297 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_globals.h
+++ main/php_globals.h
@@ -77,7 +77,7 @@ struct _php_core_globals {
 	zend_bool display_errors;
 	zend_bool display_startup_errors;
 	zend_bool log_errors;
-	int       log_errors_max_len;
+	long      log_errors_max_len;
 	zend_bool ignore_repeated_errors;
 	zend_bool ignore_repeated_source;
 	zend_bool report_memleaks;
