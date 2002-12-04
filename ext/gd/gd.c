080bd8aa1aab8927703ab713b1484097c99e5e7f
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-04 16:29:48 +0000

    WS fixes.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@106764 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -1626,12 +1626,12 @@ static void _php_image_output(INTERNAL_FUNCTION_PARAMETERS, int image_type, char
 				}
 				(*func_p)(im, q, tmp);
 				break;
-            case PHP_GDIMG_TYPE_GD:
-                if(im->trueColor){
-	                    gdImageTrueColorToPalette(im,1,256);
-		                }
-                (*func_p)(im, tmp);
-                break;
+			case PHP_GDIMG_TYPE_GD:
+				if (im->trueColor) {
+					gdImageTrueColorToPalette(im,1,256);
+				}
+				(*func_p)(im, tmp);
+				break;
 			default:
 				(*func_p)(im, tmp);
 				break;
