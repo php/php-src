5118ff00efda91c62be6c3179e28cfda911cce52
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-10 15:58:31 +0000

    Removed one more unneeded check.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@107419 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -841,9 +841,7 @@ SAPI_API SAPI_POST_HANDLER_FUNC(rfc1867_post_handler)
 			}
 			if (skip_upload) {
 				efree(param);
-				if (filename) {
-					efree(filename);
-				}
+				efree(filename);
 				continue;
 			}	
 
