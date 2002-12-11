45ffbdfb3d359cccf831c0155574c68d2fdae44b
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-11 20:04:23 +0000

    Added missing &.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@107700 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/fdf/fdf.c
+++ ext/fdf/fdf.c
@@ -1417,7 +1417,7 @@ PHP_FUNCTION(fdf_remove_item) {
 	
 	if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rsl", &r_fdf, 
 							 &fieldname, &fieldname_len,
-							 item
+							 &item
 							 )
 	   == FAILURE) {
 		return;
