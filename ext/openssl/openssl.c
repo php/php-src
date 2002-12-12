58613036fbdbada201bdf3565454ae46170adf58
Ilia Alshanetsky <iliaa@php.net>
2002-12-12 18:36:44 +0000

    Fixed ZTS build.

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -1685,7 +1685,7 @@ static EVP_PKEY * php_openssl_evp_from_zval(zval ** val, int public_key, char * 
 		}
 		else if (type == le_key) {
 			/* check whether it is actually a private key if requested */
-			if (!public_key && !php_openssl_is_private_key((EVP_PKEY*)what))
+			if (!public_key && !php_openssl_is_private_key((EVP_PKEY*)what TSRMLS_CC))
 			{
 				php_error_docref(NULL TSRMLS_CC, E_WARNING, "supplied key param is a public key");
 				return NULL;
