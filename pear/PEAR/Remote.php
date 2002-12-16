59b99587e0d185a9c7428b6aae1cd56779d6db49
ssb <ssb@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-16 20:44:50 +0000

    * fix one missing DIRECTORY_SEPARATOR and a typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@108345 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Remote.php
+++ pear/PEAR/Remote.php
@@ -58,7 +58,7 @@ class PEAR_Remote extends PEAR
         if (!file_exists($cachedir)) {
             System::mkdir('-p '.$cachedir);
         }
-        $filename = $cachedir.'/xmlrpc_cache_'.$id;
+        $filename = $cachedir . DIRECTORY_SEPARATOR . 'xmlrpc_cache_' . $id;
         if (!file_exists($filename)) {
             return null;
         };
@@ -313,16 +313,16 @@ class PEAR_Remote extends PEAR
                 $lastkey = key($php_val);
                 if ($firstkey === 0 && is_int($lastkey) &&
                     ($lastkey + 1) == count($php_val)) {
-                    $is_continous = true;
+                    $is_continuous = true;
                     reset($php_val);
                     $size = count($php_val);
                     for ($expect = 0; $expect < $size; $expect++, next($php_val)) {
                         if (key($php_val) !== $expect) {
-                            $is_continous = false;
+                            $is_continuous = false;
                             break;
                         }
                     }
-                    if ($is_continous) {
+                    if ($is_continuous) {
                         reset($php_val);
                         $arr = array();
                         while (list($k, $v) = each($php_val)) {
@@ -332,7 +332,7 @@ class PEAR_Remote extends PEAR
                         break;
                     }
                 }
-                // fall though if not numerical and continous
+                // fall though if not numerical and continuous
             case "object":
                 $arr = array();
                 while (list($k, $v) = each($php_val)) {
