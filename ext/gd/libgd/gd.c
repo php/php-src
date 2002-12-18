e4ebcad22db778c35a7d5dfa8c9d0681ac13e4f0
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-18 21:13:21 +0000

    Fix a crash while using an invalid color with imagesetstyle, see the sample script in the manual to reproduce it :)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@108427 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -589,21 +589,19 @@ gdImageColorDeallocate (gdImagePtr im, int color)
   im->open[color] = 1;
 }
 
-void
-gdImageColorTransparent (gdImagePtr im, int color)
+void gdImageColorTransparent (gdImagePtr im, int color)
 {
-  if (!im->trueColor)
-    {
-      if (im->transparent != -1)
-	{
-	  im->alpha[im->transparent] = gdAlphaOpaque;
+	if (!im->trueColor) {
+		if (im->transparent != -1) {
+			im->alpha[im->transparent] = gdAlphaOpaque;
+		}
+		if (color > -1 && color<=gdMaxColors) {
+			im->alpha[color] = gdAlphaTransparent;
+		} else {
+			return;
+		}
 	}
-      if (color != -1)
-	{
-	  im->alpha[color] = gdAlphaTransparent;
-	}
-    }
-  im->transparent = color;
+	im->transparent = color;
 }
 
 void
