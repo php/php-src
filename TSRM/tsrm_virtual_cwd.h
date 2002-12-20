62600cbf54f5b84e5af56248bd3ad0445f7b2551
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2002-12-20 00:41:41 +0000

    Make IS_ABSOLUTE_PATH use the IS_UNC_PATH macro rather then duplicate the code in the IS_UNC_PATH macro.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@108518 c90b9560-bf6c-de11-be94-00142212c4b1

--- TSRM/tsrm_virtual_cwd.h
+++ TSRM/tsrm_virtual_cwd.h
@@ -57,10 +57,10 @@ typedef unsigned short mode_t;
  * to account for volume name that is unique to NetWare absolute paths
  */
 #define COPY_WHEN_ABSOLUTE(path) 2
-#define IS_ABSOLUTE_PATH(path, len) \
-	(len >= 2 && ((isalpha(path[0]) && path[1] == ':') || (IS_SLASH(path[0]) && IS_SLASH(path[1]))))
 #define IS_UNC_PATH(path, len) \
 	(len >= 2 && IS_SLASH(path[0]) && IS_SLASH(path[1]))
+#define IS_ABSOLUTE_PATH(path, len) \
+	(len >= 2 && ((isalpha(path[0]) && path[1] == ':') || IS_UNC_PATH(path, len)))
 
 #elif defined(NETWARE)
 #ifdef HAVE_DIRENT_H
