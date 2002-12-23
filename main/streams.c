05ea039144a27f78ff30e8b357716062602a0032
Wez Furlong <wez@php.net>
2002-12-23 02:56:46 +0000

    MFB: fpos_t -> off_t

--- main/streams.c
+++ main/streams.c
@@ -2418,7 +2418,7 @@ PHPAPI php_stream *_php_stream_open_wrapper_ex(char *path, char *mode, int optio
 	}
 
 	if (stream && stream->ops->seek && (stream->flags & PHP_STREAM_FLAG_NO_SEEK) == 0 && strchr(mode, 'a')) {
-		fpos_t newpos = 0;
+		off_t newpos = 0;
 
 		/* if opened for append, we need to revise our idea of the initial file position */
 		if (0 == stream->ops->seek(stream, 0, SEEK_CUR, &newpos TSRMLS_CC)) {
