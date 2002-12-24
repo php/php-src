a747d1f864c566c5aa2d463283cf2d796640b77b
Ilia Alshanetsky <iliaa@php.net>
2002-12-24 17:30:48 +0000

    Changed strncpy() to strlcpy().

--- ext/ncurses/ncurses_functions.c
+++ ext/ncurses/ncurses_functions.c
@@ -1768,7 +1768,7 @@ PHP_FUNCTION(ncurses_termname)
 	
 	IS_NCURSES_INITIALIZED();
 
-	strncpy(temp, termname(), sizeof(temp) - 1);
+	strlcpy(temp, termname(), sizeof(temp) - 1);
 	temp[sizeof(temp) - 1] = '\0';
 
 	RETURN_STRINGL (temp, strlen(temp), 1);
@@ -1783,7 +1783,7 @@ PHP_FUNCTION(ncurses_longname)
 
 	IS_NCURSES_INITIALIZED();
 
-	strncpy(temp, longname(), sizeof(temp) - 1);
+	strlcpy(temp, longname(), sizeof(temp) - 1);
 	temp[sizeof(temp) - 1] = '\0';
 
 	RETURN_STRINGL (temp, strlen(temp), 1);
