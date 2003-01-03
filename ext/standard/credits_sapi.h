eb6dc35b49e8ed5ea2076affd36aeb98a94780e6
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-01-03 10:52:23 +0000

    - Update this one as well
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@110109 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/credits_sapi.h
+++ ext/standard/credits_sapi.h
@@ -12,18 +12,19 @@
 
 CREDIT_LINE("ActiveScript", "Wez Furlong");
 CREDIT_LINE("AOLserver", "Sascha Schumann");
+CREDIT_LINE("Apache 1.3 (apache_hooks)", "Rasmus Lerdorf, Zeev Suraski, Stig Bakken, David Sklar, George Schlossnagle, Lukas Schroeder");
 CREDIT_LINE("Apache 1.3", "Rasmus Lerdorf, Zeev Suraski, Stig Bakken, David Sklar");
 CREDIT_LINE("Apache 2.0", "Sascha Schumann, Aaron Bannert");
 CREDIT_LINE("Caudium / Roxen", "David Hedbor");
 CREDIT_LINE("CGI / FastCGI", "Rasmus Lerdorf, Stig Bakken, Shane Caraveo");
 CREDIT_LINE("CLI", "Edin Kadribasic, Marcus Boerger");
 CREDIT_LINE("Embed", "Edin Kadribasic");
-CREDIT_LINE("fastcgi", "Ben Mansell");
 CREDIT_LINE("ISAPI", "Andi Gutmans, Zeev Suraski");
 CREDIT_LINE("Java Servlet", "Sam Ruby");
 CREDIT_LINE("NSAPI", "Jayakumar Muthukumarasamy");
 CREDIT_LINE("phttpd", "Thies C. Arntzen");
 CREDIT_LINE("pi3web", "Holger Zimmermann");
+CREDIT_LINE("Sendmail Milter", "Harald Radi");
 CREDIT_LINE("thttpd", "Sascha Schumann");
 CREDIT_LINE("tux", "Sascha Schumann");
 CREDIT_LINE("WebJames", "Alex Waugh");
