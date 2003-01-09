e1c35b0f163d9e36e405bb6d097d769bab5f3344
Sara Golemon <pollita@php.net>
2003-01-09 22:29:02 +0000

    MFH

--- ext/standard/filestat.c
+++ ext/standard/filestat.c
@@ -564,7 +564,7 @@ static void php_stat(const char *filename, php_stat_len filename_length, int typ
 	char *stat_sb_names[13]={"dev", "ino", "mode", "nlink", "uid", "gid", "rdev",
 			      "size", "atime", "mtime", "ctime", "blksize", "blocks"};
 
-	if (PG(safe_mode) &&(!php_checkuid(filename, NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
+	if (PG(safe_mode) &&(!php_checkuid_ex(filename, NULL, CHECKUID_CHECK_FILE_AND_DIR, IS_EXISTS_CHECK(type) ? CHECKUID_NO_ERRORS : 0))) {
 		RETURN_FALSE;
 	}
 
