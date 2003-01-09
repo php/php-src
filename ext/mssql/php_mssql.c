392e5013b4442fba575f6e02f01d81db5fbb02cd
Frank M. Kromann <fmk@php.net>
2003-01-09 02:44:44 +0000

    changed from LVAL to RESVAL macro

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -705,7 +705,7 @@ PHP_FUNCTION(mssql_close)
 	ZEND_FETCH_RESOURCE2(mssql_ptr, mssql_link *, mssql_link_index, id, "MS SQL-Link", le_link, le_plink);
 
 	if (mssql_link_index) 
-		zend_list_delete(Z_LVAL_PP(mssql_link_index));
+		zend_list_delete(Z_RESVAL_PP(mssql_link_index));
 	else 
 		zend_list_delete(id);
 
@@ -1186,7 +1186,7 @@ PHP_FUNCTION(mssql_free_result)
 	if (dbdataready(result->mssql_ptr->link))
 		dbresults(result->mssql_ptr->link);
 #endif
-	zend_list_delete(Z_LVAL_PP(mssql_result_index));
+	zend_list_delete(Z_RESVAL_PP(mssql_result_index));
 	RETURN_TRUE;
 }
 
