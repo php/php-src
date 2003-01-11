b66b92e99682ca1e66fd84a2b5c1db8c92086271
Moriyoshi Koizumi <moriyoshi@php.net>
2003-01-11 22:32:19 +0000

    Reduced warnings in ZE2 build

--- ext/standard/datetime.c
+++ ext/standard/datetime.c
@@ -313,7 +313,7 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Unexpected error");
 		RETURN_FALSE;
 	}
-	for (i = 0; i < Z_STRLEN_PP(format); i++) {
+	for (i = 0; i < (int)Z_STRLEN_PP(format); i++) {
 		switch (Z_STRVAL_PP(format)[i]) {
 			case 'r':		/* rfc822 format */
 				size += 31;
@@ -366,7 +366,7 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 				size += 2;
 				break;
 			case '\\':
-				if(i < Z_STRLEN_PP(format)-1) {
+				if(i < ((int)Z_STRLEN_PP(format)) - 1) {
 					i++;
 				}
 				size ++;
@@ -383,10 +383,10 @@ php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 	Z_STRVAL_P(return_value) = (char *) emalloc(size + 1);
 	Z_STRVAL_P(return_value)[0] = '\0';
 
-	for (i = 0; i < Z_STRLEN_PP(format); i++) {
+	for (i = 0; i < (int)Z_STRLEN_PP(format); i++) {
 		switch (Z_STRVAL_PP(format)[i]) {
 			case '\\':
-				if(i < Z_STRLEN_PP(format)-1) {
+				if(i < (int)(Z_STRLEN_PP(format)) - 1) {
 					char ch[2];
 					ch[0]=Z_STRVAL_PP(format)[i+1];
 					ch[1]='\0';
--- ext/standard/pack.c
+++ ext/standard/pack.c
@@ -320,7 +320,7 @@ PHP_FUNCTION(pack)
 				val = argv[currentarg++];
 				convert_to_string_ex(val);
 				memcpy(&output[outputpos], Z_STRVAL_PP(val),
-					   (Z_STRLEN_PP(val) < arg) ? Z_STRLEN_PP(val) : arg);
+					   ((int)Z_STRLEN_PP(val) < arg) ? Z_STRLEN_PP(val) : arg);
 				outputpos += arg;
 				break;
 
@@ -334,7 +334,7 @@ PHP_FUNCTION(pack)
 				convert_to_string_ex(val);
 				v = Z_STRVAL_PP(val);
 				outputpos--;
-				if(arg > Z_STRLEN_PP(val)) {
+				if(arg > (int)Z_STRLEN_PP(val)) {
 					php_error_docref(NULL TSRMLS_CC, E_WARNING, "Type %c: not enough characters in string", code);
 					arg = Z_STRLEN_PP(val);
 				}
