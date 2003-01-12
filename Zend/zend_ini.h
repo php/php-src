13c2254e212b9710a7d3709c4afceb87a3365843
Harald Radi <phanto@php.net>
2003-01-12 13:14:23 +0000

    partially revert previous commit and change zend_modules.h to include a forward declaration to zend_ini_entry

--- Zend/zend_ini.h
+++ Zend/zend_ini.h
@@ -56,7 +56,31 @@
 
 #endif
 
-#include "zend_ini_entry.h"
+typedef struct _zend_ini_entry zend_ini_entry;
+
+#define ZEND_INI_MH(name) int name(zend_ini_entry *entry, char *new_value, uint new_value_length, void *mh_arg1, void *mh_arg2, void *mh_arg3, int stage TSRMLS_DC)
+#define ZEND_INI_DISP(name) void name(zend_ini_entry *ini_entry, int type)
+
+struct _zend_ini_entry {
+	int module_number;
+	int modifyable;
+	char *name;
+	uint name_length;
+	ZEND_INI_MH((*on_modify));
+	void *mh_arg1;
+	void *mh_arg2;
+	void *mh_arg3;
+
+	char *value;
+	uint value_length;
+
+	char *orig_value;
+	uint orig_value_length;
+	int modified;
+
+	void (*displayer)(zend_ini_entry *ini_entry, int type);
+};
+
 
 ZEND_API int zend_ini_startup(TSRMLS_D);
 ZEND_API int zend_ini_shutdown(TSRMLS_D);
