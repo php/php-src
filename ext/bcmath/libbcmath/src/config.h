77f7ae38141100ed30240f253c42970067f3a1d7
Harald Radi <phanto@php.net>
2003-01-16 22:08:00 +0000

    revert previous commit

--- ext/bcmath/libbcmath/src/config.h
+++ ext/bcmath/libbcmath/src/config.h
@@ -1,7 +1,3 @@
-#include <string.h>
-#include "zend.h"
-#include "zend_alloc.h"
-
 #if PHP_WIN32
 #include "../../../../main/config.w32.h"
 #elif defined(NETWARE)
@@ -9,3 +5,7 @@
 #else
 #include "php_config.h"
 #endif
+
+#include <string.h>
+#include "zend.h"
+#include "zend_alloc.h"
