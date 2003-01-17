dc4cdaaf1ba2fe0dd3b1f38b9611deaffc257d82
Moriyoshi Koizumi <moriyoshi@php.net>
2003-01-17 22:45:23 +0000

    WS fix

--- win32/md5crypt.c
+++ win32/md5crypt.c
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Author: Edin Kadribasic                                                             |
+   | Author: Edin Kadribasic                                              |
    +----------------------------------------------------------------------+
  */
 
