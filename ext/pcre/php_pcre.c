e3e364f77b737bfa0e6645a644d8ee985de4c5bd
Moriyoshi Koizumi <moriyoshi@php.net>
2003-01-20 16:45:21 +0000

    Fixed bug #21758

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -1044,7 +1044,7 @@ static void preg_replace_impl(INTERNAL_FUNCTION_PARAMETERS, zend_bool is_callabl
 		zend_get_parameters_ex(ZEND_NUM_ARGS(), &regex, &replace, &subject, &limit) == FAILURE) {
 		WRONG_PARAM_COUNT;
 	}
-	if (Z_TYPE_PP(replace) == IS_ARRAY && Z_TYPE_PP(regex) != IS_ARRAY) {
+	if (!is_callable_replace && Z_TYPE_PP(replace) == IS_ARRAY && Z_TYPE_PP(regex) != IS_ARRAY) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Parameter mismatch, pattern is a string while replacement in an array.");
 		RETURN_FALSE;
 	}
