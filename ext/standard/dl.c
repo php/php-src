81586fed7d4519f39ec1c21c956d2145cfb92e5f
Edin Kadribasic <edink@php.net>
2003-01-29 15:42:43 +0000

    MFH

--- ext/standard/dl.c
+++ ext/standard/dl.c
@@ -61,7 +61,9 @@ PHP_FUNCTION(dl)
 	pval **file;
 
 #ifdef ZTS
-	if ((strncmp(sapi_module.name, "cgi", 3)!=0) && (strcmp(sapi_module.name, "cli")!=0)) {
+	if ((strncmp(sapi_module.name, "cgi", 3)!=0) && 
+		(strcmp(sapi_module.name, "cli")!=0) &&
+		(strncmp(sapi_module.name, "embed", 5)!=0)) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Not supported in multithreaded Web servers - use extension statements in your php.ini");
 		RETURN_FALSE;
 	}
