7ed09726838a1b091cf4408e8af0169882d1d4cd
Sebastian Bergmann <sebastian@php.net>
2003-02-02 18:50:42 +0000

    Fix warning.

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -33,6 +33,7 @@
 #include "php_main.h"
 #include "zend_globals.h"		/* needs ELS */
 #include "zend_extensions.h"
+#include "zend_highlight.h"
 #ifdef HAVE_SYS_UTSNAME_H
 #include <sys/utsname.h>
 #endif
