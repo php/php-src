ac59c7f912b74ec3069c6f4799a407a48bfc19a6
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-02-04 18:34:00 +0000

    Made the row parameter in pg_result_seek() non-optional, the current implementation would result in random behavior if the 2nd argument is not passed. Fixed bug #22042 (pg_result_seek() would never seek to the 1st row in the result due to always seeking to row next to the one requested). Removed dead code from pg_result_seek().
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@114770 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -1437,23 +1437,18 @@ PHP_FUNCTION(pg_result_seek)
 	int row;
 	pgsql_result_handle *pg_result;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r|l",
-							  &result, &row) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rl", &result, &row) == FAILURE) {
 		return;
 	}
 
 	ZEND_FETCH_RESOURCE(pg_result, pgsql_result_handle *, &result, -1, "PostgreSQL result", le_result);
 
-	/* Let see if we are better to have another function for this */
-	/* if offset is omitted, return current position */
-/* 	if (ZEND_NUM_ARGS() == 1) */
-/* 		RETURN_LONG(pg_result->row); */
-
-	if (row < 0 || row >= PQntuples(pg_result->result))
+	if (row < 0 || row >= PQntuples(pg_result->result)) {
 		RETURN_FALSE;
+	}
 	
 	/* seek to offset */
-	pg_result->row = row;
+	pg_result->row = row - 1;
 	RETURN_TRUE;
 }
 /* }}} */
