ba45e21086a2efc535f5d7008fe5cb48f81ee75a
zeev <zeev@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-02-06 00:14:49 +0000

    Fix prototype (may have caused stack corruption)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@114892 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_hash.h
+++ Zend/zend_hash.h
@@ -83,7 +83,7 @@ typedef struct _zend_hash_key {
 } zend_hash_key;
 
 
-typedef int (*merge_checker_func_t)(HashTable *target_ht, void *source_data, zend_hash_key *hash_key, void *pParam);
+typedef zend_bool (*merge_checker_func_t)(HashTable *target_ht, void *source_data, zend_hash_key *hash_key, void *pParam);
 
 typedef Bucket* HashPosition;
 
