7e60988e76b0dbe7f781531e83777203ef5a19b5
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-02-07 23:44:58 +0000

    chunk_size should always be reduced to 1 while scanning response headers to avoid buffering the actual stream prior to any read calls.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@115098 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/http_fopen_wrapper.c
+++ ext/standard/http_fopen_wrapper.c
@@ -123,8 +123,7 @@ php_stream *php_stream_url_wrap_http(php_stream_wrapper *wrapper, char *path, ch
 		goto out;
 
 	/* avoid buffering issues while reading header */
-	if (options & STREAM_WILL_CAST)
-		chunk_size = php_stream_set_chunk_size(stream, 1);
+	chunk_size = php_stream_set_chunk_size(stream, 1);
 	
 	php_stream_context_set(stream, context);
 
@@ -400,8 +399,8 @@ out:
 	if (stream)	{
 		stream->wrapperdata = response_header;
 		php_stream_notify_progress_init(context, 0, file_size);
-		if (options & STREAM_WILL_CAST)
-			php_stream_set_chunk_size(stream, chunk_size);
+		/* Restore original chunk size now that we're done with headers */
+		php_stream_set_chunk_size(stream, chunk_size);
 		/* as far as streams are concerned, we are now at the start of
 		 * the stream */
 		stream->position = 0;
