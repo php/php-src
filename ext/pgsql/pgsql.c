7f81a4a62f97e5f848c4f3ad51f95beaa92ef37a
Yasuo Ohgaki <yohgaki@php.net>
2003-02-09 07:50:16 +0000

    Fixed one more improper row offset handling.

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -2975,7 +2975,7 @@ PHP_FUNCTION(pg_get_result)
 	pg_result = (pgsql_result_handle *) emalloc(sizeof(pgsql_result_handle));
 	pg_result->conn = pgsql;
 	pg_result->result = pgsql_result;
-	pg_result->row = -1;
+	pg_result->row = 0;
 	ZEND_REGISTER_RESOURCE(return_value, pg_result, le_result);
 }
 /* }}} */
