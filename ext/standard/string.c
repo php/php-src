c1ce5b3ae8a0bd9d375ec9a0f13f6b6f63bbb635
moriyoshi <moriyoshi@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-02-14 18:42:36 +0000

    Fixed bug #22224 (implode changes object references in array) Added test case for the bug
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@116398 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -835,7 +835,8 @@ PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value)
 	zend_hash_internal_pointer_reset_ex(Z_ARRVAL_P(arr), &pos);
 
 	while (zend_hash_get_current_data_ex(Z_ARRVAL_P(arr), (void **) &tmp, &pos) == SUCCESS) {
-		convert_to_string_ex(tmp);
+		SEPARATE_ZVAL(tmp);
+		convert_to_string(*tmp);
 
 		smart_str_appendl(&implstr, Z_STRVAL_PP(tmp), Z_STRLEN_PP(tmp));
 		if (++i != numelems) {
