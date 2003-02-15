cec89cfd763063df90ee071a78ce1fc16493f693
Edin Kadribasic <edink@php.net>
2003-02-15 21:37:42 +0000

    New version number.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 0
+#define PHP_RELEASE_VERSION 1
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "4.3.0"
+#define PHP_VERSION "4.3.1"
