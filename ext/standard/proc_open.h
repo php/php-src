7e92f636ef142d4200138b05e6be69863a491b2d
Wez Furlong <wez@php.net>
2003-02-16 15:38:54 +0000

    Integrate Shanes patch that allows specifying the cwd and environment for the child process created by proc_open().

--- ext/standard/proc_open.h
+++ ext/standard/proc_open.h
@@ -27,11 +27,23 @@ typedef pid_t php_process_id_t;
 
 #define PHP_PROC_OPEN_MAX_DESCRIPTORS	16
 
+/* Environment block under win32 is a NUL terminated sequence of NUL terminated
+ * name=value strings.
+ * Under unix, it is an argv style array.
+ * */
+typedef struct _php_process_env {
+	char *envp;
+#ifndef PHP_WIN32
+	char **envarray;
+#endif
+} php_process_env_t;
+
 struct php_process_handle {
 	php_process_id_t	child;
 	int npipes;
 	long pipes[PHP_PROC_OPEN_MAX_DESCRIPTORS];
 	char *command;
 	int is_persistent;
+	php_process_env_t env;
 };
 
