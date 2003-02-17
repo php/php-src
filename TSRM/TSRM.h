fb607799cc3aa512136e9c8ec480c043050e5aeb
Zeev Suraski <zeev@php.net>
2003-02-17 13:27:15 +0000

    More Win32 build improvements

--- TSRM/TSRM.h
+++ TSRM/TSRM.h
@@ -37,6 +37,9 @@
 #ifdef ZTS
 
 #ifdef TSRM_WIN32
+# ifndef TSRM_INCLUDE_FULL_WINDOWS_HEADERS
+#  define WIN32_LEAN_AND_MEAN
+# endif
 # include <windows.h>
 #elif defined(NETWARE)
 # include <nks/thread.h>
