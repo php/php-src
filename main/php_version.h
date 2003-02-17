9c6c9f8fcdc9c7e2dd2e9c613f0b9a9eedaa3a39
foobar <sniper@php.net>
2003-02-17 00:02:11 +0000

    Bump up the version.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 1
+#define PHP_RELEASE_VERSION 2
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.1-dev"
+#define PHP_VERSION "4.3.2-dev"
