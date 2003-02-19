0276d29b4bcf3c7d46f90ea4f808c81f22c975f2
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-02-19 09:27:29 +0000

    scandir -> php_scandir, alphasort -> php_alphasort
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@117077 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/dir.c
+++ ext/standard/dir.c
@@ -24,9 +24,10 @@
 #include "fopen_wrappers.h"
 #include "file.h"
 #include "php_dir.h"
+#include "php_scandir.h"
 
 #ifdef HAVE_DIRENT_H
-# include <dirent.h>
+#include <dirent.h>
 #endif
 
 #if HAVE_UNISTD_H
@@ -39,9 +40,6 @@
 #include "win32/readdir.h"
 #endif
 
-#if !HAVE_ALPHASORT || !HAVE_SCANDIR
-#include "php_scandir.h"
-#endif
 
 #ifdef HAVE_GLOB
 #ifndef PHP_WIN32
@@ -466,9 +464,9 @@ PHP_FUNCTION(scandir)
 	}
 
 	if (!flags) {
-		n = scandir(path, &namelist, 0, alphasort);
+		n = php_scandir(path, &namelist, 0, php_alphasort);
 	} else {
-		n = scandir(path, &namelist, 0, php_alphasortr);
+		n = php_scandir(path, &namelist, 0, php_alphasortr);
 	}
 
 	if (n < 0) {
