e7b71d3932dcb4e41bf6ed8ebeaf8703ad5f83f7
foobar <sniper@php.net>
2003-02-19 18:45:03 +0000

    dirent.h needs to be included always.

--- main/php_scandir.c
+++ main/php_scandir.c
@@ -27,8 +27,6 @@
 
 #include "php_scandir.h"
 
-#ifndef HAVE_SCANDIR
-
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
@@ -36,6 +34,8 @@
 #ifdef HAVE_DIRENT_H
 #include <dirent.h>
 #endif
+
+#ifndef HAVE_SCANDIR
 
 #ifdef PHP_WIN32
 #include "win32/readdir.h"
