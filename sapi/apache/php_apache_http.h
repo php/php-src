58e4503942f85a4da4f541811de00e9b83ca9bb7
foobar <sniper@php.net>
2003-02-23 09:50:43 +0000

    Added missing headers and $Id$ tags.

--- sapi/apache/php_apache_http.h
+++ sapi/apache/php_apache_http.h
@@ -1,3 +1,25 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2003 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.02 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available at through the world-wide-web at                           |
+  | http://www.php.net/license/2_02.txt.                                 |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Rasmus Lerdorf <rasmus@lerdorf.on.ca>                       |
+  |          Stig Sæther Bakken <ssb@fast.no>                            |
+  |          David Sklar <sklar@student.net>                             |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
 #define NO_REGEX_EXTRA_H
 
 #ifdef WIN32
