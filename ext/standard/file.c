1cabc773e4dd8eb231fd7ccba233db775ccef34c
Moriyoshi Koizumi <moriyoshi@php.net>
2003-02-24 22:48:39 +0000

    MFH: made fgetss() binary safe too

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -1390,6 +1390,7 @@ PHPAPI PHP_FUNCTION(fgetss)
 {
 	zval **fd, **bytes, **allow=NULL;
 	int len;
+	size_t actual_len, retval_len;
 	char *buf;
 	php_stream *stream;
 	char *allowed_tags=NULL;
@@ -1428,15 +1429,15 @@ PHPAPI PHP_FUNCTION(fgetss)
 	/*needed because recv doesnt set null char at end*/
 	memset(buf, 0, len + 1);
 
-	if (php_stream_gets(stream, buf, len) == NULL)	{
+	if (php_stream_get_line(stream, buf, len, &actual_len) == NULL)	{
 		efree(buf);
 		RETURN_FALSE;
 	}
 
 	/* strlen() can be used here since we are doing it on the return of an fgets() anyway */
-	php_strip_tags(buf, strlen(buf), &stream->fgetss_state, allowed_tags, allowed_tags_len);
+	retval_len = php_strip_tags(buf, actual_len, &stream->fgetss_state, allowed_tags, allowed_tags_len);
 
-	RETURN_STRING(buf, 0);
+	RETURN_STRINGL(buf, retval_len, 0);
 }
 /* }}} */
 
