87b3063593c58c467e55a99b3b8ebb44b74f4efa
Ilia Alshanetsky <iliaa@php.net>
2003-02-25 16:26:55 +0000

    MFH

--- ext/standard/exec.c
+++ ext/standard/exec.c
@@ -278,7 +278,7 @@ int php_Exec(int type, char *cmd, pval *array, pval *return_value TSRMLS_DC)
 	} else {
 		size_t b;
 
-		while ((b = fread(buf, buflen, 1, fp)) > 0) {
+		while((b = php_stream_read(stream, buf, EXEC_INPUT_BUF)) > 0) {
 			if (output) {
 				PHPWRITE(buf, b);
 			}
