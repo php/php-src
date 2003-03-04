802a410834d5f516a748ba69a53f44604d8f6185
Ilia Alshanetsky <iliaa@php.net>
2003-03-04 16:35:02 +0000

    MFH

--- ext/odbc/php_odbc.c
+++ ext/odbc/php_odbc.c
@@ -2220,15 +2220,7 @@ void odbc_do_connect(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 		persistent = 0;
 	}
 
-	len = strlen(db) + strlen(uid) + strlen(pwd) + sizeof(ODBC_TYPE) + 5;
-	hashed_details = emalloc(len);
-
-	if (hashed_details == NULL) {
-		php_error(E_WARNING, "Out of memory");
-		RETURN_FALSE;
-	}
-
-	hashed_len = sprintf(hashed_details, "%s_%s_%s_%s_%d", ODBC_TYPE, db, uid, pwd, cur_opt);
+	hashed_len = spprintf(hashed_details, 0, "%s_%s_%s_%s_%d", ODBC_TYPE, db, uid, pwd, cur_opt);
 
 	/* FIXME the idea of checking to see if our connection is already persistent
 		is good, but it adds a lot of overhead to non-persistent connections.  We
