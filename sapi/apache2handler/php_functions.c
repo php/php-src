16106bd1f77ae9cac66cdefa8c94bfc7eb751fa0
foobar <sniper@php.net>
2003-03-11 17:22:31 +0000

    OnUpdateLong is only in php5/HEAD. (oops! :)

--- sapi/apache2handler/php_functions.c
+++ sapi/apache2handler/php_functions.c
@@ -469,9 +469,9 @@ static function_entry apache_functions[] = {
 };
 
 PHP_INI_BEGIN()
-	STD_PHP_INI_ENTRY("xbithack",		"0",	PHP_INI_ALL,	OnUpdateLong,	xbithack,	php_apache2_info_struct, php_apache2_info)
-	STD_PHP_INI_ENTRY("engine",		"1",	PHP_INI_ALL,	OnUpdateLong,	engine, 	php_apache2_info_struct, php_apache2_info)
-	STD_PHP_INI_ENTRY("last_modified",	"0",	PHP_INI_ALL,	OnUpdateLong,	last_modified,	php_apache2_info_struct, php_apache2_info)
+	STD_PHP_INI_ENTRY("xbithack",		"0",	PHP_INI_ALL,	OnUpdateInt,	xbithack,		php_apache2_info_struct, php_apache2_info)
+	STD_PHP_INI_ENTRY("engine",			"1",	PHP_INI_ALL,	OnUpdateInt,	engine, 		php_apache2_info_struct, php_apache2_info)
+	STD_PHP_INI_ENTRY("last_modified",	"0",	PHP_INI_ALL,	OnUpdateInt,	last_modified,	php_apache2_info_struct, php_apache2_info)
 PHP_INI_END()
 
 static PHP_MINIT_FUNCTION(apache)
