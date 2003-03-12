9ea0fff51d45b6fcdb1069d22d70c92083246f04
foobar <sniper@php.net>
2003-03-12 22:18:22 +0000

    - Bump the version to RC1.

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 2
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.2-dev"
+#define PHP_EXTRA_VERSION "-RC1"
+#define PHP_VERSION "4.3.2-RC1"
