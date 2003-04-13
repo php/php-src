c777d2f0ea954f4953014476db4d7957384300b6
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-04-13 22:59:19 +0000

    Modify str_replace/str_ireplace to report how many needles were matched. (FR #8218)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@123322 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -125,7 +125,7 @@ PHPAPI char *php_basename(char *str, size_t  len , char *suffix, size_t sufflen)
 PHPAPI size_t php_dirname(char *str, size_t len);
 PHPAPI char *php_stristr(unsigned char *s, unsigned char *t, size_t s_len, size_t t_len);
 PHPAPI char *php_str_to_str_ex(char *haystack, int length, char *needle,
-		int needle_len, char *str, int str_len, int *_new_length, int case_sensitivity);
+		int needle_len, char *str, int str_len, int *_new_length, int case_sensitivity, int *replace_count);
 PHPAPI char *php_str_to_str(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length);
 PHPAPI char *php_trim(char *c, int len, char *what, int what_len, zval *return_value, int mode TSRMLS_DC);
