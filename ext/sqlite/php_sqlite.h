3929decfc5ba85ca21af7c387774eab3f344f107
tal <tal@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-04-20 13:17:17 +0000

    typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@124098 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sqlite/php_sqlite.h
+++ ext/sqlite/php_sqlite.h
@@ -12,8 +12,8 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Wez Furlong <wez@thebrainroom.com>                           |
-  |         Tal Peer <tal@php.net                                        |
+  | Authors: Wez Furlong <wez@thebrainroom.com>                          |
+  |          Tal Peer <tal@php.net                                       |
   +----------------------------------------------------------------------+
 
   $Id$ 
