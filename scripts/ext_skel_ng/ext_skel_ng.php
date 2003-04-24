508923fae1fa0bbb7fd5779ee896c770db68b439
hholzgra <hholzgra@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-04-24 18:01:57 +0000

    use PEAR::System instead of system() to remove directories ... plus some whitespace/indent changes :/
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@124496 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/ext_skel_ng/ext_skel_ng.php
+++ scripts/ext_skel_ng/ext_skel_ng.php
@@ -1,26 +1,27 @@
 <?php
-	require_once "extension_parser.php";
+  require_once "extension_parser.php";
+	require_once "System.php";
 
-	$filename = isset($_SERVER["argv"][1]) ? $_SERVER["argv"][1] : "extension.xml";
+  $filename = isset($_SERVER["argv"][1]) ? $_SERVER["argv"][1] : "extension.xml";
 
-	$ext = &new extension_parser(fopen($filename, "r"));
+  $ext = &new extension_parser(fopen($filename, "r"));
 
-  system("rm -rf {$ext->name}");
+  System::rm("-rf {$ext->name}");
   mkdir($ext->name);
 
-	// generate code
-	$ext->write_header_file();
-	$ext->write_code_file();
-	if(isset($ext->logo)) {
-		$fp = fopen("{$ext->name}/{$ext->name}_logo.h", "w");
-		fwrite($fp, $ext->logo->h_code());
-		fclose($fp);
-		$ext->logo->h_code();
-	}
+  // generate code
+  $ext->write_header_file();
+  $ext->write_code_file();
+  if(isset($ext->logo)) {
+    $fp = fopen("{$ext->name}/{$ext->name}_logo.h", "w");
+    fwrite($fp, $ext->logo->h_code());
+    fclose($fp);
+    $ext->logo->h_code();
+  }
 
-	// generate project files for configure and ms dev studio
-	$ext->write_config_m4();
-	$ext->write_ms_devstudio_dsp();
+  // generate project files for configure and ms dev studio
+  $ext->write_config_m4();
+  $ext->write_ms_devstudio_dsp();
 
-	$ext->generate_documentation();
+  $ext->generate_documentation();
 ?>
