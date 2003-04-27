037177331222e0b9ded2b1c8ca982fd7e5f01c6f
Marcus Boerger <helly@php.net>
2003-04-27 17:20:12 +0000

    MFH fix

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -600,7 +600,7 @@ int main(int argc, char *argv[])
 				php_cli_usage(argv[0]);
 				php_end_ob_buffers(1 TSRMLS_CC);
 				exit_status=1;
-				goto out;
+				goto out_err;
 
 			case 'i': /* php info & quit */
 				if (php_request_startup(TSRMLS_C)==FAILURE) {
