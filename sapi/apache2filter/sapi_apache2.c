63693db502c6a68c98d970624dd0d3fd69d626ae
Ilia Alshanetsky <iliaa@php.net>
2003-04-30 00:47:20 +0000

    MFH

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -388,7 +388,7 @@ static void php_apache_request_ctor(ap_filter_t *f, php_struct *ctx TSRMLS_DC)
 	apr_table_unset(f->r->headers_out, "Expires");
 	apr_table_unset(f->r->headers_out, "ETag");
 	apr_table_unset(f->r->headers_in, "Connection");
-	if (!PG(safe_mode) || (PG(safe_mode) && !ap_auth_type(r))) {
+	if (!PG(safe_mode) || (PG(safe_mode) && !ap_auth_type(f->r))) {
 		auth = apr_table_get(f->r->headers_in, "Authorization");
 		php_handle_auth_data(auth TSRMLS_CC);
 	} else {
