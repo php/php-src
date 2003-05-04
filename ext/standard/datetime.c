1a93d01bb9866ff82b3991d8646f684d4ae91e3f
moriyoshi <moriyoshi@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-05-04 11:19:57 +0000

    Added missing sanity check # likely a fix for bug #23457
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@125654 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/datetime.c
+++ ext/standard/datetime.c
@@ -314,7 +314,11 @@ static void php_date(INTERNAL_FUNCTION_PARAMETERS, int gm)
 #else
 		tzone = timezone;
 #endif
-		tname[0] = tzname[0];
+		if (tzname[0] != NULL) {
+			tname[0] = tzname[0];
+		} else {
+			tname[0] = "???";
+		}
 #endif
 	}
 
