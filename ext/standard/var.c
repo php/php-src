8eac6854f5b393b57237af94baa5794b2c96811b
Ilia Alshanetsky <iliaa@php.net>
2003-05-04 18:41:37 +0000

    MFH

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -514,7 +514,7 @@ static void php_var_serialize_intern(smart_str *buf, zval **struc, HashTable *va
 				char s[256];
 				ulong slen;
 
-				slen = sprintf(s, "d:%.*G;", (int) EG(precision), Z_DVAL_PP(struc));
+				slen = sprintf(s, "d:%.252g;", Z_DVAL_PP(struc));
 				smart_str_appendl(buf, s, slen);
 				return;
 			}
