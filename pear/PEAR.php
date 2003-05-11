d97802de7de4f08b045b1588ee1542dc2668146a
mj <mj@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-05-11 09:33:04 +0000

    * Fixing note
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@126536 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR.php
+++ pear/PEAR.php
@@ -63,7 +63,7 @@ ini_set('track_errors', true);
  * destructor, use error_log(), syslog() or something similar.
  *
  * IMPORTANT! To use the emulated destructors you need to create the
- * objects by reference, ej: $obj =& new PEAR_child;
+ * objects by reference: $obj =& new PEAR_child;
  *
  * @since PHP 4.0.2
  * @author Stig Bakken <ssb@php.net>
