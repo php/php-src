e2444c223b0b6aab53012ab6df856a530d6d2b09
Ilia Alshanetsky <iliaa@php.net>
2003-05-16 01:09:58 +0000

    MFB

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -2438,7 +2438,7 @@ PHP_FUNCTION(ini_set)
 }
 /* }}} */
 
-/* {{{ proto string ini_restore(string varname)
+/* {{{ proto void ini_restore(string varname)
    Restore the value of a configuration option specified by varname */
 PHP_FUNCTION(ini_restore)
 {
@@ -2501,7 +2501,7 @@ PHP_FUNCTION(get_include_path)
 
 /* }}} */
 
-/* {{{ proto string restore_include_path()
+/* {{{ proto void restore_include_path()
    Restore the value of the include_path configuration option */
 
 PHP_FUNCTION(restore_include_path)
