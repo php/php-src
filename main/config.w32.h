f6dcea8c1e09b3bc9738f5468f19725139a36ec1
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-05-19 16:28:08 +0000

    First try at getting the Windows build working again after libxml2 got unbundled.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@127769 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/config.w32.h
+++ main/config.w32.h
@@ -74,7 +74,6 @@
 
 /* Enable / Disable XML extension (default: enabled) */
 #define HAVE_LIBEXPAT 1
-#define HAVE_XML 1
 
 /* Enable / Disable ZLIB extension (default: enabled) */
 #define HAVE_ZLIB 1
