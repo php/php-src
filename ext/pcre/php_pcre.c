12ce650f0327acdd7e1f7dcffc24f9a8a4a5466c
Andrei Zmievski <andrei@php.net>
2003-05-20 18:12:07 +0000

    MFH

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -56,6 +56,7 @@ static void *php_pcre_malloc(size_t size)
 
 static void php_pcre_free(void *ptr)
 {
+	if (ptr)
 	pefree(ptr, 1);
 }
 
@@ -63,6 +64,7 @@ static void php_pcre_free(void *ptr)
 static void php_free_pcre_cache(void *data)
 {
 	pcre_cache_entry *pce = (pcre_cache_entry *) data;
+	if (!pce) return;
 	pefree(pce->re, 1);
 #if HAVE_SETLOCALE
 	if ((void*)pce->tables) pefree((void*)pce->tables, 1);
