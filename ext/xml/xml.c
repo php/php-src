fb20dbe1a62b3c39a78a8e89faf33ce6db0a8b2b
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-05-25 07:23:41 +0000

    Hopefully get correct display of version information.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@128696 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xml/xml.c
+++ ext/xml/xml.c
@@ -261,7 +261,11 @@ PHP_MINFO_FUNCTION(xml)
 	php_info_print_table_start();
 	php_info_print_table_row(2, "XML Support", "active");
 	php_info_print_table_row(2, "XML Namespace Support", "active");
-	php_info_print_table_row(2, "EXPAT Version",XML_ExpatVersion());
+#ifdef LIBXML_DOTTED_VERSION
+	php_info_print_table_row(2, "libxml2 Version", LIBXML_DOTTED_VERSION);
+#else
+	php_info_print_table_row(2, "EXPAT Version", XML_ExpatVersion());
+#endif
 	php_info_print_table_end();
 }
 /* }}} */
