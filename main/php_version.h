dc963b2f91ae48faae7edf4609a8c7427dc9317a
SVN Migration <svn@php.net>
2003-05-27 17:24:10 +0000

    This commit was manufactured by cvs2svn to create tag 'php_4_3_2RC4'.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 3
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.3-dev"
+#define PHP_RELEASE_VERSION 2
+#define PHP_EXTRA_VERSION "RC4"
+#define PHP_VERSION "4.3.2RC4"
