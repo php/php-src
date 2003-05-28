fbc32ea828c9ca17524ebe61b56eb79034d0f497
Ilia Alshanetsky <iliaa@php.net>
2003-05-28 22:21:13 +0000

    Fixed compiler warnings.

--- ext/sysvshm/php_sysvshm.h
+++ ext/sysvshm/php_sysvshm.h
@@ -64,10 +64,6 @@ PHP_FUNCTION(shm_put_var);
 PHP_FUNCTION(shm_get_var);
 PHP_FUNCTION(shm_remove_var);
 
-static int php_put_shm_data(sysvshm_chunk_head *ptr,long key,char *data, long len);
-static long php_check_shm_data(sysvshm_chunk_head *ptr, long key);
-static int php_remove_shm_data(sysvshm_chunk_head *ptr, long shm_varpos);
-
 extern sysvshm_module php_sysvshm;
 
 #else
