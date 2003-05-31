fe95410380768db908216e03160da65ffd25c942
Marcus Boerger <helly@php.net>
2003-05-31 02:31:10 +0000

    Reapply this one, too.

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -634,7 +634,11 @@ int main(int argc, char *argv[])
 				if (php_request_startup(TSRMLS_C)==FAILURE) {
 					goto err;
 				}
+#if ZEND_DEBUG
+				php_printf("PHP %s (%s) (built: %s %s) (DEBUG)\nCopyright (c) 1997-2003 The PHP Group\n%s", PHP_VERSION, sapi_module.name, __DATE__, __TIME__, get_zend_version());
+#else
 				php_printf("PHP %s (%s) (built: %s %s)\nCopyright (c) 1997-2003 The PHP Group\n%s", PHP_VERSION, sapi_module.name, __DATE__, __TIME__, get_zend_version());
+#endif
 				php_end_ob_buffers(1 TSRMLS_CC);
 				exit_status=1;
 				goto out;
