2da81fefb8b8589a9f3602386fd5f0426a19d006
Uwe Schindler <thetaphi@php.net>
2003-05-31 15:32:38 +0000

    zend_alter_ini_entry now with PHP_INI_USER

--- sapi/nsapi/nsapi.c
+++ sapi/nsapi/nsapi.c
@@ -697,7 +697,7 @@ static void nsapi_php_ini_entries(NSLS_D TSRMLS_DC)
 				/* change the ini entry */
 				if (zend_alter_ini_entry(entry->param->name, strlen(entry->param->name)+1,
 				 entry->param->value, strlen(entry->param->value),
-				 PHP_INI_SYSTEM, PHP_INI_STAGE_RUNTIME)==FAILURE) {
+				 PHP_INI_USER, PHP_INI_STAGE_RUNTIME)==FAILURE) {
 					log_error(LOG_WARN, "php4_execute", NSG(sn), NSG(rq), "Cannot change php.ini key \"%s\" to \"%s\"", entry->param->name, entry->param->value);
 				}
 			}
