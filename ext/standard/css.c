d70a038cc33d16a8d53cbef8d1fb91818ccbded7
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-06-03 00:06:01 +0000

    Fixed bug #23912 (Invalid CSS in phpinfo() output)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@129901 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/css.c
+++ ext/standard/css.c
@@ -29,23 +29,22 @@ PHPAPI void php_info_print_css(TSRMLS_D)
 	PUTS("body {background-color: #ffffff; color: #000000;}\n");
 	PUTS("body, td, th, h1, h2 {font-family: sans-serif;}\n");
 	PUTS("pre {margin: 0px; font-family: monospace;}\n");
-	PUTS("a:link {color: #000099; text-decoration: none;}\n");
+	PUTS("a:link {color: #000099; text-decoration: none; background-color: #ffffff;}\n");
 	PUTS("a:hover {text-decoration: underline;}\n");
 	PUTS("table {border-collapse: collapse;}\n");
 	PUTS(".center {text-align: center;}\n");
 	PUTS(".center table { margin-left: auto; margin-right: auto; text-align: left;}\n");
-	PUTS(".center th { text-align: center; !important }\n");
+	PUTS(".center th { text-align: center !important; }\n");
 	PUTS("td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}\n");
 	PUTS("h1 {font-size: 150%;}\n");
 	PUTS("h2 {font-size: 125%;}\n");
 	PUTS(".p {text-align: left;}\n");
-	PUTS(".e {background-color: #ccccff; font-weight: bold;}\n");
-	PUTS(".h {background-color: #9999cc; font-weight: bold;}\n");
-	PUTS(".v {background-color: #cccccc;}\n");
-	PUTS(".vr {background-color: #cccccc; text-align: right;}\n");
-	PUTS("i {color: #666666;}\n");
+ 	PUTS(".e {background-color: #ccccff; font-weight: bold; color: #000000;}\n");
+ 	PUTS(".h {background-color: #9999cc; font-weight: bold; color: #000000;}\n");
+ 	PUTS(".v {background-color: #cccccc; color: #000000;}\n");
+	PUTS(".vr {background-color: #cccccc; text-align: right; color: #000000;}\n");
 	PUTS("img {float: right; border: 0px;}\n");
-	PUTS("hr {width: 600px; align: center; background-color: #cccccc; border: 0px; height: 1px;}\n");
+	PUTS("hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}\n");
 }
 /* }}} */
 
