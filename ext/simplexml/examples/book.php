21d90d6ac40eebf68b52a8f0691c5d956d0187ba
Sterling Hughes <sterling@php.net>
2003-06-09 04:01:11 +0000

    ok, this is a zend bug.

--- ext/simplexml/examples/book.php
+++ ext/simplexml/examples/book.php
@@ -1,6 +1,7 @@
 <?php
-$books = simplexml_load_file('book.xml')->book;
-
+$books = simplexml_load_file('book.xml');
+//var_dump($books);
+$books = $books->book;
 foreach ($books as $book) {
 	echo "{$book->title} was written by {$book->author}\n";
 }
