37a8dc407696bd85986059dba11dbb32eb433919
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-06-12 12:21:33 +0000

    proto fixes
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@131190 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/bcmath/bcmath.c
+++ ext/bcmath/bcmath.c
@@ -485,7 +485,7 @@ PHP_FUNCTION(bcsqrt)
 }
 /* }}} */
 
-/* {{{ proto string bccomp(string left_operand, string right_operand [, int scale])
+/* {{{ proto int bccomp(string left_operand, string right_operand [, int scale])
    Compares two arbitrary precision numbers */
 PHP_FUNCTION(bccomp)
 {
@@ -527,7 +527,7 @@ PHP_FUNCTION(bccomp)
 }
 /* }}} */
 
-/* {{{ proto string bcscale(int scale)
+/* {{{ proto bool bcscale(int scale)
    Sets default scale parameter for all bc math functions */
 PHP_FUNCTION(bcscale)
 {
