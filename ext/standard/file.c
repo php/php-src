c04d4138634e6434b831e9caaecf89cdf61790e0
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-06-14 17:08:49 +0000

    Fix  ZTS mode
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@131566 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -131,6 +131,7 @@ static ZEND_RSRC_DTOR_FUNC(file_context_dtor)
 {
 	php_stream_context *context = (php_stream_context*)rsrc->ptr;
 	zval_dtor(context->options);
+	context->options = NULL;
 	php_stream_context_free(context);
 }
 
