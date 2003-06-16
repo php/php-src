0036953b09369ffc4604477834396e7752895ed0
cox <cox@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-06-16 07:44:46 +0000

    Do not create the registry obj at Installer object creation (it's problematic when pear install -R <DIR> is used)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@131811 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Installer.php
+++ pear/PEAR/Installer.php
@@ -117,7 +117,7 @@ class PEAR_Installer extends PEAR_Common
         parent::PEAR_Common();
         $this->setFrontendObject($ui);
         $this->debug = $this->config->get('verbose');
-        $this->registry = &new PEAR_Registry($this->config->get('php_dir'));
+        //$this->registry = &new PEAR_Registry($this->config->get('php_dir'));
     }
 
     // }}}
@@ -801,6 +801,9 @@ class PEAR_Installer extends PEAR_Common
      */
     function checkDeps(&$pkginfo, &$errors)
     {
+        if (empty($this->registry)) {
+            $this->registry = &new PEAR_Registry($this->config->get('php_dir'));
+        }
         $depchecker = &new PEAR_Dependency($this->registry);
         $error = $errors = '';
         $failed_deps = $optional_deps = array();
