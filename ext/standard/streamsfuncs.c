7ef01f01b936e3a4ec12cab2c2a8e0047c396114
Ilia Alshanetsky <iliaa@php.net>
2003-06-19 21:04:43 +0000

    Reverting previous patch (not necessary, thanks Pollita ;) )

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -669,10 +669,10 @@ static int parse_context_options(php_stream_context *context, zval *options)
 
 static int parse_context_params(php_stream_context *context, zval *params)
 {
-	int ret = FAILURE;
+	int ret = SUCCESS;
 	zval **tmp;
 
-	if ((ret = zend_hash_find(Z_ARRVAL_P(params), "notification", sizeof("notification"), (void**)&tmp)) == SUCCESS) {
+	if (SUCCESS == zend_hash_find(Z_ARRVAL_P(params), "notification", sizeof("notification"), (void**)&tmp)) {
 		
 		if (context->notifier) {
 			php_stream_notification_free(context->notifier);
@@ -684,14 +684,10 @@ static int parse_context_params(php_stream_context *context, zval *params)
 		context->notifier->ptr = *tmp;
 		ZVAL_ADDREF(*tmp);
 	}
-	if ((ret = zend_hash_find(Z_ARRVAL_P(params), "options", sizeof("options"), (void**)&tmp)) == SUCCESS) {
+	if (SUCCESS == zend_hash_find(Z_ARRVAL_P(params), "options", sizeof("options"), (void**)&tmp)) {
 		parse_context_options(context, *tmp);
 	}
-
-	if (ret != SUCCESS) {
-		php_stream_context_free(context);
-	}
-
+	
 	return ret;
 }
 
