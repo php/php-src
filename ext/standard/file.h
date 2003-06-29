555173a263e87cda217f1268d18f6378f2d13482
Edin Kadribasic <edink@php.net>
2003-06-29 23:29:49 +0000

    MFH: extensions like imap need this exported

--- ext/standard/file.h
+++ ext/standard/file.h
@@ -120,7 +120,7 @@ typedef struct {
 
 #ifdef ZTS
 #define FG(v) TSRMG(file_globals_id, php_file_globals *, v)
-extern int file_globals_id;
+extern PHPAPI int file_globals_id;
 #else
 #define FG(v) (file_globals.v)
 extern php_file_globals file_globals;
