46fd530566d9f338d3bba7910aedf35a42171b37
Marcus Boerger <helly@php.net>
2003-07-03 11:23:27 +0000

    MFH: Fix open mode (noticed by FutureQuest, Inc.)

--- ext/dba/dba_db2.c
+++ ext/dba/dba_db2.c
@@ -55,7 +55,7 @@ DBA_OPEN_FUNC(db2)
 
 	type = info->mode == DBA_READER ? DB_UNKNOWN :
 		info->mode == DBA_TRUNC ? DB_BTREE :
-		s ? DB_BTREE : DB_UNKNOWN;
+		!s ? DB_BTREE : DB_UNKNOWN;
 	  
 	gmode = info->mode == DBA_READER ? DB_RDONLY :
 		(info->mode == DBA_CREAT && s) ? DB_CREATE : 
