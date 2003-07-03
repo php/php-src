0e79c8e828f7c4360570fb0dbb642f92c386be62
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-07-03 02:59:04 +0000

    Cut the long line a bit for readability..
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@134038 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -696,7 +696,9 @@ SAPI_API SAPI_POST_HANDLER_FUNC(rfc1867_post_handler)
 {
 	char *boundary, *s=NULL, *boundary_end = NULL, *start_arr=NULL, *array_index=NULL;
 	char *temp_filename=NULL, *lbuf=NULL, *abuf=NULL;
-	int boundary_len=0, total_bytes=0, cancel_upload=0, is_arr_upload=0, array_len=0, max_file_size=0, skip_upload=0, anonindex=0, is_anonymous;	zval *http_post_files=NULL;
+	int boundary_len=0, total_bytes=0, cancel_upload=0, is_arr_upload=0, array_len=0;
+	int max_file_size=0, skip_upload=0, anonindex=0, is_anonymous;
+	zval *http_post_files=NULL;
 #if HAVE_MBSTRING && !defined(COMPILE_DL_MBSTRING)
 	int str_len=0
 #endif
