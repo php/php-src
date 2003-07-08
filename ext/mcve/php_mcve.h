c0f72d9fac75fcb7516867b7dd55983b77129a17
Brad House <bradmssw@php.net>
2003-07-08 03:16:39 +0000

    update mcve ext from HEAD. Sync with libmcve-3.2 functionality

--- ext/mcve/php_mcve.h
+++ ext/mcve/php_mcve.h
@@ -4,10 +4,10 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 1997-2003 The PHP Group                                |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.02 of the PHP license,      |
+   | This source file is subject to version 3.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
-   | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_02.txt.                                 |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_0.txt.                                  |
    | If you did not receive a copy of the PHP license and are unable to   |
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
@@ -16,6 +16,7 @@
    |          Chris Faulhaber <jedgar@fxp.org>                            |
    +----------------------------------------------------------------------+
 */
+/* $Id$ */
 
 #ifndef _PHP_MCVE_H
 #define _PHP_MCVE_H
@@ -25,7 +26,7 @@ extern zend_module_entry mcve_module_entry;
 #define mcve_module_ptr &mcve_module_entry
 #define phpext_mcve_ptr mcve_module_ptr
 
-#define PHP_MCVE_VERSION	"3.0"
+#define PHP_MCVE_VERSION	"3.2"
 
 #define MCVE_CONST (CONST_CS | CONST_PERSISTENT)
 
@@ -39,6 +40,7 @@ PHP_FUNCTION(mcve_destroyconn);
 PHP_FUNCTION(mcve_setdropfile);
 PHP_FUNCTION(mcve_setip);
 PHP_FUNCTION(mcve_setssl);
+PHP_FUNCTION(mcve_setssl_files);
 PHP_FUNCTION(mcve_setblocking);
 PHP_FUNCTION(mcve_settimeout);
 PHP_FUNCTION(mcve_verifyconnection);
