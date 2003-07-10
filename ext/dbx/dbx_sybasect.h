76aba9b7550bcaf0caf16bf443d148cbc1ff5104
Marc Boeren <mboeren@php.net>
2003-07-10 08:25:58 +0000

    Add -2003 to copyright line (Marc).

--- ext/dbx/dbx_sybasect.h
+++ ext/dbx/dbx_sybasect.h
@@ -6,7 +6,7 @@
    +----------------------------------------------------------------------+
    | dbx module version 1.0                                               |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2001 Guidance Rotterdam BV                             |
+   | Copyright (c) 2001-2003 Guidance Rotterdam BV                        |
    +----------------------------------------------------------------------+
    | This source file is subject to version 3.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
