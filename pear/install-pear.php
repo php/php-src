e301195f756c23dd535714575bb23ad0ae0904c3
cox <cox@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-07-10 01:34:46 +0000

    Made the install of the config honors $INSTALL_ROOT
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@134561 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/install-pear.php
+++ pear/install-pear.php
@@ -115,13 +115,17 @@ foreach ($install_files as $package => $instfile) {
                             'a previous PEAR installation at ' .
                             "'$ufile'. You may probably want to remove it.");
         }
-        $ui->outputData('Writing PEAR system config file at: ' . $config->files['system']);
-        $ui->outputData('You may want to add: ' . $config->get('php_dir') . ' to your php.ini include_path');
         $config->set('verbose', 1, 'default');
         foreach ($config->getKeys() as $key) {
             $data[$key] = $config->get($key);
         }
-        $config->store('system', $data);
+        $cnf_file = $config->getConfFile('system');
+        if (!empty($install_root)) {
+            $cnf_file = $install_root . DIRECTORY_SEPARATOR . $cnf_file;
+        }
+        $config->writeConfigFile($cnf_file, 'system', $data);
+        $ui->outputData('Wrote PEAR system config file at: ' . $cnf_file);
+        $ui->outputData('You may want to add: ' . $config->get('php_dir') . ' to your php.ini include_path');
     }
 }
 ?>
