839e57152aed324f31a75642080cc9f60bc6cbb5
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-07-11 07:48:13 +0000

    - CS/WS fixes - Updated CREDITS - Added some kind of version check for ming.. - Fixed some possible BC issues.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@134657 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ming/php_ming.h
+++ ext/ming/php_ming.h
@@ -1,23 +1,23 @@
 /*
-   +----------------------------------------------------------------------+
-   | PHP Version 4                                                        |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2003 The PHP Group                                |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.0 of the PHP license,       |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_0.txt.                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: dave@opaque.net                                              |
-   +----------------------------------------------------------------------+
-
-  $Id$ 
-
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2003 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors: Dave Hayden <dave@opaque.net>                               |
+  |          Frank M. Kromann <fmk@php.net>                              |
+  +----------------------------------------------------------------------+
 */
+
+/* $Id$ */
 
 #ifndef _PHP_MING_H
 #define _PHP_MING_H
@@ -26,8 +26,6 @@
 
 extern zend_module_entry ming_module_entry;
 #define ming_module_ptr &ming_module_entry
-
-#include <ming.h>
 
 PHP_RINIT_FUNCTION(ming);
 PHP_MINIT_FUNCTION(ming);
@@ -76,7 +74,10 @@ PHP_FUNCTION(swfmovie_setBackground);
 PHP_FUNCTION(swfmovie_setRate);
 PHP_FUNCTION(swfmovie_setDimension);
 PHP_FUNCTION(swfmovie_setFrames);
+
+#ifdef HAVE_NEW_MING
 PHP_FUNCTION(swfmovie_streamMp3);
+#endif
 
 PHP_FUNCTION(swfsprite_init);
 PHP_FUNCTION(swfsprite_add);
