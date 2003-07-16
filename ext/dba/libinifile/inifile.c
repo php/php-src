d3cc1a3186a7384593d2b971b50026404205418e
Marcus Boerger <helly@php.net>
2003-07-16 20:56:06 +0000

    MFH: Fix handling of empty settings

--- ext/dba/libinifile/inifile.c
+++ ext/dba/libinifile/inifile.c
@@ -154,7 +154,7 @@ static char *etrim(const char *str)
 		return NULL;
 	}
 	val = (char*)str;
-	while (strchr(" \t\r\n", *val)) {
+	while (*val && strchr(" \t\r\n", *val)) {
 		val++;
 	}
 	l = strlen(val);
