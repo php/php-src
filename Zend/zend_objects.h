96f270781bb060ac46d5e3d6c3461744f3de93c5
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-07-19 09:47:00 +0000

    Shuffle code to ease writing clone handlers
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@135421 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_objects.h
+++ Zend/zend_objects.h
@@ -27,6 +27,7 @@
 ZEND_API zend_object_value zend_objects_new(zend_object **object, zend_class_entry *class_type TSRMLS_DC);
 ZEND_API void zend_objects_destroy_object(zend_object *object, zend_object_handle handle TSRMLS_DC);
 ZEND_API zend_object *zend_objects_get_address(zval *object TSRMLS_DC);
+ZEND_API void zend_objects_clone_members(zend_object *new_object, zend_object_value new_obj_val, zend_object *old_object, zend_object_handle handle TSRMLS_DC);
 ZEND_API zend_object_value zend_objects_clone_obj(zval *object TSRMLS_DC);
 
 #endif /* ZEND_OBJECTS_H */
