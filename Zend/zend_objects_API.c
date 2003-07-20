0ba7757ea7476e6ff5ed1fccf45c529c9a97bd60
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-07-20 17:50:23 +0000

    More informative errors here and these are real core errors
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@135507 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_objects_API.c
+++ Zend/zend_objects_API.c
@@ -166,12 +166,12 @@ ZEND_API zend_object_value zend_objects_store_clone_obj(zval *zobject TSRMLS_DC)
 	zend_object_handle handle = Z_OBJ_HANDLE_P(zobject);
 
 	if (!EG(objects_store).object_buckets[handle].valid) {
-		zend_error(E_ERROR, "Trying to clone invalid object");
+		zend_error(E_CORE_ERROR, "Trying to clone invalid object of class %s", Z_OBJCE_P(zobject)->name);
 	}
 	obj = &EG(objects_store).object_buckets[handle].bucket.obj;
 	
 	if (obj->clone == NULL) {
-		zend_error(E_ERROR, "Trying to clone uncloneable object");
+		zend_error(E_CORE_ERROR, "Trying to clone uncloneable object of class %s", Z_OBJCE_P(zobject)->name);
 	}		
 
 	obj->clone(obj->object, &new_object TSRMLS_CC);
