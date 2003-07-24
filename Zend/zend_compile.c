55097d1dcf6eee94413dc2795f923c0ef9f45d3f
Zeev Suraski <zeev@php.net>
2003-07-24 13:14:57 +0000

    Fix another HANDLE_NUMERIC bug.  Looks like you opened Pandora's box, Sterling ;)

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2644,7 +2644,7 @@ void zend_do_add_static_array_element(znode *result, znode *offset, znode *expr)
 				element->type |= IS_CONSTANT_INDEX;
 				/* break missing intentionally */
 			case IS_STRING:
-				zend_hash_update(result->u.constant.value.ht, offset->u.constant.value.str.val, offset->u.constant.value.str.len+1, &element, sizeof(zval *), NULL);
+				zend_symtable_update(result->u.constant.value.ht, offset->u.constant.value.str.val, offset->u.constant.value.str.len+1, &element, sizeof(zval *), NULL);
 				zval_dtor(&offset->u.constant);
 				break;
 			case IS_LONG:
