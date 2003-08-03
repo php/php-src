23fe410bdeb19ecc29bcfbcac9d7cb097e0050a2
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-03 18:55:51 +0000

    Fix build
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@136525 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqli/mysqli_fe.c
+++ ext/mysqli/mysqli_fe.c
@@ -237,7 +237,7 @@ function_entry mysqli_result_methods[] = {
 function_entry mysqli_stmt_methods[] = {
 	PHP_FALIAS(affected_rows,mysqli_stmt_affected_rows,NULL)
 	PHP_FALIAS(bind_param,mysqli_bind_param,second_arg_force_by_ref_rest)
-	PHP_FALIAS(bind_result,mysqli_bind_result, all_args_force_ref)
+	PHP_FALIAS(bind_result,mysqli_bind_result, all_args_by_ref)
 	PHP_FALIAS(execute,mysqli_execute,NULL)
 	PHP_FALIAS(fetch,mysqli_fetch,NULL)
 	PHP_FALIAS(param_count,mysqli_param_count,NULL)
