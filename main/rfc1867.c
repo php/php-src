f3618527a6c50ab6617f6ec801416e4de7df07ce
Ilia Alshanetsky <iliaa@php.net>
2003-08-05 01:01:40 +0000

    MFH: remove useless variable when mbstring is not used.

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -692,13 +692,16 @@ SAPI_API SAPI_POST_HANDLER_FUNC(rfc1867_post_handler)
 {
 	char *boundary, *s=NULL, *boundary_end = NULL, *start_arr=NULL, *array_index=NULL;
 	char *temp_filename=NULL, *lbuf=NULL, *abuf=NULL;
-	int boundary_len=0, total_bytes=0, cancel_upload=0, is_arr_upload=0, array_len=0, max_file_size=0, skip_upload=0, str_len=0;
+	int boundary_len=0, total_bytes=0, cancel_upload=0, is_arr_upload=0, array_len=0, max_file_size=0, skip_upload=0;
 	zval *http_post_files=NULL;
 	zend_bool magic_quotes_gpc;
 	multipart_buffer *mbuff;
 	zval *array_ptr = (zval *) arg;
 	FILE *fp;
 	zend_llist header;
+#if HAVE_MBSTRING && !defined(COMPILE_DL_MBSTRING)
+	int str_len=0;
+#endif
 
 	if (SG(request_info).content_length > SG(post_max_size)) {
 		sapi_module.sapi_error(E_WARNING, "POST Content-Length of %d bytes exceeds the limit of %d bytes", SG(request_info).content_length, SG(post_max_size));
