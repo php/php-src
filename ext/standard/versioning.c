3d598476ecd5976e66de01b8fe0167b4d2d5a980
foobar <sniper@php.net>
2003-08-05 12:34:49 +0000

    MFH: - Fixed bug #21611 (version_compare() does not support "p" suffix)

--- ext/standard/versioning.c
+++ ext/standard/versioning.c
@@ -90,7 +90,7 @@ static int
 compare_special_version_forms(char *form1, char *form2)
 {
 	int found1 = -1, found2 = -1;
-	special_forms_t special_forms[9] = {
+	special_forms_t special_forms[10] = {
 		{"dev", 0},
 		{"alpha", 1},
 		{"a", 1},
@@ -99,6 +99,7 @@ compare_special_version_forms(char *form1, char *form2)
 		{"RC", 3},
 		{"#", 4},
 		{"pl", 5},
+		{"p", 5},
 		{NULL, 0},
 	};
 	special_forms_t *pp;
