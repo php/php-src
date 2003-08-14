33d7423950cb69ed7e75e9a26e42262dba383824
Ilia Alshanetsky <iliaa@php.net>
2003-08-14 14:37:33 +0000

    MFH: Fixed bug #25081 (odbc_fetch_array() may mangle numeric fields).

--- ext/odbc/php_odbc.c
+++ ext/odbc/php_odbc.c
@@ -1459,8 +1459,13 @@ static void php_odbc_fetch_hash(INTERNAL_FUNCTION_PARAMETERS, int result_type)
 		if (result_type & ODBC_NUM) {
 			zend_hash_index_update(Z_ARRVAL_P(return_value), i, &tmp, sizeof(pval *), NULL);
 		} else {
-			zend_hash_update(Z_ARRVAL_P(return_value), result->values[i].name, 
+			if (!*(result->values[i].name)) {
+				zend_hash_update(Z_ARRVAL_P(return_value), Z_STRVAL_P(tmp),
+					Z_STRLEN_P(tmp)+1, &tmp, sizeof(pval *), NULL);
+			} else {
+				zend_hash_update(Z_ARRVAL_P(return_value), result->values[i].name, 
 					strlen(result->values[i].name)+1, &tmp, sizeof(pval *), NULL);
+			}
 		}
 	}
 	if (buf) efree(buf);
