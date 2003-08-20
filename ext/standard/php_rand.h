4766b9ec93c776cff87c768d481ed682153a26c1
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-20 16:40:22 +0000

    Fixed bug #25170 (Problem with generation of random numbers on solaris)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@138352 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_rand.h
+++ ext/standard/php_rand.h
@@ -33,7 +33,7 @@
 #define RAND_MAX (1<<15)
 #endif
 
-#if HAVE_LRAND48 && !defined(__sun)
+#if defined(HAVE_LRAND48) || defined(HAVE_RANDOM)
 #define PHP_RAND_MAX 2147483647
 #else
 #define PHP_RAND_MAX RAND_MAX
