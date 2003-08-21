459277a70d99c48c8cb732722b8af6bb0ca64f1e
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-21 14:34:07 +0000

    Fix parameter spec
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@138424 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -1214,7 +1214,7 @@ PHP_FUNCTION(mysqli_options)
 	long  			ret;
 	PR_MYSQL		*prmysql;
 
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Oslz", &mysql_link, mysqli_link_class_entry, &mysql_option, &mysql_value) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Olz", &mysql_link, mysqli_link_class_entry, &mysql_option, &mysql_value) == FAILURE) {
 		return;
 	}
 	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, prmysql, PR_MYSQL *, &mysql_link, "mysqli_link");
