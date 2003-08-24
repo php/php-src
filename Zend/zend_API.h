e439749aa2cbfa30567b5131bcd518b5d1f11700
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-24 16:35:58 +0000

    Don't identify alias'ed functions
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@138658 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -50,7 +50,7 @@ typedef struct _zend_function_entry {
 
 #define ZEND_NAMED_FE(zend_name, name, arg_info)	ZEND_FENTRY(zend_name, name, arg_info, 0)
 #define ZEND_FE(name, arg_info)						ZEND_FENTRY(name, ZEND_FN(name), arg_info, 0)
-#define ZEND_FALIAS(name, alias, arg_info)			ZEND_FENTRY(name, ZEND_FN(alias), arg_info, ZEND_ACC_PUBLIC|ZEND_ACC_ALIAS)
+#define ZEND_FALIAS(name, alias, arg_info)			ZEND_FENTRY(name, ZEND_FN(alias), arg_info, 0)
 #define ZEND_ME(classname, name, arg_info, flags)	ZEND_FENTRY(name, ZEND_FN(classname##_##name), arg_info, flags)
 #define ZEND_ABSTRACT_ME(classname, name, arg_info)	ZEND_FENTRY(name, NULL, arg_info, ZEND_ACC_PUBLIC|ZEND_ACC_ABSTRACT)
 
