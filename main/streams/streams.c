af8e15b8db31582eb61db06d5c61bde4beeb8fa7
Sascha Schumann <sas@php.net>
2003-08-28 17:07:40 +0000

    kill warnings

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -1505,8 +1505,8 @@ PHPAPI php_stream *_php_stream_open_wrapper_ex(char *path, char *mode, int optio
 	php_stream *stream = NULL;
 	php_stream_wrapper *wrapper = NULL;
 	char *path_to_open;
-	int persistent = options & STREAM_OPEN_PERSISTENT;
 #if ZEND_DEBUG
+	int persistent = options & STREAM_OPEN_PERSISTENT;
 	char *copy_of_path = NULL;
 #endif
 
@@ -1574,7 +1574,7 @@ PHPAPI php_stream *_php_stream_open_wrapper_ex(char *path, char *mode, int optio
 					char *tmp = estrdup(path);
 					php_strip_url_passwd(tmp);
 					php_error_docref1(NULL TSRMLS_CC, tmp, E_WARNING, "could not make seekable - %s",
-							tmp, strerror(errno));
+							tmp);
 					efree(tmp);
 
 					options ^= REPORT_ERRORS;
