2c5fb182018e55357357e6cb87466f47664cd778
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-28 22:56:41 +0000

    Add zend_throw_exception_ex() which allows to format exception messages. # # Since we don't have any portable way of printing into a dynamic buffer i # used a stack buffer of 1K (just like the error printing) and used a dynamic # buffer in case the necessary function is available. #
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@139046 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_exceptions.h
+++ Zend/zend_exceptions.h
@@ -30,6 +30,7 @@ ZEND_API void zend_register_default_classes(TSRMLS_D);
 /* exception_ce   NULL or zend_exception_get_default() or a derived class 
  * message        NULL or the message of the exception */
 ZEND_API void zend_throw_exception(zend_class_entry *exception_ce, char *message, long code TSRMLS_DC);
+ZEND_API void zend_throw_exception_ex(zend_class_entry *exception_ce, long code TSRMLS_DC, char *format, ...);
 
 /* show an exception using zend_error(E_ERROR,...) */
 ZEND_API void zend_exception_error(zval *exception TSRMLS_DC);
