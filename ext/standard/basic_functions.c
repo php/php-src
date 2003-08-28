7a8edb50edf7fa2b8e5e73cd7c217790b28a1d9c
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-28 12:18:51 +0000

    proto fix
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@138927 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -2521,7 +2521,7 @@ PHP_FUNCTION(restore_include_path)
 
 /* }}} */
 
-/* {{{ proto bool print_r(mixed var [, bool return])
+/* {{{ proto mixed print_r(mixed var [, bool return])
    Prints out or returns information about the specified variable */
 PHP_FUNCTION(print_r)
 {
