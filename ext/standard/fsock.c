8b874551c4c0b608b9125e1078b09faa5899ffd7
sas <sas@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-28 15:16:20 +0000

    fix format string (long port)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@138958 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/fsock.c
+++ ext/standard/fsock.c
@@ -52,7 +52,7 @@ static void php_fsockopen_stream(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 	}
 
 	if (persistent) {
-		spprintf(&hashkey, 0, "pfsockopen__%s:%d", host, port);
+		spprintf(&hashkey, 0, "pfsockopen__%s:%ld", host, port);
 	}
 
 	if (port > 0) {
