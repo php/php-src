b11dfc2310c9021b50b65ffd80503c329744dbeb
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-08-30 11:40:37 +0000

    - The compiler can't detect all abstract function calls so we need to check. # In this case throwing a dedicated exception is better than the error we # show when the compiler can detect the abstract call because its run-time.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@139195 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_default_classes.c
+++ Zend/zend_default_classes.c
@@ -25,6 +25,7 @@
 #include "zend_builtin_functions.h"
 
 zend_class_entry *default_exception_ptr;
+zend_class_entry *abstract_exception_ptr;
 
 static zend_object_value zend_default_exception_new(zend_class_entry *class_type TSRMLS_DC)
 {
@@ -138,10 +139,10 @@ static zend_function_entry default_exception_functions[] = {
 
 static void zend_register_default_exception(TSRMLS_D)
 {
-	zend_class_entry default_exception;
+	zend_class_entry ce;
 
-	INIT_CLASS_ENTRY(default_exception, "exception", default_exception_functions);
-	default_exception_ptr = zend_register_internal_class(&default_exception TSRMLS_CC);
+	INIT_CLASS_ENTRY(ce, "exception", default_exception_functions);
+	default_exception_ptr = zend_register_internal_class(&ce TSRMLS_CC);
 	default_exception_ptr->create_object = zend_default_exception_new;
 
 	zend_declare_property_string(default_exception_ptr, "message", sizeof("message")-1, "Unknown exception", ZEND_ACC_PROTECTED);
@@ -149,11 +150,19 @@ static void zend_register_default_exception(TSRMLS_D)
 	zend_declare_property_null(default_exception_ptr, "file", sizeof("file")-1, ZEND_ACC_PROTECTED);
 	zend_declare_property_null(default_exception_ptr, "line", sizeof("line")-1, ZEND_ACC_PROTECTED);
 	zend_declare_property_null(default_exception_ptr, "trace", sizeof("trace")-1, ZEND_ACC_PROTECTED);
+
+	INIT_CLASS_ENTRY(ce, "abstract_exception", NULL);
+	abstract_exception_ptr = zend_register_internal_class_ex(&ce, default_exception_ptr, NULL TSRMLS_CC);
 }
 
 ZEND_API zend_class_entry *zend_exception_get_default(void)
 {
 	return default_exception_ptr;
+}
+
+ZEND_API zend_class_entry *zend_exception_get_abstract(void)
+{
+	return abstract_exception_ptr;
 }
 
 ZEND_API void zend_throw_exception_ex(zend_class_entry *exception_ce, long code TSRMLS_DC, char *format, ...)
