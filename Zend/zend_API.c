f6860c6d7b903254e7435417fc0391653189c592
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-09-03 16:13:40 +0000

    Fix static properties. # # There's only an errormessage missing which i'll wommit as soon as i find out # how to do it best. But besides that damn message everything works now and all # inheritance rules apply. #
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@139545 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -700,7 +700,6 @@ ZEND_API int _object_and_properties_init(zval *arg, zend_class_entry *class_type
 
 	if (!class_type->constants_updated) {
 		zend_hash_apply_with_argument(&class_type->default_properties, (apply_func_arg_t) zval_update_constant, (void *) 1 TSRMLS_CC);
-		zend_hash_apply_with_argument(class_type->static_members, (apply_func_arg_t) zval_update_constant, (void *) 1 TSRMLS_CC);
 		class_type->constants_updated = 1;
 	}
 	
