ed49851dd06aac55c7914edd4e6d42f027f5d90f
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-09-23 18:26:02 +0000

    Fixed bug #25636 (SNMP Session not closed on success). Patch by: nesslage[at]mwsc[dot]edu
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@140975 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/snmp/snmp.c
+++ ext/snmp/snmp.c
@@ -427,6 +427,7 @@ retry:
 					if (st == 1) {
 						*return_value = *snmpval;
 						zval_copy_ctor(return_value);
+						snmp_close(ss);
 						return;
 					} else if (st == 2) {
 						*return_value = *snmpval;
