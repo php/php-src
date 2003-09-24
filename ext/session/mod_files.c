03d9ffe50c5544f76daec1b5ae23e5016779c806
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-09-24 23:39:14 +0000

    Fixed bug #25070 (Don't forget to unlock session files on win32 before closing them).
    Regions should be locked only briefly and should be unlocked before
    closing a file or exiting the program. On Win32 locked files that are
    closed without being explicitly unlocked will be unlocked only when "system
    resources become avaliable".
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@141040 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -127,6 +127,7 @@ static char *ps_files_path_create(char *buf, size_t buflen, ps_files *data, cons
 static void ps_files_close(ps_files *data)
 {
 	if (data->fd != -1) {
+		flock(data->fd, LOCK_UN);
 		close(data->fd);
 		data->fd = -1;
 	}
