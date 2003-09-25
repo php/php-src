40a1c4b4f48d8908f505e3605e721fbd095082a4
foobar <sniper@php.net>
2003-09-25 14:53:58 +0000

    MFH

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -124,7 +124,11 @@ static char *ps_files_path_create(char *buf, size_t buflen, ps_files *data, cons
 static void ps_files_close(ps_files *data)
 {
 	if (data->fd != -1) {
+#ifdef PHP_WIN32 
+		/* On Win32 locked files that are closed without being explicitly unlocked
+		   will be unlocked only when "system resources become available". */
 		flock(data->fd, LOCK_UN);
+#endif
 		close(data->fd);
 		data->fd = -1;
 	}
