de85bccf0367f72c91f981a240acddcf348439bd
Sara Golemon <pollita@php.net>
2003-09-26 18:00:00 +0000

    MFH BugFix# 25671 subarray corruption in str_replace

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -2746,7 +2746,10 @@ PHP_FUNCTION(str_replace)
 				SEPARATE_ZVAL(subject_entry);
 				php_str_replace_in_subject(*search, *replace, subject_entry, result);
 			} else {
-				result = *subject_entry;
+				MAKE_STD_ZVAL(result);
+				SEPARATE_ZVAL(subject_entry);
+				*result = **subject_entry;
+				zval_copy_ctor(result);
 			}
 			/* Add to return array */
 			switch (zend_hash_get_current_key_ex(Z_ARRVAL_PP(subject), &string_key,
