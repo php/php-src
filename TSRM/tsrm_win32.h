f1b478c291e7fa464e357ae539f9b38a206a27f7
Stanislav Malyshev <stas@php.net>
2003-09-29 13:57:03 +0000

    add realpath for Win32 non-ZTS

--- TSRM/tsrm_win32.h
+++ TSRM/tsrm_win32.h
@@ -101,4 +101,5 @@ TSRM_API void *shmat(int key, const void *shmaddr, int flags);
 TSRM_API int shmdt(const void *shmaddr);
 TSRM_API int shmctl(int key, int cmd, struct shmid_ds *buf);
 
+TSRM_API char *realpath(char *orig_path, char *buffer);
 #endif
