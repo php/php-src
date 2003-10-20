b35a47887225d5eb0c607327cd013e9f56e862d1
SVN Migration <svn@php.net>
2003-10-20 15:33:55 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_3b3'.

--- ext/standard/tests/reg/016.phpt
+++
@@ -1,8 +0,0 @@
---TEST--
-test backslash handling in regular expressions
---POST--
---GET--
---FILE--
-<?php echo ereg_replace('\?',"abc","?123?")?>
---EXPECT--
-abc123abc
--- win32/builddef.bat
+++
@@ -1,6 +0,0 @@
-rem Generate phpts.def file, which exports symbols from our dll that
-rem are present in some of the libraries which are compiled statically
-rem into PHP
-rem $Id: builddef.bat,v 1.1 2003-08-22 21:45:58 wez Exp $
-@echo off
-type ..\ext\sqlite\php_sqlite.def
