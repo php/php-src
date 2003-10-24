4608fd6e3f8336ff635e2842dc8f0f0c73445de1
Greg Beaver <cellog@php.net>
2003-10-24 05:34:16 +0000

    MFH #25174

--- pear/OS/Guess.php
+++ pear/OS/Guess.php
@@ -176,7 +176,7 @@ class OS_Guess
         }
         pclose($cpp);
         unlink($tmpfile);
-        if (!($major && $minor) && file_exists('/lib/libc.so.6')) {
+        if (!($major && $minor) && is_link('/lib/libc.so.6')) {
             // Let's try reading the libc.so.6 symlink
             if (ereg('^libc-([.*])\.so$', basename(readlink('/lib/libc.so.6')), $matches)) {
                 list($major, $minor) = explode('.', $matches);
