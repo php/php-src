db38966aba55438e4fc2cf40e9e6fd1c0b4bc7cc
Andi Gutmans <andi@php.net>
2003-10-30 09:58:17 +0000

    - Don't fix include_once() case sensitivity problem for beta 2. - I suggest discussing to stop supporting win95 and NT 4 in our binary - distributions and change the code to #ifdef.

--- TSRM/tsrm_virtual_cwd.c
+++ TSRM/tsrm_virtual_cwd.c
@@ -332,7 +332,7 @@ CWD_API int virtual_file_ex(cwd_state *state, const char *path, verify_path_func
 		free(tmp);
 	}
 #endif
-#ifdef TSRM_WIN32
+#if 0 && defined(TSRM_WIN32)
 	{
 		char *dummy;
 		char *new_path;
