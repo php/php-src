82f1e2ad8672ffaf6fc994e0a3084335f49550e4
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-12-04 01:22:12 +0000

    fix notice
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@145520 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/PEAR/Remote.php
+++ pear/PEAR/Remote.php
@@ -307,7 +307,8 @@ class PEAR_Remote extends PEAR
                                              null, null, $ret['userinfo']);
                 }
             }
-        } elseif (is_array($ret) && sizeof($ret) == 1 && is_array($ret[0]) &&
+        } elseif (is_array($ret) && sizeof($ret) == 1 && isset($ret[0])
+                  && is_array($ret[0]) &&
                   !empty($ret[0]['faultString']) &&
                   !empty($ret[0]['faultCode'])) {
             extract($ret[0]);
