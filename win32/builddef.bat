9ec9ffece821a560d8e5da8e710ed3c97ccf422c
Wez Furlong <wez@php.net>
2003-12-05 17:45:15 +0000

    convert to DOS line endings to solve win98 issues

--- win32/builddef.bat
+++ win32/builddef.bat
@@ -1,6 +1,6 @@
 rem Generate phpts.def file, which exports symbols from our dll that
 rem are present in some of the libraries which are compiled statically
 rem into PHP
-rem $Id: builddef.bat,v 1.1 2003-08-22 21:45:58 wez Exp $
+rem $Id: builddef.bat,v 1.2 2003-12-05 17:45:15 wez Exp $
 @echo off
 type ..\ext\sqlite\php_sqlite.def
