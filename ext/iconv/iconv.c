a682e0a5c642fce1320513a96d857b0d43e8b51f
Moriyoshi Koizumi <moriyoshi@php.net>
2003-12-06 11:10:13 +0000

    Add missing iconv_close() call

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -2239,6 +2239,7 @@ typedef struct _php_iconv_stream_filter {
 /* {{{ php_iconv_stream_filter_dtor */
 static void php_iconv_stream_filter_dtor(php_iconv_stream_filter *self)
 {
+	iconv_close(self->cd);
 	pefree(self->to_charset, self->persistent);
 	pefree(self->from_charset, self->persistent);
 }
