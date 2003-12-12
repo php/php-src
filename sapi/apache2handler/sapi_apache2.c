1628eb107485e299f23326ee3653fe35bad6ddec
Ilia Alshanetsky <iliaa@php.net>
2003-12-12 17:19:11 +0000

    Fixed bug #26604 (Apache2 SAPIs implicitly disable Keep-Alive). (Ilia)

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -427,7 +427,6 @@ static void php_apache_request_ctor(request_rec *r, php_struct *ctx TSRMLS_DC)
 	apr_table_unset(r->headers_out, "Last-Modified");
 	apr_table_unset(r->headers_out, "Expires");
 	apr_table_unset(r->headers_out, "ETag");
-	apr_table_unset(r->headers_in, "Connection");
 	if (!PG(safe_mode) || (PG(safe_mode) && !ap_auth_type(r))) {
 		auth = apr_table_get(r->headers_in, "Authorization");
 		php_handle_auth_data(auth TSRMLS_CC);
