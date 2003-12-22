7a4924df88b750cdc16d6d97f04e2122c7f886d3
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2003-12-22 15:56:51 +0000

    Missing bit for bug #26625.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@147172 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -3772,9 +3772,8 @@ PHP_PGSQL_API int php_pgsql_convert(PGconn *pg_link, const char *table_name, con
 						if (Z_STRLEN_PP(val) == 0) {
 							if (opt & PGSQL_CONV_FORCE_NULL) {
 								ZVAL_STRING(new_val, "NULL", 1);
-							}
-							else {
-								ZVAL_STRING(new_val, empty_string, 1);
+							} else {
+								ZVAL_STRING(new_val, "''", 1);
 							}
 						}
 						else {
