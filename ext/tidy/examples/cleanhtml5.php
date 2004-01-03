a7841b330e861384a12e99bc26c9239faf4d86ba
john <john@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-01-03 05:23:24 +0000

    Added examples for tidy v2.0 (in PHP5) and a few more tests..
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@147735 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/tidy/examples/cleanhtml5.php
+++ ext/tidy/examples/cleanhtml5.php
@@ -1,37 +1,36 @@
 <?php
 
     /*
-     * cleanhtml.php
+     * cleanhtml5.php
      *
      * A simple script to clean and repair HTML,XHTML,PHP,ASP,etc. documents
      * if no file is provided, it reads from standard input.
      *
-     * NOTE: Works only with tidy for PHP 4.3.x, for tidy in PHP 5 see cleanhtml5.php
+     * NOTE: Works only with tidy for PHP 5, for tidy in PHP 4.3.x see cleanhtml.php
      *
      * By: John Coggeshall <john@php.net>
      *
-     * Usage: php cleanhtml.php [filename]
+     * Usage: php cleanhtml5.php [filename]
      *
      */
     
     if(!isset($_SERVER['argv'][1])) {
       $data = file_get_contents("php://stdin");
-      tidy_parse_string($data);
+      $tidy = tidy_parse_string($data);
     } else {
-       tidy_parse_file($_SERVER['argv'][1]);
+      $tidy = tidy_parse_file($_SERVER['argv'][1]);
     }
     
-    tidy_clean_repair();
+    $tidy->clean_repair();
     
-    if(tidy_warning_count() ||
-       tidy_error_count()) {
+    if(!empty($tidy->error_buf)) {
         
         echo "\n\nThe following errors or warnings occured:\n";
-        echo tidy_get_error_buffer();
-        echo "\n";
+        echo "{$tidy->error_buf}\n";
+        
     }
     
-    echo tidy_get_output();
+    echo $tidy;
     
 ?>
     
