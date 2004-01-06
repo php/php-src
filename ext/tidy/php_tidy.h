e339c85661a85d0fdf3975a3344642603dfc1ec3
john <john@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-01-06 18:24:17 +0000

    Added an optional array parameter to tidy_parse_file/string to fix a bug. Apparently some libtidy config options must be set prior to parsing in order to work properly.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@147995 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/tidy/php_tidy.h
+++ ext/tidy/php_tidy.h
@@ -213,6 +213,8 @@ static void tidy_doc_update_properties(PHPTidyObj * TSRMLS_DC);
 static void tidy_add_default_properties(PHPTidyObj *, tidy_obj_type TSRMLS_DC);
 static void *php_tidy_get_opt_val(PHPTidyDoc *, TidyOption, TidyOptionType * TSRMLS_DC);
 static void php_tidy_create_node(INTERNAL_FUNCTION_PARAMETERS, tidy_base_nodetypes);
+static int _php_tidy_set_tidy_opt(TidyDoc, char *, zval *);
+static int _php_tidy_apply_config_array(TidyDoc doc, HashTable *ht_options);
 
 void _php_tidy_register_nodetypes(INIT_FUNC_ARGS);
 void _php_tidy_register_tags(INIT_FUNC_ARGS);
