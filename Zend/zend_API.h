61fc2a920981a68cb58261243eb06211babb99b6
Marcus Boerger <helly@php.net>
2004-01-12 00:19:40 +0000

    Add missing macro # by popular demand, more and more exts need this

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -55,6 +55,7 @@ typedef struct _zend_function_entry {
 #define ZEND_ABSTRACT_ME(classname, name, arg_info)	ZEND_FENTRY(name, NULL, arg_info, ZEND_ACC_PUBLIC|ZEND_ACC_ABSTRACT)
 #define ZEND_MALIAS(name, classname, alias, arg_info, flags) \
                                                     ZEND_FENTRY(name, ZEND_FN(classname##_##alias), arg_info, flags)
+#define ZEND_ME_MAPPING(name, func_name, arg_types) ZEND_NAMED_FE(name, ZEND_FN(func_name), arg_types)
 
 #define ZEND_ARG_INFO(pass_by_ref, name)							{ #name, sizeof(#name)-1, NULL, 0, 0, pass_by_ref },
 #define ZEND_ARG_PASS_INFO(pass_by_ref)								{ NULL, 0, NULL, 0, 0, pass_by_ref },
