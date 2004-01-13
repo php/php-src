38f8ed0e0abc5580e8efe967ff4f8dea2e00f680
Rob Richards <rrichards@php.net>
2004-01-13 14:13:21 +0000

    fix getElementsByTagName and getElementsByTagNameNS - allow *

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -1085,8 +1085,8 @@ xmlNode *dom_get_elements_by_tag_name_ns_raw(xmlNodePtr nodep, char *ns, char *l
 
 	while (nodep != NULL && (*cur <= index || index == -1)) {
 		if (nodep->type == XML_ELEMENT_NODE) {
-			if (xmlStrEqual(nodep->name, local)) {
-				if (ns == NULL || (nodep->ns != NULL && xmlStrEqual(nodep->ns->href, ns))) {
+			if (xmlStrEqual(nodep->name, local) || xmlStrEqual("*", local)) {
+				if (ns == NULL || (nodep->ns != NULL && (xmlStrEqual(nodep->ns->href, ns) || xmlStrEqual("*", ns)))) {
 					if (*cur == index) {
 						ret = nodep;
 						break;
