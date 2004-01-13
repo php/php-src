848684fa96cdee7f09f171edfa7f9a758baad7fa
Wez Furlong <wez@php.net>
2004-01-13 02:07:04 +0000

    make usleep actually work properly

--- win32/time.c
+++ win32/time.c
@@ -136,9 +136,9 @@ void usleep(unsigned int useconds)
 	HANDLE timer;
 	LARGE_INTEGER due;
 
-	due.QuadPart = -1000 * useconds;
-	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	due.QuadPart = -(10 * (__int64)useconds);
 
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
 	SetWaitableTimer(timer, &due, 0, NULL, NULL, 0);
 	WaitForSingleObject(timer, INFINITE);
 	CloseHandle(timer);
