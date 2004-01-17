594f6433af44e7d223e30b247c8f995ef90a558e
Sterling Hughes <sterling@php.net>
2004-01-17 21:44:22 +0000

    update the examples

--- ext/simplexml/examples/xpath.php
+++ ext/simplexml/examples/xpath.php
@@ -1,7 +1,7 @@
 <?php
 $books = simplexml_load_file('book.xml');
 
-$xpath_result = $books->xsearch("/books/book/title");
+$xpath_result = $books->xpath("/books/book/title");
 foreach($xpath_result as $entry ) {
     print "$entry \n";
 }
