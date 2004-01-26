b1bb31f3c1f6a5e2bfa6fbb25325f3bf6e6c3aed
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-01-26 15:42:24 +0000

    change to use different allocator (Sascha, thanks =) )
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@149806 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -2649,7 +2649,7 @@ static oci_session *_oci_open_session(oci_server* server,char *username,char *pa
 			)
 		);
 
-		smart_str_append_unsigned(&hashed_details, charsetid);
+		smart_str_append_unsigned_ex(&hashed_details, charsetid, 1);
 
 		charsetid = 0;
 	}
