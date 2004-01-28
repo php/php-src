c603f3112ed0d7895d94357f3bcbc40eae80d1ee
Sara Golemon <pollita@php.net>
2004-01-28 22:50:12 +0000

    FTP dirstream_read should not leave whitespace/newlines on dirents

--- ext/standard/ftp_fopen_wrapper.c
+++ ext/standard/ftp_fopen_wrapper.c
@@ -586,10 +586,18 @@ static size_t php_ftp_dirstream_read(php_stream *stream, char *buf, size_t count
 		return 0;
 	}
 
-	tmp_len = MIN(sizeof(ent->d_name), basename_len) - 1;
+	tmp_len = MIN(sizeof(ent->d_name), basename_len - 1);
 	memcpy(ent->d_name, basename, tmp_len);
 	ent->d_name[tmp_len] = '\0';
 	efree(basename);
+
+	/* Trim off trailing whitespace characters */
+	tmp_len--;
+	while (tmp_len >= 0 &&
+			(ent->d_name[tmp_len] == '\n' || ent->d_name[tmp_len] == '\r' ||
+			 ent->d_name[tmp_len] == '\t' || ent->d_name[tmp_len] == ' ')) {
+		ent->d_name[tmp_len--] = '\0';
+	}
 
 	return sizeof(php_stream_dirent);
 }
