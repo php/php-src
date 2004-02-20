1f67407f84430cf1c15e67c4cc8d0427ca58ef17
Georg Richter <georg@php.net>
2004-02-20 13:09:14 +0000

    Moved functions for replication support in to file mysqli_repl.c   - these functions are marked as experimental now in documentation     not sure if we will replace or extend them with NDB support in     April

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -469,38 +469,6 @@ PHP_FUNCTION(mysqli_debug)
 }
 /* }}} */
 
-/* {{{ proto void mysqli_disable_reads_from_master(object link)
-*/
-PHP_FUNCTION(mysqli_disable_reads_from_master)
-{
-	MYSQL		*mysql;
-	zval		*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-	mysql_disable_reads_from_master(mysql);
-	RETURN_TRUE;
-}
-/* }}} */
-
-/* {{{ proto void mysqli_disable_rpl_parse(object link)
-*/
-PHP_FUNCTION(mysqli_disable_rpl_parse)
-{
-	MYSQL		*mysql;
-	zval    	*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-	mysql_disable_rpl_parse(mysql);
-	RETURN_TRUE;
-}
-/* }}} */
-
 /* {{{ proto bool mysqli_dump_debug_info(object link)
 */
 PHP_FUNCTION(mysqli_dump_debug_info)
@@ -520,40 +488,6 @@ PHP_FUNCTION(mysqli_dump_debug_info)
 		RETURN_FALSE;
 	}
 	RETURN_TRUE;	
-}
-/* }}} */
-
-/* {{{ proto void mysqli_enable_reads_from_master(object link)
-*/
-PHP_FUNCTION(mysqli_enable_reads_from_master)
-{
-	MYSQL		*mysql;
-	zval		*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	mysql_enable_reads_from_master(mysql);
-	RETURN_TRUE;
-}
-/* }}} */
-
-/* {{{ proto void mysqli_enable_rpl_parse(object link)
-*/
-PHP_FUNCTION(mysqli_enable_rpl_parse)
-{
-	MYSQL		*mysql;
-	zval		*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	mysql_enable_rpl_parse(mysql);
-	RETURN_TRUE;
 }
 /* }}} */
 
@@ -1084,26 +1018,6 @@ PHP_FUNCTION(mysqli_kill)
 }
 /* }}} */
 
-/* {{{ proto bool mysqli_master_query(object link, string query)
-   Enforce execution of a query on the master in a master/slave setup */
-PHP_FUNCTION(mysqli_master_query) {
-	MYSQL			*mysql;
-	zval			*mysql_link;
-	char			*query = NULL;
-	unsigned int 	query_len;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os", &mysql_link, mysqli_link_class_entry, &query, &query_len) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	if (mysql_master_query(mysql, query, query_len)) {
-		RETURN_FALSE;
-	}	
-	RETURN_TRUE;
-}
-/* }}} */
-
 /* {{{ proto bool mysqli_more_results(object link)
    check if there any more query results from a multi query */
 PHP_FUNCTION(mysqli_more_results) {
@@ -1434,60 +1348,6 @@ PHP_FUNCTION(mysqli_rollback)
 }
 /* }}} */
 
-/* {{{ proto int mysqli_rpl_parse_enabled(object link)
-*/
-PHP_FUNCTION(mysqli_rpl_parse_enabled)
-{
-	MYSQL		*mysql;
-	zval  		*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	RETURN_LONG(mysql_rpl_parse_enabled(mysql));
-}
-/* }}} */
-
-/* {{{ proto bool mysqli_rpl_probe(object link)
-*/
-PHP_FUNCTION(mysqli_rpl_probe)
-{
-	MYSQL		*mysql;
-	zval  		*mysql_link;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O", &mysql_link, mysqli_link_class_entry) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-
-	if (mysql_rpl_probe(mysql)) {
-		RETURN_FALSE;
-	}
-	RETURN_TRUE;
-}
-/* }}} */
-
-/* {{{ proto int mysqli_rpl_query_type(string query)
-*/
-PHP_FUNCTION(mysqli_rpl_query_type)
-{
-	MYSQL		*mysql;
-	zval		*mysql_link;
-	char		*query;
-	int	 		query_len;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os", &mysql_link, mysqli_link_class_entry, &query, &query_len) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	RETURN_LONG(mysql_rpl_query_type(query, query_len));
-}
-/* }}} */
-
 /* {{{ proto bool mysqli_send_long_data(object stmt, int param_nr, string data)
 */
 PHP_FUNCTION(mysqli_send_long_data)
@@ -1511,27 +1371,6 @@ PHP_FUNCTION(mysqli_send_long_data)
 	if (mysql_send_long_data(stmt->stmt, param_nr, data, data_len)) {
 		RETURN_FALSE;
 	}
-	RETURN_TRUE;
-}
-/* }}} */
-
-/* {{{ proto bool mysqli_send_query(object link, string query)
-*/
-PHP_FUNCTION(mysqli_send_query)
-{
-	MYSQL			*mysql;
-	zval			*mysql_link;
-	char			*query = NULL;
-	unsigned int 	query_len;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os", &mysql_link, mysqli_link_class_entry, &query, &query_len) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	if (mysql_send_query(mysql, query, query_len)) {
-		RETURN_FALSE;
-	}	
 	RETURN_TRUE;
 }
 /* }}} */
@@ -1569,27 +1408,6 @@ PHP_FUNCTION(mysqli_server_end)
 }
 /* }}} */
 #endif
-
-/* {{{ proto bool mysqli_slave_query(object link, string query)
-   Enforce execution of a query on a slave in a master/slave setup */
-PHP_FUNCTION(mysqli_slave_query)
-{
-	MYSQL			*mysql;
-	zval			*mysql_link;
-	char			*query = NULL;
-	unsigned int	query_len;
-
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os", &mysql_link, mysqli_link_class_entry, &query, &query_len) == FAILURE) {
-		return;
-	}
-	MYSQLI_FETCH_RESOURCE(mysql, MYSQL *, &mysql_link, "mysqli_link");
-
-	if (mysql_slave_query(mysql, query, query_len)) {
-		RETURN_FALSE;
-	}	
-	RETURN_TRUE;
-}
-/* }}} */
 
 /* {{{ proto mixed mysqli_stmt_affected_rows(object stmt)
    Return the number of rows affected in the last query for the given link */
