e88d355ab0348ef9eccb85ee8dc68957a4e41498
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-02-23 20:06:01 +0000

    Bugfix #27276: When using str_replace to expand a string, count occurances of needle in haystack to avoid massive overallocation
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@152183 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -3001,7 +3001,14 @@ PHPAPI char *php_str_to_str_ex(char *haystack, int length,
 			if (str_len < needle_len) {
 				new_str = emalloc(length + 1);
 			} else {
-				new_str = safe_emalloc((length / needle_len + 1), str_len, 0);
+				int count = 0;
+				char *o = haystack, *endp = haystack + length;
+
+				while ((o = php_memnstr(o, needle, needle_len, endp))) {
+					o += needle_len;
+					count++;
+				}
+				new_str = safe_emalloc(count, str_len - needle_len, length + 1);
 			}
 
 			e = s = new_str;
