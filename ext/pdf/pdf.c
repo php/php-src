0f116e752446c07f3efacacdf2a58d99d025417e
Ilia Alshanetsky <iliaa@php.net>
2004-02-28 22:58:51 +0000

    Remove unused variable.

--- ext/pdf/pdf.c
+++ ext/pdf/pdf.c
@@ -1944,7 +1944,7 @@ PHP_FUNCTION(pdf_open_image_file)
 PHP_FUNCTION(pdf_open_memory_image)
 {
 	zval **arg1, **arg2;
-	int i, j, color, count;
+	int i, j, color;
 	int pdf_image;
 	gdImagePtr im;
 	unsigned char *buffer, *ptr;
@@ -1962,7 +1962,6 @@ PHP_FUNCTION(pdf_open_memory_image)
 	}
 	ZEND_FETCH_RESOURCE(im, gdImagePtr, arg2, -1, "Image", le_gd);
 
-	count = 3 * im->sx * im->sy;
 	buffer = (unsigned char *) safe_emalloc(3 * im->sx, im->sy, 0);
 
 	ptr = buffer;
