596efe5197b106ecd6ab3bb84a8ef57ce7c4ddf1
Derick Rethans <derick@php.net>
2004-03-02 21:56:30 +0000

    - Fixed bug in gdImageFilledRectangle in the bundled GD library, that required   x1 < x2 and y1 < y2 for coordinates.

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -2038,6 +2038,16 @@ void gdImageFilledRectangle (gdImagePtr im, int x1, int y1, int x2, int y2, int 
 	if (y1 > gdImageSY(im)) {
 		y1 = gdImageSY(im);
 	}
+	if (x1 > x2) {
+		x = x1;
+		x1 = x2;
+		x2 = x;
+	}
+	if (y1 > y2) {
+		y = y1;
+		y1 = y2;
+		y2 = y;
+	}
 
 	for (y = y1; (y <= y2); y++) {
 		for (x = x1; (x <= x2); x++) {
