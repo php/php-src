dcf54aef30de9097b850a4d8c0af4499adadd7a5
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-03-07 21:36:55 +0000

    Less magic please
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@153089 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -1881,7 +1881,10 @@ PHPAPI int _php_stream_scandir(char *dirname, php_stream_dirent **namelist[], in
 			vector = (php_stream_dirent **) erealloc(vector, vector_size * sizeof(php_stream_dirent *));
 		}
 
-		vector[nfiles++] = (php_stream_dirent*)estrndup((const char *)&sdp, sizeof(php_stream_dirent) + ((strlen(sdp.d_name) + 1) * sizeof(char)));
+		vector[nfiles] = emalloc(sizeof(php_stream_dirent));
+		memcpy(vector[nfiles], &sdp, sizeof(sdp));
+
+		nfiles++;
 	}
 	php_stream_closedir(stream);
 
