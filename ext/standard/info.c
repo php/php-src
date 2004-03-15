633c95aa40939b9a3086035654ca83697056bde5
Jon Parise <jon@php.net>
2004-03-15 07:45:25 +0000

    Fixed crash in php_ini_scanned_files() when no additional INI files were actually parsed.
    I'll merge this into the PHP_4_3 branch as soon as I receive permission
    from the release master.

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -1080,7 +1080,7 @@ PHP_FUNCTION(php_uname)
    Return comma-separated string of .ini files parsed from the additional ini dir */
 PHP_FUNCTION(php_ini_scanned_files)
 {
-	if (strlen(PHP_CONFIG_FILE_SCAN_DIR)) {
+	if (strlen(PHP_CONFIG_FILE_SCAN_DIR) && php_ini_scanned_files) {
 		RETURN_STRING(php_ini_scanned_files, 1);
 	} else {
 		RETURN_FALSE;
