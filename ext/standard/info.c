bb7f2e421024bc2d419d79195eab6c043d9a4586
Jon Parise <jon@php.net>
2004-03-15 16:39:53 +0000

    MFH 1.243:
    Fixed crash in php_ini_scanned_files() when no additional INI files were
    actually parsed.

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -974,7 +974,7 @@ PHP_FUNCTION(php_uname)
 /* {{{ proto string php_ini_scanned_files(void)
    Return comma-separated string of .ini files parsed from the additional ini dir */
 PHP_FUNCTION(php_ini_scanned_files) {
-	if(strlen(PHP_CONFIG_FILE_SCAN_DIR)) {
+	if(strlen(PHP_CONFIG_FILE_SCAN_DIR) && php_ini_scanned_files) {
 		RETURN_STRING(php_ini_scanned_files,1);
 	} else RETURN_FALSE;
 }
