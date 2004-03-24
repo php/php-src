de4ca2ff1072d648bdfa314890bdfda39b1fdffd
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-03-24 13:33:26 +0000

    and set module startup flag on startup
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@154470 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -1315,6 +1315,7 @@ int php_module_startup(sapi_module_struct *sf, zend_module_entry *additional_mod
 #endif
 
 	module_shutdown = 0;
+	module_startup = 1;
 	sapi_initialize_empty_request(TSRMLS_C);
 	sapi_activate(TSRMLS_C);
 
