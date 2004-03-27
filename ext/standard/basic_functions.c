e2134aee4bff2bf12513c655ca0219cbadddb9f2
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-03-27 00:50:41 +0000

    Portable solution for bug #27646 that works correct on Windows, too
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@154674 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -950,7 +950,7 @@ static void basic_globals_dtor(php_basic_globals *basic_globals_p TSRMLS_DC)
 #define PHP_DOUBLE_INFINITY_HIGH       0x7ff00000
 #define PHP_DOUBLE_QUIET_NAN_HIGH      0xfff80000
 
-static double php_get_nan()
+PHPAPI double php_get_nan()
 {
 #if defined(__i386__) || defined(_X86_) || defined(ALPHA) || defined(_ALPHA) || defined(__alpha)
 	double val;
@@ -963,7 +963,7 @@ static double php_get_nan()
 #endif
 }
 
-static double php_get_inf()
+PHPAPI double php_get_inf()
 {
 #if defined(__i386__) || defined(_X86_) || defined(ALPHA) || defined(_ALPHA) || defined(__alpha)
 	double val;
