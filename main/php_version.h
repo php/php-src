2058fec25b5339b4c3f1bc29fd75121ee10fbe02
Ilia Alshanetsky <iliaa@php.net>
2004-04-01 20:51:34 +0000

    Prepare for 4.3.6RC1.

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 6
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.6-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "4.3.6RC1"
