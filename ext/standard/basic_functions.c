3e2f7678f06bdc74492f44bc7f5742622079c0d1
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-04-03 09:51:57 +0000

    Initialize to 0 to prevent valgrind warnings
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@155258 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -955,7 +955,7 @@ PHPAPI double php_get_nan()
 #if HAVE_HUGE_VAL_NAN
 	return HUGE_VAL + -HUGE_VAL;
 #elif defined(__i386__) || defined(_X86_) || defined(ALPHA) || defined(_ALPHA) || defined(__alpha)
-	double val;
+	double val = 0.0;
 	((php_uint32*)&val)[1] = PHP_DOUBLE_QUIET_NAN_HIGH;
 	((php_uint32*)&val)[0] = 0;
 	return val;
@@ -971,7 +971,7 @@ PHPAPI double php_get_inf()
 #if HAVE_HUGE_VAL_NAN
 	return HUGE_VAL;
 #elif defined(__i386__) || defined(_X86_) || defined(ALPHA) || defined(_ALPHA) || defined(__alpha)
-	double val;
+	double val = 0.0;
 	((php_uint32*)&val)[1] = PHP_DOUBLE_INFINITY_HIGH;
 	((php_uint32*)&val)[0] = 0;
 	return val;
