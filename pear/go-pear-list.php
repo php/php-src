b3cec4d9d49edcb04ec9ebcf49e904066c2221a2
Greg Beaver <cellog@php.net>
2004-04-08 15:03:21 +0000

    Dan wants tabs -> spaces

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -6,21 +6,21 @@
  * $Id$
  */
 $packages  = array(
-"PEAR"					=>	"1.3.1",
-"Mail"					=>	"1.1.2",
-"Net_SMTP"				=>	"1.2.5",
-"Net_Socket"			=>	"1.0.1",
-"PHPUnit"				=>	"2.0.0beta1",
-"Console_Getopt"		=>	"1.2",
-"DB"					=>	"1.6.2",
-"HTTP"					=>	"1.2.3",
-"Archive_Tar"			=>	"1.1",
-"Pager"					=>	"1.0.8",
-"HTML_Template_IT"		=>	"1.1",
-"XML_Parser"			=>	"1.0.1",
-"XML_RPC"				=>	"1.1.0",
-"Net_UserAgent_Detect"	=>	"1.0",
-"PEAR_Frontend_Web"		=>	"0.3"
+"PEAR"                  =>    "1.3.1",
+"Mail"                  =>    "1.1.2",
+"Net_SMTP"              =>    "1.2.5",
+"Net_Socket"            =>    "1.0.1",
+"PHPUnit"               =>    "2.0.0beta1",
+"Console_Getopt"        =>    "1.2",
+"DB"                    =>    "1.6.2",
+"HTTP"                  =>    "1.2.3",
+"Archive_Tar"           =>    "1.1",
+"Pager"                 =>    "1.0.8",
+"HTML_Template_IT"      =>    "1.1",
+"XML_Parser"            =>    "1.0.1",
+"XML_RPC"               =>    "1.1.0",
+"Net_UserAgent_Detect"  =>    "1.0",
+"PEAR_Frontend_Web"     =>    "0.3"
 );
 
 ?>
