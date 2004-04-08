c60d5f5ba99e3233314e239332afefd26f7e1562
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-04-08 13:32:50 +0000

    Add this list here, so that pear-core guys can manipulate the PEAR bundle for the win32 snapshot.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@155546 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ pear/go-pear-list.php
@@ -0,0 +1,26 @@
+<?php
+/* This is a list of packages and versions
+ * that will be used to create the PEAR folder
+ * in the windows snapshot.
+ * See win32/build/mkdist.php for more details
+ * $Id$
+ */
+$packages  = array(
+"PEAR"					=>	"1.3b6",
+"Mail"					=>	"1.1.2",
+"Net_SMTP"				=>	"1.2.2",
+"Net_Socket"			=>	"1.0.1",
+"PHPUnit"				=>	"1.0.0alpha3",
+"Console_Getopt"		=>	"1.2",
+"DB"					=>	"1.6.0RC6",
+"HTTP"					=>	"1.2.2",
+"Archive_Tar"			=>	"1.1",
+"Pager"					=>	"1.0.8",
+"HTML_Template_IT"		=>	"1.1",
+"XML_Parser"			=>	"1.0.1",
+"XML_RPC"				=>	"1.0.4",
+"Net_UserAgent_Detect"	=>	"1.0",
+"PEAR_Frontend_Web"		=>	"0.1"
+);
+
+?>
