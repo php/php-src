b30858dd53f63af22edc864981cb0fe541113fc7
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-04-09 01:12:41 +0000

    as per PEAR-Core discussion, remove all but absolutely essential packages for the installer, and for PEAR_Frontend_Web
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@155619 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -6,21 +6,17 @@
  * $Id$
  */
 $packages  = array(
+// required packages for the installer
 "PEAR"                  =>    "1.3.1",
-"Mail"                  =>    "1.1.3",
-"Net_SMTP"              =>    "1.2.5",
-"Net_Socket"            =>    "1.0.1",
-"PHPUnit2"               =>    "2.0.0beta1",
+"XML_RPC"               =>    "1.1.0",
 "Console_Getopt"        =>    "1.2",
-"DB"                    =>    "1.6.2",
-"HTTP"                  =>    "1.2.3",
 "Archive_Tar"           =>    "1.1",
+
+// required packages for the web frontend
+"PEAR_Frontend_Web"     =>    "0.3"
 "Pager"                 =>    "1.0.8",
 "HTML_Template_IT"      =>    "1.1",
-"XML_Parser"            =>    "1.0.1",
-"XML_RPC"               =>    "1.1.0",
 "Net_UserAgent_Detect"  =>    "1.0",
-"PEAR_Frontend_Web"     =>    "0.3"
 );
 
 ?>
