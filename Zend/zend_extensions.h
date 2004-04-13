c5549cc15d8cc5b15a21b7a0df1021c0633b4860
andi <andi@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-04-13 15:27:36 +0000

    - Fix API no of Engine 2. The first number is the engine version and the - rest is the API_NO. This way engine2_api_no is always greater than - engine1_api_no.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@155859 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -24,7 +24,7 @@
 
 #include "zend_compile.h"
 
-#define ZEND_EXTENSION_API_NO	90040412
+#define ZEND_EXTENSION_API_NO	220040412
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
