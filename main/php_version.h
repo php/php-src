9f6f2471f0a072f9a3f194e301148a399e3a035d
Ilia Alshanetsky <iliaa@php.net>
2004-04-14 16:39:03 +0000

    Back to dev.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 6
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "4.3.6"
+#define PHP_RELEASE_VERSION 7
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "4.3.7-dev"
