e058626c6669b112b03084e74f1bac49ce569c97
Marcus Boerger <helly@php.net>
2004-04-25 13:06:15 +0000

    Add new iterator example AppendIterator and use it in findfile.php example. # The initial idea came from a request by Sebastian

--- ext/spl/examples/findfile.php
+++ ext/spl/examples/findfile.php
@@ -4,15 +4,16 @@
  *
  * Usage: php findfile.php <path> <name>
  *
- * <path>  Path to search in.
+ * <path>  Path to search in. You can specify multiple paths by separating
+ *         them with ';'.
  * <name>  Filename to look for.
  *
- * (c) Marcus Boerger, 2003
+ * (c) Marcus Boerger, 2003 - 2004
  */
 
 if ($argc < 3) {
 	echo <<<EOF
-Usage: php findfile.php <file> <name>
+Usage: php findfile.php <path> <name>
 
 Find a specific file by name.
 
