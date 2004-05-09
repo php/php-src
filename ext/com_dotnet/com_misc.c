6f8233897eee10da21f7eb6122fed06a425a55bd
Wez Furlong <wez@php.net>
2004-05-09 14:28:19 +0000

    Urgh! Use the correct function to free messages from php_win_err(), otherwise we say hello to Mr. S. Fault.

--- ext/com_dotnet/com_misc.c
+++ ext/com_dotnet/com_misc.c
@@ -38,7 +38,7 @@ void php_com_throw_exception(HRESULT code, char *message TSRMLS_DC)
 	}
 	zend_throw_exception(php_com_exception_class_entry, message, (long)code TSRMLS_CC);
 	if (free_msg) {
-		efree(message);
+		LocalFree(message);
 	}
 }
 
