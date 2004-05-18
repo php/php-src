8635889231ae3515bc260d68108b9cab4c7def29
Ilia Alshanetsky <iliaa@php.net>
2004-05-18 23:27:03 +0000

    MFH: Fixed possible memory leak.

--- ext/ingres_ii/ii.c
+++ ext/ingres_ii/ii.c
@@ -541,6 +541,7 @@ static void php_ii_do_connect(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 		} else { /* already open persistent connection */
 
 			if (Z_TYPE_P(le) != le_ii_plink) {
+				efree(hashed_details);
 				RETURN_FALSE;
 			}
 			/* here we should ensure that the link did not die */
