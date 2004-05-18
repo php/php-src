71728cc2b24c4053aa7794a4f27b6403868dbd85
George Schlossnagle <gschlossnagle@php.net>
2004-05-18 18:01:52 +0000

    initial import of mysql 3.x pdo driver

---
+++ ext/pdo_mysql/config.m4
@@ -0,0 +1,93 @@
+dnl
+dnl $Id$
+dnl
+
+AC_DEFUN(MYSQL_LIB_CHK, [
+  str="$MYSQL_DIR/$1/libmysqlclient.*"
+  for j in `echo $str`; do
+    if test -r $j; then
+      MYSQL_LIB_DIR=$MYSQL_DIR/$1
+      break 2
+    fi
+  done
+])
+
+PHP_ARG_WITH(pdo_mysql, for MySQL support,
+[  --with-pdo_mysql[=DIR]      Include MySQL support. DIR is the MySQL base directory.])
+
+if test "$PHP_PDO_MYSQL" != "no"; then
+  AC_DEFINE(HAVE_MYSQL, 1, [Whether you have MySQL])
+
+  AC_MSG_CHECKING([for MySQL UNIX socket location])
+  if test "$PHP_MYSQL_SOCK" != "no" && test "$PHP_MYSQL_SOCK" != "yes"; then
+    MYSQL_SOCK=$PHP_MYSQL_SOCK
+    AC_DEFINE_UNQUOTED(MYSQL_UNIX_ADDR, "$MYSQL_SOCK", [ ])
+    AC_MSG_RESULT([$MYSQL_SOCK])
+  elif test "$PHP_MYSQL" = "yes" || test "$PHP_MYSQL_SOCK" = "yes"; then
+    PHP_MYSQL_SOCKET_SEARCH
+  else
+    AC_MSG_RESULT([no])
+  fi
+
+  for i in $PHP_MYSQL /usr/local /usr; do
+    if test -r $i/include/mysql/mysql.h; then
+      MYSQL_DIR=$i
+      MYSQL_INC_DIR=$i/include/mysql
+      break
+    elif test -r $i/include/mysql.h; then
+      MYSQL_DIR=$i
+      MYSQL_INC_DIR=$i/include
+      break
+    fi
+  done
+
+  if test -z "$MYSQL_DIR"; then
+    AC_MSG_ERROR([Cannot find MySQL header files under $PHP_MYSQL.
+Note that the MySQL client library is not bundled anymore.])
+  fi
+
+  for i in lib lib/mysql; do
+    MYSQL_LIB_CHK($i)
+  done
+
+  if test -z "$MYSQL_LIB_DIR"; then
+    AC_MSG_ERROR([Cannot find libmysqlclient under $MYSQL_DIR.
+Note that the MySQL client library is not bundled anymore.])
+  fi
+
+  PHP_CHECK_LIBRARY(mysqlclient, mysql_close, [ ],
+  [
+    if test "$PHP_ZLIB_DIR" != "no"; then
+      PHP_ADD_LIBRARY_WITH_PATH(z, $PHP_ZLIB_DIR, PDO_MYSQL_SHARED_LIBADD)
+      PHP_CHECK_LIBRARY(mysqlclient, mysql_error, [], [
+        AC_MSG_ERROR([mysql configure failed. Please check config.log for more information.])
+      ], [
+        -L$PHP_ZLIB_DIR/lib -L$MYSQL_LIB_DIR 
+      ])  
+      MYSQL_LIBS="-L$PHP_ZLIB_DIR/lib -lz"
+    else
+      PHP_ADD_LIBRARY(z,, PDO_MYSQL_SHARED_LIBADD)
+      PHP_CHECK_LIBRARY(mysqlclient, mysql_errno, [], [
+        AC_MSG_ERROR([Try adding --with-zlib-dir=<DIR>. Please check config.log for more information.])
+      ], [
+        -L$MYSQL_LIB_DIR
+      ])   
+      MYSQL_LIBS="-lz"
+    fi
+  ], [
+    -L$MYSQL_LIB_DIR 
+  ])
+
+  PHP_ADD_LIBRARY_WITH_PATH(mysqlclient, $MYSQL_LIB_DIR, PDO_MYSQL_SHARED_LIBADD)
+  PHP_ADD_INCLUDE($MYSQL_INC_DIR)
+
+  PHP_NEW_EXTENSION(pdo_mysql, pdo_mysql.c mysql_driver.c mysql_statement.c, $ext_shared)
+  PDO_MYSQL_MODULE_TYPE=external
+  PDO_MYSQL_LIBS="-L$MYSQL_LIB_DIR -lmysqlclient $MYSQL_LIBS"
+  PDO_MYSQL_INCLUDE=-I$MYSQL_INC_DIR
+ 
+  PHP_SUBST(PDO_MYSQL_SHARED_LIBADD)
+  PHP_SUBST_OLD(PDO_MYSQL_MODULE_TYPE)
+  PHP_SUBST_OLD(PDO_MYSQL_LIBS)
+  PHP_SUBST_OLD(PDO_MYSQL_INCLUDE)
+fi
---
+++ ext/pdo_mysql/mysql_driver.c
@@ -0,0 +1,179 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: George Schlossnagle <george@omniti.com>                      |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_ini.h"
+#include "ext/standard/info.h"
+#include "pdo/php_pdo.h"
+#include "pdo/php_pdo_driver.h"
+#include "php_pdo_mysql.h"
+#include "php_pdo_mysql_int.h"
+
+int _mysql_error(char *what, int errno, const char *file, int line TSRMLS_DC) /* {{{ */
+{
+	switch (errno) {
+		default:
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "(%s:%d) %s: %d", file, line, what, errno);
+			break;
+	}
+	return errno;
+}
+/* }}} */
+
+int mysql_handle_error(pdo_dbh_t *dbh, pdo_mysql_db_handle *H, int errcode) /* {{{ */
+{
+	return 0;
+}
+/* }}} */
+
+static int mysql_handle_closer(pdo_dbh_t *dbh TSRMLS_DC) /* {{{ */
+{
+	pdo_mysql_db_handle *H = (pdo_mysql_db_handle *)dbh->driver_data;
+
+	if (H->server) {
+		mysql_close(H->server);
+		H->server = NULL;
+	}
+	return 0;
+}
+/* }}} */
+
+static int mysql_handle_preparer(pdo_dbh_t *dbh, const char *sql, long sql_len, pdo_stmt_t *stmt TSRMLS_DC)
+{
+	pdo_mysql_db_handle *H = (pdo_mysql_db_handle *)dbh->driver_data;
+	pdo_mysql_stmt *S = ecalloc(1, sizeof(pdo_mysql_stmt));
+
+	S->H = H;
+	stmt->driver_data = S;
+	stmt->methods = &mysql_stmt_methods;
+	
+	return 1;
+}
+
+static int mysql_handle_doer(pdo_dbh_t *dbh, const char *sql TSRMLS_DC)
+{
+	pdo_mysql_db_handle *H = (pdo_mysql_db_handle *)dbh->driver_data;
+
+	return 0;
+}
+
+static int mysql_handle_quoter(pdo_dbh_t *dbh, const char *unquoted, int unquotedlen, char **quoted, int *quotedlen  TSRMLS_DC)
+{
+	pdo_mysql_db_handle *H = (pdo_mysql_db_handle *)dbh->driver_data;
+	*quoted = emalloc(2*unquotedlen + 3);
+	(*quoted)[0] = '"';
+    *quotedlen = mysql_real_escape_string(H->server, *quoted + 1, 
+                                         unquoted, unquotedlen);
+	(*quoted)[*quotedlen + 1] = '"';
+	(*quoted)[*quotedlen + 2] = '\0';
+	*quotedlen += 2;
+	return 1;
+}
+
+
+static struct pdo_dbh_methods mysql_methods = {
+	mysql_handle_closer,
+	mysql_handle_preparer,
+	mysql_handle_doer,
+	mysql_handle_quoter
+};
+
+static int pdo_mysql_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_DC) /* {{{ */
+{
+	pdo_mysql_db_handle *H;
+	int i, ret = 0;
+	char *host = NULL, *unix_socket = NULL;
+	unsigned int port;
+	char *dbname;
+	struct pdo_data_src_parser vars[] = {
+		{ "charset",  NULL,	0 },
+		{ "dbname",   "",	0 },
+		{ "host",   "localhost",	0 },
+		{ "port",   "3306",	0 },
+		{ "unix_socket",   "/var/tmp/mysql.sock",	0 },
+	};
+
+	php_pdo_parse_data_source(dbh->data_source, dbh->data_source_len, vars, 4);
+
+	H = pecalloc(1, sizeof(pdo_mysql_db_handle), dbh->is_persistent);
+	
+	/* allocate an environment */
+	
+	/* handle for the server */
+	H->server = mysql_init(NULL);
+	if(vars[2].optval && strcmp("localhost", vars[2].optval)) {
+		host = vars[2].optval;
+		port = atoi(vars[3].optval); 
+	} else {
+		host = NULL;
+		unix_socket = vars[4].optval;  
+	}
+	dbname = vars[1].optval;
+	if(mysql_real_connect(H->server, host, dbh->username, dbh->password, dbname, port, unix_socket, 0) == NULL) 
+	{
+		H->last_err = mysql_errno(H->server);
+		pdo_mysql_error("pdo_mysql_handle_factory", H->last_err);
+		goto cleanup;
+	}
+
+	H->attached = 1;
+
+	dbh->driver_data = H;
+	dbh->methods = &mysql_methods;
+	dbh->alloc_own_columns = 1;
+	dbh->supports_placeholders = 1;
+	dbh->emulate_prepare = 1;
+	dbh->placeholders_can_be_strings = 1;
+	dbh->max_escaped_char_length = 2;
+
+	ret = 1;
+	
+cleanup:
+	for (i = 0; i < sizeof(vars)/sizeof(vars[0]); i++) {
+		if (vars[i].freeme) {
+			efree(vars[i].optval);
+		}
+	}
+
+	if (!ret) {
+		mysql_handle_closer(dbh TSRMLS_CC);
+	}
+
+	return ret;
+}
+/* }}} */
+
+pdo_driver_t pdo_mysql_driver = {
+	PDO_DRIVER_HEADER(mysql),
+	pdo_mysql_handle_factory
+};
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
