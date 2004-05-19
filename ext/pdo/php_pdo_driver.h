81952b029b86a6f4c3958c13b1cd16b426af2101
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-05-19 13:43:07 +0000

    Add $dbh->exec() method. Rename $dbh->beginWork() to $dbh->beginTransaction().
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@159007 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/php_pdo_driver.h
+++ ext/pdo/php_pdo_driver.h
@@ -99,7 +99,7 @@ typedef int (*pdo_dbh_close_func)(pdo_dbh_t *dbh TSRMLS_DC);
 typedef int (*pdo_dbh_prepare_func)(pdo_dbh_t *dbh, const char *sql, long sql_len, pdo_stmt_t *stmt, long options, zval *driver_options TSRMLS_DC);
 
 /* execute a statement (that does not return a result set) */
-typedef int (*pdo_dbh_do_func)(pdo_dbh_t *dbh, const char *sql TSRMLS_DC);
+typedef int (*pdo_dbh_do_func)(pdo_dbh_t *dbh, const char *sql, int sql_len TSRMLS_DC);
 
 /* quote a string */
 typedef int (*pdo_dbh_quote_func)(pdo_dbh_t *dbh, const char *unquoted, int unquotedlen, char **quoted, int *quotedlen TSRMLS_DC);
