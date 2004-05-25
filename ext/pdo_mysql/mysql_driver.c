5c9cb55d6d063f6fe79f545cb95a536287e0084f
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-05-25 14:39:26 +0000

    Fixed memory leak.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@159631 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_mysql/mysql_driver.c
+++ ext/pdo_mysql/mysql_driver.c
@@ -141,14 +141,18 @@ static int pdo_mysql_fetch_error_func(pdo_dbh_t *dbh, pdo_stmt_t *stmt, zval *in
 static int mysql_handle_closer(pdo_dbh_t *dbh TSRMLS_DC) /* {{{ */
 {
 	pdo_mysql_db_handle *H = (pdo_mysql_db_handle *)dbh->driver_data;
-
-	if (H->server) {
-		mysql_close(H->server);
-		H->server = NULL;
-	}
-	if (H->einfo.errmsg) {
-		efree(H->einfo.errmsg);
-		H->einfo.errmsg = NULL;
+	
+	if (H) {
+		if (H->server) {
+			mysql_close(H->server);
+			H->server = NULL;
+		}
+		if (H->einfo.errmsg) {
+			efree(H->einfo.errmsg);
+			H->einfo.errmsg = NULL;
+		}
+		efree(H);
+		H = NULL;
 	}
 	return 0;
 }
