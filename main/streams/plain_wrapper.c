cb751838c28bd2aef049bff5dc5323edc9ebc3b1
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-05-25 12:59:15 +0000

    In debug mode, under CLI don't close stderr. This is so that we can see any error reports.
    # for extra paranoia, could limit this check to happen only after the script is
    # finished running.
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@159601 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -428,7 +428,16 @@ static int php_stdiop_close(php_stream *stream, int close_handle TSRMLS_DC)
 				data->file = NULL;
 			}
 		} else if (data->fd != -1) {
+#ifdef DEBUG
+			if (data->fd == 2 && strcmp(sapi_module.name, "cli")) {
+				/* don't close stderr in CLI in DEBUG mode, as we want to see any leaks */
+				ret = 0;
+			} else {
+				ret = close(data->fd);
+			}
+#else
 			ret = close(data->fd);
+#endif
 			data->fd = -1;
 		} else {
 			return 0; /* everything should be closed already -> success */
