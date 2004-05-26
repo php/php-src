1f009ea7c018f7eb0377806e2e075ae574d6c994
edink <edink@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-05-26 17:45:03 +0000

    PQfreemem() is only available in 7.4
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@159728 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_pgsql/pgsql_driver.c
+++ ext/pdo_pgsql/pgsql_driver.c
@@ -146,7 +146,7 @@ static int pgsql_handle_quoter(pdo_dbh_t *dbh, const char *unquoted, int unquote
 	(*quoted)[0] = '\'';
 	(*quoted)[*quotedlen-1] = '\'';
 	(*quoted)[*quotedlen] = '\0';
-	PQfreemem(escaped);
+	free(escaped);
 
 	return 1;
 }
