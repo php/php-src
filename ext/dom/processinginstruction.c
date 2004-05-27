edae935c266fe883a0adeff005d843b70eb2ea66
Rob Richards <rrichards@php.net>
2004-05-27 11:15:45 +0000

    namespace/tagname validation fixes (Adam Trachtenberg) added new test

--- ext/dom/processinginstruction.c
+++ ext/dom/processinginstruction.c
@@ -48,7 +48,7 @@ PHP_METHOD(domprocessinginstruction, __construct)
 	xmlNodePtr nodep = NULL, oldnode = NULL;
 	dom_object *intern;
 	char *name, *value = NULL;
-	int name_len, value_len;
+	int name_len, value_len, name_valid;
 
 	php_set_error_handling(EH_THROW, dom_domexception_class_entry TSRMLS_CC);
 	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os|s", &id, dom_processinginstruction_class_entry, &name, &name_len, &value, &value_len) == FAILURE) {
@@ -57,7 +57,9 @@ PHP_METHOD(domprocessinginstruction, __construct)
 	}
 
 	php_std_error_handling();
-	if (name_len == 0) {
+
+	name_valid = xmlValidateName((xmlChar *) name, 0);
+	if (name_valid != 0) {
 		php_dom_throw_error(INVALID_CHARACTER_ERR, 1 TSRMLS_CC);
 		RETURN_FALSE;
 	}
