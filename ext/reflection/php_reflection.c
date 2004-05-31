20b60f49d98094f11e8a6cc44eb9defd8ef090c3
Marcus Boerger <helly@php.net>
2004-05-31 21:46:00 +0000

    Add missing initialization

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -941,6 +941,9 @@ static void _reflection_export(INTERNAL_FUNCTION_PARAMETERS, zend_class_entry *c
 		}
 	}
 
+	INIT_PZVAL(&output);
+	output.refcount = 1;
+
 	/* Create object */
 	INIT_PZVAL(&reflector);
 	if (object_and_properties_init(&reflector, ce_ptr, NULL) == FAILURE) {
