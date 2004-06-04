254451337c3ce1ff86acb90f706a1db80baa1daf
Sara Golemon <pollita@php.net>
2004-06-04 01:02:21 +0000

    BugFix# 28626 You can't #ifdef check a typedef.  Just give it another name since it's only used once anyway.

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -336,7 +336,7 @@ PHP_RINIT_FUNCTION(mssql)
 	dbsetlogintime(MS_SQL_G(connect_timeout));
 	if (MS_SQL_G(timeout) < 0) MS_SQL_G(timeout) = 60;
 	dbsettime(MS_SQL_G(timeout));
-	dbsetmaxprocs((SHORT)MS_SQL_G(max_procs));
+	dbsetmaxprocs((TDS_SHORT)MS_SQL_G(max_procs));
 
 	return SUCCESS;
 }
