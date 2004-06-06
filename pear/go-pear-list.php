8217b64a33986152edd8b41c6486119a4c76a3af
Pierre Joye <pajoye@php.net>
2004-06-06 22:49:10 +0000

    - Update for 5.0RC3

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -10,10 +10,10 @@ $packages  = array(
 "PEAR"                  =>    "1.3.1",
 "XML_RPC"               =>    "1.1.0",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.1",
+"Archive_Tar"           =>    "1.2",
 
 // required packages for the web frontend
-"PEAR_Frontend_Web"     =>    "0.3"
+"PEAR_Frontend_Web"     =>    "0.4"
 "Pager"                 =>    "1.0.8",
 "HTML_Template_IT"      =>    "1.1",
 "Net_UserAgent_Detect"  =>    "1.0",
