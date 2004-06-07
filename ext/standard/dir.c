4d861228c39a746895f9be516db03593a9b8945f
Ilia Alshanetsky <iliaa@php.net>
2004-06-07 18:31:15 +0000

    MFH: Fixed bug #28668 (glob() does not work with relative paths on Windows)

--- ext/standard/dir.c
+++ ext/standard/dir.c
@@ -371,13 +371,12 @@ PHP_FUNCTION(glob)
 			cwd[0] = '\0';
 		}
 #ifdef PHP_WIN32
-		if (!IS_SLASH(cwd[0])) {
+		if (IS_SLASH(*pattern)) {
 			cwd[2] = '\0';
-			cwd_skip = 3;
 		}
-#else
-		cwd_skip = strlen(cwd)+1;
 #endif
+		cwd_skip = strlen(cwd)+1;
+
 		snprintf(work_pattern, MAXPATHLEN, "%s%c%s", cwd, DEFAULT_SLASH, pattern);
 		pattern = work_pattern;
 	} 
