a7aa47c46dc84371266489743871e85342e9b484
Ilia Alshanetsky <iliaa@php.net>
2004-06-09 15:10:19 +0000

    Correct alt text for easter egg logo.

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -411,7 +411,7 @@ PHPAPI void php_print_info(int flag TSRMLS_DC)
 				PUTS(SG(request_info).request_uri);
 			}
 			if ((ta->tm_mon==3) && (ta->tm_mday==1)) {
-				PUTS("?="PHP_EGG_LOGO_GUID"\" alt=\"Thies!\" /></a>");
+				PUTS("?="PHP_EGG_LOGO_GUID"\" alt=\"Nadia!\" /></a>");
 			} else {
 				PUTS("?="PHP_LOGO_GUID"\" alt=\"PHP Logo\" /></a>");
 			}
