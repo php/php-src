35eda3771be6b1669f4772242a78dcf4ebd4ce4a
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-06-10 12:32:09 +0000

    Add missing declaration
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@161007 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -167,6 +167,7 @@ ZEND_API int zend_parse_method_parameters_ex(int flags, int num_args TSRMLS_DC, 
 ZEND_API int zend_register_functions(zend_class_entry *scope, zend_function_entry *functions, HashTable *function_table, int type TSRMLS_DC);
 ZEND_API void zend_unregister_functions(zend_function_entry *functions, int count, HashTable *function_table TSRMLS_DC);
 ZEND_API int zend_register_module(zend_module_entry *module_entry);
+ZEND_API int zend_register_module_ex(zend_module_entry *module TSRMLS_DC);
 
 ZEND_API zend_class_entry *zend_register_internal_class(zend_class_entry *class_entry TSRMLS_DC);
 ZEND_API zend_class_entry *zend_register_internal_class_ex(zend_class_entry *class_entry, zend_class_entry *parent_ce, char *parent_name TSRMLS_DC);
