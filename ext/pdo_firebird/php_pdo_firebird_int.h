6b3cda310a7deccc67cee73ec6cb84a43eb78e21
Ard Biesheuvel <abies@php.net>
2004-06-12 18:56:07 +0000

    Added client/server info attributes

--- ext/pdo_firebird/php_pdo_firebird_int.h
+++ ext/pdo_firebird/php_pdo_firebird_int.h
@@ -49,6 +49,12 @@
 /* Firebird API has a couple of missing const decls in its API */
 #define const_cast(s) ((char*)(s))
 
+#ifdef PHP_WIN32
+typedef void (__stdcall *info_func_t)(char*);
+#else
+typedef void (*info_func_t)(char*);
+#endif
+
 typedef struct {
 
 	/* the result of the last API call */
