ab9c60fea166d20e87ffb91149c84b9b0083d222
aleigh <aleigh@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-06-22 18:08:03 +0000

    Updated the author's contact information.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@161933 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/continuity/capi.c
+++ sapi/continuity/capi.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Alex Leigh <aleigh@tessier.com>                              |
+  | Author: Alex Leigh <php (at) postfin (dot) com>                      |
   +----------------------------------------------------------------------+
 */
 
