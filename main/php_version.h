2419d2e170af64427d752720d96de95dcf9a0a31
Derick Rethans <derick@php.net>
2004-07-13 12:58:49 +0000

    - Bump version numbers in the PHP_4_3 branch.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 8
+#define PHP_RELEASE_VERSION 9
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.3.8-dev"
+#define PHP_VERSION "4.3.9-dev"
