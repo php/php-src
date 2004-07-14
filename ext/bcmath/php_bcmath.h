c4bf45e39ad40ca4de34558e5d9b3a61ed97eab4
Sara Golemon <pollita@php.net>
2004-07-14 00:14:43 +0000

    Bugfix# 28829 bcmath values for 0, 1, and 2 initialized/freed at wrong time for threaded sapis.

--- ext/bcmath/php_bcmath.h
+++ ext/bcmath/php_bcmath.h
@@ -30,6 +30,8 @@ extern zend_module_entry bcmath_module_entry;
 
 PHP_MINIT_FUNCTION(bcmath);
 PHP_MSHUTDOWN_FUNCTION(bcmath);
+PHP_RINIT_FUNCTION(bcmath);
+PHP_RSHUTDOWN_FUNCTION(bcmath);
 PHP_MINFO_FUNCTION(bcmath);
 
 PHP_FUNCTION(bcadd);
