bfe60f6a558a2f4a7ee73ac0661cf4a336a89125
Edin Kadribasic <edink@php.net>
2004-07-14 22:38:18 +0000

    MFH: Made php -m output sorted case-insensitevly

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -130,7 +130,7 @@ static int module_name_cmp(const void *a, const void *b TSRMLS_DC)
 	Bucket *f = *((Bucket **) a);
 	Bucket *s = *((Bucket **) b);
 
-	return strcmp(((zend_module_entry *)f->pData)->name,
+	return strcasecmp(((zend_module_entry *)f->pData)->name,
 				  ((zend_module_entry *)s->pData)->name);
 }
 
