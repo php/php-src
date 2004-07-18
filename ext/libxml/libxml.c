22b70fcb31d91962bf05fa38a6cc5c2f51b2198c
Rob Richards <rrichards@php.net>
2004-07-18 11:45:12 +0000

    MFB: Fixed bug #29221 (Segfault using DOM ownerDocument in a PHP object)

--- ext/libxml/libxml.c
+++ ext/libxml/libxml.c
@@ -568,7 +568,7 @@ int php_libxml_decrement_node_ptr(php_libxml_node_object *object TSRMLS_DC) {
 		obj_node = (php_libxml_node_ptr *) object->node;
 		ret_refcount = --obj_node->refcount;
 		if (ret_refcount == 0) {
-			if (obj_node->node != NULL && obj_node->node->type != XML_DOCUMENT_NODE) {
+			if (obj_node->node != NULL) {
 				obj_node->node->_private = NULL;
 			}
 			efree(obj_node);
