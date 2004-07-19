489ffdfc34a909320a66c93400ff7229873d8e23
moriyoshi <moriyoshi@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-07-19 08:34:18 +0000

    - Fix a bug in iconv stream filter that prevents pending buckets from being   processed on flush operation (same issue pollita fixed several days ago).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@163615 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -2531,27 +2531,27 @@ static php_stream_filter_status_t php_iconv_stream_filter_do_filter(
 	size_t consumed = 0;
 	php_iconv_stream_filter *self = (php_iconv_stream_filter *)filter->abstract;
 
+	while (buckets_in->head != NULL) {
+		bucket = buckets_in->head;
+
+		php_stream_bucket_unlink(bucket TSRMLS_CC);
+
+		if (php_iconv_stream_filter_append_bucket(self, stream, filter, buckets_out, bucket->buf, bucket->buflen, &consumed, self->persistent TSRMLS_CC) != SUCCESS) {				goto out_failure;
+		}
+
+		php_stream_bucket_delref(bucket TSRMLS_CC);
+	}
+
 	if (flags != PSFS_FLAG_NORMAL) {
 		if (php_iconv_stream_filter_append_bucket(self, stream, filter, buckets_out, NULL, 0, &consumed, self->persistent TSRMLS_CC) != SUCCESS) {
 			goto out_failure;
 		}
-	} else {
-		while (buckets_in->head != NULL) {
-			bucket = buckets_in->head;
-
-			php_stream_bucket_unlink(bucket TSRMLS_CC);
-
-			if (php_iconv_stream_filter_append_bucket(self, stream, filter, buckets_out, bucket->buf, bucket->buflen, &consumed, self->persistent TSRMLS_CC) != SUCCESS) {				goto out_failure;
-			}
-
-			php_stream_bucket_delref(bucket TSRMLS_CC);
-		}
 	}
 
-	if (bytes_consumed) {
+	if (bytes_consumed != NULL) {
 		*bytes_consumed = consumed;
 	}
-	
+
 	return PSFS_PASS_ON;
 
 out_failure:
