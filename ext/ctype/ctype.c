e3a480faf8a24f020707df4db19509ff389f7578
Ilia Alshanetsky <iliaa@php.net>
2004-07-20 01:03:34 +0000

    MFH: Fixed bug #29226 (ctype_* functions missing validation of numeric string representations).

--- ext/ctype/ctype.c
+++ ext/ctype/ctype.c
@@ -97,7 +97,11 @@ PHP_MINFO_FUNCTION(ctype)
 		return; \
  	switch (Z_TYPE_P(c)) { \
 	case IS_LONG: \
-		RETURN_BOOL(iswhat(Z_LVAL_P(c))); \
+		if (Z_LVAL_P(c) < 255 && Z_LVAL_P(c) > -127) {	\
+			RETURN_BOOL(iswhat(Z_LVAL_P(c))); \
+		}	\
+		SEPARATE_ZVAL(&c);	\
+		convert_to_string(c);	\
 	case IS_STRING: \
 		{ \
 			char *p; \
