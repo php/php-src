3cf261b0339f8003895d094f17c7057ffdba38fa
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-07-25 19:19:26 +0000

    Fixed bug #29369 (Uploaded files with ' or " in their names get their names truncated at those characters).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@164290 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -632,6 +632,7 @@ static char *php_ap_getword_conf(char **line TSRMLS_DC)
 
 	if ((quote = *str) == '"' || quote == '\'') {
 		strend = str + 1;
+look_for_quote:
 		while (*strend && *strend != quote) {
 			if (*strend == '\\' && strend[1] && strend[1] == quote) {
 				strend += 2;
@@ -639,6 +640,14 @@ static char *php_ap_getword_conf(char **line TSRMLS_DC)
 				++strend;
 			}
 		}
+		if (*strend && *strend == quote) {
+			char p = *(strend + 1);
+			if (p != '\r' && p != '\n' && p != '\0') {
+				strend++;
+				goto look_for_quote;
+			}
+		}
+
 		res = substring_conf(str + 1, strend - str - 1, quote TSRMLS_CC);
 
 		if (*strend == quote) {
