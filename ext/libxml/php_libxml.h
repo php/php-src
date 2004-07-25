a1c33af793c2fc8db0712d88ca291054c6d8f39d
Rob Richards <rrichards@php.net>
2004-07-25 12:00:28 +0000

    export php_libxml_error_handler for win32 build

--- ext/libxml/php_libxml.h
+++ ext/libxml/php_libxml.h
@@ -77,7 +77,7 @@ PHP_LIBXML_API int php_libxml_register_export(zend_class_entry *ce, php_libxml_e
 void php_libxml_node_free_resource(xmlNodePtr node TSRMLS_DC);
 /* When object dtor is called as node may still be referenced */
 void php_libxml_node_decrement_resource(php_libxml_node_object *object TSRMLS_DC);
-void php_libxml_error_handler(void *ctx, const char *msg, ...);
+PHP_LIBXML_API void php_libxml_error_handler(void *ctx, const char *msg, ...);
 void php_libxml_ctx_warning(void *ctx, const char *msg, ...);
 void php_libxml_ctx_error(void *ctx, const char *msg, ...);
 
