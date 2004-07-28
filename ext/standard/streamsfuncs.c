26319ee1a1955fb4e46df6ebb95d8c9ecc9ef0eb
Ilia Alshanetsky <iliaa@php.net>
2004-07-28 23:34:30 +0000

    MFH: Better address availability check.

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -273,7 +273,7 @@ PHP_FUNCTION(stream_socket_sendto)
 	php_stream *stream;
 	zval *zstream;
 	long flags = 0;
-	char *data, *target_addr = NULL;
+	char *data, *target_addr;
 	int datalen, target_addr_len = 0;
 	php_sockaddr_storage sa;
 	socklen_t sl = 0;
@@ -283,7 +283,7 @@ PHP_FUNCTION(stream_socket_sendto)
 	}
 	php_stream_from_zval(stream, &zstream);
 
-	if (target_addr) {
+	if (target_addr_len) {
 		/* parse the address */
 		if (FAILURE == php_network_parse_network_address_with_port(target_addr, target_addr_len, (struct sockaddr*)&sa, &sl TSRMLS_CC)) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed to parse `%s' into a valid network address", target_addr);
