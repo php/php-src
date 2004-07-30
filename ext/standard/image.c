40773632e0e942639a5c4d53357bedf72e6922aa
Ilia Alshanetsky <iliaa@php.net>
2004-07-30 01:10:42 +0000

    MFH: Fixed bug #29443 (Sanity check for wbmp detection).

--- ext/standard/image.c
+++ ext/standard/image.c
@@ -955,6 +955,11 @@ static int php_get_wbmp(php_stream *stream, struct gfxinfo **result, int check T
 		}
 		height = (height << 7) | (i & 0x7f);
 	} while (i & 0x80);
+
+	/* maximum valid sizes for wbmp (although 127x127 may be a more accurate one) */
+	if (height > 2048 || width > 2048) {
+		return 0;
+	}
 	
 	if (!check) {
 		(*result)->width = width;
