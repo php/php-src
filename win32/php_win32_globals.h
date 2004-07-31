47e7fef08ee55782b29e4c4c54d990f5cb8533e9
Wez Furlong <wez@php.net>
2004-07-31 17:28:27 +0000

    Allow win32 stuff to build non-zts. Fix stream crypto func (again? didn't I already commit that?)

--- win32/php_win32_globals.h
+++ win32/php_win32_globals.h
@@ -31,8 +31,8 @@ typedef struct _php_win32_core_globals php_win32_core_globals;
 # define PW32G(v)		TSRMG(php_win32_core_globals_id, php_win32_core_globals*, v)
 extern PHPAPI int php_win32_core_globals_id;
 #else
-# define PW32G(v)		(php_win32_core_globals.v)
-extern PHPAPI struct _php_win32_core_globals php_win32_core_globals;
+# define PW32G(v)		(the_php_win32_core_globals.v)
+extern PHPAPI struct _php_win32_core_globals the_php_win32_core_globals;
 #endif
 
 struct _php_win32_core_globals {
