cc39d06e4e29c2d8c1cd646d5efc6420816e4365
Antony Dovgal <tony2001@php.net>
2004-08-03 06:25:25 +0000

    fix typo in mail.c: mail_force_extra_parameters -> mail.force_extra_parameters

--- ext/standard/mail.c
+++ ext/standard/mail.c
@@ -86,7 +86,7 @@ PHP_FUNCTION(mail)
 	char *subject=NULL, *extra_cmd=NULL;
 	int to_len, message_len, headers_len;
 	int subject_len, extra_cmd_len, i;
-	char *force_extra_parameters = INI_STR("mail_force_extra_parameters");
+	char *force_extra_parameters = INI_STR("mail.force_extra_parameters");
 	char *to_r, *subject_r;
 
 	if (PG(safe_mode) && (ZEND_NUM_ARGS() == 5)) {
