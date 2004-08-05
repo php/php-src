a60a6c5209a6a75139d59d26e33dfaf3b5f21a7b
Wez Furlong <wez@php.net>
2004-08-05 12:15:17 +0000

    Generate a .reg file for the syslog stuff NB: still need a solution for zts and non-zts on the same machine.

--- win32/build/registersyslog.php
+++ win32/build/registersyslog.php
@@ -3,24 +3,43 @@
 /* This script sets up an event source for use by the php syslog() function. */
 
 if (!extension_loaded("win32std")) {
-	dl("php_win32std.dll") or die("b0rk");
+	dl("php_win32std.dll");
 }
 
 $PATH = "SYSTEM\\CurrentControlSet\\Services\\Eventlog\\Application\\PHP-" . phpversion();
 
-$key = @reg_create_key(HKEY_LOCAL_MACHINE, $PATH, KEY_ALL_ACCESS);
+$dll = $argv[1];
 
-if (!$key)
-	$key = reg_open_key(HKEY_LOCAL_MACHINE, $PATH, KEY_ALL_ACCESS);
+if (extension_loaded("win32std")) {
+	$key = @reg_create_key(HKEY_LOCAL_MACHINE, $PATH, KEY_ALL_ACCESS);
 
-if ($key) {
-	reg_set_value($key, "TypesSupported", REG_DWORD, 7) or die("Types");
-	reg_set_value($key, "EventMessageFile", REG_SZ, $argv[1]) or die("EventMessageFile");
+	if (!$key)
+		$key = reg_open_key(HKEY_LOCAL_MACHINE, $PATH, KEY_ALL_ACCESS);
 
-	define_syslog_variables();
-	syslog(LOG_NOTICE, "Registered PHP Event source");
-} else {
-	echo "Could not register event source\n";
+	if ($key) {
+		reg_set_value($key, "TypesSupported", REG_DWORD, 7) or die("Types");
+		reg_set_value($key, "EventMessageFile", REG_SZ, $dll) or die("EventMessageFile");
+
+		define_syslog_variables();
+		syslog(LOG_NOTICE, "Registered PHP Event source");
+	} else {
+		echo "Could not register event source\n";
+	}
 }
+
+/* let's also generate/update the bundled .reg file */
+
+$dll = addslashes($dll);
+
+file_put_contents("win32/syslog.reg", <<<REG
+REGEDIT4
+
+[HKEY_LOCAL_MACHINE\\$PATH]
+"TypesSupported"=dword:00000007
+"EventMessageFile"="$dll"
+
+REG
+);
+
 
 ?>
