29c32f04e3c8666cc59832fcdb59d5f3ea69352b
Andi Gutmans <andi@php.net>
2004-08-12 06:39:26 +0000

    - Back to -dev

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 0
 #define PHP_RELEASE_VERSION 1
-#define PHP_EXTRA_VERSION "RC2"
-#define PHP_VERSION "5.0.1RC2"
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.0.1-dev"
