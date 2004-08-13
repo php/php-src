ac01c8c3edbf4a95a44e1c125b0e3b619cb124a5
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-08-13 14:59:58 +0000

    fix yet another annoying compile warning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@166300 c90b9560-bf6c-de11-be94-00142212c4b1

--- regex/regexec.c
+++ regex/regexec.c
@@ -16,7 +16,7 @@
 #include "utils.h"
 #include "regex2.h"
 
-static int nope = 0;		/* for use in asserts; shuts lint up */
+#define PHP_REGEX_NOPE 0;		/* for use in asserts; shuts lint up */
 
 /* macros for manipulating states, small version */
 #define	states	unsigned
