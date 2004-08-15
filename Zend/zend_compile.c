ce8331f9a5cb2bce74ad2fa03f5589983a8acb8c
Marcus Boerger <helly@php.net>
2004-08-15 15:48:32 +0000

    Remove unnecessary check

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2013,10 +2013,7 @@ void zend_do_inheritance(zend_class_entry *ce, zend_class_entry *parent_ce TSRML
 	zend_hash_merge_ex(&ce->function_table, &parent_ce->function_table, (copy_ctor_func_t) do_inherit_method, sizeof(zend_function), (merge_checker_func_t) do_inherit_method_check, ce);
 	do_inherit_parent_constructor(ce);
 	
-	if ((ce->ce_flags & ZEND_ACC_IMPLICIT_ABSTRACT_CLASS) && !(ce->ce_flags & ZEND_ACC_EXPLICIT_ABSTRACT_CLASS))
-	{
-		zend_verify_abstract_class(ce TSRMLS_CC);
-	}
+	zend_verify_abstract_class(ce TSRMLS_CC);
 }
 
 
