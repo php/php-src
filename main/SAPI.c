949e461e8f6629a3370c114f9df7706f99f9b482
Brian France <bfrance@php.net>
2004-08-19 20:34:04 +0000

    [MFH] If you send a post with a content-type header and then the next post without the content-type header, raw_post_data will not be set.  This is because SG(request_info).post_entry is set to the first requests function pointer which makes it follow the wrong code path.

--- main/SAPI.c
+++ main/SAPI.c
@@ -331,6 +331,7 @@ SAPI_API void sapi_activate(TSRMLS_D)
 	SG(request_info).current_user = NULL;
 	SG(request_info).current_user_length = 0;
 	SG(request_info).no_headers = 0;
+	SG(request_info).post_entry = NULL;
 
 	/* It's possible to override this general case in the activate() callback, if
 	 * necessary.
