5facf4beccf657636c62a548017710370bea7872
Marcus Boerger <helly@php.net>
2004-08-24 21:41:02 +0000

    TSRM fix

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -111,7 +111,7 @@ static PHP_MSHUTDOWN_FUNCTION(pcre)
 #define PCRE_CACHE_SIZE 4096
 
 /* {{{ static pcre_clean_cache */
-static void pcre_clean_cache()
+static void pcre_clean_cache(TSRMLS_D)
 {
 	HashTable *ht = &PCRE_G(pcre_cache);
 	Bucket *p = NULL;
@@ -353,7 +353,7 @@ PHPAPI pcre* pcre_get_compiled_regex_ex(char *regex, pcre_extra **extra, int *pr
 	 * ones).
 	 */
 	if (zend_hash_num_elements(&PCRE_G(pcre_cache)) == PCRE_CACHE_SIZE) {
-		pcre_clean_cache();
+		pcre_clean_cache(TSRMLS_C);
 	}
 
 	/* Store the compiled pattern and extra info in the cache. */
