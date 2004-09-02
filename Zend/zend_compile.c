24dc8a45084e5452043c542acf7f102ef14ceeb1
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-09-02 17:27:44 +0000

    Fugbix typo.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@167861 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2129,7 +2129,7 @@ ZEND_API zend_class_entry *do_bind_inherited_class(zend_op *opline, HashTable *c
 	}
 
 	if (parent_ce->ce_flags & ZEND_ACC_INTERFACE) {
-		zend_error(E_COMPILE_ERROR, "Class %s cannot extend from interfac %s", ce->name, parent_ce->name);
+		zend_error(E_COMPILE_ERROR, "Class %s cannot extend from interface %s", ce->name, parent_ce->name);
 	}
 
 	zend_do_inheritance(ce, parent_ce TSRMLS_CC);
