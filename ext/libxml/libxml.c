f01310928cbc1c2d629df493ae88807198e9f68e
Rob Richards <rrichards@php.net>
2004-09-08 10:16:57 +0000

    MFH: implement php_libxml_xmlCheckUTF8  - workaround for <= libxml2-2.6.13 function

--- ext/libxml/libxml.c
+++ ext/libxml/libxml.c
@@ -505,6 +505,32 @@ PHP_FUNCTION(libxml_set_streams_context)
 
 
 /* {{{ Common functions shared by extensions */
+int php_libxml_xmlCheckUTF8(const unsigned char *s)
+{
+	int i;
+	unsigned char c;
+
+	for (i = 0; (c = s[i++]);) {
+		if ((c & 0x80) == 0) {
+		} else if ((c & 0xe0) == 0xc0) {
+			if ((s[i++] & 0xc0) != 0x80) {
+				return 0;
+			}
+		} else if ((c & 0xf0) == 0xe0) {
+			if ((s[i++] & 0xc0) != 0x80 || (s[i++] & 0xc0) != 0x80) {
+				return 0;
+			}
+		} else if ((c & 0xf8) == 0xf0) {
+			if ((s[i++] & 0xc0) != 0x80 || (s[i++] & 0xc0) != 0x80 || (s[i++] & 0xc0) != 0x80) {
+				return 0;
+			}
+		} else {
+			return 0;
+		}
+	}
+	return 1;
+}
+
 int php_libxml_register_export(zend_class_entry *ce, php_libxml_export_node export_function)
 {
 	php_libxml_func_handler export_hnd;
