f775ea0ae3d9fca47a8258661a9e00c9f638f57c
Marcus Boerger <helly@php.net>
2004-09-27 22:06:10 +0000

    - Fix memeleak

--- Zend/zend_exceptions.c
+++ Zend/zend_exceptions.c
@@ -620,11 +620,14 @@ ZEND_API zval * zend_throw_exception_ex(zend_class_entry *exception_ce, long cod
 {
 	va_list arg;
 	char *message;
+	zval *zexception;
 
 	va_start(arg, format); 
 	zend_vspprintf(&message, 0, format, arg);
 	va_end(arg);
-	return zend_throw_exception(exception_ce, message, code TSRMLS_CC);
+	zexception = zend_throw_exception(exception_ce, message, code TSRMLS_CC);
+	efree(message);
+	return zexception;
 }
 
 
