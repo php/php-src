cd067e641a4ec67ae94c3a140465472983d521f1
Greg Beaver <cellog@php.net>
2004-10-25 17:06:12 +0000

    update for PEAR 1.3.2

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -7,13 +7,13 @@
  */
 $packages  = array(
 // required packages for the installer
-"PEAR"                  =>    "1.3.1",
+"PEAR"                  =>    "1.3.2",
 "XML_RPC"               =>    "1.1.0",
 "Console_Getopt"        =>    "1.2",
 "Archive_Tar"           =>    "1.2",
 
 // required packages for the web frontend
-"PEAR_Frontend_Web"     =>    "0.4"
+"PEAR_Frontend_Web"     =>    "0.4",
 "HTML_Template_IT"      =>    "1.1",
 "Net_UserAgent_Detect"  =>    "1.0",
 );
