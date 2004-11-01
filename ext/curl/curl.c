a451f1c9e4759167957226c87a73ce143336c770
Ilia Alshanetsky <iliaa@php.net>
2004-11-01 04:56:10 +0000

    MFH:  Fixed bug #30613 (Prevent infinite recursion in url redirection).

--- ext/curl/curl.c
+++ ext/curl/curl.c
@@ -708,6 +708,7 @@ PHP_FUNCTION(curl_init)
 	curl_easy_setopt(ch->cp, CURLOPT_WRITEHEADER,       (void *) ch);
 	curl_easy_setopt(ch->cp, CURLOPT_DNS_USE_GLOBAL_CACHE, 1);
 	curl_easy_setopt(ch->cp, CURLOPT_DNS_CACHE_TIMEOUT, 120);
+	curl_easy_setopt(ch->cp, CURLOPT_MAXREDIRS, 20); /* prevent infinite redirects */
 
 	if (argc > 0) {
 		char *urlcopy;
