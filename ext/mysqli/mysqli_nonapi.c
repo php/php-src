b8863135a2202013c48d2933c61183279e44ae60
Georg Richter <georg@php.net>
2004-11-01 07:19:26 +0000

    Fixed tests for empty dates (see http://bugs.mysql.com/?id=6058) Fixed memleak in mysqli_stmt_bind_result Fixed error handling for mysqli_multi_query

--- ext/mysqli/mysqli_nonapi.c
+++ ext/mysqli/mysqli_nonapi.c
@@ -222,8 +222,23 @@ PHP_FUNCTION(mysqli_multi_query)
 
 	MYSQLI_ENABLE_MQ;	
 	if (mysql_real_query(mysql->mysql, query, query_len)) {
-		MYSQLI_DISABLE_MQ;
+		char s_error[MYSQL_ERRMSG_SIZE], s_sqlstate[SQLSTATE_LENGTH+1];
+		unsigned int s_errno;
 		MYSQLI_REPORT_MYSQL_ERROR(mysql->mysql);
+
+		/* we have to save error information, cause 
+		MYSQLI_DISABLE_MQ will reset error information */
+		strcpy(s_error, mysql_error(mysql->mysql));
+		strcpy(s_sqlstate, mysql_sqlstate(mysql->mysql));
+		s_errno = mysql_errno(mysql->mysql);
+
+		MYSQLI_DISABLE_MQ;
+
+		/* restore error information */
+		strcpy(mysql->mysql->net.last_error, s_error);
+		strcpy(mysql->mysql->net.sqlstate, s_sqlstate);
+		mysql->mysql->net.last_errno = s_errno;	
+
 		RETURN_FALSE;
 	}	
 	RETURN_TRUE;
