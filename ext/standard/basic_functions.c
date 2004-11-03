b5c1040240bb2ad0fe149c6ff2d5a00a29775d47
Antony Dovgal <tony2001@php.net>
2004-11-03 06:22:08 +0000

    fix build

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -1734,13 +1734,11 @@ PHP_FUNCTION(sleep)
 
 	convert_to_long_ex(num);
 #ifdef PHP_SLEEP_NON_VOID
-	RETURN_LONG(
+	RETURN_LONG(php_sleep(Z_LVAL_PP(num)));
+#else
+	php_sleep(Z_LVAL_PP(num));
 #endif
-	php_sleep(Z_LVAL_PP(num))
-#ifdef PHP_SLEEP_NON_VOID
-	)
-#endif
-	;
+
 }
 /* }}} */
 
