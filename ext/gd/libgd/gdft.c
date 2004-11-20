f3b74fe9ac7481ad8b6441a8781424175e5ff8a5
Pierre Joye <pajoye@php.net>
2004-11-20 13:09:45 +0000

    - Attempt to fix #28598, bug introduced in GD (around php 4.3.4)   The way they fix MS Symbol are wrong, multiple faces support hould added   later, safer.
      Please test it with your weird fonts, if it works, MFH will be done.

--- ext/gd/libgd/gdft.c
+++ ext/gd/libgd/gdft.c
@@ -915,10 +915,12 @@ gdImageStringFTEx (gdImage * im, int *brect, int fg, char *fontlist, double ptsi
 
 /* EAM DEBUG */
 #if (defined(FREETYPE_MAJOR) && ((FREETYPE_MAJOR == 2 && ((FREETYPE_MINOR == 1 && FREETYPE_PATCH >= 3) || FREETYPE_MINOR > 1) || FREETYPE_MAJOR > 2)))
-		if (font->face->charmap->encoding == FT_ENCODING_MS_SYMBOL) {
+		if (font->face->charmap->encoding == FT_ENCODING_MS_SYMBOL && strcmp(font->face->family_name, "Symbol") == 0) {
 			/* I do not know the significance of the constant 0xf000.
 			 * It was determined by inspection of the character codes
 			 * stored in Microsoft font symbol.
+			 * Added by Pierre (pajoye@php.net):
+			 * Convert to the Symbol glyph range only for a Symbol family member
 			 */
 			len = gdTcl_UtfToUniChar (next, &ch);
 			ch |= 0xf000;
