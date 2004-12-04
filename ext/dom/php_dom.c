390ade14cad526b8f3b49cd3c100921ed4ac35c7
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-12-04 11:39:34 +0000

    Fixed bug #28817 (Var problem when extending domDocument). (Georg)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@174052 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -235,6 +235,7 @@ zval *dom_read_property(zval *object, zval *member, int type TSRMLS_DC)
 	} else {
 		std_hnd = zend_get_std_object_handlers();
 		retval = std_hnd->read_property(object, member, type TSRMLS_CC);
+		retval->refcount = 1;
 	}
 
 	if (member == &tmp_member) {
