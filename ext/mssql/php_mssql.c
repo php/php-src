a6e1c3616bb975a60abf1361d906040bafe900aa
Ilia Alshanetsky <iliaa@php.net>
2004-12-06 19:45:57 +0000

    MFB: revert.

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -800,10 +800,6 @@ static void php_mssql_get_column_content_with_type(mssql_link *mssql_ptr,int off
 			char *data = charcol(offset);
 
 			length=dbdatlen(mssql_ptr->link,offset);
-			if (!length) {
-				ZVAL_EMPTY_STRING(result);
-				break;
-			}
 #if ilia_0
 			while (length>0 && data[length-1] == ' ') { /* nuke trailing whitespace */
 				length--;
