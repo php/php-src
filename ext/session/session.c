aea9676eb0416f5e1326f5555e1bf93068e18c9b
Antony Dovgal <tony2001@php.net>
2004-12-09 17:16:57 +0000

    CS changes

--- ext/session/session.c
+++ ext/session/session.c
@@ -1224,8 +1224,7 @@ PHP_FUNCTION(session_module_name)
 
 		if (PS(mod) && PS(mod)->s_name) {
 			RETVAL_STRING(safe_estrdup(PS(mod)->s_name), 0);
-		}
-		else {
+		} else {
 			RETVAL_EMPTY_STRING();
 		}
 		
@@ -1233,8 +1232,7 @@ PHP_FUNCTION(session_module_name)
 	} else {
 		if (PS(mod) && PS(mod)->s_name) {
 			RETURN_STRING(safe_estrdup(PS(mod)->s_name), 0);
-		}
-		else {
+		} else {
 			RETURN_EMPTY_STRING();
 		}
 	}
