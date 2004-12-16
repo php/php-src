2ec6b030aadd01487fab088e3280a643cc96e5b6
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-12-16 19:13:58 +0000

    fix leak (init array only if we got valid start key)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@174917 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -1519,13 +1519,13 @@ PHP_FUNCTION(array_fill)
 		WRONG_PARAM_COUNT;
 	}
 
-	/* allocate an array for return */
-	array_init(return_value);
-
 	switch (Z_TYPE_PP(start_key)) {
 		case IS_STRING:
 		case IS_LONG:
 		case IS_DOUBLE:
+			/* allocate an array for return */
+			array_init(return_value);
+			
 			if (PZVAL_IS_REF(*val)) {
 				SEPARATE_ZVAL(val);
 			}
