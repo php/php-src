4f8973fcdc6163a72c071fa3c52f04ab2f783324
Frank M. Kromann <fmk@php.net>
2004-12-16 21:47:23 +0000

    Fix #31120 and #31135. mssql_query returns false on successfill inserts and stored procedures

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -1196,10 +1196,8 @@ PHP_FUNCTION(mssql_query)
 	while ((num_fields = dbnumcols(mssql_ptr->link)) <= 0 && retvalue == SUCCEED) {
 		retvalue = dbresults(mssql_ptr->link);
 	}
-	if (retvalue != SUCCEED) {
-		RETURN_FALSE;
-	}
-	if ((num_fields = dbnumcols(mssql_ptr->link)) <= 0) {
+
+	if (num_fields <= 0) {
 		RETURN_TRUE;
 	}
 
