c75458bac3672b4672e1cca8049a5c2c00f0170d
Ilia Alshanetsky <iliaa@php.net>
2004-12-23 18:04:10 +0000

    Fixed bug #31270 (missing safe_mode/open_basedir check in swf_openfile()).

--- ext/swf/swf.c
+++ ext/swf/swf.c
@@ -239,12 +239,17 @@ PHP_FUNCTION(swf_openfile)
 	}
 	na = tmpna;
 #endif
+	if (php_check_open_basedir(na TSRMLS_CC) || (PG(safe_mode) && !php_checkuid(na, "wb+", CHECKUID_CHECK_MODE_PARAM))) {
+		goto err;
+	}
+	
 	if (!SWFG(use_file))
 		SWFG(tmpfile_name) = na;
 
 	swf_openfile(na,(float)Z_DVAL_PP(sizeX), (float)Z_DVAL_PP(sizeY),
       		 	 (float)Z_DVAL_PP(frameRate), (float)Z_DVAL_PP(r), 
       		 	 (float)Z_DVAL_PP(g), (float)Z_DVAL_PP(b));
+err:
 #ifdef VIRTUAL_DIR
 	free(na);
 #endif
