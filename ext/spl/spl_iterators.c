d3a5984f48794cbcfe02021a2a644b640470ee37
Antony Dovgal <tony2001@php.net>
2004-12-29 16:12:55 +0000

    MFH: more typos

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -1240,7 +1240,7 @@ SPL_METHOD(CachingIterator, hasNext)
 } /* }}} */
 
 /* {{{ proto string CachingIterator::__toString()
-   Retrun the string representation of the current element */
+   Return the string representation of the current element */
 SPL_METHOD(CachingIterator, __toString)
 {
 	spl_dual_it_object   *intern;
@@ -1295,7 +1295,7 @@ SPL_METHOD(CachingRecursiveIterator, hasChildren)
 } /* }}} */
 
 /* {{{ proto CachingRecursiveIterator CachingRecursiveIterator::getChildren()
-  Return the inenr iteraor's children as a CachingRecursiveIterator */
+  Return the inner iterator's children as a CachingRecursiveIterator */
 SPL_METHOD(CachingRecursiveIterator, getChildren)
 {
 	spl_dual_it_object   *intern;
