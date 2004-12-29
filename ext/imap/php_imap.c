3263c9c7787ae4d216a8076d22c4ed45d5b7c8a1
Ilia Alshanetsky <iliaa@php.net>
2004-12-29 21:23:11 +0000

    MFH: Fixed bug #31142 (imap_mail_compose() fails to generate correct output).

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -3168,37 +3168,29 @@ PHP_FUNCTION(imap_mail_compose)
 
 				bod=&part->body;
 
-				tempstring=emalloc(strlen(bod->contents.text.data)+strlen(CRLF)+strlen(mystring)+1);
-				strcpy(tempstring, mystring);
+				tempstring = emalloc(strlen(bod->contents.text.data)+strlen(CRLF)+strlen(mystring)+1);
+				sprintf(tempstring, "%s%s%s", mystring, bod->contents.text.data, CRLF);
 				efree(mystring);
-				mystring=tempstring;
-				sprintf(mystring, "%s%s%s", mystring, bod->contents.text.data, CRLF);
-
+				mystring = tempstring;
 			} while ((part = part->next)); /* until done */
 
 			/* output trailing cookie */
 			sprintf(tmp, "--%s--", cookie);
-			tempstring=emalloc(strlen(tmp)+strlen(CRLF)+strlen(mystring)+1);
-			strcpy(tempstring, mystring);
+			tempstring = emalloc(strlen(tmp)+strlen(CRLF)+strlen(mystring)+1);
+			sprintf(tempstring, "%s%s%s", mystring, tmp, CRLF);
 			efree(mystring);
-			mystring=tempstring;
-			sprintf(mystring, "%s%s%s", mystring, tmp, CRLF);
-
+			mystring = tempstring;
 	} else if (bod) {
-
-			tempstring=emalloc(strlen(bod->contents.text.data)+strlen(CRLF)+strlen(mystring)+1);
-			strcpy(tempstring, mystring);
+			tempstring = emalloc(strlen(bod->contents.text.data)+strlen(CRLF)+strlen(mystring)+1);
+			sprintf(tempstring, "%s%s%s", mystring, bod->contents.text.data, CRLF);
 			efree(mystring);
-			mystring=tempstring;
-			sprintf(mystring, "%s%s%s", mystring, bod->contents.text.data, CRLF);
-
+			mystring = tempstring;
 	} else {
 		efree(mystring);
 		RETURN_FALSE;
 	}
 
-	RETVAL_STRINGL(mystring, strlen(mystring), 1);  
-	efree(tempstring);
+	RETVAL_STRING(tempstring, 0);  
 }
 /* }}} */
 
