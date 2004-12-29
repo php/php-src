e2b8045928627b44389ee84ff12b1048018d1605
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2004-12-29 13:53:40 +0000

    MFB: fix typos
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@175895 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -1339,7 +1339,7 @@ SPL_METHOD(CachingIterator, next)
 } /* }}} */
 
 /* {{{ proto boolean CachingIterator::hasNext()
-   Cehck whether the inner iterator has a valid next element */
+   Check whether the inner iterator has a valid next element */
 SPL_METHOD(CachingIterator, hasNext)
 {
 	spl_dual_it_object   *intern;
@@ -1394,7 +1394,7 @@ SPL_METHOD(CachingRecursiveIterator, __construct)
 } /* }}} */
 
 /* {{{ proto bolean CachingRecursiveIterator::hasChildren()
-   Cehck whether the current element of the inner iterator has children */
+   Check whether the current element of the inner iterator has children */
 SPL_METHOD(CachingRecursiveIterator, hasChildren)
 {
 	spl_dual_it_object   *intern;
