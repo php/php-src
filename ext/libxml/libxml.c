0b0cba5bb1c1e70b2ccd24709c6848ad979e9054
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-02 12:08:41 +0000

    Fixed bug #31361 (simplexml/domxml segfault when adding node twice)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@176283 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/libxml/libxml.c
+++ ext/libxml/libxml.c
@@ -725,6 +725,10 @@ void php_libxml_node_decrement_resource(php_libxml_node_object *object TSRMLS_DC
 		ret_refcount = php_libxml_decrement_node_ptr(object TSRMLS_CC);
 		if (ret_refcount == 0) {
 			php_libxml_node_free_resource(nodep TSRMLS_CC);
+		} else {
+			if (obj_node && object == obj_node->_private) {
+				obj_node->_private = NULL;
+			}
 		}
 		/* Safe to call as if the resource were freed then doc pointer is NULL */
 		php_libxml_decrement_doc_ref(object TSRMLS_CC);
