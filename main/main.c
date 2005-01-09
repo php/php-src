411f43f226d60d890fd36f92e0f303460b0534ee
foobar <sniper@php.net>
2005-01-09 16:30:22 +0000

    MFH: - Fix leaks with registered shutdown functions.

--- main/main.c
+++ main/main.c
@@ -975,11 +975,12 @@ void php_request_shutdown(void *dummy)
 	} zend_end_try();
 
 	if (PG(modules_activated)) zend_try {
-		php_call_shutdown_functions();
+		php_call_shutdown_functions(TSRMLS_C);
 	} zend_end_try();
 	
 	if (PG(modules_activated)) {
 		zend_deactivate_modules(TSRMLS_C);
+		php_free_shutdown_functions(TSRMLS_C);
 	}
 
 	zend_try {
