ae546e85b889a452fade5305ea327d0dafd29a53
foobar <sniper@php.net>
2005-01-10 15:38:50 +0000

    - Fixed bug #28074 (FastCGI: stderr should be written in a FCGI stderr stream).   (chris at ex-parrot dot com)

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -481,6 +481,13 @@ static void sapi_cgi_register_variables(zval *track_vars_array TSRMLS_DC)
 
 static void sapi_cgi_log_message(char *message)
 {
+#if PHP_FASTCGI
+	if (!FCGX_IsCGI()) {
+		FCGX_Request *request = (FCGX_Request *)SG(server_context);
+		FCGX_FPrintF( request->err, "%s\n", message );
+		/* ignore return code */
+	} else
+#endif /* PHP_FASTCGI */
 	fprintf(stderr, "%s\n", message);
 }
 
