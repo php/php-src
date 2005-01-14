cbe8634bbe6324305e391aecfe7db07275264969
Dmitry Stogov <dmitry@php.net>
2005-01-14 08:50:00 +0000

    Revert to old behavior of $str["str"]. ($str["str"] is equivalent of $str[0])

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -967,24 +967,7 @@ static void zend_fetch_dimension_address(znode *result, znode *op1, znode *op2, 
 
 				offset = get_zval_ptr(op2, Ts, &EG(free_op2), BP_VAR_R);
 
-				if (Z_TYPE_P(offset) == IS_STRING) {
-					char *strval;
-					long  lval;
-
-					strval = Z_STRVAL_P(offset);
-					if (is_numeric_string(strval, Z_STRLEN_P(offset), &lval, NULL, 0) == IS_LONG) {
-					  ZVAL_LONG(&tmp, lval);
-						offset = &tmp;
-					} else {
-						if (type != BP_VAR_IS) {
-							zend_error(E_NOTICE, "Trying to get string index from a string");
-						}
-						*retval = &EG(error_zval_ptr);;
-						SELECTIVE_PZVAL_LOCK(**retval, result);
-						FREE_OP(Ts, op2, EG(free_op2));
-						return;
-					}
-				} else if (offset->type != IS_LONG) {
+				if (offset->type != IS_LONG) {
 					tmp = *offset;
 					zval_copy_ctor(&tmp);
 					convert_to_long(&tmp);
@@ -4052,16 +4035,7 @@ static int zend_isset_isempty_dim_prop_obj_handler(int prop_dim, ZEND_OPCODE_HAN
 		} else if ((*container)->type == IS_STRING && !prop_dim) { /* string offsets */
 			zval tmp;
 
-			if (Z_TYPE_P(offset) == IS_STRING) {
-				char *strval;
-				long  lval;
-
-				strval = Z_STRVAL_P(offset);
-				if (is_numeric_string(strval, Z_STRLEN_P(offset), &lval, NULL, 0) == IS_LONG) {
-					ZVAL_LONG(&tmp, lval);
-					offset = &tmp;
-				}
-			} else if (offset->type != IS_LONG) {
+			if (offset->type != IS_LONG) {
 				tmp = *offset;
 				zval_copy_ctor(&tmp);
 				convert_to_long(&tmp);
