f3d39ff406787514bb36927c0b97979c066f5d5d
Zak Greant <zak@php.net>
2005-01-16 19:28:55 +0000

    Added standard emacs and vim modelines

--- ext/mysqli/mysqli_report.h
+++ ext/mysqli/mysqli_report.h
@@ -52,3 +52,13 @@ typedef struct {
 
 
 #endif
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
--- ext/mysqli/php_mysqli.h
+++ ext/mysqli/php_mysqli.h
@@ -436,4 +436,6 @@ ZEND_EXTERN_MODULE_GLOBALS(mysqli)
  * c-basic-offset: 4
  * indent-tabs-mode: t
  * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
  */
