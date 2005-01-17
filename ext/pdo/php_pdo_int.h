e502e9dd22fb9e3679e922ae99d2a3cd733b8738
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-17 05:28:46 +0000

    don't raise errors for the no-error case
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@177397 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/php_pdo_int.h
+++ ext/pdo/php_pdo_int.h
@@ -53,8 +53,8 @@ extern void pdo_handle_error(pdo_dbh_t *dbh, pdo_stmt_t *stmt TSRMLS_DC);
 
 #define PDO_DBH_CLEAR_ERR()		strcpy(dbh->error_code, PDO_ERR_NONE)
 #define PDO_STMT_CLEAR_ERR()	strcpy(stmt->error_code, PDO_ERR_NONE)
-#define PDO_HANDLE_DBH_ERR()	if (dbh->error_code) { pdo_handle_error(dbh, NULL TSRMLS_CC); }
-#define PDO_HANDLE_STMT_ERR()	if (stmt->error_code) { pdo_handle_error(stmt->dbh, stmt TSRMLS_CC); }
+#define PDO_HANDLE_DBH_ERR()	if (strcmp(dbh->error_code, PDO_ERR_NONE)) { pdo_handle_error(dbh, NULL TSRMLS_CC); }
+#define PDO_HANDLE_STMT_ERR()	if (strcmp(stmt->error_code, PDO_ERR_NONE)) { pdo_handle_error(stmt->dbh, stmt TSRMLS_CC); }
 
 int pdo_sqlstate_init_error_table(void);
 void pdo_sqlstate_fini_error_table(void);
