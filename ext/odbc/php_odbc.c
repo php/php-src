c68be635e2657218b4e544edaf6d2036d6ef073e
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-18 15:03:51 +0000

    fix bug #30430 (odbc_next_result() doesn't bind values and that results in segfault) fix protos
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@177559 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/odbc/php_odbc.c
+++ ext/odbc/php_odbc.c
@@ -2465,10 +2465,7 @@ PHP_FUNCTION(odbc_next_result)
 
 	result->fetched = 0;
 	rc = SQLMoreResults(result->stmt);
-	if (rc == SQL_SUCCESS) {
-		RETURN_TRUE;
-	}
-	else if (rc == SQL_SUCCESS_WITH_INFO) {
+	if (rc == SQL_SUCCESS_WITH_INFO || rc == SQL_SUCCESS) {
 		rc = SQLFreeStmt(result->stmt, SQL_UNBIND);
 		SQLNumParams(result->stmt, &(result->numparams));
 		SQLNumResultCols(result->stmt, &(result->numcols));
@@ -2809,7 +2806,7 @@ PHP_FUNCTION(odbc_setoption)
  * metadata functions
  */
 
-/* {{{ proto resource odbc_tables(resource connection_id [, string qualifier, string owner, string name, string table_types])
+/* {{{ proto resource odbc_tables(resource connection_id [, string qualifier [, string owner [, string name [, string table_types]]]])
    Call the SQLTables function */
 PHP_FUNCTION(odbc_tables)
 {
@@ -2888,7 +2885,7 @@ PHP_FUNCTION(odbc_tables)
 }
 /* }}} */
 
-/* {{{ proto resource odbc_columns(resource connection_id, string qualifier, string owner, string table_name, string column_name)
+/* {{{ proto resource odbc_columns(resource connection_id [, string qualifier [, string owner [, string table_name [, string column_name]]]])
    Returns a result identifier that can be used to fetch a list of column names in specified tables */
 PHP_FUNCTION(odbc_columns)
 {
