75887b9ef507e3b6b505a38e9542478ce371c5e7
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-18 15:44:33 +0000

    - Make sure FD_CLOEXEC is always defined.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@177564 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -166,8 +166,11 @@ static void ps_files_open(ps_files *data, const char *key TSRMLS_DC)
 			flock(data->fd, LOCK_EX);
 
 #ifdef F_SETFD
+#ifndef FD_CLOEXEC
+#define FD_CLOEXEC 1
+#endif
 			if (fcntl(data->fd, F_SETFD, FD_CLOEXEC)) {
-				php_error_docref(NULL TSRMLS_CC, E_WARNING, "fcntl(%d, F_SETFD, 1) failed: %s (%d)", data->fd, strerror(errno), errno);
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "fcntl(%d, F_SETFD, FD_CLOEXEC) failed: %s (%d)", data->fd, strerror(errno), errno);
 			}
 #endif
 		} else {
