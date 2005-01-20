d7ff19be80101e099811a29944d7eb8aa7706c75
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-20 11:47:05 +0000

    Fixed bug #28817 (Var problem when extending domDocument).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@177735 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -201,6 +201,38 @@ static void dom_register_prop_handler(HashTable *prop_handler, char *name, dom_r
 }
 /* }}} */
 
+static zval **dom_get_property_ptr_ptr(zval *object, zval *member TSRMLS_DC)
+{
+	dom_object *obj;
+	zval tmp_member;
+	zval **retval = NULL;
+	dom_prop_handler *hnd;
+	zend_object_handlers *std_hnd;
+	int ret = FAILURE;
+
+ 	if (member->type != IS_STRING) {
+		tmp_member = *member;
+		zval_copy_ctor(&tmp_member);
+		convert_to_string(&tmp_member);
+		member = &tmp_member;
+	}
+
+	obj = (dom_object *)zend_objects_get_address(object TSRMLS_CC);
+
+	if (obj->prop_handler != NULL) {
+		ret = zend_hash_find(obj->prop_handler, Z_STRVAL_P(member), Z_STRLEN_P(member)+1, (void **) &hnd);
+	}
+	if (ret == FAILURE) {
+		std_hnd = zend_get_std_object_handlers();
+		retval = std_hnd->get_property_ptr_ptr(object, member TSRMLS_CC);
+	}
+
+	if (member == &tmp_member) {
+		zval_dtor(member);
+	}
+	return retval;
+}
+
 /* {{{ dom_read_property */
 zval *dom_read_property(zval *object, zval *member, int type TSRMLS_DC)
 {
@@ -393,13 +425,13 @@ PHP_MINIT_FUNCTION(dom)
 	memcpy(&dom_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
 	dom_object_handlers.read_property = dom_read_property;
 	dom_object_handlers.write_property = dom_write_property;
-	dom_object_handlers.get_property_ptr_ptr = NULL;
+	dom_object_handlers.get_property_ptr_ptr = dom_get_property_ptr_ptr;
 	dom_object_handlers.clone_obj = dom_objects_store_clone_obj;
 
 	memcpy(&dom_ze1_object_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
 	dom_ze1_object_handlers.read_property = dom_read_property;
 	dom_ze1_object_handlers.write_property = dom_write_property;
-	dom_ze1_object_handlers.get_property_ptr_ptr = NULL;
+	dom_object_handlers.get_property_ptr_ptr = dom_get_property_ptr_ptr;
 	dom_ze1_object_handlers.clone_obj = dom_objects_ze1_clone_obj;
 
 	zend_hash_init(&classes, 0, NULL, NULL, 1);
