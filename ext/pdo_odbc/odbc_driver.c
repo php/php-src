9c05165dd97772c2559cab50ead9f594946f063b
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-01-21 04:43:06 +0000

    Allow static build (better detection of PDO headers). Copy error code into driver code, so PDO can handle it correctly. Less leaks
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@177823 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_odbc/odbc_driver.c
+++ ext/pdo_odbc/odbc_driver.c
@@ -29,6 +29,7 @@
 #include "pdo/php_pdo_driver.h"
 #include "php_pdo_odbc.h"
 #include "php_pdo_odbc_int.h"
+#include "zend_exceptions.h"
 
 static int pdo_odbc_fetch_error_func(pdo_dbh_t *dbh, pdo_stmt_t *stmt, zval *info TSRMLS_DC)
 {
@@ -42,7 +43,7 @@ static int pdo_odbc_fetch_error_func(pdo_dbh_t *dbh, pdo_stmt_t *stmt, zval *inf
 		einfo = &S->einfo;
 	}
 
-	spprintf(&message, 0, "%s (%s[%d] at %s:%d)",
+	spprintf(&message, 0, "%s (%s[%ld] at %s:%d)",
 				einfo->last_err_msg,
 				einfo->what, einfo->last_error,
 				einfo->file, einfo->line);
@@ -129,6 +130,8 @@ static int odbc_handle_preparer(pdo_dbh_t *dbh, const char *sql, long sql_len, p
 		sql = nsql;
 	} else if (ret == -1) {
 		/* couldn't grok it */
+		strcpy(dbh->error_code, stmt->error_code);
+		efree(S);
 		return 0;
 	}
 	
