6de1f9d2f4a3cf5426b5e1d6a2f03195f363e70e
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-02-06 23:17:09 +0000

    Avoid bug where stream_copy_to_stream() call for precisely 2000000 bytes would result in entire resource being copied. The mmap brake is already present inside mmap_range() and is set to 1MB.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@179025 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_streams.h
+++ main/php_streams.h
@@ -407,10 +407,8 @@ END_EXTERN_C()
 #define PHP_STREAM_OPTION_RETURN_NOTIMPL	-2 /* underlying stream does not implement; streams can handle it instead */
 
 /* copy up to maxlen bytes from src to dest.  If maxlen is PHP_STREAM_COPY_ALL, copy until eof(src).
- * Uses mmap if the src is a plain file and at offset 0 
- * To ensure we don't take up too much memory when reading large files, set the default mmap length
- * at this many bytes */
-#define PHP_STREAM_COPY_ALL		2000000
+ * Uses mmap if the src is a plain file and at offset 0 */
+#define PHP_STREAM_COPY_ALL		((size_t)-1)
 
 BEGIN_EXTERN_C()
 PHPAPI size_t _php_stream_copy_to_stream(php_stream *src, php_stream *dest, size_t maxlen STREAMS_DC TSRMLS_DC);
