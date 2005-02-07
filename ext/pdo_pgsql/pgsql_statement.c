af162668e51f0ea23a44037708defbc40b423b31
Wez Furlong <wez@php.net>
2005-02-07 00:04:20 +0000

    fix pointer indirection (and thus leak)

--- ext/pdo_pgsql/pgsql_statement.c
+++ ext/pdo_pgsql/pgsql_statement.c
@@ -243,7 +243,7 @@ static int pgsql_stmt_get_col(pdo_stmt_t *stmt, int colno, char **ptr, unsigned 
 				tmp_ptr = PQunescapeBytea(*ptr, &tmp_len);
 				*ptr = estrndup(tmp_ptr, tmp_len);
 				*len = tmp_len;
-				caller_frees = 1;
+				*caller_frees = 1;
 				free(tmp_ptr);
 				break;
 		}
