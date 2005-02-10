71ab56b802bfcfc804c6156fc58b7a2c14bd4623
SVN Migration <svn@php.net>
2005-02-10 02:06:50 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_0_2_1'.

--- netware/geterrnoptr.c
+++
@@ -1,9 +0,0 @@
-/* Moved into a separate source file -- can be eliminated later */
-
-#include <errno.h>
-
-int *__get_errno_ptr(void)
-{   
-    return ___errno();
-}
-
--- win32/php_registry.h
+++
@@ -1,8 +0,0 @@
-#ifndef PHP_REGISTRY_H
-#define PHP_REGISTRY_H
-
-
-void UpdateIniFromRegistry(char *path TSRMLS_DC);
-char *GetIniPathFromRegistry();
-
-#endif /* PHP_REGISTRY_H */
