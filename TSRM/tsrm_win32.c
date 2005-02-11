e2d002e7c012897ca7cbbefb5b63d9ae67053e57
foobar <sniper@php.net>
2005-02-11 03:34:04 +0000

    Revert

--- TSRM/tsrm_win32.c
+++ TSRM/tsrm_win32.c
@@ -1,13 +1,13 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP Version 5                                                        |
+   | PHP Version 4                                                        |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2004 The PHP Group                                |
+   | Copyright (c) 1997-2003 The PHP Group                                |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 3.0 of the PHP license,       |
+   | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_0.txt.                                  |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
    | If you did not receive a copy of the PHP license and are unable to   |
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
@@ -24,12 +24,9 @@
 #include <process.h>
 #include <time.h>
 
-#define TSRM_INCLUDE_FULL_WINDOWS_HEADERS
-
 #include "TSRM.h"
 
 #ifdef TSRM_WIN32
-
 #include "tsrm_win32.h"
 
 #ifdef ZTS
@@ -83,19 +80,6 @@ TSRM_API void tsrm_win32_shutdown(void)
 	tsrm_win32_dtor(&win32_globals TSRMLS_CC);
 #endif
 }
-
-TSRM_API int tsrm_win32_access(const char *pathname, int mode)
-{
-	SHFILEINFO sfi;
-
-	if (mode == 1 /*X_OK*/) {
-		return access(pathname, 0) == 0 && 
-			SHGetFileInfo(pathname, 0, &sfi, sizeof(SHFILEINFO), SHGFI_EXETYPE) != 0 ? 0 : -1;
-	} else {
-		return access(pathname, mode);
-	}
-}
-
 
 static process_pair *process_get(FILE *stream TSRMLS_DC)
 {
@@ -384,5 +368,4 @@ TSRM_API char *realpath(char *orig_path, char *buffer)
 	}
 	return buffer;
 }
-
 #endif
