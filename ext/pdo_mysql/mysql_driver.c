0e42f5bffbeccdb2ed2be88a40db940d3dfb8786
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-02-13 06:37:29 +0000

    autocommit flag gets set always by PDO before invoking this method; we should unconditionally inspect it here.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@179643 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_mysql/mysql_driver.c
+++ ext/pdo_mysql/mysql_driver.c
@@ -318,9 +318,9 @@ static int pdo_mysql_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 			pdo_mysql_error(dbh);
 			goto cleanup;
 		}
-
-		mysql_handle_autocommit(dbh);
 	}
+	
+	mysql_handle_autocommit(dbh);
 
 #ifndef PHP_WIN32
 	if (vars[2].optval && !strcmp("localhost", vars[2].optval)) {
