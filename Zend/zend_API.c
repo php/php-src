cdcd930d416c4005e09054cad1882652ba61e1a3
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-02-17 20:49:56 +0000

    - Actually we must do this in two steps: 1st resize the table and set all   interfaces, 2nd implement the interfaces
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@180000 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -1633,6 +1633,7 @@ ZEND_API void zend_class_implements(zend_class_entry *class_entry TSRMLS_DC, int
 	zend_class_entry *interface_entry;
 	va_list interface_list;
 	va_start(interface_list, num_interfaces);
+	int ce_num = class_entry->num_interfaces;
 
 	if (class_entry->type & ZEND_INTERNAL_CLASS) {
 		class_entry->interfaces = realloc(class_entry->interfaces, sizeof(zend_class_entry*) * (class_entry->num_interfaces+num_interfaces));
@@ -1642,8 +1643,10 @@ ZEND_API void zend_class_implements(zend_class_entry *class_entry TSRMLS_DC, int
 	
 	while (num_interfaces--) {
 		interface_entry = va_arg(interface_list, zend_class_entry *);
-		class_entry->interfaces[class_entry->num_interfaces] = interface_entry;
-		zend_do_implement_interface(class_entry, interface_entry TSRMLS_CC);
+		class_entry->interfaces[class_entry->num_interfaces++] = interface_entry;
+	}
+	while(ce_num < class_entry->num_interfaces) {
+		zend_do_implement_interface(class_entry, class_entry->interfaces[ce_num++] TSRMLS_CC);
 	}
 	va_end(interface_list);
 }
