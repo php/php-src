c88da2bd92d3699431a9784f33051f74d2919655
Marcus Boerger <helly@php.net>
2005-02-17 20:54:56 +0000

    - No C++ ruleZ here

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -1631,9 +1631,9 @@ ZEND_API zend_class_entry *zend_register_internal_class_ex(zend_class_entry *cla
 ZEND_API void zend_class_implements(zend_class_entry *class_entry TSRMLS_DC, int num_interfaces, ...)
 {
 	zend_class_entry *interface_entry;
+	int ce_num = class_entry->num_interfaces;
 	va_list interface_list;
 	va_start(interface_list, num_interfaces);
-	int ce_num = class_entry->num_interfaces;
 
 	if (class_entry->type & ZEND_INTERNAL_CLASS) {
 		class_entry->interfaces = realloc(class_entry->interfaces, sizeof(zend_class_entry*) * (class_entry->num_interfaces+num_interfaces));
