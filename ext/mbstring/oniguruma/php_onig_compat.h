d7d7ca62d4f28ff54bb79df57944577e56a024b2
Moriyoshi Koizumi <moriyoshi@php.net>
2005-02-21 09:43:55 +0000

    - Fix bug #29989 (type re_registers redefined in oniguruma.h)

--- ext/mbstring/oniguruma/php_onig_compat.h
+++ ext/mbstring/oniguruma/php_onig_compat.h
@@ -3,5 +3,6 @@
 
 #define re_pattern_buffer           php_mb_re_pattern_buffer
 #define regex_t                     php_mb_regex_t
+#define re_registers                php_mb_re_registers
 
 #endif /* _PHP_MBREGEX_COMPAT_H */
