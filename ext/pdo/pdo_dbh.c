07d91460c1f05d37f642ecd77a1858c943182ddf
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-02-22 11:37:53 +0000

    - Call dtor
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@180444 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/pdo_dbh.c
+++ ext/pdo/pdo_dbh.c
@@ -1089,7 +1089,7 @@ zend_object_value pdo_dbh_new(zend_class_entry *ce TSRMLS_DC)
 	ALLOC_HASHTABLE(dbh->properties);
 	zend_hash_init(dbh->properties, 0, NULL, ZVAL_PTR_DTOR, 0);
 	
-	retval.handle = zend_objects_store_put(dbh, NULL, pdo_dbh_free_storage, NULL TSRMLS_CC);
+	retval.handle = zend_objects_store_put(dbh, zend_objects_destroy_object, pdo_dbh_free_storage, NULL TSRMLS_CC);
 	if(ce == pdo_dbh_ce) {
 		retval.handlers = &pdo_dbh_object_handlers;
 	}
