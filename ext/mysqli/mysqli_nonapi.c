a3085602d0a430b285c2e3a03d85765139ba7d14
Ilia Alshanetsky <iliaa@php.net>
2005-02-24 23:58:59 +0000

    Fixed bug #32081 (default socket value is not being used).

--- ext/mysqli/mysqli_nonapi.c
+++ ext/mysqli/mysqli_nonapi.c
@@ -83,6 +83,10 @@ PHP_FUNCTION(mysqli_connect)
 	}
 #endif
 
+	if (!socket) {
+		socket = MyG(default_socket);
+	}
+
 	if (mysql_real_connect(mysql->mysql,hostname,username,passwd,dbname,port,socket,CLIENT_MULTI_RESULTS) == NULL) {
 		/* Save error messages */
 
