0e230a77d0bedde48d111e0cbe08a0419b004f67
Andrei Zmievski <andrei@php.net>
2005-03-01 18:19:48 +0000

    Allow = sign in long option argument. Fixes PEAR bug #1831.

--- pear/Console/Getopt.php
+++ pear/Console/Getopt.php
@@ -184,7 +184,7 @@ class Console_Getopt {
      */
     function _parseLongOption($arg, $long_options, &$opts, &$args)
     {
-        @list($opt, $opt_arg) = explode('=', $arg);
+        @list($opt, $opt_arg) = explode('=', $arg, 2);
         $opt_len = strlen($opt);
 
         for ($i = 0; $i < count($long_options); $i++) {
