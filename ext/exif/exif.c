096ed44b41d720521c6f05fbcd12caee80216ff4
sesser <sesser@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-03-02 18:21:45 +0000

    Fixed possible bufferoverflow
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@181202 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -2733,6 +2733,11 @@ static int exif_process_IFD_TAG(image_info_type *ImageInfo, char *dir_entry, cha
 
 	byte_count = components * php_tiff_bytes_per_format[format];
 
+	if ((ssize_t)byte_count < 0) {
+		exif_error_docref("exif_read_data#error_ifd" EXIFERR_CC, ImageInfo, E_WARNING, "Process tag(x%04X=%s): Illegal byte_count(%ld)", tag, exif_get_tagname(tag, tagname, -12, tag_table TSRMLS_CC), byte_count);
+		return FALSE;
+	}
+
 	if (byte_count > 4) {
 		offset_val = php_ifd_get32u(dir_entry+8, ImageInfo->motorola_intel);
 		/* If its bigger than 4 bytes, the dir entry contains an offset. */
