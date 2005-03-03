855224c8e6dbf0f6c6feb655c304e9aaa8486144
Johannes Schlüter <johannes@php.net>
2005-03-03 10:41:19 +0000

    - Remove <b>-Tag from error message

--- ext/standard/incomplete_class.c
+++ ext/standard/incomplete_class.c
@@ -26,7 +26,7 @@
 #define INCOMPLETE_CLASS_MSG \
 		"The script tried to execute a method or "  \
 		"access a property of an incomplete object. " \
-		"Please ensure that the class definition <b>%s</b> of the object " \
+		"Please ensure that the class definition %s of the object " \
 		"you are trying to operate on was loaded _before_ " \
 		"the session was started"
 
