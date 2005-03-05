5e33c04e953dfa43f33f977c8d020686b5bd4f0e
Moriyoshi Koizumi <moriyoshi@php.net>
2005-03-05 16:41:13 +0000

    - Fixed bug #32109 ($_POST is not populated in multithreaded environment).

--- main/SAPI.c
+++ main/SAPI.c
@@ -61,6 +61,7 @@ static void sapi_globals_ctor(sapi_globals_struct *sapi_globals TSRMLS_DC)
 {
 	memset(sapi_globals, 0, sizeof(*sapi_globals));
 	zend_hash_init_ex(&sapi_globals->known_post_content_types, 5, NULL, NULL, 1, 0);
+	php_setup_sapi_content_types(TSRMLS_C);
 }
 
 static void sapi_globals_dtor(sapi_globals_struct *sapi_globals TSRMLS_DC)
