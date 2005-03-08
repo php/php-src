93e13bacbce5c7e944a6598d2e1c340585c9ddbc
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-03-08 21:42:10 +0000

    This makes more sense. (Someone might actually use php_error_docref0() :)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@181662 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php.h
+++ main/php.h
@@ -299,13 +299,15 @@ PHPAPI void php_verror(const char *docref, const char *params, int type, const c
 #endif
 
 /* PHPAPI void php_error(int type, const char *format, ...); */
-PHPAPI void php_error_docref(const char *docref TSRMLS_DC, int type, const char *format, ...)
+PHPAPI void php_error_docref0(const char *docref TSRMLS_DC, int type, const char *format, ...)
 	PHP_ATTRIBUTE_FORMAT(printf, PHP_ATTR_FMT_OFFSET + 3, PHP_ATTR_FMT_OFFSET + 4);
 PHPAPI void php_error_docref1(const char *docref TSRMLS_DC, const char *param1, int type, const char *format, ...)
 	PHP_ATTRIBUTE_FORMAT(printf, PHP_ATTR_FMT_OFFSET + 4, PHP_ATTR_FMT_OFFSET + 5);
 PHPAPI void php_error_docref2(const char *docref TSRMLS_DC, const char *param1, const char *param2, int type, const char *format, ...)
 	PHP_ATTRIBUTE_FORMAT(printf, PHP_ATTR_FMT_OFFSET + 5, PHP_ATTR_FMT_OFFSET + 6);
 END_EXTERN_C()
+
+#define php_error_docref php_error_docref0
 
 #define zenderror phperror
 #define zendlex phplex
