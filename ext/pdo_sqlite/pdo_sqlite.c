584abff629a608ded6efe7a3e20ffd731204643f
Wez Furlong <wez@php.net>
2005-03-09 18:18:53 +0000

    update extension version

--- ext/pdo_sqlite/pdo_sqlite.c
+++ ext/pdo_sqlite/pdo_sqlite.c
@@ -31,7 +31,7 @@
 #include "php_pdo_sqlite_int.h"
 #include "zend_exceptions.h"
 
-#define PHP_PDO_SQLITE_MODULE_VERSION	"0.2.2"
+#define PHP_PDO_SQLITE_MODULE_VERSION	"0.3"
 
 /* {{{ pdo_sqlite_functions[] */
 function_entry pdo_sqlite_functions[] = {
