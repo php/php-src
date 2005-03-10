49afbfac4ab1e2bb6056d05bc30eb19bb9abf78f
Anantha Kesari H Y <hyanantha@php.net>
2005-03-10 11:41:00 +0000

    NetWare stat structure difference has been handled

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -219,7 +219,11 @@ static int ps_files_cleanup_dir(const char *dirname, int maxlifetime TSRMLS_DC)
 				buf[dirname_len + entry_len + 1] = '\0';
 				/* check whether its last access was more than maxlifet ago */
 				if (VCWD_STAT(buf, &sbuf) == 0 && 
+#ifdef NETWARE
+						(now - sbuf.st_mtime.tv_sec) > maxlifetime) {
+#else
 						(now - sbuf.st_mtime) > maxlifetime) {
+#endif
 					VCWD_UNLINK(buf);
 					nrdels++;
 				}
