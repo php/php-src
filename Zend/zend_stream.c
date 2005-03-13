dd64e6dedebb81b90e284c6f908b25d827a77277
Stanislav Malyshev <stas@php.net>
2005-03-13 17:48:45 +0000

    Do not convert ZEND_HANDLE_FP to ZEND_HANDLE_STREAM but allow using reader/closer on it

--- Zend/zend_stream.c
+++ Zend/zend_stream.c
@@ -79,11 +79,10 @@ ZEND_API int zend_stream_fixup(zend_file_handle *file_handle TSRMLS_DC)
 			return FAILURE;
 		}
 
-		/* promote to stream */
+		/* make compatible with stream */
 		file_handle->handle.stream.handle = file_handle->handle.fp;
 		file_handle->handle.stream.reader = zend_stream_stdio_reader;
 		file_handle->handle.stream.closer = zend_stream_stdio_closer;
-		file_handle->type = ZEND_HANDLE_STREAM;
 
 		file_handle->handle.stream.interactive = isatty(fileno((FILE *)file_handle->handle.stream.handle));
 	}
