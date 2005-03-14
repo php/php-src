b5e227d4c5413484a495faf8800445777b21f189
Anantha Kesari H Y <hyanantha@php.net>
2005-03-14 09:41:39 +0000

    PHP_OS and PHP_UNAME for NetWare are getting populated from the ph_config.h(configure.in)

--- main/php.h
+++ main/php.h
@@ -65,16 +65,6 @@
 #endif
 #endif
 
-#ifdef NETWARE
-/* For php_get_uname() function */
-#define PHP_UNAME  "NetWare"
-/*
- * This is obtained using uname(2) on Unix and assigned in the case of Windows;
- * we'll do it this way at least for now.
- */
-#define PHP_OS      PHP_UNAME
-#endif
-
 #include "php_regex.h"
 
 #if HAVE_ASSERT_H
