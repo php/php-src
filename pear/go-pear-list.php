ad5ec99900f52ecb129e52f18f5e1c2a6bda1177
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-03-14 23:57:07 +0000

    this needs updating
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@182135 c90b9560-bf6c-de11-be94-00142212c4b1

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -8,14 +8,14 @@
 $packages  = array(
 // required packages for the installer
 "PEAR"                  =>    "1.3.5",
-"XML_RPC"               =>    "1.1.0",
+"XML_RPC"               =>    "1.2.2",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.2",
+"Archive_Tar"           =>    "1.3.0",
 
 // required packages for the web frontend
 "PEAR_Frontend_Web"     =>    "0.4",
 "HTML_Template_IT"      =>    "1.1",
-"Net_UserAgent_Detect"  =>    "1.0",
+"Net_UserAgent_Detect"  =>    "2.0.1",
 );
 
 ?>
