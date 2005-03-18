4fc2cd36f6770a20d327c02e4bf3b6aced4464c5
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-03-18 22:11:53 +0000

    Revert bad idea. (no more error for invalid -d options)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@182400 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -395,7 +395,7 @@ static void php_cli_usage(char *argv0)
 }
 /* }}} */
 
-static int define_command_line_ini_entry(char *arg TSRMLS_DC)
+static void define_command_line_ini_entry(char *arg TSRMLS_DC)
 {
 	char *name, *value;
 
@@ -413,9 +413,8 @@ static int define_command_line_ini_entry(char *arg TSRMLS_DC)
 		ZVAL_STRING(&extension, value, 0);
 		php_dl(&extension, MODULE_PERSISTENT, &zval TSRMLS_CC);
 	} else {
-		return zend_alter_ini_entry(name, strlen(name)+1, value, strlen(value), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
+		zend_alter_ini_entry(name, strlen(name)+1, value, strlen(value), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
 	}
-	return SUCCESS;
 }
 
 
@@ -662,10 +661,7 @@ int main(int argc, char *argv[])
 			switch (c) {
 
 			case 'd': /* define ini entries on command line */
-				if (define_command_line_ini_entry(php_optarg TSRMLS_CC) == FAILURE) {
-					zend_printf("Invalid php.ini entry '%s'.\n", php_optarg);
-					goto err;
-				}
+				define_command_line_ini_entry(php_optarg TSRMLS_CC);
 				break;
 
 			case 'h': /* help & quit */
