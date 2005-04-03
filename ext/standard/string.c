ca10457a581ea760bfb8307489013f44bf8118ff
Ilia Alshanetsky <iliaa@php.net>
2005-04-03 18:08:40 +0000

    Fixed bug #32530 (chunk_split() does not append endstr if chunklen is longer then the original string).

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -1887,7 +1887,13 @@ PHP_FUNCTION(chunk_split)
 	}
 
 	if (chunklen > Z_STRLEN_PP(p_str)) {
-		RETURN_STRINGL(Z_STRVAL_PP(p_str), Z_STRLEN_PP(p_str), 1);	
+		/* to maintain BC, we must return original string + ending */
+		result_len = endlen + Z_STRLEN_PP(p_str);
+		result = emalloc(result_len + 1);
+		memcpy(result, Z_STRVAL_PP(p_str), Z_STRLEN_PP(p_str));
+		memcpy(result + Z_STRLEN_PP(p_str), end, endlen);
+		result[result_len] = '\0'; 
+		RETURN_STRINGL(result, result_len, 0);	
 	}
 
 	if (!Z_STRLEN_PP(p_str)) {
