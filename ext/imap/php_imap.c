8fc219f0e3f97ab3e58102cf7a1b78a734d67327
Ilia Alshanetsky <iliaa@php.net>
2005-04-08 15:04:44 +0000

    MFH: Missing part of the patch for bug #32589

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -3046,7 +3046,7 @@ PHP_FUNCTION(imap_mail_compose)
 				convert_to_string_ex(pvalue);
 				tmp_param = mail_newbody_parameter();
 				tmp_param->value = (char *) fs_get(Z_STRLEN_PP(pvalue) + 1);
-				memcpy(disp_param->value, Z_STRVAL_PP(pvalue), Z_STRLEN_PP(pvalue) + 1);
+				memcpy(tmp_param->value, Z_STRVAL_PP(pvalue), Z_STRLEN_PP(pvalue) + 1);
 				tmp_param->attribute = "CHARSET";
 				tmp_param->next = bod->parameter;
 				bod->parameter = tmp_param;
