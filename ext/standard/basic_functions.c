1ee8138aa4a52e764ef28799d672b26af7688104
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-04-10 16:25:11 +0000

    Added time_sleep_until() function, which is a high precision mechanism of making a script sleep until specified timestamp.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@184071 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -167,6 +167,7 @@ function_entry basic_functions[] = {
 	PHP_FE(usleep,															NULL)
 #if HAVE_NANOSLEEP
 	PHP_FE(time_nanosleep,														NULL)
+	PHP_FE(time_sleep_until,														NULL)
 #endif
 	PHP_FE(time,															NULL)
 	PHP_FE(mktime,															NULL)
@@ -1788,6 +1789,48 @@ PHP_FUNCTION(time_nanosleep)
 	}
 
 	RETURN_FALSE;
+}
+/* }}} */
+
+/* {{{ proto mixed time_sleep_until(float timestamp)
+   Make the script sleep until the specified time */
+PHP_FUNCTION(time_sleep_until)
+{
+	double d_ts, c_ts;
+	struct timeval tm;
+	struct timespec php_req, php_rem;
+	
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "d", &d_ts)) {
+		WRONG_PARAM_COUNT;
+	}
+
+	if (gettimeofday((struct timeval *) &tm, NULL) != 0) {
+		RETURN_FALSE;
+	}
+
+	c_ts = (double)(d_ts - tm.tv_sec - tm.tv_usec / 1000000.00);
+	if (c_ts < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Sleep until to time is less then current time.");
+		RETURN_FALSE;
+	}
+
+	php_req.tv_sec = (time_t) c_ts;
+	if (php_req.tv_sec > c_ts) { /* rounding up occurred */
+		php_req.tv_sec--;
+	}
+	/* 1sec = 1000000000 nanoseconds */
+	php_req.tv_nsec = (long) ((c_ts - php_req.tv_sec) * 1000000000.00);
+
+	while (nanosleep(&php_req, &php_rem)) {
+		if (errno == EINTR) {
+			php_req.tv_sec = php_rem.tv_sec;
+			php_req.tv_nsec = php_rem.tv_nsec;
+		} else {
+			RETURN_FALSE;
+		}
+	}
+
+	RETURN_TRUE;
 }
 /* }}} */
 #endif
