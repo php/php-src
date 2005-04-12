abacba58eb1349a1bcd91952e4a77880aac5a4d5
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-04-12 01:00:48 +0000

    Fixed compiler warning.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@184188 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -1483,7 +1483,7 @@ static xmlNodePtr to_xml_object(encodeTypePtr type, zval *data, int style, xmlNo
 								   an implicit schema. Otherwise, use form.
 								*/
 								if (!strncmp((*attr)->namens, XML_NAMESPACE, sizeof(XML_NAMESPACE)) || 
-								    (*attr)->form == XSD_FORM_QUALIFIED && (*attr)->namens) {
+								    ((*attr)->form == XSD_FORM_QUALIFIED) && (*attr)->namens) {
 									xmlNsPtr nsp = encode_add_ns(xmlParam, (*attr)->namens);
 
 									xmlSetNsProp(xmlParam, nsp, (*attr)->name, dummy->children->content);
