a6e2a2e8090a601f504548330c067ad185decba8
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-04-14 16:05:05 +0000

    - don't use short tag - detect CLI binary and don't print &lt ;br &lt; in this case
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@184351 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/skeleton/skeleton.php
+++ ext/skeleton/skeleton.php
@@ -1,14 +1,16 @@
-<?
+<?php
+$br = (php_sapi_name() == "cli")? "":"<br>";
+
 if(!extension_loaded('extname')) {
 	dl('extname.' . PHP_SHLIB_SUFFIX);
 }
 $module = 'extname';
 $functions = get_extension_funcs($module);
-echo "Functions available in the test extension:<br>\n";
+echo "Functions available in the test extension:$br\n";
 foreach($functions as $func) {
-    echo $func."<br>\n";
+    echo $func."$br\n";
 }
-echo "<br>\n";
+echo "$br\n";
 $function = 'confirm_' . $module . '_compiled';
 if (extension_loaded($module)) {
 	$str = $function($module);
