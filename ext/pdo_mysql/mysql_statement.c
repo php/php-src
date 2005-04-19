569138e0c0edec637e87d69988482bbb9816c01c
Ilia Alshanetsky <iliaa@php.net>
2005-04-19 20:53:54 +0000

    removed unused var.

--- ext/pdo_mysql/mysql_statement.c
+++ ext/pdo_mysql/mysql_statement.c
@@ -116,7 +116,6 @@ static int pdo_mysql_stmt_describe(pdo_stmt_t *stmt, int colno TSRMLS_DC)
 {
 	pdo_mysql_stmt *S = (pdo_mysql_stmt*)stmt->driver_data;
 	MYSQL_RES *R = S->result;
-	MYSQL_FIELD *F;
 	struct pdo_column_data *cols = stmt->columns;
 	unsigned int i;
 
