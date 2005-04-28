8c2cda12517a9b0a728628f3e0eaea2fb41fda8a
foobar <sniper@php.net>
2005-04-28 14:23:57 +0000

    int != long

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -482,7 +482,7 @@ static void sapi_cgi_register_variables(zval *track_vars_array TSRMLS_DC)
 static void sapi_cgi_log_message(char *message)
 {
 #if PHP_FASTCGI
-	int logging = 1;
+	long logging = 1;
 	TSRMLS_FETCH();
 
 	if (cfg_get_long("fastcgi.logging", &logging) == FAILURE) {
@@ -979,7 +979,7 @@ int main(int argc, char *argv[])
 	int fcgi_fd = 0;
 	FCGX_Request request;
 #ifdef PHP_WIN32
-	int impersonate = 0;
+	long impersonate = 0;
 #else
     int status = 0;
 #endif
