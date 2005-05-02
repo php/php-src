4656176df94a0939714bbc0a2651f5a2601d714d
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-05-02 12:12:04 +0000

    add also math_variance() which uses the same calculation as math_std_dev()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@185635 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_math.h
+++ ext/standard/php_math.h
@@ -60,6 +60,7 @@ PHP_FUNCTION(base_convert);
 PHP_FUNCTION(number_format);
 PHP_FUNCTION(fmod);
 PHP_FUNCTION(math_std_dev);
+PHP_FUNCTION(math_variance);
 PHP_FUNCTION(deg2rad);
 PHP_FUNCTION(rad2deg);
 
