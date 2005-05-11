be3a9ccdf325ad6247f2c90bd39005cd4050d20b
Andrey Hristov <andrey@php.net>
2005-05-11 11:43:11 +0000

    add function array_product()

--- ext/standard/php_array.h
+++ ext/standard/php_array.h
@@ -92,6 +92,7 @@ PHP_FUNCTION(array_udiff_assoc);
 PHP_FUNCTION(array_diff_uassoc);
 PHP_FUNCTION(array_udiff_uassoc);
 PHP_FUNCTION(array_sum);
+PHP_FUNCTION(array_product);
 PHP_FUNCTION(array_filter);
 PHP_FUNCTION(array_map);
 PHP_FUNCTION(array_key_exists);
