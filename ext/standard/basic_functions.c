bd0ef4e6b854dd8df4142c87a2785069ddf61782
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-05-13 10:11:19 +0000

    move math_standard_deviation and math_variance to the stats PECL extension
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@186318 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -415,8 +415,6 @@ function_entry basic_functions[] = {
 	PHP_FE(base_convert,													NULL)
 	PHP_FE(number_format,													NULL)
 	PHP_FE(fmod,															NULL)
-	PHP_FE(math_standard_deviation,													NULL)
-	PHP_FE(math_variance,													NULL)
 #ifdef HAVE_INET_NTOP
 	PHP_NAMED_FE(inet_ntop,		php_inet_ntop,											NULL)
 #endif
