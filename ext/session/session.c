26d7b7fbc07ab980dae1bf0d6cf8db319187af88
foobar <sniper@php.net>
2005-05-23 06:46:25 +0000

    CS fix

--- ext/session/session.c
+++ ext/session/session.c
@@ -131,9 +131,10 @@ static PHP_INI_MH(OnUpdateSerializer)
 	return SUCCESS;
 }
 
-static PHP_INI_MH(OnUpdateSaveDir) {
+static PHP_INI_MH(OnUpdateSaveDir)
+{
 	/* Only do the safemode/open_basedir check at runtime */
-	if(stage == PHP_INI_STAGE_RUNTIME) {
+	if (stage == PHP_INI_STAGE_RUNTIME) {
 		if (PG(safe_mode) && (!php_checkuid(new_value, NULL, CHECKUID_ALLOW_ONLY_DIR))) {
 			return FAILURE;
 		}
