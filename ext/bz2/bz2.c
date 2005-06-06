cd9bc43505fec85b54cebde40900506cdacc758c
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-06-06 16:30:46 +0000

    Better storage size for output length. # Thanks to Stefan for noticing the problem.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@187773 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/bz2/bz2.c
+++ ext/bz2/bz2.c
@@ -441,7 +441,7 @@ PHP_FUNCTION(bzdecompress)
 	char *source, *dest;
 	int source_len, error;
 	long small = 0;
-	unsigned int size = 0;
+	unsigned long long size = 0;
 	bz_stream bzs;
 
 	if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|l", &source, &source_len, &small)) {
