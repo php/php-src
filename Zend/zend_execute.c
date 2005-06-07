df96b038aa7627307ad8305e92ec262f04b6da3d
Dmitry Stogov <dmitry@php.net>
2005-06-07 10:29:42 +0000

    fixed memory leak in bug #28972 ([] operator overflow treatment is incorrect), not the bug itself.

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -934,7 +934,7 @@ static void zend_fetch_dimension_address(znode *result, znode *op1, znode *op2, 
 				new_zval->refcount++;
 				if (zend_hash_next_index_insert(container->value.ht, &new_zval, sizeof(zval *), (void **) retval) == FAILURE) {
 					zend_error(E_WARNING, "Cannot add element to the array as the next element is already occupied");
-					*retval = &EG(uninitialized_zval_ptr);
+					*retval = &EG(error_zval_ptr);
 					new_zval->refcount--; 
 				}
 			} else {
