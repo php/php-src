94c2e006dde0186fae48adfec673fef516574348
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-06-10 06:11:29 +0000

    "Fix" PECL #4411 SPL is not initialized before PDO, so inheriting from SPL's runtime exception class gives PDO a broken exception class. We don't currently have a way to express this kind of "soft" dependency in the build system, so I'm disabling inheriting from the runtime exception for now.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@188055 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/pdo.c
+++ ext/pdo/pdo.c
@@ -352,7 +352,7 @@ PHP_MINIT_FUNCTION(pdo)
 #endif
 
 	INIT_CLASS_ENTRY(ce, "PDOException", NULL);
-#if defined(HAVE_SPL) && ((PHP_MAJOR_VERSION > 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 1))
+#if can_handle_soft_dependency_on_SPL && defined(HAVE_SPL) && ((PHP_MAJOR_VERSION > 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 1))
 	pdo_exception_ce = zend_register_internal_class_ex(&ce, spl_ce_RuntimeException, NULL TSRMLS_CC);
 #else
 	pdo_exception_ce = zend_register_internal_class_ex(&ce, zend_exception_get_default(), NULL TSRMLS_CC);
