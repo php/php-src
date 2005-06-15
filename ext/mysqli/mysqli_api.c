caa994f64a07ca858e98dc1becc8148ce2ce89b2
Georg Richter <georg@php.net>
2005-06-15 14:04:23 +0000

    fixed parameter parse string for mysqli_stmt_attr_set

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -1779,7 +1779,7 @@ PHP_FUNCTION(mysqli_stmt_attr_set)
 	ulong	attr;
 	int		rc;
 
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Olb", &mysql_stmt, mysqli_stmt_class_entry, &attr, &mode) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Oll", &mysql_stmt, mysqli_stmt_class_entry, &attr, &mode) == FAILURE) {
 		return;
 	}
 	MYSQLI_FETCH_RESOURCE(stmt, MY_STMT *, &mysql_stmt, "mysqli_stmt"); 
