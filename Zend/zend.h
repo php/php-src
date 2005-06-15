9a11144096e4a520ca0d7efadfdc0cb4cc9c09e7
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-06-15 19:05:55 +0000

    Fixed OS X compatibility
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@188485 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend.h
+++ Zend/zend.h
@@ -250,7 +250,7 @@ char *alloca ();
 #define INTERNAL_FUNCTION_PARAMETERS int ht, zval *return_value, zval *this_ptr, int return_value_used TSRMLS_DC
 #define INTERNAL_FUNCTION_PARAM_PASSTHRU ht, return_value, this_ptr, return_value_used TSRMLS_CC
 
-#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER) && !defined(DARWIN)
 #  define ZEND_VM_ALWAYS_INLINE  __attribute__ ((always_inline))
 void zend_error_noreturn(int type, const char *format, ...) __attribute__ ((noreturn));
 #else
