56c1b316da080f85f55b98b2d63d93872d883adb
foobar <sniper@php.net>
2005-06-20 13:37:32 +0000

    - Added session.hash_bits_per_character support. (3rd param)   (Changes by: waltzer at autumnweave dot com)

--- ext/session/mod_files.sh
+++ ext/session/mod_files.sh
@@ -9,8 +9,16 @@ if test "$2" = "0"; then
 	exit 0
 fi
 
-for i in a b c d e f 0 1 2 3 4 5 6 7 8 9; do
+hash_chars="0 1 2 3 4 5 6 7 8 9 a b c d e f"
+if test "$3" -a "$3" -ge "5"; then
+  hash_chars="$hash_chars g h i j k l m n o p q r s t u v"
+  if test "$3" -eq "6"; then
+    hash_chars="$hash_chars w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z - ,"
+  fi
+fi
+
+for i in $hash_chars; do
 	newpath="$1/$i"
 	mkdir $newpath || exit 1
-	sh $0 $newpath `expr $2 - 1`
+	sh $0 $newpath `expr $2 - 1 $3`
 done
