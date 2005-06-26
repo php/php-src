75f7db1c12e8f411e145bdbf8b65f4c64c657dcc
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-06-26 17:31:07 +0000

    Fixed bug #33475 (cURL handle is not closed on curl_close().
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@189286 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -1291,12 +1291,13 @@ PHP_FUNCTION(curl_exec)
 	ch->uses++;
 
 	if (ch->handlers->write->method == PHP_CURL_RETURN && ch->handlers->write->buf.len > 0) {
+		--ch->uses;
 		if (ch->handlers->write->type != PHP_CURL_BINARY)  {
 			smart_str_0(&ch->handlers->write->buf);
 		}
 		RETURN_STRINGL(ch->handlers->write->buf.c, ch->handlers->write->buf.len, 0);
 	}
-
+	--ch->uses;
 	RETURN_TRUE;
 }
 /* }}} */
