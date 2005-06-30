15eea0992f4fda01db248098490245fe98dd317c
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-06-30 22:44:28 +0000

    Fixed memory leak on error inside php_date().
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@189653 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -249,15 +249,16 @@ static void php_date(INTERNAL_FUNCTION_PARAMETERS, int localtime)
 		RETURN_FALSE;
 	}
 
-    t = timelib_time_ctor();
+	t = timelib_time_ctor();
 
 	if (localtime) {
 		tzi = timelib_parse_tzfile(guess_timezone());
 		if (! tzi) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot find any timezone setting");
+			timelib_time_dtor(t);
 			RETURN_FALSE;
 		}
-    	timelib_unixtime2local(t, ts, tzi);
+		timelib_unixtime2local(t, ts, tzi);
 	} else {
 		tzi = NULL;
 		timelib_unixtime2gmt(t, ts);
