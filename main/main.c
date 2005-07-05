9a54436bce84f558dae09966bd1144a626415360
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-05 14:13:46 +0000

    Fixed crash in ext\standard\tests\general_functions\bug32647.php on Windows
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@189939 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -1195,6 +1195,7 @@ void php_request_shutdown(void *dummy)
 	 * inside zend_executor callback functions.
 	 */
 	EG(opline_ptr) = NULL;
+	EG(active_op_array) = NULL;
 
 	/* 1. Call all possible __destruct() functions */
 	zend_try {
