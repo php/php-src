e258d76e86e6aabe7e754a971317d1ee9cdde231
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-08 09:36:28 +0000

    Fixed HTTP basic authentication headers during subrequsts to xsd files
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@190240 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/php_http.c
+++ ext/soap/php_http.c
@@ -55,7 +55,7 @@ static int stream_alive(php_stream *stream  TSRMLS_DC)
 }
 
 /* Proxy HTTP Authentication */
-static void proxy_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_DC)
+void proxy_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_DC)
 {
 	zval **login, **password;
 
@@ -72,6 +72,32 @@ static void proxy_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_
 		smart_str_0(&auth);
 		buf = php_base64_encode(auth.c, auth.len, &len);
 		smart_str_append_const(soap_headers, "Proxy-Authorization: Basic ");
+		smart_str_appendl(soap_headers, buf, len);
+		smart_str_append_const(soap_headers, "\r\n");
+		efree(buf);
+		smart_str_free(&auth);
+	}
+}
+
+/* HTTP Authentication */
+void basic_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_DC)
+{
+	zval **login, **password;
+
+	if (zend_hash_find(Z_OBJPROP_P(this_ptr), "_login", sizeof("_login"), (void **)&login) == SUCCESS &&
+			!zend_hash_exists(Z_OBJPROP_P(this_ptr), "_digest", sizeof("_digest"))) {
+		char* buf;
+		int len;
+		smart_str auth = {0};
+
+		smart_str_appendl(&auth, Z_STRVAL_PP(login), Z_STRLEN_PP(login));
+		smart_str_appendc(&auth, ':');
+		if (zend_hash_find(Z_OBJPROP_P(this_ptr), "_password", sizeof("_password"), (void **)&password) == SUCCESS) {
+			smart_str_appendl(&auth, Z_STRVAL_PP(password), Z_STRLEN_PP(password));
+		}
+		smart_str_0(&auth);
+		buf = php_base64_encode(auth.c, auth.len, &len);
+		smart_str_append_const(soap_headers, "Authorization: Basic ");
 		smart_str_appendl(soap_headers, buf, len);
 		smart_str_append_const(soap_headers, "\r\n");
 		efree(buf);
--- ext/soap/php_http.h
+++ ext/soap/php_http.h
@@ -31,4 +31,6 @@ int make_http_soap_request(zval  *this_ptr,
                            char **response, 
                            int   *response_len TSRMLS_DC);
 
+void proxy_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_DC);
+void basic_authentication(zval* this_ptr, smart_str* soap_headers TSRMLS_DC);
 #endif
