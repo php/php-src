bd3d6ec7f73d394809cd02cd984247c577273aa7
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-12 16:53:30 +0000

    Fixed double-free in the digest authentication handling.
    # Found and Reported by Stefan Esser
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@190588 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/SAPI.c
+++ main/SAPI.c
@@ -455,7 +455,7 @@ SAPI_API void sapi_initialize_empty_request(TSRMLS_D)
 {
 	SG(server_context) = NULL;
 	SG(request_info).request_method = NULL;
-	SG(request_info).auth_user = SG(request_info).auth_password = NULL;
+	SG(request_info).auth_digest = SG(request_info).auth_user = SG(request_info).auth_password = NULL;
 	SG(request_info).content_type_dup = NULL;
 }
 
