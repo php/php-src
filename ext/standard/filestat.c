55518f5360986573ffb5a000b342924664d3b3bc
Anantha Kesari H Y <hyanantha@php.net>
2005-07-15 09:29:19 +0000

    ext/standard/file.h NetWare LibC has fnmatch and realpath implemented. ext/standard/filestat.c Removing redundant CLIB_STAT_PATCH and fixing some NetWare specific stat structure. ext/standard/link.c Removing redundant CLIB_STAT_PATCH. ext/standard/pageinfo.c Removing redundant CLIB_STAT_PATCH and fixing some NetWare specific stat structure. ext/standard/syslog.c Except LOG_SYSLOG NetWare LibC has other macros defined.
    -- Kamesh

--- ext/standard/filestat.c
+++ ext/standard/filestat.c
@@ -464,11 +464,7 @@ PHP_FUNCTION(touch)
 {
 	pval **filename, **filetime, **fileatime;
 	int ret;
-#if defined(NETWARE) && defined(CLIB_STAT_PATCH)
-	struct stat_libc sb;
-#else
 	struct stat sb;
-#endif
 	FILE *file;
 	struct utimbuf newtimebuf;
 	struct utimbuf *newtime = NULL;
@@ -550,11 +546,7 @@ PHPAPI void php_stat(const char *filename, php_stat_len filename_length, int typ
 {
 	zval *stat_dev, *stat_ino, *stat_mode, *stat_nlink, *stat_uid, *stat_gid, *stat_rdev,
 	 	*stat_size, *stat_atime, *stat_mtime, *stat_ctime, *stat_blksize, *stat_blocks;
-#if defined(NETWARE) && defined(CLIB_STAT_PATCH)
-	struct stat_libc *stat_sb;
-#else
 	struct stat *stat_sb;
-#endif
 	php_stream_statbuf ssb;
 	int flags = 0, rmask=S_IROTH, wmask=S_IWOTH, xmask=S_IXOTH; /* access rights defaults to other */
 	char *stat_sb_names[13]={"dev", "ino", "mode", "nlink", "uid", "gid", "rdev",
@@ -644,19 +636,19 @@ PHPAPI void php_stat(const char *filename, php_stat_len filename_length, int typ
 		RETURN_LONG((long)ssb.sb.st_gid);
 	case FS_ATIME:
 #ifdef NETWARE
-		RETURN_LONG((long)((stat_sb->st_atime).tv_sec));
+		RETURN_LONG((long)ssb.sb.st_atime.tv_sec);
 #else
 		RETURN_LONG((long)ssb.sb.st_atime);
 #endif
 	case FS_MTIME:
 #ifdef NETWARE
-		RETURN_LONG((long)((stat_sb->st_mtime).tv_sec));
+		RETURN_LONG((long)ssb.sb.st_mtime.tv_sec);
 #else
 		RETURN_LONG((long)ssb.sb.st_mtime);
 #endif
 	case FS_CTIME:
 #ifdef NETWARE
-		RETURN_LONG((long)((stat_sb->st_ctime).tv_sec));
+		RETURN_LONG((long)ssb.sb.st_ctime.tv_sec);
 #else
 		RETURN_LONG((long)ssb.sb.st_ctime);
 #endif
