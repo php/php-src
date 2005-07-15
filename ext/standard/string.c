947f56a4ed2b3f2d4e0ba635da22777dbffaf78b
hyanantha <hyanantha@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-15 12:35:12 +0000

    ext/standard/file.h NetWare LibC has fnmatch and realpath implemented. ext/standard/syslog.c Except LOG_SYSLOG other syslog macros are supported by NetWare LibC. ext/standard/string.c NetWare fix for dirname -- Kamesh
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@190743 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -1225,6 +1225,22 @@ PHPAPI size_t php_dirname(char *path, size_t len)
 			return len;
 		}
 	}
+#elif defined(NETWARE)
+	/*
+	 * Find the first occurence of : from the left 
+	 * move the path pointer to the position just after :
+	 * increment the len_adjust to the length of path till colon character(inclusive)
+	 * If there is no character beyond : simple return len
+	 */
+	char *colonpos = NULL;
+	colonpos = strchr(path, ':');
+	if(colonpos != NULL) {
+		len_adjust = ((colonpos - path) + 1);
+		path += len_adjust;
+		if(len_adjust == len) {
+		return len;
+		}
+    	}
 #endif
 
 	if (len == 0) {
@@ -1249,9 +1265,21 @@ PHPAPI size_t php_dirname(char *path, size_t len)
 	}
 	if (end < path) {
 		/* No slash found, therefore return '.' */
+#ifdef NETWARE
+		if(len_adjust == 0) {
+			path[0] = '.';
+			path[1] = '\0';
+			return 1; //only one character
+		} 
+		else {
+			path[0] = '\0';
+			return len_adjust;
+		}
+#else
 		path[0] = '.';
 		path[1] = '\0';
 		return 1 + len_adjust;
+#endif
 	}
 
 	/* Strip slashes which came before the file name */
