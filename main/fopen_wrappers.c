d5ba050eee3bb2067f52fa1accb2e176082ac28a
hyanantha <hyanantha@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-16 12:21:34 +0000

    main/fopen_wrappers.c NetWare file names are case insensitive main/main.c NetWare has no sendmail binary. It uses the smart host mailing code avaiolable in php distro. Could not find a better place to put this than main/main.c. -- Kamesh
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@190793 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -123,7 +123,7 @@ PHPAPI int php_check_specific_open_basedir(const char *basedir, const char *path
 		}
 
 		/* Check the path */
-#ifdef PHP_WIN32
+#if defined(PHP_WIN32) || defined(NETWARE)
 		if (strncasecmp(resolved_basedir, resolved_name, resolved_basedir_len) == 0) {
 #else
 		if (strncmp(resolved_basedir, resolved_name, resolved_basedir_len) == 0) {
