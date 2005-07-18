b7848511a2434c76919c53d998b03844a1a04a92
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-18 23:17:24 +0000

    MFB4_4: Fix compile problem with --regex=system on some systems
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@190956 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ereg/ereg.c
+++ ext/ereg/ereg.c
@@ -46,6 +46,7 @@ static int _php_regcomp(regex_t *preg, const char *pattern, int cflags)
 	
 	if(zend_hash_find(&REG(ht_rc), (char *) pattern, patlen+1, (void **) &rc) == SUCCESS
 	   && rc->cflags == cflags) {
+#ifdef HAVE_REGEX_T_RE_MAGIC
 		/*
 		 * We use a saved magic number to see whether cache is corrupted, and if it
 		 * is, we flush it and compile the pattern from scratch.
@@ -73,7 +74,20 @@ static int _php_regcomp(regex_t *preg, const char *pattern, int cflags)
 		zend_hash_update(&REG(ht_rc), (char *) pattern, patlen+1,
 						 (void *) &rcp, sizeof(rcp), NULL);
 	}
-	
+#else
+		memcpy(preg, &rc->preg, sizeof(*preg));
+	} else {
+		r = regcomp(preg, pattern, cflags);
+		if(!r) {
+			reg_cache rcp;
+
+			rcp.cflags = cflags;
+			memcpy(&rcp.preg, preg, sizeof(*preg));
+			zend_hash_update(&REG(ht_rc), (char *) pattern, patlen+1,
+							 (void *) &rcp, sizeof(rcp), NULL);
+		}
+	}
+#endif
 	return r;
 }
 /* }}} */
