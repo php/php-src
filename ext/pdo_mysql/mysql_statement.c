393bea4e70f88f9e42ad470fa12ab9d8bbcb3c59
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-07-20 01:59:14 +0000

    this hack can and does segv; let's remove it.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@191043 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_mysql/mysql_statement.c
+++ ext/pdo_mysql/mysql_statement.c
@@ -96,20 +96,7 @@ static int pdo_mysql_stmt_execute(pdo_stmt_t *stmt TSRMLS_DC)
 			my_bool on = 1;
 			/* if we have bound the buffers don't set the attribute again */
 			if (!S->result) {
-				/* FIXME: using directly internal structs - but for now cleaner
-				 * then calling 2 times result_metadata. */
-				for (i = 0; i < S->stmt->field_count; i++) {
-					switch (S->fields[i].type) {
-						case MYSQL_TYPE_MEDIUM_BLOB:
-						case MYSQL_TYPE_LONG_BLOB:
-						case MYSQL_TYPE_BLOB:
-							mysql_stmt_attr_set(S->stmt, STMT_ATTR_UPDATE_MAX_LENGTH, &on);
-							i= stmt->column_count;
-							break;
-						default:
-							break;
-					}
-				}
+				mysql_stmt_attr_set(S->stmt, STMT_ATTR_UPDATE_MAX_LENGTH, &on);
 			}
 			mysql_stmt_store_result(S->stmt);
 		}
