c35454440cb2a150129dd90a8383924044d343fa
Rasmus Lerdorf <rasmus@php.net>
2005-07-25 22:37:35 +0000

    I could have sworn I committed this a while ago.  Just a tiny NULL safety check here.

--- main/php_variables.c
+++ main/php_variables.c
@@ -236,7 +236,7 @@ SAPI_API SAPI_POST_HANDLER_FUNC(php_std_post_handler)
 SAPI_API SAPI_INPUT_FILTER_FUNC(php_default_input_filter)
 {
 	/* TODO: check .ini setting here and apply user-defined input filter */
-	*new_val_len = val_len;
+	if(new_val_len) *new_val_len = val_len;
 	return 1;
 }
 
