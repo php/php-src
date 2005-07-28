5e2f93d2dd90ec1435c028c926e414b150718bb5
Anantha Kesari H Y <hyanantha@php.net>
2005-07-28 08:53:30 +0000

    Cleaning up the NetWare code sitting in win32/sendmail.c. Removing netware/sysexits.h --Kamesh

--- netware/sendmail_nw.h
+++ netware/sendmail_nw.h
@@ -1,12 +1,14 @@
 
+#define closesocket close
+#define LPCSTR char *
+#define LPSTR char*
+#define FAR
 #ifdef USE_WINSOCK
-/*#include <ws2nlm.h>*/
 #include <novsock2.h>
 #else
 #include <sys/socket.h>	/* For struct sockaddr, 'PF_INET' and 'AF_INET' */
 #include <netinet/in.h>	/* For struct sockaddr_in */
 #include <netdb.h>		/* For struct hostent */
-/*#include <ws2name.h>*/
 #endif	/* USE_WINSOCK */
 
 typedef int SOCKET;	/* Borrowed from winsock\novsock2.h */
