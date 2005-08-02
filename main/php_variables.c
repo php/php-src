2cf6322c5ef0b718fb8aa97a173e3d7453c74f47
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-08-02 17:01:05 +0000

    Fixed bug #33958 (duplicate cookies and magic_quotes=off may cause a crash)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@192126 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_variables.c
+++ main/php_variables.c
@@ -198,7 +198,9 @@ plain_var:
 				 */
 				if (PG(http_globals)[TRACK_VARS_COOKIE] && symtable1 == Z_ARRVAL_P(PG(http_globals)[TRACK_VARS_COOKIE]) && 
 					zend_symtable_find(symtable1, escaped_index, index_len+1, (void **) &tmp) != FAILURE) {
-					efree(escaped_index);
+					if (PG(magic_quotes_gpc)) { 
+						efree(escaped_index);
+					}
 					break;
 				}
 				zend_symtable_update(symtable1, escaped_index, index_len + 1, &gpc_element, sizeof(zval *), (void **) &gpc_element_p);
