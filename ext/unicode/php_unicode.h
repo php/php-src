d240f6965e36137a11f4f0378016567480e21b2a
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-08-12 09:10:04 +0000

    - Some minor fixes
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@193168 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/unicode/php_unicode.h
+++ ext/unicode/php_unicode.h
@@ -1,5 +1,7 @@
 /*
    +----------------------------------------------------------------------+
+   | PHP Version 6                                                        |
+   +----------------------------------------------------------------------+
    | This source file is subject to version 3.0 of the PHP license,       |
    | that is bundled with this package in the file LICENSE, and is        |
    | available through the world-wide-web at the following url:           |
@@ -12,7 +14,7 @@
    +----------------------------------------------------------------------+
 */
 
-/* $ Id: $ */ 
+/* $Id$ */ 
 
 #ifndef PHP_UNICODE_H
 #define PHP_UNICODE_H
