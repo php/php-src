8179235e5eb53e26f3fe97ecedd7d1aa12c2cb14
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-08-17 11:57:10 +0000

    Fixed bug #32981 (ReflectionMethod::getStaticVariables() causes apache2.0.54 seg fault)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@193743 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_reflection_api.c
+++ Zend/zend_reflection_api.c
@@ -1399,6 +1399,7 @@ ZEND_METHOD(reflection_function, getStaticVariables)
 	/* Return an empty array in case no static variables exist */
 	array_init(return_value);
 	if (fptr->type == ZEND_USER_FUNCTION && fptr->op_array.static_variables != NULL) {
+		zend_hash_apply_with_argument(fptr->op_array.static_variables, (apply_func_arg_t) zval_update_constant, (void*)1 TSRMLS_CC);
 		zend_hash_copy(Z_ARRVAL_P(return_value), fptr->op_array.static_variables, (copy_ctor_func_t) zval_add_ref, (void *) &tmp_copy, sizeof(zval *));
 	}
 }
