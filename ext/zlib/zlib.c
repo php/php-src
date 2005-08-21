2e60ddf4860f55370c1bd326e9d473a90c9b3196
Ilia Alshanetsky <iliaa@php.net>
2005-08-21 16:05:35 +0000

    MFH: Fixed bug #34191 (ob_gzhandler does not enforce trailing \0).

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -717,6 +717,7 @@ static int php_do_deflate(uint str_length, Bytef **p_buffer, uint *p_buffer_len,
 
 	if (do_end) {
 		err = deflate(&ZLIBG(stream), Z_FINISH);
+		buffer[outlen - ZLIBG(stream).avail_out] = '\0';
 	}
 
 
