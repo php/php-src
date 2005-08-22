a98e4ef9e8cac41f7564603501933919126df46f
Greg Beaver <cellog@php.net>
2005-08-22 16:12:17 +0000

    argh, Archive_Tar is version 1.3.1, not 1.3.2

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -7,10 +7,10 @@
  */
 $packages  = array(
 // required packages for the installer
-"PEAR"                  =>    "1.3.5",
-"XML_RPC"               =>    "1.3.0",
+"PEAR"                  =>    "1.3.6",
+"XML_RPC"               =>    "1.4.0",
 "Console_Getopt"        =>    "1.2",
-"Archive_Tar"           =>    "1.3.0",
+"Archive_Tar"           =>    "1.3.1",
 
 // required packages for the web frontend
 "PEAR_Frontend_Web"     =>    "0.4",
