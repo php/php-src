11f4c537e6e48b67e71936be7aa6c29d1d5f547f
johannes <johannes@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-08-24 10:17:43 +0000

    - MFH: Fix alphabetic order and add missing class
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@194294 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/php_spl.c
+++ ext/spl/php_spl.c
@@ -150,8 +150,8 @@ PHP_FUNCTION(class_implements)
 
 #define SPL_LIST_CLASSES(z_list, sub, allow, ce_flags) \
 	SPL_ADD_CLASS(AppendIterator, z_list, sub, allow, ce_flags); \
-	SPL_ADD_CLASS(ArrayObject, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(ArrayIterator, z_list, sub, allow, ce_flags); \
+	SPL_ADD_CLASS(ArrayObject, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(BadFunctionCallException, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(BadMethodCallException, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(CachingIterator, z_list, sub, allow, ce_flags); \
@@ -171,8 +171,8 @@ PHP_FUNCTION(class_implements)
 	SPL_ADD_CLASS(NoRewindIterator, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(Observer, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(OuterIterator, z_list, sub, allow, ce_flags); \
-	SPL_ADD_CLASS(OutOfRangeException, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(OutOfBoundsException, z_list, sub, allow, ce_flags); \
+	SPL_ADD_CLASS(OutOfRangeException, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(OverflowException, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(ParentIterator, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(RangeException, z_list, sub, allow, ce_flags); \
@@ -184,6 +184,7 @@ PHP_FUNCTION(class_implements)
 	SPL_ADD_CLASS(SimpleXMLIterator, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(Subject, z_list, sub, allow, ce_flags); \
 	SPL_ADD_CLASS(UnderflowException, z_list, sub, allow, ce_flags); \
+	SPL_ADD_CLASS(UnexpectedValueException, z_list, sub, allow, ce_flags); \
 
 /* {{{ proto array spl_classes()
  Return an array containing the names of all clsses and interfaces defined in SPL */
