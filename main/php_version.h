0785d0d4af931b9c5404f589e1dd57a932def888
Zeev Suraski <zeev@php.net>
2005-08-25 18:29:15 +0000

    Revert to -dev

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 0
 #define PHP_RELEASE_VERSION 5
-#define PHP_EXTRA_VERSION "RC2"
-#define PHP_VERSION "5.0.5RC2"
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.0.5-dev"
