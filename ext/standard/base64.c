c3b4f2fd42137d09f5206606fb8b7ba1488a1014
Ilia Alshanetsky <iliaa@php.net>
2005-08-26 03:33:07 +0000

    MFH: Fixed bug #34214 (base64_decode() does not properly ignore whitespace)

--- ext/standard/base64.c
+++ ext/standard/base64.c
@@ -153,15 +153,6 @@ PHPAPI unsigned char *php_base64_decode(const unsigned char *str, int length, in
 	while ((ch = *current++) != '\0' && length-- > 0) {
 		if (ch == base64_pad) break;
 
-	    /* When Base64 gets POSTed, all pluses are interpreted as spaces.
-		   This line changes them back.  It's not exactly the Base64 spec,
-		   but it is completely compatible with it (the spec says that
-		   spaces are invalid).  This will also save many people considerable
-		   headache.  - Turadg Aleahmad <turadg@wise.berkeley.edu>
-	    */
-
-		if (ch == ' ') ch = '+'; 
-
 		ch = base64_reverse_table[ch];
 		if (ch < 0) continue;
 
