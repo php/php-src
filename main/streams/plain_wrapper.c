cdcf566a21f6cb572d1d26ad6341e18564caec32
Derick Rethans <derick@php.net>
2005-08-26 12:50:06 +0000

    - Fixed a bug where stream_get_meta_data() did not return the "uri" element for   files opened with tmpname().

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -156,6 +156,7 @@ PHPAPI php_stream *_php_stream_fopen_tmpfile(int dummy STREAMS_DC TSRMLS_DC)
 		if (stream) {
 			php_stdio_stream_data *self = (php_stdio_stream_data*)stream->abstract;
 			stream->wrapper = &php_plain_files_wrapper;
+			stream->orig_path = estrdup(opened_path);
 
 			self->temp_file_name = opened_path;
 			self->lock_flag = LOCK_UN;
