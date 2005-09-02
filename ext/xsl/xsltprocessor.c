36a02fa6287b92dc6d883e3be5a8716d6b0d15ea
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-09-02 15:38:22 +0000

    Fix crash: handler name not length needs to be passed to php_error_docref
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@194947 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xsl/xsltprocessor.c
+++ ext/xsl/xsltprocessor.c
@@ -268,7 +268,7 @@ static void xsl_ext_function_php(xmlXPathParserContextPtr ctxt, int nargs, int t
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Unable to call handler %R()", Z_TYPE(callable), Z_UNIVAL(callable));
 		
 	} else if ( intern->registerPhpFunctions == 2 && zend_u_hash_exists(intern->registered_phpfunctions, Z_TYPE(callable), Z_UNIVAL(callable), Z_UNILEN(callable) + 1) == 0) { 
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Not allowed to call handler '%R()'.", Z_TYPE(callable), Z_UNILEN(callable));
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Not allowed to call handler '%R()'.", Z_TYPE(callable), Z_UNIVAL(callable));
 		// Push an empty string, so that we at least have an xslt result...
 		valuePush(ctxt, xmlXPathNewString(""));
 	} else {
