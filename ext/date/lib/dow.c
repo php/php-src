321732227fde2d9260c5935cf926611def363f2a
Derick Rethans <derick@php.net>
2005-09-02 09:33:08 +0000

    - Fixed bug #34304 (date() doesn't have a modifier for ISO Week Day).

--- ext/date/lib/dow.c
+++ ext/date/lib/dow.c
@@ -31,9 +31,9 @@ static timelib_sll century_value(timelib_sll j)
 	return c < 0 ? c + 7 : c;
 }
 
-timelib_sll timelib_day_of_week(timelib_sll y, timelib_sll m, timelib_sll d)
+static timelib_sll timelib_day_of_week_ex(timelib_sll y, timelib_sll m, timelib_sll d, int iso)
 {
-	timelib_sll c1, y1, m1;
+	timelib_sll c1, y1, m1, dow;
 
 	/* Only valid for Gregorian calendar */
 	if (y < 1753) {
@@ -42,7 +42,23 @@ timelib_sll timelib_day_of_week(timelib_sll y, timelib_sll m, timelib_sll d)
 	c1 = century_value(y / 100);
 	y1 = (y % 100);
 	m1 = timelib_is_leap(y) ? m_table_leap[m] : m_table_common[m];
-	return (c1 + y1 + m1 + (y1 / 4) + d) % 7;
+	dow = (c1 + y1 + m1 + (y1 / 4) + d) % 7;
+	if (iso) {
+		if (dow == 0) {
+			dow = 7;
+		}
+	}
+	return dow;
+}
+
+timelib_sll timelib_day_of_week(timelib_sll y, timelib_sll m, timelib_sll d)
+{
+	return timelib_day_of_week_ex(y, m, d, 0);
+}
+
+timelib_sll timelib_iso_day_of_week(timelib_sll y, timelib_sll m, timelib_sll d)
+{
+	return timelib_day_of_week_ex(y, m, d, 1);
 }
 
                                 /*     jan  feb  mar  apr  may  jun  jul  aug  sep  oct  nov  dec */
