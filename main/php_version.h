088693622a5142529750fa27900e8d525c332332
Zeev Suraski <zeev@php.net>
2005-09-05 12:00:35 +0000

    Roll back to dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 5
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.0.5"
+#define PHP_RELEASE_VERSION 6
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.0.6-dev"
