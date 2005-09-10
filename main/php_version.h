7262ccfac969295db3aa00536d72c1099504cd62
Derick Rethans <derick@php.net>
2005-09-10 15:44:17 +0000

    - Fix version numbers as discussed to help version_compare for the PEAR folks.

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.1.0-dev"
+#define PHP_EXTRA_VERSION "RC2-dev"
+#define PHP_VERSION "5.1.0RC2-dev"
