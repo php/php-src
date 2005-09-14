8b40230a771068b8d15f49ea4aa3168666e58782
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-09-14 14:56:01 +0000

    - Rename icu_loc* to i18n_loc* - Added i18n_strtotitle (name is not sure yet) - work in progress.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@196044 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/unicode/locale.c
+++ ext/unicode/locale.c
@@ -19,6 +19,7 @@
 #include "php_unicode.h"
 
 #if HAVE_UNICODE
+#include "unicode/ubrk.h"
 
 static void php_canonicalize_locale_id(char **target, int32_t *target_len, char *locale, UErrorCode *status)
 {
@@ -39,7 +40,7 @@ static void php_canonicalize_locale_id(char **target, int32_t *target_len, char 
 	*target_len = canonicalized_len;
 }
 
-PHP_FUNCTION(icu_loc_get_default)
+PHP_FUNCTION(i18n_loc_get_default)
 {
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "") == FAILURE) {
 		return;
@@ -48,7 +49,7 @@ PHP_FUNCTION(icu_loc_get_default)
 	RETURN_STRING(UG(default_locale), 1);
 }
 
-PHP_FUNCTION(icu_loc_set_default)
+PHP_FUNCTION(i18n_loc_set_default)
 {
 	char *locale;
 	int locale_len;
@@ -79,6 +80,67 @@ PHP_FUNCTION(icu_loc_set_default)
 	RETURN_TRUE;
 }
 
+/* {{{ php_strtotitle
+ */
+PHPAPI char *php_strtotitle(char *s, size_t len)
+{
+	s[0] = toupper(s[0]);
+	return s;
+}
+/* }}} */
+
+/* {{{ php_u_strtotitle
+ */
+PHPAPI UChar* php_u_strtotitle(UChar **s, int32_t *len, const char* locale)
+{
+	UChar *dest = NULL;
+	int32_t dest_len;
+	UErrorCode status = U_ZERO_ERROR;
+	UBreakIterator *brkiter;
+	
+	dest_len = *len;
+	brkiter = ubrk_open(UBRK_TITLE, locale, *s, *len, &status);
+	while (1) {
+		status = U_ZERO_ERROR;
+		dest = eurealloc(dest, dest_len+1);
+		dest_len = u_strToTitle(dest, dest_len, *s, *len, NULL, locale, &status);
+		if (status != U_BUFFER_OVERFLOW_ERROR) {
+			break;
+		}
+	}
+	ubrk_close(brkiter);
+
+	if (U_SUCCESS(status)) {
+		efree(*s);
+		dest[dest_len] = 0;
+		*s = dest;
+		*len = dest_len;
+	} else {
+		efree(dest);
+	}
+
+	return *s;
+}
+/* }}} */
+
+
+/* {{{ proto string strtoupper(string str)
+   Makes a string uppercase */
+PHP_FUNCTION(i18n_strtotitle)
+{
+	zval **arg;
+	
+	if (ZEND_NUM_ARGS() != 1 || zend_get_parameters_ex(1, &arg)) {
+		WRONG_PARAM_COUNT;
+	}
+	if (Z_TYPE_PP(arg) != IS_STRING && Z_TYPE_PP(arg) != IS_UNICODE) {
+		convert_to_text_ex(arg);
+	}
+
+	RETVAL_ZVAL(*arg, 1, 0);
+	php_u_strtotitle(&Z_USTRVAL_P(return_value), &Z_USTRLEN_P(return_value), UG(default_locale));
+}
+/* }}} */
 #endif /* HAVE_UNICODE */
 
 
