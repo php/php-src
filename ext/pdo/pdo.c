a3aa58fdeabcf9f5b1e3c36c172f657724b83cbe
Marcus Boerger <helly@php.net>
2005-09-19 18:05:00 +0000

    - Fix building with 5.0

--- ext/pdo/pdo.c
+++ ext/pdo/pdo.c
@@ -389,7 +389,7 @@ PHP_MINIT_FUNCTION(pdo)
 #if can_handle_soft_dependency_on_SPL && defined(HAVE_SPL) && ((PHP_MAJOR_VERSION > 5) || (PHP_MAJOR_VERSION == 5 && PHP_MINOR_VERSION >= 1))
 	pdo_exception_ce = zend_register_internal_class_ex(&ce, spl_ce_RuntimeException, NULL TSRMLS_CC);
 #else
-	pdo_exception_ce = zend_register_internal_class_ex(&ce, zend_exception_get_default(TSRMLS_C), NULL TSRMLS_CC);
+	pdo_exception_ce = zend_register_internal_class_ex(&ce, zend_exception_get_default(), NULL TSRMLS_CC);
 #endif
 	zend_declare_property_null(pdo_exception_ce, "errorInfo", sizeof("errorInfo")-1, ZEND_ACC_PUBLIC TSRMLS_CC);
 
