c50bdd5bbd922c7f47508b780da4ea88ed38642f
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-03 11:34:51 +0000

    - MFH: Only use external database when it is newer than the builtin one.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@197445 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -551,8 +551,12 @@ PHP_FUNCTION(gmdate)
 /* {{{ php_date_set_tzdb - NOT THREADSAFE */
 PHPAPI void php_date_set_tzdb(timelib_tzdb *tzdb)
 {
-	php_date_global_timezone_db = tzdb;
-	php_date_global_timezone_db_enabled = 1;
+	timelib_tzdb *builtin = timelib_builtin_db();
+	
+	if (php_version_compare(tzdb->version, builtin->version) > 0) {
+		php_date_global_timezone_db = tzdb;
+		php_date_global_timezone_db_enabled = 1;
+	}
 }
 /* }}} */
 
