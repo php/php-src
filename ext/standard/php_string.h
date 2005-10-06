d2d91443e451526bbed87bff65443a0a1a171c09
fmk <fmk@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-06 21:40:30 +0000

    Fix Win32 build by adding missing function declaration.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@197802 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -136,6 +136,7 @@ PHPAPI char *php_str_to_str_ex(char *haystack, int length, char *needle,
 PHPAPI char *php_str_to_str(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length);
 PHPAPI char *php_trim(char *c, int len, char *what, int what_len, zend_uchar str_type, zval *return_value, int mode TSRMLS_DC);
+PHPAPI int32_t php_u_strip_tags(UChar *rbuf, int32_t len, int *stateptr, UChar *allow, int32_t allow_len TSRMLS_DC);
 PHPAPI size_t php_strip_tags(char *rbuf, int len, int *state, char *allow, int allow_len);
 PHPAPI int php_char_to_str_ex(char *str, uint len, char from, char *to, int to_len, pval *result, int case_sensitivity, int *replace_count);
 PHPAPI int php_char_to_str(char *str, uint len, char from, char *to, int to_len, pval *result);
