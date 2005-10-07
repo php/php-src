c6469aba173cd6c48ad6fd4e8005a05e8cbf15bd
Derick Rethans <derick@php.net>
2005-10-07 08:55:35 +0000

    - Go with PHP 4.4.1RC1.

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 1
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.4.1-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "4.4.1RC1"
