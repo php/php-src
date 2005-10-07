c9b5b9191fdb3eba11a817e869e5453abd857729
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-07 13:26:11 +0000

    maintain BC for PECL
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@197866 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xmlreader/php_xmlreader.c
+++ ext/xmlreader/php_xmlreader.c
@@ -276,16 +276,22 @@ static xmlRelaxNGPtr _xmlreader_get_relaxNG(char *source, int source_len, int ty
 }
 #endif
 
+#if ZEND_EXTENSION_API_NO >= 220050617
 static zend_module_dep xmlreader_deps[] = {
 	ZEND_MOD_REQUIRED("libxml")
 	{NULL, NULL, NULL}
 };
+#endif
 
 /* {{{ xmlreader_module_entry
  */
 zend_module_entry xmlreader_module_entry = {
+#if ZEND_EXTENSION_API_NO >= 220050617
 	STANDARD_MODULE_HEADER_EX, NULL,
 	xmlreader_deps,
+#else
+	STANDARD_MODULE_HEADER,
+#endif
 	"xmlreader",
 	NULL,
 	PHP_MINIT(xmlreader),
