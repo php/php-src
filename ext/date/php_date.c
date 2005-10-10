fbd3dbf5429e52039cf633e0fcb26300a63ae8a4
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-10 16:42:32 +0000

    - MFH: Make sure the tzcache' dtor doesn't cleanup cached misses in the lookup   tables.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@198117 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -190,7 +190,9 @@ static void _php_date_tzinfo_dtor(void *tzinfo)
 {
 	timelib_tzinfo **tzi = (timelib_tzinfo **)tzinfo;
 
-	timelib_tzinfo_dtor(*tzi);
+	if (*tzi) {
+		timelib_tzinfo_dtor(*tzi);
+	}
 }
 
 /* {{{ PHP_RINIT_FUNCTION */
