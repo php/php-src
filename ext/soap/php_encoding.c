c784a64293d25ac93dfe383965cdfb10e2662f05
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-11 10:16:03 +0000

    Fixed bug #32455 (wrong setting property to unset value)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@198173 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -991,10 +991,18 @@ static zval* get_zval_property(zval* object, char* name TSRMLS_DC)
 		old_scope = EG(scope);
 	  EG(scope) = Z_OBJCE_P(object);
 		data = Z_OBJ_HT_P(object)->read_property(object, &member, BP_VAR_IS TSRMLS_CC);
-		EG(scope) = old_scope;
 		if (data == EG(uninitialized_zval_ptr)) {
+			/* Hack for bug #32455 */
+			zend_property_info *property_info;
+
+			property_info = zend_get_property_info(Z_OBJCE_P(object), &member, 1 TSRMLS_CC);
+			EG(scope) = old_scope;
+			if (property_info && zend_hash_quick_exists(Z_OBJPROP_P(object), property_info->name, property_info->name_length+1, property_info->h)) {
+				return data;
+			}
 			return NULL;
 		}
+		EG(scope) = old_scope;
 		return data;
 	} else if (Z_TYPE_P(object) == IS_ARRAY) {
 		zval **data_ptr;
