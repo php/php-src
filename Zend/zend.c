8ac7004ea781e301d1ebbbba7f9135c28cc2f605
Dmitry Stogov <dmitry@php.net>
2005-10-12 10:12:15 +0000

    Unicode support

--- Zend/zend.c
+++ Zend/zend.c
@@ -465,7 +465,7 @@ ZEND_API void zend_make_unicode_zval(zval *expr, zval *expr_copy, int *use_copy)
 				}
 			}
 			zend_error(EG(exception) ? E_ERROR : E_RECOVERABLE_ERROR, "Object of class %v could not be converted to string", Z_OBJCE_P(expr)->name);
-			ZVAL_EMPTY_STRING(expr_copy);
+			ZVAL_EMPTY_UNICODE(expr_copy);
 			break;
 		default:
 			*expr_copy = *expr;
