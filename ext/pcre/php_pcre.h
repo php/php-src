084e8b86dd06e78236d88df9d974082ded37d00e
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-12 22:37:30 +0000

    - Add/expose function to compile and access pcre_cache_entry structs
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@198318 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pcre/php_pcre.h
+++ ext/pcre/php_pcre.h
@@ -57,7 +57,10 @@ typedef struct {
 	unsigned const char *tables;
 #endif
 	int compile_options;
+	int refcount;
 } pcre_cache_entry;
+
+PHPAPI pcre_cache_entry* pcre_get_compiled_regex_cache(char *regex, int regex_len TSRMLS_DC);
 
 ZEND_BEGIN_MODULE_GLOBALS(pcre)
 	HashTable pcre_cache;
