7d91cc48188cfb9419dd53e0b17f68cad9778e4c
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-13 21:19:14 +0000

    move constants to class
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@198383 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xmlreader/examples/xmlreader_string.php
+++ ext/xmlreader/examples/xmlreader_string.php
@@ -13,11 +13,11 @@ $xmlstring = '<books>
 $reader = new XMLReader();
 $reader->XML($xmlstring);
 while ($reader->read()) {
-	if ($reader->nodeType != XMLREADER_END_ELEMENT) {
+	if ($reader->nodeType != XMLREADER::END_ELEMENT) {
 		print "Node Name: ".$reader->name."\n";
 		print "Node Value: ".$reader->value."\n";
 		print "Node Depth: ".$reader->depth."\n";
-		if ($reader->nodeType==XMLREADER_ELEMENT && $reader->hasAttributes) {
+		if ($reader->nodeType==XMLREADER::ELEMENT && $reader->hasAttributes) {
 			$attr = $reader->moveToFirstAttribute();
 			while ($attr) {
 				print "   Attribute Name: ".$reader->name."\n";
