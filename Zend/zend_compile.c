a1527fdaf3226a676648d19807d59808a6d61b9a
Derick Rethans <derick@php.net>
2005-10-15 12:54:19 +0000

    - Changed type hints so that they take "= NULL" as default value.

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -1337,7 +1337,7 @@ void zend_do_receive_arg(zend_uchar op, znode *var, znode *offset, znode *initia
 			cur_arg_info->class_name = NULL;
 			cur_arg_info->class_name_len = 0;
 		}
-		cur_arg_info->allow_null = 0;
+		cur_arg_info->allow_null = (op == ZEND_RECV_INIT && (Z_TYPE(initialization->u.constant) == IS_NULL || (Z_TYPE(initialization->u.constant) == IS_CONSTANT && !strcasecmp(Z_STRVAL(initialization->u.constant), "NULL")))) ? 1 : 0;
 	} else {
 		cur_arg_info->class_name = NULL;
 		cur_arg_info->class_name_len = 0;
