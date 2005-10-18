4976ddf751e19f3311262fcf8dc654c8cea7b3ee
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-18 13:51:50 +0000

    MFH: use php_error_docref() instead of php_error() everywhere
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@198705 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqli/php_mysqli.h
+++ ext/mysqli/php_mysqli.h
@@ -204,17 +204,17 @@ PHP_MYSQLI_EXPORT(zend_object_value) mysqli_objects_new(zend_class_entry * TSRML
 	MYSQLI_RESOURCE *my_res; \
 	mysqli_object *intern = (mysqli_object *)zend_object_store_get_object(*(__id) TSRMLS_CC);\
 	if (!(my_res = (MYSQLI_RESOURCE *)intern->ptr)) {\
-  		php_error(E_WARNING, "Couldn't fetch %s", intern->zo.ce->name);\
+  		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Couldn't fetch %s", intern->zo.ce->name);\
   		RETURN_NULL();\
   	}\
 	if (!intern->valid) { \
-		php_error(E_WARNING, "invalid resource %s", intern->zo.ce->name); \
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "invalid resource %s", intern->zo.ce->name); \
 		RETURN_NULL(); \
 	} \
 	__ptr = (__type)my_res->ptr; \
 	if (!strcmp((char *)__name, "mysqli_stmt")) {\
 		if (!((MY_STMT *)__ptr)->stmt->mysql) {\
-  			php_error(E_WARNING, "Statement isn't valid anymore");\
+  			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Statement isn't valid anymore");\
 			RETURN_NULL();\
 		}\
 	}\
