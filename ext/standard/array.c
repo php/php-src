ed415d3c9133e6f3c5afbe9fefa14f4825e3b25e
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-21 15:20:30 +0000

    Fixed bug #34934 (offsetExists is not called from array_key_exists)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@198941 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -4658,6 +4658,17 @@ PHP_FUNCTION(array_key_exists)
 		RETURN_FALSE;
 	}
 
+	if (Z_TYPE_PP(array) == IS_OBJECT &&
+	    Z_OBJ_HT_PP(array)->has_dimension &&
+	    (Z_OBJ_HT_PP(array)->has_dimension != std_object_handlers.has_dimension ||
+	     instanceof_function_ex(Z_OBJCE_PP(array), U_CLASS_ENTRY(zend_ce_arrayaccess), 1 TSRMLS_CC))) {
+	  if (Z_OBJ_HT_PP(array)->has_dimension(*array, *key, 0 TSRMLS_CC)) {
+	  	RETURN_TRUE;
+	  } else {
+	  	RETURN_FALSE;
+	  }
+	}
+
 	switch (Z_TYPE_PP(key)) {
 		case IS_STRING:
 		case IS_UNICODE:
