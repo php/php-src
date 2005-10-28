3c6c6d2a9a92ff23a66ddd11200cbb76886ee270
Dmitry Stogov <dmitry@php.net>
2005-10-28 06:05:41 +0000

    $id:$ tatgs are removed from generated files to avoid committing of Zend/zend_vm_opcodes.h without real changes.

--- Zend/zend_vm_gen.php
+++ Zend/zend_vm_gen.php
@@ -1,4 +1,23 @@
 <?php
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2005 Zend Technologies Ltd. (http://www.zend.com) |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.00 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+   | Authors: Dmitry Stogov <dmitry@zend.com>                             |
+   +----------------------------------------------------------------------+
+
+	 $Id$
+*/
 
 $header_text = <<< DATA
 /*
@@ -16,10 +35,10 @@ $header_text = <<< DATA
    | license@zend.com so we can mail you a copy immediately.              |
    +----------------------------------------------------------------------+
    | Authors: Andi Gutmans <andi@zend.com>                                |
+   |          Zeev Suraski <zeev@zend.com>                                |
+   |          Dmitry Stogov <dmitry@zend.com>                             |
    +----------------------------------------------------------------------+
 */
-
-/* \$Id$ */
 
 
 DATA;
