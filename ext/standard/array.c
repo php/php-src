363fccc17aa59797b94ee18c190e5ef51e58c860
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-10-30 13:46:34 +0000

    MFB51: Fixed bug #35014 (array_product() always returns 0).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@199546 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -4244,7 +4244,10 @@ PHP_FUNCTION(array_product)
 		return;
 	}
 
-	ZVAL_LONG(return_value, 0);
+	if (!zend_hash_num_elements(Z_ARRVAL_PP(input))) {
+		RETURN_LONG(0);
+	}
+	ZVAL_LONG(return_value, 1);
 
 	for (zend_hash_internal_pointer_reset_ex(Z_ARRVAL_PP(input), &pos);
 		 zend_hash_get_current_data_ex(Z_ARRVAL_PP(input), (void **)&entry, &pos) == SUCCESS;
