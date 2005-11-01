5d671dd4b26c3c13b78807a50abf5fe4482888ae
Sebastian Bergmann <sebastian@php.net>
2005-11-01 13:18:34 +0000

    Add an additional field $frame['object'] to the result array of debug_backtrace() that contains a reference to the respective object when the frame was called from an object. This revised patch has been reviewed by Dmitry and Zeev.

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -1906,7 +1906,7 @@ ZEND_FUNCTION(debug_print_backtrace)
 
 /* }}} */
 
-ZEND_API void zend_fetch_debug_backtrace(zval *return_value, int skip_last TSRMLS_DC)
+ZEND_API void zend_fetch_debug_backtrace(zval *return_value, int skip_last, int provide_object TSRMLS_DC)
 {
 	zend_execute_data *ptr, *skip;
 	int lineno;
@@ -2005,6 +2005,11 @@ ZEND_API void zend_fetch_debug_backtrace(zval *return_value, int skip_last TSRML
 					dup = zend_get_object_classname(ptr->object, &class_name, &class_name_len TSRMLS_CC);
 					add_assoc_text_ex(stack_frame, "class", sizeof("class"), class_name, dup);
 				}
+				if (provide_object) {
+					add_assoc_zval_ex(stack_frame, "object", sizeof("object"), ptr->object);
+					ptr->object->refcount++;
+				}
+
 				add_assoc_ascii_string_ex(stack_frame, "type", sizeof("type"), "->", 1);
 			} else if (ptr->function_state.function->common.scope) {
 				add_assoc_textl_ex(stack_frame, "class", sizeof("class"), ptr->function_state.function->common.scope->name, ptr->function_state.function->common.scope->name_length, 1);
@@ -2086,7 +2091,7 @@ ZEND_FUNCTION(debug_backtrace)
 		ZEND_WRONG_PARAM_COUNT();
 	}
 	
-	zend_fetch_debug_backtrace(return_value, 1 TSRMLS_CC);
+	zend_fetch_debug_backtrace(return_value, 1, 1 TSRMLS_CC);
 }
 /* }}} */
 
