fbc6470cb6479f7ebabd693cf82303fa2339f197
Edin Kadribasic <edink@php.net>
2005-11-01 14:18:20 +0000

    Fix ZTS build

--- ext/pdo_oci/oci_statement.c
+++ ext/pdo_oci/oci_statement.c
@@ -452,6 +452,7 @@ static sb4 oci_define_callback(dvoid *octxp, OCIDefine *define, ub4 iter, dvoid 
 		ub4 **alenpp, ub1 *piecep, dvoid **indpp, ub2 **rcodepp)
 {
 	pdo_oci_column *col = (pdo_oci_column*)octxp;
+	TSRMLS_FETCH();
 
 	switch (col->dtype) {
 		case SQLT_BLOB:
