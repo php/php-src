6448c559a457757264f920c4824d5022fcd1974c
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-05 22:41:09 +0000

    MFB - This isn't a DOM thing anymore, just libxml
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@200055 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/simplexml/simplexml.c
+++ ext/simplexml/simplexml.c
@@ -1189,6 +1189,7 @@ static void sxe_add_registered_namespaces(php_sxe_object *sxe, xmlNodePtr node, 
 		}
 	}
 }
+/* }}} */
 
 /* {{{ proto string SimpleXMLElement::getDocNamespaces([bool recursive])
    Return all namespaces registered with document */
@@ -1865,7 +1866,6 @@ void *simplexml_export_node(zval *object TSRMLS_DC)
 	return php_sxe_get_first_node(sxe, node TSRMLS_CC);	
 }
 
-#ifdef HAVE_DOM
 /* {{{ proto simplemxml_element simplexml_import_dom(domNode node [, string class_name])
    Get a simplexml_element object from dom to allow for processing */
 PHP_FUNCTION(simplexml_import_dom)
@@ -1918,14 +1918,11 @@ PHP_FUNCTION(simplexml_import_dom)
 	}
 }
 /* }}} */
-#endif
 
 function_entry simplexml_functions[] = {
 	PHP_FE(simplexml_load_file, NULL)
 	PHP_FE(simplexml_load_string, NULL)
-#ifdef HAVE_DOM
 	PHP_FE(simplexml_import_dom, NULL)
-#endif
 	{NULL, NULL, NULL}
 };
 
