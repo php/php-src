a15a23ddab9a5a73b821c942c8aaddce7b999e1d
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-05 22:02:15 +0000

    Fixed bug #31971 (ftp_login fails on some SSL servers). Patch by: frantisek at augusztin dot com
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@200049 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ftp/ftp.c
+++ ext/ftp/ftp.c
@@ -280,7 +280,9 @@ ftp_login(ftpbuf_t *ftp, const char *user, const char *pass TSRMLS_DC)
 				php_error_docref(NULL TSRMLS_CC, E_WARNING, "failed to create the SSL context");
 				return 0;
 			}
-			
+
+			SSL_CTX_set_options(ctx, SSL_OP_ALL);
+
 			ftp->ssl_handle = SSL_new(ctx);
 			if (ftp->ssl_handle == NULL) {
 				php_error_docref(NULL TSRMLS_CC, E_WARNING, "failed to create the SSL handle");
@@ -1513,7 +1515,9 @@ data_accepted:
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "data_accept: failed to create the SSL context");
 			return 0;
 		}
-			
+
+		SSL_CTX_set_options(ctx, SSL_OP_ALL);
+
 		data->ssl_handle = SSL_new(ctx);
 		if (data->ssl_handle == NULL) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "data_accept: failed to create the SSL handle");
