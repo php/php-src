704044f6299294e4a9db643abed3585f0965e6ba
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-14 22:03:02 +0000

    MFH: fix #35197 (Destructor is not called) and similar issues in other extensions
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@200493 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xsl/php_xsl.c
+++ ext/xsl/php_xsl.c
@@ -138,7 +138,7 @@ zend_object_value xsl_objects_new(zend_class_entry *class_type TSRMLS_DC)
 	zend_hash_init(intern->parameter, 0, NULL, ZVAL_PTR_DTOR, 0);
 	ALLOC_HASHTABLE(intern->registered_phpfunctions);
 	zend_hash_init(intern->registered_phpfunctions, 0, NULL, ZVAL_PTR_DTOR, 0);
-	retval.handle = zend_objects_store_put(intern, NULL, (zend_objects_free_object_storage_t) xsl_objects_free_storage, NULL TSRMLS_CC);
+	retval.handle = zend_objects_store_put(intern, (zend_objects_store_dtor_t)zend_objects_destroy_object, (zend_objects_free_object_storage_t) xsl_objects_free_storage, NULL TSRMLS_CC);
 	intern->handle = retval.handle;
 	retval.handlers = &xsl_object_handlers;
 	return retval;
