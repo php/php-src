da63a7f7af797cced7e1f6caf7c3937629f3ac8c
Dmitry Stogov <dmitry@php.net>
2005-11-16 09:30:53 +0000

    Fixed bug #35229 (call_user_func() crashes when arguement_stack is nearly full)

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -1891,7 +1891,7 @@ PHP_FUNCTION(call_user_func)
 
 	params = safe_emalloc(sizeof(zval **), argc, 0);
 
-	if (zend_get_parameters_array_ex(argc, params) == FAILURE) {
+	if (zend_get_parameters_array_ex(1, params) == FAILURE) {
 		efree(params);
 		RETURN_FALSE;
 	}
@@ -1906,6 +1906,11 @@ PHP_FUNCTION(call_user_func)
 		efree(name);
 		efree(params);
 		RETURN_NULL();
+	}
+
+	if (zend_get_parameters_array_ex(argc, params) == FAILURE) {
+		efree(params);
+		RETURN_FALSE;
 	}
 
 	if (call_user_function_ex(EG(function_table), NULL, *params[0], &retval_ptr, argc-1, params+1, 0, NULL TSRMLS_CC) == SUCCESS) {
