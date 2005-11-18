fddb7c4a16005c0a604905198ea7f1317042eae9
Frank M. Kromann <fmk@php.net>
2005-11-18 21:24:06 +0000

    MFH: Fix #33201 Crash when fetching some data types

--- ext/mssql/php_mssql.c
+++ ext/mssql/php_mssql.c
@@ -891,11 +891,9 @@ static void php_mssql_get_column_content_with_type(mssql_link *mssql_ptr,int off
 				if ((column_type != SQLDATETIME && column_type != SQLDATETIM4) || MS_SQL_G(datetimeconvert)) {
 
 					switch (column_type) {
-						case SQLDATETIM4 :
-							res_length += 14;
-							break;
 						case SQLDATETIME :
-							res_length += 10;
+						case SQLDATETIM4 :
+							res_length += 20;
 							break;
 						case SQLMONEY :
 						case SQLMONEY4 :
@@ -903,6 +901,8 @@ static void php_mssql_get_column_content_with_type(mssql_link *mssql_ptr,int off
 						case SQLDECIMAL :
 						case SQLNUMERIC :
 							res_length += 5;
+						case 127 :
+							res_length += 20;
 							break;
 					}
 			
@@ -965,11 +965,9 @@ static void php_mssql_get_column_content_without_type(mssql_link *mssql_ptr,int 
 		if ((column_type != SQLDATETIME && column_type != SQLDATETIM4) || MS_SQL_G(datetimeconvert)) {
 
 			switch (column_type) {
-				case SQLDATETIM4 :
-					res_length += 14;
-					break;
 				case SQLDATETIME :
-					res_length += 10;
+				case SQLDATETIM4 :
+					res_length += 20;
 					break;
 				case SQLMONEY :
 				case SQLMONEY4 :
@@ -977,6 +975,8 @@ static void php_mssql_get_column_content_without_type(mssql_link *mssql_ptr,int 
 				case SQLDECIMAL :
 				case SQLNUMERIC :
 					res_length += 5;
+				case 127 :
+					res_length += 20;
 					break;
 			}
 			
