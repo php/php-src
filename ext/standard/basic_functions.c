895e795b53a1e3d9f925546ba1829c420cd82832
foobar <sniper@php.net>
2005-11-20 18:06:09 +0000

    - Fixed bug #35243 (php_mblen() crashes when compiled with thread-safety on Linux)

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -957,6 +957,10 @@ static void basic_globals_ctor(php_basic_globals *basic_globals_p TSRMLS_DC)
 
 	memset(&BG(url_adapt_state), 0, sizeof(BG(url_adapt_state)));
 	memset(&BG(url_adapt_state_ex), 0, sizeof(BG(url_adapt_state_ex)));
+	
+#if defined(_REENTRANT) && defined(HAVE_MBRLEN) && defined(HAVE_MBSTATE_T)
+	memset(&BG(mblen_state), 0, sizeof(BG(mblen_state)));
+#endif
 
 	BG(incomplete_class) = php_create_incomplete_class(TSRMLS_C);
 }
