57deca0c8fc7ad402b161867c0b294f8a489c514
Johannes Schlüter <johannes@php.net>
2005-11-20 15:21:23 +0000

    - Fix SPL class listing for phpinfo in unicode mode

--- ext/spl/php_spl.c
+++ ext/spl/php_spl.c
@@ -559,7 +559,7 @@ int spl_build_class_list_string(zval **entry, char **list TSRMLS_DC) /* {{{ */
 {
 	char *res;
 	
-	spprintf(&res, 0, "%s, %s", *list, Z_STRVAL_PP(entry));
+	spprintf(&res, 0, "%s, %v", *list, Z_STRVAL_PP(entry));
 	efree(*list);
 	*list = res;
 	return ZEND_HASH_APPLY_KEEP;
