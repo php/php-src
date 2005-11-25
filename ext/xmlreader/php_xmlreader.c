04dffec1c4db169de2686b7330a3b1ab8a811719
foobar <sniper@php.net>
2005-11-25 15:40:14 +0000

    - Use right define: ZEND_MODULE_API_NO as these are PHP modules, not Zend extensions

--- ext/xmlreader/php_xmlreader.c
+++ ext/xmlreader/php_xmlreader.c
@@ -276,7 +276,7 @@ static xmlRelaxNGPtr _xmlreader_get_relaxNG(char *source, int source_len, int ty
 }
 #endif
 
-#if ZEND_EXTENSION_API_NO >= 220050617
+#if ZEND_MODULE_API_NO >= 20050922
 static zend_module_dep xmlreader_deps[] = {
 	ZEND_MOD_REQUIRED("libxml")
 	{NULL, NULL, NULL}
@@ -286,7 +286,7 @@ static zend_module_dep xmlreader_deps[] = {
 /* {{{ xmlreader_module_entry
  */
 zend_module_entry xmlreader_module_entry = {
-#if ZEND_EXTENSION_API_NO >= 220050617
+#if ZEND_MODULE_API_NO >= 20050922
 	STANDARD_MODULE_HEADER_EX, NULL,
 	xmlreader_deps,
 #else
