c059e824f99df963586dceb8ee691e6a4a6b2c06
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-25 12:25:05 +0000

    MFH: ICC doesn't support __attribute__ for pointers
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@201259 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend.h
+++ Zend/zend.h
@@ -171,7 +171,7 @@ char *alloca ();
 # define ZEND_ATTRIBUTE_FORMAT(type, idx, first)
 #endif
 
-#if ZEND_GCC_VERSION >= 3001
+#if ZEND_GCC_VERSION >= 3001 && !defined(__INTEL_COMPILER)
 # define ZEND_ATTRIBUTE_PTR_FORMAT(type, idx, first) __attribute__ ((format(type, idx, first)))
 #else
 # define ZEND_ATTRIBUTE_PTR_FORMAT(type, idx, first)
