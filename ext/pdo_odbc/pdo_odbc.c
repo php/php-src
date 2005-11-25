4e4b98a66db712bb316aabbecec71df87de82697
Wez Furlong <wez@php.net>
2005-11-25 15:42:26 +0000

    Use right define: ZEND_MODULE_API_NO as these are PHP modules, not Zend extensions

--- ext/pdo_odbc/pdo_odbc.c
+++ ext/pdo_odbc/pdo_odbc.c
@@ -37,7 +37,7 @@ function_entry pdo_odbc_functions[] = {
 /* }}} */
 
 /* {{{ pdo_odbc_deps[] */
-#if ZEND_EXTENSION_API_NO >= 220050617
+#if ZEND_MODULE_API_NO >= 20050922
 static zend_module_dep pdo_odbc_deps[] = {
 	ZEND_MOD_REQUIRED("pdo")
 	{NULL, NULL, NULL}
@@ -47,7 +47,7 @@ static zend_module_dep pdo_odbc_deps[] = {
 
 /* {{{ pdo_odbc_module_entry */
 zend_module_entry pdo_odbc_module_entry = {
-#if ZEND_EXTENSION_API_NO >= 220050617
+#if ZEND_MODULE_API_NO >= 20050922
 	STANDARD_MODULE_HEADER_EX, NULL,
 	pdo_odbc_deps,
 #else
