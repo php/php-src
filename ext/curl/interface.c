455bcbb600a652d20f01f563a7486611dbd6636a
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-27 20:10:15 +0000

    MF51: improve open_basedir check
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@201441 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -110,7 +110,7 @@ static void _php_curl_close(zend_rsrc_list_entry *rsrc TSRMLS_DC);
 
 #define PHP_CURL_CHECK_OPEN_BASEDIR(str, len)													\
 	if (((PG(open_basedir) && *PG(open_basedir)) || PG(safe_mode)) &&                                                \
-	    strncasecmp(str, "file://", sizeof("file://") - 1) == 0)								\
+	    strncasecmp(str, "file:", sizeof("file:") - 1) == 0)								\
 	{ 																							\
 		php_url *tmp_url; 																		\
 																								\
