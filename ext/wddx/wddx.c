99785a475e0ab0c79bf3a3b7c4e7a56bdbe7b121
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-11-30 18:13:17 +0000

    MFB51: Fixed bug #35410 (wddx_deserialize() doesn't handle large ints as keys properly).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@201724 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/wddx/wddx.c
+++ ext/wddx/wddx.c
@@ -1006,11 +1006,15 @@ static void php_wddx_pop_element(void *user_data, const XML_Char *name)
 				
 						switch (is_numeric_string(ent1->varname, strlen(ent1->varname), &l, &d, 0)) {
 							case IS_DOUBLE:
+								if (d > INT_MAX) {
+									goto bigint;
+								}
 								l = (long) d;
 							case IS_LONG:
 								zend_hash_index_update(target_hash, l, &ent1->data, sizeof(zval *), NULL);
 								break;
 							default:
+bigint:
 								zend_hash_update(target_hash,ent1->varname, strlen(ent1->varname)+1, &ent1->data, sizeof(zval *), NULL);
 						}
 					}
