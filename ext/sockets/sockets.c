d66e492c3d4b49ba0255f97d6497c912d2aae446
Antony Dovgal <tony2001@php.net>
2005-12-03 23:24:29 +0000

    MFH: yet another compile warning

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -1506,7 +1506,9 @@ PHP_FUNCTION(socket_get_option)
 	zval			*arg1;
 	struct linger	linger_val;
 	struct timeval		tv;
+#ifdef PHP_WIN32
 	int				timeout = 0;
+#endif
 	socklen_t		optlen;
 	php_socket		*php_sock;
 	int				other_val;
@@ -1581,7 +1583,10 @@ PHP_FUNCTION(socket_set_option)
 	struct linger	lv;
 	struct timeval tv;
 	php_socket		*php_sock;
-	int				ov, optlen, retval, timeout;
+	int				ov, optlen, retval; 
+#ifdef PHP_WIN32
+	int				timeout;
+#endif
 	long				level, optname;
 	void 			*opt_ptr;
 	
