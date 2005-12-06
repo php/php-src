446c0b84e1074e39226d56ebac2d688824405490
Ilia Alshanetsky <iliaa@php.net>
2005-12-06 23:33:04 +0000

    MFH: Fixed bug #30760 (Remove MessageBox on win32 for E_CORE errors if display_startup_error is off).

--- main/main.c
+++ main/main.c
@@ -640,9 +640,8 @@ static void php_error_cb(int type, const char *error_filename, const uint error_
 
 		if (!module_initialized || PG(log_errors)) {
 			char *log_buffer;
-
 #ifdef PHP_WIN32
-			if (type==E_CORE_ERROR || type==E_CORE_WARNING) {
+			if ((type == E_CORE_ERROR || type == E_CORE_WARNING) && PG(display_startup_errors)) {
 				MessageBox(NULL, buffer, error_type_str, MB_OK|ZEND_SERVICE_MB_STYLE);
 			}
 #endif
