0b8250468c312797547137f9324c9aa372e00319
Antony Dovgal <tony2001@php.net>
2005-12-08 22:05:44 +0000

    improve error message

--- ext/oci8/oci8_statement.c
+++ ext/oci8/oci8_statement.c
@@ -1121,7 +1121,7 @@ int php_oci_bind_array_by_name(php_oci_statement *statement, char *name, int nam
 			bind = php_oci_bind_array_helper_date(var, max_table_length, statement->connection TSRMLS_CC);
 			break;
 		default:
-			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Not supported bind type (%ld)", type);
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Unknown or unsupported datatype given: %ld", type);
 			return 1;
 			break;
 	}
