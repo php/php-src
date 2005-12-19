34dcc09c071c597ec3924af13ceb796135d865db
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-12-19 16:34:00 +0000

    MFB51: Fixed possible memory corruption.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@203240 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_sqlite/sqlite_driver.c
+++ ext/pdo_sqlite/sqlite_driver.c
@@ -42,7 +42,10 @@ int _pdo_sqlite_error(pdo_dbh_t *dbh, pdo_stmt_t *stmt, const char *file, int li
 	einfo->line = line;
 
 	if (einfo->errcode != SQLITE_OK) {
-		einfo->errmsg = (char*)sqlite3_errmsg(H->db);
+		if (einfo->errmsg) {
+			efree(einfo->errmsg);
+		}
+		einfo->errmsg = estrdup((char*)sqlite3_errmsg(H->db));
 	} else { /* no error */
 		strcpy(*pdo_err, PDO_ERR_NONE);
 		return 0;
@@ -133,10 +136,16 @@ static int sqlite_handle_closer(pdo_dbh_t *dbh TSRMLS_DC) /* {{{ */
 	pdo_sqlite_db_handle *H = (pdo_sqlite_db_handle *)dbh->driver_data;
 	
 	if (H) {
+		pdo_sqlite_error_info *einfo = &H->einfo;
+
 		pdo_sqlite_cleanup_callbacks(H TSRMLS_CC);
 		if (H->db) {
 			sqlite3_close(H->db);
 			H->db = NULL;
+		}
+		if (einfo->errmsg) {
+			efree(einfo->errmsg);
+			einfo->errmsg = NULL;
 		}
 		pefree(H, dbh->is_persistent);
 		dbh->driver_data = NULL;
