469cb7f3b3bae3fe5b2386ad66f9ea7c7d4bafdd
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-12-21 01:12:24 +0000

    - fix {{{
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@203337 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xmlreader/php_xmlreader.c
+++ ext/xmlreader/php_xmlreader.c
@@ -53,7 +53,7 @@ typedef struct _xmlreader_prop_handler {
 
 #define XMLREADER_LOAD_STRING 0
 #define XMLREADER_LOAD_FILE 1
-
+/* {{{ xmlreader_register_prop_handler */
 static void xmlreader_register_prop_handler(HashTable *prop_handler, char *name, xmlreader_read_int_t read_int_func, xmlreader_read_char_t read_char_func, int rettype TSRMLS_DC)
 {
 	xmlreader_prop_handler hnd;
@@ -63,7 +63,9 @@ static void xmlreader_register_prop_handler(HashTable *prop_handler, char *name,
 	hnd.type = rettype;
 	zend_hash_add(prop_handler, name, strlen(name)+1, &hnd, sizeof(xmlreader_prop_handler), NULL);
 }
+/* }}} */
 
+/* {{{ xmlreader_property_reader */
 static int xmlreader_property_reader(xmlreader_object *obj, xmlreader_prop_handler *hnd, zval **retval TSRMLS_DC)
 {
 	char *retchar = NULL;
@@ -106,6 +108,7 @@ static int xmlreader_property_reader(xmlreader_object *obj, xmlreader_prop_handl
 
 	return SUCCESS;
 }
+/* }}} */
 
 /* {{{ xmlreader_read_property */
 zval *xmlreader_read_property(zval *object, zval *member, int type TSRMLS_DC)
@@ -185,6 +188,7 @@ void xmlreader_write_property(zval *object, zval *member, zval *value TSRMLS_DC)
 }
 /* }}} */
 
+/* {{{ _xmlreader_get_valid_file_path */
 /* _xmlreader_get_valid_file_path and _xmlreader_get_relaxNG should be made a 
 	common function in libxml extension as code is common to a few xml extensions */
 char *_xmlreader_get_valid_file_path(char *source, char *resolved_path, int resolved_path_len  TSRMLS_DC) {
@@ -231,8 +235,10 @@ char *_xmlreader_get_valid_file_path(char *source, char *resolved_path, int reso
 
 	return file_dest;
 }
+/* }}} */
 
 #ifdef LIBXML_SCHEMAS_ENABLED
+/* {{{ _xmlreader_get_relaxNG */
 static xmlRelaxNGPtr _xmlreader_get_relaxNG(char *source, int source_len, int type, 
 											xmlRelaxNGValidityErrorFunc error_func, 
 											xmlRelaxNGValidityWarningFunc warn_func TSRMLS_DC)
@@ -274,6 +280,7 @@ static xmlRelaxNGPtr _xmlreader_get_relaxNG(char *source, int source_len, int ty
 
 	return sptr;
 }
+/* }}} */
 #endif
 
 static zend_module_dep xmlreader_deps[] = {
@@ -309,6 +316,7 @@ void xmlreader_objects_clone(void *object, void **object_clone TSRMLS_DC)
 }
 /* }}} */
 
+/* {{{ xmlreader_free_resources */
 static void xmlreader_free_resources(xmlreader_object *intern) {
 	if (intern) {
 		if (intern->input) {
@@ -328,6 +336,7 @@ static void xmlreader_free_resources(xmlreader_object *intern) {
 #endif
 	}
 }
+/* }}} */
 
 /* {{{ xmlreader_objects_free_storage */
 void xmlreader_objects_free_storage(void *object TSRMLS_DC)
@@ -368,6 +377,7 @@ zend_object_value xmlreader_objects_new(zend_class_entry *class_type TSRMLS_DC)
 }
 /* }}} */
 
+/* {{{ php_xmlreader_string_arg */
 static void php_xmlreader_string_arg(INTERNAL_FUNCTION_PARAMETERS, xmlreader_read_one_char_t internal_function) {
 	zval *id;
 	int name_len = 0;
@@ -398,7 +408,9 @@ static void php_xmlreader_string_arg(INTERNAL_FUNCTION_PARAMETERS, xmlreader_rea
 		RETVAL_EMPTY_STRING();
 	}
 }
+/* }}} */
 
+/* {{{ php_xmlreader_no_arg */
 static void php_xmlreader_no_arg(INTERNAL_FUNCTION_PARAMETERS, xmlreader_read_int_t internal_function) {
 	zval *id;
 	int retval;
@@ -416,6 +428,7 @@ static void php_xmlreader_no_arg(INTERNAL_FUNCTION_PARAMETERS, xmlreader_read_in
 
 	RETURN_FALSE;
 }
+/* }}} */
 
 /* This function not yet needed until some additional functions are implemented in libxml
 static void php_xmlreader_no_arg_string(INTERNAL_FUNCTION_PARAMETERS, xmlreader_read_char_t internal_function) {
@@ -439,6 +452,7 @@ static void php_xmlreader_no_arg_string(INTERNAL_FUNCTION_PARAMETERS, xmlreader_
 }
 */
 
+/* {{{ php_xmlreader_set_relaxng_schema */
 static void php_xmlreader_set_relaxng_schema(INTERNAL_FUNCTION_PARAMETERS, int type) {
 #ifdef LIBXML_SCHEMAS_ENABLED
 	zval *id;
@@ -490,6 +504,7 @@ static void php_xmlreader_set_relaxng_schema(INTERNAL_FUNCTION_PARAMETERS, int t
 	RETURN_FALSE;
 #endif
 }
+/* }}} */
 
 /* {{{ proto boolean XMLReader::close()
 Closes xmlreader - current frees resources until xmlTextReaderClose is fixed in libxml */
@@ -507,6 +522,7 @@ PHP_METHOD(xmlreader, close)
 	
 	RETURN_TRUE;
 }
+/* }}} */
 
 /* {{{ proto string XMLReader::getAttribute(string name)
 Get value of an attribute from current element */
