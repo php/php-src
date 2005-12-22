803c3645baa9c7e49a13f9f062918ab186377a54
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2005-12-22 00:35:37 +0000

    - Fix memleak
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@203434 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -384,6 +384,9 @@ SPL_METHOD(RecursiveIteratorIterator, __construct)
 		iterator = NULL;
 	}
 	if (!iterator || !instanceof_function(Z_OBJCE_P(iterator), spl_ce_RecursiveIterator TSRMLS_CC)) {
+		if (iterator && !inc_refcount) {
+			zval_ptr_dtor(&iterator);
+		}
 		php_set_error_handling(EH_NORMAL, NULL TSRMLS_CC);
 		zend_throw_exception(spl_ce_InvalidArgumentException, "An instance of RecursiveIterator or IteratorAggregate creating it is required", 0 TSRMLS_CC);
 		return;
