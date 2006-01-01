7cafc70b5b7ed6442c91e0237d0155d747c84b45
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-01 12:51:34 +0000

    bump year and license version
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@204189 c90b9560-bf6c-de11-be94-00142212c4b1

--- win32/build/buildconf.js
+++ win32/build/buildconf.js
@@ -2,12 +2,12 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2005 The PHP Group                                |
+  | Copyright (c) 1997-2006 The PHP Group                                |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.0 of the PHP license,       |
+  | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
   | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_0.txt.                                  |
+  | http://www.php.net/license/3_01.txt                                  |
   | If you did not receive a copy of the PHP license and are unable to   |
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: buildconf.js,v 1.13.2.1 2005-09-17 20:00:01 edink Exp $ */
+/* $Id: buildconf.js,v 1.13.2.2 2006-01-01 12:50:20 sniper Exp $ */
 // This generates a configure script for win32 build
 
 WScript.StdOut.WriteLine("Rebuilding configure.js");
