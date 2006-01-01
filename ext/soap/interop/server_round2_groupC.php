ba61421ae2579276355aba81b754b5539b6f831c
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-01 13:13:11 +0000

    - fix typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@204213 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/interop/server_round2_groupC.php
+++ ext/soap/interop/server_round2_groupC.php
@@ -7,7 +7,7 @@
 // +----------------------------------------------------------------------+
 // | This source file is subject to version 2.02 of the PHP license,      |
 // | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
+// | available through the world-wide-web at                        |
 // | http://www.php.net/license/2_02.txt.                                 |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
