c8d68046239bcb853fab44acc3264cd45b37b9a0
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-11 14:19:14 +0000

    5.1.2
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@204959 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 2
-#define PHP_EXTRA_VERSION "RC3-dev"
-#define PHP_VERSION "5.1.2RC3-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.1.2"
