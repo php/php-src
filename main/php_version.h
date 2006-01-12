e731da3653e3b62cddd22cfe66e9c0cd85006009
Ilia Alshanetsky <iliaa@php.net>
2006-01-12 15:24:10 +0000

    Back to dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 2
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.1.2"
+#define PHP_RELEASE_VERSION 3
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.1.3-dev"
