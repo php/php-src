01dcf535f79e5a9f446c61859d26b1b38adc2787
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-13 04:06:00 +0000

    - add php_mkdir_ex, works like php_mkdir but allows to silent any error   when no options are used
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@205083 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -1355,10 +1355,10 @@ PHPAPI PHP_FUNCTION(fseek)
 /* {{{ proto int mkdir(char *dir int mode)
 */
 
-PHPAPI int php_mkdir(char *dir, long mode TSRMLS_DC)
+PHPAPI int php_mkdir_ex(char *dir, long mode, int options TSRMLS_DC)
 {
 	int ret;
-	
+
 	if (PG(safe_mode) && (!php_checkuid(dir, NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
 		return -1;
 	}
@@ -1367,11 +1367,16 @@ PHPAPI int php_mkdir(char *dir, long mode TSRMLS_DC)
 		return -1;
 	}
 
-	if ((ret = VCWD_MKDIR(dir, (mode_t)mode)) < 0) {
+	if ((ret = VCWD_MKDIR(dir, (mode_t)mode)) < 0 && (options & REPORT_ERRORS)) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", strerror(errno));
 	}
 
-	return ret;	
+	return ret;
+}
+
+PHPAPI int php_mkdir(char *dir, long mode TSRMLS_DC)
+{
+	    return php_mkdir_ex(dir, mode, REPORT_ERRORS TSRMLS_CC);
 }
 /* }}} */
 
