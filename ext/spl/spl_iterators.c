4f07cff61c2fa5dd4e49d561d901b26e7cff7ff7
Antony Dovgal <tony2001@php.net>
2006-01-18 23:39:00 +0000

    fix typo (and possible segfault)

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -1890,7 +1890,7 @@ SPL_METHOD(CachingIterator, setFlags)
 
 	intern = (spl_dual_it_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", flags) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &flags) == FAILURE) {
 		return;
 	}
 
