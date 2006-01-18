e5c37b8432762692452306a397bf71d4f3c84eca
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-18 23:55:47 +0000

    MFH: couple of pedantic fixes: "static" should be first
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@205548 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/cyr_convert.c
+++ ext/standard/cyr_convert.c
@@ -47,9 +47,9 @@
 
 typedef unsigned char _cyr_charset_table[512];
 
-/* {{{ const static _cyr_charset_table _cyr_win1251
+/* {{{ static const _cyr_charset_table _cyr_win1251
  */
-const static _cyr_charset_table _cyr_win1251 = {
+static const _cyr_charset_table _cyr_win1251 = {
 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
 32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
