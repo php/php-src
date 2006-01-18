4f3abed0d64c70d6a00f3d9379a154bd94db00a8
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-01-18 23:55:27 +0000

    couple of pedantic fixes: "static" should be first
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@205547 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/filters.c
+++ ext/standard/filters.c
@@ -716,7 +716,7 @@ static php_conv_err_t php_conv_base64_decode_convert(php_conv_base64_decode *ins
 	size_t icnt, ocnt;
 	unsigned int ustat;
 
-	const static unsigned int nbitsof_pack = 8;
+	static const unsigned int nbitsof_pack = 8;
 
 	if (in_pp == NULL || in_left_p == NULL) {
 		if (inst->eos || inst->urem_nbits == 0) { 
