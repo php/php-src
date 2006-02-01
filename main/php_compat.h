af6dd5cb7ed05f2083e881bc2af76cabe2553d81
Joe Orton <jorton@php.net>
2006-02-01 13:10:04 +0000

    Fixed bug #36208 (symbol namespace conflicts using bundled gd).  (Jakub Moc)

--- main/php_compat.h
+++ main/php_compat.h
@@ -319,6 +319,12 @@
 #define term_source php_gd_term_source
 #define writewbmp php_gd_writewbmp
 #define ZeroDataBlock php_gd_ZeroDataBlock
+#define gdCacheCreate php_gd_gdCacheCreate
+#define gdCacheDelete php_gd_gdCacheDelete
+#define gdCacheGet php_gd_gdCacheGet
+#define gdFontCacheSetup php_gd_gdFontCacheSetup
+#define gdFontCacheShutdown php_gd_gdFontCacheShutdown
+#define gdFreeFontCache php_gd_gdFreeFontCache
 #endif /* HAVE_GD_BUNDLED */
 
 /* Define to specify how much context to retain around the current parse
