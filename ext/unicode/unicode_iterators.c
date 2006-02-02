1fba5bbe570074734563aa11a399450de961be9e
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-02-02 06:01:27 +0000

    Fix Andrei.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@206261 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/unicode/unicode_iterators.c
+++ ext/unicode/unicode_iterators.c
@@ -10,7 +10,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Andre Zmievski(andrei@php.net)                              |
+   | Authors: Andrei Zmievski <andrei@php.net>                            |
    +----------------------------------------------------------------------+
 */
 
