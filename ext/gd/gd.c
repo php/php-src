d78e8d749c5e49a9e451bbd99a1fc0962b2cdc80
Pierre Joye <pajoye@php.net>
2006-02-05 15:53:02 +0000

    - test for jpeg support

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -1454,10 +1454,13 @@ static void _php_image_create_from(INTERNAL_FUNCTION_PARAMETERS, int image_type,
 				im = gdImageCreateFromXpm(fn);
 				break;
 #endif
+
+#ifdef HAVE_GD_JPG
 			case PHP_GDIMG_TYPE_JPG:
 				ignore_warning = INI_INT("gd.jpeg_ignore_warning");
 				im = gdImageCreateFromJpeg(fp, ignore_warning);
 				break;
+#endif
 
 			default:
 				im = (*func_p)(fp);
