dcf5862221317ab2b27be79b69ad92bb3e562fba
Marcus Boerger <helly@php.net>
2006-02-06 00:49:33 +0000

    - Make all default implementations public

--- Zend/zend_interfaces.h
+++ Zend/zend_interfaces.h
@@ -49,6 +49,12 @@ ZEND_API zval* zend_call_method(zval **object_pp, zend_class_entry *obj_ce, zend
 #define zend_call_method_with_2_params(obj, obj_ce, fn_proxy, function_name, retval, arg1, arg2) \
 	zend_call_method(obj, obj_ce, fn_proxy, function_name, sizeof(function_name)-1, retval, 2, arg1, arg2 TSRMLS_CC)
 
+ZEND_API void zend_user_it_rewind(zend_object_iterator *_iter TSRMLS_DC);
+ZEND_API int zend_user_it_valid(zend_object_iterator *_iter TSRMLS_DC);
+ZEND_API int zend_user_it_get_current_key(zend_object_iterator *_iter, char **str_key, uint *str_key_len, ulong *int_key TSRMLS_DC);
+ZEND_API void zend_user_it_get_current_data(zend_object_iterator *_iter, zval ***data TSRMLS_DC);
+ZEND_API void zend_user_it_move_forward(zend_object_iterator *_iter TSRMLS_DC);
+
 ZEND_API zval *zend_user_it_new_iterator(zend_class_entry *ce, zval *object TSRMLS_DC);
 ZEND_API zend_object_iterator *zend_user_it_get_new_iterator(zend_class_entry *ce, zval *object, int by_ref TSRMLS_DC);
 
