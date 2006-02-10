4e493115574d9cbbaa3fd26a949affc3ee64997a
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-02-10 16:57:56 +0000

    MFH: fix #36359 (splFileObject::fwrite() doesn't write when no data length specified)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@206919 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -1801,13 +1801,13 @@ SPL_METHOD(SplFileObject, fwrite)
 	char *str;
 	int str_len;
 	int ret;
-	long length;
+	long length = 0;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|l", &str, &str_len, &length) == FAILURE) {
 		return;
 	}
 
-	if (ZEND_NUM_ARGS() < 2) {
+	if (ZEND_NUM_ARGS() > 1) {
 		str_len = MAX(0, MIN(length, str_len));
 	}
 	if (!str_len) {
