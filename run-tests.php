72c3fc6172df1210e65834c8e2760bd6a55c115e
Marcus Boerger <helly@php.net>
2006-02-12 18:46:07 +0000

    - Reinsert line deleted per accident

--- run-tests.php
+++ run-tests.php
@@ -184,6 +184,7 @@ More .INIs  : " , (function_exists(\'php_ini_scanned_files\') ? str_replace("\n"
 	settings2array($ini_overwrites,$info_params);
 	settings2params($info_params);
 	$php_info = `$php $pass_options $info_params $info_file`;
+	@unlink($info_file);
 	define('TESTED_PHP_VERSION', `$php -r 'echo PHP_VERSION;'`);
 
 	$unicode = `$php $pass_options $info_params -r 'echo ini_get("unicode_semantics");'`;
