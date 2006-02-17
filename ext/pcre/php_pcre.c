56fcffbec57d78f614b02698604a21ef34b28306
Andrei Zmievski <andrei@php.net>
2006-02-17 21:18:51 +0000

    MFH

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -1406,7 +1406,7 @@ PHP_FUNCTION(preg_quote)
 	
 	/* Allocate enough memory so that even if each character
 	   is quoted, we won't run out of room */
-	out_str = safe_emalloc(2, Z_STRLEN_PP(in_str_arg), 1);
+	out_str = safe_emalloc(4, Z_STRLEN_PP(in_str_arg), 1);
 	
 	/* Go through the string and quote necessary characters */
 	for(p = in_str, q = out_str; p != in_str_end; p++) {
@@ -1437,6 +1437,8 @@ PHP_FUNCTION(preg_quote)
 
 			case '\0':
 				*q++ = '\\';
+				*q++ = '0';
+				*q++ = '0';
 				*q++ = '0';
 				break;
 
