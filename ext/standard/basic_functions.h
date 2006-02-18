8ccd9215a17e19ecc41e82abdd3268612df18256
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-02-18 05:41:59 +0000

    Rename getloadavg to sys_getloadavg
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@207398 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.h
+++ ext/standard/basic_functions.h
@@ -117,7 +117,7 @@ PHP_NAMED_FUNCTION(php_if_crc32);
 PHP_FUNCTION(register_tick_function);
 PHP_FUNCTION(unregister_tick_function);
 #ifdef HAVE_GETLOADAVG
-PHP_FUNCTION(getloadavg);
+PHP_FUNCTION(sys_getloadavg);
 #endif
 
 PHP_FUNCTION(is_uploaded_file);
