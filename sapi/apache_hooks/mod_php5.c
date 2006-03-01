43ad082d6389f0296aeb0c3aa0a4b7dbda216f75
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-01 16:27:47 +0000

    Unicode support: zstr union
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@208360 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache_hooks/mod_php5.c
+++ sapi/apache_hooks/mod_php5.c
@@ -781,7 +781,7 @@ static zend_bool should_overwrite_per_dir_entry(HashTable *target_ht, php_per_di
 {
 	php_per_dir_entry *new_per_dir_entry;
 
-	if (zend_hash_find(target_ht, hash_key->arKey, hash_key->nKeyLength, (void **) &new_per_dir_entry)==FAILURE) {
+	if (zend_u_hash_find(target_ht, hash_key->type, hash_key->arKey, hash_key->nKeyLength, (void **) &new_per_dir_entry)==FAILURE) {
 		return 1; /* does not exist in dest, copy from source */
 	}
 
