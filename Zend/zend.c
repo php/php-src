7addae5089c8dc8956367efd34075e8554f3b016
Marcus Boerger <helly@php.net>
2006-03-02 00:45:28 +0000

    - MFH Use object handler get in the same way as the rest of the engine does

--- Zend/zend.c
+++ Zend/zend.c
@@ -237,16 +237,19 @@ ZEND_API void zend_make_printable_zval(zval *expr, zval *expr_copy, int *use_cop
 				} else {
 					if(Z_OBJ_HANDLER_P(expr, get)) {
 						zval *z = Z_OBJ_HANDLER_P(expr, get)(expr TSRMLS_CC);
+
+						z->refcount++;
 						if(Z_TYPE_P(z) != IS_OBJECT) {
 							zend_make_printable_zval(z, expr_copy, use_copy);
 							if (*use_copy) {
-								FREE_ZVAL(z);
+								zval_ptr_dtor(&z);
 							} else {
 								ZVAL_ZVAL(expr_copy, z, 0, 1);
 								*use_copy = 1;
 							}
 							return;
 						}
+						zval_ptr_dtor(&z);
 					}
 				}
 				if (EG(exception)) {
