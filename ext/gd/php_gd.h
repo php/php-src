f4ea8372a498643aaa8d8e366cd765e49bcd9854
Pierre Joye <pajoye@php.net>
2006-03-05 19:20:12 +0000

    - revert Harmut commit, Histogram implementation not correct and no idea   where you got the ok to add that

--- ext/gd/php_gd.h
+++ ext/gd/php_gd.h
@@ -86,7 +86,6 @@ PHP_FUNCTION(imagecolorexact);
 PHP_FUNCTION(imagecolorset);
 PHP_FUNCTION(imagecolorstotal);
 PHP_FUNCTION(imagecolorsforindex);
-PHP_FUNCTION(imagecolorhistogram);
 PHP_FUNCTION(imagecolortransparent);
 PHP_FUNCTION(imagecopy);
 PHP_FUNCTION(imagecopymerge);
