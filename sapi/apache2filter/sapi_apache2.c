87c4a01b87608b7abc12c8258aad8b052b8c5a67
Ilia Alshanetsky <iliaa@php.net>
2006-03-07 14:43:16 +0000

    Nuke safe_mode from sapi/*
    # only main/ remains at this point, this is the next step

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -406,13 +406,8 @@ static void php_apache_request_ctor(ap_filter_t *f, php_struct *ctx TSRMLS_DC)
 	apr_table_unset(f->r->headers_out, "Last-Modified");
 	apr_table_unset(f->r->headers_out, "Expires");
 	apr_table_unset(f->r->headers_out, "ETag");
-	if (!PG(safe_mode) || (PG(safe_mode) && !ap_auth_type(f->r))) {
-		auth = apr_table_get(f->r->headers_in, "Authorization");
-		php_handle_auth_data(auth TSRMLS_CC);
-	} else {
-		SG(request_info).auth_user = NULL;
-		SG(request_info).auth_password = NULL;
-	}
+	auth = apr_table_get(f->r->headers_in, "Authorization");
+	php_handle_auth_data(auth TSRMLS_CC);
 	php_request_startup(TSRMLS_C);
 }
 
