98bacb0e219eeaa865c09f17f4c07b50a848aa26
Dmitry Stogov <dmitry@php.net>
2006-03-14 15:16:07 +0000

    Fixed bug #35988 (Unknown persistent list entry type in module shutdown)

--- main/main.c
+++ main/main.c
@@ -1594,6 +1594,10 @@ void php_module_shutdown(TSRMLS_D)
 		return;
 	}
 
+#ifdef ZTS
+	ts_free_worker_threads();
+#endif
+
 #if defined(PHP_WIN32) || (defined(NETWARE) && defined(USE_WINSOCK))
 	/*close winsock */
 	WSACleanup();
