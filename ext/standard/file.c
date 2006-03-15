0c1c49adf837e8fcc5d6659c0b7dc2922fe79d15
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-15 00:28:57 +0000

    Switch (zstr) casts to use ZSTR() macro.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@209374 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -749,7 +749,7 @@ PHP_FUNCTION(file)
  		s = target_buf;
  		e = target_buf + target_len;
  	
- 		if (!(p = php_stream_locate_eol(stream, (zstr)target_buf, target_len TSRMLS_CC))) {
+ 		if (!(p = php_stream_locate_eol(stream, ZSTR(target_buf), target_len TSRMLS_CC))) {
  			p = e;
  			goto parse_eol;
 		}
