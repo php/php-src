483e10116b6114babb95c699328a2c772448788c
mike <mike@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-19 14:58:06 +0000

    MFB: fix php_apache_sapi_header_handler() modifying sapi_header
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@209649 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache2filter/sapi_apache2.c
+++ sapi/apache2filter/sapi_apache2.c
@@ -103,7 +103,7 @@ php_apache_sapi_header_handler(sapi_header_struct *sapi_header, sapi_headers_str
 {
 	php_struct *ctx;
 	ap_filter_t *f;
-	char *val;
+	char *val, *ptr;
 
 	ctx = SG(server_context);
 	f = ctx->r->output_filters;
@@ -114,6 +114,7 @@ php_apache_sapi_header_handler(sapi_header_struct *sapi_header, sapi_headers_str
 		sapi_free_header(sapi_header);
 		return 0;
 	}
+	ptr = val;
 
 	*val = '\0';
 	
@@ -128,6 +129,7 @@ php_apache_sapi_header_handler(sapi_header_struct *sapi_header, sapi_headers_str
 	else
 		apr_table_add(ctx->r->headers_out, sapi_header->header, val);
 	
+	*ptr = ':';
 	return SAPI_HEADER_ADD;
 }
 
