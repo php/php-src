70793e6c810f22501f89a8b0f11b99a5141963d3
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-22 22:06:08 +0000

    MF51: prevent segfault when exception is thrown from Countable::count()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@209909 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -318,8 +318,10 @@ PHP_FUNCTION(count)
 
 			if (Z_OBJ_HT_P(array)->get_class_entry && instanceof_function(Z_OBJCE_P(array), spl_ce_Countable TSRMLS_CC)) {
 				zend_call_method_with_0_params(&array, NULL, NULL, "count", &retval);
-				RETVAL_LONG(Z_LVAL_P(retval));
-				zval_ptr_dtor(&retval);
+				if (retval) {
+					RETVAL_LONG(Z_LVAL_P(retval));
+					zval_ptr_dtor(&retval);
+				}
 				return;
 			}
 #endif
