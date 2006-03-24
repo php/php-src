2a7f626edc36ae1d92aeacf754a8c3b4b353b23f
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-24 21:06:36 +0000

    Use intern->type for break iterator.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@210055 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/unicode/unicode_iterators.c
+++ ext/unicode/unicode_iterators.c
@@ -538,9 +538,9 @@ PHP_METHOD(TextIterator, __construct)
 		intern->flags |= ITER_REVERSE;
 	}
 
-	if (ti_type >= ITER_CHARACTER && ti_type < ITER_TYPE_LAST) {
+	if (intern->type >= ITER_CHARACTER && intern->type < ITER_TYPE_LAST) {
 		UErrorCode status = U_ZERO_ERROR;
-		intern->u.brk.iter = ubrk_open(brk_type_map[ti_type - ITER_CHARACTER], UG(default_locale), text, text_len, &status);
+		intern->u.brk.iter = ubrk_open(brk_type_map[intern->type - ITER_CHARACTER], UG(default_locale), text, text_len, &status);
 		if (!U_SUCCESS(status)) {
 			php_error(E_RECOVERABLE_ERROR, "Could not create UBreakIterator: %s", u_errorName(status));
 			return;
