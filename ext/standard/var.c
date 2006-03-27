069c73aacb254cfb0d61d42e68dbfa2e6342684b
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-27 22:16:53 +0000

    Typo.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@210251 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -607,7 +607,7 @@ PHP_FUNCTION(var_export)
 	
 	/* UTODO
 	 * We need to escape non-ASCII chars with \uXXXX format. php_var_export()
-	 * currently uses output_encoding to export Unicode strings. Support it's
+	 * currently uses output_encoding to export Unicode strings. Suppose it's
 	 * set to utf-8. If you use the result of var_export() in non-utf-8 context,
 	 * there may be trouble.
 	 */
