885388afc078b1667e1cc9b9173b2e28bc16b8cc
masugata <masugata@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-03-30 15:43:54 +0000

    added mb_stripos( ), mb_strripos( ).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@210405 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mbstring/mbstring.h
+++ ext/mbstring/mbstring.h
@@ -104,6 +104,8 @@ PHP_FUNCTION(mb_output_handler);
 PHP_FUNCTION(mb_strlen);
 PHP_FUNCTION(mb_strpos);
 PHP_FUNCTION(mb_strrpos);
+PHP_FUNCTION(mb_stripos);
+PHP_FUNCTION(mb_strripos);
 PHP_FUNCTION(mb_strstr);
 PHP_FUNCTION(mb_strrchr);
 PHP_FUNCTION(mb_substr_count);
@@ -154,6 +156,8 @@ MBSTRING_API int php_mb_encoding_converter_ex(char **str, int *len, const char *
 MBSTRING_API int php_mb_gpc_encoding_converter(char **str, int *len, int num, const char *encoding_to, const char *encoding_from TSRMLS_DC);
 
 MBSTRING_API int php_mb_gpc_encoding_detector(char **arg_string, int *arg_length, int num, char *arg_list TSRMLS_DC);
+
+MBSTRING_API int php_mb_stripos(int mode, char *old_haystack, int old_haystack_len, char *old_needle, int old_needle_len, long offset, char *from_encoding TSRMLS_DC);
 
 ZEND_BEGIN_MODULE_GLOBALS(mbstring)
 	enum mbfl_no_language language;
