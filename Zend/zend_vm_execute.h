ec12098c3b58e180334972e48c24d38f6ead66e2
Dmitry Stogov <dmitry@php.net>
2006-04-12 11:38:09 +0000

    Fixed bug #37046 (foreach breaks static scope)

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -2030,7 +2030,7 @@ static int ZEND_UNSET_VAR_SPEC_CONST_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 static int ZEND_FE_RESET_SPEC_CONST_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 {
 	zend_op *opline = EX(opline);
-
+	zend_free_op free_op1;
 	zval *array_ptr, **array_ptr_ptr;
 	HashTable *fe_ht;
 	zend_object_iterator *iter = NULL;
@@ -2071,7 +2071,20 @@ static int ZEND_FE_RESET_SPEC_CONST_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 		} else if (Z_TYPE_P(array_ptr) == IS_OBJECT) {
 			ce = Z_OBJCE_P(array_ptr);
 		} else {
-			array_ptr->refcount++;
+			if (IS_CONST == IS_VAR &&
+				free_op1.var == NULL &&
+			    !array_ptr->is_ref &&
+			    array_ptr->refcount > 1) {
+				/* non-separated return value from function */
+				zval *tmp;
+
+				ALLOC_ZVAL(tmp);
+				INIT_PZVAL_COPY(tmp, array_ptr);
+				zval_copy_ctor(tmp);
+				array_ptr = tmp;
+			} else {
+				array_ptr->refcount++;
+			}
 		}
 	}
 
@@ -4554,7 +4567,20 @@ static int ZEND_FE_RESET_SPEC_TMP_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 		} else if (Z_TYPE_P(array_ptr) == IS_OBJECT) {
 			ce = Z_OBJCE_P(array_ptr);
 		} else {
-			array_ptr->refcount++;
+			if (IS_TMP_VAR == IS_VAR &&
+				free_op1.var == NULL &&
+			    !array_ptr->is_ref &&
+			    array_ptr->refcount > 1) {
+				/* non-separated return value from function */
+				zval *tmp;
+
+				ALLOC_ZVAL(tmp);
+				INIT_PZVAL_COPY(tmp, array_ptr);
+				zval_copy_ctor(tmp);
+				array_ptr = tmp;
+			} else {
+				array_ptr->refcount++;
+			}
 		}
 	}
 
@@ -7634,7 +7660,20 @@ static int ZEND_FE_RESET_SPEC_VAR_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 		} else if (Z_TYPE_P(array_ptr) == IS_OBJECT) {
 			ce = Z_OBJCE_P(array_ptr);
 		} else {
-			array_ptr->refcount++;
+			if (IS_VAR == IS_VAR &&
+				free_op1.var == NULL &&
+			    !array_ptr->is_ref &&
+			    array_ptr->refcount > 1) {
+				/* non-separated return value from function */
+				zval *tmp;
+
+				ALLOC_ZVAL(tmp);
+				INIT_PZVAL_COPY(tmp, array_ptr);
+				zval_copy_ctor(tmp);
+				array_ptr = tmp;
+			} else {
+				array_ptr->refcount++;
+			}
 		}
 	}
 
@@ -20213,7 +20252,7 @@ static int ZEND_UNSET_VAR_SPEC_CV_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 static int ZEND_FE_RESET_SPEC_CV_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 {
 	zend_op *opline = EX(opline);
-
+	zend_free_op free_op1;
 	zval *array_ptr, **array_ptr_ptr;
 	HashTable *fe_ht;
 	zend_object_iterator *iter = NULL;
@@ -20254,7 +20293,20 @@ static int ZEND_FE_RESET_SPEC_CV_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 		} else if (Z_TYPE_P(array_ptr) == IS_OBJECT) {
 			ce = Z_OBJCE_P(array_ptr);
 		} else {
-			array_ptr->refcount++;
+			if (IS_CV == IS_VAR &&
+				free_op1.var == NULL &&
+			    !array_ptr->is_ref &&
+			    array_ptr->refcount > 1) {
+				/* non-separated return value from function */
+				zval *tmp;
+
+				ALLOC_ZVAL(tmp);
+				INIT_PZVAL_COPY(tmp, array_ptr);
+				zval_copy_ctor(tmp);
+				array_ptr = tmp;
+			} else {
+				array_ptr->refcount++;
+			}
 		}
 	}
 
