4c583d6f7327a737d3d2414b7a7a15fa47e24093
Dmitry Stogov <dmitry@php.net>
2006-04-18 06:59:31 +0000

    Fixed SUN_LEN calculation

--- sapi/cgi/fastcgi.c
+++ sapi/cgi/fastcgi.c
@@ -301,9 +301,8 @@ int fcgi_listen(const char *path, int backlog)
 		memset(&sa.sa_unix, 0, sizeof(sa.sa_unix));
 		sa.sa_unix.sun_family = AF_UNIX;
 		memcpy(sa.sa_unix.sun_path, path, path_len + 1);
-		sa_len = sizeof(sa.sa_unix.sun_family) + path_len;
+		sa_len = (size_t)(((struct sockaddr_un *)0)->sun_path)  + path_len;
 #ifdef HAVE_SOCKADDR_UN_SUN_LEN
-		sa_len += sizeof(sa.sa_unix.sun_len) + 1;
 		sa.sa_unix.sun_len = sa_len;
 #endif
 		unlink(path);
