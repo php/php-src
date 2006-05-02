30683d1ff920994b52c98082c7bb32797d40de45
andrei <andrei@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-05-02 20:58:30 +0000

    Add skeleton for character property file. Also remove some HAVE_UNICODE tests since it's non optional.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@212315 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ ext/unicode/property.c
@@ -0,0 +1,31 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 6                                                        |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Andrei Zmievski <andrei@php.net>                            |
+   +----------------------------------------------------------------------+
+ */
+
+/* $Id$ */ 
+
+#include "php_unicode.h"
+
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
