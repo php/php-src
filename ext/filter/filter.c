103da8d19d859325bf1b2f45874b0f13a66a3cbe
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-05-10 23:09:03 +0000

    auto_globals_jit support for ext/filter
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@212818 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -425,6 +425,7 @@ static void php_zval_filter_recursive(zval *value, long filter, long flags, zval
 static zval * php_filter_get_storage(long arg TSRMLS_DC) /* {{{ */
 {
 	zval * array_ptr = NULL;
+	zend_bool jit_initialization = (PG(auto_globals_jit) && !PG(register_globals) && !PG(register_long_arrays));
 	switch (arg) {
 		case PARSE_GET:
 			array_ptr = IF_G(get_array);
@@ -436,9 +437,11 @@ static zval * php_filter_get_storage(long arg TSRMLS_DC) /* {{{ */
 			array_ptr = IF_G(cookie_array);
 			break;
 		case PARSE_SERVER:
+			if(jit_initialization) zend_is_auto_global("_SERVER", sizeof("_SERVER")-1 TSRMLS_CC);
 			array_ptr = IF_G(server_array);
 			break;
 		case PARSE_ENV:
+            if(jit_initialization) zend_is_auto_global("_ENV", sizeof("_ENV")-1 TSRMLS_CC);
 			array_ptr = IF_G(env_array);
 			break;
 	}
@@ -533,8 +536,8 @@ PHP_FUNCTION(input_get)
 			break;
 
 		case PARSE_REQUEST:
-			/* FIXME: Implement session source */
-			php_error_docref(NULL TSRMLS_CC, E_WARNING, "INPUT_SESSION not implemented");
+			/* FIXME: Implement request source */
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "INPUT_REQUEST not implemented");
 			return;
 			if (PG(variables_order)) {
 				zval **a_ptr = &array_ptr;
