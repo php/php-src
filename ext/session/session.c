adcd6606656e279d005078e056677c923eda782e
Marcus Boerger <helly@php.net>
2006-05-18 22:16:27 +0000

    - MFH Fix bug #37510 session_regenerate_id changes session_id() even on failure

--- ext/session/session.c
+++ ext/session/session.c
@@ -1351,6 +1351,10 @@ PHP_FUNCTION(session_id)
    Update the current session id with a newly generated one. */
 PHP_FUNCTION(session_regenerate_id)
 {
+	if (SG(headers_sent)) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot regenerate session id - headers already sent");
+		RETURN_FALSE;
+	}
 	if (PS(session_status) == php_session_active) {
 		if (PS(id)) efree(PS(id));
 	
@@ -1780,6 +1784,6 @@ PHP_MINFO_FUNCTION(session)
  * tab-width: 4
  * c-basic-offset: 4
  * End:
- * vim600: sw=4 ts=4 fdm=marker
+ * vim600: noet sw=4 ts=4 fdm=marker
  * vim<600: sw=4 ts=4
  */
