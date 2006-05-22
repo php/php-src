b40921518f9d89e319277048e3e7adb2c53659ee
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-05-22 17:09:05 +0000

    MFH: move document property struct from xml_common.h
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@213551 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/libxml/libxml.c
+++ ext/libxml/libxml.c
@@ -942,6 +942,10 @@ int php_libxml_decrement_doc_ref(php_libxml_node_object *object TSRMLS_DC) {
 				xmlFreeDoc((xmlDoc *) object->document->ptr);
 			}
 			if (object->document->doc_props != NULL) {
+				if (object->document->doc_props->classmap) {
+					zend_hash_destroy(object->document->doc_props->classmap);
+					FREE_HASHTABLE(object->document->doc_props->classmap);
+				}
 				efree(object->document->doc_props);
 			}
 			efree(object->document);
