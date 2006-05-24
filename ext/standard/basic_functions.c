53988d8cf55468411da16fc6e90dc793c9ee314d
Ilia Alshanetsky <iliaa@php.net>
2006-05-24 23:14:08 +0000

    Fixed bug #37563 (array_key_exists performance is poor for &$array).

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -815,7 +815,7 @@ zend_function_entry basic_functions[] = {
 	PHP_FE(array_map,														NULL)
 	PHP_FE(array_chunk,														NULL)
 	PHP_FE(array_combine,													NULL)
-	PHP_FE(array_key_exists,												NULL)
+	PHP_FE(array_key_exists,												all_args_prefer_ref)
 
 	/* aliases from array.c */
 	PHP_FALIAS(pos, 				current, 				 first_arg_force_ref)
