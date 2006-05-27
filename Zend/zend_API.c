ad67b773d8403b265970285e36b1325e533d14c0
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-05-27 20:16:53 +0000

    - Revert until there is an ok for the full patch
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@213837 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -547,21 +547,6 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 			}
 			break;
 
-		case 'f':
-			{
-				zend_fcall_info       *fci = va_arg(*va, zend_fcall_info *);
-				zend_fcall_info_cache *fcc = va_arg(*va, zend_fcall_info_cache *);
-
-				if (zend_fcall_info_init(*arg, fci, fcc TSRMLS_CC) == SUCCESS) {
-					break;
-				} else if (return_null) {
-					fci->size = 0;
-					fcc->initialized = 0;
-					break;
-				} else {
-					return "function";
-				}
-			}
 
 		case 'z':
 			{
