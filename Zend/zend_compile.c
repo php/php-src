e7b7d8a3a80c555d256160514fea88371e7703f4
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-05-30 23:10:28 +0000

    - Switch back to old severities and still keep bugfix for   #37632 (Protected method access problem) # It wasn't really my intention to change the severity. We simply didn't # have tests for that.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@214003 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2028,12 +2028,12 @@ static zend_bool do_inherit_method_check(HashTable *child_function_table, zend_f
 	if (parent_flags & ZEND_ACC_ABSTRACT) {
 		child->common.fn_flags |= ZEND_ACC_IMPLEMENTED_ABSTRACT;
 		child->common.prototype = parent;
-	} else if (!(parent->common.fn_flags & ZEND_ACC_CTOR) || (parent->common.prototype && parent->common.prototype->common.scope->ce_flags && ZEND_ACC_INTERFACE)) {
+	} else if (!(parent->common.fn_flags & ZEND_ACC_CTOR) || (parent->common.prototype && (parent->common.prototype->common.scope->ce_flags & ZEND_ACC_INTERFACE))) {
 		/* ctors only have a prototype if it comes from an interface */
 		child->common.prototype = parent->common.prototype ? parent->common.prototype : parent;
 	}
 
-	if (child->common.prototype) {
+	if (child->common.prototype && (child->common.prototype->common.fn_flags & ZEND_ACC_ABSTRACT)) {
 		if (!zend_do_perform_implementation_check(child, child->common.prototype)) {
 			zend_error(E_COMPILE_ERROR, "Declaration of %s::%s() must be compatible with that of %s::%s()", ZEND_FN_SCOPE_NAME(child), child->common.function_name, ZEND_FN_SCOPE_NAME(child->common.prototype), child->common.prototype->common.function_name);
 		}
