c270a8e627fb5159eff18b9c5317808e5e358df8
Ilia Alshanetsky <iliaa@php.net>
2006-06-01 22:42:53 +0000

    MFB: fixed compiler warning

--- ext/standard/filters.c
+++ ext/standard/filters.c
@@ -1961,7 +1961,7 @@ static php_stream_filter *consumed_filter_create(const char *filtername, zval *f
 	/* Create this filter */
 	data = pecalloc(1, sizeof(php_consumed_filter_data), persistent);
 	if (!data) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed allocating %d bytes.", sizeof(php_consumed_filter_data));
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed allocating %ld bytes.", sizeof(php_consumed_filter_data));
 		return NULL;
 	}
 	data->persistent = persistent;
