0f6e901ff50161bd3ad04d91208657a5863ff3f8
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-06-02 21:55:26 +0000

    fix error messages in Unicode mode
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@214167 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dom/php_dom.c
+++ ext/dom/php_dom.c
@@ -307,7 +307,7 @@ zval *dom_read_property(zval *object, zval *member, int type TSRMLS_DC)
 	if (obj->prop_handler != NULL) {
 		ret = zend_u_hash_find(obj->prop_handler, Z_TYPE_P(member), Z_UNIVAL_P(member), Z_UNILEN_P(member)+1, (void **) &hnd);
 	} else if (instanceof_function(obj->std.ce, dom_node_class_entry TSRMLS_CC)) {
-		php_error(E_WARNING, "Couldn't fetch %s. Node no longer exists", obj->std.ce->name);
+		php_error(E_WARNING, "Couldn't fetch %v. Node no longer exists", obj->std.ce->name);
 	}
 	if (ret == SUCCESS) {
 		ret = hnd->read_func(obj, &retval TSRMLS_CC);
