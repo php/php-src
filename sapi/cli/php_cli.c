9c5fdd1b7f7b75bc7b767583d0e806c91d1132dc
Johannes Schlüter <johannes@php.net>
2006-06-03 22:00:30 +0000

    - Removed useless -g command line option from CGI and CLI - Fixed conflicting behaviour if -B and -a are set with CLI - Fixed handling of auto_prepend and exceptions in readline-a-mode

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -130,7 +130,6 @@ static const opt_struct OPTIONS[] = {
 	{'e', 0, "profile-info"},
 	{'F', 1, "process-file"},
 	{'f', 1, "file"},
-	{'g', 1, "global"},
 	{'h', 0, "help"},
 	{'i', 0, "info"},
 	{'l', 0, "syntax-check"},
@@ -424,7 +423,7 @@ static void php_cli_usage(char *argv0)
 				"  -n               No php.ini file will be used\n"
 				"  -d foo[=bar]     Define INI entry foo with value 'bar'\n"
 				"  -e               Generate extended information for debugger/profiler\n"
-				"  -f <file>        Parse <file>.\n"
+				"  -f <file>        Parse and execute <file>.\n"
 				"  -h               This help\n"
 				"  -i               PHP information\n"
 				"  -l               Syntax check only (lint)\n"
@@ -452,21 +451,6 @@ static void php_cli_usage(char *argv0)
 				, prog, prog, prog, prog, prog, prog);
 }
 /* }}} */
-
-static void php_register_command_line_global_vars(char **arg TSRMLS_DC)
-{
-	char *var, *val;
-
-	var = *arg;
-	val = strchr(var, '=');
-	if (!val) {
-		printf("No value specified for variable '%s'\n", var);
-	} else {
-		*val++ = '\0';
-		php_register_variable(var, val, NULL TSRMLS_CC);
-	}
-	efree(*arg);
-}
 
 static php_stream *s_in_process = NULL;
 
@@ -577,7 +561,6 @@ int main(int argc, char *argv[])
 	char *orig_optarg=php_optarg;
 	char *arg_free=NULL, **arg_excp=&arg_free;
 	char *script_file=NULL;
-	zend_llist global_vars;
 	int interactive=0;
 	int module_started = 0;
 	int lineno = 0;
@@ -699,8 +682,6 @@ int main(int argc, char *argv[])
 	module_started = 1;
 
 	zend_first_try {
-		zend_llist_init(&global_vars, sizeof(char *), NULL, 0);
-
 		CG(in_compilation) = 0; /* not initialized but needed for several options */
 		EG(uninitialized_zval_ptr) = NULL;
 
@@ -782,12 +763,11 @@ int main(int argc, char *argv[])
 
 			case 'a':	/* interactive mode */
 				if (!interactive) {
-#if (HAVE_LIBREADLINE || HAVE_LIBEDIT) && !defined(COMPILE_DL_READLINE)
-					printf("Interactive shell\n\n");
-#else
-					printf("Interactive mode enabled\n\n");
-#endif
-					fflush(stdout);
+					if (behavior != PHP_MODE_STANDARD) {
+						param_error = param_mode_conflict;
+						break;
+					}
+
 					interactive=1;
 				}
 				break;
@@ -825,14 +805,6 @@ int main(int argc, char *argv[])
 				script_file = php_optarg;
 				break;
 
-			case 'g': /* define global variables on command line */
-				{
-					char *arg = estrdup(php_optarg);
-
-					zend_llist_add_element(&global_vars, &arg);
-				}
-				break;
-
 			case 'l': /* syntax check mode */
 				if (behavior != PHP_MODE_STANDARD) {
 					break;
@@ -860,7 +832,7 @@ int main(int argc, char *argv[])
 						param_error = "You can use -r only once.\n";
 						break;
 					}
-				} else if (behavior != PHP_MODE_STANDARD) {
+				} else if (behavior != PHP_MODE_STANDARD || interactive) {
 					param_error = param_mode_conflict;
 					break;
 				}
@@ -888,7 +860,7 @@ int main(int argc, char *argv[])
 						param_error = "You can use -B only once.\n";
 						break;
 					}
-				} else if (behavior != PHP_MODE_STANDARD) {
+				} else if (behavior != PHP_MODE_STANDARD || interactive) {
 					param_error = param_mode_conflict;
 					break;
 				}
@@ -902,7 +874,7 @@ int main(int argc, char *argv[])
 						param_error = "You can use -E only once.\n";
 						break;
 					}
-				} else if (behavior != PHP_MODE_STANDARD) {
+				} else if (behavior != PHP_MODE_STANDARD || interactive) {
 					param_error = param_mode_conflict;
 					break;
 				}
@@ -956,6 +928,15 @@ int main(int argc, char *argv[])
 			PUTS(param_error);
 			exit_status=1;
 			goto err;
+		}
+
+		if (interactive) {
+#if (HAVE_LIBREADLINE || HAVE_LIBEDIT) && !defined(COMPILE_DL_READLINE)
+			printf("Interactive shell\n\n");
+#else
+			printf("Interactive mode enabled\n\n");
+#endif
+			fflush(stdout);
 		}
 
 		CG(interactive) = interactive;
@@ -1014,10 +995,7 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		/* This actually destructs the elements of the list - ugly hack */
 		zend_is_auto_global("_SERVER", sizeof("_SERVER")-1 TSRMLS_CC);
-		zend_llist_apply(&global_vars, (llist_apply_func_t) php_register_command_line_global_vars TSRMLS_CC);
-		zend_llist_destroy(&global_vars);
 
 		PG(during_request_startup) = 0;
 		switch (behavior) {
@@ -1033,6 +1011,19 @@ int main(int argc, char *argv[])
 				char *code = emalloc(size);
 				char *prompt = "php > ";
 				char *history_file;
+
+				if (PG(auto_prepend_file) && PG(auto_prepend_file)[0]) {
+					zend_file_handle *prepend_file_p;
+					zend_file_handle prepend_file = {0};
+
+					prepend_file.filename = PG(auto_prepend_file);
+					prepend_file.opened_path = NULL;
+					prepend_file.free_filename = 0;
+					prepend_file.type = ZEND_HANDLE_FILENAME;
+					prepend_file_p = &prepend_file;
+
+					zend_execute_scripts(ZEND_REQUIRE TSRMLS_CC, NULL, 1, prepend_file_p);
+				}
 
 				history_file = tilde_expand("~/.php_history");
 				rl_attempted_completion_function = cli_code_completion;
@@ -1077,6 +1068,11 @@ int main(int argc, char *argv[])
 					if (php_last_char != '\0' && php_last_char != '\n') {
 						sapi_cli_single_write("\n", 1);
 					}
+
+					if (EG(exception)) {
+						zend_exception_error(EG(exception) TSRMLS_CC);
+					}
+
 					php_last_char = '\0';
 				}
 				write_history(history_file);
