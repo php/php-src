de39078f7bce68aa5f9bb0fa762cd0eb84324548
Marcus Boerger <helly@php.net>
2006-06-07 09:22:08 +0000

    - Regenerated after zend_vm_def.h change

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -519,8 +519,6 @@ static int ZEND_ADD_INTERFACE_SPEC_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 		zend_error_noreturn(E_ERROR, "%s cannot implement %s - it is not an interface", ce->name, iface->name);
 	}
 
-	ce->interfaces[opline->extended_value] = iface;
-
 	zend_do_implement_interface(ce, iface TSRMLS_CC);
 
 	ZEND_VM_NEXT_OPCODE();
