a5f2e29f36cd9e8981aaadb4fcd805b12a044b89
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-06-07 21:09:52 +0000

    Get rid of useless function calls
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@214418 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/pdo.c
+++ ext/pdo/pdo.c
@@ -142,8 +142,8 @@ zend_module_entry pdo_module_entry = {
 	pdo_functions,
 	PHP_MINIT(pdo),
 	PHP_MSHUTDOWN(pdo),
-	PHP_RINIT(pdo),
-	PHP_RSHUTDOWN(pdo),
+	NULL,
+	NULL,
 	PHP_MINFO(pdo),
 	"1.0.4dev",
 	STANDARD_MODULE_PROPERTIES
@@ -349,22 +349,6 @@ PHP_MSHUTDOWN_FUNCTION(pdo)
 	UNREGISTER_INI_ENTRIES();
 	zend_hash_destroy(&pdo_driver_hash);
 	pdo_sqlstate_fini_error_table();
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_RINIT_FUNCTION */
-PHP_RINIT_FUNCTION(pdo)
-{
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_RSHUTDOWN_FUNCTION */
-PHP_RSHUTDOWN_FUNCTION(pdo)
-{
-	/* TODO: visit persistent handles: for each persistent statement handle,
-	 * remove bound parameter associations */
 	return SUCCESS;
 }
 /* }}} */
