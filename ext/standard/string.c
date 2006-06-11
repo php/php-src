53d262c04da1f7bc7b550b9785fa7a4f94cef4cc
Hannes Magnusson <bjori@php.net>
2006-06-11 20:33:29 +0000

    Nuke unused var

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -3520,7 +3520,7 @@ PHPAPI char *php_strtr(char *str, int len, char *str_from, char *str_to, int trl
  */
 PHPAPI UChar *php_u_strtr(UChar *str, int len, UChar *str_from, int str_from_len, UChar *str_to, int str_to_len, int trlen, int *outlen TSRMLS_DC)
 {
-	int i, j;
+	int i;
 	int can_optimize = 1;
 	
 	if ((trlen < 1) || (len < 1)) {
