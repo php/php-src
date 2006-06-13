cd54f8ef044b86047acbd665e7f405949d1253ea
Dmitry Stogov <dmitry@php.net>
2006-06-13 13:55:32 +0000

    Removed warning

--- sapi/cgi/fastcgi.c
+++ sapi/cgi/fastcgi.c
@@ -719,7 +719,7 @@ static inline void close_packet(fcgi_request *req)
 	if (req->out_hdr) {
 		int len = req->out_pos - ((unsigned char*)req->out_hdr + sizeof(fcgi_header));
 
-		req->out_pos += fcgi_make_header(req->out_hdr, req->out_hdr->type, req->id, len);
+		req->out_pos += fcgi_make_header(req->out_hdr, (fcgi_request_type)req->out_hdr->type, req->id, len);
 		req->out_hdr = NULL;
 	}
 }
