bf74742b49043fdc226a1ba171413e2b0ef24157
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-06-15 22:45:30 +0000

    MFH: Optimize the other string conversion functions here to just create raw text nodes.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@214883 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -779,7 +779,7 @@ static xmlNodePtr to_xml_string(encodeTypePtr type, zval *data, int style, xmlNo
 
 static xmlNodePtr to_xml_base64(encodeTypePtr type, zval *data, int style, xmlNodePtr parent)
 {
-	xmlNodePtr ret;
+	xmlNodePtr ret, text;
 	unsigned char *str;
 	int str_len;
 
@@ -789,7 +789,8 @@ static xmlNodePtr to_xml_base64(encodeTypePtr type, zval *data, int style, xmlNo
 
 	if (Z_TYPE_P(data) == IS_STRING) {
 		str = php_base64_encode((unsigned char*)Z_STRVAL_P(data), Z_STRLEN_P(data), &str_len);
-		xmlNodeSetContentLen(ret, str, str_len);
+		text = xmlNewTextLen(str, str_len);
+		xmlAddChild(ret, text);
 		efree(str);
 	} else {
 		zval tmp = *data;
@@ -797,7 +798,8 @@ static xmlNodePtr to_xml_base64(encodeTypePtr type, zval *data, int style, xmlNo
 		zval_copy_ctor(&tmp);
 		convert_to_string(&tmp);
 		str = php_base64_encode((unsigned char*)Z_STRVAL(tmp), Z_STRLEN(tmp), &str_len);
-		xmlNodeSetContentLen(ret, str, str_len);
+		text = xmlNewTextLen(str, str_len);
+		xmlAddChild(ret, text);
 		efree(str);
 		zval_dtor(&tmp);
 	}
@@ -811,7 +813,7 @@ static xmlNodePtr to_xml_base64(encodeTypePtr type, zval *data, int style, xmlNo
 static xmlNodePtr to_xml_hexbin(encodeTypePtr type, zval *data, int style, xmlNodePtr parent)
 {
 	static char hexconvtab[] = "0123456789ABCDEF";
-	xmlNodePtr ret;
+	xmlNodePtr ret, text;
 	unsigned char *str;
 	zval tmp;
 	int i, j;
@@ -834,7 +836,8 @@ static xmlNodePtr to_xml_hexbin(encodeTypePtr type, zval *data, int style, xmlNo
 	}
 	str[j] = '\0';
 
-	xmlNodeSetContentLen(ret, str, Z_STRLEN_P(data) * 2 * sizeof(char));
+	text = xmlNewTextLen(str, Z_STRLEN_P(data) * 2 * sizeof(char));
+	xmlAddChild(ret, text);
 	efree(str);
 	if (data == &tmp) {
 		zval_dtor(&tmp);
