a2b61371cd9f3ef1371aa0cd690e32ee124fb1df
Ilia Alshanetsky <iliaa@php.net>
2006-06-23 11:40:14 +0000

    MFB: Change E_ERROR to E_RECOVERABLE_ERROR

--- ext/standard/assert.c
+++ ext/standard/assert.c
@@ -161,7 +161,7 @@ PHP_FUNCTION(assert)
 		compiled_string_description = zend_make_compiled_string_description("assert code" TSRMLS_CC);
 		if (zend_eval_string(myeval, &retval, compiled_string_description TSRMLS_CC) == FAILURE) {
 			efree(compiled_string_description);
-			php_error_docref(NULL TSRMLS_CC, E_ERROR, "Failure evaluating code: %s%s", PHP_EOL, myeval);
+			php_error_docref(NULL TSRMLS_CC, E_RECOVERABLE_ERROR, "Failure evaluating code: %s%s", PHP_EOL, myeval);
 			/* php_error_docref() does not return in this case. */
 		}
 		if (free_tmp) {
