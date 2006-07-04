3590cfca720f94dc72a67b3c42c21298179168c3
bjori <bjori@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-07-04 15:33:57 +0000

    MFH: Fix protos&arginfo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@215736 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -2944,7 +2944,7 @@ static int _addmethod(zend_function *mptr, int num_args, va_list args, zend_hash
 }
 /* }}} */
 
-/* {{{ proto public ReflectionMethod[] ReflectionClass::getMethods()
+/* {{{ proto public ReflectionMethod[] ReflectionClass::getMethods([long $filter])
    Returns an array of this class' methods */
 ZEND_METHOD(reflection_class, getMethods)
 {
@@ -3100,7 +3100,7 @@ static int _adddynproperty(zval **pptr, int num_args, va_list args, zend_hash_ke
 }
 /* }}} */
 
-/* {{{ proto public ReflectionProperty[] ReflectionClass::getProperties()
+/* {{{ proto public ReflectionProperty[] ReflectionClass::getProperties([long $filter])
    Returns an array of this class' properties */
 ZEND_METHOD(reflection_class, getProperties)
 {
@@ -3349,7 +3349,7 @@ ZEND_METHOD(reflection_class, newInstance)
 }
 /* }}} */
 
-/* {{{ proto public stdclass ReflectionClass::newInstanceArgs(array args)
+/* {{{ proto public stdclass ReflectionClass::newInstanceArgs([array args])
    Returns an instance of this class */
 ZEND_METHOD(reflection_class, newInstanceArgs)
 {
@@ -4413,6 +4413,11 @@ ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_getMethod, 0)
 ZEND_END_ARG_INFO()
 
 static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_reflection_class_getMethods, 0, 0, 0)
+	ZEND_ARG_INFO(0, $filter)
+ZEND_END_ARG_INFO()
+
+static
 ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_hasProperty, 0)
 	ZEND_ARG_INFO(0, name)
 ZEND_END_ARG_INFO()
@@ -4420,6 +4425,11 @@ ZEND_END_ARG_INFO()
 static
 ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_getProperty, 0)
 	ZEND_ARG_INFO(0, name)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_getProperties, 0, 0, 0)
+	ZEND_ARG_INFO(0, filter)
 ZEND_END_ARG_INFO()
 
 static
@@ -4443,7 +4453,7 @@ ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_newInstance, 0)
 ZEND_END_ARG_INFO()
 
 static
-ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_newInstanceArgs, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_reflection_class_newInstanceArgs, 0, 0, 0)
 	ZEND_ARG_ARRAY_INFO(0, args, 0)
 ZEND_END_ARG_INFO()
 
@@ -4473,10 +4483,10 @@ static zend_function_entry reflection_class_functions[] = {
 	ZEND_ME(reflection_class, getConstructor, NULL, 0)
 	ZEND_ME(reflection_class, hasMethod, arginfo_reflection_class_hasMethod, 0)
 	ZEND_ME(reflection_class, getMethod, arginfo_reflection_class_getMethod, 0)
-	ZEND_ME(reflection_class, getMethods, NULL, 0)
+	ZEND_ME(reflection_class, getMethods, arginfo_reflection_class_getMethods, 0)
 	ZEND_ME(reflection_class, hasProperty, arginfo_reflection_class_hasProperty, 0)
 	ZEND_ME(reflection_class, getProperty, arginfo_reflection_class_getProperty, 0)
-	ZEND_ME(reflection_class, getProperties, NULL, 0)
+	ZEND_ME(reflection_class, getProperties, arginfo_reflection_class_getProperties, 0)
 	ZEND_ME(reflection_class, hasConstant, arginfo_reflection_class_hasConstant, 0)
 	ZEND_ME(reflection_class, getConstants, NULL, 0)
 	ZEND_ME(reflection_class, getConstant, arginfo_reflection_class_getConstant, 0)
