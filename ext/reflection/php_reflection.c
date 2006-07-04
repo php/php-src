2a18040cb53c5ce525648de7044238b823225f2b
Hannes Magnusson <bjori@php.net>
2006-07-04 15:37:13 +0000

    Obs. ZEND_BEGIN_ARG_INFO => ZEND_BEGIN_ARG_INFO_EX

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -4499,7 +4499,7 @@ ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_getProperty, 0)
 ZEND_END_ARG_INFO()
 
 static
-ZEND_BEGIN_ARG_INFO(arginfo_reflection_class_getProperties, 0, 0, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_reflection_class_getProperties, 0, 0, 0)
 	ZEND_ARG_INFO(0, filter)
 ZEND_END_ARG_INFO()
 
