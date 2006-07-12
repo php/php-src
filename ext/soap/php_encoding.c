6a12b2de747fc2158be6b8480f92d79d1ea40c5e
Dmitry Stogov <dmitry@php.net>
2006-07-12 09:00:15 +0000

    Fixed bug #38067 (Parameters are not decoded from utf-8 when using encoding option)

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -1349,12 +1349,12 @@ static zval *to_zval_object_ex(encodeTypePtr type, xmlNodePtr data, zend_class_e
 						str_val = (*attr)->def;
 					}
 					if (str_val) {
-						zval zstr;
-						xmlNodePtr dummy;
+						xmlNodePtr dummy, text;
 						zval *data;
 
-						ZVAL_STRING(&zstr, str_val, 0);
-						dummy = master_to_xml(get_conversion(IS_STRING), &zstr, SOAP_LITERAL, NULL);
+						dummy = xmlNewNode(NULL, BAD_CAST("BOGUS"));
+						text = xmlNewText(BAD_CAST(str_val));
+						xmlAddChild(dummy, text);
 						data = master_to_zval((*attr)->encode, dummy);
 						xmlFreeNode(dummy);
 						set_zval_property(ret, (*attr)->name, data TSRMLS_CC);
