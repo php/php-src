513e2b31f7a9d18e9d22160442e9f29d130953e3
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-07-12 13:08:38 +0000

    bailout on error and don't try to create fields of unknown type add test
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@216235 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dbase/dbase.c
+++ ext/dbase/dbase.c
@@ -713,6 +713,8 @@ PHP_FUNCTION(dbase_create)
 			break;
 		default:
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "unknown field type '%c'", cur_f->db_type);
+			free_dbf_head(dbh);
+			RETURN_FALSE;
 		}
 		cur_f->db_foffset = rlen;
 		rlen += cur_f->db_flen;
