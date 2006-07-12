2ea07900963706f3acceacf6aa12ad7ef85053b3
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-07-12 18:10:56 +0000

    MFH: initialize EG(exit_status) in ZTS mode
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@216255 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend.c
+++ Zend/zend.c
@@ -503,6 +503,7 @@ static void executor_globals_ctor(zend_executor_globals *executor_globals TSRMLS
 	EG(in_autoload) = NULL;
 	EG(current_execute_data) = NULL;
 	EG(current_module) = NULL;
+	EG(exit_status) = 0;
 }
 
 
