004ed49028451d6f05c43597861babaa0dd89631
Pierre Joye <pajoye@php.net>
2006-07-17 15:08:46 +0000

    - MFB: #38112, corrupt GIF Image segfault

--- ext/gd/libgd/gd_gif_in.c
+++ ext/gd/libgd/gd_gif_in.c
@@ -216,6 +216,12 @@ terminated:
 	if (!im) {
 		return 0;
 	}
+
+	if (!im->colorsTotal) {
+		gdImageDestroy(im);
+		return 0;
+	}
+
 	/* Check for open colors at the end, so
 	   we can reduce colorsTotal and ultimately
 	   BitsPerPixel */
@@ -499,6 +505,18 @@ static void ReadImage(gdImagePtr im, gdIOCtx *fd, int len, int height, unsigned 
 	int             v;
 	int             xpos = 0, ypos = 0, pass = 0;
 	int i;
+
+	/*
+	 **  Initialize the Compression routines
+	 */
+	if (! ReadOK(fd,&c,1)) {
+		return;
+	}
+
+	if (c > MAX_LWZ_BITS) {
+		return;	
+	}
+
 	/* Stash the color map into the image */
 	for (i=0; (i<gdMaxColors); i++) {
 		im->red[i] = cmap[CM_RED][i];
@@ -508,12 +526,6 @@ static void ReadImage(gdImagePtr im, gdIOCtx *fd, int len, int height, unsigned 
 	}
 	/* Many (perhaps most) of these colors will remain marked open. */
 	im->colorsTotal = gdMaxColors;
-	/*
-	 **  Initialize the Compression routines
-	 */
-	if (! ReadOK(fd,&c,1)) {
-		return;
-	}
 	if (LWZReadByte(fd, TRUE, c) < 0) {
 		return;
 	}
