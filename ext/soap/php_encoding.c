d0f2f6cd0467baaae6ff33c019bc635f8dde3ef3
Marcus Boerger <helly@php.net>
2006-07-24 18:03:45 +0000

    - MFH zend_unmangle_property_name(), drop zend_unmangle_property_name_ex()

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -1769,7 +1769,7 @@ static xmlNodePtr to_xml_object(encodeTypePtr type, zval *data, int style, xmlNo
 					if (Z_TYPE_P(data) == IS_OBJECT) {
 						char *class_name;
 
-						zend_unmangle_property_name_ex(str_key, str_key_len, &class_name, &prop_name);
+						zend_unmangle_property_name(str_key, str_key_len-1, &class_name, &prop_name);
 					} else {
 						prop_name = str_key;
 					}
