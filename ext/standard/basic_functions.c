d5ec3193679a0c25e338c42b4143f76007f9cec7
Hannes Magnusson <bjori@php.net>
2006-07-25 16:41:04 +0000

    MFH: Update protos & arginfo for memory_get_usage() & memory_get_peak_usage()

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -3054,11 +3054,13 @@ ZEND_END_ARG_INFO()
 
 #if MEMORY_LIMIT
 static
-ZEND_BEGIN_ARG_INFO(arginfo_memory_get_usage, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_memory_get_usage, 0, 0, 0)
+	ZEND_ARG_INFO(0, real_usage)
 ZEND_END_ARG_INFO()
 
 static
-ZEND_BEGIN_ARG_INFO(arginfo_memory_get_peak_usage, 0)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_memory_get_peak_usage, 0, 0, 0)
+	ZEND_ARG_INFO(0, real_usage)
 ZEND_END_ARG_INFO()
 #endif
 /* }}} */
