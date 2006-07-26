e1dfc4259f636deae6115a344b3d916c63f0f102
Antony Dovgal <tony2001@php.net>
2006-07-26 13:22:06 +0000

    eliminate compilation warnings on AIX

--- main/rfc1867.h
+++ main/rfc1867.h
@@ -71,6 +71,6 @@ SAPI_API SAPI_POST_HANDLER_FUNC(rfc1867_post_handler);
 
 void destroy_uploaded_files_hash(TSRMLS_D);
 void php_rfc1867_register_constants(TSRMLS_D);
-PHPAPI int (*php_rfc1867_callback)(unsigned int event, void *event_data, void **extra TSRMLS_DC);
+extern PHPAPI int (*php_rfc1867_callback)(unsigned int event, void *event_data, void **extra TSRMLS_DC);
 
 #endif /* RFC1867_H */
