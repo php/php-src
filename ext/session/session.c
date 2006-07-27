e5a1182304bcbfe42e3fd61fc9ccdf04e20209f7
Ilia Alshanetsky <iliaa@php.net>
2006-07-27 14:00:13 +0000

    Fixed bug #38224 (session extension can't handle broken cookies).

--- ext/session/session.c
+++ ext/session/session.c
@@ -745,6 +745,7 @@ static void php_session_initialize(TSRMLS_D)
 {
 	char *val;
 	int vallen;
+	zend_bool new = 0;
 
 	/* check session name for invalid characters */
 	if (PS(id) && strpbrk(PS(id), "\r\n\t <>'\"\\")) {
@@ -764,8 +765,15 @@ static void php_session_initialize(TSRMLS_D)
 	}
 	
 	/* If there is no ID, use session module to create one */
-	if (!PS(id))
+	if (!PS(id)) {
+new_session:
 		PS(id) = PS(mod)->s_create_sid(&PS(mod_data), NULL TSRMLS_CC);
+php_error_docref(NULL TSRMLS_CC, E_WARNING, "Making a new session %s.", PS(id));
+		if (PS(use_cookies)) {
+			PS(send_cookie) = 1;
+		}
+		new = 1;
+	}
 	
 	/* Read data */
 	/* Question: if you create a SID here, should you also try to read data?
@@ -777,6 +785,8 @@ static void php_session_initialize(TSRMLS_D)
 	if (PS(mod)->s_read(&PS(mod_data), PS(id), &val, &vallen TSRMLS_CC) == SUCCESS) {
 		php_session_decode(val, vallen TSRMLS_CC);
 		efree(val);
+	} else if (!new) {
+		goto new_session;
 	}
 }
 
