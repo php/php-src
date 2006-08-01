ab043daf0dd35b72fcadd33de564896fffe93295
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-01 16:31:29 +0000

    Fixed bug #38168 (Crash in pdo_pgsql on missing bound parameters).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@217397 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_pgsql/pgsql_statement.c
+++ ext/pdo_pgsql/pgsql_statement.c
@@ -247,16 +247,16 @@ static int pgsql_stmt_param_hook(pdo_stmt_t *stmt, struct pdo_bound_param_data *
 			case PDO_PARAM_EVT_EXEC_PRE:
 				if (!S->param_values) {
 					S->param_values = ecalloc(
-							zend_hash_num_elements(stmt->bound_params),
+							zend_hash_num_elements(stmt->bound_param_map),
 							sizeof(char*));
 					S->param_lengths = ecalloc(
-							zend_hash_num_elements(stmt->bound_params),
+							zend_hash_num_elements(stmt->bound_param_map),
 							sizeof(int));
 					S->param_formats = ecalloc(
-							zend_hash_num_elements(stmt->bound_params),
+							zend_hash_num_elements(stmt->bound_param_map),
 							sizeof(int));
 					S->param_types = ecalloc(
-							zend_hash_num_elements(stmt->bound_params),
+							zend_hash_num_elements(stmt->bound_param_map),
 							sizeof(Oid));
 				}
 				if (param->paramno >= 0) {
