0c4ef446e2cdf23590f41cd785637c1bd7516ebd
Antony Dovgal <tony2001@php.net>
2006-08-02 09:16:52 +0000

    MFH: fix #38289 (segfault in session_decode() when _SESSION is NULL)

--- ext/session/session.c
+++ ext/session/session.c
@@ -285,9 +285,13 @@ typedef struct {
 PHPAPI void php_add_session_var(char *name, size_t namelen TSRMLS_DC)
 {
 	zval **sym_track = NULL;
-	
-	zend_hash_find(Z_ARRVAL_P(PS(http_session_vars)), name, namelen + 1, 
-			(void *) &sym_track);
+
+	IF_SESSION_VARS() {
+		zend_hash_find(Z_ARRVAL_P(PS(http_session_vars)), name, namelen + 1, 
+				(void *) &sym_track);
+	} else {
+		return;
+	}
 
 	/*
 	 * Set up a proper reference between $_SESSION["x"] and $x.
