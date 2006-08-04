67cf28d8403408b4f66f5d85eba907396b86c539
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-04 20:34:37 +0000

    MFB: Fixed bug #37265 (Added missing safe_mode & open_basedir checks to imap_body()).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@217524 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -763,6 +763,11 @@ static void php_imap_do_open(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 		efree(IMAPG(imap_password));
 	}
 
+	/* local filename, need to perform open_basedir checks */
+	if (Z_STRVAL_PP(mailbox)[0] != '{' && php_check_open_basedir(Z_STRVAL_PP(mailbox) TSRMLS_CC)) {
+		RETURN_FALSE;
+	}
+
 	IMAPG(imap_user)     = estrndup(Z_STRVAL_PP(user), Z_STRLEN_PP(user));
 	IMAPG(imap_password) = estrndup(Z_STRVAL_PP(passwd), Z_STRLEN_PP(passwd));
 
