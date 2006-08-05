d10e40a55a7101cee7ebff80ffbed75d75efcb53
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-05 20:56:43 +0000

    fix PECL bug #7827 add small optimization
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@217566 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/oci8/oci8.c
+++ ext/oci8/oci8.c
@@ -48,6 +48,7 @@
 
 #include "php_oci8.h"
 #include "php_oci8_int.h"
+#include "zend_hash.h"
 
 ZEND_DECLARE_MODULE_GLOBALS(oci)
 static PHP_GINIT_FUNCTION(oci);
@@ -986,6 +987,13 @@ php_oci_connection *php_oci_do_connect_ex(char *username, int username_len, char
 	smart_str_appendl_ex(&hashed_details, "oci8___", sizeof("oci8___") - 1, 0);
 	smart_str_appendl_ex(&hashed_details, username, username_len, 0);
 	smart_str_appendl_ex(&hashed_details, "__", sizeof("__") - 1, 0);
+	if (password_len) {
+		ulong password_hash;
+		password_hash = zend_inline_hash_func(password, password_len);
+		smart_str_append_unsigned_ex(&hashed_details, password_hash, 0);
+	}
+	smart_str_appendl_ex(&hashed_details, "__", sizeof("__") - 1, 0);
+
 	if (dbname) {
 		smart_str_appendl_ex(&hashed_details, dbname, dbname_len, 0);
 	}
@@ -1022,14 +1030,17 @@ php_oci_connection *php_oci_do_connect_ex(char *username, int username_len, char
 	}
 	
 	if (!exclusive && !new_password) {
-		
+		zend_bool found = 0;
+
 		if (persistent && zend_hash_find(&EG(persistent_list), hashed_details.c, hashed_details.len+1, (void **) &le) == SUCCESS) {
+			found = 1;
 			/* found */
 			if (le->type == le_pconnection) {
 				connection = (php_oci_connection *)le->ptr;	
 			}
 		}
 		else if (!persistent && zend_hash_find(&EG(regular_list), hashed_details.c, hashed_details.len+1, (void **) &le) == SUCCESS) {
+			found = 1;
 			if (le->type == le_index_ptr) {
 				int type;
 				long link;
@@ -1091,8 +1102,7 @@ php_oci_connection *php_oci_do_connect_ex(char *username, int username_len, char
 				connection = NULL;
 				goto open;
 			}
-		}
-		else {
+		} else if (found) {
 			/* found something, but it's not a connection, delete it */
 			if (persistent) {
 				zend_hash_del(&EG(persistent_list), hashed_details.c, hashed_details.len+1);
