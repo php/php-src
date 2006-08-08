b7d18bdcab1c26a0f95af4fbd76517fb1317ee05
Ilia Alshanetsky <iliaa@php.net>
2006-08-08 14:57:04 +0000

    MFH: Fixed bug #38377 (session_destroy() gives warning after session_regenerate_id()).

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -368,7 +368,12 @@ PS_DESTROY_FUNC(files)
 		ps_files_close(data);
 	
 		if (VCWD_UNLINK(buf) == -1) {
-			return FAILURE;
+			/* This is a little safety check for instances when we are dealing with a regenerated session
+			 * that was not yet written to disk
+			 */
+			if (!VCWD_ACCESS(buf, F_OK)) {
+				return FAILURE;
+			}
 		}
 	}
 
