1f9d60dd25fcc0377d61f2a4b7b90bccaa34e140
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-08 18:06:43 +0000

    fix logic and memleak
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@217759 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -1504,7 +1504,7 @@ static void php_compact_var(HashTable *eg_active_symbol_table, zval *return_valu
 			UChar *norm;
 			int norm_len;
 
-			if (!zend_normalize_identifier(&norm, &norm_len, key.u, key_len, 0)) {
+			if (zend_normalize_identifier(&norm, &norm_len, key.u, key_len, 0) == FAILURE) {
 				zend_error(E_WARNING, "Could not normalize variable name: %r", key);
 			} else if (norm != key.u) {
 				key.u = norm;
