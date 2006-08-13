ade7651d6829e27b27bad8e658647632569abde5
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-13 21:09:59 +0000

    - MFP:  - add overwrite mode to ZipArchive::open, always starts a new archive  - Fix safe mode checks on extract  - Fix possible leaks when a safe mode error has been raised
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@218101 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zip/lib/zip.h
+++ ext/zip/lib/zip.h
@@ -53,6 +53,7 @@ extern "C" {
 #define ZIP_CREATE           1
 #define ZIP_EXCL             2
 #define ZIP_CHECKCONS        4
+#define ZIP_OVERWRITE        8
 
 
 /* flags for zip_name_locate, zip_fopen, zip_stat, ... */
