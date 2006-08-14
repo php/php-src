abf8d346d85b3ceac89b6ef2cec38b5db8e7ddb8
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-08-14 08:15:44 +0000

    fix segfault and leak in ext/xmlrpc add tests
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@218118 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xmlrpc/xmlrpc-epi-php.c
+++ ext/xmlrpc/xmlrpc-epi-php.c
@@ -670,6 +670,7 @@ PHP_FUNCTION(xmlrpc_encode_request)
 			if (Z_TYPE_PP(method) == IS_NULL) {
 				XMLRPC_RequestSetRequestType(xRequest, xmlrpc_request_response);
 			} else {
+				convert_to_string_ex(method);
 				XMLRPC_RequestSetMethodName(xRequest, Z_STRVAL_PP(method));
 				XMLRPC_RequestSetRequestType(xRequest, xmlrpc_request_call);
 			}
@@ -739,7 +740,7 @@ zval* decode_request_worker (zval* xml_in, zval* encoding_in, zval* method_name_
 
       if(XMLRPC_RequestGetRequestType(response) == xmlrpc_request_call) {
          if(method_name_out) {
-            convert_to_string(method_name_out);
+            zval_dtor(method_name_out);
             Z_TYPE_P(method_name_out) = IS_STRING;
             Z_STRVAL_P(method_name_out) = estrdup(XMLRPC_RequestGetMethodName(response));
             Z_STRLEN_P(method_name_out) = strlen(Z_STRVAL_P(method_name_out));
