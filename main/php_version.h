3a6312932c7352f3430d9ffb20ba68ec36682483
Derick Rethans <derick@php.net>
2006-08-15 12:23:08 +0000

    - Back to dev.

--- main/php_version.h
+++ main/php_version.h
@@ -2,6 +2,6 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 4
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "4.4.4"
+#define PHP_RELEASE_VERSION 5
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "4.4.5-dev"
