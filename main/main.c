3a211712eb59b2ad8b623dd6f32bb3a28f10fca7
Ilia Alshanetsky <iliaa@php.net>
2006-08-17 13:43:08 +0000

    Increased default memory limit to 16 megabytes to accommodate for a more accurate memory utilization measurement.

--- main/main.c
+++ main/main.c
@@ -312,7 +312,7 @@ PHP_INI_BEGIN()
 	PHP_INI_ENTRY("smtp_port",					"25",		PHP_INI_ALL,		NULL)
 	PHP_INI_ENTRY("browscap",					NULL,		PHP_INI_SYSTEM,		NULL)
 #if MEMORY_LIMIT
-	PHP_INI_ENTRY("memory_limit",				"8M",		PHP_INI_ALL,		OnChangeMemoryLimit)
+	PHP_INI_ENTRY("memory_limit",				"16M",		PHP_INI_ALL,		OnChangeMemoryLimit)
 #endif
 	PHP_INI_ENTRY("precision",					"14",		PHP_INI_ALL,		OnSetPrecision)
 	PHP_INI_ENTRY("sendmail_from",				NULL,		PHP_INI_ALL,		NULL)
