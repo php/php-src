74eb820c348f2a3d4e2f127d637af2f629ce9366
Ilia Alshanetsky <iliaa@php.net>
2006-08-23 13:17:36 +0000

    5.1.6

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 6
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.1.6-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.1.6"
