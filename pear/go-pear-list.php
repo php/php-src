22caa9380646646525c07a77096997e455dd59fe
Pierre Joye <pajoye@php.net>
2006-08-25 13:37:10 +0000

    - updated list

--- pear/go-pear-list.php
+++ pear/go-pear-list.php
@@ -7,13 +7,13 @@
  */
 $packages  = array(
 // required packages for the installer
-"PEAR"                  =>    "1.3.6",
-"XML_RPC"               =>    "1.4.0",
+"PEAR"                  =>    "1.4.11",
+"XML_RPC"               =>    "1.4.8",
 "Console_Getopt"        =>    "1.2",
 "Archive_Tar"           =>    "1.3.1",
 
 // required packages for the web frontend
-"PEAR_Frontend_Web"     =>    "0.4",
-"HTML_Template_IT"      =>    "1.1",
+"PEAR_Frontend_Web"     =>    "0.5.1",
+"HTML_Template_IT"      =>    "1.1.4",
 "Net_UserAgent_Detect"  =>    "2.0.1",
 );
