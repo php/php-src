914d36217683c2c836768fe44b83b3d399dd0428
Ilia Alshanetsky <iliaa@php.net>
2006-08-28 23:33:37 +0000

    Adjust code to avoid TSRMLS_FETCH

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -134,7 +134,7 @@ PHPAPI char *php_trim(char *c, int len, char *what, int what_len, zval *return_v
 PHPAPI size_t php_strip_tags(char *rbuf, int len, int *state, char *allow, int allow_len);
 PHPAPI int php_char_to_str_ex(char *str, uint len, char from, char *to, int to_len, zval *result, int case_sensitivity, int *replace_count);
 PHPAPI int php_char_to_str(char *str, uint len, char from, char *to, int to_len, zval *result);
-PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value);
+PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value TSRMLS_DC);
 PHPAPI void php_explode(zval *delim, zval *str, zval *return_value, int limit);
 
 PHPAPI size_t php_strspn(char *s1, char *s2, char *s1_end, char *s2_end); 
--- ext/standard/string.c
+++ ext/standard/string.c
@@ -880,7 +880,7 @@ PHP_FUNCTION(explode)
 
 /* {{{ php_implode
  */
-PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value) 
+PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value TSRMLS_DC) 
 {
 	zval         **tmp;
 	HashPosition   pos;
@@ -888,7 +888,6 @@ PHPAPI void php_implode(zval *delim, zval *arr, zval *return_value)
 	int            numelems, i = 0;
 	zval tmp_val;
 	int str_len;
-	TSRMLS_FETCH();
 
 	numelems = zend_hash_num_elements(Z_ARRVAL_P(arr));
 
@@ -1001,7 +1000,7 @@ PHP_FUNCTION(implode)
 
 	pos = Z_ARRVAL_P(arr)->pInternalPointer;
 	
-	php_implode(delim, arr, return_value);
+	php_implode(delim, arr, return_value TSRMLS_CC);
 
 	Z_ARRVAL_P(arr)->pInternalPointer = pos;
 
