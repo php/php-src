5bc435c4e804bd7c6e4666e60f085e4fd344afcd
Antony Dovgal <tony2001@php.net>
2006-08-31 15:42:18 +0000

    fix typo in errmsg

--- ext/pcntl/pcntl.c
+++ ext/pcntl/pcntl.c
@@ -549,7 +549,7 @@ PHP_FUNCTION(pcntl_signal)
 	/* Special long value case for SIG_DFL and SIG_IGN */
 	if (Z_TYPE_P(handle)==IS_LONG) {
 		if (Z_LVAL_P(handle)!= (long) SIG_DFL && Z_LVAL_P(handle) != (long) SIG_IGN) {
-			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid value for handle argument specifEied");
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid value for handle argument specified");
 		}
 		if (php_signal(signo, (Sigfunc *) Z_LVAL_P(handle), (int) restart_syscalls) == SIG_ERR) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Error assigning signal");
