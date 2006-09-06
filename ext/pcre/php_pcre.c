c55eda9f3b0d0b26b5e902d62a6ff9ea07d798dc
Nuno Lopes <nlopess@php.net>
2006-09-06 16:30:59 +0000

    fix memleak of pcre study data

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -90,6 +90,7 @@ static void php_free_pcre_cache(void *data)
 	pcre_cache_entry *pce = (pcre_cache_entry *) data;
 	if (!pce) return;
 	pefree(pce->re, 1);
+	if (pce->extra) pefree(pce->extra, 1);
 #if HAVE_SETLOCALE
 	if ((void*)pce->tables) pefree((void*)pce->tables, 1);
 	pefree(pce->locale, 1);
