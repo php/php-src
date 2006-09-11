398a45429edffec07e0ada5322acab4a54f3046b
Antony Dovgal <tony2001@php.net>
2006-09-11 14:28:19 +0000

    fix #38779 (engine crashes when require()'ing file with syntax error through userspace stream wrapper)

--- Zend/zend.c
+++ Zend/zend.c
@@ -863,6 +863,7 @@ static void executor_globals_ctor(zend_executor_globals *executor_globals TSRMLS
 	EG(current_execute_data) = NULL;
 	EG(current_module) = NULL;
 	EG(exit_status) = 0;
+	EG(active) = 0;
 }
 
 
