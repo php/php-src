09a935ed6ae37066b97dea8fb2aeec7576b6de7c
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-09-14 09:59:23 +0000

    *** empty log message ***
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@219954 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/php_streams_int.h
+++ main/streams/php_streams_int.h
@@ -21,20 +21,15 @@
 
 #if ZEND_DEBUG
 
-#if USE_ZEND_ALLOC
-# define emalloc_rel_orig(size)	\
+#define emalloc_rel_orig(size)	\
 		( __php_stream_call_depth == 0 \
 		? _emalloc((size) ZEND_FILE_LINE_CC ZEND_FILE_LINE_RELAY_CC) \
 		: _emalloc((size) ZEND_FILE_LINE_CC ZEND_FILE_LINE_ORIG_RELAY_CC) )
 
-# define erealloc_rel_orig(ptr, size)	\
+#define erealloc_rel_orig(ptr, size)	\
 		( __php_stream_call_depth == 0 \
 		? _erealloc((ptr), (size), 0 ZEND_FILE_LINE_CC ZEND_FILE_LINE_RELAY_CC) \
 		: _erealloc((ptr), (size), 0 ZEND_FILE_LINE_CC ZEND_FILE_LINE_ORIG_RELAY_CC) )
-#else
-# define emalloc_rel_orig(size)			emalloc(size)
-# define erealloc_rel_orig(ptr, size)	erealloc(ptr, size)
-#endif
 
 #define pemalloc_rel_orig(size, persistent)	((persistent) ? malloc((size)) : emalloc_rel_orig((size)))
 #define perealloc_rel_orig(ptr, size, persistent)	((persistent) ? realloc((ptr), (size)) : erealloc_rel_orig((ptr), (size)))
