5b69b326e2306ebc68222aacc9efc68f7dacaf21
rrichards <rrichards@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-09-16 13:56:25 +0000

    MFB: implement workaround for bug #38823 (DOMComment->appendData does nothing) fix bug #38850 (lookupNamespaceURI doesn't return default namespace) add test
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@220065 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/dom/node.c
+++ ext/dom/node.c
@@ -1611,20 +1611,18 @@ PHP_FUNCTION(dom_node_lookup_namespace_uri)
 	dom_object *intern;
 	xmlNsPtr nsptr;
 	int prefix_len = 0;
-	char *prefix;
+	char *prefix=NULL;
 
-	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os&", &id, dom_node_class_entry, &prefix, &prefix_len, UG(utf8_conv)) == FAILURE) {
+	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Os!&", &id, dom_node_class_entry, &prefix, &prefix_len, UG(utf8_conv)) == FAILURE) {
 		return;
 	}
 
 	DOM_GET_OBJ(nodep, id, xmlNodePtr, intern);
 
-	if (prefix_len > 0) {
-		nsptr = xmlSearchNs(nodep->doc, nodep, prefix);
-		if (nsptr && nsptr->href != NULL) {
-			RETVAL_XML_STRING((char *) nsptr->href, ZSTR_DUPLICATE);
-			return;
-		}
+	nsptr = xmlSearchNs(nodep->doc, nodep, prefix);
+	if (nsptr && nsptr->href != NULL) {
+		RETVAL_XML_STRING((char *) nsptr->href, ZSTR_DUPLICATE);
+		return;
 	}
 
 	RETURN_NULL();
