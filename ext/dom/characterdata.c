49d8ec82fefa2b6cd930015055dd56efd0f3bf3f
Rob Richards <rrichards@php.net>
2006-09-16 13:56:25 +0000

    MFB: implement workaround for bug #38823 (DOMComment->appendData does nothing) fix bug #38850 (lookupNamespaceURI doesn't return default namespace) add test

--- ext/dom/characterdata.c
+++ ext/dom/characterdata.c
@@ -207,9 +207,19 @@ PHP_FUNCTION(dom_characterdata_append_data)
 	}
 
 	DOM_GET_OBJ(nodep, id, xmlNodePtr, intern);
-
+#if LIBXML_VERSION < 20627
+/* Implement logic from libxml xmlTextConcat to add suport for comments and PI */
+    if ((nodep->content == (xmlChar *) &(nodep->properties)) ||
+        ((nodep->doc != NULL) && (nodep->doc->dict != NULL) &&
+		xmlDictOwns(nodep->doc->dict, nodep->content))) {
+	nodep->content = xmlStrncatNew(nodep->content, arg, arg_len);
+    } else {
+        nodep->content = xmlStrncat(nodep->content, arg, arg_len);
+    }
+    nodep->properties = NULL;
+#else
 	xmlTextConcat(nodep, arg, arg_len);
-
+#endif
 	RETURN_TRUE;
 }
 /* }}} end dom_characterdata_append_data */
