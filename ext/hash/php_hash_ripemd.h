6781debeb8702d6cb16dca18d41d583b55418f79
Sara Golemon <pollita@php.net>
2006-09-20 01:48:06 +0000

    Add ripemd256 and ripemd320 algos

--- ext/hash/php_hash_ripemd.h
+++ ext/hash/php_hash_ripemd.h
@@ -35,6 +35,18 @@ typedef struct {
 	unsigned char buffer[64];	/* input buffer */
 } PHP_RIPEMD160_CTX;
 
+typedef struct {
+	php_hash_uint32 state[8];		/* state (ABCD) */
+	php_hash_uint32 count[2];		/* number of bits, modulo 2^64 (lsb first) */
+	unsigned char buffer[64];	/* input buffer */
+} PHP_RIPEMD256_CTX;
+
+typedef struct {
+	php_hash_uint32 state[10];		/* state (ABCD) */
+	php_hash_uint32 count[2];		/* number of bits, modulo 2^64 (lsb first) */
+	unsigned char buffer[64];	/* input buffer */
+} PHP_RIPEMD320_CTX;
+
 PHP_HASH_API void PHP_RIPEMD128Init(PHP_RIPEMD128_CTX *);
 PHP_HASH_API void PHP_RIPEMD128Update(PHP_RIPEMD128_CTX *, const unsigned char *, unsigned int);
 PHP_HASH_API void PHP_RIPEMD128Final(unsigned char[16], PHP_RIPEMD128_CTX *);
@@ -42,5 +54,13 @@ PHP_HASH_API void PHP_RIPEMD128Final(unsigned char[16], PHP_RIPEMD128_CTX *);
 PHP_HASH_API void PHP_RIPEMD160Init(PHP_RIPEMD160_CTX *);
 PHP_HASH_API void PHP_RIPEMD160Update(PHP_RIPEMD160_CTX *, const unsigned char *, unsigned int);
 PHP_HASH_API void PHP_RIPEMD160Final(unsigned char[20], PHP_RIPEMD160_CTX *);
+
+PHP_HASH_API void PHP_RIPEMD256Init(PHP_RIPEMD256_CTX *);
+PHP_HASH_API void PHP_RIPEMD256Update(PHP_RIPEMD256_CTX *, const unsigned char *, unsigned int);
+PHP_HASH_API void PHP_RIPEMD256Final(unsigned char[32], PHP_RIPEMD256_CTX *);
+
+PHP_HASH_API void PHP_RIPEMD320Init(PHP_RIPEMD320_CTX *);
+PHP_HASH_API void PHP_RIPEMD320Update(PHP_RIPEMD320_CTX *, const unsigned char *, unsigned int);
+PHP_HASH_API void PHP_RIPEMD320Final(unsigned char[40], PHP_RIPEMD320_CTX *);
 
 #endif /* PHP_HASH_RIPEMD_H */
