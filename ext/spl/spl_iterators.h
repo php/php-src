478d42a3acfd5a16dd01755676500fef50686f24
Antony Dovgal <tony2001@php.net>
2006-09-28 22:33:06 +0000

    MFH

--- ext/spl/spl_iterators.h
+++ ext/spl/spl_iterators.h
@@ -127,7 +127,7 @@ typedef struct _spl_dual_it_object {
 			long             count;
 		} limit;
 		struct {
-			int              flags; /* CIT_* */
+			long             flags; /* CIT_* */
 			zval             *zstr;
 			zval             *zchildren;
 			zval             *zcache;
@@ -139,7 +139,7 @@ typedef struct _spl_dual_it_object {
 #if HAVE_PCRE || HAVE_BUNDLED_PCRE
 		struct {
 			int              use_flags;
-			int              flags;
+			long             flags;
 			regex_mode       mode;
 			long             preg_flags;
 			pcre_cache_entry *pce;
