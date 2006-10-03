ea69ae94ebe8dcc9c97b5068188ac917168c51b5
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-03 20:42:57 +0000

    don't forget to increase refcount when adding entry to the return_value
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@221032 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -1868,6 +1868,7 @@ PHPAPI void  php_pcre_grep_impl(pcre_cache_entry *pce, zval *input, zval *return
 		/* If the entry fits our requirements */
 		if ((count > 0 && !invert) ||
 			(count == PCRE_ERROR_NOMATCH && invert)) {
+			(*entry)->refcount++;
 
 			/* Add to return array */
 			switch (zend_hash_get_current_key_ex(Z_ARRVAL_P(input), &string_key, &string_key_len, &num_key, 0, NULL))
