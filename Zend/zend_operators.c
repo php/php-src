8e1c9371bbc68d433702b9a7e647c7ad268b050c
Antony Dovgal <tony2001@php.net>
2006-10-03 17:54:51 +0000

    MFH: throw a warning when doing modulus by 0 (fr #39027)

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -918,6 +918,7 @@ ZEND_API int mod_function(zval *result, zval *op1, zval *op2 TSRMLS_DC)
 	zendi_convert_to_long(op2, op2_copy, result);
 
 	if (op2->value.lval == 0) {
+		zend_error(E_WARNING, "Division by zero");
 		ZVAL_BOOL(result, 0);
 		return FAILURE;			/* modulus by zero */
 	}
