7119ca692943dc778b6cea388b7f4a4773a60826
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-05 23:24:55 +0000

    Preserve EG(return_value_ptr_ptr) when calling zend_execute_scripts()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@221216 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend.c
+++ Zend/zend.c
@@ -1596,6 +1596,7 @@ ZEND_API int zend_execute_scripts(int type TSRMLS_DC, zval **retval, int file_co
 	int i;
 	zend_file_handle *file_handle;
 	zend_op_array *orig_op_array = EG(active_op_array);
+	zval **orig_retval_ptr_ptr = EG(return_value_ptr_ptr);
 	zval *local_retval=NULL;
 
 	va_start(files, file_count);
@@ -1659,11 +1660,13 @@ ZEND_API int zend_execute_scripts(int type TSRMLS_DC, zval **retval, int file_co
 		} else if (type==ZEND_REQUIRE) {
 			va_end(files);
 			EG(active_op_array) = orig_op_array;
+			EG(return_value_ptr_ptr) = orig_retval_ptr_ptr;
 			return FAILURE;
 		}
 	}
 	va_end(files);
 	EG(active_op_array) = orig_op_array;
+	EG(return_value_ptr_ptr) = orig_retval_ptr_ptr;
 
 	return SUCCESS;
 }
