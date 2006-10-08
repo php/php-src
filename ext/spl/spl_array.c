96bb716eeaa4c8011c2fadc4beb1386e0cb1480f
bjori <bjori@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-08 10:50:19 +0000

    Remove duplicate 'wrong param count' warning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@221365 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -1004,7 +1004,7 @@ SPL_METHOD(Array, exchangeArray)
 	zend_hash_copy(HASH_OF(return_value), spl_array_get_hash_table(intern, 0 TSRMLS_CC), (copy_ctor_func_t) zval_add_ref, &tmp, sizeof(zval*));
 	
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "Z", &array) == FAILURE) {
-		WRONG_PARAM_COUNT;
+		return;
 	}
 	if (Z_TYPE_PP(array) == IS_OBJECT && intern == (spl_array_object*)zend_object_store_get_object(object TSRMLS_CC)) {
 		zval_ptr_dtor(&intern->array);
