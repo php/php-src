cdf5bc5a6dbb4f02bf00ca3deb8b85bbf4e32cf8
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-10 09:52:06 +0000

    - Make fileinfo compile with PHP 5.2 as well.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@221494 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/fileinfo/fileinfo.c
+++ ext/fileinfo/fileinfo.c
@@ -121,10 +121,17 @@ PHP_FILEINFO_API zend_object_value finfo_objects_new(zend_class_entry *class_typ
 /* {{{ finfo_class_functions
  */
 function_entry finfo_class_functions[] = {
-	ZEND_ME_MAPPING(finfo,		finfo_open,	NULL)
-	ZEND_ME_MAPPING(set_flags,	finfo_set_flags,NULL)
-	ZEND_ME_MAPPING(file,		finfo_file,	NULL)
-	ZEND_ME_MAPPING(buffer,		finfo_buffer,	NULL)
+#if PHP_VERSION_ID >= 50200
+	ZEND_ME_MAPPING(finfo,          finfo_open,     NULL, ZEND_ACC_PUBLIC)
+	ZEND_ME_MAPPING(set_flags,      finfo_set_flags,NULL, ZEND_ACC_PUBLIC)
+	ZEND_ME_MAPPING(file,           finfo_file,     NULL, ZEND_ACC_PUBLIC)
+	ZEND_ME_MAPPING(buffer,         finfo_buffer,   NULL, ZEND_ACC_PUBLIC)
+#else
+	ZEND_ME_MAPPING(finfo,          finfo_open,     NULL)
+	ZEND_ME_MAPPING(set_flags,      finfo_set_flags,NULL)
+	ZEND_ME_MAPPING(file,           finfo_file,     NULL)
+	ZEND_ME_MAPPING(buffer,         finfo_buffer,   NULL)
+#endif
 	{NULL, NULL, NULL}
 };
 /* }}} */
