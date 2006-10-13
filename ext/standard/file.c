149e4a2d2465f2db1376bc04aa3be046e82dd7c8
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-13 01:42:20 +0000

    Fixed bug #38934 (move_uploaded_file() cannot read uploaded file outside of open_basedir).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@221668 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -1711,9 +1711,14 @@ PHP_FUNCTION(copy)
 }
 /* }}} */
 
+PHPAPI int php_copy_file(char *src, char *dest TSRMLS_DC)
+{
+	return php_copy_file_ex(src, dest, ENFORCE_SAFE_MODE TSRMLS_CC);
+}
+
 /* {{{ php_copy_file
  */
-PHPAPI int php_copy_file(char *src, char *dest TSRMLS_DC)
+PHPAPI int php_copy_file_ex(char *src, char *dest, int src_chk TSRMLS_DC)
 {
 	php_stream *srcstream = NULL, *deststream = NULL;
 	int ret = FAILURE;
@@ -1768,7 +1773,7 @@ no_stat:
 	}
 safe_to_copy:
 
-	srcstream = php_stream_open_wrapper(src, "rb", ENFORCE_SAFE_MODE | REPORT_ERRORS, NULL);
+	srcstream = php_stream_open_wrapper(src, "rb", src_chk | REPORT_ERRORS, NULL);
 	
 	if (!srcstream) {
 		return ret;
