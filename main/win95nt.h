770b29803cb96542a59b0dfff9181e3907623a0e
Dmitry Stogov <dmitry@php.net>
2006-10-19 09:49:44 +0000

    Fixed mkdir("/tmp/foo//bar/log", 0777, true)

--- main/win95nt.h
+++ main/win95nt.h
@@ -41,7 +41,7 @@ typedef char * caddr_t;
 #ifndef S_ISREG 
 #define S_ISREG(m) (((m) & S_IFMT) == S_IFREG)
 #endif
-#define chdir(path) SetCurrentDirectory(path)
+#define chdir(path) _chdir(path)
 #define mkdir(a, b)	_mkdir(a)
 #define rmdir(a)	_rmdir(a)
 #define getpid		_getpid
