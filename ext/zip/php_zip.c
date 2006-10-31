e69d0812e571633f5e42e19cf6d83d2c982cc508
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-31 23:25:41 +0000

    - use zip_name_locate in getCommentName
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@222614 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -1306,6 +1306,10 @@ ZIPARCHIVE_METHOD(setCommentName)
 		return;
 	}
 
+	if (name_len < 1) {
+		php_error_docref(NULL TSRMLS_CC, E_NOTICE, "Empty string as entry name");
+	}
+
 	idx = zip_name_locate(intern, name, 0);
 	if (idx < 0) {
 		RETURN_FALSE;
@@ -1347,12 +1351,11 @@ ZIPARCHIVE_METHOD(getCommentName)
 {
 	struct zip *intern;
 	zval *this = getThis();
-	int name_len;
+	int name_len, idx;
 	long flags = 0;
 	int comment_len = 0;
 	const char * comment;
 	char *name;
-	struct zip_stat sb;
 
 	if (!this) {
 		RETURN_FALSE;
@@ -1365,8 +1368,17 @@ ZIPARCHIVE_METHOD(getCommentName)
 		return;
 	}
 
-	PHP_ZIP_STAT_PATH(intern, name, name_len, 0, sb);
-	comment = zip_get_file_comment(intern, sb.index, &comment_len, (int)flags);
+	if (name_len < 1) {
+		php_error_docref(NULL TSRMLS_CC, E_NOTICE, "Empty string as entry name");
+		RETURN_FALSE;
+	}
+
+	idx = zip_name_locate(intern, name, 0);
+	if (idx < 0) {
+		RETURN_FALSE;
+	}
+
+	comment = zip_get_file_comment(intern, idx, &comment_len, (int)flags);
 	RETURN_STRINGL((char *)comment, (long)comment_len, 1);
 }
 /* }}} */
