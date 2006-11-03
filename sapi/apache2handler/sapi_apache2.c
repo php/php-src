07b318a4e4bdff6aa202f4943f934125b7ec07fd
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-11-03 13:51:48 +0000

    MFH: 	Added filter support for $_SERVER in cgi/apache2 sapis 	Make sure PHP_SELF is filtered in Apache 1 sapi
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@222732 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -232,13 +232,20 @@ php_apache_sapi_register_variables(zval *track_vars_array TSRMLS_DC)
 	php_struct *ctx = SG(server_context);
 	const apr_array_header_t *arr = apr_table_elts(ctx->r->subprocess_env);
 	char *key, *val;
+	int new_val_len;
 
 	APR_ARRAY_FOREACH_OPEN(arr, key, val)
-		if (!val) val = "";
-		php_register_variable(key, val, track_vars_array TSRMLS_CC);
+		if (!val) {
+			val = "";
+		}
+		if (sapi_module.input_filter(PARSE_SERVER, key, &val, strlen(val), &new_val_len TSRMLS_CC)) {
+			php_register_variable_safe(key, val, new_val_len, track_vars_array TSRMLS_CC);
+		}
 	APR_ARRAY_FOREACH_CLOSE()
 
-	php_register_variable("PHP_SELF", ctx->r->uri, track_vars_array TSRMLS_CC);
+	if (sapi_module.input_filter(PARSE_SERVER, "PHP_SELF", &ctx->r->uri, strlen(ctx->r->uri), &new_val_len TSRMLS_CC)) {
+		php_register_variable_safe("PHP_SELF", ctx->r->uri, new_val_len, track_vars_array TSRMLS_CC);
+	}
 }
 
 static void
