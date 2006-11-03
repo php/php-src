91c604d584e858bba4fadc2ad0d53ccb3e206191
Hannes Magnusson <bjori@php.net>
2006-11-03 15:06:51 +0000

    MFH: Add "wrong param" warning to strtotime()

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -1083,7 +1083,7 @@ PHP_FUNCTION(strtotime)
 		timelib_unixtime2local(now, t->sse);
 		timelib_time_dtor(t);
 		efree(initial_ts);
-	} else if (zend_parse_parameters_ex(ZEND_PARSE_PARAMS_QUIET, ZEND_NUM_ARGS() TSRMLS_CC, "s", &times, &time_len) != FAILURE) {
+	} else if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|l", &times, &time_len, &preset_ts) != FAILURE) {
 		/* We have no initial timestamp */
 		now = timelib_time_ctor();
 		now->tz_info = tzi;
