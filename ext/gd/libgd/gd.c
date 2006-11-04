541f9940ac2c9c96386db27ede9d28993a132160
Pierre Joye <pajoye@php.net>
2006-11-04 02:21:06 +0000

    - #39273, imagecopyresized and alpha channel

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -754,7 +754,7 @@ int gdImageGetTrueColorPixel (gdImagePtr im, int x, int y)
 	int p = gdImageGetPixel(im, x, y);
 
 	if (!im->trueColor)  {
-		return gdTrueColorAlpha(im->red[p], im->green[p], im->blue[p], (im->transparent == p) ? gdAlphaTransparent : gdAlphaOpaque);
+		return gdTrueColorAlpha(im->red[p], im->green[p], im->blue[p], (im->transparent == p) ? gdAlphaTransparent : im->alpha[p]);
 	} else {
 		return p;
 	}
