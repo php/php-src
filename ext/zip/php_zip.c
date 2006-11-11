e8b42ffa1bb9e9837772cab1b0acb136c6907150
nlopess <nlopess@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-11-11 23:43:00 +0000

    fix leaks within zip entries in several tests
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@223194 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -551,6 +551,7 @@ static void php_zip_free_dir(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 	if (zip_int) {
 		if (zip_int->za) {
 			zip_close(zip_int->za);
+			zip_int->za = NULL;
 		}
 
 		efree(rsrc->ptr);
@@ -565,8 +566,14 @@ static void php_zip_free_entry(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 {
 	zip_read_rsrc *zr_rsrc = (zip_read_rsrc *) rsrc->ptr;
 
-	efree(zr_rsrc);
-	rsrc->ptr = NULL;
+	if (zr_rsrc) {
+		if (zr_rsrc->zf) {
+			zip_fclose(zr_rsrc->zf);
+			zr_rsrc->zf = NULL;
+		}
+		efree(zr_rsrc);
+		rsrc->ptr = NULL;
+	}
 }
 /* }}} */
 
