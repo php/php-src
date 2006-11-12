6d22129d9b3109cec25ad38f432d108e2ee68c15
Hannes Magnusson <bjori@php.net>
2006-11-12 01:11:58 +0000

    Fixed bug#36975 (natcasesort() causes array_pop() to misbehave)

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -2134,7 +2134,7 @@ static void _phpi_pop(INTERNAL_FUNCTION_PARAMETERS, int off_the_end)
 		if (should_rehash) {
 			zend_hash_rehash(Z_ARRVAL_P(stack));
 		}
-	} else if (!key_len) {
+	} else if (!key_len && index >= Z_ARRVAL_P(stack)->nNextFreeElement-1) {
 		Z_ARRVAL_P(stack)->nNextFreeElement = Z_ARRVAL_P(stack)->nNextFreeElement - 1;
 	}
 
