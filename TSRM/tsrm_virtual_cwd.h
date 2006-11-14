1e3546ed048af471539592b9040c9e624a2f81b4
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-11-14 09:15:55 +0000

    Added comments
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@223315 c90b9560-bf6c-de11-be94-00142212c4b1

--- TSRM/tsrm_virtual_cwd.h
+++ TSRM/tsrm_virtual_cwd.h
@@ -186,9 +186,12 @@ CWD_API int virtual_chmod(const char *filename, mode_t mode TSRMLS_DC);
 CWD_API int virtual_chown(const char *filename, uid_t owner, gid_t group, int link TSRMLS_DC);
 #endif
 
-#define CWD_EXPAND   0
-#define CWD_FILEPATH 1
-#define CWD_REALPATH 2
+/* One of the following constants must be used as the last argument 
+   in virtual_file_ex() call. */
+
+#define CWD_EXPAND   0 /* expand "." and ".." but dont resolve symlinks      */
+#define CWD_FILEPATH 1 /* resolve symlinks if file is exist otherwise expand */
+#define CWD_REALPATH 2 /* call realpath(), resolve symlinks. File must exist */
 
 CWD_API int virtual_file_ex(cwd_state *state, const char *path, verify_path_func verify_path, int use_realpath);
 
