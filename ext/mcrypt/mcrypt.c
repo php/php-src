820b30bcb81c3106b4923bb974c617402de1ff73
Ilia Alshanetsky <iliaa@php.net>
2006-11-28 00:24:16 +0000

    MFB: Replace non-threadsafe rand() with php_rand_r()

--- ext/mcrypt/mcrypt.c
+++ ext/mcrypt/mcrypt.c
@@ -1039,7 +1039,8 @@ int php_mcrypt_iv(php_mcrypt_iv_source source, int size, char **iv_str, int *iv_
 		case PHP_MCRYPT_IV_SOURCE_RAND:
 				*iv_len = size;
 				while (size) {
-					(*iv_str)[--size] = 255.0 * rand() / RAND_MAX;
+					unsigned int ctx;
+					(*iv_str)[--size] = 255.0 * php_rand_r(&ctx) / RAND_MAX;
 				}
 			break;
 	}
