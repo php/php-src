f435e5490def70eeed1c1e1a5397e21fc58942d1
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-11-30 16:10:48 +0000

    MFB: Thread-safety issues
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@224086 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_scandir.c
+++ main/php_scandir.c
@@ -62,6 +62,8 @@ int php_scandir(const char *dirname, struct dirent **namelist[], int (*selector)
 	struct dirent *dp = NULL;
 	int vector_size = 0;
 	int nfiles = 0;
+	char entry[sizeof(struct dirent)+MAXPATHLEN];
+	struct dirent *result = (struct dirent *)&entry;
 
 	if (namelist == NULL) {
 		return -1;
@@ -71,7 +73,7 @@ int php_scandir(const char *dirname, struct dirent **namelist[], int (*selector)
 		return -1;
 	}
 
-	while ((dp = readdir(dirp)) != NULL) {
+	while ((dp = php_readdir_r(dirp, (struct dirent *)entry, &result)) == 0 && result) {
 		int dsize = 0;
 		struct dirent *newdp = NULL;
 
