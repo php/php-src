5d3968ea376ae98189f8cfbf2309db3955f2830f
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-12-04 21:07:13 +0000

    - MFH: init tmp var
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@224357 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -395,6 +395,7 @@ static unsigned int php_sapi_filter(int arg, char *var, char **val, unsigned int
 		if (!(IF_G(default_filter) == FILTER_UNSAFE_RAW)) {
 			zval *tmp_new_var = &new_var;
 			Z_STRVAL(new_var) = estrndup(*val, val_len);
+			INIT_PZVAL(tmp_new_var);
 			php_zval_filter(&tmp_new_var, IF_G(default_filter), IF_G(default_filter_flags), NULL, NULL/*charset*/, 0 TSRMLS_CC);
 		} else if (PG(magic_quotes_gpc)) {
 			Z_STRVAL(new_var) = php_addslashes(*val, Z_STRLEN(new_var), &Z_STRLEN(new_var), 0 TSRMLS_CC);
