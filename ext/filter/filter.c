6b8526efce0faaf440b07a92ff74e24556772b61
Antony Dovgal <tony2001@php.net>
2006-12-07 11:00:08 +0000

    MFH: fix #39763 (magic quotes are applied twice by ext/filter)

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -397,8 +397,6 @@ static unsigned int php_sapi_filter(int arg, char *var, char **val, unsigned int
 			Z_STRVAL(new_var) = estrndup(*val, val_len);
 			INIT_PZVAL(tmp_new_var);
 			php_zval_filter(&tmp_new_var, IF_G(default_filter), IF_G(default_filter_flags), NULL, NULL/*charset*/, 0 TSRMLS_CC);
-		} else if (PG(magic_quotes_gpc)) {
-			Z_STRVAL(new_var) = php_addslashes(*val, Z_STRLEN(new_var), &Z_STRLEN(new_var), 0 TSRMLS_CC);
 		} else {
 			Z_STRVAL(new_var) = estrndup(*val, val_len);
 		}
