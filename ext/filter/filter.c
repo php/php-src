96fed7ba3caed4a98c69904e5160d9d03dfab5bc
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-12-08 17:50:04 +0000

    Fixed bug #39763 (magic quotes are applied twice by ext/filter in parse_str())
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@224650 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -397,7 +397,7 @@ static unsigned int php_sapi_filter(int arg, char *var, char **val, unsigned int
 			Z_STRVAL(new_var) = estrndup(*val, val_len);
 			INIT_PZVAL(tmp_new_var);
 			php_zval_filter(&tmp_new_var, IF_G(default_filter), IF_G(default_filter_flags), NULL, NULL/*charset*/, 0 TSRMLS_CC);
-		} else if (PG(magic_quotes_gpc)) {
+		} else if (PG(magic_quotes_gpc) && !retval) { /* for PARSE_STRING php_register_variable_safe() will do the addslashes() */
 			Z_STRVAL(new_var) = php_addslashes(*val, Z_STRLEN(new_var), &Z_STRLEN(new_var), 0 TSRMLS_CC);
 		} else {
 			Z_STRVAL(new_var) = estrndup(*val, val_len);
