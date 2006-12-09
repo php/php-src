0b6145cbe7d4c6b84970ef5a5354178d458b0a43
wez <wez@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-12-09 22:18:43 +0000

    merge PHP 5 pdo into HEAD.  This source compiles on both PHP 5 and PHP 6. If you're poking around in here, please make sure that any changes you make compile on both PHP 5 and 6.  Thanks!
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@224735 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/php_pdo_phpvers_compat.h
+++ ext/pdo/php_pdo_phpvers_compat.h
@@ -25,6 +25,9 @@
 # define PHP_PDO_PHPVERS_COMPAT_H
 # if PHP_MAJOR_VERSION < 6
 
+typedef char *zstr;
+#  define ZSTR(x) x
+
 #  define ZVAL_RT_STRINGL(a, b, c, d)		ZVAL_STRINGL(a, b, c, d)
 #  define ZVAL_ASCII_STRINGL(a, b, c, d)	ZVAL_STRINGL(a, b, c, d)
 #  define PDO_ZVAL_PP_IS_TEXT(pp)			(Z_TYPE_PP(pp) == IS_STRING)
@@ -42,13 +45,16 @@ PDO_API char *php_pdo_str_tolower_dup(const char *src, int len);
 #  define convert_to_text(x)				convert_to_string(x)
 #  define zend_u_fetch_class(a, b, c, d)	zend_fetch_class(b, c, d)
 #  define PDO_MEMBER_IS(z, strlit)			((Z_STRLEN_P(z) == sizeof(strlit)-1) && 0 == strcmp(Z_STRVAL_P(z), strlit))
-
+#  define pdo_zstr_efree(x)  efree(x)
+#  define pdo_zstr_sval(x)   x
 # else
 
 #  define PDO_ZVAL_PP_IS_TEXT(pp)			((Z_TYPE_PP(pp) == IS_STRING) || (Z_TYPE_PP(pp) == IS_UNICODE))
 #  define PDO_ZVAL_P_IS_TEXT(pp)			((Z_TYPE_P(pp) == IS_STRING) || (Z_TYPE_P(pp) == IS_UNICODE))
 
 #  define PDO_MEMBER_IS(z, strlit)			((Z_UNILEN_P(z) == sizeof(strlit)-1) && (ZEND_U_EQUAL(Z_TYPE_P(z), Z_UNIVAL_P(z), Z_UNILEN_P(z), strlit, sizeof(strlit)-1)))
+#  define pdo_zstr_efree(x)  efree(x.v)
+#  define pdo_zstr_sval(x)   x.s
 
 # endif
 #endif
