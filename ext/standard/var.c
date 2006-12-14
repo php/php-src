7ccba6624df074051f24efabd5d88d9c0d2a8ec0
Andrei Zmievski <andrei@php.net>
2006-12-14 23:41:57 +0000

    Use 'S' for escaped binary strings and 's' for non-escaped.

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -718,7 +718,7 @@ static inline void php_var_serialize_string(smart_str *buf, char *str, int len)
 	unsigned char c;
 	int i;
 
-	smart_str_appendl(buf, "s:", 2);
+	smart_str_appendl(buf, "S:", 2);
 	smart_str_append_long(buf, len);
 	smart_str_appendl(buf, ":\"", 2);
 
