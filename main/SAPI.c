8a6741f8b663b4e482f473b02aed526fb74ccfd2
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-12-18 13:16:54 +0000

    MFH: fix possible invalid read (reproducible only on big-endian machines)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@225218 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/SAPI.c
+++ main/SAPI.c
@@ -565,7 +565,7 @@ SAPI_API int sapi_header_op(sapi_header_op_enum op, void *arg TSRMLS_DC)
 	header_line = estrndup(header_line, header_line_len);
 
 	/* cut of trailing spaces, linefeeds and carriage-returns */
-	while(isspace(header_line[header_line_len-1])) 
+	while(header_line_len && isspace(header_line[header_line_len-1])) 
 		  header_line[--header_line_len]='\0';
 	
 	/* new line safety check */
