4c939863ca0265b89ba6732684775304f03069ca
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-12-19 13:27:09 +0000

    MFH
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@225299 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/snprintf.c
+++ main/snprintf.c
@@ -139,7 +139,7 @@ PHPAPI char *php_gcvt(double value, int ndigit, char dec_point, char exponent, c
 		 * Infinity or NaN, convert to inf or nan with sign.
 		 * We assume the buffer is at least ndigit long.
 		 */
-		snprintf(buf, ndigit + 1, "%s%s", sign ? "-" : "",
+		snprintf(buf, ndigit + 1, "%s%s", (sign && *digits == 'I') ? "-" : "",
 				*digits == 'I' ? "INF" : "NAN");
 		zend_freedtoa(digits);
 		return (buf);
