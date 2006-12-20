1e19ee87853fe519cee671038bd5eba0fe9cd8be
Hannes Magnusson <bjori@php.net>
2006-12-20 10:34:36 +0000

    - Fixed incorrect function names on FreeBSD where inet_pton() was named 	__inet_pton() and inet_ntop() was named __inet_ntop() - Fixed bug #39685 (iconv() - undefined function) - Fixed bug #38852 (XML-RPC Breaks iconv)

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -142,7 +142,7 @@ ZEND_END_ARG_INFO()
 /* {{{ iconv_functions[]
  */
 zend_function_entry iconv_functions[] = {
-	PHP_NAMED_FE(iconv,php_if_iconv,				arginfo_iconv)
+	PHP_RAW_NAMED_FE(iconv,php_if_iconv,				arginfo_iconv)
 	PHP_FE(ob_iconv_handler,						arginfo_ob_iconv_handler)
 	PHP_FE(iconv_get_encoding,						arginfo_iconv_get_encoding)
 	PHP_FE(iconv_set_encoding,						arginfo_iconv_set_encoding)
