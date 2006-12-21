50791bce7678ae2a32f7d04524c205fd45a4b768
Antony Dovgal <tony2001@php.net>
2006-12-21 00:46:51 +0000

    fix potential leak

--- main/streams/memory.c
+++ main/streams/memory.c
@@ -688,6 +688,7 @@ static php_stream * php_stream_url_wrap_rfc2397(php_stream_wrapper *wrapper, cha
 		ts->mode = mode && mode[0] == 'r' ? TEMP_STREAM_READONLY : 0;
 		ts->meta = meta;
 	}
+	efree(comma);
 
 	return stream;
 }
