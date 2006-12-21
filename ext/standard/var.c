8794071b216ed0f8b4036d4f39a36e8f959389e2
Marcus Boerger <helly@php.net>
2006-12-21 22:55:07 +0000

    - Serialization doesn't work with unicode data

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -990,11 +990,8 @@ static void php_var_serialize_intern(smart_str *buf, zval *struc, HashTable *var
 					
 						smart_str_append_long(buf, serialized_length);
 						smart_str_appendl(buf, ":{", 2);
-						if (UG(unicode)) {
-							php_var_serialize_ustr(buf, (UChar*)serialized_data, serialized_length);
-						} else {
-							smart_str_appendl(buf, serialized_data, serialized_length);
-						}
+						/* we need non binary or ascii at least not unicode */
+						smart_str_appendl(buf, serialized_data, serialized_length);
 						smart_str_appendc(buf, '}'); 
 					} else {
 						smart_str_appendl(buf, "N;", 2);
