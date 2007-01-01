01b644b279ce806dcee54ff1e314705215432b8f
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-01 19:32:10 +0000

    Update copyright year
    # The rest of the files were already updated by Sebastian
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@226204 c90b9560-bf6c-de11-be94-00142212c4b1

--- win32/build/buildconf.js
+++ win32/build/buildconf.js
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2006 The PHP Group                                |
+  | Copyright (c) 1997-2007 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: buildconf.js,v 1.13.2.2 2006-01-01 12:50:20 sniper Exp $ */
+/* $Id: buildconf.js,v 1.13.2.2.2.1 2007-01-01 19:32:09 iliaa Exp $ */
 // This generates a configure script for win32 build
 
 WScript.StdOut.WriteLine("Rebuilding configure.js");
