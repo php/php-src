f07f6fdda33d7ae233fd53f07d558a48da80fcc9
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-04 23:49:35 +0000

    Added missing open_basedir checks
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@226450 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -251,6 +251,13 @@ PS_OPEN_FUNC(files)
 	if (*save_path == '\0') {
 		/* if save path is an empty string, determine the temporary dir */
 		save_path = php_get_temporary_directory();
+
+		if (PG(safe_mode) && (!php_checkuid(save_path, NULL, CHECKUID_ALLOW_ONLY_DIR))) {
+			return FAILURE;
+		}
+		if (php_check_open_basedir(save_path TSRMLS_CC)) {
+			return FAILURE;
+		}
 	}
 	
 	/* split up input parameter */
