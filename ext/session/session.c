02b5505c87eca95bcb8492d91ee04e5517d6fdfd
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-05 14:53:30 +0000

    Fix ZTS issues.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@226497 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/session.c
+++ ext/session/session.c
@@ -1546,7 +1546,7 @@ static PHP_FUNCTION(session_id)
 			int id_len;
 			UErrorCode status = U_ZERO_ERROR;
 
-			zend_unicode_to_string_ex(ZEND_U_CONVERTER(UG(runtime_encoding_conv)), &id, &id_len, name.u, name_len, &status TSRMLS_CC);
+			zend_unicode_to_string_ex(ZEND_U_CONVERTER(UG(runtime_encoding_conv)), &id, &id_len, name.u, name_len, &status);
 
 			if (U_SUCCESS(status) && id) {
 				PS(id) = id;
