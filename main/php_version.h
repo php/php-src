66dcd76874d4f33941646c742f3640cf4c0fe83c
Derick Rethans <derick@php.net>
2007-01-05 08:54:57 +0000

    - 4.4.5rc1

--- main/php_version.h
+++ main/php_version.h
@@ -3,5 +3,5 @@
 #define PHP_MAJOR_VERSION 4
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 5
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "4.4.5-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "4.4.5RC1"
