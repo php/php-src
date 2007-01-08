5d02827e046be04d293d9acb6e5c092aea20e52c
nlopess <nlopess@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-08 22:52:22 +0000

    avoid calling MSHUTDOWN when built without libt1
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@226779 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -1147,7 +1147,11 @@ zend_module_entry gd_module_entry = {
 	"gd",
 	gd_functions,
 	PHP_MINIT(gd),
+#if HAVE_LIBT1
 	PHP_MSHUTDOWN(gd),
+#else
+	NULL,
+#endif
 	NULL,
 #if HAVE_LIBGD20 && HAVE_GD_STRINGFT && (HAVE_GD_FONTCACHESHUTDOWN || HAVE_GD_FREEFONTCACHE)
 	PHP_RSHUTDOWN(gd),
@@ -1191,16 +1195,16 @@ static void php_free_gd_font(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 }
 /* }}} */
 
+#if HAVE_LIBT1
 /* {{{ PHP_MSHUTDOWN_FUNCTION
  */
 PHP_MSHUTDOWN_FUNCTION(gd)
 {
-#if HAVE_LIBT1
 	T1_CloseLib();
-#endif
 	return SUCCESS;
 }
 /* }}} */
+#endif
 
 
 /* {{{ PHP_MINIT_FUNCTION
