40929d5f675a33817f44edcfc14ff96e30ac27b7
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-11 02:33:07 +0000

    MFH
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@226984 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/posix/posix.c
+++ ext/posix/posix.c
@@ -932,7 +932,7 @@ PHP_FUNCTION(posix_getpwnam)
 	struct passwd *pw;
 	char *name;
 	int name_len;
-#ifdef HAVE_GETPWNAM_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWNAM_R)
 	struct passwd pwbuf;
 	int buflen;
 	char *buf;
@@ -942,7 +942,7 @@ PHP_FUNCTION(posix_getpwnam)
 		RETURN_FALSE;
 	}
 
-#ifdef HAVE_GETPWNAM_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWNAM_R)
 	buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
 	buf = emalloc(buflen);
 	pw = &pwbuf;
@@ -965,7 +965,7 @@ PHP_FUNCTION(posix_getpwnam)
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "unable to convert posix passwd struct to array");
 		RETVAL_FALSE;
 	}
-#ifdef HAVE_GETPWNAM_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWNAM_R)
 	efree(buf);
 #endif
 }
@@ -976,7 +976,7 @@ PHP_FUNCTION(posix_getpwnam)
 PHP_FUNCTION(posix_getpwuid)
 {
 	long uid;
-#ifdef HAVE_GETPWUID_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWUID_R)
 	struct passwd _pw;
 	struct passwd *retpwptr = NULL;
 	int pwbuflen;
@@ -988,7 +988,7 @@ PHP_FUNCTION(posix_getpwuid)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &uid) == FAILURE) {
 		RETURN_FALSE;
 	}
-#ifdef HAVE_GETPWUID_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWUID_R)
 	pwbuflen = sysconf(_SC_GETPW_R_SIZE_MAX);
 	pwbuf = emalloc(pwbuflen);
 
@@ -1012,7 +1012,7 @@ PHP_FUNCTION(posix_getpwuid)
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "unable to convert posix passwd struct to array");
 		RETVAL_FALSE;
 	}
-#ifdef HAVE_GETPWUID_R
+#if defined(_SC_GETPW_R_SIZE_MAX) && defined(HAVE_GETPWUID_R)
 	efree(pwbuf);
 #endif
 }
