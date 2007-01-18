6f98a771e6ad876ec6f1da0104530f3f3ee0a885
Ilia Alshanetsky <iliaa@php.net>
2007-01-18 23:20:24 +0000

    Fixed bug #39993 (tr_TR.UTF-8 locale has problems with PHP).

--- ext/date/lib/parse_tz.c
+++ ext/date/lib/parse_tz.c
@@ -21,6 +21,7 @@
 #include "timelib.h"
 
 #include <stdio.h>
+#include <locale.h>
 
 #ifdef HAVE_STRING_H
 #include <string.h>
@@ -195,6 +196,7 @@ void timelib_dump_tzinfo(timelib_tzinfo *tz)
 static int seek_to_tz_position(const unsigned char **tzf, char *timezone, const timelib_tzdb *tzdb)
 {
 	int left = 0, right = tzdb->index_size - 1;
+	char *cur_locale = setlocale(LC_CTYPE, "C");
 
 	do {
 		int mid = ((unsigned)left + right) >> 1;
@@ -206,11 +208,13 @@ static int seek_to_tz_position(const unsigned char **tzf, char *timezone, const 
 			left = mid + 1;
 		} else { /* (cmp == 0) */
 			(*tzf) = &(tzdb->data[tzdb->index[mid].pos + 20]);
+			setlocale(LC_CTYPE, cur_locale);
 			return 1;
 		}
 
 	} while (left <= right);
 
+	setlocale(LC_CTYPE, cur_locale);
 	return 0;
 }
 
