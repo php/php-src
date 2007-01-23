be5d7ecaca557670cd6980075fb0ffaf6900cacb
johannes <johannes@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-23 15:47:28 +0000

    - Use utf-8 instead of utf8 as encoding to fix phpinfo()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@227970 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -67,7 +67,7 @@ static int php_info_print_html_esc(const char *str, int len)
 	char *new_str;
 	TSRMLS_FETCH();
 	
-	new_str = php_escape_html_entities((char *) str, len, &new_len, 0, ENT_QUOTES, "utf8" TSRMLS_CC);
+	new_str = php_escape_html_entities((char *) str, len, &new_len, 0, ENT_QUOTES, "utf-8" TSRMLS_CC);
 	written = php_output_write_utf8(new_str, new_len TSRMLS_CC);
 	efree(new_str);
 	return written;
