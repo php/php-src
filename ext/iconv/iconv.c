257c45c07ee3e0a47d6d6b206ce507f90f02bd5c
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-24 00:33:44 +0000

    initialize errno
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@228003 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/iconv/iconv.c
+++ ext/iconv/iconv.c
@@ -784,6 +784,7 @@ static php_iconv_err_t _php_iconv_substr(smart_str *pretval,
 	}
 
 	cd2 = (iconv_t)NULL;
+	errno = 0;
 
 	for (in_p = str, in_left = nbytes, cnt = 0; in_left > 0 && len > 0; ++cnt) {
 		size_t prev_in_left;
