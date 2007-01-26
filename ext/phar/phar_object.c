9cd8d736771ec67d79d6d45a6b50a179a3b7e2e8
Marcus Boerger <helly@php.net>
2007-01-26 19:58:22 +0000

    - Simplify by providing delref function

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -87,11 +87,8 @@ PHP_METHOD(Phar, canWrite)
  */
 static void phar_spl_foreign_dtor(spl_filesystem_object *object TSRMLS_DC) /* {{{ */
 {
-	phar_archive_data *phar_data = (phar_archive_data *) object->oth;
-
-	if (--phar_data->refcount < 0) {
-		phar_destroy_phar_data(phar_data TSRMLS_CC);
-	}
+	phar_archive_delref((phar_archive_data *) object->oth TSRMLS_CC);
+	object->oth = NULL;
 }
 /* }}} */
 
