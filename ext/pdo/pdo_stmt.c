9741cbde9059dbfac63f46810d2eb56dda730a38
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-01-27 21:53:26 +0000

    nullify param.parameter and avoid destroying it twice
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@228248 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -370,6 +370,8 @@ static int really_register_bound_param(struct pdo_bound_param_data *param, pdo_s
 			} else {
 				zend_hash_index_del(hash, pparam->paramno);
 			}
+			/* param->parameter is freed by hash dtor */
+			param->parameter = NULL;
 			return 0;
 		}
 	}
@@ -428,7 +430,9 @@ static PHP_METHOD(PDOStatement, execute)
 			INIT_PZVAL(param.parameter);
 
 			if (!really_register_bound_param(&param, stmt, 1 TSRMLS_CC)) {
-				zval_ptr_dtor(&param.parameter);
+				if (param.parameter) {
+					zval_ptr_dtor(&param.parameter);
+				}
 				RETURN_FALSE;
 			}
 
