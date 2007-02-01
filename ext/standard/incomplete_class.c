0c84a4d927b1d8b5bacc6d53527b737c919cff95
Antony Dovgal <tony2001@php.net>
2007-02-01 14:07:43 +0000

    MFH: free class_name when necessary

--- ext/standard/incomplete_class.c
+++ ext/standard/incomplete_class.c
@@ -39,14 +39,20 @@ static zend_object_handlers php_incomplete_object_handlers;
 static void incomplete_class_message(zval *object, int error_type TSRMLS_DC)
 {
 	char *class_name;
+	zend_bool class_name_alloced = 1;
 
 	class_name = php_lookup_class_name(object, NULL);
 	
 	if (!class_name) {
+		class_name_alloced = 0;
 		class_name = "unknown";
 	}
 	
 	php_error_docref(NULL TSRMLS_CC, error_type, INCOMPLETE_CLASS_MSG, class_name);
+
+	if (class_name_alloced) {
+		efree(class_name);
+	}
 }
 /* }}} */
 
