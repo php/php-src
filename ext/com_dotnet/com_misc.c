d97611c9b31ccc8528765abbe29df36eef66d8da
wharmby <wharmby@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-02 08:42:32 +0000

    ADW: Fix COM extension bug #35463. Fix in/out argument processing
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@228723 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/com_dotnet/com_misc.c
+++ ext/com_dotnet/com_misc.c
@@ -78,6 +78,7 @@ PHPAPI void php_com_wrap_variant(zval *z, VARIANT *v,
 
 	VariantInit(&obj->v);
 	VariantCopyInd(&obj->v, v);
+	obj->modified = 0;
 
 	if ((V_VT(&obj->v) == VT_DISPATCH) && (V_DISPATCH(&obj->v) != NULL)) {
 		IDispatch_GetTypeInfo(V_DISPATCH(&obj->v), 0, LANG_NEUTRAL, &obj->typeinfo);
