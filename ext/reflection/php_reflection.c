7992ce0649d2ffc03924fe2269da3d151d858601
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-10 20:51:42 +0000

    fix #40431 (dynamic properties may cause crash in ReflectionProperty methods)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@229497 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -1155,14 +1155,19 @@ static void reflection_property_factory(zend_class_entry *ce, zend_property_info
 	
 	if (!(prop->flags & ZEND_ACC_PRIVATE)) {
 		/* we have to search the class hierarchy for this (implicit) public or protected property */
-		zend_class_entry *tmp_ce = ce;
-		zend_property_info *tmp_info;
+		zend_class_entry *tmp_ce = ce, *store_ce = ce;
+		zend_property_info *tmp_info = NULL;
 		int prop_name_len = UG(unicode) ? u_strlen(prop_name.u) : strlen(prop_name.s);
 		
 		while (tmp_ce && zend_u_hash_find(&tmp_ce->properties_info, utype, prop_name, prop_name_len + 1, (void **) &tmp_info) != SUCCESS) {
 			ce = tmp_ce;
-			prop = tmp_info;
 			tmp_ce = tmp_ce->parent;
+		}
+
+		if (tmp_info && !(tmp_info->flags & ZEND_ACC_SHADOW)) { /* found something and it's not a parent's private */
+			prop = tmp_info;
+		} else { /* not found, use initial value */
+			ce = store_ce;
 		}
 	}
 
