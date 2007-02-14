e3794a39869e2764fb616346c81ffe1cca85baba
Derick Rethans <derick@php.net>
2007-02-14 19:35:01 +0000

    - Clarify the source of the timezone data.

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -532,7 +532,7 @@ PHP_MINFO_FUNCTION(date)
 	
 	php_info_print_table_start();
 	php_info_print_table_row(2, "date/time support", "enabled");
-	php_info_print_table_row(2, "Timezone Database Version", tzdb->version);
+	php_info_print_table_row(2, "\"Olson\" Timezone Database Version", tzdb->version);
 	php_info_print_table_row(2, "Timezone Database", php_date_global_timezone_db_enabled ? "external" : "internal");
 	php_info_print_table_row(2, "Default timezone", guess_timezone(tzdb TSRMLS_CC));
 	php_info_print_table_end();
