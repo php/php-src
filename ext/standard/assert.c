5c31e1d36f7dce2583f6390228e9d690a1c0f3fb
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-16 13:12:55 +0000

    Fixed return value of assert_options(ASSERT_CALLBACK) and possible crash (Andy Wharmby)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@229937 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/assert.c
+++ ext/standard/assert.c
@@ -114,6 +114,16 @@ PHP_MSHUTDOWN_FUNCTION(assert)
 	return SUCCESS;
 }
 
+PHP_RINIT_FUNCTION(assert)
+{
+	if (ASSERTG(cb)) {
+		MAKE_STD_ZVAL(ASSERTG(callback));
+		ZVAL_STRING(ASSERTG(callback), ASSERTG(cb), 1);
+	}
+
+	return SUCCESS;
+}
+
 PHP_RSHUTDOWN_FUNCTION(assert)
 {
 	if (ASSERTG(callback)) { 
@@ -200,11 +210,6 @@ PHP_FUNCTION(assert)
 			zval_dtor(&tmp);
 		}
 		RETURN_TRUE;
-	}
-
-	if (!ASSERTG(callback) && ASSERTG(cb)) {
-		MAKE_STD_ZVAL(ASSERTG(callback));
-		ZVAL_STRING(ASSERTG(callback), ASSERTG(cb), 1);
 	}
 
 	if (ASSERTG(callback)) {
@@ -309,6 +314,11 @@ PHP_FUNCTION(assert_options)
 		break;
 
 	case ASSERT_CALLBACK:
+		if (ASSERTG(callback) != NULL) {
+			RETVAL_ZVAL(ASSERTG(callback), 1, 0);
+		} else {
+			RETVAL_NULL();
+		}
 		if (ac == 2) {
 			if (ASSERTG(callback)) {
 				zval_ptr_dtor(&ASSERTG(callback));
@@ -316,7 +326,7 @@ PHP_FUNCTION(assert_options)
 			ASSERTG(callback) = *value;
 			zval_add_ref(value);
 		}
-		RETURN_TRUE;
+		return;
 		break;
 
 	default:
