76df668f25c1fab1e9584ebc151372d79b526f72
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-16 08:54:45 +0000

    Allow C++ extensions to use CG(), EG() ... (Caroline)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@229929 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_globals_macros.h
+++ Zend/zend_globals_macros.h
@@ -27,12 +27,12 @@ typedef struct _zend_executor_globals zend_executor_globals;
 typedef struct _zend_scanner_globals zend_scanner_globals;
 typedef struct _zend_unicode_globals zend_unicode_globals;
 
+BEGIN_EXTERN_C()
+
 /* Compiler */
 #ifdef ZTS
 # define CG(v) TSRMG(compiler_globals_id, zend_compiler_globals *, v)
-BEGIN_EXTERN_C()
 int zendparse(void *compiler_globals);
-END_EXTERN_C()
 #else
 # define CG(v) (compiler_globals.v)
 extern ZEND_API struct _zend_compiler_globals compiler_globals;
@@ -77,6 +77,7 @@ extern ZEND_API ts_rsrc_id unicode_globals_id;
 extern ZEND_API zend_unicode_globals unicode_globals;
 #endif
 
+END_EXTERN_C()
 
 /* For limited downwards source compatibility */
 #define CLS_FETCH()
