5ed01dccb5dfe3f881a73863f08bba1ec96521d0
Antony Dovgal <tony2001@php.net>
2007-02-16 07:11:33 +0000

    fix ext/interbase build

--- ext/interbase/interbase.c
+++ ext/interbase/interbase.c
@@ -3048,9 +3048,9 @@ static void _php_ibase_user(INTERNAL_FUNCTION_PARAMETERS, int operation)
 */
 
 	/* Build buffer for isc_service_attach() */
-	snprintf(buf, sizeof(buf), "%c%c%c%d%s%c%d%s", isc_spb_version, isc_spb_current_version, isc_spb_user_name, 
+	snprintf(spb_buffer, sizeof(spb_buffer), "%c%c%c%d%s%c%d%s", isc_spb_version, isc_spb_current_version, isc_spb_user_name, 
 						strlen(dba_user_name), dba_user_name, isc_spb_password, strlen(dba_password), dba_password);
-	spb_length = strlen(buf);
+	spb_length = strlen(spb_buffer);
 
 	/* Attach to the Service Manager */
 	snprintf(service_name, sizeof(service_name), "%s:service_mgr", ib_server);
