8b9bf1e2c3e16ff29dbff4958711210d9d94b3fe
Antony Dovgal <tony2001@php.net>
2007-02-21 14:24:44 +0000

    fix bug #40576 (double values are truncated to 6 decimal digits when encoding)

--- ext/xmlrpc/libxmlrpc/xml_to_xmlrpc.c
+++ ext/xmlrpc/libxmlrpc/xml_to_xmlrpc.c
@@ -36,6 +36,8 @@ static const char rcsid[] = "#(@) $Id$";
 #ifdef _WIN32
 #include "xmlrpc_win32.h"
 #endif
+#include "php.h"
+#include "main/snprintf.h"
 #include <string.h>
 #include <stdlib.h>
 #include "xml_to_xmlrpc.h"
@@ -225,9 +227,12 @@ xml_element* XMLRPC_to_xml_element_worker(XMLRPC_VALUE current_vector, XMLRPC_VA
             simplestring_add(&elem_val->text, buf);
             break;
          case xmlrpc_double:
-            elem_val->name = strdup(ELEM_DOUBLE);
-            snprintf(buf, BUF_SIZE, "%f", XMLRPC_GetValueDouble(node));
-            simplestring_add(&elem_val->text, buf);
+            {
+                TSRMLS_FETCH();
+                elem_val->name = strdup(ELEM_DOUBLE);
+                ap_php_snprintf(buf, BUF_SIZE, "%.*G", (int) EG(precision), XMLRPC_GetValueDouble(node));
+                simplestring_add(&elem_val->text, buf);
+            }
             break;
          case xmlrpc_datetime:
             elem_val->name = strdup(ELEM_DATETIME);
