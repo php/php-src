fa28ccbf25d11aa6fac98be0793fe84fc8146d84
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-23 23:09:14 +0000

    MFH (r-1.82): Add retry for interrupted reads and graceful handling for failed retries
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@230600 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -333,8 +333,15 @@ static size_t php_stdiop_read(php_stream *stream, char *buf, size_t count TSRMLS
 			return 0;
 		}
 		ret = read(data->fd, buf, count);
-		
-		stream->eof = (ret == 0 || (ret == (size_t)-1 && errno != EWOULDBLOCK));
+
+		if (ret == (size_t)-1 && errno == EINTR) {
+			/* Read was interrupted, retry once,
+			   If read still fails, giveup with feof==0
+			   so script can retry if desired */
+			ret = read(data->fd, buf, count);
+		}
+
+		stream->eof = (ret == 0 || (ret == (size_t)-1 && errno != EWOULDBLOCK && errno != EINTR));
 				
 	} else {
 #if HAVE_FLUSHIO
