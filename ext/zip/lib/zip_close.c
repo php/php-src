847adb7fc687ec692fd8d692159e279b67ac5ce4
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-24 16:23:10 +0000

    Fixed snprintf() usage
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@230673 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zip/lib/zip_close.c
+++ ext/zip/lib/zip_close.c
@@ -527,13 +527,14 @@ _zip_create_temp_output(struct zip *za, FILE **outp)
     char *temp;
     int tfd;
     FILE *tfp;
+    int len = strlen(za->zn) + 8;
 
-    if ((temp=(char *)malloc(strlen(za->zn)+8)) == NULL) {
+    if ((temp=(char *)malloc(len)) == NULL) {
 	_zip_error_set(&za->error, ZIP_ER_MEMORY, 0);
 	return NULL;
     }
 
-    snprintf(temp, sizeof(temp), "%s.XXXXXX", za->zn);
+    snprintf(temp, len, "%s.XXXXXX", za->zn);
 
     if ((tfd=mkstemp(temp)) == -1) {
 	_zip_error_set(&za->error, ZIP_ER_TMPOPEN, errno);
