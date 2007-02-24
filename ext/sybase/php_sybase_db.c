92b3565cf24088d2324c2d58d2b542f8801550d0
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-24 02:59:58 +0000

    Fixed a typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@230642 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sybase/php_sybase_db.c
+++ ext/sybase/php_sybase_db.c
@@ -340,7 +340,7 @@ static void php_sybase_do_connect(INTERNAL_FUNCTION_PARAMETERS,int persistent)
 				convert_to_string_ex(yyuser);
 				host = Z_STRVAL_PP(yyhost);
 				user = Z_STRVAL_PP(yyuser);
-				hashed_details_length = sprintf(&hashed_details, 0, "sybase_%s_%s___", Z_STRVAL_PP(yyhost), Z_STRVAL_PP(yyuser));
+				hashed_details_length = spprintf(&hashed_details, 0, "sybase_%s_%s___", Z_STRVAL_PP(yyhost), Z_STRVAL_PP(yyuser));
 			}
 			break;
 		case 3: {
