24b74421a63bc05610f542e17e1847a5936b3481
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-02-25 22:59:32 +0000

    MFH: zerofill socket structs
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@230789 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -1118,6 +1118,8 @@ PHP_FUNCTION(socket_connect)
 				php_error_docref(NULL TSRMLS_CC, E_WARNING, "Socket of type AF_INET6 requires 3 arguments");
 				RETURN_FALSE;
 			}
+			
+			memset(&sin6, 0, sizeof(struct sockaddr_in6));
 
 			sin6.sin6_family = AF_INET6;
 			sin6.sin6_port   = htons((unsigned short int)port);
@@ -1134,6 +1136,8 @@ PHP_FUNCTION(socket_connect)
 				php_error_docref(NULL TSRMLS_CC, E_WARNING, "Socket of type AF_INET requires 3 arguments");
 				RETURN_FALSE;
 			}
+			
+			memset(&sin, 0, sizeof(struct sockaddr_in));
 
 			sin.sin_family = AF_INET;
 			sin.sin_port   = htons((unsigned short int)port);
@@ -1146,6 +1150,8 @@ PHP_FUNCTION(socket_connect)
 			break;
 
 		case AF_UNIX:
+			memset(&s_un, 0, sizeof(struct sockaddr_un));
+			
 			s_un.sun_family = AF_UNIX;
 			snprintf(s_un.sun_path, 108, "%s", addr);
 			retval = connect(php_sock->bsd_socket, (struct sockaddr *) &s_un, SUN_LEN(&s_un));
