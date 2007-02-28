fc6bdb845d21c1af36d30a774bd99d0f1395db38
Derick Rethans <derick@php.net>
2007-02-28 18:20:56 +0000

    - MFH: Fix for CVE-2007-0455.

--- ext/gd/libgd/gdft.c
+++ ext/gd/libgd/gdft.c
@@ -1036,7 +1036,7 @@ gdImageStringFTEx (gdImage * im, int *brect, int fg, char *fontlist, double ptsi
 					} else {
 						ch = c & 0xFF;	/* don't extend sign */
 					}
-					next++;
+					if (next) next++;
 				}
 				break;
 			case gdFTEX_Big5: {
