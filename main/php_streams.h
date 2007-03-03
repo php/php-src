79f06a120b0ddcffa4069cce5ee47b4413421dae
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-03-03 22:28:21 +0000

    - Change to hex to help read-/maintainability
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@231095 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_streams.h
+++ main/php_streams.h
@@ -580,14 +580,15 @@ END_EXTERN_C()
 
 /* Wrappers support */
 
-#define IGNORE_PATH			0
-#define USE_PATH			1
-#define IGNORE_URL			2
-#define ENFORCE_SAFE_MODE 	4
-#define REPORT_ERRORS		8
+#define IGNORE_PATH                     0x00000000
+#define USE_PATH                        0x00000001
+#define IGNORE_URL                      0x00000002
+#define ENFORCE_SAFE_MODE               0x00000004
+#define REPORT_ERRORS                   0x00000008
+
 /* If you don't need to write to the stream, but really need to
  * be able to seek, use this flag in your options. */
-#define STREAM_MUST_SEEK	16
+#define STREAM_MUST_SEEK                0x00000010
 /* If you are going to end up casting the stream into a FILE* or
  * a socket, pass this flag and the streams/wrappers will not use
  * buffering mechanisms while reading the headers, so that HTTP
@@ -595,28 +596,28 @@ END_EXTERN_C()
  * If you omit this flag, streams will use buffering and should end 
  * up working more optimally.
  * */
-#define STREAM_WILL_CAST	32
+#define STREAM_WILL_CAST                0x00000020
 
 /* this flag applies to php_stream_locate_url_wrapper */
-#define STREAM_LOCATE_WRAPPERS_ONLY	64
+#define STREAM_LOCATE_WRAPPERS_ONLY     0x00000040
 
 /* this flag is only used by include/require functions */
-#define STREAM_OPEN_FOR_INCLUDE		128
+#define STREAM_OPEN_FOR_INCLUDE         0x00000080
 
 /* this flag tells streams to ONLY open urls */
-#define STREAM_USE_URL			256
+#define STREAM_USE_URL                  0x00000100
 
 /* this flag is used when only the headers from HTTP request are to be fetched */
-#define STREAM_ONLY_GET_HEADERS		512
+#define STREAM_ONLY_GET_HEADERS         0x00000200
 
 /* don't apply open_basedir checks */
-#define STREAM_DISABLE_OPEN_BASEDIR	1024
+#define STREAM_DISABLE_OPEN_BASEDIR     0x00000400
 
 /* get (or create) a persistent version of the stream */
-#define STREAM_OPEN_PERSISTENT	2048
+#define STREAM_OPEN_PERSISTENT	        0x00000800
 
 /* use glob stream for directory open in plain files stream */
-#define STREAM_USE_GLOB_DIR_OPEN 4096
+#define STREAM_USE_GLOB_DIR_OPEN        0x00001000
 
 /* Antique - no longer has meaning */
 #define IGNORE_URL_WIN 0
