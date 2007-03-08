299969011f35f13ca97f43c4f34fc0a2a0e4835f
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-03-08 22:49:53 +0000

    fix crash/leak in bug #38710
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@231495 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -723,7 +723,16 @@ PHP_FUNCTION(mysqli_stmt_fetch)
 						}
 #endif
 						else {
-							ZVAL_STRINGL(stmt->result.vars[i], stmt->result.buf[i].val, stmt->result.buf[i].buflen, 1);
+#if defined(MYSQL_DATA_TRUNCATED) && MYSQL_VERSION_ID > 50002
+							if(ret == MYSQL_DATA_TRUNCATED && *(stmt->stmt->bind[i].error) != 0) {
+								/* result was truncated */
+								ZVAL_STRINGL(stmt->result.vars[i], stmt->result.buf[i].val, stmt->stmt->bind[i].buffer_length, 1);
+							} else {
+#else
+							{
+#endif
+								ZVAL_STRINGL(stmt->result.vars[i], stmt->result.buf[i].val, stmt->result.buf[i].buflen, 1);
+							}
 						}
 						break;
 					default:
