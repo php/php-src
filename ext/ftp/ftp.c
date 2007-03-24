75ad7747eb52b5b52d8009101edf2191b0c0ea4a
Ilia Alshanetsky <iliaa@php.net>
2007-03-24 16:26:50 +0000

    MFB: Fixed CRLF injection inside ftp_putcmd().

--- ext/ftp/ftp.c
+++ ext/ftp/ftp.c
@@ -962,11 +962,19 @@ ftp_putcmd(ftpbuf_t *ftp, const char *cmd, const char *args)
 	int		size;
 	char		*data;
 
+	if (strpbrk(cmd, "\r\n")) {
+		return 0;
+	} 
+
 	/* build the output buffer */
 	if (args && args[0]) {
 		/* "cmd args\r\n\0" */
 		if (strlen(cmd) + strlen(args) + 4 > FTP_BUFSIZE)
 			return 0;
+
+		if (strpbrk(args, "\r\n")) {
+			return 0;
+		}
 		size = sprintf(ftp->outbuf, "%s %s\r\n", cmd, args);
 	}
 	else {
