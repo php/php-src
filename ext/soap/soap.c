36c6ad915bf9ce0c0894258957feb1fedad193c5
Dmitry Stogov <dmitry@php.net>
2007-04-02 13:43:08 +0000

    Fixed bug #37013 (server hangs when returning circular object references)

--- ext/soap/soap.c
+++ ext/soap/soap.c
@@ -479,6 +479,7 @@ static void php_soap_init_globals(zend_soap_globals *soap_globals TSRMLS_DC)
 	soap_globals->sdl = NULL;
 	soap_globals->soap_version = SOAP_1_1;
 	soap_globals->mem_cache = NULL;
+	soap_globals->ref_map = NULL;
 }
 
 PHP_MSHUTDOWN_FUNCTION(soap)
@@ -2583,7 +2584,9 @@ static void do_soap_call(zval* this_ptr,
 			xmlFreeDoc(request);
 
 			if (ret && Z_TYPE(response) == IS_STRING) {
+				encode_reset_ns();
 				ret = parse_packet_soap(this_ptr, Z_STRVAL(response), Z_STRLEN(response), fn, NULL, return_value, output_headers TSRMLS_CC);
+				encode_finish();
 			}
 
 			zval_dtor(&response);
@@ -2626,7 +2629,9 @@ static void do_soap_call(zval* this_ptr,
 			xmlFreeDoc(request);
 
 			if (ret && Z_TYPE(response) == IS_STRING) {
+				encode_reset_ns();
 				ret = parse_packet_soap(this_ptr, Z_STRVAL(response), Z_STRLEN(response), NULL, function, return_value, output_headers TSRMLS_CC);
+				encode_finish();
 			}
 
 			zval_dtor(&response);
@@ -3282,6 +3287,8 @@ static sdlFunctionPtr deserialize_function_call(sdlPtr sdl, xmlDocPtr request, c
 	xmlAttrPtr attr;
 	sdlFunctionPtr function;
 
+	encode_reset_ns();
+
 	/* Get <Envelope> element */
 	env = NULL;
 	trav = request->children;
@@ -3530,6 +3537,9 @@ ignore_header:
 		func = func->children;
 	}
 	deserialize_parameters(func, function, num_params, parameters);
+	
+	encode_finish();
+
 	return function;
 }
 
@@ -3982,6 +3992,8 @@ static xmlDocPtr serialize_response_call(sdlFunctionPtr function, char *function
 		}
 	}
 
+	encode_finish();
+
 	if (function && function->responseName == NULL && 
 	    body->children == NULL && head == NULL) {
 		xmlFreeDoc(doc);
@@ -4195,6 +4207,8 @@ static xmlDocPtr serialize_function_call(zval *this_ptr, sdlFunctionPtr function
 			}
 		}
 	}
+
+	encode_finish();
 
 	return doc;
 }
