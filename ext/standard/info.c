e6d7ad843b37fa6194be878de110b33caa740857
sniper <sniper@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-04-02 12:42:18 +0000

    MFB52:- Changed phpinfo() to make difference between set php.ini path and what was actually loaded.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@233125 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -530,7 +530,8 @@ PHPAPI void php_print_info(int flag TSRMLS_DC)
 		php_info_print_table_row(2, "Virtual Directory Support", "disabled" );
 #endif
 
-		php_info_print_table_row(2, "Configuration File (php.ini) Path", php_ini_opened_path?php_ini_opened_path:PHP_CONFIG_FILE_PATH);
+		php_info_print_table_row(2, "Configuration File (php.ini) Path", PHP_CONFIG_FILE_PATH);
+		php_info_print_table_row(2, "Loaded Configuration File", php_ini_opened_path ? php_ini_opened_path : "(none)");
 
 		if (strlen(PHP_CONFIG_FILE_SCAN_DIR)) {
 			php_info_print_table_row(2, "Scan this dir for additional .ini files", PHP_CONFIG_FILE_SCAN_DIR);
