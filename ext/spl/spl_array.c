10989bc73e40eeed00aa4f737d6eb4d3de0433f4
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-04-06 17:57:10 +0000

    - Fix 40442
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@233397 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -520,7 +520,7 @@ SPL_METHOD(Array, offsetExists)
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z", &index) == FAILURE) {
 		return;
 	}
-	RETURN_BOOL(spl_array_has_dimension_ex(0, getThis(), index, 1 TSRMLS_CC));
+	RETURN_BOOL(spl_array_has_dimension_ex(0, getThis(), index, 0 TSRMLS_CC));
 } /* }}} */
 
 /* {{{ proto mixed ArrayObject::offsetGet(mixed $index)
