efcb227198fffb560c7d6209516e58d38eb5381c
Antony Dovgal <tony2001@php.net>
2007-04-18 20:36:54 +0000

    MFH

--- ext/gmp/gmp.c
+++ ext/gmp/gmp.c
@@ -1484,6 +1484,11 @@ ZEND_FUNCTION(gmp_setbit)
 			break;
 	}
 
+	if (index < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Index must be greater than or equal to zero");
+		return;
+	}
+
 	if (set) {
 		mpz_setbit(*gmpnum_a, index);
 	} else {
@@ -1508,6 +1513,11 @@ ZEND_FUNCTION(gmp_clrbit)
 
 	convert_to_long_ex(ind_arg);
 	index = Z_LVAL_PP(ind_arg);
+	
+	if (index < 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Index must be greater than or equal to zero");
+		return;
+	}
 
 	mpz_clrbit(*gmpnum_a, index);
 }
