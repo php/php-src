3f13bc2d71acd1e7aef7e147937eeb67fba9a6e5
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-04-26 09:39:03 +0000

    removed unused highlight.bg ini option
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@234520 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -348,7 +348,6 @@ static PHP_INI_MH(OnUpdateDefaultMimetype)
  */
 PHP_INI_BEGIN()
 	PHP_INI_ENTRY_EX("define_syslog_variables",	"0",				PHP_INI_ALL,	NULL,			php_ini_boolean_displayer_cb)
-	PHP_INI_ENTRY_EX("highlight.bg",			HL_BG_COLOR,		PHP_INI_ALL,	NULL,			php_ini_color_displayer_cb)
 	PHP_INI_ENTRY_EX("highlight.comment",		HL_COMMENT_COLOR,	PHP_INI_ALL,	NULL,			php_ini_color_displayer_cb)
 	PHP_INI_ENTRY_EX("highlight.default",		HL_DEFAULT_COLOR,	PHP_INI_ALL,	NULL,			php_ini_color_displayer_cb)
 	PHP_INI_ENTRY_EX("highlight.html",			HL_HTML_COLOR,		PHP_INI_ALL,	NULL,			php_ini_color_displayer_cb)
