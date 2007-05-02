e70145964697dded7de153e6c5779bd0fa050fd1
Dmitry Stogov <dmitry@php.net>
2007-05-02 08:22:32 +0000

    Fixed bug #41097 (ext/soap returning associative array as indexed without using WSDL)

--- ext/soap/php_encoding.c
+++ ext/soap/php_encoding.c
@@ -3345,8 +3345,12 @@ static int is_map(zval *array)
 	int i, count = zend_hash_num_elements(Z_ARRVAL_P(array));
 
 	zend_hash_internal_pointer_reset(Z_ARRVAL_P(array));
-	for (i = 0;i < count;i++) {
-		if (zend_hash_get_current_key_type(Z_ARRVAL_P(array)) != HASH_KEY_IS_LONG) {
+	for (i = 0; i < count; i++) {
+		zstr str_index;
+		ulong num_index;
+
+		if (zend_hash_get_current_key(Z_ARRVAL_P(array), &str_index, &num_index, 0) != HASH_KEY_IS_LONG ||
+		    num_index != i) {
 			return TRUE;
 		}
 		zend_hash_move_forward(Z_ARRVAL_P(array));
