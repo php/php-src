6ae03f2722b0d25ac205dc9f755c24dfa9ebd13a
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-02 15:41:06 +0000

    MFH: add USE_ZEND_ALLOC env var
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@234796 c90b9560-bf6c-de11-be94-00142212c4b1

--- run-tests.php
+++ run-tests.php
@@ -1176,6 +1176,12 @@ TEST $file
 			save_text($test_skipif, $section_text['SKIPIF'], $temp_skipif);
 			$extra = substr(PHP_OS, 0, 3) !== "WIN" ?
 				"unset REQUEST_METHOD; unset QUERY_STRING; unset PATH_TRANSLATED; unset SCRIPT_FILENAME; unset REQUEST_METHOD;": "";
+
+			if ($leak_check) {
+				$env['USE_ZEND_ALLOC'] = '0';
+			} else {
+				$env['USE_ZEND_ALLOC'] = '1';
+			}
 			$output = system_with_timeout("$extra $php -q $ini_settings $test_skipif", $env);
 			if (!$cfg['keep']['skip']) {
 				@unlink($test_skipif);
