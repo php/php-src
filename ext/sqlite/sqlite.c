c62ef9e39960668431f36dbd0d756025854213fb
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-05 15:36:15 +0000

    Fixed bug #41285 (Improved fix for CVE-2007-1887 to work with non-bundled sqlite2 lib).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@235016 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sqlite/sqlite.c
+++ ext/sqlite/sqlite.c
@@ -73,7 +73,7 @@ extern int sqlite_encode_binary(const unsigned char *in, int n, unsigned char *o
 extern int sqlite_decode_binary(const unsigned char *in, unsigned char *out);
 
 #define php_sqlite_encode_binary(in, n, out) sqlite_encode_binary((const unsigned char *)in, n, (unsigned char *)out)
-#define php_sqlite_decode_binary(in, out)    sqlite_decode_binary((const unsigned char *)in, (unsigned char *)out)
+#define php_sqlite_decode_binary(in, out) in && *in ? sqlite_decode_binary((const unsigned char *)in, (unsigned char *)out) : 0
 
 static int sqlite_count_elements(zval *object, long *count TSRMLS_DC);
 
