098897f03e2c5b12cca5133b56cbf810608fb367
Marcus Boerger <helly@php.net>
2007-05-10 20:55:28 +0000

    - Add phar.phar command input files

---
+++ ext/phar/phar/phar.php
@@ -0,0 +1,40 @@
+<?php
+
+/** @file phar.php
+ * @ingroup Phar
+ * @brief class CLICommand
+ * @author  Marcus Boerger
+ * @date    2007 - 2007
+ *
+ * Phar Command
+ */
+foreach(array("SPL", "Reflection", "Phar") as $ext)
+{
+	if (!extension_loaded($ext))
+	{
+		echo "$argv[0] requires PHP extension $ext.\n";
+		exit(1);
+	}
+}
+
+function command_include($file)
+{
+	$file = 'phar://' . __FILE__ . '/' . $file;
+	if (file_exists($file))
+	{
+		include($file);
+	}
+}
+
+function command_autoload($classname)
+{
+	command_include(strtolower($classname) . '.inc');
+}
+
+Phar::loadPhar(__FILE__);
+
+spl_autoload_register('command_autoload');
+
+new PharCommand($argc, $argv);
+
+__HALT_COMPILER();
