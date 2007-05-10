db9016adbd3c0750835c3f0186e79eed236a3941
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-10 21:05:14 +0000

    Fixed random number generation for http auth mechanism in SOAP
    # Identified by Stefan Esser
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@235361 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/soap/php_http.c
+++ ext/soap/php_http.c
@@ -22,6 +22,7 @@
 #include "php_soap.h"
 #include "ext/standard/base64.h"
 #include "ext/standard/md5.h"
+#include "ext/standard/php_rand.h"
 
 static char *get_http_header_value(char *headers, char *type);
 static int get_http_body(php_stream *socketd, int close, char *headers,  char **response, int *out_size TSRMLS_DC);
@@ -469,10 +470,9 @@ try_again:
 					char          HA1[33], HA2[33], response[33], cnonce[33], nc[9];
 					PHP_MD5_CTX   md5ctx;
 					unsigned char hash[16];
-					unsigned int ctx;
 
 					PHP_MD5Init(&md5ctx);
-					snprintf(cnonce, sizeof(cnonce), "%d", php_rand_r(&ctx));
+					snprintf(cnonce, sizeof(cnonce), "%d", php_rand(TSRMLS_C));
 					PHP_MD5Update(&md5ctx, (unsigned char*)cnonce, strlen(cnonce));
 					PHP_MD5Final(hash, &md5ctx);
 					make_digest(cnonce, hash);
