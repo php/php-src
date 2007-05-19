b828d1939fb17669ac733027bc12eb0ead5bd521
Ilia Alshanetsky <iliaa@php.net>
2007-05-19 20:30:35 +0000

    Optimized digest generation in md5() and sha1() functions by using code from ext/hash. This makes the functions 20-50% faster on short strings.

--- ext/standard/md5.c
+++ ext/standard/md5.c
@@ -28,14 +28,19 @@
 
 PHPAPI void make_digest(char *md5str, unsigned char *digest)
 {
+	return make_digest_ex(md5str, digest, strlen(digest));
+}
+
+PHPAPI void make_digest_ex(char *md5str, unsigned char *digest, int len)
+{
+	static const char hexits[17] = "0123456789abcdef";
 	int i;
 
-	for (i = 0; i < 16; i++) {
-		sprintf(md5str, "%02x", digest[i]);
-		md5str += 2;
+	for (i = 0; i < len; i++) {
+		md5str[i * 2]       = hexits[digest[i] >> 4];
+		md5str[(i * 2) + 1] = hexits[digest[i] &  0x0F];
 	}
-
-	*md5str = '\0';
+	md5str[len * 2] = '\0';
 }
 
 /* {{{ proto string md5(string str, [ bool raw_output])
@@ -60,7 +65,7 @@ PHP_NAMED_FUNCTION(php_if_md5)
 	if (raw_output) {
 		RETURN_STRINGL(digest, 16, 1);
 	} else {
-		make_digest(md5str, digest);
+		make_digest_ex(md5str, digest, 16);
 		RETVAL_STRING(md5str, 1);
 	}
 
@@ -107,7 +112,7 @@ PHP_NAMED_FUNCTION(php_if_md5_file)
 	if (raw_output) {
 		RETURN_STRINGL(digest, 16, 1);
 	} else {
-		make_digest(md5str, digest);
+		make_digest_ex(md5str, digest, 16);
 		RETVAL_STRING(md5str, 1);
 	}
 }
--- ext/standard/sha1.c
+++ ext/standard/sha1.c
@@ -23,17 +23,11 @@
 /* This code is heavily based on the PHP md5 implementation */ 
 
 #include "sha1.h"
+#include "md5.h"
 
 PHPAPI void make_sha1_digest(char *sha1str, unsigned char *digest)
 {
-	int i;
-
-	for (i = 0; i < 20; i++) {
-		sprintf(sha1str, "%02x", digest[i]);
-		sha1str += 2;
-	}
-
-	*sha1str = '\0';
+	return make_digest_ex(sha1str, digest, 20);
 }
 
 /* {{{ proto string sha1(string str [, bool raw_output])
@@ -58,7 +52,7 @@ PHP_FUNCTION(sha1)
 	if (raw_output) {
 		RETURN_STRINGL(digest, 20, 1);
 	} else {
-		make_sha1_digest(sha1str, digest);
+		make_digest_ex(sha1str, digest, 20);
 		RETVAL_STRING(sha1str, 1);
 	}
 
@@ -107,7 +101,7 @@ PHP_FUNCTION(sha1_file)
 	if (raw_output) {
 		RETURN_STRINGL(digest, 20, 1);
 	} else {
-		make_sha1_digest(sha1str, digest);
+		make_digest_ex(sha1str, digest, 20);
 		RETVAL_STRING(sha1str, 1);
 	}
 }
