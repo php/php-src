7ddded83603fc99421944d5dfc92bd881bafe781
davidc <davidc@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-24 08:42:23 +0000

    - Fixed few bugs - Made sure that the word type[s] is correctly written and not typ*
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@236233 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar/phar.php
+++ ext/phar/phar/phar.php
@@ -10,40 +10,35 @@
  * Phar Command
  */
 
-if (!extension_loaded("phar"))
-{
-	if (!class_exists("PHP_Archive"))
-	{
-		echo "PHP_Archive not available.\n";
-		exit(1);
-	}
-	if (!class_exists("Phar"))
-	{
-		class Phar extends PHP_Archive {}
-	}
+if (!extension_loaded("phar")) {
+    if (!class_exists("PHP_Archive")) {
+        echo "PHP_Archive not available.\n";
+        exit(1);
+    }
+
+    if (!class_exists("Phar")) {
+        class Phar extends PHP_Archive {}
+    }
 }
 
-foreach(array("SPL", "Reflection") as $ext)
-{
-	if (!extension_loaded($ext))
-	{
-		echo "$argv[0] requires PHP extension $ext.\n";
-		exit(1);
-	}
+foreach(array("SPL", "Reflection") as $ext) {
+    if (!extension_loaded($ext)) {
+        echo "$argv[0] requires PHP extension $ext.\n";
+        exit(1);
+    }
 }
 
 function command_include($file)
 {
-	$file = 'phar://' . __FILE__ . '/' . $file;
-	if (file_exists($file))
-	{
-		include($file);
-	}
+    $file = 'phar://' . __FILE__ . '/' . $file;
+    if (file_exists($file)) {
+        include($file);
+    }
 }
 
 function command_autoload($classname)
 {
-	command_include(strtolower($classname) . '.inc');
+    command_include(strtolower($classname) . '.inc');
 }
 
 Phar::loadPhar(__FILE__);
