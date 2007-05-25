d2170dab978ef32e30073e241075bda95d74ff9b
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-25 09:20:01 +0000

    Slightly cleaner code
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@236315 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/SAPI.c
+++ main/SAPI.c
@@ -988,8 +988,8 @@ SAPI_API time_t sapi_get_request_time(TSRMLS_D)
 {
 	if(SG(global_request_time)) return SG(global_request_time);
 
-	if (sapi_module.get_request_time) {
-		SG(global_request_time) = (SG(server_context))?sapi_module.get_request_time(TSRMLS_C):time(0);
+	if (sapi_module.get_request_time && SG(server_context)) {
+		SG(global_request_time) = sapi_module.get_request_time(TSRMLS_C);
 	} else {
 		SG(global_request_time) = time(0);
 	}
