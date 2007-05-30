a7efd81d36e586dd53f8defb3a141a41e7dc7b5a
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-30 00:33:13 +0000

    Fixed an interger overflow inside chunk_split(), identified by Gerhard Wagner
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@236678 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -1956,11 +1956,18 @@ static char *php_chunk_split(char *src, int srclen, char *end, int endlen, int c
 	char *p, *q;
 	int chunks; /* complete chunks! */
 	int restlen;
+	int out_len; 
 
 	chunks = srclen / chunklen;
 	restlen = srclen - chunks * chunklen; /* srclen % chunklen */
 
-	dest = safe_emalloc((srclen + (chunks + 1) * endlen + 1), sizeof(char), 0);
+	out_len = (srclen + (chunks + 1) * endlen + 1);
+
+	if (out_len > INT_MAX || out_len <= 0) {
+		return NULL;
+	}
+
+	dest = safe_emalloc(out_len, sizeof(char), 0);
 
 	for (p = src, q = dest; p < (src + srclen - chunklen + 1); ) {
 		memcpy(q, p, chunklen);
