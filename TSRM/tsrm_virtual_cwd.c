59f3b526df5d2133ddfee19fb3566d97c22e8add
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-05-30 10:50:47 +0000

    MFH: fix lstat() function resolving symlinks
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@236690 c90b9560-bf6c-de11-be94-00142212c4b1

--- TSRM/tsrm_virtual_cwd.c
+++ TSRM/tsrm_virtual_cwd.c
@@ -1009,7 +1009,7 @@ CWD_API int virtual_lstat(const char *path, struct stat *buf TSRMLS_DC)
 	int retval;
 
 	CWD_STATE_COPY(&new_state, &CWDG(cwd));
-	if (virtual_file_ex(&new_state, path, NULL, CWD_REALPATH)) {
+	if (virtual_file_ex(&new_state, path, NULL, CWD_EXPAND)) {
 		CWD_STATE_FREE(&new_state);
 		return -1;
 	}
