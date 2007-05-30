699c05e62461ed53edf69485972ef1da31a6f1d7
Ilia Alshanetsky <iliaa@php.net>
2007-05-30 00:35:41 +0000

    MFH: Fixed an interger overflow inside chunk_split(), identified by Gerhard Wagner

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -1511,11 +1511,18 @@ static PHP_ATTRIBUTE_MALLOC char *php_chunk_split(char *src, int srclen, char *e
 	char *p, *q;
 	int chunks; /* complete chunks! */
 	int restlen;
+	int out_len;
 
 	chunks = srclen / chunklen;
 	restlen = srclen - chunks * chunklen; /* srclen % chunklen */
 
-	dest = safe_emalloc(sizeof(char), (srclen + (chunks + 1) * endlen + 1), 0);
+	out_len = (srclen + (chunks + 1) * endlen + 1);
+
+	if (out_len > INT_MAX || out_len <= 0) {
+		return NULL;
+	}
+
+	dest = safe_emalloc(out_len, sizeof(char), 0);
 
 	for (p = src, q = dest; p < (src + srclen - chunklen + 1); ) {
 		memcpy(q, p, chunklen);
