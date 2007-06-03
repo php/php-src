9cfd000ba31ad83277aa4049a7c7265e4dd0aedf
Ilia Alshanetsky <iliaa@php.net>
2007-06-03 18:53:51 +0000

    MFB: Corrected fix for CVE-2007-2872

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -3083,7 +3083,7 @@ static char* php_chunk_split(char *src, int srclen, char *end, int endlen, int c
 	int chunks; /* complete chunks! */
 	int restlen;
 	int charsize = sizeof(char);
-	int out_len;
+	float out_len;
 
 	if (str_type == IS_UNICODE) {
 		charsize = sizeof(UChar);
@@ -3092,13 +3092,15 @@ static char* php_chunk_split(char *src, int srclen, char *end, int endlen, int c
 	chunks = srclen / chunklen;
 	restlen = srclen - chunks * chunklen; /* srclen % chunklen */
 
-	out_len = (srclen + (chunks + 1) * endlen + 1);
+	out_len = chunks + 1;
+	out_len *= endlen;
+	out_len += srclen + 1;
 
 	if ((out_len > INT_MAX || out_len <= 0) || ((out_len * charsize) > INT_MAX || (out_len * charsize) <= 0)) {
 		return NULL;
 	}
 
-	dest = safe_emalloc(out_len, charsize, 0);
+	dest = safe_emalloc((int)out_len, charsize, 0);
 
 	for (p = src, q = dest; p < (src + charsize * (srclen - chunklen + 1)); ) {
 		memcpy(q, p, chunklen * charsize);
--- ext/standard/tests/strings/chunk_split.phpt
+++ ext/standard/tests/strings/chunk_split.phpt
@@ -12,6 +12,12 @@ $b=1;
 $c=str_repeat("B", 65535);
 var_dump(chunk_split($a,$b,$c));
 
+$a=str_repeat("B", 65536);
+$b=1;
+$c=str_repeat("B", 65536);
+var_dump(chunk_split($a,$b,$c));
+
+
 ?>
 --EXPECT--
 a-b-c-
@@ -24,4 +30,5 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 test|end
+bool(false)
 bool(false)
