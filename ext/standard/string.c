0252a0f840465bd4e8f532c3e7d5b8436dc170e3
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-06-03 18:47:10 +0000

    Corrected fix for CVE-2007-2872
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@236909 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -1956,18 +1956,20 @@ static char *php_chunk_split(char *src, int srclen, char *end, int endlen, int c
 	char *p, *q;
 	int chunks; /* complete chunks! */
 	int restlen;
-	int out_len; 
+	float out_len; 
 
 	chunks = srclen / chunklen;
 	restlen = srclen - chunks * chunklen; /* srclen % chunklen */
 
-	out_len = (srclen + (chunks + 1) * endlen + 1);
+	out_len = chunks + 1;
+	out_len *= endlen;
+	out_len += srclen + 1;
 
 	if (out_len > INT_MAX || out_len <= 0) {
 		return NULL;
 	}
 
-	dest = safe_emalloc(out_len, sizeof(char), 0);
+	dest = safe_emalloc((int)out_len, sizeof(char), 0);
 
 	for (p = src, q = dest; p < (src + srclen - chunklen + 1); ) {
 		memcpy(q, p, chunklen);
