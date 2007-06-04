0f0c569d7bbc948f8eb4677624a6b4dd6d1a29fa
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-06-04 15:38:12 +0000

    MFH: add missing open_basedir checks to CGI
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@236958 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -1682,7 +1682,9 @@ consult the installation file that came with this distribution, or visit \n\
 		*/
 		retval = FAILURE;
 		if (cgi || SG(request_info).path_translated) {
-			retval = php_fopen_primary_script(&file_handle TSRMLS_CC);
+			if (!php_check_open_basedir(SG(request_info).path_translated TSRMLS_CC)) {
+				retval = php_fopen_primary_script(&file_handle TSRMLS_CC);
+			}
 		}
 		/* 
 			if we are unable to open path_translated and we are not
@@ -1704,9 +1706,21 @@ consult the installation file that came with this distribution, or visit \n\
 				goto fastcgi_request_done;
 			}
 #endif
+
+			STR_FREE(SG(request_info).path_translated);
+
+			if (free_query_string && SG(request_info).query_string) {
+				free(SG(request_info).query_string);
+				SG(request_info).query_string = NULL;
+			}
+
 			php_request_shutdown((void *) 0);
 			SG(server_context) = NULL;
 			php_module_shutdown(TSRMLS_C);
+			sapi_shutdown();
+#ifdef ZTS
+			tsrm_shutdown();
+#endif
 			return FAILURE;
 		}
 
