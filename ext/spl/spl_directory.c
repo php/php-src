cf53dfaff3c3e71afa319af92168ed106acbb93e
Antony Dovgal <tony2001@php.net>
2007-06-05 12:02:46 +0000

    MFH

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -829,7 +829,11 @@ SPL_METHOD(SplFileInfo, getLinkTarget)
 
 	php_set_error_handling(EH_THROW, spl_ce_RuntimeException TSRMLS_CC);
 
+#ifdef HAVE_SYMLINK
 	ret = readlink(intern->file_name, buff, MAXPATHLEN-1);
+#else
+	ret = -1; /* always fail if not implemented */
+#endif
 
 	if (ret == -1) {
 		zend_throw_exception_ex(spl_ce_RuntimeException, 0 TSRMLS_CC, "Unable to read link %s, error: %s", intern->file_name, strerror(errno));
