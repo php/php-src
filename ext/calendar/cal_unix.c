c9805e7b9240c9513c4fb6add41b004328caf463
Antony Dovgal <tony2001@php.net>
2007-06-07 08:44:41 +0000

    MFH: check return value of *time_r() functions for NULL

--- ext/calendar/cal_unix.c
+++ ext/calendar/cal_unix.c
@@ -50,6 +50,10 @@ PHP_FUNCTION(unixtojd)
   }
 
   ta = php_localtime_r(&t, &tmbuf);
+  if (!ta) {
+	  RETURN_FALSE;
+  }
+
   jdate = GregorianToSdn(ta->tm_year+1900, ta->tm_mon+1, ta->tm_mday);
   
   RETURN_LONG(jdate);
