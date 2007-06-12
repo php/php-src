cf3bba6424b5b95c93da011cf09c00f48c35a0f0
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-06-12 14:07:42 +0000

    Display PCRE ini entries as part of module info
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@237489 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -138,6 +138,8 @@ static PHP_MINFO_FUNCTION(pcre)
 	php_info_print_table_row(2, "PCRE (Perl Compatible Regular Expressions) Support", "enabled" );
 	php_info_print_table_row(2, "PCRE Library Version", pcre_version() );
 	php_info_print_table_end();
+
+	DISPLAY_INI_ENTRIES();
 }
 /* }}} */
 
