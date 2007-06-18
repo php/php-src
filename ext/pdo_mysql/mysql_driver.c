b925a9248b11823f834a139a7af106aaea1ca087
Stanislav Malyshev <stas@php.net>
2007-06-18 21:51:32 +0000

    Fix INFILE LOCAL option handling with MySQL - now not allowed when open_basedir or safe_mode is active

--- ext/pdo_mysql/mysql_driver.c
+++ ext/pdo_mysql/mysql_driver.c
@@ -492,7 +492,11 @@ static int pdo_mysql_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 			pdo_mysql_error(dbh);
 			goto cleanup;
 		}
-		
+
+		if ((PG(open_basedir) && PG(open_basedir)[0] != '\0') || PG(safe_mode)) {
+			local_infile = 0;
+		}
+
 		if (mysql_options(H->server, MYSQL_OPT_LOCAL_INFILE, (const char *)&local_infile)) {
 			pdo_mysql_error(dbh);
 			goto cleanup;
