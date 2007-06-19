d6200e650192e014b82569401499dd9c5cb2da1b
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-06-19 22:09:49 +0000

    Fixed a memory leak inside load_all_certs_file()
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@238098 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -1109,17 +1109,20 @@ static STACK_OF(X509) * load_all_certs_from_file(char *certfile)
 	}
 
 	if (php_openssl_safe_mode_chk(certfile TSRMLS_CC)) {
+		sk_X509_free(stack);
 		goto end;
 	}
 
 	if(!(in=BIO_new_file(certfile, "r"))) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "error opening the file, %s", certfile);
+		sk_X509_free(stack);
 		goto end;
 	}
 
 	/* This loads from a file, a stack of x509/crl/pkey sets */
 	if(!(sk=PEM_X509_INFO_read_bio(in, NULL, NULL, NULL))) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "error reading the file, %s", certfile);
+		sk_X509_free(stack);
 		goto end;
 	}
 
