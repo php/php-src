e9658dfc4d9e5164391e7e0984c87890ad90fabf
Stanislav Malyshev <stas@php.net>
2007-06-28 20:27:35 +0000

    remove unused vars

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -3341,11 +3341,10 @@ void zend_do_fetch_property(znode *result, znode *object, znode *property TSRMLS
 void zend_do_halt_compiler_register(TSRMLS_D)
 {
 	char *name, *cfilename;
-	char haltoff[] = ZEND_HALT_CONSTANT_NAME;
 	int len, clen;
 	cfilename = zend_get_compiled_filename(TSRMLS_C);
 	clen = strlen(cfilename);
-	zend_mangle_property_name(&name, &len, haltoff,
+	zend_mangle_property_name(&name, &len, ZEND_HALT_CONSTANT_NAME,
 		sizeof(ZEND_HALT_CONSTANT_NAME)-1, cfilename, clen, 0);
 	zend_register_long_constant(name, len+1, zend_get_scanned_file_offset(TSRMLS_C), CONST_CS, 0 TSRMLS_CC);
 	pefree(name, 0);
