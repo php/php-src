e99013d039b5f1d012237a1f8bea5c5cd9622bf4
Dmitry Stogov <dmitry@php.net>
2007-07-03 14:48:01 +0000

    Store existent file in realpath cache

--- TSRM/tsrm_virtual_cwd.c
+++ TSRM/tsrm_virtual_cwd.c
@@ -547,6 +547,7 @@ CWD_API int virtual_file_ex(cwd_state *state, const char *path, verify_path_func
 			}
 			goto no_realpath;
 		}
+		use_realpath = CWD_REALPATH;
 		CWD_STATE_COPY(&old_state, state);
 
 		state->cwd_length = strlen(resolved_path);
