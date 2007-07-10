ad31680f346d84ed2e315ff088be1fbdfc3b48d6
Sascha Schumann <sas@php.net>
2007-07-10 20:13:29 +0000

    - fix concurrent reader/writer use case   (we read, while the other process writes -> if we don't try    read we won't notice that the file has been growing.)

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -333,9 +333,6 @@ static size_t php_stdiop_read(php_stream *stream, char *buf, size_t count TSRMLS
 	assert(data != NULL);
 
 	if (data->fd >= 0) {
-		if (stream->eof && !data->is_pipe) {
-			return 0;
-		}
 		ret = read(data->fd, buf, count);
 
 		if (ret == (size_t)-1 && errno == EINTR) {
