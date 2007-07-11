c5d186f16c9132cc53124e858b18b3b0c1932472
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-07-11 10:24:04 +0000

    MFH: No return values for void function
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@239473 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -357,11 +357,11 @@ PHPAPI void php_log_err(char *log_message TSRMLS_DC)
 		}
 #endif
 		if (PG(safe_mode) && (!php_checkuid(PG(error_log), NULL, CHECKUID_CHECK_FILE_AND_DIR|CHECKUID_NO_ERRORS))) {
-			return FAILURE;
+			return;
 		}
 
 		if (PG(open_basedir) && php_check_open_basedir_ex(PG(error_log), 0 TSRMLS_CC)) {
-			return FAILURE;
+			return;
 		}
 		
 		fd = VCWD_OPEN_MODE(PG(error_log), O_CREAT | O_APPEND | O_WRONLY, 0644);
