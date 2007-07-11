8e399854e616970eb4f2814ec099156ff22d7eea
Jani Taskinen <jani@php.net>
2007-07-11 10:23:47 +0000

    - No return value for void function

--- main/main.c
+++ main/main.c
@@ -454,7 +454,7 @@ PHPAPI void php_log_err(char *log_message TSRMLS_DC)
 		}
 #endif
 		if (PG(open_basedir) && php_check_open_basedir_ex(PG(error_log), 0 TSRMLS_CC)) {
-			return FAILURE;
+			return;
 		}
 		fd = VCWD_OPEN_MODE(PG(error_log), O_CREAT | O_APPEND | O_WRONLY, 0644);
 		if (fd != -1) {
