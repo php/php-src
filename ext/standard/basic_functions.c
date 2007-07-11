bf16e86999873fd16805fe6782a65e3e79acb0d4
Dmitry Stogov <dmitry@php.net>
2007-07-11 15:52:44 +0000

    Unicode support

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -5030,8 +5030,8 @@ PHP_FUNCTION(error_get_last)
 	if (PG(last_error_message)) {
 		array_init(return_value);
 		add_ascii_assoc_long_ex(return_value, "type", sizeof("type"), PG(last_error_type));
-		add_ascii_assoc_string_ex(return_value, "message", sizeof("message"), PG(last_error_message), 1);
-		add_ascii_assoc_string_ex(return_value, "file", sizeof("file"), PG(last_error_file)?PG(last_error_file):"-", 1 );
+		add_ascii_assoc_rt_string_ex(return_value, "message", sizeof("message"), PG(last_error_message), 1);
+		add_ascii_assoc_rt_string_ex(return_value, "file", sizeof("file"), PG(last_error_file)?PG(last_error_file):"-", 1 );
 		add_ascii_assoc_long_ex(return_value, "line", sizeof("line"), PG(last_error_lineno));
 	}
 }
