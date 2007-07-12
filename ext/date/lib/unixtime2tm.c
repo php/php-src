1e096be0b3d795812565d708d8fc2619c42c88fa
Derick Rethans <derick@php.net>
2007-07-12 18:56:42 +0000

    - Fixed bug #41964 (strtotime returns a timestamp for non-time string of   pattern '(A|a) .+'). - Fixed bug #41844 (Format returns incorrect number of digits for negative   years -0001 to -0999). - Fixed bug #41842 (Cannot create years < 0100 & negative years with   date_create or new DateTime). - Fixed bug #41709 (strtotime() does not handle 00.00.0000). - Fixed bug #41523 (strtotime('0000-00-00 00:00:00') is parsed as 1999-11-30).

--- ext/date/lib/unixtime2tm.c
+++ ext/date/lib/unixtime2tm.c
@@ -69,9 +69,11 @@ void timelib_unixtime2gmt(timelib_time* tm, timelib_sll ts)
 		/* Guess why this might be for, it has to do with a pope ;-). It's also
 		 * only valid for Great Brittain and it's colonies. It needs fixing for
 		 * other locales. *sigh*, why is this crap so complex! */
+		/*
 		if (ts <= TIMELIB_LL_CONST(-6857352000)) {
 			tmp_days -= 11;
 		}
+		*/
 
 		while (tmp_days <= 0) {
 			cur_year--;
