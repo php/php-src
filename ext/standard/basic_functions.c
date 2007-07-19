93af1c1f84aca471a2e6d48a0705b5db66d5a819
Antony Dovgal <tony2001@php.net>
2007-07-19 15:49:45 +0000

    MFH: fix #41989 (move_uploaded_file() & relative path in ZTS mode)

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -6096,7 +6096,7 @@ PHP_FUNCTION(move_uploaded_file)
 	}
 
 	VCWD_UNLINK(Z_STRVAL_PP(new_path));
-	if (rename(Z_STRVAL_PP(path), Z_STRVAL_PP(new_path)) == 0) {
+	if (VCWD_RENAME(Z_STRVAL_PP(path), Z_STRVAL_PP(new_path)) == 0) {
 		successful = 1;
 	} else if (php_copy_file_ex(Z_STRVAL_PP(path), Z_STRVAL_PP(new_path), STREAM_DISABLE_OPEN_BASEDIR TSRMLS_CC) == SUCCESS) {
 		VCWD_UNLINK(Z_STRVAL_PP(path));
