221fefdf6fe792730c133d4196d670750b3dad12
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-07-21 11:24:53 +0000

    - MFH: Allow the use of a user defined template.rc file to override the   default ones. This makes it possible to include the extension's version   inside the version properties of the generated extension DLL instead   of the PHP version the extension is build against.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@240073 c90b9560-bf6c-de11-be94-00142212c4b1

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -17,7 +17,7 @@
   +----------------------------------------------------------------------+
 */
 
-// $Id: confutils.js,v 1.60.2.1.2.6 2007-03-19 03:15:48 edink Exp $
+// $Id: confutils.js,v 1.60.2.1.2.7 2007-07-21 11:24:53 derick Exp $
 
 var STDOUT = WScript.StdOut;
 var STDERR = WScript.StdErr;
@@ -860,7 +860,19 @@ function generate_version_info_resource(makefiletarget, creditspath)
 	if (makefiletarget.match(new RegExp("\\.exe$"))) {
 		logo = " /D WANT_LOGO ";
 	}
-	
+
+	/**
+	 * Use user supplied template.rc if it exists
+	 */
+	if (FSO.FileExists(creditspath + '\\template.rc')) {
+		MFO.WriteLine("$(BUILD_DIR)\\" + resname + ": " + creditspath + "\\template.rc");
+		MFO.WriteLine("\t$(RC) /fo $(BUILD_DIR)\\" + resname + logo +
+		   	' /d FILE_DESCRIPTION="\\"' + res_desc + '\\"" /d FILE_NAME="\\"' + makefiletarget +
+	   		'\\"" /d PRODUCT_NAME="\\"' + res_prod_name + '\\"" /d THANKS_GUYS="\\"' +
+			thanks + '\\"" ' + creditspath + '\\template.rc');
+		return resname;
+	}
+
 	MFO.WriteLine("$(BUILD_DIR)\\" + resname + ": win32\\build\\template.rc");
 	MFO.WriteLine("\t$(RC) /fo $(BUILD_DIR)\\" + resname + logo +
 	   	' /d FILE_DESCRIPTION="\\"' + res_desc + '\\"" /d FILE_NAME="\\"' + makefiletarget +
