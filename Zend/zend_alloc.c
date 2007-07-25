e9ccedf4276b905c0563337ddb38299b2ed4a7c5
Dmitry Stogov <dmitry@php.net>
2007-07-25 11:13:17 +0000

    Fixed bug #41713 (Persistent memory consumption on win32 since 5.2)

--- Zend/zend_alloc.c
+++ Zend/zend_alloc.c
@@ -1591,6 +1591,15 @@ ZEND_API void zend_mm_shutdown(zend_mm_heap *heap, int full_shutdown, int silent
 			free(heap);
 		}
 	} else {
+#ifdef HAVE_MEM_WIN32
+		/* FIX for bug #41713 */
+		/* TODO: add new "compact" handler */
+		if (storage->handlers->dtor == zend_mm_mem_win32_dtor &&
+		    storage->handlers->init == zend_mm_mem_win32_init) {
+		    HeapDestroy((HANDLE)storage->data);
+		    storage->data = (void*)HeapCreate(HEAP_NO_SERIALIZE, 0, 0);
+		}
+#endif
 		heap->segments_list = NULL;
 		zend_mm_init(heap);
 		heap->real_size = 0;
