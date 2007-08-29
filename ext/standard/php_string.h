21dcbb2ce9253aed9fc6139ac3ac1d9cb09bf9a0
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-08-29 14:11:30 +0000

    - Put the PHPAPI php_u_str_to_str() function in the .h, just like the rest.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@241908 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -148,6 +148,8 @@ PHPAPI char *php_str_to_str_ex(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length, int case_sensitivity, int *replace_count);
 PHPAPI char *php_str_to_str(char *haystack, int length, char *needle,
 		int needle_len, char *str, int str_len, int *_new_length);
+PHPAPI UChar *php_u_str_to_str_ex(UChar *haystack, int length,
+	UChar *needle, int needle_len, UChar *repl, int repl_len, int *_new_length, int *replace_count);
 PHPAPI char *php_trim(char *c, int len, char *what, int what_len, zval *return_value, int mode TSRMLS_DC);
 PHPAPI int php_u_strip_tags(UChar *rbuf, int len, int *stateptr, UChar *allow, int allow_len TSRMLS_DC);
 PHPAPI size_t php_strip_tags(char *rbuf, int len, int *state, char *allow, int allow_len);
