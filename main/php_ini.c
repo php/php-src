820cbd5d013054422b54a16a827c2347a96922a2
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-08-31 08:09:41 +0000

    ws + apify cfg_get_entry
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@241988 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_ini.c
+++ main/php_ini.c
@@ -592,7 +592,7 @@ void php_ini_register_extensions(TSRMLS_D)
 
 /* {{{ cfg_get_entry
  */
-zval *cfg_get_entry(char *name, uint name_length)
+PHPAPI zval *cfg_get_entry(char *name, uint name_length)
 {
 	zval *tmp;
 
@@ -609,7 +609,7 @@ zval *cfg_get_entry(char *name, uint name_length)
 PHPAPI int cfg_get_long(char *varname, long *result)
 {
 	zval *tmp, var;
-	
+
 	if (zend_hash_find(&configuration_hash, varname, strlen(varname) + 1, (void **) &tmp) == FAILURE) {
 		*result = 0;
 		return FAILURE;
@@ -627,7 +627,7 @@ PHPAPI int cfg_get_long(char *varname, long *result)
 PHPAPI int cfg_get_double(char *varname, double *result)
 {
 	zval *tmp, var;
-	
+
 	if (zend_hash_find(&configuration_hash, varname, strlen(varname) + 1, (void **) &tmp) == FAILURE) {
 		*result = (double) 0;
 		return FAILURE;
