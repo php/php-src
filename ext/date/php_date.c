14df6eb97541666697622ffe115667edc2213496
Derick Rethans <derick@php.net>
2007-09-03 12:12:51 +0000

    - Ugh, fix this properly.

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -2316,7 +2316,7 @@ PHP_FUNCTION(timezone_transitions_get)
 		array_init(element);
 		add_ascii_assoc_long(element, "ts",     tzobj->tz->trans[i]);
 		if (UG(unicode)) {
-			add_assoc_unicode(element, "time", (UChar*) php_format_date(DATE_FORMAT_ISO8601, 13, tzobj->tz->trans[i], 0 TSRMLS_CC), 0);
+			add_ascii_assoc_unicode(element, "time", (UChar*) php_format_date(DATE_FORMAT_ISO8601, 13, tzobj->tz->trans[i], 0 TSRMLS_CC), 0);
 		} else {
 			add_assoc_string(element, "time", php_format_date(DATE_FORMAT_ISO8601, 13, tzobj->tz->trans[i], 0 TSRMLS_CC), 0);
 		}
