6f95c12b38cbde8908786fd06a32139aff043cc0
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-09-11 21:03:48 +0000

    - [DOC] add alpha support for imagefilter's IMG_FILTER_COLORIZE
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_2@242422 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -5178,8 +5178,9 @@ static void php_image_filter_colorize(INTERNAL_FUNCTION_PARAMETERS)
 	zval *SIM;
 	gdImagePtr im_src;
 	long r,g,b,tmp;
+	long a = 0;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rllll", &SIM, &tmp, &r, &g, &b) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rllll|l", &SIM, &tmp, &r, &g, &b, &a) == FAILURE) {
 		RETURN_FALSE;
 	}
 
@@ -5189,7 +5190,7 @@ static void php_image_filter_colorize(INTERNAL_FUNCTION_PARAMETERS)
 		RETURN_FALSE;
 	}
 
-	if (gdImageColor(im_src, (int) r, (int) g, (int) b) == 1) {
+	if (gdImageColor(im_src, (int) r, (int) g, (int) b, (int) a) == 1) {
 		RETURN_TRUE;
 	}
 
@@ -5298,7 +5299,7 @@ PHP_FUNCTION(imagefilter)
 		php_image_filter_smooth
 	};
 
-	if (ZEND_NUM_ARGS() < 2 || ZEND_NUM_ARGS() > 5) {
+	if (ZEND_NUM_ARGS() < 2 || ZEND_NUM_ARGS() > 6) {
 		WRONG_PARAM_COUNT;
 	} else if (zend_parse_parameters(2 TSRMLS_CC, "rl", &tmp, &filtertype) == FAILURE) {
 		return;
