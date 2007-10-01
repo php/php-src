086289983e574e1c396f57a1a2e238f899e543cd
Ilia Alshanetsky <iliaa@php.net>
2007-10-01 14:51:11 +0000

    Refactor code to make fixes for coverity issues #385 and #386 a bit more obvious

--- main/main.c
+++ main/main.c
@@ -218,7 +218,11 @@ static PHP_INI_MH(OnUpdateTimeout)
 static int php_get_display_errors_mode(char *value, int value_length)
 {
 	int mode;
-	
+
+	if (!value) {
+		return PHP_DISPLAY_ERRORS_STDOUT;
+	}
+
 	if (value_length == 2 && !strcasecmp("on", value)) {
 		mode = PHP_DISPLAY_ERRORS_STDOUT;
 	} else if (value_length == 3 && !strcasecmp("yes", value)) {
@@ -229,14 +233,13 @@ static int php_get_display_errors_mode(char *value, int value_length)
 		mode = PHP_DISPLAY_ERRORS_STDERR;
 	} else if (value_length == 6 && !strcasecmp(value, "stdout")) {
 		mode = PHP_DISPLAY_ERRORS_STDOUT;
-	} else if (value) {
+	} else {
 		mode = atoi(value);
 		if (mode && mode != PHP_DISPLAY_ERRORS_STDOUT && mode != PHP_DISPLAY_ERRORS_STDERR) {
 			mode = PHP_DISPLAY_ERRORS_STDOUT;
 		}
-	} else {
-		mode = PHP_DISPLAY_ERRORS_STDOUT;
 	}
+
 	return mode;
 }
 /* }}} */
