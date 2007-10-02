362613eb8fdfefc90438562eb6a8b1a25a11a308
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-10-02 08:47:22 +0000

    Fixed access to freed memory in tests/classes/__call_001.phpt
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@243363 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -174,6 +174,7 @@ static int zend_do_fcall_common_helper_SPEC(ZEND_OPCODE_HANDLER_ARGS)
 	if (EX(function_state).function->type == ZEND_INTERNAL_FUNCTION) {
 		ALLOC_ZVAL(EX_T(opline->result.u.var).var.ptr);
 		INIT_ZVAL(*(EX_T(opline->result.u.var).var.ptr));
+        EX_T(opline->result.u.var).var.fcall_returned_reference = EX(function_state).function->common.return_reference;
 
 		if (EX(function_state).function->common.arg_info) {
 			zend_uint i=0;
@@ -206,8 +207,6 @@ static int zend_do_fcall_common_helper_SPEC(ZEND_OPCODE_HANDLER_ARGS)
 */
 		if (!return_value_used) {
 			zval_ptr_dtor(&EX_T(opline->result.u.var).var.ptr);
-		} else {
-			EX_T(opline->result.u.var).var.fcall_returned_reference = EX(function_state).function->common.return_reference;
 		}
 	} else if (EX(function_state).function->type == ZEND_USER_FUNCTION) {
 		HashTable *function_symbol_table;
