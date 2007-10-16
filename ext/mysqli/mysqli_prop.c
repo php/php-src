1421ccd4c08289705dc6902febc5e099dfbd4309
Andrey Hristov <andrey@php.net>
2007-10-16 12:55:05 +0000

    Fix bug due to shadowing of variables. This one is pretty-pretty rare, caught by source code inspection.

--- ext/mysqli/mysqli_prop.c
+++ ext/mysqli/mysqli_prop.c
@@ -86,8 +86,8 @@ static int __func(mysqli_object *obj, zval **retval TSRMLS_DC) \
 			ZVAL_LONG(*retval, l);\
 		} else { \
 			char *ret; \
-			int l = spprintf(&ret, 0, MYSQLI_LLU_SPEC, (my_ulonglong)l); \
-			ZVAL_STRINGL(*retval, ret, l, 0); \
+			int ret_len = spprintf(&ret, 0, MYSQLI_LLU_SPEC, (my_ulonglong)l); \
+			ZVAL_STRINGL(*retval, ret, ret_len, 0); \
 		} \
 	}\
 	return SUCCESS;\
