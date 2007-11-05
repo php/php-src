7eb0f143c6247760d9db2de0cc2e59264d5683b0
Jani Taskinen <jani@php.net>
2007-11-05 12:44:47 +0000

    ws + cs

--- ext/standard/cyr_convert.c
+++ ext/standard/cyr_convert.c
@@ -275,13 +275,12 @@ PHP_FUNCTION(convert_cyr_string)
 	int input_len, fr_cs_len, to_cs_len;
 	unsigned char *str;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "Sss", &input,
-							  &input_len, &fr_cs, &fr_cs_len, &to_cs, &to_cs_len) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "Sss", &input, &input_len, &fr_cs, &fr_cs_len, &to_cs, &to_cs_len) == FAILURE) {
 		return;
 	}
 
 	str = (unsigned char*) estrndup(input, input_len);
-	
+
 	php_convert_cyr_string(str, input_len, fr_cs[0], to_cs[0] TSRMLS_CC);
 	RETVAL_STRING((char *)str, 0)
 }
