144419b3a1423965526409d16afaa9530f34c182
Antony Dovgal <tony2001@php.net>
2007-11-12 16:56:39 +0000

    MFH: fix error message (reported by Felipe Nascimento)

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -3005,7 +3005,7 @@ void zend_do_declare_property(znode *var_name, znode *value, zend_uint access_ty
 	}
 
 	if (access_type & ZEND_ACC_FINAL) {
-		zend_error(E_COMPILE_ERROR, "Cannot declare property %s::$%s final, the final modifier is allowed only for methods",
+		zend_error(E_COMPILE_ERROR, "Cannot declare property %s::$%s final, the final modifier is allowed only for methods and classes",
 				   CG(active_class_entry)->name, var_name->u.constant.value.str.val);
 	}
 
