ebc540d2c9147ef8da5d56828651cf212a39ddf2
Hannes Magnusson <bjori@php.net>
2007-11-15 13:11:48 +0000

    - Allow numeric options - Add tests

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -4470,7 +4470,8 @@ static int parse_opts(char * opts, opt_struct ** result)
 	int i, count = 0;
 
 	for (i = 0; i < strlen(opts); i++) {
-		if ((opts[i] >= 65 && opts[i] <= 90) ||
+		if ((opts[i] >= 48 && opts[i] <= 57) ||
+			(opts[i] >= 65 && opts[i] <= 90) ||
 			(opts[i] >= 97 && opts[i] <= 122)
 		) {
 			count++;
@@ -4480,8 +4481,9 @@ static int parse_opts(char * opts, opt_struct ** result)
 	paras = safe_emalloc(sizeof(opt_struct), count, 0);
 	memset(paras, 0, sizeof(opt_struct) * count);
 	*result = paras;
-	while (	(*opts >= 65 && *opts <= 90) ||
-			(*opts >= 97 && *opts <= 122)
+	while ( (*opts >= 48 && *opts <= 57) || /* 0 - 9 */
+			(*opts >= 65 && *opts <= 90) || /* A - Z */
+			(*opts >= 97 && *opts <= 122)   /* a - z */
 	) {
 		paras->opt_char = *opts;
 		paras->need_param = (*(++opts) == ':') ? 1 : 0;
