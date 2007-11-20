3e8d7780ccb9550d5c0998afc7748f3b155dd56a
lwe <lwe@c90b9560-bf6c-de11-be94-00142212c4b1>
2007-11-20 21:36:20 +0000

    Win32 still fails to build ...
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@246530 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/interbase/ibase_query.c
+++ ext/interbase/ibase_query.c
@@ -120,12 +120,12 @@ static void _php_ibase_free_xsqlda(XSQLDA *sqlda) /* {{{ */
 
 static void _php_ibase_free_stmt_handle(ibase_db_link *link, isc_stmt_handle stmt TSRMLS_DC) /* {{{ */
 {
+	static char info[] = { isc_info_base_level, isc_info_end };
+
 	if (stmt) {
+		char res_buf[8];
 		IBDEBUG("Dropping statement handle (free_stmt_handle)...");
 		/* Only free statement if db-connection is still open */
-		static char info[] = { isc_info_base_level, isc_info_end };
-		char res_buf[8];
-
 		if (SUCCESS == isc_database_info(IB_STATUS, &link->handle, 
 							sizeof(info), info, sizeof(res_buf), res_buf)) {
 			if (isc_dsql_free_statement(IB_STATUS, &stmt, DSQL_drop)) {
