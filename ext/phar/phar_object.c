4ae47c1df96d3bc0e718ce469bf016a83301f71d
Greg Beaver <cellog@php.net>
2007-12-24 20:48:12 +0000

    remove unused variable

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -209,7 +209,6 @@ PHP_METHOD(Phar, webPhar)
 	zval *mimeoverride = NULL, *redirects = NULL;
 	char *alias = NULL, *error, *plain_map, *index_php;
 	int alias_len = 0, ret;
-	long dataoffset;
 	char *fname, *basename, *path_info, *mime_type, *entry;
 	int fname_len, entry_len, code, index_php_len = 0;
 	phar_entry_data *phar;
