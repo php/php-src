e33ce442148e2c18b1198d697157639b6acf381e
David Soria Parra <dsp@php.net>
2007-12-30 17:43:08 +0000

    - Fixed bug #43663 (Extending PDO class with a __call() function doesn't work).

--- ext/pdo/pdo_dbh.c
+++ ext/pdo/pdo_dbh.c
@@ -1254,12 +1254,17 @@ static union _zend_function *dbh_method_get(
 
 		if (zend_u_hash_find(dbh->cls_methods[PDO_DBH_DRIVER_METHOD_KIND_DBH],
 				ztype, lc_method_name, method_len+1, (void**)&fbc) == FAILURE) {
-			fbc = NULL;
+			if (std_object_handlers.get_method) {
+				fbc = std_object_handlers.get_method(object_pp, lc_method_name, method_len TSRMLS_CC);
+			}
+			if (!fbc) {
+				fbc = NULL;
+			}
 			goto out;
 		}
 		/* got it */
 	}
-	
+
 out:
 	pdo_zstr_efree(lc_method_name);
 	return fbc;
