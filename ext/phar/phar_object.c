392133fc06baaa9dcd3610eb2d4ebeef71b05e19
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-01-05 05:12:44 +0000

    new test
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@249934 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -477,6 +477,7 @@ PHP_METHOD(Phar, webPhar)
 		if (SUCCESS == zend_hash_find(Z_ARRVAL_P(rewrites), entry, entry_len+1, (void **) &fd_ptr)) {
 			if (IS_STRING != Z_TYPE_PP(fd_ptr)) {
 				zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC, "phar rewrite value for \"%s\" was not a string", entry);
+				return;
 			}
 			if (entry != index_php) {
 				efree(entry);
