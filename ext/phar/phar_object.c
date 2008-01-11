1c14ee14b86e096e2f2c75cb353d749052930d48
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-01-11 07:39:02 +0000

    stupid windows doesn't allow varargs macros, so remove it.  remove unused variable
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@250375 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -526,38 +526,49 @@ PHP_METHOD(Phar, webPhar)
 
 	/* set up mime types */
 	zend_hash_init(&mimetypes, sizeof(phar_mime_type *), zend_get_hash_value, NULL, 0);
-#define PHAR_SET_MIME(mimetype, ret, ...) \
+#define PHAR_SET_MIME(mimetype, ret, fileext) \
 		mime.mime = mimetype; \
 		mime.len = sizeof((mimetype))+1; \
 		mime.type = ret; \
-		{ \
-			char mimes[][5] = {__VA_ARGS__, "\0"}; \
-			int i = 0; \
-			for (; mimes[i][0]; i++) { \
-				zend_hash_add(&mimetypes, mimes[i], strlen(mimes[i]), (void *)&mime, sizeof(phar_mime_type), NULL); \
-			} \
-		}
+		zend_hash_add(&mimetypes, fileext, sizeof(fileext)-1, (void *)&mime, sizeof(phar_mime_type), NULL); \
 
 	PHAR_SET_MIME("text/html", PHAR_MIME_PHPS, "phps")
-	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "c", "cc", "cpp", "c++", "dtd", "h", "log", "rng", "txt", "xsd")
-	PHAR_SET_MIME("", PHAR_MIME_PHP, "php", "inc")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "c")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "cc")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "cpp")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "c++")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "dtd")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "h")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "log")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "rng")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "txt")
+	PHAR_SET_MIME("text/plain", PHAR_MIME_OTHER, "xsd")
+	PHAR_SET_MIME("", PHAR_MIME_PHP, "php")
+	PHAR_SET_MIME("", PHAR_MIME_PHP, "inc")
 	PHAR_SET_MIME("video/avi", PHAR_MIME_OTHER, "avi")
 	PHAR_SET_MIME("image/bmp", PHAR_MIME_OTHER, "bmp")
 	PHAR_SET_MIME("text/css", PHAR_MIME_OTHER, "css")
 	PHAR_SET_MIME("image/gif", PHAR_MIME_OTHER, "gif")
-	PHAR_SET_MIME("text/html", PHAR_MIME_OTHER, "htm", "html", "htmls")
+	PHAR_SET_MIME("text/html", PHAR_MIME_OTHER, "htm")
+	PHAR_SET_MIME("text/html", PHAR_MIME_OTHER, "html")
+	PHAR_SET_MIME("text/html", PHAR_MIME_OTHER, "htmls")
 	PHAR_SET_MIME("image/x-ico", PHAR_MIME_OTHER, "ico")
-	PHAR_SET_MIME("image/jpeg", PHAR_MIME_OTHER, "jpe", "jpg", "jpeg")
+	PHAR_SET_MIME("image/jpeg", PHAR_MIME_OTHER, "jpe")
+	PHAR_SET_MIME("image/jpeg", PHAR_MIME_OTHER, "jpg")
+	PHAR_SET_MIME("image/jpeg", PHAR_MIME_OTHER, "jpeg")
 	PHAR_SET_MIME("application/x-javascript", PHAR_MIME_OTHER, "js")
-	PHAR_SET_MIME("audio/midi", PHAR_MIME_OTHER, "mid", "midi")
+	PHAR_SET_MIME("audio/midi", PHAR_MIME_OTHER, "midi")
+	PHAR_SET_MIME("audio/midi", PHAR_MIME_OTHER, "midi")
 	PHAR_SET_MIME("audio/mod", PHAR_MIME_OTHER, "mod")
 	PHAR_SET_MIME("movie/quicktime", PHAR_MIME_OTHER, "mov")
 	PHAR_SET_MIME("audio/mp3", PHAR_MIME_OTHER, "mp3")
-	PHAR_SET_MIME("video/mpeg", PHAR_MIME_OTHER, "mpg", "mpeg")
+	PHAR_SET_MIME("video/mpeg", PHAR_MIME_OTHER, "mpg")
+	PHAR_SET_MIME("video/mpeg", PHAR_MIME_OTHER, "mpeg")
 	PHAR_SET_MIME("application/pdf", PHAR_MIME_OTHER, "pdf")
 	PHAR_SET_MIME("image/png", PHAR_MIME_OTHER, "png")
 	PHAR_SET_MIME("application/shockwave-flash", PHAR_MIME_OTHER, "swf")
-	PHAR_SET_MIME("image/tiff", PHAR_MIME_OTHER, "tif", "tiff")
+	PHAR_SET_MIME("image/tiff", PHAR_MIME_OTHER, "tif")
+	PHAR_SET_MIME("image/tiff", PHAR_MIME_OTHER, "tiff")
 	PHAR_SET_MIME("audio/wav", PHAR_MIME_OTHER, "wav")
 	PHAR_SET_MIME("image/xbm", PHAR_MIME_OTHER, "xbm")
 	PHAR_SET_MIME("text/xml", PHAR_MIME_OTHER, "xml")
