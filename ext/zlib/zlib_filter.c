e498c5a44628f07e3735aa3c231858bdf3004f14
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-01-12 22:03:32 +0000

    far better fix for bug #40189
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@250497 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zlib/zlib_filter.c
+++ ext/zlib/zlib_filter.c
@@ -100,7 +100,7 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 			consumed += desired;
 			bin += desired;
 
-			if (status == Z_STREAM_END || data->strm.avail_out < data->outbuf_len) {
+			if (data->strm.avail_out < data->outbuf_len) {
 				php_stream_bucket *out_bucket;
 				size_t bucketlen = data->outbuf_len - data->strm.avail_out;
 				out_bucket = php_stream_bucket_new(stream, estrndup(data->outbuf, bucketlen), bucketlen, 1, 0 TSRMLS_CC);
@@ -108,13 +108,10 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 				data->strm.avail_out = data->outbuf_len;
 				data->strm.next_out = data->outbuf;
 				exit_status = PSFS_PASS_ON;
-				if (status == Z_STREAM_END) {
-					/* no more data to decompress, and nothing was spat out */
-					if (data->strm.avail_out >= data->outbuf_len) {
-						php_stream_bucket_delref(bucket TSRMLS_CC);
-					}
-					return PSFS_PASS_ON;
-				}
+			} else if (status == Z_STREAM_END && data->strm.avail_out >= data->outbuf_len) {
+				/* no more data to decompress, and nothing was spat out */
+				php_stream_bucket_delref(bucket TSRMLS_CC);
+				return PSFS_PASS_ON;
 			}
 		}
 		php_stream_bucket_delref(bucket TSRMLS_CC);
