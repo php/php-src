ab3212d8f5cb87e9cc06535a84e897bb14e35821
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-01-12 21:25:43 +0000

    fix faulty fix for Bug #40189, and provide real fix for the bug
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@250494 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zlib/zlib_filter.c
+++ ext/zlib/zlib_filter.c
@@ -106,12 +106,7 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 			data->strm.avail_in = 0;
 			bin += desired;
 
-			if (!desired) {
-				flags |= PSFS_FLAG_FLUSH_CLOSE;
-				break;
-			}
-
-			if (data->strm.avail_out < data->outbuf_len) {
+			if (status == Z_STREAM_END || data->strm.avail_out < data->outbuf_len) {
 				php_stream_bucket *out_bucket;
 				size_t bucketlen = data->outbuf_len - data->strm.avail_out;
 				out_bucket = php_stream_bucket_new(stream, estrndup(data->outbuf, bucketlen), bucketlen, 1, 0 TSRMLS_CC);
@@ -119,6 +114,13 @@ static php_stream_filter_status_t php_zlib_inflate_filter(
 				data->strm.avail_out = data->outbuf_len;
 				data->strm.next_out = data->outbuf;
 				exit_status = PSFS_PASS_ON;
+				if (status == Z_STREAM_END) {
+					/* no more data to decompress, and nothing was spat out */
+					if (data->strm.avail_out >= data->outbuf_len) {
+						php_stream_bucket_delref(bucket TSRMLS_CC);
+					}
+					return PSFS_PASS_ON;
+				}
 			}
 		}
 		consumed += bucket->buflen;
