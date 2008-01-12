64cb8c81118b962b613afb50c946d0c2b9382477
Greg Beaver <cellog@php.net>
2008-01-12 16:18:26 +0000

    fix conflict on win32 if phar compiled statically

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -1852,7 +1852,7 @@ static int php_check_dots(const char *element, int n)
 
 #define IS_BACKSLASH(c)     ((c) == '/')
 
-#ifdef PHP_WIN32
+#ifdef COMPILE_PHAR_DL
 /* stupid-ass non-extern declaration in tsrm_strtok.h breaks dumbass MS compiler */
 static inline int in_character_class(char ch, const char *delim)
 {
