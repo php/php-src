6be48257773dd2d5f985c91e9fa1ebdabbe754c6
Derick Rethans <derick@php.net>
2008-01-13 15:16:48 +0000

    - MF53: Added new date/time functionality:
      * date_parse_from_format(): Parse date/time strings according to a format.
      * date_create_from_format()/DateTime::createFromFormat(): Create a date/time
        object by parsing a date/time string according to a given format.
      * date_get_last_errors()/DateTime::getLastErrors(): Return a list of warnings
        and errors that were found while parsing a date/time string through:
        - strtotime() / new DateTime
        - date_create_from_format() / DateTime::createFromFormat()
        - date_parse_from_format()

--- ext/date/lib/timelib.h
+++ ext/date/lib/timelib.h
@@ -54,6 +54,7 @@ void timelib_isoweek_from_date(timelib_sll y, timelib_sll m, timelib_sll d, time
 
 /* From parse_date.re */
 timelib_time *timelib_strtotime(char *s, int len, timelib_error_container **errors, const timelib_tzdb *tzdb);
+timelib_time *timelib_parse_from_format(char *format, char *s, int len, timelib_error_container **errors, const timelib_tzdb *tzdb);
 void timelib_fill_holes(timelib_time *parsed, timelib_time *now, int options);
 char *timelib_timezone_id_from_abbr(const char *abbr, long gmtoffset, int isdst);
 const timelib_tz_lookup_table *timelib_timezone_abbreviations_list(void);
