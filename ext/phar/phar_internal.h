a9b6a23421db87a01d90ac9b5085717f8aeb15a7
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-01-19 19:51:14 +0000

    bump 1.3.0 to 2.0.0, this is a big feature addition release
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@250911 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar_internal.h
+++ ext/phar/phar_internal.h
@@ -79,7 +79,7 @@
 #define E_RECOVERABLE_ERROR E_ERROR
 #endif
 
-#define PHAR_EXT_VERSION_STR      "1.3.0"
+#define PHAR_EXT_VERSION_STR      "2.0.0"
 #define PHAR_API_VERSION_STR      "1.1.1"
 /* x.y.z maps to 0xyz0 */
 #define PHAR_API_VERSION          0x1110
