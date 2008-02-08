a3c910739fb504bfa14a1832a761834e43130332
Greg Beaver <cellog@php.net>
2008-02-08 01:08:02 +0000

    fix flipped logic

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -489,7 +489,7 @@ PHP_METHOD(Phar, webPhar)
 			entry_len = 0;
 		}
 		pt = estrndup(testit, (pt - testit) + (fname_len - (basename - fname)));
-		not_cgi = 1;
+		not_cgi = 0;
 	} else {
 		path_info = SG(request_info).request_uri;
 
@@ -503,7 +503,7 @@ PHP_METHOD(Phar, webPhar)
 		entry = estrndup(pt + (fname_len - (basename - fname)), entry_len);
 
 		pt = estrndup(path_info, (pt - path_info) + (fname_len - (basename - fname)));
-		not_cgi = 0;
+		not_cgi = 1;
 	}
 
 	if (rewrite) {
