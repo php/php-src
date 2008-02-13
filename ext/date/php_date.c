cde47fb193e6df2ac4fb9d447cec0675c156b6bb
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-02-13 21:53:01 +0000

    - Add some missing elements from the return value.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@252872 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -2098,7 +2098,7 @@ void php_date_do_return_parsed_time(INTERNAL_FUNCTION_PARAMETERS, timelib_time *
 				break;
 		}
 	}
-	if (parsed_time->have_relative || parsed_time->have_weekday_relative) {
+	if (parsed_time->have_relative || parsed_time->have_weekday_relative || parsed_time->have_special_relative || parsed_time->relative.first_last_day_of) {
 		MAKE_STD_ZVAL(element);
 		array_init(element);
 	}
@@ -2113,7 +2113,13 @@ void php_date_do_return_parsed_time(INTERNAL_FUNCTION_PARAMETERS, timelib_time *
 	if (parsed_time->have_weekday_relative) {
 		add_ascii_assoc_long(element, "weekday", parsed_time->relative.weekday);
 	}
-	if (parsed_time->have_relative || parsed_time->have_weekday_relative) {
+	if (parsed_time->have_special_relative && (parsed_time->special.type == TIMELIB_SPECIAL_WEEKDAY)) {
+		add_ascii_assoc_long(element, "weekdays", parsed_time->special.amount);
+	}
+	if (parsed_time->relative.first_last_day_of) {
+		add_ascii_assoc_bool(element, parsed_time->relative.first_last_day_of == 1 ? "first_day_of_month" : "last_day_of_month", 1);
+	}
+	if (parsed_time->have_relative || parsed_time->have_weekday_relative || parsed_time->have_special_relative || parsed_time->relative.first_last_day_of) {
 		add_ascii_assoc_zval(return_value, "relative", element);
 	}
 	timelib_time_dtor(parsed_time);
