b377ab8a1854447a398b69f118e83e7d0edb9c59
Dmitry Stogov <dmitry@php.net>
2008-02-14 10:25:10 +0000

    Reverted memrchr() patch, because it is broken on linux 64-bit

--- Zend/zend_operators.h
+++ Zend/zend_operators.h
@@ -24,7 +24,6 @@
 
 #include <errno.h>
 #include <math.h>
-#include <string.h>
 
 #ifdef HAVE_IEEEFP_H
 #include <ieeefp.h>
@@ -251,9 +250,6 @@ zend_memnstr(char *haystack, char *needle, int needle_len, char *end)
 	return NULL;
 }
 
-#ifdef HAVE_MEMRCHR
-# define zend_memrchr(s,c,n) memrchr(s,c,n)
-#else
 static inline void *zend_memrchr(const void *s, int c, size_t n)
 {
 	register unsigned char *e;
@@ -270,7 +266,6 @@ static inline void *zend_memrchr(const void *s, int c, size_t n)
 
 	return NULL;
 }
-#endif
 
 BEGIN_EXTERN_C()
 ZEND_API int increment_function(zval *op1);
