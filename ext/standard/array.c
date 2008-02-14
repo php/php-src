7412aae87374f1d2ae5d928753872af0424c64fb
Dmitry Stogov <dmitry@php.net>
2008-02-14 08:46:42 +0000

    Fixed memory leak

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -2092,6 +2092,7 @@ PHP_FUNCTION(array_push)
 		Z_ADDREF_P(new_var);
 
 		if (zend_hash_next_index_insert(Z_ARRVAL_P(stack), &new_var, sizeof(zval *), NULL) == FAILURE) {
+			Z_DELREF_P(new_var);
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Cannot add element to the array as the next element is already occupied");
 			efree(args);
 			RETURN_FALSE;
