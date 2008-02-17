419b7283d336ebcbd40423d7e8105d3bc339c114
Rui Hirokawa <hirokawa@php.net>
2008-02-17 02:04:12 +0000

    MF PHP5_3 (fixed #43994)

--- ext/mbstring/php_mbregex.c
+++ ext/mbstring/php_mbregex.c
@@ -546,6 +546,13 @@ static void _php_mb_regex_ereg_exec(INTERNAL_FUNCTION_PARAMETERS, int icase)
 		convert_to_string_ex(arg_pattern);
 		/* don't bother doing an extended regex with just a number */
 	}
+
+	if (!Z_STRVAL_PP(arg_pattern) || Z_STRLEN_PP(arg_pattern) == 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "empty pattern");
+		RETVAL_FALSE;
+		goto out;
+	}
+
 	re = php_mbregex_compile_pattern(Z_STRVAL_PP(arg_pattern), Z_STRLEN_PP(arg_pattern), options, MBSTRG(current_mbctype), MBSTRG(regex_default_syntax) TSRMLS_CC);
 	if (re == NULL) {
 		RETVAL_FALSE;
