de4d349bee3ba85ba80696d7379417734d14b2b1
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-02-18 14:31:01 +0000

    Made code opcode-cache friendly. (It is disallowed to change code during execution)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@253168 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -2178,7 +2178,6 @@ ZEND_METHOD(reflection_parameter, getDefaultValue)
 	reflection_object *intern;
 	parameter_reference *param;
 	zend_op *precv;
-	zval *zv, zv_copy;
 
 	METHOD_NOTSTATIC_NUMPARAMS(reflection_parameter_ptr, 0);
 	GET_REFLECTION_OBJECT_PTR(param);
@@ -2198,10 +2197,12 @@ ZEND_METHOD(reflection_parameter, getDefaultValue)
 		return;
 	}
 
-	zv_copy = precv->op2.u.constant;
-	zv = &zv_copy;
-	zval_update_constant_ex(&zv, (void*)0, param->fptr->common.scope TSRMLS_CC);
-	RETURN_ZVAL(zv, 1, 1);
+	*return_value = precv->op2.u.constant;
+	INIT_PZVAL(return_value);
+	if (Z_TYPE_P(return_value) != IS_CONSTANT) {
+		zval_copy_ctor(return_value);
+	}
+	zval_update_constant_ex(&return_value, (void*)0, param->fptr->common.scope TSRMLS_CC);
 }
 /* }}} */
 
