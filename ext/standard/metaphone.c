13ba2e23acae27273695ff2792686a1dd6f590ae
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-02-27 01:25:40 +0000

    MFB: Improved fix for #44242
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@253843 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/metaphone.c
+++ ext/standard/metaphone.c
@@ -145,7 +145,7 @@ static char Lookahead(char *word, int how_far) /* {{{ */
  * could be one though; or more too). */
 #define Phonize(c)	{ \
 						if (p_idx >= max_buffer_len) { \
-							*phoned_word = erealloc(*phoned_word, max_buffer_len + 2); \
+							*phoned_word = safe_erealloc(*phoned_word, 2, sizeof(char), max_buffer_len); \
 							max_buffer_len += 2; \
 						} \
 						(*phoned_word)[p_idx++] = c; \
@@ -153,7 +153,7 @@ static char Lookahead(char *word, int how_far) /* {{{ */
 /* Slap a null character on the end of the phoned word */
 #define End_Phoned_Word	{ \
 							if (p_idx == max_buffer_len) { \
-								*phoned_word = erealloc(*phoned_word, max_buffer_len + 1); \
+								*phoned_word = safe_erealloc(*phoned_word, 1, sizeof(char), max_buffer_len); \
 							} \
 							(*phoned_word)[p_idx] = '\0'; \
 						}
