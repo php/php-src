ee6cacf92a28a08da207d6c0fa9451ea63082efe
sfox <sfox@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-02-28 01:16:43 +0000

    Forgot to return... not that I understand this stuff...
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@253900 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -1824,8 +1824,9 @@ PHP_METHOD(Phar, convertToZip)
 // need to check that the string isn't Phar::GZ etc
 	if (ext_len) {
 		if (strncmp(ext, "Phar::GZ", 8) || strncmp(ext, "Phar::BZ2", 9)) {
-		zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC,
-			"Cannot compress a zip-based archive with gz or bz2");
+			zend_throw_exception_ex(spl_ce_UnexpectedValueException, 0 TSRMLS_CC,
+				"Cannot compress a zip-based archive with gz or bz2");
+			return;
 		}
 	}
 
