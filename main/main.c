38b94446a41c6d3e9e7cbf0b241cfdae75de1d13
Pierre Joye <pajoye@php.net>
2008-03-05 20:58:08 +0000

    - #42505, new sendmail default path breaks on Novell (Guenter Knauf)

--- main/main.c
+++ main/main.c
@@ -352,9 +352,8 @@ static PHP_INI_MH(OnChangeMailForceExtra)
 #	define PHP_SAFE_MODE_EXEC_DIR ""
 #endif
 
-#if defined(PHP_PROG_SENDMAIL) && !defined(NETWARE)
-#	define DEFAULT_SENDMAIL_PATH PHP_PROG_SENDMAIL " -t -i "
-#elif defined(PHP_WIN32)
+/* Windows and Netware use the internal mail */
+#if defined(PHP_WIN32) || defined(NETWARE)
 #	define DEFAULT_SENDMAIL_PATH NULL
 #else
 #	define DEFAULT_SENDMAIL_PATH "/usr/sbin/sendmail -t -i" 
