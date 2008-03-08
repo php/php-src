cd9b7889ec4ef8d254b244ba819499809a3aa064
Andrey Hristov <andrey@php.net>
2008-03-08 15:17:45 +0000

    Bug fixed in MySQL 5.0.46

--- ext/mysql/php_mysql.c
+++ ext/mysql/php_mysql.c
@@ -470,10 +470,10 @@ PHP_MSHUTDOWN_FUNCTION(mysql)
 #ifdef PHP_WIN32
 	unsigned long client_ver = mysql_get_client_version();
 	/*
-	  Can't call mysql_server_end() multiple times prior to 5.0.42 on Windows.
+	  Can't call mysql_server_end() multiple times prior to 5.0.46 on Windows.
 	  PHP bug#41350 MySQL bug#25621
 	*/
-	if ((client_ver >= 50042 && client_ver < 50100) || client_ver > 50122) {
+	if ((client_ver >= 50046 && client_ver < 50100) || client_ver > 50122) {
 		mysql_server_end();
 	}
 #else
