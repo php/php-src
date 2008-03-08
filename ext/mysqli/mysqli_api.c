ff8a3104682c47e2ff01fcf115337fb1bb6a7dd7
Andrey Hristov <andrey@php.net>
2008-03-08 14:55:52 +0000

    Fix a problem with $mysqi->connect() not working with default values. Fix a leak, bug#39457

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -1595,7 +1595,7 @@ PHP_FUNCTION(mysqli_prepare)
    Open a connection to a mysql server */ 
 PHP_FUNCTION(mysqli_real_connect)
 {
-	mysqli_common_connect(INTERNAL_FUNCTION_PARAM_PASSTHRU, TRUE);
+	mysqli_common_connect(INTERNAL_FUNCTION_PARAM_PASSTHRU, TRUE, FALSE);
 }
 /* }}} */
 
