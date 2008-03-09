0d2b2d6edfb42245b1465e26d885e77309579ca6
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-03-09 18:10:09 +0000

    MFB: Initialize ts only after successful argument parsing
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@254710 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -1013,11 +1013,11 @@ static void php_date(INTERNAL_FUNCTION_PARAMETERS, int localtime)
 	long    ts;
 	char   *string;
 
-	if (ZEND_NUM_ARGS() == 1) {
-		ts = time(NULL);
-	}
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|l", &format, &format_len, &ts) == FAILURE) {
 		RETURN_FALSE;
+	}
+	if (ZEND_NUM_ARGS() == 1) {
+		ts = time(NULL);
 	}
 
 	string = php_format_date(format, format_len, ts, localtime TSRMLS_CC);
