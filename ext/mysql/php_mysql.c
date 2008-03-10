a8e4046c72d300bde42070dee92a8ca005650ae3
Andrey Hristov <andrey@php.net>
2008-03-10 09:16:13 +0000

    - Fix a warning in php_mysql.c - Fixed #44371 Extension compile failed

--- ext/mysql/php_mysql.c
+++ ext/mysql/php_mysql.c
@@ -663,7 +663,7 @@ static void php_mysql_do_connect(INTERNAL_FUNCTION_PARAMETERS, int persistent)
 			client_flags ^= CLIENT_LOCAL_FILES;
 		}
 
-		hashed_details_length = spprintf(&hashed_details, 0, "mysql_%s_%s_%s_%d", SAFE_STRING(host_and_port), SAFE_STRING(user), SAFE_STRING(passwd), client_flags);
+		hashed_details_length = spprintf(&hashed_details, 0, "mysql_%s_%s_%s_%ld", SAFE_STRING(host_and_port), SAFE_STRING(user), SAFE_STRING(passwd), client_flags);
 	}
 
 	/* We cannot use mysql_port anymore in windows, need to use
