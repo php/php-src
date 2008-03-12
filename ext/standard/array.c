aae9ad3f731e0eabc485781c9efd4587d930261e
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-03-12 19:21:30 +0000

    MFB: Fixed bug #42177 (Warning "array_merge_recursive(): recursion detected" comes again...)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@255001 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -2410,7 +2410,7 @@ ukey:
 				if (recursive && zend_u_hash_find(dest, utype, string_key, string_key_len, (void **)&dest_entry) == SUCCESS) {
 					HashTable *thash = HASH_OF(*dest_entry);
 
-					if ((thash && thash->nApplyCount > 1) || (*src_entry == *dest_entry && (Z_REFCOUNT_PP(dest_entry) % 2))) {
+					if ((thash && thash->nApplyCount > 1) || (*src_entry == *dest_entry && Z_ISREF_PP(dest_entry) && (Z_REFCOUNT_PP(dest_entry) % 2))) {
 						php_error_docref(NULL TSRMLS_CC, E_WARNING, "recursion detected");
 						return 0;
 					}
