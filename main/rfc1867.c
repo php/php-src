c383fa213d55f407135388831abc655ce0a1f962
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-03-14 13:10:22 +0000

    check if return value of write() is -1 and abort upload in this case setting the correct error status
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@255067 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -1263,7 +1263,13 @@ var_done:
 				} else if (blen > 0) {
 					wlen = fwrite(buff, 1, blen, fp);
 
-					if (wlen < blen) {
+					if (wlen == -1) {
+						/* write failed */
+#if DEBUG_FILE_UPLOAD
+						sapi_module.sapi_error(E_NOTICE, "write() failed - %s", strerror(errno));
+#endif
+						cancel_upload = UPLOAD_ERROR_F;
+					} else if (wlen < blen) {
 #if DEBUG_FILE_UPLOAD
 						sapi_module.sapi_error(E_NOTICE, "Only %d bytes were written, expected to write %d", wlen, blen);
 #endif
@@ -1712,7 +1718,13 @@ static SAPI_POST_HANDLER_FUNC(rfc1867_post_handler_legacy)
 				} else if (blen > 0) {
 					wlen = write(fd, buff, blen);
 			
-					if (wlen < blen) {
+					if (wlen == -1) {
+						/* write failed */
+#if DEBUG_FILE_UPLOAD
+						sapi_module.sapi_error(E_NOTICE, "write() failed - %s", strerror(errno));
+#endif
+						cancel_upload = UPLOAD_ERROR_F;
+					} else if (wlen < blen) {
 #if DEBUG_FILE_UPLOAD
 						sapi_module.sapi_error(E_NOTICE, "Only %d bytes were written, expected to write %d", wlen, blen);
 #endif
