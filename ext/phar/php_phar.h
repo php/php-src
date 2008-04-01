90c80e2f24780be16c685b0a18720e0762089e1e
sfox <sfox@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-04-01 17:59:00 +0000

    Bump dev version ready for any new changes
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@256614 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/php_phar.h
+++ ext/phar/php_phar.h
@@ -22,7 +22,7 @@
 #ifndef PHP_PHAR_H
 #define PHP_PHAR_H
 
-#define PHP_PHAR_VERSION      "2.0.0a2"
+#define PHP_PHAR_VERSION      "2.0.0a3-dev"
 
 #include "ext/standard/basic_functions.h"
 extern zend_module_entry phar_module_entry;
