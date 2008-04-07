bf3017fa3db5aea9dbf8400c4af99005dddd5670
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-04-07 17:43:49 +0000

    - Fixing returned offset. - Algorithm optimization.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@256903 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/lib/parse_tz.c
+++ ext/date/lib/parse_tz.c
@@ -370,7 +370,7 @@ timelib_sll timelib_get_current_offset(timelib_time *t)
 	switch (t->zone_type) {
 		case TIMELIB_ZONETYPE_ABBR:
 		case TIMELIB_ZONETYPE_OFFSET:
-			return t->z * 60;
+			return (t->z + t->dst) * -60;
 			
 		case TIMELIB_ZONETYPE_ID:
 			gmt_offset = timelib_get_time_zone_info(t->sse, t->tz_info);
