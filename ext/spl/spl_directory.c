15531140affea828910bc7e5f0456dd045934c84
colder <colder@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-04-09 19:01:12 +0000

    MFH: Fix #44511 (Ensure no exception was already thrown by php_stream_opendir)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@257049 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -215,7 +215,7 @@ static void spl_filesystem_dir_open(spl_filesystem_object* intern, char *path TS
 	}
 	intern->u.dir.index = 0;
 
-	if (intern->u.dir.dirp == NULL) {
+	if (EG(exception) || intern->u.dir.dirp == NULL) {
 		/* throw exception: should've been already happened */
 		intern->u.dir.entry.d_name[0] = '\0';
 	} else {
