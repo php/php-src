653b67b34ff2e41c72aa136d70a4596189501d67
Greg Beaver <cellog@php.net>
2008-04-13 23:47:15 +0000

    minor optimization

--- ext/phar/dirstream.c
+++ ext/phar/dirstream.c
@@ -63,27 +63,27 @@ static int phar_dir_seek(php_stream *stream, off_t offset, int whence, off_t *ne
 {
 	HashTable *data = (HashTable *)stream->abstract;
 
-	if (data)
+	if (!data)
 	{
-		if (whence == SEEK_END) {
-			whence = SEEK_SET;
-			offset = zend_hash_num_elements(data) + offset;
-		}
-		if (whence == SEEK_SET) {
-			zend_hash_internal_pointer_reset(data);
-		}
-
-		if (offset < 0) {
-			return -1;
-		} else {
-			*newoffset = 0;
-			while (*newoffset < offset && zend_hash_move_forward(data) == SUCCESS) {
-				++(*newoffset);
-			}
-			return 0;
-		}
+		return -1;
 	}
-	return -1;
+	if (whence == SEEK_END) {
+		whence = SEEK_SET;
+		offset = zend_hash_num_elements(data) + offset;
+	}
+	if (whence == SEEK_SET) {
+		zend_hash_internal_pointer_reset(data);
+	}
+
+	if (offset < 0) {
+		return -1;
+	} else {
+		*newoffset = 0;
+		while (*newoffset < offset && zend_hash_move_forward(data) == SUCCESS) {
+			++(*newoffset);
+		}
+		return 0;
+	}
 }
 /* }}} */
 
