e8b4ff5029cc227b8ea5187dd37e07ab32593936
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-04-16 17:24:00 +0000

    - MF52: Fixed bug #44703 (htmlspecialchars() does not detect bad character set   argument). #- WIll merge to 5.3 later, that branch has some more modifications.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@257697 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -2750,9 +2750,19 @@ PHP_FUNCTION(timezone_offset_get)
 	dateobj = (php_date_obj *) zend_object_store_get_object(dateobject TSRMLS_CC);
 	DATE_CHECK_INITIALIZED(dateobj->time, DateTime);
 
-	offset = timelib_get_time_zone_info(dateobj->time->sse, tzobj->tzi.tz);
-	RETVAL_LONG(offset->offset);
-	timelib_time_offset_dtor(offset);
+	switch (tzobj->type) {
+		case TIMELIB_ZONETYPE_ID:
+			offset = timelib_get_time_zone_info(dateobj->time->sse, tzobj->tzi.tz);
+			RETVAL_LONG(offset->offset);
+			timelib_time_offset_dtor(offset);
+			break;
+		case TIMELIB_ZONETYPE_OFFSET:
+			RETURN_LONG(tzobj->tzi.utc_offset * -60);
+			break;
+		case TIMELIB_ZONETYPE_ABBR:
+			RETURN_LONG((tzobj->tzi.z.utc_offset - (tzobj->tzi.z.dst*60)) * -60);
+			break;
+	}
 }
 /* }}} */
 
