0f5f61c071dcd23954933d8c75909c1b0f3d8c9f
Greg Beaver <cellog@php.net>
2008-05-07 06:43:07 +0000

    prepare for 2.0.0b1 release, pending review (finally)

--- ext/phar/php_phar.h
+++ ext/phar/php_phar.h
@@ -22,7 +22,7 @@
 #ifndef PHP_PHAR_H
 #define PHP_PHAR_H
 
-#define PHP_PHAR_VERSION      "2.0.0a3-dev"
+#define PHP_PHAR_VERSION      "2.0.0b1"
 
 #include "ext/standard/basic_functions.h"
 extern zend_module_entry phar_module_entry;
