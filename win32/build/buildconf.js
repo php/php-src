58160b95dc14d92c8f7b93506902fe4bb5dac614
mattwil <mattwil@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-05-14 11:32:28 +0000

    MFB: The "Steph special" :-) to allow plain 'configure' to be used
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@259745 c90b9560-bf6c-de11-be94-00142212c4b1

--- win32/build/buildconf.js
+++ win32/build/buildconf.js
@@ -16,12 +16,13 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: buildconf.js,v 1.16 2007-01-29 04:40:46 iliaa Exp $ */
+/* $Id: buildconf.js,v 1.17 2008-05-14 11:32:28 mattwil Exp $ */
 // This generates a configure script for win32 build
 
 WScript.StdOut.WriteLine("Rebuilding configure.js");
 var FSO = WScript.CreateObject("Scripting.FileSystemObject");
 var C = FSO.CreateTextFile("configure.js", true);
+var B = FSO.CreateTextFile("configure.bat", true);
 
 var modules = "";
 var MODULES = WScript.CreateObject("Scripting.Dictionary");
@@ -259,5 +260,6 @@ C.Write(modules);
 C.WriteBlankLines(1);
 C.Write(file_get_contents("win32/build/configure.tail"));
 
-WScript.StdOut.WriteLine("Now run 'cscript /nologo configure.js --help'");
-
+B.WriteLine("@echo off");
+B.WriteLine("cscript /nologo configure.js %*");
+WScript.StdOut.WriteLine("Now run 'configure --help'");
