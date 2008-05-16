04a87d9f630fa78a39092fa7d836c28b393e3e2a
Felipe Pena <felipe@php.net>
2008-05-16 12:44:11 +0000

    - Changed 'Z' to 's' (parameter parsing), the code didn't check if the parameter is a string.

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -1717,24 +1717,26 @@ PHP_NAMED_FUNCTION(php_if_fstat)
    Copy a file */
 PHP_FUNCTION(copy)
 {
-	zval **source, **target, *zcontext = NULL;
+	char *source, *target;
+	int source_len, target_len;
+	zval *zcontext = NULL;
 	php_stream_context *context;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ZZ|r", &source, &target, &zcontext) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss|r", &source, &source_len, &target, &target_len, &zcontext) == FAILURE) {
 		return;
 	}
 
-	if (PG(safe_mode) &&(!php_checkuid(Z_STRVAL_PP(source), NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
+	if (PG(safe_mode) &&(!php_checkuid(source, NULL, CHECKUID_CHECK_FILE_AND_DIR))) {
 		RETURN_FALSE;
 	}
 
-	if (php_check_open_basedir(Z_STRVAL_PP(source) TSRMLS_CC)) {
+	if (php_check_open_basedir(source TSRMLS_CC)) {
 		RETURN_FALSE;
 	}
 
 	context = php_stream_context_from_zval(zcontext, 0);
 	
-	if (php_copy_file(Z_STRVAL_PP(source), Z_STRVAL_PP(target) TSRMLS_CC) == SUCCESS) {
+	if (php_copy_file(source, target TSRMLS_CC) == SUCCESS) {
 		RETURN_TRUE;
 	} else {
 		RETURN_FALSE;
