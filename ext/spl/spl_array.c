6a2ab0f8433015a9e7c0dc938a8db8f445ce310f
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-05-20 12:04:37 +0000

    nuke unused variables
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@260038 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -1443,8 +1443,7 @@ SPL_METHOD(Array, serialize)
 	zval *object = getThis();
 	spl_array_object *intern = (spl_array_object*)zend_object_store_get_object(object TSRMLS_CC);
 	HashTable *aht = spl_array_get_hash_table(intern, 0 TSRMLS_CC);
-	zval **entry, members, *pmembers;
-	HashPosition  pos;
+	zval members, *pmembers;
 	php_serialize_data_t var_hash;
 	smart_str buf = {0};
 
@@ -1494,7 +1493,7 @@ SPL_METHOD(Array, unserialize)
 	int buf_len;
 	const unsigned char *p, *s;
 	php_unserialize_data_t var_hash;
-	zval *pentry, *pmembers, *pflags = NULL;
+	zval *pmembers, *pflags = NULL;
 	long flags;
 	
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &buf, &buf_len) == FAILURE) {
