1db4d44c7b6bbc61805bd8953223039599de4f1f
Antony Dovgal <tony2001@php.net>
2008-05-20 12:07:13 +0000

    fix function signature and silence compile warning

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -1788,7 +1788,7 @@ static void date_period_it_current_data(zend_object_iterator *iter, zval ***data
 
 
 /* {{{ date_period_it_current_key */
-static int date_period_it_current_key(zend_object_iterator *iter, char **str_key, uint *str_key_len, ulong *int_key TSRMLS_DC)
+static int date_period_it_current_key(zend_object_iterator *iter, zstr *str_key, uint *str_key_len, ulong *int_key TSRMLS_DC)
 {
 	date_period_it   *iterator = (date_period_it *)iter;
 	*int_key = iterator->current_index;
