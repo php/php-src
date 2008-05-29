da431843f0fd8ca99a40cbef6f513f241ef1205b
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-05-29 17:34:50 +0000

    fix PECL bug #13981: Third-party zips fail entry interrogation
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@260443 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -1980,15 +1980,16 @@ int phar_postprocess_file(php_stream_wrapper *wrapper, int options, phar_entry_d
 			spprintf(error, 0, "phar error: internal corruption of zip-based phar \"%s\" (cannot read local file header for file \"%s\")", idata->phar->fname, entry->filename);
 			return FAILURE;
 		}
-		/* fix up for big-endian systems */
-		/* verify local header if not yet verified */
+		/* verify local header */
 		if (entry->filename_len != PHAR_ZIP_16(local.filename_len) || entry->crc32 != PHAR_ZIP_32(local.crc32) || entry->uncompressed_filesize != PHAR_ZIP_32(local.uncompsize) || entry->compressed_filesize != PHAR_ZIP_32(local.compsize)) {
 			spprintf(error, 0, "phar error: internal corruption of zip-based phar \"%s\" (local head of file \"%s\" does not match central directory)", idata->phar->fname, entry->filename);
 			return FAILURE;
 		}
-		if (-1 == php_stream_seek(idata->phar->fp, PHAR_ZIP_16(local.filename_len) + PHAR_ZIP_16(local.extra_len), SEEK_CUR)) {
-			spprintf(error, 0, "phar error: internal corruption of zip-based phar \"%s\" (cannot seek to start of file data for file \"%s\")", idata->phar->fname, entry->filename);
-			return FAILURE;
+		/* construct actual offset to file start - local extra_len can be different from central extra_len */
+		entry->offset = entry->offset_abs =
+			sizeof(local) + entry->header_offset + PHAR_ZIP_16(local.filename_len) + PHAR_ZIP_16(local.extra_len);
+		if (idata->zero != entry->offset_abs) {
+			idata->zero = entry->offset_abs;
 		}
 	}
 	php_stream_seek(fp, idata->zero, SEEK_SET);	
