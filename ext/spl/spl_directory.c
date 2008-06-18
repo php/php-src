9431f5e59ba4c3d9320dd86f6d121ff6e2562704
colder <colder@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-06-18 10:05:15 +0000

    Fix #45216 (Add a default size for fgetss)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@261316 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -2427,7 +2427,12 @@ SPL_METHOD(SplFileObject, fgetss)
 	spl_filesystem_object *intern = (spl_filesystem_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
 	zval *arg2 = NULL;
 	MAKE_STD_ZVAL(arg2);
-	ZVAL_LONG(arg2, intern->u.file.max_line_len);
+
+	if (intern->u.file.max_line_len > 0) {
+		ZVAL_LONG(arg2, intern->u.file.max_line_len);
+	} else {
+		ZVAL_LONG(arg2, 1024);
+	}
 
 	spl_filesystem_file_free_line(intern TSRMLS_CC);
 	intern->u.file.current_line_num++;
