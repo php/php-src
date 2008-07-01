3c0972a4f4c8cae6d0f96b3a91e7e378e9f9527e
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-01 10:01:25 +0000

    Use new parameter parsing API
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@261838 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/formatted_print.c
+++ ext/standard/formatted_print.c
@@ -669,16 +669,13 @@ static char * php_formatted_print(int ht, int *len, int use_array, int format_of
 	char *format, *result, padding;
 	int always_sign;
 
-	argc = ZEND_NUM_ARGS();
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "+", &args, &argc) == FAILURE) {
+		return NULL;
+	}
 
 	/* verify the number of args */
 	if ((use_array && argc != (2 + format_offset)) 
 			|| (!use_array && argc < (1 + format_offset))) {
-		WRONG_PARAM_COUNT_WITH_RETVAL(NULL);
-	}
-	args = (zval ***)safe_emalloc(argc, sizeof(zval *), 0);
-
-	if (zend_get_parameters_array_ex(argc, args) == FAILURE) {
 		efree(args);
 		WRONG_PARAM_COUNT_WITH_RETVAL(NULL);
 	}
