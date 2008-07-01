469d3db830b0f17b3d559791d5d84e6e2515d620
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-01 15:50:09 +0000

    - MFB: Added arginfo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@261860 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sysvshm/sysvshm.c
+++ ext/sysvshm/sysvshm.c
@@ -39,16 +39,60 @@
 #include "ext/standard/php_smart_str.h"
 #include "php_ini.h"
 
+/* {{{ arginfo */
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_attach, 0, 0, 1)
+	ZEND_ARG_INFO(0, key)
+	ZEND_ARG_INFO(0, memsize)
+	ZEND_ARG_INFO(0, perm)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_detach, 0, 0, 1)
+	ZEND_ARG_INFO(0, shm_identifier)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_remove, 0, 0, 1)
+	ZEND_ARG_INFO(0, shm_identifier)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_put_var, 0, 0, 3)
+	ZEND_ARG_INFO(0, shm_identifier)
+	ZEND_ARG_INFO(0, variable_key)
+	ZEND_ARG_INFO(0, variable)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_get_var, 0, 0, 2)
+	ZEND_ARG_INFO(0, id)
+	ZEND_ARG_INFO(0, variable_key)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_has_var, 0, 0, 2)
+	ZEND_ARG_INFO(0, id)
+	ZEND_ARG_INFO(0, variable_key)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_shm_remove_var, 0, 0, 2)
+	ZEND_ARG_INFO(0, id)
+	ZEND_ARG_INFO(0, variable_key)
+ZEND_END_ARG_INFO()
+/* }}} */
+
 /* {{{ sysvshm_functions[]
  */
 const zend_function_entry sysvshm_functions[] = {
-	PHP_FE(shm_attach, NULL)
-	PHP_FE(shm_remove, NULL)
-	PHP_FE(shm_detach, NULL)
-	PHP_FE(shm_put_var, NULL)
-	PHP_FE(shm_has_var, NULL)
-	PHP_FE(shm_get_var, NULL)
-	PHP_FE(shm_remove_var, NULL)
+	PHP_FE(shm_attach,		arginfo_shm_attach)
+	PHP_FE(shm_remove,		arginfo_shm_detach)
+	PHP_FE(shm_detach, 		arginfo_shm_remove)
+	PHP_FE(shm_put_var,		arginfo_shm_put_var)
+	PHP_FE(shm_has_var, 	arginfo_shm_has_var)
+	PHP_FE(shm_get_var,		arginfo_shm_get_var)
+	PHP_FE(shm_remove_var,	arginfo_shm_remove_var)
 	{NULL, NULL, NULL}	
 };
 /* }}} */
