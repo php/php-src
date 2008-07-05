6817f3fae4233af5f77fb5557081a0347b4e3756
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-05 18:17:02 +0000

    - silent redefinition warning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@262072 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/xmlrpc/libxmlrpc/xmlrpc_win32.h
+++ ext/xmlrpc/libxmlrpc/xmlrpc_win32.h
@@ -3,9 +3,14 @@
 /* just some things needed to compile win32 */
 #include <windows.h>
 #include <stdlib.h>
-#define inline __inline
-#define snprintf _snprintf
-#define strcasecmp(s1, s2) stricmp(s1, s2)
-
+#ifndef inline
+# define inline __inline
+#endif
+#ifndef snprintf
+# define snprintf _snprintf
+#endif
+#ifndef strcasecmp
+# define strcasecmp(s1, s2) stricmp(s1, s2)
+#endif
 
 #endif
