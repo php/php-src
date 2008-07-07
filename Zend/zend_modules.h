30d2360b65e59788155ac95fee991419ee7e64a4
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-07 14:45:09 +0000

    MFB: - Removed zend_arg_defs.c   The first_arg_force_ref, second_arg_force_ref, third_arg_force_ref,    fourth_arg_force_ref, fifth_arg_force_ref and all_args_by_ref are not more used.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@262196 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_modules.h
+++ Zend/zend_modules.h
@@ -32,12 +32,6 @@
 #define ZEND_MODULE_INFO_FUNC_ARGS zend_module_entry *zend_module TSRMLS_DC
 #define ZEND_MODULE_INFO_FUNC_ARGS_PASSTHRU zend_module TSRMLS_CC
 
-extern const struct _zend_arg_info first_arg_force_ref[2];
-extern const struct _zend_arg_info second_arg_force_ref[3];
-extern const struct _zend_arg_info third_arg_force_ref[4];
-extern const struct _zend_arg_info fourth_arg_force_ref[5];
-extern const struct _zend_arg_info fifth_arg_force_ref[6];
-extern const struct _zend_arg_info all_args_by_ref[1];
 
 #define ZEND_MODULE_API_NO 20070729
 #ifdef ZTS
