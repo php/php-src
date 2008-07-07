f2655e0e5db87520c899c22f3a53c7a5593646f9
Felipe Pena <felipe@php.net>
2008-07-07 13:52:44 +0000

    MFB: - Added arginfo - Removed unnecessary #include "zend_arg_defs.c"

--- ext/mbstring/php_mbregex.h
+++ ext/mbstring/php_mbregex.h
@@ -43,35 +43,35 @@
 
 /* {{{ PHP_MBREGEX_FUNCTION_ENTRIES */
 #define PHP_MBREGEX_FUNCTION_ENTRIES \
-	PHP_FE(mb_regex_encoding,	NULL) \
-	PHP_FE(mb_regex_set_options,	NULL) \
-	PHP_FE(mb_ereg,			third_arg_force_ref) \
-	PHP_FE(mb_eregi,			third_arg_force_ref) \
-	PHP_FE(mb_ereg_replace,			NULL) \
-	PHP_FE(mb_eregi_replace,			NULL) \
-	PHP_FE(mb_split,					NULL) \
-	PHP_FE(mb_ereg_match,			NULL) \
-	PHP_FE(mb_ereg_search,			NULL) \
-	PHP_FE(mb_ereg_search_pos,		NULL) \
-	PHP_FE(mb_ereg_search_regs,		NULL) \
-	PHP_FE(mb_ereg_search_init,		NULL) \
-	PHP_FE(mb_ereg_search_getregs,	NULL) \
-	PHP_FE(mb_ereg_search_getpos,	NULL) \
-	PHP_FE(mb_ereg_search_setpos,	NULL) \
-	PHP_FALIAS(mbregex_encoding,	mb_regex_encoding,	NULL) \
-	PHP_FALIAS(mbereg,	mb_ereg,	NULL) \
-	PHP_FALIAS(mberegi,	mb_eregi,	NULL) \
-	PHP_FALIAS(mbereg_replace,	mb_ereg_replace,	NULL) \
-	PHP_FALIAS(mberegi_replace,	mb_eregi_replace,	NULL) \
-	PHP_FALIAS(mbsplit,	mb_split,	NULL) \
-	PHP_FALIAS(mbereg_match,	mb_ereg_match,	NULL) \
-	PHP_FALIAS(mbereg_search,	mb_ereg_search,	NULL) \
-	PHP_FALIAS(mbereg_search_pos,	mb_ereg_search_pos,	NULL) \
-	PHP_FALIAS(mbereg_search_regs,	mb_ereg_search_regs,	NULL) \
-	PHP_FALIAS(mbereg_search_init,	mb_ereg_search_init,	NULL) \
-	PHP_FALIAS(mbereg_search_getregs,	mb_ereg_search_getregs,	NULL) \
-	PHP_FALIAS(mbereg_search_getpos,	mb_ereg_search_getpos,	NULL) \
-	PHP_FALIAS(mbereg_search_setpos,	mb_ereg_search_setpos,	NULL)
+	PHP_FE(mb_regex_encoding,	arginfo_mb_regex_encoding) \
+	PHP_FE(mb_regex_set_options,	arginfo_mb_regex_set_options) \
+	PHP_FE(mb_ereg,			arginfo_mb_ereg) \
+	PHP_FE(mb_eregi,			arginfo_mb_eregi) \
+	PHP_FE(mb_ereg_replace,			arginfo_mb_ereg_replace) \
+	PHP_FE(mb_eregi_replace,			arginfo_mb_eregi_replace) \
+	PHP_FE(mb_split,					arginfo_mb_split) \
+	PHP_FE(mb_ereg_match,			arginfo_mb_ereg_match) \
+	PHP_FE(mb_ereg_search,			arginfo_mb_ereg_search) \
+	PHP_FE(mb_ereg_search_pos,		arginfo_mb_ereg_search_pos) \
+	PHP_FE(mb_ereg_search_regs,		arginfo_mb_ereg_search_regs) \
+	PHP_FE(mb_ereg_search_init,		arginfo_mb_ereg_search_init) \
+	PHP_FE(mb_ereg_search_getregs,	arginfo_mb_ereg_search_getregs) \
+	PHP_FE(mb_ereg_search_getpos,	arginfo_mb_ereg_search_getpos) \
+	PHP_FE(mb_ereg_search_setpos,	arginfo_mb_ereg_search_setpos) \
+	PHP_FALIAS(mbregex_encoding,	mb_regex_encoding,	arginfo_mb_regex_encoding) \
+	PHP_FALIAS(mbereg,	mb_ereg,	arginfo_mb_ereg) \
+	PHP_FALIAS(mberegi,	mb_eregi,	arginfo_mb_eregi) \
+	PHP_FALIAS(mbereg_replace,	mb_ereg_replace,	arginfo_mb_ereg_replace) \
+	PHP_FALIAS(mberegi_replace,	mb_eregi_replace,	arginfo_mb_eregi_replace) \
+	PHP_FALIAS(mbsplit,	mb_split,	arginfo_mb_split) \
+	PHP_FALIAS(mbereg_match,	mb_ereg_match,	arginfo_mb_ereg_match) \
+	PHP_FALIAS(mbereg_search,	mb_ereg_search,	arginfo_mb_ereg_search) \
+	PHP_FALIAS(mbereg_search_pos,	mb_ereg_search_pos,	arginfo_mb_ereg_search_pos) \
+	PHP_FALIAS(mbereg_search_regs,	mb_ereg_search_regs,	arginfo_mb_ereg_search_regs) \
+	PHP_FALIAS(mbereg_search_init,	mb_ereg_search_init,	arginfo_mb_ereg_search_init) \
+	PHP_FALIAS(mbereg_search_getregs,	mb_ereg_search_getregs,	arginfo_mb_ereg_search_getregs) \
+	PHP_FALIAS(mbereg_search_getpos,	mb_ereg_search_getpos,	arginfo_mb_ereg_search_getpos) \
+	PHP_FALIAS(mbereg_search_setpos,	mb_ereg_search_setpos,	arginfo_mb_ereg_search_setpos)
 /* }}} */
 
 typedef struct _zend_mbstring_globals * zend_mbstring_globals_ptr;
