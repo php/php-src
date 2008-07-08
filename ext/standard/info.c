8eeaf9df95879f4499b3b133daeed42b75de9fd1
Antony Dovgal <tony2001@php.net>
2008-07-08 08:37:56 +0000

    capitalize multibyte support

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -515,9 +515,9 @@ PHPAPI void php_print_info(int flag TSRMLS_DC)
 		php_info_print_table_row(2, "Zend Memory Manager", is_zend_mm(TSRMLS_C) ? "enabled" : "disabled" );
 
 #ifdef ZEND_MULTIBYTE
-		php_info_print_table_row(2, "Zend multibyte support", "enabled");
+		php_info_print_table_row(2, "Zend Multibyte Support", "enabled");
 #else
-		php_info_print_table_row(2, "Zend multibyte support", "disabled");
+		php_info_print_table_row(2, "Zend Multibyte Support", "disabled");
 #endif
 
 #if HAVE_IPV6
