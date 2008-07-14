84625f5e7e02b935b68bcb180839fef6c80df491
Dmitry Stogov <dmitry@php.net>
2008-07-14 12:17:16 +0000

    Fixed is_callable() to support closures and return appropriate function name

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -1482,7 +1482,7 @@ void zend_do_begin_lambda_function_declaration(znode *result, znode *function_to
 	zend_op       *current_op;
 
 	function_name.op_type = IS_CONST;
-	ZVAL_ASCII_STRING(&function_name.u.constant, "lambda", ZSTR_DUPLICATE);
+	ZVAL_ASCII_STRING(&function_name.u.constant, "", ZSTR_DUPLICATE);
 
 	zend_do_begin_function_declaration(function_token, &function_name, 0, return_reference, NULL TSRMLS_CC);
 
