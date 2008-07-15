64970f48fe63550bf95887d3e86dd1694fe72a3d
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-15 17:38:27 +0000

    - MFH: Also add the warnings for times. This does not make sense for the   english text parser, as invalid times are never allowed in there.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@262767 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/date/lib/timelib.h
+++ ext/date/lib/timelib.h
@@ -56,6 +56,7 @@ timelib_sll timelib_day_of_year(timelib_sll y, timelib_sll m, timelib_sll d);
 timelib_sll timelib_daynr_from_weeknr(timelib_sll y, timelib_sll w, timelib_sll d);
 timelib_sll timelib_days_in_month(timelib_sll y, timelib_sll m);
 void timelib_isoweek_from_date(timelib_sll y, timelib_sll m, timelib_sll d, timelib_sll *iw, timelib_sll *iy);
+int timelib_valid_time(timelib_sll h, timelib_sll i, timelib_sll s);
 int timelib_valid_date(timelib_sll y, timelib_sll m, timelib_sll d);
 
 /* From parse_date.re */
