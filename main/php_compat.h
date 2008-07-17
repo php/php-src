0b858d15e3675be8b4e7a80018ff618db4c767e3
Nuno Lopes <nlopess@php.net>
2008-07-17 14:35:33 +0000

    update PCRE symbols

--- main/php_compat.h
+++ main/php_compat.h
@@ -56,6 +56,7 @@
 #define _pcre_xclass	 		php__pcre_xclass
 #define pcre_callout			php_pcre_callout
 #define _pcre_OP_lengths		php__pcre_OP_lengths
+#define _pcre_utt_names			php__pcre_utt_names
 #define _pcre_default_tables		php__pcre_default_tables
 #define pcre_get_stringtable_entries	php_pcre_get_stringtable_entries
 #define _pcre_is_newline		php__pcre_is_newline
