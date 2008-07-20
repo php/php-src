6931789f31dcc8169485320f2e1711cbe8bb3c35
Steph Fox <sfox@php.net>
2008-07-20 02:20:31 +0000

    - Happy New Year! (better late than never, eh?)

--- win32/build/cvsclean.js
+++ win32/build/cvsclean.js
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2007 The PHP Group                                |
+  | Copyright (c) 1997-2008 The PHP Group                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
@@ -16,7 +16,7 @@
   +----------------------------------------------------------------------+
 */
 
-/* $Id: cvsclean.js,v 1.5.2.1.2.1 2007-01-01 19:32:09 iliaa Exp $ */
+/* $Id: cvsclean.js,v 1.5.2.1.2.1.2.1 2008-07-20 02:20:31 sfox Exp $ */
 // Cleans up files that do not belong in CVS
 
 var FSO = WScript.CreateObject("Scripting.FileSystemObject");
