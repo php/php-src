d986dd8bc14353c4b31ee603211317c8534c40e0
Felipe Pena <felipe@php.net>
2008-07-20 14:29:30 +0000

    - MFB: Added arginfo

--- sapi/apache2filter/php_functions.c
+++ sapi/apache2filter/php_functions.c
@@ -359,17 +359,64 @@ PHP_MINFO_FUNCTION(apache)
 	php_info_print_table_end();
 }
 
+/* {{{ arginfo */
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_apache2filter_lookup_uri, 0, 0, 1)
+	ZEND_ARG_INFO(0, filename)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_apache2filter_virtual, 0, 0, 1)
+	ZEND_ARG_INFO(0, uri)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO(arginfo_apache2filter_getallheaders, 0)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO(arginfo_apache2filter_response_headers, 0)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_apache2filter_note, 0, 0, 1)
+	ZEND_ARG_INFO(0, note_name)
+	ZEND_ARG_INFO(0, note_value)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_apache2filter_setenv, 0, 0, 2)
+	ZEND_ARG_INFO(0, variable)
+	ZEND_ARG_INFO(0, value)
+	ZEND_ARG_INFO(0, walk_to_top)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO_EX(arginfo_apache2filter_getenv, 0, 0, 1)
+	ZEND_ARG_INFO(0, variable)
+	ZEND_ARG_INFO(0, walk_to_top)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO(arginfo_apache2filter_get_version, 0)
+ZEND_END_ARG_INFO()
+
+static
+ZEND_BEGIN_ARG_INFO(arginfo_apache2filter_get_modules, 0)
+ZEND_END_ARG_INFO()
+/* }}} */
+
 static const zend_function_entry apache_functions[] = {
-	PHP_FE(apache_lookup_uri, NULL)
-	PHP_FE(virtual, NULL)
-	PHP_FE(apache_request_headers, NULL)
-	PHP_FE(apache_response_headers, NULL)
-	PHP_FE(apache_setenv, NULL)
-	PHP_FE(apache_getenv, NULL)
-	PHP_FE(apache_note, NULL)
-	PHP_FE(apache_get_version, NULL)
-	PHP_FE(apache_get_modules, NULL)
-	PHP_FALIAS(getallheaders, apache_request_headers, NULL)
+	PHP_FE(apache_lookup_uri, 		arginfo_apache2filter_lookup_uri)
+	PHP_FE(virtual, 				arginfo_apache2filter_virtual)
+	PHP_FE(apache_request_headers, 	arginfo_apache2filter_getallheaders)
+	PHP_FE(apache_response_headers, arginfo_apache2filter_response_headers)
+	PHP_FE(apache_setenv, 			arginfo_apache2filter_setenv)
+	PHP_FE(apache_getenv, 			arginfo_apache2filter_getenv)
+	PHP_FE(apache_note, 			arginfo_apache2filter_note)
+	PHP_FE(apache_get_version, 		arginfo_apache2filter_get_version)
+	PHP_FE(apache_get_modules, 		arginfo_apache2filter_get_modules)
+	PHP_FALIAS(getallheaders, 		apache_request_headers, arginfo_apache2filter_getallheaders)
 	{NULL, NULL, NULL}
 };
 
