18b1b59aa62366c15a07e80152d583490bd04046
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-21 19:32:21 +0000

    - Fixed securities issue detailed in CVE-2008-2665 and CVE-2008-2666.   (patch by Christian Hoffmann)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@263123 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/safe_mode.c
+++ main/safe_mode.c
@@ -73,14 +73,6 @@ PHPAPI int php_checkuid_ex(const char *filename, const char *fopen_mode, int mod
 			mode = CHECKUID_CHECK_FILE_AND_DIR;
 		}
 	}
-
-	/* 
-	 * If given filepath is a URL, allow - safe mode stuff
-	 * related to URL's is checked in individual functions
-	 */
-	wrapper = php_stream_locate_url_wrapper(filename, NULL, STREAM_LOCATE_WRAPPERS_ONLY TSRMLS_CC);
-	if (wrapper != NULL)
-		return 1;
 		
 	/* First we see if the file is owned by the same user...
 	 * If that fails, passthrough and check directory...
