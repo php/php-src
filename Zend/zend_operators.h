5d2ea80b60885b7bc062634ce6b99a4fb002c4d0
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-21 18:43:46 +0000

    Optimisation for zend_memnstr when the needle is only a single character. (Patch by Michal Dziemianko - GSoC08)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@263120 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_operators.h
+++ Zend/zend_operators.h
@@ -231,6 +231,10 @@ zend_memnstr(char *haystack, char *needle, int needle_len, char *end)
 	char *p = haystack;
 	char ne = needle[needle_len-1];
 
+	if (needle_len == 1) {
+		return (char *)memchr(p, *needle, (end-p));
+	}
+
 	end -= needle_len;
 
 	while (p <= end) {
