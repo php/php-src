ca536ea9445a611210f1680b6fc74d755803d6a3
Felipe Pena <felipe@php.net>
2008-07-23 16:27:16 +0000

    - Fixed bug #45605 (array_uintersect_assoc gives incorrect error message)   (patch by ilewis at uk dot ibm dot com)

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -3061,8 +3061,12 @@ static void php_array_intersect_key(INTERNAL_FUNCTION_PARAMETERS, int data_compa
 	/* Get the argument count */
 	argc = ZEND_NUM_ARGS();
 	if (data_compare_type == INTERSECT_COMP_DATA_USER) {
-		if (argc < 3 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "+f", &args, &argc, &BG(user_compare_fci), &BG(user_compare_fci_cache)) == FAILURE) {
+		if (argc < 3) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "at least 3 parameters are required, %d given", ZEND_NUM_ARGS());
+			return;
+		}
+		
+		if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "+f", &args, &argc, &BG(user_compare_fci), &BG(user_compare_fci_cache)) == FAILURE) {
 			return;
 		}
 		intersect_data_compare_func = zval_user_compare;
