2d3a5620e48b85df7e58c49f98ed13da1c591342
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-07-26 19:14:38 +0000

    *** empty log message ***
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@263609 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -734,7 +734,9 @@ int zend_call_function(zend_fcall_info *fci, zend_fcall_info_cache *fci_cache TS
 				zend_error(E_WARNING, "Invalid callback %s, %s", callable_name, error);
 				efree(error);
 			}
-			efree(callable_name);
+			if (callable_name) {
+				efree(callable_name);
+			}
 			return FAILURE;
 		} else if (error) {
 			/* Capitalize the first latter of the error message */
@@ -744,9 +746,7 @@ int zend_call_function(zend_fcall_info *fci, zend_fcall_info_cache *fci_cache TS
 			zend_error(E_STRICT, "%s", error);
 			efree(error);
 		}
-		if (callable_name) {
-			efree(callable_name);
-		}
+		efree(callable_name);
 	}
 
 	EX(function_state).function = fci_cache->function_handler;
