b6cca302116e9c3e1f8ef5d77699c05b94226de7
Dmitry Stogov <dmitry@php.net>
2008-07-29 14:28:11 +0000

    Fixed memory leak

--- Zend/zend_exceptions.c
+++ Zend/zend_exceptions.c
@@ -52,6 +52,7 @@ void zend_exception_set_previous(zval *add_previous TSRMLS_DC)
 		previous = zend_read_property(default_exception_ce, exception, "previous", sizeof("previous")-1, 1 TSRMLS_CC);
 		if (Z_TYPE_P(previous) == IS_NULL) {
 			zend_update_property(default_exception_ce, exception, "previous", sizeof("previous")-1, add_previous TSRMLS_CC);
+			Z_DELREF_P(add_previous);
 			return;
 		}
 		exception = previous;
