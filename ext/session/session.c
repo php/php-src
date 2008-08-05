ba2d7e5b567615e867187d99232605ec26c5b589
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-08-05 22:52:35 +0000

    - Nuke ending period
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@264260 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/session/session.c
+++ ext/session/session.c
@@ -1527,7 +1527,7 @@ static PHP_FUNCTION(session_save_path)
 
 	if (name) {
 		if (memchr(name, '\0', name_len) != NULL) {
-			php_error_docref(NULL TSRMLS_CC, E_WARNING, "The save_path cannot contain NULL characters.");
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "The save_path cannot contain NULL characters");
 
 			zval_dtor(return_value);
 			RETURN_FALSE;
