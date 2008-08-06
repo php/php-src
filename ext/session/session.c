b6c67597995000d5c7d9b84e30aa1ca3d5f80750
Kalle Sommer Nielsen <kalle@php.net>
2008-08-06 21:28:38 +0000

    Backport fix for #45406 - Patch by oleg dot grenrus at dynamoid dot com

--- ext/session/session.c
+++ ext/session/session.c
@@ -1925,6 +1925,21 @@ PHP_RINIT_FUNCTION(session)
 		}
 	}
 
+	if (PS(serializer) == NULL) {
+		char *value;
+
+		value = zend_ini_string("session.serialize_handler", sizeof("session.serialize_handler"), 0);
+		if (value) {
+			PS(serializer) = _php_find_ps_serializer(value TSRMLS_CC);
+		}
+	}
+
+	if (PS(mod) == NULL || PS(serializer) == NULL) {
+		/* current status is unusable */
+		PS(session_status) = php_session_disabled;
+		return SUCCESS;
+	}
+
 	if (PS(auto_start)) {
 		php_session_start(TSRMLS_C);
 	}
