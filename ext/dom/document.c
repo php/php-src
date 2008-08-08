c77ca4c34aae8122cc473e618e737d316a008374
Etienne Kneuss <colder@php.net>
2008-08-08 22:07:07 +0000

    MFH: Fix error_handling usage in various extensions

--- ext/dom/document.c
+++ ext/dom/document.c
@@ -1501,14 +1501,14 @@ PHP_METHOD(domdocument, __construct)
 	dom_object *intern;
 	char *encoding, *version = NULL;
 	int encoding_len = 0, version_len = 0, refcount;
+	zend_error_handling error_handling;
 
-	php_set_error_handling(EH_THROW, dom_domexception_class_entry TSRMLS_CC);
+	zend_replace_error_handling(EH_THROW, dom_domexception_class_entry, &error_handling TSRMLS_CC);
 	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "O|ss", &id, dom_document_class_entry, &version, &version_len, &encoding, &encoding_len) == FAILURE) {
-		php_std_error_handling();
 		return;
 	}
 
-	php_std_error_handling();
+	zend_restore_error_handling(&error_handling TSRMLS_CC);
 	docp = xmlNewDoc(version);
 
 	if (!docp) {
