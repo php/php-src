504557b5a5e044d842aff67db300935ffacaa225
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-08-15 07:30:50 +0000

    Fixed double call to MSHUTDOWN in case of `php-cgi -m`
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@264919 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -247,6 +247,7 @@ static void print_extensions(TSRMLS_D)
 	zend_llist sorted_exts;
 
 	zend_llist_copy(&sorted_exts, &zend_extensions);
+	sorted_exts.dtor = NULL;
 	zend_llist_sort(&sorted_exts, extension_name_cmp TSRMLS_CC);
 	zend_llist_apply_with_argument(&sorted_exts, (llist_apply_with_arg_func_t) print_extension_info, NULL TSRMLS_CC);
 	zend_llist_destroy(&sorted_exts);
