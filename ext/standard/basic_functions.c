85309f686af9d406e78933530b9d487de8a201fb
Pierre Joye <pajoye@php.net>
2008-08-18 07:50:10 +0000

    - MFH: this change was not part of the env patch

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -63,11 +63,7 @@ typedef struct yy_buffer_state *YY_BUFFER_STATE;
 #include <netinet/in.h>
 #endif
 
-#ifndef PHP_WIN32
 # include<netdb.h>
-#else
-# include "win32/inet.h"
-#endif
 
 #if HAVE_ARPA_INET_H
 # include <arpa/inet.h>
