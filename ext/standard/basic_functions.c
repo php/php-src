20f397cd729414c62c471c9109367e9a890e633f
helly <helly@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-08-21 16:55:31 +0000

    - Fix 64bit issue
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@265234 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -4929,7 +4929,7 @@ PHP_FUNCTION(get_current_user)
  */
 static int add_config_entry_cb(zval *entry TSRMLS_DC, int num_args, va_list args, zend_hash_key *hash_key)
 {
-	zval *retval = (zval *)va_arg(args, int);
+	zval *retval = (zval *)va_arg(args, zval*);
 	zval *tmp;
 
 	if (Z_TYPE_P(entry) == IS_STRING) {
