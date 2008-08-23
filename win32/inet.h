27e0a16fba5f8ad927d7912d052102e981d3052b
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-08-23 19:55:44 +0000

    - MFB: fix logic, build correctly when the build targets xp/2k
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@265388 c90b9560-bf6c-de11-be94-00142212c4b1

--- win32/inet.h
+++ win32/inet.h
@@ -2,8 +2,7 @@
 # include <In6addr.h>
 #endif
 #include <Ws2tcpip.h>
-
-#if _MSC_VER < 1500
+#if (_WIN32_WINNT < 0x0600) /* Vita/2k8 have these functions */
 PHPAPI int inet_pton(int af, const char* src, void* dst);
 PHPAPI const char* inet_ntop(int af, const void* src, char* dst, size_t size);
 PHPAPI int inet_aton(const char *cp, struct in_addr *inp);
