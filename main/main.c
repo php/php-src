77e07a631a7d74ce15325c2ca7f23526f59fd5a0
Jani Taskinen <jani@php.net>
2008-08-26 23:26:27 +0000

    MFH

--- main/main.c
+++ main/main.c
@@ -352,13 +352,15 @@ static PHP_INI_MH(OnChangeMailForceExtra)
 #	define PHP_SAFE_MODE_EXEC_DIR ""
 #endif
 
-#if defined(PHP_PROG_SENDMAIL) && !defined(NETWARE)
-#	define DEFAULT_SENDMAIL_PATH PHP_PROG_SENDMAIL " -t -i "
-#elif defined(PHP_WIN32)
-#	define DEFAULT_SENDMAIL_PATH NULL
+ /* Windows and Netware use the internal mail */
+#if defined(PHP_WIN32) || defined(NETWARE)
+# define DEFAULT_SENDMAIL_PATH NULL
+#elif defined(PHP_PROG_SENDMAIL)
+# define DEFAULT_SENDMAIL_PATH PHP_PROG_SENDMAIL " -t -i "
 #else
-#	define DEFAULT_SENDMAIL_PATH "/usr/sbin/sendmail -t -i" 
+# define DEFAULT_SENDMAIL_PATH "/usr/sbin/sendmail -t -i"
 #endif
+
 /* {{{ PHP_INI
  */
 PHP_INI_BEGIN()
