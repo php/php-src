421751d240734fffac73d3909bbc6cdc50e86d5f
Rasmus Lerdorf <rasmus@php.net>
2008-08-26 15:37:01 +0000

    Remove stray folding tag here

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -175,7 +175,7 @@ typedef struct _user_config_cache_entry {
 	HashTable *user_config;
 } user_config_cache_entry;
 
-static void user_config_cache_entry_dtor(user_config_cache_entry *entry) /* {{{ */
+static void user_config_cache_entry_dtor(user_config_cache_entry *entry)
 {
 	zend_hash_destroy(entry->user_config);
 	free(entry->user_config);
