8ed2adecf4613cb80f40d36eadd9668697ebfbc3
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-08-26 23:22:31 +0000

    Sync with PHP_5_3 (what a nice mess..thanks guys for keeping the branches in sync..NOT)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@265539 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/main.c
+++ main/main.c
@@ -462,8 +462,9 @@ static PHP_INI_MH(OnChangeMailForceExtra)
  * PHP_INCLUDE_PATH
  */
 
-/* Windows and Netware use the internal mail */
-#if defined(PHP_WIN32) || defined(NETWARE)
+#if defined(PHP_PROG_SENDMAIL) && !defined(NETWARE)
+#	define DEFAULT_SENDMAIL_PATH PHP_PROG_SENDMAIL " -t -i "
+#elif defined(PHP_WIN32)
 #	define DEFAULT_SENDMAIL_PATH NULL
 #else
 #	define DEFAULT_SENDMAIL_PATH "/usr/sbin/sendmail -t -i" 
