debb7666e37ddc91dbc68f2fc4acac945d8d2db3
lbarnaud <lbarnaud@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-09-12 14:15:05 +0000

    MFH: Fix cwd when running tests out of tree with ZTS builds (fixes #45837)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@266197 c90b9560-bf6c-de11-be94-00142212c4b1

--- run-tests.php
+++ run-tests.php
@@ -996,7 +996,7 @@ function error_report($testname, $logname, $tested)
 
 function system_with_timeout($commandline, $env = null, $stdin = null)
 {
-	global $leak_check;
+	global $leak_check, $cwd;
 
 	$data = '';
 
@@ -1004,7 +1004,7 @@ function system_with_timeout($commandline, $env = null, $stdin = null)
 		0 => array('pipe', 'r'),
 		1 => array('pipe', 'w'),
 		2 => array('pipe', 'w')
-		), $pipes, null, $env, array('suppress_errors' => true, 'binary_pipes' => true));
+		), $pipes, $cwd, $env, array('suppress_errors' => true, 'binary_pipes' => true));
 
 	if (!$proc) {
 		return false;
