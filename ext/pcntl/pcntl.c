9a011835fa0cce16c30ed7d6767f748cfbb885e9
lbarnaud <lbarnaud@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-09-12 19:49:04 +0000

    initialize optional args
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@266208 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pcntl/pcntl.c
+++ ext/pcntl/pcntl.c
@@ -602,7 +602,7 @@ PHP_FUNCTION(pcntl_wstopsig)
    Executes specified program in current process space as defined by exec(2) */
 PHP_FUNCTION(pcntl_exec)
 {
-	zval *args, *envs;
+	zval *args = NULL, *envs = NULL;
 	zval **element;
 	HashTable *args_hash, *envs_hash;
 	int argc = 0, argi = 0;
