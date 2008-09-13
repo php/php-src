3d9876cdbef3ae7771feae5096f7adf786b4556d
Moriyoshi Koizumi <moriyoshi@php.net>
2008-09-13 02:32:51 +0000

    - Properly initialize/finalize the library. This plugs the leaks caused by   onig_new().

--- ext/mbstring/php_mbregex.c
+++ ext/mbstring/php_mbregex.c
@@ -114,6 +114,7 @@ void php_mb_regex_globals_free(zend_mb_regex_globals *pglobals TSRMLS_DC)
 /* {{{ PHP_MINIT_FUNCTION(mb_regex) */
 PHP_MINIT_FUNCTION(mb_regex)
 {
+	onig_init();
 	return SUCCESS;
 }
 /* }}} */
@@ -121,6 +122,7 @@ PHP_MINIT_FUNCTION(mb_regex)
 /* {{{ PHP_MSHUTDOWN_FUNCTION(mb_regex) */
 PHP_MSHUTDOWN_FUNCTION(mb_regex)
 {
+	onig_end();
 	return SUCCESS;
 }
 /* }}} */
