fbd44bdc44221ad00781a5d20376ee83312cfd7c
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-09-18 16:02:09 +0000

    Show the library version currently loaded as well as the version the extension was compiled with. Useful for checking PHP is using the latest version.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@266488 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -1092,7 +1092,8 @@ PHP_MINFO_FUNCTION(openssl)
 {
 	php_info_print_table_start();
 	php_info_print_table_row(2, "OpenSSL support", "enabled");
-	php_info_print_table_row(2, "OpenSSL Version", OPENSSL_VERSION_TEXT);
+	php_info_print_table_row(2, "OpenSSL Library Version", SSLeay_version(SSLEAY_VERSION));
+	php_info_print_table_row(2, "OpenSSL Header Version", OPENSSL_VERSION_TEXT);
 	php_info_print_table_end();
 }
 /* }}} */
