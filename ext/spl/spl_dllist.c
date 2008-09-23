d2ccbcc9faccf916103df678e000cc41bd0d8ba0
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-09-23 12:05:02 +0000

    - Fixed bug #46160 (SPL - Memory leak when exception is throwed in offsetSet method)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@266683 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_dllist.c
+++ ext/spl/spl_dllist.c
@@ -816,6 +816,7 @@ SPL_METHOD(SplDoublyLinkedList, offsetSet)
 		index = spl_offset_convert_to_long(zindex TSRMLS_CC);
 
 		if (index < 0 || index >= intern->llist->count) {
+			zval_ptr_dtor(&value);
 			zend_throw_exception(spl_ce_OutOfRangeException, "Offset invalid or out of range", 0 TSRMLS_CC);
 			return;
 		}
@@ -838,6 +839,7 @@ SPL_METHOD(SplDoublyLinkedList, offsetSet)
 				intern->llist->ctor(element TSRMLS_CC);
 			}
 		} else {
+			zval_ptr_dtor(&value);
 			zend_throw_exception(spl_ce_OutOfRangeException, "Offset invalid", 0 TSRMLS_CC);
 			return;
 		}
