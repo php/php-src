08139f40b39eb83e317a24be4b16fb724e416d89
Greg Beaver <cellog@php.net>
2008-09-26 16:30:10 +0000

    MFB: fix bug #46178: memory leak in ext/phar

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -440,6 +440,11 @@ int phar_entry_delref(phar_entry_data *idata TSRMLS_DC) /* {{{ */
 		if (idata->fp && idata->fp != idata->phar->fp && idata->fp != idata->phar->ufp && idata->fp != idata->internal_file->fp) {
 			php_stream_close(idata->fp);
 		}
+		/* if phar_get_or_create_entry_data returns a sub-directory, we have to free it */
+		if (idata->internal_file->is_temp_dir) {
+			destroy_phar_manifest_entry((void *)idata->internal_file);
+			efree(idata->internal_file);
+		}
 	}
 
 	phar_archive_delref(idata->phar TSRMLS_CC);
