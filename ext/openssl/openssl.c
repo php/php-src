ad1c02c3f4f987933df7205378cac481db84e1dc
Rob Richards <rrichards@php.net>
2008-09-30 14:40:58 +0000

    initialize keyresource

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -2408,7 +2408,7 @@ PHP_FUNCTION(openssl_csr_sign)
 	X509 * cert = NULL, *new_cert = NULL;
 	X509_REQ * csr;
 	EVP_PKEY * key = NULL, *priv_key = NULL;
-	long csr_resource, certresource = 0, keyresource;
+	long csr_resource, certresource = 0, keyresource = -1;
 	int i;
 	struct php_x509_request req;
 	
