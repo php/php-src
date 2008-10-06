6e6014405ecf181a91477442568d7d1f54e0e26a
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-06 01:19:07 +0000

    MFB: Fixed a few signedness warnings
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@267004 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -2738,7 +2738,7 @@ PHP_FUNCTION(imap_utf7_decode)
 #if PHP_DEBUG
 	/* warn if we computed outlen incorrectly */
 	if (outp - out != outlen) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "outp - out [%d] != outlen [%d]", outp - out, outlen);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "outp - out [%ld] != outlen [%d]", outp - out, outlen);
 	}
 #endif
 
@@ -2855,7 +2855,7 @@ PHP_FUNCTION(imap_utf7_encode)
 #if PHP_DEBUG
 	/* warn if we computed outlen incorrectly */
 	if (outp - out != outlen) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "outp - out [%d] != outlen [%d]", outp - out, outlen);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "outp - out [%ld] != outlen [%d]", outp - out, outlen);
 	}
 #endif
 
