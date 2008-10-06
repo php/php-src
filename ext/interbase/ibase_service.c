9254c27bed96829f5b478eafd1c216d3c56bd60d
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-06 15:13:27 +0000

    - Fixed compiler warning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@267017 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/interbase/ibase_service.c
+++ ext/interbase/ibase_service.c
@@ -229,8 +229,7 @@ PHP_FUNCTION(ibase_service_attach)
 		user, isc_spb_password, (char)plen, pass, host);
 
 	if (spb_len > sizeof(buf) || spb_len == -1) {
-		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%ld)"
-			TSRMLS_CC, spb_len);
+		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%d)" TSRMLS_CC, spb_len);
 		RETURN_FALSE;
 	}
 
@@ -451,8 +450,7 @@ static void _php_ibase_backup_restore(INTERNAL_FUNCTION_PARAMETERS, char operati
 	}
 
 	if (spb_len > sizeof(buf) || spb_len <= 0) {
-		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%ld)"
-			TSRMLS_CC, spb_len);
+		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%d)" TSRMLS_CC, spb_len);
 		RETURN_FALSE;
 	}
 
@@ -560,8 +558,7 @@ options_argument:
 	}
 
 	if (spb_len > sizeof(buf) || spb_len == -1) {
-		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%ld)"
-			TSRMLS_CC, spb_len);
+		_php_ibase_module_error("Internal error: insufficient buffer space for SPB (%d)" TSRMLS_CC, spb_len);
 		RETURN_FALSE;
 	}
 
