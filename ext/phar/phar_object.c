15fc837b990c8354f8b69aab8bb870466d76577e
Greg Beaver <cellog@php.net>
2008-10-12 19:45:12 +0000

    MFB: fix incorrect key for Phar destructor

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -1389,7 +1389,7 @@ PHP_METHOD(Phar, __destruct)
 	phar_archive_object *phar_obj = (phar_archive_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
 
 	if (phar_obj->arc.archive && phar_obj->arc.archive->is_persistent) {
-		zend_hash_del(&PHAR_GLOBALS->phar_persist_map, (const char *) &(phar_obj->arc.archive), sizeof(&(phar_obj->arc.archive)));
+		zend_hash_del(&PHAR_GLOBALS->phar_persist_map, (const char *) phar_obj->arc.archive, sizeof(phar_obj->arc.archive));
 	}
 }
 /* }}} */
