3e3ff0d2a6590a10f8d0f4c06a6d02b9acf3d898
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-12 19:40:11 +0000

    add tests for copy-on-write support  - fix metadata handling with cached phars  - fix virtual_dirs with rmdir  - ensure that after copy-on-write, all existing Phar objects link to the newly copied phar data
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@267242 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/tar.c
+++ ext/phar/tar.c
@@ -620,6 +620,7 @@ static int phar_tar_writeheaders(void *pDest, void *argument TSRMLS_DC) /* {{{ *
 		}
 	}
 
+	phar_add_virtual_dirs(entry->phar, entry->filename, entry->filename_len);
 	memset((char *) &header, 0, sizeof(header));
 
 	if (entry->filename_len > 100) {
