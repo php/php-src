6eefb1e44a6b98933e0c9c6c46b6536f53408245
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-15 13:34:13 +0000

    - Fixed possible efree(NULL)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@267349 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -1713,7 +1713,9 @@ static void _php_pgsql_free_params(char **params, int num_params)
 	if (num_params > 0) {
 		int i;
 		for (i = 0; i < num_params; i++) {
-			efree(params[i]);
+			if (params[i]) {
+				efree(params[i]);
+			}
 		}
 		efree(params);
 	}
