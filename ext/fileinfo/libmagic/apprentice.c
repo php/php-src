d1f1a8471410daa0819ecceab1a28ae409037900
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-22 14:15:47 +0000

    MFH: When using the internal database and there is an error it could try free some invalid things. This was causing a bus error on ppc.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@267625 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/fileinfo/libmagic/apprentice.c
+++ ext/fileinfo/libmagic/apprentice.c
@@ -1842,6 +1842,9 @@ eatsize(const char **p)
 
 /*
  * handle a compiled file.
+ * return -1 = error
+ * return 1  = memory structure you can free
+ * return 3  = bundled library from PHP
  */
 private int
 apprentice_map(struct magic_set *ms, struct magic **magicp, uint32_t *nmagicp,
@@ -1853,7 +1856,7 @@ apprentice_map(struct magic_set *ms, struct magic **magicp, uint32_t *nmagicp,
 	char *dbname = NULL;
 	void *mm = NULL;
 	int   ret = 0;
-	php_stream *stream;
+	php_stream *stream = NULL;
 	php_stream_statbuf st;
 
 
@@ -1908,12 +1911,15 @@ internal_loaded:
 			goto error1;
 		}
 		needsbyteswap = 1;
-	} else
+	} else {
 		needsbyteswap = 0;
+	}
+
 	if (needsbyteswap)
 		version = swap4(ptr[1]);
 	else
 		version = ptr[1];
+
 	if (version != VERSIONNO) {
 		file_error(ms, 0, "File %d.%d supports only %d version magic "
 		    "files. `%s' is version %d", FILE_VERSION_MAJOR, patchlevel,
@@ -1953,14 +1959,17 @@ error1:
 	if (stream) {
 		php_stream_close(stream);
 	}
-	if (mm) {
+
+	if (mm && ret == 1) {
 		efree(mm);
 	} else {
 		*magicp = NULL;
 		*nmagicp = 0;
 	}
 error2:
-	efree(dbname);
+	if (dbname) {
+		efree(dbname);
+	}
 	return -1;
 }
 
