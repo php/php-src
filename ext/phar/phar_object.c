11c83c19de56b8a839a23369474587991e762b69
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-26 05:49:09 +0000

    fix several errors found by valgrind 1 - entry metadata not properly processed or retrieved from cached phars 2 - copy on write was using a void return value instead of int, a dangerous oversight in phar_update_cached_entry 3 - metadata creation in entries for cached phars was causing an invalid read
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@267774 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/phar_object.c
+++ ext/phar/phar_object.c
@@ -4583,6 +4583,15 @@ PHP_METHOD(PharFileInfo, getMetadata)
 	PHAR_ENTRY_OBJECT();
 
 	if (entry_obj->ent.entry->metadata) {
+		if (entry_obj->ent.entry->is_persistent) {
+			zval *ret;
+			char *buf = estrndup((char *) entry_obj->ent.entry->metadata, entry_obj->ent.entry->metadata_len);
+			/* assume success, we would have failed before */
+			phar_parse_metadata(&buf, &ret, entry_obj->ent.entry->metadata_len TSRMLS_CC);
+			efree(buf);
+			RETURN_ZVAL(ret, 0, 1);
+			return;
+		}
 		RETURN_ZVAL(entry_obj->ent.entry->metadata, 1, 0);
 	}
 }
