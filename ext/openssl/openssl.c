8bd294387723d8e199a32aac8fa42f6e9e550a7e
Ilia Alshanetsky <iliaa@php.net>
2008-10-26 14:35:41 +0000

    Fixed compiler warning

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -3439,7 +3439,7 @@ PHP_FUNCTION(openssl_pkcs7_encrypt)
 		}
 	}
 
-	BIO_reset(infile);
+	(void)BIO_reset(infile);
 
 	/* write the encrypted data */
 	SMIME_write_PKCS7(outfile, p7, infile, flags);
@@ -3528,7 +3528,7 @@ PHP_FUNCTION(openssl_pkcs7_sign)
 		goto clean_exit;
 	}
 
-	BIO_reset(infile);
+	(void)BIO_reset(infile);
 
 	/* tack on extra headers */
 	if (zheaders) {
