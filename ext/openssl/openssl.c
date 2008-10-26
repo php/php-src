7a8daaa36f6fb99c7b4b55c0f158d55e3863f52d
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-10-26 14:36:25 +0000

    MFB: Fixed compiler warning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@267791 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -3637,7 +3637,7 @@ PHP_FUNCTION(openssl_pkcs7_encrypt)
 		}
 	}
 
-	BIO_reset(infile);
+	(void)BIO_reset(infile);
 
 	/* write the encrypted data */
 	SMIME_write_PKCS7(outfile, p7, infile, flags);
@@ -3732,7 +3732,7 @@ PHP_FUNCTION(openssl_pkcs7_sign)
 		goto clean_exit;
 	}
 
-	BIO_reset(infile);
+	(void)BIO_reset(infile);
 
 	/* tack on extra headers */
 	if (zheaders) {
