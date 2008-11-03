c644ba688b6a798cc48d166430a71c7ada4d558c
Arnaud Le Blanc <lbarnaud@php.net>
2008-11-03 23:29:50 +0000

    MFH: Fixed bug #44607 (stream_get_line unable to correctly identify the "ending" in the stream content)

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -531,16 +531,16 @@ static void php_stream_fill_read_buffer(php_stream *stream, size_t size TSRMLS_D
 		efree(chunk_buf);
 
 	} else {
+		/* reduce buffer memory consumption if possible, to avoid a realloc */
+		if (stream->readbuf && stream->readbuflen - stream->writepos < stream->chunk_size) {
+			memmove(stream->readbuf, stream->readbuf + stream->readpos, stream->readbuflen - stream->readpos);
+			stream->writepos -= stream->readpos;
+			stream->readpos = 0;
+		}
 		/* is there enough data in the buffer ? */
-		if (stream->writepos - stream->readpos < (off_t)size) {
+		while (stream->writepos - stream->readpos < (off_t)size) {
 			size_t justread = 0;
-
-			/* reduce buffer memory consumption if possible, to avoid a realloc */
-			if (stream->readbuf && stream->readbuflen - stream->writepos < stream->chunk_size) {
-				memmove(stream->readbuf, stream->readbuf + stream->readpos, stream->readbuflen - stream->readpos);
-				stream->writepos -= stream->readpos;
-				stream->readpos = 0;
-			}
+			size_t toread;
 
 			/* grow the buffer if required
 			 * TODO: this can fail for persistent streams */
@@ -550,12 +550,16 @@ static void php_stream_fill_read_buffer(php_stream *stream, size_t size TSRMLS_D
 						stream->is_persistent);
 			}
 
+			toread = stream->readbuflen - stream->writepos;
 			justread = stream->ops->read(stream, stream->readbuf + stream->writepos,
-					stream->readbuflen - stream->writepos
+					toread
 					TSRMLS_CC);
 
 			if (justread != (size_t)-1) {
 				stream->writepos += justread;
+			}
+			if (stream->eof || justread != toread) {
+				break;
 			}
 		}
 	}
