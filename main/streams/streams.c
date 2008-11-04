1f5021e6c3739b2ee31e4b2c8d78798beab30d68
lbarnaud <lbarnaud@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-11-04 17:05:17 +0000

    Fixed stream_get_line() to behave as documented on non-blocking streams
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@268295 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -1136,6 +1136,9 @@ PHPAPI char *php_stream_get_record(php_stream *stream, size_t maxlen, size_t *re
 		}
 
 		if (!e) {
+			if (seek_len < maxlen && !stream->eof) {
+				return NULL;
+			}
 			toread = maxlen;
 		} else {
 			toread = e - stream->readbuf.s - stream->readpos;
