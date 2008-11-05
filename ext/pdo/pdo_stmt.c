27357e9ce5dc5e67a8400ea146f2e2b5484e6cd9
Felipe Pena <felipe@php.net>
2008-11-05 23:39:09 +0000

    - Fixed wrong check - Improved test

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -2200,14 +2200,14 @@ static PHP_METHOD(PDOStatement, debugDumpParams)
 			uint len;
 			ulong num;
 
-			if (zend_hash_get_current_key_ex(stmt->bound_params, &str, &len, &num, 0, &pos) == HASH_KEY_IS_STRING) {
-				php_stream_printf(out TSRMLS_CC, "Key: Position #%d:\n", num);
+			if (zend_hash_get_current_key_ex(stmt->bound_params, &str, &len, &num, 0, &pos) == HASH_KEY_IS_LONG) {
+				php_stream_printf(out TSRMLS_CC, "Key: Position #%ld:\n", num);
 			} else {
 				php_stream_printf(out TSRMLS_CC, "Key: Name: [%d] %.*s\n", len, len, str);
 			}
 
-			php_stream_printf(out TSRMLS_CC, "paramno=%d\nname=[%d] %.*s\nis_param=%d\nparam_type=%d\n",
-				param->paramno, param->namelen, param->namelen, param->name,
+			php_stream_printf(out TSRMLS_CC, "paramno=%d\nname=[%d] \"%.*s\"\nis_param=%d\nparam_type=%d\n",
+				param->paramno, param->namelen, param->namelen, param->name ? param->name : "",
 				param->is_param,
 				param->param_type);
 			
