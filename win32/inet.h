41e3343a26f465383317f5fe8aeed3b3fc39c1e5
Pierre Joye <pajoye@php.net>
2008-11-06 14:16:14 +0000

    - MFH, has this feature before Vista/2k8 (use the same test in 5.3)

--- win32/inet.h
+++ win32/inet.h
@@ -4,7 +4,7 @@
 #endif
 #include <Ws2tcpip.h>
 
-#if (_WIN32_WINNT < 0x0600) /* Vita/2k8 have these functions */
+#if (_WIN32_WINNT <= 0x500)
 PHPAPI int inet_pton(int af, const char* src, void* dst);
 PHPAPI const char* inet_ntop(int af, const void* src, char* dst, size_t size);
 PHPAPI int inet_aton(const char *cp, struct in_addr *inp);
