9334bbd650c65f34c2f7cb5e96e12ba0518f119b
David Coallier <davidc@php.net>
2008-11-10 18:47:28 +0000

    - After readying Johannes's mail, the conclusion that a "smarter" system   to find out if the return_value had the correct number of elements was   definitely needed. Simply added a difference to both dbh and stmt to   make sure that the error info always has 3 elements.
    - Bug #44154 (pdo->errorInfo doesn't always return three elements)
    - Now pdo->errorInfo() AND stmt->errorInfo() return three elements.
    
    - [DOC] Make sure that not only the pdo->errorInfo() is returning 3 elms, but
      also the PDOStatement object

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -1778,6 +1778,10 @@ static PHP_METHOD(PDOStatement, errorCode)
 		return;
 	}
 
+	if (stmt->error_code[0] == '\0') {
+		RETURN_NULL();
+	}
+
 	RETURN_STRING(stmt->error_code, 1);
 }
 /* }}} */
@@ -1786,6 +1790,10 @@ static PHP_METHOD(PDOStatement, errorCode)
    Fetch extended error information associated with the last operation on the statement handle */
 static PHP_METHOD(PDOStatement, errorInfo)
 {
+	int error_count;
+	int error_count_diff     = 0;
+	int error_expected_count = 3;
+
 	PHP_STMT_GET_OBJ;
 
 	if (zend_parse_parameters_none() == FAILURE) {
@@ -1797,6 +1805,17 @@ static PHP_METHOD(PDOStatement, errorInfo)
 
 	if (stmt->dbh->methods->fetch_err) {
 		stmt->dbh->methods->fetch_err(stmt->dbh, stmt, return_value TSRMLS_CC);
+	}
+
+	error_count = zend_hash_num_elements(Z_ARRVAL_P(return_value));
+
+	if (error_expected_count > error_count) {
+		error_count_diff = error_expected_count - error_count;
+		
+		int current_index;
+		for (current_index = 0; current_index < error_count_diff; current_index++) {
+			add_next_index_null(return_value);
+		}
 	}
 }
 /* }}} */
