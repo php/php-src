f24bc6f421642ddb9f4b7d36683bd202717be662
Arnaud Le Blanc <lbarnaud@php.net>
2008-11-13 05:46:51 +0000

    Added stream_context_get_params() [DOC] proto bool stream_context_get_params(resource context|stream)       Get parameters of a file context (parameters set by       stream_context_set_params())

--- ext/standard/streamsfuncs.c
+++ ext/standard/streamsfuncs.c
@@ -1151,6 +1151,35 @@ PHP_FUNCTION(stream_context_set_params)
 }
 /* }}} */
 
+/* {{{ proto bool stream_context_get_params(resource context|resource stream) U
+   Get parameters of a file context */
+PHP_FUNCTION(stream_context_get_params)
+{
+	zval *zcontext, *options;
+	php_stream_context *context;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "r", &zcontext) == FAILURE) {
+		RETURN_FALSE;
+	}
+
+	context = decode_context_param(zcontext TSRMLS_CC);
+	if (!context) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid stream/context parameter");
+		RETURN_FALSE;
+	}
+
+	array_init(return_value);
+	if (context->notifier && context->notifier->ptr && context->notifier->func == user_space_stream_notifier) {
+		add_ascii_assoc_zval_ex(return_value, ZEND_STRS("notification"), context->notifier->ptr);
+		Z_ADDREF_P(context->notifier->ptr);
+	}
+	ALLOC_INIT_ZVAL(options);
+	ZVAL_ZVAL(options, context->options, 1, 0);
+	add_ascii_assoc_zval_ex(return_value, ZEND_STRS("options"), options);
+}
+/* }}} */
+
+
 /* {{{ proto resource stream_context_get_default([array options]) U
    Get a handle on the default file/stream context and optionally set parameters */
 PHP_FUNCTION(stream_context_get_default)
