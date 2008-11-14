dd9e9f26e2e30175de7f750e143bdebc3daca008
Hannes Magnusson <bjori@php.net>
2008-11-14 19:22:43 +0000

    MFH: Interfaces extend other interfaces, not implement

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -371,7 +371,11 @@ static void _class_string(string *str, zend_class_entry *ce, zval *obj, char *in
 	if (ce->num_interfaces) {
 		zend_uint i;
 
-		string_printf(str, " implements %s", ce->interfaces[0]->name);
+		if (ce->ce_flags & ZEND_ACC_INTERFACE) {
+			string_printf(str, " extends %s", ce->interfaces[0]->name);
+		} else {
+			string_printf(str, " implements %s", ce->interfaces[0]->name);
+		}
 		for (i = 1; i < ce->num_interfaces; ++i) {
 			string_printf(str, ", %s", ce->interfaces[i]->name);
 		}
