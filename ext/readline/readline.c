381a7b4a40755d3f6b85cde852bdee782d9bfd1e
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-11-16 18:02:17 +0000

    MFB: Fixed memory leak inside readline_callback_handler_install() function
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@269095 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/readline/readline.c
+++ ext/readline/readline.c
@@ -187,7 +187,7 @@ PHP_RSHUTDOWN_FUNCTION(readline)
 #if HAVE_RL_CALLBACK_READ_CHAR
 	if (_prepped_callback) {
 		rl_callback_handler_remove();
-		FREE_ZVAL(_prepped_callback);
+		zval_ptr_dtor(&_prepped_callback);
 		_prepped_callback = 0;
 	}
 #endif
@@ -561,7 +561,7 @@ PHP_FUNCTION(readline_callback_handler_install)
 
 	if (_prepped_callback) {
 		rl_callback_handler_remove();
-		FREE_ZVAL(_prepped_callback);
+		zval_ptr_dtor(&_prepped_callback);
 	}
 
 	MAKE_STD_ZVAL(_prepped_callback);
