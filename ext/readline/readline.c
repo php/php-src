22851007ca8ff33274e844ad08706cb4e22d6d02
Felipe Pena <felipe@php.net>
2008-11-20 22:26:32 +0000

    - MFH: Fixed memory leaks in readline_completion_function()

--- ext/readline/readline.c
+++ ext/readline/readline.c
@@ -115,8 +115,10 @@ PHP_MINIT_FUNCTION(readline)
 
 PHP_RSHUTDOWN_FUNCTION(readline)
 {
-	if (_readline_completion) 
+	if (_readline_completion) {
+		zval_dtor(_readline_completion);
 		FREE_ZVAL(_readline_completion);
+	}
 #if HAVE_RL_CALLBACK_READ_CHAR
 	if (_prepped_callback) {
 		rl_callback_handler_remove();
@@ -456,9 +458,12 @@ PHP_FUNCTION(readline_completion_function)
 		efree(name);
 		RETURN_FALSE;
 	}
+	efree(name);
 
-	if (_readline_completion)
+	if (_readline_completion) {
+		zval_dtor(_readline_completion);
 		FREE_ZVAL(_readline_completion);
+	}
 
 	MAKE_STD_ZVAL(_readline_completion);
 	*_readline_completion = *arg;
