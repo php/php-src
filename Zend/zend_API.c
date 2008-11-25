7842f555227aa4f61c9f902615e886efe27bf755
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-11-25 22:23:28 +0000

    cleanup
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@269836 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -319,7 +319,7 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 {
 	char *spec_walk = *spec;
 	char c = *spec_walk++;
-	int return_null = 0, obj_array = 0;
+	int return_null = 0;
 	int alternate_form = 0;
 	int return_orig_type = 0;
 	zend_uchar orig_type;
@@ -687,7 +687,6 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 			}
 			break;
 		case 'A':
-				obj_array = 1;
 		case 'a':
 			{
 				zval **p = va_arg(*va, zval **);
@@ -695,7 +694,7 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 					*p = NULL;
 					break;
 				}
-				if (Z_TYPE_PP(arg) == IS_ARRAY || (Z_TYPE_PP(arg) == IS_OBJECT && obj_array != 0)) {
+				if (Z_TYPE_PP(arg) == IS_ARRAY || (c == 'A' && Z_TYPE_PP(arg) == IS_OBJECT)) {
 					*p = *arg;
 				} else {
 					return "array";
@@ -703,7 +702,6 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 			}
 			break;
 		case 'H':
-				obj_array = 1;
 		case 'h':
 			{
 				HashTable **p = va_arg(*va, HashTable **);
@@ -713,7 +711,7 @@ static char *zend_parse_arg_impl(int arg_num, zval **arg, va_list *va, char **sp
 				}
 				if (Z_TYPE_PP(arg) == IS_ARRAY) {
 					*p = Z_ARRVAL_PP(arg);
-				} else if(obj_array && Z_TYPE_PP(arg) == IS_OBJECT) {
+				} else if(c == 'H' && Z_TYPE_PP(arg) == IS_OBJECT) {
 					*p = HASH_OF(*arg);
 					if(*p == NULL) {
 						return "array";
