819880cf1b2e166da422533d6f17f22e7087f70b
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-12-02 10:15:08 +0000

    - export php_scandir and php_alphasort correctly (used by APC for example)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@270283 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_scandir.c
+++ main/php_scandir.c
@@ -50,14 +50,14 @@
 #include <string.h>
 #endif
 
-int php_alphasort(const struct dirent **a, const struct dirent **b)
+PHPAPI int php_alphasort(const struct dirent **a, const struct dirent **b)
 {
 	return strcoll((*a)->d_name,(*b)->d_name);
 }
 #endif /* HAVE_ALPHASORT */
 
 #ifndef HAVE_SCANDIR
-int php_scandir(const char *dirname, struct dirent **namelist[], int (*selector) (const struct dirent *entry), int (*compare) (const struct dirent **a, const struct dirent **b))
+PHPAPI int php_scandir(const char *dirname, struct dirent **namelist[], int (*selector) (const struct dirent *entry), int (*compare) (const struct dirent **a, const struct dirent **b))
 {
 	DIR *dirp = NULL;
 	struct dirent **vector = NULL;
