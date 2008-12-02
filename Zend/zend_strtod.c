17842e5d8d47d0b2a9d560f6aace4d2ce03cb6d5
cseiler <cseiler@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-12-02 16:19:10 +0000

    - MFH: Changed floating point behaviour to consistently use double precision   on all platforms and with all compilers.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@270301 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_strtod.c
+++ Zend/zend_strtod.c
@@ -93,6 +93,7 @@
 
 #include <zend_operators.h>
 #include <zend_strtod.h>
+#include <zend_float.h>
 
 #ifdef ZTS
 #include <TSRM.h>
@@ -2032,6 +2033,7 @@ ret1:
 
 ZEND_API double zend_strtod (CONST char *s00, char **se)
 {
+	ZEND_FLOAT_DECLARE
 	int bb2, bb5, bbe, bd2, bd5, bbbits, bs2, c, dsign,
 		e, e1, esign, i, j, k, nd, nd0, nf, nz, nz0, sign;
 	CONST char *s, *s0, *s1;
@@ -2043,6 +2045,8 @@ ZEND_API double zend_strtod (CONST char *s00, char **se)
 	double result;
 
 	CONST char decimal_point = '.';
+
+	ZEND_FLOAT_ENSURE();
 
 	sign = nz0 = nz = 0;
 	value(rv) = 0.;
@@ -2574,7 +2578,7 @@ ret:
 	}
 	_THREAD_PRIVATE_MUTEX_UNLOCK(pow5mult_mutex);
 
-	return result;
+	ZEND_FLOAT_RETURN(result);
 }
 
 ZEND_API double zend_hex_strtod(const char *str, char **endptr)
