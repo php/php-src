61f0a0ddd4e151be7846f8471fecdae98fd29134
Derick Rethans <derick@php.net>
2008-12-02 18:01:07 +0000

    - Fixed bug #46732 (mktime.year description is wrong).

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -1558,7 +1558,7 @@ PHPAPI void php_mktime(INTERNAL_FUNCTION_PARAMETERS, int gmt)
 		case 6:
 			if (yea >= 0 && yea < 70) {
 				yea += 2000;
-			} else if (yea >= 70 && yea <= 110) {
+			} else if (yea >= 70 && yea <= 100) {
 				yea += 1900;
 			}
 			now->y = yea;
