b822f247488daa1a9c496b521f5863b27981c773
Ilia Alshanetsky <iliaa@php.net>
2008-12-03 15:54:03 +0000

    PHP 5.2.7

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 2
 #define PHP_RELEASE_VERSION 7
-#define PHP_EXTRA_VERSION "RC6-dev"
-#define PHP_VERSION "5.2.7RC6-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.2.7"
 #define PHP_VERSION_ID 50207
