1f780aeeaece47dda9cfbd4a45d522a6e02d29fa
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-12-18 20:04:26 +0000

    MFB: Fixed bug #46887 (Invalid calls to php_error_docref())
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@271525 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqli/mysqli.c
+++ ext/mysqli/mysqli.c
@@ -1390,7 +1390,7 @@ if (a) {\
 #define LOCAL_INFILE_ERROR_MSG(source,dest)\
 	memset(source, 0, LOCAL_INFILE_ERROR_LEN);\
 	memcpy(source, dest, MIN(strlen(dest), LOCAL_INFILE_ERROR_LEN-1));\
-	php_error_docref(NULL TSRMLS_CC, E_WARNING, dest);
+	php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s", dest);
 
 
 /* {{{ php_local_infile_init
