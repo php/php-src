a1a4c02290748d79437497e14143b7f98c2c4345
Felipe Pena <felipe@php.net>
2008-12-24 11:24:38 +0000

    - Fixed bug #46918 (imap_rfc822_parse_adrlist host part not filled in correctly) [only in this branch]

--- ext/imap/php_imap.c
+++ ext/imap/php_imap.c
@@ -2201,7 +2201,7 @@ PHP_FUNCTION(imap_rfc822_parse_adrlist)
 
 	/* rfc822_parse_adrlist() modifies passed string. Copy it. */
 	str_copy = estrndup(Z_STRVAL_PP(str), Z_STRLEN_PP(str));
-	rfc822_parse_adrlist(&env->to, str_copy, defaulthost);
+	rfc822_parse_adrlist(&env->to, str_copy, Z_STRVAL_PP(defaulthost));
 	efree(str_copy);
 
 	array_init(return_value);
