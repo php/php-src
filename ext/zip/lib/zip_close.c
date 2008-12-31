609a9038ffb5f34fa66565136ef51b56dce33925
sebastian <sebastian@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-12-31 11:12:40 +0000

    Bump copyright year, 3 of 3.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@272367 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zip/lib/zip_close.c
+++ ext/zip/lib/zip_close.c
@@ -1,6 +1,6 @@
 /*
   zip_close.c -- close zip archive and update changes
-  Copyright (C) 1999-2008 Dieter Baron and Thomas Klausner
+  Copyright (C) 1999-2009 Dieter Baron and Thomas Klausner
 
   This file is part of libzip, a library to manipulate ZIP archives.
   The authors can be contacted at <libzip@nih.at>
