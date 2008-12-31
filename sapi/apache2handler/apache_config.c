a615d3aa609c3b007611c2eece8d43e10d863d85
bjori <bjori@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-12-31 14:46:38 +0000

    MFB: Nuke unused variables
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@272415 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache2handler/apache_config.c
+++ sapi/apache2handler/apache_config.c
@@ -138,11 +138,13 @@ static zend_bool should_overwrite_per_dir_entry(HashTable *target_ht, php_dir_en
 void *merge_php_config(apr_pool_t *p, void *base_conf, void *new_conf)
 {
 	php_conf_rec *d = base_conf, *e = new_conf, *n = NULL;
+#if STAS_0
 	php_dir_entry *pe;
 	php_dir_entry *data;
 	zstr str;
 	uint str_len;
 	ulong num_index;
+#endif
 
 	n = create_php_config(p, "merge_php_config");
 	/* copy old config */
