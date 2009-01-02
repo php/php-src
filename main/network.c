e812a31c9b6f87779451fe448828a8e95b7302a7
Felipe Pena <felipe@php.net>
2009-01-02 21:43:07 +0000

    - MFH: Errr, rule nÂº 1: be portable :)

--- main/network.c
+++ main/network.c
@@ -1141,7 +1141,9 @@ PHPAPI int php_poll2(php_pollfd *ufds, unsigned int nfds, int timeout)
 		tv.tv_sec = timeout / 1000;
 		tv.tv_usec = (timeout - (tv.tv_sec * 1000)) * 1000;
 	}
+#ifndef PHP_WIN32
 	errno = 0;
+#endif
 	n = select(max_fd + 1, &rset, &wset, &eset, timeout >= 0 ? &tv : NULL);
 
 	if (n >= 0) {
