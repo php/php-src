96b1c50ea9fe1610028875e97b04a1ad096252dc
Ilia Alshanetsky <iliaa@php.net>
2009-01-04 16:31:12 +0000

    Added missing initialization

--- ext/sqlite3/sqlite3.c
+++ ext/sqlite3/sqlite3.c
@@ -70,7 +70,7 @@ PHP_METHOD(sqlite3, open)
 	php_sqlite3_db_object *db_obj;
 	zval *object = getThis();
 	char *filename, *encryption_key, *fullpath;
-	int filename_len, encryption_key_len;
+	int filename_len, encryption_key_len = 0;
 	long flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
 	db_obj = (php_sqlite3_db_object *)zend_object_store_get_object(object TSRMLS_CC);
 
