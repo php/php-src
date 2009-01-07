d2287d770133f216805e43bd6fcc0c35bd2fe318
Felipe Pena <felipe@php.net>
2009-01-07 20:19:17 +0000

    - Added check for EWOULDBLOCK - Using EWOULDBLOCK instead of EAGAIN by portability issues (Related to #46917)

--- main/php_network.h
+++ main/php_network.h
@@ -40,6 +40,10 @@
 #	define ftruncate(a, b) chsize(a, b)
 #endif /* defined(PHP_WIN32) */
 
+#ifndef EWOULDBLOCK
+# define EWOULDBLOCK EAGAIN
+#endif
+
 #ifdef PHP_WIN32
 #define php_socket_errno() WSAGetLastError()
 #else
