4c8bc05bf22f06d7ec6cb7ef9527abfcdb197418
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-01-07 14:35:50 +0000

    - Fixed bug #47027 (var_export doesn't show numeric indices on ArrayObject).
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@272963 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -486,13 +486,15 @@ static int php_object_element_export(zval **zv TSRMLS_DC, int num_args, va_list 
 
 	level = va_arg(args, int);
 
+	php_printf("%*c", level + 1, ' ');
 	if (hash_key->nKeyLength != 0) {
-		php_printf("%*c", level + 1, ' ');
 		zend_u_unmangle_property_name(hash_key->type, hash_key->arKey, hash_key->nKeyLength - 1, &class_name, &prop_name);
 		php_printf(" '%R' => ", hash_key->type, prop_name);
-		php_var_export(zv, level + 2 TSRMLS_CC);
-		PUTS (",\n");
+	} else {
+		php_printf(" %ld => ", hash_key->h);
 	}
+	php_var_export(zv, level + 2 TSRMLS_CC);
+	PUTS (",\n");
 	return 0;
 }
 /* }}} */
