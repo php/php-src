e28ad0cd2836961f0ec2468c940b1256a0ca858e
Felipe Pena <felipe@php.net>
2009-01-08 19:21:25 +0000

    MFH: - Fixed bug #47037 (No error when using fopen with emty string) Patch by Cristian Rodriguez R.

--- main/streams/streams.c
+++ main/streams/streams.c
@@ -1763,6 +1763,7 @@ PHPAPI php_stream *_php_stream_open_wrapper_ex(char *path, char *mode, int optio
 	}
 
 	if (!path || !*path) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Filename cannot be empty");
 		return NULL;
 	}
 
