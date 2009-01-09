514e6333278ff089a9fd27b7d1bba4ea23c28641
Antony Dovgal <tony2001@php.net>
2009-01-09 19:16:55 +0000

    MFH: add zend_ts_hash_copy_to_hash()

--- Zend/zend_ts_hash.c
+++ Zend/zend_ts_hash.c
@@ -273,6 +273,13 @@ ZEND_API void zend_ts_hash_copy(TsHashTable *target, TsHashTable *source, copy_c
 	end_read(source);
 }
 
+ZEND_API void zend_ts_hash_copy_to_hash(HashTable *target, TsHashTable *source, copy_ctor_func_t pCopyConstructor, void *tmp, uint size)
+{
+	begin_read(source);
+	zend_hash_copy(target, TS_HASH(source), pCopyConstructor, tmp, size);
+	end_read(source);
+}
+
 ZEND_API void zend_ts_hash_merge(TsHashTable *target, TsHashTable *source, copy_ctor_func_t pCopyConstructor, void *tmp, uint size, int overwrite)
 {
 	begin_read(source);
