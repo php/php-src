bce6bc73fc3697348a1e5c49a618cfba83db2869
Pierre Joye <pajoye@php.net>
2009-01-16 13:52:13 +0000

    -add some doc about versions

--- main/win32_internal_function_disabled.h
+++ main/win32_internal_function_disabled.h
@@ -19,6 +19,15 @@
 /* $Id$ */
 
 /* 5 means the min version is 5 (XP/2000), 6 (2k8/vista), etc. */
+
+/*
+Windows Server 2008     6.0
+Windows Vista           6.0
+Windows Server 2003 R2  5.2
+Windows Server 2003     5.2
+Windows XP              5.1
+Windows 2000            5.0
+*/
 static const char *function_name_5[] = {"link", NULL};
 const int function_name_cnt_5 = 1;
 static const char *function_name_6[] = {"readlink", "symlink", NULL};
