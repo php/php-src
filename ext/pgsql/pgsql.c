0b134ac73352d9618042c310c8a2fc985d5da6b0
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-01-18 23:49:11 +0000

    - Fixed bug #47048 (Segfault with new pg_meta_data)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@273805 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -4830,7 +4830,7 @@ PHP_PGSQL_API int php_pgsql_meta_data(PGconn *pg_link, const char *table_name, z
 	src = estrdup(table_name);
 	tmp_name = php_strtok_r(src, ".", &tmp_name2);
 	
-	if (!*tmp_name2) {
+	if (!tmp_name2 || !*tmp_name2) {
 		/* Default schema */
 		tmp_name2 = tmp_name;
 		tmp_name = "public";
