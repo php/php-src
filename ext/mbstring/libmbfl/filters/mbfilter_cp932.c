a0e7fd7ab7b274bdac4d08ff1259f9e89d9a0b0d
moriyoshi <moriyoshi@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-02-16 01:57:37 +0000

    - MFH: Fix bug #47399 (mb_check_encoding() return true for some illegal SJIS   charcters)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@275894 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mbstring/libmbfl/filters/mbfilter_cp932.c
+++ ext/mbstring/libmbfl/filters/mbfilter_cp932.c
@@ -167,7 +167,7 @@ mbfl_filt_conv_sjiswin_wchar(int c, mbfl_convert_filter *filter)
 	case 1:		/* kanji second char */
 		filter->status = 0;
 		c1 = filter->cache;
-		if (c > 0x39 && c < 0xfd && c != 0x7f) {
+		if (c >= 0x40 && c <= 0xfc && c != 0x7f) {
 			w = 0;
 			SJIS_DECODE(c1, c, s1, s2);
 			s = (s1 - 0x21)*94 + s2 - 0x21;
