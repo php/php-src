ac3183128c307574b32e89b5317db0f142a1b372
Moriyoshi Koizumi <moriyoshi@php.net>
2009-02-17 16:28:33 +0000

    - Add an entry for http_output_conv_mimetypes to the output of mb_get_info().

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -4152,6 +4152,9 @@ PHP_FUNCTION(mb_get_info)
 		if ((name = (char *)mbfl_no_encoding2name(MBSTRG(current_http_output_encoding))) != NULL) {
 			add_assoc_string(return_value, "http_output", name, 1);
 		}
+		if ((name = (char *)zend_ini_string("mbstring.http_output_conv_mimetypes", sizeof("mbstring.http_output_conv_mimetypes"), 0)) != NULL) {
+			add_assoc_string(return_value, "http_output_conv_mimetypes", name, 1);
+		}
 		add_assoc_long(return_value, "func_overload", MBSTRG(func_overload));
 		if (MBSTRG(func_overload)){
 			over_func = &(mb_ovld[0]);
@@ -4243,6 +4246,10 @@ PHP_FUNCTION(mb_get_info)
 		if ((name = (char *)mbfl_no_encoding2name(MBSTRG(current_http_output_encoding))) != NULL) {
 			RETVAL_STRING(name, 1);
 		}		
+	} else if (!strcasecmp("http_output_conv_mimetypes", typ)) {
+		if ((name = (char *)zend_ini_string("mbstring.http_output_conv_mimetypes", sizeof("mbstring.http_output_conv_mimetypes"), 0)) != NULL) {
+			RETVAL_STRING(name, 1);
+		}
 	} else if (!strcasecmp("func_overload", typ)) {
  		RETVAL_LONG(MBSTRG(func_overload));
 	} else if (!strcasecmp("func_overload_list", typ)) {
