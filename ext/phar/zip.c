4c1775367bf254a64694f8cc2e8d1f49f0ee0b33
cellog <cellog@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-03-01 06:58:01 +0000

    update NEWS with phar enabling, fix swapping of time/date in timestamp saving for zip, which can cause crash on windows
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@276615 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/phar/zip.c
+++ ext/phar/zip.c
@@ -147,8 +147,8 @@ static void phar_zip_u2d_time(time_t time, char *dtime, char *ddate) /* {{{ */
 	struct tm *tm, tmbuf;
 
 	tm = php_localtime_r(&time, &tmbuf);
-	ctime = ((tm->tm_year+1900-1980)<<9) + ((tm->tm_mon+1)<<5) + tm->tm_mday;
-	cdate = ((tm->tm_hour)<<11) + ((tm->tm_min)<<5) + ((tm->tm_sec)>>1);
+	cdate = ((tm->tm_year+1900-1980)<<9) + ((tm->tm_mon+1)<<5) + tm->tm_mday;
+	ctime = ((tm->tm_hour)<<11) + ((tm->tm_min)<<5) + ((tm->tm_sec)>>1);
 	PHAR_SET_16(dtime, ctime);
 	PHAR_SET_16(ddate, cdate);
 }
