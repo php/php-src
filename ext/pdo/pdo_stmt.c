baa869110b0e50c85ea98c232f69f6ff2ca294ac
Andrei Zmievski <andrei@php.net>
2009-03-12 16:13:39 +0000

    MFH

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -2717,7 +2717,7 @@ static union _zend_function *row_get_ctor(zval *object TSRMLS_DC)
 
 static zend_class_entry *row_get_ce(zval *object TSRMLS_DC)
 {
-	return pdo_dbstmt_ce;
+	return pdo_row_ce;
 }
 
 static int row_get_classname(zval *object, char **class_name, zend_uint *class_name_len, int parent TSRMLS_DC)
