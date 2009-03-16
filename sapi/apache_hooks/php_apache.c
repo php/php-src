3612ddde1630f944c4d1328f717d3da1869048f3
Pierre Joye <pajoye@php.net>
2009-03-16 10:08:51 +0000

    - revert last commit (tree was not updated)

--- sapi/apache_hooks/php_apache.c
+++ sapi/apache_hooks/php_apache.c
@@ -713,23 +713,21 @@ PHP_FUNCTION(apache_request_server_port)
 PHP_FUNCTION(apache_request_remote_host)
 {
 	zval *id;
-	long ztype;
+	long type = 0;
 	request_rec *r;
 	char *res;
-	int type = REMOTE_NAME;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &ztype) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|l", &type) == FAILURE) {
 		return;
 	}
 
-	if (ZEND_NUM_ARGS() == 1) {
-		type = ztype;
+	if (!type) {
+		type = REMOTE_NAME
 	}
-
 
 	APREQ_GET_REQUEST(id, r);
 
-	res = (char *)ap_get_remote_host(r->connection, r->per_dir_config, type);
+	res = (char *)ap_get_remote_host(r->connection, r->per_dir_config, (int)type);
 
 	if (res) {
 		RETURN_STRING(res, 1);
