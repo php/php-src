1abe544f4147ad6ea2a3eec58aa88a9490db8324
Steph Fox <sfox@php.net>
2009-03-19 22:15:03 +0000

    - Fix bug #38805 ( PDO Truncates Text from SQL Server Text Data Type Field) - Bring behaviour into line with ext/mssql - Several memleaks plugged

--- ext/pdo_dblib/dblib_driver.c
+++ ext/pdo_dblib/dblib_driver.c
@@ -229,7 +229,11 @@ static int pdo_dblib_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 	if (H->link == NULL) {
 		goto cleanup;
 	}
-	
+
+	if (DBSETOPT(H->link, DBTEXTLIMIT, "2147483647") == FAIL) {
+		goto cleanup;
+	}
+
 	if (vars[3].optval && FAIL == dbuse(H->link, vars[3].optval)) {
 		goto cleanup;
 	}
