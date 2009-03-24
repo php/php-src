c15312c6419d32f85cbe97d15000fbae4f9667b8
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-03-24 01:57:53 +0000

    Fix bug #42362 - certain status codes never have output, so sending the gzip compression headers cause problems
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@277679 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -1034,7 +1034,7 @@ static void php_gzip_output_handler(char *output, uint output_len, char **handle
 {
 	zend_bool do_start, do_end;
 
-	if (!ZLIBG(output_compression)) {
+	if (!ZLIBG(output_compression) || SG(sapi_headers).http_response_code == 204 || SG(sapi_headers).http_response_code == 304) {
 		*handled_output = NULL;
 	} else {
 		do_start = (mode & PHP_OUTPUT_HANDLER_START ? 1 : 0);
