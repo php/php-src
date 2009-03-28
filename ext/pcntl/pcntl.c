724ae4b95e043563952267cd26b36f03470c35bf
Matteo Beccati <mbeccati@php.net>
2009-03-28 01:32:51 +0000

    MFH: - Fixed pcntl_signal() returning NULL when passing wrong parameters

--- ext/pcntl/pcntl.c
+++ ext/pcntl/pcntl.c
@@ -757,7 +757,7 @@ PHP_FUNCTION(pcntl_signal)
 	zend_bool restart_syscalls = 1;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "lz|b", &signo, &handle, &restart_syscalls) == FAILURE) {
-		return;
+		RETURN_FALSE;
 	}
 
 	if (!PCNTL_G(spares)) {
