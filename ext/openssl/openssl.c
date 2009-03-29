2c399c040eda9c361d596ee87ccabc9f1b31cc2c
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-03-29 23:32:17 +0000

    Fix bug #47828 - Converting to UTF-8 can sometimes fail, check error codes and avoid segfault.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@277996 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -557,10 +557,12 @@ static void add_ascii_assoc_name_entry(zval * val, char * key, X509_NAME * name,
 				str = X509_NAME_ENTRY_get_data(ne);
 				if (ASN1_STRING_type(str) != V_ASN1_UTF8STRING) {
 					to_add_len = ASN1_STRING_to_UTF8(&to_add, str);
-					add_next_index_utf8_stringl(subentries, (char *)to_add, to_add_len, 1);
 				} else {
 					to_add = ASN1_STRING_data(str);
 					to_add_len = ASN1_STRING_length(str);
+				}
+
+				if (to_add_len != -1) {
 					add_next_index_utf8_stringl(subentries, (char *)to_add, to_add_len, 1);
 				}
 			}
@@ -573,7 +575,7 @@ static void add_ascii_assoc_name_entry(zval * val, char * key, X509_NAME * name,
 		} else {
 			zval_dtor(subentries);
 			FREE_ZVAL(subentries);
-			if (obj_cnt && str) {
+			if (obj_cnt && str && to_add_len != -1) {
 				add_ascii_assoc_utf8_stringl(subitem, sname, (char *)to_add, to_add_len, 1);
 			}
 		}
