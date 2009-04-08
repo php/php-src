84b5243b8e17e64d5b43623547a38bd1392318bd
rasmus <rasmus@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-04-08 18:18:49 +0000

    Fixed bug #44929 - Better handling of leading zeros This is obviously not going to work for Unicode strings, but the whole function needs to be rewritten, so keep it in synch with the other branches for now.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@278454 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/strnatcmp.c
+++ ext/standard/strnatcmp.c
@@ -112,10 +112,10 @@ PHPAPI int strnatcmp_ex(char const *a, size_t a_len, char const *b, size_t b_len
 		ca = a[ai]; cb = b[bi];
 
 		/* skip over leading spaces or zeros */
-		while (isspace((int)(unsigned char)ca))
+		while (isspace((int)(unsigned char)ca) || (ca == '0' && ap+1 < aend))
 			ca = a[++ai];
 
-		while (isspace((int)(unsigned char)cb))
+		while (isspace((int)(unsigned char)cb) || (cb == '0' && bp+1 < bend))
 			cb = b[++bi];
 
 		/* process run of digits */
