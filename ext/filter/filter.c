507f21943553f16a8bc1862f0bc823a00efa07b1
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-04-14 14:18:14 +0000

    revert wrong fix for bug #47930
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@278689 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -138,7 +138,7 @@ zend_module_entry filter_module_entry = {
 	filter_functions,
 	PHP_MINIT(filter),
 	PHP_MSHUTDOWN(filter),
-	PHP_RINIT(filter),
+	NULL,
 	PHP_RSHUTDOWN(filter),
 	PHP_MINFO(filter),
 	"0.11.0",
@@ -282,20 +282,6 @@ PHP_MSHUTDOWN_FUNCTION(filter)
 {
 	UNREGISTER_INI_ENTRIES();
 
-	return SUCCESS;
-}
-/* }}} */
-
-/* {{{ PHP_RINIT_FUNCTION
- */
-PHP_RINIT_FUNCTION(filter)
-{
-	IF_G(get_array) = NULL;
-	IF_G(post_array) = NULL;
-	IF_G(cookie_array) = NULL;
-	IF_G(server_array) = NULL;
-	IF_G(env_array) = NULL;
-	IF_G(session_array) = NULL;
 	return SUCCESS;
 }
 /* }}} */
