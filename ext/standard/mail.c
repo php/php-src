0a685410559cfba63b80d83bf8e589ce8dd28c3b
Ilia Alshanetsky <iliaa@php.net>
2009-04-15 14:25:06 +0000

    MFB: Fixed bug #47969 (ezmlm_hash() returns different values depend on OS)

--- ext/standard/mail.c
+++ ext/standard/mail.c
@@ -73,7 +73,7 @@
 PHP_FUNCTION(ezmlm_hash)
 {
 	char *str = NULL;
-	unsigned long h = 5381L;
+	unsigned int h = 5381;
 	int j, str_len;
 	
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s",
