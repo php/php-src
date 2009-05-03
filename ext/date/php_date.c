6d32ad3c1f27b27e6ebad24289ac979d0319b04c
Derick Rethans <derick@php.net>
2009-05-03 18:22:59 +0000

    - MFH: Fixed bug #48058 (Year formatter goes wrong with out-of-int range). - MFH: Fixed bug #45822 (Near infinite-loops while parsing huge relative offsets).

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -787,7 +787,7 @@ static char *date_format(char *format, int format_len, timelib_time *t, int loca
 			/* year */
 			case 'L': length = slprintf(buffer, 32, "%d", timelib_is_leap((int) t->y)); break;
 			case 'y': length = slprintf(buffer, 32, "%02d", (int) t->y % 100); break;
-			case 'Y': length = slprintf(buffer, 32, "%s%04d", t->y < 0 ? "-" : "", abs((int) t->y)); break;
+			case 'Y': length = slprintf(buffer, 32, "%s%04ld", t->y < 0 ? "-" : "", llabs(t->y)); break;
 
 			/* time */
 			case 'a': length = slprintf(buffer, 32, "%s", t->h >= 12 ? "pm" : "am"); break;
