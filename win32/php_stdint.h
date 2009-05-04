50daa8c6d42da46c78d2a4e78b03366c08242bee
Pierre Joye <pajoye@php.net>
2009-05-04 17:10:26 +0000

    - add llabs

--- win32/php_stdint.h
+++ win32/php_stdint.h
@@ -264,6 +264,8 @@ typedef uint64_t  uintmax_t;
 #define INTMAX_C   INT64_C
 #define UINTMAX_C  UINT64_C
 
+static inline int64_t llabs( int64_t i ) { return i >= 0? i: -i; }
+
 #endif // __STDC_CONSTANT_MACROS ]
 
 
