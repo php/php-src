456f16ca123f2b5baf4f424053f36b3adbc3b7bd
tony2001 <tony2001@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-05-04 14:25:17 +0000

    MFH: don't segfault on bindto == NULL
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@279857 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/network.c
+++ main/network.c
@@ -792,7 +792,7 @@ php_socket_t php_network_connect_socket_to_host(const char *host, unsigned short
 		switch (sa->sa_family) {
 #if HAVE_GETADDRINFO && HAVE_IPV6
 			case AF_INET6:
-				if (strstr(bindto, ':')) {
+				if (bindto && strstr(bindto, ':')) {
 					((struct sockaddr_in6 *)sa)->sin6_family = sa->sa_family;
 					((struct sockaddr_in6 *)sa)->sin6_port = htons(port);
 					socklen = sizeof(struct sockaddr_in6);
