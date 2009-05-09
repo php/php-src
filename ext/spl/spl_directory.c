8f82daf428236b6d5d9e253f933e9af735e336e4
colder <colder@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-05-09 21:06:59 +0000

    MFH: Fix bug #47534 (RecursiveDirectoryIterator::getChildren ignoring CURRENT_AS_PATHNAME)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@280232 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -1268,24 +1268,27 @@ SPL_METHOD(RecursiveDirectoryIterator, getChildren)
 	
 	spl_filesystem_object_get_file_name(intern TSRMLS_CC);
 
-	INIT_PZVAL(&zflags);
-	INIT_PZVAL(&zpath);
-	ZVAL_LONG(&zflags, intern->flags);
-	ZVAL_STRINGL(&zpath, intern->file_name, intern->file_name_len, 0);
-
-	spl_instantiate_arg_ex2(Z_OBJCE_P(getThis()), &return_value, 0, &zpath, &zflags TSRMLS_CC);
-	
-	subdir = (spl_filesystem_object*)zend_object_store_get_object(return_value TSRMLS_CC);
-	if (subdir) {
-		if (intern->u.dir.sub_path && intern->u.dir.sub_path[0]) {
-			subdir->u.dir.sub_path_len = spprintf(&subdir->u.dir.sub_path, 0, "%s%c%s", intern->u.dir.sub_path, slash, intern->u.dir.entry.d_name);
-		} else {
-			subdir->u.dir.sub_path_len = strlen(intern->u.dir.entry.d_name);
-			subdir->u.dir.sub_path = estrndup(intern->u.dir.entry.d_name, subdir->u.dir.sub_path_len);
+	if (SPL_HAS_FLAG(intern->flags, SPL_FILE_DIR_CURRENT_AS_PATHNAME)) {
+		RETURN_STRINGL(intern->file_name, intern->file_name_len, 1);
+	} else {
+		INIT_PZVAL(&zflags);
+		INIT_PZVAL(&zpath);
+		ZVAL_LONG(&zflags, intern->flags);
+		ZVAL_STRINGL(&zpath, intern->file_name, intern->file_name_len, 0);
+		spl_instantiate_arg_ex2(Z_OBJCE_P(getThis()), &return_value, 0, &zpath, &zflags TSRMLS_CC);
+		
+		subdir = (spl_filesystem_object*)zend_object_store_get_object(return_value TSRMLS_CC);
+		if (subdir) {
+			if (intern->u.dir.sub_path && intern->u.dir.sub_path[0]) {
+				subdir->u.dir.sub_path_len = spprintf(&subdir->u.dir.sub_path, 0, "%s%c%s", intern->u.dir.sub_path, slash, intern->u.dir.entry.d_name);
+			} else {
+				subdir->u.dir.sub_path_len = strlen(intern->u.dir.entry.d_name);
+				subdir->u.dir.sub_path = estrndup(intern->u.dir.entry.d_name, subdir->u.dir.sub_path_len);
+			}
+			subdir->info_class = intern->info_class;
+			subdir->file_class = intern->file_class;
+			subdir->oth = intern->oth;
 		}
-		subdir->info_class = intern->info_class;
-		subdir->file_class = intern->file_class;
-		subdir->oth = intern->oth;
 	}
 }
 /* }}} */
