5150d15eed9852dfd58c543865b2eb8d7a4ead1e
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-05-12 12:34:55 +0000

    Fixed bug #47564 (unpacking unsigned long 32bit bit endian returns wrong result)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@280377 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/pack.c
+++ ext/standard/pack.c
@@ -784,6 +784,13 @@ PHP_FUNCTION(unpack)
 						}
 
 						v |= php_unpack(&input[inputpos], 4, issigned, map);
+						if (sizeof(long) > 4) {
+ 							if (type == 'l') {
+								v = (signed int) v; 
+							} else {
+								v = (unsigned int) v;
+							}
+						}
 						add_assoc_long(return_value, n, v);
 						break;
 					}
