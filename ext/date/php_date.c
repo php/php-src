71b329c7037552a7adb45adf08a868cf94c3d0e7
Jani Taskinen <jani@php.net>
2009-05-14 18:34:27 +0000

    MFH: Fixed module loading order and made request shutdown functions also to      be loaded in reverse like all other shutdowns are.

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -18,6 +18,8 @@
 
 /* $Id$ */
 
+#define _ISOC9X_SOURCE
+
 #include "php.h"
 #include "php_streams.h"
 #include "php_main.h"
@@ -316,17 +318,11 @@ static zend_object_value date_object_clone_date(zval *this_ptr TSRMLS_DC);
 static int date_object_compare_date(zval *d1, zval *d2 TSRMLS_DC);
 static zend_object_value date_object_clone_timezone(zval *this_ptr TSRMLS_DC);
 
-/* This is need to ensure that session extension request shutdown occurs 1st, because it uses the date extension */ 
-static zend_module_dep date_deps[] = {
-        ZEND_MOD_OPTIONAL("session")
-        {NULL, NULL, NULL}
-};
-
 /* {{{ Module struct */
 zend_module_entry date_module_entry = {
 	STANDARD_MODULE_HEADER_EX,
 	NULL,
-	date_deps,
+	NULL,
 	"date",                     /* extension name */
 	date_functions,             /* function list */
 	PHP_MINIT(date),            /* process startup */
