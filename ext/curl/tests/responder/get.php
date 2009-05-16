30ef00c15cc82af5d8a6b166a0adb04733a71bcf
Felipe Pena <felipe@php.net>
2009-05-16 17:08:10 +0000

    - Added responder dir (synced)

---
+++ ext/curl/tests/responder/get.php
@@ -0,0 +1,30 @@
+<?php
+  switch($_GET['test']) {
+    case 'getpost':
+      var_dump($_GET);
+      var_dump($_POST);
+      break;
+    case 'referer':
+      echo $_SERVER['HTTP_REFERER'];
+      break;
+    case 'useragent':
+      echo $_SERVER['HTTP_USER_AGENT'];
+      break;
+    case 'httpversion':
+      echo $_SERVER['SERVER_PROTOCOL'];
+      break;
+    case 'cookie':
+      echo $_COOKIE['foo'];
+      break;
+    case 'encoding':
+      echo $_SERVER['HTTP_ACCEPT_ENCODING'];  
+      break;
+    case 'contenttype':
+      header('Content-Type: text/plain;charset=utf-8');
+      break;
+    default:
+      echo "Hello World!\n";
+      echo "Hello World!";
+      break;
+  }
+?>
