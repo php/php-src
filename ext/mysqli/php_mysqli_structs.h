0ebb041393b354d7cab016854fadb0aa39fee540
Andrey Hristov <andrey@php.net>
2009-05-27 19:56:47 +0000

    because we use int64_t we need corresponding printf modifiers. the type can be different on 32 and 64 bit, thus we can't use ld and lld but C helps us with inttypes.h and PRId64 and PRIu64 modifiers (without the %)

--- ext/mysqli/php_mysqli_structs.h
+++ ext/mysqli/php_mysqli_structs.h
@@ -173,10 +173,12 @@ typedef __int64 my_longlong;
 # else
 #  define PHP_MYSQLI_API
 # endif
-#define MYSQLI_LLU_SPEC "%llu"
-#define MYSQLI_LL_SPEC "%lld"
+/* we need this for PRIu64 and PRId64 */
+#include <inttypes.h>
+#define MYSQLI_LLU_SPEC "%" PRIu64
+#define MYSQLI_LL_SPEC "%" PRId64
 #define L64(x) x##LL
-typedef long long my_longlong;
+typedef int64_t my_longlong;
 #endif
 
 #ifdef ZTS
