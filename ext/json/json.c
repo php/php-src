e636ed5789563eb4cfa83e2f7114b36519702408
Jani Taskinen <jani@php.net>
2009-05-31 13:51:23 +0000

    MFH: no dots in errors

--- ext/json/json.c
+++ ext/json/json.c
@@ -371,7 +371,7 @@ PHPAPI void php_json_encode(smart_str *buf, zval *val TSRMLS_DC) /* {{{ */
 			smart_str_appendl(buf, d, len);
 			efree(d);
                 } else {
-                    zend_error(E_WARNING, "[json] (php_json_encode) double %.9g does not conform to the JSON spec, encoded as 0.", dbl);
+                    zend_error(E_WARNING, "[json] (php_json_encode) double %.9g does not conform to the JSON spec, encoded as 0", dbl);
                     smart_str_appendc(buf, '0');
                 }
             }
@@ -384,7 +384,7 @@ PHPAPI void php_json_encode(smart_str *buf, zval *val TSRMLS_DC) /* {{{ */
             json_encode_array(buf, &val TSRMLS_CC);
             break;
         default:
-            zend_error(E_WARNING, "[json] (php_json_encode) type is unsupported, encoded as null.");
+            zend_error(E_WARNING, "[json] (php_json_encode) type is unsupported, encoded as null");
             smart_str_appendl(buf, "null", 4);
             break;
     }
