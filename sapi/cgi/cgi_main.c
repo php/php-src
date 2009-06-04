bf3fa507ad4cfc5d986cb3f8ff123143dd031796
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-04 11:51:43 +0000

    MFH:- Fixed bug #48419 (non-portable STDIN fileno in cgi_main.c)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@281645 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -481,6 +481,9 @@ static int sapi_cgi_send_headers(sapi_headers_struct *sapi_headers TSRMLS_DC)
 	return SAPI_HEADER_SENT_SUCCESSFULLY;
 }
 
+#ifndef STDIN_FILENO
+# define STDIN_FILENO 0
+#endif
 
 static int sapi_cgi_read_post(char *buffer, uint count_bytes TSRMLS_DC)
 {
@@ -493,7 +496,7 @@ static int sapi_cgi_read_post(char *buffer, uint count_bytes TSRMLS_DC)
 			fcgi_request *request = (fcgi_request*) SG(server_context);
 			tmp_read_bytes = fcgi_read(request, buffer + read_bytes, count_bytes - read_bytes);
 		} else {
-			tmp_read_bytes = read(0, buffer + read_bytes, count_bytes - read_bytes);
+			tmp_read_bytes = read(STDIN_FILENO, buffer + read_bytes, count_bytes - read_bytes);
 		}
 		if (tmp_read_bytes <= 0) {
 			break;
