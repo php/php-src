8db405f75dc3d94317780908dd4be491414c5520
lbarnaud <lbarnaud@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-05 11:21:31 +0000

    MFH: better fix for #48409 , #48428 , #48228
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@281712 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -2288,7 +2288,7 @@ ZEND_VM_HELPER(zend_do_fcall_common_helper, ANY, ANY)
 		EG(called_scope) = EX(called_scope);
 	}
 
-	zend_ptr_stack_3_pop(&EG(arg_types_stack), (zend_alias*)&EX(called_scope), (zend_alias*)&EX(current_object), (zend_alias*)&EX(fbc));
+	zend_arg_types_stack_3_pop(&EG(arg_types_stack), &EX(called_scope), &EX(current_object), &EX(fbc));
 	EX(function_state).arguments = zend_vm_stack_push_args(opline->extended_value TSRMLS_CC);
 
 	if (EX(function_state).function->type == ZEND_INTERNAL_FUNCTION) {
@@ -4288,7 +4288,7 @@ ZEND_VM_HANDLER(149, ZEND_HANDLE_EXCEPTION, ANY, ANY)
 			zval_ptr_dtor(&EX(object));
 		}
 		EX(called_scope) = DECODE_CTOR(EX(called_scope));
-		zend_ptr_stack_2_pop(&EG(arg_types_stack), (zend_alias*)&EX(object), (zend_alias*)&EX(fbc));
+		zend_arg_types_stack_2_pop(&EG(arg_types_stack), &EX(object), &EX(fbc));
 	}
 
 	for (i=0; i<EX(op_array)->last_brk_cont; i++) {
