78276bdc7a03da221dcb5a409ed1664b10330835
patrickallaert <patrickallaert@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-07 13:06:03 +0000

    Fixed bug #48469 (ldap_get_entries() memory leaks on empty search results)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@281761 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -19,6 +19,7 @@
    |          Jani Taskinen  <sniper@iki.fi>                              |
    |          Stig Venaas    <venaas@uninett.no>                          |
    |          Doug Goldstein <cardoe@cardoe.com>                          |
+   |          Patrick Allaert <patrickallaert@php.net>                    |
    | PHP 4.0 updates:  Zeev Suraski <zeev@zend.com>                       |
    +----------------------------------------------------------------------+
  */
@@ -972,9 +973,6 @@ PHP_FUNCTION(ldap_get_entries)
 	ldap = ld->link;
 	num_entries = ldap_count_entries(ldap, ldap_result);
 
-	array_init(return_value);
-	add_assoc_long(return_value, "count", num_entries);
-
 	if (num_entries == 0) {
 		RETURN_NULL();
 	}
@@ -984,6 +982,9 @@ PHP_FUNCTION(ldap_get_entries)
 	if (ldap_result_entry == NULL) {
 		RETURN_FALSE;
 	}
+
+	array_init(return_value);
+	add_assoc_long(return_value, "count", num_entries);
 
 	while (ldap_result_entry != NULL) {
 
