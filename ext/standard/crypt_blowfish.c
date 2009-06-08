4cb1d57fd5ff231e24896d4b4a24a69c6cdc15d9
Pierre Joye <pajoye@php.net>
2009-06-08 08:44:15 +0000

    - sync with 1.0.2, improve perf x86-64 (patch by Solar Designer)

--- ext/standard/crypt_blowfish.c
+++ ext/standard/crypt_blowfish.c
@@ -54,7 +54,7 @@
 #ifdef __i386__
 #define BF_ASM				0
 #define BF_SCALE			1
-#elif defined(__alpha__) || defined(__hppa__)
+#elif defined(__x86_64__) || defined(__alpha__) || defined(__hppa__)
 #define BF_ASM				0
 #define BF_SCALE			1
 #else
