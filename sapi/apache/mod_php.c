632d56c47eb7b82fd4340aa8f25375c8d5a83a44
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-09 10:23:10 +0000

    - fix build (test before commit maybe?)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@281863 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/apache/mod_php.c
+++ sapi/apache/mod_php.c
@@ -257,7 +257,7 @@ static void sapi_apache_register_server_variables(zval *track_vars_array TSRMLS_
 	zval **path_translated;
 	HashTable *symbol_table;
 	unsigned int new_val_len;
-	UConveter *conv = ZEND_U_CONVERTER(UG(runtime_encoding_conv));
+	UConverter *conv = ZEND_U_CONVERTER(UG(runtime_encoding_conv));
 
 	for (i = 0; i < arr->nelts; i++) {
 		char *val;
