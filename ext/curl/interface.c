d4d49f4f12faa4ad8f7ea8ed681e9b76ed34d051
Antony Dovgal <tony2001@php.net>
2009-06-10 11:10:19 +0000

    MFH: fix arginfo for curl_multi_info_read()

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -295,7 +295,7 @@ ZEND_END_ARG_INFO()
 static
 ZEND_BEGIN_ARG_INFO_EX(arginfo_curl_multi_info_read, 0, 0, 1)
 	ZEND_ARG_INFO(0, mh)
-	ZEND_ARG_INFO(0, msgs_in_queue)
+	ZEND_ARG_INFO(1, msgs_in_queue)
 ZEND_END_ARG_INFO()
 
 static
