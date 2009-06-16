6e42b6c8fcfc71b4392265f37883b0a2bf700fa0
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-16 02:54:26 +0000

    - MFH: Fixed bug #48562 (Reference recursion causes segfault when used in wddx_serialize_vars())
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@282200 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/wddx/wddx.c
+++ ext/wddx/wddx.c
@@ -704,13 +704,27 @@ static void php_wddx_add_var(wddx_packet *packet, zval *name_var)
 			php_wddx_serialize_var(packet, *val, Z_STRVAL_P(name_var), Z_STRLEN_P(name_var) TSRMLS_CC);
 		}		
 	} else if (Z_TYPE_P(name_var) == IS_ARRAY || Z_TYPE_P(name_var) == IS_OBJECT)	{
+		int is_array = Z_TYPE_P(name_var) == IS_ARRAY;
+		
 		target_hash = HASH_OF(name_var);
 		
+		if (is_array && target_hash->nApplyCount > 1) {
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "recursion detected");
+			return;
+		}
+
 		zend_hash_internal_pointer_reset(target_hash);
 
 		while(zend_hash_get_current_data(target_hash, (void**)&val) == SUCCESS) {
+			if (is_array) {
+				target_hash->nApplyCount++;
+			}
+
 			php_wddx_add_var(packet, *val);
-				
+
+			if (is_array) {
+				target_hash->nApplyCount--;
+			}
 			zend_hash_move_forward(target_hash);
 		}
 	}
