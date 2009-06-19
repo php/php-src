875c84384c30fdfe7150dae54db3bfe7faa37c53
kalle <kalle@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-19 22:15:28 +0000

    MFH: Fix memleak
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@282455 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -2010,7 +2010,6 @@ PHP_FUNCTION(imagegrabwindow)
 		pPrintWindow(window, memDC, (UINT) client_area);
 	} else {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Windows API too old");
-		RETURN_FALSE;
 		goto clean;
 	}
 
