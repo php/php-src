01c9097c74c0870ef23d8a0d34b105984f8ba292
Felipe Pena <felipe@php.net>
2009-06-22 00:03:24 +0000

    - Fixed bug #48629 (get_defined_constants() ignores categorize parameter)

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -1618,15 +1618,15 @@ ZEND_FUNCTION(get_loaded_extensions)
    Return an array containing the names and values of all defined constants */
 ZEND_FUNCTION(get_defined_constants)
 {
-	int argc = ZEND_NUM_ARGS();
+	zend_bool categorize = 0;
 
-	if (argc != 0 && argc != 1) {
-		ZEND_WRONG_PARAM_COUNT();
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|b", &categorize) == FAILURE) {
+		return;
 	}
 
 	array_init(return_value);
 
-	if (argc) {
+	if (categorize) {
 		HashPosition pos;
 		zend_constant *val;
 		int module_number;
