1eff0c751025b22a87071b1f3376511c41d57512
johannes <johannes@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-06-29 16:33:25 +0000

    Back to -dev
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@283042 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.3.0"
-#define PHP_VERSION_ID 50300
+#define PHP_RELEASE_VERSION 1
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.3.1-dev"
+#define PHP_VERSION_ID 50301
