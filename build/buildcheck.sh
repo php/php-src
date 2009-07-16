97d67cf57323e7860b23aeb825acdd93d133bdfb
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-07-16 12:10:38 +0000

    - Fix the cleanup routine (it is not very nice to delete everything..)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@284185 c90b9560-bf6c-de11-be94-00142212c4b1

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -48,9 +48,9 @@ fi
 
 if test "$1" = "2" && test "$2" -ge "50"; then
   echo "buildconf: Your version of autoconf likely contains buggy cache code."
-  echo "           Running svnclean for you."
+  echo "           Running vcsclean for you."
   echo "           To avoid this, install autoconf-2.13."
-  ./svnclean
+  ./vcsclean
   stamp=
 fi
 
