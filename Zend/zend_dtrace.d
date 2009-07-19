a114566d7428f8fb66c057379f01711dcc88f81f
dsp <dsp@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-07-19 15:57:06 +0000

    - Add license headers
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@284367 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_dtrace.d
+++ Zend/zend_dtrace.d
@@ -1,3 +1,20 @@
+/*
+   +----------------------------------------------------------------------+
+   | Zend Engine                                                          |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1998-2009 Zend Technologies Ltd. (http://www.zend.com) |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.00 of the Zend license,     |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@zend.com so we can mail you a copy immediately.              |
+   +----------------------------------------------------------------------+
+   | Authors: David Soria Parra <david.soriaparra@sun.com>                |
+   +----------------------------------------------------------------------+
+*/
 provider php {
 	probe exception__caught(char *classname);
 	probe exception__thrown(char* classname);
