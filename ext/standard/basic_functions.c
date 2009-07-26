ae492897c6603db9ba4efc3d7d5db005d9f34295
Jani Taskinen <jani@php.net>
2009-07-26 22:59:46 +0000

    - Removed unused code (replaced long time ago by url_scanner_ex.*

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -1,4 +1,3 @@
-
 /*
    +----------------------------------------------------------------------+
    | PHP Version 5                                                        |
@@ -3462,7 +3461,6 @@ static void basic_globals_ctor(php_basic_globals *basic_globals_p TSRMLS_DC) /* 
 	zend_hash_init(&BG(sm_protected_env_vars), 5, NULL, NULL, 1);
 	BG(sm_allowed_env_vars) = NULL;
 
-	memset(&BG(url_adapt_state), 0, sizeof(BG(url_adapt_state)));
 	memset(&BG(url_adapt_state_ex), 0, sizeof(BG(url_adapt_state_ex)));
 
 #if defined(_REENTRANT) && defined(HAVE_MBRLEN) && defined(HAVE_MBSTATE_T)
