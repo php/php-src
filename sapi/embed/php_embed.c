63530f0e89bbc4770f92ddfa4d77188060ad3dc0
Jani Taskinen <jani@php.net>
2009-07-28 21:07:43 +0000

    - Fixed bug #48911 (embed sapi misses SAPI_API)

--- sapi/embed/php_embed.c
+++ sapi/embed/php_embed.c
@@ -108,7 +108,7 @@ static int php_embed_startup(sapi_module_struct *sapi_module)
 	return SUCCESS;
 }
 
-sapi_module_struct php_embed_module = {
+extern SAPI_API sapi_module_struct php_embed_module = {
 	"embed",                       /* name */
 	"PHP Embedded Library",        /* pretty name */
 	
@@ -152,7 +152,7 @@ static const zend_function_entry additional_functions[] = {
 	{NULL, NULL, NULL}
 };
 
-int php_embed_init(int argc, char **argv PTSRMLS_DC)
+SAPI_API int php_embed_init(int argc, char **argv PTSRMLS_DC)
 {
 	zend_llist global_vars;
 #ifdef ZTS
@@ -217,7 +217,7 @@ int php_embed_init(int argc, char **argv PTSRMLS_DC)
   return SUCCESS;
 }
 
-void php_embed_shutdown(TSRMLS_D)
+SAPI_API void php_embed_shutdown(TSRMLS_D)
 {
 	php_request_shutdown((void *) 0);
 	php_module_shutdown(TSRMLS_C);
