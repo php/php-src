5d7c5eaee2b853dccbf51b89920d612418cb6ce2
Pierre Joye <pajoye@php.net>
2009-08-11 17:19:35 +0000

    - destroy the file entry stream first

--- ext/zip/zip_stream.c
+++ ext/zip/zip_stream.c
@@ -68,13 +68,13 @@ static int php_zip_ops_close(php_stream *stream, int close_handle TSRMLS_DC)
 {
 	STREAM_DATA_FROM_STREAM();
 	if (close_handle) {
-		if (self->za) {
-			zip_close(self->za);
-			self->za = NULL;
-		}
 		if (self->zf) {
 			zip_fclose(self->zf);
 			self->zf = NULL;
+		}
+		if (self->za) {
+			zip_close(self->za);
+			self->za = NULL;
 		}
 	}
 	efree(self);
