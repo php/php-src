c3e759372e9cdc0f18ef870b91720140c9753a22
Ilia Alshanetsky <iliaa@php.net>
2009-08-18 12:44:32 +0000

    NULL the value after free

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -221,6 +221,7 @@ static void _free_ldap_result_entry(zend_rsrc_list_entry *rsrc TSRMLS_DC)
 
 	if (entry->ber != NULL) {
 		ber_free(entry->ber, 0);
+		entry->ber = NULL;
 	}
 	zend_list_delete(entry->id);
 	efree(entry);
