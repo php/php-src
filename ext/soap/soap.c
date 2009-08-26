60ef4a1ef6b8d19642db101a4d86ecf76ba41dfe
Dmitry Stogov <dmitry@php.net>
2009-08-26 14:05:48 +0000

    Fixed bug #47273 (Encoding bug in SoapServer->fault)

--- ext/soap/soap.c
+++ ext/soap/soap.c
@@ -2128,8 +2128,13 @@ PHP_METHOD(SoapServer, fault)
 	char *code, *string, *actor=NULL, *name=NULL;
 	int code_len, string_len, actor_len = 0, name_len = 0;
 	zval* details = NULL;
+	soapServicePtr service;
+	xmlCharEncodingHandlerPtr old_encoding;
 
 	SOAP_SERVER_BEGIN_CODE();
+	FETCH_THIS_SERVICE(service);
+	old_encoding = SOAP_GLOBAL(encoding);
+	SOAP_GLOBAL(encoding) = service->encoding;
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss|szs",
 	    &code, &code_len, &string, &string_len, &actor, &actor_len, &details,
@@ -2138,6 +2143,8 @@ PHP_METHOD(SoapServer, fault)
 	}
 
 	soap_server_fault(code, string, actor, details, name TSRMLS_CC);
+
+	SOAP_GLOBAL(encoding) = old_encoding;
 	SOAP_SERVER_END_CODE();
 }
 /* }}} */
