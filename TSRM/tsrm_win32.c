a966ab428ac07726e6e6996e902364679ea81b77
Pierre Joye <pajoye@php.net>
2009-08-30 17:13:45 +0000

    - Fixed leak on error in popen/exec (and related functions)

--- TSRM/tsrm_win32.c
+++ TSRM/tsrm_win32.c
@@ -239,6 +239,7 @@ TSRM_API FILE *popen_ex(const char *command, const char *type, const char *cwd, 
 	cmd = (char*)malloc(strlen(command)+strlen(TWG(comspec))+sizeof(" /c "));
 	sprintf(cmd, "%s /c %s", TWG(comspec), command);
 	if (!CreateProcess(NULL, cmd, &security, &security, security.bInheritHandle, NORMAL_PRIORITY_CLASS|CREATE_NO_WINDOW, env, cwd, &startup, &process)) {
+		free(cmd);
 		return NULL;
 	}
 	free(cmd);
