e792ce39ed16b8ef225a1b5960303352505625c3
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-09-10 01:20:42 +0000

    - Possible fix for bug #49344 on Windows (pdo_mssql fails to connect,throws PDOException SQLSTATE[] (null) (severity 0))
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@288215 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/pdo_dblib/dblib_driver.c
+++ ext/pdo_dblib/dblib_driver.c
@@ -230,11 +230,11 @@ static int pdo_dblib_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 		goto cleanup;
 	}
 
-#if PHP_DBLIB_IS_MSSQL
-	if (DBSETOPT(H->link, DBTEXTLIMIT, "2147483647") == FAIL) {
-		goto cleanup;
-	}
-#endif
+	/* dblib do not return more than this length from text/image */
+	DBSETOPT(H->link, DBTEXTLIMIT, "2147483647");
+	
+	/* limit text/image from network */
+	DBSETOPT(H->link, DBTEXTSIZE, "2147483647");
 
 	if (vars[3].optval && FAIL == dbuse(H->link, vars[3].optval)) {
 		goto cleanup;
