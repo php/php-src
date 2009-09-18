14bd3cfe48e44448d975d88b947041a486273a46
bjori <bjori@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-09-18 23:06:21 +0000

    And now use proper colspanning
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@288448 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/credits.c
+++ ext/standard/credits.c
@@ -123,7 +123,7 @@ PHPAPI void php_print_credits(int flag TSRMLS_DC) /* {{{ */
 		/* Websites and infrastructure */
 
 		php_info_print_table_start();
-		php_info_print_table_header(1, "Websites and Infrastructure team");
+		php_info_print_table_colspan_header(2, "Websites and Infrastructure team");
 		/* www., wiki., windows., master., and others, I guess pecl. too? */
 		CREDIT_LINE("PHP Websites Team", "Rasmus Lerdorf, Hannes Magnusson, Philip Olson, Lukas Kahwe Smith, Pierre-Alain Joye, Kalle Sommer Nielsen");
 		CREDIT_LINE("Event Maintainers", "Damien Seguy, Daniel P. Brown");
