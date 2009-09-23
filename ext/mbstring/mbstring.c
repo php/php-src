php/php-src

commit 0f46648c2bfb40e47f92f874181745777c57c49d

Fixed possible memory corruption inside mb_strcut().
git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_1@202895 c90b9560-bf6c-de11-be94-00142212c4b1

ext/mbstring/mbstring.c
@@ -1844,6 +1844,13 @@ PHP_FUNCTION(mb_strcut)
 		}
 	}
 
+	if (from > Z_STRLEN_PP(arg1)) {
+		RETURN_FALSE;
+	}
+	if (((unsigned) from + (unsigned) len) > Z_STRLEN_PP(arg1)) {
+		len = Z_STRLEN_PP(arg1) - from;
+	}
+
 	ret = mbfl_strcut(&string, &result, from, len);
 	if (ret != NULL) {
 		RETVAL_STRINGL(ret->val, ret->len, 0);		/* the string is already strdup()'ed */

commit 0c974164e248c602988bc9d6725beb1a81658b50

- Fixed bug #49354 (mb_strcut() cuts wrong length when offset is within a   multibyte character).
(This bug was introduced by the commit by r202895. Please double-check the
 specification of the function you are going to *fix*.)

ext/mbstring/mbstring.c
@@ -2665,10 +2665,6 @@ PHP_FUNCTION(mb_strcut)
 		RETURN_FALSE;
 	}
 
-	if (((unsigned int)from + (unsigned int)len) > string.len) {
-		len = string.len - from;
-	}
-
 	ret = mbfl_strcut(&string, &result, from, len);
 	if (ret == NULL) {
 		RETURN_FALSE;

ext/mbstring/tests/bug49354.phpt
@@ -1,0 +1,21 @@
+--TEST--
+Bug #49354 (mb_strcut() cuts wrong length when offset is in the middle of a multibyte character)
+--SKIPIF--
+<?php extension_loaded('mbstring') or die('skip mbstring not available'); ?>
+--FILE--
+<?php
+$crap = 'AåBäCöDü';
+var_dump(mb_strcut($crap, 0, 100, 'UTF-8'));
+var_dump(mb_strcut($crap, 1, 100, 'UTF-8'));
+var_dump(mb_strcut($crap, 2, 100, 'UTF-8'));
+var_dump(mb_strcut($crap, 3, 100, 'UTF-8'));
+var_dump(mb_strcut($crap, 12, 100, 'UTF-8'));
+var_dump(mb_strcut($crap, 13, 100, 'UTF-8'));
+?>
+--EXPECT--
+string(12) "AåBäCöDü"
+string(11) "åBäCöDü"
+string(11) "åBäCöDü"
+string(9) "BäCöDü"
+string(0) ""
+bool(false)