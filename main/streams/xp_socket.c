efb20fd5c058072ccd4982cdfe7c67186dccde55
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-09-23 10:25:54 +0000

    Fixed ext/standard/tests/streams/stream_get_contents_002.phpt
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@288604 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/streams/xp_socket.c
+++ main/streams/xp_socket.c
@@ -289,7 +289,7 @@ static int php_sockop_set_option(php_stream *stream, int option, int value, void
 				if (sock->socket == -1) {
 					alive = 0;
 				} else if (php_pollfd_for(sock->socket, PHP_POLLREADABLE|POLLPRI, &tv) > 0) {
-					if (recv(sock->socket, &buf, sizeof(buf), MSG_PEEK) != SOCK_CONN_ERR && php_socket_errno() != EWOULDBLOCK) {
+					if (0 >= recv(sock->socket, &buf, sizeof(buf), MSG_PEEK) && php_socket_errno() != EWOULDBLOCK) {
 						alive = 0;
 					}
 				}
