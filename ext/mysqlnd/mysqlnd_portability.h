7cc4fbbfb7804dcba1cdccdfb06c975568492e47
jani <jani@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-09-24 18:01:17 +0000

    - Fix the build issue with php_mysqlnd_config.h ending up in wrong build directory
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@288676 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqlnd/mysqlnd_portability.h
+++ ext/mysqlnd/mysqlnd_portability.h
@@ -33,9 +33,9 @@ This file is public domain and comes with NO WARRANTY of any kind */
 #endif /* __CYGWIN__ */
 
 #if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(WIN32)
-#  include <ext/mysqlnd/config-win.h>
+#  include "ext/mysqlnd/config-win.h"
 #else 
-#  include "ext/mysqlnd/php_mysqlnd_config.h"
+#  include <ext/mysqlnd/php_mysqlnd_config.h>
 #endif /* _WIN32... */
 
 #ifdef HAVE_SYS_TYPES_H
