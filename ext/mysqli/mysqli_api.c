ef17ae4ecb55f18429ff502115ee1c9f6520722a
Andrey Hristov <andrey@php.net>
2009-09-25 12:35:58 +0000

    Fix returned value, if mysql_stmt_attr_set fails then mysqli returns true, otherwise false. Wrong logic.

--- ext/mysqli/mysqli_api.c
+++ ext/mysqli/mysqli_api.c
@@ -1848,14 +1848,13 @@ PHP_FUNCTION(mysqli_stmt_attr_set)
 	zval 	*mysql_stmt;
 	ulong   mode;
 	ulong	attr;
-	int		rc;
 
 	if (zend_parse_method_parameters(ZEND_NUM_ARGS() TSRMLS_CC, getThis(), "Oll", &mysql_stmt, mysqli_stmt_class_entry, &attr, &mode) == FAILURE) {
 		return;
 	}
 	MYSQLI_FETCH_RESOURCE(stmt, MY_STMT *, &mysql_stmt, "mysqli_stmt", MYSQLI_STATUS_VALID);
 
-	if ((rc = mysql_stmt_attr_set(stmt->stmt, attr, (void *)&mode))) {
+	if (FALSE == mysql_stmt_attr_set(stmt->stmt, attr, (void *)&mode)) {
 		RETURN_FALSE;
 	}
 	RETURN_TRUE;
