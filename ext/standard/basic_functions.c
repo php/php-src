7e583714fb403314eaf616331964451093528c9c
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-10-05 14:45:54 +0000

    Fixed bug #49757 (long2ip() can return wrong value in a multi-threaded applications). # original patch by Florian Anderiasch
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@289216 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/basic_functions.c
+++ ext/standard/basic_functions.c
@@ -3923,6 +3923,9 @@ PHP_FUNCTION(long2ip)
 	int ip_len;
 	unsigned long n;
 	struct in_addr myaddr;
+#ifdef HAVE_INET_PTON
+	char str[40];
+#endif
 
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &ip, &ip_len) == FAILURE) {
 		return;
@@ -3931,7 +3934,15 @@ PHP_FUNCTION(long2ip)
 	n = strtoul(ip, NULL, 0);
 
 	myaddr.s_addr = htonl(n);
+#ifdef HAVE_INET_PTON
+	if (inet_ntop(AF_INET, &myaddr, str, sizeof(str))) {
+		RETURN_RT_STRING(str, ZSTR_DUPLICATE);
+	} else {
+		RETURN_FALSE;
+	}
+#else
 	RETURN_RT_STRING(inet_ntoa(myaddr), ZSTR_DUPLICATE);
+#endif
 }
 /* }}} */
 
