a084ea2e787f353d469d46a399ebba3ef73713ae
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-10-08 20:40:48 +0000

    - typo
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@289368 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/enchant/enchant.c
+++ ext/enchant/enchant.c
@@ -290,7 +290,7 @@ PHP_MINIT_FUNCTION(enchant)
 	le_enchant_broker = zend_register_list_destructors_ex(php_enchant_broker_free, NULL, "enchant_broker", module_number);
 	le_enchant_dict = zend_register_list_destructors_ex(php_enchant_dict_free, NULL, "enchant_dict", module_number);
 	REGISTER_LONG_CONSTANT("ENCHANT_MYSPELL", PHP_ENCHANT_MYSPELL, CONST_CS | CONST_PERSISTENT);
-	REGISTER_LONG_CONSTANT("ENCHAT_ISPELL", PHP_ENCHANT_ISPELL, CONST_CS | CONST_PERSISTENT);
+	REGISTER_LONG_CONSTANT("ENCHANT_ISPELL", PHP_ENCHANT_ISPELL, CONST_CS | CONST_PERSISTENT);
 	return SUCCESS;
 }
 /* }}} */
