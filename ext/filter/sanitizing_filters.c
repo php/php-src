1a57cb01e66beb9986990d463404a7ce6cef3b4d
Pierre Joye <pajoye@php.net>
2009-10-09 17:50:17 +0000

    - Merge: Fixed bug #49470 (FILTER_SANITIZE_EMAIL allows disallowed characters)

--- ext/filter/sanitizing_filters.c
+++ ext/filter/sanitizing_filters.c
@@ -275,7 +275,7 @@ void php_filter_unsafe_raw(PHP_INPUT_FILTER_PARAM_DECL)
 void php_filter_email(PHP_INPUT_FILTER_PARAM_DECL)
 {
 	/* Check section 6 of rfc 822 http://www.faqs.org/rfcs/rfc822.html */
-	const unsigned char allowed_list[] = LOWALPHA HIALPHA DIGIT "!#$%&'*+-/=?^_`{|}~@.[]";
+	const unsigned char allowed_list[] = LOWALPHA HIALPHA DIGIT "!#$%&'*+-=?^_`{|}~@.[]";
 	filter_map     map;
 
 	filter_map_init(&map);
