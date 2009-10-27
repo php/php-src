7b6dce6a622d0e3c5b7ac0d6e963509d10da2041
Felipe Pena <felipe@php.net>
2009-10-27 16:21:03 +0000

    - Fix build

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -371,7 +371,6 @@ PHP_RINIT_FUNCTION(date)
 	}
 	DATEG(timezone) = NULL;
 	DATEG(tzcache) = NULL;
-	DATEG(last_errors) = NULL;
 
 	return SUCCESS;
 }
@@ -388,10 +387,6 @@ PHP_RSHUTDOWN_FUNCTION(date)
 		zend_hash_destroy(DATEG(tzcache));
 		FREE_HASHTABLE(DATEG(tzcache));
 		DATEG(tzcache) = NULL;
-	}
-	if (DATEG(last_errors)) {
-		timelib_error_container_dtor(DATEG(last_errors));
-		DATEG(last_errors) = NULL;
 	}
 
 	return SUCCESS;
