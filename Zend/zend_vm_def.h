37116c8f55755eb2d53be712603ed29e5ff220f3
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-11-01 21:26:03 +0000

    - Fixed bug #49908 (throwing exception in __autoload crashes when interface is not defined)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@290129 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -4453,11 +4453,12 @@ ZEND_VM_HANDLER(144, ZEND_ADD_INTERFACE, ANY, CONST)
 	zend_class_entry *ce = EX_T(opline->op1.u.var).class_entry;
 	zend_class_entry *iface = zend_u_fetch_class(Z_TYPE(opline->op2.u.constant), Z_UNIVAL(opline->op2.u.constant), Z_UNILEN(opline->op2.u.constant), opline->extended_value TSRMLS_CC);
 
-	if (!(iface->ce_flags & ZEND_ACC_INTERFACE)) {
-		zend_error_noreturn(E_ERROR, "%v cannot implement %v - it is not an interface", ce->name, iface->name);
+	if (iface) {
+		if (!(iface->ce_flags & ZEND_ACC_INTERFACE)) {
+			zend_error_noreturn(E_ERROR, "%v cannot implement %v - it is not an interface", ce->name, iface->name);
+		}
+		zend_do_implement_interface(ce, iface TSRMLS_CC);
 	}
-
-	zend_do_implement_interface(ce, iface TSRMLS_CC);
 
 	ZEND_VM_NEXT_OPCODE();
 }
