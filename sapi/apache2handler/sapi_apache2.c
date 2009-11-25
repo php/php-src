8c3509c8944f5c6e69bbb0bf7f5f3c4aa68b3ccc
Jani Taskinen <jani@php.net>
2009-11-25 12:35:42 +0000

    - Fixed bug #49677 (ini parser crashes with apache2 and using ${something} ini variables)

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -224,6 +224,10 @@ php_apache_sapi_getenv(char *name, size_t name_len TSRMLS_DC)
 	php_struct *ctx = SG(server_context);
 	const char *env_var;
 
+	if (ctx == NULL) {
+		return NULL;
+	}
+	
 	env_var = apr_table_get(ctx->r->subprocess_env, name);
 
 	return (char *) env_var;
