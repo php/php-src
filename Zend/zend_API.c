7ca2b629e89443bba7eb9c3e98d5845752b97874
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-11-30 12:27:18 +0000

    Reverted accidental change
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@291491 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2117,7 +2117,7 @@ void module_destructor(zend_module_entry *module) /* {{{ */
 #if HAVE_LIBDL
 #if !(defined(NETWARE) && defined(APACHE_1_BUILD))
 	if (module->handle) {
-//		DL_UNLOAD(module->handle);
+		DL_UNLOAD(module->handle);
 	}
 #endif
 #endif
