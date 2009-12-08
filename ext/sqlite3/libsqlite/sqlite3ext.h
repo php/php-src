8ed6440f24cd3947d0e6fff11f58b6f3e96d7c27
iliaa <iliaa@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-12-08 13:24:27 +0000

    Upgraded to Sqlite 3.6.21
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@291875 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/sqlite3/libsqlite/sqlite3ext.h
+++ ext/sqlite3/libsqlite/sqlite3ext.h
@@ -14,8 +14,6 @@
 ** an SQLite instance.  Shared libraries that intend to be loaded
 ** as extensions by SQLite should #include this file instead of 
 ** sqlite3.h.
-**
-** @(#) $Id$
 */
 #ifndef _SQLITE3EXT_H_
 #define _SQLITE3EXT_H_
