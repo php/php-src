cf9e91475405fca17f26e4ec810abbf47a54aec2
Felipe Pena <felipe@php.net>
2009-12-11 22:30:46 +0000

    - Fixed bug #50445 (PDO-ODBC stored procedure call from Solaris 64-bit causes seg fault).   (Original patch by davbrown4 at yahoo dot com)

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -1659,19 +1659,21 @@ static PHP_METHOD(PDOStatement, fetchAll)
 static int register_bound_param(INTERNAL_FUNCTION_PARAMETERS, pdo_stmt_t *stmt, int is_param) /* {{{ */
 {
 	struct pdo_bound_param_data param = {0};
+	long param_type = PDO_PARAM_STR;
 
 	param.paramno = -1;
-	param.param_type = PDO_PARAM_STR;
 
 	if (FAILURE == zend_parse_parameters_ex(ZEND_PARSE_PARAMS_QUIET, ZEND_NUM_ARGS() TSRMLS_CC,
-			"lz|llz!", &param.paramno, &param.parameter, &param.param_type, &param.max_value_len,
+			"lz|llz!", &param.paramno, &param.parameter, &param_type, &param.max_value_len,
 			&param.driver_params)) {
 		if (FAILURE == zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz|llz!", &param.name,
-				&param.namelen, &param.parameter, &param.param_type, &param.max_value_len, 
+				&param.namelen, &param.parameter, &param_type, &param.max_value_len, 
 				&param.driver_params)) {
 			return 0;
 		}	
 	}
+	
+	param.param_type = (int) param_type;
 
 	if (param.paramno > 0) {
 		--param.paramno; /* make it zero-based internally */
