3cc15c11a0e50010c79932eeb3faf92519bd3da7
Jérôme Loyet <fat@php.net>
2009-12-19 18:56:57 +0000

    typo

--- sapi/fpm/fpm/fpm.c
+++ sapi/fpm/fpm/fpm.c
@@ -29,7 +29,7 @@ int fpm_init(int argc, char **argv, char *config) /* {{{ */
 	fpm_globals.argv = argv;
 	fpm_globals.config = config;
 
-	if (0 > fpm_php_init_main()              ||
+	if (0 > fpm_php_init_main()            ||
 		0 > fpm_stdio_init_main()            ||
 		0 > fpm_conf_init_main()             ||
 		0 > fpm_unix_init_main()             ||
