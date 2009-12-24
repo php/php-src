29ac18dda227b0e28b030a6b65766e3ae3f09e60
Stanislav Malyshev <stas@php.net>
2009-12-24 00:15:13 +0000

    fix flags handling in zend_parse_method_parameters_ex

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -795,7 +795,7 @@ ZEND_API int zend_parse_method_parameters_ex(int flags, int num_args TSRMLS_DC, 
 
 	if (!this_ptr) {
 		va_start(va, type_spec);
-		retval = zend_parse_va_args(num_args, type_spec, &va, 0 TSRMLS_CC);
+		retval = zend_parse_va_args(num_args, type_spec, &va, flags TSRMLS_CC);
 		va_end(va);
 	} else {
 		p++;
