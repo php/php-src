a059cf4685f4878796337d1a0bd4056f03ea440c
Andrey Hristov <andrey@php.net>
2010-01-11 18:31:02 +0000

    missed to commit in 5_3, here it comes, reusability of stats

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 13
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.2.13-dev"
-#define PHP_VERSION_ID 50213
+#define PHP_RELEASE_VERSION 12
+#define PHP_EXTRA_VERSION "RC4-dev"
+#define PHP_VERSION "5.2.12RC4-dev"
+#define PHP_VERSION_ID 50212
