d8a32814f7cd18a86fb15a3c9c0a58047423f70a
Ilia Alshanetsky <iliaa@php.net>
2010-01-12 12:46:54 +0000

    Fixed bug #50728 (All PDOExceptions hardcode 'code' property to 0)

--- ext/pdo_mysql/mysql_driver.c
+++ ext/pdo_mysql/mysql_driver.c
@@ -106,7 +106,7 @@ int _pdo_mysql_error(pdo_dbh_t *dbh, pdo_stmt_t *stmt, const char *file, int lin
 #endif
 
 	if (!dbh->methods) {
-		zend_throw_exception_ex(php_pdo_get_exception(), 0 TSRMLS_CC, "SQLSTATE[%s] [%d] %s",
+		zend_throw_exception_ex(php_pdo_get_exception(), einfo->errcode TSRMLS_CC, "SQLSTATE[%s] [%d] %s",
 				*pdo_err, einfo->errcode, einfo->errmsg);
 	}
 /* printf("** [%s:%d] %s %s\n", file, line, *pdo_err, einfo->errmsg); */
