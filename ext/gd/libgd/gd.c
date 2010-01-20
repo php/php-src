bac747a67b336c9dc692a46b14f59301f30bef6d
tabe <tabe@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-01-20 09:39:06 +0000

    fix libgd FS100 (spurious horizontal line drawn by gdImageFilledPolygon)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@293762 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -2609,7 +2609,7 @@ void gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c)
 {
 	int i;
 	int y;
-	int miny, maxy;
+	int miny, maxy, pmaxy;
 	int x1, y1;
 	int x2, y2;
 	int ind1, ind2;
@@ -2653,7 +2653,7 @@ void gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c)
 			maxy = p[i].y;
 		}
 	}
-
+	pmaxy = maxy;
 	/* 2.0.16: Optimization by Ilia Chipitsine -- don't waste time offscreen */
 	if (miny < 0) {
 		miny = 0;
@@ -2695,13 +2695,13 @@ void gdImageFilledPolygon (gdImagePtr im, gdPointPtr p, int n, int c)
 			 */
 			if (y >= y1 && y < y2) {
 				im->polyInts[ints++] = (float) ((y - y1) * (x2 - x1)) / (float) (y2 - y1) + 0.5 + x1;
-			} else if (y == maxy && y > y1 && y <= y2) {
-				im->polyInts[ints++] = (float) ((y - y1) * (x2 - x1)) / (float) (y2 - y1) + 0.5 + x1;
+			} else if (y == pmaxy && y == y2) {
+				im->polyInts[ints++] = x2;
 			}
 		}
 		qsort(im->polyInts, ints, sizeof(int), gdCompareInt);
 
-		for (i = 0; i < ints; i += 2) {
+		for (i = 0; i < ints - 1; i += 2) {
 			gdImageLine(im, im->polyInts[i], y, im->polyInts[i + 1], y, fill_color);
 		}
 	}
