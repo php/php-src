61fbecfd55b5f064ff5c8bfc3696233d2c8604e0
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-01-21 18:07:28 +0000

    fix TS build
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@293813 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/intl/resourcebundle/resourcebundle_class.c
+++ ext/intl/resourcebundle/resourcebundle_class.c
@@ -86,7 +86,7 @@ static void resourcebundle_ctor(INTERNAL_FUNCTION_PARAMETERS) {
 	char *    pbuf;
 
 	zval                  *object = return_value;
-	ResourceBundle_object *rb = (ResourceBundle_object *) zend_object_store_get_object( object );
+	ResourceBundle_object *rb = (ResourceBundle_object *) zend_object_store_get_object( object TSRMLS_CC);
 
 	intl_error_reset( NULL TSRMLS_CC );
 
@@ -253,7 +253,7 @@ PHP_FUNCTION( resourcebundle_get )
 /* {{{ resourcebundle_array_count */
 int resourcebundle_array_count(zval *object, long *count TSRMLS_DC) 
 {
-	ResourceBundle_object *rb = (ResourceBundle_object *) zend_object_store_get_object( object );
+	ResourceBundle_object *rb = (ResourceBundle_object *) zend_object_store_get_object( object TSRMLS_CC);
 
 	*count = ures_getSize( rb->me );
 
