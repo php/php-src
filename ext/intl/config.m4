0e26c0d93e6033b5874ca97aa168d0dd77c8af70
Johannes Schlüter <johannes@php.net>
2010-01-27 17:29:38 +0000

    Merge r293114: add ICU resource bundle support, contributed by Hans-Peter Oeri (stas)

--- ext/intl/config.m4
+++ ext/intl/config.m4
@@ -52,6 +52,9 @@ if test "$PHP_INTL" != "no"; then
     msgformat/msgformat_parse.c \
     grapheme/grapheme_string.c \
     grapheme/grapheme_util.c \
+    resourcebundle/resourcebundle.c \
+    resourcebundle/resourcebundle_class.c \
+    resourcebundle/resourcebundle_iterator.c \
     idn/idn.c, $ext_shared,,$ICU_INCS)
 
   PHP_ADD_BUILD_DIR($ext_builddir/collator)
@@ -62,5 +65,6 @@ if test "$PHP_INTL" != "no"; then
   PHP_ADD_BUILD_DIR($ext_builddir/locale)
   PHP_ADD_BUILD_DIR($ext_builddir/msgformat)
   PHP_ADD_BUILD_DIR($ext_builddir/grapheme)
+  PHP_ADD_BUILD_DIR($ext_builddir/resourcebundle)
   PHP_ADD_BUILD_DIR($ext_builddir/idn)
 fi
---
+++ ext/intl/resourcebundle/resourcebundle_iterator.h
@@ -0,0 +1,36 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5                                                        |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Hans-Peter Oeri (University of St.Gallen) <hp@oeri.ch>      |
+   +----------------------------------------------------------------------+
+ */
+
+#ifndef RESOURCEBUNDLE_ITERATOR_H
+#define RESOURCEBUNDLE_ITERATOR_H
+
+#include <zend.h>
+
+#include "resourcebundle/resourcebundle_class.h"
+
+typedef struct {
+	zend_object_iterator  intern;
+	ResourceBundle_object *subject;
+	zend_bool             is_table;
+	long                  length;
+	zval                  *current;
+	char                  *currentkey;
+	long                  i;
+} ResourceBundle_iterator;
+
+zend_object_iterator *resourcebundle_get_iterator( zend_class_entry *ce, zval *object, int byref TSRMLS_DC );
+
+#endif // #ifndef RESOURCEBUNDLE_ITERATOR_H
