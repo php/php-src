00f07b92a9fb2dabb37f6f5b60a07ff2f7b6b17b
Ilia Alshanetsky <iliaa@php.net>
2010-03-02 12:54:23 +0000

    Fixed bug #51171 (curl_setopt() doesn't output any errors or warnings when an invalid option is provided)

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -2164,6 +2164,11 @@ PHP_FUNCTION(curl_setopt)
 
 	ZEND_FETCH_RESOURCE(ch, php_curl *, &zid, -1, le_curl_name, le_curl);
 
+	if (options <= 0) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Invalid curl configuration option");
+		RETURN_FALSE;
+	}
+
 	if (!_php_curl_setopt(ch, options, zvalue, return_value TSRMLS_CC)) {
 		RETURN_TRUE;
 	} else {
