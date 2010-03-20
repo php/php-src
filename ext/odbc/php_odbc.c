2aa4429d5e42d26f3a4af4883b1d321b93ff8935
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-03-20 18:19:16 +0000

    - Fixed bug #49429, #51331 (odbc_autocommit doesn't work)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/branches/PHP_5_3@296467 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/odbc/php_odbc.c
+++ ext/odbc/php_odbc.c
@@ -2864,7 +2864,7 @@ PHP_FUNCTION(odbc_autocommit)
 
 	ZEND_FETCH_RESOURCE2(conn, odbc_connection *, &pv_conn, -1, "ODBC-Link", le_conn, le_pconn);
 	
-	if (pv_onoff) {
+	if (ZEND_NUM_ARGS() > 1) {
 		rc = SQLSetConnectOption(conn->hdbc, SQL_AUTOCOMMIT, (pv_onoff) ? SQL_AUTOCOMMIT_ON : SQL_AUTOCOMMIT_OFF);
 		if (rc != SQL_SUCCESS && rc != SQL_SUCCESS_WITH_INFO) {
 			odbc_sql_error(conn, SQL_NULL_HSTMT, "Set autocommit");
