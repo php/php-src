b30e78b84cc31dba91a3249743b4aee2b56547cf
derick <derick@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-03-23 16:04:53 +0000

    - Set trunk version.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@296680 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 3
+#define PHP_RELEASE_VERSION 99
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.3.3-dev"
-#define PHP_VERSION_ID 50303
+#define PHP_VERSION "5.3.99-dev"
+#define PHP_VERSION_ID 50399
