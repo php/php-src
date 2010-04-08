e665f1596213f7c62f5e8653c186346afb4d5ff2
Antony Dovgal <tony2001@php.net>
2010-04-08 14:44:03 +0000

    output phpinfo() as text, no need for HTML here

--- sapi/fpm/fpm/fpm_main.c
+++ sapi/fpm/fpm/fpm_main.c
@@ -1562,6 +1562,7 @@ int main(int argc, char *argv[])
 
 	sapi_startup(&cgi_sapi_module);
 	cgi_sapi_module.php_ini_path_override = NULL;
+	cgi_sapi_module.phpinfo_as_text = 1;
 
 #ifdef PHP_WIN32
 	_fmode = _O_BINARY; /* sets default for file streams to binary */
