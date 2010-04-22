5234958f8a835587abea7abf7492eadcc5f75773
Felipe Pena <felipe@php.net>
2010-04-22 20:54:35 +0000

    - Fixed bug #51435 (Missing ifdefs / logic bug in crypt code cause compile errors)

--- ext/standard/crypt.c
+++ ext/standard/crypt.c
@@ -29,7 +29,7 @@
 #if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-#ifdef PHP_USE_PHP_CRYPT_R
+#if PHP_USE_PHP_CRYPT_R
 # include "php_crypt_r.h"
 # include "crypt_freesec.h"
 #else
@@ -116,7 +116,7 @@ PHP_MINIT_FUNCTION(crypt) /* {{{ */
    REGISTER_LONG_CONSTANT("CRYPT_SHA512", PHP_SHA512_CRYPT, CONST_CS | CONST_PERSISTENT);
 #endif
 
-#ifdef PHP_USE_PHP_CRYPT_R
+#if PHP_USE_PHP_CRYPT_R
 	php_init_crypt_r();
 #endif
 
@@ -126,7 +126,7 @@ PHP_MINIT_FUNCTION(crypt) /* {{{ */
 
 PHP_MSHUTDOWN_FUNCTION(crypt) /* {{{ */
 {
-#ifdef PHP_USE_PHP_CRYPT_R
+#if PHP_USE_PHP_CRYPT_R
 	php_shutdown_crypt_r();
 #endif
 
