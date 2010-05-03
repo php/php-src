11040eae16e5090f240a533813cb6490684a0604
Pierre Joye <pajoye@php.net>
2010-05-03 19:51:59 +0000

    - revert fix for #51723 in 5.2, vc6 does not support strtoi64, use builtin functioon in 5.3 (trunk is vc9+ only)

--- sapi/apache2handler/sapi_apache2.c
+++ sapi/apache2handler/sapi_apache2.c
@@ -53,6 +53,10 @@
 
 #include "php_apache.h"
 
+#if _MSC_VER <= 1300
+# include "win32/php_strtoi64.h"
+#endif
+
 /* UnixWare and Netware define shutdown to _shutdown, which causes problems later
  * on when using a structure member named shutdown. Since this source
  * file does not use the system call shutdown, it is safe to #undef it.K
