0bb08c61f8a5d7f4f194c58165e90f2fb9d7abd7
Michael Wallner <mike@php.net>
2010-05-12 09:37:25 +0000

    Fixed bug #51725 (xmlrpc_get_type() returns true on invalid dates). (Mike)

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -1361,10 +1361,16 @@ PHPAPI void php_date_set_tzdb(timelib_tzdb *tzdb)
 PHPAPI signed long php_parse_date(char *string, signed long *now)
 {
 	timelib_time *parsed_time;
+	timelib_error_container *error = NULL;
 	int           error2;
 	signed long   retval;
 
-	parsed_time = timelib_strtotime(string, strlen(string), NULL, DATE_TIMEZONEDB);
+	parsed_time = timelib_strtotime(string, strlen(string), &error, DATE_TIMEZONEDB);
+	if (error->error_count) {
+		timelib_error_container_dtor(error);
+		return -1;
+	}
+	timelib_error_container_dtor(error);
 	timelib_update_ts(parsed_time, NULL);
 	retval = timelib_date_to_int(parsed_time, &error2);
 	timelib_time_dtor(parsed_time);
