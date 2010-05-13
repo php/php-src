76fd19998d97ec760efae805144de033242cff9e
Andrey Hristov <andrey@php.net>
2010-05-13 11:06:00 +0000

    Somehow missed to commit this, for better fix for #51605

--- ext/mysqli/php_mysqli_structs.h
+++ ext/mysqli/php_mysqli_structs.h
@@ -215,7 +215,7 @@ extern zend_class_entry *mysqli_exception_class_entry;
 extern int php_le_pmysqli(void);
 extern void php_mysqli_dtor_p_elements(void *data);
 
-extern void php_mysqli_close(MY_MYSQL * mysql, int close_type TSRMLS_DC);
+extern void php_mysqli_close(MY_MYSQL * mysql, int close_type, int resource_status TSRMLS_DC);
 
 
 #ifdef HAVE_SPL
