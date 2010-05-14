d0345dbbb3b2bd254adb8fa839e8f7717bfe95ba
andrey <andrey@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-05-14 16:42:17 +0000

    result_meta local storage
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@299389 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/mysqlnd/mysqlnd_result_meta.h
+++ ext/mysqlnd/mysqlnd_result_meta.h
@@ -25,6 +25,7 @@
 
 PHPAPI MYSQLND_RES_METADATA * mysqlnd_result_meta_init(unsigned int field_count, zend_bool persistent TSRMLS_DC);
 PHPAPI struct st_mysqlnd_res_meta_methods * mysqlnd_result_metadata_get_methods();
+PHPAPI void ** _mysqlnd_plugin_get_plugin_result_metadata_data(const MYSQLND_RES_METADATA * meta, unsigned int plugin_id TSRMLS_DC);
 
 #endif /* MYSQLND_RESULT_META_H */
 
