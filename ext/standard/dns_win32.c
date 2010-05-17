46c4029341326277d490f606fd668aaf970f00da
Pierre Joye <pajoye@php.net>
2010-05-17 18:55:35 +0000

    - #51844, checkdnsrr does not support types other than MX

--- ext/standard/dns_win32.c
+++ ext/standard/dns_win32.c
@@ -122,8 +122,10 @@ PHP_FUNCTION(dns_check_record)
 		}
 	}
 
-	status = DnsQuery_A(hostname, DNS_TYPE_MX, DNS_QUERY_STANDARD, NULL, &pResult, NULL);
+	status = DnsQuery_A(hostname, type, DNS_QUERY_STANDARD, NULL, &pResult, NULL);
 
+	
+	
 	if (status) {
 		RETURN_FALSE;
 	}
