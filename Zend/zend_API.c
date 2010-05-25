435aa876fc35545454680bfa7bd7b0a37ce1f7e5
Antony Dovgal <tony2001@php.net>
2010-05-25 20:16:14 +0000

    fix zend_std_get_static_property() in ZTS mode

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -1043,7 +1043,11 @@ ZEND_API void zend_update_class_constants(zend_class_entry *class_type TSRMLS_DC
 				) {
 					Z_ADDREF_PP(p);
 					Z_SET_ISREF_PP(p);
+#if ZTS
+					CG(static_members_table)[(zend_intptr_t)(class_type->static_members_table)][i] = *p;
+#else
 					class_type->static_members_table[i] = *p;
+#endif
 				} else {
 					zval *r;
 
@@ -1051,7 +1055,11 @@ ZEND_API void zend_update_class_constants(zend_class_entry *class_type TSRMLS_DC
 					*r = **p;
 					INIT_PZVAL(r);
 					zval_copy_ctor(r);
+#if ZTS
+					CG(static_members_table)[(zend_intptr_t)(class_type->static_members_table)][i] = r;
+#else
 					class_type->static_members_table[i] = r;
+#endif
 				}
 			}
 		}
