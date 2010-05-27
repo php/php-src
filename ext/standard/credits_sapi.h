b4cf36ee8bf3aaad978e1dd9cd8c5c055dec1f64
johannes <johannes@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-05-27 22:51:28 +0000

    Update credits
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@299884 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/credits_sapi.h
+++ ext/standard/credits_sapi.h
@@ -20,6 +20,7 @@ CREDIT_LINE("CGI / FastCGI", "Rasmus Lerdorf, Stig Bakken, Shane Caraveo, Dmitry
 CREDIT_LINE("CLI", "Edin Kadribasic, Marcus Boerger, Johannes Schlueter");
 CREDIT_LINE("Continuity", "Alex Leigh (based on nsapi code)");
 CREDIT_LINE("Embed", "Edin Kadribasic");
+CREDIT_LINE("FastCGI Process Manager", "Andrei Nigmatulin, dreamcat4, Antony Dovgal, Jerome Loyet");
 CREDIT_LINE("ISAPI", "Andi Gutmans, Zeev Suraski");
 CREDIT_LINE("litespeed", "George Wang");
 CREDIT_LINE("NSAPI", "Jayakumar Muthukumarasamy, Uwe Schindler");
