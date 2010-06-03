8454cca44c57980893cdf577ee49b57c4f495cf1
Felipe Pena <felipe@php.net>
2010-06-03 00:43:37 +0000

    - Fix memleak when passing a non string/object parameter

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -3002,7 +3002,7 @@ static void reflection_class_object_ctor(INTERNAL_FUNCTION_PARAMETERS, int is_ob
 			return;
 		}
 	} else {
-		if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z", &argument) == FAILURE) {
+		if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z/", &argument) == FAILURE) {
 			return;
 		}
 	}
