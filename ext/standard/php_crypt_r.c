33ac0d0e338e09907c708d1469a631d3881c67d5
Pierre Joye <pajoye@php.net>
2010-06-15 13:50:14 +0000

    - cleanup, no vc6 support in trunk

--- ext/standard/php_crypt_r.c
+++ ext/standard/php_crypt_r.c
@@ -218,9 +218,7 @@ char * php_md5_crypt_r(const char *pw, const char *salt, char *out) {
 	strcat_s(passwd, MD5_HASH_MAX_LEN, "$");
 #else
 	/* VC6 version doesn't have strcat_s or strncpy_s */
-	if (strncpy(passwd + MD5_MAGIC_LEN, sp, sl + 1) < sl) {
-		goto _destroyCtx1;
-	}
+	strncpy(passwd + MD5_MAGIC_LEN, sp, sl + 1);
 	strcat(passwd, "$");
 #endif
 	dwHashLen = 16;
