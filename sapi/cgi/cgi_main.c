2072fa9937e0057c32e86eaaef21bcec870dbf85
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-06-30 02:30:14 +0000

    - MF5.3: Reverted fix for bug #48930 (due binary compatibility breakage) # To commit a new common fix
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@300869 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -2079,7 +2079,6 @@ consult the installation file that came with this distribution, or visit \n\
 				}
 			}
 
-			CG(shebang_len) = 0;
 			if (CGIG(check_shebang_line) && file_handle.handle.fp && (file_handle.handle.fp != stdin)) {
 				/* #!php support */
 				c = fgetc(file_handle.handle.fp);
@@ -2094,7 +2093,6 @@ consult the installation file that came with this distribution, or visit \n\
 							fseek(file_handle.handle.fp, pos - 1, SEEK_SET);
 						}
 					}
-					CG(shebang_len) = ftell(file_handle.handle.fp);
 					CG(start_lineno) = 2;
 				} else {
 					rewind(file_handle.handle.fp);
