eea8fc6122d80179adbfd9f0a234d9cb23c7984e
Dmitry Stogov <dmitry@php.net>
2010-07-08 07:40:02 +0000

    Reduced overhead of request startup

--- ext/mbstring/mb_gpc.c
+++ ext/mbstring/mb_gpc.c
@@ -60,7 +60,7 @@ MBSTRING_API SAPI_TREAT_DATA_FUNC(mbstr_treat_data)
 	php_mb_encoding_handler_info_t info;
 
 	if (arg != PARSE_STRING) {
-		char *value = zend_ini_string("mbstring.internal_encoding", sizeof("mbstring.internal_encoding"), 0);
+		char *value = MBSTRG(internal_encoding_name);
 		_php_mb_ini_mbstring_internal_encoding_set(value, value ? strlen(value): 0 TSRMLS_CC);
 	}
 
