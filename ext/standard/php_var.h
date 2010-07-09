587ff8dda386d99d3688d5af831790f37bd2c195
scottmac <scottmac@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-07-09 21:11:37 +0000

    Fix a bug when var_export() causes a fatal error that could inadvertently display data due to flushing of the output buffer.
    Examples include, memory limit, execution time and recursion.
    
    
    
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@301143 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/php_var.h
+++ ext/standard/php_var.h
@@ -34,6 +34,8 @@ PHP_FUNCTION(memory_get_peak_usage);
 
 PHPAPI void php_var_dump(zval **struc, int level TSRMLS_DC);
 PHPAPI void php_var_export(zval **struc, int level TSRMLS_DC);
+PHPAPI void php_var_export_ex(zval **struc, int level, smart_str *buf TSRMLS_DC);
+
 PHPAPI void php_debug_zval_dump(zval **struc, int level TSRMLS_DC);
 
 typedef HashTable* php_serialize_data_t;
