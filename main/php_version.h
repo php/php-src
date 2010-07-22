60cda4261256335e22a791a8841e972d6b7474f1
Johannes Schlüter <johannes@php.net>
2010-07-22 11:06:58 +0000

    - back to -dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 14
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.2.14"
-#define PHP_VERSION_ID 50214
+#define PHP_RELEASE_VERSION 15
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.2.15-dev"
+#define PHP_VERSION_ID 50215
