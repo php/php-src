eea2de1da837712b4b9f34ff1d3b42e47162f7c6
dmitry <dmitry@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-08-16 09:20:46 +0000

    Bug #52361 (Throwing an exception in a destructor causes invalid catching)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@302323 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_objects.c
+++ Zend/zend_objects.c
@@ -117,15 +117,13 @@ ZEND_API void zend_objects_destroy_object(zend_object *object, zend_object_handl
 				zend_error(E_ERROR, "Attempt to destruct pending exception");
 			} else {
 				old_exception = EG(exception);
-				Z_ADDREF_P(old_exception);
+				EG(exception) = NULL;
 			}
 		}
-		zend_exception_save(TSRMLS_C);
 		zend_call_method_with_0_params(&obj, object->ce, &destructor, ZEND_DESTRUCTOR_FUNC_NAME, NULL);
-		zend_exception_restore(TSRMLS_C);
 		if (old_exception) {
 			if (EG(exception)) {
-				zval_ptr_dtor(&old_exception);				
+				zend_exception_set_previous(EG(exception), old_exception TSRMLS_CC);
 			} else {
 				EG(exception) = old_exception;
 			}
