d6e772a1d00463f309fa0f34add2ebe107520797
Dmitry Stogov <dmitry@php.net>
2010-08-18 11:59:30 +0000

    Fixed ZTS build

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -590,9 +590,11 @@ static char *sapi_cgi_read_cookies(TSRMLS_D)
 static void cgi_php_load_env_var(char *var, unsigned int var_len, char *val, unsigned int val_len, void *arg)
 {
 	zval *array_ptr = (zval*)arg;	
-	int filter_arg = (array_ptr == PG(http_globals)[TRACK_VARS_ENV])?PARSE_ENV:PARSE_SERVER;
+	int filter_arg;
 	unsigned int new_val_len;
+	TSRMLS_FETCH();
 
+	filter_arg = (array_ptr == PG(http_globals)[TRACK_VARS_ENV])?PARSE_ENV:PARSE_SERVER;
 	if (sapi_module.input_filter(filter_arg, var, &val, strlen(val), &new_val_len TSRMLS_CC)) {
 		php_register_variable_safe(var, val, new_val_len, array_ptr TSRMLS_CC);
 	}
