edb4351004e29cdc4fc3212daca146ddc9a4b0c7
Pierre Joye <pajoye@php.net>
2010-09-28 13:29:33 +0000

    - Fixed possible flaw in open_basedir (CVE-2010-3436)

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -250,8 +250,13 @@ PHPAPI int php_check_specific_open_basedir(const char *basedir, const char *path
 #else
 		if (strncmp(resolved_basedir, resolved_name, resolved_basedir_len) == 0) {
 #endif
-			/* File is in the right directory */
-			return 0;
+			if (resolved_name_len > resolved_basedir_len &&
+				resolved_name[resolved_basedir_len] != PHP_DIR_SEPARATOR) {
+				return -1;
+			} else {
+				/* File is in the right directory */
+				return 0;
+			}
 		} else {
 			/* /openbasedir/ and /openbasedir are the same directory */
 			if (resolved_basedir_len == (resolved_name_len + 1) && resolved_basedir[resolved_basedir_len - 1] == PHP_DIR_SEPARATOR) {
