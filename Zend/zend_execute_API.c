f4173a8ece11a20ac09dc68e7cee746f741750b2
Dmitry Stogov <dmitry@php.net>
2010-10-13 08:51:39 +0000

    Fixed bug #52939 (zend_call_function does not respect ZEND_SEND_PREFER_REF)

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -854,7 +854,8 @@ int zend_call_function(zend_fcall_info *fci, zend_fcall_info_cache *fci_cache TS
 			if (!PZVAL_IS_REF(*fci->params[i]) && Z_REFCOUNT_PP(fci->params[i]) > 1) {
 				zval *new_zval;
 
-				if (fci->no_separation) {
+				if (fci->no_separation &&
+				    !ARG_MAY_BE_SENT_BY_REF(EX(function_state).function, i + 1)) {
 					if(i) {
 						/* hack to clean up the stack */
 						zend_vm_stack_push_nocheck((void *) (zend_uintptr_t)i TSRMLS_CC);
