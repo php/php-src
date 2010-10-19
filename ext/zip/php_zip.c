6ab767262c0fc9269e56cf72746a353d7d4389a9
Pierre Joye <pajoye@php.net>
2010-10-19 09:55:36 +0000

    - fix Fixed NULL pointer dereference in ZipArchive::getArchiveComment, (CVE-2010-3709), report&patch from Maksymilian Arciemowicz

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -1961,6 +1961,9 @@ static ZIPARCHIVE_METHOD(getArchiveComment)
 	}
 
 	comment = zip_get_archive_comment(intern, &comment_len, (int)flags);
+	if(comment==NULL) {
+		RETURN_FALSE;
+	}
 	RETURN_STRINGL((char *)comment, (long)comment_len, 1);
 }
 /* }}} */
