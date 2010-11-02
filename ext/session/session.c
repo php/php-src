477b8a6664f7e5200cf8d5ae4f6733f6893e3d73
Felipe Pena <felipe@php.net>
2010-11-02 18:34:56 +0000

    - Fixed bug #53141 (autoload misbehaves if called from closing session)   patch by: ladislav at marek dot su

--- ext/session/session.c
+++ ext/session/session.c
@@ -2074,6 +2074,7 @@ static PHP_MINFO_FUNCTION(session) /* {{{ */
 
 static const zend_module_dep session_deps[] = { /* {{{ */
 	ZEND_MOD_OPTIONAL("hash")
+	ZEND_MOD_REQUIRED("spl")
 	{NULL, NULL, NULL}
 };
 /* }}} */
