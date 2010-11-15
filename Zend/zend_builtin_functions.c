924ac71daaaf04b869583166fa6eed409d8c6a30
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-11-15 17:06:27 +0000

    - Moved leak_variable() to zend_builtin_functions.c (Gustavo)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@305376 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -54,6 +54,7 @@ static ZEND_FUNCTION(function_exists);
 static ZEND_FUNCTION(class_alias);
 #if ZEND_DEBUG
 static ZEND_FUNCTION(leak);
+static ZEND_FUNCTION(leak_variable);
 #ifdef ZEND_TEST_EXCEPTIONS
 static ZEND_FUNCTION(crash);
 #endif
@@ -180,6 +181,13 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_class_alias, 0, 0, 2)
 	ZEND_ARG_INFO(0, autoload)
 ZEND_END_ARG_INFO()
 
+#if ZEND_DEBUG
+ZEND_BEGIN_ARG_INFO_EX(arginfo_leak_variable, 0, 0, 1)
+	ZEND_ARG_INFO(0, variable)
+	ZEND_ARG_INFO(0, leak_data)
+ZEND_END_ARG_INFO()
+#endif
+
 ZEND_BEGIN_ARG_INFO_EX(arginfo_trigger_error, 0, 0, 1)
 	ZEND_ARG_INFO(0, message)
 	ZEND_ARG_INFO(0, error_type)
@@ -245,6 +253,7 @@ static const zend_function_entry builtin_functions[] = { /* {{{ */
 	ZEND_FE(class_alias,		arginfo_class_alias)
 #if ZEND_DEBUG
 	ZEND_FE(leak,				NULL)
+	ZEND_FE(leak_variable,		arginfo_leak_variable)
 #ifdef ZEND_TEST_EXCEPTIONS
 	ZEND_FE(crash,				NULL)
 #endif
@@ -1364,6 +1373,28 @@ ZEND_FUNCTION(leak)
 	}
 
 	emalloc(leakbytes);
+}
+/* }}} */
+
+/* {{{ proto leak_variable(mixed variable [, bool leak_data]) */
+ZEND_FUNCTION(leak_variable)
+{
+	zval *zv;
+	zend_bool leak_data = 0;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z|b", &zv, &leak_data) == FAILURE) {
+		return;
+	}
+
+	if (!leak_data) {
+		zval_add_ref(&zv);
+	} else if (Z_TYPE_P(zv) == IS_RESOURCE) {
+		zend_list_addref(Z_RESVAL_P(zv));
+	} else if (Z_TYPE_P(zv) == IS_OBJECT) {
+		Z_OBJ_HANDLER_P(zv, add_ref)(zv TSRMLS_CC);
+	} else {
+		zend_error(E_WARNING, "Leaking non-zval data is only applicable to resources and objects");
+	}
 }
 /* }}} */
 
