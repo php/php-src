61924ece78a323bd8cc915f21affb04fceef0841
Pierre Joye <pajoye@php.net>
2010-11-30 11:04:06 +0000

    Fixed crash in zip extract method (possible CWE-170)

--- ext/zip/php_zip.c
+++ ext/zip/php_zip.c
@@ -413,6 +413,9 @@ static int php_zip_extract_file(struct zip * za, char *dest, char *file, int fil
 		return 0;
 	}
 	path_cleaned =  php_zip_make_relative_path(new_state.cwd, new_state.cwd_length);
+	if(!path_cleaned) {
+		return 0;
+	}
 	path_cleaned_len = strlen(path_cleaned);
 
 	if (path_cleaned_len >= MAXPATHLEN || zip_stat(za, file, 0, &sb) != 0) {
