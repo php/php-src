726db3f295d85496564581e76d4a78bc5acbc735
Ilia Alshanetsky <iliaa@php.net>
2010-12-10 12:50:25 +0000

    Fixed bug #53516 (Regression in open_basedir handling).

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -192,7 +192,7 @@ PHPAPI int php_check_specific_open_basedir(const char *basedir, const char *path
 		if (strncmp(resolved_basedir, resolved_name, resolved_basedir_len) == 0) {
 #endif
 			if (resolved_name_len > resolved_basedir_len &&
-				resolved_name[resolved_basedir_len] != PHP_DIR_SEPARATOR) {
+				resolved_name[resolved_basedir_len - 1] != PHP_DIR_SEPARATOR) {
 				return -1;
 			} else {
 				/* File is in the right directory */
