8f44d2ca36acd9bcd465ab56022c2ce2a8ab616e
Gustavo André dos Santos Lopes <cataphract@php.net>
2010-12-21 19:52:17 +0000

    - Revert of revision 306542, turns out the semantics of compare() are   different in SplMinHeap and SplMaxHeap.

--- ext/spl/spl_heap.c
+++ ext/spl/spl_heap.c
@@ -176,7 +176,7 @@ static int spl_ptr_heap_zval_min_cmp(spl_ptr_heap_element a, spl_ptr_heap_elemen
 		spl_heap_object *heap_object = (spl_heap_object*)zend_object_store_get_object((zval *)object TSRMLS_CC);
 		if (heap_object->fptr_cmp) {
 			long lval = 0;
-			if (spl_ptr_heap_cmp_cb_helper((zval *)object, heap_object, (zval *)b, (zval *)a, &lval TSRMLS_CC) == FAILURE) {
+			if (spl_ptr_heap_cmp_cb_helper((zval *)object, heap_object, (zval *)a, (zval *)b, &lval TSRMLS_CC) == FAILURE) {
 				/* exception or call failure */
 				return 0;
 			}
