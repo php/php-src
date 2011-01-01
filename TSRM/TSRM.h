0203cc3d446b659e477ae488eaf4780bf58384d5
Felipe Pena <felipe@php.net>
2011-01-01 02:17:06 +0000

    - Year++

--- TSRM/TSRM.h
+++ TSRM/TSRM.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | Thread Safe Resource Manager                                         |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1999-2010, Andi Gutmans, Sascha Schumann, Zeev Suraski |
+   | Copyright (c) 1999-2011, Andi Gutmans, Sascha Schumann, Zeev Suraski |
    | This source file is subject to the TSRM license, that is bundled     |
    | with this package in the file LICENSE                                |
    +----------------------------------------------------------------------+
