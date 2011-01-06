30e646497e17825e642863a54b6a26efe903372e
Ilia Alshanetsky <iliaa@php.net>
2011-01-06 23:01:19 +0000

    re-tag

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 18
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.2.18-dev"
-#define PHP_VERSION_ID 50218
+#define PHP_RELEASE_VERSION 17
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.2.17"
+#define PHP_VERSION_ID 50217
