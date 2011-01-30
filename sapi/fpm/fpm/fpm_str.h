d5c7adb4e532ee2e8d117636f9ad81c6ca6b968c
fat <fat@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-01-30 13:51:20 +0000

    - remove cpystrn from FPM and use strlcpy instead.
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@307861 c90b9560-bf6c-de11-be94-00142212c4b1

--- sapi/fpm/fpm/fpm_str.h
+++ sapi/fpm/fpm/fpm_str.h
@@ -5,29 +5,6 @@
 #ifndef FPM_STR_H
 #define FPM_STR_H 1
 
-static inline char *cpystrn(char *dst, const char *src, size_t dst_size) /* {{{ */
-{
-	char *d, *end;
-	
-	if (!dst_size) {
-		return dst;
-	}
-	
-	d = dst;
-	end = dst + dst_size - 1;
-	
-	for (; d < end; ++d, ++src) {
-		if (!(*d = *src)) {
-			return d;
-		}
-	}
-
-	*d = '\0';
-
-	return d;
-}
-/* }}} */
-
 static inline char *str_purify_filename(char *dst, char *src, size_t size) /* {{{ */
 {
 	char *d, *end;
