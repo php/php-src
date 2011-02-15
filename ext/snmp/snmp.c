c2f2cb9c6103f0397bfdd3be1b397f6c881e1388
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-02-15 00:07:26 +0000

    - Fixed crash on unserialize(serialize(new SNMP))
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@308346 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/snmp/snmp.c
+++ ext/snmp/snmp.c
@@ -2007,6 +2007,10 @@ static int php_snmp_read_info(php_snmp_object *snmp_object, zval **retval TSRMLS
 	MAKE_STD_ZVAL(*retval);
 	array_init(*retval);
 
+	if (snmp_object->session == NULL) {
+		return SUCCESS;
+	}
+		
 	MAKE_STD_ZVAL(val);
 	ZVAL_STRINGL(val, snmp_object->session->peername, strlen(snmp_object->session->peername), 1);
 	add_assoc_zval(*retval, "hostname", val);
