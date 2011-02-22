cb8d9f9c197b931cb4b541fa382a183e0b44b48d
Pierre Joye <pajoye@php.net>
2011-02-22 12:59:50 +0000

    - missing files

---
+++ sapi/cli/php_cli_readline.h
@@ -0,0 +1,25 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 5                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2011 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Marcus Boerger <helly@php.net>                               |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id: php_cli_readline.h 306939 2011-01-01 02:19:59Z felipe $ */
+
+#include "php.h"
+
+int cli_is_valid_code(char *code, int len, char **prompt TSRMLS_DC);
+
+char **cli_code_completion(const char *text, int start, int end);
