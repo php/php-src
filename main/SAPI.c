ee861b0cf7974ce2e0e3e3d30e540de4b12ee172
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-03-15 17:14:32 +0000

    - windows only
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@309262 c90b9560-bf6c-de11-be94-00142212c4b1

--- main/SAPI.c
+++ main/SAPI.c
@@ -80,7 +80,9 @@ SAPI_API void sapi_startup(sapi_module_struct *sf)
 
 #ifdef ZTS
 	ts_allocate_id(&sapi_globals_id, sizeof(sapi_globals_struct), (ts_allocate_ctor) sapi_globals_ctor, (ts_allocate_dtor) sapi_globals_dtor);
+# ifdef PHP_WIN32
 	_configthreadlocale(_ENABLE_PER_THREAD_LOCALE);
+# endif
 #else
 	sapi_globals_ctor(&sapi_globals);
 #endif
