bd80844e72740414853d594a2ec0a23f608ee1af
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-03-31 23:58:06 +0000

    - fix bug #54423, fix build in trunk, introduce by previous related commit
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@309861 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2210,7 +2210,7 @@ ZEND_API int zend_get_module_started(const char *module_name) /* {{{ */
 
 static int clean_module_class(const zend_class_entry **ce, int *module_number TSRMLS_DC) /* {{{ */
 {
-	if ((*ce)->type == ZEND_INTERNAL_CLASS && (*ce)->module->module_number == *module_number) {
+	if ((*ce)->type == ZEND_INTERNAL_CLASS && (*ce)->info.internal.module->module_number == *module_number) {
 		return ZEND_HASH_APPLY_REMOVE;
 	} else {
 		return ZEND_HASH_APPLY_KEEP;
