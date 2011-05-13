6ec23faa99eeb484189edc681693b08bc43d51d9
Scott MacVicar <scottmac@php.net>
2011-05-13 07:00:45 +0000

    Update bundled PCRE to 8.12

--- ext/pcre/pcrelib/pcre.h
+++ ext/pcre/pcrelib/pcre.h
@@ -42,9 +42,9 @@ POSSIBILITY OF SUCH DAMAGE.
 /* The current PCRE version information. */
 
 #define PCRE_MAJOR          8
-#define PCRE_MINOR          11
+#define PCRE_MINOR          12
 #define PCRE_PRERELEASE     
-#define PCRE_DATE           2010-12-10
+#define PCRE_DATE           2011-01-15
 
 /* When an application links to a PCRE DLL in Windows, the symbols that are
 imported have to be identified as such. When building PCRE, the appropriate
