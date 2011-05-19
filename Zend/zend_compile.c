e789683bb56e3dfa4d98b138b182c0be2dec1c67
felipe <felipe@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-05-19 23:20:47 +0000

    - Fixed bug #54804 (__halt_compiler and imported namespaces) (Pierrick)
    git-svn-id: http://svn.php.net/repository/php/php-src/php/php-src/trunk@311276 c90b9560-bf6c-de11-be94-00142212c4b1

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -5020,6 +5020,10 @@ void zend_do_halt_compiler_register(TSRMLS_D) /* {{{ */
 	zend_mangle_property_name(&name, &len, haltoff, sizeof(haltoff) - 1, cfilename, clen, 0);
 	zend_register_long_constant(name, len+1, zend_get_scanned_file_offset(TSRMLS_C), CONST_CS, 0 TSRMLS_CC);
 	pefree(name, 0);
+	
+	if (CG(in_namespace)) {
+		zend_do_end_namespace(TSRMLS_C);
+	}
 }
 /* }}} */
 
