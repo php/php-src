e9f4cfd6afae2efc6fe7bf9de5ba8944f3781c1f
Felipe Pena <felipe@php.net>
2011-05-24 00:05:50 +0000

    - Fixed stack buffer overflow in socket_connect().   Found by: Mateusz Kocielski, Marek Kroemeke and Filip Palian

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -1336,6 +1336,11 @@ PHP_FUNCTION(socket_connect)
 			break;
 
 		case AF_UNIX:
+			if (addr_len >= sizeof(s_un.sun_path)) {
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, "Path too long", php_sock->type);
+				RETURN_FALSE;
+			}
+				
 			memset(&s_un, 0, sizeof(struct sockaddr_un));
 
 			s_un.sun_family = AF_UNIX;
