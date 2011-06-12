75fa63b57fc8df45e3b7b4ae5a1e50a169b171cd
Felipe Pena <felipe@php.net>
2011-06-12 15:14:18 +0000

    - Fixed bug #54939 (File path injection vulnerability in RFC1867 File upload filename)   Reported by: kkotowicz at gmail dot com

--- main/rfc1867.c
+++ main/rfc1867.c
@@ -1223,7 +1223,7 @@ filedone:
 #endif
 
 			if (!is_anonymous) {
-				if (s && s > filename) {
+				if (s && s >= filename) {
 					safe_php_register_variable(lbuf, s+1, strlen(s+1), NULL, 0 TSRMLS_CC);
 				} else {
 					safe_php_register_variable(lbuf, filename, strlen(filename), NULL, 0 TSRMLS_CC);
@@ -1236,7 +1236,7 @@ filedone:
 			} else {
 				snprintf(lbuf, llen, "%s[name]", param);
 			}
-			if (s && s > filename) {
+			if (s && s >= filename) {
 				register_http_post_files_variable(lbuf, s+1, http_post_files, 0 TSRMLS_CC);
 			} else {
 				register_http_post_files_variable(lbuf, filename, http_post_files, 0 TSRMLS_CC);
