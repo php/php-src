6481440f3fe85ccbb8988d7edddb3fcadb0ee7c9
Felipe Pena <felipe@php.net>
2011-06-18 17:41:56 +0000

    - Fixed ZTS build

--- sapi/fpm/fpm/fpm_main.c
+++ sapi/fpm/fpm/fpm_main.c
@@ -1854,7 +1854,7 @@ consult the installation file that came with this distribution, or visit \n\
 				goto fastcgi_request_done;
 			}
 
-			if (fpm_status_handle_request()) {
+			if (fpm_status_handle_request(TSRMLS_C)) {
 				goto fastcgi_request_done;
 			}
 
