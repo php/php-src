7b91a9778ac921667535604a33288e318076bc28
Felipe Pena <felipe@php.net>
2011-06-23 00:40:31 +0000

    - Fixed crash (take 2)

--- sapi/fpm/fpm/fpm_main.c
+++ sapi/fpm/fpm/fpm_main.c
@@ -1603,11 +1603,11 @@ int main(int argc, char *argv[])
 	tsrm_ls = ts_resource(0);
 #endif
 
-	fcgi_init();
-
 	sapi_startup(&cgi_sapi_module);
 	cgi_sapi_module.php_ini_path_override = NULL;
 	cgi_sapi_module.php_ini_ignore_cwd = 1;
+	
+	fcgi_init();
 
 #ifdef PHP_WIN32
 	_fmode = _O_BINARY; /* sets default for file streams to binary */
