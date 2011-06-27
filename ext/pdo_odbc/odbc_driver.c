9a38856c3ab6d4237a249b46b88f1a91a9b2b5cd
Felipe Pena <felipe@php.net>
2011-06-27 01:36:39 +0000

    - Fixed data type usage in 64bit   Reported by: Leonildo Costa

--- ext/pdo_odbc/odbc_driver.c
+++ ext/pdo_odbc/odbc_driver.c
@@ -224,7 +224,7 @@ static long odbc_handle_doer(pdo_dbh_t *dbh, const char *sql, long sql_len TSRML
 {
 	pdo_odbc_db_handle *H = (pdo_odbc_db_handle *)dbh->driver_data;
 	RETCODE rc;
-	long row_count = -1;
+	SQLLEN row_count = -1;
 	PDO_ODBC_HSTMT	stmt;
 	
 	rc = SQLAllocHandle(SQL_HANDLE_STMT, H->dbc, &stmt);
