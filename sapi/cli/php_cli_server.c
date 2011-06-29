cdad24cda58947161e2cb93b035264569e277f51
David Soria Parra <dsp@php.net>
2011-06-29 23:44:02 +0000

    correct error message for a missing document root in the cli webserver
    the document root is always a directory.

--- sapi/cli/php_cli_server.c
+++ sapi/cli/php_cli_server.c
@@ -2061,7 +2061,7 @@ int do_cli_server(int argc, char **argv TSRMLS_DC) /* {{{ */
 	if (document_root) {
 		struct stat sb;
 		if (stat(document_root, &sb)) {
-			fprintf(stderr, "Directory or script %s does not exist.\n", document_root);
+			fprintf(stderr, "Directory %s does not exist.\n", document_root);
 			return 1;
 		}
 		if (!S_ISDIR(sb.st_mode)) {
