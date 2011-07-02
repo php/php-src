2fb5b8200b898f577d3fee3924fa2fd1af3ca0e5
Jérôme Loyet <fat@php.net>
2011-07-02 16:10:18 +0000

    - Implemented FR #54577 (Enhanced status page with full status and details about each processes - Added a web page (status.html) for real-time FPM status - Fixed missing Expires and Cache-Control headers for ping and status pages

--- sapi/fpm/fpm/fpm_log.h
+++ sapi/fpm/fpm/fpm_log.h
@@ -6,7 +6,6 @@
 #define FPM_LOG_H 1
 #include "fpm_worker_pool.h"
 
-int fpm_log_init_main();
 int fpm_log_init_child(struct fpm_worker_pool_s *wp);
 int fpm_log_write(char *log_format TSRMLS_DC);
 int fpm_log_open(int reopen);
