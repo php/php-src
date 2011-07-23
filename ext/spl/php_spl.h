1fc4bc1d569864279058e40bdea8a47a51187da3
Stefan Marr <gron@php.net>
2011-07-23 19:08:43 +0000

    Added missing class_uses(..) function to SPL to mirror class_implements(..).
    # Was pointed out as missing in bug #55266.

--- ext/spl/php_spl.h
+++ ext/spl/php_spl.h
@@ -85,6 +85,7 @@ extern zend_spl_globals spl_globals;
 PHP_FUNCTION(spl_classes);
 PHP_FUNCTION(class_parents);
 PHP_FUNCTION(class_implements);
+PHP_FUNCTION(class_uses);
 
 PHPAPI void php_spl_object_hash(zval *obj, char* md5str TSRMLS_DC);
 
