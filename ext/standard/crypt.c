caf6a6dc7ac9264bdf3131635b9b57f34ddf4fad
Pierre Joye <pajoye@php.net>
2011-07-31 20:50:09 +0000

    - blowfish 1.2 update, 2nd part

--- ext/standard/crypt.c
+++ ext/standard/crypt.c
@@ -242,7 +242,7 @@ PHP_FUNCTION(crypt)
 		} else if (
 				salt[0] == '$' &&
 				salt[1] == '2' &&
-				salt[2] == 'a' &&
+				salt[2] >= 'a' && salt[2] <= 'z' &&
 				salt[3] == '$' &&
 				salt[4] >= '0' && salt[4] <= '3' &&
 				salt[5] >= '0' && salt[5] <= '9' &&
