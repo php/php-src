99a49480622ccebf4fea3119369f8131bd82ceeb
Rui Hirokawa <hirokawa@php.net>
2011-08-03 12:09:28 +0000

    MF PHP_5_4.

--- ext/mbstring/libmbfl/filters/mbfilter_sjis_mobile.h
+++ ext/mbstring/libmbfl/filters/mbfilter_sjis_mobile.h
@@ -60,5 +60,6 @@ extern const struct mbfl_convert_vtbl vtbl_wchar_sjis_sb_pua;
 
 int mbfl_filt_conv_sjis_mobile_wchar(int c, mbfl_convert_filter *filter);
 int mbfl_filt_conv_wchar_sjis_mobile(int c, mbfl_convert_filter *filter);
+int mbfl_filt_conv_sjis_mobile_flush(mbfl_convert_filter *filter);
 
 #endif /* MBFL_MBFILTER_SJIS_MOBILE_H */
