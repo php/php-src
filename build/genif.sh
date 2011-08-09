e34dc6c6f712d89f1a1c6c3b206f7d59de4c3048
Scott MacVicar <scottmac@php.net>
2011-08-09 18:12:06 +0000

    Fix building of pure C++ extensions as static

--- build/genif.sh
+++ build/genif.sh
@@ -24,7 +24,7 @@ cd $srcdir
 module_ptrs="$extra_module_ptrs`echo $@ | $awk -f ./build/order_by_dep.awk`"
 
 for ext in ${1+"$@"} ; do
-	header_list="$header_list ext/$ext/*.h"
+	header_list="$header_list ext/$ext/*.h*"
 done
 
 includes=`$awk -f ./build/print_include.awk $header_list`
