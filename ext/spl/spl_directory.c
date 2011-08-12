cf2db201238f801f9b2d3fd935f10cdfa201b9cb
Etienne Kneuss <colder@php.net>
2011-08-12 21:57:54 +0000

    Fix CID 547, make static analyzer happy

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -2431,7 +2431,9 @@ SPL_METHOD(SplFileObject, setFlags)
 {
 	spl_filesystem_object *intern = (spl_filesystem_object*)zend_object_store_get_object(getThis() TSRMLS_CC);
 
-	zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &intern->flags);
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &intern->flags) == FAILURE) {
+		return;
+	}
 } /* }}} */
 
 /* {{{ proto int SplFileObject::getFlags()
