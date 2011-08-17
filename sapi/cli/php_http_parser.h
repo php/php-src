cef071fd805ecc08fb1e676ec20df1d470d3be76
Pierre Joye <pajoye@php.net>
2011-08-17 11:56:04 +0000

    - use provided stdint.h and avoid double definitions

--- sapi/cli/php_http_parser.h
+++ sapi/cli/php_http_parser.h
@@ -28,17 +28,7 @@ extern "C" {
 
 #include <sys/types.h>
 #if defined(_WIN32) && !defined(__MINGW32__)
-typedef __int8 int8_t;
-typedef unsigned __int8 uint8_t;
-typedef __int16 int16_t;
-typedef unsigned __int16 uint16_t;
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
-
-typedef unsigned int size_t;
-typedef int ssize_t;
+#include "win32/php_stdint.h"
 #else
 #include <stdint.h>
 #endif
