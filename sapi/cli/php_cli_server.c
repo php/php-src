36bfb46ce66b5f7dbf3a52c379bac644ea012118
Xinchen Hui <laruence@php.net>
2011-08-19 08:35:43 +0000

    Fixed typo when syncing changes to branch

--- sapi/cli/php_cli_server.c
+++ sapi/cli/php_cli_server.c
@@ -1411,7 +1411,7 @@ static void php_cli_server_client_populate_request_info(const php_cli_server_cli
 	request_info->post_data = client->request.content;
 	request_info->content_length = request_info->post_data_length = client->request.content_len;
 	if (SUCCESS == zend_hash_find(&client->request.headers, "Content-Type", sizeof("Content-Type"), (void**)&val)) {
-		request_info->content_type = strtok(*val, delimiter);
+		request_info->content_type = *val;
 	}
 } /* }}} */
 
