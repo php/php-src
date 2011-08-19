5b74b328026d9dfcccded1339301a7a9a82df73d
pajoye <pajoye@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-08-19 09:59:47 +0000

    - Fixed bug #52461 (Incomplete doctype and missing xmlns)
    git-svn-id: http://svn.php.net/repository/php/php-src/trunk@315170 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -633,7 +633,7 @@ PHPAPI char *php_get_uname(char mode)
 PHPAPI void php_print_info_htmlhead(TSRMLS_D)
 {
 	php_info_print("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"DTD/xhtml1-transitional.dtd\">\n");
-	php_info_print("<html>");
+	php_info_print("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
 	php_info_print("<head>\n");
 	php_info_print_style(TSRMLS_C);
 	php_info_print("<title>phpinfo()</title>");
