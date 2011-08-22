5f502879ffd4ccbbd66ddb51884be73722d01feb
Johannes Schlüter <johannes@php.net>
2011-08-22 23:24:22 +0000

    Back to -dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 8
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.3.8"
-#define PHP_VERSION_ID 50308
+#define PHP_RELEASE_VERSION 9
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.3.9-dev"
+#define PHP_VERSION_ID 50309
