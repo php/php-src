a84c667e0e753d5d8051957de08fbf3efb08009a
Hannes Magnusson <bjori@php.net>
2011-08-30 13:41:57 +0000

    Fixed bug#48476

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -2047,6 +2047,9 @@ static zend_object_value date_object_clone_date(zval *this_ptr TSRMLS_DC)
 	zend_object_value new_ov = date_object_new_date_ex(old_obj->std.ce, &new_obj TSRMLS_CC);
 	
 	zend_objects_clone_members(&new_obj->std, new_ov, &old_obj->std, Z_OBJ_HANDLE_P(this_ptr) TSRMLS_CC);
+	if (!old_obj->time) {
+		return new_ov;
+	}
 	
 	/* this should probably moved to a new `timelib_time *timelime_time_clone(timelib_time *)` */
 	new_obj->time = timelib_time_ctor();
@@ -2168,6 +2171,10 @@ static zend_object_value date_object_clone_timezone(zval *this_ptr TSRMLS_DC)
 	zend_object_value new_ov = date_object_new_timezone_ex(old_obj->std.ce, &new_obj TSRMLS_CC);
 	
 	zend_objects_clone_members(&new_obj->std, new_ov, &old_obj->std, Z_OBJ_HANDLE_P(this_ptr) TSRMLS_CC);
+	if (!old_obj->initialized) {
+		return new_ov;
+	}
+	
 	new_obj->type = old_obj->type;
 	new_obj->initialized = 1;
 	switch (new_obj->type) {
