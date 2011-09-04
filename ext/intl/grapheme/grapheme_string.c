281ad87375b846c169659c2fcb39d233c6499e53
stas <stas@c90b9560-bf6c-de11-be94-00142212c4b1>
2011-09-04 18:50:05 +0000

    fix bug 55562 - make substr always return rest of the string if length is too long
    git-svn-id: http://svn.php.net/repository/php/php-src/trunk@316113 c90b9560-bf6c-de11-be94-00142212c4b1

--- ext/intl/grapheme/grapheme_string.c
+++ ext/intl/grapheme/grapheme_string.c
@@ -554,13 +554,17 @@ PHP_FUNCTION(grapheme_substr)
 		length += iter_val;
 	}
 
-	if ( UBRK_DONE == sub_str_end_pos && length < 0) {
+	if ( UBRK_DONE == sub_str_end_pos) {
+		if(length < 0) {
 
-		intl_error_set( NULL, U_ILLEGAL_ARGUMENT_ERROR, "grapheme_substr: length not contained in string", 1 TSRMLS_CC );
+			intl_error_set( NULL, U_ILLEGAL_ARGUMENT_ERROR, "grapheme_substr: length not contained in string", 1 TSRMLS_CC );
 
-		efree(ustr);
-		ubrk_close(bi);
-		RETURN_FALSE;
+			efree(ustr);
+			ubrk_close(bi);
+			RETURN_FALSE;
+		} else {
+			sub_str_end_pos = ustr_len;
+		}
 	}
 
 	sub_str = NULL;
