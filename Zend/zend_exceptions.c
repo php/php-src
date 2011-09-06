7f3ab56cecf29fa0063276a446ecdc4188ea9afa
Pierre Joye <pajoye@php.net>
2011-09-06 06:32:42 +0000

    - WS

--- Zend/zend_exceptions.c
+++ Zend/zend_exceptions.c
@@ -90,7 +90,6 @@ void zend_throw_exception_internal(zval *exception TSRMLS_DC) /* {{{ */
 
 		if (exception != NULL) {
 			zend_get_object_classname(exception, &classname, &name_len TSRMLS_CC);
-			
 			DTRACE_EXCEPTION_THROWN(classname);
 		} else {
 			DTRACE_EXCEPTION_THROWN(NULL);
