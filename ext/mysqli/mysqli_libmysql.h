fd4fc39dc93df679c48a5448259774b3c5618c86
Andrey Hristov <andrey@php.net>
2011-09-06 19:52:19 +0000

    Fix problems with the unbuffered set detection, for libmysql, which resulted in always true. Thanks Stas.

--- ext/mysqli/mysqli_libmysql.h
+++ ext/mysqli/mysqli_libmysql.h
@@ -29,8 +29,8 @@
 #define MYSQLND_OPT_NUMERIC_AND_DATETIME_AS_UNICODE	200
 #define MYSQLND_OPT_INT_AND_YEAR_AS_INT				201
 
-#define mysqli_result_is_unbuffered(r)		((r)->handle && (r)->handle->status == MYSQL_STATUS_USE_RESULT)
-#define mysqli_result_is_unbuffered_and_not_everything_is_fetched(r)		(((r)->handle && (r)->handle->status == MYSQL_STATUS_USE_RESULT) || ((r)->data == NULL))
+#define mysqli_result_is_unbuffered(r)		((r)->handle && (r)->handle->status == MYSQL_STATUS_USE_RESULT && (r)->data == NULL)
+#define mysqli_result_is_unbuffered_and_not_everything_is_fetched(r)   mysqli_result_is_unbuffered(r)
 #define mysqli_server_status(c)				(c)->server_status
 #define mysqli_stmt_get_id(s)				((s)->stmt_id)
 #define mysqli_stmt_warning_count(s)		mysql_warning_count((s)->mysql)
