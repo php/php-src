4ce7228b0084ce0604586771a6bef14fd9efa870
Pierre Joye <pajoye@php.net>
2011-09-12 12:59:52 +0000

    - revert incorrect fix introduced in r316528

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -1013,7 +1013,6 @@ PHPAPI char *php_pcre_replace_impl(pcre_cache_entry *pce, char *subject, int sub
 			return NULL;
 		}
 	} else {
-		convert_to_string_ex(&replace_val);
 		replace = Z_STRVAL_P(replace_val);
 		replace_len = Z_STRLEN_P(replace_val);
 		replace_end = replace + replace_len;
