3e14579486cc550bf1ec985abc80ed0c9904b431
Xinchen Hui <laruence@php.net>
2011-09-30 15:08:32 +0000

    A better fix for #55817 (thanks to bjori)

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -3190,7 +3190,7 @@ PHP_FUNCTION(mb_detect_encoding)
 	/* make encoding list */
 	list = NULL;
 	size = 0;
-	if (ZEND_NUM_ARGS() >= 2) {
+	if (ZEND_NUM_ARGS() >= 2 && !ZVAL_IS_NULL(encoding_list)) {
 		switch (Z_TYPE_P(encoding_list)) {
 		case IS_ARRAY:
 			if (FAILURE == php_mb_parse_encoding_array(encoding_list, &list, &size, 0 TSRMLS_CC)) {
