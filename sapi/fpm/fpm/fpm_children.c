dd243e5c752ae349baf7019228a07b8a43293c0c
Jérôme Loyet <fat@php.net>
2011-10-08 14:11:37 +0000

    ws

--- sapi/fpm/fpm/fpm_children.c
+++ sapi/fpm/fpm/fpm_children.c
@@ -344,6 +344,7 @@ static void fpm_child_resources_use(struct fpm_child_s *child) /* {{{ */
 		}
 		fpm_scoreboard_free(wp->scoreboard);
 	}
+
 	fpm_scoreboard_child_use(child->wp->scoreboard, child->scoreboard_i, getpid());
 	fpm_stdio_child_use_pipes(child);
 	fpm_child_free(child);
