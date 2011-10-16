05c5c8958ea59ea11f3709acab39503ba9674d0b
Stanislav Malyshev <stas@php.net>
2011-10-16 00:34:01 +0000

    Bug #55754 - Only variables should be passed by reference for ZEND_SEND_PREFER_REF params

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2487,7 +2487,7 @@ void zend_do_pass_param(znode *param, zend_uchar op, int offset TSRMLS_DC) /* {{
 	
 	if (function_ptr) {
 		if (ARG_MAY_BE_SENT_BY_REF(function_ptr, (zend_uint) offset)) {
-			if (param->op_type & (IS_VAR|IS_CV)) {
+			if (param->op_type & (IS_VAR|IS_CV) && original_op != ZEND_SEND_VAL) {
 				send_by_reference = 1;
 				if (op == ZEND_SEND_VAR && zend_is_function_or_method_call(param)) {
 					/* Method call */
