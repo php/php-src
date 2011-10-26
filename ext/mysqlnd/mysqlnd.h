7edb8a69b65c8332a793e8cd16ab3562a12aca0c
Andrey Hristov <andrey@php.net>
2011-10-26 09:00:32 +0000

    Bump internal version id after recent changes

--- ext/mysqlnd/mysqlnd.h
+++ ext/mysqlnd/mysqlnd.h
@@ -22,8 +22,8 @@
 #ifndef MYSQLND_H
 #define MYSQLND_H
 
-#define MYSQLND_VERSION "mysqlnd 5.0.9-dev - 20110325 - $Revision$"
-#define MYSQLND_VERSION_ID 50009
+#define MYSQLND_VERSION "mysqlnd 5.0.10-dev - 20111026 - $Revision$"
+#define MYSQLND_VERSION_ID 50010
 
 #define MYSQLND_PLUGIN_API_VERSION 1
 
