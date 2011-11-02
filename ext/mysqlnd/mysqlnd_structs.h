de50e98a07aa542ed29d1d558389bdbbf5cfeda9
Andrey Hristov <andrey@php.net>
2011-11-02 17:06:30 +0000

    Add a middleman which can intercept the calls and can reroute them

--- ext/mysqlnd/mysqlnd_structs.h
+++ ext/mysqlnd/mysqlnd_structs.h
@@ -550,12 +550,14 @@ struct st_mysqlnd_conn_data_methods
 };
 
 
+typedef enum_func_status	(*func_mysqlnd_data__connect)(MYSQLND * conn, const char * host, const char * user, const char * passwd, unsigned int passwd_len, const char * db, unsigned int db_len, unsigned int port, const char * socket_or_pipe, unsigned int mysql_flags TSRMLS_DC);
 typedef MYSQLND *			(*func_mysqlnd_conn__clone_object)(MYSQLND * const conn TSRMLS_DC);
 typedef void				(*func_mysqlnd_conn__dtor)(MYSQLND * conn TSRMLS_DC);
 typedef enum_func_status	(*func_mysqlnd_conn__close)(MYSQLND * conn, enum_connection_close_type close_type TSRMLS_DC);
 
 struct st_mysqlnd_conn_methods
 {
+	func_mysqlnd_data__connect connect;
 	func_mysqlnd_conn__clone_object clone_object;
 	func_mysqlnd_conn__dtor dtor;
 	func_mysqlnd_conn__close close;
