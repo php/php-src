c1e510aea8f4cebb6863a65b9219201951912819
Felipe Pena <felipe@php.net>
2011-11-11 21:39:11 +0000

    - Fixed bug #60261 (phar dos null pointer)

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -1230,7 +1230,10 @@ SPL_METHOD(SplFileInfo, getLinkTarget)
 	zend_replace_error_handling(EH_THROW, spl_ce_RuntimeException, &error_handling TSRMLS_CC);
 
 #if defined(PHP_WIN32) || HAVE_SYMLINK
-	if (!IS_ABSOLUTE_PATH(intern->file_name, intern->file_name_len)) {
+	if (intern->file_name == NULL) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Empty filename");
+		RETURN_FALSE;
+	} else if (!IS_ABSOLUTE_PATH(intern->file_name, intern->file_name_len)) {
 		char expanded_path[MAXPATHLEN];
 		if (!expand_filepath_with_mode(intern->file_name, expanded_path, NULL, 0, CWD_EXPAND  TSRMLS_CC)) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "No such file or directory");
