dfa30f03b6db930e4f68830d3bd4d89c7135cfe7
Pierre Joye <pajoye@php.net>
2011-12-26 12:32:05 +0000

    - Fix bug #60326, fix TS build (introduced by r321406)

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -432,7 +432,7 @@ static void php_zlib_cleanup_ob_gzhandler_mess(TSRMLS_D)
 {
 	if (ZLIBG(ob_gzhandler)) {
 		deflateEnd(&(ZLIBG(ob_gzhandler)->Z));
-		php_zlib_output_handler_context_dtor(ZLIBG(ob_gzhandler));
+		php_zlib_output_handler_context_dtor(ZLIBG(ob_gzhandler) TSRMLS_CC);
 		ZLIBG(ob_gzhandler) = NULL;
 	}
 }
