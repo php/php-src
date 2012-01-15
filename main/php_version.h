93ee94adb609f34699e5535465589bea05c51b8a
Johannes Schlüter <johannes@php.net>
2012-01-15 19:51:36 +0000

    We should be back to -dev ...

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 9
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.3.9"
-#define PHP_VERSION_ID 50309
+#define PHP_RELEASE_VERSION 10
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.3.10-dev"
+#define PHP_VERSION_ID 50310
