995a668eab1ce346cfd02b550777ab6d946848c3
Rasmus Lerdorf <rasmus@php.net>
2012-03-01 22:34:23 +0000

    Merge fix for bug 61193

--- main/php.h
+++ main/php.h
@@ -61,13 +61,9 @@
 #		define PHPAPI
 #	endif
 
-#define THREAD_LS
-#define PHP_DIR_SEPARATOR '/'
-#if defined(__MacOSX__)
-#define PHP_EOL "\r"
-#else 
-#define PHP_EOL "\n"
-#endif
+#	define THREAD_LS
+#	define PHP_DIR_SEPARATOR '/'
+#	define PHP_EOL "\n"
 #endif
 
 #ifdef NETWARE
