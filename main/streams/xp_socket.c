3e6bc8fbceaa6d696a92e55a1cfe8a2dde9c7190
Gustavo André dos Santos Lopes <cataphract@php.net>
2012-03-04 19:30:01 +0000

    - size_t may be shorter than long and definitely is not signed. Note that the   z modifier was only added in C99, so we can't use it.

--- main/streams/xp_socket.c
+++ main/streams/xp_socket.c
@@ -510,7 +510,9 @@ static inline int parse_unix_address(php_stream_xport_param *xparam, struct sock
 		 * BUT, to get into this branch of code, the name is too long,
 		 * so we don't care. */
 		xparam->inputs.namelen = sizeof(unix_addr->sun_path) - 1;
-		php_error_docref(NULL TSRMLS_CC, E_NOTICE, "socket path exceeded the maximum allowed length of %ld bytes and was truncated", sizeof(unix_addr->sun_path));
+		php_error_docref(NULL TSRMLS_CC, E_NOTICE,
+			"socket path exceeded the maximum allowed length of %lu bytes "
+			"and was truncated", (unsigned long)sizeof(unix_addr->sun_path));
 	}
 
 	memcpy(unix_addr->sun_path, xparam->inputs.name, xparam->inputs.namelen);
