2c43149f21d67fa3614972585b78bb5dbf1cc00e
Pierre Joye <pajoye@php.net>
2012-03-05 20:53:40 +0000

    - fix bug #61290, fix build

--- win32/inet.h
+++ win32/inet.h
@@ -3,7 +3,7 @@
 #endif
 #include <Ws2tcpip.h>
 
-#if (_WIN32_WINNT <= 0x500)
+#if (_WIN32_WINNT < 0x0600)
 PHPAPI int inet_pton(int af, const char* src, void* dst);
 PHPAPI const char* inet_ntop(int af, const void* src, char* dst, size_t size);
 PHPAPI int inet_aton(const char *cp, struct in_addr *inp);
