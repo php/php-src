de8d30b0a50ca610303e44434317b17a735606a8
Nikita Popov <nikic@php.net>
2012-03-24 13:10:51 +0100

    Return previous error handler when resetting the error handler
    set_error_handler(null) and set_exception_handler(null) now return the
    previous error/exception handler instead of just returning bool(true).
    This is consistent with the behavior of these functions with non-null
    values.

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -1444,7 +1444,7 @@ ZEND_FUNCTION(set_error_handler)
 	if (Z_TYPE_P(error_handler) == IS_NULL) { /* unset user-defined handler */
 		FREE_ZVAL(EG(user_error_handler));
 		EG(user_error_handler) = NULL;
-		RETURN_TRUE;
+		return;
 	}
 
 	EG(user_error_handler_error_reporting) = (int)error_type;
@@ -1515,7 +1515,7 @@ ZEND_FUNCTION(set_exception_handler)
 	if (Z_TYPE_P(exception_handler) == IS_NULL) { /* unset user-defined handler */
 		FREE_ZVAL(EG(user_exception_handler));
 		EG(user_exception_handler) = NULL;
-		RETURN_TRUE;
+		return;
 	}
 
 	MAKE_COPY_ZVAL(&exception_handler, EG(user_exception_handler))
