0fe8b495add557251519050bb94b5dd451d9f7cb
Davey Shafik <me@daveyshafik.com>
2012-04-27 21:27:29 -0400

    Add extension configs, compile more extensions

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 ./buildconf
-./configure --with-pdo-mysql --with-mysql --with-sqlite --with-pdo-sqlite
+./configure \
+--with-pdo-mysql \
+--with-mysql \
+--with-mysqli \
+--with-pgsql \
+--with-pdo-pgsql \
+--with-pdo-sqlite \
+--enable-intl \
+--without-pear \
+--with-gd \
+--with-jpeg-dir=/usr \
+--with-png-dir=/usr \
+--enable-exif \
+--enable-zip \
+--with-zlib \
+--with-zlib-dir=/usr \
+--with-mcrypt=/usr \
+--enable-soap \
+--enable-xmlreader \
+--with-xsl \
+--with-curl=/usr \
+--with-tidy \
+--with-xmlrpc \
+--enable-sysvsem \
+--enable-sysvshm \
+--enable-shmop \
+--enable-pcntl \
+--with-readline \
+--enable-mbstring \
+--with-curl \
+--with-gettext \
+--enable-sockets \
+--with-bz2 \
+--enable-bcmath \
+--enable-fastcgi \
+--with-mime-magic
 make
