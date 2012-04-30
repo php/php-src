bae56a87f81b91cd815604b0f404f616b0d73c2b
Xinchen Hui <laruence@php.net>
2012-04-30 12:09:22 +0800

    Fixed bug #61546 (functions related to current script failed when chdir() in cli sapi).

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -648,7 +648,7 @@ int main(int argc, char *argv[])
 	int orig_optind=php_optind;
 	char *orig_optarg=php_optarg;
 	char *arg_free=NULL, **arg_excp=&arg_free;
-	char *script_file=NULL;
+	char *script_file=NULL, *translated_path = NULL;
 	int interactive=0;
 	volatile int module_started = 0;
 	volatile int request_started = 0;
@@ -1053,8 +1053,13 @@ int main(int argc, char *argv[])
 		if (script_file) {
 			if (cli_seek_file_begin(&file_handle, script_file, &lineno TSRMLS_CC) != SUCCESS) {
 				goto err;
+			} else {
+				char real_path[MAXPATHLEN];
+				if (VCWD_REALPATH(script_file, real_path)) {
+					translated_path = strdup(real_path);
+				}
+				script_filename = script_file;
 			}
-			script_filename = script_file;
 		} else {
 			/* We could handle PHP_MODE_PROCESS_STDIN in a different manner  */
 			/* here but this would make things only more complicated. And it */
@@ -1073,7 +1078,7 @@ int main(int argc, char *argv[])
 		SG(request_info).argc=argc-php_optind+1;
 		arg_excp = argv+php_optind-1;
 		arg_free = argv[php_optind-1];
-		SG(request_info).path_translated = file_handle.filename;
+		SG(request_info).path_translated = translated_path? translated_path : file_handle.filename;
 		argv[php_optind-1] = file_handle.filename;
 		SG(request_info).argv=argv+php_optind-1;
 
