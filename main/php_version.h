72a9b595ca3e1bc07cf1aa51c91dcbc973f89d10
Johannes Schlüter <johannes@php.net>
2012-05-16 16:49:35 +0200

    PHP 5.3.15-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 14
+#define PHP_RELEASE_VERSION 15
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.3.14-dev"
-#define PHP_VERSION_ID 50314
+#define PHP_VERSION "5.3.15-dev"
+#define PHP_VERSION_ID 50315
