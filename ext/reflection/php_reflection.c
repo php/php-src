0eb02b874de8c97b6ee4c4226a95971e1d4d27ff
Xinchen Hui <laruence@php.net>
2012-05-23 13:39:00 +0800

    Revert "Implemented FR #61602 Allow access to name of constant used as default value"
    This reverts commit 054f3e3ce5af13c2c3a6ccd54f7dc3e2f6cd4f74.
    
    See: http://news.php.net/php.cvs/69137 and the author confirmed.
    Will commit later after the author fixed this then make a new PR.
    
    Conflicts:
    
    	ext/reflection/php_reflection.c

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -2588,6 +2588,15 @@ ZEND_METHOD(reflection_parameter, getDefaultValue)
 	if (!(param && precv)) {
 		return;
 	}
+	if (param->offset < param->required) {
+		zend_throw_exception_ex(reflection_exception_ptr, 0 TSRMLS_CC, "Parameter is not optional");
+		return;
+	}
+	precv = _get_recv_op((zend_op_array*)param->fptr, param->offset);
+	if (!precv || precv->opcode != ZEND_RECV_INIT || precv->op2_type == IS_UNUSED) {
+		zend_throw_exception_ex(reflection_exception_ptr, 0 TSRMLS_CC, "Internal error");
+		return;
+	}
 
 	*return_value = *precv->op2.zv;
 	INIT_PZVAL(return_value);
