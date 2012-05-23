f4c38016b634fbacd6daab6e43d0cd57d1ec5357
Jerome Loyet <fat@php.net>
2012-05-23 10:36:45 +0200

    - Fix help message for '-R'

--- sapi/fpm/fpm/fpm_main.c
+++ sapi/fpm/fpm/fpm_main.c
@@ -937,7 +937,7 @@ static void php_cgi_usage(char *argv0)
 				"  -y, --fpm-config <file>\n"
 				"                   Specify alternative path to FastCGI process manager config file.\n"
 				"  -t, --test       Test FPM configuration and exit\n"
-				"  -R, allow-to-run-as-root\n"
+				"  -R, --allow-to-run-as-root\n"
 				"                   Allow pool to run as root (disabled by default)\n",
 
 				prog, PHP_PREFIX);
