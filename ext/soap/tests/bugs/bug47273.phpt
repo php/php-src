af2d6a63bf6a187a68164a3cc3384539357ac7b8
Nuno Lopes <nlopess@php.net>
2012-05-27 19:57:34 -0400

    remove remaining traces of unicode.* ini settings

--- ext/soap/tests/bugs/bug47273.phpt
+++ ext/soap/tests/bugs/bug47273.phpt
@@ -2,9 +2,6 @@
 Bug #47273 (Encoding bug in SoapServer->fault)
 --SKIPIF--
 <?php require_once('skipif.inc'); ?>
---INI--
-unicode.script_encoding=ISO-8859-1
-unicode.output_encoding=ISO-8859-1
 --FILE--
 <?php
 $request1 = <<<EOF
--- run-tests.php
+++ run-tests.php
@@ -239,10 +239,6 @@ $ini_overwrites = array(
 		'magic_quotes_runtime=0',
 		'ignore_repeated_errors=0',
 		'precision=14',
-		'unicode.runtime_encoding=ISO-8859-1',
-		'unicode.script_encoding=UTF-8',
-		'unicode.output_encoding=UTF-8',
-		'unicode.from_error_mode=U_INVALID_SUBSTITUTE',
 	);
 
 function write_information($show_html)
