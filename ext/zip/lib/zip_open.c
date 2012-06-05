b8cdc731b840cf3b8fd49d34ede7485180326a9c
Anatoliy Belsky <ab@php.net>
2012-06-05 23:22:27 +0200

    brought the fix for #47667 back

--- ext/zip/lib/zip_open.c
+++ ext/zip/lib/zip_open.c
@@ -61,10 +61,16 @@ ZIP_EXTERN(struct zip *)
 zip_open(const char *fn, int flags, int *zep)
 {
     FILE *fp;
-    
+
+    if (flags & ZIP_OVERWRITE) {
+	return _zip_allocate_new(fn, zep);
+    }
+
     switch (_zip_file_exists(fn, flags, zep)) {
     case -1:
-	return NULL;
+			if (!(flags & ZIP_OVERWRITE)) {
+				return NULL;
+			}
     case 0:
 	return _zip_allocate_new(fn, zep);
     default:
