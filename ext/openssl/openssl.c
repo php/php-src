aadf59dfa4be09147671de33786dc157716705df
Scott MacVicar <scottmac@php.net>
2012-06-11 00:16:30 -0700

    Add PBKDF2 support via openssl()
    Summary:
    No easy way to put these in the hash extension since we don't really support optional
    parameters to certain algorithms. Implemented in openssl for now since it has it already
    and is pretty stable.
    
    Only SHA1 is confirmed to work as an algorithm but openssl has a parameter so it can be
    changed in the future.
    
    Will backport to 5.4 potentially with Stas' approval.
    
    Test Plan:
    Ran newly added tests which came from RFC 6070

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -3328,7 +3328,6 @@ PHP_FUNCTION(openssl_pkey_get_details)
 /* }}} */
 
 #if OPENSSL_VERSION_NUMBER >= 0x10000000L
-
 /* {{{ proto string openssl_pbkdf2(string password, string salt, long key_length, long iterations [, string digest_method = "sha1"])
    Generates a PKCS5 v2 PBKDF2 string, defaults to sha1 */
 PHP_FUNCTION(openssl_pbkdf2)
