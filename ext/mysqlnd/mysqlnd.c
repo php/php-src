049abdfb2635f4f05bb4c51eccc246ffcdfa0dfe
andrey <andrey@php.net>
2012-07-09 18:13:35 +0300

    fix valgrind warning

--- ext/mysqlnd/mysqlnd.c
+++ ext/mysqlnd/mysqlnd.c
@@ -581,7 +581,7 @@ mysqlnd_run_authentication(
 			}
 			memcpy(conn->auth_plugin_data, plugin_data, plugin_data_len);
 
-			DBG_INF_FMT("salt=[%*s]", plugin_data_len - 1, plugin_data);
+			DBG_INF_FMT("salt=[%*.s]", plugin_data_len - 1, plugin_data);
 			/* The data should be allocated with malloc() */
 			scrambled_data =
 				auth_plugin->methods.get_auth_data(NULL, &scrambled_data_len, conn, user, passwd, passwd_len,
