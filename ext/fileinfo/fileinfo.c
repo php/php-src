068fc008c6828c2e045064a831ecb90136c6365b
Felipe Pena <felipensp@gmail.com>
2012-07-28 19:05:13 -0300

    - Fixed bug #57933 (Wrong table type used in phpinfo output) patch by: selsky at columbia dot edu

--- ext/fileinfo/fileinfo.c
+++ ext/fileinfo/fileinfo.c
@@ -271,7 +271,7 @@ ZEND_GET_MODULE(fileinfo)
 PHP_MINFO_FUNCTION(fileinfo)
 {
 	php_info_print_table_start();
-	php_info_print_table_header(2, "fileinfo support", "enabled");
+	php_info_print_table_row(2, "fileinfo support", "enabled");
 	php_info_print_table_row(2, "version", PHP_FILEINFO_VERSION);
 	php_info_print_table_end();
 }
