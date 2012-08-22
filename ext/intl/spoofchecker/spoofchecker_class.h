cd1f45b3be2e630aab32f58770124d38661b518e
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2012-08-22 22:38:31 +0200

    Fix handling of several uinitialized intl objects
    PHP 5.4 specific changes. Not having this in the merge commit helps
    porting to pecl/intl

--- ext/intl/spoofchecker/spoofchecker_class.h
+++ ext/intl/spoofchecker/spoofchecker_class.h
@@ -55,6 +55,15 @@ extern zend_class_entry *Spoofchecker_ce_ptr;
     Spoofchecker_object*  co  = NULL;   \
     intl_error_reset(NULL TSRMLS_CC); \
 
+#define SPOOFCHECKER_METHOD_FETCH_OBJECT_NO_CHECK	INTL_METHOD_FETCH_OBJECT(Spoofchecker, co)
+#define SPOOFCHECKER_METHOD_FETCH_OBJECT							\
+	SPOOFCHECKERMETHOD_FETCH_OBJECT_NO_CHECK;					\
+	if (co->uspoof == NULL)	{										\
+		intl_errors_set(&co->err, U_ILLEGAL_ARGUMENT_ERROR,			\
+				"Found unconstructed Spoofchecker", 0 TSRMLS_CC);	\
+		RETURN_FALSE;												\
+	}
+
 #define SPOOFCHECKER_METHOD_FETCH_OBJECT                                           \
     co = (Spoofchecker_object *) zend_object_store_get_object(object TSRMLS_CC); \
     intl_error_reset(SPOOFCHECKER_ERROR_P(co) TSRMLS_CC);                      \
