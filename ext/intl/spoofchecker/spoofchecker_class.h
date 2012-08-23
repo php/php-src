d212a5d391a8d4c0de32001175314dbf78b957d1
Gustavo André dos Santos Lopes <cataphract@php.net>
2012-08-23 10:32:05 +0100

    Fix SPOOFCHECKER_METHOD_FETCH_OBJECT definition

--- ext/intl/spoofchecker/spoofchecker_class.h
+++ ext/intl/spoofchecker/spoofchecker_class.h
@@ -22,6 +22,7 @@
 #include "intl_common.h"
 #include "spoofchecker_create.h"
 #include "intl_error.h"
+#include "intl_data.h"
 
 #include <unicode/uspoof.h>
 
@@ -57,16 +58,12 @@ extern zend_class_entry *Spoofchecker_ce_ptr;
 
 #define SPOOFCHECKER_METHOD_FETCH_OBJECT_NO_CHECK	INTL_METHOD_FETCH_OBJECT(Spoofchecker, co)
 #define SPOOFCHECKER_METHOD_FETCH_OBJECT							\
-	SPOOFCHECKERMETHOD_FETCH_OBJECT_NO_CHECK;					\
+	SPOOFCHECKER_METHOD_FETCH_OBJECT_NO_CHECK;						\
 	if (co->uspoof == NULL)	{										\
 		intl_errors_set(&co->err, U_ILLEGAL_ARGUMENT_ERROR,			\
 				"Found unconstructed Spoofchecker", 0 TSRMLS_CC);	\
 		RETURN_FALSE;												\
 	}
-
-#define SPOOFCHECKER_METHOD_FETCH_OBJECT                                           \
-    co = (Spoofchecker_object *) zend_object_store_get_object(object TSRMLS_CC); \
-    intl_error_reset(SPOOFCHECKER_ERROR_P(co) TSRMLS_CC);                      \
 
 // Macro to check return value of a ucol_* function call.
 #define SPOOFCHECKER_CHECK_STATUS(co, msg)                                        \
