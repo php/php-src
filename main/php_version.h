64c09f68ec66061d1085ad5a5ef0a5ced63450b6
Johannes Schlüter <johannes@php.net>
2012-10-04 02:35:42 +0200

    PHP-5.3 branch is 5.3.19 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 18
+#define PHP_RELEASE_VERSION 19
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.3.18-dev"
-#define PHP_VERSION_ID 50318
+#define PHP_VERSION "5.3.19-dev"
+#define PHP_VERSION_ID 50319
