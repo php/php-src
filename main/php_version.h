012519839a0d3bb4775e53b739c85ade198cacdc
David Soria Parra <dsp@php.net>
2012-11-13 17:42:01 +0100

    Bump version to 5.5.99 and reset NEWS

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 0
+#define PHP_RELEASE_VERSION 99
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.0-dev"
-#define PHP_VERSION_ID 50500
+#define PHP_VERSION "5.5.99-dev"
+#define PHP_VERSION_ID 50599
