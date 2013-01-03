712710bfecb2e421306246f2e179727f0494a019
Johannes Schlüter <johannes@php.net>
2013-01-03 22:56:46 +0100

    This will be PHP 5.3.22

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 21
+#define PHP_RELEASE_VERSION 22
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.3.21-dev"
-#define PHP_VERSION_ID 50321
+#define PHP_VERSION "5.3.22-dev"
+#define PHP_VERSION_ID 50322
