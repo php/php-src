bcd278ad7f3a8e549a3281600decd897dd7d73ce
Andrey Hristov <andrey@php.net>
2013-01-09 18:39:59 +0100

    fix compilation error after merge

--- ext/mysqlnd/mysqlnd.c
+++ ext/mysqlnd/mysqlnd.c
@@ -774,7 +774,7 @@ MYSQLND_METHOD(mysqlnd_conn_data, connect)(MYSQLND_CONN_DATA * conn,
 	/* we allow load data local infile by default */
 	mysql_flags |= MYSQLND_CAPABILITIES;
 
-	mysql_flags |= conn->options.flags; /* use the flags from set_client_option() */
+	mysql_flags |= conn->options->flags; /* use the flags from set_client_option() */
 
 	if (db) {
 		mysql_flags |= CLIENT_CONNECT_WITH_DB;
