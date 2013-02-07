afacdecd1f8001b143e86676859d570ebf4659b9
Andrey Hristov <andrey@php.net>
2013-02-07 18:45:49 +0100

    Add support for begin_transaction in libmysql mode. Add support for flags and name for commit/rollback in libmysql mode

--- ext/mysqli/mysqli_fe.h
+++ ext/mysqli/mysqli_fe.h
@@ -25,6 +25,7 @@
 PHP_FUNCTION(mysqli);
 PHP_FUNCTION(mysqli_affected_rows);
 PHP_FUNCTION(mysqli_autocommit);
+PHP_FUNCTION(mysqli_begin_transaction);
 PHP_FUNCTION(mysqli_change_user);
 PHP_FUNCTION(mysqli_character_set_name);
 PHP_FUNCTION(mysqli_set_charset);
