6100846193c8cbc2b7b70ba27cb93adf4fa40c8a
Yousha Aleayoub <Yousha.A@Hotmail.com>
2013-02-13 04:40:53 +0430

    One minor improvement at win32/inet.c .

--- win32/inet.c
+++ win32/inet.c
@@ -1,9 +1,7 @@
 #include "inet.h"
 
 int inet_aton(const char *cp, struct in_addr *inp) {
-  inp->s_addr = inet_addr(cp);
-
-  if (inp->s_addr == INADDR_NONE) {
+  if ((inp->s_addr= inet_addr(cp)) == INADDR_NONE) {
           return 0;
   }
 
