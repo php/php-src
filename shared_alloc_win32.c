b95964535cfa9561a3d18c3573d8afe47a8b7343
Dmitry Stogov <dmitry@zend.com>
2013-02-20 11:21:15 +0400

    Fixed compilation error

--- shared_alloc_win32.c
+++ shared_alloc_win32.c
@@ -212,7 +212,7 @@ static int create_segments(size_t requested_size, zend_shared_segment ***shared_
 	*shared_segments_count = 1;
 	*shared_segments_p = (zend_shared_segment **) calloc(1, sizeof(zend_shared_segment)+sizeof(void *));
 	if (!*shared_segments_p) {
-		zend_win_error_message(ACCEL_LOG_FATAL, "calloc() failed");
+		zend_win_error_message(ACCEL_LOG_FATAL, "calloc() failed", GetLastError());
 		*error_in = "calloc";
 		return ALLOC_FAILURE;
 	}
