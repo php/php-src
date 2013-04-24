dd0399f5c6d2acfe53621f9e5daf61c57e8e66c6
Remi Collet <remi@php.net>
2013-04-24 16:07:50 +0200

    We need both gdImageCreateFromJpeg* and gdImageCreateFromJpeg*Ex
    The new (Ex) functions are used with the new ignore_warning parameter
    
    The standard functions are used in _php_image_create_from_string,
    sent as a function pointer, and call without this optional parameter.

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -2670,7 +2670,7 @@ static void _php_image_create_from(INTERNAL_FUNCTION_PARAMETERS, int image_type,
 			case PHP_GDIMG_TYPE_JPG:
 				ignore_warning = INI_INT("gd.jpeg_ignore_warning");
 #ifdef HAVE_GD_BUNDLED
-				im = gdImageCreateFromJpeg(fp, ignore_warning);
+				im = gdImageCreateFromJpegEx(fp, ignore_warning);
 #else
 				im = gdImageCreateFromJpeg(fp);
 #endif
@@ -4783,7 +4783,7 @@ static void _php_image_convert(INTERNAL_FUNCTION_PARAMETERS, int image_type )
 		case PHP_GDIMG_TYPE_JPG:
 			ignore_warning = INI_INT("gd.jpeg_ignore_warning");
 #ifdef HAVE_GD_BUNDLED
-			im_org = gdImageCreateFromJpeg(org, ignore_warning);
+			im_org = gdImageCreateFromJpegEx(org, ignore_warning);
 #else
 			im_org = gdImageCreateFromJpeg(org);
 #endif
