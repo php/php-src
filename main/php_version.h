c53058763ada0050fd7f0b9efb9818f06671d3d5
Stanislav Malyshev <stas@php.net>
2013-05-21 23:36:28 -0700

    5.4.17 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 16
+#define PHP_RELEASE_VERSION 17
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.16-dev"
-#define PHP_VERSION_ID 50416
+#define PHP_VERSION "5.4.17-dev"
+#define PHP_VERSION_ID 50417
