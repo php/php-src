6ac7c9d0a7d93a26d077e9c321f030118d279cd8
Christopher Jones <sixd@php.net>
2013-07-08 13:29:49 -0700

    Bump PECL package info version check to allow PECL installs with PHP 5.5+

--- ext/oci8/php_oci8.h
+++ ext/oci8/php_oci8.h
@@ -46,7 +46,7 @@
  */
 #undef PHP_OCI8_VERSION
 #endif
-#define PHP_OCI8_VERSION "1.4.9"
+#define PHP_OCI8_VERSION "1.4.10"
 
 extern zend_module_entry oci8_module_entry;
 #define phpext_oci8_ptr &oci8_module_entry
