d4c546b9562d120ceb9f1e6d1ac3c973a48bfd6b
Anatol Belski <ab@php.net>
2013-07-17 09:11:51 -0700

    fixes to zend

--- Zend/zend.h
+++ Zend/zend.h
@@ -558,11 +558,11 @@ typedef struct _zend_utility_functions {
 
 typedef struct _zend_utility_values {
 	char *import_use_extension;
-	uint import_use_extension_length;
+	zend_str_size_uint import_use_extension_length;
 	zend_bool html_errors;
 } zend_utility_values;
 
-typedef int (*zend_write_func_t)(const char *str, uint str_length);
+typedef int (*zend_write_func_t)(const char *str, zend_str_size_uint str_length);
 
 #undef MIN
 #undef MAX
@@ -643,8 +643,8 @@ END_EXTERN_C()
 BEGIN_EXTERN_C()
 ZEND_API char *get_zend_version(void);
 ZEND_API void zend_make_printable_zval(zval *expr, zval *expr_copy, int *use_copy);
-ZEND_API int zend_print_zval(zval *expr, int indent);
-ZEND_API int zend_print_zval_ex(zend_write_func_t write_func, zval *expr, int indent);
+ZEND_API zend_str_size_int zend_print_zval(zval *expr, int indent);
+ZEND_API zend_str_size_int zend_print_zval_ex(zend_write_func_t write_func, zval *expr, int indent);
 ZEND_API void zend_print_zval_r(zval *expr, int indent TSRMLS_DC);
 ZEND_API void zend_print_flat_zval_r(zval *expr TSRMLS_DC);
 ZEND_API void zend_print_zval_r_ex(zend_write_func_t write_func, zval *expr, int indent TSRMLS_DC);
