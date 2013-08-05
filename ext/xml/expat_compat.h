d44953df1a8d7ee8ad33d6b3f12ca9da356c1c46
Veres Lajos <vlajos@gmail.com>
2013-08-05 20:23:18 +0100

    support misspelled version also

--- ext/xml/expat_compat.h
+++ ext/xml/expat_compat.h
@@ -38,6 +38,9 @@
 #include <libxml/tree.h>
 #include <libxml/hash.h>
 
+/* For compatibility with the misspelled version. */
+#define _ns_seperator _ns_separator
+
 typedef xmlChar XML_Char;
 
 typedef void (*XML_StartElementHandler)(void *, const XML_Char *, const XML_Char **);
