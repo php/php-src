625acc345070758e70e9b9f6bd6c49156d1ac637
krakjoe <joe.watkins@live.co.uk>
2013-08-12 16:51:00 +0100

    fix strangeness for pthreads

--- ext/opcache/zend_accelerator_module.c
+++ ext/opcache/zend_accelerator_module.c
@@ -212,7 +212,7 @@ static ZEND_INI_MH(OnEnable)
 	    stage == ZEND_INI_STAGE_SHUTDOWN ||
 	    stage == ZEND_INI_STAGE_DEACTIVATE) {
 		return OnUpdateBool(entry, new_value, new_value_length, mh_arg1, mh_arg2, mh_arg3, stage TSRMLS_CC);
-	} else {
+	} else if (!ZCG(enabled)) {
 		/* It may be only temporary disabled */
 		zend_bool *p;
 #ifndef ZTS
@@ -232,6 +232,8 @@ static ZEND_INI_MH(OnEnable)
 			*p = 0;
 			return SUCCESS;
 		}
+	} else {
+	    return SUCCESS;
 	}
 }
 
