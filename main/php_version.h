47a5e202bfb6e8b64accd1917b2ea975ee7e03a2
Stanislav Malyshev <stas@php.net>
2013-08-19 21:42:41 -0700

    5.4.19

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 18
+#define PHP_RELEASE_VERSION 19
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.4.18"
-#define PHP_VERSION_ID 50418
+#define PHP_VERSION "5.4.19"
+#define PHP_VERSION_ID 50419
