d005ed9681d6797012570c4e32abb3abcdf3b72a
Stanislav Malyshev <stas@php.net>
2013-08-19 21:46:43 -0700

    5.5.3

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 2
+#define PHP_RELEASE_VERSION 3
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.5.2"
-#define PHP_VERSION_ID 50502
+#define PHP_VERSION "5.5.3"
+#define PHP_VERSION_ID 50503
