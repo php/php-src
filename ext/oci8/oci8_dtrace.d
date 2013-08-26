4b67c781f3fdd93e6a602ee980176720f1ace10e
Christopher Jones <sixd@php.net>
2013-08-26 15:42:57 -0700

    Use "__" consistently in OCI8 DTrace probe definitions. It maps to "-" in the probe name.

--- ext/oci8/oci8_dtrace.d
+++ ext/oci8/oci8_dtrace.d
@@ -17,18 +17,18 @@
 */
 
 provider php {
-	probe oci8__connect_start(char *username, char *dbname, char *charset, long session_mode, int persistent, int exclusive);
-	probe oci8__connect_done();
+	probe oci8__connect__start(char *username, char *dbname, char *charset, long session_mode, int persistent, int exclusive);
+	probe oci8__connect__done();
 	probe oci8__sqltext(char *sql);
 	probe oci8__error(int status, long errcode);
-	probe oci8__execute_mode(unsigned int mode);
+	probe oci8__execute__mode(unsigned int mode);
 
-	probe oci8__connect_p_dtor_close(void *connection);
-	probe oci8__connect_p_dtor_release(void *connection);
-	probe oci8__connect_lookup(void *connection, int is_stub);
-	probe oci8__connect_expiry(void *connection, int is_stub, time_t idle_expiry, time_t timestamp);
-	probe oci8__connect_type(int is_persistent, int exclusive, void *connection, long num_persistent, long num_links);
-	probe oci8__sesspool_create(void *session_pool);
-	probe oci8__sesspool_stats(unsigned long free, unsigned long busy, unsigned long open);
-	probe oci8__sesspool_type(int type, void *session_pool);
+	probe oci8__connect__p__dtor__close(void *connection);
+	probe oci8__connect__p__dtor__release(void *connection);
+	probe oci8__connect__lookup(void *connection, int is_stub);
+	probe oci8__connect__expiry(void *connection, int is_stub, time_t idle_expiry, time_t timestamp);
+	probe oci8__connect__type(int is_persistent, int exclusive, void *connection, long num_persistent, long num_links);
+	probe oci8__sesspool__create(void *session_pool);
+	probe oci8__sesspool__stats(unsigned long free, unsigned long busy, unsigned long open);
+	probe oci8__sesspool__type(int type, void *session_pool);
 };
