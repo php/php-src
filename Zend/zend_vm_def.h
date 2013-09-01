d515455589d836e709a65590e14d85575bdd2766
Gustavo André dos Santos Lopes <cataphract@php.net>
2013-09-02 01:53:06 +0200

    Implement phase 1 of rfc/incompat_ctx
    Just changing the error level of the message from E_STRICT to
    E_DEPRECATED. This comes one version later than the timeline
    mentioned in the RFC.
    
    Oddly, there were no tests for this ‘feature’. I added a simple
    one.

--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -2609,7 +2609,7 @@ ZEND_VM_HANDLER(113, ZEND_INIT_STATIC_METHOD_CALL, CONST|VAR, CONST|TMP|VAR|UNUS
 		    /* We are calling method of the other (incompatible) class,
 		       but passing $this. This is done for compatibility with php-4. */
 			if (call->fbc->common.fn_flags & ZEND_ACC_ALLOW_STATIC) {
-				zend_error(E_STRICT, "Non-static method %s::%s() should not be called statically, assuming $this from incompatible context", call->fbc->common.scope->name, call->fbc->common.function_name);
+				zend_error(E_DEPRECATED, "Non-static method %s::%s() should not be called statically, assuming $this from incompatible context", call->fbc->common.scope->name, call->fbc->common.function_name);
 			} else {
 				/* An internal function assumes $this is present and won't check that. So PHP would crash by allowing the call. */
 				zend_error_noreturn(E_ERROR, "Non-static method %s::%s() cannot be called statically, assuming $this from incompatible context", call->fbc->common.scope->name, call->fbc->common.function_name);
