89479fe65c7ee5aa3b9775993ac91271f94d082d
Aleksey Parshukov <parshukovag@gmail.com>
2013-09-03 17:03:26 +0400

    Fixed bug #48770: when call_user_func() fails to call parent from inheriting class

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2612,6 +2612,7 @@ static int zend_is_callable_check_class(const char *name, int name_len, zend_fca
 {
 	int ret = 0;
 	zend_class_entry **pce;
+	zend_class_entry *scope = EG(active_op_array) ? EG(active_op_array)->scope : NULL;
 	char *lcname = zend_str_tolower_dup(name, name_len);
 
 	*strict_class = 0;
@@ -2621,7 +2622,9 @@ static int zend_is_callable_check_class(const char *name, int name_len, zend_fca
 			if (error) *error = estrdup("cannot access self:: when no class scope is active");
 		} else {
 			fcc->called_scope = EG(called_scope);
-			fcc->calling_scope = EG(scope);
+			fcc->calling_scope = scope && instanceof_function(EG(scope), scope TSRMLS_CC)
+				? scope
+				: EG(scope);
 			if (!fcc->object_ptr) {
 				fcc->object_ptr = EG(This);
 			}
@@ -2631,16 +2634,22 @@ static int zend_is_callable_check_class(const char *name, int name_len, zend_fca
 		       !memcmp(lcname, "parent", sizeof("parent") - 1)) {
 		if (!EG(scope)) {
 			if (error) *error = estrdup("cannot access parent:: when no class scope is active");
-		} else if (!EG(scope)->parent) {
-			if (error) *error = estrdup("cannot access parent:: when current class scope has no parent");
 		} else {
-			fcc->called_scope = EG(called_scope);
-			fcc->calling_scope = EG(scope)->parent;
-			if (!fcc->object_ptr) {
-				fcc->object_ptr = EG(This);
+			if (!(scope && instanceof_function(EG(scope), scope TSRMLS_CC))) {
+				scope = EG(scope);
 			}
-			*strict_class = 1;
-			ret = 1;
+
+			if (!scope->parent) {
+				if (error) *error = estrdup("cannot access parent:: when current class scope has no parent");
+			} else {
+				fcc->calling_scope = scope->parent;
+				fcc->called_scope = EG(called_scope);
+				if (!fcc->object_ptr) {
+					fcc->object_ptr = EG(This);
+				}
+				*strict_class = 1;
+				ret = 1;
+			}
 		}
 	} else if (name_len == sizeof("static") - 1 &&
 	           !memcmp(lcname, "static", sizeof("static") - 1)) {
@@ -2656,8 +2665,6 @@ static int zend_is_callable_check_class(const char *name, int name_len, zend_fca
 			ret = 1;
 		}
 	} else if (zend_lookup_class_ex(name, name_len, NULL, 1, &pce TSRMLS_CC) == SUCCESS) {
-		zend_class_entry *scope = EG(active_op_array) ? EG(active_op_array)->scope : NULL;
-
 		fcc->calling_scope = *pce;
 		if (scope && !fcc->object_ptr && EG(This) &&
 		    instanceof_function(Z_OBJCE_P(EG(This)), scope TSRMLS_CC) &&
