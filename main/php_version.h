7beef74a827f393753852f7239af75af236fb4f8
Stanislav Malyshev <stas@php.net>
2013-09-03 13:38:15 -0700

    5.4.21 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 20
+#define PHP_RELEASE_VERSION 21
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.20-dev"
-#define PHP_VERSION_ID 50420
+#define PHP_VERSION "5.4.21-dev"
+#define PHP_VERSION_ID 50421
