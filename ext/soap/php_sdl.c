php/php-src

commit 1a9e668a04badaba65138718e50e413c89deaa57

Fixed bug #42952 (soap cache file is created with insecure permissions)

ext/soap/php_sdl.c
@@ -2015,7 +2015,7 @@ static void add_sdl_to_cache(const char *fn, const char *uri, time_t t, sdlPtr s
 #ifdef ZEND_WIN32
 	f = open(fn,O_CREAT|O_WRONLY|O_EXCL|O_BINARY,S_IREAD|S_IWRITE);
 #else
-	f = open(fn,O_CREAT|O_WRONLY|O_EXCL|O_BINARY,S_IREAD|S_IWRITE|S_IROTH|S_IWOTH|S_IRGRP|S_IWGRP);
+	f = open(fn,O_CREAT|O_WRONLY|O_EXCL|O_BINARY,S_IREAD|S_IWRITE);
 #endif
 	if (f < 0) {return;}
 
@@ -3117,16 +3117,24 @@ sdlPtr get_sdl(zval *this_ptr, char *uri, long cache_wsdl TSRMLS_DC)
 		unsigned char digest[16];
 		int len = strlen(SOAP_GLOBAL(cache_dir));
 		time_t cached;
+		char *user = php_get_current_user();
+		int user_len = user ? strlen(user) + 1 : 0;
 
 		md5str[0] = '\0';
 		PHP_MD5Init(&context);
 		PHP_MD5Update(&context, (unsigned char*)uri, uri_len);
 		PHP_MD5Final(digest, &context);
 		make_digest(md5str, digest);
-		key = emalloc(len+sizeof("/wsdl-")-1+sizeof(md5str));
+		key = emalloc(len+sizeof("/wsdl-")-1+user_len+sizeof(md5str));
 		memcpy(key,SOAP_GLOBAL(cache_dir),len);
 		memcpy(key+len,"/wsdl-",sizeof("/wsdl-")-1);
-		memcpy(key+len+sizeof("/wsdl-")-1,md5str,sizeof(md5str));
+		len += sizeof("/wsdl-")-1;
+		if (user_len) {
+			memcpy(key+len, user, user_len-1);
+			len += user_len-1;
+			key[len++] = '-';
+		}
+		memcpy(key+len,md5str,sizeof(md5str));
 
 		if ((sdl = get_sdl_from_cache(key, uri, t-SOAP_GLOBAL(cache_ttl), &cached TSRMLS_CC)) != NULL) {
 			t = cached;

commit c02f05a103e36cfc902bcdd49aa22332760a2ae4

Fix #61094 Wrong WSDL cache file name
WSDL cache files are created with the format wsdl-owner_username-uri_md5sum
and with permission 0600. This will result in an error when two or more
users are trying to run the same script that uses the same wsdl: only the
first user can access to the wsdl cache file, while a second user will
encounter a SOAP-ERROR complaining about not being about to load the WSDL.
The bug was introduced in commit 1a9e668a04 which was supposed to use getuid()
instead of php_get_current_user() in the cache filename.

ext/soap/php_sdl.c
@@ -30,6 +30,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
 #ifndef O_BINARY
 # define O_BINARY 0
 #endif
@@ -3228,7 +3232,12 @@ sdlPtr get_sdl(zval *this_ptr, char *uri, long cache_wsdl TSRMLS_DC)
 		unsigned char digest[16];
 		int len = strlen(SOAP_GLOBAL(cache_dir));
 		time_t cached;
+#ifdef HAVE_UNISTD_H
+		char user[10]; /* storage for a 32-bit UID */
+		snprintf(user, 10, "%d", getuid());
+#else
 		char *user = php_get_current_user(TSRMLS_C);
+#endif
 		int user_len = user ? strlen(user) + 1 : 0;
 
 		md5str[0] = '\0';