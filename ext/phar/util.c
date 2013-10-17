06994a419d625291c2ae071e3b22716901b45cf6
Xinchen Hui <laruence@php.net>
2013-10-17 12:23:09 +0800

    Add ability of alias resolving (it cause problem while work with opcache)

--- ext/phar/util.c
+++ ext/phar/util.c
@@ -1185,6 +1185,17 @@ phar_entry_info * phar_open_jit(phar_archive_data *phar, phar_entry_info *entry,
 }
 /* }}} */
 
+PHP_PHAR_API int phar_resolve_alias(char *alias, int alias_len, char **filename, int *filename_len TSRMLS_DC) /* {{{ */ {
+	phar_archive_data **fd_ptr;
+	if (SUCCESS == zend_hash_find(&(PHAR_GLOBALS->phar_alias_map), alias, alias_len, (void**)&fd_ptr)) {
+		*filename = (*fd_ptr)->fname;
+		*filename_len = (*fd_ptr)->fname_len;
+		return 1;
+	}
+	return 0;
+}
+/* }}} */
+
 int phar_free_alias(phar_archive_data *phar, char *alias, int alias_len TSRMLS_DC) /* {{{ */
 {
 	if (phar->refcount || phar->is_persistent) {
