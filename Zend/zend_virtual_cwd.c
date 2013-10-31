f1c2f8f9f17c063ac26aa5e3d0240bc6e0d07c39
Anatol Belski <ab@php.net>
2013-10-31 21:38:24 +0100

    removed unnecessary call

--- Zend/zend_virtual_cwd.c
+++ Zend/zend_virtual_cwd.c
@@ -434,7 +434,6 @@ static void cwd_globals_ctor(virtual_cwd_globals *cwd_g TSRMLS_DC) /* {{{ */
 	cwd_g->realpath_cache_size_limit = REALPATH_CACHE_SIZE;
 	cwd_g->realpath_cache_ttl = REALPATH_CACHE_TTL;
 	memset(cwd_g->realpath_cache, 0, sizeof(cwd_g->realpath_cache));
- 	virtual_cwd_activate(TSRMLS_C);
 }
 /* }}} */
 
