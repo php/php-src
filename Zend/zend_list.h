28cb72f4806ff654822c3c443309a57bfedd4e71
Anatol Belski <ab@php.net>
2013-11-22 08:51:13 +0100

    made zend list to use 64 bit indexes

--- Zend/zend_list.h
+++ Zend/zend_list.h
@@ -33,7 +33,7 @@ BEGIN_EXTERN_C()
 typedef struct _zend_rsrc_list_entry {
 	void *ptr;
 	int type;
-	int refcount;
+	zend_int_t refcount;
 } zend_rsrc_list_entry;
 
 typedef void (*rsrc_dtor_func_t)(zend_rsrc_list_entry *rsrc TSRMLS_DC);
@@ -70,17 +70,17 @@ void zend_destroy_rsrc_list(HashTable *ht TSRMLS_DC);
 int zend_init_rsrc_list_dtors(void);
 void zend_destroy_rsrc_list_dtors(void);
 
-ZEND_API int zend_list_insert(void *ptr, int type TSRMLS_DC);
-ZEND_API int _zend_list_addref(int id TSRMLS_DC);
-ZEND_API int _zend_list_delete(int id TSRMLS_DC);
-ZEND_API void *_zend_list_find(int id, int *type TSRMLS_DC);
+ZEND_API zend_int_t zend_list_insert(void *ptr, int type TSRMLS_DC);
+ZEND_API int _zend_list_addref(zend_int_t id TSRMLS_DC);
+ZEND_API int _zend_list_delete(zend_int_t id TSRMLS_DC);
+ZEND_API void *_zend_list_find(zend_int_t id, int *type TSRMLS_DC);
 
 #define zend_list_addref(id)		_zend_list_addref(id TSRMLS_CC)
 #define zend_list_delete(id)		_zend_list_delete(id TSRMLS_CC)
 #define zend_list_find(id, type)	_zend_list_find(id, type TSRMLS_CC)
 
-ZEND_API int zend_register_resource(zval *rsrc_result, void *rsrc_pointer, int rsrc_type TSRMLS_DC);
-ZEND_API void *zend_fetch_resource(zval **passed_id TSRMLS_DC, int default_id, const char *resource_type_name, int *found_resource_type, int num_resource_types, ...);
+ZEND_API zend_int_t zend_register_resource(zval *rsrc_result, void *rsrc_pointer, int rsrc_type TSRMLS_DC);
+ZEND_API void *zend_fetch_resource(zval **passed_id TSRMLS_DC, zend_int_t default_id, const char *resource_type_name, int *found_resource_type, int num_resource_types, ...);
 
 ZEND_API const char *zend_rsrc_list_get_rsrc_type(int resource TSRMLS_DC);
 ZEND_API int zend_fetch_list_dtor_id(char *type_name);
