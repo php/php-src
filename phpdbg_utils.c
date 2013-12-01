4dda2980ff8dc8c3b9e24c5ecc3d2578ca783418
Felipe Pena <felipensp@gmail.com>
2013-12-01 15:00:28 -0200

    - CS

--- phpdbg_utils.c
+++ phpdbg_utils.c
@@ -246,12 +246,10 @@ PHPDBG_API int phpdbg_rlog(FILE *fp, const char *fmt, ...) { /* {{{ */
 	int rc = 0;
 	
 	va_list args;
-	time_t now;
 	struct timeval tp;
 	
 	va_start(args, fmt);
-	if (gettimeofday(&tp, NULL) == SUCCESS)
-	{
+	if (gettimeofday(&tp, NULL) == SUCCESS) {
 		char friendly[100];
 		char *format = NULL, *buffer = NULL;
 		
