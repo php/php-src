277895717c57a514312a9257ce783d0936dc1c94
Julien Pauli <jpauli@php.net>
2014-01-10 10:01:30 +0100

    5.5.9-dev now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 8
+#define PHP_RELEASE_VERSION 9
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.8-dev"
-#define PHP_VERSION_ID 50508
+#define PHP_VERSION "5.5.9-dev"
+#define PHP_VERSION_ID 50509
