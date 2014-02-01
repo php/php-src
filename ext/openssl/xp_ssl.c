05c309f2d85bcc33e95415d7f50d7748a7c27498
Daniel Lowrey <rdlowrey@php.net>
2014-02-01 08:01:13 -0700

    Remove #if PHP_VERSION_ID version checks

--- ext/openssl/xp_ssl.c
+++ ext/openssl/xp_ssl.c
@@ -407,11 +407,7 @@ static inline int php_openssl_setup_crypto(php_stream *stream,
 
 		if (stream->context && SUCCESS == php_stream_context_get_option(
 					stream->context, "ssl", "no_ticket", &val) && 
-#if PHP_VERSION_ID >= 50700
-				zend_is_true(*val TSRMLS_CC)
-#else
 				zend_is_true(*val)
-#endif
 		) {
 			SSL_CTX_set_options(sslsock->ctx, SSL_OP_NO_TICKET);
 		}
@@ -424,11 +420,7 @@ static inline int php_openssl_setup_crypto(php_stream *stream,
 
 		if (stream->context && SUCCESS == php_stream_context_get_option(
 					stream->context, "ssl", "disable_compression", &val) &&
-#if PHP_VERSION_ID >= 50700
-				zend_is_true(*val TSRMLS_CC)
-#else
 				zend_is_true(*val)
-#endif
 		) {
 			SSL_CTX_set_options(sslsock->ctx, SSL_OP_NO_COMPRESSION);
 		}
@@ -479,11 +471,7 @@ static inline int php_openssl_enable_crypto(php_stream *stream,
 
 		if (sslsock->is_client
 			&& (php_stream_context_get_option(stream->context, "ssl", "SNI_enabled", &val) == FAILURE
-#if PHP_VERSION_ID >= 50700
-				|| zend_is_true(*val TSRMLS_CC))
-#else
 				|| zend_is_true(*val))
-#endif
 		) {
 			if (php_stream_context_get_option(stream->context, "ssl", "SNI_server_name", &val) == SUCCESS) {
 				convert_to_string_ex(val);
@@ -590,11 +578,7 @@ static inline int php_openssl_enable_crypto(php_stream *stream,
 					if (SUCCESS == php_stream_context_get_option(
 								stream->context, "ssl",
 								"capture_peer_cert", &val) &&
-#if PHP_VERSION_ID >= 50700
-							zend_is_true(*val TSRMLS_CC)) {
-#else
 							zend_is_true(*val)) {
-#endif
 						MAKE_STD_ZVAL(zcert);
 						ZVAL_RESOURCE(zcert, zend_list_insert(peer_cert, 
 									php_openssl_get_x509_list_id() TSRMLS_CC));
@@ -608,11 +592,7 @@ static inline int php_openssl_enable_crypto(php_stream *stream,
 					if (SUCCESS == php_stream_context_get_option(
 								stream->context, "ssl",
 								"capture_peer_cert_chain", &val) &&
-#if PHP_VERSION_ID >= 50700
-							zend_is_true(*val TSRMLS_CC)) {
-#else
 							zend_is_true(*val)) {
-#endif
 						zval *arr;
 						STACK_OF(X509) *chain;
 
