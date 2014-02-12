e2fc6b52f04ce2bf2295011d8b768cc0d8097ed7
Dmitry Panin <pank.dm@gmail.com>
2014-02-11 16:39:27 -0800

    Fix HTML entity table generation

--- ext/standard/html_tables/html_table_gen.php
+++ ext/standard/html_tables/html_table_gen.php
@@ -461,6 +461,7 @@ if (empty($multicp_rows))
 
 ksort($multicp_rows);
 foreach ($multicp_rows as &$v) { ksort($v); }
+unset($v);
 
 echo
 "/* {{{ Start of double code point tables for $name */", "\n\n";
