35101e9ef4669bb076f6687f2fd1aa13f63695d0
Veres Lajos <vlajos@gmail.com>
2014-02-14 14:51:10 +0200

    a few typofixes

--- ext/oci8/php_oci8_int.h
+++ ext/oci8/php_oci8_int.h
@@ -191,7 +191,7 @@ typedef struct { /* php_oci_statement {{{ */
 	sword				 errcode;				/* last errcode*/
 	OCIError			*err;					/* private error handle */
 	OCIStmt				*stmt;					/* statement handle */
-	char				*last_query;			/* last query issued. also used to determine if this is a statement or a refcursor recieved from Oracle */
+	char				*last_query;			/* last query issued. also used to determine if this is a statement or a refcursor received from Oracle */
 	long				 last_query_len;		/* last query length */
 	HashTable			*columns;				/* hash containing all the result columns */
 	HashTable			*binds;					/* binds hash */
