7c623c0e89e2e5798bc40e3cb48e4151f2eab1ca
Xinchen Hui <laruence@gmail.com>
2014-03-07 15:12:33 +0800

    Fixed asseration failed in ob_get_status

--- main/output.c
+++ main/output.c
@@ -1170,9 +1170,9 @@ static int php_output_stack_apply_list(void *h, void *z)
 static int php_output_stack_apply_status(void *h, void *z)
 {
 	php_output_handler *handler = *(php_output_handler **) h;
-	zval *array = (zval *) z;
+	zval arr, *array = (zval *) z;
 
-	add_next_index_zval(array, php_output_handler_status(handler, NULL));
+	add_next_index_zval(array, php_output_handler_status(handler, &arr));
 
 	return 0;
 }
