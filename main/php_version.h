b8d029435dd436487a455a758e8dc10d1c94096e
Julien Pauli <jpauli@php.net>
2014-04-15 17:57:27 +0200

    5.5.12RC1 setup

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 12
+#define PHP_RELEASE_VERSION 13
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.12-dev"
-#define PHP_VERSION_ID 50512
+#define PHP_VERSION "5.5.13-dev"
+#define PHP_VERSION_ID 50513
