9343f874cd9e1a006c897c1dc8d5a334e73af942
Nikita Popov <nikic@php.net>
2014-05-09 15:46:43 +0200

    Fix identical comparison of arrays with references
    Also commit a test I forgot.

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -1890,6 +1890,8 @@ static int hash_zval_identical_function(zval *z1, zval *z2) /* {{{ */
 	 * whereas this comparison function is expected to return 0 on identity,
 	 * and non zero otherwise.
 	 */
+	ZVAL_DEREF(z1);
+	ZVAL_DEREF(z2);
 	if (is_identical_function(&result, z1, z2 TSRMLS_CC)==FAILURE) {
 		return 1;
 	}
