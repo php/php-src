3c328f09840c58698cedd6bbd30bdc8a24f5b41f
Stanislav Malyshev <stas@php.net>
2014-05-11 19:34:21 -0700

    Fix bug #67251 - date_parse_from_format out-of-bounds read
    Conflicts:
    	ext/date/lib/parse_date.c
    	ext/date/lib/parse_date.re

--- ext/date/lib/parse_date.c
+++ ext/date/lib/parse_date.c
@@ -25121,7 +25121,11 @@ timelib_time *timelib_parse_from_format(char *format, char *string, int len, tim
 				break;
 
 			case '\\': /* escaped char */
-				*fptr++;
+                if(!fptr[1]) {
+				    add_pbf_error(s, "Escaped character expected", string, begin);
+                    break;
+                }
+				fptr++;
 				if (*ptr == *fptr) {
 					++ptr;
 				} else {
