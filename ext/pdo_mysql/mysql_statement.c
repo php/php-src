4e521c92ec7c713bb1bdf0f25593d8d1871ae883
Dmitry Stogov <dmitry@zend.com>
2014-05-19 11:52:33 +0400

    Prevent crash in case of SQL error

--- ext/pdo_mysql/mysql_statement.c
+++ ext/pdo_mysql/mysql_statement.c
@@ -404,7 +404,9 @@ static int pdo_mysql_stmt_next_rowset(pdo_stmt_t *stmt TSRMLS_DC) /* {{{ */
 /* ensure that we free any previous unfetched results */
 #ifndef PDO_USE_MYSQLND
 	if (S->stmt) {
-		stmt->column_count = (int)mysql_num_fields(S->result);
+		if (S->result) {
+			stmt->column_count = (int)mysql_num_fields(S->result);
+		}
 		mysql_stmt_free_result(S->stmt);
 	}
 #endif
