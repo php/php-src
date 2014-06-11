0235a8acdc9b99d4d3d4dd107b75d7eb6ca0ef97
Stanislav Malyshev <stas@php.net>
2014-06-10 21:17:37 -0700

    5.4.30 rc1

--- main/php_version.h
+++ main/php_version.h
@@ -4,6 +4,6 @@
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 30
 
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.30-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "5.4.30RC1"
 #define PHP_VERSION_ID 50430
