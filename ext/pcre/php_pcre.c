6ea228722b2b928eae3b2c30143eda6290fe1a7b
Dmitry Stogov <dmitry@zend.com>
2014-06-20 15:04:25 +0400

    fixed memory leak

--- ext/pcre/php_pcre.c
+++ ext/pcre/php_pcre.c
@@ -91,7 +91,9 @@ static void php_free_pcre_cache(zval *data) /* {{{ */
 	pcre_cache_entry *pce = (pcre_cache_entry *) Z_PTR_P(data);
 	if (!pce) return;
 	pefree(pce->re, 1);
-	if (pce->extra) pefree(pce->extra, 1);
+	if (pce->extra) {
+		pcre_free_study(pce->extra);
+	}
 #if HAVE_SETLOCALE
 	if ((void*)pce->tables) pefree((void*)pce->tables, 1);
 	pefree(pce->locale, 1);
