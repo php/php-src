php/php-src

commit 2d9885c8cbbc2199bac4801eaef7c18c76f28033

Fix handling of session user module custom handlers.
According to the documentation, returning TRUE from
user based session handlers should indicate success,
while returning FALSE should indicate failure.

The existing logic relied on casting the return value
to an integer and returning that from the function.
However, the internal handlers use SUCCESS/FAILURE
where SUCCESS == 0, and FAILURE == -1, so the following
behavior map occurs:

  return false; => return 0; => return SUCCESS
  return true; => return 1; => return <undefined>

Since the session API checks against FAILURE,
both boolean responses wind up appearing like "not FAILURE".

This diff reasserts boolean responses to behave as
documented and introduces some special handling
for integer responses of 0 and -1 so that code can be
written for older and newer versions of PHP.

ext/session/mod_user.c
@@ -68,12 +68,22 @@ static zval *ps_call_handler(zval *func, int argc, zval **argv TSRMLS_DC)
 
 #define PSF(a) PS(mod_user_names).name.ps_##a
 
-#define FINISH								\
-	if (retval) {							\
-		convert_to_long(retval);			\
-		ret = Z_LVAL_P(retval);				\
-		zval_ptr_dtor(&retval);				\
-	}										\
+#define FINISH \
+	if (retval) { \
+		if (Z_TYPE_P(retval) == IS_BOOL) { \
+			ret = Z_BVAL_P(retval) ? SUCCESS : FAILURE; \
+                }  else if ((Z_TYPE_P(retval) == IS_LONG) && (Z_LVAL_P(retval) == -1)) { \
+			/* BC for clever users - Deprecate me */ \
+			ret = FAILURE; \
+		} else if ((Z_TYPE_P(retval) == IS_LONG) && (Z_LVAL_P(retval) == 0)) { \
+			/* BC for clever users - Deprecate me */ \
+			ret = SUCCESS; \
+		} else { \
+			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Session callback expects true/false return value"); \
+			ret = FAILURE; \
+		} \
+		zval_ptr_dtor(&retval); \
+	} \
 	return ret
 
 PS_OPEN_FUNC(user)

commit 7c2489751cc48961487edd69b49083d4ca6b4828

Followup fix to custom session save handlers
2d9885c introduced some regressions.  This addresses those.

  * Don't throw return type notice or session write failure when in an exception
  * Fix tests to properly return true/false since null is no longer falsy/successy
  * Rerecord a few tests to accomodate difference in raised warnings

ext/session/mod_user.c
@@ -79,7 +79,10 @@ static zval *ps_call_handler(zval *func, int argc, zval **argv TSRMLS_DC)
 			/* BC for clever users - Deprecate me */ \
 			ret = SUCCESS; \
 		} else { \
-			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Session callback expects true/false return value"); \
+			if (!EG(exception)) { \
+				php_error_docref(NULL TSRMLS_CC, E_WARNING, \
+				                 "Session callback expects true/false return value"); \
+			} \
 			ret = FAILURE; \
 		} \
 		zval_ptr_dtor(&retval); \

ext/session/session.c
@@ -566,7 +566,7 @@ static void php_session_save_current_state(TSRMLS_D) /* {{{ */
 			}
 		}
 
-		if (ret == FAILURE) {
+		if ((ret == FAILURE) && !EG(exception)) {
 			php_error_docref(NULL TSRMLS_CC, E_WARNING, "Failed to write session data (%s). Please "
 					"verify that the current setting of session.save_path "
 					"is correct (%s)",

ext/session/tests/bug60634_error_1.phpt
@@ -16,6 +16,7 @@ function open($save_path, $session_name) {
 
 function close() {
 	echo "close: goodbye cruel world\n";
+	return true;
 }
 
 function read($id) {

ext/session/tests/bug60634_error_2.phpt
@@ -16,6 +16,7 @@ function open($save_path, $session_name) {
 
 function close() {
 	echo "close: goodbye cruel world\n";
+	return true;
 }
 
 function read($id) {

ext/session/tests/save_handler.inc
@@ -31,7 +31,7 @@ function write($id, $session_data) {
     if ($fp = fopen($session_file, "w")) {
         $return = fwrite($fp, $session_data);
         fclose($fp);
-        return $return;
+        return (bool)$return;
     }
     return false;
 }
@@ -40,7 +40,8 @@ function destroy($id) {
     global $session_save_path, $name;
     echo "Destroy [${session_save_path},${id}]\n";
     $session_file = "$session_save_path/".SESSION_FILE_PREFIX.$id;
-    return unlink($session_file);
+    unlink($session_file);
+    return true;
 }
 
 function gc($maxlifetime) {

ext/session/tests/session_module_name_variation3.phpt
@@ -21,11 +21,11 @@ function open($save_path, $session_name) {
     throw new Exception("Stop...!");
 }
 
-function close() { }
-function read($id) { }
-function write($id, $session_data) { }
-function destroy($id) { }
-function gc($maxlifetime) { }
+function close() { return true; }
+function read($id) { return ''; }
+function write($id, $session_data) { return true; }
+function destroy($id) { return true; }
+function gc($maxlifetime) { return true; }
 
 var_dump(session_module_name("files"));
 session_set_save_handler("open", "close", "read", "write", "destroy", "gc");
@@ -41,9 +41,11 @@ ob_end_flush();
 string(%d) "%s"
 string(4) "user"
 
-Fatal error: Uncaught exception 'Exception' with message 'Stop...!' in %s:%d
+Warning: Uncaught exception 'Exception' with message 'Stop...!' in %s:%d
 Stack trace:
 #0 [internal function]: open('', 'PHPSESSID')
 #1 %s(%d): session_start()
 #2 {main}
   thrown in %s on line %d
+
+Fatal error: session_start(): Failed to initialize storage module: %s in %s/session_module_name_variation3.php on line %d

ext/session/tests/session_set_save_handler_class_002.phpt
@@ -38,11 +38,12 @@ class MySession2 extends SessionHandler {
 	}
 
 	public function write($id, $data) {
-		return file_put_contents($this->path . $id, $data);
+		return (bool)file_put_contents($this->path . $id, $data);
 	}
 
 	public function destroy($id) {
 		@unlink($this->path . $id);
+		return true;
 	}
 
 	public function gc($maxlifetime) {

ext/session/tests/session_set_save_handler_class_005.phpt
@@ -52,4 +52,6 @@ array(0) {
 
 Warning: SessionHandler::write(): Parent session handler is not open in %ssession_set_save_handler_class_005.php on line %d
 
+Warning: session_write_close(): Failed to write session data %s in %ssession_set_save_handler_class_005.php on line %d
+
 Warning: SessionHandler::close(): Parent session handler is not open in %ssession_set_save_handler_class_005.php on line %d

ext/session/tests/session_set_save_handler_class_012.phpt
@@ -24,7 +24,9 @@ class MySession extends SessionHandler {
 	public function open($path, $name) {
 		++$this->i;
 		echo 'Open ', session_id(), "\n";
-		return parent::open();
+		// This test was written for broken return value handling
+		// Mimmick what was actually being tested by returning true here
+		return (null === parent::open());
 	}
 	public function read($key) {
 		++$this->i;
@@ -56,5 +58,7 @@ array(0) {
 }
 
 Warning: Unknown: Parent session handler is not open in Unknown on line 0
+
+Warning: Unknown: Failed to write session data %s in %s on line %d
 
 Warning: Unknown: Parent session handler is not open in Unknown on line 0

ext/session/tests/session_set_save_handler_class_016.phpt
@@ -38,7 +38,7 @@ class MySession2 extends SessionHandler {
 	}
 
 	public function write($id, $data) {
-		return file_put_contents($this->path . $id, $data);
+		return (bool)file_put_contents($this->path . $id, $data);
 	}
 
 	public function destroy($id) {

ext/session/tests/session_set_save_handler_error3.phpt
@@ -34,9 +34,11 @@ ob_end_flush();
 --EXPECTF--
 *** Testing session_set_save_handler() : error functionality ***
 
-Fatal error: Uncaught exception 'Exception' with message 'Do something bad..!' in %s:%d
+Warning: Uncaught exception 'Exception' with message 'Do something bad..!' in %s:%d
 Stack trace:
 #0 [internal function]: open('', 'PHPSESSID')
 #1 %s(%d): session_start()
 #2 {main}
   thrown in %s on line %d
+
+Fatal error: session_start(): Failed to initialize storage module: %s in %ssession_set_save_handler_error3.php on line %d

ext/session/tests/session_set_save_handler_error4.phpt
@@ -15,7 +15,7 @@ ob_start();
 
 echo "*** Testing session_set_save_handler() : error functionality ***\n";
 
-function callback() { }
+function callback() { return true; }
 
 session_set_save_handler("callback", "callback", "callback", "callback", "callback", "callback");
 session_set_save_handler("callback", "echo", "callback", "callback", "callback", "callback");

ext/session/tests/session_set_save_handler_iface_001.phpt
@@ -38,7 +38,7 @@ class MySession2 implements SessionHandlerInterface {
 	}
 
 	public function write($id, $data) {
-		return file_put_contents($this->path . $id, $data);
+		return (bool)file_put_contents($this->path . $id, $data);
 	}
 
 	public function destroy($id) {

ext/session/tests/session_set_save_handler_variation4.phpt
@@ -24,6 +24,7 @@ echo "*** Testing session_set_save_handler() : variation ***\n";
 function noisy_gc($maxlifetime) {
 	echo("GC [".$maxlifetime."]\n");
 	gc($maxlifetime);
+	return true;
 }
 
 require_once "save_handler.inc";