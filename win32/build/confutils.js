02b661ecfcc6ddfbeb75b8226690eac5786d6189
Anatol Belski <ab@php.net>
2014-07-10 22:33:55 +0200

    fix end of stream exception when generating makefile

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -1806,7 +1806,9 @@ function generate_makefile()
 
 	MFO.Close();
 	TF = FSO.OpenTextFile("Makefile.objects", 1);
-	MF.Write(TF.ReadAll());
+	if (!TF.AtEndOfStream) {
+		MF.Write(TF.ReadAll());
+	}
 	TF.Close();
 
 	MF.Close();	
