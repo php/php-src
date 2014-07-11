2d7625a1b8e354efeb919a25b0e01636116d7f79
George Wang <gwang@php.net>
2014-07-11 14:31:59 -0400

    Fixed a bug that cannot access custom request header stored in apache_request_headers() though array index.

--- sapi/litespeed/lsapilib.c
+++ sapi/litespeed/lsapilib.c
@@ -2129,6 +2129,7 @@ int LSAPI_ForeachOrgHeader_r( LSAPI_Request * pReq,
         {
             pKey = pReq->m_pHttpHeader + pCur->nameOff;
             keyLen = pCur->nameLen;
+            *(pKey + keyLen ) = 0;
 
             pValue = pReq->m_pHttpHeader + pCur->valueOff;
             *(pValue + pCur->valueLen ) = 0;
