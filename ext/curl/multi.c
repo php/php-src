acd0d7b4037de9498ee5686b90b349342db08f4c
Jille Timmermans <jille@quis.cx>
2014-07-17 17:21:52 +0200

    Fixed bug #67643 (curl_multi_getcontent returns '' when CURLOPT_RETURNTRANSFER isn't set)

--- ext/curl/multi.c
+++ ext/curl/multi.c
@@ -244,12 +244,15 @@ PHP_FUNCTION(curl_multi_getcontent)
 
 	ZEND_FETCH_RESOURCE(ch, php_curl *, &z_ch, -1, le_curl_name, le_curl);
 
-	if (ch->handlers->write->method == PHP_CURL_RETURN && ch->handlers->write->buf.len > 0) {
+	if (ch->handlers->write->method == PHP_CURL_RETURN) {
+		if (ch->handlers->write->buf.len == 0) {
+			RETURN_EMPTY_STRING();
+		}
 		smart_str_0(&ch->handlers->write->buf);
 		RETURN_STRINGL(ch->handlers->write->buf.c, ch->handlers->write->buf.len, 1);
 	}
 
-        RETURN_EMPTY_STRING();
+	RETURN_NULL();
 }
 /* }}} */
 
