75d54c13ca2191123b03608db9f685f78e5ebe85
George Brighton <george@gebn.co.uk>
2014-08-04 12:01:33 +0100

    Improved readability of php_get_display_errors_mode().

--- main/main.c
+++ main/main.c
@@ -332,28 +332,26 @@ static int php_get_display_errors_mode(char *value, int value_length)
 {
 	int mode;
 
-	if (!value) {
+	if (!value ||
+			value_length == 2 && !strcasecmp("on", value) ||
+			value_length == 3 && !strcasecmp("yes", value) ||
+			value_length == 4 && !strcasecmp("true", value) ||
+			value_length == 6 && !strcasecmp("stdout", value)) {
 		return PHP_DISPLAY_ERRORS_STDOUT;
 	}
 
-	if (value_length == 2 && !strcasecmp("on", value)) {
-		mode = PHP_DISPLAY_ERRORS_STDOUT;
-	} else if (value_length == 3 && !strcasecmp("yes", value)) {
-		mode = PHP_DISPLAY_ERRORS_STDOUT;
-	} else if (value_length == 4 && !strcasecmp("true", value)) {
-		mode = PHP_DISPLAY_ERRORS_STDOUT;
-	} else if (value_length == 6 && !strcasecmp(value, "stderr")) {
-		mode = PHP_DISPLAY_ERRORS_STDERR;
-	} else if (value_length == 6 && !strcasecmp(value, "stdout")) {
-		mode = PHP_DISPLAY_ERRORS_STDOUT;
-	} else {
-		mode = atoi(value);
-		if (mode && mode != PHP_DISPLAY_ERRORS_STDOUT && mode != PHP_DISPLAY_ERRORS_STDERR) {
-			mode = PHP_DISPLAY_ERRORS_STDOUT;
-		}
+	if (value_length == 6 && !strcasecmp("stderr", value)) {
+		return PHP_DISPLAY_ERRORS_STDERR;
 	}
 
-	return mode;
+	mode = atoi(value);
+	if (!mode ||
+			mode == PHP_DISPLAY_ERRORS_STDOUT ||
+			mode == PHP_DISPLAY_ERRORS_STDERR) {
+		return mode;
+	}
+
+	return PHP_DISPLAY_ERRORS_STDOUT;
 }
 /* }}} */
 
