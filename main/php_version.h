65cf73c93113cec7e4bca46ce8393c85f5e21fc5
Stanislav Malyshev <stas@php.net>
2014-08-05 21:24:55 -0700

    5.4.33-dev now

--- main/php_version.h
+++ main/php_version.h
@@ -2,8 +2,8 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 32
+#define PHP_RELEASE_VERSION 33
 
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.32-dev"
-#define PHP_VERSION_ID 50432
+#define PHP_VERSION "5.4.33-dev"
+#define PHP_VERSION_ID 50433
