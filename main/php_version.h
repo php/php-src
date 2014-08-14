1355ea60ccc3d2b289e5ec72877adc3a99ef9f78
Johannes Schlüter <johannes@php.net>
2014-08-14 17:05:20 +0200

    Back to -dev (with EOL notice in NEWS)

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 29
-#define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "5.3.29"
-#define PHP_VERSION_ID 50329
+#define PHP_RELEASE_VERSION 30
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.3.30-dev"
+#define PHP_VERSION_ID 50330
