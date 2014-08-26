baee5922c35d280d12bad645538ae445e73f7a30
Anatol Belski <ab@php.net>
2014-08-26 09:52:22 +0200

    edit comment

--- Zend/zend_long.h
+++ Zend/zend_long.h
@@ -24,8 +24,7 @@
 
 #include "main/php_stdint.h"
 
-/* This is the heart of the whole int64 enablement in zval. Based on this
-   macros the size_t datatype is handled, see zend_types.h */
+/* This is the heart of the whole int64 enablement in zval. */
 #if defined(__X86_64__) || defined(__LP64__) || defined(_LP64) || defined(_WIN64)
 # define ZEND_ENABLE_INT64 1
 #endif
