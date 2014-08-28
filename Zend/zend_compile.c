90576d9db22d363d8c8d20fd763c43c5d29fe820
Andrea Faulds <ajf@ajf.me>
2014-08-28 02:42:27 +0100

    oops

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -4276,7 +4276,7 @@ int zend_try_compile_special_func(
 		return zend_compile_func_typecheck(result, args, IS_NULL TSRMLS_CC);
 	} else if (zend_string_equals_literal(lcname, "is_bool")) {
 		return zend_compile_func_typecheck(result, args, _IS_BOOL TSRMLS_CC);
-	} else if (zend_string_equals_literal(lcname, "is_long") || zend_string_equals_literal(lcname, "is_int" || zend_string_equals_literal(lcname, "is_long"))) {
+	} else if (zend_string_equals_literal(lcname, "is_long") || zend_string_equals_literal(lcname, "is_int" || zend_string_equals_literal(lcname, "is_integer"))) {
 		return zend_compile_func_typecheck(result, args, IS_BIGINT_OR_LONG TSRMLS_CC);
 	} else if (zend_string_equals_literal(lcname, "is_float")) {
 		return zend_compile_func_typecheck(result, args, IS_DOUBLE TSRMLS_CC);
