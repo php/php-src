ca042bb18cad305266bcc10086115f2d24000342
Johannes Schlüter <johannes@php.net>
2014-08-30 00:02:31 +0200

    Use global symbol table for completion after $ in readline.

--- ext/readline/readline_cli.c
+++ ext/readline/readline_cli.c
@@ -435,7 +435,7 @@ static char *cli_completion_generator_ht(const char *text, int textlen, int *sta
 static char *cli_completion_generator_var(const char *text, int textlen, int *state TSRMLS_DC) /* {{{ */
 {
 	char *retval, *tmp;
-	zend_array *symbol_table = zend_rebuild_symbol_table(TSRMLS_C);
+	zend_array *symbol_table = &EG(symbol_table);
 
 	tmp = retval = cli_completion_generator_ht(text + 1, textlen - 1, state, symbol_table ? &symbol_table->ht : NULL, NULL TSRMLS_CC);
 	if (retval) {
