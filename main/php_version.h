9266227402f50e1ad9953952b68a5ff3a25806b0
Julien Pauli <jpauli@php.net>
2014-09-03 10:18:51 +0200

    5.5.18 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 17
+#define PHP_RELEASE_VERSION 18
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.17-dev"
-#define PHP_VERSION_ID 50517
+#define PHP_VERSION "5.5.18-dev"
+#define PHP_VERSION_ID 50518
