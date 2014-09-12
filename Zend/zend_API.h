993d4751260b1e1fbc62dcaeea98fc4988e925b6
Anatol Belski <ab@php.net>
2014-09-13 00:03:42 +0200

    fix condition
    A char array like val[1] would always be valid when checking like
    !val. Probably better were to check for ->len, but actually ->val[0]
    is fine as the actual zend_string should have been initalized
    before.

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -1193,7 +1193,7 @@ static zend_always_inline int _z_param_string(zval *arg, char **dest, size_t *de
 static zend_always_inline int _z_param_path_str(zval *arg, zend_string **dest, int check_null TSRMLS_DC)
 {
 	if (!_z_param_str(arg, dest, check_null TSRMLS_CC) ||
-		(check_null && UNEXPECTED(!(*dest)->val)) ||
+		(check_null && UNEXPECTED(!(*dest)->val[0])) ||
 	    UNEXPECTED(CHECK_NULL_PATH((*dest)->val, (*dest)->len))) {
 		return 0;
 	}
