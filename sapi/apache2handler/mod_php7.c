9e512db9efda35d5b63f9d1880d5cd877cc28469
jfha73 <jfha73@gmail.com>
2014-09-18 13:21:09 -0400

    Update and rename mod_php5.c to mod_php7.c
    Change of php5 for php7

--- sapi/apache2handler/mod_php7.c
+++ sapi/apache2handler/mod_php7.c
@@ -1,6 +1,6 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP Version 5                                                        |
+   | PHP Version 7                                                        |
    +----------------------------------------------------------------------+
    | Copyright (c) 1997-2014 The PHP Group                                |
    +----------------------------------------------------------------------+
@@ -25,7 +25,7 @@
 #include "php.h"
 #include "php_apache.h"
 
-AP_MODULE_DECLARE_DATA module php5_module = {
+AP_MODULE_DECLARE_DATA module php7_module = {
 	STANDARD20_MODULE_STUFF,
 	create_php_config,		/* create per-directory config structure */
 	merge_php_config,		/* merge per-directory config structures */
