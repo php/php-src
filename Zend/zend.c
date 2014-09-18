21514b0d9a9479430d5f70b6f1c27a703138f113
Dmitry Stogov <dmitry@zend.com>
2014-09-19 00:38:34 +0400

    Fix for Clang

--- Zend/zend.c
+++ Zend/zend.c
@@ -1287,7 +1287,7 @@ static void zend_error_va_list(int type, const char *format, va_list args)
 }
 /* }}} */
 
-#if defined(__GNUC__) && __GNUC__ >= 3 && !defined(__INTEL_COMPILER) && !defined(DARWIN) && !defined(__hpux) && !defined(_AIX) && !defined(__osf__)
+#if (defined(__GNUC__) && __GNUC__ >= 3 && !defined(__INTEL_COMPILER) && !defined(DARWIN) && !defined(__hpux) && !defined(_AIX) && !defined(__osf__)) || __has_attribute(noreturn)
 void zend_error_noreturn(int type, const char *format, ...) __attribute__ ((alias("zend_error"),noreturn));
 #elif defined(ZEND_WIN32)
 ZEND_API void zend_error(int type, const char *format, ...) /* {{{ */
