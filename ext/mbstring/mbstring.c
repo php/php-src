bae92295fd7bf54b8b3e8440c1ce705e6e5e2119
Anatol Belski <ab@php.net>
2014-09-19 10:09:37 +0200

    remove useless condition

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -4195,7 +4195,7 @@ PHP_FUNCTION(mb_send_mail)
 	}
 
 	/* Subject: */
-	if (subject != NULL && subject_len >= 0) {
+	if (subject != NULL) {
 		orig_str.no_language = MBSTRG(language);
 		orig_str.val = (unsigned char *)subject;
 		orig_str.len = subject_len;
