8d553829cc7ee052e5def4dff3c8f4e9e4f570f2
Stanislav Malyshev <stas@php.net>
2014-10-14 11:42:33 -0700

    Fix return code (merges are hard :( )

--- ext/curl/interface.c
+++ ext/curl/interface.c
@@ -171,7 +171,7 @@ static int php_curl_option_str(php_curl *ch, long option, const char *str, const
 
 	if (strlen(str) != len) {
 		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Curl option contains invalid characters (\\0)");
-		return 0;
+		return FAILURE;
 	}
 
 #if LIBCURL_VERSION_NUM >= 0x071100
