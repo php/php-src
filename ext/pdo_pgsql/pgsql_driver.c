8c5e2e66bcbb6fa24db6515a09be36bb4bbf0860
Matteo Beccati <mbeccati@php.net>
2014-10-17 16:22:52 +0200

    Fixed bug #68199 (PDO::pgsqlGetNotify doesn't support NOTIFY payloads)

--- ext/pdo_pgsql/pgsql_driver.c
+++ ext/pdo_pgsql/pgsql_driver.c
@@ -1058,10 +1058,16 @@ static PHP_METHOD(PDO, pgsqlGetNotify)
 	if (result_type == PDO_FETCH_NUM || result_type == PDO_FETCH_BOTH) {
 		add_index_string(return_value, 0, pgsql_notify->relname, 1);
 		add_index_long(return_value, 1, pgsql_notify->be_pid);
+		if (pgsql_notify->extra && pgsql_notify->extra[0]) {
+			add_index_string(return_value, 2, pgsql_notify->extra, 1);
+		}
 	}
 	if (result_type == PDO_FETCH_ASSOC || result_type == PDO_FETCH_BOTH) {
 		add_assoc_string(return_value, "message", pgsql_notify->relname, 1);
 		add_assoc_long(return_value, "pid", pgsql_notify->be_pid);
+		if (pgsql_notify->extra && pgsql_notify->extra[0]) {
+			add_assoc_string(return_value, "payload", pgsql_notify->extra, 1);
+		}
 	}
 
 	PQfreemem(pgsql_notify);
