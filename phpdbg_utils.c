b0089e3c2f649af08d27acf6762c4f4a166f9710
Bob Weinand <bobwei9@hotmail.com>
2014-10-24 13:11:14 +0200

    Do not print out xml as PHP print...

--- phpdbg_utils.c
+++ phpdbg_utils.c
@@ -583,10 +583,10 @@ PHPDBG_API void phpdbg_xml_var_dump(zval **zv TSRMLS_DC) {
 				phpdbg_xml("<int refstatus=\"%s\" value=\"%ld\" />", COMMON, Z_LVAL_PP(zv));
 				break;
 			case IS_DOUBLE:
-				php_printf("<float refstatus=\"%s\" value=\"%.*G\" />", COMMON, (int) EG(precision), Z_DVAL_PP(zv));
-					break;
+				phpdbg_xml("<float refstatus=\"%s\" value=\"%.*G\" />", COMMON, (int) EG(precision), Z_DVAL_PP(zv));
+				break;
 			case IS_STRING:
-				php_printf("<string refstatus=\"%s\" length=\"%d\" value=\"%.*s\" />", COMMON, Z_STRLEN_PP(zv), Z_STRLEN_PP(zv), Z_STRVAL_PP(zv));
+				phpdbg_xml("<string refstatus=\"%s\" length=\"%d\" value=\"%.*s\" />", COMMON, Z_STRLEN_PP(zv), Z_STRLEN_PP(zv), Z_STRVAL_PP(zv));
 					break;
 			case IS_ARRAY:
 				myht = Z_ARRVAL_PP(zv);
@@ -601,9 +601,9 @@ PHPDBG_API void phpdbg_xml_var_dump(zval **zv TSRMLS_DC) {
 				goto head_done;
 			case IS_OBJECT:
 				myht = Z_OBJDEBUG_PP(zv, is_temp);
-					if (myht && ++myht->nApplyCount > 1) {
+				if (myht && ++myht->nApplyCount > 1) {
 					phpdbg_xml("<recursion />");
-						--myht->nApplyCount;
+					--myht->nApplyCount;
 					break;
 				}
 	
@@ -621,11 +621,11 @@ head_done:
 					--myht->nApplyCount;
 					if (is_temp) {
 						zend_hash_destroy(myht);
-							efree(myht);
+						efree(myht);
 					}
 				}
 				if (Z_TYPE_PP(zv) == IS_ARRAY) {
-						phpdbg_xml("</array>");
+					phpdbg_xml("</array>");
 				} else {
 					phpdbg_xml("</object>");
 				}
@@ -634,7 +634,7 @@ head_done:
 				const char *type_name = zend_rsrc_list_get_rsrc_type(Z_LVAL_PP(zv) TSRMLS_CC);
 				phpdbg_xml("<resource refstatus=\"%s\" id=\"%ld\" type=\"%ld\" />", COMMON, Z_LVAL_PP(zv), type_name ? type_name : "unknown");
 				break;
-				}
+			}
 			default:
 				break;
 		}
