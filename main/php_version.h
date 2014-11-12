45e4e84c90903992b82d75df4267107b08a2e0b8
Stanislav Malyshev <stas@php.net>
2014-11-11 16:29:14 -0800

    5.4.35

--- main/php_version.h
+++ main/php_version.h
@@ -4,6 +4,6 @@
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 35
 
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.35-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.4.35"
 #define PHP_VERSION_ID 50435
