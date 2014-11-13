0f7f800d8a6570ba64c9da175a5ba9715edae7ce
Anatol Belski <ab@php.net>
2014-11-13 12:31:29 +0100

    added missing type specifier

--- win32/winutil.h
+++ win32/winutil.h
@@ -20,7 +20,7 @@ PHPAPI char *php_win32_error_to_msg(HRESULT error);
 
 #define php_win_err()	php_win32_error_to_msg(GetLastError())
 int php_win32_check_trailing_space(const char * path, const int path_len);
-PHPAPI php_win32_get_random_bytes(unsigned char *buf, size_t size);
+PHPAPI int php_win32_get_random_bytes(unsigned char *buf, size_t size);
 
 #ifdef ZTS
 void php_win32_init_rng_lock();
