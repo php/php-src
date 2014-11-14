b7eee5bfaf12588d9b3558c82f35ea9d14791a02
Anatol Belski <ab@php.net>
2014-11-14 10:20:03 +0100

    added comment

--- Zend/zend_portability.h
+++ Zend/zend_portability.h
@@ -394,6 +394,7 @@ char *alloca();
 #define ZEND_SECURE_ZERO(var, size) memset((var), 0, (size))
 #endif
 
+/* This check should only be used on network socket, not file descriptors */
 #ifdef ZEND_WIN32
 #define ZEND_VALID_SOCKET(sock) (INVALID_SOCKET != (sock))
 #else
