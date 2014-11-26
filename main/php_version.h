3656349990cf67b32df8155901e76a88ea2cea44
Julien Pauli <jpauli@php.net>
2014-11-26 11:31:58 +0100

    5.5.21 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 20
+#define PHP_RELEASE_VERSION 21
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.20-dev"
-#define PHP_VERSION_ID 50520
+#define PHP_VERSION "5.5.21-dev"
+#define PHP_VERSION_ID 50521
