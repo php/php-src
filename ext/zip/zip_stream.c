8a065c554be63154125ef2bdd33e350d17a3f26f
Andrea Faulds <ajf@ajf.me>
2014-12-05 13:41:38 +0000

    Restored zip/oci8 PHP 4 code, add PHP 7 checks

--- ext/zip/zip_stream.c
+++ ext/zip/zip_stream.c
@@ -22,6 +22,7 @@
 #endif
 #include "php.h"
 #if HAVE_ZIP
+#if defined(ZEND_ENGINE_2) || defined(ZEND_ENGINE_3)
 
 #include "php_streams.h"
 #include "ext/standard/file.h"
@@ -347,4 +348,5 @@ php_stream_wrapper php_stream_zip_wrapper = {
 	NULL,
 	0 /* is_url */
 };
+#endif /* defined(ZEND_ENGINE_2) || defined(ZEND_ENGINE_3) */
 #endif /* HAVE_ZIP */
