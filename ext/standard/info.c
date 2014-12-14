ad01fd8b49c6117d5c5fce98871c41781a5c00b9
Kalle Sommer Nielsen <kalle@php.net>
2014-12-14 23:09:13 +0100

    Fixed bug #55415 (php_info produces invalid anchor names)
    @@ Slightly based off Johannes' patch

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -146,7 +146,13 @@ PHPAPI void php_info_print_module(zend_module_entry *zend_module TSRMLS_DC) /* {
 {
 	if (zend_module->info_func || zend_module->version) {
 		if (!sapi_module.phpinfo_as_text) {
-			php_info_printf("<h2><a name=\"module_%s\">%s</a></h2>\n", zend_module->name, zend_module->name);
+			int len = 0;
+			zend_string *url_name = php_url_encode(zend_module->name, strlen(zend_module->name), &len);
+
+			php_strtolower(url_name->val, url_name->len);
+			php_info_printf("<h2><a name=\"module_%s\">%s</a></h2>\n", url_name->val, zend_module->name);
+
+			efree(url_name);
 		} else {
 			php_info_print_table_start();
 			php_info_print_table_header(1, zend_module->name);
