caf55214165ed32014aae7863bce621d1cb19a9e
Dmitry Stogov <dmitry@zend.com>
2014-12-16 01:44:10 +0300

    Revert "Removed useless local variable"
    This reverts commit 44ae32ad55b9ceee9eb980edefcc62df6cff3eef.
    
    It doesn't work properly. We can't access call->scope after free.

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -513,9 +513,11 @@ static int ZEND_FASTCALL  ZEND_DO_FCALL_SPEC_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 	LOAD_OPLINE();
 
 	if (UNEXPECTED(fbc->type == ZEND_INTERNAL_FUNCTION)) {
+		int should_change_scope = 0;
 		zval *ret;
 
 		if (fbc->common.scope) {
+			should_change_scope = 1;
 			/* TODO: we don't set scope if we call an object method ??? */
 			/* See: ext/pdo_sqlite/tests/pdo_fetch_func_001.phpt */
 #if 1
@@ -546,7 +548,7 @@ static int ZEND_FASTCALL  ZEND_DO_FCALL_SPEC_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 				if (RETURN_VALUE_USED(opline)) {
 					ZVAL_UNDEF(EX_VAR(opline->result.var));
 				}
-				if (UNEXPECTED(fbc->common.scope)) {
+				if (UNEXPECTED(should_change_scope)) {
 					goto fcall_end_change_scope;
 				} else {
 					goto fcall_end;
@@ -572,7 +574,7 @@ static int ZEND_FASTCALL  ZEND_DO_FCALL_SPEC_HANDLER(ZEND_OPCODE_HANDLER_ARGS)
 			zval_ptr_dtor(ret);
 		}
 
-		if (UNEXPECTED(fbc->common.scope)) {
+		if (UNEXPECTED(should_change_scope)) {
 			goto fcall_end_change_scope;
 		} else {
 			goto fcall_end;
