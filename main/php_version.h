94ce084be00dd3b477b46402beefe4343cd2c59c
Stanislav Malyshev <stas@php.net>
2014-12-16 10:41:23 -0800

    5.4.36

--- main/php_version.h
+++ main/php_version.h
@@ -4,6 +4,6 @@
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 36
 
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.36-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.4.36"
 #define PHP_VERSION_ID 50436
