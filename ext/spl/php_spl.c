ad728725cb8e899a8415aa81db48825fac25e0d5
Levi Morrison <levim@php.net>
2014-12-28 10:16:14 -0700

    Add type info for spl_autoload_register

--- ext/spl/php_spl.c
+++ ext/spl/php_spl.c
@@ -866,7 +866,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_spl_autoload_call, 0, 0, 1)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_spl_autoload_register, 0, 0, 0)
-	ZEND_ARG_INFO(0, autoload_function)
+	ZEND_ARG_TYPE_INFO(0, autoload_function, IS_CALLABLE, 0)
 	ZEND_ARG_INFO(0, throw)
 	ZEND_ARG_INFO(0, prepend)
 ZEND_END_ARG_INFO()
