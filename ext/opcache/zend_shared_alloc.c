dafdb64e67b86510e078a5915e796c730f029958
Xinchen Hui <laruence@php.net>
2015-01-08 16:32:20 +0800

    Fixed #68677
    (cherry picked from commit 777c39f4042327eac4b63c7ee87dc1c7a09a3115)

--- ext/opcache/zend_shared_alloc.c
+++ ext/opcache/zend_shared_alloc.c
@@ -346,10 +346,10 @@ void *_zend_shared_memdup(void *source, size_t size, zend_bool free_source TSRML
 	retval = ZCG(mem);;
 	ZCG(mem) = (void*)(((char*)ZCG(mem)) + ZEND_ALIGNED_SIZE(size));
 	memcpy(retval, source, size);
+	zend_shared_alloc_register_xlat_entry(source, retval);
 	if (free_source) {
 		interned_efree((char*)source);
 	}
-	zend_shared_alloc_register_xlat_entry(source, retval);
 	return retval;
 }
 
