1b8cfaf23dc6c2e89f502bdc3e9da7e5abb2488b
Joshua Rogers <git@internot.info>
2015-01-19 21:31:15 +1100

    Fix integer overflow in calender.
    Fix int overflows in conversation functions for calendar.
    Add tests for the overflows.

--- ext/calendar/calendar.c
+++ ext/calendar/calendar.c
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Shane Caraveo             <shane@caraveo.com>               | 
+   | Authors: Shane Caraveo             <shane@caraveo.com>               |
    |          Colin Viebrock            <colin@easydns.com>               |
    |          Hartmut Holzgraefe        <hholzgra@php.net>                |
    |          Wez Furlong               <wez@thebrainroom.com>            |
