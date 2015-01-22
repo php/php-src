56c5b8e399683754323016dc5d9732aaea8beee6
Popa Adrian Marius <mapopa@reea.net>
2015-01-22 10:16:51 +0200

    Revert "Remove Jouni Ahto from maintainers and files as requested , anyway we have git blame/history"
    This reverts commit 7cf1450df14b8da0c3c255470ebe4c237fb49b88.

--- ext/interbase/php_interbase.h
+++ ext/interbase/php_interbase.h
@@ -12,7 +12,8 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Andrew Avdeev <andy@simgts.mv.ru>                           |
+   | Authors: Jouni Ahto <jouni.ahto@exdec.fi>                            |
+   |          Andrew Avdeev <andy@simgts.mv.ru>                           |
    |          Ard Biesheuvel <a.k.biesheuvel@its.tudelft.nl>              |
    +----------------------------------------------------------------------+
  */
