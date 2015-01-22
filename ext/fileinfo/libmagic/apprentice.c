43652d386a753c0a280790525e3593ecb416793c
Anatol Belski <ab@php.net>
2015-01-22 10:16:13 +0100

    simplify error handling for dirs as magic

--- ext/fileinfo/libmagic/apprentice.c
+++ ext/fileinfo/libmagic/apprentice.c
@@ -2616,7 +2616,7 @@ apprentice_map(struct magic_set *ms, const char *fn)
 	return to give apprentice_load() a chance. */
 	if (php_stream_stat_path_ex((char *)fn, 0, &st, NULL) == SUCCESS) {
                if (st.sb.st_mode & S_IFDIR) {
-                       goto error;
+                       return NULL;
                }
        }
 #endif
