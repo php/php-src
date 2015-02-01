94d6cb4a78956ac1505dc4141f052f7146114d74
Stanislav Malyshev <stas@php.net>
2015-01-31 23:34:14 -0800

    fix TSRM

--- ext/standard/dns.c
+++ ext/standard/dns.c
@@ -224,7 +224,7 @@ PHP_FUNCTION(gethostbyname)
 
 	if(hostname_len > MAXFQDNLEN) {
 		/* name too long, protect from CVE-2015-0235 */
-		php_error_docref(NULL, E_WARNING, "Host name is too long, the limit is %d characters", MAXFQDNLEN);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Host name is too long, the limit is %d characters", MAXFQDNLEN);
 		RETURN_STRINGL(hostname, hostname_len, 1);
 	}
 	addr = php_gethostbyname(hostname);
@@ -249,7 +249,7 @@ PHP_FUNCTION(gethostbynamel)
 
 	if(hostname_len > MAXFQDNLEN) {
 		/* name too long, protect from CVE-2015-0235 */
-		php_error_docref(NULL, E_WARNING, "Host name is too long, the limit is %d characters", MAXFQDNLEN);
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Host name is too long, the limit is %d characters", MAXFQDNLEN);
 		RETURN_FALSE;
 	}
 
