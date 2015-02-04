8b46d45a9d6c630afb57c87ba469f2aa4cf380e9
Dmitry Stogov <dmitry@zend.com>
2015-02-04 17:56:14 +0300

    Simplify code and add comments

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -437,12 +437,13 @@ static zend_always_inline zval *_get_zval_ptr_ptr_var(uint32_t var, const zend_e
 	if (EXPECTED(Z_TYPE_P(ret) == IS_INDIRECT)) {
 		*should_free = NULL;
 		ret = Z_INDIRECT_P(ret);
-	} else {
+	} else if (!Z_REFCOUNTED_P(ret)) {
+		*should_free = ret; /* immutable array may be converted to regular */
+	} else if (Z_REFCOUNT_P(ret) == 1) {
 		*should_free = ret;
-		if (Z_REFCOUNTED_P(ret) && Z_REFCOUNT_P(ret) > 1) {
-			*should_free = NULL;
-			Z_DELREF_P(ret);
-		}
+	} else {
+		*should_free = NULL;
+		Z_DELREF_P(ret);
 	}
 	return ret;
 }
