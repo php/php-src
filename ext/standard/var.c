4076a4d2a9b004c5898ea60b549f824631e0983d
Juan Basso <jrbasso@gmail.com>
2015-02-05 23:45:04 -0500

    Fixed serialization of non string values on __sleep
    Returning just N; (null) on the __sleep makes the number of fields/values be incomplete and corrupting the generated value from serialize, making impossible to unserialize it.
    
    Conflicts:
    	ext/standard/var.c

--- ext/standard/var.c
+++ ext/standard/var.c
@@ -658,10 +658,7 @@ static void php_var_serialize_class(smart_str *buf, zval *struc, zval *retval_pt
 
 			if (Z_TYPE_PP(name) != IS_STRING) {
 				php_error_docref(NULL TSRMLS_CC, E_NOTICE, "__sleep should return an array only containing the names of instance-variables to serialize.");
-				/* we should still add element even if it's not OK,
-				 * since we already wrote the length of the array before */
-				smart_str_appendl(buf,"N;", 2);
-				continue;
+				convert_to_string(name);
 			}
 			propers = Z_OBJPROP_P(struc);
 			if (zend_hash_find(propers, Z_STRVAL_PP(name), Z_STRLEN_PP(name) + 1, (void *) &d) == SUCCESS) {
