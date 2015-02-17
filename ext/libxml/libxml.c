8742276eb3905eb97a585417000c7b8df85006d4
Anatol Belski <ab@php.net>
2015-02-17 10:37:52 +0100

    abstain from using xmlCleanupParser
    Depending on circumstances, usage of xmlCleanupParser can affect
    the thread local storage, or even cause crashes in single threaded
    programs. On shutdown the memory will be freed anyway, however
    not using xmlCleanupParser helps to avoid possible shutdown
    crashes.

--- ext/libxml/libxml.c
+++ ext/libxml/libxml.c
@@ -752,7 +752,7 @@ PHP_LIBXML_API void php_libxml_shutdown(void)
 #if defined(LIBXML_SCHEMAS_ENABLED)
 		xmlRelaxNGCleanupTypes();
 #endif
-		xmlCleanupParser();
+		/* xmlCleanupParser(); */
 		zend_hash_destroy(&php_libxml_exports);
 
 		xmlSetExternalEntityLoader(_php_libxml_default_entity_loader);
