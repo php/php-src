4629f8978e86aa20e6254e7282e6c5a4fb26796c
Xinchen Hui <laruence@php.net>
2015-02-21 11:35:16 +0800

    Cleanup globals initialization
    No needs explicit inititlize zero for globals in no-zts build.
    executor_globals should be initialized in zend side.

--- Zend/zend.c
+++ Zend/zend.c
@@ -466,22 +466,28 @@ static void compiler_globals_dtor(zend_compiler_globals *compiler_globals) /* {{
 static void executor_globals_ctor(zend_executor_globals *executor_globals) /* {{{ */
 {
 	ZEND_TSRMLS_CACHE_UPDATE();
+
 	zend_startup_constants();
 	zend_copy_constants(EG(zend_constants), GLOBAL_CONSTANTS_TABLE);
 	zend_init_rsrc_plist();
 	zend_init_exception_op();
-	EG(lambda_count) = 0;
-	ZVAL_UNDEF(&EG(user_error_handler));
-	ZVAL_UNDEF(&EG(user_exception_handler));
-	EG(in_autoload) = NULL;
-	EG(current_execute_data) = NULL;
-	EG(current_module) = NULL;
-	EG(exit_status) = 0;
+	executor_globals->lambda_count = 0;
+	ZVAL_UNDEF(&executor_globals->user_error_handler);
+	ZVAL_UNDEF(&executor_globals->user_exception_handler);
+	executor_globals->in_autoload = NULL;
+	executor_globals->current_execute_data = NULL;
+	executor_globals->current_module = NULL;
+	executor_globals->exit_status = 0;
 #if XPFPA_HAVE_CW
-	EG(saved_fpu_cw) = 0;
+	executor_globals->saved_fpu_cw = 0;
 #endif
-	EG(saved_fpu_cw_ptr) = NULL;
-	EG(active) = 0;
+	executor_globals->saved_fpu_cw_ptr = NULL;
+	executor_globals->active = 0;
+	executor_globals->bailout = NULL;
+	executor_globals->error_handling  = EH_NORMAL;
+	executor_globals->exception_class = NULL;
+	executor_globals->exception = NULL;
+	executor_globals->objects_store.object_buckets = NULL;
 }
 /* }}} */
 
@@ -662,9 +668,8 @@ int zend_startup(zend_utility_functions *utility_functions, char **extensions) /
 	ini_scanner_globals_ctor(&ini_scanner_globals);
 	php_scanner_globals_ctor(&language_scanner_globals);
 	zend_set_default_compile_time_values();
-	ZVAL_UNDEF(&EG(user_error_handler));
-	ZVAL_UNDEF(&EG(user_exception_handler));
 #endif
+	EG(error_reporting) = E_ALL & ~E_NOTICE;
 
 	zend_interned_strings_init();
 	zend_startup_builtin_functions();
