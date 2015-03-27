f7952b90cad735e2a588c305d425c24550b3775d
Leigh <leigh@php.net>
2015-03-27 20:14:33 +0100

    Close fd at the end, otherwise people complain
    Even though it's closed when the process terminates!

--- ext/mcrypt/mcrypt.c
+++ ext/mcrypt/mcrypt.c
@@ -491,6 +491,14 @@ static PHP_MSHUTDOWN_FUNCTION(mcrypt) /* {{{ */
 	php_stream_filter_unregister_factory("mcrypt.*" TSRMLS_CC);
 	php_stream_filter_unregister_factory("mdecrypt.*" TSRMLS_CC);
 
+	if (MCG(fd[RANDOM]) > 0) {
+		close(MCG(fd[RANDOM]));
+	}
+
+	if (MCG(fd[URANDOM]) > 0) {
+		close(MCG(fd[URANDOM]));
+	}
+
 	UNREGISTER_INI_ENTRIES();
 	return SUCCESS;
 }
