3d4aad5e277e08c3a972eb09ec2f44f06c31ef14
Kalle Sommer Nielsen <kalle@php.net>
2015-03-29 06:50:09 +0200

    Remove this VC8 check as our minimum target version of VC is higher

--- ext/mysqlnd/mysqlnd_debug.h
+++ ext/mysqlnd/mysqlnd_debug.h
@@ -71,7 +71,7 @@ void mysqlnd_debug_trace_plugin_register(void);
 PHPAPI MYSQLND_DEBUG * mysqlnd_debug_init(const char * skip_functions[]);
 
 
-#if defined(__GNUC__) || (defined(_MSC_VER) && (_MSC_VER >= 1400))
+#if defined(__GNUC__) || defined(PHP_WIN32)
 #ifdef PHP_WIN32
 #include "win32/time.h"
 #elif defined(NETWARE)
@@ -157,7 +157,7 @@ PHPAPI MYSQLND_DEBUG * mysqlnd_debug_init(const char * skip_functions[]);
 
 
 
-#else  /* defined(__GNUC__) || (defined(_MSC_VER) && (_MSC_VER >= 1400)) */
+#else  /* defined(__GNUC__) || defined(PHP_WIN32) */
 static inline void DBG_INF_EX(MYSQLND_DEBUG * dbg_obj, const char * const msg) {}
 static inline void DBG_ERR_EX(MYSQLND_DEBUG * dbg_obj, const char * const msg) {}
 static inline void DBG_INF_FMT_EX(MYSQLND_DEBUG * dbg_obj, ...) {}
