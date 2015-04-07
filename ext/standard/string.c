346738b9d431076e2f0ca17bee506881b795f61f
ucat <ucat2015@163.com>
2015-04-07 17:59:58 +0800

    do some optimization in php_strtr in string.c

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -3100,9 +3100,9 @@ static void php_strtr_array(zval *return_value, zend_string *input, HashTable *p
 		old_pos = pos = 0;
 		while (pos <= slen - minlen) {
 			key = str + pos;
-			ZEND_HASH_FOREACH_NUM_KEY(&num_hash, len) {
+			ZEND_HASH_FOREACH_NUM_KEY(&num_hash, len) {				
+				if ((bitset[((unsigned char)key[0]) / sizeof(zend_ulong)] & Z_UL(1) << (((unsigned char)key[0]) % sizeof(zend_ulong))) == 0) break;
 				if (len > slen - pos) continue;
-				if ((bitset[((unsigned char)key[0]) / sizeof(zend_ulong)] & Z_UL(1) << (((unsigned char)key[0]) % sizeof(zend_ulong))) == 0) continue;
 				entry = zend_hash_str_find(pats, key, len);
 				if (entry != NULL) {
 					zend_string *s = zval_get_string(entry);
@@ -3125,7 +3125,7 @@ static void php_strtr_array(zval *return_value, zend_string *input, HashTable *p
 			}
 			key = str + pos;
 			for (len = maxlen; len >= minlen; len--) {
-				if ((bitset[((unsigned char)key[0]) / sizeof(zend_ulong)] & Z_UL(1) << (((unsigned char)key[0]) % sizeof(zend_ulong))) == 0) continue;
+				if ((bitset[((unsigned char)key[0]) / sizeof(zend_ulong)] & Z_UL(1) << (((unsigned char)key[0]) % sizeof(zend_ulong))) == 0) break;
 				entry = zend_hash_str_find(pats, key, len);
 				if (entry != NULL) {
 					zend_string *s = zval_get_string(entry);
