ff0eff952116131b848f99bc02127ded5f9c7c55
Medinski <medinsky94@gmail.com>
2015-04-21 05:56:38 -0400

    Indentation fixed

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -1346,8 +1346,12 @@ static inline void php_array_binary_search(INTERNAL_FUNCTION_PARAMETERS, int beh
     zval *value,                /* value to check for */
          *array,                /* array to check in */
          *entry;                /* pointer to array entry*/
+	zval result;				/* comparision result */
     zend_bool strict = 0;       /* strict comparison or not */
-
+    HashTable *arr_hash;
+    int low = 0;
+    int mid; 
+    int high;        
 #ifndef FAST_ZPP
     if (zend_parse_parameters(ZEND_NUM_ARGS(), "za|b", &value, &array, &strict) == FAILURE) {
         return;
@@ -1360,50 +1364,46 @@ static inline void php_array_binary_search(INTERNAL_FUNCTION_PARAMETERS, int beh
         Z_PARAM_BOOL(strict)
     ZEND_PARSE_PARAMETERS_END();
 #endif
-	HashTable *arr_hash;
 	arr_hash = Z_ARRVAL_P(array);
  
-	int low=0; 
-	int high=zend_hash_num_elements(arr_hash)-1;
-	int mid;
-
-	zval result;
-	if(strict){	
-		while(low<=high){
-			mid=(low+high)/2;
+	high = zend_hash_num_elements(arr_hash) - 1;
+	
+	if (strict) {	
+		while (low <= high) {
+			mid = (low + high) / 2;
         
 			ZVAL_DEREF(entry);
 			entry = zend_hash_index_find(arr_hash, mid);
 
 			compare_function(&result, value, entry);
   
-			if(Z_LVAL(result) < 0){
-				high=mid-1;
-			}else if(Z_LVAL(result) > 0){
-				low=mid+1;
-			}else{
+			if (Z_LVAL(result) < 0) {
+				high = mid - 1;
+			} else if (Z_LVAL(result) > 0) {
+				low = mid + 1;
+			} else {
 				fast_is_identical_function(&result, value, entry);
-				if(Z_TYPE(result) == IS_TRUE){
+				if (Z_TYPE(result) == IS_TRUE) {
 					RETURN_TRUE;
-				}else{
-					high=mid-1;
+				} else {
+					high = mid - 1;
 				}	
 			}
 		}
-	}else{
-		while(low<=high){
-			mid=(low+high)/2;
+	} else {
+		while (low <= high) {
+			mid = (low + high) / 2;
 		
 			ZVAL_DEREF(entry);
 			entry = zend_hash_index_find(arr_hash, mid);	     
     
 			compare_function(&result,entry,value);
          
-			if(Z_LVAL(result) < 0){
-				low=mid+1;
-			}else if(Z_LVAL(result) > 0){
-				high=mid-1;
-			}else{
+			if (Z_LVAL(result) < 0) {
+				low = mid + 1;
+			} else if (Z_LVAL(result) > 0) {
+				high = mid - 1;
+			} else {
 				RETURN_TRUE;
 			}		
 		}
@@ -1745,10 +1745,6 @@ PHP_FUNCTION(array_fill)
 	}
 }
 /* }}} */
-
-
-
-
 
 /* {{{ proto array array_fill_keys(array keys, mixed val)
    Create an array using the elements of the first parameter as keys each initialized to val */
