php/php-src

commit 16a9bc1ec20533c76ba992bfc64dd69e7b7d9001

Disallow self etc outside classes at compile-time
Also fix a bug with return types where "self" was rejected inside
a class, but not on a method.

Fallout: A couple of tests changed to more generic error messages.

Zend/zend_compile.c
@@ -1612,6 +1612,27 @@ uint32_t zend_get_class_fetch_type(zend_string *name) /* {{{ */
 }
 /* }}} */
 
+static uint32_t zend_get_class_fetch_type_ast(zend_ast *name_ast) /* {{{ */
+{
+	/* Fully qualified names are always default refs */
+	if (name_ast->attr == ZEND_NAME_FQ) {
+		return ZEND_FETCH_CLASS_DEFAULT;
+	}
+
+	return zend_get_class_fetch_type(zend_ast_get_str(name_ast));
+}
+/* }}} */
+
+void zend_ensure_valid_class_fetch_type(uint32_t fetch_type) /* {{{ */
+{
+	if (fetch_type != ZEND_FETCH_CLASS_DEFAULT && !CG(active_class_entry)) {
+		zend_error_noreturn(E_COMPILE_ERROR, "Cannot use \"%s\" when no class scope is active",
+			fetch_type == ZEND_FETCH_CLASS_SELF ? "self" :
+			fetch_type == ZEND_FETCH_CLASS_PARENT ? "parent" : "static");
+	}
+}
+/* }}} */
+
 ZEND_API zend_string *zend_get_compiled_variable_name(const zend_op_array *op_array, uint32_t var) /* {{{ */
 {
 	return op_array->vars[EX_VAR_TO_NUM(var)];
@@ -2018,19 +2039,11 @@ static inline zend_bool zend_can_write_to_variable(zend_ast *ast) /* {{{ */
 
 static inline zend_bool zend_is_const_default_class_ref(zend_ast *name_ast) /* {{{ */
 {
-	zend_string *name;
-
 	if (name_ast->kind != ZEND_AST_ZVAL) {
 		return 0;
 	}
 
-	/* Fully qualified names are always default refs */
-	if (!name_ast->attr) {
-		return 1;
-	}
-
-	name = zend_ast_get_str(name_ast);
-	return ZEND_FETCH_CLASS_DEFAULT == zend_get_class_fetch_type(name);
+	return ZEND_FETCH_CLASS_DEFAULT == zend_get_class_fetch_type_ast(name_ast);
 }
 /* }}} */
 
@@ -2077,6 +2090,8 @@ static zend_op *zend_compile_class_ref(znode *result, zend_ast *name_ast, int th
 			opline->op2_type = IS_CONST;
 			opline->op2.constant = zend_add_class_name_literal(CG(active_op_array),
 				zend_resolve_class_name(name, type));
+		} else {
+			zend_ensure_valid_class_fetch_type(fetch_type);
 		}
 
 		zend_string_release(name);
@@ -4147,7 +4162,7 @@ ZEND_API void zend_set_function_arg_flags(zend_function *func) /* {{{ */
 }
 /* }}} */
 
-void zend_compile_params(zend_ast *ast, zend_ast *return_type_ast, zend_bool is_method) /* {{{ */
+void zend_compile_params(zend_ast *ast, zend_ast *return_type_ast) /* {{{ */
 {
 	zend_ast_list *list = zend_ast_get_list(ast);
 	uint32_t i;
@@ -4173,15 +4188,13 @@ void zend_compile_params(zend_ast *ast, zend_ast *return_type_ast, zend_bool is_
 			if (type != 0) {
 				arg_infos->type_hint = type;
 			} else {
-				if (zend_is_const_default_class_ref(return_type_ast)) {
+				uint32_t fetch_type = zend_get_class_fetch_type_ast(return_type_ast);
+				if (fetch_type == ZEND_FETCH_CLASS_DEFAULT) {
 					class_name = zend_resolve_class_name_ast(return_type_ast);
 					zend_assert_valid_class_name(class_name);
 				} else {
+					zend_ensure_valid_class_fetch_type(fetch_type);
 					zend_string_addref(class_name);
-					if (!is_method) {
-						zend_error_noreturn(E_COMPILE_ERROR, "Cannot declare a return type of %s outside of a class scope", class_name->val);
-						return;
-					}
 				}
 
 				arg_infos->type_hint = IS_OBJECT;
@@ -4302,10 +4315,12 @@ void zend_compile_params(zend_ast *ast, zend_ast *return_type_ast, zend_bool is_
 				if (type != 0) {
 					arg_info->type_hint = type;
 				} else {
-					if (zend_is_const_default_class_ref(type_ast)) {
+					uint32_t fetch_type = zend_get_class_fetch_type_ast(type_ast);
+					if (fetch_type == ZEND_FETCH_CLASS_DEFAULT) {
 						class_name = zend_resolve_class_name_ast(type_ast);
 						zend_assert_valid_class_name(class_name);
 					} else {
+						zend_ensure_valid_class_fetch_type(fetch_type);
 						zend_string_addref(class_name);
 					}
 
@@ -4626,7 +4641,7 @@ void zend_compile_func_decl(znode *result, zend_ast *ast) /* {{{ */
 		zend_stack_push(&CG(loop_var_stack), (void *) &dummy_var);
 	}
 
-	zend_compile_params(params_ast, return_type_ast, is_method);
+	zend_compile_params(params_ast, return_type_ast);
 	if (uses_ast) {
 		zend_compile_closure_uses(uses_ast);
 	}
@@ -6329,13 +6344,10 @@ void zend_compile_resolve_class_name(znode *result, zend_ast *ast) /* {{{ */
 {
 	zend_ast *name_ast = ast->child[0];
 	uint32_t fetch_type = zend_get_class_fetch_type(zend_ast_get_str(name_ast));
+	zend_ensure_valid_class_fetch_type(fetch_type);
 
 	switch (fetch_type) {
 		case ZEND_FETCH_CLASS_SELF:
-			if (!CG(active_class_entry)) {
-				zend_error_noreturn(E_COMPILE_ERROR,
-					"Cannot access self::class when no class scope is active");
-			}
 			if (CG(active_class_entry)->ce_flags & ZEND_ACC_TRAIT) {
 				zval class_str_zv;
 				zend_ast *class_str_ast, *class_const_ast;
@@ -6354,11 +6366,7 @@ void zend_compile_resolve_class_name(znode *result, zend_ast *ast) /* {{{ */
 			break;
 		case ZEND_FETCH_CLASS_STATIC:
 		case ZEND_FETCH_CLASS_PARENT:
-			if (!CG(active_class_entry)) {
-				zend_error_noreturn(E_COMPILE_ERROR,
-					"Cannot access %s::class when no class scope is active",
-					fetch_type == ZEND_FETCH_CLASS_STATIC ? "static" : "parent");
-			} else {
+			{
 				zval class_str_zv;
 				zend_ast *class_str_ast, *class_const_ast;
 
@@ -6611,15 +6619,12 @@ void zend_compile_const_expr_resolve_class_name(zend_ast **ast_ptr) /* {{{ */
 {
 	zend_ast *ast = *ast_ptr;
 	zend_ast *name_ast = ast->child[0];
-	uint32_t fetch_type = zend_get_class_fetch_type(zend_ast_get_str(name_ast));
 	zval result;
+	uint32_t fetch_type = zend_get_class_fetch_type(zend_ast_get_str(name_ast));
+	zend_ensure_valid_class_fetch_type(fetch_type);
 
 	switch (fetch_type) {
 		case ZEND_FETCH_CLASS_SELF:
-			if (!CG(active_class_entry)) {
-				zend_error_noreturn(E_COMPILE_ERROR,
-					"Cannot access self::class when no class scope is active");
-			}
 			ZVAL_STR_COPY(&result, CG(active_class_entry)->name);
 			break;
         case ZEND_FETCH_CLASS_STATIC:

Zend/tests/self_class_const_outside_class.phpt
@@ -5,4 +5,4 @@ Accessing self::FOO outside a class
 var_dump(self::FOO);
 ?>
 --EXPECTF--
-Fatal error: Cannot access self:: when no class scope is active in %s on line %d
+Fatal error: Cannot use "self" when no class scope is active in %s on line %d

Zend/tests/class_name_as_scalar_error_005.phpt
@@ -7,4 +7,4 @@ $x = static::class;
 
 ?>
 --EXPECTF--
-Fatal error: Cannot access static::class when no class scope is active in %s on line %d
+Fatal error: Cannot use "static" when no class scope is active in %s on line 3

Zend/tests/class_name_as_scalar_error_006.phpt
@@ -7,4 +7,4 @@ $x = parent::class;
 
 ?>
 --EXPECTF--
-Fatal error: Cannot access parent::class when no class scope is active in %s on line %d
+Fatal error: Cannot use "parent" when no class scope is active in %s on line 3

Zend/tests/class_name_as_scalar_error_007.phpt
@@ -7,4 +7,4 @@ var_dump(self::class);
 
 ?>
 --EXPECTF--
-Fatal error: Cannot access self::class when no class scope is active in %s on line %d
+Fatal error: Cannot use "self" when no class scope is active in %s on line 3

Zend/tests/return_types/024.phpt
@@ -7,4 +7,4 @@ Return type of self is not allowed in function
 function test(): self {}
 
 --EXPECTF--
-Fatal error: Cannot declare a return type of self outside of a class scope in %s on line 3
+Fatal error: Cannot use "self" when no class scope is active in %s on line 3

Zend/tests/return_types/025.phpt
@@ -7,4 +7,4 @@ Return type of self is not allowed in closure
 $c = function(): self {};
 
 --EXPECTF--
-Fatal error: Cannot declare a return type of self outside of a class scope in %s on line 3
+Fatal error: Cannot use "self" when no class scope is active in %s on line 3

Zend/tests/return_types/026.phpt
@@ -6,4 +6,4 @@ Return type of parent is not allowed in function
 function test(): parent {}
 
 --EXPECTF--
-Fatal error: Cannot declare a return type of parent outside of a class scope in %s on line 3
+Fatal error: Cannot use "parent" when no class scope is active in %s on line %d

commit 6ef8ae65a2f8f973d419c193aa4a1e6150f3646a

Allow self etc in eval / file scope
This fixes a regression from 16a9bc1ec20533c76ba992bfc64dd69e7b7d9001.
Together with the recent closure related changes this should allow
all usages of self etc, while previously (in PHP 5) some things like
self::class did not work.

Zend/zend_compile.c
@@ -1325,6 +1325,11 @@ static inline zend_bool zend_is_scope_known() /* {{{ */
 		return 0;
 	}
 
+	if (!CG(active_op_array)->function_name) {
+		/* A file/eval will be run in the including/eval'ing scope */
+		return 0;
+	}
+
 	if (!CG(active_class_entry)) {
 		/* Not being in a scope is a known scope */
 		return 1;

Zend/tests/self_class_const_outside_class.phpt
@@ -1,8 +1,10 @@
 --TEST--
-Accessing self::FOO outside a class
+Accessing self::FOO in a free function
 --FILE--
 <?php
-var_dump(self::FOO);
+function test() {
+    var_dump(self::FOO);
+}
 ?>
 --EXPECTF--
 Fatal error: Cannot use "self" when no class scope is active in %s on line %d

Zend/tests/self_in_eval.phpt
@@ -1,0 +1,28 @@
+--TEST--
+self etc. can be used in eval() in a class scope
+--FILE--
+<?php
+
+class C {
+    const FOO = 1;
+    private static $bar = 2;
+    public static function f() {
+        eval(<<<'PHP'
+var_dump(self::FOO);
+var_dump(self::$bar);
+var_dump(self::class);
+var_dump(static::class);
+PHP
+        );
+    }
+}
+
+C::f();
+
+?>
+--EXPECT--
+int(1)
+int(2)
+string(1) "C"
+string(1) "C"
+