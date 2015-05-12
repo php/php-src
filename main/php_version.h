184d4e9fded736b607167a4185eabfbd083322b9
Stanislav Malyshev <stas@php.net>
2015-05-12 12:38:38 -0700

    prep for 5.4.41

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 41
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.41-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.4.41"
 #define PHP_VERSION_ID 50441
