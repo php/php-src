33b79c875bfbd4bec19a17225862819d702a2b23
Aaron Piotrowski <aaron@trowski.com>
2015-05-16 00:40:52 -0500

    Change BaseException to Throwable; Add Error class.
    EngineException, TypeException, and ParseException
    were renamed to EngineError, TypeError, and ParseError.
    Each now extends Error. Exception and Error extends
    Throwable, the common exception class previously called
    BaseException.

--- Zend/zend_language_scanner.c
+++ Zend/zend_language_scanner.c
@@ -998,7 +998,7 @@ static int zend_scan_escape_string(zval *zendlval, char *str, int len, char quot
 						}
 
 						if (!valid) {
-							zend_throw_exception(zend_get_parse_exception(),
+							zend_throw_exception(zend_get_parse_error(),
 								"Invalid UTF-8 codepoint escape sequence", E_PARSE);
 							zval_ptr_dtor(zendlval);
 							return FAILURE;
@@ -1009,7 +1009,7 @@ static int zend_scan_escape_string(zval *zendlval, char *str, int len, char quot
 
 						/* per RFC 3629, UTF-8 can only represent 21 bits */
 						if (codepoint > 0x10FFFF || errno) {
-							zend_throw_exception(zend_get_parse_exception(),
+							zend_throw_exception(zend_get_parse_error(),
 								"Invalid UTF-8 codepoint escape sequence: Codepoint too large", E_PARSE);
 							zval_ptr_dtor(zendlval);
 							return FAILURE;
@@ -2720,7 +2720,7 @@ yy136:
 		 * Because the lexing itself doesn't do that for us
 		 */
 		if (end != yytext + yyleng) {
-			zend_throw_exception(zend_get_parse_exception(), "Invalid numeric literal", E_PARSE);
+			zend_throw_exception(zend_get_parse_error(), "Invalid numeric literal", E_PARSE);
 			return T_ERROR;
 		}
 	} else {
@@ -2736,7 +2736,7 @@ yy136:
 			}
 			/* Also not an assert for the same reason */
 			if (end != yytext + yyleng) {
-				zend_throw_exception(zend_get_parse_exception(),
+				zend_throw_exception(zend_get_parse_error(),
 					"Invalid numeric literal", E_PARSE);
 				return T_ERROR;
 			}
@@ -2745,7 +2745,7 @@ yy136:
 		}
 		/* Also not an assert for the same reason */
 		if (end != yytext + yyleng) {
-			zend_throw_exception(zend_get_parse_exception(), "Invalid numeric literal", E_PARSE);
+			zend_throw_exception(zend_get_parse_error(), "Invalid numeric literal", E_PARSE);
 			return T_ERROR;
 		}
 	}
