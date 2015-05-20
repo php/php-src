a233af16b9c8c26583271498db404585582ef036
Anatol Belski <ab@php.net>
2015-05-20 18:46:30 +0200

    add vim modeline

--- win32/getrusage.c
+++ win32/getrusage.c
@@ -74,3 +74,11 @@ PHPAPI int getrusage(int who, struct rusage *usage)
 	}
 }
 
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
