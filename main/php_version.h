5f7c1917e3ab7ef7120dba6b2a3c46601781ea22
Julien Pauli <jpauli@php.net>
2015-06-09 10:29:29 +0200

    5.5.27 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 26
+#define PHP_RELEASE_VERSION 27
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.26-dev"
-#define PHP_VERSION_ID 50526
+#define PHP_VERSION "5.5.27-dev"
+#define PHP_VERSION_ID 50527
