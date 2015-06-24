1efe5b115788080b5d086fafd342322cee874eb5
Ferenc Kovacs <tyrael@php.net>
2015-06-25 00:21:45 +0200

    5.6.12 next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 11
+#define PHP_RELEASE_VERSION 12
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.11-dev"
-#define PHP_VERSION_ID 50611
+#define PHP_VERSION "5.6.12-dev"
+#define PHP_VERSION_ID 50612
