php/php-src

commit 4a4529adb03a5e0d8c46809074a9bae37686e201

Fix rope memory leaks upon exception

Zend/zend_compile.c
@@ -6622,6 +6622,11 @@ static void zend_compile_encaps_list(znode *result, zend_ast *ast) /* {{{ */
 		GET_NODE(result, opline->result);
 	} else {
 		uint32_t var;
+		zend_brk_cont_element *info = get_next_brk_cont_element(CG(active_op_array));
+		info->brk = opline - CG(active_op_array)->opcodes;
+		info->start = rope_init_lineno;
+		info->parent = CG(context).current_brk_cont;
+		info->cont = -1;
 
 		init_opline->extended_value = j;
 		opline->opcode = ZEND_ROPE_END;

Zend/zend_execute.c
@@ -2519,6 +2519,21 @@ static zend_always_inline void i_cleanup_unfinished_execution(zend_execute_data 
 						zend_hash_iterator_del(Z_FE_ITER_P(var));
 					}
 					zval_ptr_dtor_nogc(var);
+				} else if (brk_opline->opcode == ZEND_ROPE_END) {
+					zend_string **rope = (zend_string **) EX_VAR(brk_opline->op1.var);
+					zend_op *last = EX(func)->op_array.opcodes + op_num;
+					while (last->opcode != ZEND_ROPE_ADD && last->opcode != ZEND_ROPE_INIT) {
+						ZEND_ASSERT(last >= EX(func)->op_array.opcodes);
+						last--;
+					}
+					if (last->opcode == ZEND_ROPE_INIT) {
+						zend_string_release(*rope);
+					} else {
+						int j = last->extended_value;
+						do {
+							zend_string_release(rope[j]);
+						} while (j--);
+					}
 				} else if (brk_opline->opcode == ZEND_END_SILENCE) {
 					/* restore previous error_reporting value */
 					if (!EG(error_reporting) && Z_LVAL_P(EX_VAR(brk_opline->op1.var)) != 0) {

ext/opcache/Optimizer/block_pass.c
@@ -208,6 +208,7 @@ static int find_code_blocks(zend_op_array *op_array, zend_cfg *cfg, zend_optimiz
 			if (op_array->brk_cont_array[i].start >= 0 &&
 			    (op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_FREE ||
 			     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_FE_FREE ||
+			     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_ROPE_END ||
 			     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_END_SILENCE)) {
 				int parent = op_array->brk_cont_array[i].parent;
 
@@ -215,6 +216,7 @@ static int find_code_blocks(zend_op_array *op_array, zend_cfg *cfg, zend_optimiz
 				       op_array->brk_cont_array[parent].start < 0 &&
 				       (op_array->opcodes[op_array->brk_cont_array[parent].brk].opcode != ZEND_FREE ||
 				        op_array->opcodes[op_array->brk_cont_array[parent].brk].opcode != ZEND_FE_FREE ||
+					     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode != ZEND_ROPE_END ||
 				        op_array->opcodes[op_array->brk_cont_array[parent].brk].opcode != ZEND_END_SILENCE)) {
 					parent = op_array->brk_cont_array[parent].parent;
 				}
@@ -231,6 +233,7 @@ static int find_code_blocks(zend_op_array *op_array, zend_cfg *cfg, zend_optimiz
 				if (op_array->brk_cont_array[i].start >= 0 &&
 				    (op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_FREE ||
 				     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_FE_FREE ||
+				     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_ROPE_END ||
 				     op_array->opcodes[op_array->brk_cont_array[i].brk].opcode == ZEND_END_SILENCE)) {
 					if (i != j) {
 						op_array->brk_cont_array[j] = op_array->brk_cont_array[i];

commit 5ba28d76d2e5bfe7aaa9a052898e5f759b84b4ba

Fixed segfault in wordpress (introduced in rev 4a4529adb03a5e0d8c46809074a9bae37686e201)

Zend/zend_compile.c
@@ -6623,10 +6623,9 @@ static void zend_compile_encaps_list(znode *result, zend_ast *ast) /* {{{ */
 	} else {
 		uint32_t var;
 		zend_brk_cont_element *info = get_next_brk_cont_element(CG(active_op_array));
-		info->brk = opline - CG(active_op_array)->opcodes;
 		info->start = rope_init_lineno;
 		info->parent = CG(context).current_brk_cont;
-		info->cont = -1;
+		info->cont = info->brk = opline - CG(active_op_array)->opcodes;
 
 		init_opline->extended_value = j;
 		opline->opcode = ZEND_ROPE_END;