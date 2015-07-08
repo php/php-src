34b624f122dd7b0ef31707532cb11cfc2bdf50f7
Julien Pauli <jpauli@php.net>
2015-07-08 16:59:44 +0200

    5.5.27 release

--- main/php_version.h
+++ main/php_version.h
@@ -4,5 +4,5 @@
 #define PHP_MINOR_VERSION 5
 #define PHP_RELEASE_VERSION 28
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.27-dev"
+#define PHP_VERSION "5.5.28-dev"
 #define PHP_VERSION_ID 50528
