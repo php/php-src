e79cf7c4239fd2510a6428ced60838daad54d128
Tjerk Meesters <datibbaw@php.net>
2015-07-14 20:46:47 +0800

    Added file intros, renamed traverse function type

--- ext/standard/php_traversal.c
+++ ext/standard/php_traversal.c
@@ -1,7 +1,25 @@
+/*
+   +----------------------------------------------------------------------+
+   | PHP Version 7                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2015 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Tjerk Meesters <datibbaw@php.net>                           |
+   +----------------------------------------------------------------------+
+*/
+
 #include "php.h"
 #include "php_traversal.h"
 
-void php_traverse(zval *t, php_iterator_each_t each_func, int traverse_mode, void *context)
+void php_traverse(zval *t, php_traverse_each_t each_func, int traverse_mode, void *context)
 {
 	zend_bool should_continue;
 	zval *value;
