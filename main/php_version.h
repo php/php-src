3eee49557068fb4d07230668a23d2c50738cc3f4
Julien Pauli <jpauli@php.net>
2015-07-22 09:27:49 +0200

    5.6.13 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 12
+#define PHP_RELEASE_VERSION 13
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.12-dev"
-#define PHP_VERSION_ID 50612
+#define PHP_VERSION "5.6.13-dev"
+#define PHP_VERSION_ID 50613
