c18f5e1edddf68bd449cb2b8e55d47a7da622cde
Anatol Belski <ab@php.net>
2015-07-25 20:05:58 +0200

    add NULL check

--- sapi/phpdbg/phpdbg_list.c
+++ sapi/phpdbg/phpdbg_list.c
@@ -244,9 +244,12 @@ zend_op_array *phpdbg_compile_file(zend_file_handle *file, int type) {
 		return NULL;
 	}
 
-	data.buf = emalloc(data.len + 1);
-	memcpy(data.buf, bufptr, data.len);
-	data.buf[data.len] = 0;
+	data.buf = NULL;
+	if (data.len > 0) {
+		data.buf = emalloc(data.len + 1);
+		memcpy(data.buf, bufptr, data.len);
+		data.buf[data.len] = 0;
+	}
 	data.filename = filename;
 	data.line[0] = 0;
 
