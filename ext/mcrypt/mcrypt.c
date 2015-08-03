167725dcb7ad6a734bb7e3bfebc1c9c808e45f62
Leigh <leigh@php.net>
2015-08-03 13:13:27 +0100

    File descriptors can be 0 - Bug #69833

--- ext/mcrypt/mcrypt.c
+++ ext/mcrypt/mcrypt.c
@@ -491,11 +491,11 @@ static PHP_MSHUTDOWN_FUNCTION(mcrypt) /* {{{ */
 	php_stream_filter_unregister_factory("mcrypt.*" TSRMLS_CC);
 	php_stream_filter_unregister_factory("mdecrypt.*" TSRMLS_CC);
 
-	if (MCG(fd[RANDOM]) > 0) {
+	if (MCG(fd[RANDOM]) >= 0) {
 		close(MCG(fd[RANDOM]));
 	}
 
-	if (MCG(fd[URANDOM]) > 0) {
+	if (MCG(fd[URANDOM]) >= 0) {
 		close(MCG(fd[URANDOM]));
 	}
 
