d6d5fb0aa1b2a014f7a800331a4f631cbb5ad5e5
Stanislav Malyshev <stas@php.net>
2015-08-04 23:58:00 -0700

    5.5.28

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
 #define PHP_RELEASE_VERSION 28
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.28-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.5.28"
 #define PHP_VERSION_ID 50528
