15e9f4baf48f1a02091d1dbf505d9c6d561bc1d4
Stanislav Malyshev <stas@php.net>
2015-08-04 23:59:55 -0700

    5.5.29 next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 28
+#define PHP_RELEASE_VERSION 29
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.28-dev"
-#define PHP_VERSION_ID 50528
+#define PHP_VERSION "5.5.29-dev"
+#define PHP_VERSION_ID 50529
