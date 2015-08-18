501407aa49e4c664381b9849b8385d6ab390b18d
Bob Weinand <bobwei9@hotmail.com>
2015-08-18 13:59:37 +0200

    Revert fix for nicer truncation on log_errors_max_len

--- Zend/zend_exceptions.c
+++ Zend/zend_exceptions.c
@@ -1037,7 +1037,7 @@ ZEND_API void zend_exception_error(zend_object *ex, int severity) /* {{{ */
 		line = zval_get_long(GET_PROPERTY_SILENT(&exception, "line"));
 
 		zend_error_va(severity, (file && ZSTR_LEN(file) > 0) ? ZSTR_VAL(file) : NULL, line,
-			"Uncaught %.*s\n  thrown", PG(log_errors_max_len) ? MIN(ZSTR_LEN(str), MAX(PG(log_errors_max_len) - 18, 0)) : ZSTR_LEN(str), ZSTR_VAL(str));
+			"Uncaught %s\n  thrown", ZSTR_VAL(str));
 
 		zend_string_release(str);
 		zend_string_release(file);
