c7c9858ba16eb71c9ffc6cabfa00f8ece8600b47
Ferenc Kovacs <tyrael@php.net>
2015-08-20 09:56:47 +0200

    5.6.14 next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 13
+#define PHP_RELEASE_VERSION 14
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.13-dev"
-#define PHP_VERSION_ID 50613
+#define PHP_VERSION "5.6.14-dev"
+#define PHP_VERSION_ID 50614
