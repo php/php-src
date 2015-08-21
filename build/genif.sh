5ed1b70841e7f1652814a454efe1d24762cc0fde
Ferenc Kovacs <tyrael@php.net>
2015-08-21 18:27:03 +0200

    $() isn't supported on every shell

--- build/genif.sh
+++ build/genif.sh
@@ -24,7 +24,7 @@ cd $srcdir
 module_ptrs="$extra_module_ptrs`echo $@ | $awk -f ./build/order_by_dep.awk`"
 
 for ext in ${1+"$@"} ; do
-	ext_dir="$( cut -d ';' -f 2 <<< "$ext" )"
+	ext_dir=`cut -d ';' -f 2 <<< "$ext"`
 	header_list="$header_list $ext_dir/*.h*"
 done
 
