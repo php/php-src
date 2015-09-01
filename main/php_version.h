51fca4b77d05cde26cfa59f54bf79fd97973c79a
Stanislav Malyshev <stas@php.net>
2015-09-01 13:12:16 -0700

    bump version

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 45
+#define PHP_RELEASE_VERSION 46
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.4.45-dev"
-#define PHP_VERSION_ID 50445
+#define PHP_VERSION "5.4.46-dev"
+#define PHP_VERSION_ID 50446
