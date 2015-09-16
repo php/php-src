11cddaf6f4e4ace569d429f21e5b581e7602f27e
Ferenc Kovacs <tyrael@php.net>
2015-09-17 01:48:02 +0200

    prepare for PHP-5.6.14RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 14
+#define PHP_RELEASE_VERSION 15
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.14-dev"
-#define PHP_VERSION_ID 50614
+#define PHP_VERSION "5.6.15-dev"
+#define PHP_VERSION_ID 50615
