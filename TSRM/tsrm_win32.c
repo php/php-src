0e843a45820941345d1b1695040ab699cf3a0b2a
Anatol Belski <ab@php.net>
2015-09-22 15:31:02 +0200

    check for NULL return

--- TSRM/tsrm_win32.c
+++ TSRM/tsrm_win32.c
@@ -620,6 +620,11 @@ TSRM_API int shmget(int key, int size, int flags)
 	}
 
 	shm = shm_get(key, NULL);
+	if (!shm) {
+		UnmapViewOfFile(shm_handle);
+		UnmapViewOfFile(shm_handle);
+		return -1;
+	}
 	shm->segment = shm_handle;
 	shm->info	 = info_handle;
 	shm->descriptor = MapViewOfFileEx(shm->info, FILE_MAP_ALL_ACCESS, 0, 0, 0, NULL);
