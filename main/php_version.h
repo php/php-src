7eedb5bf11d34adc694ccbfd96b636629223ff03
Julien Pauli <jpauli@php.net>
2015-09-30 13:02:12 +0200

    5.5.30 release

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
 #define PHP_RELEASE_VERSION 30
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.30-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.5.30"
 #define PHP_VERSION_ID 50530
