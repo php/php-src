3f08fc0466a2acd38763b23f85e06eb02950c6ea
Julien Pauli <jpauli@php.net>
2015-09-30 13:01:11 +0200

    5.5.31 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 30
+#define PHP_RELEASE_VERSION 31
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.30-dev"
-#define PHP_VERSION_ID 50530
+#define PHP_VERSION "5.5.31-dev"
+#define PHP_VERSION_ID 50531
