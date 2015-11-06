f88a9a964368ad21e78cf96197952aa3e03d4604
Christopher Jones <christopher.jones@oracle.com>
2015-11-07 02:10:57 +1100

    Bump OCI8 version

--- ext/oci8/php_oci8.h
+++ ext/oci8/php_oci8.h
@@ -45,7 +45,7 @@
  */
 #undef PHP_OCI8_VERSION
 #endif
-#define PHP_OCI8_VERSION "2.0.9"
+#define PHP_OCI8_VERSION "2.0.10"
 
 extern zend_module_entry oci8_module_entry;
 #define phpext_oci8_ptr &oci8_module_entry
