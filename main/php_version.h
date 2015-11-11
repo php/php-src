b22636c838cb39eeb2c108c189a883ae9a9fd906
Ferenc Kovacs <tyrael@php.net>
2015-11-11 09:20:16 +0100

    propare for 5.6.16RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 16
+#define PHP_RELEASE_VERSION 17
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.16-dev"
-#define PHP_VERSION_ID 50616
+#define PHP_VERSION "5.6.17-dev"
+#define PHP_VERSION_ID 50617
