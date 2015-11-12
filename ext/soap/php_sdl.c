f8bf9bd86bdeef33a48e43d772d8a25a4fe3e6ff
Dmitry Stogov <dmitry@zend.com>
2015-11-12 20:31:52 +0300

    Fixed bug #70900 (SoapClient systematic out of memory error)

--- ext/soap/php_sdl.c
+++ ext/soap/php_sdl.c
@@ -1157,7 +1157,9 @@ static sdlPtr load_wsdl(zval *this_ptr, char *struri TSRMLS_DC)
 					zend_hash_init(ctx.sdl->bindings, 0, NULL, delete_binding, 0);
 				}
 
-				zend_hash_add(ctx.sdl->bindings, tmpbinding->name, strlen(tmpbinding->name), &tmpbinding, sizeof(sdlBindingPtr), NULL);
+				if (zend_hash_add(ctx.sdl->bindings, tmpbinding->name, strlen(tmpbinding->name), &tmpbinding, sizeof(sdlBindingPtr), NULL) != SUCCESS) {
+					zend_hash_next_index_insert(ctx.sdl->bindings, &tmpbinding, sizeof(sdlBindingPtr), NULL);
+				}
 				trav= trav->next;
 			}
 
