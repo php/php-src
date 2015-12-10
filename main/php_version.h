7d5dbaed765c8b8f2bbf3f6159a4658686245a9b
Ferenc Kovacs <tyrael@php.net>
2015-12-10 01:28:51 +0100

    5.6.18 will be next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 17
+#define PHP_RELEASE_VERSION 18
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.17-dev"
-#define PHP_VERSION_ID 50617
+#define PHP_VERSION "5.6.18-dev"
+#define PHP_VERSION_ID 50618
