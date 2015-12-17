6f8b0d6b74276d7923ebee51143175f95233996a
Xinchen Hui <laruence@gmail.com>
2015-12-17 14:27:33 +0800

    Fixed bug #71127 (Define in auto_prepend_file is overwrite)

--- ext/opcache/Optimizer/pass1_5.c
+++ ext/opcache/Optimizer/pass1_5.c
@@ -16,8 +16,9 @@ if (ZEND_OPTIMIZER_PASS_1 & OPTIMIZATION_LEVEL) {
 	int i = 0;
 	zend_op *opline = op_array->opcodes;
 	zend_op *end = opline + op_array->last;
-	zend_bool collect_constants = (ZEND_OPTIMIZER_PASS_15 & OPTIMIZATION_LEVEL)?
-		(op_array == &script->main_op_array) : 0;
+	/* bug #71127*
+	 zend_bool collect_constants = (op_array == &script->main_op_array); */
+	zend_bool collect_constants = 0;
 
 	while (opline < end) {
 		switch (opline->opcode) {
