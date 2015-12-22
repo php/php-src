6b0b29edd6084699ae6690064c997b9bf44462d4
Julien Pauli <jpauli@php.net>
2015-12-22 16:25:51 +0100

    Fix #70720

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -4590,6 +4590,7 @@ PHPAPI size_t php_strip_tags_ex(char *rbuf, int len, int *stateptr, char *allow,
 	int br, i=0, depth=0, in_q = 0;
 	int state = 0, pos;
 	char *allow_free = NULL;
+	char is_xml = 0;
 
 	if (stateptr)
 		state = *stateptr;
@@ -4689,10 +4690,10 @@ PHPAPI size_t php_strip_tags_ex(char *rbuf, int len, int *stateptr, char *allow,
 				switch (state) {
 					case 1: /* HTML/XML */
 						lc = '>';
-						if (*(p -1) == '-') {
+						if (is_xml && *(p -1) == '-') {
 							break;
 						}
-						in_q = state = 0;
+						in_q = state = is_xml = 0;
 						if (allow) {
 							if (tp - tbuf >= PHP_TAG_BUF_SIZE) {
 								pos = tp - tbuf;
@@ -4822,7 +4823,7 @@ PHPAPI size_t php_strip_tags_ex(char *rbuf, int len, int *stateptr, char *allow,
 				 */
 
 				if (state == 2 && p > buf+2 && strncasecmp(p-4, "<?xm", 4) == 0) {
-					state = 1;
+					state = 1; is_xml=1;
 					break;
 				}
 
--- ext/standard/tests/strings/bug70720.phpt
+++ ext/standard/tests/strings/bug70720.phpt
@@ -5,8 +5,12 @@ Bug #70720 (strip_tags() doesnt handle "xml" correctly)
 var_dump(strip_tags('<?php $dom->test(); ?> this is a test'));
 var_dump(strip_tags('<?php $xml->test(); ?> this is a test'));
 var_dump(strip_tags('<?xml $xml->test(); ?> this is a test'));
+
+/* "->" case in HTML */
+var_dump(strip_tags("<span class=sf-dump-> this is a test</span>"));
 ?>
 --EXPECTF--
+string(15) " this is a test"
 string(15) " this is a test"
 string(15) " this is a test"
 string(15) " this is a test"
