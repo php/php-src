129946529e48fa0d5af176b5de1cc4b217688cae
Francois Laupretre <francois@tekwire.net>
2016-01-04 16:31:52 +0100

    mb_ereg_search_setpos(): Add support for negative position
    Also add missing test for this function

--- ext/mbstring/php_mbregex.c
+++ ext/mbstring/php_mbregex.c
@@ -1400,6 +1400,11 @@ PHP_FUNCTION(mb_ereg_search_setpos)
 		return;
 	}
 
+	/* Accept negative position if length of search string can be determined */
+	if ((position < 0) && (!Z_ISUNDEF(MBREX(search_str))) && (Z_TYPE(MBREX(search_str)) == IS_STRING)) {
+		position += Z_STRLEN(MBREX(search_str));
+	}
+		
 	if (position < 0 || (!Z_ISUNDEF(MBREX(search_str)) && Z_TYPE(MBREX(search_str)) == IS_STRING && (size_t)position >= Z_STRLEN(MBREX(search_str)))) {
 		php_error_docref(NULL, E_WARNING, "Position is out of range");
 		MBREX(search_pos) = 0;
