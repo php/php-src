f7e6ff99d7633da3ab3cfa4e80e879a31ee8ab29
Anatol Belski <ab@php.net>
2016-01-18 23:03:59 +0100

    set versions to 7.0.3RC1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
 #define PHP_RELEASE_VERSION 3
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.3-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.0.3RC1"
 #define PHP_VERSION_ID 70003
