b5cd7c30f82d2307f39aa69f850d5bfe581db015
Anatol Belski <ab@php.net>
2016-01-19 08:02:39 +0100

    bump version to 7.0.4-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 3
+#define PHP_RELEASE_VERSION 4
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.3-dev"
-#define PHP_VERSION_ID 70003
+#define PHP_VERSION "7.0.4-dev"
+#define PHP_VERSION_ID 70004
