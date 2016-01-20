a4fc2e1eb267e71fbff4522d10e3cac4315866f7
Ferenc Kovacs <tyrael@php.net>
2016-01-20 15:39:05 +0100

    5.6.19 will be next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 18
+#define PHP_RELEASE_VERSION 19
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.18-dev"
-#define PHP_VERSION_ID 50618
+#define PHP_VERSION "5.6.19-dev"
+#define PHP_VERSION_ID 50619
