a0b1e87dcfb63e71dba3040f987d449097644c8c
Rouven Weßling <rouven@contentful.com>
2016-01-29 14:07:43 +0100

    Use uint32_t instead of uint32 in the litespeed SAPI.

--- sapi/litespeed/lsapilib.c
+++ sapi/litespeed/lsapilib.c
@@ -87,13 +87,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #include <inttypes.h>
-#ifndef uint32
-#define uint32 uint32_t
-#endif
 
 struct lsapi_MD5Context {
-    uint32 buf[4];
-    uint32 bits[2];
+    uint32_t buf[4];
+    uint32_t bits[2];
     unsigned char in[64];
 };
 
@@ -3339,18 +3336,18 @@ int LSAPI_ErrResponse_r( LSAPI_Request * pReq, int code, const char ** pRespHead
 }
 
 
-static void lsapi_MD5Transform(uint32 buf[4], uint32 const in[16]);
+static void lsapi_MD5Transform(uint32_t buf[4], uint32_t const in[16]);
 
 /*
  * Note: this code is harmless on little-endian machines.
  */
 static void byteReverse(unsigned char *buf, unsigned longs)
 {
-    uint32 t;
+    uint32_t t;
     do {
-        t = (uint32) ((unsigned) buf[3] << 8 | buf[2]) << 16 |
+        t = (uint32_t) ((unsigned) buf[3] << 8 | buf[2]) << 16 |
             ((unsigned) buf[1] << 8 | buf[0]);
-        *(uint32 *) buf = t;
+        *(uint32_t *) buf = t;
         buf += 4;
     } while (--longs);
 }
@@ -3376,12 +3373,12 @@ void lsapi_MD5Init(struct lsapi_MD5Context *ctx)
  */
 void lsapi_MD5Update(struct lsapi_MD5Context *ctx, unsigned char const *buf, unsigned len)
 {
-    register uint32 t;
+    register uint32_t t;
 
     /* Update bitcount */
 
     t = ctx->bits[0];
-    if ((ctx->bits[0] = t + ((uint32) len << 3)) < t)
+    if ((ctx->bits[0] = t + ((uint32_t) len << 3)) < t)
         ctx->bits[1]++;                /* Carry from low to high */
     ctx->bits[1] += len >> 29;
 
@@ -3399,7 +3396,7 @@ void lsapi_MD5Update(struct lsapi_MD5Context *ctx, unsigned char const *buf, uns
         }
         memmove(p, buf, t);
         byteReverse(ctx->in, 16);
-        lsapi_MD5Transform(ctx->buf, (uint32 *) ctx->in);
+        lsapi_MD5Transform(ctx->buf, (uint32_t *) ctx->in);
         buf += t;
         len -= t;
     }
@@ -3408,7 +3405,7 @@ void lsapi_MD5Update(struct lsapi_MD5Context *ctx, unsigned char const *buf, uns
     while (len >= 64) {
         memmove(ctx->in, buf, 64);
         byteReverse(ctx->in, 16);
-        lsapi_MD5Transform(ctx->buf, (uint32 *) ctx->in);
+        lsapi_MD5Transform(ctx->buf, (uint32_t *) ctx->in);
         buf += 64;
         len -= 64;
     }
@@ -3443,7 +3440,7 @@ void lsapi_MD5Final(unsigned char digest[16], struct lsapi_MD5Context *ctx)
         /* Two lots of padding:  Pad the first block to 64 bytes */
         memset(p, 0, count);
         byteReverse(ctx->in, 16);
-        lsapi_MD5Transform(ctx->buf, (uint32 *) ctx->in);
+        lsapi_MD5Transform(ctx->buf, (uint32_t *) ctx->in);
 
         /* Now fill the next block with 56 bytes */
         memset(ctx->in, 0, 56);
@@ -3454,10 +3451,10 @@ void lsapi_MD5Final(unsigned char digest[16], struct lsapi_MD5Context *ctx)
     byteReverse(ctx->in, 14);
 
     /* Append length in bits and transform */
-    ((uint32 *) ctx->in)[14] = ctx->bits[0];
-    ((uint32 *) ctx->in)[15] = ctx->bits[1];
+    ((uint32_t *) ctx->in)[14] = ctx->bits[0];
+    ((uint32_t *) ctx->in)[15] = ctx->bits[1];
 
-    lsapi_MD5Transform(ctx->buf, (uint32 *) ctx->in);
+    lsapi_MD5Transform(ctx->buf, (uint32_t *) ctx->in);
     byteReverse((unsigned char *) ctx->buf, 4);
     memmove(digest, ctx->buf, 16);
     memset(ctx, 0, sizeof(*ctx));        /* In case it's sensitive */
@@ -3480,9 +3477,9 @@ void lsapi_MD5Final(unsigned char digest[16], struct lsapi_MD5Context *ctx)
  * reflect the addition of 16 longwords of new data.  MD5Update blocks
  * the data and converts bytes into longwords for this routine.
  */
-static void lsapi_MD5Transform(uint32 buf[4], uint32 const in[16])
+static void lsapi_MD5Transform(uint32_t buf[4], uint32_t const in[16])
 {
-    register uint32 a, b, c, d;
+    register uint32_t a, b, c, d;
 
     a = buf[0];
     b = buf[1];
