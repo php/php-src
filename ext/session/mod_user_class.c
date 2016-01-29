8696d25dee08955c6ce4b8bcb3ae1bc8db843afd
Anatol Belski <ab@php.net>
2016-01-29 08:33:09 +0100

    reset the ext/session to the state of 5.6.17

--- ext/session/mod_user_class.c
+++ ext/session/mod_user_class.c
@@ -22,10 +22,6 @@
 #include "php_session.h"
 
 #define PS_SANITY_CHECK						\
-	if (PS(session_status) != php_session_active) { \
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Session is not active"); \
-		RETURN_FALSE; \
-	} \
 	if (PS(default_mod) == NULL) {				\
 		php_error_docref(NULL TSRMLS_CC, E_CORE_ERROR, "Cannot call default session handler"); \
 		RETURN_FALSE;						\
@@ -43,7 +39,7 @@
 PHP_METHOD(SessionHandler, open)
 {
 	char *save_path = NULL, *session_name = NULL;
-	int ret, save_path_len, session_name_len;
+	int save_path_len, session_name_len;
 
 	PS_SANITY_CHECK;
 
@@ -52,14 +48,7 @@ PHP_METHOD(SessionHandler, open)
 	}
 
 	PS(mod_user_is_open) = 1;
-	zend_try {
-		ret =PS(default_mod)->s_open(&PS(mod_data), save_path, session_name TSRMLS_CC);
-	} zend_catch {
-		PS(session_status) = php_session_none;
-		zend_bailout();
-	} zend_end_try();
-
-	RETVAL_BOOL(SUCCESS == ret);
+	RETVAL_BOOL(SUCCESS == PS(default_mod)->s_open(&PS(mod_data), save_path, session_name TSRMLS_CC));
 }
 /* }}} */
 
@@ -67,8 +56,6 @@ PHP_METHOD(SessionHandler, open)
    Wraps the old close handler */
 PHP_METHOD(SessionHandler, close)
 {
-	int ret;
-
 	PS_SANITY_CHECK_IS_OPEN;
 
 	// don't return on failure, since not closing the default handler
@@ -76,15 +63,7 @@ PHP_METHOD(SessionHandler, close)
 	zend_parse_parameters_none();
 	
 	PS(mod_user_is_open) = 0;
-
-	zend_try {
-		ret = PS(default_mod)->s_close(&PS(mod_data) TSRMLS_CC);
-	} zend_catch {
-		PS(session_status) = php_session_none;
-		zend_bailout();
-	} zend_end_try();
-
-	RETVAL_BOOL(SUCCESS == ret);
+	RETVAL_BOOL(SUCCESS == PS(default_mod)->s_close(&PS(mod_data) TSRMLS_CC));
 }
 /* }}} */
 
--- ext/session/tests/session_set_save_handler_variation4.phpt
+++ ext/session/tests/session_set_save_handler_variation4.phpt
@@ -51,8 +51,8 @@ ob_end_flush();
 *** Testing session_set_save_handler() : variation ***
 
 Open [%s,PHPSESSID]
-GC [0]
 Read [%s,%s]
+GC [0]
 array(3) {
   ["Blah"]=>
   string(12) "Hello World!"
@@ -65,9 +65,15 @@ Write [%s,%s,Blah|s:12:"Hello World!";Foo|b:0;Guff|i:1234567890;]
 Close [%s,PHPSESSID]
 NULL
 Open [%s,PHPSESSID]
-GC [0]
 Read [%s,%s]
-array(0) {
+GC [0]
+array(3) {
+  ["Blah"]=>
+  string(12) "Hello World!"
+  ["Foo"]=>
+  bool(false)
+  ["Guff"]=>
+  int(1234567890)
 }
 Destroy [%s,%s]
 
