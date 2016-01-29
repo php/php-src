57d9797b6f653ab86036f5b0dd4ff132cd967dc3
Anatol Belski <ab@php.net>
2016-01-29 20:46:47 +0100

    Revert "fix leak in 7.0"
    This reverts commit ee49df011ce55f088908c54ef24ce4db45574414.

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -119,7 +119,7 @@ static char *ps_files_path_create(char *buf, size_t buflen, ps_files *data, cons
 
 	key_len = strlen(key);
 	if (key_len <= data->dirdepth ||
-		buflen < (data->basedir_len + 2 * data->dirdepth + key_len + 5 + sizeof(FILE_PREFIX))) {
+		buflen < (strlen(data->basedir) + 2 * data->dirdepth + key_len + 5 + sizeof(FILE_PREFIX))) {
 		return NULL;
 	}
 
