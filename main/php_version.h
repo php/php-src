7d875fb9df7336a220b3a61bc6ebe87c7182c4a9
Julien Pauli <jpauli@php.net>
2016-02-02 10:42:49 +0100

    Going for 5.5.33 now

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 32
+#define PHP_RELEASE_VERSION 33
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.32-dev"
-#define PHP_VERSION_ID 50532
+#define PHP_VERSION "5.5.33-dev"
+#define PHP_VERSION_ID 50533
