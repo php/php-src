6a8a518ed082cd809cfa7d5572d0c98a7746f495
Anatol Belski <ab@php.net>
2016-02-03 22:53:16 +0100

    add php_win32_ioutil_any_to_w() and remove unavailable funcs

--- win32/ioutil.h
+++ win32/ioutil.h
@@ -71,118 +71,51 @@ typedef struct {
 	DWORD attributes;
 } php_ioutil_open_opts;
 
-typedef struct {
-	unsigned long  ReparseTag;
-	unsigned short ReparseDataLength;
-	unsigned short Reserved;
-	union {
-		struct {
-			unsigned short SubstituteNameOffset;
-			unsigned short SubstituteNameLength;
-			unsigned short PrintNameOffset;
-			unsigned short PrintNameLength;
-			unsigned long  Flags;
-			wchar_t        ReparseTarget[1];
-		} SymbolicLinkReparseBuffer;
-		struct {
-			unsigned short SubstituteNameOffset;
-			unsigned short SubstituteNameLength;
-			unsigned short PrintNameOffset;
-			unsigned short PrintNameLength;
-			wchar_t        ReparseTarget[1];
-		} MountPointReparseBuffer;
-		struct {
-			unsigned char  ReparseTarget[1];
-		} GenericReparseBuffer;
-	};
-} REPARSE_DATA_BUFFER, *PREPARSE_DATA_BUFFER;
-
-#ifdef CTL_CODE
-#undef CTL_CODE
-#endif
-#define CTL_CODE(DeviceType,Function,Method,Access) (((DeviceType) << 16) | ((Access) << 14) | ((Function) << 2) | (Method))
-#define FILE_DEVICE_FILE_SYSTEM 0x00000009
-#define METHOD_BUFFERED		0
-#define FILE_ANY_ACCESS 	0
-#define FSCTL_GET_REPARSE_POINT CTL_CODE(FILE_DEVICE_FILE_SYSTEM, 42, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define MAXIMUM_REPARSE_DATA_BUFFER_SIZE  ( 16 * 1024 )
-
-#ifndef O_ASYNC
-# define O_ASYNC 0x100000
-#endif
-
-#ifndef O_SYNC
-# define O_SYNC 0x200000
-#endif
-
-#define PHP_WIN32_IOUTIL_MAXPATHLEN 32767
-/* This might be tricky, but this way we override MAXPATHLEN in the files
-   where this header is included, so the win32 io utils are supported. */
-#if !defined(MAXPATHLEN) || MAXPATHLEN < PHP_WIN32_IOUTIL_MAXPATHLEN
-# undef MAXPATHLEN
-# define MAXPATHLEN PHP_WIN32_IOUTIL_MAXPATHLEN
-#endif
-
-#define PHP_WIN32_IOUTIL_DEFAULT_SLASH '\\'
-#define PHP_WIN32_IOUTIL_DEFAULT_DIR_SEPARATOR	';'
-#define PHP_WIN32_IOUTIL_IS_SLASH(c)	((c) == '/' || (c) == '\\')
-#define PHP_WIN32_IOUTIL_IS_LETTER(c) (((c) >= 'a' && (c) <= 'z') || ((c) >= 'A' && (c) <= 'Z'))
-#define PHP_WIN32_IOUTIL_JUNCTION_PREFIX "\\??\\"
-#define PHP_WIN32_IOUTIL_JUNCTION_PREFIX_LEN 4
-#define PHP_WIN32_IOUTIL_LONG_PATH_PREFIX "\\\\?\\"
-#define PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LEN 4
-#define PHP_WIN32_IOUTIL_UNC_PATH_PREFIX "\\\\?\\UNC\\"
-#define PHP_WIN32_IOUTIL_UNC_PATH_PREFIX_LEN 8
-
-#define PHP_WIN32_IOUTIL_IS_LONG_PATH(path, path_len) (path_len >= PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LEN \
-	&& 0 == strncmp((path), PHP_WIN32_IOUTIL_LONG_PATH_PREFIX, PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LEN))
-#define PHP_WIN32_IOUTIL_IS_ABSOLUTE(path, path_len) (PHP_WIN32_IOUTIL_IS_LONG_PATH(path, path_len) \
-	|| path_len >= 3 && PHP_WIN32_IOUTIL_IS_LETTER(path[0]) && ':' == path[1] && IS_SLASH(path[2]))
-
-#define PHP_WIN32_IOUTIL_DEFAULT_SLASHW L'\\'
-#define PHP_WIN32_IOUTIL_DEFAULT_DIR_SEPARATORW	L';'
-#define PHP_WIN32_IOUTIL_IS_SLASHW(c) ((c) == L'\\' || (c) == L'/')
-#define PHP_WIN32_IOUTIL_IS_LETTERW(c) (((c) >= L'a' && (c) <= L'z') || ((c) >= L'A' && (c) <= L'Z'))
-#define PHP_WIN32_IOUTIL_JUNCTION_PREFIXW L"\\??\\"
-#define PHP_WIN32_IOUTIL_JUNCTION_PREFIX_LENW 4
-#define PHP_WIN32_IOUTIL_LONG_PATH_PREFIXW L"\\\\?\\"
-#define PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LENW 4
-#define PHP_WIN32_IOUTIL_UNC_PATH_PREFIXW L"\\\\?\\UNC\\"
-#define PHP_WIN32_IOUTIL_UNC_PATH_PREFIX_LENW 8
-
-#define PHP_WIN32_IOUTIL_IS_LONG_PATHW(pathw, path_lenw) (path_lenw >= PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LENW \
-	&& 0 == wcsncmp((pathw), PHP_WIN32_IOUTIL_LONG_PATH_PREFIXW, PHP_WIN32_IOUTIL_LONG_PATH_PREFIX_LENW))
-#define PHP_WIN32_IOUTIL_IS_ABSOLUTEW(pathw, path_lenw) (PHP_WIN32_IOUTIL_IS_LONG_PATHW(pathw, path_lenw) \
-	|| path_lenw >= 3 && PHP_WIN32_IOUTIL_IS_LETTERW(pathw[0]) && L':' == pathw[1] && IS_SLASHW(pathw[2]))
-
-/* these are not defined in win32 headers */
-#ifndef W_OK
-#define W_OK 0x02
-#endif
-#ifndef R_OK
-#define R_OK 0x04
-#endif
-#ifndef X_OK
-#define X_OK 0x01
-#endif
-#ifndef F_OK
-#define F_OK 0x00
-#endif
+typedef enum {
+	PHP_WIN32_IOUTIL_IS_ASCII,
+	PHP_WIN32_IOUTIL_IS_ANSI,
+	PHP_WIN32_IOUTIL_IS_UTF8
+} php_win32_ioutil_encoding;
 
 PW32IO wchar_t *php_win32_ioutil_mb_to_w(const char* path);
 PW32IO char *php_win32_ioutil_w_to_utf8(wchar_t* w_source_ptr);
+/* This function tries to make the best guess to convert any
+   given string to a wide char, also prefering the fastest code
+   path to unicode. It returns NULL on fail. */
+__forceinline wchar_t *php_win32_ioutil_any_to_w(const char* in)
+{
+	const char *idx = in, *end = in + strlen(in);
+	
+	while (idx <= end) {
+		if (!__isascii(*idx)) {
+			break;
+		}
+		idx++;
+	}
+
+	/* This means we've got an ASCII path, an ANSI function can be used
+		safely. This is the fastest way to do the thing. */
+	if (idx == end) {
+		return NULL;
+	}
+
+	/* Otherwise, go try to convert to multibyte. If it is failed, still
+	   NULL is delivered, indicating an ANSI IO function should be used.
+	   That still might not work with every ANSI string when the current
+	   system locale is incompatible. Then, it is up to the high level to
+	   convert the path to a multibyte string before. */
+	return php_win32_ioutil_mb_to_w(in);
+}
 
 PW32IO int php_win32_ioutil_close(int fd);
 PW32IO BOOL php_win32_ioutil_posix_to_open_opts(int flags, mode_t mode, php_ioutil_open_opts *opts);
 PW32IO int php_win32_ioutil_mkdir(const char *path, mode_t mode);
 
 #if PHP_WIN32_IOUTIL_ANSI_COMPAT_MODE
-PW32IO int php_win32_ioutil_sys_stat_ex_a(const char *path, zend_stat_t *buf, int lstat);
 PW32IO int php_win32_ioutil_mkdir_a(const char *path, mode_t mode);
 PW32IO int php_win32_ioutil_open_a(const char *path, int flags, ...);
 #endif
 
-PW32IO int php_win32_ioutil_sys_stat_ex_w(const wchar_t *path, zend_stat_t *buf, int lstat);
 PW32IO int php_win32_ioutil_mkdir_w(const wchar_t *path, mode_t mode);
 PW32IO int php_win32_ioutil_open_w(const wchar_t *path, int flags, ...);
 
@@ -217,28 +150,6 @@ __forceinline static int php_win32_ioutil_access(const char *path, mode_t mode)
 	}
 
 	PHP_WIN32_IOUTIL_CLEANUP_W();
-
-	return ret;
-}
-
-#define php_win32_ioutil_sys_stat_ex_cond(path, buf, lstat) (use_w) ? php_win32_ioutil_sys_stat_ex_w(pathw, buf, lstat) : php_win32_ioutil_sys_stat_ex_a(patha, buf, lstat)
-__forceinline static int php_win32_ioutil_sys_stat_ex(const char *path, zend_stat_t *buf, int lstat)
-{
-	PHP_WIN32_IOUTIL_INIT_W(path);
-	int ret;
-
-	if (use_w) {
-		ret = php_win32_ioutil_sys_stat_ex_w(pathw, buf, lstat);
-	} else {
-		ret = php_win32_ioutil_sys_stat_ex_a(patha, buf, lstat);
-	}
-
-	PHP_WIN32_IOUTIL_CLEANUP_W();
-
-	if (0 > ret) {
-		DWORD err = GetLastError();
-		SET_ERRNO_FROM_WIN32_CODE(err);
-	}
 
 	return ret;
 }
