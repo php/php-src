efaafd45b4e0d4939f3fd5a31d7712ba4cc48162
Anatol Belski <ab@php.net>
2016-02-29 15:24:31 +0100

    fix C89 compat

--- ext/pdo_dblib/dblib_driver.c
+++ ext/pdo_dblib/dblib_driver.c
@@ -286,8 +286,6 @@ static int pdo_dblib_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 		
 	};
 	
-	nvers = sizeof(tdsver)/sizeof(tdsver[0]);
-	
 	struct pdo_data_src_parser vars[] = {
 		{ "charset",	NULL,	0 }
 		,{ "appname",	"PHP " PDO_DBLIB_FLAVOUR,	0 }
@@ -298,6 +296,7 @@ static int pdo_dblib_handle_factory(pdo_dbh_t *dbh, zval *driver_options TSRMLS_
 	};
 	
 	nvars = sizeof(vars)/sizeof(vars[0]);
+	nvers = sizeof(tdsver)/sizeof(tdsver[0]);
 	
 	php_pdo_parse_data_source(dbh->data_source, dbh->data_source_len, vars, nvars);
 
