0fccddba75c68e7dd70f26ee595995bffba6dfe1
Anatol Belski <ab@php.net>
2016-03-07 16:26:01 +0100

    add error check

--- ext/gd/libgd/gd_webp.c
+++ ext/gd/libgd/gd_webp.c
@@ -14,6 +14,8 @@ gdImagePtr gdImageCreateFromWebp (FILE * inFile)
 {
 	gdImagePtr im;
 	gdIOCtx *in = gdNewFileCtx(inFile);
+	if (!in)
+		return 0;
 	im = gdImageCreateFromWebpCtx(in);
 	in->gd_free(in);
 
