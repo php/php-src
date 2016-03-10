67f0770036f866f6f0dadc044cdc8f94a5b9f030
Xinchen Hui <laruence@gmail.com>
2016-03-10 14:24:13 +0800

    tsrm_win32_get_path_sid_key might returns NULL (Partially fix bug #71752)
    cherry-picked from 7d5f71b0b125c89211e5a2a6d925deba938abd41

--- Zend/zend_virtual_cwd.c
+++ Zend/zend_virtual_cwd.c
@@ -587,11 +587,13 @@ static inline unsigned long realpath_cache_key(const char *path, int path_len TS
 	register unsigned long h;
 	char *bucket_key_start = tsrm_win32_get_path_sid_key(path TSRMLS_CC);
 	char *bucket_key = (char *)bucket_key_start;
-	const char *e = bucket_key + strlen(bucket_key);
+	const char *e;
 
 	if (!bucket_key) {
 		return 0;
 	}
+
+	e = bucket_key + strlen(bucket_key);
 
 	for (h = 2166136261U; bucket_key < e;) {
 		h *= 16777619;
