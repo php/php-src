14f4848fb0e900c34129eead3c9ca92157b23aac
Ferenc Kovacs <tyrael@php.net>
2016-03-17 01:46:00 +0100

    prepare for 5.6.20RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 19
-#define PHP_EXTRA_VERSION "RC1"
-#define PHP_VERSION "5.6.19RC1"
-#define PHP_VERSION_ID 50619
+#define PHP_RELEASE_VERSION 21
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "5.6.21-dev"
+#define PHP_VERSION_ID 50621
