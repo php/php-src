cc3cdd00578006a5684e6dfaf81532a13326b9fe
Nikita Popov <nikic@php.net>
2016-03-20 17:46:12 +0100

    Fixed bug #67582

--- ext/spl/spl_observer.c
+++ ext/spl/spl_observer.c
@@ -275,11 +275,6 @@ static zend_object_value spl_object_storage_new_ex(zend_class_entry *class_type,
 	retval.handle = zend_objects_store_put(intern, (zend_objects_store_dtor_t)zend_objects_destroy_object, (zend_objects_free_object_storage_t) spl_SplOjectStorage_free_storage, NULL TSRMLS_CC);
 	retval.handlers = &spl_handler_SplObjectStorage;
 
-	if (orig) {
-		spl_SplObjectStorage *other = (spl_SplObjectStorage*)zend_object_store_get_object(orig TSRMLS_CC);
-		spl_object_storage_addall(intern, orig, other TSRMLS_CC);
-	}
-
 	while (parent) {
 		if (parent == spl_ce_SplObjectStorage) {
 			if (class_type != spl_ce_SplObjectStorage) {
@@ -292,6 +287,11 @@ static zend_object_value spl_object_storage_new_ex(zend_class_entry *class_type,
 		}
 
 		parent = parent->parent;
+	}
+
+	if (orig) {
+		spl_SplObjectStorage *other = (spl_SplObjectStorage*)zend_object_store_get_object(orig TSRMLS_CC);
+		spl_object_storage_addall(intern, orig, other TSRMLS_CC);
 	}
 
 	return retval;
