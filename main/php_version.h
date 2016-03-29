e93fdf6330e5cbf5eb0d2d2c9f0eb3d294b07f8a
Julien Pauli <jpauli@php.net>
2016-03-29 18:08:54 +0200

    5.5.34

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
 #define PHP_RELEASE_VERSION 34
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.34-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.5.34"
 #define PHP_VERSION_ID 50534
