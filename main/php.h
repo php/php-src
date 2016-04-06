49c89ba200d381e82519f872395731114deee717
Jani Ollikainen <jani.ollikainen@valve.fi>
2016-04-06 14:14:41 +0300

    Added backwards compability with php_log_err by using macro

--- main/php.h
+++ main/php.h
@@ -280,7 +280,8 @@ PHPAPI size_t php_write(void *buf, size_t size);
 PHPAPI size_t php_printf(const char *format, ...) PHP_ATTRIBUTE_FORMAT(printf, 1,
 		2);
 PHPAPI int php_get_module_initialized(void);
-PHPAPI ZEND_COLD void php_log_err(char *log_message, int syslog_type_int);
+#define php_log_err(msg) php_log_err_with_serevity(msg, LOG_NOTICE)
+PHPAPI ZEND_COLD void php_log_err_with_severity(char *log_message, int syslog_type_int);
 int Debug(char *format, ...) PHP_ATTRIBUTE_FORMAT(printf, 1, 2);
 int cfgparse(void);
 END_EXTERN_C()
