8921449785163556b1565b6c5c2a27a1f2b2a20b
Dmitry Stogov <dmitry@zend.com>
2016-04-07 18:27:49 +0300

    Added missing "break"

--- ext/opcache/Optimizer/zend_ssa.c
+++ ext/opcache/Optimizer/zend_ssa.c
@@ -664,6 +664,7 @@ static int zend_ssa_rename(const zend_op_array *op_array, uint32_t build_flags, 
 						var[EX_VAR_TO_NUM(opline->op1.var)] = ssa_vars_count;
 						ssa_vars_count++;
 					}
+					break;
 				case ZEND_VERIFY_RETURN_TYPE:
 					if (opline->op1_type != IS_CONST) {
 						ssa_ops[k].op1_def = ssa_vars_count;
