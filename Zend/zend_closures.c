fea04a528bd66f6dc678e5272e7598543ea40d09
Nikita Popov <nikic@php.net>
2016-04-16 14:34:32 +0200

    Drop dead assignment

--- Zend/zend_closures.c
+++ Zend/zend_closures.c
@@ -523,7 +523,6 @@ void zend_register_closure_ce(void) /* {{{ */
 
 	memcpy(&closure_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
 	closure_handlers.free_obj = zend_closure_free_storage;
-	closure_handlers.clone_obj = NULL;
 	closure_handlers.get_constructor = zend_closure_get_constructor;
 	closure_handlers.get_method = zend_closure_get_method;
 	closure_handlers.write_property = zend_closure_write_property;
