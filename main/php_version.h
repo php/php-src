d650063a0457aec56364e4005a636dc6c401f9cd
Stanislav Malyshev <stas@php.net>
2016-04-24 18:33:32 -0700

    Fix bug #72093: bcpowmod accepts negative scale and corrupts _one_ definition
    We can not modify result since it can be copy of _zero_ or _one_, etc. and
    "copy" in bcmath is just bumping the refcount.

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 35
+#define PHP_RELEASE_VERSION 27
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.5.35-dev"
-#define PHP_VERSION_ID 50535
+#define PHP_VERSION "5.5.27-dev"
+#define PHP_VERSION_ID 50527
