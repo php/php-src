84bf7ae59b995614b8503b2492a533bf92ffbec3
Dmitry Stogov <dmitry@zend.com>
2016-04-29 13:43:43 +0300

    Fixed "comparison between signed and unsigned integer expressions" compilation warnings

--- Zend/zend_object_handlers.h
+++ Zend/zend_object_handlers.h
@@ -27,8 +27,8 @@ struct _zend_property_info;
 #define ZEND_WRONG_PROPERTY_INFO \
 	((struct _zend_property_info*)((zend_intptr_t)-1))
 
-#define ZEND_DYNAMIC_PROPERTY_OFFSET (-1)
-#define ZEND_WRONG_PROPERTY_OFFSET   (-2)
+#define ZEND_DYNAMIC_PROPERTY_OFFSET ((uint32_t)(-1))
+#define ZEND_WRONG_PROPERTY_OFFSET   ((uint32_t)(-2))
 
 /* The following rule applies to read_property() and read_dimension() implementations:
    If you return a zval which is not otherwise referenced by the extension or the engine's
