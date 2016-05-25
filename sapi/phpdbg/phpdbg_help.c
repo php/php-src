54eb659032e23904393974ff610b6df97922c549
Adam Harvey <aharvey@php.net>
2016-05-25 12:47:16 -0500

    Fix typo in phpdbg help.
    Sorry, I would have included this in the last push had I spotted it a
    minute earlier.

--- sapi/phpdbg/phpdbg_help.c
+++ sapi/phpdbg/phpdbg_help.c
@@ -780,7 +780,7 @@ phpdbg_help_text_t phpdbg_help_text[] = {
 },
 
 {"run",
-"Enter the vm, startinging execution. Execution will then continue until the next breakpoint "
+"Enter the vm, starting execution. Execution will then continue until the next breakpoint "
 "or completion of the script. Add parameters you want to use as $argv" CR CR
 
 "**Examples**" CR CR
