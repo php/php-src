5bf4d17df93ada6821e37eb73268050f6a300ef3
Anatol Belski <ab@php.net>
2016-05-28 18:54:27 +0200

    clear arg names

--- win32/codepage.h
+++ win32/codepage.h
@@ -61,14 +61,14 @@ PW32CP const struct php_win32_cp *php_win32_cp_cli_do_restore(DWORD);
 
 /* This API is binary safe and expects a \0 terminated input.
    The returned out is \0 terminated, but the length doesn't count \0. */
-PW32CP wchar_t *php_win32_cp_conv_to_w(DWORD cp, DWORD flags, const char* in, size_t in_len, size_t *out_len);
+PW32CP wchar_t *php_win32_cp_conv_to_w(DWORD in_cp, DWORD flags, const char* in, size_t in_len, size_t *out_len);
 PW32CP wchar_t *php_win32_cp_conv_utf8_to_w(const char* in, size_t in_len, size_t *out_len);
 #define php_win32_cp_utf8_to_w(in) php_win32_cp_conv_utf8_to_w(in, PHP_WIN32_CP_IGNORE_LEN, PHP_WIN32_CP_IGNORE_LEN_P)
 PW32CP wchar_t *php_win32_cp_conv_cur_to_w(const char* in, size_t in_len, size_t *out_len);
 #define php_win32_cp_cur_to_w(in) php_win32_cp_conv_cur_to_w(in, PHP_WIN32_CP_IGNORE_LEN, PHP_WIN32_CP_IGNORE_LEN_P)
 PW32CP wchar_t *php_win32_cp_conv_ascii_to_w(const char* in, size_t in_len, size_t *out_len);
 #define php_win32_cp_ascii_to_w(in) php_win32_cp_conv_ascii_to_w(in, PHP_WIN32_CP_IGNORE_LEN, PHP_WIN32_CP_IGNORE_LEN_P)
-PW32CP char *php_win32_cp_conv_from_w(DWORD cp, DWORD flags, wchar_t* in, size_t in_len, size_t *out_len);
+PW32CP char *php_win32_cp_conv_from_w(DWORD out_cp, DWORD flags, wchar_t* in, size_t in_len, size_t *out_len);
 PW32CP char *php_win32_cp_conv_w_to_utf8(wchar_t* in, size_t in_len, size_t *out_len);
 #define php_win32_cp_w_to_utf8(in) php_win32_cp_conv_w_to_utf8(in, PHP_WIN32_CP_IGNORE_LEN, PHP_WIN32_CP_IGNORE_LEN_P)
 PW32CP char *php_win32_cp_conv_w_to_cur(wchar_t* in, size_t in_len, size_t *out_len);
