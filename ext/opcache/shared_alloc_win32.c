bf62f030d6f5f29bd4334fdfa6f8e1caf643517c
Anatol Belski <ab@php.net>
2016-06-01 14:32:04 +0200

    use W API to get user name

--- ext/opcache/shared_alloc_win32.c
+++ ext/opcache/shared_alloc_win32.c
@@ -77,11 +77,26 @@ static void zend_win_error_message(int type, char *msg, int err)
 static char *create_name_with_username(char *name)
 {
 	static char newname[MAXPATHLEN + UNLEN + 4 + 1 + 32];
-	char uname[UNLEN + 1];
+	wchar_t unamew[UNLEN + 1];
+	char *uname;
+	size_t uname_len;
 	DWORD unsize = UNLEN;
 
-	GetUserName(uname, &unsize);
+	GetUserNameW(unamew, &unsize);
+	uname = php_win32_cp_conv_w_to_any(unamew, unsize - 1, &uname_len);
+	if (!uname) {
+		return NULL;
+	}
+
+	/* Ensure the length doesn't overflow. */
+	if (uname_len > UNLEN) {
+		uname[uname_len] = '\0';
+	}
+
 	snprintf(newname, sizeof(newname) - 1, "%s@%s@%.32s", name, uname, ZCG(system_id));
+
+	free(uname);
+
 	return newname;
 }
 
