05033c9ebd015baf825581e5725a594da277d560
Jakub Zelenka <bukka@php.net>
2016-06-08 18:21:39 +0100

    Fix bug #72140 (segfault after calling ERR_free_strings())

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -1278,6 +1278,9 @@ PHP_MSHUTDOWN_FUNCTION(openssl)
 	EVP_cleanup();
 
 #if OPENSSL_VERSION_NUMBER >= 0x00090805f
+	/* prevent accessing locking callback from unloaded extension */
+	CRYPTO_set_locking_callback(NULL);
+	/* free allocated error strings */
 	ERR_free_strings();
 #endif
 
