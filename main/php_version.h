378de94faad2f6c74e47e06db22639aaee94841b
Joe Watkins <krakjoe@php.net>
2016-06-22 06:33:20 +0100

    bump version after release

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "alpha2"
-#define PHP_VERSION "7.1.0alpha2"
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "7.1.0-dev"
 #define PHP_VERSION_ID 70100
