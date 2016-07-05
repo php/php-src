a66cae883bff56904e366f0e0da72dce3f9eccb1
Anatol Belski <ab@php.net>
2016-07-05 08:29:51 +0200

    set versions

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
 #define PHP_RELEASE_VERSION 9
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.9-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.0.9RC1"
 #define PHP_VERSION_ID 70009
