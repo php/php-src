c196cac71e4877880de800eff578097760446eee
Davey Shafik <me@daveyshafik.com>
2016-07-05 12:18:16 -0700

    Bump versions/dates for release of 7.1.0alpha3

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.0-dev"
+#define PHP_EXTRA_VERSION "-alpha3"
+#define PHP_VERSION "7.1.0-alpha3"
 #define PHP_VERSION_ID 70100
