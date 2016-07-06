c1280db28afd537c59611b87abf114586ebbe5d0
Ferenc Kovacs <tyra3l@gmail.com>
2016-07-07 00:09:48 +0200

    5.6.25 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 24
+#define PHP_RELEASE_VERSION 25
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.24-dev"
-#define PHP_VERSION_ID 50624
+#define PHP_VERSION "5.6.25-dev"
+#define PHP_VERSION_ID 50625
