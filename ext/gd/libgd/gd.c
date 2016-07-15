3666cfab973c5aba86d0562d14de8ed74e66cdb7
Christoph M. Becker <cmb@php.net>
2016-07-15 19:42:26 +0200

    Fix #72604: imagearc() ignores thickness for full arcs
    We remove the special casing for full arcs, what conforms to external libgd.

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -1673,11 +1673,7 @@ long lsqrt (long n)
 
 void gdImageArc (gdImagePtr im, int cx, int cy, int w, int h, int s, int e, int color)
 {
-	if ((s % 360) == (e % 360)) {
-		gdImageEllipse(im, cx, cy, w, h, color);
-	} else {
-		gdImageFilledArc(im, cx, cy, w, h, s, e, color, gdNoFill);
-	}
+	gdImageFilledArc(im, cx, cy, w, h, s, e, color, gdNoFill);
 }
 
 void gdImageFilledArc (gdImagePtr im, int cx, int cy, int w, int h, int s, int e, int color, int style)
