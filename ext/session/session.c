4742cce11d4946e00f6798c084033315e09d0169
Yasuo Ohgaki <yohgaki@php.net>
2016-07-18 13:45:09 +0900

    Add test. Relax validtion

--- ext/session/session.c
+++ ext/session/session.c
@@ -344,7 +344,7 @@ PHPAPI int php_session_valid_key(const char *key) /* {{{ */
 
 	/* Somewhat arbitrary length limit here, but should be way more than
 	   anyone needs and avoids file-level warnings later on if we exceed MAX_PATH */
-	if (len == 0 || len > 128) {
+	if (len == 0 || len > 256) {
 		ret = FAILURE;
 	}
 
