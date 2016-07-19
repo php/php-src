928aecc002e906b309b28f0062f03d4e5eda3e45
Pierre Joye <pierre.php@gmail.com>
2016-07-19 13:37:23 +0700

    fix #72512, invalid read or write for palette image when invalid transparent index is used
    Conflicts:
    	ext/gd/libgd/gd.c

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -602,11 +602,11 @@ void gdImageColorTransparent (gdImagePtr im, int color)
 	if (color < 0) {
 		return;
 	}
-
 	if (!im->trueColor) {
-		if((color >= gdMaxColors)) {
+		if((color >= im->colorsTotal)) {
 			return;
 		}
+		/* Make the old transparent color opaque again */
 		if (im->transparent != -1) {
 			im->alpha[im->transparent] = gdAlphaOpaque;
 		}
