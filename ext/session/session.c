49acf57eba055326905ef06b96d9be96d434caae
Yasuo Ohgaki <yohgaki@php.net>
2016-07-26 04:22:12 +0900

    Use macro

--- ext/session/session.c
+++ ext/session/session.c
@@ -344,7 +344,7 @@ PHPAPI int php_session_valid_key(const char *key) /* {{{ */
 
 	/* Somewhat arbitrary length limit here, but should be way more than
 	   anyone needs and avoids file-level warnings later on if we exceed MAX_PATH */
-	if (len == 0 || len > 256) {
+	if (len == 0 || len > PS_MAX_SID_LENGTH) {
 		ret = FAILURE;
 	}
 
