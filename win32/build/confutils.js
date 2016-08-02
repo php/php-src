845f66ba64c8a51f5764df64ac99c9fc89efce3c
Anatol Belski <ab@php.net>
2016-08-02 11:00:54 +0200

    increase versions for 7.2

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -103,7 +103,7 @@ var PHP_VERSION = 7;
 var PHP_MINOR_VERSION = 1;
 var PHP_RELEASE_VERSION = 0;
 var PHP_EXTRA_VERSION = "";
-var PHP_VERSION_STRING = "7.1.0";
+var PHP_VERSION_STRING = "7.2.0";
 
 /* Get version numbers and DEFINE as a string */
 function get_version_numbers()
