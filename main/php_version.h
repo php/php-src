562c17eb4e78541a2aa648aef6a4454636d54f09
Ferenc Kovacs <tyra3l@gmail.com>
2016-08-04 01:39:37 +0200

    5.6.26 will be next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 25
+#define PHP_RELEASE_VERSION 26
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.25-dev"
-#define PHP_VERSION_ID 50625
+#define PHP_VERSION "5.6.26-dev"
+#define PHP_VERSION_ID 50626
