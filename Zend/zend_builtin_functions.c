86aa1c784d3b584e6d0c235870a882c8fd169a74
Danack <Danack@basereality.com>
2016-08-11 23:21:04 +0100

    Require parameter to be an object if passed.

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -1009,7 +1009,7 @@ ZEND_FUNCTION(get_class)
 {
 	zval *obj = NULL;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "|o!", &obj) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "|o", &obj) == FAILURE) {
 		RETURN_FALSE;
 	}
 
