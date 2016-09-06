dad793630d5966a9c22f3fcd7f24b7937bd1a36f
Christoph M. Becker <cmbecker69@gmx.de>
2016-09-06 12:05:22 +0200

    Fix #73025: Heap Buffer Overflow in virtual_popen of zend_virtual_cwd.c
    `command_length` is retrieved via strlen() and later passed to emalloc()
    and memcpy(), so the appropriate type is `size_t`.
    
    We don't add a regression test, because that would need to allocate a string
    of at least 2 GiB.

--- Zend/zend_virtual_cwd.c
+++ Zend/zend_virtual_cwd.c
@@ -1896,7 +1896,7 @@ CWD_API FILE *virtual_popen(const char *command, const char *type TSRMLS_DC) /* 
 #else /* Unix */
 CWD_API FILE *virtual_popen(const char *command, const char *type TSRMLS_DC) /* {{{ */
 {
-	int command_length;
+	size_t command_length;
 	int dir_length, extra = 0;
 	char *command_line;
 	char *ptr, *dir;
