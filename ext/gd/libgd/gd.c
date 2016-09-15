04b166a6c56315e84a9d40c751f29b81f84a10c1
Christoph M. Becker <cmbecker69@gmx.de>
2016-09-15 13:04:30 +0200

    Removed unused function
    The earlier gdAlphaBlend() has been replaced with commit 1286d9e2, and has
    been renamed to gdAlphaBlendOld(). The old function is, however, not used,
    and after more than 9 years we're pretty sure we won't need it anymore.

--- ext/gd/libgd/gd.c
+++ ext/gd/libgd/gd.c
@@ -2918,29 +2918,6 @@ int gdImageCompare (gdImagePtr im1, gdImagePtr im2)
 	return cmpStatus;
 }
 
-int
-gdAlphaBlendOld (int dst, int src)
-{
-	/* 2.0.12: TBB: alpha in the destination should be a
-	 * component of the result. Thanks to Frank Warmerdam for
-	 * pointing out the issue.
-	 */
-	return ((((gdTrueColorGetAlpha (src) *
-	     gdTrueColorGetAlpha (dst)) / gdAlphaMax) << 24) +
-	  ((((gdAlphaTransparent - gdTrueColorGetAlpha (src)) *
-	     gdTrueColorGetRed (src) / gdAlphaMax) +
-	    (gdTrueColorGetAlpha (src) *
-	     gdTrueColorGetRed (dst)) / gdAlphaMax) << 16) +
-	  ((((gdAlphaTransparent - gdTrueColorGetAlpha (src)) *
-	     gdTrueColorGetGreen (src) / gdAlphaMax) +
-	    (gdTrueColorGetAlpha (src) *
-	     gdTrueColorGetGreen (dst)) / gdAlphaMax) << 8) +
-	  (((gdAlphaTransparent - gdTrueColorGetAlpha (src)) *
-	    gdTrueColorGetBlue (src) / gdAlphaMax) +
-	   (gdTrueColorGetAlpha (src) *
-	    gdTrueColorGetBlue (dst)) / gdAlphaMax));
-}
-
 int gdAlphaBlend (int dst, int src) {
     int src_alpha = gdTrueColorGetAlpha(src);
     int dst_alpha, alpha, red, green, blue;
