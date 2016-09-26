d13507d2e6d7c280888cc9c3cab125b347662e90
Stanislav Malyshev <stas@php.net>
2016-09-25 19:53:59 -0700

    Fix bug #73147: Use After Free in PHP7 unserialize()
    (cherry picked from commit 0e6fe3a4c96be2d3e88389a5776f878021b4c59f)
    (cherry picked from commit f42cbd749cde1f91274c1d03df9024baba141a8f)

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -343,6 +343,7 @@ ZEND_API void zend_update_property_double(zend_class_entry *scope, zval *object,
 ZEND_API void zend_update_property_str(zend_class_entry *scope, zval *object, const char *name, size_t name_length, zend_string *value);
 ZEND_API void zend_update_property_string(zend_class_entry *scope, zval *object, const char *name, size_t name_length, const char *value);
 ZEND_API void zend_update_property_stringl(zend_class_entry *scope, zval *object, const char *name, size_t name_length, const char *value, size_t value_length);
+ZEND_API void zend_unset_property(zend_class_entry *scope, zval *object, const char *name, size_t name_length);
 
 ZEND_API int zend_update_static_property(zend_class_entry *scope, const char *name, size_t name_length, zval *value);
 ZEND_API int zend_update_static_property_null(zend_class_entry *scope, const char *name, size_t name_length);
