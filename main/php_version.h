40e7baab3c90001beee4c8f0ed0ef79ad18ee0d6
Stanislav Malyshev <stas@php.net>
2016-10-03 00:09:02 -0700

    Fix bug #73190: memcpy negative parameter _bc_new_num_ex

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 27
+#define PHP_RELEASE_VERSION 26
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.27-dev"
-#define PHP_VERSION_ID 50627
+#define PHP_VERSION "5.6.26-dev"
+#define PHP_VERSION_ID 50626
