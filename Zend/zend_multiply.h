0362d61ab75c4b6faa99ca57d49cd800530b482f
Stanislav Malyshev <stas@php.net>
2016-10-12 23:07:47 -0700

    Fix potential overflows in php_pcre_replace_impl
    (cherry picked from commit 9c50ba42d6503a5fcfffad6c6823a9bee0e033c0)

--- Zend/zend_multiply.h
+++ Zend/zend_multiply.h
@@ -266,6 +266,18 @@ static zend_always_inline size_t zend_safe_address(size_t nmemb, size_t size, si
 }
 #endif
 
+static zend_always_inline size_t safe_address(size_t nmemb, size_t size, size_t offset)
+{
+	int overflow;
+	size_t ret = zend_safe_address(nmemb, size, offset, &overflow);
+
+	if (UNEXPECTED(overflow)) {
+		zend_error_noreturn(E_ERROR, "Possible integer overflow in memory allocation (%zu * %zu + %zu)", nmemb, size, offset);
+		return 0;
+	}
+	return ret;
+}
+
 #endif /* ZEND_MULTIPLY_H */
 
 /*
