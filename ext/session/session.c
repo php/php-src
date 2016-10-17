bcae9698addf98f931ec9bbcc4393e933c21c2be
Yasuo Ohgaki <yohgaki@php.net>
2016-10-17 19:53:10 +0900

    Add missing SUCCESS

--- ext/session/session.c
+++ ext/session/session.c
@@ -1624,7 +1624,7 @@ static int php_session_abort(void) /* {{{ */
 static int php_session_reset(void) /* {{{ */
 {
 	if (PS(session_status) == php_session_active
-		&& php_session_initialize()) {
+		&& php_session_initialize() == SUCCESS) {
 		return SUCCESS;
 	}
 	return FAILURE;
