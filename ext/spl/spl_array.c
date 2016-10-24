f74d7d92c8bc1edc2505e0b58546217e9e1ecb40
Stanislav Malyshev <stas@php.net>
2016-10-23 21:56:35 -0700

    Fix bug #73144 and bug #73341 - remove extra dtor

--- ext/spl/spl_array.c
+++ ext/spl/spl_array.c
@@ -1812,7 +1812,7 @@ SPL_METHOD(Array, unserialize)
 		ALLOC_INIT_ZVAL(intern->array);
 		if (!php_var_unserialize(&intern->array, &p, s + buf_len, &var_hash TSRMLS_CC)
 				|| (Z_TYPE_P(intern->array) != IS_ARRAY && Z_TYPE_P(intern->array) != IS_OBJECT)) {
-			zval_ptr_dtor(&intern->array);
+			// zval_ptr_dtor(&intern->array);
 			goto outexcept;
 		}
 		var_push_dtor(&var_hash, &intern->array);
