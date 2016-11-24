e715dafae0010f7d7b69d81b6ce3dd1bfc773477
Ferenc Kovacs <tyra3l@gmail.com>
2016-11-24 01:54:23 +0100

    5.6.30 will be next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 29
+#define PHP_RELEASE_VERSION 30
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.29-dev"
-#define PHP_VERSION_ID 50629
+#define PHP_VERSION "5.6.30-dev"
+#define PHP_VERSION_ID 50630
