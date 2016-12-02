2f5090168fe759a0fb94e5d78470a50f0e868796
Davey Shafik <me@daveyshafik.com>
2016-12-02 02:14:51 -0800

    Bump versions and NEWS for 7.1.1-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 0
+#define PHP_RELEASE_VERSION 1
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.0-dev"
-#define PHP_VERSION_ID 70100
+#define PHP_VERSION "7.1.1-dev"
+#define PHP_VERSION_ID 70101
