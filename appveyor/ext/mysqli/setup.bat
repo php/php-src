572403371fcf41b90d17ee180ca18154a481dd1f
Anatol Belski <ab@php.net>
2016-12-04 22:50:24 +0100

    remove setup scripts, that are integrated into the task script

--- appveyor/ext/mysqli/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Preparing MySQL for tests
-
-set MYSQL_PWD=Password12!
-
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" --user=%MYSQL_TEST_USER% -e "CREATE DATABASE IF NOT EXISTS test"
-
