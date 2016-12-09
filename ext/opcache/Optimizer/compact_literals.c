786d4bbc48906efbe8e9823c3dc709935f51df35
Dmitry Stogov <dmitry@zend.com>
2016-12-09 13:33:07 +0300

    Fixed optimizer support for ZEND_ASSIGN_OBJ_REF

--- ext/opcache/Optimizer/compact_literals.c
+++ ext/opcache/Optimizer/compact_literals.c
@@ -230,6 +230,7 @@ void zend_optimizer_compact_literals(zend_op_array *op_array, zend_optimizer_ctx
 					}
 					break;
 				case ZEND_ASSIGN_OBJ:
+				case ZEND_ASSIGN_OBJ_REF:
 				case ZEND_FETCH_OBJ_R:
 				case ZEND_FETCH_OBJ_W:
 				case ZEND_FETCH_OBJ_RW:
