6cb4c9f1d3b7d44b2c837aa0e1493353485ebab3
Anatol Belski <ab@php.net>
2016-12-09 21:07:56 +0100

    better parametrization

--- appveyor/build_task.bat
+++ appveyor/build_task.bat
@@ -17,7 +17,7 @@ if /i "%APPVEYOR_REPO_BRANCH:~0,4%" equ "php-" (
 	set BRANCH=master
 	set STABILITY=staging
 )
-set DEPS_DIR=%PHP_BUILD_CACHE_BASE_DIR%\deps-vc14-%PLATFORM%-%APPVEYOR_REPO_BRANCH%
+set DEPS_DIR=%PHP_BUILD_CACHE_BASE_DIR%\deps-%PHP_SDK_VC%-%PHP_SDK_ARCH%-%APPVEYOR_REPO_BRANCH%
 rem SDK is cached, deps info is cached as well
 echo Updating dependencies
 call phpsdk_deps --update --branch %BRANCH% --stability %STABILITY% --deps %DEPS_DIR%
