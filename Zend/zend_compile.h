2c70581338257cba48991e313309839737c94c64
Nikita Popov <nikic@php.net>
2016-12-11 13:31:37 +0100

    Fix T_NUM_STRING negation
    T_NUM_STRING follows the rules of symtable numeric string
    conversion. If the offset isn't an integer under those rules, it
    is treated as a string. This should apply to negated T_NUM_STRINGs
    as well.

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -726,7 +726,10 @@ ZEND_API binary_op_type get_binary_op(int opcode);
 
 void zend_stop_lexing(void);
 void zend_emit_final_return(int return_one);
+
+/* Used during AST construction */
 zend_ast *zend_ast_append_str(zend_ast *left, zend_ast *right);
+zend_ast *zend_negate_num_string(zend_ast *ast);
 uint32_t zend_add_class_modifier(uint32_t flags, uint32_t new_flag);
 uint32_t zend_add_member_modifier(uint32_t flags, uint32_t new_flag);
 void zend_handle_encoding_declaration(zend_ast *ast);
