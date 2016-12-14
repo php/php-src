e33ec61f9c1baa73bfe1b03b8c48a824ab2a867e
Anatol Belski <ab@php.net>
2016-12-14 02:51:29 +0100

    Care about intput and output encoding, as per default encoding RFC
    If PHP CLI is used with programs with no Unicode support, the default
    PHP console codepage might cause backward incompatible behaviors. This
    is solved with this patch by separating the handling of I/O codepage.
    As per https://wiki.php.net/rfc/default_encoding input_encodnig and
    output_encoding are centralized INI settings, so they're used for the
    purpose of adjusting the codepage. This gives user the ability to use
    UTF-8 internally, while letting the system API to convert the output
    to a compatible codepage. This also might solve the font issues on
    systems without good true type support.
    
    Note, that there no change to the default behavior - if input and
    output encoding are not set, which is the default case, the default
    charset applies to both console input and output. The patch is just
    a compilment to the backward compatibility, not more and not less.

--- main/main.c
+++ main/main.c
@@ -435,6 +435,9 @@ static PHP_INI_MH(OnUpdateInputEncoding)
 {
 	if (new_value) {
 		OnUpdateString(entry, new_value, mh_arg1, mh_arg2, mh_arg3, stage);
+#ifdef PHP_WIN32
+		php_win32_cp_do_update(NULL);
+#endif
 	}
 	return SUCCESS;
 }
@@ -446,6 +449,9 @@ static PHP_INI_MH(OnUpdateOutputEncoding)
 {
 	if (new_value) {
 		OnUpdateString(entry, new_value, mh_arg1, mh_arg2, mh_arg3, stage);
+#ifdef PHP_WIN32
+		php_win32_cp_do_update(NULL);
+#endif
 	}
 	return SUCCESS;
 }
