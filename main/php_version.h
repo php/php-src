4cce1e9a94e50a3f07e38e6c7110501e3b90a964
Joe Watkins <krakjoe@php.net>
2017-01-05 17:00:40 +0000

    wrong version

--- main/php_version.h
+++ main/php_version.h
@@ -4,5 +4,5 @@
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 2
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.1-dev"
+#define PHP_VERSION "7.1.2-dev"
 #define PHP_VERSION_ID 70102
