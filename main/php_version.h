d2eca4da421317be535abe3ec7d22c03997fbe34
Ferenc Kovacs <tyra3l@gmail.com>
2017-01-06 01:56:05 +0100

    prepare 5.6.30RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 31
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.31-dev"
-#define PHP_VERSION_ID 50631
+#define PHP_RELEASE_VERSION 30
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "5.6.30RC1"
+#define PHP_VERSION_ID 50630
