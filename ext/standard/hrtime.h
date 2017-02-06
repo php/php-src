2b32f686882ba0a8202fe38a6295e440219dd92a
Niklas Keller <me@kelunik.com>
2017-02-06 18:16:45 +0100

    Fix bogus elif

--- ext/standard/hrtime.h
+++ ext/standard/hrtime.h
@@ -27,7 +27,7 @@
 #define HRTIME_AVAILABLE 1
 #elif defined(__APPLE__)
 #define HRTIME_AVAILABLE 1
-#elif #elif (defined(__hpux) || defined(hpux)) || ((defined(__sun__) || defined(__sun) || defined(sun)) && (defined(__SVR4) || defined(__svr4__)))
+#elif (defined(__hpux) || defined(hpux)) || ((defined(__sun__) || defined(__sun) || defined(sun)) && (defined(__SVR4) || defined(__svr4__)))
 #define HRTIME_AVAILABLE 1
 #else
 #define HRTIME_AVAILABLE 0
