32d72b3b05f9325f0f81a9bf0b636817912ec1b5
Anatol Belski <ab@php.net>
2017-03-01 13:01:46 +0100

    extend SHM string set on demand in reattached process

--- ext/opcache/ZendAccelerator.c
+++ ext/opcache/ZendAccelerator.c
@@ -626,12 +626,18 @@ static zend_string *accel_replace_string_by_shm_permanent(zend_string *str)
 {
 	zend_string *ret = accel_find_interned_string(str);
 
-	if (ret) {
-		zend_string_release(str);
-		return ret;
+	if (!ret) {
+		/* If a string is not in SHM but in the local permanent table,
+		       the particular process might have another set of shared
+		       extensions or were going a different code path and
+		       produce another set of SHM strings. It is no reason to
+		       fail in a reattached process, extending the SHM string
+		       set is ok. */
+		ret = accel_new_interned_string(str);
 	}
-	ZEND_ASSERT(0);
-	return str;
+
+	zend_string_release(str);
+	return ret;
 }
 
 static zend_string *accel_replace_string_by_process_permanent(zend_string *str)
