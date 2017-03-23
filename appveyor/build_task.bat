0f6232675d6607e7f61182ec0b93fc3092feead8
Anatol Belski <ab@php.net>
2017-03-23 19:26:30 +0100

    parametrize crt, so only one place has to be changed

--- appveyor/build_task.bat
+++ appveyor/build_task.bat
@@ -20,7 +20,7 @@ if /i "%APPVEYOR_REPO_BRANCH:~0,4%" equ "php-" (
 set DEPS_DIR=%PHP_BUILD_CACHE_BASE_DIR%\deps-%BRANCH%-%PHP_SDK_VC%-%PHP_SDK_ARCH%
 rem SDK is cached, deps info is cached as well
 echo Updating dependencies in %DEPS_DIR%
-call phpsdk_deps --update --branch %BRANCH% --stability %STABILITY% --deps %DEPS_DIR% --crt vc15
+call phpsdk_deps --update --branch %BRANCH% --stability %STABILITY% --deps %DEPS_DIR% --crt %PHP_BUILD_CRT%
 if %errorlevel% neq 0 exit /b 3
 
 call buildconf.bat --force
