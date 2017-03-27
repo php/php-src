867c29caf07384828d9f7670647a6a9c1ed4717c
Anatol Belski <ab@php.net>
2017-03-27 18:07:16 +0200

    always use staging deps for CI, that's what we have a CI for as well

--- appveyor/build_task.bat
+++ appveyor/build_task.bat
@@ -12,11 +12,10 @@ if %errorlevel% neq 0 exit /b 3
 
 if /i "%APPVEYOR_REPO_BRANCH:~0,4%" equ "php-" (
 	set BRANCH=%APPVEYOR_REPO_BRANCH:~4,3%
-	set STABILITY=stable
 ) else (
 	set BRANCH=master
-	set STABILITY=staging
 )
+set STABILITY=staging
 set DEPS_DIR=%PHP_BUILD_CACHE_BASE_DIR%\deps-%BRANCH%-%PHP_SDK_VC%-%PHP_SDK_ARCH%
 rem SDK is cached, deps info is cached as well
 echo Updating dependencies in %DEPS_DIR%
