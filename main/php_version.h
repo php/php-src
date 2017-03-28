e38310a151bf1fc5a92ed7d277efb900007f2774
Joe Watkins <krakjoe@php.net>
2017-03-28 15:22:10 +0100

    prepare next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 4
+#define PHP_RELEASE_VERSION 5
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.4-dev"
-#define PHP_VERSION_ID 70104
+#define PHP_VERSION "7.1.5-dev"
+#define PHP_VERSION_ID 70105
