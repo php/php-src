53a702c0206b58454b24a5a9d361a7526bcf0217
Anatol Belski <ab@php.net>
2017-04-25 14:06:17 +0200

    move next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 19
+#define PHP_RELEASE_VERSION 20
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.19-dev"
-#define PHP_VERSION_ID 70019
+#define PHP_VERSION "7.0.20-dev"
+#define PHP_VERSION_ID 70020
