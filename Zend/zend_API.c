ece7d223e821f57593e7c005a83500f6b491d723
Xinchen Hui <laruence@gmail.com>
2017-05-27 12:11:53 +0800

    Correct the location

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -1160,9 +1160,8 @@ ZEND_API int zend_update_class_constants(zend_class_entry *class_type) /* {{{ */
 
 			*scope = old_scope;
 		}
+		class_type->ce_flags |= ZEND_ACC_CONSTANTS_UPDATED;
 	}
-
-	class_type->ce_flags |= ZEND_ACC_CONSTANTS_UPDATED;
 
 	return SUCCESS;
 }
