9da0c5c0ded5b526342406db1408cc4a688bb7d9
Anatol Belski <ab@php.net>
2017-05-28 18:51:04 +0200

    enable ext/dba on AppVeyor

--- appveyor/build_task.bat
+++ appveyor/build_task.bat
@@ -27,7 +27,7 @@ if %errorlevel% neq 0 exit /b 3
 
 if "%THREAD_SAFE%" equ "0" set ADD_CONF=--disable-zts
 
-set EXT_EXCLUDE_FROM_TEST=snmp,oci8_12c,pdo_oci,pdo_odbc,odbc,pdo_firebird,interbase,ldap,imap,dba
+set EXT_EXCLUDE_FROM_TEST=snmp,oci8_12c,pdo_oci,pdo_odbc,odbc,pdo_firebird,interbase,ldap,imap
 if "%OPCACHE%" equ "0" set EXT_EXCLUDE_FROM_TEST=%EXT_EXCLUDE_FROM_TEST%,opcache
 
 call configure.bat ^
