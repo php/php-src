2693e52113ea0369144073e84d568931ffc173a7
Remi Collet <remi@php.net>
2017-05-30 15:37:11 +0200

    Patch from the upstream git https://github.com/kkos/oniguruma/issues/57 (CVE-2017-9224)
    Thanks to Mamoru TASAKA <mtasaka@fedoraproject.org>

--- ext/mbstring/oniguruma/regexec.c
+++ ext/mbstring/oniguruma/regexec.c
@@ -1425,14 +1425,9 @@ match_at(regex_t* reg, const UChar* str, const UChar* end,
       break;
 
     case OP_EXACT1:  MOP_IN(OP_EXACT1);
-#if 0
       DATA_ENSURE(1);
       if (*p != *s) goto fail;
       p++; s++;
-#endif
-      if (*p != *s++) goto fail;
-      DATA_ENSURE(0);
-      p++;
       MOP_OUT;
       break;
 
