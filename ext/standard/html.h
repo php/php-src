68dc7549988fcc929717a7b0c69e007000512fc1
Dmitry Stogov <dmitry@zend.com>
2017-06-06 16:09:26 +0300

    Avoid string reallocations in html_entity_decode() and htmlspecialchars_decode()

--- ext/standard/html.h
+++ ext/standard/html.h
@@ -56,7 +56,7 @@ PHP_FUNCTION(get_html_translation_table);
 
 PHPAPI zend_string *php_escape_html_entities(unsigned char *old, size_t oldlen, int all, int flags, char *hint_charset);
 PHPAPI zend_string *php_escape_html_entities_ex(unsigned char *old, size_t oldlen, int all, int flags, char *hint_charset, zend_bool double_encode);
-PHPAPI zend_string *php_unescape_html_entities(unsigned char *old, size_t oldlen, int all, int flags, char *hint_charset);
+PHPAPI zend_string *php_unescape_html_entities(zend_string *str, int all, int flags, char *hint_charset);
 PHPAPI unsigned int php_next_utf8_char(const unsigned char *str, size_t str_len, size_t *cursor, int *status);
 
 #endif /* HTML_H */
