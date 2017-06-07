c3b910370c5c92007c3e3579024490345cb7f9a7
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-07 15:22:20 +0200

    Use reasonable SQLite3 module version
    Since PECL/sqlite3 has been moved to ext/sqlite3 in 2008, the SQLite3
    module version is hardcoded to `0.7-dev`. It doesn't appear to make
    much sense to have an own module version for a bundled extensions, but
    as other code might rely on it, we don't remove the constant but rather
    make it an alias of the PHP version.

--- ext/sqlite3/php_sqlite3.h
+++ ext/sqlite3/php_sqlite3.h
@@ -21,7 +21,7 @@
 #ifndef PHP_SQLITE3_H
 #define PHP_SQLITE3_H
 
-#define PHP_SQLITE3_VERSION	 "0.7-dev"
+#define PHP_SQLITE3_VERSION	PHP_VERSION
 
 extern zend_module_entry sqlite3_module_entry;
 #define phpext_sqlite3_ptr &sqlite3_module_entry
