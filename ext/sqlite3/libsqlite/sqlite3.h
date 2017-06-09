130f06a8a13cc23c7e3ae5f1b639110f310aea17
Christoph M. Becker <cmbecker69@gmx.de>
2017-06-09 14:21:19 +0200

    Update to SQLite 3.19.3

--- ext/sqlite3/libsqlite/sqlite3.h
+++ ext/sqlite3/libsqlite/sqlite3.h
@@ -121,9 +121,9 @@ extern "C" {
 ** [sqlite3_libversion_number()], [sqlite3_sourceid()],
 ** [sqlite_version()] and [sqlite_source_id()].
 */
-#define SQLITE_VERSION        "3.19.2"
-#define SQLITE_VERSION_NUMBER 3019002
-#define SQLITE_SOURCE_ID      "2017-05-25 16:50:27 edb4e819b0c058c7d74d27ebd14cc5ceb2bad6a6144a486a970182b7afe3f8b9"
+#define SQLITE_VERSION        "3.19.3"
+#define SQLITE_VERSION_NUMBER 3019003
+#define SQLITE_SOURCE_ID      "2017-06-08 14:26:16 0ee482a1e0eae22e08edc8978c9733a96603d4509645f348ebf55b579e89636b"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
