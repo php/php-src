b4fa2ba29572eca34d19df73347b1d1b73171821
Anatol Belski <ab@php.net>
2017-06-20 14:05:50 +0200

    move next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 21
+#define PHP_RELEASE_VERSION 22
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.21-dev"
-#define PHP_VERSION_ID 70021
+#define PHP_VERSION "7.0.22-dev"
+#define PHP_VERSION_ID 70022
