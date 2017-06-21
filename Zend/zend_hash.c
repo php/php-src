b1301a0686c5f573395b752295e569730fbd7de7
Thomas Punt <tpunt@hotmail.co.uk>
2017-06-21 21:47:22 +0100

    Remove superfluous branch

--- Zend/zend_hash.c
+++ Zend/zend_hash.c
@@ -291,7 +291,6 @@ static uint32_t zend_array_recalc_elements(HashTable *ht)
        uint32_t num = ht->nNumOfElements;
 
 	   ZEND_HASH_FOREACH_VAL(ht, val) {
-		   if (Z_TYPE_P(val) == IS_UNDEF) continue;
 		   if (Z_TYPE_P(val) == IS_INDIRECT) {
 			   if (UNEXPECTED(Z_TYPE_P(Z_INDIRECT_P(val)) == IS_UNDEF)) {
 				   num--;
