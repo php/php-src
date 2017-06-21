c7a75b7112b12fb7a62bd7cc67d2ef70d3a1280c
Dmitry Stogov <dmitry@zend.com>
2017-06-21 18:50:08 +0300

    Fixed few test failures if php.ini contains zend.assertions=-1

--- run-tests.php
+++ run-tests.php
@@ -254,6 +254,7 @@ $ini_overwrites = array(
 		'log_errors_max_len=0',
 		'opcache.fast_shutdown=0',
 		'opcache.file_update_protection=0',
+		'zend.assertions=1',
 	);
 
 $no_file_cache = '-d opcache.file_cache= -d opcache.file_cache_only=0';
