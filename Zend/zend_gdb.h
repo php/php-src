09cd2c8bce924ee1e4a39ccad8a631973e2072b9
Dmitry Stogov <dmitry@zend.com>
2017-07-17 15:04:31 +0300

    Fixed license

--- Zend/zend_gdb.h
+++ Zend/zend_gdb.h
@@ -1,16 +1,16 @@
 /*
    +----------------------------------------------------------------------+
-   | Zend JIT                                                             |
+   | Zend Engine                                                          |
    +----------------------------------------------------------------------+
-   | Copyright (c) 1998-2016 The PHP Group                                |
+   | Copyright (c) 1998-2017 Zend Technologies Ltd. (http://www.zend.com) |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
+   | This source file is subject to version 2.00 of the Zend license,     |
    | that is bundled with this package in the file LICENSE, and is        |
    | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
+   | http://www.zend.com/license/2_00.txt.                                |
+   | If you did not receive a copy of the Zend license and are unable to  |
    | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
+   | license@zend.com so we can mail you a copy immediately.              |
    +----------------------------------------------------------------------+
    | Authors: Dmitry Stogov <dmitry@zend.com>                             |
    |          Xinchen Hui <xinchen.h@zend.com>                            |
