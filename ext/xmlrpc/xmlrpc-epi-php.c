dc7c5da4963679bf851c2bd709f87aad8722af41
Andreas Treichel <gmblar+github@gmail.com>
2017-07-23 05:29:18 +0200

    Bug #74975: Different serialization for classes

--- ext/xmlrpc/xmlrpc-epi-php.c
+++ ext/xmlrpc/xmlrpc-epi-php.c
@@ -1355,6 +1355,9 @@ XMLRPC_VALUE_TYPE get_zval_xmlrpc_type(zval* value, zval* newvalue) /* {{{ */
 					type = xmlrpc_vector;
 
 					if ((attr = zend_hash_str_find(Z_OBJPROP_P(value), OBJECT_TYPE_ATTR, sizeof(OBJECT_TYPE_ATTR) - 1)) != NULL) {
+						if(Z_TYPE_P(attr) == IS_INDIRECT) {
+							attr = Z_INDIRECT_P(attr);
+						}
 						if (Z_TYPE_P(attr) == IS_STRING) {
 							type = xmlrpc_str_as_type(Z_STRVAL_P(attr));
 						}
@@ -1369,6 +1372,9 @@ XMLRPC_VALUE_TYPE get_zval_xmlrpc_type(zval* value, zval* newvalue) /* {{{ */
 
 			if ((type == xmlrpc_base64 && Z_TYPE_P(value) == IS_OBJECT) || type == xmlrpc_datetime) {
 				if ((val = zend_hash_str_find(Z_OBJPROP_P(value), OBJECT_VALUE_ATTR, sizeof(OBJECT_VALUE_ATTR) - 1)) != NULL) {
+					if(Z_TYPE_P(val) == IS_INDIRECT) {
+						val = Z_INDIRECT_P(val);
+					}
 					ZVAL_COPY_VALUE(newvalue, val);
 				}
 			} else {
