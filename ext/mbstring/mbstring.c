2cc1cbf2f424ce9d66fab00710f93409c75bf381
Fabien Villepinte <fabien.villepinte@gmail.com>
2017-07-28 13:41:09 +0200

    Fix Bug #75001: Wrong reflection on mb_eregi_replace

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -457,6 +457,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_mb_eregi_replace, 0, 0, 3)
 	ZEND_ARG_INFO(0, pattern)
 	ZEND_ARG_INFO(0, replacement)
 	ZEND_ARG_INFO(0, string)
+	ZEND_ARG_INFO(0, option)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_mb_ereg_replace_callback, 0, 0, 3)
