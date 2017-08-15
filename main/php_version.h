3af6201224f0160570afa1ec21e9db3c69e7bafc
Anatol Belski <ab@php.net>
2017-08-15 09:33:30 +0200

    move dev to 7.0.24

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 23
+#define PHP_RELEASE_VERSION 24
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.23-dev"
-#define PHP_VERSION_ID 70023
+#define PHP_VERSION "7.0.24-dev"
+#define PHP_VERSION_ID 70024
