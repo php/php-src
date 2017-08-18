065551bc9c94eb5842f0ccaf654d8e187f6485fd
Anatol Belski <ab@php.net>
2017-08-18 11:54:56 +0200

    Fix phpize exit status
    Configure and others are generated, not copied. Seems there's yet much
    more to do wrt exit status catching.

--- win32/build/phpize.bat
+++ win32/build/phpize.bat
@@ -1,6 +1,11 @@
 @echo off
 SET PHP_BUILDCONF_PATH=%~dp0
 cscript /nologo %PHP_BUILDCONF_PATH%\script\phpize.js %*
-copy %PHP_BUILDCONF_PATH%\win32\build\configure.bat %PHP_BUILDCONF_PATH% > nul
-IF NOT EXIST %PHP_BUILDCONF_PATH% (echo Error generating configure script, configure script was not copied) ELSE (echo Now run 'configure --help')
+IF NOT EXIST configure.bat (
+	echo Error generating configure script, configure script was not copied
+	exit /b 3
+) ELSE (
+	echo Now run 'configure --help'
+)
 SET PHP_BUILDCONF_PATH=
+
