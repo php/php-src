1287c205a976fbc64f0efff55badd6406a4f9420
Anatol Belski <ab@php.net>
2017-08-21 19:38:44 +0200

    Correct default value in master

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -104,10 +104,10 @@ if (typeof(CWD) == "undefined") {
 
 /* defaults; we pick up the precise versions from configure.ac */
 var PHP_VERSION = 7;
-var PHP_MINOR_VERSION = 2;
+var PHP_MINOR_VERSION = 3;
 var PHP_RELEASE_VERSION = 0;
 var PHP_EXTRA_VERSION = "";
-var PHP_VERSION_STRING = "7.2.0";
+var PHP_VERSION_STRING = "7.3.0";
 
 /* Get version numbers and DEFINE as a string */
 function get_version_numbers()
