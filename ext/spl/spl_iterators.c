b689857d53177b6027679f67fecbde8f36c174d1
Nikita Popov <nikita.ppv@gmail.com>
2017-09-04 21:20:49 +0200

    Revert "Fixed #74977 - Appending AppendIterator leads to segfault"
    This reverts commit dbc2ffba822d13e74e37f5ba6e106f163c1008e8.

--- ext/spl/spl_iterators.c
+++ ext/spl/spl_iterators.c
@@ -3367,7 +3367,7 @@ SPL_METHOD(AppendIterator, __construct)
    Append an iterator */
 SPL_METHOD(AppendIterator, append)
 {
-	spl_dual_it_object   *intern, *appender;
+	spl_dual_it_object   *intern;
 	zval *it;
 
 	SPL_FETCH_AND_CHECK_DUAL_IT(intern, getThis());
@@ -3379,11 +3379,6 @@ SPL_METHOD(AppendIterator, append)
 		spl_array_iterator_append(&intern->u.append.zarrayit, it);
 		intern->u.append.iterator->funcs->move_forward(intern->u.append.iterator);
 	}else{
-		appender = Z_SPLDUAL_IT_P(it);
-		if (appender->dit_type == DIT_AppendIterator) {
-			spl_array_iterator_append(&intern->u.append.zarrayit, &appender->u.append.zarrayit);
-			return;
-		}
 		spl_array_iterator_append(&intern->u.append.zarrayit, it);
 	}
 
