296d84405848ac2b46dd3a07ee61c1c99b5fd5ca
Dmitry Stogov <dmitry@zend.com>
2017-09-04 20:40:42 +0300

    Fix inference (result of ++/-- can't be a reference)

--- ext/opcache/Optimizer/zend_inference.c
+++ ext/opcache/Optimizer/zend_inference.c
@@ -2577,7 +2577,7 @@ static int zend_update_type_info(const zend_op_array *op_array,
 			}
 			if (ssa_ops[i].result_def >= 0) {
 				// TODO: ???
-				tmp = MAY_BE_REF | MAY_BE_RC1 | MAY_BE_RCN | MAY_BE_ANY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY | MAY_BE_ARRAY_OF_REF;
+				tmp = MAY_BE_RC1 | MAY_BE_RCN | MAY_BE_ANY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY | MAY_BE_ARRAY_OF_REF;
 				UPDATE_SSA_TYPE(tmp, ssa_ops[i].result_def);
 			}
 			break;
