54686debea205a21e6d0612655111765968c39b4
Andrea Faulds <ajf@ajf.me>
2017-09-06 20:22:26 +0100

    Move operator functions from /ext/standard to /Zend

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -85,6 +85,32 @@ static ZEND_FUNCTION(gc_collect_cycles);
 static ZEND_FUNCTION(gc_enabled);
 static ZEND_FUNCTION(gc_enable);
 static ZEND_FUNCTION(gc_disable);
+static ZEND_NAMED_FUNCTION(zend_operator_add);
+static ZEND_NAMED_FUNCTION(zend_operator_sub);
+static ZEND_NAMED_FUNCTION(zend_operator_mul);
+static ZEND_NAMED_FUNCTION(zend_operator_div);
+static ZEND_NAMED_FUNCTION(zend_operator_mod);
+static ZEND_NAMED_FUNCTION(zend_operator_pow);
+static ZEND_NAMED_FUNCTION(zend_operator_bitwise_and);
+static ZEND_NAMED_FUNCTION(zend_operator_bitwise_or);
+static ZEND_NAMED_FUNCTION(zend_operator_bitwise_xor);
+static ZEND_NAMED_FUNCTION(zend_operator_bitwise_not);
+static ZEND_NAMED_FUNCTION(zend_operator_shift_left);
+static ZEND_NAMED_FUNCTION(zend_operator_shift_right);
+static ZEND_NAMED_FUNCTION(zend_operator_equal);
+static ZEND_NAMED_FUNCTION(zend_operator_identical);
+static ZEND_NAMED_FUNCTION(zend_operator_not_equal);
+static ZEND_NAMED_FUNCTION(zend_operator_not_identical);
+static ZEND_NAMED_FUNCTION(zend_operator_less_than);
+static ZEND_NAMED_FUNCTION(zend_operator_greater_than);
+static ZEND_NAMED_FUNCTION(zend_operator_less_than_or_equal_to);
+static ZEND_NAMED_FUNCTION(zend_operator_greater_than_or_equal_to);
+static ZEND_NAMED_FUNCTION(zend_operator_spaceship);
+static ZEND_NAMED_FUNCTION(zend_operator_boolean_and);
+static ZEND_NAMED_FUNCTION(zend_operator_boolean_or);
+static ZEND_NAMED_FUNCTION(zend_operator_boolean_xor);
+static ZEND_NAMED_FUNCTION(zend_operator_boolean_not);
+static ZEND_NAMED_FUNCTION(zend_operator_concat);
 
 /* {{{ arginfo */
 ZEND_BEGIN_ARG_INFO(arginfo_zend__void, 0)
@@ -227,7 +253,136 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_INFO_EX(arginfo_extension_loaded, 0, 0, 1)
 	ZEND_ARG_INFO(0, extension_name)
 ZEND_END_ARG_INFO()
+/* }}} */
 
+/* {{{ operator functions */
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_add, 0, 0, 1)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_sub, 0, 0, 1)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_mul, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_div, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_mod, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_pow, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_bitwise_and, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_bitwise_or, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_bitwise_xor, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_bitwise_not, 0, 0, 1)
+	ZEND_ARG_INFO(0, a)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_shift_left, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_shift_right, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_equal, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_identical, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_not_equal, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_not_identical, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_less_than, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_greater_than, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_less_than_or_equal_to, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_greater_than_or_equal_to, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_spaceship, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_boolean_and, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_boolean_or, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_boolean_xor, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_boolean_not, 0, 0, 1)
+	ZEND_ARG_INFO(0, a)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_operator_concat, 0, 0, 2)
+	ZEND_ARG_INFO(0, a)
+	ZEND_ARG_INFO(0, b)
+ZEND_END_ARG_INFO()
 /* }}} */
 
 static const zend_function_entry builtin_functions[] = { /* {{{ */
@@ -289,6 +444,38 @@ static const zend_function_entry builtin_functions[] = { /* {{{ */
 	ZEND_FE(gc_enabled, 		arginfo_zend__void)
 	ZEND_FE(gc_enable, 		arginfo_zend__void)
 	ZEND_FE(gc_disable, 		arginfo_zend__void)
+
+	/* operator functions */
+	ZEND_NAMED_FE(+,			zend_operator_add,								arginfo_operator_add)
+	ZEND_NAMED_FE(-,			zend_operator_sub,								arginfo_operator_sub)
+	ZEND_NAMED_FE(*,			zend_operator_mul,								arginfo_operator_mul)
+	ZEND_NAMED_FE(/,			zend_operator_div,								arginfo_operator_div)
+	ZEND_NAMED_FE(%,			zend_operator_mod,								arginfo_operator_mod)
+	ZEND_NAMED_FE(**,			zend_operator_pow,								arginfo_operator_pow)
+	ZEND_NAMED_FE(&,			zend_operator_bitwise_and,						arginfo_operator_bitwise_and)
+	ZEND_NAMED_FE(|,			zend_operator_bitwise_or,						arginfo_operator_bitwise_or)
+	ZEND_NAMED_FE(^,			zend_operator_bitwise_xor,						arginfo_operator_bitwise_xor)
+	ZEND_NAMED_FE(~,			zend_operator_bitwise_not,						arginfo_operator_bitwise_not)
+	ZEND_NAMED_FE(<<,			zend_operator_shift_left,						arginfo_operator_shift_left)
+	ZEND_NAMED_FE(>>,			zend_operator_shift_right,						arginfo_operator_shift_right)
+	ZEND_NAMED_FE(==,			zend_operator_equal,							arginfo_operator_equal)
+	ZEND_NAMED_FE(===,			zend_operator_identical,						arginfo_operator_identical)
+	ZEND_NAMED_FE(!=,			zend_operator_not_equal,						arginfo_operator_not_equal)
+	ZEND_NAMED_FE(<>,			zend_operator_not_equal,						arginfo_operator_not_equal)
+	ZEND_NAMED_FE(!==,			zend_operator_not_identical,					arginfo_operator_not_identical)
+	ZEND_NAMED_FE(<,			zend_operator_less_than,						arginfo_operator_less_than)
+	ZEND_NAMED_FE(>,			zend_operator_greater_than,						arginfo_operator_greater_than)
+	ZEND_NAMED_FE(<=,			zend_operator_less_than_or_equal_to,			arginfo_operator_less_than_or_equal_to)
+	ZEND_NAMED_FE(>=,			zend_operator_greater_than_or_equal_to,			arginfo_operator_greater_than_or_equal_to)
+	ZEND_NAMED_FE(<=>,			zend_operator_spaceship,						arginfo_operator_spaceship)
+	ZEND_NAMED_FE(&&,			zend_operator_boolean_and,						arginfo_operator_boolean_and)
+	ZEND_NAMED_FE(and,			zend_operator_boolean_and,						arginfo_operator_boolean_and)
+	ZEND_NAMED_FE(||,			zend_operator_boolean_or,						arginfo_operator_boolean_or)
+	ZEND_NAMED_FE(or,			zend_operator_boolean_or,						arginfo_operator_boolean_or)
+	ZEND_NAMED_FE(xor,			zend_operator_boolean_xor,						arginfo_operator_boolean_xor)
+	ZEND_NAMED_FE(!,			zend_operator_boolean_not,						arginfo_operator_boolean_not)
+	ZEND_NAMED_FE(.,			zend_operator_concat,							arginfo_operator_concat)
+
 	ZEND_FE_END
 };
 /* }}} */
@@ -2695,6 +2882,387 @@ ZEND_FUNCTION(get_extension_funcs)
 	if (!array) {
 		RETURN_FALSE;
 	}
+}
+/* }}} */
+
+/* {{{ proto number +(number|array a[, number|array b])
+   Returns sum or union of a and b (or convesion of a to int or float as appropriate) */
+ZEND_NAMED_FUNCTION(zend_operator_add)
+{
+	zval *a, *b = NULL;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "z|z", &a, &b) == FAILURE) {
+		return;
+	}
+
+	/* Unary +a is actually (+1 * a) since (0 + a) handles signs wrong */
+	if (!b) {
+		zval factor;
+		ZVAL_LONG(&factor, 1);
+		mul_function(return_value, &factor, a);
+	} else {
+		add_function(return_value, a, b);
+	}
+}
+/* }}} */
+
+/* {{{ proto number -(number a[, number b])
+   Returns difference of a and b (or opposite of a) */
+ZEND_NAMED_FUNCTION(zend_operator_sub)
+{
+	zval *a, *b = NULL;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "z|z", &a, &b) == FAILURE) {
+		return;
+	}
+
+	/* Unary -a is actually (-1 * a) since (0 - a) handles signs wrong */
+	if (!b) {
+		zval factor;
+		ZVAL_LONG(&factor, -1);
+		mul_function(return_value, &factor, a);
+	} else {
+		sub_function(return_value, a, b);
+	}
+}
+/* }}} */
+
+/* {{{ proto number *(number a, number b)
+   Returns product of a and b */
+ZEND_NAMED_FUNCTION(zend_operator_mul)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	mul_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto number /(number a, number b)
+   Returns quotient of a and b */
+ZEND_NAMED_FUNCTION(zend_operator_div)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	div_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int %(int a, int b)
+   Returns remainder of a divided by b */
+ZEND_NAMED_FUNCTION(zend_operator_mod)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	mod_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto number **(number a, number b)
+   Returns result of raising a to the b'th power */
+ZEND_NAMED_FUNCTION(zend_operator_pow)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	pow_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int|string &(int|string a, int|string b)
+   Returns bitwise AND of a and b */
+ZEND_NAMED_FUNCTION(zend_operator_bitwise_and)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	bitwise_and_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int|string |(int|string a, int|string b)
+   Returns bitwise OR of a and b */
+ZEND_NAMED_FUNCTION(zend_operator_bitwise_or)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	bitwise_or_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int|string ^(int|string a, int|string b)
+   Returns bitwise XOR of a and b */
+ZEND_NAMED_FUNCTION(zend_operator_bitwise_xor)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	bitwise_xor_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int|string ~(int|string a)
+   Returns bitwise NOT of a */
+ZEND_NAMED_FUNCTION(zend_operator_bitwise_not)
+{
+	zval *a;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "z", &a) == FAILURE) {
+		return;
+	}
+
+	bitwise_not_function(return_value, a);
+}
+/* }}} */
+
+/* {{{ proto int <<(int a, int b)
+   Returns result of shifting bits of a left by b */
+ZEND_NAMED_FUNCTION(zend_operator_shift_left)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	shift_left_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto int >>(int a, int b)
+   Returns result of shifting bits of a left by b */
+ZEND_NAMED_FUNCTION(zend_operator_shift_right)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	shift_right_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool ==(mixed a, mixed b)
+   Returns TRUE if a is equal to b after type juggling */
+ZEND_NAMED_FUNCTION(zend_operator_equal)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_equal_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool ===(mixed a, mixed b)
+   Returns TRUE if a is equal to b, and they are of the same type */
+ZEND_NAMED_FUNCTION(zend_operator_identical)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_identical_function(return_value, a, b);
+}
+/* }}} */
+
+/* Also <>(a, b) */
+/* {{{ proto bool !=(mixed a, mixed b)
+   Returns TRUE if a is not equal to b after type juggling */
+ZEND_NAMED_FUNCTION(zend_operator_not_equal)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_not_equal_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool !==(mixed a, mixed b)
+   Returns TRUE if a is not equal to b, or they are not of the same type */
+ZEND_NAMED_FUNCTION(zend_operator_not_identical)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_not_identical_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool <(mixed a, mixed b)
+   Returns TRUE if a is strictly less than b */
+ZEND_NAMED_FUNCTION(zend_operator_less_than)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_smaller_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool >(mixed a, mixed b)
+   Returns TRUE if a is strictly greater than b */
+ZEND_NAMED_FUNCTION(zend_operator_greater_than)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_smaller_function(return_value, b, a);
+}
+/* }}} */
+
+/* {{{ proto bool <=(mixed a, mixed b)
+   Returns TRUE if a is strictly less than or equal to b */
+ZEND_NAMED_FUNCTION(zend_operator_less_than_or_equal_to)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_smaller_or_equal_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool >=(mixed a, mixed b)
+   Returns TRUE if a is strictly greater than or equal to b */
+ZEND_NAMED_FUNCTION(zend_operator_greater_than_or_equal_to)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	is_smaller_or_equal_function(return_value, b, a);
+}
+/* }}} */
+
+/* {{{ proto int <=>(mixed a, mixed b)
+   Returns -1 if a is less than b, 0 if a is equal to b, or 1 if a is greater than b */
+ZEND_NAMED_FUNCTION(zend_operator_spaceship)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	compare_function(return_value, a, b);
+}
+/* }}} */
+
+/* Also and(a, b) */
+/* {{{ proto bool &&(bool a, bool b)
+   Returns TRUE if both a and b are true */
+ZEND_NAMED_FUNCTION(zend_operator_boolean_and)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	RETURN_BOOL(i_zend_is_true(a) && i_zend_is_true(b));
+}
+/* }}} */
+
+/* Also or(a, b) */
+/* {{{ proto bool ||(bool a, bool b)
+   Returns TRUE if either a or b is true */
+ZEND_NAMED_FUNCTION(zend_operator_boolean_or)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	RETURN_BOOL(i_zend_is_true(a) || i_zend_is_true(b));
+}
+/* }}} */
+
+/* {{{ proto bool xor(bool a, bool b)
+   Returns TRUE if either a or b is true, but not both */
+ZEND_NAMED_FUNCTION(zend_operator_boolean_xor)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	boolean_xor_function(return_value, a, b);
+}
+/* }}} */
+
+/* {{{ proto bool !(bool a)
+   Returns TRUE if a is not true */
+ZEND_NAMED_FUNCTION(zend_operator_boolean_not)
+{
+	zval *a;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "z", &a) == FAILURE) {
+		return;
+	}
+
+	boolean_not_function(return_value, a);
+}
+/* }}} */
+
+/* {{{ proto string .(string a, string b)
+   Returns the result of concatenating a and b */
+ZEND_NAMED_FUNCTION(zend_operator_concat)
+{
+	zval *a, *b;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &a, &b) == FAILURE) {
+		return;
+	}
+
+	concat_function(return_value, a, b);
 }
 /* }}} */
 
