f46ec03fcd288eb5b6289801edc7fe8e437b317c
Xinchen Hui <laruence@gmail.com>
2017-09-08 13:56:50 +0800

    Added filter func infos

--- ext/opcache/Optimizer/zend_func_info.c
+++ ext/opcache/Optimizer/zend_func_info.c
@@ -1512,6 +1512,16 @@ static const func_info_t func_infos[] = {
 	F1("exif_read_data",						MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_STRING | MAY_BE_ARRAY_OF_ANY),
 	F1("exif_thumbnail",						MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_STRING),
 	F0("exif_imagetype",						MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_LONG),
+
+	/* ext/filter */
+	F0("filter_has_var",						MAY_BE_FALSE | MAY_BE_TRUE),
+	FN("filter_input",							UNKNOWN_INFO),
+	FN("filter_var",							UNKNOWN_INFO),
+	F1("filter_input_array",					MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY),
+	F1("filter_var_array",						MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY),
+	F1("filter_list",							MAY_BE_NULL | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_LONG | MAY_BE_ARRAY_OF_STRING),
+	F0("filter_id",								MAY_BE_NULL | MAY_BE_FALSE | MAY_BE_LONG),
+
 };
 
 static HashTable func_info;
