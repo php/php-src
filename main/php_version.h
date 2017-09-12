7f01872a24c388434ab5cc928f0b6142f4822012
Joe Watkins <krakjoe@php.net>
2017-09-12 15:34:30 +0100

    bump versions

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 10
+#define PHP_RELEASE_VERSION 11
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.10-dev"
-#define PHP_VERSION_ID 70110
+#define PHP_VERSION "7.1.11-dev"
+#define PHP_VERSION_ID 70111
