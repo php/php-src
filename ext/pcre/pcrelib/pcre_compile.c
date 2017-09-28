d37658be6c1016fe0965fa9d947ec01eae0c7946
Anatol Belski <ab@php.net>
2017-09-28 15:40:49 +0200

    Apply upstream patch for CVE-2016-1283
    Fix bug #75207, see also
    https://bugzilla.redhat.com/show_bug.cgi?id=1295385
    https://vcs.pcre.org/pcre?view=revision&revision=1636
    
    (cherry picked from commit d11fceab151cd0410645f81eb7444af4388470c3)

--- ext/pcre/pcrelib/pcre_compile.c
+++ ext/pcre/pcrelib/pcre_compile.c
@@ -7274,7 +7274,12 @@ for (;; ptr++)
           so far in order to get the number. If the name is not found, leave
           the value of recno as 0 for a forward reference. */
 
-          else
+          /* This patch (removing "else") fixes a problem when a reference is
+          to multiple identically named nested groups from within the nest.
+          Once again, it is not the "proper" fix, and it results in an
+          over-allocation of memory. */
+
+          /* else */
             {
             ng = cd->named_groups;
             for (i = 0; i < cd->names_found; i++, ng++)
