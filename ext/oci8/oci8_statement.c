4d69587035f6d622134f86bbc9ee0afb00eeec88
Dmitry Stogov <dmitry@zend.com>
2017-10-03 09:08:28 +0300

    Fixed compilation errors

--- ext/oci8/oci8_statement.c
+++ ext/oci8/oci8_statement.c
@@ -1083,12 +1083,12 @@ int php_oci_bind_post_exec(zval *data)
 				}
 				break;
 		}
-	} else if ((Z_TYPE_P(bind->zval) == IS_TRUE) || (Z_TYPE_P(bind->zval) == IS_FALSE)) {
+	} else if ((Z_TYPE(bind->val) == IS_TRUE) || (Z_TYPE(bind->val) == IS_FALSE)) {
 		/* This convetrsion is done on purpose (ext/oci8 uses LVAL as a temorary value) */
-		if (Z_LVAL_P(bind->zval) == 0)
-			ZVAL_BOOL(bind->zval, FALSE);
-		else if (Z_LVAL_P(bind->zval) == 1)
-			ZVAL_BOOL(bind->zval, TRUE);
+		if (Z_LVAL(bind->val) == 0)
+			ZVAL_BOOL(&bind->val, FALSE);
+		else if (Z_LVAL(bind->val) == 1)
+			ZVAL_BOOL(&bind->val, TRUE);
 	}
 
 	return 0;
