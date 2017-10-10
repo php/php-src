51658bac3550761253144b316f8d9bf5e6b06487
Anatol Belski <ab@php.net>
2017-10-10 08:26:04 +0200

    Move dev to 7.0.26

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 25
+#define PHP_RELEASE_VERSION 26
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.25-dev"
-#define PHP_VERSION_ID 70025
+#define PHP_VERSION "7.0.26-dev"
+#define PHP_VERSION_ID 70026
