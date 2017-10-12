98446f0f14f9ef1916795ea233c1e6a257107abc
Christoph M. Becker <cmbecker69@gmx.de>
2017-10-12 15:37:47 +0200

    Fixed bug #75365 (Enchant still reports version 1.1.0)
    Since Enchant is bundled, we make `PHP_ENCHANT_VERSION` an alias of
    `PHP_VERSION` and also drop the rather meaningless revision hash from
    the PHP info.

--- ext/enchant/php_enchant.h
+++ ext/enchant/php_enchant.h
@@ -24,7 +24,7 @@
 extern zend_module_entry enchant_module_entry;
 #define phpext_enchant_ptr &enchant_module_entry
 
-#define PHP_ENCHANT_VERSION "1.1.0"
+#define PHP_ENCHANT_VERSION PHP_VERSION
 
 #ifdef PHP_WIN32
 #define PHP_ENCHANT_API __declspec(dllexport)
