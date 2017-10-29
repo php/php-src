6b88b3bc2591f926b28975c07a43eb16b11bb2a8
Kalle Sommer Nielsen <kalle@php.net>
2017-10-29 16:43:21 +0100

    Fix compiler warnings

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -1514,12 +1514,12 @@ static void php_ifd_set32u(char *data, size_t value, int motorola_intel)
 {
 	if (motorola_intel) {
 		data[0] = (value & 0xFF000000) >> 24;
-		data[1] = (value & 0x00FF0000) >> 16;
+		data[1] = (char) ((value & 0x00FF0000) >> 16);
 		data[2] = (value & 0x0000FF00) >>  8;
 		data[3] = (value & 0x000000FF);
 	} else {
 		data[3] = (value & 0xFF000000) >> 24;
-		data[2] = (value & 0x00FF0000) >> 16;
+		data[2] = (char) ((value & 0x00FF0000) >> 16);
 		data[1] = (value & 0x0000FF00) >>  8;
 		data[0] = (value & 0x000000FF);
 	}
