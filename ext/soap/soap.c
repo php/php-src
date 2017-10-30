97ff1c9243bf2d65a9d8baf35033c5a41779fb2c
Fabien Villepinte <fabien.villepinte@gmail.com>
2017-10-30 12:51:03 +0100

    Fix bug #75461 Wrong reflection on SoapServer:setClass

--- ext/soap/soap.c
+++ ext/soap/soap.c
@@ -296,7 +296,7 @@ ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_soapserver_setclass, 0, 0, 1)
 	ZEND_ARG_INFO(0, class_name)
-	ZEND_ARG_INFO(0, args)
+	ZEND_ARG_VARIADIC_INFO(0, args)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_soapserver_setobject, 0, 0, 1)
