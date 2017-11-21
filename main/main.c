6b54b3409a46119bc702af5d905718a88a0115a3
Anatol Belski <ab@php.net>
2017-11-21 18:48:37 +0100

    Ensure shift doesn't overflow, closes #2916

--- main/main.c
+++ main/main.c
@@ -186,7 +186,7 @@ static PHP_INI_MH(OnChangeMemoryLimit)
 	if (new_value) {
 		PG(memory_limit) = zend_atol(ZSTR_VAL(new_value), ZSTR_LEN(new_value));
 	} else {
-		PG(memory_limit) = 1<<30;		/* effectively, no limit */
+		PG(memory_limit) = Z_L(1)<<30;		/* effectively, no limit */
 	}
 	return zend_set_memory_limit(PG(memory_limit));
 }
