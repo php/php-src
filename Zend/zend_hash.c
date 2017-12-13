03aa4363a615efdcc94b153aabc5169f2cd192b5
Li-Wen Hsu <lwhsu@lwhsu.org>
2017-12-14 04:18:16 +0800

    Also drop fastcall attribute on variadic function in zend_hash.c

--- Zend/zend_hash.c
+++ Zend/zend_hash.c
@@ -1615,7 +1615,7 @@ ZEND_API void ZEND_FASTCALL zend_hash_apply_with_argument(HashTable *ht, apply_f
 }
 
 
-ZEND_API void ZEND_FASTCALL zend_hash_apply_with_arguments(HashTable *ht, apply_func_args_t apply_func, int num_args, ...)
+ZEND_API void zend_hash_apply_with_arguments(HashTable *ht, apply_func_args_t apply_func, int num_args, ...)
 {
 	uint32_t idx;
 	Bucket *p;
