27908bace1d39c28a3645d40eccc56f63349128a
Anatol Belski <ab@php.net>
2018-02-15 21:34:55 +0100

    Reduce variable scope

--- ext/intl/collator/collator_is_numeric.c
+++ ext/intl/collator/collator_is_numeric.c
@@ -25,7 +25,6 @@ static double collator_u_strtod(const UChar *nptr, UChar **endptr) /* {{{ */
 	const UChar *u = nptr, *nstart;
 	UChar c = *u;
 	int any = 0;
-	ALLOCA_FLAG(use_heap);
 
 	while (u_isspace(c)) {
 		c = *++u;
@@ -72,6 +71,7 @@ static double collator_u_strtod(const UChar *nptr, UChar **endptr) /* {{{ */
 		char buf[64], *numbuf, *bufpos;
 		size_t length = u - nstart;
 		double value;
+		ALLOCA_FLAG(use_heap);
 
 		if (length < sizeof(buf)) {
 			numbuf = buf;
