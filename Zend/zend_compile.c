2390c0559023bbb4e988eaefea5d57ccbe6e0b9d
Silvio Marijic <marijic.silvio@gmail.com>
2018-02-18 22:49:53 +0100

    Remove immutable restriction on abstract class

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -746,9 +746,6 @@ uint32_t zend_add_class_modifier(uint32_t flags, uint32_t new_flag) /* {{{ */
 	if ((new_flags & ZEND_ACC_EXPLICIT_ABSTRACT_CLASS) && (new_flags & ZEND_ACC_FINAL)) {
 		zend_error_noreturn(E_COMPILE_ERROR, "Cannot use the final modifier on an abstract class");
 	}
-	if ((new_flags & ZEND_ACC_EXPLICIT_ABSTRACT_CLASS) && (new_flags & ZEND_ACC_IMMUTABLE)) {
-		zend_error_noreturn(E_COMPILE_ERROR, "Cannot use the immutable modifier on an abstract class");
-	}
 	if ((flags & ZEND_ACC_IMMUTABLE) && (new_flags & ZEND_ACC_IMMUTABLE)) {
 		zend_error_noreturn(E_COMPILE_ERROR, "Multiple immutable modifiers are not allowed");
 	}
