b3a8f7facd1630b06eac37732f3e45596905deec
Silvio Marijic <marijic.silvio@gmail.com>
2018-02-18 22:51:48 +0100

    Disallow multiple immutable modifiers on class member

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -771,6 +771,9 @@ uint32_t zend_add_member_modifier(uint32_t flags, uint32_t new_flag) /* {{{ */
 	if ((new_flags & ZEND_ACC_ABSTRACT) && (new_flags & ZEND_ACC_FINAL)) {
 		zend_error_noreturn(E_COMPILE_ERROR, "Cannot use the final modifier on an abstract class member");
 	}
+	if ((flags & ZEND_ACC_IMMUTABLE) && (new_flag & ZEND_ACC_IMMUTABLE)) {
+		zend_error_noreturn(E_COMPILE_ERROR, "Multiple immutable modifiers are not allowed");
+	}
 	return new_flags;
 }
 /* }}} */
