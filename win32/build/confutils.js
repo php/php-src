d1d228838b94fb24baeb60d1960ac3dee154e7c7
Anatol Belski <ab@php.net>
2018-02-19 17:43:03 +0100

    Add basic cppcheck suppressions file

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -1705,7 +1705,9 @@ function ADD_SOURCES(dir, file_list, target, obj_dir)
 						"--library=" + cppcheck_lib + " " +
 						/* "--rule-file=win32\build\cppcheck_rules.xml " + */
 						" --std=c89 --std=c++11 " + 
-						"--quiet --inconclusive --template=vs -j 4 ";
+						"--quiet --inconclusive --template=vs -j 4 " +
+						"--suppress=unmatchedSuppression " +
+						"--suppressions-list=win32\\build\\cppcheck_suppress.txt ";
 
 			var cppcheck_build_dir = get_define("CPPCHECK_BUILD_DIR");
 			if (!!cppcheck_build_dir) {
