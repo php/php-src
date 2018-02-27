3db7427b71db80d9a73da3b019b1f86b362dad55
Ferenc Kovacs <tyra3l@gmail.com>
2018-02-27 23:57:06 +0100

    5.6.35 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 34
+#define PHP_RELEASE_VERSION 35
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.34-dev"
-#define PHP_VERSION_ID 50634
+#define PHP_VERSION "5.6.35-dev"
+#define PHP_VERSION_ID 50635
