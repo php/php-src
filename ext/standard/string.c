ada9d31ac3e5e89b547c7a73818e60ad11a6c3ea
David Carlier <devnexen@gmail.com>
2018-04-04 23:06:26 +0100

    SSE2 optimisation, tweaking variables as _N constant is already defined in OpenBSD.

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -5776,10 +5776,10 @@ static zend_string *php_str_rot13(zend_string *str)
 		const __m128i m_ = _mm_set1_epi8('m' + 1);
 		const __m128i _n = _mm_set1_epi8('n' - 1);
 		const __m128i z_ = _mm_set1_epi8('z' + 1);
-		const __m128i _A = _mm_set1_epi8('A' - 1);
-		const __m128i M_ = _mm_set1_epi8('M' + 1);
-		const __m128i _N = _mm_set1_epi8('N' - 1);
-		const __m128i Z_ = _mm_set1_epi8('Z' + 1);
+		const __m128i A_ = _mm_set1_epi8('A' - 1);
+		const __m128i _M = _mm_set1_epi8('M' + 1);
+		const __m128i N_ = _mm_set1_epi8('N' - 1);
+		const __m128i _Z = _mm_set1_epi8('Z' + 1);
 		const __m128i add = _mm_set1_epi8(13);
 		const __m128i sub = _mm_set1_epi8(-13);
 
@@ -5805,16 +5805,16 @@ static zend_string *php_str_rot13(zend_string *str)
 				delta = _mm_or_si128(delta, cmp);
 			}
 
-			gt = _mm_cmpgt_epi8(in, _A);
-			lt = _mm_cmplt_epi8(in, M_);
+			gt = _mm_cmpgt_epi8(in, A_);
+			lt = _mm_cmplt_epi8(in, _M);
 			cmp = _mm_and_si128(lt, gt);
 			if (_mm_movemask_epi8(cmp)) {
 				cmp = _mm_and_si128(cmp, add);
 				delta = _mm_or_si128(delta, cmp);
 			}
 
-			gt = _mm_cmpgt_epi8(in, _N);
-			lt = _mm_cmplt_epi8(in, Z_);
+			gt = _mm_cmpgt_epi8(in, N_);
+			lt = _mm_cmplt_epi8(in, _Z);
 			cmp = _mm_and_si128(lt, gt);
 			if (_mm_movemask_epi8(cmp)) {
 				cmp = _mm_and_si128(cmp, sub);
