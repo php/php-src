1dc2b3338f55c363a0c6de31f095de61063cb9b9
Adam Duskett <aduskett@gmail.com>
2018-04-13 09:59:04 -0400

    fix building against LibreSSL 2.7.x
    LibreSSL 2.7.x introduced most of the OpenSSL 1.1 API's. This breaks
    a few things.

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -566,7 +566,8 @@ ZEND_GET_MODULE(openssl)
 #endif
 
 /* {{{ OpenSSL compatibility functions and macros */
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined (LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined (LIBRESSL_VERSION_NUMBER) && \
+	LIBRESSL_VERSION_NUMBER < 0x20700000L
 #define EVP_PKEY_get0_RSA(_pkey) _pkey->pkey.rsa
 #define EVP_PKEY_get0_DH(_pkey) _pkey->pkey.dh
 #define EVP_PKEY_get0_DSA(_pkey) _pkey->pkey.dsa
