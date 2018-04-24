ebbef8503f4b5cdd63b7da49b106f970353c7ac0
Stanislav Malyshev <stas@php.net>
2018-04-23 22:03:33 -0700

    Merge branch 'PHP-7.1' into PHP-7.2
    * PHP-7.1:
      Fix tsrm_ls
      Fix #76129 - remove more potential unfiltered outputs for phar
      Fix test
      Fix bug #76248 - Malicious LDAP-Server Response causes Crash
      Fix bug #76249 - fail on invalid sequences
      Fix #76130: Heap Buffer Overflow (READ: 1786) in exif_iif_add_value
      Fix bug #75981: prevent reading beyond buffer start
    
    (cherry picked from commit 4c06d929c048158a623a6f285af006f8531d372b)

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -2112,7 +2112,7 @@ static void exif_iif_add_value(image_info_type *image_info, int section_index, c
 		case TAG_FMT_UNDEFINED:
 			if (value) {
 				if (tag == TAG_MAKER_NOTE) {
-					length = MIN(length, (int) strlen(value));
+					length = (int) php_strnlen(value, length);
 				}
 
 				/* do not recompute length here */
