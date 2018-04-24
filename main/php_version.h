3d5d1e569e8c35fb600801d96fb66a3458808381
Anatol Belski <ab@php.net>
2018-04-24 11:40:32 +0200

    Set versions

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
 #define PHP_RELEASE_VERSION 30
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.30-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "7.0.30"
 #define PHP_VERSION_ID 70030
