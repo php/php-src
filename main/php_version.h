090fef553da5f13a9af78023de58f771d9ffd9e7
Anatol Belski <ab@php.net>
2018-04-24 11:36:12 +0200

    7.0.31 next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 30
+#define PHP_RELEASE_VERSION 31
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.30-dev"
-#define PHP_VERSION_ID 70030
+#define PHP_VERSION "7.0.31-dev"
+#define PHP_VERSION_ID 70031
