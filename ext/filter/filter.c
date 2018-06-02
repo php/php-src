39a588bd894f70e10c0e025daf0fa1b6e2cc6bc7
Peter Kokot <peterkokot@gmail.com>
2018-06-02 03:30:54 +0200

    Normalize filter phpinfo() output
    This patch normalizes the filter extension version in the php info
    output. Instead of the Git attributes ident blob object name from Git
    repository only extension status is displayed.

--- ext/filter/filter.c
+++ ext/filter/filter.c
@@ -330,7 +330,6 @@ PHP_MINFO_FUNCTION(filter)
 {
 	php_info_print_table_start();
 	php_info_print_table_row( 2, "Input Validation and Filtering", "enabled" );
-	php_info_print_table_row( 2, "Revision", "$Id$");
 	php_info_print_table_end();
 
 	DISPLAY_INI_ENTRIES();
