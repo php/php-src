66767b9f3491e5c870a9901de7f70fc0c8954dd8
Peter Kokot <peterkokot@gmail.com>
2018-06-02 04:58:30 +0200

    Normalize the POSIX phpinfo output
    Instead of the Git attributes ident blob object name display only
    extension status (enabled).

--- ext/posix/posix.c
+++ ext/posix/posix.c
@@ -321,7 +321,7 @@ static const zend_function_entry posix_functions[] = {
 static PHP_MINFO_FUNCTION(posix)
 {
 	php_info_print_table_start();
-	php_info_print_table_row(2, "Revision", "$Id$");
+	php_info_print_table_row(2, "POSIX support", "enabled");
 	php_info_print_table_end();
 }
 /* }}} */
