d94da951990cf0e9a3d240b1d6d3e647eea41596
Peter Kokot <peterkokot@gmail.com>
2018-06-03 05:23:15 +0200

    Remove exif version from the phpinfo output
    This patch removes the exif version in the phpinfo output to sync it
    with the rest of the php bundled extensions. Also in the past the
    revision id from the version control system was used and is today not
    needed anymore.

--- ext/exif/exif.c
+++ ext/exif/exif.c
@@ -17,8 +17,6 @@
    +----------------------------------------------------------------------+
  */
 
-/* $Id$ */
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -113,7 +111,6 @@ PHP_MINFO_FUNCTION(exif)
 {
 	php_info_print_table_start();
 	php_info_print_table_row(2, "EXIF Support", "enabled");
-	php_info_print_table_row(2, "EXIF Version", PHP_EXIF_VERSION);
 	php_info_print_table_row(2, "Supported EXIF Version", "0220");
 	php_info_print_table_row(2, "Supported filetypes", "JPEG, TIFF");
 
