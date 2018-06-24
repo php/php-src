4f26e8f350f0c4a9b9c9e1981367b9e8d67e425c
Rudi Theunissen <rudolf.theunissen@gmail.com>
2018-06-24 08:33:44 -0400

    Add __compareTo to struct _zend_class_entry

--- Zend/zend.h
+++ Zend/zend.h
@@ -139,6 +139,7 @@ struct _zend_class_entry {
 	union _zend_function *__callstatic;
 	union _zend_function *__tostring;
 	union _zend_function *__debugInfo;
+	union _zend_function *__compareTo;
 	union _zend_function *serialize_func;
 	union _zend_function *unserialize_func;
 
