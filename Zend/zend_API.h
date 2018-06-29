10b484e624e628e5f2255fab6252fc338ff114ad
Nikita Popov <nikita.ppv@gmail.com>
2018-06-29 22:44:42 +0200

    Fix build
    These were changed in 6e77a60a75445c863f85f81a83e5c2dd28f0e877.

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -211,8 +211,8 @@ typedef struct _zend_fcall_info_cache {
 		class_container.__unset = NULL;							\
 		class_container.__isset = NULL;							\
 		class_container.__debugInfo = NULL;						\
-		class_container.serialize = NULL;						\
-		class_container.unserialize = NULL;						\
+		class_container.serialize_func = NULL;					\
+		class_container.unserialize_func = NULL;				\
 		class_container.parent = NULL;							\
 		class_container.num_interfaces = 0;						\
 		class_container.traits = NULL;							\
