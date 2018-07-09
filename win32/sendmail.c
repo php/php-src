3e1e3ab728ae33477a549b91c2ed2df1987b30aa
Anatol Belski <ab@php.net>
2018-07-09 12:32:26 +0200

    Remove conflicting declaration

--- win32/sendmail.c
+++ win32/sendmail.c
@@ -338,9 +338,6 @@ PHPAPI char *GetSMErrorText(int index)
 	}
 }
 
-PHPAPI zend_string *php_str_to_str(char *haystack, size_t length, char *needle,
-		size_t needle_len, char *str, size_t str_len);
-
 
 /*********************************************************************
 // Name:  SendText
