1891246c6637119e7ff5ab0d9826cf8f19223dac
Joe Watkins <krakjoe@php.net>
2018-07-31 16:37:25 +0200

    set version

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 20
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.20-dev"
-#define PHP_VERSION_ID 70120
+#define PHP_RELEASE_VERSION 21
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.1.20RC1"
+#define PHP_VERSION_ID 70121
