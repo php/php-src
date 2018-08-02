9d7c510893b7e3648fceaa14620e7d9b04721c0f
Anatol Belski <ab@php.net>
2018-08-02 18:28:18 +0200

    Cleanup unused items

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -1940,7 +1940,6 @@ PHP_FUNCTION(imagegrabwindow)
 	HWND window;
 	zend_long client_area = 0;
 	RECT rc = {0};
-	RECT rc_win = {0};
 	int Width, Height;
 	HDC		hdc;
 	HDC memDC;
@@ -2015,8 +2014,6 @@ PHP_FUNCTION(imagegrabscreen)
 	HDC memDC;
 	HBITMAP memBM;
 	HBITMAP hOld;
-	typedef BOOL (WINAPI *tPrintWindow)(HWND, HDC,UINT);
-	tPrintWindow pPrintWindow = 0;
 	gdImagePtr im;
 	hdc		= GetDC(0);
 
