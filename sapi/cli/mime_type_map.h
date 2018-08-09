334f9393590ef016f4676ecf47d80cda1bb413bf
DesWurstes <DesWurstes@users.noreply.github.com>
2018-08-09 13:31:27 +0300

    Add WASM to supported MIME types

--- sapi/cli/mime_type_map.h
+++ sapi/cli/mime_type_map.h
@@ -1013,6 +1013,7 @@ static const php_cli_server_ext_mime_type_pair mime_type_map[] = {
 	{ "ice", "x-conference/x-cooltalk" },
 	{ "map", "application/json" },
 	{ "jsm", "application/javascript" },
+	{ "wasm", "application/wasm" },
 	{ NULL, NULL }
 };
 
