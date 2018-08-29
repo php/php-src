2c83dbb2f76082d5e0adcbe6956907448af1e3ba
Remi Collet <remi@remirepo.net>
2018-08-29 09:10:59 +0200

    fix double cast to int on 32-bit

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -3101,7 +3101,7 @@ ZEND_API zend_long ZEND_FASTCALL zend_dval_to_lval_slow(double d)
 	if (dmod < 0) {
 		/* we're going to make this number positive; call ceil()
 		 * to simulate rounding towards 0 of the negative number */
-		dmod = ceil(dmod);// + two_pow_32;
+		dmod = ceil(dmod) + two_pow_32;
 	}
 	return (zend_long)(zend_ulong)dmod;
 }
