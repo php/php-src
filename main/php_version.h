1b89522974d7596df23d7875a0d1e155c503c50a
Anatol Belski <ab@php.net>
2018-09-11 07:00:15 +0200

    7.0.33 next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 32
+#define PHP_RELEASE_VERSION 33
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.0.32-dev"
-#define PHP_VERSION_ID 70032
+#define PHP_VERSION "7.0.33-dev"
+#define PHP_VERSION_ID 70033
