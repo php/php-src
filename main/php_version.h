b0822792c8d4c8d535ea1beb8ea63025496668e7
Ferenc Kovacs <tyra3l@gmail.com>
2018-09-12 00:12:36 +0200

    5.6.38

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 39
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.39-dev"
-#define PHP_VERSION_ID 50639
+#define PHP_RELEASE_VERSION 38
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.6.38"
+#define PHP_VERSION_ID 50638
