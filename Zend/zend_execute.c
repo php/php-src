bc288c47230919c326b82133ff5306f4eff690b4
Dmitry Stogov <dmitry@zend.com>
2018-09-13 13:36:09 +0300

    Simplify __clone() visibility check (magic methods still don't respect ZEND_ACC_CHANGED).

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -2287,6 +2287,10 @@ static zend_never_inline void zend_fetch_this_var(int type OPLINE_DC EXECUTE_DAT
 	}
 }
 
+static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_wrong_clone_call(zend_function *clone, zend_class_entry *scope)
+{
+	zend_throw_error(NULL, "Call to %s %s::__clone() from context '%s'", zend_visibility_string(clone->common.fn_flags), ZSTR_VAL(clone->common.scope->name), scope ? ZSTR_VAL(scope->name) : "");
+}
 
 #if ZEND_INTENSIVE_DEBUGGING
 
