5f29e3fb6e3049c2c020ee05e71460ee238ab040
Sara Golemon <pollita@php.net>
2018-09-25 16:37:41 -0400

    Bump version

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 11
+#define PHP_RELEASE_VERSION 12
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.2.11-dev"
-#define PHP_VERSION_ID 70211
+#define PHP_VERSION "7.2.12-dev"
+#define PHP_VERSION_ID 70212
