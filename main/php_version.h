69c1b619c9d6a20dd5a50a058659c5c4233aea82
Sara Golemon <pollita@php.net>
2018-09-28 11:31:22 -0400

    Bump versions for 7.1.24-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 22
+#define PHP_RELEASE_VERSION 24
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.22-dev"
-#define PHP_VERSION_ID 70122
+#define PHP_VERSION "7.1.24-dev"
+#define PHP_VERSION_ID 70124
