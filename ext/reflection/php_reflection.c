f0647edd88ca2a2d4e15b4019a3694ef1dbe33f9
Nikita Popov <nikita.ppv@gmail.com>
2018-10-02 18:19:53 +0200

    Fixed bug #66430

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -1897,6 +1897,11 @@ ZEND_METHOD(reflection_function, invoke)
 	fcc.called_scope = NULL;
 	fcc.object = NULL;
 
+	if (!Z_ISUNDEF(intern->obj)) {
+		Z_OBJ_HT(intern->obj)->get_closure(
+			&intern->obj, &fcc.called_scope, &fcc.function_handler, &fcc.object);
+	}
+
 	result = zend_call_function(&fci, &fcc);
 
 	if (result == FAILURE) {
@@ -1957,6 +1962,11 @@ ZEND_METHOD(reflection_function, invokeArgs)
 	fcc.calling_scope = zend_get_executed_scope();
 	fcc.called_scope = NULL;
 	fcc.object = NULL;
+
+	if (!Z_ISUNDEF(intern->obj)) {
+		Z_OBJ_HT(intern->obj)->get_closure(
+			&intern->obj, &fcc.called_scope, &fcc.function_handler, &fcc.object);
+	}
 
 	result = zend_call_function(&fci, &fcc);
 
