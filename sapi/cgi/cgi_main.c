044eb39e633c91d55b339415ee5749a46496ffd9
Anatol Belski <ab@php.net>
2018-10-03 14:32:52 +0200

    Avoid code duplication and don't miss env restore

--- sapi/cgi/cgi_main.c
+++ sapi/cgi/cgi_main.c
@@ -2242,8 +2242,7 @@ consult the installation file that came with this distribution, or visit \n\
 			while (parent) {
 				EnterCriticalSection(&cleanup_lock);
 				if (cleaning_up) {
-					DeleteCriticalSection(&cleanup_lock);
-					goto parent_out;
+					goto parent_loop_end;
 				}
 				LeaveCriticalSection(&cleanup_lock);
 
@@ -2299,6 +2298,7 @@ consult the installation file that came with this distribution, or visit \n\
 				WaitForMultipleObjects(kids, kid_cgi_ps, FALSE, INFINITE);
 			}
 			
+parent_loop_end:
 			/* restore my env */
 			SetEnvironmentVariable("PHP_FCGI_CHILDREN", kid_buf);
 
