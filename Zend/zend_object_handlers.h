3b6ca5b352d372de3e9ad457e5306c76f4e38702
Nikita Popov <nikita.ppv@gmail.com>
2018-10-13 16:42:10 +0200

    Mark clone_obj as optional

--- Zend/zend_object_handlers.h
+++ Zend/zend_object_handlers.h
@@ -156,7 +156,7 @@ struct _zend_object_handlers {
 	/* general object functions */
 	zend_object_free_obj_t					free_obj; /* required */
 	zend_object_dtor_obj_t					dtor_obj; /* required */
-	zend_object_clone_obj_t					clone_obj;
+	zend_object_clone_obj_t					clone_obj; /* optional */
 	/* individual object functions */
 	zend_object_read_property_t				read_property; /* required */
 	zend_object_write_property_t			write_property; /* required */
