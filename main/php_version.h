0b3cbd665ee18f6ff462f4c188d4b82c5f68e477
Sara Golemon <pollita@php.net>
2018-11-21 14:37:37 -0500

    Prep for 7.1.26

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 25
+#define PHP_RELEASE_VERSION 26
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.25-dev"
-#define PHP_VERSION_ID 70125
+#define PHP_VERSION "7.1.26-dev"
+#define PHP_VERSION_ID 70126
