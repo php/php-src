10b69ead9ccd7773fb9d4a2e0bb5402a80074922
Christoph M. Becker <cmbecker69@gmx.de>
2018-11-25 23:26:12 +0100

    Fix #73291: imagecropauto() $threshold differs from external libgd
    Since upstream does not appear to move in any way[1], we sync our
    behavior.  Even though the BC break is ugly (which is the reason we
    target master only), having to deal with different algorithms is even
    worse for portable userland code.
    
    [1] <https://github.com/libgd/libgd/issues/334>

--- ext/gd/libgd/gd_crop.c
+++ ext/gd/libgd/gd_crop.c
@@ -198,7 +198,7 @@ gdImagePtr gdImageCropThreshold(gdImagePtr im, const unsigned int color, const f
 	crop.height = 0;
 
 	/* Pierre: crop everything sounds bad */
-	if (threshold > 1.0) {
+	if (threshold > 100.0) {
 		return NULL;
 	}
 
@@ -303,10 +303,9 @@ static int gdColorMatch(gdImagePtr im, int col1, int col2, float threshold)
 	const int dg = gdImageGreen(im, col1) - gdImageGreen(im, col2);
 	const int db = gdImageBlue(im, col1) - gdImageBlue(im, col2);
 	const int da = gdImageAlpha(im, col1) - gdImageAlpha(im, col2);
-	const double dist = sqrt(dr * dr + dg * dg + db * db + da * da);
-	const double dist_perc = sqrt(dist / (255^2 + 255^2 + 255^2));
-	return (dist_perc <= threshold);
-	//return (100.0 * dist / 195075) < threshold;
+	const int dist = dr * dr + dg * dg + db * db + da * da;
+
+	return (100.0 * dist / 195075) < threshold;
 }
 
 /*
