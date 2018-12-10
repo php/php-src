59cacdcefd21a4f6e48fc98e1c88e18a183748d6
Nikita Popov <nikita.ppv@gmail.com>
2018-12-10 12:50:00 +0100

    Fix typos in compact literals debugging code

--- ext/opcache/Optimizer/compact_literals.c
+++ ext/opcache/Optimizer/compact_literals.c
@@ -311,13 +311,13 @@ void zend_optimizer_compact_literals(zend_op_array *op_array, zend_optimizer_ctx
 			int i, use_copy;
 			fprintf(stderr, "File %s func %s\n", op_array->filename->val,
 					op_array->function_name ? op_array->function_name->val : "main");
-			fprintf(stderr, "Literlas table size %d\n", op_array->last_literal);
+			fprintf(stderr, "Literals table size %d\n", op_array->last_literal);
 
 			for (i = 0; i < op_array->last_literal; i++) {
 				zval zv;
 				ZVAL_COPY_VALUE(&zv, op_array->literals + i);
 				use_copy = zend_make_printable_zval(op_array->literals + i, &zv);
-				fprintf(stderr, "Literal %d, val (%d):%s\n", i, Z_STRLEN(zv), Z_STRVAL(zv));
+				fprintf(stderr, "Literal %d, val (%zu):%s\n", i, Z_STRLEN(zv), Z_STRVAL(zv));
 				if (use_copy) {
 					zval_ptr_dtor_nogc(&zv);
 				}
@@ -821,13 +821,13 @@ void zend_optimizer_compact_literals(zend_op_array *op_array, zend_optimizer_ctx
 #if DEBUG_COMPACT_LITERALS
 		{
 			int i, use_copy;
-			fprintf(stderr, "Optimized literlas table size %d\n", op_array->last_literal);
+			fprintf(stderr, "Optimized literals table size %d\n", op_array->last_literal);
 
 			for (i = 0; i < op_array->last_literal; i++) {
 				zval zv;
 				ZVAL_COPY_VALUE(&zv, op_array->literals + i);
 				use_copy = zend_make_printable_zval(op_array->literals + i, &zv);
-				fprintf(stderr, "Literal %d, val (%d):%s\n", i, Z_STRLEN(zv), Z_STRVAL(zv));
+				fprintf(stderr, "Literal %d, val (%zu):%s\n", i, Z_STRLEN(zv), Z_STRVAL(zv));
 				if (use_copy) {
 					zval_ptr_dtor_nogc(&zv);
 				}
