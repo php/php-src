php/php-src

commit 7e597f48e9fda982e930e4f617d2b2d98d8878a5

Fixed bug #77339 (__callStatic may get incorrect arguments)

Zend/zend_compile.c
@@ -4127,6 +4127,14 @@ void zend_compile_static_call(znode *result, zend_ast *ast, uint32_t type) /* {{
 		if (ce) {
 			zend_string *lcname = Z_STR_P(CT_CONSTANT(opline->op2) + 1);
 			fbc = zend_hash_find_ptr(&ce->function_table, lcname);
+			if (fbc && !(fbc->common.fn_flags & ZEND_ACC_PUBLIC)) {
+				if (ce != CG(active_class_entry)
+				 &&((fbc->common.fn_flags & ZEND_ACC_PRIVATE)
+				  || !zend_check_protected(zend_get_function_root_class(fbc), CG(active_class_entry)))) {
+					/* incompatibe function */
+					fbc = NULL;
+				}
+			}
 		}
 	}
 

Zend/tests/bug77339.phpt
@@ -1,0 +1,33 @@
+--TEST--
+Bug #77339 (__callStatic may get incorrect arguments)
+--FILE--
+<?php
+class Foo
+{
+    static function __callStatic($name, $arguments) {
+       if ($name === 'get') {
+            if (!isset($arguments[0])) {
+                var_dump(['getSomeWhat']);
+                var_dump($arguments);
+                exit;
+            }
+        } 
+        echo "OK\n";
+    }
+
+    protected function get ($key) {
+        echo "BUG!!!\n";
+    }
+}
+
+class Bar
+{
+    static function __callStatic($name, $arguments) {
+        echo Foo::get('getSomeWhat');
+    }
+}
+
+Bar::someUndefinedStaticFunction();
+?>
+--EXPECT--
+OK

commit 636b16402b7c086e51d80d709508391aa95ebed3

Fixed crashes introduced by 7e597f48e9fda982e930e4f617d2b2d98d8878a5 (only master was affected)

Zend/zend_compile.c
@@ -4057,7 +4057,10 @@ void zend_compile_static_call(znode *result, zend_ast *ast, uint32_t type) /* {{
 			fbc = zend_hash_find_ptr(&ce->function_table, lcname);
 			if (fbc && !(fbc->common.fn_flags & ZEND_ACC_PUBLIC)) {
 				if (ce != CG(active_class_entry)
-				 &&((fbc->common.fn_flags & ZEND_ACC_PRIVATE)
+				 && ((fbc->common.fn_flags & ZEND_ACC_PRIVATE)
+				  || !(fbc->common.scope->ce_flags & ZEND_ACC_LINKED)
+				  || (CG(active_class_entry)
+				   && !(CG(active_class_entry)->ce_flags & ZEND_ACC_LINKED))
 				  || !zend_check_protected(zend_get_function_root_class(fbc), CG(active_class_entry)))) {
 					/* incompatibe function */
 					fbc = NULL;