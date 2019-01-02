php/php-src

commit cdd8368d6f3f000a2e30a6be9976c5539f10fe85

Clean up unnecessary ternary expressions and simplify some returns
- Simplify conditions
- Use ZEND_HASH_APPLY_* instead of hard-coded booleans
- Use ZEND_NORMALIZE_BOOL
- Drop sign in favor of ZEND_NORMALIZE_BOOL

ext/standard/array.c
@@ -170,7 +170,7 @@ static int php_array_key_compare(const void *a, const void *b) /* {{{ */
 			}
 		}
 	}
-	return l1 > l2 ? 1 : (l1 < l2 ? -1 : 0);
+	return ZEND_NORMALIZE_BOOL(l1 > l2);
 }
 /* }}} */
 
@@ -978,7 +978,7 @@ static int php_array_user_compare(const void *a, const void *b) /* {{{ */
 		zval_ptr_dtor(&retval);
 		zval_ptr_dtor(&args[1]);
 		zval_ptr_dtor(&args[0]);
-		return ret < 0 ? -1 : ret > 0 ? 1 : 0;
+		return ZEND_NORMALIZE_BOOL(ret);
 	} else {
 		zval_ptr_dtor(&args[1]);
 		zval_ptr_dtor(&args[0]);
@@ -1101,7 +1101,7 @@ static int php_array_user_key_compare(const void *a, const void *b) /* {{{ */
 	zval_ptr_dtor(&args[0]);
 	zval_ptr_dtor(&args[1]);
 
-	return result < 0 ? -1 : result > 0 ? 1 : 0;
+	return ZEND_NORMALIZE_BOOL(result);
 }
 /* }}} */
 
@@ -4583,7 +4583,7 @@ static int zval_user_compare(zval *a, zval *b) /* {{{ */
 	if (zend_call_function(&BG(user_compare_fci), &BG(user_compare_fci_cache)) == SUCCESS && Z_TYPE(retval) != IS_UNDEF) {
 		zend_long ret = zval_get_long(&retval);
 		zval_ptr_dtor(&retval);
-		return ret < 0 ? -1 : ret > 0 ? 1 : 0;
+		return ZEND_NORMALIZE_BOOL(ret);
 	} else {
 		return 0;
 	}

Zend/zend_compile.c
@@ -1576,7 +1576,7 @@ int ZEND_FASTCALL zendlex(zend_parser_stack_elem *elem) /* {{{ */
 
 ZEND_API void zend_initialize_class_data(zend_class_entry *ce, zend_bool nullify_handlers) /* {{{ */
 {
-	zend_bool persistent_hashes = (ce->type == ZEND_INTERNAL_CLASS) ? 1 : 0;
+	zend_bool persistent_hashes = ce->type == ZEND_INTERNAL_CLASS;
 
 	ce->refcount = 1;
 	ce->ce_flags = ZEND_ACC_CONSTANTS_UPDATED;

Zend/zend_constants.c
@@ -83,9 +83,9 @@ static int clean_module_constant(zval *el, void *arg)
 	int module_number = *(int *)arg;
 
 	if (ZEND_CONSTANT_MODULE_NUMBER(c) == module_number) {
-		return 1;
+		return ZEND_HASH_APPLY_REMOVE;
 	} else {
-		return 0;
+		return ZEND_HASH_APPLY_KEEP;
 	}
 }
 

Zend/zend_execute.c
@@ -845,10 +845,7 @@ static zend_bool zend_verify_weak_scalar_type_hint(zend_uchar type_hint, zval *a
 			zend_string *dest;
 
 			/* on success "arg" is converted to IS_STRING */
-			if (!zend_parse_arg_str_weak(arg, &dest)) {
-				return 0;
-			}
-			return 1;
+			return zend_parse_arg_str_weak(arg, &dest);
 		}
 		default:
 			return 0;

Zend/zend_ini.c
@@ -36,11 +36,8 @@ static int zend_remove_ini_entries(zval *el, void *arg) /* {{{ */
 {
 	zend_ini_entry *ini_entry = (zend_ini_entry *)Z_PTR_P(el);
 	int module_number = *(int *)arg;
-	if (ini_entry->module_number == module_number) {
-		return 1;
-	} else {
-		return 0;
-	}
+
+	return ini_entry->module_number == module_number;
 }
 /* }}} */
 

Zend/zend_list.c
@@ -245,11 +245,8 @@ void zend_destroy_rsrc_list(HashTable *ht)
 static int clean_module_resource(zval *zv, void *arg)
 {
 	int resource_id = *(int *)arg;
-	if (Z_RES_TYPE_P(zv) == resource_id) {
-		return 1;
-	} else {
-		return 0;
-	}
+
+	return Z_RES_TYPE_P(zv) == resource_id;
 }
 
 

Zend/zend_stream.c
@@ -186,7 +186,7 @@ ZEND_API int zend_stream_fixup(zend_file_handle *file_handle, char **buf, size_t
 				return FAILURE;
 			}
 			memset(&file_handle->handle.stream.mmap, 0, sizeof(zend_mmap));
-			file_handle->handle.stream.isatty     = isatty(fileno((FILE *)file_handle->handle.stream.handle)) ? 1 : 0;
+			file_handle->handle.stream.isatty     = isatty(fileno((FILE *)file_handle->handle.stream.handle));
 			file_handle->handle.stream.reader     = (zend_stream_reader_t)zend_stream_stdio_reader;
 			file_handle->handle.stream.closer     = (zend_stream_closer_t)zend_stream_stdio_closer;
 			file_handle->handle.stream.fsizer     = (zend_stream_fsizer_t)zend_stream_stdio_fsizer;

Zend/zend_virtual_cwd.c
@@ -665,7 +665,7 @@ static size_t tsrm_realpath_r(char *path, size_t start, size_t len, int *ll, tim
 
 			if(pbuffer->ReparseTag == IO_REPARSE_TAG_SYMLINK) {
 				reparsetarget = pbuffer->SymbolicLinkReparseBuffer.ReparseTarget;
-				isabsolute = (pbuffer->SymbolicLinkReparseBuffer.Flags == 0) ? 1 : 0;
+				isabsolute = pbuffer->SymbolicLinkReparseBuffer.Flags == 0;
 #if VIRTUAL_CWD_DEBUG
 				printname = php_win32_ioutil_w_to_any(reparsetarget + pbuffer->MountPointReparseBuffer.PrintNameOffset  / sizeof(WCHAR));
 				if (!printname) {

commit 7ac3c2fa655d1961937f86a8d3ef2c5e96cf4f76

Fix ZEND_NORMALIZE_BOOL usage
Typo introduced in cdd8368d6f3f000a2e30a6be9976c5539f10fe85.

ext/standard/array.c
@@ -170,7 +170,7 @@ static int php_array_key_compare(const void *a, const void *b) /* {{{ */
 			}
 		}
 	}
-	return ZEND_NORMALIZE_BOOL(l1 > l2);
+	return ZEND_NORMALIZE_BOOL(l1 - l2);
 }
 /* }}} */
 