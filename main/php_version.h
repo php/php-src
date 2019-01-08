fabade157323a0998ef2b97208b8f58779c40f2b
Sara Golemon <pollita@php.net>
2019-01-08 14:17:43 -0500

    Bump for 7.1.27

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 26
+#define PHP_RELEASE_VERSION 27
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.26-dev"
-#define PHP_VERSION_ID 70126
+#define PHP_VERSION "7.1.27-dev"
+#define PHP_VERSION_ID 70127
