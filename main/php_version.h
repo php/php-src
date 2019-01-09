4b8f72da5dfb201af4e82dee960261d8657e414f
Ferenc Kovacs <tyra3l@gmail.com>
2019-01-09 10:54:13 +0100

    5.6.40

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 5
 #define PHP_MINOR_VERSION 6
-#define PHP_RELEASE_VERSION 41
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "5.6.41-dev"
-#define PHP_VERSION_ID 50641
+#define PHP_RELEASE_VERSION 40
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "5.6.40"
+#define PHP_VERSION_ID 50640
