53a6e0044f06b051db061074f0661ec33599a769
Nikita Popov <nikita.ppv@gmail.com>
2019-01-28 12:04:51 +0100

    Remove major version from apache module

--- sapi/apache2handler/mod_php.c
+++ sapi/apache2handler/mod_php.c
@@ -23,7 +23,7 @@
 #include "php.h"
 #include "php_apache.h"
 
-AP_MODULE_DECLARE_DATA module php7_module = {
+AP_MODULE_DECLARE_DATA module php_module = {
 	STANDARD20_MODULE_STUFF,
 	create_php_config,		/* create per-directory config structure */
 	merge_php_config,		/* merge per-directory config structures */
