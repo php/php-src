db0079023421b8048f090ee04adb992e09132553
Dmitry Stogov <dmitry@zend.com>
2019-02-04 12:07:11 +0300

    ext/com_dotnet support for new object handlers API

--- ext/com_dotnet/com_persist.c
+++ ext/com_dotnet/com_persist.c
@@ -709,9 +709,9 @@ static void helper_free_storage(zend_object *obj)
 }
 
 
-static zend_object* helper_clone(zval *obj)
+static zend_object* helper_clone(zend_object *obj)
 {
-	php_com_persist_helper *clone, *object = (php_com_persist_helper*)Z_OBJ_P(obj);
+	php_com_persist_helper *clone, *object = (php_com_persist_helper*) obj;
 
 	clone = emalloc(sizeof(*object));
 	memcpy(clone, object, sizeof(*object));
