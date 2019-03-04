7f0ab7c20c83a52862ad7c8acf31c3fa739f1274
Stanislav Malyshev <stas@php.net>
2019-03-03 18:22:32 -0800

    Fix bug #77396 - Null Pointer Dereference in phar_create_or_parse_filename

--- ext/phar/phar.c
+++ ext/phar/phar.c
@@ -1385,6 +1385,9 @@ int phar_create_or_parse_filename(char *fname, int fname_len, char *alias, int a
 	/* set up our manifest */
 	mydata = ecalloc(1, sizeof(phar_archive_data));
 	mydata->fname = expand_filepath(fname, NULL);
+	if (mydata->fname == NULL) {
+		return FAILURE;
+	}
 	fname_len = strlen(mydata->fname);
 #ifdef PHP_WIN32
 	phar_unixify_path_separators(mydata->fname, fname_len);
