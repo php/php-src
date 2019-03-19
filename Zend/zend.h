a2de42041c7ac70460521fc4bb8cbe370532f1d0
Christoph M. Becker <cmbecker69@gmx.de>
2019-03-19 16:20:55 +0100

    Prepare main branch for PHP 7.3.5

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "3.3.4-dev"
+#define ZEND_VERSION "3.3.5-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 4
+#define PHP_RELEASE_VERSION 5
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.4-dev"
-#define PHP_VERSION_ID 70304
+#define PHP_VERSION "7.3.5-dev"
+#define PHP_VERSION_ID 70305
