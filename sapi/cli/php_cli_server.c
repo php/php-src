5408fb373478cc40dc490351bc69df70ba5064d0
Niklas Keller <me@kelunik.com>
2019-03-24 21:06:33 +0100

    Remove extra whitespace

--- sapi/cli/php_cli_server.c
+++ sapi/cli/php_cli_server.c
@@ -359,7 +359,7 @@ static void append_essential_headers(smart_str* buffer, php_cli_server_client *c
 		zend_string *dt = php_format_date("D, d M Y H:i:s", 14, tv.tv_sec, 0);
 		smart_str_appendl_ex(buffer, "Date: ", 6, persistent);
 		smart_str_appends_ex(buffer, dt->val, persistent);
-		smart_str_appendl_ex(buffer, " GMT \r\n", 7, persistent);
+		smart_str_appendl_ex(buffer, " GMT\r\n", 6, persistent);
 		zend_string_release(dt);
 	}
 
