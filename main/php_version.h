edcb43edfc54d69968531dc4ef37150e21c45f4c
Joe Watkins <krakjoe@php.net>
2019-04-02 15:28:17 +0200

    set versions for 7.1.28

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 28
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.1.28-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "7.1.28"
 #define PHP_VERSION_ID 70128
