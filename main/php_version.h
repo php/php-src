3caa67740a94a5669ad419c4378476ba7825a294
Christoph M. Becker <cmb@php.net>
2019-04-16 09:30:02 +0000

    Update for PHP 7.3.5RC1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 5
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.5-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.3.5RC1"
 #define PHP_VERSION_ID 70305
