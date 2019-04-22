e311f2a320b07b6d210c5b1db24a37a0259859f1
Nikita Popov <nikita.ppv@gmail.com>
2019-04-22 12:01:04 +0200

    Fix flag value clash affecting defined() function

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -921,8 +921,9 @@ void zend_assert_valid_class_name(const zend_string *const_name);
 
 #define ZEND_DIM_IS 1
 
-#define IS_CONSTANT_CLASS           0x080  /* __CLASS__ in trait */
-#define IS_CONSTANT_UNQUALIFIED_IN_NAMESPACE 0x100
+/* Make sure these don't clash with ZEND_FETCH_CLASS_* flags. */
+#define IS_CONSTANT_CLASS                    0x400 /* __CLASS__ in trait */
+#define IS_CONSTANT_UNQUALIFIED_IN_NAMESPACE 0x800
 
 static zend_always_inline int zend_check_arg_send_type(const zend_function *zf, uint32_t arg_num, uint32_t mask)
 {
