2d93cce03a96ee7b0265e15e2d56acd173dec682
Christoph M. Becker <cmbecker69@gmx.de>
2019-05-14 11:44:58 +0200

    Prepare 7.3.7-dev

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "3.3.6-dev"
+#define ZEND_VERSION "3.3.7-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 6
+#define PHP_RELEASE_VERSION 7
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.6-dev"
-#define PHP_VERSION_ID 70306
+#define PHP_VERSION "7.3.7-dev"
+#define PHP_VERSION_ID 70307
