4fa32d67bf3fbea0241f0e786dbcb5517d25e1a2
Sara Golemon <pollita@php.net>
2019-05-14 20:22:00 -0400

    Bump for 7.2.20

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 19
+#define PHP_RELEASE_VERSION 20
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.2.19-dev"
-#define PHP_VERSION_ID 70219
+#define PHP_VERSION "7.2.20-dev"
+#define PHP_VERSION_ID 70220
