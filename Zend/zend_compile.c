f53b9939fe4f66e1a539a42d607fefbe45be6239
Dmitry Stogov <dmitry@zend.com>
2019-05-20 18:15:38 +0300

    Fixed bug #78014 (Preloaded classes may depend on non-preloaded classes due to unresolved consts)

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -6389,6 +6389,7 @@ zend_op *zend_compile_class_decl(zend_ast *ast, zend_bool toplevel) /* {{{ */
 			zend_class_entry *parent_ce = zend_lookup_class_ex(ce->parent_name, NULL, 0);
 
 			if (parent_ce
+			 && !(CG(compiler_options) & ZEND_COMPILE_PRELOAD) /* delay inheritance till preloading */
 			 && ((parent_ce->type != ZEND_INTERNAL_CLASS) || !(CG(compiler_options) & ZEND_COMPILE_IGNORE_INTERNAL_CLASSES))
 			 && ((parent_ce->type != ZEND_USER_CLASS) || !(CG(compiler_options) & ZEND_COMPILE_IGNORE_OTHER_FILES) || (parent_ce->info.user.filename == ce->info.user.filename))
 				) {
