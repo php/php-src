819801383046a7a15208b2f96db324124f980047
Peter Kokot <peterkokot@gmail.com>
2019-05-21 01:24:11 +0200

    Remove PHP version from LiteSpeed SAPI

--- sapi/litespeed/lsapi_main.c
+++ sapi/litespeed/lsapi_main.c
@@ -448,7 +448,7 @@ static int sapi_lsapi_activate()
 static sapi_module_struct lsapi_sapi_module =
 {
     "litespeed",
-    "LiteSpeed" PHP_VERSION,
+    "LiteSpeed",
 
     php_lsapi_startup,              /* startup */
     php_module_shutdown_wrapper,    /* shutdown */
