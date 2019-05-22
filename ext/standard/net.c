665623b383d15a70e63b7d42ddf10de2c698144b
David Carlier <devnexen@gmail.com>
2019-05-22 23:04:56 +0100

    Using php_win32_error api

--- ext/standard/net.c
+++ ext/standard/net.c
@@ -177,14 +177,9 @@ PHP_FUNCTION(net_get_interfaces) {
 	dwRetVal = GetAdaptersAddresses(family, flags, NULL, pAddresses, &outBufLen);
 
 	if (NO_ERROR != dwRetVal) {
-		// Avoiding dynamic allocation deliberately
-		char buf[256];
-		DWORD err = GetLastError();
-		DWORD bufsize = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS,
-					     NULL, err, 0, buf, 256, NULL);
-		if (bufsize) {
-			zend_error(E_WARNING, "GetAdaptersAddresses failed: %s", buf);
-		}
+		char *buf = php_win32_error_to_msg(GetLastError());
+		zend_error(E_WARNING, "GetAdaptersAddresses failed: %s", buf);
+		php_win32_error_msg_free(buf);
 		FREE(pAddresses);
 		RETURN_FALSE;
 	}
