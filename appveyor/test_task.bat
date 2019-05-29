680daf66aa5e4e7c40c3875afe05e9d69b6cf3d5
Christoph M. Becker <cmbecker69@gmx.de>
2019-05-29 19:34:34 +0200

    Enable JIT in Appveyor build

--- appveyor/test_task.bat
+++ appveyor/test_task.bat
@@ -58,7 +58,7 @@ rem set OPENSSL_CONF=
 rem set SSLEAY_CONF=
 
 rem prepare for Opcache
-if "%OPCACHE%" equ "1" set OPCACHE_OPTS=-d opcache.enable=1 -d opcache.enable_cli=1 -d opcache.protect_memory=1
+if "%OPCACHE%" equ "1" set OPCACHE_OPTS=-d opcache.enable=1 -d opcache.enable_cli=1 -d opcache.protect_memory=1 -d opcache.jit_buffer_size=16M
 
 rem prepare for enchant
 mkdir c:\enchant_plugins
