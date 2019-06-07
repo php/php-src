9d27d12799249a86c545479492dcca4cf2d71430
William Hudgins <will@wkhudgins.info>
2019-06-07 13:46:04 -0500

    Update mbstring.c

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -2712,6 +2712,7 @@ PHP_FUNCTION(mb_str_ends)
     if (!mbfl_is_error(n)) {
         RETURN_BOOL(1);
     } else {
+		char buffer[42];
         switch (-n) {
             case 1:
                 break;
@@ -2722,7 +2723,7 @@ PHP_FUNCTION(mb_str_ends)
                 php_error_docref(NULL, E_NOTICE, "Argument is empty");
                 break;
             default:
-				char buffer[42];
+				
 				sprintf(buffer, "Unknown error in mb_strpos. Error code: %zu", n);
                 php_error_docref(NULL, E_WARNING, buffer);
                 break;
