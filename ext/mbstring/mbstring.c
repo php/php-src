edc593d66755006f0e4a77b908ba4ca051f5c5c9
William Hudgins <will@wkhudgins.info>
2019-06-10 13:35:33 -0500

    Update mbstring.c

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -2719,6 +2719,9 @@ PHP_FUNCTION(mb_str_ends)
             case 8:
                 php_error_docref(NULL, E_NOTICE, "Argument is empty");
                 break;
+			case 16:
+				php_error_docref(NULL, E_WARNING, "Unknown needle, haystack error");
+				break;
             default:
                 php_error_docref(NULL, E_WARNING, "Unknown error in mb_strpos");
                 break;
