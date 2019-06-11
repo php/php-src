bcf20963c10ffdb8d0cf3adafcf440a3e6d6484c
Christoph M. Becker <cmbecker69@gmx.de>
2019-06-11 13:31:30 +0200

    Next is 7.3.8

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 7
+#define PHP_RELEASE_VERSION 8
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.7-dev"
-#define PHP_VERSION_ID 70307
+#define PHP_VERSION "7.3.8-dev"
+#define PHP_VERSION_ID 70308
