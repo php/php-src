4c6de12b2d3641dd30c51506eb41546b4d66c3d7
Christoph M. Becker <cmbecker69@gmx.de>
2019-06-11 12:26:00 +0200

    Update for PHP 7.3.7RC1

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "3.3.7-dev"
+#define ZEND_VERSION "3.3.7RC1"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 7
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.7-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.3.7RC1"
 #define PHP_VERSION_ID 70307
