56c022ed9548cfb93db3ad9f13ef6982586a8d74
Derick Rethans <github@derickrethans.nl>
2019-06-12 11:11:36 +0000

    Update versions for PHP 7.4.0alpha1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.0-dev"
+#define PHP_EXTRA_VERSION "alpha1"
+#define PHP_VERSION "7.4.0alpha1"
 #define PHP_VERSION_ID 70400
