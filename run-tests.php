f8a68fd9350ac97d33d1773453c4e9d47a97b33f
Nikita Popov <nikita.ppv@gmail.com>
2019-06-13 12:35:29 +0200

    Add test for bug #78106
    Also add PHP_TEST_EXTRA_ARGS environment variable, which allows
    to pass on -c, -d etc flags provided by run-tests.php. Otherwise
    we won't get the built-in server to run with opcache.

--- run-tests.php
+++ run-tests.php
@@ -820,7 +820,7 @@ HELP;
 		if (substr(PHP_OS, 0, 3) == "WIN") {
 			$pass_options .= " -c " . escapeshellarg($conf_passed);
 		} else {
-			$pass_options .= " -c '$conf_passed'";
+			$pass_options .= " -c '" . realpath($conf_passed) . "'";
 		}
 	}
 
@@ -1610,6 +1610,8 @@ TEST $file
 	}
 
 	settings2params($ini_settings);
+
+	$env['TEST_PHP_EXTRA_ARGS'] = $pass_options . ' ' . $ini_settings;
 
 	// Check if test should be skipped.
 	$info = '';
