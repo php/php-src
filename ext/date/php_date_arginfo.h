33886f710cd45fd883d85558197e2df4eaba9ea1
Nikita Popov <nikita.ppv@gmail.com>
2019-06-18 11:33:07 +0200

    Generate arginfo from PHP stub files
    Signature stubs for internal functions are specified in xyz.stub.php,
    from which we generate actual arginfo structures in xyz_arginfo.h.
    This file then needs to be included in the implementation appropriately.
    
    Arginfo from stubs can be regenerated using scripts/dev/gen_stub.php.
    However, this should also automatically happen when the stub file is
    modified.

---
+++ ext/date/php_date_arginfo.h
@@ -0,0 +1,356 @@
+/* This is a generated file, edit the .stub.php file instead. */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_strtotime, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, time, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, now, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_date, 0, 1, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_idate, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gmdate arginfo_date
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_mktime, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, hour, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, min, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, sec, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, mon, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, day, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, year, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gmmktime arginfo_mktime
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_checkdate, 0, 3, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, m, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, d, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, y, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_strftime arginfo_idate
+
+#define arginfo_gmstrftime arginfo_idate
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_time, 0, 0, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_localtime, 0, 0, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, associative, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_getdate, 0, 0, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_create, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, time, IS_STRING, 0)
+	ZEND_ARG_OBJ_INFO(0, timezone, DateTimeZone, 1)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_create_immutable arginfo_date_create
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_create_from_format, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, time, IS_STRING, 0)
+	ZEND_ARG_OBJ_INFO(0, timezone, DateTimeZone, 1)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_create_immutable_from_format arginfo_date_create_from_format
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_date_parse, 0, 1, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, date, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_date_parse_from_format, 0, 2, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, date, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_get_last_errors, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_format, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeInterface, 0)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_modify, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_TYPE_INFO(0, modify, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_add, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_OBJ_INFO(0, interval, DateInterval, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_sub arginfo_date_add
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_timezone_get, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeInterface, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_timezone_set, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeInterface, 0)
+	ZEND_ARG_OBJ_INFO(0, timezone, DateTimeZone, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_offset_get arginfo_date_timezone_get
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_diff, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeInterface, 0)
+	ZEND_ARG_OBJ_INFO(0, object2, DateTimeInterface, 0)
+	ZEND_ARG_TYPE_INFO(0, absolute, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_time_set, 0, 0, 3)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_TYPE_INFO(0, hour, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, minute, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, second, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, microseconds, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_date_set, 0, 0, 4)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_TYPE_INFO(0, year, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, month, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, day, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_isodate_set, 0, 0, 3)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_TYPE_INFO(0, year, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, week, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, day, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_timestamp_set, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_timestamp_get arginfo_date_timezone_get
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_open, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, timezone, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_name_get, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeZone, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_name_from_abbr, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, abbr, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, gmtoffset, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, isdst, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_offset_get, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeZone, 0)
+	ZEND_ARG_OBJ_INFO(0, datetime, DateTimeInterface, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_transitions_get, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeZone, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp_begin, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp_end, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_timezone_location_get arginfo_timezone_name_get
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_timezone_identifiers_list, 0, 0, 0)
+	ZEND_ARG_TYPE_INFO(0, what, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, country, IS_STRING, 1)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_timezone_abbreviations_list, 0, 0, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_timezone_version_get, 0, 0, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_interval_create_from_date_string, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, time, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_interval_format, 0, 0, 2)
+	ZEND_ARG_OBJ_INFO(0, object, DateInterval, 0)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_date_default_timezone_set, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_TYPE_INFO(0, timezone_identifier, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_default_timezone_get arginfo_timezone_version_get
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_date_sunrise, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, time, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, retformat, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, latitude, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, longitude, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, zenith, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, gmt_offset, IS_DOUBLE, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_date_sunset arginfo_date_sunrise
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_date_sun_info, 0, 3, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, time, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, latitude, IS_DOUBLE, 0)
+	ZEND_ARG_TYPE_INFO(0, longitude, IS_DOUBLE, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTimeInterface_format, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, format, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTimeInterface_getTimezone arginfo_date_get_last_errors
+
+#define arginfo_DateTimeInterface_getOffset arginfo_date_get_last_errors
+
+#define arginfo_DateTimeInterface_getTimestamp arginfo_date_get_last_errors
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTimeInterface_diff, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeInterface, 0)
+	ZEND_ARG_TYPE_INFO(0, absolute, _IS_BOOL, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTimeInterface___wakeup arginfo_date_get_last_errors
+
+#define arginfo_DateTime___construct arginfo_date_create
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime___set_state, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, array, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_createFromImmutable, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTimeImmutable, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTime_createFromFormat arginfo_date_create_from_format
+
+#define arginfo_DateTime_getLastErrors arginfo_date_get_last_errors
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_modify, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, modify, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_add, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, interval, DateInterval, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTime_sub arginfo_DateTime_add
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_setTimezone, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, timezone, DateTimeZone, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_setTime, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, hour, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, minute, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, second, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, microseconds, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_setDate, 0, 0, 3)
+	ZEND_ARG_TYPE_INFO(0, year, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, month, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, day, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_setISODate, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, year, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, week, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, day, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTime_setTimestamp, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, timestampt, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTimeImmutable___construct arginfo_date_create
+
+#define arginfo_DateTimeImmutable___set_state arginfo_date_get_last_errors
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTimeImmutable_createFromMutable, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, object, DateTime, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTimeImmutable_modify arginfo_DateTime_modify
+
+#define arginfo_DateTimeImmutable_add arginfo_DateTime_add
+
+#define arginfo_DateTimeImmutable_sub arginfo_DateTime_add
+
+#define arginfo_DateTimeImmutable_setTimezone arginfo_DateTime_setTimezone
+
+#define arginfo_DateTimeImmutable_setTime arginfo_DateTime_setTime
+
+#define arginfo_DateTimeImmutable_setDate arginfo_DateTime_setDate
+
+#define arginfo_DateTimeImmutable_setISODate arginfo_DateTime_setISODate
+
+#define arginfo_DateTimeImmutable_setTimestamp arginfo_DateTime_setTimestamp
+
+#define arginfo_DateTimeZone___construct arginfo_timezone_open
+
+#define arginfo_DateTimeZone_getName arginfo_date_get_last_errors
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTimeZone_getOffset, 0, 0, 1)
+	ZEND_ARG_OBJ_INFO(0, datetime, DateTimeInterface, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateTimeZone_getTransitions, 0, 0, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp_begin, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, timestamp_end, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateTimeZone_getLocation arginfo_date_get_last_errors
+
+#define arginfo_DateTimeZone_listAbbreviations arginfo_date_get_last_errors
+
+#define arginfo_DateTimeZone_listIdentifiers arginfo_timezone_identifiers_list
+
+#define arginfo_DateTimeZone___wakeup arginfo_date_get_last_errors
+
+#define arginfo_DateTimeZone___set_state arginfo_date_get_last_errors
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DateInterval___construct, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, interval_spec, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DateInterval_createFromDateString arginfo_date_interval_create_from_date_string
+
+#define arginfo_DateInterval_format arginfo_DateTimeInterface_format
+
+#define arginfo_DateInterval___wakeup arginfo_date_get_last_errors
+
+#define arginfo_DateInterval___set_state arginfo_DateTime___set_state
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_DatePeriod___construct, 0, 0, 1)
+	ZEND_ARG_INFO(0, start)
+	ZEND_ARG_INFO(0, interval)
+	ZEND_ARG_INFO(0, end)
+ZEND_END_ARG_INFO()
+
+#define arginfo_DatePeriod_getStartDate arginfo_date_get_last_errors
+
+#define arginfo_DatePeriod_getEndDate arginfo_date_get_last_errors
+
+#define arginfo_DatePeriod_getDateInterval arginfo_date_get_last_errors
+
+#define arginfo_DatePeriod_getRecurrences arginfo_date_get_last_errors
+
+#define arginfo_DatePeriod___wakeup arginfo_date_get_last_errors
+
+#define arginfo_DatePeriod___set_state arginfo_DateTime___set_state
---
+++ scripts/dev/gen_stub.php
@@ -0,0 +1,380 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt;
+
+error_reporting(E_ALL);
+
+try {
+    initPhpParser();
+} catch (Exception $e) {
+    echo "{$e->getMessage()}\n";
+    exit(1);
+}
+
+if ($argc < 2) {
+    die("Usage: php gen_stub.php foobar.stub.php\n");
+}
+
+$stubFile = $argv[1];
+$arginfoFile = str_replace('.stub.php', '', $stubFile) . '_arginfo.h';
+
+try {
+    $funcInfos = parseStubFile($stubFile);
+    $arginfoCode = generateArgInfoCode($funcInfos);
+    file_put_contents($arginfoFile, $arginfoCode);
+} catch (Exception $e) {
+    echo "Caught {$e->getMessage()} while processing $stubFile\n";
+    exit(1);
+}
+
+class Type {
+    /** @var string */
+    public $name;
+    /** @var bool */
+    public $isBuiltin;
+    /** @var bool */
+    public $isNullable;
+
+    public function __construct(string $name, bool $isBuiltin, bool $isNullable = false) {
+        $this->name = $name;
+        $this->isBuiltin = $isBuiltin;
+        $this->isNullable = $isNullable;
+    }
+
+    public static function fromNode(Node $node) {
+        if ($node instanceof Node\NullableType) {
+            $type = self::fromNode($node->type);
+            return new Type($type->name, $type->isBuiltin, true);
+        }
+        if ($node instanceof Node\Name) {
+            assert($node->isFullyQualified());
+            return new Type($node->toString(), false);
+        }
+        if ($node instanceof Node\Identifier) {
+            return new Type($node->toString(), true);
+        }
+        throw new Exception("Unexpected node type");
+    }
+
+    public function toTypeCode() {
+        assert($this->isBuiltin);
+        switch (strtolower($this->name)) {
+        case "bool":
+            return "_IS_BOOL";
+        case "int":
+            return "IS_LONG";
+        case "float":
+            return "IS_DOUBLE";
+        case "string":
+            return "IS_STRING";
+        case "array":
+            return "IS_ARRAY";
+        case "object":
+            return "IS_OBJECT";
+        case "void":
+            return "IS_VOID";
+        default:
+            throw new Exception("Not implemented: $this->name");
+        }
+    }
+
+    public static function equals(?Type $a, ?Type $b): bool {
+        if ($a === null || $b === null) {
+            return $a === $b;
+        }
+
+        return $a->name === $b->name
+            && $a->isBuiltin === $b->isBuiltin
+            && $a->isNullable === $b->isNullable;
+    }
+}
+
+class ArgInfo {
+    /** @var string */
+    public $name;
+    /** @var bool */
+    public $byRef;
+    /** @var bool */
+    public $isVariadic;
+    /** @var Type|null */
+    public $type;
+
+    public function __construct(string $name, bool $byRef, bool $isVariadic, ?Type $type) {
+        $this->name = $name;
+        $this->byRef = $byRef;
+        $this->isVariadic = $isVariadic;
+        $this->type = $type;
+    }
+
+    public function equals(ArgInfo $other): bool {
+        return $this->name === $other->name
+            && $this->byRef === $other->byRef
+            && $this->isVariadic === $other->isVariadic
+            && Type::equals($this->type, $other->type);
+    }
+}
+
+class ReturnInfo {
+    /** @var bool */
+    public $byRef;
+    /** @var Type|null */
+    public $type;
+
+    public function __construct(bool $byRef, ?Type $type) {
+        $this->byRef = $byRef;
+        $this->type = $type;
+    }
+
+    public function equals(ReturnInfo $other): bool {
+        return $this->byRef === $other->byRef
+            && Type::equals($this->type, $other->type);
+    }
+}
+
+class FuncInfo {
+    /** @var string */
+    public $name;
+    /** @var ArgInfo[] */
+    public $args;
+    /** @var ReturnInfo */
+    public $return;
+    /** @var int */
+    public $numRequiredArgs;
+    /** @var string|null */
+    public $cond;
+
+    public function __construct(
+        string $name, array $args, ReturnInfo $return, int $numRequiredArgs, ?string $cond
+    ) {
+        $this->name = $name;
+        $this->args = $args;
+        $this->return = $return;
+        $this->numRequiredArgs = $numRequiredArgs;
+        $this->cond = $cond;
+    }
+
+    public function equalsApartFromName(FuncInfo $other): bool {
+        if (count($this->args) !== count($other->args)) {
+            return false;
+        }
+
+        for ($i = 0; $i < count($this->args); $i++) {
+            if (!$this->args[$i]->equals($other->args[$i])) {
+                return false;
+            }
+        }
+
+        return $this->return->equals($other->return)
+            && $this->numRequiredArgs === $other->numRequiredArgs
+            && $this->cond === $other->cond;
+    }
+}
+
+function parseFunctionLike(string $name, Node\FunctionLike $func, ?string $cond): FuncInfo {
+    $args = [];
+    $numRequiredArgs = 0;
+    foreach ($func->getParams() as $i => $param) {
+        $args[] = new ArgInfo(
+            $param->var->name,
+            $param->byRef,
+            $param->variadic,
+            $param->type ? Type::fromNode($param->type) : null
+        );
+        if (!$param->default && !$param->variadic) {
+            $numRequiredArgs = $i + 1;
+        }
+    }
+
+    $returnType = $func->getReturnType();
+    $return = new ReturnInfo(
+        $func->returnsByRef(),
+        $returnType ? Type::fromNode($returnType) : null);
+    return new FuncInfo($name, $args, $return, $numRequiredArgs, $cond);
+}
+
+function parseClass(Stmt\Class_ $class): ClassInfo {
+    $funcs = [];
+    $className = $class->name->toString();
+    foreach ($class as $stmt) {
+        if (!$stmt instanceof Stmt\ClassMethod) {
+            throw new Exception("Not implemented");
+        }
+
+        $funcs[] = parseFunctionLike($className . '_' . $stmt->name->toString(), $stmt);
+    }
+    return new ClassInfo($className, $funcs);
+}
+
+/** @return FuncInfo[] */
+function parseStubFile(string $fileName) {
+    if (!file_exists($fileName)) {
+        throw new Exception("File $fileName does not exist");
+    }
+
+    $code = file_get_contents($fileName);
+
+    $lexer = new PhpParser\Lexer();
+    $parser = new PhpParser\Parser\Php7($lexer);
+    $nodeTraverser = new PhpParser\NodeTraverser;
+    $nodeTraverser->addVisitor(new PhpParser\NodeVisitor\NameResolver);
+
+    $stmts = $parser->parse($code);
+    $nodeTraverser->traverse($stmts);
+
+    $funcInfos = [];
+    $cond = null;
+    foreach ($stmts as $stmt) {
+        foreach ($stmt->getComments() as $comment) {
+            $text = trim($comment->getText());
+            if (preg_match('/^#if\s+(.+)$/', $text, $matches)) {
+                if ($cond !== null) {
+                    throw new Exception("Not implemented");
+                }
+                $cond = $matches[1];
+            } else if ($text === '#endif') {
+                $cond = null;
+            }
+        }
+
+        if ($stmt instanceof Stmt\Nop) {
+            continue;
+        }
+
+        if ($stmt instanceof Stmt\Function_) {
+            $funcInfos[] = parseFunctionLike($stmt->name->toString(), $stmt, $cond);
+            continue;
+        }
+
+        if ($stmt instanceof Stmt\ClassLike) {
+            $className = $stmt->name->toString();
+            foreach ($stmt->stmts as $classStmt) {
+                if ($classStmt instanceof Stmt\Nop) {
+                    continue;
+                }
+
+                if (!$classStmt instanceof Stmt\ClassMethod) {
+                    throw new Exception("Not implemented {$classStmt->getType()}");
+                }
+
+                $funcInfos[] = parseFunctionLike(
+                    $className . '_' . $classStmt->name->toString(), $classStmt, $cond);
+            }
+            continue;
+        }
+
+        throw new Exception("Unexpected node {$stmt->getType()}");
+    }
+
+    return $funcInfos;
+}
+
+function funcInfoToCode(FuncInfo $funcInfo): string {
+    $code = '';
+    if ($funcInfo->cond) {
+        $code .= "#if {$funcInfo->cond}\n";
+    }
+    if ($funcInfo->return->type) {
+        $returnType = $funcInfo->return->type;
+        if ($returnType->isBuiltin) {
+            $code .= sprintf(
+                "ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_%s, %d, %d, %s, %d)\n",
+                $funcInfo->name, $funcInfo->return->byRef, $funcInfo->numRequiredArgs,
+                $returnType->toTypeCode(), $returnType->isNullable
+            );
+        } else {
+            throw new Exception("Not implemented");
+        }
+    } else {
+        $code .= sprintf(
+            "ZEND_BEGIN_ARG_INFO_EX(arginfo_%s, 0, %d, %d)\n",
+            $funcInfo->name, $funcInfo->return->byRef, $funcInfo->numRequiredArgs
+        );
+    }
+
+    foreach ($funcInfo->args as $argInfo) {
+        if ($argInfo->isVariadic) {
+            if ($argInfo->type) {
+                throw new Exception("Not implemented");
+            }
+            $code .= sprintf(
+                "\tZEND_ARG_VARIADIC_INFO(%d, %s)\n",
+                $argInfo->byRef, $argInfo->name
+            );
+        } else if ($argInfo->type) {
+            if ($argInfo->type->isBuiltin) {
+                $code .= sprintf(
+                    "\tZEND_ARG_TYPE_INFO(%d, %s, %s, %d)\n",
+                    $argInfo->byRef, $argInfo->name,
+                    $argInfo->type->toTypeCode(), $argInfo->type->isNullable
+                );
+            } else {
+                $code .= sprintf(
+                    "\tZEND_ARG_OBJ_INFO(%d, %s, %s, %d)\n",
+                    $argInfo->byRef, $argInfo->name,
+                    $argInfo->type->name, $argInfo->type->isNullable
+                );
+            }
+        } else {
+            $code .= sprintf("\tZEND_ARG_INFO(%d, %s)\n", $argInfo->byRef, $argInfo->name);
+        }
+    }
+
+    $code .= "ZEND_END_ARG_INFO()";
+    if ($funcInfo->cond) {
+        $code .= "\n#endif";
+    }
+    return $code;
+}
+
+/** @param FuncInfo[] $funcInfos */
+function generateArginfoCode(array $funcInfos): string {
+    $code = "/* This is a generated file, edit the .stub.php file instead. */";
+    $generatedFuncInfos = [];
+    foreach ($funcInfos as $funcInfo) {
+        /* If there already is an equivalent arginfo structure, only emit a #define */
+        foreach ($generatedFuncInfos as $generatedFuncInfo) {
+            if ($generatedFuncInfo->equalsApartFromName($funcInfo)) {
+                $code .= sprintf(
+                    "\n\n#define arginfo_%s arginfo_%s",
+                    $funcInfo->name, $generatedFuncInfo->name
+                );
+                continue 2;
+            }
+        }
+
+        $code .= "\n\n" . funcInfoToCode($funcInfo);
+        $generatedFuncInfos[] = $funcInfo;
+    }
+    return $code . "\n";
+}
+
+function initPhpParser() {
+    $version = "4.2.2";
+    $phpParserDir = __DIR__ . "/PHP-Parser-$version";
+    if (!is_dir($phpParserDir)) {
+        $cwd = getcwd();
+        chdir(__DIR__);
+        passthru("wget https://github.com/nikic/PHP-Parser/archive/v$version.tar.gz", $exit);
+        if ($exit !== 0) {
+            throw new Exception("Failed to download PHP-Parser tarball");
+        }
+        if (!mkdir($phpParserDir)) {
+            throw new Exception("Failed to create directory $phpParserDir");
+        }
+        passthru("tar xvzf v$version.tar.gz -C PHP-Parser-$version --strip-components 1", $exit);
+        if ($exit !== 0) {
+            throw new Exception("Failed to extract PHP-Parser tarball");
+        }
+        unlink(__DIR__ . "/v$version.tar.gz");
+        chdir($cwd);
+    }
+
+    spl_autoload_register(function(string $class) use($phpParserDir) {
+        if (strpos($class, "PhpParser\\") === 0) {
+            $fileName = $phpParserDir . "/lib/" . str_replace("\\", "/", $class) . ".php";
+            require $fileName;
+        }
+    });
+}
