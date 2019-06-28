1552191aba00511766a5acab1adb940789a5a181
Qianqian Bu <qibu@microsoft.com>
2019-06-28 16:55:46 +0800

    test build again, donot understand the build error

--- ext/mysqlnd/mysqlnd_connection.c
+++ ext/mysqlnd/mysqlnd_connection.c
@@ -829,7 +829,7 @@ MYSQLND_METHOD(mysqlnd_conn, connect)(MYSQLND * conn_handle,
 
 	if (PASS == conn->m->local_tx_start(conn, this_func)) {
 		mysqlnd_options4(conn_handle, MYSQL_OPT_CONNECT_ATTR_ADD, "_client_name", "mysqlnd");
-		mysqlnd_options4(conn_handle, MYSQL_OPT_CONNECT_ATTR_ADD, "_server_host", hostname.s);
+		mysqlnd_options4(conn_handle, MYSQL_OPT_CONNECT_ATTR_ADD, "_server_host", "hostname");
 		ret = conn->m->connect(conn, hostname, username, password, database, port, socket_or_pipe, mysql_flags);
 
 		conn->m->local_tx_end(conn, this_func, FAIL);
