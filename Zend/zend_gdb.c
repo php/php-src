40eea802b009dcdd36ae60ffedcf937b27bbcd9b
David Carlier <devnexen@gmail.com>
2019-07-06 12:55:56 +0100

    Detecting if we are debugged FreeBSD/MacOS.

--- Zend/zend_gdb.c
+++ Zend/zend_gdb.c
@@ -22,6 +22,10 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#if defined(__APPLE__) || defined(__FreeBSD__)
+#include <sys/sysctl.h>
+#include <sys/user.h>
+#endif
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -105,6 +109,7 @@ ZEND_API void zend_gdb_unregister_all(void)
 ZEND_API int zend_gdb_present(void)
 {
 	int ret = 0;
+#if defined(__linux__)
 	int fd = open("/proc/self/status", O_RDONLY);
 
 	if (fd > 0) {
@@ -136,6 +141,44 @@ ZEND_API int zend_gdb_present(void)
 
 		close(fd);
 	}
+#elif defined(__APPLE__) || defined(__FreeBSD__)
+#if defined(__APPLE__)
+#define KINFO_PROC_FLAG (cproc->kp_proc.p_flag)
+#define KINFO_PROC_PPID (cproc->kp_proc.p_oppid)
+#else
+#define KINFO_PROC_FLAG (cproc->ki_flag)
+#define KINFO_PROC_PPID (cproc->ki_ppid)
+#endif
+	struct kinfo_proc *cproc;
+	size_t cproclen = 0;
+	int mib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()};
+	if (sysctl(mib, 4, NULL, &cproclen, NULL, 0) != 0) {
+		return 0;
+	}
+	cproc = (struct kinfo_proc *)malloc(cproclen);
+	if (sysctl(mib, 4, cproc, &cproclen, NULL, 0) == 0) {
+		if ((KINFO_PROC_FLAG & P_TRACED) != 0) {
+			pid_t ppid = KINFO_PROC_PPID;
+#if defined(__FreeBSD__)
+			char path[PATH_MAX];
+			size_t pathlen = sizeof(path);
+			int smib[4] = {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, ppid};
+			if (sysctl(smib, 4, path, &pathlen, NULL, 0) == 0) {
+				if (strstr(path, "gdb")) {
+					ret = 1;
+				}
+			}
+#else
+			// Does not seem there is a way to get a full path
+			// but only from the current process ...
+			// here considered "gdb-ish"
+			// open to debate if we throw away the mac support altogether...
+			ret = 1;
+#endif
+		}
+	}
+	free(cproc);
+#endif
 
 	return ret;
 }
