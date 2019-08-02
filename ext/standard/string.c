a87ef5e3ddfdb3083c2b62bb25835b74d9a3d083
Christoph M. Becker <cmbecker69@gmx.de>
2019-08-02 17:03:20 +0200

    Fix #78346: strip_tags no longer handling nested php tags
    When the strip tags state machine has been flattened, an if statement
    has mistakenly been treated as else if.  We fix this, and also simplify
    a bit right away.

--- ext/standard/string.c
+++ ext/standard/string.c
@@ -5271,8 +5271,7 @@ state_2:
 				} else if (lc != '\\') {
 					lc = c;
 				}
-			} else {
-				if (p != buf && *(p-1) != '\\' && (!in_q || *p == in_q)) {
+				if (p != buf && (!in_q || *p == in_q)) {
 					if (in_q) {
 						in_q = 0;
 					} else {
