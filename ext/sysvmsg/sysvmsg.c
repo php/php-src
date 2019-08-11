2881655f4989e2eaec5c4fdc15dc14ee4c62683b
Stephen Reay <stephen.reay@me.com>
2019-08-12 00:42:26 +0700

    Added arginfo stubs for sysvmsg

--- ext/sysvmsg/sysvmsg.c
+++ ext/sysvmsg/sysvmsg.c
@@ -24,6 +24,7 @@
 #include "php_globals.h"
 #include "ext/standard/info.h"
 #include "php_sysvmsg.h"
+#include "php_sysvmsg_arginfo.h"
 #include "ext/standard/php_var.h"
 #include "zend_smart_str.h"
 
@@ -61,50 +62,6 @@ struct php_msgbuf {
 
 /* True global resources - no need for thread safety here */
 static int le_sysvmsg;
-
-/* {{{ arginfo */
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_get_queue, 0, 0, 1)
-	ZEND_ARG_INFO(0, key)
-	ZEND_ARG_INFO(0, perms)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_send, 0, 0, 3)
-	ZEND_ARG_INFO(0, queue)
-	ZEND_ARG_INFO(0, msgtype)
-	ZEND_ARG_INFO(0, message)
-	ZEND_ARG_INFO(0, serialize)
-	ZEND_ARG_INFO(0, blocking)
-	ZEND_ARG_INFO(1, errorcode)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_receive, 0, 0, 5)
-	ZEND_ARG_INFO(0, queue)
-	ZEND_ARG_INFO(0, desiredmsgtype)
-	ZEND_ARG_INFO(1, msgtype)
-	ZEND_ARG_INFO(0, maxsize)
-	ZEND_ARG_INFO(1, message)
-	ZEND_ARG_INFO(0, unserialize)
-	ZEND_ARG_INFO(0, flags)
-	ZEND_ARG_INFO(1, errorcode)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_remove_queue, 0, 0, 1)
-	ZEND_ARG_INFO(0, queue)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_stat_queue, 0, 0, 1)
-	ZEND_ARG_INFO(0, queue)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_set_queue, 0, 0, 2)
-	ZEND_ARG_INFO(0, queue)
-	ZEND_ARG_INFO(0, data)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO_EX(arginfo_msg_queue_exists, 0, 0, 1)
-	ZEND_ARG_INFO(0, key)
-ZEND_END_ARG_INFO()
-/* }}} */
 
 /* {{{ sysvmsg_functions[]
  *
