127b34c47544f8c082824bf1e26720d5ac4acd18
Stephen Reay <stephen.reay@me.com>
2019-08-11 23:21:27 +0700

    Updated readline arginfo stubs

--- ext/readline/readline.stub.php
+++ ext/readline/readline.stub.php
@@ -18,35 +18,19 @@ function readline_read_history(string $filename = null): bool {}
 
 function readline_write_history(string $filename = null): bool {}
 
-function readline_completion_function(callable $funcname): ?string {}
-
-
-//ZEND_BEGIN_ARG_INFO_EX(arginfo_readline_completion_function, 0, 0, 1)
-//	ZEND_ARG_INFO(0, funcname)
-//ZEND_END_ARG_INFO()
+function readline_completion_function(callable $funcname): bool {}
 
 
 #if HAVE_RL_CALLBACK_READ_CHAR
-//ZEND_BEGIN_ARG_INFO_EX(arginfo_readline_callback_handler_install, 0, 0, 2)
-//	ZEND_ARG_INFO(0, prompt)
-//	ZEND_ARG_INFO(0, callback)
-//ZEND_END_ARG_INFO()
+function readline_callback_handler_install(string $prompt, callable $callback): bool {}
 
+function readline_callback_read_char(): void {}
 
-//ZEND_BEGIN_ARG_INFO(arginfo_readline_callback_read_char, 0)
-//ZEND_END_ARG_INFO()
+function readline_callback_handler_remove(): bool {}
 
-
-//ZEND_BEGIN_ARG_INFO(arginfo_readline_callback_handler_remove, 0)
-//ZEND_END_ARG_INFO()
-
-
-//ZEND_BEGIN_ARG_INFO(arginfo_readline_redisplay, 0)
-//ZEND_END_ARG_INFO()
-
+function readline_redisplay(): void {}
 
 #if HAVE_RL_ON_NEW_LINE
-//ZEND_BEGIN_ARG_INFO(arginfo_readline_on_new_line, 0)
-//ZEND_END_ARG_INFO()
+function readline_on_new_line(): void {}
 #endif
 #endif
