f1e43fa7e6db0eb57a6703e7391bab64505e4485
Victor Gazotti <vtr.gomes@hotmail.com>
2019-08-13 17:26:26 -0300

    autogenerating arginfo

--- ext/ctype/ctype.stubs.php_arginfo.h
+++ ext/ctype/ctype.stubs.php_arginfo.h
@@ -1,6 +1,6 @@
 /* This is a generated file, edit the .stub.php file instead. */
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_ctype_alnum, 0, 0, 1)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_ctype_alnum, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, text, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
