b951aebded384d71e256b6bd05d748f9123e0c64
Victor Gazotti <vtr.gomes@hotmail.com>
2019-08-13 15:48:21 -0300

    Create ctype.stubs.php

---
+++ ext/ctype/ctype.stubs.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @return bool */
+function ctype_alnum(string $text) {}
+
+/** @return bool */
+function ctype_alpha(string $text) {}
+
+/** @return bool */
+function ctype_cntrl(string $text) {}
+
+/** @return bool */
+function ctype_digit(string $text) {}
+
+/** @return bool */
+function ctype_lower(string $text) {}
+
+/** @return bool */
+function ctype_graph(string $text) {}
+
+/** @return bool */
+function ctype_print(string $text) {}
+
+/** @return bool */
+function ctype_punct(string $text) {}
+
+/** @return bool */
+function ctype_space(string $text) {}
+
+/** @return bool */
+function ctype_upper(string $text) {}
+
+/** @return bool */
+function ctype_xdigit(string $text) {}
