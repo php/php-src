87c870dcb2c1ba5146b532c97e59195ebd6aadb4
Victor Gazotti <vtr.gomes@hotmail.com>
2019-08-13 15:50:28 -0300

    Create ctype.stubs.php_arginfo.h

---
+++ ext/ctype/ctype.stubs.php_arginfo.h
@@ -0,0 +1,25 @@
+/* This is a generated file, edit the .stub.php file instead. */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_ctype_alnum, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, text, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_ctype_alpha arginfo_ctype_alnum
+
+#define arginfo_ctype_cntrl arginfo_ctype_alnum
+
+#define arginfo_ctype_digit arginfo_ctype_alnum
+
+#define arginfo_ctype_lower arginfo_ctype_alnum
+
+#define arginfo_ctype_graph arginfo_ctype_alnum
+
+#define arginfo_ctype_print arginfo_ctype_alnum
+
+#define arginfo_ctype_punct arginfo_ctype_alnum
+
+#define arginfo_ctype_space arginfo_ctype_alnum
+
+#define arginfo_ctype_upper arginfo_ctype_alnum
+
+#define arginfo_ctype_xdigit arginfo_ctype_alnum
