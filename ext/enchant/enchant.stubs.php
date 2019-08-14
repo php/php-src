d49811592a4a5a27de0d5cc29b4bf62178420898
Victor Gomes Clube <victor@debian>
2019-08-14 11:38:55 -0300

    changing string to resource at enchant functions

--- ext/enchant/enchant.stubs.php
+++ ext/enchant/enchant.stubs.php
@@ -3,25 +3,41 @@
 /** @return resource|false */
 function enchant_broker_init() {}
 
-function enchant_broker_free(string $broker): bool {}
+/** @resource $broker */
+function enchant_broker_free($broker): bool {}
 
-function enchant_broker_set_dict_path(string $broker, int $dict_type, string $value): bool {}
+/** @resource $broker */
+function enchant_broker_set_dict_path($broker, int $dict_type, string $value): bool {}
 
-/** @return string|false */
-function enchant_broker_get_dict_path(string $broker, int $dict_type) {}
+/**
+ * @param resource $broker
+ * @return string|false
+ */
+function enchant_broker_get_dict_path($broker, int $dict_type) {}
 
-/** @return resource|false */
-function enchant_broker_request_dict(string $broker, string $tag) {}
+/**
+ * @param resource $broker
+ * @return resource|false
+ */
+function enchant_broker_request_dict($broker, string $tag) {}
 
-/** @return resource|false */
-function enchant_broker_request_pwl_dict(string $broker, string $filename) {}
+/**
+ * @param resource $broker
+ * @return resource|false
+ */
+function enchant_broker_request_pwl_dict($broker, string $filename) {}
 
-function enchant_broker_free_dict(string $dict): bool {}
+/** @resource $dict */
+function enchant_broker_free_dict($dict): bool {}
 
-function enchant_broker_set_ordering(string $broker, string $tag, string $ordering): bool {}
+/** @resource $broker */
+function enchant_broker_set_ordering($broker, string $tag, string $ordering): bool {}
 
-function enchant_dict_quick_check(string $dict, string $word, array $suggestions = null): bool {}
+/** @resource $dict */
+function enchant_dict_quick_check($dict, string $word, array $suggestions = null): bool {}
 
-function enchant_dict_check(string $dict, string $word): bool {}
+/** @resource $dict */
+function enchant_dict_check($dict, string $word): bool {}
 
-function enchant_dict_store_replacement(string $dict, string $mis, string $cor): void {}
+/** @resource $dict */
+function enchant_dict_store_replacement($dict, string $mis, string $cor): void {}
