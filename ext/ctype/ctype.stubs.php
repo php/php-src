3e6ead761fe5b9faab949519c2e9306f67ff089d
Victor Gazotti <vtr.gomes@hotmail.com>
2019-08-14 09:32:33 -0300

    Changing string to allow any type at $text

--- ext/ctype/ctype.stubs.php
+++ ext/ctype/ctype.stubs.php
@@ -1,23 +1,23 @@
 <?php
 
-function ctype_alnum(string $text): bool {}
+function ctype_alnum($text): bool {}
 
-function ctype_alpha(string $text): bool {}
+function ctype_alpha($text): bool {}
 
-function ctype_cntrl(string $text): bool {}
+function ctype_cntrl($text): bool {}
 
-function ctype_digit(string $text): bool {}
+function ctype_digit($text): bool {}
 
-function ctype_lower(string $text): bool {}
+function ctype_lower($text): bool {}
 
-function ctype_graph(string $text): bool {}
+function ctype_graph($text): bool {}
 
-function ctype_print(string $text): bool {}
+function ctype_print($text): bool {}
 
-function ctype_punct(string $text): bool {}
+function ctype_punct($text): bool {}
 
-function ctype_space(string $text): bool {}
+function ctype_space($text): bool {}
 
-function ctype_upper(string $text): bool {}
+function ctype_upper($text): bool {}
 
-function ctype_xdigit(string $text): bool {}
+function ctype_xdigit($text): bool {}
