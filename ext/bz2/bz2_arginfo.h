aa1c111a381fded497d8ac63464874f89f4b1332
Kauan Sousa <kauanslr.ks@gmail.com>
2019-08-15 15:01:16 -0300

    arginfo definitions

--- ext/bz2/bz2_arginfo.h
+++ ext/bz2/bz2_arginfo.h
@@ -1,19 +1,19 @@
 /* This is a generated file, edit the .stub.php file instead. */
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_bzopen, 0, 0, 2)
-	ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+	ZEND_ARG_INFO(0, file)
 	ZEND_ARG_TYPE_INFO(0, mode, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_bzread, 0, 0, 1)
 	ZEND_ARG_INFO(0, bz)
-	ZEND_ARG_INFO(0, length)
+	ZEND_ARG_TYPE_INFO(0, length, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_bzwrite, 0, 0, 2)
 	ZEND_ARG_INFO(0, bz)
-	ZEND_ARG_INFO(0, data)
-	ZEND_ARG_INFO(0, length)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, length, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_bzflush, 0, 1, _IS_BOOL, 0)
