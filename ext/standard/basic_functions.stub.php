a4ecbce469de343c65ef4f88816a113448038ef7
Theodore Brown <theodorejb@outlook.com>
2019-08-15 15:10:46 -0500

    Convert remaining array function arginfo to PHP stubs

--- ext/standard/basic_functions.stub.php
+++ ext/standard/basic_functions.stub.php
@@ -79,6 +79,161 @@ function sort(array &$arg, int $sort_flags = SORT_REGULAR): bool {}
 
 function rsort(array &$arg, int $sort_flags = SORT_REGULAR): bool {}
 
+function usort(array &$arg, callable $cmp_function): bool {}
+
+function uasort(array &$arg, callable $cmp_function): bool {}
+
+function uksort(array &$arg, callable $cmp_function): bool {}
+
+function end(array &$arg) {}
+
+function prev(array &$arg) {}
+
+function next(array &$arg) {}
+
+function reset(array &$arg) {}
+
+function current(array $arg) {}
+
+/** @return int|string|null */
+function key(array $arg) {}
+
+function min(...$args) {}
+
+function max(...$args) {}
+
+function array_walk(array &$input, callable $funcname, $userdata = null): bool {}
+
+function array_walk_recursive(array &$input, callable $funcname, $userdata = null): bool {}
+
+function in_array($needle, array $haystack, bool $strict = false): bool {}
+
+/** @return int|string|false */
+function array_search($needle, array $haystack, bool $strict = false) {}
+
+function extract(array &$arg, int $extract_type = EXTR_OVERWRITE, string $prefix = null): int {}
+
+function compact(...$var_names): array {}
+
+/** @return array|false */
+function array_fill(int $start_key, int $num, $val) {}
+
+function array_fill_keys(array $keys, $val): array {}
+
+/**
+ * @param int|float|string $low
+ * @param int|float|string $high
+ * @param int|float $step
+ * @return array|false
+ */
+function range($low, $high, $step = 1) {}
+
+function shuffle(array &$arg): bool {}
+
+function array_pop(array &$stack) {}
+
+function array_shift(array &$stack) {}
+
+function array_unshift(array &$stack, ...$vars): int {}
+
+function array_splice(array &$arg, int $offset, int $length = UNKNOWN, $replacement = []): array {}
+
+function array_slice(array $arg, int $offset, ?int $length = null, bool $preserve_keys = false): array {}
+
+function array_merge(array ...$arrays): array {}
+
+function array_merge_recursive(array ...$arrays): array {}
+
+function array_replace(array $arr1, array ...$arrays): array {}
+
+function array_replace_recursive(array $arr1, array ...$arrays): array {}
+
+function array_keys(array $arg, $search_value = UNKNOWN, bool $strict = false): array {}
+
+/** @return int|string|null */
+function array_key_first(array $arg) {}
+
+/** @return int|string|null */
+function array_key_last(array $arg) {}
+
+function array_values(array $arg): array {}
+
+function array_count_values(array $arg): array {}
+
+/**
+ * @param int|string|null $column_key
+ * @param int|string $index_key
+ * @return array|false
+ */
+function array_column(array $arg, $column_key, $index_key = null) {}
+
+function array_reverse(array $input, bool $preserve_keys = false): array {}
+
+/** @return array|false */
+function array_pad(array $arg, int $pad_size, $pad_value) {}
+
+function array_flip(array $arg): array {}
+
+function array_change_key_case(array $input, int $case = CASE_LOWER): array {}
+
+function array_unique(array $arg, int $flags = SORT_STRING): array {}
+
+function array_intersect_key(array $arr1, array ...$arrays): array {}
+
+function array_intersect_ukey(array $arr1, array ...$arr2, callable $callback_key_compare_func): array {}
+
+function array_intersect(array $arr1, array ...$arrays): array {}
+
+function array_uintersect(array $arr1, array ...$arr2, callable $callback_data_compare_func): array {}
+
+function array_intersect_assoc(array $arr1, array ...$arrays): array {}
+
+function array_uintersect_assoc(array $arr1, array ...$arr2, callable $callback_data_compare_func): array {}
+
+function array_intersect_uassoc(array $arr1, array ...$arr2, callable $callback_key_compare_func): array {}
+
+function array_uintersect_uassoc(array $arr1, array ...$arr2, callable $callback_data_compare_func, callable $callback_key_compare_func): array {}
+
+function array_diff_key(array $arr1, array ...$arrays): array {}
+
+function array_diff_ukey(array $arr1, array ...$arr2, callable $callback_key_comp_func): array {}
+
+function array_diff(array $arr1, array ...$arrays): array {}
+
+function array_udiff(array $arr1, array ...$arr2, callable $callback_data_comp_func): array {}
+
+function array_diff_assoc(array $arr1, array ...$arrays): array {}
+
+function array_diff_uassoc(array $arr1, array ...$arr2, callable $callback_data_comp_func): array {}
+
+function array_udiff_assoc(array $arr1, array ...$arr2, callable $callback_key_comp_func): array {}
+
+function array_udiff_uassoc(array $arr1, array ...$arr2, callable $callback_data_comp_func, callable $callback_key_comp_func): array {}
+
+function array_multisort(array &$arr1, $sort_order = SORT_ASC, $sort_flags = SORT_REGULAR, &...$arr2): bool {}
+
+function array_rand(array $arg, int $num_req = 1): ?array {}
+
+/** @return int|float */
+function array_sum(array $arg) {}
+
+/** @return int|float */
+function array_product(array $arg) {}
+
+function array_reduce(array $arg, callable $callback, $initial = null) {}
+
+function array_filter(array $arg, callable $callback, int $use_keys = 0): array {}
+
+function array_map(callable $callback, array ...$arrays): array {}
+
+/** @param int|string $key */
+function array_key_exists($key, array $search): bool {}
+
+function array_chunk(array $arg, int $size, bool $preserve_keys = false): ?array {}
+
+/** @return array|false */
+function array_combine(array $keys, array $values) {}
+
 /* base64.c */
 
 function base64_encode(string $str): string {}
