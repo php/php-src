90317ea8690b2f074fce100ad424ab54f7e00bdd
Victor Gazotti <victor@victor-note.victor>
2019-08-15 00:02:51 -0300

    changing name of dict_type var and referencing suggestions param

--- ext/enchant/enchant.stubs.php
+++ ext/enchant/enchant.stubs.php
@@ -7,13 +7,13 @@ function enchant_broker_init() {}
 function enchant_broker_free($broker): bool {}
 
 /** @resource $broker */
-function enchant_broker_set_dict_path($broker, int $dict_type, string $value): bool {}
+function enchant_broker_set_dict_path($broker, int $name, string $value): bool {}
 
 /**
  * @param resource $broker
  * @return string|false
  */
-function enchant_broker_get_dict_path($broker, int $dict_type) {}
+function enchant_broker_get_dict_path($broker, int $name) {}
 
 /**
  * @param resource $broker
@@ -34,7 +34,7 @@ function enchant_broker_free_dict($dict): bool {}
 function enchant_broker_set_ordering($broker, string $tag, string $ordering): bool {}
 
 /** @resource $dict */
-function enchant_dict_quick_check($dict, string $word, array $suggestions = null): bool {}
+function enchant_dict_quick_check($dict, string $word, &$suggestions = UNKNOWN): bool {}
 
 /** @resource $dict */
 function enchant_dict_check($dict, string $word): bool {}
