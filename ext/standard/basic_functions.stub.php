8150c65ae4f72545eac40d0374e1d34f215f27bc
inem0o <cuneaz.leo@gmail.com>
2019-08-19 10:18:06 +0200

    Add pageinfo arginfo stubs

--- ext/standard/basic_functions.stub.php
+++ ext/standard/basic_functions.stub.php
@@ -116,6 +116,22 @@ function md5(string $str, bool $raw_output = false): string {}
 /** @return string|false */
 function md5_file(string $filename, bool $raw_output = false) {}
 
+/* pageinfo.c */
+
+/** @return int|false */
+function getmyuid() {}
+
+/** @return int|false */
+function getmygid() {}
+
+/** @return int|false */
+function getmypid() {}
+
+/** @return int|false */
+function getmyinode() {}
+
+function getlastmod(): int {}
+
 /* syslog.c */
 
 #ifdef HAVE_SYSLOG_H
