70fa43edbe50d136d1f99182f897ee5ca8e7c8e0
Thomas Gerbet <thomas@gerbet.me>
2019-08-21 21:32:57 +0200

    Add stub for ext zlib

---
+++ ext/zlib/zlib_arginfo.h
@@ -0,0 +1,116 @@
+/* This is a generated file, edit the .stub.php file instead. */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_ob_gzhandler, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, flags, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_zlib_get_coding_type, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzfile, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, use_include_path, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzopen, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, filename, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, mode, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, use_include_path, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_readgzfile arginfo_gzfile
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_zlib_encode, 0, 0, 2)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, encoding, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, level, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_zlib_decode, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, max_decoded_len, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzdeflate, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, level, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, encoding, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gzencode arginfo_gzdeflate
+
+#define arginfo_gzcompress arginfo_gzdeflate
+
+#define arginfo_gzinflate arginfo_zlib_decode
+
+#define arginfo_gzdecode arginfo_zlib_decode
+
+#define arginfo_gzuncompress arginfo_zlib_decode
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzwrite, 0, 0, 2)
+	ZEND_ARG_INFO(0, fp)
+	ZEND_ARG_TYPE_INFO(0, str, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, length, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gzputs arginfo_gzwrite
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gzrewind, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_INFO(0, fp)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gzclose arginfo_gzrewind
+
+#define arginfo_gzeof arginfo_gzrewind
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzgetc, 0, 0, 1)
+	ZEND_ARG_INFO(0, fp)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gzpassthru, 0, 1, IS_LONG, 0)
+	ZEND_ARG_INFO(0, fp)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_gzseek, 0, 2, IS_LONG, 0)
+	ZEND_ARG_INFO(0, fp)
+	ZEND_ARG_TYPE_INFO(0, offset, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, whence, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_gztell arginfo_gzgetc
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzread, 0, 0, 2)
+	ZEND_ARG_INFO(0, fp)
+	ZEND_ARG_TYPE_INFO(0, length, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_gzgets, 0, 0, 1)
+	ZEND_ARG_INFO(0, fp)
+	ZEND_ARG_TYPE_INFO(0, length, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_deflate_init, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, encoding, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, options, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_deflate_add, 0, 0, 2)
+	ZEND_ARG_INFO(0, resource)
+	ZEND_ARG_TYPE_INFO(0, add, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, flush_behavior, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_inflate_init arginfo_deflate_init
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_inflate_add, 0, 0, 2)
+	ZEND_ARG_INFO(0, context)
+	ZEND_ARG_TYPE_INFO(0, encoded_data, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, flush_mode, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_inflate_get_status, 0, 0, 1)
+	ZEND_ARG_INFO(0, resource)
+ZEND_END_ARG_INFO()
+
+#define arginfo_inflate_get_read_len arginfo_inflate_get_status
