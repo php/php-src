481520d3819b0b68b65539ff59b4bd2f018d6e5f
Joe Watkins <krakjoe@php.net>
2019-08-28 09:06:38 +0200

    set versions for release

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 31
+#define PHP_RELEASE_VERSION 32
 #define PHP_EXTRA_VERSION ""
-#define PHP_VERSION "7.1.31"
-#define PHP_VERSION_ID 70131
+#define PHP_VERSION "7.1.32"
+#define PHP_VERSION_ID 70132
