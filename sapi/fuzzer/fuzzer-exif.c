872a759f88f258bd9b331d36629796b551588d1c
Nikita Popov <nikita.ppv@gmail.com>
2019-10-01 18:29:23 +0200

    Limit input size in exif fuzzer
    Probably still too much...

--- sapi/fuzzer/fuzzer-exif.c
+++ sapi/fuzzer/fuzzer-exif.c
@@ -35,6 +35,12 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 	char *filename;
 	int filedes;
 
+	if (Size > 256 * 1024) {
+		/* Large inputs have a large impact on fuzzer performance,
+		 * but are unlikely to be necessary to reach new codepaths. */
+		return 0;
+	}
+
 	if (fuzzer_request_startup() == FAILURE) {
 		return 0;
 	}
