c81f0380b728532eddf3bc596aec59d459b21b7f
Simonov Denis <sim-mail@list.ru>
2019-10-04 18:49:12 +0300

    pad space

--- ext/pdo_firebird/firebird_statement.c
+++ ext/pdo_firebird/firebird_statement.c
@@ -372,7 +372,7 @@ static int firebird_stmt_get_col(pdo_stmt_t *stmt, int colno, char **ptr,  /* {{
 
 			*ptr = FETCH_BUF(S->fetch_buf[colno], char, CHAR_BUF_LEN, NULL);
 
-			if((var->sqltype & ~1)==SQL_DOUBLE) {
+			if ((var->sqltype & ~1) == SQL_DOUBLE) {
 				*len = slprintf(*ptr, CHAR_BUF_LEN, "%.*F", -var->sqlscale, *(double*)var->sqldata);
 			} else if (n >= 0) {
 				*len = slprintf(*ptr, CHAR_BUF_LEN, "%" LL_MASK "d.%0*" LL_MASK "d",
