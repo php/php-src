15c5905f47b2122a9723700b057d97f876fa3152
Nikita Popov <nikita.ppv@gmail.com>
2019-10-25 14:25:33 +0200

    Setup mysql

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo '
 <?php $conn_str .= " user=postgres"; ?>' >> "./ext/pgsql/tests/config.inc"
-psql -c 'create database test;' -U postgres
+if [ -z "$ARM64" ]; then
+  psql -c 'create database test;' -U postgres
+fi
