e4593c51ff95f4716653f538781d0b23e1553d5a
Rimvydas Zilinskas <rimvydas@loftdigital.com>
2019-10-30 11:20:48 +0100

    Add more openssl stubs
    Co-authored-by: Christoph M. Becker <cmbecker69@gmx.de>

--- ext/openssl/openssl.stub.php
+++ ext/openssl/openssl.stub.php
@@ -22,3 +22,82 @@ function openssl_x509_read($cert) {}
 
 /** @return false|void */
 function openssl_x509_free($x509) {}
+
+/**
+ * @param resource|string $x509cert
+ * @param resource|string|array $priv_key
+ */
+function openssl_pkcs12_export_to_file($x509cert, string $filename, $priv_key, string $pass, array $args = UNKNOWN): bool {}
+
+/**
+ * @param resource|string $x509
+ * @param resource|string|array $priv_key
+ */
+function openssl_pkcs12_export($x509 , &$out, $priv_key, string $pass, array $args = UNKNOWN): bool {}
+
+function openssl_pkcs12_read(string $pkcs12, &$certs, string $pass): bool {}
+
+/** @param resource $csr */
+function openssl_csr_export_to_file($csr, string $outfilename, bool $notext = true): bool {}
+
+/** @param resource $csr */
+function openssl_csr_export($csr, &$out, bool $notext = true): bool {}
+
+/**
+ * @param resource|string $csr
+ * @param resource|string $cacert
+ * @param resource|string|array $priv_key
+ * @return resource|false
+ */
+function openssl_csr_sign($csr, $cacert = null, $priv_key, int $days, ?array $config_args = null, int $serial = 0) {}
+
+/** @return resource|false */
+function openssl_csr_new(array $dn, &$privkey, ?array $configargs = null, ?array $extraattribs = null) {}
+
+/**
+ * @param resource|string $csr
+ * @return array|false
+ */
+function openssl_csr_get_subject($csr, bool $use_shortnames = true) {}
+
+/**
+ * @param resource|string $csr
+ * @return resource|false
+ */
+function openssl_csr_get_public_key($csr, bool $use_shortnames = true) {}
+
+/** @return resource|false */
+function openssl_pkey_new(?array $configargs = null) {}
+
+/** @param resource|string|array $key */
+function openssl_pkey_export_to_file($key, string $outfilename, ?string $passphrase = null, ?array $configargs = null): bool {}
+
+/** @param resource|string|array $key */
+function openssl_pkey_export($key, &$out, ?string $passphrase = null, ?array $configargs = null): bool {}
+
+/** 
+ * @param resource|string|array $cert
+ * @return resource|false
+ */
+function openssl_pkey_get_public($cert) {}
+
+/** @param resource $key */
+function openssl_pkey_free($key): void {}
+
+/**
+ * @param resource|string|array $key
+ * @return resource|false
+ */
+function openssl_pkey_get_private($key, string $passphrase = UNKNOWN) {}
+
+/**
+ * @param resource $key
+ * @return array|false
+ */
+function openssl_pkey_get_details($key) {}
+
+/** @return string|false */
+function openssl_pbkdf2(string $password, string $salt, int $key_length, int $iterations, string $digest_algorithm = 'sha1') {}
+
+/** @return bool|int */
+function openssl_pkcs7_verify(string $filename, int $flags, string $signerscerts = UNKNOWN, array $cainfo = UNKNOWN, string $extracerts = UNKNOWN, string $content = UNKNOWN, string $pk7 = UNKNOWN) {}
