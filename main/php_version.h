4c9ba3e0428693a89cc4beff5c8773e97075880a
Christoph M. Becker <cmbecker69@gmx.de>
2019-11-05 18:17:04 +0100

    7.3.13 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 12
+#define PHP_RELEASE_VERSION 13
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.12-dev"
-#define PHP_VERSION_ID 70312
+#define PHP_VERSION "7.3.13-dev"
+#define PHP_VERSION_ID 70313
