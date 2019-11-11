dccd137e1a77d9ea04c2a0bb67eb1e14cd992b37
Christoph M. Becker <cmbecker69@gmx.de>
2019-11-11 15:39:52 +0100

    iconv_strlen() cannot return a string

--- ext/iconv/iconv.stub.php
+++ ext/iconv/iconv.stub.php
@@ -1,6 +1,6 @@
 <?php
 
-function iconv_strlen(string $str, string $charset = UNKNOWN): string|int|false {}
+function iconv_strlen(string $str, string $charset = UNKNOWN): int|false {}
 
 function iconv_substr(string $str, int $offset, ?int $length = null, string $charset = UNKNOWN): string|false {}
 
