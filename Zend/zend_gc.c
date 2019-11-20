266e71c621d37a3567a0ef0bbaf97955ab71fc56
Tyson Andre <tysonandre775@hotmail.com>
2019-11-20 17:52:42 -0500

    [ci skip]: Add space in comment

--- Zend/zend_gc.c
+++ Zend/zend_gc.c
@@ -1561,7 +1561,7 @@ ZEND_API int zend_gc_collect_cycles(void)
 					EG(objects_store).object_buckets[obj->handle] = SET_OBJ_INVALID(obj);
 					GC_TYPE_INFO(obj) = IS_NULL |
 						(GC_TYPE_INFO(obj) & ~GC_TYPE_MASK);
-					/* Modify current before calling free_obj(bug #78811: free_obj() can cause the root buffer (with current) to be reallocated.) */
+					/* Modify current before calling free_obj (bug #78811: free_obj() can cause the root buffer (with current) to be reallocated.) */
 					current->ref = GC_MAKE_GARBAGE(((char*)obj) - obj->handlers->offset);
 					if (!(OBJ_FLAGS(obj) & IS_OBJ_FREE_CALLED)) {
 						GC_ADD_FLAGS(obj, IS_OBJ_FREE_CALLED);
