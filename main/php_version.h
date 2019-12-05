eeb69ac7a9d327fdd3c6bbcb075f7db1c1a5b8d6
Joe Watkins <krakjoe@php.net>
2019-12-05 06:50:59 +0100

    fix version for sake of history ... goodbye 7.1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 34
-#define PHP_EXTRA_VERSION "dev"
-#define PHP_VERSION "7.1.34"
+#define PHP_EXTRA_VERSION "-dev"
+#define PHP_VERSION "7.1.34-dev"
 #define PHP_VERSION_ID 70134
