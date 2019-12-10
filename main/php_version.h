4f491838d832ece4e90d3d639cc72e5852001073
Derick Rethans <github@derickrethans.nl>
2019-12-10 16:40:43 +0000

    Update versions for PHP 7.4.1RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.0-dev"
-#define PHP_VERSION_ID 70400
+#define PHP_RELEASE_VERSION 1
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.4.1RC1"
+#define PHP_VERSION_ID 70401
