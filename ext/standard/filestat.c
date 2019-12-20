46daa99903cf4f398f9fcb659ca4fbebf91ccb69
GITSRC <34047788+gitsrc@users.noreply.github.com>
2019-12-20 11:07:47 +0800

    Try different expressions to calculate
    Try different expressions to calculate

--- ext/standard/filestat.c
+++ ext/standard/filestat.c
@@ -216,9 +216,8 @@ static int php_disk_free_space(char *path, double *space) /* {{{ */
 		PHP_WIN32_IOUTIL_CLEANUP_W()
 		return FAILURE;
 	}
-
-	/* i know - this is ugly, but i works <thies@thieso.net> */
-	*space = FreeBytesAvailableToCaller.HighPart * (double) (((zend_ulong)1) << 31) * 2.0 + FreeBytesAvailableToCaller.LowPart;
+	
+	*space = FreeBytesAvailableToCaller.HighPart * (double) ((zend_ulong)1 << 32)  + FreeBytesAvailableToCaller.LowPart;
 
 	PHP_WIN32_IOUTIL_CLEANUP_W()
 
