b3f17ea5d55f5c541f07bf28fda95d54bbf958e0
George Peter Banyard <girgias@php.net>
2020-02-04 22:25:43 +0100

    Promote invalid mode/unknow option from Warning to ValueError for plain streams.
    Closes GH-5076

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -1029,7 +1029,7 @@ PHPAPI php_stream *_php_stream_fopen(const char *filename, const char *mode, zen
 
 	if (FAILURE == php_stream_parse_fopen_modes(mode, &open_flags)) {
 		if (options & REPORT_ERRORS) {
-			php_error_docref(NULL, E_WARNING, "`%s' is not a valid mode for fopen", mode);
+			zend_value_error("`%s' is not a valid mode for fopen", mode);
 		}
 		return NULL;
 	}
@@ -1464,7 +1464,7 @@ static int php_plain_files_metadata(php_stream_wrapper *wrapper, const char *url
 			ret = VCWD_CHMOD(url, mode);
 			break;
 		default:
-			php_error_docref1(NULL, url, E_WARNING, "Unknown option %d for stream_metadata", option);
+			zend_value_error("Unknown option %d for stream_metadata", option);
 			return 0;
 	}
 	if (ret == -1) {
