3e2524b5da84e239af29e25a30599afcff6b8b6a
Máté Kocsis <kocsismate@woohoolabs.com>
2020-02-28 15:34:20 +0100

    Fix code review comments

--- ext/intl/uchar/uchar.stub.php
+++ ext/intl/uchar/uchar.stub.php
@@ -3,25 +3,25 @@
 class IntlChar
 {
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function hasBinaryProperty($codepoint, int $property) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return array|null
      */
     public static function charAge($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function charDigitValue($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function charDirection($codepoint) {}
@@ -30,80 +30,76 @@ class IntlChar
     public static function charFromName(string $characterName, int $nameChoice = IntlChar::UNICODE_CHAR_NAME) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|string|null
      */
     public static function charMirror($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return string|null
      */
     public static function charName($codepoint, int $nameChoice = IntlChar::UNICODE_CHAR_NAME) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function charType($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return string|null
      */
     public static function chr($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|false|null
      */
     public static function digit($codepoint, int $radix = 10) {}
 
     /**
-     * @param mixed $start
-     * @param mixed $limit
-     * @param callable $callback
+     * @param int|string $start
+     * @param int|string $limit
      * @return bool|null
      */
-    public static function enumCharNames($start, $limit, $callback, int $nameChoice = IntlChar::UNICODE_CHAR_NAME) {}
+    public static function enumCharNames($start, $limit, callable $callback, int $nameChoice = IntlChar::UNICODE_CHAR_NAME) {}
+
+    /** @return void */
+    public static function enumCharTypes(callable $callback) {}
 
     /**
-     * @param callable $callable
-     * @return void
+     * @param int|string $codepoint
+     * @return int|string|null
      */
-    public static function enumCharTypes($callable) {}
-
-    /**
-     * @param mixed $codepoint
-     * @return string|int|null
-     */
-    public static function foldCase($codepoint, int $options = IntlChar::FOLD_CASE_DEFAULT ) {}
+    public static function foldCase($codepoint, int $options = IntlChar::FOLD_CASE_DEFAULT) {}
 
     /** @return int */
     public static function forDigit(int $digit, $radix = 10) {}
 
 #if U_ICU_VERSION_MAJOR_NUM >= 52
     /**
-     * @param mixed $codepoint
-     * @return string|int|null
+     * @param int|string $codepoint
+     * @return int|string|null
      */
     public static function getBidiPairedBracket($codepoint) {}
 #endif
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function getBlockCode($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function getCombiningClass($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return string|false|null
      */
     public static function getFC_NFKC_Closure($codepoint) {}
@@ -115,13 +111,13 @@ class IntlChar
     public static function getIntPropertyMinValue(int $property) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return int|null
      */
     public static function getIntPropertyValue($codepoint, int $property) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return float|null
      */
     public static function getNumericValue($codepoint) {}
@@ -145,194 +141,194 @@ class IntlChar
     public static function getUnicodeVersion() {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isalnum($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isalpha($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isbase($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isblank($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function iscntrl($codepoint ) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isdefined($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isdigit($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isgraph($codepoint ) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isIDIgnorable($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isIDPart($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isIDStart($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isISOControl($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isJavaIDPart($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isJavaIDStart($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isJavaSpaceChar($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function islower($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isMirrored($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isprint($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function ispunct($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isspace($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function istitle($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isUAlphabetic($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isULowercase($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isupper($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isUUppercase($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isUWhiteSpace($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isWhitespace($codepoint) {}
 
     /**
-     * @param mixed $codepoint
+     * @param int|string $codepoint
      * @return bool|null
      */
     public static function isxdigit($codepoint) {}
 
     /**
-     * @param mixed $character
+     * @param int|string $character
      * @return int|null
      */
     public static function ord($character) {}
 
     /**
-     * @param mixed $codepoint
-     * @return string|int|null
+     * @param int|string $codepoint
+     * @return int|string|null
      */
     public static function tolower($codepoint) {}
 
     /**
      * @param mixed $codepoint
-     * @return string|int|null
+     * @return int|string|null
      */
     public static function totitle($codepoint) {}
 
     /**
-     * @param mixed $codepoint
-     * @return string|int|null
+     * @param int|string $codepoint
+     * @return int|string|null
      */
     public static function toupper($codepoint) {}
 }
