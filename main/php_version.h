9dda3b9eb2d47bda7805f02c417ead54487d655e
Christoph M. Becker <cmbecker69@gmx.de>
2020-03-03 10:19:31 +0100

    Next is 7.3.17

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 16
+#define PHP_RELEASE_VERSION 17
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.16-dev"
-#define PHP_VERSION_ID 70316
+#define PHP_VERSION "7.3.17-dev"
+#define PHP_VERSION_ID 70317
