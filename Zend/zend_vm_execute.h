760faa12b20db56bd3e2faab8d658f622af7da6a
Nikita Popov <nikita.ppv@gmail.com>
2020-03-09 14:59:59 +0100

    Fixed bug #79357
    Peculiarly, for once the cause was not SOAPs "interesting" error
    handling, but a bug in the call trampoline for internal functions...

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -2806,10 +2806,10 @@ static ZEND_OPCODE_HANDLER_RET ZEND_FASTCALL ZEND_CALL_TRAMPOLINE_SPEC_HANDLER(Z
 		}
 
 		if (ret == NULL) {
-			ZVAL_NULL(&retval);
 			ret = &retval;
 		}
 
+		ZVAL_NULL(ret);
 		if (!zend_execute_internal) {
 			/* saves one function call if zend_execute_internal is not used */
 			fbc->internal_function.handler(call, ret);
