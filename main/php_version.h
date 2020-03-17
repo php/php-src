3072b77c215409fa20f925728ac3059ba6e13484
Remi Collet <remi@php.net>
2020-03-17 11:28:34 +0100

    bump verison to 7.2.30-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 29
+#define PHP_RELEASE_VERSION 30
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.2.29-dev"
-#define PHP_VERSION_ID 70229
+#define PHP_VERSION "7.2.30-dev"
+#define PHP_VERSION_ID 70230
