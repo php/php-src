c033d4495048aec8c2254015348ec817569ee326
Máté Kocsis <kocsismate@woohoolabs.com>
2020-03-25 09:24:52 +0100

    Add stubs for SplDoublyLinkedList

---
+++ ext/spl/spl_dllist_arginfo.h
@@ -0,0 +1,69 @@
+/* This is a generated file, edit the .stub.php file instead. */
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplQueue_enqueue, 0, 0, 1)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplQueue_dequeue, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplDoublyLinkedList_add, 0, 0, 2)
+	ZEND_ARG_INFO(0, index)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_SplDoublyLinkedList_pop arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_shift arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_push arginfo_class_SplQueue_enqueue
+
+#define arginfo_class_SplDoublyLinkedList_unshift arginfo_class_SplQueue_enqueue
+
+#define arginfo_class_SplDoublyLinkedList_top arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_bottom arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_count arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_isEmpty arginfo_class_SplQueue_dequeue
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplDoublyLinkedList_setIteratorMode, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, mode, IS_LONG, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_SplDoublyLinkedList_getIteratorMode arginfo_class_SplQueue_dequeue
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplDoublyLinkedList_offsetExists, 0, 0, 1)
+	ZEND_ARG_INFO(0, index)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_SplDoublyLinkedList_offsetGet arginfo_class_SplDoublyLinkedList_offsetExists
+
+#define arginfo_class_SplDoublyLinkedList_offsetSet arginfo_class_SplDoublyLinkedList_add
+
+#define arginfo_class_SplDoublyLinkedList_offsetUnset arginfo_class_SplDoublyLinkedList_offsetExists
+
+#define arginfo_class_SplDoublyLinkedList_rewind arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_current arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_key arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_prev arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_next arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList_valid arginfo_class_SplQueue_dequeue
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplDoublyLinkedList_unserialize, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, serialized, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+#define arginfo_class_SplDoublyLinkedList_serialize arginfo_class_SplQueue_dequeue
+
+#define arginfo_class_SplDoublyLinkedList___serialize arginfo_class_SplQueue_dequeue
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_class_SplDoublyLinkedList___unserialize, 0, 0, 1)
+	ZEND_ARG_TYPE_INFO(0, data, IS_ARRAY, 0)
+ZEND_END_ARG_INFO()
