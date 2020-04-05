0ee1dfba5b6ff2b001ba2674335e54da6fb68517
Máté Kocsis <kocsismate@woohoolabs.com>
2020-04-05 20:09:58 +0200

    Add missing stub for xmlrpc_get_type()

--- ext/xmlrpc/xmlrpc_arginfo.h
+++ ext/xmlrpc/xmlrpc_arginfo.h
@@ -21,6 +21,10 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xmlrpc_encode_request, 0, 2, IS_
 	ZEND_ARG_TYPE_INFO(0, output_options, IS_ARRAY, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xmlrpc_get_type, 0, 1, IS_STRING, 0)
+	ZEND_ARG_INFO(0, value)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xmlrpc_set_type, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_INFO(1, value)
 	ZEND_ARG_TYPE_INFO(0, type, IS_STRING, 0)
@@ -69,6 +73,7 @@ ZEND_FUNCTION(xmlrpc_encode);
 ZEND_FUNCTION(xmlrpc_decode);
 ZEND_FUNCTION(xmlrpc_decode_request);
 ZEND_FUNCTION(xmlrpc_encode_request);
+ZEND_FUNCTION(xmlrpc_get_type);
 ZEND_FUNCTION(xmlrpc_set_type);
 ZEND_FUNCTION(xmlrpc_is_fault);
 ZEND_FUNCTION(xmlrpc_server_create);
@@ -85,6 +90,7 @@ static const zend_function_entry ext_functions[] = {
 	ZEND_FE(xmlrpc_decode, arginfo_xmlrpc_decode)
 	ZEND_FE(xmlrpc_decode_request, arginfo_xmlrpc_decode_request)
 	ZEND_FE(xmlrpc_encode_request, arginfo_xmlrpc_encode_request)
+	ZEND_FE(xmlrpc_get_type, arginfo_xmlrpc_get_type)
 	ZEND_FE(xmlrpc_set_type, arginfo_xmlrpc_set_type)
 	ZEND_FE(xmlrpc_is_fault, arginfo_xmlrpc_is_fault)
 	ZEND_FE(xmlrpc_server_create, arginfo_xmlrpc_server_create)
