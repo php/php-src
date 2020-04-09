e0da5965537b3e37a22a0f8c279cab2683c0a3e5
Nikita Popov <nikita.ppv@gmail.com>
2020-04-09 15:33:32 +0200

    Add mode for verifying internal function params

--- Zend/zend_vm_execute.h
+++ Zend/zend_vm_execute.h
@@ -1219,8 +1219,11 @@ static ZEND_VM_HOT ZEND_OPCODE_HANDLER_RET ZEND_FASTCALL ZEND_DO_ICALL_SPEC_RETV
 	zval retval;
 
 	SAVE_OPLINE();
-	EX(call) = call->prev_execute_data;
+#if ZEND_VERIFY_INTERNAL_PARAM_DEFAULTS
+	zend_verify_internal_param_defaults(&call);
+#endif
 
+	EX(call) = call->prev_execute_data;
 	call->prev_execute_data = execute_data;
 	EG(current_execute_data) = call;
 
@@ -1280,8 +1283,11 @@ static ZEND_VM_HOT ZEND_OPCODE_HANDLER_RET ZEND_FASTCALL ZEND_DO_ICALL_SPEC_RETV
 	zval retval;
 
 	SAVE_OPLINE();
-	EX(call) = call->prev_execute_data;
+#if ZEND_VERIFY_INTERNAL_PARAM_DEFAULTS
+	zend_verify_internal_param_defaults(&call);
+#endif
 
+	EX(call) = call->prev_execute_data;
 	call->prev_execute_data = execute_data;
 	EG(current_execute_data) = call;
 
