cf378f336867bb37e7edd806905889e9f3c13440
Christoph M. Becker <cmbecker69@gmx.de>
2020-04-23 14:49:26 +0200

    Fix #36365: scandir duplicates file name at every 65535th file
    Since DIR_W32.offset is declared as `uint16_t`, we have an overflow for
    directories with many entries.  This patch changes the field to
    `uint32_t`.

--- win32/readdir.h
+++ win32/readdir.h
@@ -26,7 +26,7 @@ struct dirent {
 /* typedef DIR - not the same as Unix */
 struct DIR_W32 {
 	HANDLE handle;			/* _findfirst/_findnext handle */
-	uint16_t offset;		/* offset into directory */
+	uint32_t offset;		/* offset into directory */
 	uint8_t finished;		/* 1 if there are not more files */
 	WIN32_FIND_DATAW fileinfo;	/* from _findfirst/_findnext */
 	wchar_t *dirw;			/* the dir we are reading */
