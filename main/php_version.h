830e566998d091d8d1b02097ec661cbffd81c060
Christoph M. Becker <cmbecker69@gmx.de>
2020-04-28 09:20:04 +0200

    Prepare for 7.3.18RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 19
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.19-dev"
-#define PHP_VERSION_ID 70319
+#define PHP_RELEASE_VERSION 18
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.3.18RC1"
+#define PHP_VERSION_ID 70318
