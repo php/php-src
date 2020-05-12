fd6c4b18446b94cc181c7788d80855b0401c8dc5
Derick Rethans <github@derickrethans.nl>
2020-05-12 08:09:10 +0000

    Update versions for PHP 7.4.6

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 5
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.5-dev"
-#define PHP_VERSION_ID 70405
+#define PHP_RELEASE_VERSION 6
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "7.4.6"
+#define PHP_VERSION_ID 70406
