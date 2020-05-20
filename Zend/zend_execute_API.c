8819d247c68bd1b86959b3e7dfb66d54803c5d85
Nikita Popov <nikita.ppv@gmail.com>
2020-05-20 10:26:08 +0200

    Don't require rc=1 for function static variables
    If file cache only is used, then static_variables_ptr may point
    to an immutable static_variables HT, which we do not want to
    destroy here.

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -287,8 +287,9 @@ void shutdown_executor(void) /* {{{ */
 			if (op_array->static_variables) {
 				HashTable *ht = ZEND_MAP_PTR_GET(op_array->static_variables_ptr);
 				if (ht) {
-					ZEND_ASSERT(GC_REFCOUNT(ht) == 1);
-					zend_array_destroy(ht);
+					if (!(GC_FLAGS(ht) & IS_ARRAY_IMMUTABLE) && GC_DELREF(ht) == 0) {
+						zend_array_destroy(ht);
+					}
 					ZEND_MAP_PTR_SET(op_array->static_variables_ptr, NULL);
 				}
 			}
