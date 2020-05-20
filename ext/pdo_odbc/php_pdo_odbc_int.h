50af36a8aae73d313e3fc72d76e8640296a7e04e
George Peter Banyard <girgias@php.net>
2020-05-20 15:41:24 +0200

    Fix [-Wundef] warning in PDO ODBC extension

--- ext/pdo_odbc/php_pdo_odbc_int.h
+++ ext/pdo_odbc/php_pdo_odbc_int.h
@@ -24,74 +24,74 @@
 #endif
 
 /* {{{ Roll a dice, pick a header at random... */
-#if HAVE_SQLCLI1_H
+#ifdef HAVE_SQLCLI1_H
 # include <sqlcli1.h>
 # if defined(DB268K) && HAVE_LIBRARYMANAGER_H
 #  include <LibraryManager.h>
 # endif
 #endif
 
-#if HAVE_ODBC_H
+#ifdef HAVE_ODBC_H
 # include <odbc.h>
 #endif
 
-#if HAVE_IODBC_H
+#ifdef HAVE_IODBC_H
 # include <iodbc.h>
 #endif
 
-#if HAVE_SQLUNIX_H && !defined(PHP_WIN32)
+#if defined(HAVE_SQLUNIX_H) && !defined(PHP_WIN32)
 # include <sqlunix.h>
 #endif
 
-#if HAVE_SQLTYPES_H
+#ifdef HAVE_SQLTYPES_H
 # include <sqltypes.h>
 #endif
 
-#if HAVE_SQLUCODE_H
+#ifdef HAVE_SQLUCODE_H
 # include <sqlucode.h>
 #endif
 
-#if HAVE_SQL_H
+#ifdef HAVE_SQL_H
 # include <sql.h>
 #endif
 
-#if HAVE_ISQL_H
+#ifdef HAVE_ISQL_H
 # include <isql.h>
 #endif
 
-#if HAVE_SQLEXT_H
+#ifdef HAVE_SQLEXT_H
 # include <sqlext.h>
 #endif
 
-#if HAVE_ISQLEXT_H
+#ifdef HAVE_ISQLEXT_H
 # include <isqlext.h>
 #endif
 
-#if HAVE_UDBCEXT_H
+#ifdef HAVE_UDBCEXT_H
 # include <udbcext.h>
 #endif
 
-#if HAVE_CLI0CORE_H
+#ifdef HAVE_CLI0CORE_H
 # include <cli0core.h>
 #endif
 
-#if HAVE_CLI0EXT1_H
+#ifdef HAVE_CLI0EXT1_H
 # include <cli0ext.h>
 #endif
 
-#if HAVE_CLI0CLI_H
+#ifdef HAVE_CLI0CLI_H
 # include <cli0cli.h>
 #endif
 
-#if HAVE_CLI0DEFS_H
+#ifdef HAVE_CLI0DEFS_H
 # include <cli0defs.h>
 #endif
 
-#if HAVE_CLI0ENV_H
+#ifdef HAVE_CLI0ENV_H
 # include <cli0env.h>
 #endif
 
-#if HAVE_ODBCSDK_H
+#ifdef HAVE_ODBCSDK_H
 # include <odbcsdk.h>
 #endif
 
