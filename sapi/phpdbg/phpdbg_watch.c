af4a9bf1bf9109714e0e45aaf907e9809abb6217
Christoph M. Becker <cmbecker69@gmx.de>
2020-05-27 18:26:32 +0200

    Fix #73927: phpdbg fails with windows error prompt at "watch array"
    We expect zvals, so we should request zvals.
    
    We also suppress spurious watchpoint removal notices.

--- sapi/phpdbg/phpdbg_watch.c
+++ sapi/phpdbg/phpdbg_watch.c
@@ -713,8 +713,9 @@ void phpdbg_automatic_dequeue_free(phpdbg_watch_element *element) {
 		child = child->child;
 	}
 	PHPDBG_G(watchpoint_hit) = 1;
-	phpdbg_notice("watchdelete", "variable=\"%.*s\" recursive=\"%s\"", "%.*s has been removed, removing watchpoint%s", (int) ZSTR_LEN(child->str), ZSTR_VAL(child->str), (child->flags & PHPDBG_WATCH_RECURSIVE_ROOT) ? " recursively" : "");
-	zend_hash_index_del(&PHPDBG_G(watch_elements), child->id);
+	if (zend_hash_index_del(&PHPDBG_G(watch_elements), child->id) == SUCCESS) {
+		phpdbg_notice("watchdelete", "variable=\"%.*s\" recursive=\"%s\"", "%.*s has been removed, removing watchpoint%s", (int) ZSTR_LEN(child->str), ZSTR_VAL(child->str), (child->flags & PHPDBG_WATCH_RECURSIVE_ROOT) ? " recursively" : "");
+	}
 	phpdbg_free_watch_element_tree(element);
 }
 
