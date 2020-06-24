5889c41c766fedbbaf741a2d0ace1a1c624565b8
Nikita Popov <nikita.ppv@gmail.com>
2020-06-24 16:31:46 +0200

    Generate tabs in generate_mime_type_map.php
    The PHP file is space indented, but we need the C file to be tab
    indented.

--- sapi/cli/generate_mime_type_map.php
+++ sapi/cli/generate_mime_type_map.php
@@ -40,7 +40,7 @@ foreach($additional_mime_maps as $ext => $mime) {
     }
 }
 
-?>
+echo <<<HEADER
 /*
    +----------------------------------------------------------------------+
    | Copyright (c) The PHP Group                                          |
@@ -64,15 +64,23 @@ foreach($additional_mime_maps as $ext => $mime) {
 #define PHP_CLI_SERVER_MIME_TYPE_MAP_H
 
 typedef struct php_cli_server_ext_mime_type_pair {
-    const char *ext;
-    const char *mime_type;
+\tconst char *ext;
+\tconst char *mime_type;
 } php_cli_server_ext_mime_type_pair;
 
 static const php_cli_server_ext_mime_type_pair mime_type_map[] = {
-<?php foreach ($extensions as $extension => $mime): ?>
-    { "<?= addcslashes($extension, "\0..\37!@\@\177..\377") ?>", "<?= addcslashes($mime, "\0..\37!@\@\177..\377") ?>" },
-<?php endforeach ?>
-    { NULL, NULL }
+
+HEADER;
+
+foreach ($extensions as $extension => $mime) {
+    echo "\t{ \"" .addcslashes($extension, "\0..\37!@\@\177..\377") . "\", \""
+        . addcslashes($mime, "\0..\37!@\@\177..\377") . "\" },\n";
+}
+
+echo <<<FOOTER
+\t{ NULL, NULL }
 };
 
 #endif /* PHP_CLI_SERVER_MIME_TYPE_MAP_H */
+
+FOOTER;
