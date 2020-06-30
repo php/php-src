b52470843d5ee00044329468b40cf2dbd65e408d
davkor <david@adalogics.com>
2020-06-30 22:34:08 +0100

    Fixup author name.

--- sapi/fuzzer/fuzzer-pregmatch.c
+++ sapi/fuzzer/fuzzer-pregmatch.c
@@ -10,7 +10,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Stanislav Malyshev <stas@php.net>                           |
+   | Authors: David Korczynski <david@adalogics.com>                      |
    +----------------------------------------------------------------------+
  */
 #include "fuzzer.h"
