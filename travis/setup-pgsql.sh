336da0f832ebdcf42e06dad25c3a8b19f83c5cfc
Nikita Popov <nikita.ppv@gmail.com>
2020-07-01 15:19:51 +0200

    Enable ppc64el job on travis

--- travis/setup-pgsql.sh
+++ travis/setup-pgsql.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # PostgreSQL tests currently don't work on some architectures.
-if test -z "${ARM64}${S390X}"; then
+if test -z "${ARM64}${S390X}${PPC64EL}"; then
     psql -c "ALTER USER postgres PASSWORD 'postgres';" -U postgres
     psql -c "CREATE DATABASE test;" -U postgres
 fi
