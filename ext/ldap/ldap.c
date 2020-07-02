8aae192bef0ee4a4097776109d15c54d96bdb286
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-07-02 19:13:38 +0200

    fix some memory bugs in ldap.c

--- ext/ldap/ldap.c
+++ ext/ldap/ldap.c
@@ -394,7 +394,6 @@ static int _php_ldap_control_from_array(LDAP *ld, LDAPControl** ctrl, zval* arra
 						if (ber_flatten2(vrber, control_value, 0) == -1) {
 							rc = -1;
 						}
-						ber_free(vrber, 1);
 					}
 				}
 			}
@@ -422,6 +421,10 @@ static int _php_ldap_control_from_array(LDAP *ld, LDAPControl** ctrl, zval* arra
 							rc = -1;
 							php_error_docref(NULL, E_WARNING, "Failed to encode attribute list");
 							goto failure;
+						}
+
+						if (tmpstring != NULL) {
+							zend_string_release(tmpstring);
 						}
 
 						tmpstring = zval_get_string(attr);
@@ -468,6 +471,9 @@ static int _php_ldap_control_from_array(LDAP *ld, LDAPControl** ctrl, zval* arra
 					goto failure;
 				}
 				sort_keys[i] = emalloc(sizeof(LDAPSortKey));
+				if (tmpstring) {
+					zend_string_release(tmpstring);
+				}
 				tmpstring = zval_get_string(tmp);
 				if (EG(exception)) {
 					rc = -1;
@@ -476,6 +482,9 @@ static int _php_ldap_control_from_array(LDAP *ld, LDAPControl** ctrl, zval* arra
 				sort_keys[i]->attributeType = ZSTR_VAL(tmpstring);
 
 				if ((tmp = zend_hash_str_find(Z_ARRVAL_P(sortkey), "oid", sizeof("oid") - 1)) != NULL) {
+					if (tmpstring) {
+						zend_string_release(tmpstring);
+					}
 					tmpstring = zval_get_string(tmp);
 					if (EG(exception)) {
 						rc = -1;
@@ -4279,6 +4288,11 @@ PHP_FUNCTION(ldap_exop_passwd)
 		lnewpw.bv_len > 0 ? &lnewpw : NULL,
 		requestctrls,
 		NULL, &msgid);
+
+	if (requestctrls != NULL) {
+		efree(requestctrls);
+	}
+
 	if (rc != LDAP_SUCCESS ) {
 		php_error_docref(NULL, E_WARNING, "Passwd modify extended operation failed: %s (%d)", ldap_err2string(rc), rc);
 		RETURN_FALSE;
