9b823f64df391e3e63b0c07502600d06af53c475
George Peter Banyard <girgias@php.net>
2020-07-06 20:09:04 +0200

    Pass zend_string to underlying implementation function

--- ext/standard/levenshtein.c
+++ ext/standard/levenshtein.c
@@ -15,39 +15,36 @@
  */
 
 #include "php.h"
-#include <stdlib.h>
-#include <errno.h>
-#include <ctype.h>
 #include "php_string.h"
 
 #define LEVENSHTEIN_MAX_LENGTH 255
 
 /* {{{ reference_levdist
  * reference implementation, only optimized for memory usage, not speed */
-static zend_long reference_levdist(const char *s1, size_t l1, const char *s2, size_t l2, zend_long cost_ins, zend_long cost_rep, zend_long cost_del )
+static zend_long reference_levdist(const zend_string *string1, const zend_string *string2, zend_long cost_ins, zend_long cost_rep, zend_long cost_del )
 {
 	zend_long *p1, *p2, *tmp;
 	zend_long c0, c1, c2;
 	size_t i1, i2;
 
-	if (l1 == 0) {
-		return l2 * cost_ins;
+	if (ZSTR_LEN(string1) == 0) {
+		return ZSTR_LEN(string2) * cost_ins;
 	}
-	if (l2 == 0) {
-		return l1 * cost_del;
+	if (ZSTR_LEN(string2) == 0) {
+		return ZSTR_LEN(string1) * cost_del;
 	}
 
-	p1 = safe_emalloc((l2 + 1), sizeof(zend_long), 0);
-	p2 = safe_emalloc((l2 + 1), sizeof(zend_long), 0);
+	p1 = safe_emalloc((ZSTR_LEN(string2) + 1), sizeof(zend_long), 0);
+	p2 = safe_emalloc((ZSTR_LEN(string2) + 1), sizeof(zend_long), 0);
 
-	for (i2 = 0; i2 <= l2; i2++) {
+	for (i2 = 0; i2 <= ZSTR_LEN(string2); i2++) {
 		p1[i2] = i2 * cost_ins;
 	}
-	for (i1 = 0; i1 < l1 ; i1++) {
+	for (i1 = 0; i1 < ZSTR_LEN(string1) ; i1++) {
 		p2[0] = p1[0] + cost_del;
 
-		for (i2 = 0; i2 < l2; i2++) {
-			c0 = p1[i2] + ((s1[i1] == s2[i2]) ? 0 : cost_rep);
+		for (i2 = 0; i2 < ZSTR_LEN(string2); i2++) {
+			c0 = p1[i2] + ((ZSTR_VAL(string1)[i1] == ZSTR_VAL(string2)[i2]) ? 0 : cost_rep);
 			c1 = p1[i2 + 1] + cost_del;
 			if (c1 < c0) {
 				c0 = c1;
@@ -62,7 +59,7 @@ static zend_long reference_levdist(const char *s1, size_t l1, const char *s2, si
 		p1 = p2;
 		p2 = tmp;
 	}
-	c0 = p1[l2];
+	c0 = p1[ZSTR_LEN(string2)];
 
 	efree(p1);
 	efree(p2);
@@ -95,7 +92,7 @@ PHP_FUNCTION(levenshtein)
 		RETURN_THROWS();
 	}
 
-	distance = reference_levdist(ZSTR_VAL(string1), ZSTR_LEN(string1), ZSTR_VAL(string2), ZSTR_LEN(string2), cost_ins, cost_rep, cost_del);
+	distance = reference_levdist(string1, string2, cost_ins, cost_rep, cost_del);
 
 	RETURN_LONG(distance);
 }
