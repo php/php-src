e0fa48f69dd14b52c8f1b2904ac7bd30472849a8
Dik Takken <d.h.j.takken@freedom.nl>
2020-07-16 14:20:41 +0200

    Deprecate libxml_disable_entity_loader()
    This method was used to protect code against XXE processing attacks.
    Since PHP now requires libxml >= 2.9.0 external entity loading no longer
    needs to be disabled to prevent these attacks. It is disabled by default.
    Also, the method has an unwanted side effect that causes a lot of
    confusion: Parsing XML data from resources like files is no longer possible.
    
    Closes GH-5867.

--- ext/libxml/libxml_arginfo.h
+++ ext/libxml/libxml_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 2d793e5134ea8633c432f03d20c1d8b80a05795b */
+ * Stub hash: ded229511dc2bc3912d35b8055c0fd69420baff0 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_libxml_set_streams_context, 0, 1, IS_VOID, 0)
 	ZEND_ARG_INFO(0, context)
@@ -42,7 +42,7 @@ static const zend_function_entry ext_functions[] = {
 	ZEND_FE(libxml_get_last_error, arginfo_libxml_get_last_error)
 	ZEND_FE(libxml_get_errors, arginfo_libxml_get_errors)
 	ZEND_FE(libxml_clear_errors, arginfo_libxml_clear_errors)
-	ZEND_FE(libxml_disable_entity_loader, arginfo_libxml_disable_entity_loader)
+	ZEND_DEP_FE(libxml_disable_entity_loader, arginfo_libxml_disable_entity_loader)
 	ZEND_FE(libxml_set_external_entity_loader, arginfo_libxml_set_external_entity_loader)
 	ZEND_FE_END
 };
