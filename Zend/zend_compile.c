ee7eecf321fb480485fce7efce0d6deb8629870e
Nikita Popov <nikita.ppv@gmail.com>
2020-07-29 15:51:47 +0200

    Fix leak with nullsafe operator with constant LHS
    Followup to 5303bcdd32e8171399c2c60bf431e5304a66c39a. We need to
    perform the addref after emitting the opline, because that might
    intern the string.
    
    Fixes oss-fuzz #24479.

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -2308,10 +2308,10 @@ static void zend_short_circuiting_commit(uint32_t checkpoint, znode *result, zen
 static void zend_emit_jmp_null(znode *obj_node)
 {
 	uint32_t jmp_null_opnum = get_next_op_number();
-	if (obj_node->op_type == IS_CONST) {
-		Z_TRY_ADDREF(obj_node->u.constant);
+	zend_op *opline = zend_emit_op(NULL, ZEND_JMP_NULL, obj_node, NULL);
+	if (opline->op1_type == IS_CONST) {
+		Z_TRY_ADDREF_P(CT_CONSTANT(opline->op1));
 	}
-	zend_emit_op(NULL, ZEND_JMP_NULL, obj_node, NULL);
 	zend_stack_push(&CG(short_circuiting_opnums), &jmp_null_opnum);
 }
 
