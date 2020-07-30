dad81423966f2db9cc4e8f6e744a939fd0ae1bf5
郭新华 <393323503@qq.com>
2020-07-30 23:41:26 +0800

    Update zend_execute_API.c
    in some case , these is no opline in execute_data ,such like this:
     memset(&fake_execute_data, 0, sizeof(fake_execute_data));

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -552,7 +552,7 @@ ZEND_API uint32_t zend_get_executed_lineno(void) /* {{{ */
 	while (ex && (!ex->func || !ZEND_USER_CODE(ex->func->type))) {
 		ex = ex->prev_execute_data;
 	}
-	if (ex) {
+	if (ex && ex->opline) {
 		if (EG(exception) && ex->opline->opcode == ZEND_HANDLE_EXCEPTION &&
 		    ex->opline->lineno == 0 && EG(opline_before_exception)) {
 			return EG(opline_before_exception)->lineno;
