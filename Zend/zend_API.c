5d4659c25c1a40fcadd709560d60101ec524f730
Nikita Popov <nikita.ppv@gmail.com>
2020-08-03 10:23:06 +0200

    Enforce __set_state() parameter type
    This fixes one of the issues reported in bug #79925. The parameter
    type check for this particular method was missed.

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2187,6 +2187,7 @@ ZEND_API void zend_check_magic_method_implementation(const zend_class_entry *ce,
 		zend_check_magic_method_args(1, ce, fptr, error_type);
 		zend_check_magic_method_static(ce, fptr, error_type);
 		zend_check_magic_method_public(ce, fptr, error_type);
+		zend_check_magic_method_arg_type(0, ce, fptr, error_type, MAY_BE_ARRAY);
 		zend_check_magic_method_return_type(ce, fptr, error_type, MAY_BE_OBJECT);
 	} else if (zend_string_equals_literal(lcname, "__invoke")) {
 		zend_check_magic_method_non_static(ce, fptr, error_type);
