482af5f150118c3da57de58a0a86026f5f4357d2
Christoph M. Becker <cmbecker69@gmx.de>
2020-08-05 08:59:41 +0200

    Check linker compatibility directly from HMODULE
    Checking the linker compatibility with extranous `ImageLoad()` calls is
    possible, but unnecessary, since the modules are either already loaded
    or loaded shortly afterwards, so that we can get the required
    information directly from the module handles.  And actually, doing
    `ImageLoad()` as well as `LoadLibrary()` leaves a tiny room for a race
    condition, because both functions will lookup the module in the search
    path, so there is no *guarantee* that both are dealing with the same
    module.  Dropping the `ImageLoad()` calls also has the advantage to no
    longer face the issue reported in bug #79557.  A very minor additional
    advantage is that we no longer have to link against Imagehlp.dll.
    
    Furthermore, there is no need to check for CRT compatibility multiple
    times, so we can simplify the signature of `php_win32_crt_compatible`,
    and at the same time clean up main.c a bit.
    
    These changes require to change the signature of the exported
    `php_win32_image_compatible` and `php_win32_crt_compatible` functions,
    which now expect a `HMODULE` and nothing, respectively, instead of the
    module name.

--- ext/standard/dl.c
+++ ext/standard/dl.c
@@ -161,18 +161,16 @@ PHPAPI int php_load_extension(const char *filename, int type, int start_now)
 		efree(orig_libpath);
 		efree(err1);
 	}
+	efree(libpath);
 
 #ifdef PHP_WIN32
-	if (!php_win32_image_compatible(libpath, &err1)) {
+	if (!php_win32_image_compatible(handle, &err1)) {
 			php_error_docref(NULL, error_type, err1);
 			efree(err1);
-			efree(libpath);
 			DL_UNLOAD(handle);
 			return FAILURE;
 	}
 #endif
-
-	efree(libpath);
 
 	get_module = (zend_module_entry *(*)(void)) DL_FETCH_SYMBOL(handle, "get_module");
 
