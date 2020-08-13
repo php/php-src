ef308a7bc5f9bf40f78a0029cab3b155dee95164
Konrad K <46222525+KujawaK@users.noreply.github.com>
2020-08-13 11:16:42 +0200

    segfault when passing NULL to zend_list_delete
    related with Bug #79757

--- Zend/zend_list.c
+++ Zend/zend_list.c
@@ -44,7 +44,7 @@ ZEND_API zval* ZEND_FASTCALL zend_list_insert(void *ptr, int type)
 
 ZEND_API int ZEND_FASTCALL zend_list_delete(zend_resource *res)
 {
-	if (GC_DELREF(res) <= 0) {
+	if (res && GC_DELREF(res) <= 0) {
 		return zend_hash_index_del(&EG(regular_list), res->handle);
 	} else {
 		return SUCCESS;
