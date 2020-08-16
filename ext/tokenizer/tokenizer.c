f93e1056b2ba2fc0fdb435889fb8617f6e3fa9a0
George Peter Banyard <girgias@php.net>
2020-08-16 16:14:01 +0200

    Fix Tokenizer extension after voidification of zend_prepare_string_for_scanning()

--- ext/tokenizer/tokenizer.c
+++ ext/tokenizer/tokenizer.c
@@ -358,10 +358,7 @@ static zend_bool tokenize(zval *return_value, zend_string *source, zend_class_en
 	ZVAL_STR_COPY(&source_zval, source);
 	zend_save_lexical_state(&original_lex_state);
 
-	if (zend_prepare_string_for_scanning(&source_zval, "") == FAILURE) {
-		zend_restore_lexical_state(&original_lex_state);
-		return 0;
-	}
+	zend_prepare_string_for_scanning(&source_zval, "");
 
 	LANG_SCNG(yy_state) = yycINITIAL;
 	zend_hash_init(&interned_strings, 0, NULL, NULL, 0);
@@ -484,6 +481,8 @@ static zend_bool tokenize_parse(
 		zval *return_value, zend_string *source, zend_class_entry *token_class)
 {
 	zval source_zval;
+	struct event_context ctx;
+	zval token_stream;
 	zend_lex_state original_lex_state;
 	zend_bool original_in_compilation;
 	zend_bool success;
@@ -494,29 +493,26 @@ static zend_bool tokenize_parse(
 	CG(in_compilation) = 1;
 	zend_save_lexical_state(&original_lex_state);
 
-	if ((success = (zend_prepare_string_for_scanning(&source_zval, "") == SUCCESS))) {
-		struct event_context ctx;
-		zval token_stream;
-		array_init(&token_stream);
+	zend_prepare_string_for_scanning(&source_zval, "");
+	array_init(&token_stream);
 
-		ctx.tokens = &token_stream;
-		ctx.token_class = token_class;
+	ctx.tokens = &token_stream;
+	ctx.token_class = token_class;
 
-		CG(ast) = NULL;
-		CG(ast_arena) = zend_arena_create(1024 * 32);
-		LANG_SCNG(yy_state) = yycINITIAL;
-		LANG_SCNG(on_event) = on_event;
-		LANG_SCNG(on_event_context) = &ctx;
+	CG(ast) = NULL;
+	CG(ast_arena) = zend_arena_create(1024 * 32);
+	LANG_SCNG(yy_state) = yycINITIAL;
+	LANG_SCNG(on_event) = on_event;
+	LANG_SCNG(on_event_context) = &ctx;
 
-		if((success = (zendparse() == SUCCESS))) {
-			ZVAL_COPY_VALUE(return_value, &token_stream);
-		} else {
-			zval_ptr_dtor(&token_stream);
-		}
-
-		zend_ast_destroy(CG(ast));
-		zend_arena_destroy(CG(ast_arena));
+	if((success = (zendparse() == SUCCESS))) {
+		ZVAL_COPY_VALUE(return_value, &token_stream);
+	} else {
+		zval_ptr_dtor(&token_stream);
 	}
+
+	zend_ast_destroy(CG(ast));
+	zend_arena_destroy(CG(ast_arena));
 
 	/* restore compiler and scanner global states */
 	zend_restore_lexical_state(&original_lex_state);
