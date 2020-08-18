ff14b7adad4c63cc9f7118fd1794e006593391f8
Christoph M. Becker <cmbecker69@gmx.de>
2020-08-18 09:55:37 +0200

    7.3.23 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 22
+#define PHP_RELEASE_VERSION 23
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.22-dev"
-#define PHP_VERSION_ID 70322
+#define PHP_VERSION "7.3.23-dev"
+#define PHP_VERSION_ID 70323
