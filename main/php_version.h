499bf91cd0af7e2a283f89d15604478b111af955
Derick Rethans <github@derickrethans.nl>
2020-08-18 08:06:20 +0100

    Prepare for 7.4.11

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 10
+#define PHP_RELEASE_VERSION 11
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.10-dev"
-#define PHP_VERSION_ID 70410
+#define PHP_VERSION "7.4.11-dev"
+#define PHP_VERSION_ID 70411
