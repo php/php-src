b2a33ab06bab4f2d79634e294632bebebd04acdb
Andy Postnikov <apostnikov@gmail.com>
2020-08-22 02:44:48 +0300

    Fix #80007: Potential type confusion in unixtojd() parameter parsing
    Also it fixes test on 32-bit armv7 and x86
    - Test unixtojd() function : error conditions [ext/calendar/tests/unixtojd_error1.phpt]
    
    Closes GH-6033

--- ext/calendar/cal_unix.c
+++ ext/calendar/cal_unix.c
@@ -28,15 +28,18 @@
 PHP_FUNCTION(unixtojd)
 {
 	time_t ts = 0;
+	zend_long tl = 0;
 	struct tm *ta, tmbuf;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "|l", &ts) == FAILURE) {
+	if (zend_parse_parameters(ZEND_NUM_ARGS(), "|l", &tl) == FAILURE) {
 		return;
 	}
 
-	if (!ts) {
+	if (!tl) {
 		ts = time(NULL);
-	} else if (ts < 0) {
+	} else if (tl >= 0) {
+		ts = (time_t) tl;
+	} else {
 		RETURN_FALSE;
 	}
 
