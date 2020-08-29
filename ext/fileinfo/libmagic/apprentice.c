c676e8f356e0610ef257df4546c58522796205cf
Anatol Belski <ab@php.net>
2020-08-29 19:58:26 +0200

    libmagic: Fix VS compat

--- ext/fileinfo/libmagic/apprentice.c
+++ ext/fileinfo/libmagic/apprentice.c
@@ -2220,7 +2220,7 @@ parse(struct magic_set *ms, struct magic_entry *me, const char *line,
  */
 private int
 parse_strength(struct magic_set *ms, struct magic_entry *me, const char *line,
-    size_t len __attribute__((__unused__)))
+    size_t len)
 {
 	const char *l = line;
 	char *el;
