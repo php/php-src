0e01d8ae83cf41439230e5cf744d6a4000ebd46a
Alex Dowad <alexinbeijing@gmail.com>
2020-09-04 20:32:50 +0200

    Minor cleanup in mbfilter_uuencode.c

--- ext/mbstring/libmbfl/filters/mbfilter_uuencode.c
+++ ext/mbstring/libmbfl/filters/mbfilter_uuencode.c
@@ -60,29 +60,34 @@ const struct mbfl_convert_vtbl vtbl_uuencode_8bit = {
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)
 
 /* uuencode => any */
-#define UUDEC(c)	(char)(((c)-' ')&077)
+#define UUDEC(c)	(char)(((c)-' ') & 0x3f)
 static const char * uuenc_begin_text = "begin ";
-enum { uudec_state_ground=0, uudec_state_inbegin,
+enum {
+	uudec_state_ground=0,
+	uudec_state_inbegin,
 	uudec_state_until_newline,
-	uudec_state_size, uudec_state_a, uudec_state_b, uudec_state_c, uudec_state_d,
-	uudec_state_skip_newline};
+	uudec_state_size,
+	uudec_state_a,
+	uudec_state_b,
+	uudec_state_c,
+	uudec_state_d,
+	uudec_state_skip_newline
+};
 
 int mbfl_filt_conv_uudec(int c, mbfl_convert_filter * filter)
 {
 	int n;
-
 	switch(filter->status)	{
 		case uudec_state_ground:
 			/* looking for "begin 0666 filename\n" line */
-			if (filter->cache == 0 && c == 'b')
-			{
+			if (filter->cache == 0 && c == 'b') {
 				filter->status = uudec_state_inbegin;
 				filter->cache = 1; /* move to 'e' */
-			}
-			else if (c == '\n')
+			} else if (c == '\n') {
 				filter->cache = 0;
-			else
+			} else {
 				filter->cache++;
+			}
 			break;
 		case uudec_state_inbegin:
 			if (uuenc_begin_text[filter->cache++] != c)	{
@@ -90,61 +95,54 @@ int mbfl_filt_conv_uudec(int c, mbfl_convert_filter * filter)
 				filter->status = uudec_state_ground;
 				break;
 			}
-			if (filter->cache == 5)
-			{
+			if (filter->cache == 5) {
 				/* that's good enough - wait for a newline */
 				filter->status = uudec_state_until_newline;
 				filter->cache = 0;
 			}
 			break;
 		case uudec_state_until_newline:
-			if (c == '\n')
+			if (c == '\n') {
 				filter->status = uudec_state_size;
+			}
 			break;
 		case uudec_state_size:
 			/* get "size" byte */
-			n = UUDEC(c);
-			filter->cache = n << 24;
+			filter->cache = UUDEC(c) << 24;
 			filter->status = uudec_state_a;
 			break;
 		case uudec_state_a:
 			/* get "a" byte */
-			n = UUDEC(c);
-			filter->cache |= (n << 16);
+			filter->cache |= (UUDEC(c) << 16);
 			filter->status = uudec_state_b;
 			break;
 		case uudec_state_b:
 			/* get "b" byte */
-			n = UUDEC(c);
-			filter->cache |= (n << 8);
+			filter->cache |= (UUDEC(c) << 8);
 			filter->status = uudec_state_c;
 			break;
 		case uudec_state_c:
 			/* get "c" byte */
-			n = UUDEC(c);
-			filter->cache |= n;
+			filter->cache |= UUDEC(c);
 			filter->status = uudec_state_d;
 			break;
 		case uudec_state_d:
 			/* get "d" byte */
-			{
-				int A, B, C, D = UUDEC(c);
-				A = (filter->cache >> 16) & 0xff;
-				B = (filter->cache >> 8) & 0xff;
-				C = (filter->cache) & 0xff;
-				n = (filter->cache >> 24) & 0xff;
-				if (n-- > 0)
-					CK((*filter->output_function)( (A << 2) | (B >> 4), filter->data));
-				if (n-- > 0)
-					CK((*filter->output_function)( (B << 4) | (C >> 2), filter->data));
-				if (n-- > 0)
-					CK((*filter->output_function)( (C << 6) | D, filter->data));
-				filter->cache = n << 24;
+			n = (filter->cache >> 24) & 0xff;
+			int A = (filter->cache >> 16) & 0xff, B = (filter->cache >> 8) & 0xff,
+				C = (filter->cache) & 0xff, D = UUDEC(c);
+			if (n-- > 0)
+				CK((*filter->output_function)((A << 2) | (B >> 4), filter->data));
+			if (n-- > 0)
+				CK((*filter->output_function)((B << 4) | (C >> 2), filter->data));
+			if (n-- > 0)
+				CK((*filter->output_function)((C << 6) | D, filter->data));
+			filter->cache = n << 24;
 
-				if (n == 0)
-					filter->status = uudec_state_skip_newline;	/* skip next byte (newline) */
-				else
-					filter->status = uudec_state_a; /* go back to fetch "A" byte */
+			if (n == 0) {
+				filter->status = uudec_state_skip_newline;	/* skip next byte (newline) */
+			} else {
+				filter->status = uudec_state_a; /* go back to fetch "A" byte */
 			}
 			break;
 		case uudec_state_skip_newline:
