5629ff3f5ba6a9bddfc4e1f79c48bd4c1014d28a
Dmitry Stogov <dmitry@zend.com>
2020-09-08 00:39:41 +0300

    Fixed Windows version of zend_ulong_nlz()

--- Zend/zend_bitset.h
+++ Zend/zend_bitset.h
@@ -95,7 +95,7 @@ static zend_always_inline int zend_ulong_nlz(zend_ulong num)
 		return SIZEOF_ZEND_LONG * 8;
 	}
 
-	return (int) index;
+	return (int) SIZEOF_ZEND_LONG * 8 - index;
 #else
 	zend_ulong x;
 	int n;
