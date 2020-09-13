4b95fdf2cac7269a38d035141e7321a295e19b29
Alex Dowad <alexinbeijing@gmail.com>
2020-09-13 19:25:46 +0200

    ISO-2022-JP-2004 conversion: handle invalid characters correctly

--- ext/mbstring/libmbfl/filters/mbfilter_sjis_2004.c
+++ ext/mbstring/libmbfl/filters/mbfilter_sjis_2004.c
@@ -206,9 +206,14 @@ retry:
 				CK((*filter->output_function)(c | MBFL_WCSGROUP_THROUGH, filter->data));
 				break;
 			}
-		} else {
-			s1 = c1;
-			s2 = c;
+		} else { /* ISO-2022-JP-2004 */
+			if (c >= 0x21 && c <= 0x7E) {
+				s1 = c1;
+				s2 = c;
+			} else {
+				CK((*filter->output_function)(c | MBFL_WCSGROUP_THROUGH, filter->data));
+				break;
+			}
 		}
 		w1 = (s1 << 8) | s2;
 
@@ -316,6 +321,13 @@ retry:
 		} else {
 			c2 = c;
 		}
+
+		if (c2 < 0x21 || c2 > 0x7E) {
+			w = (c1 << 8) | c2 | MBFL_WCSGROUP_THROUGH;
+			CK((*filter->output_function)(w, filter->data));
+			break;
+		}
+
 		s1 = c1 - 0x21;
 		s2 = c2 - 0x21;
 
