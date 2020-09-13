0d4c6a153e8fe9509ced1f1c6cace8d0ab4efa8b
George Peter Banyard <girgias@php.net>
2020-09-13 16:05:59 +0200

    Return SUCCESS as FAILURE implies retry with type conversion

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -1260,7 +1260,7 @@ static zend_result ZEND_FASTCALL div_function_base(zval *result, zval *op1, zval
 	if (EXPECTED(type_pair == TYPE_PAIR(IS_LONG, IS_LONG))) {
 		if (Z_LVAL_P(op2) == 0) {
 			zend_throw_error(zend_ce_division_by_zero_error, "Division by zero");
-			return FAILURE;
+			return SUCCESS;
 		} else if (Z_LVAL_P(op2) == -1 && Z_LVAL_P(op1) == ZEND_LONG_MIN) {
 			/* Prevent overflow error/crash */
 			ZVAL_DOUBLE(result, (double) ZEND_LONG_MIN / -1);
@@ -1275,21 +1275,21 @@ static zend_result ZEND_FASTCALL div_function_base(zval *result, zval *op1, zval
 	} else if (EXPECTED(type_pair == TYPE_PAIR(IS_DOUBLE, IS_DOUBLE))) {
 		if (Z_DVAL_P(op2) == 0) {
 			zend_throw_error(zend_ce_division_by_zero_error, "Division by zero");
-			return FAILURE;
+			return SUCCESS;
 		}
 		ZVAL_DOUBLE(result, Z_DVAL_P(op1) / Z_DVAL_P(op2));
 		return SUCCESS;
 	} else if (EXPECTED(type_pair == TYPE_PAIR(IS_DOUBLE, IS_LONG))) {
 		if (Z_LVAL_P(op2) == 0) {
 			zend_throw_error(zend_ce_division_by_zero_error, "Division by zero");
-			return FAILURE;
+			return SUCCESS;
 		}
 		ZVAL_DOUBLE(result, Z_DVAL_P(op1) / (double)Z_LVAL_P(op2));
 		return SUCCESS;
 	} else if (EXPECTED(type_pair == TYPE_PAIR(IS_LONG, IS_DOUBLE))) {
 		if (Z_DVAL_P(op2) == 0) {
 			zend_throw_error(zend_ce_division_by_zero_error, "Division by zero");
-			return FAILURE;
+			return SUCCESS;
 		}
 		ZVAL_DOUBLE(result, (double)Z_LVAL_P(op1) / Z_DVAL_P(op2));
 		return SUCCESS;
@@ -1327,7 +1327,7 @@ ZEND_API zend_result ZEND_FASTCALL div_function(zval *result, zval *op1, zval *o
 		return SUCCESS;
 	}
 
-	ZEND_ASSERT(EG(exception));
+	ZEND_ASSERT(0 && "Operation must succeed");
 	return FAILURE;
 }
 /* }}} */
