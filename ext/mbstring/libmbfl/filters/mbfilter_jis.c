b67e358e75fc55ec802215fac0ca5a8e21b1f307
Alex Dowad <alexinbeijing@gmail.com>
2020-09-14 21:07:03 +0200

    JIS7/JIS8 encoding: handle invalid 2nd byte for Kanji correctly
    Previously, in ISO-2022-JP/JIS7/JIS8, if an escape sequence (starting with 0x1B)
    appeared where the 2nd byte of a multibyte character should have been, mbstring
    would forget all about the truncated multibyte character and happily accept the
    escape sequence. However, such sequences are not legal and should be flagged as
    errors.
    
    Also, any other illegal bytes appearing where the 2nd byte of a multibyte
    character was expected were just passed through quietly to the output. Fix that.
    
    Also add a test suite for both ISO-2022-JP and JIS7/JIS8. (These are extremely
    similar encodings; JIS7 and JIS8 are variants of ISO-2022-JP. mbstring's 'JIS'
    is actually a combination of JIS7 _and_ JIS8, since the extensions which each
    one adds to ISO-2022-JP are disjoint.)

--- ext/mbstring/libmbfl/filters/mbfilter_jis.c
+++ ext/mbstring/libmbfl/filters/mbfilter_jis.c
@@ -170,10 +170,6 @@ retry:
 				}
 			}
 			CK((*filter->output_function)(w, filter->data));
-		} else if (c == 0x1b) {
-			filter->status += 2;
-		} else if ((c >= 0 && c < 0x21) || c == 0x7f) {		/* CTLs */
-			CK((*filter->output_function)(c, filter->data));
 		} else {
 			w = (c1 << 8) | c;
 			w &= MBFL_WCSGROUP_MASK;
