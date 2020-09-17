5646597d1a3678689847de1b6e5787b3d4d0ee92
Dharman <tekiela246@gmail.com>
2020-09-17 12:35:26 +0100

    Fixing segfault with get_result and PS cursors
    I decided to temporarily switch on error reporting since I want to make sure that the error is not only populating the $conn->error variable but also is triggering the exception when in exception mode.
    Of course if we want to trigger a segfault we would need to fetch a row. This does not change the outcome of the test, but it is nice to explain why we are testing this.

--- ext/mysqlnd/mysqlnd_ps.c
+++ ext/mysqlnd/mysqlnd_ps.c
@@ -149,8 +149,14 @@ MYSQLND_METHOD(mysqlnd_stmt, get_result)(MYSQLND_STMT * const s)
 	}
 
 	if (stmt->cursor_exists) {
-		/* Silently convert buffered to unbuffered, for now */
-		DBG_RETURN(s->m->use_result(s));
+		/* Prepared statement cursors are not supported as of yet */
+		char * msg;
+		mnd_sprintf(&msg, 0, "%s() cannot be used with cursors", get_active_function_name());
+		SET_CLIENT_ERROR(stmt->error_info, CR_NOT_IMPLEMENTED, UNKNOWN_SQLSTATE, msg);
+		if (msg) {
+			mnd_sprintf_free(msg);
+		}
+		DBG_RETURN(NULL);
 	}
 
 	/* Nothing to store for UPSERT/LOAD DATA*/
