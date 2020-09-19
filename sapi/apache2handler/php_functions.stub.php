9d1c7b242ae1206065fdb57f1bbb4375ef645163
Máté Kocsis <kocsismate@woohoolabs.com>
2020-09-19 14:35:37 +0200

    Fix UNKNOWN default value of apache_note()

--- sapi/apache2handler/php_functions.stub.php
+++ sapi/apache2handler/php_functions.stub.php
@@ -13,7 +13,7 @@ function getallheaders(): array {}
 
 function apache_response_headers(): array {}
 
-function apache_note(string $note_name, string $note_value = UNKNOWN): string|false {}
+function apache_note(string $note_name, ?string $note_value = null): string|false {}
 
 function apache_setenv(string $variable, string $value, bool $walk_to_top = false): bool {}
 
