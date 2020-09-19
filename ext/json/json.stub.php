bfa70797e01a165f35599988bd9c41e94c054cfa
Larry Garfield <larry@garfieldtech.com>
2020-09-19 11:50:22 -0500

    Give a bool a bool default value.

--- ext/json/json.stub.php
+++ ext/json/json.stub.php
@@ -4,7 +4,7 @@
 
 function json_encode(mixed $value, int $options = 0, int $depth = 512): string|false {}
 
-function json_decode(string $json, ?bool $assoc = null, int $depth = 512, int $options = 0): mixed {}
+function json_decode(string $json, ?bool $assoc = false, int $depth = 512, int $options = 0): mixed {}
 
 function json_last_error(): int {}
 
