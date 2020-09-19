40e8c7c90b1715a661f0a7056883d99198da18fe
Máté Kocsis <kocsismate@woohoolabs.com>
2020-09-19 12:31:36 +0200

    Adjust the order of method modifiers in stub
    All the other method modifiers in stubs follow the guidelines of PSR-12, so let's use it in case of PhpToken::__construct() as well.

--- ext/tokenizer/tokenizer.stub.php
+++ ext/tokenizer/tokenizer.stub.php
@@ -11,7 +11,7 @@ class PhpToken implements Stringable
     /** @return static[] */
     public static function getAll(string $code, int $flags = 0): array {}
 
-    public final function __construct(int $id, string $text, int $line = -1, int $pos = -1) {}
+    final public function __construct(int $id, string $text, int $line = -1, int $pos = -1) {}
 
     /** @param int|string|array $kind */
     public function is($kind): bool {}
