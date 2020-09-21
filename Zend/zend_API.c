2bf2cede8933586bb5be2fd77af650a23fcc936e
Nikita Popov <nikita.ppv@gmail.com>
2020-09-21 10:50:29 +0200

    Initialize attributes for internal functions
    Fixes a crash in Symfony SecurityBundle tests.

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -2325,6 +2325,7 @@ ZEND_API zend_result zend_register_functions(zend_class_entry *scope, const zend
 		internal_function->function_name = zend_string_init_interned(ptr->fname, fname_len, 1);
 		internal_function->scope = scope;
 		internal_function->prototype = NULL;
+		internal_function->attributes = NULL;
 		if (ptr->flags) {
 			if (!(ptr->flags & ZEND_ACC_PPP_MASK)) {
 				if (ptr->flags != ZEND_ACC_DEPRECATED && scope) {
