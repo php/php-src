150ebfdf77320b24b0358f8a903e90d7940ad4a4
George Peter Banyard <girgias@php.net>
2020-09-23 23:46:58 +0100

    Suppress bogus [-Wlogical-op] warning from GCC
    See GCC bug 69602: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=69602
    which emits the warning for (errno == EWOULDBLOCK || errno == EAGAIN)
    which is the correct way of handling errors as the value of EWOULDBLOCK
    and EAGAIN is implementation defined.
    
    Therefore introduce a new macro function PHP_IS_TRANSIENT_ERROR()
    which handles the case when EWOULDBLOCK and EAGAIN are identical.
    
    Thanks to @twose for the idea.

--- ext/sockets/sockets.c
+++ ext/sockets/sockets.c
@@ -1045,11 +1045,7 @@ PHP_FUNCTION(socket_read)
 	if (retval == -1) {
 		/* if the socket is in non-blocking mode and there's no data to read,
 		don't output any error, as this is a normal situation, and not an error */
-		if (errno == EAGAIN
-#ifdef EWOULDBLOCK
-		|| errno == EWOULDBLOCK
-#endif
-		) {
+		if (PHP_IS_TRANSIENT_ERROR(errno)) {
 			php_sock->error = errno;
 			SOCKETS_G(last_error) = errno;
 		} else {
