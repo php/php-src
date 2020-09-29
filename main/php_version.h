e1daa19750730b5d94ed2525289d869a4d3cff2b
Derick Rethans <github@derickrethans.nl>
2020-09-29 10:17:01 +0000

    Update versions for PHP 7.4.11

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 11
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.11-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "7.4.11"
 #define PHP_VERSION_ID 70411
