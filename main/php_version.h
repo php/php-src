662083fc4f3a570f5b9180e80c2ddec86a8fded8
Remi Collet <remi@php.net>
2020-09-30 07:36:59 +0200

    bump version to 7.2.35-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 34
+#define PHP_RELEASE_VERSION 35
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.2.34-dev"
-#define PHP_VERSION_ID 70234
+#define PHP_VERSION "7.2.35-dev"
+#define PHP_VERSION_ID 70235
