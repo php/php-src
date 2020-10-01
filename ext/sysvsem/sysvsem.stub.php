d39ac7e754eba8f554ee6957673dc2b778d15b56
Máté Kocsis <kocsismate@woohoolabs.com>
2020-10-01 10:00:43 +0200

    Review parameter names in ext/sysvsem

--- ext/sysvsem/sysvsem.stub.php
+++ ext/sysvsem/sysvsem.stub.php
@@ -6,9 +6,9 @@ final class SysvSemaphore
 {
 }
 
-function sem_get(int $key, int $max_acquire = 1, int $perm = 0666, bool $auto_release = true): SysvSemaphore|false {}
+function sem_get(int $key, int $max_acquire = 1, int $permissions = 0666, bool $auto_release = true): SysvSemaphore|false {}
 
-function sem_acquire(SysvSemaphore $semaphore, bool $nowait = false): bool {}
+function sem_acquire(SysvSemaphore $semaphore, bool $non_blocking = false): bool {}
 
 function sem_release(SysvSemaphore $semaphore): bool {}
 
