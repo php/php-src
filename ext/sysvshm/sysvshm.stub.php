4a30c7684587fcb34c50f796b23518705e5e5874
Máté Kocsis <kocsismate@woohoolabs.com>
2020-10-01 10:09:27 +0200

    Review parameter names in ext/sysvshm
    Closes GH-6249

--- ext/sysvshm/sysvshm.stub.php
+++ ext/sysvshm/sysvshm.stub.php
@@ -6,16 +6,16 @@ final class SysvSharedMemory
 {
 }
 
-function shm_attach(int $key, ?int $memsize = null, int $perm = 0666): SysvSharedMemory|false {}
+function shm_attach(int $key, ?int $size = null, int $permissions = 0666): SysvSharedMemory|false {}
 
 function shm_detach(SysvSharedMemory $shm): bool {}
 
-function shm_has_var(SysvSharedMemory $shm, int $variable_key): bool {}
+function shm_has_var(SysvSharedMemory $shm, int $key): bool {}
 
 function shm_remove(SysvSharedMemory $shm): bool {}
 
-function shm_put_var(SysvSharedMemory $shm, int $variable_key, mixed $variable): bool {}
+function shm_put_var(SysvSharedMemory $shm, int $key, mixed $value): bool {}
 
-function shm_get_var(SysvSharedMemory $shm, int $variable_key): mixed {}
+function shm_get_var(SysvSharedMemory $shm, int $key): mixed {}
 
-function shm_remove_var(SysvSharedMemory $shm, int $variable_key): bool {}
+function shm_remove_var(SysvSharedMemory $shm, int $key): bool {}
--- ext/sysvshm/sysvshm_arginfo.h
+++ ext/sysvshm/sysvshm_arginfo.h
@@ -1,10 +1,10 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 9a57af33a6658387f706ea121612c0f0eb55e5b3 */
+ * Stub hash: 12f78387e5b7c436b608dbecc4c6b3eec82b6db1 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_TYPE_MASK_EX(arginfo_shm_attach, 0, 1, SysvSharedMemory, MAY_BE_FALSE)
 	ZEND_ARG_TYPE_INFO(0, key, IS_LONG, 0)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, memsize, IS_LONG, 1, "null")
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, perm, IS_LONG, 0, "0666")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, size, IS_LONG, 1, "null")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, permissions, IS_LONG, 0, "0666")
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_shm_detach, 0, 1, _IS_BOOL, 0)
@@ -13,20 +13,20 @@ ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_shm_has_var, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_INFO(0, shm, SysvSharedMemory, 0)
-	ZEND_ARG_TYPE_INFO(0, variable_key, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, key, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 #define arginfo_shm_remove arginfo_shm_detach
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_shm_put_var, 0, 3, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_INFO(0, shm, SysvSharedMemory, 0)
-	ZEND_ARG_TYPE_INFO(0, variable_key, IS_LONG, 0)
-	ZEND_ARG_TYPE_INFO(0, variable, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO(0, key, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, value, IS_MIXED, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_shm_get_var, 0, 2, IS_MIXED, 0)
 	ZEND_ARG_OBJ_INFO(0, shm, SysvSharedMemory, 0)
-	ZEND_ARG_TYPE_INFO(0, variable_key, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, key, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 #define arginfo_shm_remove_var arginfo_shm_has_var
