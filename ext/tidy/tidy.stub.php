87199c7549d315e9cba0b22fd65e28efdcb726e3
Nikita Popov <nikita.ppv@gmail.com>
2020-10-06 14:49:04 +0200

    Update ext/tidy parameter names

--- ext/tidy/tidy.stub.php
+++ ext/tidy/tidy.stub.php
@@ -2,59 +2,59 @@
 
 /** @generate-function-entries */
 
-function tidy_parse_string(string $input, array|string|null $config_options = null, ?string $encoding = null): tidy|false {}
+function tidy_parse_string(string $string, array|string|null $config = null, ?string $encoding = null): tidy|false {}
 
-function tidy_get_error_buffer(tidy $object): string|false {}
+function tidy_get_error_buffer(tidy $tidy): string|false {}
 
-function tidy_get_output(tidy $object): string {}
+function tidy_get_output(tidy $tidy): string {}
 
-function tidy_parse_file(string $file, array|string|null $config_options = null, ?string $encoding = null, bool $use_include_path = false): tidy|false {}
+function tidy_parse_file(string $filename, array|string|null $config = null, ?string $encoding = null, bool $useIncludePath = false): tidy|false {}
 
-function tidy_clean_repair(tidy $object): bool {}
+function tidy_clean_repair(tidy $tidy): bool {}
 
-function tidy_repair_string(string $data, array|string|null $config_options = null, ?string $encoding = null): string|false {}
+function tidy_repair_string(string $string, array|string|null $config = null, ?string $encoding = null): string|false {}
 
-function tidy_repair_file(string $filename, array|string|null $config_options = null, ?string $encoding = null, bool $use_include_path = false): string|false {}
+function tidy_repair_file(string $filename, array|string|null $config = null, ?string $encoding = null, bool $useIncludePath = false): string|false {}
 
-function tidy_diagnose(tidy $object): bool {}
+function tidy_diagnose(tidy $tidy): bool {}
 
 function tidy_get_release(): string {}
 
 #ifdef HAVE_TIDYOPTGETDOC
-function tidy_get_opt_doc(tidy $object, string $optname): string|false {}
+function tidy_get_opt_doc(tidy $tidy, string $option): string|false {}
 #endif
 
-function tidy_get_config(tidy $object): array {}
+function tidy_get_config(tidy $tidy): array {}
 
-function tidy_get_status(tidy $object): int {}
+function tidy_get_status(tidy $tidy): int {}
 
-function tidy_get_html_ver(tidy $object): int {}
+function tidy_get_html_ver(tidy $tidy): int {}
 
-function tidy_is_xhtml(tidy $object): bool {}
+function tidy_is_xhtml(tidy $tidy): bool {}
 
-function tidy_is_xml(tidy $object): bool {}
+function tidy_is_xml(tidy $tidy): bool {}
 
-function tidy_error_count(tidy $object): int {}
+function tidy_error_count(tidy $tidy): int {}
 
-function tidy_warning_count(tidy $object): int {}
+function tidy_warning_count(tidy $tidy): int {}
 
-function tidy_access_count(tidy $object): int {}
+function tidy_access_count(tidy $tidy): int {}
 
-function tidy_config_count(tidy $object): int {}
+function tidy_config_count(tidy $tidy): int {}
 
-function tidy_getopt(tidy $object, string $option): string|int|bool {}
+function tidy_getopt(tidy $tidy, string $option): string|int|bool {}
 
-function tidy_get_root(tidy $object): ?tidyNode {}
+function tidy_get_root(tidy $tidy): ?tidyNode {}
 
-function tidy_get_html(tidy $object): ?tidyNode {}
+function tidy_get_html(tidy $tidy): ?tidyNode {}
 
-function tidy_get_head(tidy $object): ?tidyNode {}
+function tidy_get_head(tidy $tidy): ?tidyNode {}
 
 function tidy_get_body(tidy $tidy): ?tidyNode {}
 
 class tidy
 {
-    public function __construct(?string $filename = null, array|string|null $config_options = null, ?string $encoding = null, bool $use_include_path = false) {}
+    public function __construct(?string $filename = null, array|string|null $config = null, ?string $encoding = null, bool $useIncludePath = false) {}
 
     /**
      * @return string|int|bool
@@ -69,22 +69,22 @@ class tidy
     public function cleanRepair() {}
 
     /** @return bool */
-    public function parseFile(string $file, array|string|null $config_options = null, ?string $encoding = null, bool $use_include_path = false) {}
+    public function parseFile(string $filename, array|string|null $config = null, ?string $encoding = null, bool $useIncludePath = false) {}
 
     /** @return bool */
-    public function parseString(string $input, array|string|null $config_options = null, ?string $encoding = null) {}
+    public function parseString(string $string, array|string|null $config = null, ?string $encoding = null) {}
 
     /**
      * @return bool
      * @alias tidy_repair_string
      */
-    public function repairString(string $data, array|string|null $config_options = null, ?string $encoding = null) {}
+    public function repairString(string $string, array|string|null $config = null, ?string $encoding = null) {}
 
     /**
      * @return bool
      * @alias tidy_repair_file
      */
-    public function repairFile(string $filename, array|string|null $config_options = null, ?string $encoding = null, bool $use_include_path = false) {}
+    public function repairFile(string $filename, array|string|null $config = null, ?string $encoding = null, bool $useIncludePath = false) {}
 
     /**
      * @return bool
@@ -121,7 +121,7 @@ class tidy
      * @return string|false
      * @alias tidy_get_opt_doc
      */
-    public function getOptDoc(string $optname) {}
+    public function getOptDoc(string $option) {}
 #endif
 
     /**
