6215cd62b741c2b736e0348a1ac2db56eb69d5fc
Christoph M. Becker <cmbecker69@gmx.de>
2020-10-08 09:53:34 +0200

    Rename some parameters according to PR discussion

--- ext/com_dotnet/com_extension.stub.php
+++ ext/com_dotnet/com_extension.stub.php
@@ -42,7 +42,7 @@ function variant_not(mixed $left): variant {}
 
 function variant_round(mixed $left, int $decimals): ?variant {}
 
-function variant_cmp(mixed $left, mixed $right, int $lcid = LOCALE_SYSTEM_DEFAULT, int $flags = 0): int {}
+function variant_cmp(mixed $left, mixed $right, int $locale_id = LOCALE_SYSTEM_DEFAULT, int $flags = 0): int {}
 
 function variant_date_to_timestamp(variant $variant): ?int {}
 
@@ -54,31 +54,31 @@ function variant_set_type(variant $variant, int $type): void {}
 
 function variant_cast(variant $variant, int $type): variant {}
 
-function com_get_active_object(string $prog_id, ?int $code_page = null): variant {}
+function com_get_active_object(string $prog_id, ?int $codepage = null): variant {}
 
 function com_create_guid(): string|false {}
 
 function com_event_sink(variant $variant, object $sink_object, array|string|null $sink_interface = null): bool {}
 
-function com_print_typeinfo(variant|string $variant, ?string $disp_interface = null, bool $want_sink = false): bool {}
+function com_print_typeinfo(variant|string $variant, ?string $dispatch_interface = null, bool $display_sink = false): bool {}
 
-function com_message_pump(int $timeoutms = 0): bool {}
+function com_message_pump(int $timeout_milliseconds  = 0): bool {}
 
 function com_load_typelib(string $typelib, bool $case_insensitive = true): bool {}
 
 class variant
 {
-    public function __construct(mixed $value = null, int $type = VT_EMPTY, int $code_page = CP_ACP) {}
+    public function __construct(mixed $value = null, int $type = VT_EMPTY, int $codepage = CP_ACP) {}
 }
 
 class com
 {
-    public function __construct(string $module_name, array|string|null $server_name = null, int $code_page = CP_ACP, string $typelib = "") {}
+    public function __construct(string $module_name, array|string|null $server_name = null, int $codepage = CP_ACP, string $typelib = "") {}
 }
 
 #if HAVE_MSCOREE_H
 class dotnet
 {
-    public function __construct(string $assembly_name, string $datatype_name, int $code_page = CP_ACP) {}
+    public function __construct(string $assembly_name, string $datatype_name, int $codepage = CP_ACP) {}
 }
 #endif
