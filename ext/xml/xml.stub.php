2a1d3e8388306e84a0b990988dce9f34e6c39025
Nikita Popov <nikita.ppv@gmail.com>
2020-10-08 10:01:23 +0200

    Update ext/xml parameter names

--- ext/xml/xml.stub.php
+++ ext/xml/xml.stub.php
@@ -4,41 +4,41 @@
 
 function xml_parser_create(?string $encoding = null): XmlParser {}
 
-function xml_parser_create_ns(?string $encoding = null, string $sep = ":"): XmlParser {}
+function xml_parser_create_ns(?string $encoding = null, string $separator = ":"): XmlParser {}
 
-function xml_set_object(XmlParser $parser, object $obj): bool {}
+function xml_set_object(XmlParser $parser, object $object): bool {}
 
 /**
- * @param callable $shdl
- * @param callable $ehdl
+ * @param callable $start_handler
+ * @param callable $end_handler
  */
-function xml_set_element_handler(XmlParser $parser, $shdl, $ehdl): bool {}
+function xml_set_element_handler(XmlParser $parser, $start_handler, $end_handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_character_data_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_character_data_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_processing_instruction_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_processing_instruction_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_default_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_default_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_unparsed_entity_decl_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_unparsed_entity_decl_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_notation_decl_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_notation_decl_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_external_entity_ref_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_external_entity_ref_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_start_namespace_decl_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_start_namespace_decl_handler(XmlParser $parser, $handler): bool {}
 
-/** @param callable $hdl */
-function xml_set_end_namespace_decl_handler(XmlParser $parser, $hdl): bool {}
+/** @param callable $handler */
+function xml_set_end_namespace_decl_handler(XmlParser $parser, $handler): bool {}
 
-function xml_parse(XmlParser $parser, string $data, bool $isfinal = false): int {}
+function xml_parse(XmlParser $parser, string $data, bool $is_final = false): int {}
 
 /**
  * @param array $values
@@ -48,7 +48,7 @@ function xml_parse_into_struct(XmlParser $parser, string $data, &$values, &$inde
 
 function xml_get_error_code(XmlParser $parser): int {}
 
-function xml_error_string(int $code): ?string {}
+function xml_error_string(int $error_code): ?string {}
 
 function xml_get_current_line_number(XmlParser $parser): int {}
 
--- ext/xml/xml_arginfo.h
+++ ext/xml/xml_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 5be46eaeaea40f3494a6bbad269f85256030f758 */
+ * Stub hash: 591ba6543081654cbf55945d2a446e7039a81372 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_xml_parser_create, 0, 0, XmlParser, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, encoding, IS_STRING, 1, "null")
@@ -7,23 +7,23 @@ ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_xml_parser_create_ns, 0, 0, XmlParser, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, encoding, IS_STRING, 1, "null")
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, sep, IS_STRING, 0, "\":\"")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, separator, IS_STRING, 0, "\":\"")
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_set_object, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_INFO(0, parser, XmlParser, 0)
-	ZEND_ARG_TYPE_INFO(0, obj, IS_OBJECT, 0)
+	ZEND_ARG_TYPE_INFO(0, object, IS_OBJECT, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_set_element_handler, 0, 3, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_INFO(0, parser, XmlParser, 0)
-	ZEND_ARG_INFO(0, shdl)
-	ZEND_ARG_INFO(0, ehdl)
+	ZEND_ARG_INFO(0, start_handler)
+	ZEND_ARG_INFO(0, end_handler)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_set_character_data_handler, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_INFO(0, parser, XmlParser, 0)
-	ZEND_ARG_INFO(0, hdl)
+	ZEND_ARG_INFO(0, handler)
 ZEND_END_ARG_INFO()
 
 #define arginfo_xml_set_processing_instruction_handler arginfo_xml_set_character_data_handler
@@ -43,7 +43,7 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_parse, 0, 2, IS_LONG, 0)
 	ZEND_ARG_OBJ_INFO(0, parser, XmlParser, 0)
 	ZEND_ARG_TYPE_INFO(0, data, IS_STRING, 0)
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, isfinal, _IS_BOOL, 0, "false")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, is_final, _IS_BOOL, 0, "false")
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_parse_into_struct, 0, 3, IS_LONG, 0)
@@ -58,7 +58,7 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_get_error_code, 0, 1, IS_LON
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_xml_error_string, 0, 1, IS_STRING, 1)
-	ZEND_ARG_TYPE_INFO(0, code, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, error_code, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 #define arginfo_xml_get_current_line_number arginfo_xml_get_error_code
