99320dfced730895d62bc0080ecb2d0366422948
Alex Dowad <alexinbeijing@gmail.com>
2020-10-08 22:34:29 +0200

    Stricter handling of erroneous input when converting CP5022{0,1,2} text encoding

--- ext/mbstring/libmbfl/filters/mbfilter_cp5022x.c
+++ ext/mbstring/libmbfl/filters/mbfilter_cp5022x.c
@@ -264,10 +264,6 @@ retry:
 				}
 			}
 			(*filter->output_function)(w, filter->data);
-		} else if (c == 0x1b) {
-			filter->status += 2;
-		} else if ((c >= 0 && c < 0x21) || c == 0x7f) {		/* CTLs */
-			(*filter->output_function)(c, filter->data);
 		} else {
 			w = (c1 << 8) | c;
 			w &= MBFL_WCSGROUP_MASK;
