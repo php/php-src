eef994d6211ae96b108caccd77669336f4ec46e8
Máté Kocsis <kocsismate@woohoolabs.com>
2020-10-09 23:02:14 +0200

    Improve parameter names in ext/pdo_sqlite
    Closes GH-6310

--- ext/pdo_sqlite/sqlite_driver.stub.php
+++ ext/pdo_sqlite/sqlite_driver.stub.php
@@ -5,10 +5,10 @@
 // These are extension methods for PDO. This is not a real class.
 class PDO_SQLite_Ext {
     /** @return bool */
-    public function sqliteCreateFunction(string $function_name, callable $callback, int $num_args = -1, int $flags = 0) {}
+    public function sqliteCreateFunction(string $name, callable $callback, int $numArgs = -1, int $flags = 0) {}
 
     /** @return bool */
-    public function sqliteCreateAggregate(string $function_name, callable $step_func, callable $finalize_func, int $num_args = -1) {}
+    public function sqliteCreateAggregate(string $name, callable $step, callable $finalize, int $numArgs = -1) {}
 
     /** @return bool */
     public function sqliteCreateCollation(string $name, callable $callback) {}
