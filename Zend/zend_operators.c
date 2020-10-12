3c4dd73c023e4aea317f774e045fdccc644f24b5
Nikita Popov <nikita.ppv@gmail.com>
2020-10-12 11:22:39 +0200

    Detect self-addition of array more accurately
    While the zvals may be different, they may still point to the
    same array.
    
    Fixes oss-fuzz #26245.

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -903,7 +903,7 @@ try_again:
 
 static zend_never_inline void ZEND_FASTCALL add_function_array(zval *result, zval *op1, zval *op2) /* {{{ */
 {
-	if ((result == op1) && (result == op2)) {
+	if (result == op1 && Z_ARR_P(op1) == Z_ARR_P(op2)) {
 		/* $a += $a */
 		return;
 	}
