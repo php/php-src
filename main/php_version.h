6f2cdb40fa98b5866b6244c5a2bc59214d87dc1d
Derick Rethans <github@derickrethans.nl>
2020-10-13 11:19:37 +0000

    Update versions for PHP 7.4.12RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 11
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.11-dev"
-#define PHP_VERSION_ID 70411
+#define PHP_RELEASE_VERSION 12
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "7.4.12RC1"
+#define PHP_VERSION_ID 70412
