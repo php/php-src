44bd5804b00b6d37da266c0401fde3ac3476c6d9
Alex Dowad <alexinbeijing@gmail.com>
2020-10-18 15:12:11 +0200

    Fix mbstring support for CP1251 encoding
    - Identify filter was as wrong as wrong can be.
    - Invalid CP1251 byte 0x98 was converted to Unicode 0xFFFD (generic
      replacement character), rather than respecting `mb_substitute_character`.
    - Unicode 0xFFFD was converted to some random CP1251 byte.
    - When converting CP1251 to CP1251, don't pass invalid bytes through silently.

--- ext/mbstring/libmbfl/filters/mbfilter_cp1251.c
+++ ext/mbstring/libmbfl/filters/mbfilter_cp1251.c
@@ -74,74 +74,45 @@ const struct mbfl_convert_vtbl vtbl_cp1251_wchar = {
 
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)
 
-/*
- * cp1251 => wchar
- */
-int
-mbfl_filt_conv_cp1251_wchar(int c, mbfl_convert_filter *filter)
+int mbfl_filt_conv_cp1251_wchar(int c, mbfl_convert_filter *filter)
 {
 	int s;
 
-	if (c >= 0 && c < cp1251_ucs_table_min) {
+	if (c < cp1251_ucs_table_min) {
 		s = c;
-	} else if (c >= cp1251_ucs_table_min && c < 0x100) {
-		s = cp1251_ucs_table[c - cp1251_ucs_table_min];
-		if (s <= 0) {
-			s = c;
-			s &= MBFL_WCSPLANE_MASK;
-			s |= MBFL_WCSPLANE_CP1251;
-		}
 	} else {
-		s = c;
-		s &= MBFL_WCSGROUP_MASK;
-		s |= MBFL_WCSGROUP_THROUGH;
+		s = cp1251_ucs_table[c - cp1251_ucs_table_min];
+		if (!s) {
+			s = c | MBFL_WCSGROUP_THROUGH;
+		}
 	}
 
 	CK((*filter->output_function)(s, filter->data));
-
 	return c;
 }
 
-/*
- * wchar => cp1251
- */
-int
-mbfl_filt_conv_wchar_cp1251(int c, mbfl_convert_filter *filter)
+int mbfl_filt_conv_wchar_cp1251(int c, mbfl_convert_filter *filter)
 {
-	int s, n;
-
 	if (c < 0x80) {
-		s = c;
+		CK((*filter->output_function)(c, filter->data));
 	} else {
-		s = -1;
-		n = cp1251_ucs_table_len-1;
-		while (n >= 0) {
+		for (int n = 0; n < cp1251_ucs_table_len; n++) {
 			if (c == cp1251_ucs_table[n]) {
-				s = cp1251_ucs_table_min + n;
-				break;
+				CK((*filter->output_function)(cp1251_ucs_table_min + n, filter->data));
+				return c;
 			}
-			n--;
 		}
-		if (s <= 0 && (c & ~MBFL_WCSPLANE_MASK) == MBFL_WCSPLANE_CP1251) {
-			s = c & MBFL_WCSPLANE_MASK;
-		}
-	}
-
-	if (s >= 0) {
-		CK((*filter->output_function)(s, filter->data));
-	} else {
 		CK(mbfl_filt_conv_illegal_output(c, filter));
 	}
 
 	return c;
 }
 
-/* all of this is so ugly now! */
 static int mbfl_filt_ident_cp1251(int c, mbfl_identify_filter *filter)
 {
-	if (c >= 0x80 && c <= 0xff)
-		filter->flag = 0;
-	else
-		filter->flag = 1; /* not it */
+	/* Only one byte in this single-byte encoding is not used */
+	if (c == 0x98) {
+		filter->flag = 1;
+	}
 	return c;
 }
