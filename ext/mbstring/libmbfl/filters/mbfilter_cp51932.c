6cbeb6476e9a5aed7b1630c8186c9fcddf8286a0
Alex Dowad <alexinbeijing@gmail.com>
2020-10-18 07:31:43 +0200

    Remove unused macros from mbfilter_cp51932.c, mbfilter_iso2022jp_mobile.c

--- ext/mbstring/libmbfl/filters/mbfilter_cp51932.c
+++ ext/mbstring/libmbfl/filters/mbfilter_cp51932.c
@@ -90,13 +90,6 @@ const struct mbfl_convert_vtbl vtbl_wchar_cp51932 = {
 
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)
 
-#define sjistoidx(c1, c2) \
-        (((c1) > 0x9f) \
-        ? (((c1) - 0xc1) * 188 + (c2) - (((c2) > 0x7e) ? 0x41 : 0x40)) \
-        : (((c1) - 0x81) * 188 + (c2) - (((c2) > 0x7e) ? 0x41 : 0x40)))
-#define idxtoeuc1(c) (((c) / 94) + 0xa1)
-#define idxtoeuc2(c) (((c) % 94) + 0xa1)
-
 /*
  * cp51932 => wchar
  */
--- ext/mbstring/libmbfl/filters/mbfilter_iso2022jp_mobile.c
+++ ext/mbstring/libmbfl/filters/mbfilter_iso2022jp_mobile.c
@@ -72,13 +72,6 @@ const struct mbfl_convert_vtbl vtbl_wchar_2022jp_kddi = {
 
 #define CK(statement)	do { if ((statement) < 0) return (-1); } while (0)
 
-#define sjistoidx(c1, c2) \
-        (((c1) > 0x9f) \
-        ? (((c1) - 0xc1) * 188 + (c2) - (((c2) > 0x7e) ? 0x41 : 0x40)) \
-        : (((c1) - 0x81) * 188 + (c2) - (((c2) > 0x7e) ? 0x41 : 0x40)))
-#define idxtojis1(c) (((c) / 94) + 0x21)
-#define idxtojis2(c) (((c) % 94) + 0x21)
-
 #define SJIS_ENCODE(c1,c2,s1,s2)	\
 		do {						\
 			s1 = c1;				\
