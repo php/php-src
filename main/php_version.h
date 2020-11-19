3c44e9c080d91bf78061d2d90bb1df3d31dc3d95
Sara Golemon <pollita@php.net>
2020-11-19 14:37:44 +0000

    8.0 branch should be prepping for 8.0.1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 0
+#define PHP_RELEASE_VERSION 1
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.0.0-dev"
-#define PHP_VERSION_ID 80000
+#define PHP_VERSION "8.0.1-dev"
+#define PHP_VERSION_ID 80001
