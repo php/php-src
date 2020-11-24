fa713ec17ba6c1b7cbf4c00b121f34a1a6f77919
Nikita Popov <nikita.ppv@gmail.com>
2020-11-24 13:00:53 +0100

    Fixed bug #80362: Running dtrace scripts can cause php to crash
    Patch by al at coralnet dot name.

--- Zend/zend.c
+++ Zend/zend.c
@@ -1309,7 +1309,9 @@ static ZEND_COLD void zend_error_va_list(
 #ifdef HAVE_DTRACE
 	if (DTRACE_ERROR_ENABLED()) {
 		char *dtrace_error_buffer;
-		zend_vspprintf(&dtrace_error_buffer, 0, format, args);
+		va_copy(usr_copy, args);
+		zend_vspprintf(&dtrace_error_buffer, 0, format, usr_copy);
+		va_end(usr_copy);
 		DTRACE_ERROR(dtrace_error_buffer, (char *)error_filename, error_lineno);
 		efree(dtrace_error_buffer);
 	}
