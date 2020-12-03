b0822c4555869de4a2c8d0310ffb998d17824d22
Bradley Jarvis <brad@pocketinnovations.com.au>
2020-12-03 12:12:17 +1100

    Update openssl.c
    check that EVP_CIPH_OCB_MODE is also defined, this fixes libressl support which does not include OCB

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -6496,7 +6496,7 @@ static void php_openssl_load_cipher_mode(struct php_openssl_cipher_mode *mode, c
 	int cipher_mode = EVP_CIPHER_mode(cipher_type);
 	memset(mode, 0, sizeof(struct php_openssl_cipher_mode));
 	switch (cipher_mode) {
-#if PHP_OPENSSL_API_VERSION >= 0x10100
+#if PHP_OPENSSL_API_VERSION >= 0x10100 && defined(EVP_CIPH_OCB_MODE)
 		case EVP_CIPH_GCM_MODE:
 		case EVP_CIPH_OCB_MODE:
 		case EVP_CIPH_CCM_MODE:
