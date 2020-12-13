23590f7c5348de4f8b69da81439b7c42c7ba084c
Anatol Belski <ab@php.net>
2020-12-13 17:40:46 +0100

    hash: Implement xxHash
    The implementation bundles the xxHash v0.8.0 release and includes all the variants
    
    - xxh32, 32-bit wide
    - xxh64, 64-bit wide
    - xxh3, 64-bit wide
    - xxh128, 128-bit wide
    
    An initial hash state can be passed through the options arrray. An additional
    functionality not targeted in this implementation is the secret support in xxh3
    and xxh128. That can be added at a later point.
    
    The serialization for xxh3 and xxh128 should not be implemented, as the
    state would contain the secret. Despite the xxHash is a non crypto
    algorithm, the secret would be serialized as plain text which would be
    insecure.
    
    Closes GH-6524
    
    Signed-off-by: Anatol Belski <ab@php.net>

--- ext/hash/php_hash.h
+++ ext/hash/php_hash.h
@@ -108,6 +108,10 @@ extern const php_hash_ops php_hash_joaat_ops;
 extern const php_hash_ops php_hash_murmur3a_ops;
 extern const php_hash_ops php_hash_murmur3c_ops;
 extern const php_hash_ops php_hash_murmur3f_ops;
+extern const php_hash_ops php_hash_xxh32_ops;
+extern const php_hash_ops php_hash_xxh64_ops;
+extern const php_hash_ops php_hash_xxh3_64_ops;
+extern const php_hash_ops php_hash_xxh3_128_ops;
 
 #define PHP_HASH_HAVAL_OPS(p,b)	extern const php_hash_ops php_hash_##p##haval##b##_ops;
 
