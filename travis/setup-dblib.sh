6ae65c4369825a63b0475f607325283d2abdf07a
Adam Baratz <adambaratz@php.net>
2021-01-02 12:16:02 -0500

    Mark as executable

--- travis/setup-dblib.sh
+++ travis/setup-dblib.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -ev
+
 # Microsoft provides a Docker image for SQL Server 2019 on Linux
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=<YourStrong@Passw0rd>" -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:2019-CU3-ubuntu-18.04
 
