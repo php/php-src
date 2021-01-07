3460af5262e268fb6f5150c33b38ae6ad42d9b95
Nikita Popov <nikita.ppv@gmail.com>
2021-01-07 14:26:42 +0100

    Fold dirname in sccp for non-windows platforms

--- ext/opcache/Optimizer/sccp.c
+++ ext/opcache/Optimizer/sccp.c
@@ -797,6 +797,10 @@ static zend_bool can_ct_eval_func_call(zend_string *name, uint32_t num_args, zva
 		|| zend_string_equals_literal(name, "array_values")
 		|| zend_string_equals_literal(name, "base64_decode")
 		|| zend_string_equals_literal(name, "base64_encode")
+#ifndef ZEND_WIN32
+		/* On Windows this function may be code page dependent. */
+		|| zend_string_equals_literal(name, "dirname")
+#endif
 		|| zend_string_equals_literal(name, "imagetypes")
 		|| zend_string_equals_literal(name, "in_array")
 		|| zend_string_equals_literal(name, "ltrim")
