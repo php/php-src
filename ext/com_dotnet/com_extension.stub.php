bff7761653ab44c848e71f9d9727c7801588a50c
Christoph M. Becker <cmbecker69@gmx.de>
2021-01-24 16:06:49 +0100

    Add missing stub for com_exception

--- ext/com_dotnet/com_extension.stub.php
+++ ext/com_dotnet/com_extension.stub.php
@@ -82,3 +82,7 @@ class dotnet
     public function __construct(string $assembly_name, string $datatype_name, int $codepage = CP_ACP) {}
 }
 #endif
+
+final class com_exception extends exception
+{
+}
