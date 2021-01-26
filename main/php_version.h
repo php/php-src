94af11d5e169451e4f18c60ee8e3ac84b066b589
Christoph M. Becker <cmbecker69@gmx.de>
2021-01-26 19:07:57 +0100

    Update version
    That appears to have been forgotten for a while.

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 13
+#define PHP_RELEASE_VERSION 16
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.13-dev"
-#define PHP_VERSION_ID 70413
+#define PHP_VERSION "7.4.16-dev"
+#define PHP_VERSION_ID 70416
