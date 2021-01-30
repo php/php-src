559fc46b9fcdb014b0ac43ff0228f663ce78ccb0
David CARLIER <devnexen@gmail.com>
2021-01-30 09:47:52 +0000

    Fix ubsan warning on macos
    Closes GH-6652.

--- ext/opcache/shared_alloc_mmap.c
+++ ext/opcache/shared_alloc_mmap.c
@@ -50,7 +50,7 @@ static int create_segments(size_t requested_size, zend_shared_segment ***shared_
 #endif
 #ifdef VM_MAKE_TAG
 	/* allows tracking segments via tools such as vmmap */
-	fd = VM_MAKE_TAG(251);
+	fd = VM_MAKE_TAG(251U);
 #endif
 #ifdef MAP_HUGETLB
 	size_t huge_page_size = 2 * 1024 * 1024;
