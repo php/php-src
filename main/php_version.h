8c6db654e7109a7219f209cf38c55d16f6db5e18
Sara Golemon <pollita@php.net>
2021-02-17 15:24:36 +0000

    Prep for 8.0.4

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 3
+#define PHP_RELEASE_VERSION 4
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.0.3-dev"
-#define PHP_VERSION_ID 80003
+#define PHP_VERSION "8.0.4-dev"
+#define PHP_VERSION_ID 80004
