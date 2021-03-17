c7fadd23282b105adbdd17692734bc2940831d62
Christoph M. Becker <cmbecker69@gmx.de>
2021-03-17 12:12:01 +0100

    8.0.5 is next
    We also move the NEWS entry for bug 80847 to the proper version.

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 4
+#define PHP_RELEASE_VERSION 5
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.0.4-dev"
-#define PHP_VERSION_ID 80004
+#define PHP_VERSION "8.0.5-dev"
+#define PHP_VERSION_ID 80005
