f60dc3bd3409c6724c48b518dbdd3725d667cbdd
Anatol Belski <ab@php.net>
2021-04-05 21:23:04 +0200

    fileinfo: More VS compat
    Signed-off-by: Anatol Belski <ab@php.net>

--- ext/fileinfo/libmagic/funcs.c
+++ ext/fileinfo/libmagic/funcs.c
@@ -779,6 +779,7 @@ file_print_guid(char *str, size_t len, const uint64_t *guid)
 	    g->data4[6], g->data4[7]);
 }
 
+#if 0
 protected int
 file_pipe_closexec(int *fds)
 {
@@ -797,6 +798,7 @@ protected int
 file_clear_closexec(int fd) {
 	return fcntl(fd, F_SETFD, 0);
 }
+#endif
 
 protected char *
 file_strtrim(char *str)
