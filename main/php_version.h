0a181ca27ac4ad6fe9ed796e704dc13c1d0a016a
Christoph M. Becker <cmbecker69@gmx.de>
2021-04-07 12:45:21 +0200

    Update version in main/php_version.h as well
    Otherwise the file is modified by each new build, what is confusing.

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 17
+#define PHP_RELEASE_VERSION 18
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.17-dev"
-#define PHP_VERSION_ID 70417
+#define PHP_VERSION "7.4.18-dev"
+#define PHP_VERSION_ID 70418
