e50fe2f13677788ea814fdcb99733640c148786d
Kamil Tekiela <tekiela246@gmail.com>
2021-04-09 21:43:00 +0100

    remove aligment

--- ext/mysqlnd/mysqlnd_ps.h
+++ ext/mysqlnd/mysqlnd_ps.h
@@ -24,8 +24,8 @@ typedef void (*ps_field_fetch_func)(zval * zv, const MYSQLND_FIELD * const field
 struct st_mysqlnd_perm_bind {
 	ps_field_fetch_func func;
 	/* should be signed int */
-	int					        pack_len;
-	unsigned int		    php_type;
+	int pack_len;
+	unsigned int php_type;
 };
 
 extern struct st_mysqlnd_perm_bind mysqlnd_ps_fetch_functions[MYSQL_TYPE_LAST + 1];
