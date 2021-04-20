bc7aacbbdd0b03c52d5bc469d3bb8bd77fa06d7c
thinkverse <hallberg.kim@gmail.com>
2021-04-20 11:48:59 +0200

    Fix clamp function error message

--- ext/standard/math.c
+++ ext/standard/math.c
@@ -244,7 +244,7 @@ PHP_FUNCTION(clamp)
 	is_smaller_function(&check, zmax, zmin);
 
 	if (Z_TYPE(check) == IS_TRUE) {
-		zend_argument_value_error(2, "cannot be greater than ($max)");
+		zend_argument_value_error(2, "cannot be greater than Argument #3 ($max)");
 		RETURN_THROWS();
 	}
 
