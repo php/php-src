25e46f182ae04df1b4ec5ec5be0e755c197e5ccf
George Peter Banyard <girgias@php.net>
2021-04-26 01:46:53 +0100

    Attempt to fix JIT

--- ext/opcache/jit/zend_jit.c
+++ ext/opcache/jit/zend_jit.c
@@ -2396,7 +2396,8 @@ static int zend_jit(const zend_op_array *op_array, zend_ssa *ssa, const zend_op 
 						}
 						if (!(op1_info & MAY_BE_LONG)
 						 || !(op2_info & MAY_BE_LONG)) {
-							break;
+						 	/* Might emit deprecation notices */
+						 	goto jit_failure;
 						}
 						res_addr = RES_REG_ADDR();
 						if (Z_MODE(res_addr) != IS_REG
@@ -2562,7 +2563,8 @@ static int zend_jit(const zend_op_array *op_array, zend_ssa *ssa, const zend_op 
 						 || opline->extended_value == ZEND_MOD) {
 							if (!(op1_info & MAY_BE_LONG)
 							 || !(op2_info & MAY_BE_LONG)) {
-								break;
+								/* Might emit deprecation notices */
+								goto jit_failure;
 							}
 						} else if (opline->extended_value == ZEND_CONCAT) {
 							if (!(op1_info & MAY_BE_STRING)
