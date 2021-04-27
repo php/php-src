98c8ad92182e06aa0331f38a7884b6f6b427040e
Christoph M. Becker <cmbecker69@gmx.de>
2021-04-27 16:55:20 +0200

    7.3.29 is next

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 28
+#define PHP_RELEASE_VERSION 29
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.3.28-dev"
-#define PHP_VERSION_ID 70328
+#define PHP_VERSION "7.3.29-dev"
+#define PHP_VERSION_ID 70329
