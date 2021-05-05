592ac3aa218c4553e8f16609211e4e35ad23bddf
Ben Morss <morss@google.com>
2021-05-05 18:47:22 -0400

    Initial commit before rebase

--- ext/gd/gd.stub.php
+++ ext/gd/gd.stub.php
@@ -68,6 +68,10 @@ function imagecreatefromstring(string $data): GdImage|false {}
 
 function imagecreatefromgif(string $filename): GdImage|false {}
 
+#ifdef HAVE_GD_AVIF
+function imagecreatefromavif(string $filename): GdImage|false {}
+#endif
+
 #ifdef HAVE_GD_JPG
 function imagecreatefromjpeg(string $filename): GdImage|false {}
 #endif
