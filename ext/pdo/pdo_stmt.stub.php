09d8c7cb98d093ff367c1703eadbf2b91c33d50d
Nikita Popov <nikita.ppv@gmail.com>
2021-05-06 10:15:33 +0200

    Reset to 27d40da95d8cc301ba8479734ae4bcff595bccd1

--- ext/pdo/pdo_stmt.stub.php
+++ ext/pdo/pdo_stmt.stub.php
@@ -43,7 +43,7 @@ class PDOStatement implements IteratorAggregate
     public function fetchColumn(int $column = 0) {}
 
     /** @return object|false */
-    public function fetchObject(?string $class = "stdClass", array $constructorArgs = []) {}
+    public function fetchObject(?string $class = "stdClass", ?array $ctorArgs = null) {}
 
     /** @return mixed */
     public function getAttribute(int $name) {}
