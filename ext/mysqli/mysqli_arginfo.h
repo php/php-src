4def97680689ec048a8260a25b547403697c964b
Máté Kocsis <kocsismate@woohoolabs.com>
2021-05-06 15:20:58 +0200

    Fix return type of mysqli::init()

--- ext/mysqli/mysqli_arginfo.h
+++ ext/mysqli/mysqli_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 36c7ee22aa4c5bb5e0174031a3defcaaadde0dbd */
+ * Stub hash: c2ab263780902447855ecb0d8a9e3432577a32d1 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_mysqli_affected_rows, 0, 1, MAY_BE_LONG|MAY_BE_STRING)
 	ZEND_ARG_OBJ_INFO(0, mysql, mysqli, 0)
