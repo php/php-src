7e08fe6f58b34092078443e65e2fc21b982c94ef
Ben Morss <morss@google.com>
2021-05-11 19:08:23 -0400

    Change gd includes to quotes instead of angle brackets
    To avoid possible collision with the original libgd header files, which may be installed somewhere common like /usr/local/include

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -30,30 +30,10 @@
 #include "ext/standard/head.h"
 #include <math.h>
 #include "SAPI.h"
-#include "php_gd.h"
 #include "ext/standard/info.h"
 #include "php_open_temporary_file.h"
 #include "zend_object_handlers.h"
 #include "zend_interfaces.h"
-
-#ifdef HAVE_GD_BUNDLED
-# include "libgd/gd.h"
-# include "libgd/gd_errors.h"
-# include "libgd/gdfontt.h"  /* 1 Tiny font */
-# include "libgd/gdfonts.h"  /* 2 Small font */
-# include "libgd/gdfontmb.h" /* 3 Medium bold font */
-# include "libgd/gdfontl.h"  /* 4 Large font */
-# include "libgd/gdfontg.h"  /* 5 Giant font */
-#else
-# include <gd.h>
-# include <gd_errors.h>
-# include <gdfontt.h>  /* 1 Tiny font */
-# include <gdfonts.h>  /* 2 Small font */
-# include <gdfontmb.h> /* 3 Medium bold font */
-# include <gdfontl.h>  /* 4 Large font */
-# include <gdfontg.h>  /* 5 Giant font */
-#endif
-
 
 #ifdef HAVE_SYS_WAIT_H
 # include <sys/wait.h>
