5dd539b866c6aadb9ce054bfba0f38d9eb74ec14
Dmitry Stogov <dmitry@zend.com>
2021-05-12 15:47:14 +0300

    Fixed JIT memory usage debug info (opcache.jit_debug=0x200)

--- ext/opcache/jit/zend_jit.c
+++ ext/opcache/jit/zend_jit.c
@@ -4407,7 +4407,7 @@ ZEND_EXT_API int zend_jit_startup(void *buf, size_t size, bool reattached)
 ZEND_EXT_API void zend_jit_shutdown(void)
 {
 	if (JIT_G(debug) & ZEND_JIT_DEBUG_SIZE) {
-		fprintf(stderr, "\nJIT memory usage: %td\n", (char*)*dasm_ptr - (char*)dasm_buf);
+		fprintf(stderr, "\nJIT memory usage: %td\n", (ptrdiff_t)((char*)*dasm_ptr - (char*)dasm_buf));
 	}
 
 #ifdef HAVE_OPROFILE
