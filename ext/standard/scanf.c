19332883013b6d6b3a83c278e95cfa60bfd92f97
George Peter Banyard <girgias@php.net>
2021-05-12 14:41:11 +0100

    Fix [-Wstrict-prototypes] in standard/scanf.c

--- ext/standard/scanf.c
+++ ext/standard/scanf.c
@@ -583,7 +583,7 @@ PHPAPI int php_sscanf_internal( char *string, char *format,
 	int  base = 0;
 	int  underflow = 0;
 	size_t width;
-	zend_long (*fn)() = NULL;
+	zend_long (*fn)(const char*, char**, int) = NULL;
 	char *ch, sch;
 	int  flags;
 	char buf[64];	/* Temporary buffer to hold scanned number
@@ -740,29 +740,29 @@ literal:
 			case 'D':
 				op = 'i';
 				base = 10;
-				fn = (zend_long (*)())ZEND_STRTOL_PTR;
+				fn = (zend_long (*)(const char*, char**, int))ZEND_STRTOL_PTR;
 				break;
 			case 'i':
 				op = 'i';
 				base = 0;
-				fn = (zend_long (*)())ZEND_STRTOL_PTR;
+				fn = (zend_long (*)(const char*, char**, int))ZEND_STRTOL_PTR;
 				break;
 			case 'o':
 				op = 'i';
 				base = 8;
-				fn = (zend_long (*)())ZEND_STRTOL_PTR;
+				fn = (zend_long (*)(const char*, char**, int))ZEND_STRTOL_PTR;
 				break;
 			case 'x':
 			case 'X':
 				op = 'i';
 				base = 16;
-				fn = (zend_long (*)())ZEND_STRTOL_PTR;
+				fn = (zend_long (*)(const char*, char**, int))ZEND_STRTOL_PTR;
 				break;
 			case 'u':
 				op = 'i';
 				base = 10;
 				flags |= SCAN_UNSIGNED;
-				fn = (zend_long (*)())ZEND_STRTOUL_PTR;
+				fn = (zend_long (*)(const char*, char**, int))ZEND_STRTOUL_PTR;
 				break;
 
 			case 'f':
