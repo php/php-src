ee9e07541f9f07762e3ee781102eea3a4190787c
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-21 15:00:41 +0200

    Fix #76359: open_basedir bypass through adding ".."
    We explicitly forbid adding paths with a leading `..` to `open_basedir`
    at runtime.
    
    Closes GH-7024.

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -110,6 +110,11 @@ PHPAPI ZEND_INI_MH(OnUpdateBaseDir)
 			*end = '\0';
 			end++;
 		}
+		if (ptr[0] == '.' && ptr[1] == '.' && (ptr[2] == '\0' || IS_SLASH(ptr[2]))) {
+			/* Don't allow paths with a leading .. path component to be set at runtime */
+			efree(pathbuf);
+			return FAILURE;
+		}
 		if (php_check_open_basedir_ex(ptr, 0) != 0) {
 			/* At least one portion of this open_basedir is less restrictive than the prior one, FAIL */
 			efree(pathbuf);
