a8422d7a82dcf8401cf76626aaf55fed5c407160
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-21 12:17:33 +0200

    Fix #76359: open_basedir bypass through adding ".."
    We explicitly forbid adding `..` to `open_basedir`at runtime.

--- main/fopen_wrappers.c
+++ main/fopen_wrappers.c
@@ -110,6 +110,11 @@ PHPAPI ZEND_INI_MH(OnUpdateBaseDir)
 			*end = '\0';
 			end++;
 		}
+		if (ptr[0] == '.' && ptr[1] == '.' && ptr[2] == '\0') {
+			/* Don't allow .. to be set at runtime */
+			efree(pathbuf);
+			return FAILURE;
+		}
 		if (php_check_open_basedir_ex(ptr, 0) != 0) {
 			/* At least one portion of this open_basedir is less restrictive than the prior one, FAIL */
 			efree(pathbuf);
