59522ba968d9e736c8c1bc80aa96c5d336b4f979
Christoph M. Becker <cmbecker69@gmx.de>
2021-05-22 15:42:57 +0200

    Update version in php_version.h as well
    The version there is automatically updated during `./configure`, but
    not on Windows.

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 20
+#define PHP_RELEASE_VERSION 21
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.20-dev"
-#define PHP_VERSION_ID 70420
+#define PHP_VERSION "7.4.21-dev"
+#define PHP_VERSION_ID 70421
