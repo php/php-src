187c56ae2d22ddd10faf3f03400d95b4f5201b56
Máté Kocsis <kocsismate@woohoolabs.com>
2021-05-26 12:09:54 +0200

    Update mysqli stub hash

--- ext/mysqli/mysqli_arginfo.h
+++ ext/mysqli/mysqli_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 5a2483f099fbebc795747e3b51402b2194020743 */
+ * Stub hash: 44c9bfee4b52160b69a83a457a3176b030046bb7 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_mysqli_affected_rows, 0, 1, MAY_BE_LONG|MAY_BE_STRING)
 	ZEND_ARG_OBJ_INFO(0, mysql, mysqli, 0)
