ab8f256877926cb2f52594b972dbf342de01c534
Máté Kocsis <kocsismate@woohoolabs.com>
2021-05-27 17:31:45 +0200

    Declare tentative return types for ext/snmp

--- ext/snmp/snmp.stub.php
+++ ext/snmp/snmp.stub.php
@@ -6,14 +6,14 @@ function snmpget(string $hostname, string $community, array|string $object_id, i
 
 function snmpgetnext(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): stdClass|array|string|bool {}
 
-function snmpwalk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+function snmpwalk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
-function snmprealwalk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+function snmprealwalk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
 /** @alias snmprealwalk */
-function snmpwalkoid(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+function snmpwalkoid(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
-function snmpset(string $hostname, string $community, array|string $object_id, array|string $type, array|string $value, int $timeout = -1, int $retries = -1): array|bool {}
+function snmpset(string $hostname, string $community, array|string $object_id, array|string $type, array|string $value, int $timeout = -1, int $retries = -1): bool {}
 
 function snmp_get_quick_print(): bool {}
 
@@ -30,11 +30,11 @@ function snmp2_get(string $hostname, string $community, array|string $object_id,
 
 function snmp2_getnext(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): stdClass|array|string|bool {}
 
-function snmp2_walk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+function snmp2_walk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
-function snmp2_real_walk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+function snmp2_real_walk(string $hostname, string $community, array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
-function snmp2_set(string $hostname, string $community, array|string $object_id, array|string $type, array|string $value, int $timeout = -1, int $retries = -1): array|bool {}
+function snmp2_set(string $hostname, string $community, array|string $object_id, array|string $type, array|string $value, int $timeout = -1, int $retries = -1): bool {}
 
 function snmp3_get(
     string $hostname, string $security_name, string $security_level,
@@ -52,20 +52,20 @@ function snmp3_walk(
     string $hostname, string $security_name, string $security_level,
     string $auth_protocol, string $auth_passphrase,
     string $privacy_protocol, string $privacy_passphrase,
-    array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+    array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
 function snmp3_real_walk(
     string $hostname, string $security_name, string $security_level,
     string $auth_protocol, string $auth_passphrase,
     string $privacy_protocol, string $privacy_passphrase,
-    array|string $object_id, int $timeout = -1, int $retries = -1): array|bool {}
+    array|string $object_id, int $timeout = -1, int $retries = -1): array|false {}
 
 function snmp3_set(
     string $hostname, string $security_name, string $security_level,
     string $auth_protocol, string $auth_passphrase,
     string $privacy_protocol, string $privacy_passphrase,
     array|string $object_id, array|string $type, array|string $value,
-    int $timeout = -1, int $retries = -1): array|bool {}
+    int $timeout = -1, int $retries = -1): bool {}
 
 function snmp_set_valueretrieval(int $method): bool {}
 
@@ -87,32 +87,32 @@ class SNMP
 
     public function __construct(int $version, string $hostname, string $community, int $timeout = -1, int $retries = -1) {}
 
-    /** @return bool */
-    public function close() {}
+    /** @tentative-return-type */
+    public function close(): bool {}
 
-    /** @return bool */
+    /** @tentative-return-type */
     public function setSecurity(
         string $securityLevel, string $authProtocol = "", string $authPassphrase = "",
         string $privacyProtocol = "", string $privacyPassphrase = "",
-        string $contextName = "", string $contextEngineId = "") {}
+        string $contextName = "", string $contextEngineId = ""): bool {}
 
-    /** @return array|bool */
-    public function get(array|string $objectId, bool $preserveKeys = false) {}
+    /** @tentative-return-type */
+    public function get(array|string $objectId, bool $preserveKeys = false): array|string|false {}
 
-    /** @return array|bool */
-    public function getnext(array|string $objectId) {}
+    /** @tentative-return-type */
+    public function getnext(array|string $objectId): array|string|false {}
 
-    /** @return array|bool */
-    public function walk(array|string $objectId, bool $suffixAsKey = false, int $maxRepetitions = -1, int $nonRepeaters = -1) {}
+    /** @tentative-return-type */
+    public function walk(array|string $objectId, bool $suffixAsKey = false, int $maxRepetitions = -1, int $nonRepeaters = -1): array|false {}
 
-    /** @return array|bool */
-    public function set(array|string $objectId, array|string $type, array|string $value) {}
+    /** @tentative-return-type */
+    public function set(array|string $objectId, array|string $type, array|string $value): bool {}
 
-    /** @return int */
-    public function getErrno() {}
+    /** @tentative-return-type */
+    public function getErrno(): int {}
 
-    /** @return string */
-    public function getError() {}
+    /** @tentative-return-type */
+    public function getError(): string {}
 }
 
 class SNMPException extends RuntimeException
