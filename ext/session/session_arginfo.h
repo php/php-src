6fb08f46077ea8cad9788eaa86ddf3bc4dbe358c
Máté Kocsis <kocsismate@woohoolabs.com>
2021-05-27 23:07:27 +0200

    Fix return type of SessionHandler::gc()
    Relates to GH-7006

--- ext/session/session_arginfo.h
+++ ext/session/session_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 88f428841bc3e12b949a3308d60eae80d87e563a */
+ * Stub hash: 4221e895bdb0c3e903b7688f79e2863fc0788cee */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_session_name, 0, 0, MAY_BE_STRING|MAY_BE_FALSE)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, name, IS_STRING, 1, "null")
