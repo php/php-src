514daed2bab150ba38873ed4586f7bd5f0e9b41f
Nikita Popov <nikita.ppv@gmail.com>
2021-05-28 13:08:25 +0200

    Add %0 format to run-tests.php
    This format matches against null bytes, and prevents the test
    expectation from being interpreted as binary data.
    
    bless_tests.php will automatically replace \0 with %0 as well.

--- run-tests.php
+++ run-tests.php
@@ -2627,6 +2627,7 @@ COMMAND $cmd
             $wanted_re = str_replace('%x', '[0-9a-fA-F]+', $wanted_re);
             $wanted_re = str_replace('%f', '[+-]?\.?\d+\.?\d*(?:[Ee][+-]?\d+)?', $wanted_re);
             $wanted_re = str_replace('%c', '.', $wanted_re);
+            $wanted_re = str_replace('%0', '\x00', $wanted_re);
             // %f allows two points "-.0.0" but that is the best *simple* expression
         }
 
