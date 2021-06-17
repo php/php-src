805ef2caee38fcc5c76b12464d0dbaa959008097
Patrick Allaert <patrickallaert@php.net>
2021-06-17 19:26:34 +0200

    Fix Zend License header inconsistencies
    * Mentioned URL does not exist anymore.
    * Zend cannot be contacted anymore using license@zend.com.
    
    Given that the Zend LICENSE file is still provided, removing those lines
    still respect the license.

--- Zend/zend_multibyte.h
+++ Zend/zend_multibyte.h
@@ -5,12 +5,8 @@
    | Copyright (c) Zend Technologies Ltd. (http://www.zend.com)           |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.00 of the Zend license,     |
-   | that is bundled with this package in the file LICENSE, and is        |
+   | that is bundled with this package in the file LICENSE.               |
    | available through the world-wide-web at                              |
-   | http://www.zend.com/license/2_00.txt.                                |
-   | If you did not receive a copy of the Zend license and are unable to  |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@zend.com so we can mail you a copy immediately.              |
    +----------------------------------------------------------------------+
    | Authors: Masaki Fujimoto <fujimoto@php.net>                          |
    |          Rui Hirokawa <hirokawa@php.net>                             |
