dd2191cd72dcfc153ff12ef8df321597ba53138f
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-28 22:42:08 +0200

    fix

--- appveyor/test_task.bat
+++ appveyor/test_task.bat
@@ -114,9 +114,9 @@ set TEST_PHP_JUNIT=c:\junit.out.xml
 cd "%APPVEYOR_BUILD_FOLDER%"
 dir ext\ffi\tests
 nmake test TESTS="%OPCACHE_OPTS% -q --offline --show-diff --show-slow 1000 --set-timeout 120 --temp-source c:\tests_tmp --temp-target c:\tests_tmp %PARALLEL%"
-dir ext\ffi\tests
 
 set EXIT_CODE=%errorlevel%
+dir ext\ffi\tests
 
 appveyor PushArtifact %TEST_PHP_JUNIT%
 
