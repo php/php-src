b420d09f4d5519931c4d0a805360d797cbc5ed17
Nikita Popov <nikita.ppv@gmail.com>
2021-07-20 14:44:14 +0200

    Fix type info for pg_socket()
    This was mistakenly changed to MAY_BE_OBJECT when pgsql was
    migrated to use objects. However, this particular function
    still returns a (stream) resource.

--- Zend/Optimizer/zend_func_info.c
+++ Zend/Optimizer/zend_func_info.c
@@ -690,7 +690,7 @@ static const func_info_t func_infos[] = {
 	F1("pg_get_result",							MAY_BE_FALSE | MAY_BE_OBJECT),
 	F1("pg_result_status",						MAY_BE_LONG | MAY_BE_STRING),
 	F1("pg_get_notify",							MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY),
-	F1("pg_socket",								MAY_BE_FALSE | MAY_BE_OBJECT),
+	F1("pg_socket",								MAY_BE_FALSE | MAY_BE_RESOURCE),
 	F1("pg_meta_data",							MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_STRING | MAY_BE_ARRAY_OF_ARRAY),
 	F1("pg_convert",							MAY_BE_FALSE | MAY_BE_ARRAY | MAY_BE_ARRAY_KEY_STRING | MAY_BE_ARRAY_OF_ANY),
 	F1("pg_insert",								MAY_BE_FALSE | MAY_BE_TRUE | MAY_BE_OBJECT | MAY_BE_STRING),
