00b0432e4f5741dbc26fef415682de229acbc7c7
lizj <1056159381@qq.com>
2021-08-02 14:23:01 +0800

    remove dir_len - (size_t) strlen(buf) > 1

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -1442,8 +1442,7 @@ static int php_plain_files_mkdir(php_stream_wrapper *wrapper, const char *dir, i
 		} else {
 		    /* split php_mkdir() into php_check_open_basedir() and VCWD_MKDIR() */
             if ((ret = php_check_open_basedir(dir)) < 0
-                ||
-                ((ret = VCWD_MKDIR(buf, (mode_t) mode)) < 0 && EEXIST != errno && dir_len - (size_t) strlen(buf) > 1)) {
+                || ((ret = VCWD_MKDIR(buf, (mode_t) mode)) < 0 && EEXIST != errno)) {
 		        php_error_docref(NULL, E_WARNING, "%s", strerror(errno));
 		    } else {
                 if (!p) {
