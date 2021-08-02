12098a9112977fb5065f72b3a9f6f59a452208a2
lizj <1056159381@qq.com>
2021-08-02 12:16:40 +0800

    keep creating directory when parent directory has created.

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -1410,22 +1410,32 @@ static int php_plain_files_mkdir(php_stream_wrapper *wrapper, const char *dir, i
 
 		if (p == buf) {
 			ret = php_mkdir(dir, mode);
-		} else if (!(ret = php_mkdir(buf, mode))) {
-			if (!p) {
-				p = buf;
-			}
-			/* create any needed directories if the creation of the 1st directory worked */
-			while (++p != e) {
-				if (*p == '\0') {
-					*p = DEFAULT_SLASH;
-					if ((*(p+1) != '\0') && (ret = VCWD_MKDIR(buf, (mode_t)mode)) < 0) {
-						if (options & REPORT_ERRORS) {
-							php_error_docref(NULL, E_WARNING, "%s", strerror(errno));
-						}
-						break;
-					}
-				}
-			}
+		} else {
+		    if ((ret = php_check_open_basedir(dir)) < 0 || (ret = VCWD_MKDIR(buf, (mode_t)mode)) < 0) {
+		        if (EEXIST != errno) {
+		            php_error_docref(NULL, E_WARNING, "%s", strerror(errno));
+		        }
+		    } else {
+                if (!p) {
+                    p = buf;
+                }
+                /* create any needed directories if the creation of the 1st directory worked */
+                while (++p != e) {
+                    if (*p == '\0') {
+                        *p = DEFAULT_SLASH;
+                        if ((*(p+1) != '\0') && (ret = VCWD_MKDIR(buf, (mode_t)mode)) < 0) {+
+                            /* parent directory already exists, try to create child directory */
+                            if (EEXIST == errno && (size_t) strlen(buf) < dir_len) {
+                                continue;
+                            }
+                            if (options & REPORT_ERRORS) {
+                                php_error_docref(NULL, E_WARNING, "%s", strerror(errno));
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
 		}
 	}
 	if (ret < 0) {
