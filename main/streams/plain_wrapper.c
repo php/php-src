88fe4c83227349cb998fc35b7730ee4e69515fc1
lizj <1056159381@qq.com>
2021-08-02 13:45:12 +0800

    modify if condition

--- main/streams/plain_wrapper.c
+++ main/streams/plain_wrapper.c
@@ -1412,7 +1412,8 @@ static int php_plain_files_mkdir(php_stream_wrapper *wrapper, const char *dir, i
 			ret = php_mkdir(dir, mode);
 		} else {
 		    /* split php_mkdir() into php_check_open_basedir() and VCWD_MKDIR() */
-		    if ((ret = php_check_open_basedir(dir)) < 0 || ((ret = VCWD_MKDIR(buf, (mode_t)mode)) < 0 && EEXIST != errno)) {
+            if ((ret = php_check_open_basedir(dir)) < 0
+                || ((ret = VCWD_MKDIR(buf, (mode_t) mode)) < 0 && EEXIST != errno && (size_t) strlen(buf) < dir_len)) {
 		        php_error_docref(NULL, E_WARNING, "%s", strerror(errno));
 		    } else {
                 if (!p) {
