ee11a6065c4a9280e1d6188fbb2a3d5aa532d84d
Máté Kocsis <kocsismate@woohoolabs.com>
2021-08-05 09:02:31 +0200

    Fix DOMNameSpaceNode casing

--- ext/dom/php_dom.stub.php
+++ ext/dom/php_dom.stub.php
@@ -184,10 +184,10 @@ class DOMElement extends DOMNode implements DOMParentNode, DOMChildNode
     /** @return string */
     public function getAttributeNS(?string $namespace, string $localName) {}
 
-    /** @return DOMAttr|DOMNamespaceNode|false */
+    /** @return DOMAttr|DOMNameSpaceNode|false */
     public function getAttributeNode(string $qualifiedName) {}
 
-    /** @return DOMAttr|DOMNamespaceNode|null */
+    /** @return DOMAttr|DOMNameSpaceNode|null */
     public function getAttributeNodeNS(?string $namespace, string $localName) {}
 
     /** @return DOMNodeList */
--- ext/dom/php_dom_arginfo.h
+++ ext/dom/php_dom_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: f6cb7dbafec4d360cae9f19ae23d34e3ecafd6cf */
+ * Stub hash: 1d7decd89754f2fe94cae120a34473173bbffae3 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_dom_import_simplexml, 0, 1, DOMElement, 0)
 	ZEND_ARG_TYPE_INFO(0, node, IS_OBJECT, 0)
