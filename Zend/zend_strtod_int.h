5f00ca8ff8520dec37b9e35dbb186b8a3960acf1
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-08-10 15:13:10 -0500

    docs: updates copyright doc header
    Updated to:
    
    - Reference 2.01 of the Zend Engine license, which updates the legal entity owning the license.
    - Reference a URL to the 2.01 version of the license
    - Provide the 2.01 version of the license in the LICENSE file
    
    (See #7166 for the discussion that prompted this patch.)

--- Zend/zend_strtod_int.h
+++ Zend/zend_strtod_int.h
@@ -2,12 +2,12 @@
    +----------------------------------------------------------------------+
    | Zend Engine                                                          |
    +----------------------------------------------------------------------+
-   | Copyright (c) Zend Technologies Ltd. (http://www.zend.com)           |
+   | Copyright (c) Zend Technologies USA, Inc. (https://www.zend.com)     |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.00 of the Zend license,     |
+   | This source file is subject to version 2.01 of the Zend license,     |
    | that is bundled with this package in the file LICENSE, and is        |
    | available through the world-wide-web at the following url:           |
-   | http://www.zend.com/license/2_00.txt.                                |
+   | https://www.zend.com/sites/zend/files/pdfs/zend-engine-license-2.01.txt |
    | If you did not receive a copy of the Zend license and are unable to  |
    | obtain it through the world-wide-web, please send a note to          |
    | license@zend.com so we can mail you a copy immediately.              |
