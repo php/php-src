4d70e48b4e8b76d6c887e8df3c3093180f5b2d91
Remi Collet <remi@remirepo.net>
2021-08-12 07:56:35 +0200

    Fix #80833 ZipArchive::getStream doesn't use setPassword

---
+++ ext/zip/tests/bug80833.phpt
@@ -0,0 +1,36 @@
+--TEST--
+Bug #80833 (ZipArchive::getStream doesn't use setPassword)
+--SKIPIF--
+<?php
+if (!extension_loaded('zip')) die("skip zip extension not available");
+?>
+--FILE--
+<?php
+$create_zip = new ZipArchive();
+$create_zip->open(__DIR__ . "/80833.zip", ZipArchive::CREATE);
+$create_zip->setPassword("default_password");
+$create_zip->addFromString("test.txt", "This is a test string.");
+$create_zip->setEncryptionName("test.txt", ZipArchive::EM_AES_256, "first_password");
+$create_zip->addFromString("test2.txt", "This is another test string.");
+$create_zip->setEncryptionName("test2.txt", ZipArchive::EM_AES_256, "second_password");
+$create_zip->close();
+
+$extract_zip = new ZipArchive();
+$extract_zip->open(__DIR__ . "/80833.zip", ZipArchive::RDONLY);
+$extract_zip->setPassword("first_password");
+$file_stream = $extract_zip->getStream("test.txt");
+var_dump(stream_get_contents($file_stream));
+fclose($file_stream);
+$extract_zip->setPassword("second_password");
+$file_stream = $extract_zip->getStream("test2.txt");
+var_dump(stream_get_contents($file_stream));
+fclose($file_stream);
+$extract_zip->close();
+?>
+--CLEAN--
+<?php
+@unlink(__DIR__ . "/80833.zip");
+?>
+--EXPECT--
+string(22) "This is a test string."
+string(28) "This is another test string."
--- ext/zip/zip_stream.c
+++ ext/zip/zip_stream.c
@@ -48,7 +48,7 @@ static ssize_t php_zip_ops_read(php_stream *stream, char *buf, size_t count)
 	ssize_t n = 0;
 	STREAM_DATA_FROM_STREAM();
 
-	if (self->za && self->zf) {
+	if (self->zf) {
 		n = zip_fread(self->zf, buf, count);
 		if (n < 0) {
 #if LIBZIP_VERSION_MAJOR < 1
@@ -208,51 +208,32 @@ const php_stream_ops php_stream_zipio_ops = {
 };
 
 /* {{{ php_stream_zip_open */
-php_stream *php_stream_zip_open(const char *filename, const char *path, const char *mode STREAMS_DC)
+php_stream *php_stream_zip_open(struct zip *arch, const char *path, const char *mode STREAMS_DC)
 {
 	struct zip_file *zf = NULL;
-	int err = 0;
 
 	php_stream *stream = NULL;
 	struct php_zip_stream_data_t *self;
-	struct zip *stream_za;
 
 	if (strncmp(mode,"r", strlen("r")) != 0) {
 		return NULL;
 	}
 
-	if (filename) {
-		if (ZIP_OPENBASEDIR_CHECKPATH(filename)) {
-			return NULL;
-		}
-
-		/* duplicate to make the stream za independent (esp. for MSHUTDOWN) */
-		stream_za = zip_open(filename, ZIP_CREATE, &err);
-		if (!stream_za) {
-			return NULL;
-		}
-
-		zf = zip_fopen(stream_za, path, 0);
+	if (arch) {
+		zf = zip_fopen(arch, path, 0);
 		if (zf) {
 			self = emalloc(sizeof(*self));
 
-			self->za = stream_za;
+			self->za = NULL; /* to keep it open on stream close */
 			self->zf = zf;
 			self->stream = NULL;
 			self->cursor = 0;
 			stream = php_stream_alloc(&php_stream_zipio_ops, self, NULL, mode);
 			stream->orig_path = estrdup(path);
-		} else {
-			zip_close(stream_za);
 		}
 	}
 
-	if (!stream) {
-		return NULL;
-	} else {
-		return stream;
-	}
-
+	return stream;
 }
 /* }}} */
 
