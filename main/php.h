1c33ddb5e5598c5385c4c965992c6e031fd00dd6
Patrick Allaert <patrickallaert@php.net>
2021-08-31 19:13:49 +0200

    Prepare for PHP 8.2

--- main/php.h
+++ main/php.h
@@ -22,7 +22,7 @@
 #include <dmalloc.h>
 #endif
 
-#define PHP_API_VERSION 20201009
+#define PHP_API_VERSION 20210903
 #define PHP_HAVE_STREAMS
 #define YYDEBUG 0
 #define PHP_DEFAULT_CHARSET "UTF-8"
