138c810d520b202bd35926ea94f70ffe59d7b18f
Alex Dowad <alexinbeijing@gmail.com>
2021-09-05 15:00:21 +0200

    Remove COMPAT2 conversion modes for mb_convert_kana
    mb_convert_kana has conversion modes selected using 'M'/'m', which
    convert a few various punctuation and symbol characters between
    'ordinary' and full-width forms. The constants which refer to these
    modes have names ending with COMPAT1.
    
    Internally, there are similar conversion modes with names ending in
    COMPAT2. They are like COMPAT1 modes, but they operate on a smaller
    set of characters. But... that is all just dead code, because there is
    no way for user code to select the COMPAT2 modes.
    
    I have no idea what the original author intended those COMPAT2 modes to
    actually be used for. Guess it doesn't really matter, anyways. At this
    point, it's just more food for the flames.

--- ext/mbstring/libmbfl/filters/mbfilter_tl_jisx0201_jisx0208.c
+++ ext/mbstring/libmbfl/filters/mbfilter_tl_jisx0201_jisx0208.c
@@ -114,16 +114,6 @@ int mbfl_convert_kana(int c, int next, bool *consumed, int *second, int mode)
 		} else if (c == 0x22) {
 			return 0x201d; /* RIGHT DOUBLE QUOTATION MARK */
 		}
-	} else if (mode & MBFL_FILT_TL_HAN2ZEN_COMPAT2) { /* special ascii to symbol */
-		if (c == 0x5c) {
-			return 0xff3c; /* FULLWIDTH REVERSE SOLIDUS */
-		} else if (c == 0x7e) {
-			return 0xff5e; /* FULLWIDTH TILDE */
-		} else if (c == 0x27) {
-			return 0xff07; /* FULLWIDTH APOSTROPHE */
-		} else if (c == 0x22) {
-			return 0xff02; /* FULLWIDTH QUOTATION MARK */
-		}
 	}
 
 	if (mode & (MBFL_FILT_TL_ZEN2HAN_ALL | MBFL_FILT_TL_ZEN2HAN_ALPHA | MBFL_FILT_TL_ZEN2HAN_NUMERIC | MBFL_FILT_TL_ZEN2HAN_SPACE)) {
@@ -201,18 +191,6 @@ int mbfl_convert_kana(int c, int next, bool *consumed, int *second, int mode)
 		} else if (c == 0x201c) { /* LEFT DOUBLE QUOTATION MARK */
 			return 0x22;
 		} else if (c == 0x201d) { /* RIGHT DOUBLE QUOTATION MARK */
-			return 0x22;
-		}
-	}
-
-	if (mode & MBFL_FILT_TL_ZEN2HAN_COMPAT2) { /* special symbol to ascii */
-		if (c == 0xff3c) { /* FULLWIDTH REVERSE SOLIDUS */
-			return 0x5c;
-		} else if (c == 0xff5e) { /* FULLWIDTH TILDE */
-			return 0x7e;
-		} else if (c == 0xff07) { /* FULLWIDTH APOSTROPHE */
-			return 0x27;
-		} else if (c == 0xff02) { /* FULLWIDTH QUOTATION MARK */
 			return 0x22;
 		}
 	}
