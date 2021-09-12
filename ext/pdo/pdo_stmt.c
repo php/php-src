00b3feac8c1f248d3151221f5994fbba3c246c21
NathanFreeman <1056159381@qq.com>
2021-09-12 18:22:47 +0800

    fix bug 78227

--- ext/pdo/pdo_stmt.c
+++ ext/pdo/pdo_stmt.c
@@ -464,7 +464,7 @@ PHP_METHOD(PDOStatement, execute)
 		/* When selecting all fields,
 		 * we have to check column name and reload columns descriptions to prevent a column name change. */
 		if (stmt->select_all && stmt->columns) {
-			pdo_stmt_reset_columns(stmt);
+			php_pdo_stmt_set_column_count(stmt, stmt->column_count);
 			stmt->executed = 0;
 		}
 
