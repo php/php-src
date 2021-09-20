dc20a71257b3d93d6d416adaef924a0bb21783c3
George Peter Banyard <girgias@php.net>
2021-09-20 18:30:37 +0100

    Voidify zend_ssa_unlink_use_chain()
    It always returned 1 or threw an assertion failure
    
    Also drop else branch by moving it to the main scope

--- Zend/Optimizer/zend_ssa.c
+++ Zend/Optimizer/zend_ssa.c
@@ -1161,44 +1161,42 @@ ZEND_API void zend_ssa_compute_use_def_chains(zend_arena **arena, const zend_op_
 }
 /* }}} */
 
-int zend_ssa_unlink_use_chain(zend_ssa *ssa, int op, int var) /* {{{ */
+void zend_ssa_unlink_use_chain(zend_ssa *ssa, int op, int var) /* {{{ */
 {
 	if (ssa->vars[var].use_chain == op) {
 		ssa->vars[var].use_chain = zend_ssa_next_use(ssa->ops, var, op);
-		return 1;
-	} else {
-		int use = ssa->vars[var].use_chain;
-
-		while (use >= 0) {
-			if (ssa->ops[use].result_use == var) {
-				if (ssa->ops[use].res_use_chain == op) {
-					ssa->ops[use].res_use_chain = zend_ssa_next_use(ssa->ops, var, op);
-					return 1;
-				} else {
-					use = ssa->ops[use].res_use_chain;
-				}
-			} else if (ssa->ops[use].op1_use == var) {
-				if (ssa->ops[use].op1_use_chain == op) {
-					ssa->ops[use].op1_use_chain = zend_ssa_next_use(ssa->ops, var, op);
-					return 1;
-				} else {
-					use = ssa->ops[use].op1_use_chain;
-				}
-			} else if (ssa->ops[use].op2_use == var) {
-				if (ssa->ops[use].op2_use_chain == op) {
-					ssa->ops[use].op2_use_chain = zend_ssa_next_use(ssa->ops, var, op);
-					return 1;
-				} else {
-					use = ssa->ops[use].op2_use_chain;
-				}
-			} else {
-				break;
-			}
-		}
-		/* something wrong */
-		ZEND_UNREACHABLE();
-		return 0;
+		return;
 	}
+	int use = ssa->vars[var].use_chain;
+
+	while (use >= 0) {
+		if (ssa->ops[use].result_use == var) {
+			if (ssa->ops[use].res_use_chain == op) {
+				ssa->ops[use].res_use_chain = zend_ssa_next_use(ssa->ops, var, op);
+				return;
+			} else {
+				use = ssa->ops[use].res_use_chain;
+			}
+		} else if (ssa->ops[use].op1_use == var) {
+			if (ssa->ops[use].op1_use_chain == op) {
+				ssa->ops[use].op1_use_chain = zend_ssa_next_use(ssa->ops, var, op);
+				return;
+			} else {
+				use = ssa->ops[use].op1_use_chain;
+			}
+		} else if (ssa->ops[use].op2_use == var) {
+			if (ssa->ops[use].op2_use_chain == op) {
+				ssa->ops[use].op2_use_chain = zend_ssa_next_use(ssa->ops, var, op);
+				return;
+			} else {
+				use = ssa->ops[use].op2_use_chain;
+			}
+		} else {
+			break;
+		}
+	}
+	/* something wrong */
+	ZEND_UNREACHABLE();
 }
 /* }}} */
 
