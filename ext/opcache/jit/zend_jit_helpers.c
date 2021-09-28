php/php-src

commit fe1633f0104521117edabefea78803d77d3d57b1

Undef result if undef dim warning promoted to exception
Fixes oss-fuzz #39278.

ext/opcache/jit/zend_jit_helpers.c
@@ -684,6 +684,7 @@ static zval* ZEND_FASTCALL zend_jit_fetch_dim_rw_helper(zend_array *ht, zval *di
 			goto str_index;
 		case IS_UNDEF:
 			if (!zend_jit_undefined_op_helper_write(ht, EG(current_execute_data)->opline->op2.var)) {
+				undef_result_after_exception();
 				return NULL;
 			}
 			/* break missing intentionally */
@@ -768,6 +769,7 @@ static zval* ZEND_FASTCALL zend_jit_fetch_dim_w_helper(zend_array *ht, zval *dim
 			goto str_index;
 		case IS_UNDEF:
 			if (!zend_jit_undefined_op_helper_write(ht, EG(current_execute_data)->opline->op2.var)) {
+				undef_result_after_exception();
 				return NULL;
 			}
 			/* break missing intentionally */

ext/opcache/tests/jit/assign_dim_undef_exception.phpt
@@ -1,0 +1,38 @@
+--TEST--
+Undef to exception for assign dim offset
+--INI--
+opcache.enable=1
+opcache.enable_cli=1
+opcache.file_update_protection=0
+opcache.jit_buffer_size=1M
+--FILE--
+<?php
+set_error_handler(function($_, $m){
+    throw new Exception($m);
+});
+function test1() {
+    $a = [];
+    $res = $a[$undef] = null;
+}
+function test2() {
+    $a = [];
+    $res = $a[$undef] += 1;
+}
+function test3() {
+    $a = [];
+    $res = isset($a[$undef]);
+}
+try {
+    test1();
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+try {
+    test2();
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+?>
+--EXPECT--
+Undefined variable $undef
+Undefined variable $undef

commit 0391c55b0c2a4febc1e4331e1e9a2dc88276aca9

Check exception before using undef_result_after_exception()
undefined_op_helper_write() can return 0 not just if an exception
is thrown, so check this explicitly. This fixes an issue introduced
in fe1633f0104521117edabefea78803d77d3d57b1.

ext/opcache/jit/zend_jit_helpers.c
@@ -684,7 +684,9 @@ static zval* ZEND_FASTCALL zend_jit_fetch_dim_rw_helper(zend_array *ht, zval *di
 			goto str_index;
 		case IS_UNDEF:
 			if (!zend_jit_undefined_op_helper_write(ht, EG(current_execute_data)->opline->op2.var)) {
-				undef_result_after_exception();
+				if (EG(exception)) {
+					undef_result_after_exception();
+				}
 				return NULL;
 			}
 			/* break missing intentionally */
@@ -769,7 +771,9 @@ static zval* ZEND_FASTCALL zend_jit_fetch_dim_w_helper(zend_array *ht, zval *dim
 			goto str_index;
 		case IS_UNDEF:
 			if (!zend_jit_undefined_op_helper_write(ht, EG(current_execute_data)->opline->op2.var)) {
-				undef_result_after_exception();
+				if (EG(exception)) {
+					undef_result_after_exception();
+				}
 				return NULL;
 			}
 			/* break missing intentionally */