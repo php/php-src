d3402bfd3e9a87b1d4ce3785e393e698746c645c
Andy Postnikov <apostnikov@gmail.com>
2021-09-28 23:35:37 +0300

    display libavif version and codecs via phpinfo()

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -605,6 +605,15 @@ PHP_MINFO_FUNCTION(gd)
 #endif
 #ifdef HAVE_GD_AVIF
 	php_info_print_table_row(2, "AVIF Support", "enabled");
+#ifdef HAVE_GD_BUNDLED
+#include <avif/avif.h>
+	{
+		php_info_print_table_row(2, "AVIF Version", avifVersion());
+		char tmp[256];
+		avifCodecVersions(tmp);
+		php_info_print_table_row(2, "AVIF Codecs", tmp);
+	}
+#endif
 #endif
 #ifdef HAVE_GD_TGA
 	php_info_print_table_row(2, "TGA Read Support", "enabled");
