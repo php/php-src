c19c3803231acc50e3ad1dcf7c5f10f1e024d123
Nikita Popov <nikita.ppv@gmail.com>
2021-11-05 11:13:08 +0100

    Promote incorrect bzerr stream type to TypeError
    This restores the signature we originally promised for PHP-8.0,
    without the spurious false return type.

--- ext/bz2/bz2.stub.php
+++ ext/bz2/bz2.stub.php
@@ -33,13 +33,13 @@ function bzflush($bz): bool {}
 function bzclose($bz): bool {}
 
 /** @param resource $bz */
-function bzerrno($bz): int|false {}
+function bzerrno($bz): int {}
 
 /** @param resource $bz */
-function bzerrstr($bz): string|false {}
+function bzerrstr($bz): string {}
 
 /** @param resource $bz */
-function bzerror($bz): array|false {}
+function bzerror($bz): array {}
 
 function bzcompress(string $data, int $block_size = 4, int $work_factor = 0): string|int {}
 
