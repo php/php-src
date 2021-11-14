45f52285f6ee251dc40f31c365b0eb9e6a3e0881
Matt <fsbruva@yahoo.com>
2021-11-13 21:27:31 -0500

    Fix bug #81618: Correct dns_get_record on FreeBSD
    Modify dns_get_record to test for records result based on dns_errno to
    accommodate modern FreeBSD, for which res_nsearch() does not update
    h_errno directly. Add new php_dns_errno macro, and have it consult
    statp->res_h_errno when OS has res_nsearch().
    
    Closes GH-7655.

--- ext/standard/dns.c
+++ ext/standard/dns.c
@@ -808,6 +808,7 @@ PHP_FUNCTION(dns_get_record)
 	zend_long type_param = PHP_DNS_ANY;
 	zval *authns = NULL, *addtl = NULL;
 	int type_to_fetch;
+	int dns_errno;
 #if defined(HAVE_DNS_SEARCH)
 	struct sockaddr_storage from;
 	uint32_t fromsize = sizeof(from);
@@ -957,8 +958,9 @@ PHP_FUNCTION(dns_get_record)
 			n = php_dns_search(handle, hostname, C_IN, type_to_fetch, answer.qb2, sizeof answer);
 
 			if (n < 0) {
+				dns_errno = php_dns_errno(handle);
 				php_dns_free_handle(handle);
-				switch (h_errno) {
+				switch (dns_errno) {
 					case NO_DATA:
 					case HOST_NOT_FOUND:
 						continue;
--- ext/standard/php_dns.h
+++ ext/standard/php_dns.h
@@ -26,6 +26,7 @@
     	((int)dns_search(res, dname, class, type, (char *) answer, anslen, (struct sockaddr *)&from, &fromsize))
 #define php_dns_free_handle(res) \
 		dns_free(res)
+#define php_dns_errno(handle) h_errno
 
 #elif defined(HAVE_RES_NSEARCH)
 #define php_dns_search(res, dname, class, type, answer, anslen) \
@@ -39,11 +40,13 @@
 			res_nclose(res); \
 			php_dns_free_res(res)
 #endif
+#define php_dns_errno(handle) handle->res_h_errno
 
 #elif defined(HAVE_RES_SEARCH)
 #define php_dns_search(res, dname, class, type, answer, anslen) \
 			res_search(dname, class, type, answer, anslen)
 #define php_dns_free_handle(res) /* noop */
+#define php_dns_errno(handle) h_errno
 
 #endif
 
