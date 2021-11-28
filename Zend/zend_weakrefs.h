7504cf189b086e38315c1fd62656160d407a1c13
Tyson Andre <tysonandre775@hotmail.com>
2021-11-27 19:52:30 -0500

    Improve performance of WeakReference/WeakMap. Avoid hash collisions on pointers. (#7690)
    Shift pointers by ZEND_MM_ALIGNMENT_LOG2
    to avoid the noticeable performance degradation caused by hash table collisions.
    in `EG(weakrefs)` and zend_weakmap->ht
    
    On 64-bit platforms, pointers are usually aligned to at least 8 bytes,
    so only one in 8 hash buckets were actually getting used.
    (With the metadata needed to track allocations,
    alignment might be at least 16 bytes in practice)
    
    Address review comments, add optimization
    
    Make it public for any extensions that need to work with EG(weakrefs)
    for instrumentation, debugging, etc. (e.g. zend_test)
    
    PHP 8.1 and previous releases would use the raw pointer value as a hash key instead.

--- Zend/zend_weakrefs.h
+++ Zend/zend_weakrefs.h
@@ -17,6 +17,8 @@
 #ifndef ZEND_WEAKREFS_H
 #define ZEND_WEAKREFS_H
 
+#include "zend_alloc.h"
+
 BEGIN_EXTERN_C()
 
 extern ZEND_API zend_class_entry *zend_ce_weakref;
@@ -38,6 +40,26 @@ static zend_always_inline void *zend_weakrefs_hash_add_ptr(HashTable *ht, zend_o
 	} else {
 		return NULL;
 	}
+}
+
+/* Because php uses the raw numbers as a hash function, raw pointers will lead to hash collisions.
+ * We have a guarantee that the lowest ZEND_MM_ALIGNED_OFFSET_LOG2 bits of a pointer are zero.
+ *
+ * E.g. On most 64-bit platforms, pointers are aligned to 8 bytes, so the least significant 3 bits are always 0 and can be discarded.
+ *
+ * NOTE: This function is only used for EG(weakrefs) and zend_weakmap->ht.
+ * It is not used for the HashTable instances associated with ZEND_WEAKREF_TAG_HT tags (created in zend_weakref_register, which uses ZEND_WEAKREF_ENCODE instead).
+ * The ZEND_WEAKREF_TAG_HT instances are used to disambiguate between multiple weak references to the same zend_object.
+ */
+static zend_always_inline zend_ulong zend_object_to_weakref_key(const zend_object *object)
+{
+	ZEND_ASSERT(((uintptr_t)object) % ZEND_MM_ALIGNMENT == 0);
+	return ((uintptr_t) object) >> ZEND_MM_ALIGNMENT_LOG2;
+}
+
+static zend_always_inline zend_object *zend_weakref_key_to_object(zend_ulong key)
+{
+	return (zend_object *) (((uintptr_t) key) << ZEND_MM_ALIGNMENT_LOG2);
 }
 
 END_EXTERN_C()
