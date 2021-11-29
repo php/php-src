297117bbc58f581b4f9a36b3c5c53eac0e0883a0
Dmitry Stogov <dmitry@zend.com>
2021-11-29 15:51:54 +0300

    Disable type narrowing optimization when we contruct SSA for JIT
    This also revets incorrect fix introduced in f9518c3850982aca41fac22b87a1e11096eb1643

--- ext/opcache/Optimizer/zend_inference.c
+++ ext/opcache/Optimizer/zend_inference.c
@@ -3918,7 +3918,7 @@ static zend_bool can_convert_to_double(
 	for (phi = var->phi_use_chain; phi; phi = zend_ssa_next_use_phi(ssa, var_num, phi)) {
 		/* Check that narrowing can actually be useful */
 		type = ssa->var_info[phi->ssa_var].type;
-		if (type & ((MAY_BE_ANY|MAY_BE_UNDEF) - (MAY_BE_LONG|MAY_BE_DOUBLE))) {
+		if ((type & MAY_BE_ANY) & ~(MAY_BE_LONG|MAY_BE_DOUBLE)) {
 			return 0;
 		}
 
@@ -4240,8 +4240,10 @@ static int zend_infer_types(const zend_op_array *op_array, const zend_script *sc
 		return FAILURE;
 	}
 
-	/* Narrowing integer initialization to doubles */
-	zend_type_narrowing(op_array, script, ssa, optimization_level);
+	if (optimization_level & ZEND_OPTIMIZER_NARROW_TO_DOUBLE) {
+		/* Narrowing integer initialization to doubles */
+		zend_type_narrowing(op_array, script, ssa, optimization_level);
+	}
 
 	if (ZEND_FUNC_INFO(op_array)) {
 		zend_func_return_info(op_array, script, 1, 0, &ZEND_FUNC_INFO(op_array)->return_info);
