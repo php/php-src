776ece2f3e3714d255b90926171bc1c90a77f4d8
Aliaksandr Bystry <flashwadexxx1990@gmail.com>
2021-11-30 14:23:50 +0100

    Free file line for when SPL_FILE_OBJECT_READ_CSV is set.

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -1939,6 +1939,8 @@ static int spl_filesystem_file_read_line_ex(zval * this_ptr, spl_filesystem_obje
 
 	/* 1) use fgetcsv? 2) overloaded call the function, 3) do it directly */
 	if (SPL_HAS_FLAG(intern->flags, SPL_FILE_OBJECT_READ_CSV) || intern->u.file.func_getCurr->common.scope != spl_ce_SplFileObject) {
+		spl_filesystem_file_free_line(intern);
+
 		if (php_stream_eof(intern->u.file.stream)) {
 			if (!silent) {
 				zend_throw_exception_ex(spl_ce_RuntimeException, 0, "Cannot read from file %s", intern->file_name);
@@ -2732,7 +2734,7 @@ PHP_METHOD(SplFileObject, seek)
 	spl_filesystem_file_rewind(ZEND_THIS, intern);
 
 	for (i = 0; i < line_pos; i++) {
-		if (spl_filesystem_file_read_line(ZEND_THIS, intern, 1) == FAILURE && !intern->flags) {
+		if (spl_filesystem_file_read_line(ZEND_THIS, intern, 1) == FAILURE) {
 			return;
 		}
 	}
