999c6f2c5d272bd52f5d7f57da384939c4b79e2a
Sara Golemon <pollita@php.net>
2021-12-02 05:09:07 +0000

    Bump for 8.0.15

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 14
+#define PHP_RELEASE_VERSION 15
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.0.14-dev"
-#define PHP_VERSION_ID 80014
+#define PHP_VERSION "8.0.15-dev"
+#define PHP_VERSION_ID 80015
