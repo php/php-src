7693a01053daa81914ab12562cdc70c0a560d13b
dixyes <dixyes@gmail.com>
2021-12-06 10:46:53 +0800

    Use IsProcessorFeaturePresent instead

--- ext/standard/crc32.c
+++ ext/standard/crc32.c
@@ -39,56 +39,6 @@ static unsigned long getauxval(unsigned long key) {
 		return 0;
 	return ret;
 }
-# elif defined(PHP_WIN32)
-/*
-get crc32 support info
-this is undocumented, to be confirmed
-read id_aa64isar0_el1 from registry HKLM\HARDWARE\DESCRIPTION\System\CentralProcessor\x\CP 4030
-*/
-int winaa64_support_crc32c() {
-	const WCHAR cpus_key[] = L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\";
-	HKEY cpus_handle;
-	if (ERROR_SUCCESS != RegOpenKeyExW(HKEY_LOCAL_MACHINE, cpus_key, 0, KEY_ENUMERATE_SUB_KEYS, &cpus_handle)) {
-		return 0;
-	}
-	DWORD cpu_index = 0;
-	WCHAR cpu_num[256] = { 0 };
-	uint64_t CP4030 = 0;
-	WCHAR cpu_key[(sizeof(cpus_key) / sizeof(*cpus_key)) + 256] = L"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\";
-	while (ERROR_SUCCESS == RegEnumKeyW(cpus_handle, cpu_index++, cpu_num, 255)) {
-		if (0 != wcscpy_s(&cpu_key[sizeof(cpus_key) / sizeof(*cpus_key) - 1], 256, cpu_num)) {
-			break;
-		}
-		HKEY cpu_handle;
-		if (ERROR_SUCCESS != RegOpenKeyExW(HKEY_LOCAL_MACHINE, cpu_key, 0, KEY_READ, &cpu_handle)) {
-			break;
-		}
-		DWORD value_type;
-		uint64_t value;
-		DWORD value_size = sizeof(value);
-		if (ERROR_SUCCESS != RegQueryValueExW(cpu_handle, L"CP 4030", NULL, &value_type, (LPBYTE)&value, &value_size)) {
-			RegCloseKey(cpu_handle);
-			break;
-		}
-
-		if (REG_QWORD != value_type) {
-			RegCloseKey(cpu_handle);
-			break;
-		}
-		//wprintf(L"cp4030 for cpu[%d] \"%s\" is %016zx\n", cpu_index-1, cpu_num, value);
-		// save any bits not set
-		CP4030 |= ~value;
-
-		RegCloseKey(cpu_handle);
-	}
-	//printf("rev CP4030 is %016zx\n", CP4030);
-	RegCloseKey(cpus_handle);
-	if (CP4030 & 0x10000 /* crc32: bit 16-19 (only bit 16 used) */) {
-		//printf("not supported by at least one cpu\n");
-		return 0;
-	}
-	return 1;
-}
 # endif
 
 static inline int has_crc32_insn() {
@@ -108,7 +58,7 @@ static inline int has_crc32_insn() {
 		res = 0;
 	return res;
 # elif defined(WIN32)
-	res = winaa64_support_crc32c();
+	res = (int)IsProcessorFeaturePresent(PF_ARM_V8_CRC32_INSTRUCTIONS_AVAILABLE);
 	return res;
 # else
 	res = 0;
