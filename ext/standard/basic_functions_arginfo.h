f9fb81b5dc9c1918364109ff0d526d9cf318730c
Christoph M. Becker <cmbecker69@gmx.de>
2021-12-22 23:42:29 +0100

    $context parameter of get_headers() is nullable

--- ext/standard/basic_functions_arginfo.h
+++ ext/standard/basic_functions_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 2da40c5fd9726f98ff96cf9fb5e0c41521e1e6ae */
+ * Stub hash: ff0ec0005317a22c41e61e9c58f67e968d1243c4 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_set_time_limit, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, seconds, IS_LONG, 0)
