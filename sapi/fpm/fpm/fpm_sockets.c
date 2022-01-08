c99a026c9ccdd7c06f1774187552746fb0e5affc
David Carlier <devnexen@gmail.com>
2022-01-08 07:48:28 +0000

    Fix memory leak on invalid port
    Closes GH-7911.

--- sapi/fpm/fpm/fpm_sockets.c
+++ sapi/fpm/fpm/fpm_sockets.c
@@ -336,6 +336,7 @@ static int fpm_socket_af_inet_listening_socket(struct fpm_worker_pool_s *wp) /* 
 
 	if (port == 0) {
 		zlog(ZLOG_ERROR, "invalid port value '%s'", port_str);
+		free(dup_address);
 		return -1;
 	}
 
