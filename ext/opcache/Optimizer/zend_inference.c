b80d30d821e64de96c36d24e7e931028678750de
Dmitry Stogov <dmitry@zend.com>
2022-01-10 16:03:54 +0300

    Fix type inference for assign to string offset with invalid index.
    Fixes oss-fuzz #43277

--- ext/opcache/Optimizer/zend_inference.c
+++ ext/opcache/Optimizer/zend_inference.c
@@ -2623,7 +2623,7 @@ static zend_always_inline int _zend_update_type_info(
 			if (ssa_op->result_def >= 0) {
 				tmp = 0;
 				if (t1 & MAY_BE_STRING) {
-					tmp |= MAY_BE_STRING;
+					tmp |= MAY_BE_STRING | MAY_BE_NULL;
 				}
 				if (t1 & (MAY_BE_ARRAY|MAY_BE_FALSE|MAY_BE_NULL|MAY_BE_UNDEF)) {
 					tmp |= (OP1_DATA_INFO() & (MAY_BE_ANY | MAY_BE_ARRAY_KEY_ANY | MAY_BE_ARRAY_OF_ANY | MAY_BE_ARRAY_OF_REF));
