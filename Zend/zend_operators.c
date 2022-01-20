348feb7a4e0f83339be928d205da49ae1d834f89
Jordan LeDoux <jordan.ledoux@gmail.com>
2022-01-19 16:22:48 -0800

    Added fallthrough for == operator with objects for extensions

--- Zend/zend_operators.c
+++ Zend/zend_operators.c
@@ -118,7 +118,7 @@ ZEND_API const unsigned char zend_toupper_map[256] = {
 0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,
 0xd0,0xd1,0xd2,0xd3,0xd4,0xd5,0xd6,0xd7,0xd8,0xd9,0xda,0xdb,0xdc,0xdd,0xde,0xdf,
 0xe0,0xe1,0xe2,0xe3,0xe4,0xe5,0xe6,0xe7,0xe8,0xe9,0xea,0xeb,0xec,0xed,0xee,0xef,
-0xf0,0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,0xf9,0xfa,0xfb,0xfc,0xfd,0xfe,0xff	
+0xf0,0xf1,0xf2,0xf3,0xf4,0xf5,0xf6,0xf7,0xf8,0xf9,0xfa,0xfb,0xfc,0xfd,0xfe,0xff
 };
 
 
@@ -2131,6 +2131,58 @@ static int compare_double_to_string(double dval, zend_string *str) /* {{{ */
 	return ZEND_NORMALIZE_BOOL(cmp_result);
 }
 /* }}} */
+
+ZEND_API int ZEND_FASTCALL zend_equals_object(zval *op1, zval *op2, zend_uchar equals) /* {{{ */
+{
+	zval ret;
+	int retVal;
+	int result;
+	zend_function *op_handler;
+	if (Z_TYPE_P(op1) == IS_OBJECT &&
+		Z_TYPE_P(op2) == IS_OBJECT &&
+		Z_OBJ_P(op1) == Z_OBJ_P(op2)) {
+		return 0;
+	} else if (Z_TYPE_P(op1) == IS_OBJECT) {
+		if (Z_OBJ_HANDLER_P(op1, do_operation)) {
+			result = Z_OBJ_HANDLER_P(op1, do_operation)(equals, &ret, op1, op2);
+			if (result == FAILURE) {
+				goto op2_equals_handler;
+			} else {
+				retVal = (Z_TYPE_INFO(ret) == IS_TRUE ? 0 : ZEND_UNCOMPARABLE);
+			}
+			return retVal;
+		} else {
+			if (Z_TYPE_P(op2) == IS_OBJECT) {
+				goto op2_equals_handler;
+			} else {
+				goto default_equals_handler;
+			}
+		}
+	} else if (Z_TYPE_P(op2) == IS_OBJECT) {
+op2_equals_handler:
+		if (Z_OBJ_HANDLER_P(op2, do_operation)) {
+			result = Z_OBJ_HANDLER_P(op2, do_operation)(equals, &ret, op1, op2);
+			if (result == FAILURE) {
+				goto default_equals_handler;
+			} else {
+				retVal = (Z_TYPE_INFO(ret) == IS_TRUE ? 0 : ZEND_UNCOMPARABLE);
+			}
+			return retVal;
+		} else {
+			if (Z_TYPE_P(op2) == IS_OBJECT) {
+				goto op2_equals_handler;
+			} else {
+				goto default_equals_handler;
+			}
+		}
+	} else {
+default_equals_handler:
+		return zend_compare(op1, op2);
+	}
+}
+/* }}} */
+
+
 
 ZEND_API int ZEND_FASTCALL zend_compare(zval *op1, zval *op2) /* {{{ */
 {
