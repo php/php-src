2ca6581a798019775425c5c22b164f1d1fd55e11
Sara Golemon <pollita@php.net>
2022-03-30 17:42:08 +0000

    Update versions for PHP 8.0.18RC1

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 0
-#define PHP_RELEASE_VERSION 17
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.0.17-dev"
-#define PHP_VERSION_ID 80017
+#define PHP_RELEASE_VERSION 18
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "8.0.18RC1"
+#define PHP_VERSION_ID 80018
