3035464c3cc5430dcf70e184423b8f59c163cb8a
George Peter Banyard <girgias@php.net>
2022-04-05 13:39:35 +0100

    Fix [-Wundef] warning in standard/random.c

--- ext/standard/random.c
+++ ext/standard/random.c
@@ -31,7 +31,7 @@
 #endif
 #if HAVE_SYS_PARAM_H
 # include <sys/param.h>
-# if (__FreeBSD__ && __FreeBSD_version > 1200000) || (__DragonFly__ && __DragonFly_version >= 500700) || defined(__sun)
+# if (defined(__FreeBSD__) && __FreeBSD_version > 1200000) || (defined(__DragonFly__) && __DragonFly_version >= 500700) || defined(__sun)
 #  include <sys/random.h>
 # endif
 #endif
