30d3b042bbdc16519a74e12707d7774cab7ecfab
Levi Morrison <levi.morrison@datadoghq.com>
2022-04-08 09:13:17 -0600

    Add license

--- Zend/zend_atomic.c
+++ Zend/zend_atomic.c
@@ -1,4 +1,16 @@
-// TODO: license
+/*
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Levi Morrison <morrison.levi@gmail.com>                     |
+   +----------------------------------------------------------------------+
+ */
 
 #include "zend_atomic.h"
 
