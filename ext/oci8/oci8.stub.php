ac6dbbd183d1c3b01b31931fc2e369455603a924
Tim Düsterhus <duesterhus@woltlab.com>
2022-04-12 14:47:46 +0200

    Mark parameter in ext/oci8 as sensitive

--- ext/oci8/oci8.stub.php
+++ ext/oci8/oci8.stub.php
@@ -294,10 +294,14 @@ function oci_close($connection): ?bool {}
  */
 function ocilogoff($connection): ?bool {}
 
-/** @return resource|false */
+/**
+ * @sensitive-param $password
+ * @return resource|false
+ */
 function oci_new_connect(string $username, string $password, ?string $connection_string = null, string $encoding = "", int $session_mode = OCI_DEFAULT) {}
 
 /**
+ * @sensitive-param $password
  * @return resource|false
  * @alias oci_new_connect
  * @deprecated
@@ -305,21 +309,27 @@ function oci_new_connect(string $username, string $password, ?string $connection
 function ocinlogon(string $username, string $password, ?string $connection_string = null, string $encoding = "", int $session_mode = OCI_DEFAULT) {}
 
 /**
+ * @sensitive-param $password
  * @return resource|false
  */
 function oci_connect(string $username, string $password, ?string $connection_string = null, string $encoding = "", int $session_mode = OCI_DEFAULT) {}
 
 /**
+ * @sensitive-param $password
  * @return resource|false
  * @alias oci_connect
  * @deprecated
  */
 function ocilogon(string $username, string $password, ?string $connection_string = null, string $encoding = "", int $session_mode = OCI_DEFAULT) {}
 
-/** @return resource|false */
+/**
+ * @sensitive-param $password
+ * @return resource|false
+ */
 function oci_pconnect(string $username, string $password, ?string $connection_string = null, string $encoding = "", int $session_mode = OCI_DEFAULT) {}
 
 /**
+ * @sensitive-param $password
  * @return resource|false
  * @alias oci_pconnect
  * @deprecated
