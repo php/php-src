a8b6aea5a2ae99e4185c606eb95fde015eee4052
Nikita Popov <nikita.ppv@gmail.com>
2022-04-18 18:24:52 +0200

    Remove unused macros

--- Zend/Optimizer/optimize_func_calls.c
+++ Zend/Optimizer/optimize_func_calls.c
@@ -28,13 +28,6 @@
 #include "zend_execute.h"
 #include "zend_vm.h"
 
-#define ZEND_OP1_IS_CONST_STRING(opline) \
-	(opline->op1_type == IS_CONST && \
-	Z_TYPE(op_array->literals[(opline)->op1.constant]) == IS_STRING)
-#define ZEND_OP2_IS_CONST_STRING(opline) \
-	(opline->op2_type == IS_CONST && \
-	Z_TYPE(op_array->literals[(opline)->op2.constant]) == IS_STRING)
-
 typedef struct _optimizer_call_info {
 	zend_function *func;
 	zend_op       *opline;
