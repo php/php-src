8a2b6cdbfe432d221e4e6e0414db33fa1f169da2
Jordan LeDoux <jordan.ledoux@gmail.com>
2022-05-14 08:52:21 -0700

    Fixing broken compile

--- ext/com_dotnet/com_saproxy.c
+++ ext/com_dotnet/com_saproxy.c
@@ -413,7 +413,7 @@ zend_object_handlers php_com_saproxy_handlers = {
 	NULL,									/* do_operation */
 	saproxy_objects_compare,				/* compare */
 	NULL,									/* get_properties_for */
-	zend_std_equals_objects,				/* equals */
+	NULL,				/* equals */
 };
 
 void php_com_saproxy_create(zend_object *com_object, zval *proxy_out, zval *index)
