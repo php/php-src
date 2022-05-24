3b6ee1eb19c14c3339ebfcf5c967065a9f828971
Patrick Allaert <patrickallaert@php.net>
2022-05-25 00:54:00 +0200

    Bump for 8.1.8-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 7
+#define PHP_RELEASE_VERSION 8
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.7-dev"
-#define PHP_VERSION_ID 80107
+#define PHP_VERSION "8.1.8-dev"
+#define PHP_VERSION_ID 80108
