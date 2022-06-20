d5d0b07b4e7f57cc1c074caf8bb8229c2f489f06
George Peter Banyard <girgias@php.net>
2022-06-20 17:26:40 +0100

    Early return in spl_filesystem_file_rewind()

--- ext/spl/spl_directory.c
+++ ext/spl/spl_directory.c
@@ -2047,10 +2047,12 @@ static void spl_filesystem_file_rewind(zval * this_ptr, spl_filesystem_object *i
 	}
 	if (-1 == php_stream_rewind(intern->u.file.stream)) {
 		zend_throw_exception_ex(spl_ce_RuntimeException, 0, "Cannot rewind file %s", ZSTR_VAL(intern->file_name));
-	} else {
-		spl_filesystem_file_free_line(intern);
-		intern->u.file.current_line_num = 0;
+		return;
 	}
+
+	spl_filesystem_file_free_line(intern);
+	intern->u.file.current_line_num = 0;
+
 	if (SPL_HAS_FLAG(intern->flags, SPL_FILE_OBJECT_READ_AHEAD)) {
 		spl_filesystem_file_read_line(this_ptr, intern, /* silent */ true);
 	}
