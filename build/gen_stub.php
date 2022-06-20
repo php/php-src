848b9bd2917eabfa8af9e73a8dcede681dd25f03
Yurun <admin@yurunsoft.com>
2022-06-20 09:38:01 +0800

    Update

--- build/gen_stub.php
+++ build/gen_stub.php
@@ -1640,8 +1640,7 @@ class EvaluatedValue
         if ($this->type->isNull()) {
             $code .= "\tZVAL_NULL(&$zvalName);\n";
         } elseif ($this->type->isBool()) {
-            $boolType = $this->value ? 'TRUE' : 'FALSE';
-            $code .= "\tZVAL_$boolType(&$zvalName);\n";
+            $code .= "\t" . ($this->value ? 'ZVAL_TRUE' : 'ZVAL_FALSE') . "(&$zvalName);\n";
         } elseif ($this->type->isInt()) {
             $code .= "\tZVAL_LONG(&$zvalName, " . ($cConstValue ?: $this->value) . ");\n";
         } elseif ($this->type->isFloat()) {
