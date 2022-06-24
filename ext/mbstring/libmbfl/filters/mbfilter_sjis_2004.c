9b21b8f44303b212d3f97135d4c58de6b6c2e28d
Alex Dowad <alexinbeijing@gmail.com>
2022-06-24 09:25:35 +0200

    Fix legacy conversion filter for SJIS-2004

--- ext/mbstring/libmbfl/filters/mbfilter_sjis_2004.c
+++ ext/mbstring/libmbfl/filters/mbfilter_sjis_2004.c
@@ -491,6 +491,11 @@ int mbfl_filt_conv_jis2004_wchar_flush(mbfl_convert_filter *filter)
 	if (filter->status & 0xF) {
 		CK((*filter->output_function)(MBFL_BAD_INPUT, filter->data));
 	}
+
+	if (filter->flush_function) {
+		return (*filter->flush_function)(filter->data);
+	}
+
 	return 0;
 }
 
