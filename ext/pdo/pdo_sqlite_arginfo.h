a3f77ccf26bebef634210fb43308ccafb642e893
Danack <Danack@basereality.com>
2022-06-24 01:43:17 +0100

    PDOSQLite::blobOpen is working.

--- ext/pdo/pdo_sqlite_arginfo.h
+++ ext/pdo/pdo_sqlite_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: b0396a798b67a578c9d0c7a1f04c6fc3dcd24691 */
+ * Stub hash: 9e4de4487e2652b510d91a903ce7267b089a83c1 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_PDOSqlite_createFunction, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, function_name, IS_STRING, 0)
@@ -14,11 +14,20 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_PDOSqlite_loadExtension, 0
 ZEND_END_ARG_INFO()
 #endif
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_PDOSqlite_openBlob, 0, 3, IS_MIXED, 0)
+	ZEND_ARG_TYPE_INFO(0, table, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, column, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, rowid, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, dbname, IS_STRING, 1, "\"main\"")
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, flags, IS_LONG, 0, "PDO::SQLITE_OPEN_READONLY")
+ZEND_END_ARG_INFO()
+
 
 ZEND_METHOD(PDOSqlite, createFunction);
 #if !defined(SQLITE_OMIT_LOAD_EXTENSION)
 ZEND_METHOD(PDOSqlite, loadExtension);
 #endif
+ZEND_METHOD(PDOSqlite, openBlob);
 
 
 static const zend_function_entry class_PDOSqlite_methods[] = {
@@ -26,6 +35,7 @@ static const zend_function_entry class_PDOSqlite_methods[] = {
 #if !defined(SQLITE_OMIT_LOAD_EXTENSION)
 	ZEND_ME(PDOSqlite, loadExtension, arginfo_class_PDOSqlite_loadExtension, ZEND_ACC_PUBLIC)
 #endif
+	ZEND_ME(PDOSqlite, openBlob, arginfo_class_PDOSqlite_openBlob, ZEND_ACC_PUBLIC)
 	ZEND_FE_END
 };
 
