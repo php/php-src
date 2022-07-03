f3a5ef386d9f9ca3fb95319b610ee4382516f734
Dmitry Menshikov <d.menshikov@gmail.com>
2022-07-03 16:37:14 +0300

    FPM. access.log with stderr begins to write logs to error_log after daemon reload (GH-8885)

--- sapi/fpm/fpm/fpm_stdio.h
+++ sapi/fpm/fpm/fpm_stdio.h
@@ -16,5 +16,7 @@ void fpm_stdio_child_use_pipes(struct fpm_child_s *child);
 int fpm_stdio_parent_use_pipes(struct fpm_child_s *child);
 int fpm_stdio_discard_pipes(struct fpm_child_s *child);
 int fpm_stdio_open_error_log(int reopen);
+int fpm_stdio_save_original_stderr(void);
+int fpm_stdio_restore_original_stderr(void);
 
 #endif
