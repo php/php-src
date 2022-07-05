096b1ece34b1ad0db20ac9be5323fdaea582e43e
David CARLIER <devnexen@gmail.com>
2022-07-05 19:05:36 +0100

    attempt fo fix the intl module using deprecated apis,  using replacement no longer relying on user provided stacks but still thread safe.

--- ext/intl/converter/converter.c
+++ ext/intl/converter/converter.c
@@ -933,10 +933,18 @@ static zend_object *php_converter_clone_object(zend_object *object) {
 
 	intl_errors_reset(&oldobj->error);
 
+#if U_ICU_VERSION_MAJOR_NUM > 70
+	objval->src = ucnv_clone(oldobj->src, &error);
+#else
 	objval->src = ucnv_safeClone(oldobj->src, NULL, NULL, &error);
+#endif
 	if (U_SUCCESS(error)) {
 		error = U_ZERO_ERROR;
+#if U_ICU_VERSION_MAJOR_NUM > 70
+		objval->dest = ucnv_clone(oldobj->dest, &error);
+#else
 		objval->dest = ucnv_safeClone(oldobj->dest, NULL, NULL, &error);
+#endif
 	}
 	if (U_FAILURE(error)) {
 		zend_string *err_msg;
