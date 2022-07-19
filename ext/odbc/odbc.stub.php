e90379462ecc330f1de677a4b148a9be41cadc0c
Máté Kocsis <kocsismate@woohoolabs.com>
2022-07-19 10:18:54 +0200

    Declare ext/odbc constants in stubs

--- ext/odbc/odbc.stub.php
+++ ext/odbc/odbc.stub.php
@@ -2,6 +2,316 @@
 
 /** @generate-class-entries */
 
+/**
+ * @var string
+ * @cname PHP_ODBC_TYPE
+ */
+const ODBC_TYPE = UNKNOWN;
+/**
+ * @var int
+ * @cname PHP_ODBC_BINMODE_PASSTHRU
+ */
+const ODBC_BINMODE_PASSTHRU = UNKNOWN;
+/**
+ * @var int
+ * @cname PHP_ODBC_BINMODE_RETURN
+ */
+const ODBC_BINMODE_RETURN = UNKNOWN;
+/**
+ * @var int
+ * @cname PHP_ODBC_BINMODE_CONVERT
+ */
+const ODBC_BINMODE_CONVERT = UNKNOWN;
+
+/* Define Constants for options. These Constants are defined in <sqlext.h> */
+
+/**
+ * @var int
+ * @cname SQL_ODBC_CURSORS
+ */
+const SQL_ODBC_CURSORS = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CUR_USE_DRIVER
+ */
+const SQL_CUR_USE_DRIVER = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CUR_USE_IF_NEEDED
+ */
+const SQL_CUR_USE_IF_NEEDED = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CUR_USE_ODBC
+ */
+const SQL_CUR_USE_ODBC = UNKNOWN;
+
+/**
+ * @var int
+ * @cname SQL_CONCURRENCY
+ */
+const SQL_CONCURRENCY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CONCUR_READ_ONLY
+ */
+const SQL_CONCUR_READ_ONLY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CONCUR_LOCK
+ */
+const SQL_CONCUR_LOCK = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CONCUR_ROWVER
+ */
+const SQL_CONCUR_ROWVER = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CONCUR_VALUES
+ */
+const SQL_CONCUR_VALUES = UNKNOWN;
+
+/**
+ * @var int
+ * @cname SQL_CURSOR_TYPE
+ */
+const SQL_CURSOR_TYPE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CURSOR_FORWARD_ONLY
+ */
+const SQL_CURSOR_FORWARD_ONLY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CURSOR_KEYSET_DRIVEN
+ */
+const SQL_CURSOR_KEYSET_DRIVEN = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CURSOR_DYNAMIC
+ */
+const SQL_CURSOR_DYNAMIC = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_CURSOR_STATIC
+ */
+const SQL_CURSOR_STATIC = UNKNOWN;
+
+/**
+ * @var int
+ * @cname SQL_KEYSET_SIZE
+ */
+const SQL_KEYSET_SIZE = UNKNOWN;
+
+/* these are for the Data Source type */
+
+/**
+ * @var int
+ * @cname SQL_FETCH_FIRST
+ */
+const SQL_FETCH_FIRST = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_FETCH_NEXT
+ */
+const SQL_FETCH_NEXT = UNKNOWN;
+
+/* register the standard data types */
+
+/**
+ * @var int
+ * @cname SQL_CHAR
+ */
+const SQL_CHAR = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_VARCHAR
+ */
+const SQL_VARCHAR = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_LONGVARCHAR
+ */
+const SQL_LONGVARCHAR = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_DECIMAL
+ */
+const SQL_DECIMAL = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_NUMERIC
+ */
+const SQL_NUMERIC = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_BIT
+ */
+const SQL_BIT = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_TINYINT
+ */
+const SQL_TINYINT = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_SMALLINT
+ */
+const SQL_SMALLINT = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_INTEGER
+ */
+const SQL_INTEGER = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_BIGINT
+ */
+const SQL_BIGINT = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_REAL
+ */
+const SQL_REAL = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_FLOAT
+ */
+const SQL_FLOAT = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_DOUBLE
+ */
+const SQL_DOUBLE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_BINARY
+ */
+const SQL_BINARY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_VARBINARY
+ */
+const SQL_VARBINARY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_LONGVARBINARY
+ */
+const SQL_LONGVARBINARY = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_DATE
+ */
+const SQL_DATE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_TIME
+ */
+const SQL_TIME = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_TIMESTAMP
+ */
+const SQL_TIMESTAMP = UNKNOWN;
+
+#if defined(ODBCVER) && (ODBCVER >= 0x0300)
+/**
+ * @var int
+ * @cname SQL_TYPE_DATE
+ */
+const SQL_TYPE_DATE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_TYPE_TIME
+ */
+const SQL_TYPE_TIME = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_TYPE_TIMESTAMP
+ */
+const SQL_TYPE_TIMESTAMP = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_WCHAR
+ */
+const SQL_WCHAR = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_WVARCHAR
+ */
+const SQL_WVARCHAR = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_WLONGVARCHAR
+ */
+const SQL_WLONGVARCHAR = UNKNOWN;
+
+/* SQLSpecialColumns values */
+
+/**
+ * @var int
+ * @cname SQL_BEST_ROWID
+ */
+const SQL_BEST_ROWID = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_ROWVER
+ */
+const SQL_ROWVER = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_SCOPE_CURROW
+ */
+const SQL_SCOPE_CURROW = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_SCOPE_TRANSACTION
+ */
+const SQL_SCOPE_TRANSACTION = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_SCOPE_SESSION
+ */
+const SQL_SCOPE_SESSION = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_NO_NULLS
+ */
+const SQL_NO_NULLS = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_NULLABLE
+ */
+const SQL_NULLABLE = UNKNOWN;
+
+/* SQLStatistics values */
+
+/**
+ * @var int
+ * @cname SQL_INDEX_UNIQUE
+ */
+const SQL_INDEX_UNIQUE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_INDEX_ALL
+ */
+const SQL_INDEX_ALL = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_ENSURE
+ */
+const SQL_ENSURE = UNKNOWN;
+/**
+ * @var int
+ * @cname SQL_QUICK
+ */
+const SQL_QUICK = UNKNOWN;
+
+#endif
+
+
+
 function odbc_close_all(): void {}
 
 /** @param resource $statement */
