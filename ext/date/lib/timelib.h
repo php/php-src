557e02bf9f96073ddd7786e26521b3851b88bd4e
Derick Rethans <github@derickrethans.nl>
2022-07-21 11:31:16 +0100

    Merge timelib 2021.14

--- ext/date/lib/timelib.h
+++ ext/date/lib/timelib.h
@@ -30,9 +30,9 @@
 # include "timelib_config.h"
 #endif
 
-#define TIMELIB_VERSION 202113
-#define TIMELIB_EXTENDED_VERSION 20211301
-#define TIMELIB_ASCII_VERSION "2021.13"
+#define TIMELIB_VERSION 202114
+#define TIMELIB_EXTENDED_VERSION 20211401
+#define TIMELIB_ASCII_VERSION "2021.14"
 
 #include <stdlib.h>
 #include <stdbool.h>
