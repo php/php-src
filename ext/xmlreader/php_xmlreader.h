ad508651e5f634ddbe1554065d1b4b75a0aabf98
Máté Kocsis <kocsismate@woohoolabs.com>
2022-07-25 19:07:33 +0200

    Declare ext/xmlreader constants in stubs

--- ext/xmlreader/php_xmlreader.h
+++ ext/xmlreader/php_xmlreader.h
@@ -57,7 +57,4 @@ PHP_MINIT_FUNCTION(xmlreader);
 PHP_MSHUTDOWN_FUNCTION(xmlreader);
 PHP_MINFO_FUNCTION(xmlreader);
 
-#define REGISTER_XMLREADER_CLASS_CONST_LONG(const_name, value) \
-	zend_declare_class_constant_long(xmlreader_class_entry, const_name, sizeof(const_name)-1, (zend_long)value);
-
 #endif	/* PHP_XMLREADER_H */
