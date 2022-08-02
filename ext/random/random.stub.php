5ec392d410bf06cffe6f0b5bae30eeb1f79fd176
Tim Düsterhus <tim@bastelstu.be>
2022-08-02 17:39:50 +0200

    Fix PcgOneseq128XslRr64::__construct() definition in random.stub.php
    The second parameter does not actually exist for a Oneseq PCG. It was removed
    from the RFC before it went into voting.

--- ext/random/random.stub.php
+++ ext/random/random.stub.php
@@ -59,7 +59,7 @@ namespace Random\Engine
      */
     final class PcgOneseq128XslRr64 implements \Random\Engine
     {
-        public function __construct(string|int|null $seed = null, string|int $sequence = 0) {}
+        public function __construct(string|int|null $seed = null) {}
 
         /** @implementation-alias Random\Engine\Mt19937::generate */
         public function generate(): string {}
