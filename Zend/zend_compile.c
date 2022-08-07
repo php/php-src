c4180f2e4d9f39abc851c478536c1c71d0ecb000
Tyson Andre <tysonandre775@hotmail.com>
2022-08-07 11:54:29 -0400

    Explicitly check for AST_VAR/AST_DIM kind for future compatibility
    `AST_PROP`/`AST_METHOD_CALL` and nullsafe variants can also be found in
    encapsulated strings - currently they have no flags but they may have flags in
    the future. This also clarifies that this deprecation warning can only happen
    for AST_VAR/AST_DIM nodes.

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -9625,9 +9625,9 @@ static void zend_compile_encaps_list(znode *result, zend_ast *ast) /* {{{ */
 		zend_ast *encaps_var = list->child[i];
 
 		if (encaps_var->attr & (ZEND_ENCAPS_VAR_DOLLAR_CURLY|ZEND_ENCAPS_VAR_DOLLAR_CURLY_VAR_VAR)) {
-			if (encaps_var->attr & ZEND_ENCAPS_VAR_DOLLAR_CURLY) {
+			if ((encaps_var->kind == ZEND_AST_VAR || encaps_var->kind == ZEND_AST_DIM) && (encaps_var->attr & ZEND_ENCAPS_VAR_DOLLAR_CURLY)) {
 				zend_error(E_DEPRECATED, "Using ${var} in strings is deprecated, use {$var} instead");
-			} else if (encaps_var->kind == ZEND_AST_VAR) {
+			} else if (encaps_var->kind == ZEND_AST_VAR && (encaps_var->attr & ZEND_ENCAPS_VAR_DOLLAR_CURLY_VAR_VAR)) {
 				zend_error(E_DEPRECATED, "Using ${expr} (variable variables) in strings is deprecated, use {${expr}} instead");
 			}
 		}
