4eb8c2e628c30b48aed0a6317e3e514ad5e6e3a2
Jakub Zelenka <bukka@php.net>
2022-08-09 15:41:47 +0100

    Fix GH-8409: SSL handshake timeout persistent connections hanging
    This is not actually related to SSL handshake but stream socket creation
    which does not clean errors if the error handler is set. This fix
    prevents emitting errors until the stream is freed.

--- Zend/zend.c
+++ Zend/zend.c
@@ -1366,6 +1366,9 @@ ZEND_API ZEND_COLD void zend_error_zstr_at(
 		EG(num_errors)++;
 		EG(errors) = erealloc(EG(errors), sizeof(zend_error_info) * EG(num_errors));
 		EG(errors)[EG(num_errors)-1] = info;
+		if (EG(record_errors_without_emitting)) {
+			return;
+		}
 	}
 
 	/* Report about uncaught exception in case of fatal errors */
@@ -1619,12 +1622,32 @@ ZEND_API ZEND_COLD void zend_error_zstr(int type, zend_string *message) {
 	zend_error_zstr_at(type, filename, lineno, message);
 }
 
-ZEND_API void zend_begin_record_errors(void)
+static zend_always_inline void zend_begin_record_errors_ex(bool no_emmitting)
 {
-	ZEND_ASSERT(!EG(record_errors) && "Error recoreding already enabled");
+	ZEND_ASSERT(!EG(record_errors) && "Error recording already enabled");
 	EG(record_errors) = true;
+	EG(record_errors_without_emitting) = no_emmitting;
 	EG(num_errors) = 0;
 	EG(errors) = NULL;
+}
+
+ZEND_API void zend_begin_record_errors(void)
+{
+	zend_begin_record_errors_ex(false);
+}
+
+ZEND_API void zend_begin_record_errors_without_emitting(void)
+{
+	zend_begin_record_errors_ex(true);
+}
+
+ZEND_API void zend_emit_recorded_errors(void)
+{
+	EG(record_errors) = false;
+	for (uint32_t i = 0; i < EG(num_errors); i++) {
+		zend_error_info *error = EG(errors)[i];
+		zend_error_zstr_at(error->type, error->filename, error->lineno, error->message);
+	}
 }
 
 ZEND_API void zend_free_recorded_errors(void)
