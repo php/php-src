7f26661993d1605f1b6e17cdc24ad6114e983ae4
Ben Ramsey <ramsey@php.net>
2022-08-16 10:45:29 -0500

    PHP-8.1 is now for PHP 8.1.11-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 10
+#define PHP_RELEASE_VERSION 11
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.10-dev"
-#define PHP_VERSION_ID 80110
+#define PHP_VERSION "8.1.11-dev"
+#define PHP_VERSION_ID 80111
