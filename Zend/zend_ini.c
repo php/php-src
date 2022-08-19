860b418e5c4575092725a80def909317bf61a872
George Peter Banyard <girgias@php.net>
2022-08-19 13:11:30 +0100

    Simplify in a similar fashion

--- Zend/zend_ini.c
+++ Zend/zend_ini.c
@@ -892,7 +892,7 @@ ZEND_API ZEND_INI_MH(OnUpdateStrNotEmpty) /* {{{ */
 	}
 
 	zend_string **p = (zend_string **) ZEND_INI_GET_ADDR();
-	*p = new_value ? new_value : NULL;
+	*p = new_value;
 	return SUCCESS;
 }
 /* }}} */
