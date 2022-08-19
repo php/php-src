e9749a3c1e5e7edf2e74326498513c29507ac60b
George Peter Banyard <girgias@php.net>
2022-08-19 15:38:33 +0100

    Remove OnUpdateLazyWrite validator
    It is identical to the OnUpdateSessionBool one

--- ext/session/session.c
+++ ext/session/session.c
@@ -766,17 +766,6 @@ static PHP_INI_MH(OnUpdateSidBits) /* {{{ */
 }
 /* }}} */
 
-
-static PHP_INI_MH(OnUpdateLazyWrite) /* {{{ */
-{
-	SESSION_CHECK_ACTIVE_STATE;
-	SESSION_CHECK_OUTPUT_STATE;
-	return OnUpdateBool(entry, new_value, mh_arg1, mh_arg2, mh_arg3, stage);
-}
-/* }}} */
-
-
-
 static PHP_INI_MH(OnUpdateRfc1867Freq) /* {{{ */
 {
 	int tmp = ZEND_ATOL(ZSTR_VAL(new_value));
@@ -821,7 +810,7 @@ PHP_INI_BEGIN()
 	PHP_INI_ENTRY("session.use_trans_sid",          "0",         PHP_INI_ALL, OnUpdateTransSid)
 	PHP_INI_ENTRY("session.sid_length",             "32",        PHP_INI_ALL, OnUpdateSidLength)
 	PHP_INI_ENTRY("session.sid_bits_per_character", "4",         PHP_INI_ALL, OnUpdateSidBits)
-	STD_PHP_INI_BOOLEAN("session.lazy_write",       "1",         PHP_INI_ALL, OnUpdateLazyWrite,     lazy_write,         php_ps_globals,    ps_globals)
+	STD_PHP_INI_BOOLEAN("session.lazy_write",       "1",         PHP_INI_ALL, OnUpdateSessionBool,    lazy_write,         php_ps_globals,    ps_globals)
 
 	/* Upload progress */
 	STD_PHP_INI_BOOLEAN("session.upload_progress.enabled",
