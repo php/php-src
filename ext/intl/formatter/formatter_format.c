47500f3300d4dab8508cd3f897f5a0b8a4cfa159
George Peter Banyard <girgias@php.net>
2022-08-30 17:16:27 +0100

    Fix GH-9421 Incorrect argument number for ValueError in NumberFormatter
    Closes GH-9489

--- ext/intl/formatter/formatter_format.c
+++ ext/intl/formatter/formatter_format.c
@@ -103,9 +103,18 @@ PHP_FUNCTION( numfmt_format )
 			}
 			INTL_METHOD_CHECK_STATUS( nfo, "Number formatting failed" );
 			break;
-
+		case FORMAT_TYPE_CURRENCY:
+			if (getThis()) {
+				const char *space;
+				const char *class_name = get_active_class_name(&space);
+				zend_argument_value_error(2, "cannot be NumberFormatter::TYPE_CURRENCY constant, "
+					"use %s%sformatCurrency() method instead", class_name, space);
+			} else {
+				zend_argument_value_error(3, "cannot be NumberFormatter::TYPE_CURRENCY constant, use numfmt_format_currency() function instead");
+			}
+			RETURN_THROWS();
 		default:
-			zend_argument_value_error(3, "must be a NumberFormatter::TYPE_* constant");
+			zend_argument_value_error(getThis() ? 2 : 3, "must be a NumberFormatter::TYPE_* constant");
 			RETURN_THROWS();
 	}
 
