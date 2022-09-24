994e2ba82abda70ad11d8000237a163ceed6a1cc
George Peter Banyard <girgias@php.net>
2022-09-24 12:05:50 +0100

    Fix GH-9583: session_create_id() fails with user defined save handler that doesn't have a validateId() method

--- ext/session/session.c
+++ ext/session/session.c
@@ -1083,8 +1083,9 @@ PHPAPI int php_session_register_module(const ps_module *ptr) /* {{{ */
 /* }}} */
 
 /* Dummy PS module function */
+/* We consider any ID valid, so we return FAILURE to indicate that a session doesn't exist */
 PHPAPI int php_session_validate_sid(PS_VALIDATE_SID_ARGS) {
-	return SUCCESS;
+	return FAILURE;
 }
 
 /* Dummy PS module function */
