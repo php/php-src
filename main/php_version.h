613fcb5251f2b8742bf03f43fc9a8429963db927
Derick Rethans <github@derickrethans.nl>
2022-09-28 09:09:48 +0000

    Update versions for PHP 7.4.32

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 7
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 31
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "7.4.31-dev"
-#define PHP_VERSION_ID 70431
+#define PHP_RELEASE_VERSION 32
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "7.4.32"
+#define PHP_VERSION_ID 70432
