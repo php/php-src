647bb27008d63ea383c1316be9ead9a365bb025b
Tyson Andre <tysonandre775@hotmail.com>
2022-10-10 14:12:41 -0400

    Avoid crash for reset/end/next/prev() on ffi classes
    (And any PECLs returning `zend_empty_array` in the handler->get_properties
    overrides)
    
    Closes GH-9697
    
    This is similar to the fix used in d9651a941915eb5fb5ad557090b65256fd8509b6
    for array_walk.
    
    This should make it safer for php-src (and PECLs, long-term) to return
    the empty immutable array in `handler->get_properties` to avoid wasting memory.
    See https://github.com/php/php-src/issues/9697#issuecomment-1273613175
    
    The only possible internal iterator position for the empty array is at the end
    of the empty array (nInternalPointer=0).
    The `zend_hash*del*` helpers will always set nInternalPointer to 0 when an
    array becomes empty,
    regardless of previous insertions/deletions/updates to the array.

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -1073,6 +1073,10 @@ PHP_FUNCTION(end)
 	ZEND_PARSE_PARAMETERS_END();
 
 	HashTable *array = get_ht_for_iap(array_zv, /* separate */ true);
+	if (zend_hash_num_elements(array) == 0) {
+		/* array->nInternalPointer is already 0 if the array is empty, even after removing elements */
+		RETURN_FALSE;
+	}
 	zend_hash_internal_pointer_end(array);
 
 	if (USED_RET()) {
@@ -1100,6 +1104,10 @@ PHP_FUNCTION(prev)
 	ZEND_PARSE_PARAMETERS_END();
 
 	HashTable *array = get_ht_for_iap(array_zv, /* separate */ true);
+	if (zend_hash_num_elements(array) == 0) {
+		/* array->nInternalPointer is already 0 if the array is empty, even after removing elements */
+		RETURN_FALSE;
+	}
 	zend_hash_move_backwards(array);
 
 	if (USED_RET()) {
@@ -1127,6 +1135,10 @@ PHP_FUNCTION(next)
 	ZEND_PARSE_PARAMETERS_END();
 
 	HashTable *array = get_ht_for_iap(array_zv, /* separate */ true);
+	if (zend_hash_num_elements(array) == 0) {
+		/* array->nInternalPointer is already 0 if the array is empty, even after removing elements */
+		RETURN_FALSE;
+	}
 	zend_hash_move_forward(array);
 
 	if (USED_RET()) {
@@ -1154,6 +1166,10 @@ PHP_FUNCTION(reset)
 	ZEND_PARSE_PARAMETERS_END();
 
 	HashTable *array = get_ht_for_iap(array_zv, /* separate */ true);
+	if (zend_hash_num_elements(array) == 0) {
+		/* array->nInternalPointer is already 0 if the array is empty, even after removing elements */
+		RETURN_FALSE;
+	}
 	zend_hash_internal_pointer_reset(array);
 
 	if (USED_RET()) {
