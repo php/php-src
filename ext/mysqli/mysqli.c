5b3eabb468ce7cef7bfd236288e1ec716bc69e89
George Peter Banyard <girgias@php.net>
2022-10-31 15:04:35 +0000

    Use zend_call_known_function() in ext-mysqli instead of building FCI/FCC

--- ext/mysqli/mysqli.c
+++ ext/mysqli/mysqli.c
@@ -787,9 +787,7 @@ void php_mysqli_fetch_into_hash(INTERNAL_FUNCTION_PARAMETERS, int override_flags
 	php_mysqli_fetch_into_hash_aux(return_value, result, fetchtype);
 
 	if (into_object && Z_TYPE_P(return_value) == IS_ARRAY) {
-		zval dataset, retval;
-		zend_fcall_info fci;
-		zend_fcall_info_cache fcc;
+		zval dataset;
 
 		ZVAL_COPY_VALUE(&dataset, return_value);
 
@@ -804,23 +802,8 @@ void php_mysqli_fetch_into_hash(INTERNAL_FUNCTION_PARAMETERS, int override_flags
 		}
 
 		if (ce->constructor) {
-			fci.size = sizeof(fci);
-			ZVAL_UNDEF(&fci.function_name);
-			fci.object = Z_OBJ_P(return_value);
-			fci.retval = &retval;
-			fci.params = NULL;
-			fci.param_count = 0;
-			fci.named_params = ctor_params;
-
-			fcc.function_handler = ce->constructor;
-			fcc.called_scope = Z_OBJCE_P(return_value);
-			fcc.object = Z_OBJ_P(return_value);
-
-			if (zend_call_function(&fci, &fcc) == FAILURE) {
-				zend_throw_exception_ex(zend_ce_exception, 0, "Could not execute %s::%s()", ZSTR_VAL(ce->name), ZSTR_VAL(ce->constructor->common.function_name));
-			} else {
-				zval_ptr_dtor(&retval);
-			}
+			zend_call_known_function(ce->constructor, Z_OBJ_P(return_value), Z_OBJCE_P(return_value),
+				/* retval */ NULL, /* argc */ 0, /* params */ NULL, ctor_params);
 		} else if (ctor_params && zend_hash_num_elements(ctor_params) > 0) {
 			/* TODO Convert this to a ValueError */
 			zend_argument_error(zend_ce_exception, ERROR_ARG_POS(3),
