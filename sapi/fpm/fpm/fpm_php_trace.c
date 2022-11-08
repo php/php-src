a59a94280032d174e0a4aa96efadfc9bb691f97d
George Peter Banyard <girgias@php.net>
2022-11-08 15:19:34 +0000

    Change return type of fpm_php_trace_dump() to bool
    As it only returns 0 or -1

--- sapi/fpm/fpm/fpm_php_trace.c
+++ sapi/fpm/fpm/fpm_php_trace.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <inttypes.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -34,7 +35,7 @@
 #endif
 
 
-static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ */
+static bool fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ */
 {
 	int callers_limit = child->wp->config->request_slowlog_trace_depth;
 	pid_t pid = child->pid;
@@ -52,19 +53,19 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 	fprintf(slowlog, "\n%s [pool %s] pid %d\n", buf, child->wp->config->name, (int) pid);
 
 	if (0 > fpm_trace_get_long((long) &SG(request_info).path_translated, &l)) {
-		return -1;
+		return false;
 	}
 
 	path_translated = l;
 
 	if (0 > fpm_trace_get_strz(buf, buf_size, path_translated)) {
-		return -1;
+		return false;
 	}
 
 	fprintf(slowlog, "script_filename = %s\n", buf);
 
 	if (0 > fpm_trace_get_long((long) &EG(current_execute_data), &l)) {
-		return -1;
+		return false;
 	}
 
 	execute_data = l;
@@ -77,14 +78,14 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 		uint32_t lineno = 0;
 
 		if (0 > fpm_trace_get_long(execute_data + offsetof(zend_execute_data, func), &l)) {
-			return -1;
+			return false;
 		}
 
 		function = l;
 
 		if (valid_ptr(function)) {
 			if (0 > fpm_trace_get_long(function + offsetof(zend_function, common.function_name), &l)) {
-				return -1;
+				return false;
 			}
 
 			function_name = l;
@@ -92,11 +93,11 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 			if (function_name == 0) {
 				uint32_t *call_info = (uint32_t *)&l;
 				if (0 > fpm_trace_get_long(execute_data + offsetof(zend_execute_data, This.u1.type_info), &l)) {
-					return -1;
+					return false;
 				}
 
 				if (ZEND_CALL_KIND_EX(*call_info) == ZEND_CALL_TOP_CODE) {
-					return 0;
+					return true;
 				} else if (ZEND_CALL_KIND_EX(*call_info) == ZEND_CALL_NESTED_CODE) {
 					memcpy(buf, "[INCLUDE_OR_EVAL]", sizeof("[INCLUDE_OR_EVAL]"));
 				} else {
@@ -104,7 +105,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 				}
 			} else {
 				if (0 > fpm_trace_get_strz(buf, buf_size, function_name + offsetof(zend_string, val))) {
-					return -1;
+					return false;
 				}
 
 			}
@@ -119,7 +120,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 		*buf = '\0';
 
 		if (0 > fpm_trace_get_long(execute_data + offsetof(zend_execute_data, prev_execute_data), &l)) {
-			return -1;
+			return false;
 		}
 
 		execute_data = prev = l;
@@ -128,7 +129,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 			zend_uchar *type;
 
 			if (0 > fpm_trace_get_long(prev + offsetof(zend_execute_data, func), &l)) {
-				return -1;
+				return false;
 			}
 
 			function = l;
@@ -139,22 +140,22 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 
 			type = (zend_uchar *)&l;
 			if (0 > fpm_trace_get_long(function + offsetof(zend_function, type), &l)) {
-				return -1;
+				return false;
 			}
 
 			if (ZEND_USER_CODE(*type)) {
 				if (0 > fpm_trace_get_long(function + offsetof(zend_op_array, filename), &l)) {
-					return -1;
+					return false;
 				}
 
 				file_name = l;
 
 				if (0 > fpm_trace_get_strz(buf, buf_size, file_name + offsetof(zend_string, val))) {
-					return -1;
+					return false;
 				}
 
 				if (0 > fpm_trace_get_long(prev + offsetof(zend_execute_data, opline), &l)) {
-					return -1;
+					return false;
 				}
 
 				if (valid_ptr(l)) {
@@ -162,7 +163,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 					uint32_t *lu = (uint32_t *) &l;
 
 					if (0 > fpm_trace_get_long(opline + offsetof(struct _zend_op, lineno), &l)) {
-						return -1;
+						return false;
 					}
 
 					lineno = *lu;
@@ -171,7 +172,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 			}
 
 			if (0 > fpm_trace_get_long(prev + offsetof(zend_execute_data, prev_execute_data), &l)) {
-				return -1;
+				return false;
 			}
 
 			prev = l;
@@ -184,7 +185,7 @@ static int fpm_php_trace_dump(struct fpm_child_s *child, FILE *slowlog) /* {{{ *
 		}
 	}
 
-	return 0;
+	return true;
 }
 /* }}} */
 
@@ -206,7 +207,7 @@ void fpm_php_trace(struct fpm_child_s *child) /* {{{ */
 		goto done1;
 	}
 
-	if (0 > fpm_php_trace_dump(child, slowlog)) {
+	if (!fpm_php_trace_dump(child, slowlog)) {
 		fprintf(slowlog, "+++ dump failed\n");
 	}
 
