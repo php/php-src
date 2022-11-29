28ae0c46707486f314b7a69e73cd3496c7ef6237
Christoph M. Becker <cmbecker69@gmx.de>
2022-11-29 12:17:54 +0100

    Fix #81742: open_basedir bypass in SQLite3 by using file URI
    A previous fix[1] was not sufficient to catch all potential file URIs,
    because the patch did not cater to URL encoding.  Properly parsing and
    decoding the URI may yield a different result than the handling of
    SQLite3, so we play it safe, and reject any file URIs if open_basedir
    is configured.
    
    [1] <https://bugs.php.net/bug.php?id=77967>

--- ext/sqlite3/sqlite3.c
+++ ext/sqlite3/sqlite3.c
@@ -2040,14 +2040,10 @@ static int php_sqlite3_authorizer(void *autharg, int action, const char *arg1, c
 			if (memcmp(arg1, ":memory:", sizeof(":memory:")) && *arg1) {
 				if (strncmp(arg1, "file:", 5) == 0) {
 					/* starts with "file:" */
-					if (!arg1[5]) {
+					if (PG(open_basedir) && *PG(open_basedir)) {
 						return SQLITE_DENY;
 					}
-					if (php_check_open_basedir(arg1 + 5)) {
-						return SQLITE_DENY;
-					}
-				}
-				if (php_check_open_basedir(arg1)) {
+				} else if (php_check_open_basedir(arg1)) {
 					return SQLITE_DENY;
 				}
 			}
