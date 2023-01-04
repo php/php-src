919dbb9e5eee36e4c2462caf6cc208df250d25d9
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 10:18:49 +0100

    Zend/zend_portability: include php_config.h for HAVE_*

--- Zend/zend_portability.h
+++ Zend/zend_portability.h
@@ -21,6 +21,12 @@
 #ifndef ZEND_PORTABILITY_H
 #define ZEND_PORTABILITY_H
 
+#ifdef PHP_WIN32
+#include "config.w32.h"
+#else
+#include "zend_config.h"
+#endif
+
 #ifdef __cplusplus
 #define BEGIN_EXTERN_C() extern "C" {
 #define END_EXTERN_C() }
