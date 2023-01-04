888b6a19c7443e5cbaba621e5750025cede6bbd8
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 09:21:39 +0100

    Zend/zend_compile: include cleanup

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -20,13 +20,11 @@
 #ifndef ZEND_COMPILE_H
 #define ZEND_COMPILE_H
 
-#include "zend.h"
+#include "zend.h" // for INTERNAL_FUNCTION_PARAMETERS
 #include "zend_ast.h"
+#include "zend_portability.h" //for ZEND_FASTCALL
 
-#include <stdarg.h>
 #include <stdint.h>
-
-#include "zend_llist.h"
 
 #define SET_UNUSED(op) do { \
 	op ## _type = IS_UNUSED; \
@@ -758,8 +756,6 @@ struct _zend_execute_data {
 #define IS_SMART_BRANCH_JMPNZ (1<<5)
 
 #define ZEND_EXTRA_VALUE 1
-
-#include "zend_globals.h"
 
 typedef enum _zend_compile_position {
 	ZEND_COMPILE_POSITION_AT_SHEBANG = 0,
