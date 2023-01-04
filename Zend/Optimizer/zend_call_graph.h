c7a4633891392d16d40b08135598807b3f0443b0
Max Kellermann <max.kellermann@ionos.com>
2023-01-04 21:55:41 +0100

    Zend/Optimizer/zend_call_graph: include cleanup

--- Zend/Optimizer/zend_call_graph.h
+++ Zend/Optimizer/zend_call_graph.h
@@ -20,8 +20,9 @@
 #define ZEND_CALL_GRAPH_H
 
 #include "zend_ssa.h"
-#include "zend_func_info.h"
-#include "zend_optimizer.h"
+
+typedef struct _zend_func_info zend_func_info;
+typedef struct _zend_call_info zend_call_info;
 
 typedef struct _zend_send_arg_info {
 	zend_op                *opline;
