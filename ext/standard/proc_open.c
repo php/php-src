62dc267ae559de86e51b475bf50d15fe61308031
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2023-01-06 21:47:22 +0100

    Fix GH-10239: proc_close after proc_get_status always returns -1
    The waitpid function only works once when a process is exited.
    Cache the result so subsequent status reads succeed.

--- ext/standard/proc_open.c
+++ ext/standard/proc_open.c
@@ -226,6 +226,28 @@ static void _php_free_envp(php_process_env env)
 }
 /* }}} */
 
+#if HAVE_SYS_WAIT_H
+static pid_t waitpid_cached(php_process_handle *proc, int *wait_status, int options)
+{
+	if (proc->has_stored_exit_wait_status) {
+		*wait_status = proc->stored_exit_wait_status_value;
+		return proc->child;
+	}
+
+	pid_t wait_pid = waitpid(proc->child, wait_status, options);
+
+	/* The "exit" status is the final status of the process.
+	 * If we were to store the status unconditionally,
+	 * we would return stale statuses in the future after the process continues. */
+	if (wait_pid > 0 && WIFEXITED(*wait_status)) {
+		proc->has_stored_exit_wait_status = true;
+		proc->stored_exit_wait_status_value = *wait_status;
+	}
+
+	return wait_pid;
+}
+#endif
+
 /* {{{ proc_open_rsrc_dtor
  * Free `proc` resource, either because all references to it were dropped or because `pclose` or
  * `proc_close` were called */
@@ -270,7 +292,7 @@ static void proc_open_rsrc_dtor(zend_resource *rsrc)
 		waitpid_options = WNOHANG;
 	}
 	do {
-		wait_pid = waitpid(proc->child, &wstatus, waitpid_options);
+		wait_pid = waitpid_cached(proc, &wstatus, waitpid_options);
 	} while (wait_pid == -1 && errno == EINTR);
 
 	if (wait_pid <= 0) {
@@ -383,7 +405,7 @@ PHP_FUNCTION(proc_get_status)
 	exitcode = running ? -1 : wstatus;
 
 #elif HAVE_SYS_WAIT_H
-	wait_pid = waitpid(proc->child, &wstatus, WNOHANG|WUNTRACED);
+	wait_pid = waitpid_cached(proc, &wstatus, WNOHANG|WUNTRACED);
 
 	if (wait_pid == proc->child) {
 		if (WIFEXITED(wstatus)) {
@@ -1244,6 +1266,9 @@ PHP_FUNCTION(proc_open)
 	proc->childHandle = childHandle;
 #endif
 	proc->env = env;
+#if HAVE_SYS_WAIT_H
+	proc->has_stored_exit_wait_status = false;
+#endif
 
 	/* Clean up all the child ends and then open streams on the parent
 	 *   ends, where appropriate */
