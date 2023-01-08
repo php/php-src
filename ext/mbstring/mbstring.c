d8b5b9fa559c4bbd7bba56e15e566f812a530e1c
Alex Dowad <alexinbeijing@gmail.com>
2023-01-08 13:40:26 +0200

    Add unit tests for mb_str_split/mb_substr on MacJapanese encoding
    MacJapanese has a somewhat unusual feature that when mapped to
    Unicode, many characters map to sequences of several codepoints.
    Add test cases demonstrating how mb_str_split and mb_substr behave in
    this situation.
    
    When adding these tests, I found the behavior of mb_substr was wrong
    due to an inconsistency between the string "length" as measured by
    mb_strlen and the number of native MacJapanese characters which
    mb_substr would count when iterating over the string using the
    mblen_table. This has been fixed.
    
    I believe that mb_strstr will also return wrong results in some cases
    for MacJapanese. I still need to come up with unit tests which
    demonstrate the problem and figure out how to fix it.

--- ext/mbstring/mbstring.c
+++ ext/mbstring/mbstring.c
@@ -2041,7 +2041,10 @@ static zend_string* mb_get_substr(zend_string *input, size_t from, size_t len, c
 			len = in_len;
 		}
 		return zend_string_init_fast((const char*)in, len);
-	} else if (enc->mblen_table != NULL) {
+	} else if (enc->mblen_table) {
+		/* The use of the `mblen_table` means that for encodings like MacJapanese,
+		 * we treat each character in its native charset as "1 character", even if it
+		 * maps to a sequence of several codepoints */
 		const unsigned char *mbtab = enc->mblen_table;
 		unsigned char *limit = in + in_len;
 		while (from && in < limit) {
@@ -2254,7 +2257,21 @@ PHP_FUNCTION(mb_substr)
 
 	size_t mblen = 0;
 	if (from < 0 || (!len_is_null && len < 0)) {
-		mblen = mb_get_strlen(str, enc);
+		if (enc->mblen_table) {
+			/* Because we use the `mblen_table` when iterating over the string and
+			 * extracting the requested part, we also need to use it here for counting
+			 * the "length" of the string
+			 * Otherwise, we can get wrong results for text encodings like MacJapanese,
+			 * where one native 'character' can map to a sequence of several codepoints */
+			const unsigned char *mbtab = enc->mblen_table;
+			unsigned char *p = (unsigned char*)ZSTR_VAL(str), *e = p + ZSTR_LEN(str);
+			while (p < e) {
+				p += mbtab[*p];
+				mblen++;
+			}
+		} else {
+			mblen = mb_get_strlen(str, enc);
+		}
 	}
 
 	/* if "from" position is negative, count start position from the end
