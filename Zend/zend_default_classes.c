70a8cb43ea6d9afdfceaf23948a393926fe9d890
Max Kellermann <max.kellermann@ionos.com>
2023-01-20 11:30:41 +0100

    Zend/zend_default_classes: include cleanup

--- Zend/zend_default_classes.c
+++ Zend/zend_default_classes.c
@@ -17,12 +17,9 @@
    +----------------------------------------------------------------------+
 */
 
-#include "zend.h"
-#include "zend_API.h"
-#include "zend_attributes.h"
-#include "zend_builtin_functions.h"
-#include "zend_interfaces.h"
 #include "zend_exceptions.h"
+#include "zend_attributes.h"
+#include "zend_interfaces.h"
 #include "zend_closures.h"
 #include "zend_generators.h"
 #include "zend_weakrefs.h"
