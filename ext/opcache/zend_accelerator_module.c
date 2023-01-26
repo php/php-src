php/php-src

commit 003346c450b58a07af9e0061bffb14c287c39be8

Simpler overflow check

ext/opcache/zend_accelerator_module.c
@@ -107,8 +107,6 @@ static ZEND_INI_MH(OnUpdateMemoryConsumption)
 #else
 	char *base = (char *) ts_resource(*((int *) mh_arg2));
 #endif
-	zend_long megabyte, overflow;
-	double dummy;
 
 	/* keep the compiler happy */
 	(void)entry; (void)mh_arg2; (void)mh_arg3; (void)stage;
@@ -132,10 +130,10 @@ static ZEND_INI_MH(OnUpdateMemoryConsumption)
 
 		ini_entry->value = zend_string_init(new_new_value, 1, 1);
 	}
-	megabyte = 1024 * 1024;
-	ZEND_SIGNED_MULTIPLY_LONG(memsize, megabyte, *p, dummy, overflow);
-	if (UNEXPECTED(overflow)) {
+	if (UNEXPECTED(memsize > ZEND_ULONG_MAX / (1024 * 1024))) {
 		*p = ZEND_ULONG_MAX;
+	} else {
+		*p = memsize * (1024 * 1024);
 	}
 	return SUCCESS;
 }

commit b51a802d0a9fc74c820cfbb634c48760db999aef

Fix overflow check in OnUpdateMemoryConsumption
memsize is a signed long, therefore the check against the
(*un*signed long maximum) / 1024² will never trigger. This check worked
correctly in d4b3f89c53f8 where it checked against the maximum signed
value, but was broken in 003346c450b5. Fix it by changing ZEND_ULONG_MAX
to ZEND_LONG_MAX.

ext/opcache/zend_accelerator_module.c
@@ -70,8 +70,8 @@ static ZEND_INI_MH(OnUpdateMemoryConsumption)
 		zend_accel_error(ACCEL_LOG_WARNING, "opcache.memory_consumption is set below the required 8MB.\n");
 		return FAILURE;
 	}
-	if (UNEXPECTED(memsize > ZEND_ULONG_MAX / (1024 * 1024))) {
-		*p = ZEND_ULONG_MAX;
+	if (UNEXPECTED(memsize > ZEND_LONG_MAX / (1024 * 1024))) {
+		*p = ZEND_LONG_MAX;
 	} else {
 		*p = memsize * (1024 * 1024);
 	}