b25347b5cc633f1761a26f6577a14843fa50400f
Guillaume Outters <guillaume-php@outters.eu>
2023-01-31 20:40:11 +0100

    Fix bug #65106: PHP fails to compile ext/fileinfo
    Make data_file.c's generator output its array initialization "by list of strings", instead of "by list of chars": that makes the compiler happier.
    Use strtr() with a precomputed map, instead of a loop over ord(), to generate in 1/100th the time.

--- ext/fileinfo/create_data_file.php
+++ ext/fileinfo/create_data_file.php
@@ -2,17 +2,29 @@
 /* This is a generated file, do not modify */
 /* Usage: php create_data_file.php /path/to/magic.mgc > data_file.c */
 <?php
-    $dta = file_get_contents( $argv[1] );
-    $dta_l = strlen($dta);
-    $j = 0;
+    /*--- Initialization of our translation table ---*/
 
-    echo "const unsigned char php_magic_database[$dta_l] = {\n";
-    for ($i = 0; $i < $dta_l; $i++) {
-        printf("0x%02X, ", ord($dta[$i]));
-        if ($j % 16 == 15) {
-            echo "\n";
-        }
-        $j++;
+    for ($i = 0; $i < 0x100; ++$i) {
+        $map[chr($i)] = sprintf('\x%02X', $i);
+    }
+    // \0 is a shortcut for \x00; as the majority of the input file is \0's,
+    // we divide the generated file's size by nearly 2 (30 MB -> 16 MB).
+    $map[chr(0)] = '\0';
+
+    /*--- File generation ---*/
+
+    // https://github.com/php/php-src/pull/10422
+    // Some compilers (GCC, clang) do not like long lists; some (MSVC) do not like long strings.
+    // CHUNK_SIZE splitting our ~10 MB binary source should give a good compromise between both.
+    const CHUNK_SIZE = 1024;
+
+    $dta = file_get_contents( $argv[1] );
+    $chunks = str_split($dta, CHUNK_SIZE);
+    $chunks[count($chunks) - 1] = str_pad($chunks[count($chunks) - 1], CHUNK_SIZE, chr(0));
+
+    echo 'const unsigned char php_magic_database[' . count($chunks) . '][' . CHUNK_SIZE . "] = {\n";
+    foreach ($chunks as $chunk) {
+        echo '"' . strtr($chunk, $map) . "\",\n";
     }
     echo "};\n";
 ?>
