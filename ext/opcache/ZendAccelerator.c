php/php-src

commit 44e5c04e5546e60cdbc31360adf3876b7c706152

Fix incorrect inheritance cache update (#10719)

ext/opcache/ZendAccelerator.c
@@ -2354,9 +2354,9 @@ static zend_class_entry* zend_accel_inheritance_cache_add(zend_class_entry *ce, 
 	SHM_UNPROTECT();
 	zend_shared_alloc_lock();
 
-	entry = ce->inheritance_cache;
+	entry = proto->inheritance_cache;
 	while (entry) {
-		entry = zend_accel_inheritance_cache_find(entry, ce, parent, traits_and_interfaces, &needs_autoload);
+		entry = zend_accel_inheritance_cache_find(entry, proto, parent, traits_and_interfaces, &needs_autoload);
 		if (entry) {
 			if (!needs_autoload) {
 				zend_shared_alloc_unlock();

commit 18b43d295043124a20ff147b1eb77a4df8945a49

Fix Zend/tests/type_declarations/variance/class_order_autoload1.phpt test failre introduced by 44e5c04e5546e60cdbc31360adf3876b7c706152
This ASSERT-ion path was never reached becuase of the bug fixed by
44e5c04e5546e60cdbc31360adf3876b7c706152. It's possible in case of
circular class dependencies that may resolved by __autoload().
Unfortunately these circular dependencies can't be stored in the
inheritace cahce.

ext/opcache/ZendAccelerator.c
@@ -2358,14 +2358,14 @@ static zend_class_entry* zend_accel_inheritance_cache_add(zend_class_entry *ce, 
 	while (entry) {
 		entry = zend_accel_inheritance_cache_find(entry, proto, parent, traits_and_interfaces, &needs_autoload);
 		if (entry) {
+			zend_shared_alloc_unlock();
+			SHM_PROTECT();
 			if (!needs_autoload) {
-				zend_shared_alloc_unlock();
-				SHM_PROTECT();
-
 				zend_map_ptr_extend(ZCSG(map_ptr_last));
 				return entry->ce;
+			} else {
+				return NULL;
 			}
-			ZEND_ASSERT(0); // entry = entry->next; // This shouldn't be posible ???
 		}
 	}
 