729f006de8dbd5294c2d4af176b021a7b0c11b4a
Patrick Allaert <patrickallaert@php.net>
2023-02-28 21:37:52 +0100

    PHP-8.1 is now for PHP 8.1.18-dev

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "4.1.17-dev"
+#define ZEND_VERSION "4.1.18-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 17
+#define PHP_RELEASE_VERSION 18
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.17-dev"
-#define PHP_VERSION_ID 80117
+#define PHP_VERSION "8.1.18-dev"
+#define PHP_VERSION_ID 80118
