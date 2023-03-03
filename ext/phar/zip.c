e633be3e87c59aa20c1a7edce2c259b48e2833f7
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2023-03-04 00:04:02 +0100

    Fix GH-10766: PharData archive created with Phar::Zip format does not keep files metadata (datetime)
    Due to an incorrect check, the datetime was never actually set.
    To test this we need to write the file using phar, but read the file
    using a different method to not get a cached, or a value that's been
    transformed twice and is therefore accidentally correct.
    
    Closes GH-10769

--- ext/phar/zip.c
+++ ext/phar/zip.c
@@ -147,7 +147,8 @@ static void phar_zip_u2d_time(time_t time, char *dtime, char *ddate) /* {{{ */
 	struct tm *tm, tmbuf;
 
 	tm = php_localtime_r(&time, &tmbuf);
-	if (tm->tm_year >= 1980) {
+	/* Note: tm_year is the year - 1900 */
+	if (tm->tm_year >= 80) {
 		cdate = ((tm->tm_year+1900-1980)<<9) + ((tm->tm_mon+1)<<5) + tm->tm_mday;
 		ctime = ((tm->tm_hour)<<11) + ((tm->tm_min)<<5) + ((tm->tm_sec)>>1);
 	} else {
