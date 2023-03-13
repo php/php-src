069fa1af7b4c3af94893e77aa6281835d2b01a37
Michael Voříšek <mvorisek@mvorisek.cz>
2023-03-13 14:03:23 +0100

    ctype functions

--- ext/ctype/ctype.stub.php
+++ ext/ctype/ctype.stub.php
@@ -2,24 +2,51 @@
 
 /** @generate-class-entries */
 
+/**
+ * @compile-time-eval
+ */
 function ctype_alnum(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_alpha(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_cntrl(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_digit(mixed $text): bool {}
 
 function ctype_lower(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_graph(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_print(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_punct(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_space(mixed $text): bool {}
 
 function ctype_upper(mixed $text): bool {}
 
+/**
+ * @compile-time-eval
+ */
 function ctype_xdigit(mixed $text): bool {}
