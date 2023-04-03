5d25f639147d3aa8e14b7fa0616b4209b1a769f7
Máté Kocsis <kocsismate@woohoolabs.com>
2023-04-03 21:56:26 +0200

    Minor changes

--- sapi/cli/php_cli.c
+++ sapi/cli/php_cli.c
@@ -558,19 +558,13 @@ static void cli_register_file_handles(void)
 	php_stream_to_zval(s_err, &ec.value);
 
 	Z_CONSTANT_FLAGS(ic.value) = 0;
-	zend_string *stdin_name = zend_string_init_interned("STDIN", sizeof("STDIN")-1, 0);
-	zend_register_constant(stdin_name, &ic);
-	zend_string_release(stdin_name);
+	zend_register_internal_constant("STDIN", sizeof("STDIN")-1, &ic);
 
 	Z_CONSTANT_FLAGS(oc.value) = 0;
-	zend_string *stdout_name = zend_string_init_interned("STDOUT", sizeof("STDOUT")-1, 0);
-	zend_register_constant(stdout_name, &oc);
-	zend_string_release(stdout_name);
+	zend_register_internal_constant("STDOUT", sizeof("STDOUT")-1, &oc);
 
 	Z_CONSTANT_FLAGS(ec.value) = 0;
-	zend_string *stderr_name = zend_string_init_interned("STDERR", sizeof("STDERR")-1, 0);
-	zend_register_constant(stderr_name, &ec);
-	zend_string_release(stderr_name);
+	zend_register_internal_constant("STDERR", sizeof("STDERR")-1, &ec);
 }
 
 static const char *param_mode_conflict = "Either execute direct code, process stdin or use a file.\n";
