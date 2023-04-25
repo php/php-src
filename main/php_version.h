725f136f9a86bd37591683899c852f77f7fbfc7c
Patrick Allaert <patrickallaert@php.net>
2023-04-25 16:18:30 +0200

    PHP-8.1 is now for PHP 8.1.20-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
-#define PHP_RELEASE_VERSION 19
+#define PHP_RELEASE_VERSION 20
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.19-dev"
-#define PHP_VERSION_ID 80119
+#define PHP_VERSION "8.1.20-dev"
+#define PHP_VERSION_ID 80120
