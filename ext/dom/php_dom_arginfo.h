761b9a44f8f097f77d0f96d479c485e9b11e51d6
divinity76 <divinity76@gmail.com>
2023-05-29 16:53:00 +0200

    Fix return value in stub file for DOMNodeList::item
    Not explicitly documenting the possibility of returning DOMElement causes
    the Intelephense linter (a popular PHP linter with ~9 million downloads:
    https://marketplace.visualstudio.com/items?itemName=bmewburn.vscode-intelephense-client)
    to think this code is bad:
    
      $xp->query("whatever")->item(0)->getAttribute("foo");
    
    DOMNode does not have getAttribute (while DOMElement does).
    Documenting the DOMElement return type should fix Intelephense's linter.
    
    Closes GH-11342.

--- ext/dom/php_dom_arginfo.h
+++ ext/dom/php_dom_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 74698bea9c5e0635cf91345e8512b9677489510c */
+ * Stub hash: a62e383b05df81ea245a7993215fb8ff4e1c7f9d */
 
 ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_dom_import_simplexml, 0, 1, DOMElement, 0)
 	ZEND_ARG_TYPE_INFO(0, node, IS_OBJECT, 0)
