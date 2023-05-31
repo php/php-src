737f8be45a0162763fddfa59d416c6ec82cce5e6
nielsdos <7771979+nielsdos@users.noreply.github.com>
2023-05-31 20:01:38 +0200

    Fix SimpleXML invalidations
    Test was Co-authored-by: tstarling

--- ext/simplexml/simplexml.c
+++ ext/simplexml/simplexml.c
@@ -43,6 +43,16 @@ PHP_SXE_API zend_class_entry *sxe_get_element_class_entry(void) /* {{{ */
 }
 /* }}} */
 
+/* Similar to php_dom_invalidate_node_list_cache(), but SimpleXML shouldn't depend on ext/dom. */
+static zend_always_inline void php_sxe_invalidate_node_list_cache(xmlNodePtr node)
+{
+	ZEND_ASSERT(node != NULL);
+	xmlDocPtr docp = node->doc;
+	if (docp && docp->_private) {
+		php_libxml_invalidate_node_list_cache(docp->_private);
+	}
+}
+
 static php_sxe_object* php_sxe_object_new(zend_class_entry *ce, zend_function *fptr_count);
 static xmlNodePtr php_sxe_reset_iterator(php_sxe_object *sxe, int use_data);
 static xmlNodePtr php_sxe_iterator_fetch(php_sxe_object *sxe, xmlNodePtr node, int use_data);
@@ -442,6 +452,8 @@ long_dim:
 
 	GET_NODE(sxe, node);
 
+	php_sxe_invalidate_node_list_cache(node);
+
 	if (sxe->iter.type == SXE_ITER_ATTRLIST) {
 		attribs = 1;
 		elements = 0;
@@ -812,6 +824,8 @@ static void sxe_prop_dim_delete(zend_object *object, zval *member, bool elements
 	sxe = php_sxe_fetch_object(object);
 
 	GET_NODE(sxe, node);
+
+	php_sxe_invalidate_node_list_cache(node);
 
 	if (Z_TYPE_P(member) == IS_LONG) {
 		if (sxe->iter.type != SXE_ITER_ATTRLIST) {
@@ -1685,6 +1699,8 @@ PHP_METHOD(SimpleXMLElement, addChild)
 
 	sxe = Z_SXEOBJ_P(ZEND_THIS);
 	GET_NODE(sxe, node);
+
+	php_sxe_invalidate_node_list_cache(node);
 
 	if (sxe->iter.type == SXE_ITER_ATTRLIST) {
 		php_error_docref(NULL, E_WARNING, "Cannot add element to attributes");
