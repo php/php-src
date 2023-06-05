4e587493ac2a2e0752567091ab54d43b9c1fe5a5
George Peter Banyard <girgias@php.net>
2023-06-05 15:37:24 +0100

    Use specialized VM functions again

--- Zend/zend_execute.c
+++ Zend/zend_execute.c
@@ -1506,6 +1506,26 @@ static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_use_object_as_array(v
 	zend_throw_error(NULL, "Cannot use object as array");
 }
 
+static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_illegal_array_offset_access(const zval *offset)
+{
+	zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_RW);
+}
+
+static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_illegal_array_offset_isset(const zval *offset)
+{
+	zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_IS);
+}
+
+static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_illegal_array_offset_unset(const zval *offset)
+{
+	zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_UNSET);
+}
+
+static zend_never_inline ZEND_COLD void ZEND_FASTCALL zend_illegal_string_offset_access(const zval *offset)
+{
+	zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_STRING), offset, BP_VAR_RW);
+}
+
 static zend_never_inline void zend_assign_to_object_dim(zend_object *obj, zval *dim, zval *value OPLINE_DC EXECUTE_DATA_DC)
 {
 	obj->handlers->write_dimension(obj, dim, value);
@@ -1631,7 +1651,7 @@ try_again:
 				}
 				return offset;
 			}
-			zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_STRING), dim, type);
+			zend_illegal_string_offset_access(dim);
 			return 0;
 		}
 		case IS_UNDEF:
@@ -1647,7 +1667,7 @@ try_again:
 			dim = Z_REFVAL_P(dim);
 			goto try_again;
 		default:
-			zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_STRING), dim, type);
+			zend_illegal_string_offset_access(dim);
 			return 0;
 	}
 
@@ -2370,7 +2390,7 @@ static zend_never_inline uint8_t slow_index_convert(HashTable *ht, const zval *d
 			value->lval = 1;
 			return IS_LONG;
 		default:
-			zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), dim, BP_VAR_R);
+			zend_illegal_array_offset_access(dim);
 			return IS_NULL;
 	}
 }
@@ -2444,7 +2464,7 @@ static zend_never_inline uint8_t slow_index_convert_w(HashTable *ht, const zval 
 			value->lval = 1;
 			return IS_LONG;
 		default:
-			zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), dim, BP_VAR_W);
+			zend_illegal_array_offset_access(dim);
 			return IS_NULL;
 	}
 }
@@ -2742,7 +2762,7 @@ try_string_offset:
 						ZVAL_NULL(result);
 						return;
 					}
-					zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_STRING), dim, BP_VAR_R);
+					zend_illegal_string_offset_access(dim);
 					ZVAL_NULL(result);
 					return;
 				}
@@ -2781,7 +2801,7 @@ try_string_offset:
 					dim = Z_REFVAL_P(dim);
 					goto try_string_offset;
 				default:
-					zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_STRING), dim, BP_VAR_R);
+					zend_illegal_string_offset_access(dim);
 					ZVAL_NULL(result);
 					return;
 			}
@@ -2903,7 +2923,7 @@ str_idx:
 		ZVAL_UNDEFINED_OP2();
 		goto str_idx;
 	} else {
-		zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_IS);
+		zend_illegal_array_offset_isset(offset);
 		return NULL;
 	}
 }
@@ -3026,7 +3046,7 @@ num_key:
 		str = ZSTR_EMPTY_ALLOC();
 		goto str_key;
 	} else {
-		zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), key, BP_VAR_R);
+		zend_illegal_array_offset_access(key);
 		return 0;
 	}
 }
--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -6098,7 +6098,7 @@ ZEND_VM_C_LABEL(num_index):
 			str = ZSTR_EMPTY_ALLOC();
 			ZEND_VM_C_GOTO(str_index);
 		} else {
-			zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_W);
+			zend_illegal_array_offset_access(offset);
 			zval_ptr_dtor_nogc(expr_ptr);
 		}
 		FREE_OP2();
@@ -6610,7 +6610,7 @@ ZEND_VM_C_LABEL(num_index_dim):
 				key = ZSTR_EMPTY_ALLOC();
 				ZEND_VM_C_GOTO(str_index_dim);
 			} else {
-				zend_illegal_container_offset(ZSTR_KNOWN(ZEND_STR_ARRAY), offset, BP_VAR_UNSET);
+				zend_illegal_array_offset_unset(offset);
 			}
 			break;
 		} else if (Z_ISREF_P(container)) {
