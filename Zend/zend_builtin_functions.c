bde6f2a2f74d0b5022c2657f5fd2f2b1196ae088
Florian Engelhardt <flo@dotbox.org>
2023-06-07 23:51:18 +0200

    Fix initial array size in `gc_status()` (#11393)
    Small fix for the initial array size to reflect the number of items that will be added.

--- Zend/zend_builtin_functions.c
+++ Zend/zend_builtin_functions.c
@@ -138,7 +138,7 @@ ZEND_FUNCTION(gc_status)
 
 	zend_gc_get_status(&status);
 
-	array_init_size(return_value, 3);
+	array_init_size(return_value, 8);
 
 	add_assoc_bool_ex(return_value, "running", sizeof("running")-1, status.active);
 	add_assoc_bool_ex(return_value, "protected", sizeof("protected")-1, status.gc_protected);
