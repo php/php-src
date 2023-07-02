13a1b4e47e93a7ba1d87b0e9d313ae62c2409558
David CARLIER <devnexen@gmail.com>
2023-07-02 17:03:24 +0100

    ext/intl: intl_CharFromString using zend_string_truncate to adjust the size of the buffer.

--- ext/intl/intl_convertcpp.cpp
+++ ext/intl/intl_convertcpp.cpp
@@ -79,8 +79,8 @@ zend_string* intl_charFromString(const UnicodeString &from, UErrorCode *status)
 		zend_string_free(u8res);
 		return NULL;
 	}
+	u8res = zend_string_truncate(u8res, actual_len, 0);
 	ZSTR_VAL(u8res)[actual_len] = '\0';
-	ZSTR_LEN(u8res) = actual_len;
 
 	return u8res;
 }
