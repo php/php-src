b3bd55f2448a2cef58ecda45403e5c4124dae57d
Máté Kocsis <kocsismate@woohoolabs.com>
2023-07-12 11:31:15 +0200

    Implement ldap_exop_sync

--- ext/ldap/ldap.stub.php
+++ ext/ldap/ldap.stub.php
@@ -777,6 +777,11 @@ namespace {
      * @param string $response_oid
      */
     function ldap_exop(LDAP\Connection $ldap, string $request_oid, ?string $request_data = null, ?array $controls = NULL, &$response_data = UNKNOWN, &$response_oid = null): LDAP\Result|bool {}
+    /**
+     * @param string $response_data
+     * @param string $response_oid
+     */
+    function ldap_exop_sync(LDAP\Connection $ldap, string $request_oid, ?string $request_data = null, ?array $controls = NULL, &$response_data = null, &$response_oid = null): LDAP\Result|bool {}
     #endif
 
     #ifdef HAVE_LDAP_PASSWD
