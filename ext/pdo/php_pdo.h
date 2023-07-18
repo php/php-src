46dd89aba9f4c16425d7c1f2921bb690f107cbcf
Danack <Danack@basereality.com>
2023-07-18 18:47:13 +0100

    Back to extern.

--- ext/pdo/php_pdo.h
+++ ext/pdo/php_pdo.h
@@ -22,8 +22,8 @@
 extern zend_module_entry pdo_module_entry;
 #define phpext_pdo_ptr &pdo_module_entry
 
-ZEND_API zend_class_entry *pdo_dbh_ce;
-ZEND_API zend_object *pdo_dbh_new(zend_class_entry *ce);
+extern zend_class_entry *pdo_dbh_ce;
+extern zend_object *pdo_dbh_new(zend_class_entry *ce);
 
 #include "php_version.h"
 #define PHP_PDO_VERSION PHP_VERSION
