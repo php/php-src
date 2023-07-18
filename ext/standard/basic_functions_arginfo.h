961cb3977f2cc48036771a8eb00a43ea4a0c8f1e
Jorg Adam Sowa <jorg.sowa@gmail.com>
2023-07-18 22:11:54 +0200

    Generated stub for basic_functions_arginfo.h

--- ext/standard/basic_functions_arginfo.h
+++ ext/standard/basic_functions_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: decfa1e3d862d81880ea18150e2ba239bf15b8af */
+ * Stub hash: c52a5a3fb7b22f324fa070cf999b53bf708f58bc */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_set_time_limit, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, seconds, IS_LONG, 0)
