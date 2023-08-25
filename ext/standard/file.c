e0dc5bddc310922f5c5434e6e828834a1b697d7d
Jakub Zelenka <bukka@php.net>
2023-08-25 18:26:19 +0100

    Fix GH-11982: str_getcsv returns null byte for unterminated quoted string

--- ext/standard/file.c
+++ ext/standard/file.c
@@ -2017,7 +2017,14 @@ PHPAPI HashTable *php_fgetcsv(php_stream *stream, char delimiter, char enclosure
 								memcpy(tptr, line_end, line_end_len);
 								tptr += line_end_len;
 
+								/* nothing can be fetched if stream is NULL (e.g. str_getcsv()) */
 								if (stream == NULL) {
+									/* the enclosure is unterminated */
+									if (bptr > limit) {
+										/* if the line ends with enclosure, we need to go back by
+										 * one character so the \0 character is not copied. */
+										--bptr;
+									}
 									goto quit_loop_2;
 								}
 
@@ -2028,6 +2035,11 @@ PHPAPI HashTable *php_fgetcsv(php_stream *stream, char delimiter, char enclosure
 									 * assign all the data from the start of
 									 * the enclosure to end of data to the
 									 * last element */
+									if (bptr > limit) {
+										/* if the line ends with enclosure, we need to go back by
+										 * one character so the \0 character is not copied. */
+										--bptr;
+									}
 									goto quit_loop_2;
 								}
 
