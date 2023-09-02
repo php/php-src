9a747059094ee55d53d40e781b0d4990c7457017
George Peter Banyard <girgias@php.net>
2023-09-03 00:12:25 +0100

    Fix OSS Fuzz #61865: Undef variable in ++/-- for declared property that is unset in error handler
    Reorder when we assign the property value to NULL which is identical to
    a3a3964497922bcd74282685218d90acea69c296
    
    Just for the declared property case instead of dynamic.

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -1117,8 +1117,10 @@ ZEND_API zval *zend_std_get_property_ptr_ptr(zend_object *zobj, zend_string *nam
 							ZSTR_VAL(name));
 						retval = &EG(error_zval);
 					} else {
-						ZVAL_NULL(retval);
 						zend_error(E_WARNING, "Undefined property: %s::$%s", ZSTR_VAL(zobj->ce->name), ZSTR_VAL(name));
+						/* We set the retval to null AFTER the warning so that an error handler cannot mess
+						 * with the property value... */
+						ZVAL_NULL(retval);
 					}
 				} else if (prop_info && UNEXPECTED(prop_info->flags & ZEND_ACC_READONLY)) {
 					/* Readonly property, delegate to read_property + write_property. */
