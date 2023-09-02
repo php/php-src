1a0300a8b8237cc899eb337b4bb2086e49fb74d1
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2023-09-02 20:04:53 +0200

    Update bundled pcre2 to 10.42

--- ext/pcre/pcre2lib/pcre2.h
+++ ext/pcre/pcre2lib/pcre2.h
@@ -42,9 +42,9 @@ POSSIBILITY OF SUCH DAMAGE.
 /* The current PCRE version information. */
 
 #define PCRE2_MAJOR           10
-#define PCRE2_MINOR           40
+#define PCRE2_MINOR           42
 #define PCRE2_PRERELEASE      
-#define PCRE2_DATE            2022-04-14
+#define PCRE2_DATE            2022-12-12
 
 /* When an application links to a PCRE DLL in Windows, the symbols that are
 imported have to be identified as such. When building PCRE2, the appropriate
