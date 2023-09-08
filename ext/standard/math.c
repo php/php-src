efd8e7d13254b8eb87bead4a5741d2b99b5ec715
Jorg Adam Sowa <jorg.sowa@gmail.com>
2023-09-09 00:16:52 +0200

    Fix rounding for large precision FP numbers for PHP_ROUND

--- ext/standard/math.c
+++ ext/standard/math.c
@@ -98,7 +98,7 @@ static inline double php_round_helper(double value, int mode) {
 	if (value >= 0.0) {
 		tmp_value = floor(value + 0.5);
 		if ((mode == PHP_ROUND_HALF_UP && value < (-0.5 + tmp_value)) ||
-			(mode == PHP_ROUND_HALF_DOWN && value == (-0.5 + tmp_value)) ||
+			(mode == PHP_ROUND_HALF_DOWN && value <= (-0.5 + tmp_value)) ||
 			(mode == PHP_ROUND_HALF_EVEN && value == (0.5 + 2 * floor(tmp_value/2.0))) ||
 			(mode == PHP_ROUND_HALF_ODD  && value == (0.5 + 2 * floor(tmp_value/2.0) - 1.0)))
 		{
@@ -107,7 +107,7 @@ static inline double php_round_helper(double value, int mode) {
 	} else {
 		tmp_value = ceil(value - 0.5);
 		if ((mode == PHP_ROUND_HALF_UP && value > (0.5 + tmp_value)) ||
-			(mode == PHP_ROUND_HALF_DOWN && value == (0.5 + tmp_value)) ||
+			(mode == PHP_ROUND_HALF_DOWN && value >= (0.5 + tmp_value)) ||
 			(mode == PHP_ROUND_HALF_EVEN && value == (-0.5 + 2 * ceil(tmp_value/2.0))) ||
 			(mode == PHP_ROUND_HALF_ODD  && value == (-0.5 + 2 * ceil(tmp_value/2.0) + 1.0)))
 		{
