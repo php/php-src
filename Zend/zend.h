5c1f7467169abf4e857bfe6163d0a92e7b2617a9
Sergey Panteleev <sergey@php.net>
2023-09-12 14:53:56 +0300

    PHP-8.2 is now for PHP 8.2.12-dev

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "4.2.11-dev"
+#define ZEND_VERSION "4.2.12-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 11
+#define PHP_RELEASE_VERSION 12
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.11-dev"
-#define PHP_VERSION_ID 80211
+#define PHP_VERSION "8.2.12-dev"
+#define PHP_VERSION_ID 80212
