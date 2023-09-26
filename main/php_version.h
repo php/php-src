e82057afb8f9aed8efecbcfee3a9f56c2003279c
Ben Ramsey <ramsey@php.net>
2023-09-26 18:41:46 -0500

    Update versions for PHP 8.1.24

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 24
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.24-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.1.24"
 #define PHP_VERSION_ID 80124
