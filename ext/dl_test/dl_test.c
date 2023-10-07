a8c6c6165b9c470577cab3db2a9ae3625cf17a84
Jakub Zelenka <bukka@php.net>
2023-10-07 13:59:40 +0100

    Fix GH-9921: Loading ext in FPM config does not register module handlers
    Closes GH-12377

--- ext/dl_test/dl_test.c
+++ ext/dl_test/dl_test.c
@@ -69,6 +69,10 @@ PHP_MINIT_FUNCTION(dl_test)
 		REGISTER_INI_ENTRIES();
 	}
 
+	if (getenv("PHP_DL_TEST_MODULE_DEBUG")) {
+		fprintf(stderr, "DL TEST MINIT\n");
+	}
+
 	return SUCCESS;
 }
 /* }}} */
@@ -83,6 +87,10 @@ static PHP_MSHUTDOWN_FUNCTION(dl_test)
 		UNREGISTER_INI_ENTRIES();
 	}
 
+	if (getenv("PHP_DL_TEST_MODULE_DEBUG")) {
+		fprintf(stderr, "DL TEST MSHUTDOWN\n");
+	}
+
 	return SUCCESS;
 }
 /* }}} */
@@ -93,6 +101,21 @@ PHP_RINIT_FUNCTION(dl_test)
 #if defined(ZTS) && defined(COMPILE_DL_DL_TEST)
 	ZEND_TSRMLS_CACHE_UPDATE();
 #endif
+
+	if (getenv("PHP_DL_TEST_MODULE_DEBUG")) {
+		fprintf(stderr, "DL TEST RINIT\n");
+	}
+
+	return SUCCESS;
+}
+/* }}} */
+
+/* {{{ PHP_RSHUTDOWN_FUNCTION */
+PHP_RSHUTDOWN_FUNCTION(dl_test)
+{
+	if (getenv("PHP_DL_TEST_MODULE_DEBUG")) {
+		fprintf(stderr, "DL TEST RSHUTDOWN\n");
+	}
 
 	return SUCCESS;
 }
@@ -127,7 +150,7 @@ zend_module_entry dl_test_module_entry = {
 	PHP_MINIT(dl_test),
 	PHP_MSHUTDOWN(dl_test),
 	PHP_RINIT(dl_test),
-	NULL,
+	PHP_RSHUTDOWN(dl_test),
 	PHP_MINFO(dl_test),
 	PHP_DL_TEST_VERSION,
 	PHP_MODULE_GLOBALS(dl_test),
