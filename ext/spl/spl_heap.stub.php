f0757100a41647b777e03179ded2b279ae57bfa5
Jorg Adam Sowa <jorg.sowa@gmail.com>
2023-10-07 14:14:57 +0200

    Typed constants in SPL extension (#12358)

--- ext/spl/spl_heap.stub.php
+++ ext/spl/spl_heap.stub.php
@@ -4,21 +4,12 @@
 
 class SplPriorityQueue implements Iterator, Countable
 {
-    /**
-     * @var int
-     * @cvalue SPL_PQUEUE_EXTR_BOTH
-     */
-    public const EXTR_BOTH = UNKNOWN;
-    /**
-     * @var int
-     * @cvalue SPL_PQUEUE_EXTR_PRIORITY
-     */
-    public const EXTR_PRIORITY = UNKNOWN;
-    /**
-     * @var int
-     * @cvalue SPL_PQUEUE_EXTR_DATA
-     */
-    public const EXTR_DATA = UNKNOWN;
+    /** @cvalue SPL_PQUEUE_EXTR_BOTH */
+    public const int EXTR_BOTH = UNKNOWN;
+    /** @cvalue SPL_PQUEUE_EXTR_PRIORITY */
+    public const int EXTR_PRIORITY = UNKNOWN;
+    /** @cvalue SPL_PQUEUE_EXTR_DATA */
+    public const int EXTR_DATA = UNKNOWN;
 
     /** @tentative-return-type */
     public function compare(mixed $priority1, mixed $priority2): int {}
