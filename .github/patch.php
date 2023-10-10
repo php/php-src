6588b796739b5d33c9cf3d84a7ba219f387bfc9e
Daniil Gentili <daniil@daniil.it>
2023-10-10 15:04:42 +0200

    Fixup

--- .github/patch.php
+++ .github/patch.php
@@ -4,22 +4,34 @@ register_shutdown_function(function () {
     $status = opcache_get_status(false);
     var_dump($status);
 
+    $ok = true;
     if ($status["memory_usage"]["free_memory"] < 10*1024) {
         echo "Not enough free opcache memory!".PHP_EOL;
-        die(130);
+        $ok = false;
     }
     if ($status["interned_strings_usage"]["free_memory"] < 1*1024) {
         echo "Not enough free interned strings memory!".PHP_EOL;
-        die(130);
+        $ok = false;
     }
     if ($status["jit"]["buffer_free"] < 10*1024) {
         echo "Not enough free JIT memory!".PHP_EOL;
-        die(130);
+        $ok = false;
+    }
+    if (!$status["jit"]["on"]) {
+        echo "JIT is not enabled!".PHP_EOL;
+        $ok = false;
     }
 
     unset($status);
-
     gc_collect_cycles();
+
+    if (!$ok) die(130);
 });
 
-require $argv[1];
+$argc--;
+array_shift($argv);
+
+$_SERVER['argc']--;
+array_shift($_SERVER['argv']);
+
+require $argv[0];
