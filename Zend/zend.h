2642a08697448668506237adf591dcdd69de1533
Pierrick Charron <pierrick@php.net>
2023-10-10 11:45:26 -0400

    PHP-8.2 is now for PHP 8.2.13-dev

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "4.2.12-dev"
+#define ZEND_VERSION "4.2.13-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 12
+#define PHP_RELEASE_VERSION 13
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.12-dev"
-#define PHP_VERSION_ID 80212
+#define PHP_VERSION "8.2.13-dev"
+#define PHP_VERSION_ID 80213
