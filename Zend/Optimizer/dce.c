e8cf3a369ac629502412d362a38c1d7056211a90
Dmitry Stogov <dmitry@zend.com>
2023-11-13 21:38:33 +0300

    Fixed incorrect optimization

--- Zend/Optimizer/dce.c
+++ Zend/Optimizer/dce.c
@@ -520,10 +520,6 @@ static inline bool may_break_varargs(const zend_op_array *op_array, const zend_s
 	return 0;
 }
 
-static inline bool may_throw_dce_exception(const zend_op *opline) {
-	return opline->opcode == ZEND_ADD_ARRAY_ELEMENT && opline->op2_type == IS_UNUSED;
-}
-
 int dce_optimize_op_array(zend_op_array *op_array, zend_ssa *ssa, bool reorder_dtor_effects) {
 	int i;
 	zend_ssa_phi *phi;
@@ -590,8 +586,7 @@ int dce_optimize_op_array(zend_op_array *op_array, zend_ssa *ssa, bool reorder_d
 					add_operands_to_worklists(&ctx, &op_array->opcodes[op_data], &ssa->ops[op_data], ssa, 0);
 				}
 			} else if (may_have_side_effects(op_array, ssa, &op_array->opcodes[i], &ssa->ops[i], ctx.reorder_dtor_effects)
-					|| (zend_may_throw(&op_array->opcodes[i], &ssa->ops[i], op_array, ssa)
-						&& !may_throw_dce_exception(&op_array->opcodes[i]))
+					|| zend_may_throw(&op_array->opcodes[i], &ssa->ops[i], op_array, ssa)
 					|| (has_varargs && may_break_varargs(op_array, ssa, &ssa->ops[i]))) {
 				if (op_array->opcodes[i].opcode == ZEND_NEW
 						&& op_array->opcodes[i+1].opcode == ZEND_DO_FCALL
--- ext/opcache/tests/jit/array_elem_002.phpt
+++ ext/opcache/tests/jit/array_elem_002.phpt
@@ -7,14 +7,19 @@ opcache.file_update_protection=0
 opcache.jit_buffer_size=1M
 --FILE--
 <?php
-$float = 100000000000000000000000000000000000000;
-$string_float= PHP_INT_MAX;
-$a = [$float => 'a', $string_float => 'b', 'c', 'd'];
+function foo() {
+    $float = 100000000000000000000000000000000000000;
+    $string_float= PHP_INT_MAX;
+    $a = [$float => 'a', $string_float => 'b', 'c', 'd'];
+}
+foo();
 ?>
+DONE
 --EXPECTF--
-Deprecated: Implicit conversion from float 1.0E+38 to int loses precision in %sarray_elem_002.php on line 4
+Deprecated: Implicit conversion from float 1.0E+38 to int loses precision in %sarray_elem_002.php on line 5
 
-Fatal error: Uncaught Error: Cannot add element to the array as the next element is already occupied in %sarray_elem_002.php:4
+Fatal error: Uncaught Error: Cannot add element to the array as the next element is already occupied in %sarray_elem_002.php:5
 Stack trace:
-#0 {main}
-  thrown in %sarray_elem_002.php on line 4
+#0 %sarray_elem_002.php(7): foo()
+#1 {main}
+  thrown in %sarray_elem_002.php on line 5
