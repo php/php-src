8a95e616b91ac0eeedba90a61e36e652919763f2
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2023-11-17 19:45:40 +0100

    Fix GH-12702: libxml2 2.12.0 issue building from src
    Fixes GH-12702.
    
    Co-authored-by: nono303 <github@nono303.net>

--- ext/dom/document.c
+++ ext/dom/document.c
@@ -23,6 +23,7 @@
 #if defined(HAVE_LIBXML) && defined(HAVE_DOM)
 #include "php_dom.h"
 #include <libxml/SAX.h>
+#include <libxml/xmlsave.h>
 #ifdef LIBXML_SCHEMAS_ENABLED
 #include <libxml/relaxng.h>
 #include <libxml/xmlschemas.h>
