f1e88dfa82271aaaf0244f5156a3fe3cb1caaedc
Sergey Panteleev <sergey@php.net>
2023-11-21 12:47:00 +0300

    Update versions for PHP 8.2.13

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
 #define PHP_RELEASE_VERSION 13
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.13-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.2.13"
 #define PHP_VERSION_ID 80213
