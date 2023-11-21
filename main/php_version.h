d26068059e83fe40de3430a512471d194119bee0
Jakub Zelenka <bukka@php.net>
2023-11-21 14:39:29 +0000

    Update versions for PHP 8.3.0

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.0-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.3.0"
 #define PHP_VERSION_ID 80300
