dc3641ea0f39fe0f71c5cecaaf2646437e780fff
Pierrick Charron <pierrick@php.net>
2023-12-05 15:01:08 -0500

    PHP-8.2 is now for PHP 8.2.15-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 14
+#define PHP_RELEASE_VERSION 15
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.14-dev"
-#define PHP_VERSION_ID 80214
+#define PHP_VERSION "8.2.15-dev"
+#define PHP_VERSION_ID 80215
