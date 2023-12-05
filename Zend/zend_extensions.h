78f5ea1eba7c1247d375d5dd40f34d9076d5e1fd
Eric Mann <ericmann@php.net>
2023-12-05 07:48:07 -0800

    Update versions for PHP 8.3.1RC1

--- Zend/zend_extensions.h
+++ Zend/zend_extensions.h
@@ -44,7 +44,7 @@ You can use the following macro to check the extension API version for compatibi
 
 /* The first number is the engine version and the rest is the date (YYYYMMDD).
  * This way engine 2/3 API no. is always greater than engine 1 API no..  */
-#define ZEND_EXTENSION_API_NO	420230831
+#define ZEND_EXTENSION_API_NO	420231207
 
 typedef struct _zend_extension_version_info {
 	int zend_extension_api_no;
