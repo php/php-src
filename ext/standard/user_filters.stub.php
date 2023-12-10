e706eeba8393290ebef0749f28667987eba55432
Javier Spagnoletti <phansys@gmail.com>
2023-12-10 19:25:51 -0300

    Use type declaration in argument 3 of `php_user_filter::filter()`

--- ext/standard/user_filters.stub.php
+++ ext/standard/user_filters.stub.php
@@ -44,10 +44,9 @@ class php_user_filter
     /**
      * @param resource $in
      * @param resource $out
-     * @param int $consumed
      * @tentative-return-type
      */
-    public function filter($in, $out, &$consumed, bool $closing): int {}
+    public function filter($in, $out, int &$consumed, bool $closing): int {}
 
     /** @tentative-return-type */
     public function onCreate(): bool {}
