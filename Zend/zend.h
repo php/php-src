7f5048c8e0360edd6f06b29c182db235e9361f0d
Patrick Allaert <patrickallaert@php.net>
2023-12-19 21:33:38 +0100

    Update versions for PHP 8.1.27

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "4.1.27-dev"
+#define ZEND_VERSION "4.1.27"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 1
 #define PHP_RELEASE_VERSION 27
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.1.27-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.1.27"
 #define PHP_VERSION_ID 80127
