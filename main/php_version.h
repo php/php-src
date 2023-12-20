6d39b647b4be03498e5e8f2a23b5ccad5c2e35e7
Pierrick Charron <pierrick@php.net>
2023-12-20 07:38:41 -0500

    Prepare PHP 8.3.1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 1
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.1-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.3.1"
 #define PHP_VERSION_ID 80301
