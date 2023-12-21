727c2fec174e8857436024130b4b631aabc05a22
Muhammad Moinur Rahman <moin@bofh.im>
2023-12-21 21:19:54 +0100

    Fix sysconf functions in FreeBSD
    All FreeBSD do not implement the function sysconf with arguments
    _SC_GETGR_R_SIZE_MAX , _SC_GETPW_R_SIZE_MAX.
    
    php use this sysconf() in:
    - main/main.c php_get_current_user()
    - main/fopen_wrappers.c php_fopen_primary_script()
    - ext/posix/posix.c posix_getpwnam(), posix_getpwuid(),
      posix_getgrnam(), posix_getgrgid()
    - ext/standard/filestat.c php_get_uid_by_name(), php_get_gid_by_name()

--- ext/standard/filestat.c
+++ ext/standard/filestat.c
@@ -281,7 +281,11 @@ PHPAPI zend_result php_get_gid_by_name(const char *name, gid_t *gid)
 		char *grbuf;
 
 #if defined(__FreeBSD__) && defined(_SC_PAGESIZE)
+<<<<<<< HEAD
                 if (grbuflen < 1) buflen = sysconf(_SC_PAGESIZE);
+=======
+                if (grbuflen < 1) grbuflen = sysconf(_SC_PAGESIZE);
+>>>>>>> 47e82f12f8 (Fix sysconf functions in FreeBSD)
 #endif
 		if (grbuflen < 1) {
 			return FAILURE;
@@ -410,7 +414,7 @@ PHPAPI zend_result php_get_uid_by_name(const char *name, uid_t *uid)
 		char *pwbuf;
 
 #if defined(__FreeBSD__) && defined(_SC_PAGESIZE)
-                if (pwbuflen < 1) buflen = sysconf(_SC_PAGESIZE);
+                if (pwbuflen < 1) pwbuflen = sysconf(_SC_PAGESIZE);
 #endif
 		if (pwbuflen < 1) {
 			return FAILURE;
