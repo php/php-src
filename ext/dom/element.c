b2d778c36e8a3edbf5c11cd7ab475095fbb281cb
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2023-12-22 21:12:59 +0100

    Fix crash when toggleAttribute() is used without a document

--- ext/dom/element.c
+++ ext/dom/element.c
@@ -1554,7 +1554,7 @@ PHP_METHOD(DOMElement, toggleAttribute)
 	}
 
 	/* Step 2 */
-	if (thisp->doc->type == XML_HTML_DOCUMENT_NODE && (thisp->ns == NULL || xmlStrEqual(thisp->ns->href, (const xmlChar *) "http://www.w3.org/1999/xhtml"))) {
+	if (thisp->doc != NULL && thisp->doc->type == XML_HTML_DOCUMENT_NODE && (thisp->ns == NULL || xmlStrEqual(thisp->ns->href, (const xmlChar *) "http://www.w3.org/1999/xhtml"))) {
 		qname_tmp = zend_str_tolower_dup_ex(qname, qname_length);
 		if (qname_tmp != NULL) {
 			qname = qname_tmp;
