c5dbda2a145899ed34c58ac7847290cf35d853e2
Máté Kocsis <kocsismate@woohoolabs.com>
2023-12-23 15:02:39 +0100

    Add new Firebird class constants

--- ext/pdo_firebird/pdo_firebird.stub.php
+++ ext/pdo_firebird/pdo_firebird.stub.php
@@ -16,4 +16,19 @@ class PdoFirebird extends PDO
 
     /** @cvalue PDO_FB_ATTR_TIMESTAMP_FORMAT */
     public const int ATTR_TIMESTAMP_FORMAT = UNKNOWN;
+
+    /** @cvalue PDO_FB_TRANSACTION_ISOLATION_LEVEL */
+    public const int TRANSACTION_ISOLATION_LEVEL = UNKNOWN;
+
+    /** @cvalue PDO_FB_READ_COMMITTED */
+    public const int READ_COMMITTED = UNKNOWN;
+
+    /** @cvalue PDO_FB_REPEATABLE_READ */
+    public const int REPEATABLE_READ = UNKNOWN;
+
+    /** @cvalue PDO_FB_SERIALIZABLE */
+    public const int SERIALIZABLE = UNKNOWN;
+
+    /** @cvalue PDO_FB_WRITABLE_TRANSACTION */
+    public const int WRITABLE_TRANSACTION = UNKNOWN;
 }
