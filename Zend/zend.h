d33a5348631ffd1ddb7c7c4795fabd77412bb389
Jakub Zelenka <bukka@php.net>
2024-01-02 15:50:36 +0000

    PHP-8.3 is now for PHP-8.3.3-dev

--- Zend/zend.h
+++ Zend/zend.h
@@ -20,7 +20,7 @@
 #ifndef ZEND_H
 #define ZEND_H
 
-#define ZEND_VERSION "4.3.2-dev"
+#define ZEND_VERSION "4.3.3-dev"
 
 #define ZEND_ENGINE_3
 
--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 2
+#define PHP_RELEASE_VERSION 3
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.2-dev"
-#define PHP_VERSION_ID 80302
+#define PHP_VERSION "8.3.3-dev"
+#define PHP_VERSION_ID 80303
