5d79c1b74d32a4fcce487c33edf8158f54f86c6d
Sergey Panteleev <sergey@php.net>
2024-01-02 16:42:14 +0300

    PHP-8.2 is now for PHP 8.2.16-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 15
+#define PHP_RELEASE_VERSION 16
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.15-dev"
-#define PHP_VERSION_ID 80215
+#define PHP_VERSION "8.2.16-dev"
+#define PHP_VERSION_ID 80216
