10e8a0d17e4c1b2e9148560a843e18b891954c49
Máté Kocsis <kocsismate@woohoolabs.com>
2024-01-08 16:16:45 +0100

    Backport upgrading PHP-Parser to PHP-8.3

--- build/gen_stub.php
+++ build/gen_stub.php
@@ -4925,7 +4925,7 @@ function initPhpParser() {
     }
 
     $isInitialized = true;
-    $version = "5.0.0alpha3";
+    $version = "5.0.0";
     $phpParserDir = __DIR__ . "/PHP-Parser-$version";
     if (!is_dir($phpParserDir)) {
         installPhpParser($version, $phpParserDir);
