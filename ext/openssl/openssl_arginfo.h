b4d4bc4d4a3d9351abb4bdb57da280722c686938
Vincent JARDIN <vjardin@free.fr>
2024-01-14 18:58:28 +0100

    generate openssl_arginfo.h from the sub
    using:
      ./configure --prefix=/home/vjardin/bin --enable-debug --with-openssl --enable-tokenizer  --disable-all --disable-cgi
      sapi/cli/php build/gen_stub.php -f ext/openssl/openssl.stub.php

--- ext/openssl/openssl_arginfo.h
+++ ext/openssl/openssl_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 1bb1eab5199d2e72e624f7e93f59dd5114ed7c86 */
+ * Stub hash: 295c11739a3588a8254db2fe904be6f9911f85b3 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_openssl_x509_export_to_file, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_TYPE_MASK(0, certificate, OpenSSLCertificate, MAY_BE_STRING, NULL)
