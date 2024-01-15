935c4c9a918d38de04b25a2bf005c5e7b0b885b1
divinity76 <hans@loltek.net>
2024-01-15 22:18:04 +0100

    throw instead of false

--- ext/curl/multi.c
+++ ext/curl/multi.c
@@ -65,9 +65,8 @@ PHP_FUNCTION(curl_multi_init)
 	ZEND_PARSE_PARAMETERS_NONE();
 	multi = curl_multi_init();
 	if(UNEXPECTED(multi == NULL)) {
-		// todo: should it throw or return false? (curl_init returns false)
-		php_error_docref(NULL, E_WARNING, "Could not initialize a new cURL multi handle");
-		RETURN_FALSE;
+		zend_throw_error(NULL, "Could not initialize a new cURL multi handle");
+		RETURN_THROWS();
 	}
 	object_init_ex(return_value, curl_multi_ce);
 	mh = Z_CURL_MULTI_P(return_value);
