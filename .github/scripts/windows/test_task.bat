2d94d0fb9acea3f360aca3cebde9226f5ea80cdc
Saki Takamachi <saki@sakiot.com>
2024-01-23 01:31:54 +0900

    wip

--- .github/scripts/windows/test_task.bat
+++ .github/scripts/windows/test_task.bat
@@ -109,10 +109,7 @@ set PHP_BUILD_DIR=%PHP_BUILD_OBJ_DIR%\Release
 if "%THREAD_SAFE%" equ "1" set PHP_BUILD_DIR=%PHP_BUILD_DIR%_TS
 
 rem prepare for mail
-curl -sLo hMailServer.exe https://www.hmailserver.com/download_file/?downloadid=271
-hMailServer.exe /verysilent
-cd %APPVEYOR_BUILD_FOLDER%
-%PHP_BUILD_DIR%\php.exe -dextension_dir=%PHP_BUILD_DIR% -dextension=com_dotnet appveyor\setup_hmailserver.php
+start %PHP_BUILD_CACHE_SDK_DIR%\bin\mailpit_amd.exe
 
 mkdir %PHP_BUILD_DIR%\test_file_cache
 rem generate php.ini
