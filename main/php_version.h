528cb5aea8da14bc25cd6bf7371888632fcbc9e4
Eric Mann <ericmann@php.net>
2024-01-30 12:36:59 -0800

    Update versions for PHP 8.3.3RC1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
 #define PHP_RELEASE_VERSION 3
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.3-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "8.3.3RC1"
 #define PHP_VERSION_ID 80303
