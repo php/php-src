1a349ab2096760f838d0b7188229ceaf21538fc9
Peter Kokot <peterkokot@gmail.com>
2024-01-31 11:52:38 +0100

    Update PHP 8.3 minor version

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 3
+#define PHP_RELEASE_VERSION 4
 #define PHP_EXTRA_VERSION "-dev"
 #define PHP_VERSION "8.3.4-dev"
 #define PHP_VERSION_ID 80304
