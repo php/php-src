8d90d861cd4d40f542aa1d0e81455d2994878905
Shivam Mathur <shivam_jpr@hotmail.com>
2024-02-02 09:45:07 +0530

    Update .github/nightly_matrix.php
    Co-authored-by: Ilija Tovilo <ilija.tovilo@me.com>

--- .github/nightly_matrix.php
+++ .github/nightly_matrix.php
@@ -103,7 +103,7 @@ function get_macos_matrix_include(array $branches) {
                     'os' => $branch === 'master' ? '13' : '12',
                     'arch' => 'X64',
                 ];
-                if($branch['version']['minor'] === 4) {
+                if ($branch['version']['minor'] >= 4 || $branch['version']['major'] >= 9) {
                     $jobs[] = [
                         'branch' => $branch,
                         'debug' => $debug,
