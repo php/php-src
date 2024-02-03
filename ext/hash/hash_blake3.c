1954b37c3933e4992dd8baf0712fbd4804739075
hanshenrik <hans@easy-ad.no>
2024-02-03 16:33:01 +0100

    license headers

--- ext/hash/hash_blake3.c
+++ ext/hash/hash_blake3.c
@@ -1,3 +1,19 @@
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Hans Henrik Bergan <hans@loltek.net>                         |
+   +----------------------------------------------------------------------+
+*/
+
 #include "php_hash.h"
 #include "php_hash_blake3.h"
 #include "blake3/upstream_blake3/c/blake3.h"
