20c49f8524abad71b1cbae2caed0a8601486dbbc
haszi <haszi@users.noreply.github.com>
2024-02-12 22:37:32 +0100

    Move PASSWORD constants to stubs (#13349)

--- ext/sodium/sodium_pwhash.c
+++ ext/sodium/sodium_pwhash.c
@@ -19,24 +19,14 @@
 #endif
 
 #include "php.h"
-#include "php_libsodium.h"
 #include "ext/standard/php_password.h"
 
 #include <sodium.h>
 
-#if SODIUM_LIBRARY_VERSION_MAJOR > 9 || (SODIUM_LIBRARY_VERSION_MAJOR == 9 && SODIUM_LIBRARY_VERSION_MINOR >= 6)
+#include "php_libsodium.h"
+#include "sodium_pwhash_arginfo.h"
 
-/**
- * MEMLIMIT is normalized to KB even though sodium uses Bytes in order to
- * present a consistent user-facing API.
- *
- * Threads are fixed at 1 by libsodium.
- *
- * When updating these values, synchronize ext/standard/php_password.h values.
- */
-#define PHP_SODIUM_PWHASH_MEMLIMIT (64 << 10)
-#define PHP_SODIUM_PWHASH_OPSLIMIT 4
-#define PHP_SODIUM_PWHASH_THREADS 1
+#if SODIUM_LIBRARY_VERSION_MAJOR > 9 || (SODIUM_LIBRARY_VERSION_MAJOR == 9 && SODIUM_LIBRARY_VERSION_MINOR >= 6)
 
 static inline int get_options(zend_array *options, size_t *memlimit, size_t *opslimit) {
 	zval *opt;
@@ -180,21 +170,14 @@ PHP_MINIT_FUNCTION(sodium_password_hash) /* {{{ */ {
 	}
 	zend_string_release(argon2i);
 
+	register_sodium_pwhash_symbols(module_number);
+
 	if (FAILURE == php_password_algo_register("argon2i", &sodium_algo_argon2i)) {
 		return FAILURE;
 	}
-	REGISTER_STRING_CONSTANT("PASSWORD_ARGON2I", "argon2i", CONST_PERSISTENT);
-
 	if (FAILURE == php_password_algo_register("argon2id", &sodium_algo_argon2id)) {
 		return FAILURE;
 	}
-	REGISTER_STRING_CONSTANT("PASSWORD_ARGON2ID", "argon2id", CONST_PERSISTENT);
-
-	REGISTER_LONG_CONSTANT("PASSWORD_ARGON2_DEFAULT_MEMORY_COST", PHP_SODIUM_PWHASH_MEMLIMIT, CONST_PERSISTENT);
-	REGISTER_LONG_CONSTANT("PASSWORD_ARGON2_DEFAULT_TIME_COST", PHP_SODIUM_PWHASH_OPSLIMIT, CONST_PERSISTENT);
-	REGISTER_LONG_CONSTANT("PASSWORD_ARGON2_DEFAULT_THREADS", PHP_SODIUM_PWHASH_THREADS, CONST_PERSISTENT);
-
-	REGISTER_STRING_CONSTANT("PASSWORD_ARGON2_PROVIDER", "sodium", CONST_PERSISTENT);
 
 	return SUCCESS;
 }
