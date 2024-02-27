e1a8ebd61b053aa9a2837e0c6718586f752b279a
Sergey Panteleev <sergey@php.net>
2024-02-27 17:26:44 +0300

    PHP-8.2 is now for PHP 8.2.18-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
-#define PHP_RELEASE_VERSION 17
+#define PHP_RELEASE_VERSION 18
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.17-dev"
-#define PHP_VERSION_ID 80217
+#define PHP_VERSION "8.2.18-dev"
+#define PHP_VERSION_ID 80218
