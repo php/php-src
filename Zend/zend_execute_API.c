7c7a3a7eba410fee5249984aab584b2a33686859
Manuel Kress <6232639+windaishi@users.noreply.github.com>
2024-03-02 18:27:27 +0100

    Update Zend/zend_execute_API.c

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -1522,7 +1522,7 @@ static void zend_set_timeout_ex(zend_long seconds, bool reset_signals) /* {{{ */
 			t_r.it_value.tv_sec = seconds;
 			t_r.it_value.tv_usec = t_r.it_interval.tv_sec = t_r.it_interval.tv_usec = 0;
 
-# if defined(__CYGWIN__) || defined(__PASE__) || defined(__aarch64__)
+# if defined(__CYGWIN__) || defined(__PASE__) || (defined(__aarch64__) && defined(__APPLE__))
             // ITIMER_PROF is broken in Apple Silicon system with MacOS >= 14
 			setitimer(ITIMER_REAL, &t_r, NULL);
 		}
