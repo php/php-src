caae7c71d86d180d2974c12096a681587d7fe6a3
Saki Takamachi <saki@sakiot.com>
2024-03-12 23:47:26 +0900

    Changed to PHP license

--- ext/bcmath/libbcmath/src/round.c
+++ ext/bcmath/libbcmath/src/round.c
@@ -1,33 +1,18 @@
-/* round.c: bcmath library file. */
 /*
-    Copyright (C) 1991, 1992, 1993, 1994, 1997 Free Software Foundation, Inc.
-    Copyright (C) 2000 Philip A. Nelson
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.  (LICENSE)
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to:
-
-      The Free Software Foundation, Inc.
-      59 Temple Place, Suite 330
-      Boston, MA 02111-1307 USA.
-
-    You may contact the author by:
-       e-mail:  philnelson@acm.org
-      us-mail:  Philip A. Nelson
-                Computer Science Department, 9062
-                Western Washington University
-                Bellingham, WA 98226-9062
-
-*************************************************************************/
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Saki Takamachi <saki@php.net>                               |
+   +----------------------------------------------------------------------+
+*/
 
 #include "bcmath.h"
 #include "private.h"
