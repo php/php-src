114422f0330f0d04aad2c723b8632029ea268396
Jorg Sowa <jorg.sowa@gmail.com>
2024-03-14 22:06:47 +0100

    Updated auto-generated date header from stub

--- ext/date/php_date_arginfo.h
+++ ext/date/php_date_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 199789cfbbb53ead5c5418ae30935ffdd7c9685f */
+ * Stub hash: 41560e99d4ea9b1f160c13345727f6d193af5133 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_strtotime, 0, 1, MAY_BE_LONG|MAY_BE_FALSE)
 	ZEND_ARG_TYPE_INFO(0, datetime, IS_STRING, 0)
