php/php-src

commit 9a250cc9d640865200c380b7881823716e062404

Add separate static property through trait if parent already declares it
Fixes GH-10935
Closes GH-10937

ext/opcache/zend_persist.c
@@ -1082,7 +1082,15 @@ void zend_update_parent_ce(zend_class_entry *ce)
 				end = parent->parent ? parent->parent->default_static_members_count : 0;
 				for (; i >= end; i--) {
 					zval *p = &ce->default_static_members_table[i];
-					ZVAL_INDIRECT(p, &parent->default_static_members_table[i]);
+					/* The static property may have been overridden by a trait
+					 * during inheritance. In that case, the property default
+					 * value is replaced by zend_declare_typed_property() at the
+					 * property index of the parent property. Make sure we only
+					 * point to the parent property value if the child value was
+					 * already indirect. */
+					if (Z_TYPE_P(p) == IS_INDIRECT) {
+						ZVAL_INDIRECT(p, &parent->default_static_members_table[i]);
+					}
 				}
 
 				parent = parent->parent;

UPGRADING
@@ -35,6 +35,10 @@ PHP 8.3 UPGRADE NOTES
     proc_get_status() to check whether the result was cached.
   . Zend Max Execution Timers is now enabled by default for ZTS builds on
     Linux.
+  . Uses of traits with static properties will now redeclare static properties
+    inherited from the parent class. This will create a separate static property
+    storage for the current class. This is analogous to adding the static
+    property to the class directly without traits.
 
 - FFI:
   . C functions that have a return type of void now return null instead of

Zend/tests/gh10935.phpt
@@ -1,0 +1,83 @@
+--TEST--
+GH-1093: Add separate static property through trait if parent already declares it
+--FILE--
+<?php
+trait Foo {
+    static $test;
+
+    public static function getFooSelf() {
+        return self::$test;
+    }
+
+    public static function getFooStatic() {
+        return static::$test;
+    }
+}
+trait Bar {
+    public static function getBarSelf() {
+        return self::$test;
+    }
+
+    public static function getBarStatic() {
+        return static::$test;
+    }
+}
+
+class A {
+    use Foo;
+    use Bar;
+
+    public static function getASelf() {
+        return self::$test;
+    }
+
+    public static function getAStatic() {
+        return static::$test;
+    }
+}
+
+class B extends A {
+    use Foo;
+
+    public static function getBSelf() {
+        return self::$test;
+    }
+
+    public static function getBStatic() {
+        return static::$test;
+    }
+}
+
+A::$test = 'A';
+B::$test = 'B';
+
+echo 'A::$test: ' . A::$test . "\n";
+echo 'A::getASelf(): ' . A::getASelf() . "\n";
+echo 'A::getAStatic(): ' . A::getAStatic() . "\n";
+echo 'A::getFooSelf(): ' . A::getFooSelf() . "\n";
+echo 'A::getFooStatic(): ' . A::getFooStatic() . "\n";
+echo 'B::$test: ' . B::$test . "\n";
+echo 'B::getASelf(): ' . B::getASelf() . "\n";
+echo 'B::getAStatic(): ' . B::getAStatic() . "\n";
+echo 'B::getBSelf(): ' . B::getBSelf() . "\n";
+echo 'B::getBStatic(): ' . B::getBStatic() . "\n";
+echo 'B::getFooSelf(): ' . B::getFooSelf() . "\n";
+echo 'B::getFooStatic(): ' . B::getFooStatic() . "\n";
+echo 'B::getBarSelf(): ' . B::getBarSelf() . "\n";
+echo 'B::getBarStatic(): ' . B::getBarStatic() . "\n";
+?>
+--EXPECT--
+A::$test: A
+A::getASelf(): A
+A::getAStatic(): A
+A::getFooSelf(): A
+A::getFooStatic(): A
+B::$test: B
+B::getASelf(): A
+B::getAStatic(): B
+B::getBSelf(): B
+B::getBStatic(): B
+B::getFooSelf(): B
+B::getFooStatic(): B
+B::getBarSelf(): A
+B::getBarStatic(): B

Zend/zend_inheritance.c
@@ -2377,7 +2377,9 @@ static void zend_do_traits_property_binding(zend_class_entry *ce, zend_class_ent
 								ZSTR_VAL(prop_name),
 								ZSTR_VAL(ce->name));
 					}
-					continue;
+					if (!(flags & ZEND_ACC_STATIC)) {
+						continue;
+					}
 				}
 			}
 

commit 5de11d1e34f1150468d176168f17453ecc998aba

Fix GH-13433: Segmentation Fault in zend_class_init_statics when using opcache.preload
This regressed in 9a250cc9d6, which allowed static properties to get
overridden by a trait during inheritance. In particular, because of the
change to the loop in zend_update_parent_ce(), it's not guaranteed that
all indirects are after one another.

This means that during persisting the zvals of the static members table,
some static properties may be skipped. In case of the test code, this
means that the array in the trait will keep referring to the old, new
freed, stale value. To solve this, we check the type for IS_INDIRECT,
which is the same as what zend_persist_calc() is already doing anyway.

Since 2543e61aed we can check for IS_INDIRECT to see if it should be
persisted or not.

ext/opcache/zend_persist.c
@@ -903,10 +903,11 @@ zend_class_entry *zend_persist_class_entry(zend_class_entry *orig_ce)
 			ce->default_static_members_table = zend_shared_memdup_free(ce->default_static_members_table, sizeof(zval) * ce->default_static_members_count);
 
 			/* Persist only static properties in this class.
-			 * Static properties from parent classes will be handled in class_copy_ctor */
-			i = (ce->parent && (ce->ce_flags & ZEND_ACC_LINKED)) ? ce->parent->default_static_members_count : 0;
-			for (; i < ce->default_static_members_count; i++) {
-				zend_persist_zval(&ce->default_static_members_table[i]);
+			 * Static properties from parent classes will be handled in class_copy_ctor and are marked with IS_INDIRECT */
+			for (i = 0; i < ce->default_static_members_count; i++) {
+				if (Z_TYPE(ce->default_static_members_table[i]) != IS_INDIRECT) {
+					zend_persist_zval(&ce->default_static_members_table[i]);
+				}
 			}
 			if (ce->ce_flags & ZEND_ACC_IMMUTABLE) {
 				if (ce->ce_flags & ZEND_ACC_LINKED) {

ext/opcache/tests/gh13433/ParentClass.inc
@@ -1,0 +1,4 @@
+<?php
+abstract class ParentClass {
+	use TraitA;
+}

ext/opcache/tests/gh13433/TheClass.inc
@@ -1,0 +1,8 @@
+<?php
+class TheClass extends ParentClass {
+	use TraitA;
+
+	public function test() {
+		return self::$non_empty_array;
+	}
+}

ext/opcache/tests/gh13433/TraitA.inc
@@ -1,0 +1,4 @@
+<?php
+trait TraitA {
+    protected static $non_empty_array = ['non-empty'];
+}

ext/opcache/tests/gh13433/gh13433.phpt
@@ -1,0 +1,32 @@
+--TEST--
+GH-13433 (Segmentation Fault in zend_class_init_statics when using opcache.preload)
+--INI--
+opcache.enable=1
+opcache.enable_cli=1
+opcache.optimization_level=0
+opcache.preload={PWD}/preload.inc
+--EXTENSIONS--
+opcache
+--SKIPIF--
+<?php
+if (PHP_OS_FAMILY == 'Windows') die('skip Preloading is not supported on Windows');
+?>
+--FILE--
+<?php
+require __DIR__.'/TraitA.inc';
+require __DIR__.'/ParentClass.inc';
+require __DIR__.'/TheClass.inc';
+
+$instance = new TheClass;
+var_dump($instance);
+var_dump($instance->test());
+?>
+OK
+--EXPECT--
+object(TheClass)#1 (0) {
+}
+array(1) {
+  [0]=>
+  string(9) "non-empty"
+}
+OK

ext/opcache/tests/gh13433/preload.inc
@@ -1,0 +1,4 @@
+<?php
+require __DIR__.'/TraitA.inc';
+require __DIR__.'/ParentClass.inc';
+require __DIR__.'/TheClass.inc';