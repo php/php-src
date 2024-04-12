e7e216092f603a942ac53c0460418b58b343d332
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-04-12 13:32:27 +0200

    Only recover the compiler on compilation or parse error

--- main/main.c
+++ main/main.c
@@ -1405,7 +1405,7 @@ static ZEND_COLD void php_error_cb(int orig_type, zend_string *error_filename, c
 					/* restore memory limit */
 					zend_set_memory_limit(PG(memory_limit));
 					zend_objects_store_mark_destructed(&EG(objects_store));
-					if (CG(in_compilation)) {
+					if (CG(in_compilation) && (type == E_COMPILE_ERROR || type == E_PARSE)) {
 						/* We bailout during compilation which may for example leave stale entries in CG(loop_var_stack).
 						* If code is compiled during shutdown, we need to make sure the compiler is reset to a clean state,
 						* otherwise this will lead to incorrect compilation during shutdown.
