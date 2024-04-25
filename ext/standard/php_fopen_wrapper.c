35fd9f297eda05c57756a715ddc41ba273c9eef3
richardtmiles <richard@miles.systems>
2024-04-25 08:04:01 -0600

    Adding PHP_FUNCTION apache_connection_stream to expose ap_get_conn_socket
    Apache CGI doesn't support WebSocket Upgrade requests out-of-the-box. php://input uses apache brigade/buckets but these are not fired with websocket Input; moreover, the php://input stream is cached and not seekable. php://stdio is the php code buffer so it is not useful here either. This allow a direct connection to the client.

--- ext/standard/php_fopen_wrapper.c
+++ ext/standard/php_fopen_wrapper.c
@@ -243,7 +243,8 @@ static php_stream * php_stream_url_wrap_php(php_stream_wrapper *wrapper, const c
 	}
 
 	if (!strcasecmp(path, "input")) {
-		php_stream_input_t *input;
+
+        php_stream_input_t *input;
 
 		if ((options & STREAM_OPEN_FOR_INCLUDE) && !PG(allow_url_include) ) {
 			php_stream_wrapper_warn(wrapper, context, options,
@@ -253,14 +254,21 @@ static php_stream * php_stream_url_wrap_php(php_stream_wrapper *wrapper, const c
 		}
 
 		input = ecalloc(1, sizeof(*input));
+
 		if ((input->body = SG(request_info).request_body)) {
+
 			php_stream_rewind(input->body);
-		} else {
+
+        } else {
+
 			input->body = php_stream_temp_create_ex(TEMP_STREAM_DEFAULT, SAPI_POST_BLOCK_SIZE, PG(upload_tmp_dir));
+
 			SG(request_info).request_body = input->body;
-		}
+
+        }
 
 		return php_stream_alloc(&php_stream_input_ops, input, 0, "rb");
+
 	}
 
 	if (!strcasecmp(path, "stdin")) {
@@ -289,7 +297,7 @@ static php_stream * php_stream_url_wrap_php(php_stream_wrapper *wrapper, const c
 		if (!strcmp(sapi_module.name, "cli")) {
 			static int cli_out = 0;
 			fd = STDOUT_FILENO;
-			if (cli_out++) {
+			if (cli_out) {
 				fd = dup(fd);
 			} else {
 				cli_out = 1;
@@ -305,7 +313,7 @@ static php_stream * php_stream_url_wrap_php(php_stream_wrapper *wrapper, const c
 		if (!strcmp(sapi_module.name, "cli")) {
 			static int cli_err = 0;
 			fd = STDERR_FILENO;
-			if (cli_err++) {
+			if (cli_err) {
 				fd = dup(fd);
 			} else {
 				cli_err = 1;
