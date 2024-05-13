1267d9ab8274e2bd7cc28f9cf62e745337465e81
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-05-13 22:32:03 +0200

    Directly set the binary flag in open

--- ext/ffi/ffi.c
+++ ext/ffi/ffi.c
@@ -3255,10 +3255,11 @@ static zend_ffi *zend_ffi_load(const char *filename, bool preload) /* {{{ */
 
 	code_size = buf.st_size;
 	code = emalloc(code_size + 1);
-	fd = open(filename, O_RDONLY, 0);
+	int open_flags = O_RDONLY;
 #ifdef PHP_WIN32
-	_setmode(fd, _O_BINARY);
+	open_flags |= _O_BINARY;
 #endif
+	fd = open(filename, open_flags, 0);
 	if (fd < 0 || read(fd, code, code_size) != code_size) {
 		if (preload) {
 			zend_error(E_WARNING, "FFI: Failed pre-loading '%s', cannot read_file", filename);
