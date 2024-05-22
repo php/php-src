0d05928dfaa2057b2d0b87f60c21cfae3dead22e
Arnaud Le Blanc <arnaud.lb@gmail.com>
2024-05-22 16:11:43 +0200

    Remote heap spraying / feng chui protection
    Isolate request environment / input in separate chunks to makes it more
    difficult to remotely control the layout of the heap.

--- Zend/zend_compile.c
+++ Zend/zend_compile.c
@@ -1916,7 +1916,9 @@ ZEND_API bool zend_is_auto_global_str(const char *name, size_t len) /* {{{ */ {
 
 	if ((auto_global = zend_hash_str_find_ptr(CG(auto_globals), name, len)) != NULL) {
 		if (auto_global->armed) {
+			zend_mm_input_begin();
 			auto_global->armed = auto_global->auto_global_callback(auto_global->name);
+			zend_mm_input_end();
 		}
 		return 1;
 	}
@@ -1930,7 +1932,9 @@ ZEND_API bool zend_is_auto_global(zend_string *name) /* {{{ */
 
 	if ((auto_global = zend_hash_find_ptr(CG(auto_globals), name)) != NULL) {
 		if (auto_global->armed) {
+			zend_mm_input_begin();
 			auto_global->armed = auto_global->auto_global_callback(auto_global->name);
+			zend_mm_input_end();
 		}
 		return 1;
 	}
@@ -1956,6 +1960,8 @@ ZEND_API zend_result zend_register_auto_global(zend_string *name, bool jit, zend
 ZEND_API void zend_activate_auto_globals(void) /* {{{ */
 {
 	zend_auto_global *auto_global;
+
+	ZEND_ASSERT(zend_mm_check_in_input());
 
 	ZEND_HASH_MAP_FOREACH_PTR(CG(auto_globals), auto_global) {
 		if (auto_global->jit) {
@@ -7714,7 +7720,7 @@ static zend_string *zend_begin_func_decl(znode *result, zend_op_array *op_array,
 		zend_string *separator = zend_empty_string;
 		zend_string *function = filename;
 		char *parens = "";
-		
+
 		if (CG(active_op_array) && CG(active_op_array)->function_name) {
 			if (CG(active_op_array)->fn_flags & ZEND_ACC_CLOSURE) {
 				/* If the parent function is a closure, don't redundantly
--- main/main.c
+++ main/main.c
@@ -1748,6 +1748,8 @@ zend_result php_request_startup(void)
 {
 	zend_result retval = SUCCESS;
 
+	ZEND_ASSERT(zend_mm_check_in_input());
+
 	zend_interned_strings_activate();
 
 #ifdef HAVE_DTRACE
@@ -1822,6 +1824,8 @@ zend_result php_request_startup(void)
 	} zend_end_try();
 
 	SG(sapi_started) = 1;
+
+	zend_mm_input_end();
 
 	return retval;
 }
