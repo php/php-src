0e16bd9b2f21046f184556f0638a8d791ee4dfe1
Saki Takamachi <saki@sakiot.com>
2024-06-04 14:06:44 +0900

    Fixed a problem where usec was carry up
    If round to the fractional part of a timestamp, a carry will occur in cases
    such as 999 999 9. In that case, set usec to 0 and add 1 to sec.

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -2540,6 +2540,11 @@ PHPAPI bool php_date_initialize_from_ts_double(php_date_obj *dateobj, double ts)
 	sec = (zend_long)sec_dval;
 	usec = (int) round(fmod(ts, 1) * 1000000);
 
+	if (UNEXPECTED(usec == 1000000)) {
+		sec++;
+		usec = 0;
+	}
+
 	if (UNEXPECTED(usec < 0)) {
 		if (UNEXPECTED(sec == TIMELIB_LONG_MIN)) {
 			zend_argument_error(
