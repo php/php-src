49fb8eac4f88e7ebebb7da7b6428aa8e5eaf070b
Gina Peter Banyard <girgias@php.net>
2024-06-04 18:02:16 +0100

    ext/bzip: Fix [-Wsign-compare] warning

--- ext/bz2/bz2.c
+++ ext/bz2/bz2.c
@@ -96,7 +96,7 @@ static ssize_t php_bz2iop_read(php_stream *stream, char *buf, size_t count)
 
 static ssize_t php_bz2iop_write(php_stream *stream, const char *buf, size_t count)
 {
-	ssize_t wrote = 0;
+	size_t wrote = 0;
 	struct php_bz2_stream_data_t *self = (struct php_bz2_stream_data_t *)stream->abstract;
 
 	do {
