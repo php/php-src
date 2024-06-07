dd20ff98cf62a9f88815a07b1c310316a7c5801e
Dmitry Stogov <dmitry@zend.com>
2024-06-07 12:45:00 +0300

    JIT: Fix FFI pointer access through FETCH_DIM, ASSIGN_DIM

--- ext/opcache/jit/zend_jit_ir.c
+++ ext/opcache/jit/zend_jit_ir.c
@@ -12802,6 +12802,11 @@ static int zend_jit_ffi_fetch_dim_read(zend_jit_ctx       *jit,
 
 	ir_ref obj_ref = jit_Z_PTR(jit, op1_addr);
 	ir_ref cdata_ref = ir_LOAD_A(ir_ADD_OFFSET(obj_ref, offsetof(zend_ffi_cdata, ptr)));
+
+	if (op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER) {
+		cdata_ref = ir_LOAD_A(cdata_ref);
+	}
+
 	ir_ref ptr = ir_ADD_A(cdata_ref, ir_MUL_L(jit_Z_LVAL(jit, op2_addr), ir_CONST_LONG(el_type->size)));
 
 	switch (el_type->kind) {
@@ -13545,6 +13550,11 @@ static int zend_jit_ffi_assign_dim(zend_jit_ctx  *jit,
 
 	ir_ref obj_ref = jit_Z_PTR(jit, op1_addr);
 	ir_ref cdata_ref = ir_LOAD_A(ir_ADD_OFFSET(obj_ref, offsetof(zend_ffi_cdata, ptr)));
+
+	if (op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER) {
+		cdata_ref = ir_LOAD_A(cdata_ref);
+	}
+
 	ir_ref ptr = ir_ADD_A(cdata_ref, ir_MUL_L(jit_Z_LVAL(jit, op2_addr), ir_CONST_LONG(el_type->size)));
 
 	ZEND_ASSERT(!res_addr);
@@ -14012,6 +14022,11 @@ static int zend_jit_ffi_assign_dim_op(zend_jit_ctx   *jit,
 
 	ir_ref obj_ref = jit_Z_PTR(jit, op1_addr);
 	ir_ref cdata_ref = ir_LOAD_A(ir_ADD_OFFSET(obj_ref, offsetof(zend_ffi_cdata, ptr)));
+
+	if (op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER) {
+		cdata_ref = ir_LOAD_A(cdata_ref);
+	}
+
 	ir_ref ptr = ir_ADD_A(cdata_ref, ir_MUL_L(jit_Z_LVAL(jit, op2_addr), ir_CONST_LONG(el_type->size)));
 
 	if (!zend_jit_ffi_assign_op_helper(jit, opline, opline->extended_value,
