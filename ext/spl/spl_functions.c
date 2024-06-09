809410423f11f03c061b746c2a0e1937bb5b9fce
Gina Peter Banyard <girgias@php.net>
2024-06-09 04:18:46 +0100

    ext/spl: Refactor debug handlers
    Mainly to use zend_mangle_property_name() directly instead of spl_gen_private_prop_name()

--- ext/spl/spl_functions.c
+++ ext/spl/spl_functions.c
@@ -77,8 +77,21 @@ void spl_add_classes(zend_class_entry *pce, zval *list, bool sub, int allow, int
 }
 /* }}} */
 
-zend_string * spl_gen_private_prop_name(zend_class_entry *ce, char *prop_name, size_t prop_len) /* {{{ */
+void spl_set_private_debug_info_property(
+	const zend_class_entry *ce,
+	const char *property,
+	size_t property_len,
+	HashTable *debug_info,
+	zval *value
+)
 {
-	return zend_mangle_property_name(ZSTR_VAL(ce->name), ZSTR_LEN(ce->name), prop_name, prop_len, 0);
+	zend_string *mangled_named = zend_mangle_property_name(
+		ZSTR_VAL(ce->name),
+		ZSTR_LEN(ce->name),
+		property,
+		property_len,
+		/* persistent */ false
+	);
+	zend_hash_update(debug_info, mangled_named, value);
+	zend_string_release_ex(mangled_named, /* persistent */ false);
 }
-/* }}} */
