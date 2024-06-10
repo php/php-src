3d5ae78e50bbc7ca358c7cd3e210019afb7bdae7
Dmitry Stogov <dmitry@zend.com>
2024-06-10 13:11:56 +0300

    Support for 32-bit build

--- ext/opcache/jit/zend_jit_ir.c
+++ ext/opcache/jit/zend_jit_ir.c
@@ -12539,6 +12539,7 @@ static int zend_jit_ffi_fetch_dim_read(zend_jit_ctx       *jit,
 			jit_set_Z_LVAL(jit, res_addr, ir_SEXT_L(ir_LOAD_I16(ptr)));
 			res_type = IS_LONG;
 			break;
+#ifdef ZEND_ENABLE_ZVAL_LONG64
 		case ZEND_FFI_TYPE_UINT32:
 			jit_set_Z_LVAL(jit, res_addr, ir_ZEXT_L(ir_LOAD_U32(ptr)));
 			res_type = IS_LONG;
@@ -12555,6 +12556,16 @@ static int zend_jit_ffi_fetch_dim_read(zend_jit_ctx       *jit,
 			jit_set_Z_LVAL(jit, res_addr, ir_LOAD_I32(ptr));
 			res_type = IS_LONG;
 			break;
+#else
+		case ZEND_FFI_TYPE_UINT32:
+			jit_set_Z_LVAL(jit, res_addr, ir_LOAD_U32(ptr));
+			res_type = IS_LONG;
+			break;
+		case ZEND_FFI_TYPE_SINT32:
+			jit_set_Z_LVAL(jit, res_addr, ir_LOAD_I32(ptr));
+			res_type = IS_LONG;
+			break;
+#endif
 		default:
 			ZEND_UNREACHABLE();
 	}
@@ -13312,6 +13323,7 @@ static int zend_jit_ffi_assign_dim(zend_jit_ctx  *jit,
 				ZEND_UNREACHABLE();
 			}
 			break;
+#ifdef ZEND_ENABLE_ZVAL_LONG64
 		case ZEND_FFI_TYPE_UINT32:
 			if (val_info == MAY_BE_LONG) {
 				ir_STORE(ptr, ir_TRUNC_U32(jit_Z_LVAL(jit, op3_addr)));
@@ -13334,6 +13346,16 @@ static int zend_jit_ffi_assign_dim(zend_jit_ctx  *jit,
 				ZEND_UNREACHABLE();
 			}
 			break;
+#else
+		case ZEND_FFI_TYPE_UINT32:
+		case ZEND_FFI_TYPE_SINT32:
+			if (val_info == MAY_BE_LONG) {
+				ir_STORE(ptr, jit_Z_LVAL(jit, op3_addr));
+			} else {
+				ZEND_UNREACHABLE();
+			}
+			break;
+#endif
 		default:
 			ZEND_UNREACHABLE();
 	}
@@ -13662,6 +13684,7 @@ static int zend_jit_ffi_assign_op_helper(zend_jit_ctx   *jit,
 				return 0;
 			}
 			break;
+#ifdef ZEND_ENABLE_ZVAL_LONG64
 		case ZEND_FFI_TYPE_UINT32:
 			type = IR_U32;
 			if (op2_info == MAY_BE_LONG) {
@@ -13690,6 +13713,18 @@ static int zend_jit_ffi_assign_op_helper(zend_jit_ctx   *jit,
 				return 0;
 			}
 			break;
+#else
+		case ZEND_FFI_TYPE_UINT32:
+		case ZEND_FFI_TYPE_SINT32:
+			type = IR_I32;
+			if (op2_info == MAY_BE_LONG) {
+				op2 = jit_Z_LVAL(jit, op2_addr);
+			} else {
+				ZEND_UNREACHABLE();
+				return 0;
+			}
+			break;
+#endif
 		default:
 			ZEND_UNREACHABLE();
 			return 0;
