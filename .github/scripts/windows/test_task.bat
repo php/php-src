d8b50b57b29cf0e04cb2c260c80c94d7713c6d59
Ilija Tovilo <ilija.tovilo@me.com>
2024-06-13 11:42:21 +0200

    Remove GitHub actions junit artifacts
    Nobody looks at those, and nightly regularly fails due to uploading them.

--- .github/scripts/windows/test_task.bat
+++ .github/scripts/windows/test_task.bat
@@ -133,8 +133,6 @@ set TEST_PHPDBG_EXECUTABLE=%PHP_BUILD_DIR%\phpdbg.exe
 
 mkdir c:\tests_tmp
 
-set TEST_PHP_JUNIT=c:\junit.out.xml
-
 nmake test TESTS="%OPCACHE_OPTS% -g FAIL,BORK,LEAK,XLEAK --no-progress -q --offline --show-diff --show-slow 1000 --set-timeout 120 --temp-source c:\tests_tmp --temp-target c:\tests_tmp --bless %PARALLEL%"
 
 set EXIT_CODE=%errorlevel%
