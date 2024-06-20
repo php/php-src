f95646287390726c5d71bc809ce761ecd4f8d531
Dmitry Stogov <dmitry@zend.com>
2024-06-20 12:18:33 +0300

    Prevent JIT code generation for 64-bit FFI data in 32-bit build

--- ext/opcache/jit/zend_jit_trace.c
+++ ext/opcache/jit/zend_jit_trace.c
@@ -4697,6 +4697,10 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 						 && (op1_ffi_type->kind == ZEND_FFI_TYPE_ARRAY || op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER)
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind >= ZEND_FFI_TYPE_FLOAT
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_UINT64
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
 						 && op2_info == MAY_BE_LONG) {
 							if (!ffi_info) {
 								ffi_info = zend_arena_calloc(&CG(arena), ssa->vars_count, sizeof(zend_jit_ffi_info));
@@ -4903,6 +4907,10 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 							 && !field->bits
 							 && ZEND_FFI_TYPE(field->type)->kind >= ZEND_FFI_TYPE_FLOAT
 							 && ZEND_FFI_TYPE(field->type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_UINT64
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
 							 && (op1_data_info == MAY_BE_LONG || op1_data_info == MAY_BE_DOUBLE)) {
 								if (!ffi_info) {
 									ffi_info = zend_arena_calloc(&CG(arena), ssa->vars_count, sizeof(zend_jit_ffi_info));
@@ -5009,6 +5017,10 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 							 && !field->bits
 							 && ZEND_FFI_TYPE(field->type)->kind >= ZEND_FFI_TYPE_FLOAT
 							 && ZEND_FFI_TYPE(field->type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_UINT64
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
 							 && (op1_data_info == MAY_BE_LONG || op1_data_info == MAY_BE_DOUBLE)) {
 								if (!ffi_info) {
 									ffi_info = zend_arena_calloc(&CG(arena), ssa->vars_count, sizeof(zend_jit_ffi_info));
@@ -5086,6 +5098,10 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 						 && (op1_ffi_type->kind == ZEND_FFI_TYPE_ARRAY || op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER)
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind >= ZEND_FFI_TYPE_FLOAT
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_UINT64
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
 						 && op2_info == MAY_BE_LONG
 						 && (op1_data_info == MAY_BE_LONG || op1_data_info == MAY_BE_DOUBLE)) {
 							if (!ffi_info) {
@@ -5893,6 +5909,10 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 						 && (op1_ffi_type->kind == ZEND_FFI_TYPE_ARRAY || op1_ffi_type->kind == ZEND_FFI_TYPE_POINTER)
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind >= ZEND_FFI_TYPE_FLOAT
 						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_UINT64
+						 && ZEND_FFI_TYPE(op1_ffi_type->array.type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
 						 && op2_info == MAY_BE_LONG) {
 							if (!ffi_info) {
 								ffi_info = zend_arena_calloc(&CG(arena), ssa->vars_count, sizeof(zend_jit_ffi_info));
@@ -6148,7 +6168,12 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 							if (field
 							 && !field->bits
 							 && ZEND_FFI_TYPE(field->type)->kind >= ZEND_FFI_TYPE_FLOAT
-							 && ZEND_FFI_TYPE(field->type)->kind <= ZEND_FFI_TYPE_ENUM) {
+							 && ZEND_FFI_TYPE(field->type)->kind <= ZEND_FFI_TYPE_ENUM
+#if defined(IR_TARGET_X86)
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_UINT64
+							 && ZEND_FFI_TYPE(field->type)->kind != ZEND_FFI_TYPE_SINT64
+#endif
+							) {
 								if (!ffi_info) {
 									ffi_info = zend_arena_calloc(&CG(arena), ssa->vars_count, sizeof(zend_jit_ffi_info));
 								}
