f86f95abcf78706cdd8aff88d564f385499bad64
jvoisin <julien.voisin@dustri.org>
2024-06-24 15:57:01 +0200

    Compute the size of pages before allocating memory
    `start_memory_manager()` calls `zend_mm_init()` via `alloc_globals_ctor()`
    before setting `REAL_PAGE_SIZE` to the right value. Moving the `REAL_PAGE_SIZE`
    setting block before the call to `alloc_globals_ctor()` makes the allocator
    behave properly on systems with a page size different than 4k.
    
    Suggested-by: arnaud-lb

--- Zend/zend_alloc.c
+++ Zend/zend_alloc.c
@@ -3109,17 +3109,17 @@ static void alloc_globals_dtor(zend_alloc_globals *alloc_globals)
 
 ZEND_API void start_memory_manager(void)
 {
-#ifdef ZTS
-	ts_allocate_fast_id(&alloc_globals_id, &alloc_globals_offset, sizeof(zend_alloc_globals), (ts_allocate_ctor) alloc_globals_ctor, (ts_allocate_dtor) alloc_globals_dtor);
-#else
-	alloc_globals_ctor(&alloc_globals);
-#endif
 #ifndef _WIN32
 #  if defined(_SC_PAGESIZE)
 	REAL_PAGE_SIZE = sysconf(_SC_PAGESIZE);
 #  elif defined(_SC_PAGE_SIZE)
 	REAL_PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
 #  endif
+#endif
+#ifdef ZTS
+	ts_allocate_fast_id(&alloc_globals_id, &alloc_globals_offset, sizeof(zend_alloc_globals), (ts_allocate_ctor) alloc_globals_ctor, (ts_allocate_dtor) alloc_globals_dtor);
+#else
+	alloc_globals_ctor(&alloc_globals);
 #endif
 }
 
