83e6e8e2b642b04e559a902c07527af6c613594c
Dmitry Stogov <dmitry@zend.com>
2024-06-24 12:02:17 +0300

    Disable modification of "const" fields

--- ext/opcache/jit/zend_jit_trace.c
+++ ext/opcache/jit/zend_jit_trace.c
@@ -4997,6 +4997,7 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 								Z_STR_P(RT_CONSTANT(opline, opline->op2)));
 
 							if (field
+							 && !field->is_const
 							 && !field->bits
 							 && zend_jit_ffi_compatible(field->type, op1_data_info, op3_ffi_type)) {
 								if (!ffi_info) {
@@ -5101,6 +5102,7 @@ static const void *zend_jit_trace(zend_jit_trace_rec *trace_buffer, uint32_t par
 								Z_STR_P(RT_CONSTANT(opline, opline->op2)));
 
 							if (field
+							 && !field->is_const
 							 && !field->bits
 							 && zend_jit_ffi_compatible(field->type, op1_data_info, op3_ffi_type)) {
 								if (!ffi_info) {
