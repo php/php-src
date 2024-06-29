48690af9b43132ba6449741b09c0e31625fa428f
Jorg Sowa <jorg.sowa@gmail.com>
2024-06-29 23:09:05 +0200

    Get len of string directly from zend_string

--- ext/session/session.c
+++ ext/session/session.c
@@ -376,11 +376,9 @@ PHPAPI zend_result php_session_valid_key(const zend_string *str) /* {{{ */
 		}
 	}
 
-	len = p - key;
-
 	/* Somewhat arbitrary length limit here, but should be way more than
 	   anyone needs and avoids file-level warnings later on if we exceed MAX_PATH */
-	if (len > PS_MAX_SID_LENGTH) {
+	if (str->len > PS_MAX_SID_LENGTH) {
 		return FAILURE;
 	}
 
