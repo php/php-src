9f1d2060e0fecb6011347e86b1dcc05112f1594e
Jorg Sowa <jorg.sowa@gmail.com>
2024-06-30 14:26:04 +0200

    Added assertion on key length in mod_files.c

--- ext/session/mod_files.c
+++ ext/session/mod_files.c
@@ -165,6 +165,8 @@ static void ps_files_open(ps_files *data, /* const */ zend_string *key)
 
 		ps_files_close(data);
 
+		ZEND_ASSERT(ZSTR_LEN(key) > 0);
+
 		if (php_session_valid_key(key) == FAILURE) {
 			php_error_docref(NULL, E_WARNING, "Session ID is too long or contains illegal characters. Only the A-Z, a-z, 0-9, \"-\", and \",\" characters are allowed");
 			return;
