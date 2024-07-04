php/php-src

commit 6318040df2b6c6f4824b99fd27369179d29ee93b

remove specialized printing from phpdbg (#7156)

sapi/phpdbg/phpdbg.c
@@ -873,7 +873,7 @@ static ssize_t phpdbg_stdiop_write(php_stream *stream, const char *buf, size_t c
 			return count;
 		}
 		if (stat[2].st_dev == stat[1].st_dev && stat[2].st_ino == stat[1].st_ino) {
-			phpdbg_script_ex(PHPDBG_G(io)[PHPDBG_STDERR].fd, P_STDERR, "%.*s", (int) count, buf);
+			phpdbg_script(P_STDERR, "%.*s", (int) count, buf);
 			return count;
 		}
 		break;
@@ -881,13 +881,6 @@ static ssize_t phpdbg_stdiop_write(php_stream *stream, const char *buf, size_t c
 
 	return PHPDBG_G(php_stdiop_write)(stream, buf, count);
 }
-
-static inline void php_sapi_phpdbg_flush(void *context)  /* {{{ */
-{
-	if (!phpdbg_active_sigsafe_mem()) {
-		fflush(PHPDBG_G(io)[PHPDBG_STDOUT].ptr);
-	}
-} /* }}} */
 
 /* copied from sapi/cli/php_cli.c cli_register_file_handles */
 void phpdbg_register_file_handles(void) /* {{{ */
@@ -950,7 +943,7 @@ static sapi_module_struct phpdbg_sapi_module = {
 	php_sapi_phpdbg_deactivate,     /* deactivate */
 
 	php_sapi_phpdbg_ub_write,       /* unbuffered write */
-	php_sapi_phpdbg_flush,          /* flush */
+	NULL,                           /* flush */
 	NULL,                           /* get uid */
 	NULL,                           /* getenv */
 
@@ -1425,7 +1418,6 @@ phpdbg_main:
 		if (show_version || show_help) {
 			/* It ain't gonna proceed to real execution anyway,
 				but the correct descriptor is needed already. */
-			PHPDBG_G(io)[PHPDBG_STDOUT].ptr = stdout;
 			PHPDBG_G(io)[PHPDBG_STDOUT].fd = fileno(stdout);
 			if (show_help) {
 				phpdbg_do_help_cmd(exec);
@@ -1532,11 +1524,9 @@ phpdbg_main:
 #endif
         zend_try { zend_signal(SIGINT, phpdbg_sigint_handler); } zend_end_try();
 
-		PHPDBG_G(io)[PHPDBG_STDIN].ptr = stdin;
+
 		PHPDBG_G(io)[PHPDBG_STDIN].fd = fileno(stdin);
-		PHPDBG_G(io)[PHPDBG_STDOUT].ptr = stdout;
 		PHPDBG_G(io)[PHPDBG_STDOUT].fd = fileno(stdout);
-		PHPDBG_G(io)[PHPDBG_STDERR].ptr = stderr;
 		PHPDBG_G(io)[PHPDBG_STDERR].fd = fileno(stderr);
 
 #ifndef _WIN32
@@ -1690,7 +1680,7 @@ phpdbg_main:
 					PHPDBG_G(flags) &= ~PHPDBG_DISCARD_OUTPUT;
 					if (bp_tmp_str) {
 						bp_tmp = strdup(bp_tmp_str);
-						efree(bp_tmp_str);
+						free(bp_tmp_str);
 					}
 					cleaning = 1;
 				} else {

sapi/phpdbg/phpdbg_out.h
@@ -32,38 +32,25 @@ enum {
 	P_LOG
 };
 
-/* phpdbg uses lots of custom format specifiers, so we disable format checks by default. */
-#if defined(PHPDBG_CHECK_FORMAT_STRINGS)
-# define PHPDBG_ATTRIBUTE_FORMAT(type, idx, first) PHP_ATTRIBUTE_FORMAT(type, idx, first)
-#else
-# define PHPDBG_ATTRIBUTE_FORMAT(type, idx, first)
-#endif
-
-PHPDBG_API int phpdbg_print(int severity, int fd, const char *strfmt, ...) PHPDBG_ATTRIBUTE_FORMAT(printf, 3, 4);
-PHPDBG_API int phpdbg_log_internal(int fd, const char *fmt, ...) PHPDBG_ATTRIBUTE_FORMAT(printf, 2, 3);
-PHPDBG_API int phpdbg_out_internal(int fd, const char *fmt, ...) PHPDBG_ATTRIBUTE_FORMAT(printf, 2, 3);
+PHPDBG_API int phpdbg_print(int severity, int fd, const char *strfmt, ...) PHP_ATTRIBUTE_FORMAT(printf, 3, 4);
+PHPDBG_API int phpdbg_log_internal(int fd, const char *fmt, ...) PHP_ATTRIBUTE_FORMAT(printf, 2, 3);
+PHPDBG_API int phpdbg_out_internal(int fd, const char *fmt, ...) PHP_ATTRIBUTE_FORMAT(printf, 2, 3);
 
 #define phpdbg_error(strfmt, ...)              phpdbg_print(P_ERROR  , PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
 #define phpdbg_notice(strfmt, ...)             phpdbg_print(P_NOTICE , PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
 #define phpdbg_writeln(strfmt, ...)            phpdbg_print(P_WRITELN, PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
 #define phpdbg_write(strfmt, ...)              phpdbg_print(P_WRITE  , PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
-#define phpdbg_script(type, strfmt, ...)       phpdbg_print(type     , PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
-#define phpdbg_log(fmt, ...) phpdbg_log_internal(PHPDBG_G(io)[PHPDBG_STDOUT].fd, fmt, ##__VA_ARGS__)
-#define phpdbg_out(fmt, ...) phpdbg_out_internal(PHPDBG_G(io)[PHPDBG_STDOUT].fd, fmt, ##__VA_ARGS__)
 
-#define phpdbg_error_ex(out, strfmt, ...)         phpdbg_print(P_ERROR  , out, strfmt, ##__VA_ARGS__)
-#define phpdbg_notice_ex(out, trfmt, ...)         phpdbg_print(P_NOTICE , out, strfmt, ##__VA_ARGS__)
-#define phpdbg_writeln_ex(out, strfmt, ...)       phpdbg_print(P_WRITELN, out, strfmt, ##__VA_ARGS__)
-#define phpdbg_write_ex(out, strfmt, ...)         phpdbg_print(P_WRITE  , out, strfmt, ##__VA_ARGS__)
-#define phpdbg_script_ex(out, type, strfmt, ...)  phpdbg_print(type,      out, strfmt, ##__VA_ARGS__)
-#define phpdbg_log_ex(out, strfmt, ...) phpdbg_log_internal(out, strfmt, ##__VA_ARGS__)
-#define phpdbg_out_ex(out, strfmt, ...) phpdbg_out_internal(out, strfmt, ##__VA_ARGS__)
+#define phpdbg_log(fmt, ...)                   phpdbg_log_internal(PHPDBG_G(io)[PHPDBG_STDOUT].fd, fmt, ##__VA_ARGS__)
+#define phpdbg_out(fmt, ...)                   phpdbg_out_internal(PHPDBG_G(io)[PHPDBG_STDOUT].fd, fmt, ##__VA_ARGS__)
+
+#define phpdbg_script(type, strfmt, ...)       phpdbg_print(type,      PHPDBG_G(io)[PHPDBG_STDOUT].fd, strfmt, ##__VA_ARGS__)
 
 #define phpdbg_asprintf(buf, ...) _phpdbg_asprintf(buf, ##__VA_ARGS__)
 PHPDBG_API int _phpdbg_asprintf(char **buf, const char *format, ...);
 
 #if PHPDBG_DEBUG
-#	define phpdbg_debug(strfmt, ...) phpdbg_log_ex(PHPDBG_G(io)[PHPDBG_STDERR].fd, strfmt, ##__VA_ARGS__)
+#	define phpdbg_debug(strfmt, ...) phpdbg_log_internal(PHPDBG_G(io)[PHPDBG_STDERR].fd, strfmt, ##__VA_ARGS__)
 #else
 #	define phpdbg_debug(strfmt, ...)
 #endif

sapi/phpdbg/phpdbg.h
@@ -272,7 +272,6 @@ ZEND_BEGIN_MODULE_GLOBALS(phpdbg)
 	phpdbg_oplog_entry *oplog_cur;               /* current oplog entry */
 
 	struct {
-		FILE *ptr;
 		int fd;
 	} io[PHPDBG_IO_FDS];                         /* io */
 	ssize_t (*php_stdiop_write)(php_stream *, const char *, size_t);
@@ -302,7 +301,7 @@ ZEND_BEGIN_MODULE_GLOBALS(phpdbg)
 	uint64_t flags;                              /* phpdbg flags */
 
 	char *sapi_name_ptr;                         /* store sapi name to free it if necessary to not leak memory */
-	long lines;                                  /* max number of lines to display */
+	zend_ulong lines;                                  /* max number of lines to display */
 ZEND_END_MODULE_GLOBALS(phpdbg) /* }}} */
 
 #endif

sapi/phpdbg/phpdbg_bp.h
@@ -54,7 +54,7 @@ typedef struct _phpdbg_breakbase_t {
  */
 typedef struct _phpdbg_breakfile_t {
 	phpdbg_breakbase(filename);
-	long        line;
+	zend_ulong line;
 } phpdbg_breakfile_t;
 
 /**
@@ -123,7 +123,7 @@ PHPDBG_API HashTable *phpdbg_resolve_pending_file_break_ex(const char *file, uin
 PHPDBG_API void phpdbg_resolve_pending_file_break(const char *file); /* }}} */
 
 /* {{{ Breakpoint Creation API */
-PHPDBG_API void phpdbg_set_breakpoint_file(const char* filename, size_t path_len, long lineno);
+PHPDBG_API void phpdbg_set_breakpoint_file(const char* filename, size_t path_len, zend_ulong lineno);
 PHPDBG_API void phpdbg_set_breakpoint_symbol(const char* func_name, size_t func_name_len);
 PHPDBG_API void phpdbg_set_breakpoint_method(const char* class_name, const char* func_name);
 PHPDBG_API void phpdbg_set_breakpoint_opcode(const char* opname, size_t opname_len);

sapi/phpdbg/phpdbg_cmd.c
@@ -101,7 +101,7 @@ PHPDBG_API char* phpdbg_param_tostring(const phpdbg_param_t *param, char **point
 		break;
 
 		case NUMERIC_PARAM:
-			ZEND_IGNORE_VALUE(asprintf(pointer, "%li", param->num));
+			ZEND_IGNORE_VALUE(asprintf(pointer, ZEND_LONG_FMT, param->num));
 		break;
 
 		case METHOD_PARAM:
@@ -110,18 +110,18 @@ PHPDBG_API char* phpdbg_param_tostring(const phpdbg_param_t *param, char **point
 
 		case FILE_PARAM:
 			if (param->num) {
-				ZEND_IGNORE_VALUE(asprintf(pointer, "%s:%lu#%lu", param->file.name, param->file.line, param->num));
+				ZEND_IGNORE_VALUE(asprintf(pointer, "%s:"ZEND_ULONG_FMT"#"ZEND_ULONG_FMT, param->file.name, param->file.line, param->num));
 			} else {
-				ZEND_IGNORE_VALUE(asprintf(pointer, "%s:%lu", param->file.name, param->file.line));
+				ZEND_IGNORE_VALUE(asprintf(pointer, "%s:"ZEND_ULONG_FMT, param->file.name, param->file.line));
 			}
 		break;
 
 		case NUMERIC_FUNCTION_PARAM:
-			ZEND_IGNORE_VALUE(asprintf(pointer, "%s#%lu", param->str, param->num));
+			ZEND_IGNORE_VALUE(asprintf(pointer, "%s#"ZEND_ULONG_FMT, param->str, param->num));
 		break;
 
 		case NUMERIC_METHOD_PARAM:
-			ZEND_IGNORE_VALUE(asprintf(pointer, "%s::%s#%lu", param->method.class, param->method.name, param->num));
+			ZEND_IGNORE_VALUE(asprintf(pointer, "%s::%s#"ZEND_ULONG_FMT, param->method.class, param->method.name, param->num));
 		break;
 
 		default:
@@ -331,11 +331,11 @@ PHPDBG_API void phpdbg_param_debug(const phpdbg_param_t *param, const char *msg)
 			break;
 
 			case NUMERIC_FILE_PARAM:
-				fprintf(stderr, "%s NUMERIC_FILE_PARAM(%s:#%lu)\n", msg, param->file.name, param->file.line);
+				fprintf(stderr, "%s NUMERIC_FILE_PARAM(%s:#"ZEND_ULONG_FMT")\n", msg, param->file.name, param->file.line);
 			break;
 
 			case FILE_PARAM:
-				fprintf(stderr, "%s FILE_PARAM(%s:%lu)\n", msg, param->file.name, param->file.line);
+				fprintf(stderr, "%s FILE_PARAM(%s:"ZEND_ULONG_FMT")\n", msg, param->file.name, param->file.line);
 			break;
 
 			case METHOD_PARAM:
@@ -347,11 +347,11 @@ PHPDBG_API void phpdbg_param_debug(const phpdbg_param_t *param, const char *msg)
 			break;
 
 			case NUMERIC_FUNCTION_PARAM:
-				fprintf(stderr, "%s NUMERIC_FUNCTION_PARAM(%s::%ld)\n", msg, param->str, param->num);
+				fprintf(stderr, "%s NUMERIC_FUNCTION_PARAM(%s::"ZEND_LONG_FMT")\n", msg, param->str, param->num);
 			break;
 
 			case NUMERIC_PARAM:
-				fprintf(stderr, "%s NUMERIC_PARAM(%ld)\n", msg, param->num);
+				fprintf(stderr, "%s NUMERIC_PARAM("ZEND_LONG_FMT")\n", msg, param->num);
 			break;
 
 			case COND_PARAM:
@@ -468,9 +468,9 @@ PHPDBG_API int phpdbg_stack_verify(const phpdbg_command_t *command, phpdbg_param
 		char buffer[128] = {0,};
 		const phpdbg_param_t *top = (stack != NULL) ? *stack : NULL;
 		const char *arg = command->args;
-		size_t least = 0L,
-		       received = 0L,
-		       current = 0L;
+		zend_ulong least = 0L,
+		           received = 0L,
+		           current = 0L;
 		bool optional = 0;
 
 		/* check for arg spec */
@@ -499,14 +499,14 @@ PHPDBG_API int phpdbg_stack_verify(const phpdbg_command_t *command, phpdbg_param
 
 #define verify_arg(e, a, t) if (!(a)) { \
 	if (!optional) { \
-		phpdbg_error("The command \"%s\" expected %s and got nothing at parameter %lu", \
+		phpdbg_error("The command \"%s\" expected %s and got nothing at parameter "ZEND_ULONG_FMT, \
 			phpdbg_command_name(command, buffer), \
 			(e), \
 			current); \
 		return FAILURE;\
 	} \
 } else if ((a)->type != (t)) { \
-	phpdbg_error("The command \"%s\" expected %s and got %s at parameter %lu", \
+	phpdbg_error("The command \"%s\" expected %s and got %s at parameter "ZEND_ULONG_FMT, \
 		phpdbg_command_name(command, buffer), \
 		(e),\
 		phpdbg_get_param_type((a)), \
@@ -554,7 +554,7 @@ PHPDBG_API int phpdbg_stack_verify(const phpdbg_command_t *command, phpdbg_param
 #undef verify_arg
 
 		if ((received < least)) {
-			phpdbg_error("The command \"%s\" expected at least %lu arguments (%s) and received %lu",
+			phpdbg_error("The command \"%s\" expected at least "ZEND_ULONG_FMT" arguments (%s) and received "ZEND_ULONG_FMT,
 				phpdbg_command_name(command, buffer),
 				least,
 				command->args,
@@ -643,7 +643,7 @@ PHPDBG_API const phpdbg_command_t *phpdbg_stack_resolve(const phpdbg_command_t *
 			}
 
 			/* ", " separated matches */
-			phpdbg_error("The command \"%s\" is ambiguous, matching %lu commands (%s)", name->str, matches, list);
+			phpdbg_error("The command \"%s\" is ambiguous, matching "ZEND_ULONG_FMT" commands (%s)", name->str, matches, list);
 			efree(list);
 
 			return NULL;

sapi/phpdbg/phpdbg_cmd.h
@@ -51,11 +51,11 @@ typedef enum {
 typedef struct _phpdbg_param phpdbg_param_t;
 struct _phpdbg_param {
 	phpdbg_param_type type;
-	long num;
+	zend_long num;
 	zend_ulong addr;
 	struct {
 		char *name;
-		long line;
+		zend_ulong line;
 	} file;
 	struct {
 		char *class;
@@ -175,7 +175,7 @@ PHPDBG_API void phpdbg_param_debug(const phpdbg_param_t *param, const char *msg)
 */
 #define phpdbg_default_switch_case() \
 	default: \
-		phpdbg_error("command", "type=\"wrongarg\" got=\"%s\"", "Unsupported parameter type (%s) for command", phpdbg_get_param_type(param)); \
+		phpdbg_error("Unsupported parameter type (%s) for command", phpdbg_get_param_type(param)); \
 	break
 
 #endif /* PHPDBG_CMD_H */

commit a176269d42ff26290c7dc47c9c416b8a116d4f68

Fix GH-14553: Bug in phpdbg8.3 (also 8.1 and 8.2) echo output - trimmed at NULL byte (?)
This broke in 6318040df2b6c6f4824b99fd27369179d29ee93b when phpdbg
stopped using its custom printing routines. By relying on standard
printing routines, the embedded NUL bytes are causing the strings to be
cut off, even when using %.*s. Solve this by going straight to the
output routine, which is what the printf routine would've done anyway.

sapi/phpdbg/phpdbg.c
@@ -845,7 +845,7 @@ static void php_sapi_phpdbg_register_vars(zval *track_vars_array) /* {{{ */
 
 static inline size_t php_sapi_phpdbg_ub_write(const char *message, size_t length) /* {{{ */
 {
-	return phpdbg_script(P_STDOUT, "%.*s", (int) length, message);
+	return phpdbg_process_print(PHPDBG_G(io)[PHPDBG_STDOUT].fd, P_STDOUT, message, (int) length);
 } /* }}} */
 
 /* beginning of struct, see main/streams/plain_wrapper.c line 111 */

sapi/phpdbg/phpdbg_out.c
@@ -40,7 +40,7 @@ PHPDBG_API int _phpdbg_asprintf(char **buf, const char *format, ...) {
 	return ret;
 }
 
-static int phpdbg_process_print(int fd, int type, const char *msg, int msglen) {
+int phpdbg_process_print(int fd, int type, const char *msg, int msglen) {
 	char *msgout = NULL;
 	int msgoutlen = FAILURE;
 

sapi/phpdbg/phpdbg_out.h
@@ -59,6 +59,8 @@ PHPDBG_API void phpdbg_free_err_buf(void);
 PHPDBG_API void phpdbg_activate_err_buf(bool active);
 PHPDBG_API int phpdbg_output_err_buf(const char *strfmt, ...);
 
+int phpdbg_process_print(int fd, int type, const char *msg, int msglen);
+
 
 /* {{{ For separation */
 #define SEPARATE "------------------------------------------------" /* }}} */

sapi/phpdbg/tests/gh14553.phpt
@@ -1,0 +1,14 @@
+--TEST--
+GH-14553 (Bug in phpdbg8.3 (also 8.1 and 8.2) echo output - trimmed at NULL byte (?))
+--PHPDBG--
+r
+q
+--FILE--
+<?php
+echo "hello\0world";
+?>
+--EXPECTF--
+[Successful compilation of %s]
+prompt> hello%0world
+[Script ended normally]
+prompt>