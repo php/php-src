5471f3d9223448f20583131dbaba0922a8648008
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-07-18 15:22:44 +0200

    Don't open-code string value accesses in xpath (#15007)

--- ext/dom/xpath.c
+++ ext/dom/xpath.c
@@ -486,22 +486,22 @@ PHP_METHOD(DOMXPath, quote) {
 	}
 	if (memchr(input, '\'', input_len) == NULL) {
 		zend_string *const output = zend_string_safe_alloc(1, input_len, 2, false);
-		output->val[0] = '\'';
-		memcpy(output->val + 1, input, input_len);
-		output->val[input_len + 1] = '\'';
-		output->val[input_len + 2] = '\0';
+		ZSTR_VAL(output)[0] = '\'';
+		memcpy(ZSTR_VAL(output) + 1, input, input_len);
+		ZSTR_VAL(output)[input_len + 1] = '\'';
+		ZSTR_VAL(output)[input_len + 2] = '\0';
 		RETURN_STR(output);
 	} else if (memchr(input, '"', input_len) == NULL) {
 		zend_string *const output = zend_string_safe_alloc(1, input_len, 2, false);
-		output->val[0] = '"';
-		memcpy(output->val + 1, input, input_len);
-		output->val[input_len + 1] = '"';
-		output->val[input_len + 2] = '\0';
+		ZSTR_VAL(output)[0] = '"';
+		memcpy(ZSTR_VAL(output) + 1, input, input_len);
+		ZSTR_VAL(output)[input_len + 1] = '"';
+		ZSTR_VAL(output)[input_len + 2] = '\0';
 		RETURN_STR(output);
 	} else {
 		smart_str output = {0};
 		// need to use the concat() trick published by Robert Rossney at https://stackoverflow.com/a/1352556/1067003
-		smart_str_appendl(&output, "concat(", 7);
+		smart_str_appendl(&output, ZEND_STRL("concat("));
 		const char *ptr = input;
 		const char *const end = input + input_len;
 		while (ptr < end) {
@@ -518,7 +518,7 @@ PHP_METHOD(DOMXPath, quote) {
 			smart_str_appendc(&output, ',');
 		}
 		ZEND_ASSERT(ptr == end);
-		output.s->val[output.s->len - 1] = ')';
+		ZSTR_VAL(output.s)[output.s->len - 1] = ')';
 		RETURN_STR(smart_str_extract(&output));
 	}
 }
