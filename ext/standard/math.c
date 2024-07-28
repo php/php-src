php/php-src

commit 5905857fd2652585989ed03e5a5b0beea89ab18e

RFC: Add the RoundingMode enum (#14833)
see https://wiki.php.net/rfc/correctly_name_the_rounding_mode_and_make_it_an_enum

Co-authored-by: Saki Takamachi <saki@php.net>
Co-authored-by: Niels Dossche <7771979+nielsdos@users.noreply.github.com>

ext/standard/math.c
@@ -20,6 +20,7 @@
 #include "php.h"
 #include "php_math.h"
 #include "zend_bitset.h"
+#include "zend_enum.h"
 #include "zend_exceptions.h"
 #include "zend_multiply.h"
 #include "zend_portability.h"
@@ -29,6 +30,8 @@
 #include <stdlib.h>
 
 #include "basic_functions.h"
+
+PHPAPI zend_class_entry *rounding_mode_ce;
 
 /* {{{ php_intpow10
        Returns pow(10.0, (double)power), uses fast lookup table for exact powers */
@@ -301,6 +304,32 @@ PHP_FUNCTION(floor)
 }
 /* }}} */
 
+int php_math_round_mode_from_enum(zend_object *mode)
+{
+	zval *case_name = zend_enum_fetch_case_name(mode);
+	zend_string *mode_name = Z_STR_P(case_name);
+
+	switch (ZSTR_VAL(mode_name)[0] + ZSTR_VAL(mode_name)[4]) {
+		case 'H' + 'A':
+			return PHP_ROUND_HALF_UP;
+		case 'H' + 'T':
+			return PHP_ROUND_HALF_DOWN;
+		case 'H' + 'E':
+			return PHP_ROUND_HALF_EVEN;
+		case 'H' + 'O':
+			return PHP_ROUND_HALF_ODD;
+		case 'T' + 'r':
+			return PHP_ROUND_TOWARD_ZERO;
+		case 'A' + 'F':
+			return PHP_ROUND_AWAY_FROM_ZERO;
+		case 'N' + 't':
+			return PHP_ROUND_FLOOR;
+		case 'P' + 't':
+			return PHP_ROUND_CEILING;
+		EMPTY_SWITCH_DEFAULT_CASE();
+	}
+}
+
 /* {{{ Returns the number rounded to specified precision */
 PHP_FUNCTION(round)
 {
@@ -308,12 +337,13 @@ PHP_FUNCTION(round)
 	int places = 0;
 	zend_long precision = 0;
 	zend_long mode = PHP_ROUND_HALF_UP;
+	zend_object *mode_object = NULL;
 
 	ZEND_PARSE_PARAMETERS_START(1, 3)
 		Z_PARAM_NUMBER(value)
 		Z_PARAM_OPTIONAL
 		Z_PARAM_LONG(precision)
-		Z_PARAM_LONG(mode)
+		Z_PARAM_OBJ_OF_CLASS_OR_LONG(mode_object, rounding_mode_ce, mode)
 	ZEND_PARSE_PARAMETERS_END();
 
 	if (ZEND_NUM_ARGS() >= 2) {
@@ -322,6 +352,10 @@ PHP_FUNCTION(round)
 		} else {
 			places = ZEND_LONG_INT_UDFL(precision) ? INT_MIN : (int)precision;
 		}
+	}
+
+	if (mode_object != NULL) {
+		mode = php_math_round_mode_from_enum(mode_object);
 	}
 
 	switch (mode) {
@@ -335,7 +369,7 @@ PHP_FUNCTION(round)
 		case PHP_ROUND_FLOOR:
 			break;
 		default:
-			zend_argument_value_error(3, "must be a valid rounding mode (PHP_ROUND_*)");
+			zend_argument_value_error(3, "must be a valid rounding mode (RoundingMode::*)");
 			RETURN_THROWS();
 	}
 

ext/standard/php_math_round_mode.h
@@ -15,6 +15,8 @@
    +----------------------------------------------------------------------+
 */
 
+#include "php.h"
+
 /* Define rounding modes (all are round-to-nearest) */
 #ifndef PHP_ROUND_HALF_UP
 #define PHP_ROUND_HALF_UP        0x01    /* Arithmetic rounding, up == away from zero */
@@ -47,3 +49,7 @@
 #ifndef PHP_ROUND_AWAY_FROM_ZERO
 #define PHP_ROUND_AWAY_FROM_ZERO 0x08
 #endif
+
+extern PHPAPI zend_class_entry *rounding_mode_ce;
+
+int php_math_round_mode_from_enum(zend_object *mode);

UPGRADING
@@ -344,6 +344,9 @@ PHP 8.4 UPGRADE NOTES
   . stream_bucket_make_writeable() and stream_bucket_new() will now return a
     StreamBucket instance instead of an stdClass instance.
     RFC: https://wiki.php.net/rfc/dedicated_stream_bucket
+  . Added a new RoundingMode enum with clearer naming and improved discoverability
+    compared to the PHP_ROUND_* constants.
+    RFC: https://wiki.php.net/rfc/correctly_name_the_rounding_mode_and_make_it_an_enum
 
 - SOAP:
   . Added support for clark notation for namespaces in class map.
@@ -549,23 +552,27 @@ PHP 8.4 UPGRADE NOTES
     would have resulted in 1.0 instead of the correct result 0.0. Additional
     inputs might also be affected and result in different outputs compared to
     earlier PHP versions.
-  . The default value of the 'cost' option for PASSWORD_BCRYPT for password_hash()
-    has been increased from '10' to '12'.
+  . The $mode parameter of the round() function has been widened to RoundingMode|int,
+    accepting instances of a new RoundingMode enum.
 
-    RFC: https://wiki.php.net/rfc/bcrypt_cost_2023
-  . Four new modes have been added to the round() function: PHP_ROUND_CEILING,
-    PHP_ROUND_FLOOR, PHP_ROUND_TOWARD_ZERO, PHP_ROUND_AWAY_FROM_ZERO.
+    RFC: https://wiki.php.net/rfc/correctly_name_the_rounding_mode_and_make_it_an_enum
+  . Four new modes have been added to the round() function: RoundingMode::PositiveInfinity,
+    RoundingMode::NegativeInfinity, RoundingMode::TowardsZero, RoundingMode::AwayFromZero.
 
     RFC: https://wiki.php.net/rfc/new_rounding_modes_to_round_function
-  . debug_zval_dump() now indicates whether an array is packed.
   . Fixed a bug caused by "pre-rounding" of the round() function. Previously, using
     "pre-rounding" to treat a value like 0.285 (actually 0.28499999999999998) as a
     decimal number and round it to 0.29. However, "pre-rounding" incorrectly rounds
     certain numbers, so this fix removes "pre-rounding" and changes the way numbers
     are compared, so that the values are correctly rounded as decimal numbers.
-  . long2ip() now returns string instead of string|false.
   . The maximum precision that can be handled by round() has been extended by
     one digit.
+  . The default value of the 'cost' option for PASSWORD_BCRYPT for password_hash()
+    has been increased from '10' to '12'.
+
+    RFC: https://wiki.php.net/rfc/bcrypt_cost_2023
+  . debug_zval_dump() now indicates whether an array is packed.
+  . long2ip() now returns string instead of string|false.
   . output_add_rewrite_var() now uses url_rewriter.hosts instead of
     session.trans_sid_hosts for selecting hosts that will be rewritten.
   . highlight_string() now has a return type of string|true instead of string|bool.
@@ -808,12 +815,6 @@ PHP 8.4 UPGRADE NOTES
   . P_GID (NetBSD/FreeBSD only).
   . P_SID (NetBSD/FreeBSD only).
   . P_JAILID (FreeBSD only).
-
-- Standard:
-  . PHP_ROUND_CEILING.
-  . PHP_ROUND_FLOOR.
-  . PHP_ROUND_TOWARD_ZERO.
-  . PHP_ROUND_AWAY_FROM_ZERO.
 
 - Sockets:
   . SO_EXCLUSIVEADDRUSE (Windows only).

ext/bcmath/bcmath.c
@@ -696,14 +696,19 @@ PHP_FUNCTION(bcround)
 	zend_string *numstr;
 	zend_long precision = 0;
 	zend_long mode = PHP_ROUND_HALF_UP;
+	zend_object *mode_object = NULL;
 	bc_num num = NULL, result;
 
 	ZEND_PARSE_PARAMETERS_START(1, 3)
 		Z_PARAM_STR(numstr)
 		Z_PARAM_OPTIONAL
 		Z_PARAM_LONG(precision)
-		Z_PARAM_LONG(mode)
+		Z_PARAM_OBJ_OF_CLASS(mode_object, rounding_mode_ce)
 	ZEND_PARSE_PARAMETERS_END();
+
+	if (mode_object != NULL) {
+		mode = php_math_round_mode_from_enum(mode_object);
+	}
 
 	switch (mode) {
 		case PHP_ROUND_HALF_UP:
@@ -716,7 +721,8 @@ PHP_FUNCTION(bcround)
 		case PHP_ROUND_AWAY_FROM_ZERO:
 			break;
 		default:
-			zend_argument_value_error(3, "must be a valid rounding mode (PHP_ROUND_*)");
+			/* This is currently unreachable, but might become reachable when new modes are added. */
+			zend_argument_value_error(3, "is an unsupported rounding mode");
 			return;
 	}
 

ext/bcmath/bcmath.stub.php
@@ -37,4 +37,4 @@ function bcfloor(string $num): string {}
 function bcceil(string $num): string {}
 
 /** @refcount 1 */
-function bcround(string $num, int $precision = 0, int $mode = PHP_ROUND_HALF_UP): string {}
+function bcround(string $num, int $precision = 0, RoundingMode $mode = RoundingMode::HalfAwayFromZero): string {}

ext/bcmath/bcmath_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: cd3d182e13cb0ca22b27c13a8d0a86c20fde5b76 */
+ * Stub hash: 7439a5fca649b8c4df317b0da1416c8fe59faf72 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_bcadd, 0, 2, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, num1, IS_STRING, 0)
@@ -52,7 +52,7 @@ ZEND_END_ARG_INFO()
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_bcround, 0, 1, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO(0, num, IS_STRING, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, precision, IS_LONG, 0, "0")
-	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, mode, IS_LONG, 0, "PHP_ROUND_HALF_UP")
+	ZEND_ARG_OBJ_INFO_WITH_DEFAULT_VALUE(0, mode, RoundingMode, 0, "RoundingMode::HalfAwayFromZero")
 ZEND_END_ARG_INFO()
 
 ZEND_FUNCTION(bcadd);

ext/bcmath/tests/bcround_all.phpt
@@ -1,0 +1,104 @@
+--TEST--
+bcround() function all modes
+--EXTENSIONS--
+bcmath
+--FILE--
+<?php
+foreach (RoundingMode::cases() as $mode) {
+     foreach ([
+         '1.0',
+         '-1.0',
+         '1.2',
+         '-1.2',
+         '1.7',
+         '-1.7',
+         '1.5',
+         '-1.5',
+         '2.5',
+         '-2.5',
+     ] as $number) {
+         printf("%-20s: %s -> %s\n", $mode->name, $number, bcround($number, 0, $mode));
+     }
+ }
+?>
+--EXPECT--
+HalfAwayFromZero    : 1.0 -> 1
+HalfAwayFromZero    : -1.0 -> -1
+HalfAwayFromZero    : 1.2 -> 1
+HalfAwayFromZero    : -1.2 -> -1
+HalfAwayFromZero    : 1.7 -> 2
+HalfAwayFromZero    : -1.7 -> -2
+HalfAwayFromZero    : 1.5 -> 2
+HalfAwayFromZero    : -1.5 -> -2
+HalfAwayFromZero    : 2.5 -> 3
+HalfAwayFromZero    : -2.5 -> -3
+HalfTowardsZero     : 1.0 -> 1
+HalfTowardsZero     : -1.0 -> -1
+HalfTowardsZero     : 1.2 -> 1
+HalfTowardsZero     : -1.2 -> -1
+HalfTowardsZero     : 1.7 -> 2
+HalfTowardsZero     : -1.7 -> -2
+HalfTowardsZero     : 1.5 -> 1
+HalfTowardsZero     : -1.5 -> -1
+HalfTowardsZero     : 2.5 -> 2
+HalfTowardsZero     : -2.5 -> -2
+HalfEven            : 1.0 -> 1
+HalfEven            : -1.0 -> -1
+HalfEven            : 1.2 -> 1
+HalfEven            : -1.2 -> -1
+HalfEven            : 1.7 -> 2
+HalfEven            : -1.7 -> -2
+HalfEven            : 1.5 -> 2
+HalfEven            : -1.5 -> -2
+HalfEven            : 2.5 -> 2
+HalfEven            : -2.5 -> -2
+HalfOdd             : 1.0 -> 1
+HalfOdd             : -1.0 -> -1
+HalfOdd             : 1.2 -> 1
+HalfOdd             : -1.2 -> -1
+HalfOdd             : 1.7 -> 2
+HalfOdd             : -1.7 -> -2
+HalfOdd             : 1.5 -> 1
+HalfOdd             : -1.5 -> -1
+HalfOdd             : 2.5 -> 3
+HalfOdd             : -2.5 -> -3
+TowardsZero         : 1.0 -> 1
+TowardsZero         : -1.0 -> -1
+TowardsZero         : 1.2 -> 1
+TowardsZero         : -1.2 -> -1
+TowardsZero         : 1.7 -> 1
+TowardsZero         : -1.7 -> -1
+TowardsZero         : 1.5 -> 1
+TowardsZero         : -1.5 -> -1
+TowardsZero         : 2.5 -> 2
+TowardsZero         : -2.5 -> -2
+AwayFromZero        : 1.0 -> 1
+AwayFromZero        : -1.0 -> -1
+AwayFromZero        : 1.2 -> 2
+AwayFromZero        : -1.2 -> -2
+AwayFromZero        : 1.7 -> 2
+AwayFromZero        : -1.7 -> -2
+AwayFromZero        : 1.5 -> 2
+AwayFromZero        : -1.5 -> -2
+AwayFromZero        : 2.5 -> 3
+AwayFromZero        : -2.5 -> -3
+NegativeInfinity    : 1.0 -> 1
+NegativeInfinity    : -1.0 -> -1
+NegativeInfinity    : 1.2 -> 1
+NegativeInfinity    : -1.2 -> -2
+NegativeInfinity    : 1.7 -> 1
+NegativeInfinity    : -1.7 -> -2
+NegativeInfinity    : 1.5 -> 1
+NegativeInfinity    : -1.5 -> -2
+NegativeInfinity    : 2.5 -> 2
+NegativeInfinity    : -2.5 -> -3
+PositiveInfinity    : 1.0 -> 1
+PositiveInfinity    : -1.0 -> -1
+PositiveInfinity    : 1.2 -> 2
+PositiveInfinity    : -1.2 -> -1
+PositiveInfinity    : 1.7 -> 2
+PositiveInfinity    : -1.7 -> -1
+PositiveInfinity    : 1.5 -> 2
+PositiveInfinity    : -1.5 -> -1
+PositiveInfinity    : 2.5 -> 3
+PositiveInfinity    : -2.5 -> -2

ext/bcmath/tests/bcround_away_from_zero.phpt
@@ -1,11 +1,11 @@
 --TEST--
-bcround() function PHP_ROUND_AWAY_FROM_ZERO
+bcround() function AwayFromZero
 --EXTENSIONS--
 bcmath
 --FILE--
 <?php
 require_once __DIR__ . '/bcround_test_helper.inc';
-run_round_test(PHP_ROUND_AWAY_FROM_ZERO);
+run_round_test(RoundingMode::AwayFromZero);
 ?>
 --EXPECT--
 ========== non-boundary value ==========

commit b2d8306857fb2193e96bdfe7545124e9f2f91012

fix: add missing `PHPAPI`
Issue introduced in commit https://github.com/php/php-src/commit/5905857fd2652585989ed03e5a5b0beea89ab18e from PR https://github.com/php/php-src/pull/14833

ext/standard/math.c
@@ -304,7 +304,7 @@ PHP_FUNCTION(floor)
 }
 /* }}} */
 
-int php_math_round_mode_from_enum(zend_object *mode)
+PHPAPI int php_math_round_mode_from_enum(zend_object *mode)
 {
 	zval *case_name = zend_enum_fetch_case_name(mode);
 	zend_string *mode_name = Z_STR_P(case_name);

ext/standard/php_math_round_mode.h
@@ -52,4 +52,4 @@
 
 extern PHPAPI zend_class_entry *rounding_mode_ce;
 
-int php_math_round_mode_from_enum(zend_object *mode);
+PHPAPI int php_math_round_mode_from_enum(zend_object *mode);