ded8fb79bdce40bdb4c7ebd0a5dc91de0c9539a0
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-08-03 00:09:01 +0200

    Fix UAF issues with PCRE after request shutdown
    There are two related issues, each tested.
    
    First problem:
    What happens is that on the CLI SAPI we have a per-request pcre cache,
    and on there the request shutdown for the pcre module happens prior to
    the remaining live object destruction. So when the SPL object wants to
    clean up the regular expression object it gets a use-after-free.
    
    Second problem:
    Very similarly, the non-persistent resources are destroyed after request
    shutdown, so on the CLI SAPI the pcre request cache is already gone, but
    if a userspace stream references a regex in the pcre cache, this breaks.
    
    Two things that come immediately to mind:
      -  We could fix it by no longer treating the CLI SAPI special and just use
         the same lifecycle as the module. This simplifies the pcre module code
         a bit too. I wonder why we even have the separation in the first place.
         The downside here is that we're using more the system allocator
         than Zend's allocator for cache entries.
      -  We could modify the shutdown code to not remove regular expressions
         with a refcount>0 and modify php_pcre_pce_decref code such that it
         becomes php_pcre_pce_decref's job to clean up when the refcount
         becomes 0 during shutdown. However, this gets nasty quickly.
    
    I chose the first solution here as it should be reliable and simple.
    
    Closes GH-15064.

--- ext/opcache/ZendAccelerator.c
+++ ext/opcache/ZendAccelerator.c
@@ -2611,10 +2611,6 @@ static void accel_reset_pcre_cache(void)
 {
 	Bucket *p;
 
-	if (PCRE_G(per_request_cache)) {
-		return;
-	}
-
 	ZEND_HASH_MAP_FOREACH_BUCKET(&PCRE_G(pcre_cache), p) {
 		/* Remove PCRE cache entries with inconsistent keys */
 		if (zend_accel_in_shm(p->key)) {
