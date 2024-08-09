2e26559fcd8109dd75e68a240a3007ce5c831d3a
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-08-09 23:05:10 +0200

    Fix uninitialized value watchpoint_hit
    Closes GH-15317.

--- sapi/phpdbg/phpdbg_watch.c
+++ sapi/phpdbg/phpdbg_watch.c
@@ -1479,6 +1479,7 @@ void phpdbg_setup_watchpoints(void) {
 	zend_hash_init(PHPDBG_G(watchlist_mem_backup), phpdbg_pagesize / (sizeof(Bucket) + sizeof(uint32_t)), NULL, NULL, 1);
 
 	PHPDBG_G(watch_tmp) = NULL;
+	PHPDBG_G(watchpoint_hit) = false;
 
 #ifdef HAVE_USERFAULTFD_WRITEFAULT
 	PHPDBG_G(watch_userfaultfd) = syscall(SYS_userfaultfd, O_CLOEXEC);
