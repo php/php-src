f25e9ce0fe348b7e5e7e01823704773f1dae5d36
Gina Peter Bnayard <girgias@php.net>
2024-08-13 17:48:39 +0200

    ext/standard/quot_print.c: Minor refactoring to php_hex2int()
    We already check, and assume, that the value is hexadecimal

--- ext/standard/quot_print.c
+++ ext/standard/quot_print.c
@@ -34,15 +34,11 @@ static char php_hex2int(int c) /* {{{ */
 {
 	if (isdigit(c)) {
 		return c - '0';
-	}
-	else if (c >= 'A' && c <= 'F') {
+	} else if (c >= 'A' && c <= 'F') {
 		return c - 'A' + 10;
-	}
-	else if (c >= 'a' && c <= 'f') {
+	} else {
+		ZEND_ASSERT(c >= 'a' && c <= 'f');
 		return c - 'a' + 10;
-	}
-	else {
-		return -1;
 	}
 }
 /* }}} */
