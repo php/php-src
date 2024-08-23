b960ae60475ae84b619a0e9410885df2b7b33cea
Calvin Buckley <calvin@cmpct.info>
2024-08-23 14:45:32 -0300

    Clean up remaining gethostbyname(_r) detritus
    Includes some global state as well.

--- ext/standard/file.h
+++ ext/standard/file.h
@@ -102,11 +102,6 @@ typedef struct {
 	HashTable *stream_filters;			/* per-request copy of stream_filters_hash */
 	HashTable *wrapper_errors;			/* key: wrapper address; value: linked list of char* */
 	int pclose_wait;
-#ifdef HAVE_GETHOSTBYNAME_R
-	struct hostent tmp_host_info;
-	char *tmp_host_buf;
-	size_t tmp_host_buf_len;
-#endif
 } php_file_globals;
 
 #ifdef ZTS
