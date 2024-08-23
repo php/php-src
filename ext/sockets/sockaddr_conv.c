b7b502848047dce1ed2a61e922bf1ae6e71678bc
Calvin Buckley <calvin@cmpct.info>
2024-08-23 11:42:13 -0300

    Convert sockaddr_conv.c to use getaddrinfo for v4
    Not ideal right now; it should really be using the PHP wrapper function,
    and have bool conversion. But it does get rid of a use of gethostbyname.

--- ext/sockets/sockaddr_conv.c
+++ ext/sockets/sockaddr_conv.c
@@ -11,24 +11,24 @@
 
 extern zend_result php_string_to_if_index(const char *val, unsigned *out);
 
-#ifdef HAVE_IPV6
-/* Sets addr by hostname, or by ip in string form (AF_INET6) */
-int php_set_inet6_addr(struct sockaddr_in6 *sin6, char *string, php_socket *php_sock) /* {{{ */
+int php_set_common_addr(struct sockaddr *sin, int family, char *string, php_socket *php_sock) /* {{{ */
 {
-	struct in6_addr tmp;
 #ifdef HAVE_GETADDRINFO
+	struct sockaddr_in6 *sin6 = (struct sockaddr_in6*)sin;
+	struct sockaddr_in *sin4 = (struct sockaddr_in*)sin;
+	struct in6_addr tmp6;
+	struct in_addr tmp4;
+
 	struct addrinfo hints;
 	struct addrinfo *addrinfo = NULL;
-#endif
-	char *scope = strchr(string, '%');
 
-	if (inet_pton(AF_INET6, string, &tmp)) {
-		memcpy(&(sin6->sin6_addr.s6_addr), &(tmp.s6_addr), sizeof(struct in6_addr));
+	if (family == AF_INET6 && inet_pton(AF_INET6, string, &tmp6)) {
+		memcpy(&(sin6->sin6_addr.s6_addr), &(tmp6.s6_addr), sizeof(struct in6_addr));
+	} else if (family == AF_INET && inet_pton(AF_INET, string, &tmp4)) {
+		sin4->sin_addr.s_addr = tmp4.s_addr;
 	} else {
-#ifdef HAVE_GETADDRINFO
-
 		memset(&hints, 0, sizeof(struct addrinfo));
-		hints.ai_family = AF_INET6;
+		hints.ai_family = family;
 #ifdef AI_V4MAPPED
 		hints.ai_flags = AI_V4MAPPED | AI_ADDRCONFIG;
 #else
@@ -43,21 +43,37 @@ int php_set_inet6_addr(struct sockaddr_in6 *sin6, char *string, php_socket *php_
 #endif
 			return 0;
 		}
-		if (addrinfo->ai_family != PF_INET6 || addrinfo->ai_addrlen != sizeof(struct sockaddr_in6)) {
-			php_error_docref(NULL, E_WARNING, "Host lookup failed: Non AF_INET6 domain returned on AF_INET6 socket");
+		if (addrinfo->ai_family != family) {
+			php_error_docref(NULL, E_WARNING, "Host lookup failed: Wrong address family returned for socket");
 			freeaddrinfo(addrinfo);
 			return 0;
 		}
 
-		memcpy(&(sin6->sin6_addr.s6_addr), ((struct sockaddr_in6*)(addrinfo->ai_addr))->sin6_addr.s6_addr, sizeof(struct in6_addr));
+		if (addrinfo->ai_family == AF_INET6) {
+			memcpy(&(sin6->sin6_addr.s6_addr), ((struct sockaddr_in6*)(addrinfo->ai_addr))->sin6_addr.s6_addr, sizeof(struct in6_addr));
+		} else if (addrinfo->ai_family == AF_INET) {
+			memcpy(&(sin4->sin_addr.s_addr), &((struct sockaddr_in*)(addrinfo->ai_addr))->sin_addr.s_addr, sizeof(struct in_addr));
+		}
 		freeaddrinfo(addrinfo);
+	}
 
+	return 1;
 #else
-		/* No IPv6 specific hostname resolution is available on this system? */
-		php_error_docref(NULL, E_WARNING, "Host lookup failed: getaddrinfo() not available on this system");
-		return 0;
+	php_error_docref(NULL, E_WARNING, "Host lookup failed: getaddrinfo() not available on this system");
+	return 0;
 #endif
+}
+/* }}} */
 
+#ifdef HAVE_IPV6
+/* Sets addr by hostname, or by ip in string form (AF_INET6) */
+int php_set_inet6_addr(struct sockaddr_in6 *sin6, char *string, php_socket *php_sock) /* {{{ */
+{
+	char *scope = strchr(string, '%');
+
+	int ret = php_set_common_addr((struct sockaddr*)sin6, AF_INET6, string, php_sock);
+	if (!ret) {
+		return 0;
 	}
 
 	if (scope) {
@@ -86,29 +102,7 @@ int php_set_inet6_addr(struct sockaddr_in6 *sin6, char *string, php_socket *php_
 /* Sets addr by hostname, or by ip in string form (AF_INET)  */
 int php_set_inet_addr(struct sockaddr_in *sin, char *string, php_socket *php_sock) /* {{{ */
 {
-	struct in_addr tmp;
-	struct hostent *host_entry;
-
-	if (inet_pton(AF_INET, string, &tmp)) {
-		sin->sin_addr.s_addr = tmp.s_addr;
-	} else {
-		if (strlen(string) > MAXFQDNLEN || ! (host_entry = php_network_gethostbyname(string))) {
-			/* Note: < -10000 indicates a host lookup error */
-#ifdef PHP_WIN32
-			PHP_SOCKET_ERROR(php_sock, "Host lookup failed", WSAGetLastError());
-#else
-			PHP_SOCKET_ERROR(php_sock, "Host lookup failed", (-10000 - h_errno));
-#endif
-			return 0;
-		}
-		if (host_entry->h_addrtype != AF_INET) {
-			php_error_docref(NULL, E_WARNING, "Host lookup failed: Non AF_INET domain returned on AF_INET socket");
-			return 0;
-		}
-		memcpy(&(sin->sin_addr.s_addr), host_entry->h_addr_list[0], host_entry->h_length);
-	}
-
-	return 1;
+	return php_set_common_addr((struct sockaddr*)sin, AF_INET, string, php_sock);
 }
 /* }}} */
 
