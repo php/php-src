4acfe22e8b201fd660b9510b95e1349c37188632
Saki Takamachi <saki@php.net>
2024-08-29 11:46:49 +0900

    Fixed and rename bcmath_number_isset_property

--- ext/bcmath/bcmath.c
+++ ext/bcmath/bcmath.c
@@ -908,8 +908,19 @@ static zval *bcmath_number_read_property(zend_object *obj, zend_string *name, in
 	return zend_std_read_property(obj, name, type, cache_slot, rv);
 }
 
-static int bcmath_number_isset_property(zend_object *obj, zend_string *name, int check_empty, void **cache_slot)
+static int bcmath_number_has_property(zend_object *obj, zend_string *name, int check_empty, void **cache_slot)
 {
+	if (check_empty == ZEND_PROPERTY_NOT_EMPTY) {
+		bcmath_number_obj_t *intern = get_bcmath_number_from_obj(obj);
+
+		if (zend_string_equals_literal(name, "value")) {
+			return !bc_is_zero(intern->num);
+		}
+
+		if (zend_string_equals_literal(name, "scale")) {
+			return intern->scale != 0;
+		}
+	}
 	return zend_string_equals_literal(name, "value") || zend_string_equals_literal(name, "scale");
 }
 
@@ -941,7 +952,7 @@ static void bcmath_number_register_class(void)
 	bcmath_number_obj_handlers.compare = bcmath_number_compare;
 	bcmath_number_obj_handlers.write_property = bcmath_number_write_property;
 	bcmath_number_obj_handlers.unset_property = bcmath_number_unset_property;
-	bcmath_number_obj_handlers.has_property = bcmath_number_isset_property;
+	bcmath_number_obj_handlers.has_property = bcmath_number_has_property;
 	bcmath_number_obj_handlers.read_property = bcmath_number_read_property;
 	bcmath_number_obj_handlers.get_properties_for = bcmath_number_get_properties_for;
 	bcmath_number_obj_handlers.cast_object = bcmath_number_cast_object;
