bf98023f1af4316250c32244a98e9d3d31a39ded
Dmitry Stogov <dmitry@zend.com>
2024-08-29 23:10:34 +0300

    Prevent possible incorrect optimization caused by ZEND_ASSUME()

--- ext/opcache/zend_shared_alloc.c
+++ ext/opcache/zend_shared_alloc.c
@@ -357,8 +357,6 @@ static size_t zend_shared_alloc_get_largest_free_block(void)
 
 void *zend_shared_alloc(size_t size)
 {
-	ZEND_ASSERT(ZCG(locked));
-
 	int i;
 	unsigned int block_size = ZEND_ALIGNED_SIZE(size);
 
@@ -368,6 +366,7 @@ void *zend_shared_alloc(size_t size)
 
 #if 1
 	if (!ZCG(locked)) {
+		ZEND_ASSERT(0 && "Shared memory lock not obtained");
 		zend_accel_error_noreturn(ACCEL_LOG_ERROR, "Shared memory lock not obtained");
 	}
 #endif
