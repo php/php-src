7fcb0a3d3d2875124b536b8307b40445c0f60f7d
Calvin Buckley <calvin@cmpct.info>
2024-09-10 13:07:00 -0300

    Update versions for PHP 8.4.0beta5
    Signed-off-by: Calvin Buckley <calvin@cmpct.info>

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 0
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.4.0-dev"
+#define PHP_EXTRA_VERSION "beta5"
+#define PHP_VERSION "8.4.0beta5"
 #define PHP_VERSION_ID 80400
