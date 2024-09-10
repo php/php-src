bb2b7df9dc31cbc66ac26757366c46b8455d22dd
Eric Mann <ericmann@php.net>
2024-09-10 06:38:48 -0700

    PHP-8.3 is now for PHP 8.3.13-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 12
+#define PHP_RELEASE_VERSION 13
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.12-dev"
-#define PHP_VERSION_ID 80312
+#define PHP_VERSION "8.3.13-dev"
+#define PHP_VERSION_ID 80313
