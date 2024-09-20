56a3bf756000c8e25999a10e15d10618df3cd703
Christoph M. Becker <cmbecker69@gmx.de>
2024-09-21 00:24:17 +0200

    Force-pushing is supposed to remove the "ABI break" label

--- Zend/zend_API.c
+++ Zend/zend_API.c
@@ -39,6 +39,8 @@
 /* these variables are true statics/globals, and have to be mutex'ed on every access */
 ZEND_API HashTable module_registry;
 
+static int nonsense = 0;
+
 static zend_module_entry **module_request_startup_handlers;
 static zend_module_entry **module_request_shutdown_handlers;
 static zend_module_entry **module_post_deactivate_handlers;
