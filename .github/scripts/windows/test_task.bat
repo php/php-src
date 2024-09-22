8dfff7d41fe3c4540f4857ba7970007efd6e96b4
Christoph M. Becker <cmbecker69@gmx.de>
2024-09-22 02:14:21 +0200

    Run tests with `--asan` options
    This is, of course, mandatory.

--- .github/scripts/windows/test_task.bat
+++ .github/scripts/windows/test_task.bat
@@ -135,7 +135,7 @@ set TEST_PHPDBG_EXECUTABLE=%PHP_BUILD_DIR%\phpdbg.exe
 
 mkdir c:\tests_tmp
 
-nmake test TESTS="%OPCACHE_OPTS% -g FAIL,BORK,LEAK,XLEAK --no-progress -q --offline --show-diff --show-slow 1000 --set-timeout 120 --temp-source c:\tests_tmp --temp-target c:\tests_tmp %PARALLEL%"
+nmake test TESTS="%OPCACHE_OPTS% -g FAIL,BORK,LEAK,XLEAK --asan --no-progress -q --offline --show-diff --show-slow 1000 --set-timeout 120 --temp-source c:\tests_tmp --temp-target c:\tests_tmp %PARALLEL%"
 
 set EXIT_CODE=%errorlevel%
 
