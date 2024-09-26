49099732f6690d1f44de2ef52a06da01713dcdec
Gina Peter Banyard <girgias@php.net>
2024-09-26 14:51:14 +0100

    ext/phar: Use HASH_FOREACH macro in phar_make_dirstream()
    Also simplify and refactor the inside of the loop

--- ext/phar/dirstream.c
+++ ext/phar/dirstream.c
@@ -135,22 +135,6 @@ static int phar_dir_flush(php_stream *stream) /* {{{ */
 /* }}} */
 
 /**
- * add an empty element with a char * key to a hash table, avoiding duplicates
- *
- * This is used to get a unique listing of virtual directories within a phar,
- * for iterating over opendir()ed phar directories.
- */
-static int phar_add_empty(HashTable *ht, char *arKey, uint32_t nKeyLength)  /* {{{ */
-{
-	zval dummy;
-
-	ZVAL_NULL(&dummy);
-	zend_hash_str_update(ht, arKey, nKeyLength, &dummy);
-	return SUCCESS;
-}
-/* }}} */
-
-/**
  * Used for sorting directories alphabetically
  */
 static int phar_compare_dir_name(Bucket *f, Bucket *s)  /* {{{ */
@@ -166,14 +150,11 @@ static int phar_compare_dir_name(Bucket *f, Bucket *s)  /* {{{ */
  * files in a phar and retrieving its relative path.  From this, construct
  * a list of files/directories that are "in" the directory represented by dir
  */
-static php_stream *phar_make_dirstream(char *dir, HashTable *manifest) /* {{{ */
+static php_stream *phar_make_dirstream(char *dir, const HashTable *manifest) /* {{{ */
 {
 	HashTable *data;
 	size_t dirlen = strlen(dir);
-	char *entry, *found, *save;
-	zend_string *str_key;
-	size_t keylen;
-	zend_ulong unused;
+	char *entry;
 
 	ALLOC_HASHTABLE(data);
 	zend_hash_init(data, 64, NULL, NULL, 0);
@@ -185,92 +166,78 @@ static php_stream *phar_make_dirstream(char *dir, HashTable *manifest) /* {{{ */
 		return php_stream_alloc(&phar_dir_ops, data, NULL, "r");
 	}
 
-	zend_hash_internal_pointer_reset(manifest);
-
-	while (FAILURE != zend_hash_has_more_elements(manifest)) {
-		if (HASH_KEY_NON_EXISTENT == zend_hash_get_current_key(manifest, &str_key, &unused)) {
-			break;
-		}
-
-		keylen = ZSTR_LEN(str_key);
+	zend_string *str_key;
+	ZEND_HASH_MAP_FOREACH_STR_KEY(manifest, str_key) {
+		size_t keylen = ZSTR_LEN(str_key);
 		if (keylen <= dirlen) {
 			if (keylen == 0 || keylen < dirlen || !strncmp(ZSTR_VAL(str_key), dir, dirlen)) {
-				if (SUCCESS != zend_hash_move_forward(manifest)) {
-					break;
-				}
 				continue;
 			}
 		}
 
 		if (*dir == '/') {
 			/* root directory */
-			if (keylen >= sizeof(".phar")-1 && !memcmp(ZSTR_VAL(str_key), ".phar", sizeof(".phar")-1)) {
+			if (zend_string_starts_with_literal(str_key, ".phar")) {
 				/* do not add any magic entries to this directory */
-				if (SUCCESS != zend_hash_move_forward(manifest)) {
-					break;
-				}
 				continue;
 			}
 
-			if (NULL != (found = (char *) memchr(ZSTR_VAL(str_key), '/', keylen))) {
+			const char *has_slash = memchr(ZSTR_VAL(str_key), '/', keylen);
+			if (has_slash) {
 				/* the entry has a path separator and is a subdirectory */
-				entry = (char *) safe_emalloc(found - ZSTR_VAL(str_key), 1, 1);
-				memcpy(entry, ZSTR_VAL(str_key), found - ZSTR_VAL(str_key));
-				keylen = found - ZSTR_VAL(str_key);
-				entry[keylen] = '\0';
-			} else {
-				entry = (char *) safe_emalloc(keylen, 1, 1);
-				memcpy(entry, ZSTR_VAL(str_key), keylen);
-				entry[keylen] = '\0';
+				keylen = has_slash - ZSTR_VAL(str_key);
 			}
+			entry = safe_emalloc(keylen, 1, 1);
+			memcpy(entry, ZSTR_VAL(str_key), keylen);
+			entry[keylen] = '\0';
 
 			goto PHAR_ADD_ENTRY;
 		} else {
 			if (0 != memcmp(ZSTR_VAL(str_key), dir, dirlen)) {
 				/* entry in directory not found */
-				if (SUCCESS != zend_hash_move_forward(manifest)) {
-					break;
-				}
 				continue;
 			} else {
 				if (ZSTR_VAL(str_key)[dirlen] != '/') {
-					if (SUCCESS != zend_hash_move_forward(manifest)) {
-						break;
-					}
 					continue;
 				}
 			}
 		}
 
-		save = ZSTR_VAL(str_key);
+		const char *save = ZSTR_VAL(str_key);
 		save += dirlen + 1; /* seek to just past the path separator */
 
-		if (NULL != (found = (char *) memchr(save, '/', keylen - dirlen - 1))) {
+		const char *has_slash = memchr(save, '/', keylen - dirlen - 1);
+		if (has_slash) {
 			/* is subdirectory */
 			save -= dirlen + 1;
-			entry = (char *) safe_emalloc(found - save + dirlen, 1, 1);
-			memcpy(entry, save + dirlen + 1, found - save - dirlen - 1);
-			keylen = found - save - dirlen - 1;
+			entry = safe_emalloc(has_slash - save + dirlen, 1, 1);
+			memcpy(entry, save + dirlen + 1, has_slash - save - dirlen - 1);
+			keylen = has_slash - save - dirlen - 1;
 			entry[keylen] = '\0';
 		} else {
 			/* is file */
 			save -= dirlen + 1;
-			entry = (char *) safe_emalloc(keylen - dirlen, 1, 1);
+			entry = safe_emalloc(keylen - dirlen, 1, 1);
 			memcpy(entry, save + dirlen + 1, keylen - dirlen - 1);
 			entry[keylen - dirlen - 1] = '\0';
 			keylen = keylen - dirlen - 1;
 		}
 PHAR_ADD_ENTRY:
 		if (keylen) {
-			phar_add_empty(data, entry, keylen);
+			/**
+			 * Add an empty element to avoid duplicates
+			 *
+			 * This is used to get a unique listing of virtual directories within a phar,
+			 * for iterating over opendir()ed phar directories.
+			 */
+			zval dummy;
+
+			ZVAL_NULL(&dummy);
+			zend_hash_str_update(data, entry, keylen, &dummy);
 		}
 
 		efree(entry);
-
-		if (SUCCESS != zend_hash_move_forward(manifest)) {
-			break;
-		}
-	}
+	} ZEND_HASH_FOREACH_END();
 
 	if (FAILURE != zend_hash_has_more_elements(data)) {
 		efree(dir);
