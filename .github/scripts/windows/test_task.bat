4d81fb96c2b3ecc68ee3b1fe1d1cf9d7db572a70
Christoph M. Becker <cmbecker69@gmx.de>
2024-09-30 00:38:52 +0200

    Re-introduce caching for Windows CI
    This had been done when running on AppVeyor, but had been dropped when
    Windows CI was migrated to Github.  While not terribly useful regarding
    the performance (maybe a minute can be saved), it still appears to be
    reasonable to avoid unnecessary requests to downloads.php.net.
    
    To have the branch available in the action, we make detection of the
    target branch a separate step.

--- .github/scripts/windows/test_task.bat
+++ .github/scripts/windows/test_task.bat
@@ -3,16 +3,12 @@ if /i "%GITHUB_ACTIONS%" neq "True" (
     exit /b 3
 )
 
+set BRANCH=%1
+
 set NO_INTERACTION=1
 set REPORT_EXIT_STATUS=1
 set SKIP_IO_CAPTURE_TESTS=1
 
-call %~dp0find-target-branch.bat
-if "%BRANCH%" neq "master" (
-	set STABILITY=stable
-) else (
-	set STABILITY=staging
-)
 set DEPS_DIR=%PHP_BUILD_CACHE_BASE_DIR%\deps-%BRANCH%-%PHP_SDK_VS%-%PHP_SDK_ARCH%
 if not exist "%DEPS_DIR%" (
 	echo "%DEPS_DIR%" doesn't exist
