754061caf6ec38ddd05f8fb9de4b789a05ffba88
Tim Düsterhus <tim@tideways-gmbh.com>
2024-09-30 08:59:47 +0200

    reflection: Fix the return value of ReflectionFunction::{getNamespaceName,inNamespace}() for closures
    Fixes GH-16122

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -3584,9 +3584,13 @@ ZEND_METHOD(ReflectionFunctionAbstract, inNamespace)
 
 	GET_REFLECTION_OBJECT_PTR(fptr);
 
-	zend_string *name = fptr->common.function_name;
-	const char *backslash = zend_memrchr(ZSTR_VAL(name), '\\', ZSTR_LEN(name));
-	RETURN_BOOL(backslash);
+	if ((fptr->common.fn_flags & (ZEND_ACC_CLOSURE | ZEND_ACC_FAKE_CLOSURE)) != ZEND_ACC_CLOSURE) {
+		zend_string *name = fptr->common.function_name;
+		const char *backslash = zend_memrchr(ZSTR_VAL(name), '\\', ZSTR_LEN(name));
+		RETURN_BOOL(backslash);
+	}
+
+	RETURN_FALSE;
 }
 /* }}} */
 
@@ -3602,11 +3606,14 @@ ZEND_METHOD(ReflectionFunctionAbstract, getNamespaceName)
 
 	GET_REFLECTION_OBJECT_PTR(fptr);
 
-	zend_string *name = fptr->common.function_name;
-	const char *backslash = zend_memrchr(ZSTR_VAL(name), '\\', ZSTR_LEN(name));
-	if (backslash) {
-		RETURN_STRINGL(ZSTR_VAL(name), backslash - ZSTR_VAL(name));
+	if ((fptr->common.fn_flags & (ZEND_ACC_CLOSURE | ZEND_ACC_FAKE_CLOSURE)) != ZEND_ACC_CLOSURE) {
+		zend_string *name = fptr->common.function_name;
+		const char *backslash = zend_memrchr(ZSTR_VAL(name), '\\', ZSTR_LEN(name));
+		if (backslash) {
+			RETURN_STRINGL(ZSTR_VAL(name), backslash - ZSTR_VAL(name));
+		}
 	}
+
 	RETURN_EMPTY_STRING();
 }
 /* }}} */
