312dea671ace3672797a1dc313d0b341c6929b20
Kasey Jenkins <KaseyJenkins@protonmail.com>
2024-10-17 10:27:49 +0200

    Making zend_hash_str_find_ptr_lc && zend_hash_find_ptr_lc C++ friendly

--- Zend/zend_hash.h
+++ Zend/zend_hash.h
@@ -899,6 +899,8 @@ static zend_always_inline void *zend_hash_str_find_ptr(const HashTable *ht, cons
 	}
 }
 
+BEGIN_EXTERN_C()
+
 /* Will lowercase the str; use only if you don't need the lowercased string for
  * anything else. If you have a lowered string, use zend_hash_str_find_ptr. */
 ZEND_API void *zend_hash_str_find_ptr_lc(const HashTable *ht, const char *str, size_t len);
@@ -906,6 +908,8 @@ ZEND_API void *zend_hash_str_find_ptr_lc(const HashTable *ht, const char *str, s
 /* Will lowercase the str; use only if you don't need the lowercased string for
  * anything else. If you have a lowered string, use zend_hash_find_ptr. */
 ZEND_API void *zend_hash_find_ptr_lc(const HashTable *ht, zend_string *key);
+
+END_EXTERN_C()
 
 static zend_always_inline void *zend_hash_index_find_ptr(const HashTable *ht, zend_ulong h)
 {
