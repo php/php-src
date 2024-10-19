77163913ba41aa73ae0d94a7fee585605e0f40e5
hanshenrik <hans@easy-ad.no>
2024-10-19 19:06:03 +0200

    asdfgh

--- ext/standard/array.c
+++ ext/standard/array.c
@@ -2876,12 +2876,12 @@ PHP_FUNCTION(array_fill_keys)
 #define RANGE_CHECK_LONG_INIT_ARRAY(start, end, _step) do { \
 		zend_ulong __calc_size = ((zend_ulong) start - end) / (_step); \
 		if (__calc_size >= HT_MAX_SIZE - 1) { \
-			zend_ulong __excess = __calc_size - (HT_MAX_SIZE - 1); \
+			uint64_t __excess = __calc_size - (HT_MAX_SIZE - 1); \
 			zend_value_error(\
-				"The supplied range exceeds the maximum array size by %lu elements: " \
+				"The supplied range exceeds the maximum array size by %" PRIu64 " elements: " \
 				"start=" ZEND_LONG_FMT ", end=" ZEND_LONG_FMT ", step=" ZEND_LONG_FMT ". " \
-				"Calculated size: " ZEND_LONG_FMT ", Maximum size: " ZEND_LONG_FMT ".", \
-				__excess, end, start, (_step), __calc_size + 1, (zend_ulong)HT_MAX_SIZE); \
+				"Calculated size: %" PRIu64 ", Maximum size: %" PRIu64 ".", \
+				__excess, end, start, (_step), (uint64_t)__calc_size, (uint64_t)HT_MAX_SIZE); \
 			RETURN_THROWS(); \
 		} \
 		size = (uint32_t)(__calc_size + 1); \
