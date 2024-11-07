620d641dbabb220aacfef0ec92b1385396623173
David Carlier <devnexen@gmail.com>
2024-11-07 18:39:37 +0000

    changes from feedback

--- Zend/zend_strtod.c
+++ Zend/zend_strtod.c
@@ -3614,20 +3614,18 @@ rv_alloc(int i)
 #endif
 {
 
-	int k, *r;
-	size_t j, rem;
+	int j, k, *r;
+	size_t rem;
 
 	rem = sizeof(Bigint) - sizeof(ULong) - sizeof(int);
 
 
 	j = sizeof(ULong);
-	if (i > (INT_MAX - rem))
+	if (i > ((INT_MAX >> 2) + rem))
 		zend_error_noreturn(E_ERROR, "rv_alloc() allocation overflow %d", i);
 	for(k = 0;
-		j <= (INT_MAX >> 1) && rem + j <= (size_t)i; j <<= 1)
+		rem + j <= (size_t)i; j <<= 1)
 			k++;
-	if (j > (INT_MAX >> 1))
-		zend_error_noreturn(E_ERROR, "rv_alloc() computation overflow " ZEND_LONG_FMT, j);
 
 	r = (int*)Balloc(k);
 	*r = k;
