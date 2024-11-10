6dec6a6dbaa14ef5ebd998f9ed75919c6dd83661
Christoph M. Becker <cmbecker69@gmx.de>
2024-11-10 21:58:50 +0100

    Add PHP_BUILD_DATE constant
    This information can be occasionally useful, and would otherwise need
    to be parsed from `phpinfo()` output.
    
    However, maybe more importantly we unify the build date between what is
    given by `php -v` and `php -i`, since these compilation units are not
    necessarily preprocessed within the same second.
    
    Closes GH-16747.

--- ext/standard/info.c
+++ ext/standard/info.c
@@ -801,7 +801,7 @@ PHPAPI ZEND_COLD void php_print_info(int flag)
 		php_info_print_box_end();
 		php_info_print_table_start();
 		php_info_print_table_row(2, "System", ZSTR_VAL(php_uname));
-		php_info_print_table_row(2, "Build Date", __DATE__ " " __TIME__);
+		php_info_print_table_row(2, "Build Date", php_build_date);
 #ifdef PHP_BUILD_SYSTEM
 		php_info_print_table_row(2, "Build System", PHP_BUILD_SYSTEM);
 #endif
