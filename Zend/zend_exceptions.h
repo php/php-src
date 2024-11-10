23b8d64cf7419f8049f90d81a8e8e3ef7d11f769
Gina Peter Banyard <girgias@php.net>
2024-11-10 21:44:30 +0000

    Zend: Minor refactorings to zend_exceptions() (#16684)

--- Zend/zend_exceptions.h
+++ Zend/zend_exceptions.h
@@ -72,7 +72,7 @@ extern ZEND_API void (*zend_throw_exception_hook)(zend_object *ex);
 /* show an exception using zend_error(severity,...), severity should be E_ERROR */
 ZEND_API ZEND_COLD zend_result zend_exception_error(zend_object *exception, int severity);
 ZEND_NORETURN void zend_exception_uncaught_error(const char *prefix, ...) ZEND_ATTRIBUTE_FORMAT(printf, 1, 2);
-ZEND_API zend_string *zend_trace_to_string(HashTable *trace, bool include_main);
+ZEND_API zend_string *zend_trace_to_string(const HashTable *trace, bool include_main);
 
 ZEND_API ZEND_COLD zend_object *zend_create_unwind_exit(void);
 ZEND_API ZEND_COLD zend_object *zend_create_graceful_exit(void);
