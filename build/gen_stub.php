php/php-src

commit 32789c00476e185daf4a24e82694164295a3b31f

[skip ci] gen_stub: Use simpara by default instead of para tags for documentation

build/gen_stub.php
@@ -1686,7 +1686,7 @@ class FuncInfo {
         $undocumentedEntity = $doc->createEntityReference('warn.undocumented.func');
         $descriptionRefSec->appendChild($undocumentedEntity);
         $descriptionRefSec->appendChild(new DOMText("\n  "));
-        $returnDescriptionPara = $doc->createElement('para');
+        $returnDescriptionPara = $doc->createElement('simpara');
         $returnDescriptionPara->appendChild(new DOMText("\n   Description.\n  "));
         $descriptionRefSec->appendChild($returnDescriptionPara);
 
@@ -1709,7 +1709,7 @@ class FuncInfo {
         $errorsDescriptionParaConstantTag->append('E_*');
         $errorsDescriptionParaExceptionTag = $doc->createElement('exceptionname');
         $errorsDescriptionParaExceptionTag->append('Exception');
-        $errorsDescriptionPara = $doc->createElement('para');
+        $errorsDescriptionPara = $doc->createElement('simpara');
         $errorsDescriptionPara->append(
             "\n   When does this function issue ",
             $errorsDescriptionParaConstantTag,
@@ -1813,7 +1813,7 @@ ENDCOMMENT
             $parametersRefSec->appendChild($noParamEntity);
             return $parametersRefSec;
         } else {
-            $parametersPara = $doc->createElement('para');
+            $parametersPara = $doc->createElement('simpara');
             $parametersRefSec->appendChild($parametersPara);
 
             $parametersPara->appendChild(new DOMText("\n   "));
@@ -1824,9 +1824,9 @@ ENDCOMMENT
             <varlistentry>
              <term><parameter>name</parameter></term>
              <listitem>
-              <para>
+              <simpara>
                Description.
-              </para>
+              </simpara>
              </listitem>
             </varlistentry>
             */
@@ -1835,7 +1835,7 @@ ENDCOMMENT
                 $parameterTerm = $doc->createElement('term');
                 $parameterTerm->appendChild($parameter);
 
-                $listItemPara = $doc->createElement('para');
+                $listItemPara = $doc->createElement('simpara');
                 $listItemPara->append(
                     "\n       ",
                     "Description.",
@@ -1871,7 +1871,7 @@ ENDCOMMENT
     private function getReturnValueSection(DOMDocument $doc): DOMElement {
         $returnRefSec = $this->generateRefSect1($doc, 'returnvalues');
 
-        $returnDescriptionPara = $doc->createElement('para');
+        $returnDescriptionPara = $doc->createElement('simpara');
         $returnDescriptionPara->appendChild(new DOMText("\n   "));
 
         $returnType = $this->return->getMethodSynopsisType();
@@ -2010,7 +2010,7 @@ ENDCOMMENT
 
         $example->append("\n   ", $title);
 
-        $para = $doc->createElement('para');
+        $para = $doc->createElement('simpara');
         $para->append("\n    ", "Description.", "\n   ");
         $example->append("\n   ", $para);
 

commit 7202d119cd1889bcbbe6280c3e2fb757499b71d3

Fix parameter list generation in build/gen_stub.php (#16764)
[ci skip]

<variablelist> is not valid within <simpara>, this should not be wrapped
at all in a paragraph.
Regressed in 32789c00476e185daf4a24e82694164295a3b31f.

build/gen_stub.php
@@ -1813,12 +1813,11 @@ ENDCOMMENT
             $parametersRefSec->appendChild($noParamEntity);
             return $parametersRefSec;
         } else {
-            $parametersPara = $doc->createElement('simpara');
-            $parametersRefSec->appendChild($parametersPara);
+            $parametersContainer = $doc->createDocumentFragment();
 
-            $parametersPara->appendChild(new DOMText("\n   "));
+            $parametersContainer->appendChild(new DOMText("\n  "));
             $parametersList = $doc->createElement('variablelist');
-            $parametersPara->appendChild($parametersList);
+            $parametersContainer->appendChild($parametersList);
 
             /*
             <varlistentry>
@@ -1837,33 +1836,34 @@ ENDCOMMENT
 
                 $listItemPara = $doc->createElement('simpara');
                 $listItemPara->append(
-                    "\n       ",
-                    "Description.",
                     "\n      ",
+                    "Description.",
+                    "\n     ",
                 );
 
                 $parameterEntryListItem = $doc->createElement('listitem');
                 $parameterEntryListItem->append(
-                    "\n      ",
-                    $listItemPara,
                     "\n     ",
+                    $listItemPara,
+                    "\n    ",
                 );
 
                 $parameterEntry = $doc->createElement('varlistentry');
                 $parameterEntry->append(
-                    "\n     ",
-                    $parameterTerm,
-                    "\n     ",
-                    $parameterEntryListItem,
                     "\n    ",
+                    $parameterTerm,
+                    "\n    ",
+                    $parameterEntryListItem,
+                    "\n   ",
                 );
 
-                $parametersList->appendChild(new DOMText("\n    "));
+                $parametersList->appendChild(new DOMText("\n   "));
                 $parametersList->appendChild($parameterEntry);
             }
-            $parametersList->appendChild(new DOMText("\n   "));
+            $parametersList->appendChild(new DOMText("\n  "));
         }
-        $parametersPara->appendChild(new DOMText("\n  "));
+        $parametersContainer->appendChild(new DOMText("\n "));
+        $parametersRefSec->appendChild($parametersContainer);
         $parametersRefSec->appendChild(new DOMText("\n "));
         return $parametersRefSec;
     }