27cf6648026fccb250bd240e59ccae3a2a0c9d75
Christoph M. Becker <cmbecker69@gmx.de>
2024-11-18 23:34:41 +0100

    Fix condition
    See https://github.com/php/php-src/pull/16801#discussion_r1843652081.

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -3628,8 +3628,7 @@ function ADD_MAKEFILE_FRAGMENT(src_file)
 
 function SETUP_ZLIB_LIB(target, path_to_check)
 {
-	return ((PHP_ZLIB == "no" || PHP_ZLIB_SHARED) && CHECK_LIB("zlib_a.lib;zlib.lib", target, path_to_check)) ||
-		(PHP_ZLIB == "yes" && !PHP_ZLIB_SHARED);
+	return (PHP_ZLIB != "no" && !PHP_ZLIB_SHARED) || CHECK_LIB("zlib_a.lib;zlib.lib", target, path_to_check);
 }
 
 function SETUP_OPENSSL(target, path_to_check, common_name, use_env, add_dir_part, add_to_flag_only)
