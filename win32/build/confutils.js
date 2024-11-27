7e1d035077d51bd22d3c49ab453f05a2ff23abad
Christoph M. Becker <cmbecker69@gmx.de>
2024-11-27 18:35:34 +0100

    Factor out SETUP_ZLIB_LIB() (GH-16798)
    Note that this function is similar to `SETUP_OPENSSL`, but since the
    zlib headers are not necessarily required, we append `_LIB`.
    
    We are also more liberal regarding zlib(_a).lib, because
    extensions requiring zlib are looking only for zlib.lib if ext/zlib has
    been built as shared extension.  This is overly restrictive at best,
    and actually makes no sense, since (a) we're not shipping shared zlib
    builds for years, and (b) users could have a zlib.lib which is a static
    build (they could even just rename zlib_a.lib to zlib.lib).

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -3626,6 +3626,11 @@ function ADD_MAKEFILE_FRAGMENT(src_file)
 	}
 }
 
+function SETUP_ZLIB_LIB(target, path_to_check)
+{
+	return (PHP_ZLIB != "no" && !PHP_ZLIB_SHARED) || CHECK_LIB("zlib_a.lib;zlib.lib", target, path_to_check);
+}
+
 function SETUP_OPENSSL(target, path_to_check, common_name, use_env, add_dir_part, add_to_flag_only)
 {
 	var ret = 0;
