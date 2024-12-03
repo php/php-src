5d7719bf436ca49e0d5f320a114849f54177b51e
Calvin Buckley <calvin@cmpct.info>
2024-12-03 11:13:30 -0400

    Update versions for PHP 8.4.2RC1

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 4
 #define PHP_RELEASE_VERSION 2
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.4.2-dev"
+#define PHP_EXTRA_VERSION "RC1"
+#define PHP_VERSION "8.4.2RC1"
 #define PHP_VERSION_ID 80402
