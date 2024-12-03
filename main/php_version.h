b1e3dcf88a2251fbc3665905560e27dd9539b53f
Jakub Zelenka <bukka@php.net>
2024-12-03 18:45:43 +0100

    PHP-8.3 is now for PHP 8.3.16-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 3
-#define PHP_RELEASE_VERSION 15
+#define PHP_RELEASE_VERSION 16
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.3.15-dev"
-#define PHP_VERSION_ID 80315
+#define PHP_VERSION "8.3.16-dev"
+#define PHP_VERSION_ID 80316
