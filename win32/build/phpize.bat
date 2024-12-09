8e169f924f9cc737a4e16291861d10a4446ad66f
Christoph M. Becker <cmbecker69@gmx.de>
2024-12-09 20:44:02 +0100

    Fail phpize early if config.w32 is missing
    On Windows, phpize happily builds configure even if there is no
    config.w32, but running configure then error with "Must be run from the
    root of the extension source".  This is confusing.
    
    We bring phpize's behavior on par with POSIX systems, where the missing
    config.m4 is detected and reported right away.

--- win32/build/phpize.bat
+++ win32/build/phpize.bat
@@ -1,6 +1,7 @@
 @echo off
 SET PHP_BUILDCONF_PATH=%~dp0
 cscript /nologo /e:jscript %PHP_BUILDCONF_PATH%\script\phpize.js %*
+IF ERRORLEVEL 0 exit /b 3
 IF NOT EXIST configure.bat (
 	echo Error generating configure script, configure script was not copied
 	exit /b 3
