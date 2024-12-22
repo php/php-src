68f1098ef3c867ea1124e4847788c990a6dba227
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2024-12-22 21:34:32 +0100

    Fix memory leak on ZEND_FFI_TYPE_CHAR conversion failure
    The success path frees tmp_str, but the error path does not.

--- ext/ffi/ffi.c
+++ ext/ffi/ffi.c
@@ -807,6 +807,7 @@ again:
 			if (ZSTR_LEN(str) == 1) {
 				*(char*)ptr = ZSTR_VAL(str)[0];
 			} else {
+				zend_tmp_string_release(tmp_str);
 				zend_ffi_assign_incompatible(value, type);
 				return FAILURE;
 			}
