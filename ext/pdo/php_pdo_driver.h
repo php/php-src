d917497f94924f9145819bcba9795e421fd6e3df
Gina Peter Banyard <girgias@php.net>
2025-02-04 11:47:01 +0000

    ext/pdo: Add const modifiers to pdo_get_TYPE_param() functions

--- ext/pdo/php_pdo_driver.h
+++ ext/pdo/php_pdo_driver.h
@@ -698,8 +698,8 @@ PDO_API void php_pdo_stmt_set_column_count(pdo_stmt_t *stmt, int new_count);
 PDO_API void php_pdo_internal_construct_driver(INTERNAL_FUNCTION_PARAMETERS, zend_object *current_object, zend_class_entry *called_scope, zval *new_zval_object);
 
 /* Normalization for fetching long param for driver attributes */
-PDO_API bool pdo_get_long_param(zend_long *lval, zval *value);
-PDO_API bool pdo_get_bool_param(bool *bval, zval *value);
+PDO_API bool pdo_get_long_param(zend_long *lval, const zval *value);
+PDO_API bool pdo_get_bool_param(bool *bval, const zval *value);
 
 PDO_API void pdo_throw_exception(unsigned int driver_errcode, char *driver_errmsg, pdo_error_type *pdo_error);
 
