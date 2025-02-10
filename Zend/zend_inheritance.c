9369f53c1021a511cd50c9f703eef48f1e35ec5c
rekmixa <rekmixa@gmail.com>
2025-02-10 23:35:42 +0500

    #17725 add additional check for mask MAY_BE_STATIC

--- Zend/zend_inheritance.c
+++ Zend/zend_inheritance.c
@@ -572,7 +572,8 @@ static inheritance_status zend_is_class_subtype_of_type(
 	}
 
 	// replacing static with self in final classes is okay
-	if (!is_intersection && fe_scope->ce_flags & ZEND_ACC_FINAL && instanceof_function(fe_scope, proto_scope)) {
+	if (!is_intersection && fe_scope->ce_flags & ZEND_ACC_FINAL &&
+		ZEND_TYPE_FULL_MASK(proto_type) & MAY_BE_STATIC && instanceof_function(fe_scope, proto_scope)) {
 		if (!fe_ce) fe_ce = lookup_class(fe_scope, fe_class_name);
 
 		if (fe_ce && instanceof_function(fe_ce, fe_scope)) {
