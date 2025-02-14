de36608113de1730f2a163a1e170a53d577f6f8e
Džuris <juris@glaive.pro>
2025-02-14 23:20:40 +0200

    Fix mask

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -1034,7 +1034,7 @@ ZEND_API zend_string *zend_type_to_string(zend_type type);
 #define ZEND_NAME_FQ       0
 #define ZEND_NAME_NOT_FQ   1
 #define ZEND_NAME_RELATIVE 2
-#define ZEND_NAME_QUALIFIED_MASK 0x0011
+#define ZEND_NAME_QUALIFIED_MASK 0x03
 #define NAME_QUAL(type) (type & ZEND_NAME_QUALIFIED_MASK)
 
 #define ZEND_NAME_OPTIONAL 4
