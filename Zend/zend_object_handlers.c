7aba100d1b72766220253cc89d98b8ef04c56bde
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2025-02-24 20:35:23 +0100

    Add comment

--- Zend/zend_object_handlers.c
+++ Zend/zend_object_handlers.c
@@ -1618,6 +1618,7 @@ ZEND_API zend_function *zend_get_call_trampoline_func(const zend_class_entry *ce
 		| ZEND_ACC_PUBLIC
 		| ZEND_ACC_VARIADIC
 		| (fbc->common.fn_flags & (ZEND_ACC_RETURN_REFERENCE|ZEND_ACC_ABSTRACT|ZEND_ACC_DEPRECATED));
+	/* Attributes outlive the trampoline because they are created by the compiler. */
 	func->attributes = fbc->common.attributes;
 	if (is_static) {
 		func->fn_flags |= ZEND_ACC_STATIC;
