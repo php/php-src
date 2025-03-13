ae67eb049035dca26740d85ab34dc873e86502d7
Dmitry Stogov <dmitry@zend.com>
2025-03-13 03:07:43 +0300

    Update IR
    IR commit: 8bb0acca45a7b0f12691f4258e41462599efbd74

--- ext/opcache/jit/ir/ir_gcm.c
+++ ext/opcache/jit/ir/ir_gcm.c
@@ -413,10 +413,11 @@ static bool ir_split_partially_dead_node(ir_ctx *ctx, ir_ref ref, uint32_t b)
 	n = ctx->use_lists[ref].refs;
 	for (i = 0; i < clones_count; i++) {
 		clone = clones[i].ref;
-		if (clones[i].use_count == 1) {
+		if (clones[i].use_count == 1
+		 && ctx->cfg_blocks[clones[i].block].loop_depth >= ctx->cfg_blocks[uses[clones[i].use].block].loop_depth) {
 			/* TOTALLY_USEFUL block may be a head of a diamond above the real usage.
 			 * Sink it down to the real usage block.
-			 * Clones with few uses we be sunk into the LCA block.
+			 * Clones with few uses will be sunk into the LCA block.
 			 */
 			clones[i].block = uses[clones[i].use].block;
 		}
