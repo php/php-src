5471a5a9582b06d28439f5136d21c2c4fc625c4a
Emmanuel Dreyfus <manu@netbsd.org>
2025-04-06 02:25:06 +0200

    Another attempt to addresse the performance when the system clock misbehaves, while keeping the documented behavior of returning a time based value.
    Instead of looping on gettimeofday until the system time changes,
    call it once, and if time has not changed, return the previous
    value +1us.

--- ext/standard/uniqid.c
+++ ext/standard/uniqid.c
@@ -23,25 +23,20 @@
 
 #include <string.h>
 #include <errno.h>
-#include <stdio.h>
 
-#ifdef HAVE_UUIDGEN
-#include <sys/types.h>
-#include <sys/uuid.h>
-#else
+#include <stdio.h>
 #ifdef PHP_WIN32
 #include "win32/time.h"
 #else
 #include <sys/time.h>
 #endif
 
-ZEND_TLS struct timeval prev_tv = { 0, 0 };
-#endif
-
 #include "ext/random/php_random.h"
 #include "ext/random/php_random_csprng.h"
 
-#if defined(HAVE_UUIDGEN) || defined(HAVE_GETTIMEOFDAY)
+#ifdef HAVE_GETTIMEOFDAY
+ZEND_TLS struct timeval prev_tv = { 0, 0 };
+
 /* {{{ Generates a unique ID */
 PHP_FUNCTION(uniqid)
 {
@@ -50,12 +45,7 @@ PHP_FUNCTION(uniqid)
 	zend_string *uniqid;
 	int sec, usec;
 	size_t prefix_len = 0;
-#ifdef HAVE_UUIDGEN
-	struct uuid uuid;
-	int *n = (int *)&uuid;
-#else
 	struct timeval tv;
-#endif
 
 	ZEND_PARSE_PARAMETERS_START(0, 2)
 		Z_PARAM_OPTIONAL
@@ -63,27 +53,29 @@ PHP_FUNCTION(uniqid)
 		Z_PARAM_BOOL(more_entropy)
 	ZEND_PARSE_PARAMETERS_END();
 
-#ifdef HAVE_UUIDGEN
-	(void)uuidgen(&uuid, 1);
-
-	sec = n[0];
-	usec = n[1] % 0x100000;
-#else
 	/* This implementation needs current microsecond to change,
-	 * hence we poll time until it does. This is much faster than
-	 * calling usleep(1) which may cause the kernel to schedule
-	 * another process, causing a pause of around 10ms.
+	 * If system clock lags and report the same time again,
+	 * we just return the previous value +1us.
+	 * This is much faster than calling usleep(1) which may 
+	 * cause the kernel to schedule another process, causing 
+	 * a pause of around 10ms.
 	 */
-	do {
-		(void)gettimeofday((struct timeval *) &tv, (struct timezone *) NULL);
-	} while (tv.tv_sec == prev_tv.tv_sec && tv.tv_usec == prev_tv.tv_usec);
+	(void)gettimeofday((struct timeval *) &tv, (struct timezone *) NULL);
+	if (tv.tv_sec <= prev_tv.tv_sec ||
+            (tv.tv_sec == prev_tv.tv_sec && tv.tv_usec <= prev_tv.tv_usec)) {
+		tv.tv_sec = prev_tv.tv_sec;
+		tv.tv_usec = prev_tv.tv_usec + 1;
+		if (tv.tv_usec >= 1000000) {
+			tv.tv_sec++;
+			tv.tv_usec -= 1000000;
+		}
+	}
 
 	prev_tv.tv_sec = tv.tv_sec;
 	prev_tv.tv_usec = tv.tv_usec;
 
 	sec = (int) tv.tv_sec;
 	usec = (int) (tv.tv_usec % 0x100000);
-#endif
 
 	/* The max value usec can have is 0xF423F, so we use only five hex
 	 * digits for usecs.
@@ -102,5 +94,5 @@ PHP_FUNCTION(uniqid)
 
 	RETURN_STR(uniqid);
 }
-/* }}} */
 #endif
+/* }}} */
