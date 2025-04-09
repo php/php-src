fc0446c4cc24dd1f90d54f71986298c9c4f83603
Tim Düsterhus <tim@tideways-gmbh.com>
2025-04-09 15:53:47 +0200

    Add `clone()` function

--- build/gen_stub.php
+++ build/gen_stub.php
@@ -1009,6 +1009,9 @@ class FunctionName implements FunctionOrMethodName {
     private /* readonly */ Name $name;
 
     public function __construct(Name $name) {
+        if ($name->name === '_clone') {
+            $name = new Name('clone', $name->getAttributes());
+        }
         $this->name = $name;
     }
 
