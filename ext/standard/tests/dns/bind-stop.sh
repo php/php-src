8a604f6de24692ffb48a58e08da46eeacdd9b14b
Jakub Zelenka <bukka@php.net>
2025-04-11 20:04:03 +0200

    Add DNS testing setup using BIND 9
    This creates a test frameword for testing PHP DNS functions that are
    currently completely untested. It provides script for starting BIND 9
    DNS server that uses locally defined zone files.  Those can be then used
    for creating specific test scenarios without a need to use online tests.
    As part of that, the systemd resolv script is provided to use BIND as a
    primary resolver. This all is then used in the CI.

---
+++ ext/standard/tests/dns/bind-stop.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+# Resolve script location
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ZONES_DIR="$SCRIPT_DIR/zones"
+PID_FILE="$ZONES_DIR/named.pid"
+
+if [ -f "$PID_FILE" ]; then
+    NAMED_PID=$(cat $PID_FILE)
+    if [ -n "$NAMED_PID" ]; then
+        echo "Stopping BIND running on pid $NAMED_PID"
+        kill $NAMED_PID
+    else
+        echo "BIND pid is empty"
+    fi
+else
+    echo "BIND is not running"
+fi
