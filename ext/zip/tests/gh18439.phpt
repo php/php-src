php/php-src

commit 8765e9f5e74651b582cd3de1218135a72dd4eea8

ext/zip: Throw a TypeError if the return value of the cancel callback is not an int

ext/zip/php_zip.c
@@ -3066,7 +3066,6 @@ PHP_METHOD(ZipArchive, registerProgressCallback)
 static int php_zip_cancel_callback(zip_t *arch, void *ptr)
 {
 	zval cb_retval;
-	int retval = 0;
 	ze_zip_object *obj = ptr;
 
 	zend_call_known_fcc(&obj->cancel_callback, &cb_retval, 0, NULL, NULL);
@@ -3074,10 +3073,17 @@ static int php_zip_cancel_callback(zip_t *arch, void *ptr)
 		/* Cancel if an exception has been thrown */
 		return -1;
 	}
-	retval = zval_get_long(&cb_retval);
+	bool failed = false;
+	zend_long retval = zval_try_get_long(&cb_retval, &failed);
+	if (failed) {
+		zend_type_error("Return value of callback provided to ZipArchive::registerCancelCallback()"
+			" must be of type int, %s returned", zend_zval_value_name(&cb_retval));
+		zval_ptr_dtor(&cb_retval);
+		return -1;
+	}
 	zval_ptr_dtor(&cb_retval);
 
-	return retval;
+	return (int) retval;
 }
 
 /* {{{ register a progression callback: int callback(double state); */

ext/zip/tests/oo_cancel_non_int_return.phpt
@@ -24,8 +24,11 @@ var_dump($zip->registerCancelCallback(function () {
     return [];
 }));
 var_dump($zip->addFromString(PHP_BINARY, 'entry #1'));
-
-var_dump($zip->close());
+try {
+    var_dump($zip->close());
+} catch (Throwable $e) {
+    echo $e::class, ': ', $e->getMessage(), PHP_EOL;
+}
 var_dump($zip->status == ZipArchive::ER_CANCELLED);
 var_dump($zip->getStatusString());
 @unlink($file);
@@ -38,10 +41,12 @@ $file = $dirname . '__tmp_oo_cancel_incorrect_return.zip';
 
 @unlink($file);
 ?>
---EXPECT--
+--EXPECTF--
 bool(true)
 bool(true)
+
+Warning: ZipArchive::close(): Operation cancelled in %s on line %d
+TypeError: Return value of callback provided to ZipArchive::registerCancelCallback() must be of type int, array returned
 bool(true)
-bool(false)
-string(8) "No error"
+string(19) "Operation cancelled"
 Done

commit 94d73de143e7a8297e25af7423fc0fdb27e1e808

Fix reference handling in cancel callback
Broke in 8765e9f5e74651b582cd3de1218135a72dd4eea8

ext/zip/php_zip.c
@@ -3073,10 +3073,12 @@ static int php_zip_cancel_callback(zip_t *arch, void *ptr)
 		return -1;
 	}
 	bool failed = false;
-	zend_long retval = zval_try_get_long(&cb_retval, &failed);
+	zval *cb_retval_ptr = &cb_retval;
+	ZVAL_DEREF(cb_retval_ptr);
+	zend_long retval = zval_try_get_long(cb_retval_ptr, &failed);
 	if (failed) {
 		zend_type_error("Return value of callback provided to ZipArchive::registerCancelCallback()"
-			" must be of type int, %s returned", zend_zval_value_name(&cb_retval));
+			" must be of type int, %s returned", zend_zval_value_name(cb_retval_ptr));
 		zval_ptr_dtor(&cb_retval);
 		return -1;
 	}

ext/zip/tests/gh18439.phpt
@@ -1,0 +1,27 @@
+--TEST--
+GH-18439 (Reference handling in cancel callback)
+--EXTENSIONS--
+zip
+--FILE--
+<?php
+
+function &cb() {
+    $test = false;
+    return $test;
+}
+
+$file = __DIR__ . '/gh18439.zip';
+$zip = new ZipArchive;
+$zip->open($file, ZIPARCHIVE::CREATE);
+$zip->registerCancelCallback(cb(...));
+$zip->addFromString('test', 'test');
+echo "Done\n";
+
+?>
+--CLEAN--
+<?php
+$file = __DIR__ . '/gh18439.zip';
+@unlink($file);
+?>
+--EXPECT--
+Done