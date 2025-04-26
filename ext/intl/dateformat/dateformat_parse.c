8ece18aef8a3357796ec6b350a5084d49935fd20
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2025-04-27 00:29:12 +0200

    datefmt_parse/datefmt_localtime references type system fixes

--- ext/intl/dateformat/dateformat_parse.c
+++ ext/intl/dateformat/dateformat_parse.c
@@ -136,9 +136,9 @@ PHP_FUNCTION(datefmt_parse)
 	DATE_FORMAT_METHOD_FETCH_OBJECT;
 
 	if (z_parse_pos) {
-		zend_long long_parse_pos;
-		ZVAL_DEREF(z_parse_pos);
-		long_parse_pos = zval_get_long(z_parse_pos);
+		zval *z_parse_pos_tmp = z_parse_pos;
+		ZVAL_DEREF(z_parse_pos_tmp);
+		zend_long long_parse_pos = zval_get_long(z_parse_pos_tmp);
 		if (ZEND_LONG_INT_OVFL(long_parse_pos)) {
 			intl_error_set_code(NULL, U_ILLEGAL_ARGUMENT_ERROR);
 			intl_error_set_custom_msg(NULL, "String index is out of valid range.", 0);
@@ -151,8 +151,7 @@ PHP_FUNCTION(datefmt_parse)
 	}
 	internal_parse_to_timestamp( dfo, text_to_parse, text_len, z_parse_pos?&parse_pos:NULL, return_value);
 	if(z_parse_pos) {
-		zval_ptr_dtor(z_parse_pos);
-		ZVAL_LONG(z_parse_pos, parse_pos);
+		ZEND_TRY_ASSIGN_REF_LONG(z_parse_pos, parse_pos);
 	}
 }
 /* }}} */
@@ -177,9 +176,9 @@ PHP_FUNCTION(datefmt_localtime)
 	DATE_FORMAT_METHOD_FETCH_OBJECT;
 
 	if (z_parse_pos) {
-		zend_long long_parse_pos;
-		ZVAL_DEREF(z_parse_pos);
-		long_parse_pos = zval_get_long(z_parse_pos);
+		zval *z_parse_pos_tmp = z_parse_pos;
+		ZVAL_DEREF(z_parse_pos_tmp);
+		zend_long long_parse_pos = zval_get_long(z_parse_pos_tmp);
 		if (ZEND_LONG_INT_OVFL(long_parse_pos)) {
 			intl_error_set_code(NULL, U_ILLEGAL_ARGUMENT_ERROR);
 			intl_error_set_custom_msg(NULL, "String index is out of valid range.", 0);
@@ -192,8 +191,7 @@ PHP_FUNCTION(datefmt_localtime)
 	}
 	internal_parse_to_localtime( dfo, text_to_parse, text_len, z_parse_pos?&parse_pos:NULL, return_value);
 	if (z_parse_pos) {
-		zval_ptr_dtor(z_parse_pos);
-		ZVAL_LONG(z_parse_pos, parse_pos);
+		ZEND_TRY_ASSIGN_REF_LONG(z_parse_pos, parse_pos);
 	}
 }
 /* }}} */
