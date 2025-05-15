92a45c7cf2e8ec2a9570f4f623cc167b309ac2fc
Gina Peter Banyard <girgias@php.net>
2025-05-15 16:42:32 +0200

    Add bound resolved type to string for error messages

--- Zend/zend_compile.h
+++ Zend/zend_compile.h
@@ -1014,7 +1014,7 @@ int ZEND_FASTCALL zendlex(zend_parser_stack_elem *elem);
 
 void zend_assert_valid_class_name(const zend_string *const_name, const char *type);
 
-zend_string *zend_type_to_string_resolved(zend_type type, zend_class_entry *scope);
+zend_string *zend_type_to_string_resolved(zend_type type, zend_class_entry *scope, const HashTable *bound_types_to_scope);
 ZEND_API zend_string *zend_type_to_string(zend_type type);
 
 /* BEGIN: OPCODES */
