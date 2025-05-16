578cdf1f98f906e99aa7180535105f756c256de8
Levi Morrison <levi.morrison@datadoghq.com>
2025-05-16 15:21:19 -0600

    fix: dangling opline in ZEND_INIT_ARRAY
    This causes problems if an allocation profiler decides to walk the
    stack, or if the engine itself OOMs on this opcode, and it tries to
    print file and line information.

--- Zend/zend_vm_def.h
+++ Zend/zend_vm_def.h
@@ -6281,6 +6281,7 @@ ZEND_VM_HANDLER(71, ZEND_INIT_ARRAY, CONST|TMP|VAR|CV|UNUSED, CONST|TMPVAR|UNUSE
 	uint32_t size;
 	USE_OPLINE
 
+	SAVE_OPLINE();
 	array = EX_VAR(opline->result.var);
 	if (OP1_TYPE != IS_UNUSED) {
 		size = opline->extended_value >> ZEND_ARRAY_SIZE_SHIFT;
