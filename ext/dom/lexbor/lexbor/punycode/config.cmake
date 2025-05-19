dbcf895f592127d5e3f4f26204029ce07dcea689
Máté Kocsis <kocsismate@woohoolabs.com>
2025-05-19 21:45:00 +0200

    Proper build support

--- ext/dom/lexbor/lexbor/punycode/config.cmake
+++
@@ -1,3 +0,0 @@
-set(DEPENDENCIES "core encoding")
-set(DESCRIPTION "The module implemented by PUNYCODE specification.
-Specification: https://www.rfc-editor.org/rfc/inline-errata/rfc3492.html")
--- ext/dom/lexbor/lexbor/url/config.cmake
+++
@@ -1,4 +0,0 @@
-set(DEPENDENCIES "core encoding unicode punycode")
-set(DESCRIPTION "The module implemented by WHATWG URL specification.
-Includes functions for URL parsing and serialization.
-Specification: https://url.spec.whatwg.org/")
