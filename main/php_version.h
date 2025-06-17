a6749046f6c5fd043919cb9090452165542ab612
Saki Takamachi <saki@php.net>
2025-06-17 21:05:02 +0900

    PHP-8.4 is now for PHP 8.4.10-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 4
-#define PHP_RELEASE_VERSION 9
+#define PHP_RELEASE_VERSION 10
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.4.9-dev"
-#define PHP_VERSION_ID 80409
+#define PHP_VERSION "8.4.10-dev"
+#define PHP_VERSION_ID 80410
