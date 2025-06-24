73b1ebfa203e7c391fcccc5171e1d800e17f013c
Arnaud Le Blanc <arnaud.lb@gmail.com>
2025-06-24 11:29:23 +0200

    Fix linker failure when building Opcache statically
    We use linker relocations to fetch the TLS index and offset of _tsrm_ls_cache.
    When building Opcache statically, linkers may attempt to optimize that into a
    more efficient code sequence (relaxing from "General Dynamic" to "Local Exec"
    model [1]). Unfortunately, linkers will fail, rather than ignore our
    relocations, when they don't recognize the exact code sequence they are
    expecting.
    
    This results in errors as reported by GH-15074:
    
        TLS transition from R_X86_64_TLSGD to R_X86_64_GOTTPOFF against
        `_tsrm_ls_cache' at 0x12fc3 in section `.text' failed"
    
    Here I take a different approach:
    
     * Emit the exact full code sequence expected by linkers
     * Extract the TLS index/offset by inspecting the linked ASM code, rather than
       executing it (execution would give us the thread-local address).
     * We detect when the code was relaxed, in which case we can extract the TCB
       offset instead.
     * This is done in a conservative way so that if the linker did something we
       didn't expect, we fallback to a safer (but slower) mechanism.
    
    One additional benefit of that is we are now able to use the Local Exec model in
    more cases, in JIT'ed code. This makes non-glibc builds faster in these cases.
    
    Closes GH-18939.
    
    Related RFC: https://wiki.php.net/rfc/make_opcache_required.
    
    [1] https://www.akkadia.org/drepper/tls.pdf

---
+++ ext/opcache/jit/tls/zend_jit_tls.h
@@ -0,0 +1,40 @@
+/*
+ *  +----------------------------------------------------------------------+
+ *  | Zend JIT                                                             |
+ *  +----------------------------------------------------------------------+
+ *  | Copyright (c) The PHP Group                                          |
+ *  +----------------------------------------------------------------------+
+ *  | This source file is subject to version 3.01 of the PHP license,      |
+ *  | that is bundled with this package in the file LICENSE, and is        |
+ *  | available through the world-wide-web at the following url:           |
+ *  | https://www.php.net/license/3_01.txt                                 |
+ *  | If you did not receive a copy of the PHP license and are unable to   |
+ *  | obtain it through the world-wide-web, please send a note to          |
+ *  | license@php.net so we can mail you a copy immediately.               |
+ *  +----------------------------------------------------------------------+
+ *  | Authors: Arnaud Le Blanc <arnaud.lb@gmail.com>                       |
+ *  +----------------------------------------------------------------------+
+ */
+
+#ifndef ZEND_JIT_TLS_H
+#define ZEND_JIT_TLS_H
+
+#include "Zend/zend_types.h"
+
+#include <stdint.h>
+#include <stddef.h>
+
+zend_result zend_jit_resolve_tsrm_ls_cache_offsets(
+	size_t *tcb_offset,
+	size_t *module_index,
+	size_t *module_offset
+);
+
+/* Used for testing */
+void *zend_jit_tsrm_ls_cache_address(
+	size_t tcb_offset,
+	size_t module_index,
+	size_t module_offset
+);
+
+#endif /* ZEND_JIT_TLS_H */
