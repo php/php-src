193beb5419b42df8748ed88ea91b138cad1a75c0
Ben Ramsey <ramsey@php.net>
2025-06-28 12:00:19 -0500

    license-update: ext/calendar source headers
    RFC: https://wiki.php.net/rfc/php_license_update

--- ext/calendar/php_calendar.h
+++ ext/calendar/php_calendar.h
@@ -1,3 +1,15 @@
+/*
+   +----------------------------------------------------------------------+
+   | Copyright © The PHP Group and Contributors.                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to the Modified BSD License that is      |
+   | bundled with this package in the file LICENSE, and is available      |
+   | through the World Wide Web at <https://www.php.net/license/>.        |
+   |                                                                      |
+   | SPDX-License-Identifier: BSD-3-Clause                                |
+   +----------------------------------------------------------------------+
+ */
+
 #ifndef PHP_CALENDAR_H
 #define PHP_CALENDAR_H
 
