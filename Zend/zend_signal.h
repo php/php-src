812406fd69cba1b510c8c18dc1cb0460167e78fd
Ørjan Malde <red@foxi.me>
2025-07-03 11:54:13 +0200

    Fix visibility on zend_signal_{deactivate,activate,init}

--- Zend/zend_signal.h
+++ Zend/zend_signal.h
@@ -86,12 +86,12 @@ END_EXTERN_C()
 # endif /* not ZTS */
 
 ZEND_API void zend_signal_handler_unblock(void);
-void zend_signal_activate(void);
-void zend_signal_deactivate(void);
+ZEND_API void zend_signal_activate(void);
+ZEND_API void zend_signal_deactivate(void);
 BEGIN_EXTERN_C()
 ZEND_API void zend_signal_startup(void);
 END_EXTERN_C()
-void zend_signal_init(void);
+ZEND_API void zend_signal_init(void);
 
 ZEND_API void zend_signal(int signo, void (*handler)(int));
 ZEND_API void zend_sigaction(int signo, const struct sigaction *act, struct sigaction *oldact);
