a32b9c773223a58d623ae517663d7bfca9031143
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2025-07-17 23:24:22 +0300

    % remove zval_call_destructor

--- Zend/zend_execute_API.c
+++ Zend/zend_execute_API.c
@@ -216,19 +216,6 @@ void init_executor(void) /* {{{ */
 }
 /* }}} */
 
-static int zval_call_destructor(zval *zv) /* {{{ */
-{
-	if (Z_TYPE_P(zv) == IS_INDIRECT) {
-		zv = Z_INDIRECT_P(zv);
-	}
-	if (Z_TYPE_P(zv) == IS_OBJECT && Z_REFCOUNT_P(zv) == 1) {
-		return ZEND_HASH_APPLY_REMOVE;
-	} else {
-		return ZEND_HASH_APPLY_KEEP;
-	}
-}
-/* }}} */
-
 static void zend_unclean_zval_ptr_dtor(zval *zv) /* {{{ */
 {
 	if (Z_TYPE_P(zv) == IS_INDIRECT) {
