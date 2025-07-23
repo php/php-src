987f3279211d0ef7ee373d912344d52dba2299d5
Dmitriy Derepko <xepozz@list.ru>
2025-07-23 15:09:03 +0300

    use empty string pointer

--- ext/standard/html.c
+++ ext/standard/html.c
@@ -1321,7 +1321,7 @@ static void php_html_entities(INTERNAL_FUNCTION_PARAMETERS, int all)
 	ZEND_PARSE_PARAMETERS_END();
 
 	if (EXPECTED(ZSTR_LEN(str) == 0)) {
-		RETVAL_STR_COPY(str);
+		RETURN_EMPTY_STRING();
 		return;
 	}
 	replaced = php_escape_html_entities_ex(
