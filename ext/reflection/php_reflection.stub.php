7158ac977100667f53b5a4176394a9a8a42346fb
Matt D <mattsd1410@gmail.com>
2025-08-08 09:58:01 +1000

    Update ext/reflection/php_reflection.stub.php
    Co-authored-by: Gina Peter Banyard <girgias@php.net>

--- ext/reflection/php_reflection.stub.php
+++ ext/reflection/php_reflection.stub.php
@@ -371,7 +371,6 @@ class ReflectionClass implements Reflector
     /** @tentative-return-type */
     public function newInstanceArgs(array $args = []): ?object {}
 
-    /** @tentative-return-type */
     public function newInstanceFromData(array $data, array $args = []): ?object {}
 
     public function newLazyGhost(callable $initializer, int $options = 0): object {}
