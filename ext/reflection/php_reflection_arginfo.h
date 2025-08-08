7fa88f878b6f847884d9f6205ecff4af0fe39bec
Matt Dwyer <mattsd1410@gmail.com>
2025-08-08 19:59:13 +1000

    there we go - now i know how stub files work

--- ext/reflection/php_reflection_arginfo.h
+++ ext/reflection/php_reflection_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: d7aa264ff59eaf10b71955bc55eee2b1e03081e3 */
+ * Stub hash: cebf3006f3e20da18dd9d886d883a93bda74e18d */
 
 ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(arginfo_class_Reflection_getModifierNames, 0, 1, IS_ARRAY, 0)
 	ZEND_ARG_TYPE_INFO(0, modifiers, IS_LONG, 0)
