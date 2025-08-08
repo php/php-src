67a6521ff92b3bd8ab572af9e2c23928f8155e42
David Carlier <devnexen@gmail.com>
2025-08-08 06:58:59 +0100

    ext/intl: migrate C code to C++ step 3.

--- ext/intl/dateformat/dateformat_class.h
+++ ext/intl/dateformat/dateformat_class.h
@@ -35,8 +35,14 @@ static inline IntlDateFormatter_object *php_intl_dateformatter_fetch_object(zend
 }
 #define Z_INTL_DATEFORMATTER_P(zv) php_intl_dateformatter_fetch_object(Z_OBJ_P(zv))
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 void dateformat_register_IntlDateFormatter_class( void );
 extern zend_class_entry *IntlDateFormatter_ce_ptr;
+#ifdef __cplusplus
+}
+#endif
 
 /* Auxiliary macros */
 
