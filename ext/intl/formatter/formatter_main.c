260dc27078409bc499818a2721d2c0c33303373b
Alexandre Daubois <alex.daubois@gmail.com>
2025-08-26 09:20:19 +0200

    Fix GH-11952: better locale strings canonicalization for IntlDateFormatter and NumberFormatter

--- ext/intl/formatter/formatter_main.c
+++ ext/intl/formatter/formatter_main.c
@@ -17,6 +17,7 @@
 #endif
 
 #include <unicode/ustring.h>
+#include <unicode/uloc.h>
 
 #include "php_intl.h"
 #include "formatter_class.h"
@@ -63,11 +64,17 @@ static int numfmt_ctor(INTERNAL_FUNCTION_PARAMETERS, zend_error_handling *error_
 		locale = intl_locale_get_default();
 	}
 
-	/* Create an ICU number formatter. */
-	FORMATTER_OBJECT(nfo) = unum_open(style, spattern, spattern_len, locale, NULL, &INTL_DATA_ERROR_CODE(nfo));
+	char* canonicalized_locale = canonicalize_locale_string(locale);
+	const char* final_locale = canonicalized_locale ? canonicalized_locale : locale;
 
-	if(spattern) {
+	FORMATTER_OBJECT(nfo) = unum_open(style, spattern, spattern_len, final_locale, NULL, &INTL_DATA_ERROR_CODE(nfo));
+
+	if (spattern) {
 		efree(spattern);
+	}
+	
+	if (canonicalized_locale) {
+		efree(canonicalized_locale);
 	}
 
 	INTL_CTOR_CHECK_STATUS(nfo, "numfmt_create: number formatter creation failed");
