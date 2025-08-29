ba326b6a7135aca0f29997676ca65bbe9463a08f
Tim Düsterhus <tim@bastelstu.be>
2025-08-29 16:57:23 +0200

    zend_portability: Support C++ for `ZEND_ATTRIBUTE_NODISCARD`
    Co-authored-by: David CARLIER <devnexen@gmail.com>

--- Zend/zend_portability.h
+++ Zend/zend_portability.h
@@ -248,7 +248,7 @@ char *alloca();
 # define ZEND_ATTRIBUTE_ALLOC_SIZE2(X,Y)
 #endif
 
-#if __STDC_VERSION__ >= 202311L
+#if __STDC_VERSION__ >= 202311L || (defined(__cplusplus) && __cplusplus >= 201703L)
 # define ZEND_ATTRIBUTE_NODISCARD [[nodiscard]]
 #elif __has_attribute(__warn_unused_result__)
 # define ZEND_ATTRIBUTE_NODISCARD __attribute__((__warn_unused_result__))
