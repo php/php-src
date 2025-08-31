24d78eae5748a85a27fbd337fc0f779de46f3cfc
Ilija Tovilo <ilija.tovilo@me.com>
2025-08-31 13:28:37 +0200

    Fix glob() RC info
    glob() can return the immutable, empty, RCn array since GH-19642.

--- ext/standard/basic_functions_arginfo.h
+++ ext/standard/basic_functions_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: deb4ea96dd130d8a0174678095c30a61e118bd60 */
+ * Stub hash: 01fc9901c45992450d60ceedbb82d3a6fb8500de */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_set_time_limit, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, seconds, IS_LONG, 0)
