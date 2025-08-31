a544fe107ec6da343df8ef21e42740c0da1c7b3a
Ilija Tovilo <ilija.tovilo@me.com>
2025-08-31 13:28:37 +0200

    Fix glob() RC info
    glob() can return the immutable, empty, RCn array since GH-19642.
    
    Closes GH-19652

--- ext/standard/basic_functions.stub.php
+++ ext/standard/basic_functions.stub.php
@@ -2697,7 +2697,6 @@ function scandir(string $directory, int $sorting_order = SCANDIR_SORT_ASCENDING,
 
 /**
  * @return array<int, string>|false
- * @refcount 1
  */
 function glob(string $pattern, int $flags = 0): array|false {}
 
