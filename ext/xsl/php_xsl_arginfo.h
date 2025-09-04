3f3a266a2bab9fda95096cf4a17156a5dc66e8bb
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2025-09-03 18:26:43 -0700

    gen_stub: Infer constants' types from values (#19568)
    Don't require `@var` with a type when the type can be inferred from a literal
    value in the stub file.

--- ext/xsl/php_xsl_arginfo.h
+++ ext/xsl/php_xsl_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 5b4ce3f5f7dee60bde803b3c2eb3994777f56914 */
+ * Stub hash: cb1005b601e72e8d36d0f6aa5d08872f5c7ea2e6 */
 
 ZEND_BEGIN_ARG_WITH_TENTATIVE_RETURN_TYPE_INFO_EX(arginfo_class_XSLTProcessor_importStylesheet, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, stylesheet, IS_OBJECT, 0)
