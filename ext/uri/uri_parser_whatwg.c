15388aff05a52b132c604d720a344662648ba7e6
Tim Düsterhus <tim@bastelstu.be>
2025-09-07 16:26:21 +0200

    uri: Fix handling of the `errors == NULL && !silent` for uri_parser_whatwg
    Previously, when `errors` was `NULL`, the `errors` pointer was used to set the
    `$errors` property when throwing the exception, leading to a crash. Use a local
    zval to pass the errors to the Exception and copy it into the `errors` input
    when it is non-`NULL`.

--- ext/uri/uri_parser_whatwg.c
+++ ext/uri/uri_parser_whatwg.c
@@ -65,12 +65,6 @@ static zend_always_inline void zval_long_or_null_to_lexbor_str(zval *value, lexb
  */
 static const char *fill_errors(zval *errors)
 {
-	if (errors == NULL) {
-		return NULL;
-	}
-
-	ZEND_ASSERT(Z_ISUNDEF_P(errors));
-
 	array_init(errors);
 
 	if (lexbor_parser.log == NULL) {
@@ -224,7 +218,8 @@ static const char *fill_errors(zval *errors)
 
 static void throw_invalid_url_exception_during_write(zval *errors, const char *component)
 {
-	const char *reason = fill_errors(errors);
+	zval err;
+	const char *reason = fill_errors(&err);
 	zend_object *exception = zend_throw_exception_ex(
 		uri_whatwg_invalid_url_exception_ce,
 		0,
@@ -234,7 +229,12 @@ static void throw_invalid_url_exception_during_write(zval *errors, const char *c
 		reason ? reason : "",
 		reason ? ")" : ""
 	);
-	zend_update_property(exception->ce, exception, ZEND_STRL("errors"), errors);
+	zend_update_property(exception->ce, exception, ZEND_STRL("errors"), &err);
+	if (errors) {
+		zval_ptr_dtor(errors);
+		ZVAL_COPY(errors, &err);
+	}
+	zval_ptr_dtor(&err);
 }
 
 static lxb_status_t serialize_to_smart_str_callback(const lxb_char_t *data, size_t length, void *ctx)
@@ -561,11 +561,19 @@ lxb_url_t *php_uri_parser_whatwg_parse_ex(const char *uri_str, size_t uri_str_le
 	lxb_url_parser_clean(&lexbor_parser);
 
 	lxb_url_t *url = lxb_url_parse(&lexbor_parser, lexbor_base_url, (unsigned char *) uri_str, uri_str_len);
-	const char *reason = fill_errors(errors);
 
-	if (url == NULL && !silent) {
-		zend_object *exception = zend_throw_exception_ex(uri_whatwg_invalid_url_exception_ce, 0, "The specified URI is malformed%s%s%s", reason ? " (" : "", reason ? reason : "", reason ? ")" : "");
-		zend_update_property(exception->ce, exception, ZEND_STRL("errors"), errors);
+	if ((url == NULL && !silent) || errors != NULL) {
+		zval err;
+		const char *reason = fill_errors(&err);
+		if (url == NULL && !silent) {
+			zend_object *exception = zend_throw_exception_ex(uri_whatwg_invalid_url_exception_ce, 0, "The specified URI is malformed%s%s%s", reason ? " (" : "", reason ? reason : "", reason ? ")" : "");
+			zend_update_property(exception->ce, exception, ZEND_STRL("errors"), &err);
+		}
+		if (errors != NULL) {
+			zval_ptr_dtor(errors);
+			ZVAL_COPY(errors, &err);
+		}
+		zval_ptr_dtor(&err);
 	}
 
 	return url;
