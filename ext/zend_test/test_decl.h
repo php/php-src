59eeace96b05b5dc717a61293debced23bbac130
Tim Düsterhus <tim@tideways-gmbh.com>
2025-09-18 11:06:03 +0200

    gen_stub: Add support for generation C `#include` statements
    This is useful to include the necessary files for a constant’s `@cvalue` from
    within the arginfo itself.

--- ext/zend_test/test_decl.h
+++ ext/zend_test/test_decl.h
@@ -1,8 +1,8 @@
 /* This is a generated file, edit test.stub.php instead.
- * Stub hash: 4d728e740122add9d4c91f5c1abb5f5017690636 */
+ * Stub hash: 2954e482259ba13b5b27c8652f388fb5e1b5dcb5 */
 
-#ifndef ZEND_TEST_DECL_4d728e740122add9d4c91f5c1abb5f5017690636_H
-#define ZEND_TEST_DECL_4d728e740122add9d4c91f5c1abb5f5017690636_H
+#ifndef ZEND_TEST_DECL_2954e482259ba13b5b27c8652f388fb5e1b5dcb5_H
+#define ZEND_TEST_DECL_2954e482259ba13b5b27c8652f388fb5e1b5dcb5_H
 
 typedef enum zend_enum_ZendTestUnitEnum {
 	ZEND_ENUM_ZendTestUnitEnum_Foo = 1,
@@ -27,4 +27,4 @@ typedef enum zend_enum_ZendTestEnumWithInterface {
 	ZEND_ENUM_ZendTestEnumWithInterface_Bar = 2,
 } zend_enum_ZendTestEnumWithInterface;
 
-#endif /* ZEND_TEST_DECL_4d728e740122add9d4c91f5c1abb5f5017690636_H */
+#endif /* ZEND_TEST_DECL_2954e482259ba13b5b27c8652f388fb5e1b5dcb5_H */
