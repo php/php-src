fb91931d6655989a18bfcaff9557f4a761f490e7
Tim Düsterhus <tim@bastelstu.be>
2025-09-23 23:26:25 +0200

    com_dotnet: Use `true` / `false` instead of `1` / `0` for `bool` parameters
    Changes done with Coccinelle:
    
        @r1@
        identifier F;
        identifier p;
        typedef bool;
        parameter list [n1] PL1;
        parameter list [n2] PL2;
        @@
    
        F(PL1, bool p, PL2) {
        ...
        }
    
        @r2@
        identifier r1.F;
        expression list [r1.n1] EL1;
        expression list [r1.n2] EL2;
        @@
    
        F(EL1,
        (
        - 1
        + true
        |
        - 0
        + false
        )
        , EL2)

--- ext/com_dotnet/com_com.c
+++ ext/com_dotnet/com_com.c
@@ -538,7 +538,7 @@ zend_result php_com_do_invoke_byref(php_com_dotnet_object *obj, zend_internal_fu
 	}
 
 	/* this will create an exception if needed */
-	hr = php_com_invoke_helper(obj, dispid, flags, &disp_params, v, 0, 0);
+	hr = php_com_invoke_helper(obj, dispid, flags, &disp_params, v, false, false);
 
 	/* release variants */
 	if (vargs) {
@@ -649,7 +649,7 @@ zend_result php_com_do_invoke(php_com_dotnet_object *obj, zend_string *name,
 		return FAILURE;
 	}
 
-	return php_com_do_invoke_by_id(obj, dispid, flags, v, nargs, args, 0, allow_noarg);
+	return php_com_do_invoke_by_id(obj, dispid, flags, v, nargs, args, false, allow_noarg);
 }
 
 /* {{{ Generate a globally unique identifier (GUID) */
