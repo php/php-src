c32db7e8e96bb6dcdda79c802944edc8c36994b2
Tim Düsterhus <tim@bastelstu.be>
2025-09-23 23:37:16 +0200

    openssl: Use `true` / `false` instead of `1` / `0` for `bool` parameters (2)
    Changes done with Coccinelle:
    
        @r1@
        identifier F;
        identifier p;
        typedef bool;
        type T;
        parameter list [n1] PL1;
        parameter list [n2] PL2;
        @@
    
        T F(PL1, bool p, PL2);
    
        @r2@
        identifier r1.F;
        expression list [r1.n1] EL1;
        expression list [r1.n2] EL2;
        @@
    
        F(EL1,
        (
        - 1
        + true
        |
        - 0
        + false
        )
        , EL2)

--- ext/openssl/xp_ssl.c
+++ ext/openssl/xp_ssl.c
@@ -360,7 +360,7 @@ static int php_openssl_x509_fingerprint_cmp(X509 *peer, const char *method, cons
 	zend_string *fingerprint;
 	int result = -1;
 
-	fingerprint = php_openssl_x509_fingerprint(peer, method, 0);
+	fingerprint = php_openssl_x509_fingerprint(peer, method, false);
 	if (fingerprint) {
 		result = strcasecmp(expected, ZSTR_VAL(fingerprint));
 		zend_string_release_ex(fingerprint, 0);
