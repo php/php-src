3a8e6a709ac1d86fa7fb2f1c18d7c7dae05355c2
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2025-10-01 14:15:33 +0200

    DOM Windows build fix (#20026)

--- ext/dom/lexbor/selectors-adapted/selectors.c
+++ ext/dom/lexbor/selectors-adapted/selectors.c
@@ -13,7 +13,7 @@
 #include <Zend/zend_API.h>
 #include <php.h>
 
-#include "lexbor/selectors-adapted/selectors.h"
+#include "selectors.h"
 #include "../../namespace_compat.h"
 #include "../../domexception.h"
 #include "../../php_dom.h"
