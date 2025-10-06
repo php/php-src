7ba105f417a2f9e3bfe0139533e7548c546d8732
Steve Wall <Stephen.Wall@redcom.com>
2025-10-06 13:01:09 -0400

    Extend x.509 parsing to include machine-readable info about subjectAlternativeNames
    This adds an additional array element to the output of `openssl_x509_parse()` that includes detailed info about the subjectAlternativeName extension in an easily machine parsable format.
    
    Example:
    If the contents of the extension appear as this in the current openssl_x509_parse() output (wrapped for readability):
    ```
    Array
    (
        [name] => ...
        ...
        [extensions] => Array
            (
                [subjectAltName] =>
                    DNS:www.good.org,
                    email:good@good.org,
                    IP Address:192.168.0.1,
                    othername:<unsupported>,
                    othername:SmtpUTF8Mailbox:test@test.example.com,
                    URI:sip:6000@192.168.0.1,
                    DirName/C=US/ST=CA/L=San Francisco/O=Example Company/OU=Example Company Unit/CN=Bob,
                    Registered ID:1.2.3.4.5
            )
    )
    ```
    you would see a new top-level array element like this:
    ```
    Array
    (
        ...
        [subjectAlternativeName] => Array
            (
                [0] => Array
                    (
                        [type] => DNS
                        [value] => www.good.org
                    )
    
                [1] => Array
                    (
                        [type] => email
                        [value] => good@good.org
                    )
    
                [2] => Array
                    (
                        [type] => IP Address
                        [value] => 192.168.0.1
                    )
    
                [3] => Array
                    (
                        [type] => othername
                        [value] => Array
                            (
                                [1.3.6.1.5.5.7.8.7] => foo@example.org
                            )
                    )
    
                [4] => Array
                    (
                        [type] => othername
                        [value] => Array
                            (
                                [1.3.6.1.5.5.7.8.9] => test@test.example.com
                            )
                    )
    
                [5] => Array
                    (
                        [type] => URI
                        [value] => sip:6000@192.168.0.1
                    )
    
                [6] => Array
                    (
                        [type] => DirName
                        [value] => Array
                            (
                                [2.5.4.6] => US
                                [2.5.4.8] => CA
                                [2.5.4.7] => San Francisco
                                [2.5.4.10] => Example Company
                                [2.5.4.11] => Example Company Unit
                                [2.5.4.3] => Bob
                            )
    
                    )
    
                [7] => Array
                    (
                        [type] => Registered ID
                        [value] => 1.2.3.4.5
                    )
    
            )
    )
    ```

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -1400,6 +1400,7 @@ PHP_FUNCTION(openssl_x509_parse)
 	char *str_serial;
 	char *hex_serial;
 	char buf[256];
+	zval *altname = NULL;
 
 	ZEND_PARSE_PARAMETERS_START(1, 2)
 		Z_PARAM_OBJ_OF_CLASS_OR_STR(cert_obj, php_openssl_certificate_ce, cert_str)
@@ -1424,7 +1425,8 @@ PHP_FUNCTION(openssl_x509_parse)
 	add_assoc_string(return_value, "name", cert_name);
 	OPENSSL_free(cert_name);
 
-	php_openssl_add_assoc_name_entry(return_value, "subject", subject_name, useshortnames);
+	php_openssl_add_assoc_name_entry(return_value, "subject", subject_name, useshortnames ?
+					 PHP_OPENSSL_SHORT_NAME : PHP_OPENSSL_LONG_NAME);
 	/* hash as used in CA directories to lookup cert by subject name */
 	{
 		char buf[32];
@@ -1432,7 +1434,8 @@ PHP_FUNCTION(openssl_x509_parse)
 		add_assoc_string(return_value, "hash", buf);
 	}
 
-	php_openssl_add_assoc_name_entry(return_value, "issuer", X509_get_issuer_name(cert), useshortnames);
+	php_openssl_add_assoc_name_entry(return_value, "issuer", X509_get_issuer_name(cert), useshortnames ?
+					 PHP_OPENSSL_SHORT_NAME : PHP_OPENSSL_LONG_NAME);
 	add_assoc_long(return_value, "version", X509_get_version(cert));
 
 	asn1_serial = X509_get_serialNumber(cert);
@@ -1534,7 +1537,7 @@ PHP_FUNCTION(openssl_x509_parse)
 			goto err_subitem;
 		}
 		if (nid == NID_subject_alt_name) {
-			if (openssl_x509v3_subjectAltName(bio_out, extension) == 0) {
+			if (openssl_x509v3_subjectAltName(bio_out, extension, &altname) == 0) {
 				BIO_get_mem_ptr(bio_out, &bio_buf);
 				add_assoc_stringl(&subitem, extname, bio_buf->data, bio_buf->length);
 			} else {
@@ -1555,6 +1558,9 @@ PHP_FUNCTION(openssl_x509_parse)
 		add_assoc_zval(return_value, "criticalExtensions", &critext);
 	} else {
 		zval_ptr_dtor(&critext);
+	}
+	if (altname != NULL) {
+	    add_assoc_zval(return_value, "subjectAlternativeName", altname);
 	}
 	if (cert_str) {
 		X509_free(cert);
@@ -2380,7 +2386,8 @@ PHP_FUNCTION(openssl_csr_get_subject)
 	subject = X509_REQ_get_subject_name(csr);
 
 	array_init(return_value);
-	php_openssl_add_assoc_name_entry(return_value, NULL, subject, use_shortnames);
+	php_openssl_add_assoc_name_entry(return_value, NULL, subject, use_shortnames ?
+					 PHP_OPENSSL_SHORT_NAME : PHP_OPENSSL_LONG_NAME);
 
 	if (csr_str) {
 		X509_REQ_free(csr);
