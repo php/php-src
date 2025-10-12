ee43384a0be40c35aa61d618e4d57854b2116a31
Niels Dossche <7771979+nielsdos@users.noreply.github.com>
2025-10-12 16:59:18 +0200

    [ci skip] Remove duplicated comment block

--- ext/intl/locale/locale_methods.cpp
+++ ext/intl/locale/locale_methods.cpp
@@ -1574,8 +1574,6 @@ U_CFUNC PHP_FUNCTION(locale_lookup)
 /* }}} */
 
 /* {{{ Tries to find out best available locale based on HTTP "Accept-Language" header */
-/* }}} */
-/* {{{ Tries to find out best available locale based on HTTP "Accept-Language" header */
 U_CFUNC PHP_FUNCTION(locale_accept_from_http)
 {
 	UEnumeration *available;
