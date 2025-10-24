5dac1fe980cf6ccb157bda557ed7d9d7d4db3247
Marc Bennewitz <m.bennewitz@dcmn.com>
2025-10-24 16:28:48 +0200

    Do not inline current time retrieval to be mockable

--- Zend/zend_time.h
+++ Zend/zend_time.h
@@ -80,46 +80,15 @@ static zend_always_inline void zend_time_val2spec(struct timeval tv, struct time
 	ts->tv_nsec = (long) (tv.tv_usec * 1000);
 }
 
-/* Get current timestamp in seconds */
-static zend_always_inline time_t zend_time_real_get(void) {
-	return time(NULL);
-}
+/* Current real/wall-time in seconds */
+ZEND_API time_t zend_time_real_get(void);
 
-/* wrapper around clock_gettime/timestamp_get/gettimeofday/time */
-static zend_always_inline void zend_time_real_spec(struct timespec *ts) {
-	#if defined(HAVE_CLOCK_GETTIME)
-
-		(void) clock_gettime(CLOCK_REALTIME, ts);
-
-	#elif defined(HAVE_TIMESPEC_GET)
-
-		(void) timespec_get(ts, TIME_UTC);
-
-	#elif defined(HAVE_GETTIMEOFDAY)
-
-		struct timeval tv;
-		(void) gettimeofday(&tv, NULL);
-		zend_time_val2spec(tv, ts);
-
-	#else
-
-		ts->tv_sec  = zend_time_real_get();
-		ts->tv_nsec = 0;
-
-	#endif
-}
+/* Current real/wall-time in up-to nano seconds */
+ZEND_API void zend_time_real_spec(struct timespec *ts);
 
 /* Monotonic time in nanoseconds with a fallback to real/wall-time 
    if no monotonic timer is available */
-static zend_always_inline uint64_t zend_time_mono_fallback(void) {
-	#if ZEND_HRTIME_AVAILABLE
-		return (uint64_t)zend_hrtime();
-	#else
-		struct timespec ts;
-		zend_time_real_spec(&ts);
-		return ((uint64_t) ts.tv_sec * ZEND_NANO_IN_SEC) + ts.tv_nsec;
-	#endif
-}
+ZEND_API uint64_t zend_time_mono_fallback(void);
 
 END_EXTERN_C()
 
