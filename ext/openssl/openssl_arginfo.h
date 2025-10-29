0cbdc1c299598b2565905056f0aee7bf65235205
Steve Wall <Stephen.Wall@redcom.com>
2025-10-29 13:32:56 -0400

    Fix merge error, update stubs hash

--- ext/openssl/openssl_arginfo.h
+++ ext/openssl/openssl_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: 8233a8abc8ab7145d905d0fa51478edfe1e55a06 */
+ * Stub hash: a927069eb0fc1bdae7b75387705d9391a44fea6e */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_openssl_x509_export_to_file, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_TYPE_MASK(0, certificate, OpenSSLCertificate, MAY_BE_STRING, NULL)
