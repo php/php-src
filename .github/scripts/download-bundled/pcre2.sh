ff07fc5b7a7aa68ecf779105535be5a863e4b6b8
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-01 12:41:31 +0100

    wip update local bundle from upstream

--- .github/scripts/download-bundled/pcre2.sh
+++ .github/scripts/download-bundled/pcre2.sh
@@ -5,3 +5,9 @@ cd "$(dirname "$0")/../../.."
 commit=b2bd4254b379b9d7dc9a3dda060a7e27009ccdff # 10.46 release
 
 git clone --revision=$commit https://github.com/PCRE2Project/pcre2.git /tmp/php-src-bundled/pcre2
+
+rm -rf ext/pcre/pcre2lib
+cp -R /tmp/php-src-bundled/pcre2/src ext/pcre/pcre2lib
+
+cd ext/pcre/pcre2lib
+git restore config.h
--- .github/scripts/download-bundled/sljit.sh
+++ .github/scripts/download-bundled/sljit.sh
@@ -5,3 +5,8 @@ cd "$(dirname "$0")/../../.."
 commit=8d40e0306aacca3596f64a338b5033d2050fc20e
 
 git clone --revision=$commit https://github.com/zherczeg/sljit.git /tmp/php-src-bundled/sljit
+
+rm -rf ext/pcre/pcre2lib/sljit
+cp -R /tmp/php-src-bundled/sljit/sljit_src ext/pcre/pcre2lib/sljit
+
+cd ext/pcre/pcre2lib/sljit
