662ea92965a38e6dfe3046821270e8ee27735135
Steve Wall <Stephen.Wall@redcom.com>
2025-11-02 21:04:19 -0500

    fix stub hash

--- ext/openssl/openssl_arginfo.h
+++ ext/openssl/openssl_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit openssl.stub.php instead.
- * Stub hash: 4d38e81a2f73bb6dd4bbe7a3e0b8ba86600654e2 */
+ * Stub hash: 2e0289886ca838855a6ca342f53128f896514e50 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_openssl_x509_export_to_file, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_OBJ_TYPE_MASK(0, certificate, OpenSSLCertificate, MAY_BE_STRING, NULL)
