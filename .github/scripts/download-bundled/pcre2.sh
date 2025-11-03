8a9e8282aab437942db26680bf19896f1a66c7f7
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-03 16:25:01 +0100

    reference using tag

--- .github/scripts/download-bundled/pcre2.sh
+++ .github/scripts/download-bundled/pcre2.sh
@@ -2,9 +2,9 @@
 set -ex
 cd "$(dirname "$0")/../../.."
 
-commit=b2bd4254b379b9d7dc9a3dda060a7e27009ccdff # 10.46 release
+revision=refs/tags/pcre2-10.46
 
-git clone --recurse-submodules --revision=$commit https://github.com/PCRE2Project/pcre2.git /tmp/php-src-bundled/pcre2
+git clone --depth 1 --recurse-submodules --revision=$revision https://github.com/PCRE2Project/pcre2.git /tmp/php-src-bundled/pcre2
 
 rm -rf ext/pcre/pcre2lib
 cp -R /tmp/php-src-bundled/pcre2/src ext/pcre/pcre2lib
