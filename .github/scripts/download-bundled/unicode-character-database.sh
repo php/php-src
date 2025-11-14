eac717a50922ece3a11ef9a79205cf790d2779b3
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-14 14:06:23 +0100

    impl UCD download

---
+++ .github/scripts/download-bundled/unicode-character-database.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+cd "$(dirname "$0")/../../.."
+
+tmp_dir=/tmp/php-src-download-bundled/unicode-character-database
+rm -rf "$tmp_dir"
+
+version=17.0.0
+
+mkdir -p "$tmp_dir"
+curl --fail https://www.unicode.org/Public/$version/ucd/UCD.zip -o "$tmp_dir/data.zip"
+unzip "$tmp_dir/data.zip" -d "$tmp_dir"
+
+cd ext/mbstring
+
+rm libmbfl/mbfl/eaw_table.h
+rm unicode_data.h
+
+./ucgendat/ucgendat.php "$tmp_dir"
