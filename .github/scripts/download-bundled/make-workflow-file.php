d33eb7bdbbafc2d976f7afc2904816eb0e5b1f1d
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-14 14:19:24 +0100

    DEBUG faster debug

--- .github/scripts/download-bundled/make-workflow-file.php
+++ .github/scripts/download-bundled/make-workflow-file.php
@@ -8,7 +8,7 @@ namespace Phpsrc\Ci\DownloadBundled;
 $bundles = [
     new Bundle('boost.context', ['Zend/asm']),
     new Bundle('timelib', ['ext/date/lib']),
-    new Bundle('Unicode Character Database', ['ext/mbstring']),
+    // new Bundle('Unicode Character Database', ['ext/mbstring']),
     new Bundle('IR', ['ext/opcache/jit/ir']),
     new Bundle('PCRE2', ['ext/pcre/pcre2lib']),
     new Bundle('uriparser', ['ext/uri/uriparser']),
