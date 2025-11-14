9b20b0440e78717a9a7d2b357596c7b63dd53394
Michael Voříšek <mvorisek@mvorisek.cz>
2025-11-14 14:06:23 +0100

    impl UCD download

--- .github/scripts/download-bundled/make-workflow-file.php
+++ .github/scripts/download-bundled/make-workflow-file.php
@@ -8,6 +8,7 @@ namespace Phpsrc\Ci\DownloadBundled;
 $bundles = [
     new Bundle('boost.context', ['Zend/asm']),
     new Bundle('timelib', ['ext/date/lib']),
+    new Bundle('Unicode Character Database', ['ext/mbstring']),
     new Bundle('PCRE2', ['ext/pcre/pcre2lib']),
     new Bundle('uriparser', ['ext/uri/uriparser']),
 ];
