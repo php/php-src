21a1fc0e6f124057d2d6cfbfca386d4f16ee81ce
Remi Collet <remi@remirepo.net>
2025-11-20 02:58:45 +0100

    Fix GH-20528: Regression breaks mysql connexion using an IPv6 address enclosed in square brackets

--- ext/mysqlnd/mysqlnd_connection.c
+++ ext/mysqlnd/mysqlnd_connection.c
@@ -557,10 +557,11 @@ MYSQLND_METHOD(mysqlnd_conn_data, get_scheme)(MYSQLND_CONN_DATA * conn, MYSQLND_
 		if (hostname.s[0] != '[' && mysqlnd_fast_is_ipv6_address(hostname.s)) {
 			transport.l = mnd_sprintf(&transport.s, 0, "tcp://[%s]:%u", hostname.s, port);
 		} else {
-			/* Not ipv6, but could already contain a port number, in which case we should not add an extra port.
+			/* Could already contain a port number, in which case we should not add an extra port.
 			 * See GH-8978. In a port doubling scenario, the first port would be used so we do the same to keep BC. */
-			if (strchr(hostname.s, ':')) {
+			if (strchr(hostname.s, ':') && !mysqlnd_fast_is_ipv6_address(hostname.s)) {
 				/* TODO: Ideally we should be able to get rid of this workaround in the future. */
+				/* TODO: IPv6 address enclosed in square brackets is not handled, ex [::1]:3306 */
 				transport.l = mnd_sprintf(&transport.s, 0, "tcp://%s", hostname.s);
 			} else {
 				transport.l = mnd_sprintf(&transport.s, 0, "tcp://%s:%u", hostname.s, port);
