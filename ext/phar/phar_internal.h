6c86e2c3fbd199fe86b7d41c761b67f230d80144
Niels Dossche <7771979+ndossche@users.noreply.github.com>
2025-11-29 12:14:50 +0100

    Revert accidental change for other PR

--- ext/phar/phar_internal.h
+++ ext/phar/phar_internal.h
@@ -259,6 +259,7 @@ struct _phar_archive_data {
 	/* hash of mounted directory paths */
 	HashTable                mounted_dirs;
 	uint32_t                 flags;
+	uint32_t                 min_timestamp;
 	uint32_t                 max_timestamp;
 	int                      refcount;
 	php_stream               *fp;
