472a774405abb50f5eaf56511e84a6897f625429
David Carlier <devnexen@gmail.com>
2025-12-02 19:04:36 +0000

    Fix GH-20603 issue on windows 32 bits.
    the timeout needed to be unsigned.

--- ext/ftp/php_ftp.c
+++ ext/ftp/php_ftp.c
@@ -158,7 +158,7 @@ PHP_FUNCTION(ftp_connect)
 		RETURN_THROWS();
 	}
 
-	const zend_long timeoutmax = (zend_long)((double) PHP_TIMEOUT_ULL_MAX / 1000000.0);
+	const zend_ulong timeoutmax = (zend_ulong)((double) PHP_TIMEOUT_ULL_MAX / 1000000.0);
 
 	if (timeout_sec <= 0) {
 		zend_argument_value_error(3, "must be greater than 0");
@@ -166,7 +166,7 @@ PHP_FUNCTION(ftp_connect)
 	}
 
 	if (timeout_sec >= timeoutmax) {
-		zend_argument_value_error(3, "must be less than " ZEND_LONG_FMT, timeoutmax);
+		zend_argument_value_error(3, "must be less than " ZEND_ULONG_FMT, timeoutmax);
 		RETURN_THROWS();
 	}
 
