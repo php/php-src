6772c8fdf26da147d57fc586b9d959c60756183d
Gina Peter Banyard <girgias@php.net>
2025-12-05 00:13:40 +0000

    ext/standard/scanf: remove unused includes

--- ext/standard/scanf.c
+++ ext/standard/scanf.c
@@ -65,13 +65,8 @@
 #include <limits.h>
 #include <ctype.h>
 #include "php.h"
-#include "php_variables.h"
 #include <locale.h>
-#include "zend_execute.h"
-#include "zend_operators.h"
 #include "zend_strtod.h"
-#include "php_globals.h"
-#include "basic_functions.h"
 #include "scanf.h"
 
 /*
