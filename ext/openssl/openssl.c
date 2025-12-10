01c95c72cce7d04706fbac766abb681833ea5814
Steve Wall <Stephen.Wall@redcom.com>
2025-12-10 09:28:36 -0500

    Remove example from comments

--- ext/openssl/openssl.c
+++ ext/openssl/openssl.c
@@ -4592,39 +4592,6 @@ PHP_FUNCTION(openssl_random_pseudo_bytes)
 /* {{{ Given an Object ID, or object short or long name, return an associative
    array containing any known OID, short name, and long name, or false if the
    object is not known.
-
-   Example:
-
-   var_dump( openssl_oid_lookup( "CN" ) );
-   var_dump( openssl_oid_lookup( "unstructuredAddress" ) );
-   var_dump( openssl_oid_lookup( "1.2.3.4.5" ) );
-   var_dump( openssl_oid_lookup( "junk" ) );
-
-   Produces;
-
-   array(3) {
-     ["oid"]=>
-     string(7) "2.5.4.3"
-     ["lname"]=>
-     string(10) "commonName"
-     ["sname"]=>
-     string(2) "CN"
-   }
-
-   array(2) {
-     ["oid"]=>
-     string(20) "1.2.840.113549.1.9.8"
-     ["lname"]=>
-     string(19) "unstructuredAddress"
-   }
-
-   array(1) {
-     ["oid"]=>
-     string(9) "1.2.3.4.5"
-   }
-
-   bool(false)
-
 */
 PHP_FUNCTION(openssl_oid_lookup)
 {
