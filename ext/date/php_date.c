67effbd5753f8bf89ce421f5f4eed49d1a2bf1ae
Niels Dossche <7771979+ndossche@users.noreply.github.com>
2026-01-14 18:38:37 +0100

    Fix GH-20936: DatePeriod::__set_state() cannot handle null start
    The "current" and "end" field also rely on start_ce, which is set by
    "start". Therefore, if "current" or "end" are provided, so must "start"
    be provided.

--- ext/date/php_date.c
+++ ext/date/php_date.c
@@ -5811,7 +5811,7 @@ static bool php_date_period_initialize_from_hash(php_period_obj *period_obj, Has
 			php_date_obj *date_obj;
 			date_obj = Z_PHPDATE_P(ht_entry);
 
-			if (!date_obj->time) {
+			if (!date_obj->time || !period_obj->start_ce) {
 				return 0;
 			}
 
@@ -5832,7 +5832,7 @@ static bool php_date_period_initialize_from_hash(php_period_obj *period_obj, Has
 			php_date_obj *date_obj;
 			date_obj = Z_PHPDATE_P(ht_entry);
 
-			if (!date_obj->time) {
+			if (!date_obj->time || !period_obj->start_ce) {
 				return 0;
 			}
 
