0db76cf9b10cf4d6c4b30887765edcb8f7e979a1
Calvin Buckley <calvinb@php.net>
2026-01-30 14:38:08 -0400

    Split config.* script

--- scripts/dev/update-libtool.sh
+++ scripts/dev/update-libtool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Update the PHP autoconf/libtool bits.
+# Update the PHP libtool bits.
 
 # Go to project root directory
 cd "$(CDPATH='' cd -- "$(dirname -- "$0")/../../" && pwd -P)" || exit
@@ -21,18 +21,3 @@ chmod +x build/ltmain.sh
 
 new_libtool_serial=$($grep -Po '(?<=^# serial )(\d*)(?=.*)' build/libtool.m4)
 echo "New libtool serial: $new_libtool_serial"
-
-# For config.*, GNU recommendation for updates is to download from Savannah
-# See: https://www.gnu.org/software/gettext/manual/html_node/config_002eguess.html
-
-update_file() {
-	old_ts=$("build/$1" -t)
-	echo "Old $1 timestamp: $old_ts"
-	url="https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=$1;hb=HEAD"
-	wget -O "build/$1" "$url"
-	new_ts=$("build/$1" -t)
-	echo "New $1 timestamp: $new_ts"
-}
-
-update_file config.guess
-update_file config.sub
