766c721c71142a9209d7d836cc1ec1b1e16d8f98
Weixie Cui <cuiweixie@gmail.com>
2026-01-30 19:18:10 +0800

    perf: save strlen to var instead of calc it each iterator

--- sapi/fpm/fpm/fpm_conf.c
+++ sapi/fpm/fpm/fpm_conf.c
@@ -973,7 +973,8 @@ static int fpm_conf_process_all_pools(void)
 				return -1;
 			}
 
-			for (i = 0; i < strlen(status); i++) {
+			size_t status_len = strlen(status);
+			for (i = 0; i < status_len; i++) {
 				if (!isalnum(status[i]) && status[i] != '/' && status[i] != '-' && status[i] != '_' && status[i] != '.' && status[i] != '~') {
 					zlog(ZLOG_ERROR, "[pool %s] the status path '%s' must contain only the following characters '[alphanum]/_-.~'", wp->config->name, status);
 					return -1;
@@ -991,12 +992,13 @@ static int fpm_conf_process_all_pools(void)
 				return -1;
 			}
 
-			if (strlen(ping) < 2) {
+			size_t ping_len = strlen(ping);
+			if (ping_len < 2) {
 				zlog(ZLOG_ERROR, "[pool %s] the ping path '%s' is not long enough", wp->config->name, ping);
 				return -1;
 			}
 
-			for (i = 0; i < strlen(ping); i++) {
+			for (i = 0; i < ping_len; i++) {
 				if (!isalnum(ping[i]) && ping[i] != '/' && ping[i] != '-' && ping[i] != '_' && ping[i] != '.' && ping[i] != '~') {
 					zlog(ZLOG_ERROR, "[pool %s] the ping path '%s' must contain only the following characters '[alphanum]/_-.~'", wp->config->name, ping);
 					return -1;
