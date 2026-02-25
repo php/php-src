60461ada51b8eb2d96b50b7dc0d65df1bc0a7a41
David Carlier <devnexen@gmail.com>
2026-02-25 19:36:26 +0000

    ext/pcntl: use RETURN_THROWS() instead of RETURN_FALSE when exception is pending
    In pcntl_sigprocmask(), pcntl_sigwaitinfo(), and pcntl_sigtimedwait().

--- ext/pcntl/pcntl.c
+++ ext/pcntl/pcntl.c
@@ -934,6 +934,9 @@ PHP_FUNCTION(pcntl_sigprocmask)
 	bool status = php_pcntl_set_user_signal_infos(user_set, &set, 2, /* allow_empty_signal_array */ how == SIG_SETMASK);
 	/* Some error occurred */
 	if (!status) {
+		if (EG(exception)) {
+			RETURN_THROWS();
+		}
 		RETURN_FALSE;
 	}
 
@@ -982,6 +985,9 @@ PHP_FUNCTION(pcntl_sigwaitinfo)
 	bool status = php_pcntl_set_user_signal_infos(user_set, &set, 1, /* allow_empty_signal_array */ false);
 	/* Some error occurred */
 	if (!status) {
+		if (EG(exception)) {
+			RETURN_THROWS();
+		}
 		RETURN_FALSE;
 	}
 
@@ -1028,6 +1034,9 @@ PHP_FUNCTION(pcntl_sigtimedwait)
 	bool status = php_pcntl_set_user_signal_infos(user_set, &set, 1, /* allow_empty_signal_array */ false);
 	/* Some error occurred */
 	if (!status) {
+		if (EG(exception)) {
+			RETURN_THROWS();
+		}
 		RETURN_FALSE;
 	}
 	if (tv_sec < 0) {
