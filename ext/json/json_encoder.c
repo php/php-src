b6875ae97a5729c76a340240933a2ac61f25273a
lamentxu <1372449351@qq.com>
2026-03-21 13:25:40 +0800

    Update json_encoder.c

--- ext/json/json_encoder.c
+++ ext/json/json_encoder.c
@@ -53,13 +53,19 @@ static inline void php_json_pretty_print_char(smart_str *buf, int options, char 
 
 static inline void php_json_pretty_print_indent(smart_str *buf, int options, const php_json_encoder *encoder) /* {{{ */
 {
-	if (options & PHP_JSON_PRETTY_PRINT) {
-		size_t remaining = (size_t) encoder->depth * 4;
-		if (remaining) {
-			char *dst = smart_str_extend(buf, remaining);
-			memset(dst, ' ', remaining);
-		}
-	}
+    if (options & PHP_JSON_PRETTY_PRINT) {
+        int depth = encoder->depth;
+        if (depth <= 2) {
+            int i;
+            for (i = 0; i < depth; i++) {
+                smart_str_appendl(buf, "    ", 4);
+            }
+        } else {
+            size_t remaining = (size_t) depth * 4;
+            char *dst = smart_str_extend(buf, remaining);
+            memset(dst, ' ', remaining);
+        }
+    }
 }
 /* }}} */
 
