e0ac05670f751c28790bf306200664cc8d78e950
lamentxu <1372449351@qq.com>
2026-04-02 10:05:42 +0800

    Update pgsql.c

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -3428,12 +3428,9 @@ static zend_result pgsql_copy_from_query(PGconn *pgsql, PGresult *pgsql_result, 
 
 	int result;
 	if (ZSTR_LEN(tmp) > 0 && ZSTR_VAL(tmp)[ZSTR_LEN(tmp) - 1] != '\n') {
-		char *zquery = emalloc(ZSTR_LEN(tmp) + 2);
-		memcpy(zquery, ZSTR_VAL(tmp), ZSTR_LEN(tmp) + 1);
-		zquery[ZSTR_LEN(tmp)] = '\n';
-		zquery[ZSTR_LEN(tmp) + 1] = '\0';
-		result = PQputCopyData(pgsql, zquery, ZSTR_LEN(tmp) + 1);
-		efree(zquery);
+		zend_string *new_tmp = zend_string_concat2(ZSTR_VAL(tmp), ZSTR_LEN(tmp), "\n", 1);
+		result = PQputCopyData(pgsql, ZSTR_VAL(new_tmp), ZSTR_LEN(new_tmp));
+		zend_string_release(new_tmp);
 	} else {
 		result = PQputCopyData(pgsql, ZSTR_VAL(tmp), ZSTR_LEN(tmp));
 	}
