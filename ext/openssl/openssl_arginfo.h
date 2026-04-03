d2b545a8cfabd96e6b3e696becbf3a11c500f96f
Jordi Kroon <jkroon@onyourmarks.agency>
2026-04-03 19:35:48 +0200

    re-add filename to openssl arginfo

--- ext/openssl/openssl_arginfo.h
+++ ext/openssl/openssl_arginfo.h
@@ -1,4 +1,4 @@
-/* This is a generated file, edit the .stub.php file instead.
+/* This is a generated file, edit openssl.stub.php instead.
  * Stub hash: a571945d38a3460de017405454b61609811fe1b1 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_openssl_x509_export_to_file, 0, 2, _IS_BOOL, 0)
