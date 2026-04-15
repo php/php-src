8bfb10d58a9d75f79561a7ca68d9d80421719cd0
henderkes <m@pyc.ac>
2026-04-15 18:57:39 +0700

    fix default to asan => false

--- .github/matrix.php
+++ .github/matrix.php
@@ -144,8 +144,9 @@ function select_jobs($repository, $trigger, $nightly, $labels, $php_version, $re
         $jobs['SOLARIS'] = true;
     }
     if ($all_jobs || !$no_jobs || $test_windows) {
-        $matrix = [['asan' => true, 'opcache' => true, 'x64' => true, 'zts' => true]];
+        $matrix = [['asan' => false, 'opcache' => true, 'x64' => true, 'zts' => true]];
         if ($all_variations) {
+            $matrix[] = ['asan' => true, 'opcache' => true, 'x64' => true, 'zts' => true],
             $matrix[] = ['asan' => false, 'opcache' => false, 'x64' => false, 'zts' => false];
             if (version_compare($php_version, '8.5', '>=')) {
                 $matrix[] = ['asan' => false, 'opcache' => true, 'x64' => true, 'zts' => true, 'clang' => true];
