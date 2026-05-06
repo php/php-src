191f8e7c0ac463c0b02e1b14f230733515c96b55
Jarne Clauw <67628242+JarneClauw@users.noreply.github.com>
2026-05-06 17:09:16 +0200

    fix order

--- ext/openssl/openssl_backend_common.c
+++ ext/openssl/openssl_backend_common.c
@@ -597,9 +597,9 @@ zend_string* php_openssl_x509_fingerprint(X509 *peer, const char *method, bool r
 		php_error_docref(NULL, E_WARNING, "Unknown digest algorithm");
 		return NULL;
 	} else if (!X509_digest(peer, mdtype, md, &n)) {
+		php_openssl_release_evp_md(mdtype);
 		php_openssl_store_errors();
 		php_error_docref(NULL, E_ERROR, "Could not generate signature");
-		php_openssl_release_evp_md(mdtype);
 		return NULL;
 	}
 
