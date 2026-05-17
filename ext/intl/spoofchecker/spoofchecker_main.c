136e32b1dba57ba7173aa365b28d2b3715de78e5
“LamentXU123” <108666168+LamentXU123@users.noreply.github.com>
2026-05-17 08:58:30 +0800

    Fix setAllowedChars error message

--- ext/intl/spoofchecker/spoofchecker_main.c
+++ ext/intl/spoofchecker/spoofchecker_main.c
@@ -206,9 +206,9 @@ PHP_METHOD(Spoofchecker, setAllowedChars)
 #endif
             pattern_option != (USET_IGNORE_SPACE|USET_CASE_INSENSITIVE) &&
             pattern_option != (USET_IGNORE_SPACE|USET_ADD_CASE_MAPPINGS)) {
-		zend_argument_value_error(2, "must be a valid pattern option, 0 or (SpoofChecker::IGNORE_SPACE|(<none> or SpoofChecker::USET_CASE_INSENSITIVE or SpoofChecker::USET_ADD_CASE_MAPPINGS"
+		zend_argument_value_error(2, "must be a valid pattern option, 0 or (SpoofChecker::IGNORE_SPACE|(<none> or SpoofChecker::CASE_INSENSITIVE or SpoofChecker::ADD_CASE_MAPPINGS"
 #if U_ICU_VERSION_MAJOR_NUM >= 73
-				" or SpoofChecker::USET_SIMPLE_CASE_INSENSITIVE"
+				" or SpoofChecker::SIMPLE_CASE_INSENSITIVE"
 #endif
 				"))"
 		);
