a3084cf42a76203dd4b499bbe8deaa8825003bbc
Calvin Buckley <calvinb@php.net>
2026-05-18 13:17:15 -0300

    Suggestion from Tim, append comma first to avoid needing to count

--- Zend/zend_exceptions.c
+++ Zend/zend_exceptions.c
@@ -547,20 +547,17 @@ static void build_trace_args_list(zval *tmp, smart_str *str) /* {{{ */
 		return;
 	}
 
-	zend_string *name;
-	zval *arg;
-	HashTable *ht = Z_ARRVAL_P(tmp);
-	uint32_t index = 0;
-
-	ZEND_HASH_FOREACH_STR_KEY_VAL(ht, name, arg) {
+	bool first = true;
+	ZEND_HASH_FOREACH_STR_KEY_VAL(Z_ARRVAL_P(tmp), zend_string *name, zval *arg) {
+		if (!first) {
+			smart_str_appends(str, ", ");
+		}
+		first = false;
 		if (name) {
 			smart_str_append(str, name);
 			smart_str_appends(str, ": ");
 		}
 		build_trace_args(arg, str);
-		if (++index != zend_hash_num_elements(ht)) {
-			smart_str_appends(str, ", ");
-		}
 	} ZEND_HASH_FOREACH_END();
 }
 /* }}} */
