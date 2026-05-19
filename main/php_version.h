a846c8f4141ead34ee47bcf3307acc910fe91486
Volker Dusch <edorian@php.net>
2026-05-19 19:09:48 +0200

    PHP-8.5 is now for PHP 8.5.8-dev

--- main/php_version.h
+++ main/php_version.h
@@ -2,7 +2,7 @@
 /* edit configure.ac to change version number */
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 5
-#define PHP_RELEASE_VERSION 7
+#define PHP_RELEASE_VERSION 8
 #define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.5.7-dev"
-#define PHP_VERSION_ID 80507
+#define PHP_VERSION "8.5.8-dev"
+#define PHP_VERSION_ID 80508
