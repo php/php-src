debc17cb80ca798d9176cebd2f2ec91b49ea41d5
Calvin Buckley <calvinb@php.net>
2026-06-10 16:00:12 -0300

    gen_stub: Fix handling of escape sequences in generated C strings
    When handling sequences like this in a stub:
    
    ```php
    <?php
    class Whatever {
        public static string $foobar1 = "CCC \n\r\t\v\e\f\\\$\"\101\x41\u{41} CCC";
        public static string $foobar2 = 'CCC \n\r\t\v\e\f\\\$\"\101\x41\u{41} CCC';
    }
    ```
    
    ...properly generate C headers that properly escape the string.
    Otherwise, the differing escaping rules and differences between PHP's
    single and double quoted strings could lead to mangled headers.
    
    The output of these strings after the stub has been generated:
    
    ```
    string(22) "BEGIN
    
    	\$"AAA END"
    string(43) "BEGIN \n\r\t\v\e\f\\\\$\"\101\x41\u{41} END"
    ```
    
    And the generated arginfo:
    
    ```c
            zval property_doubleQuoteEscaped_default_value;
            zend_string *property_doubleQuoteEscaped_default_value_str = zend_string_init("BEGIN \n\r\t\v\x1b\f\\$\"AAA END", strlen("BEGIN \n\r\t\v\x1b\f\\$\"AAA END"), 1);
    pp      ZVAL_STR(&property_doubleQuoteEscaped_default_value, property_doubleQuoteEscaped_default_value_str);
            zend_string *property_doubleQuoteEscaped_name = zend_string_init("doubleQuoteEscaped", sizeof("doubleQuoteEscaped") - 1, true);
            zend_declare_typed_property(class_entry, property_doubleQuoteEscaped_name, &property_doubleQuoteEscaped_default_value, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_STRING));
            zend_string_release_ex(property_doubleQuoteEscaped_name, true);
    
            zval property_singleQuoteEscaped_default_value;
            zend_string *property_singleQuoteEscaped_default_value_str = zend_string_init("BEGIN \\n\\r\\t\\v\\e\\f\\\\\\\\$\\\"\\101\\x41\\u{41} END", strlen("BEGIN \\n\\r\\t\\v\\e\\f\\\\\\\\$\\\"\\101\\x41\\u{41} END"), 1);
            ZVAL_STR(&property_singleQuoteEscaped_default_value, property_singleQuoteEscaped_default_value_str);
            zend_string *property_singleQuoteEscaped_name = zend_string_init("singleQuoteEscaped", sizeof("singleQuoteEscaped") - 1, true);
            zend_declare_typed_property(class_entry, property_singleQuoteEscaped_name, &property_singleQuoteEscaped_default_value, ZEND_ACC_PUBLIC|ZEND_ACC_STATIC, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_STRING));
            zend_string_release_ex(property_singleQuoteEscaped_name, true);
    ```
    
    Note that the PHP escape sequence "\$" will be handled in a special
    manner to avoid providing it to C, to avoid C compiler warnings.
    
    Tests are included via the zend_test stub and a phpt file to ensure that
    the escape sequences match.
    
    Fixes GH-22169.

--- ext/zend_test/test_decl.h
+++ ext/zend_test/test_decl.h
@@ -1,8 +1,8 @@
 /* This is a generated file, edit test.stub.php instead.
- * Stub hash: 4bb5b467b9d62c0e0c6a7c1e069e8755403a0af9 */
+ * Stub hash: 8ca2fc33013d5a1c325bf5f0090cc6416a242297 */
 
-#ifndef ZEND_TEST_DECL_4bb5b467b9d62c0e0c6a7c1e069e8755403a0af9_H
-#define ZEND_TEST_DECL_4bb5b467b9d62c0e0c6a7c1e069e8755403a0af9_H
+#ifndef ZEND_TEST_DECL_8ca2fc33013d5a1c325bf5f0090cc6416a242297_H
+#define ZEND_TEST_DECL_8ca2fc33013d5a1c325bf5f0090cc6416a242297_H
 
 typedef enum zend_enum_ZendTestUnitEnum {
 	ZEND_ENUM_ZendTestUnitEnum_Foo = 1,
@@ -27,4 +27,4 @@ typedef enum zend_enum_ZendTestEnumWithInterface {
 	ZEND_ENUM_ZendTestEnumWithInterface_Bar = 2,
 } zend_enum_ZendTestEnumWithInterface;
 
-#endif /* ZEND_TEST_DECL_4bb5b467b9d62c0e0c6a7c1e069e8755403a0af9_H */
+#endif /* ZEND_TEST_DECL_8ca2fc33013d5a1c325bf5f0090cc6416a242297_H */
