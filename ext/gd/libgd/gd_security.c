3be49009ce922d9bec6e32f45c32bcdcad16dadc
Pierre Joye <pierre.php@gmail.com>
2026-06-30 16:11:08 +0700

    sync gd_security

--- ext/gd/libgd/gd_security.c
+++ ext/gd/libgd/gd_security.c
@@ -12,39 +12,50 @@
 #include "config.h"
 #endif
 
+#include "gd.h"
+#include "gd_errors.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <limits.h>
-#include "gd.h"
-#include "gd_errors.h"
 
-int overflow2(int a, int b)
-{
+int overflow2(int a, int b) {
 	if(a <= 0 || b <= 0) {
-		gd_error("One parameter to a memory allocation multiplication is negative or zero, failing operation gracefully\n");
+		gd_error_ex(GD_WARNING,
+					"one parameter to a memory allocation multiplication is "
+					"negative or zero, failing operation gracefully\n");
 		return 1;
 	}
 	if(a > INT_MAX / b) {
-		gd_error("Product of memory allocation multiplication would exceed INT_MAX, failing operation gracefully\n");
+		gd_error_ex(GD_WARNING,
+					"Product of memory allocation multiplication would exceed "
+					"INT_MAX, failing operation gracefully\n");
 		return 1;
 	}
 	return 0;
 }
 
-int overflowMul3(int a, int b, int c)
-{
+int overflowMul3(int a, int b, int c) {
 	if (a < 0 || b < 0 || c < 0) {
 		return 1;
 	}
 	if (a == 0 || b == 0 || c == 0) {
 		return 0;
 	}
+	/* check a*b fits in int first */
 	if (a > INT_MAX / b) {
 		return 1;
 	}
+#ifdef HAVE_INT64_T
+	/* check a*b*c fits in int64 */
 	if ((int64_t)a * b > INT64_MAX / c) {
 		return 1;
 	}
+#else
+	/* no 64-bit type available, check against INT_MAX */
+	if (a > INT_MAX / b / c) {
+		return 1;
+	}
+#endif
 	return 0;
 }
