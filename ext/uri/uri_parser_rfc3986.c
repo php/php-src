c1a57ab45960fc0fc78b498879097ca3bbd4653d
Máté Kocsis <kocsismate@woohoolabs.com>
2026-07-03 13:05:25 +0200

    Update ext/uri/uri_parser_rfc3986.c
    Co-authored-by: Tim Düsterhus <timwolla@googlemail.com>

--- ext/uri/uri_parser_rfc3986.c
+++ ext/uri/uri_parser_rfc3986.c
@@ -737,7 +737,7 @@ ZEND_ATTRIBUTE_NONNULL zend_result php_uri_parser_rfc3986_validate_host(const ze
 
 		if (len >= 2 && (p[1] == 'v' || p[1] == 'V')) {
 			return php_uri_parser_rfc3986_validate_component_result(
-				uriIsWellFormedHostIpFutureA(p + 1, p + len - 1) == URI_SUCCESS,
+				uriIsWellFormedHostIpFutureMmA(p + 1, p + len - 1, mm) == URI_SUCCESS,
 				"host"
 			);
 		}
