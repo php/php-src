4a0f186b8abb3c7c0730ced98cdd82c558115703
sepehrphpr <sepehrphpr@gmail.com>
2026-07-04 18:43:51 +0330

    Update grapheme_mask.h

--- ext/intl/grapheme/grapheme_mask.h
+++ ext/intl/grapheme/grapheme_mask.h
@@ -1,8 +1,8 @@
 #ifndef GRAPHEME_MASK_H
 #define GRAPHEME_MASK_H
 
-#include <stdbool.h>
+#include <php.h>
 
-bool grapheme_mask_validate_mask_char(const char *mask_str, size_t mask_str_len);
+PHP_FUNCTION(grapheme_mask);
 
 #endif /* GRAPHEME_MASK_H */
