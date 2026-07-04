f172a163f256f80ceb18584a3bb1eb7df9e618b9
sepehrphpr <sepehrphpr@gmail.com>
2026-07-04 14:35:21 +0330

    Update grapheme_mask.stub.php

--- ext/intl/grapheme/grapheme_mask.stub.php
+++ ext/intl/grapheme/grapheme_mask.stub.php
@@ -1,5 +1,13 @@
 <?php
 
-/** @generate-class-entries */
+/**
+ * @generate-legacy-arginfo
+ * @generate-function-entries
+ */
 
-function grapheme_mask(string $string, string $mask_char, int $offset = 0, ?int $length = null): string|false {}
+function grapheme_mask(
+    string $string,
+    string $mask_char = "*",
+    int $offset = 0,
+    ?int $length = null
+): string|false {}
