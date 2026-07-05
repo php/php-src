9649b6fffbd5a8147ff75616f5e2368a755f363d
sepehrphpr <sepehrphpr@gmail.com>
2026-07-05 13:28:07 +0330

    Update grapheme.c

--- ext/intl/grapheme/grapheme.c
+++ ext/intl/grapheme/grapheme.c
@@ -12,7 +12,7 @@
    +----------------------------------------------------------------------+
    | Authors: Ed Batutis <ed@batutis.com>                                 |
    |          Sara Golemon <pollita@php.net>                              |
-   |          Sepehr mahmoodi <sepehrphpr@gmail.com> (grapheme_mask)     |
+   |          Sepehr mahmoodi <sepehrphpr@gmail.com> (grapheme_mask)      |
    +----------------------------------------------------------------------+
 */
 
@@ -27,100 +27,9 @@
 #include "intl_convert.h"
 #include "intl_data.h"
 #include "grapheme.h"
-#include "grapheme_mask.h"   /* ← تابع grapheme_mask در grapheme_mask.c تعریف شده */
-
-#include <unicode/ubrk.h>
-#include <unicode/ustring.h>
-#include <unicode/utypes.h>
-#include <unicode/utf8.h>
-
-/* ************************************ *
- *   توابع داخلی که قبلاً در grapheme.c
- *   پیاده‌سازی شدن (بدون تغییر)
- * ************************************ */
-
-/* {{{ grapheme_count_graphemes */
-static int32_t grapheme_count_graphemes(const char *str, size_t str_len)
-{
-    UErrorCode status = U_ZERO_ERROR;
-    UBreakIterator *bi;
-    int32_t count = 0;
-    UChar *u_str;
-    int32_t u_str_len;
-
-    if (str_len == 0) {
-        return 0;
-    }
-
-    u_str = (UChar *)emalloc(sizeof(UChar) * (str_len + 1));
-    u_charsToUChars(str, u_str, (int32_t)str_len);
-    u_str[str_len] = 0;
-    u_str_len = (int32_t)str_len;
-
-    bi = ubrk_open(UBRK_CHARACTER, NULL, u_str, u_str_len, &status);
-    if (U_FAILURE(status)) {
-        efree(u_str);
-        return -1;
-    }
-
-    while (ubrk_next(bi) != UBRK_DONE) {
-        count++;
-    }
-
-    ubrk_close(bi);
-    efree(u_str);
-    return count;
-}
-/* }}} */
-
-/* {{{ grapheme_find_boundary */
-static int32_t grapheme_find_boundary(const char *str, size_t str_len, int32_t offset, int direction)
-{
-    UErrorCode status = U_ZERO_ERROR;
-    UBreakIterator *bi;
-    UChar *u_str;
-    int32_t u_str_len, pos = 0, grapheme_count = 0, target = offset;
-
-    if (str_len == 0) {
-        return -1;
-    }
-
-    u_str = (UChar *)emalloc(sizeof(UChar) * (str_len + 1));
-    u_charsToUChars(str, u_str, (int32_t)str_len);
-    u_str[str_len] = 0;
-    u_str_len = (int32_t)str_len;
-
-    bi = ubrk_open(UBRK_CHARACTER, NULL, u_str, u_str_len, &status);
-    if (U_FAILURE(status)) {
-        efree(u_str);
-        return -1;
-    }
-
-    if (direction > 0) {
-        while (grapheme_count < target && (pos = ubrk_next(bi)) != UBRK_DONE) {
-            grapheme_count++;
-        }
-    } else {
-        pos = u_str_len;
-        ubrk_last(bi);
-        while (grapheme_count < (-target) && (pos = ubrk_previous(bi)) != UBRK_DONE) {
-            grapheme_count++;
-        }
-    }
-
-    ubrk_close(bi);
-    efree(u_str);
-    if (grapheme_count != target) {
-        return -1;
-    }
-    return pos;
-}
-/* }}} */
-
-/* ************************************ *
- *   تابع grapheme_mask حذف شده و
- *   در grapheme_mask.c قرار دارد.
- * ************************************ */
+#include "grapheme_util.h"
+#include "grapheme_arginfo.h"
+#include "grapheme_mask.h"   /* ← تابع جدید grapheme_mask */
 
 /* ************************************ *
  *   توابع عمومی موجود در ماژول
@@ -129,179 +38,339 @@ static int32_t grapheme_find_boundary(const char *str, size_t str_len, int32_t o
 /* {{{ PHP_FUNCTION(grapheme_strlen) */
 PHP_FUNCTION(grapheme_strlen)
 {
-    char *str;
-    size_t str_len;
-    int32_t count;
+	char *str;
+	size_t str_len;
+	int32_t count;
 
-    ZEND_PARSE_PARAMETERS_START(1, 1)
-        Z_PARAM_STR(str)
-    ZEND_PARSE_PARAMETERS_END();
+	ZEND_PARSE_PARAMETERS_START(1, 1)
+		Z_PARAM_STRING(str, str_len)
+	ZEND_PARSE_PARAMETERS_END();
 
-    count = grapheme_count_graphemes(str, str_len);
-    if (count >= 0) {
-        RETURN_LONG(count);
-    }
-    RETURN_FALSE;
+	count = grapheme_count_graphemes(str, str_len);
+	if (count >= 0) {
+		RETURN_LONG(count);
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_strpos) */
 PHP_FUNCTION(grapheme_strpos)
 {
-    char *haystack, *needle;
-    size_t haystack_len, needle_len;
-    zend_long offset = 0;
-    int32_t pos;
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_long offset = 0;
+	int32_t pos;
 
-    ZEND_PARSE_PARAMETERS_START(2, 3)
-        Z_PARAM_STR(haystack)
-        Z_PARAM_STR(needle)
-        Z_PARAM_OPTIONAL
-        Z_PARAM_LONG(offset)
-    ZEND_PARSE_PARAMETERS_END();
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(offset)
+	ZEND_PARSE_PARAMETERS_END();
 
-    pos = grapheme_find_boundary(haystack, haystack_len, (int32_t)offset, 1);
-    if (pos >= 0) {
-        char *found = zend_memnstr(haystack + pos, needle, needle_len, haystack + haystack_len);
-        if (found) {
-            RETURN_LONG(found - haystack);
-        }
-    }
-    RETURN_FALSE;
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_strpos_utf8(haystack, haystack_len, needle, needle_len, offset, NULL);
+	if (pos >= 0) {
+		RETURN_LONG(pos);
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_stripos) */
 PHP_FUNCTION(grapheme_stripos)
 {
-    /* Placeholder */
-    zend_throw_error(NULL, "grapheme_stripos not implemented yet");
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_long offset = 0;
+	int32_t pos;
+
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(offset)
+	ZEND_PARSE_PARAMETERS_END();
+
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_stripos_utf8(haystack, haystack_len, needle, needle_len, offset, NULL);
+	if (pos >= 0) {
+		RETURN_LONG(pos);
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_strrpos) */
 PHP_FUNCTION(grapheme_strrpos)
 {
-    RETURN_FALSE;
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_long offset = 0;
+	int32_t pos;
+
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(offset)
+	ZEND_PARSE_PARAMETERS_END();
+
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_strrpos_utf8(haystack, haystack_len, needle, needle_len, offset, NULL);
+	if (pos >= 0) {
+		RETURN_LONG(pos);
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_strripos) */
 PHP_FUNCTION(grapheme_strripos)
 {
-    RETURN_FALSE;
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_long offset = 0;
+	int32_t pos;
+
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(offset)
+	ZEND_PARSE_PARAMETERS_END();
+
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_strripos_utf8(haystack, haystack_len, needle, needle_len, offset, NULL);
+	if (pos >= 0) {
+		RETURN_LONG(pos);
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_substr) */
 PHP_FUNCTION(grapheme_substr)
 {
-    char *str;
-    size_t str_len;
-    zend_long start, length = 0;
-    int32_t start_offset, end_offset;
-    UBreakIterator *bi;
-    UChar *u_str;
-    UErrorCode status = U_ZERO_ERROR;
-    int32_t u_str_len, pos;
-    zend_bool length_is_null = 1;
+	char *str;
+	size_t str_len;
+	zend_long start, length = 0;
+	int32_t start_offset, end_offset, grapheme_len;
+	UChar *u_str = NULL;
+	UErrorCode status = U_ZERO_ERROR;
+	int32_t u_str_len;
+	UBreakIterator *bi = NULL;
+	zend_bool length_is_null = 1;
 
-    ZEND_PARSE_PARAMETERS_START(2, 3)
-        Z_PARAM_STR(str)
-        Z_PARAM_LONG(start)
-        Z_PARAM_OPTIONAL
-        Z_PARAM_LONG_OR_NULL(length, length_is_null)
-    ZEND_PARSE_PARAMETERS_END();
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(str, str_len)
+		Z_PARAM_LONG(start)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG_OR_NULL(length, length_is_null)
+	ZEND_PARSE_PARAMETERS_END();
 
-    u_str_len = (int32_t)str_len;
-    u_str = (UChar *)emalloc(sizeof(UChar) * (u_str_len + 1));
-    u_charsToUChars(str, u_str, u_str_len);
-    u_str[u_str_len] = 0;
+	if (str_len == 0) {
+		RETURN_EMPTY_STRING();
+	}
 
-    bi = ubrk_open(UBRK_CHARACTER, NULL, u_str, u_str_len, &status);
-    if (U_FAILURE(status)) {
-        efree(u_str);
-        RETURN_FALSE;
-    }
+	/* محاسبه تعداد grapheme‌ها */
+	grapheme_len = grapheme_count_graphemes(str, str_len);
+	if (grapheme_len < 0) {
+		RETURN_FALSE;
+	}
 
-    int32_t count = 0;
-    start_offset = 0;
-    if (start < 0) {
-        start = grapheme_count_graphemes(str, str_len) + start;
-        if (start < 0) start = 0;
-    }
-    for (count = 0; count < start; count++) {
-        start_offset = ubrk_following(bi, start_offset);
-    }
+	/* تنظیم start */
+	if (start < 0) {
+		start = grapheme_len + start;
+		if (start < 0) {
+			start = 0;
+		}
+	}
+	if (start >= grapheme_len) {
+		RETURN_FALSE;
+	}
 
-    if (length_is_null) {
-        end_offset = u_str_len;
-    } else {
-        if (length < 0) {
-            length = grapheme_count_graphemes(str, str_len) - start + length;
-            if (length < 0) length = 0;
-        }
-        end_offset = start_offset;
-        for (count = 0; count < length && end_offset < u_str_len; count++) {
-            end_offset = ubrk_following(bi, end_offset);
-        }
-    }
+	/* تنظیم length */
+	if (length_is_null) {
+		length = grapheme_len - start;
+	} else if (length < 0) {
+		length = grapheme_len - start + length;
+		if (length < 0) {
+			RETURN_FALSE;
+		}
+	}
 
-    ubrk_close(bi);
+	if (length == 0) {
+		RETURN_EMPTY_STRING();
+	}
 
-    int32_t out_len = end_offset - start_offset;
-    char *out = (char *)emalloc(out_len + 1);
-    u_strToUTF8(out, out_len + 1, &out_len, u_str + start_offset, end_offset - start_offset, &status);
-    if (U_FAILURE(status)) {
-        efree(u_str);
-        efree(out);
-        RETURN_FALSE;
-    }
-    out[out_len] = '\0';
-    efree(u_str);
-    RETURN_STRINGL(out, out_len);
-    efree(out);
+	/* تبدیل به UChar */
+	u_str_len = (int32_t)str_len;
+	u_str = (UChar *)emalloc(sizeof(UChar) * (u_str_len + 1));
+	u_charsToUChars(str, u_str, u_str_len);
+	u_str[u_str_len] = 0;
+
+	/* ایجاد iterator */
+	bi = ubrk_open(UBRK_CHARACTER, NULL, u_str, u_str_len, &status);
+	if (U_FAILURE(status)) {
+		efree(u_str);
+		RETURN_FALSE;
+	}
+
+	/* پیدا کردن start_offset */
+	start_offset = 0;
+	for (int32_t i = 0; i < start; i++) {
+		start_offset = ubrk_following(bi, start_offset);
+		if (start_offset == UBRK_DONE) {
+			ubrk_close(bi);
+			efree(u_str);
+			RETURN_FALSE;
+		}
+	}
+
+	/* پیدا کردن end_offset */
+	end_offset = start_offset;
+	for (int32_t i = 0; i < length; i++) {
+		end_offset = ubrk_following(bi, end_offset);
+		if (end_offset == UBRK_DONE) {
+			end_offset = u_str_len;
+			break;
+		}
+	}
+
+	ubrk_close(bi);
+
+	/* تبدیل به UTF-8 */
+	int32_t out_capacity = (end_offset - start_offset) * 3 + 1;
+	char *out = (char *)emalloc(out_capacity);
+	int32_t out_len;
+	u_strToUTF8(out, out_capacity, &out_len, u_str + start_offset, end_offset - start_offset, &status);
+
+	efree(u_str);
+
+	if (U_FAILURE(status)) {
+		efree(out);
+		RETURN_FALSE;
+	}
+
+	out[out_len] = '\0';
+	RETURN_STRINGL(out, out_len);
+	efree(out);
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_strstr) */
 PHP_FUNCTION(grapheme_strstr)
 {
-    char *haystack, *needle;
-    size_t haystack_len, needle_len;
-    zend_bool before_needle = 0;
-    char *found;
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_bool before_needle = 0;
+	int32_t pos;
 
-    ZEND_PARSE_PARAMETERS_START(2, 3)
-        Z_PARAM_STR(haystack)
-        Z_PARAM_STR(needle)
-        Z_PARAM_OPTIONAL
-        Z_PARAM_BOOL(before_needle)
-    ZEND_PARSE_PARAMETERS_END();
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_BOOL(before_needle)
+	ZEND_PARSE_PARAMETERS_END();
 
-    found = zend_memnstr(haystack, needle, needle_len, haystack + haystack_len);
-    if (found) {
-        if (before_needle) {
-            RETURN_STRINGL(haystack, found - haystack);
-        } else {
-            RETURN_STRINGL(found, haystack_len - (found - haystack));
-        }
-    }
-    RETURN_FALSE;
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_strpos_utf8(haystack, haystack_len, needle, needle_len, 0, NULL);
+	if (pos >= 0) {
+		if (before_needle) {
+			RETURN_STRINGL(haystack, pos);
+		} else {
+			RETURN_STRINGL(haystack + pos, haystack_len - pos);
+		}
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_stristr) */
 PHP_FUNCTION(grapheme_stristr)
 {
-    zend_throw_error(NULL, "grapheme_stristr not implemented yet");
+	char *haystack, *needle;
+	size_t haystack_len, needle_len;
+	zend_bool before_needle = 0;
+	int32_t pos;
+
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_STRING(needle, needle_len)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_BOOL(before_needle)
+	ZEND_PARSE_PARAMETERS_END();
+
+	if (needle_len == 0) {
+		php_error_docref(NULL, E_WARNING, "Empty needle");
+		RETURN_FALSE;
+	}
+
+	pos = grapheme_stripos_utf8(haystack, haystack_len, needle, needle_len, 0, NULL);
+	if (pos >= 0) {
+		if (before_needle) {
+			RETURN_STRINGL(haystack, pos);
+		} else {
+			RETURN_STRINGL(haystack + pos, haystack_len - pos);
+		}
+	}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ PHP_FUNCTION(grapheme_extract) */
 PHP_FUNCTION(grapheme_extract)
 {
-    RETURN_FALSE;
+	char *haystack, *next;
+	size_t haystack_len;
+	zend_long size, start = 0;
+	int32_t extracted_len, next_offset;
+	UErrorCode status = U_ZERO_ERROR;
+
+	ZEND_PARSE_PARAMETERS_START(2, 3)
+		Z_PARAM_STRING(haystack, haystack_len)
+		Z_PARAM_LONG(size)
+		Z_PARAM_OPTIONAL
+		Z_PARAM_LONG(start)
+	ZEND_PARSE_PARAMETERS_END();
+
+	if (start < 0) {
+		start = 0;
+	}
+	if ((size_t)start >= haystack_len) {
+		RETURN_FALSE;
+	}
+
+	extracted_len = grapheme_extract_utf8(haystack + start, haystack_len - start,
+										  size, &next_offset, &status);
+
+	if (extracted_len <= 0 || U_FAILURE(status)) {
+		RETURN_FALSE;
+	}
+
+	RETURN_STRINGL(haystack + start, extracted_len);
 }
 /* }}} */
 
@@ -311,17 +380,17 @@ PHP_FUNCTION(grapheme_extract)
 
 /* {{{ grapheme_functions[] */
 static const zend_function_entry grapheme_functions[] = {
-    PHP_FE(grapheme_strlen, arginfo_grapheme_strlen)
-    PHP_FE(grapheme_strpos, arginfo_grapheme_strpos)
-    PHP_FE(grapheme_stripos, arginfo_grapheme_stripos)
-    PHP_FE(grapheme_strrpos, arginfo_grapheme_strrpos)
-    PHP_FE(grapheme_strripos, arginfo_grapheme_strripos)
-    PHP_FE(grapheme_substr, arginfo_grapheme_substr)
-    PHP_FE(grapheme_strstr, arginfo_grapheme_strstr)
-    PHP_FE(grapheme_stristr, arginfo_grapheme_stristr)
-    PHP_FE(grapheme_extract, arginfo_grapheme_extract)
-    PHP_FE(grapheme_mask, arginfo_grapheme_mask)   /* ← تابع جدید */
-    PHP_FE_END
+	PHP_FE(grapheme_strlen,	arginfo_grapheme_strlen)
+	PHP_FE(grapheme_strpos,	arginfo_grapheme_strpos)
+	PHP_FE(grapheme_stripos,	arginfo_grapheme_stripos)
+	PHP_FE(grapheme_strrpos,	arginfo_grapheme_strrpos)
+	PHP_FE(grapheme_strripos,	arginfo_grapheme_strripos)
+	PHP_FE(grapheme_substr,	arginfo_grapheme_substr)
+	PHP_FE(grapheme_strstr,	arginfo_grapheme_strstr)
+	PHP_FE(grapheme_stristr,	arginfo_grapheme_stristr)
+	PHP_FE(grapheme_extract,	arginfo_grapheme_extract)
+	PHP_FE(grapheme_mask,		arginfo_grapheme_mask)   /* ← تابع جدید */
+	PHP_FE_END
 };
 /* }}} */
 
@@ -332,31 +401,31 @@ static const zend_function_entry grapheme_functions[] = {
 /* {{{ PHP_MINIT_FUNCTION */
 PHP_MINIT_FUNCTION(grapheme)
 {
-    return SUCCESS;
+	return SUCCESS;
 }
 /* }}} */
 
 /* {{{ PHP_MINFO_FUNCTION */
 PHP_MINFO_FUNCTION(grapheme)
 {
-    php_info_print_table_start();
-    php_info_print_table_row(2, "grapheme support", "enabled");
-    php_info_print_table_end();
+	php_info_print_table_start();
+	php_info_print_table_row(2, "grapheme support", "enabled");
+	php_info_print_table_end();
 }
 /* }}} */
 
 /* {{{ grapheme_module_entry */
 zend_module_entry grapheme_module_entry = {
-    STANDARD_MODULE_HEADER,
-    "grapheme",
-    grapheme_functions,
-    PHP_MINIT(grapheme),
-    NULL, /* MSHUTDOWN */
-    NULL, /* RINIT */
-    NULL, /* RSHUTDOWN */
-    PHP_MINFO(grapheme),
-    PHP_GRAPHEME_VERSION,
-    STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_HEADER,
+	"grapheme",
+	grapheme_functions,
+	PHP_MINIT(grapheme),
+	NULL,	/* MSHUTDOWN */
+	NULL,	/* RINIT */
+	NULL,	/* RSHUTDOWN */
+	PHP_MINFO(grapheme),
+	PHP_GRAPHEME_VERSION,
+	STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
