f90e9ae8b9782ae51fb77645d00346da5c2b58f8
sepehrphpr <sepehrphpr@gmail.com>
2026-07-05 14:04:31 +0330

    Update grapheme_util.h

--- ext/intl/grapheme/grapheme_util.h
+++ ext/intl/grapheme/grapheme_util.h
@@ -30,4 +30,29 @@ int32_t grapheme_strpos_utf8(const char *haystack, size_t haystack_len,
 
 int32_t grapheme_stripos_utf8(const char *haystack, size_t haystack_len,
                               const char *needle, size_t needle_len,
-                              int32_t offset, int32
+                              int32_t offset, int32_t *puchar_pos);
+
+int32_t grapheme_strrpos_utf8(const char *haystack, size_t haystack_len,
+                              const char *needle, size_t needle_len,
+                              int32_t offset, int32_t *puchar_pos);
+
+int32_t grapheme_strripos_utf8(const char *haystack, size_t haystack_len,
+                               const char *needle, size_t needle_len,
+                               int32_t offset, int32_t *puchar_pos);
+
+int32_t grapheme_extract_utf8(const char *str, size_t str_len,
+                              int32_t size, int32_t extract_type,
+                              int32_t start, int32_t *next_pos);
+
+UText *grapheme_open_utext(UText *ut, const char *str, size_t str_len);
+
+#define GRAPHEME_STRPOS_UTF8_MAXLEN INT32_MAX
+
+#ifdef __cplusplus
+}
+#endif
+
+/* OUTSIDE_STRING: check if (possibly negative) long offset is outside the string with int32_t length */
+#define OUTSIDE_STRING(offset, max_len) ( offset <= INT32_MIN || offset > INT32_MAX || (offset < 0 ? -offset > (zend_long) max_len : offset > (zend_long) max_len) )
+
+#endif // GRAPHEME_GRAPHEME_UTIL_H
