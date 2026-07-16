27968574f1a2985e7d73df90edb12877c1302d35
Ilia Alshanetsky <ilia@ilia.ws>
2026-07-16 12:59:43 -0400

    ext/standard: check the from address for control characters before PASS
    GH-17976 sanitized from and user_agent where the HTTP wrapper emits headers,
    and left the FTP wrapper for a follow-up. php_ftp_fopen_connect() sends
    FG(from_address) as the anonymous password without a check, so an ini_set()
    value carrying CRLF puts extra commands on the control channel. Apply
    PHP_FTP_CNTRL_CHK, as the branch that sends a URL-supplied password already
    does.
    
    Closes GH-22770

--- ext/standard/ftp_fopen_wrapper.c
+++ ext/standard/ftp_fopen_wrapper.c
@@ -269,6 +269,8 @@ static php_stream *php_ftp_fopen_connect(php_stream_wrapper *wrapper, const char
 			/* if the user has configured who they are,
 			   send that as the password */
 			if (FG(from_address)) {
+				PHP_FTP_CNTRL_CHK(ZSTR_VAL(FG(from_address)), ZSTR_LEN(FG(from_address)), "Invalid password %s")
+
 				php_stream_printf(stream, "PASS %s\r\n", ZSTR_VAL(FG(from_address)));
 			} else {
 				php_stream_write_string(stream, "PASS anonymous\r\n");
