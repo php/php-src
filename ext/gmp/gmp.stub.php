c459eb72a0c167887ed52c73a267f65f8c3c737a
Weilin Du <weilindu@php.net>
2026-07-28 22:38:58 +0800

    Update ext/gmp/gmp.stub.php
    Co-authored-by: Arnaud Le Blanc <365207+arnaud-lb@users.noreply.github.com>

--- ext/gmp/gmp.stub.php
+++ ext/gmp/gmp.stub.php
@@ -188,7 +188,7 @@ function gmp_hamdist(GMP|int|string $num1, GMP|int|string $num2): int {}
 function gmp_nextprime(GMP|int|string $num): GMP {}
 
 #ifdef HAVE___GMPZ_PREVPRIME
-function gmp_prevprime(GMP|int|string $num, ?bool &$definitely_prime = null): GMP {}
+function gmp_prevprime(GMP|int|string $num, &$definitely_prime = null): GMP {}
 #endif
 
 function gmp_binomial(GMP|int|string $n, int $k): GMP {}
