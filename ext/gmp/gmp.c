1a7d07809a0f5af66ae7d54368cc84386e9ab9be
Weilin Du <weilindu@php.net>
2026-07-28 20:38:30 +0800

    update the UNEXPECTED gate

--- ext/gmp/gmp.c
+++ ext/gmp/gmp.c
@@ -1234,9 +1234,9 @@ ZEND_FUNCTION(gmp_powm_sec)
 		RETURN_THROWS();
 	}
 
-	if (!mpz_odd_p(gmpnum_mod)) {
+	if (UNEXPECTED(!mpz_odd_p(gmpnum_mod))) {
 		/* Zero is an even modulus, but report it like gmp_powm() does. */
-		if (UNEXPECTED(!mpz_cmp_ui(gmpnum_mod, 0))) {
+		if (!mpz_cmp_ui(gmpnum_mod, 0)) {
 			zend_argument_error(zend_ce_division_by_zero_error, 3, "Modulo by zero");
 		} else {
 			zend_argument_value_error(3, "must be odd");
