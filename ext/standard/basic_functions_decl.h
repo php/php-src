4b26ff1698b96e00682a2afd063ff01dc915d478
Weilin Du <weilindu@php.net>
2026-08-11 03:02:18 +0800

    RFC: Deprecate metaphone() (#21889)
    This implement RFC:
    https://wiki.php.net/rfc/deprecations_php_8_6#deprecate_metaphone_function
    which deprecate the metaphone function.

--- ext/standard/basic_functions_decl.h
+++ ext/standard/basic_functions_decl.h
@@ -1,8 +1,8 @@
 /* This is a generated file, edit basic_functions.stub.php instead.
- * Stub hash: 21b2089bd39aadeeb8730e4bb2fecbb32e65d1c8 */
+ * Stub hash: 8f5682d85611126ee12182af46cb2905a0bd0e57 */
 
-#ifndef ZEND_BASIC_FUNCTIONS_DECL_21b2089bd39aadeeb8730e4bb2fecbb32e65d1c8_H
-#define ZEND_BASIC_FUNCTIONS_DECL_21b2089bd39aadeeb8730e4bb2fecbb32e65d1c8_H
+#ifndef ZEND_BASIC_FUNCTIONS_DECL_8f5682d85611126ee12182af46cb2905a0bd0e57_H
+#define ZEND_BASIC_FUNCTIONS_DECL_8f5682d85611126ee12182af46cb2905a0bd0e57_H
 
 typedef enum zend_enum_SortDirection {
 	ZEND_ENUM_SortDirection_Ascending = 1,
@@ -20,4 +20,4 @@ typedef enum zend_enum_RoundingMode {
 	ZEND_ENUM_RoundingMode_PositiveInfinity = 8,
 } zend_enum_RoundingMode;
 
-#endif /* ZEND_BASIC_FUNCTIONS_DECL_21b2089bd39aadeeb8730e4bb2fecbb32e65d1c8_H */
+#endif /* ZEND_BASIC_FUNCTIONS_DECL_8f5682d85611126ee12182af46cb2905a0bd0e57_H */
