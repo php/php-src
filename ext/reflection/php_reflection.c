d3f4eada1001627df6330e7606081e7ab42ca311
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2026-08-11 15:15:37 -0700

    [RFC] Deprecate `ReflectionMethod` invocation for static methods with objects
    `ReflectionMethod::invoke()` and `::invokeArgs()` are affected, but share
    implementation code and so are updated together.
    
    https://wiki.php.net/rfc/deprecations_php_8_6

--- ext/reflection/php_reflection.c
+++ ext/reflection/php_reflection.c
@@ -3375,7 +3375,21 @@ static void reflection_method_invoke(INTERNAL_FUNCTION_PARAMETERS, bool variadic
 	 * Else, we verify that the given object is an instance of the class.
 	 */
 	if (mptr->common.fn_flags & ZEND_ACC_STATIC) {
-		object = NULL;
+		if (object) {
+			zend_string *method_name = get_active_function_or_method_name();
+			zend_error(
+				E_DEPRECATED,
+				"Calling %pS() for static method %pS::%pS() does not need an object parameter",
+				method_name,
+				mptr->common.scope->name,
+				mptr->common.function_name
+			);
+			zend_string_release(method_name);
+			if (UNEXPECTED(EG(exception))) {
+				RETURN_THROWS();
+			}
+			object = NULL;
+		} 
 		obj_ce = mptr->common.scope;
 	} else {
 		if (!object) {
