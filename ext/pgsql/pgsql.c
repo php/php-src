19869fee9e807ff598d68772a78ace92cc97ad70
Weilin Du <weilindu@php.net>
2026-08-13 01:21:50 +0800

    feedback

--- ext/pgsql/pgsql.c
+++ ext/pgsql/pgsql.c
@@ -3427,15 +3427,13 @@ static zend_result pgsql_copy_from_query(PGconn *pgsql, PGresult *pgsql_result, 
 	}
 
 	int result;
-	if (ZSTR_LEN(tmp) == 0) {
-		result = PQputCopyData(pgsql, ZSTR_VAL(tmp), 0);
-	} else if (!zend_string_ends_with_literal(tmp, "\n")) {
+	if (ZSTR_LEN(tmp) == 0 || zend_string_ends_with_literal(tmp, "\n")) {
+		result = PQputCopyData(pgsql, ZSTR_VAL(tmp), ZSTR_LEN(tmp));
+	} else {
 		char *zquery = zend_cstr_append_char(
 			ZSTR_VAL(tmp), ZSTR_LEN(tmp), '\n');
 		result = PQputCopyData(pgsql, zquery, ZSTR_LEN(tmp) + 1);
 		efree(zquery);
-	} else {
-		result = PQputCopyData(pgsql, ZSTR_VAL(tmp), ZSTR_LEN(tmp));
 	}
 
 	zend_tmp_string_release(tmp_tmp);
