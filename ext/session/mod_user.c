2b96d1137939f0473e4cac4f11f3e57302a9d832
Ilia Alshanetsky <ilia@ilia.ws>
2026-08-24 16:14:44 -0400

    ext/session: Clear in_save_handler recursion guard on bailout
    ps_call_handler() set PS(in_save_handler) around call_user_function() with
    no protection against a zend_bailout() from inside a userland save handler,
    so a fatal error in open/read/write/close left the flag sticky: the shutdown
    flush then reported "Cannot call session save handler in a recursive manner"
    and skipped the pending handler calls. Wrap the invocation in
    zend_try/zend_catch, clear the flag unconditionally once the block has run,
    and defer the re-bail until after the argument cleanup. bug60634_error_3.phpt
    and bug60634_error_4.phpt expected the sticky-flag warning and now expect the
    close handler to run.
    
    Fixes GH-15529

--- ext/session/mod_user.c
+++ ext/session/mod_user.c
@@ -26,22 +26,30 @@ const ps_module ps_mod_user = {
 static void ps_call_handler(zval *func, int argc, zval *argv, zval *retval)
 {
 	int i;
+	bool bailout = false;
 	if (PS(in_save_handler)) {
 		PS(in_save_handler) = 0;
 		ZVAL_UNDEF(retval);
 		php_error_docref(NULL, E_WARNING, "Cannot call session save handler in a recursive manner");
 	} else {
 		PS(in_save_handler) = 1;
-		if (call_user_function(NULL, NULL, func, retval, argc, argv) == FAILURE) {
-			zval_ptr_dtor(retval);
-			ZVAL_UNDEF(retval);
-		} else if (Z_ISUNDEF_P(retval)) {
-			ZVAL_NULL(retval);
-		}
+		zend_try {
+			if (call_user_function(NULL, NULL, func, retval, argc, argv) == FAILURE) {
+				zval_ptr_dtor(retval);
+				ZVAL_UNDEF(retval);
+			} else if (Z_ISUNDEF_P(retval)) {
+				ZVAL_NULL(retval);
+			}
+		} zend_catch {
+			bailout = true;
+		} zend_end_try();
 		PS(in_save_handler) = 0;
 	}
 	for (i = 0; i < argc; i++) {
 		zval_ptr_dtor(&argv[i]);
+	}
+	if (bailout) {
+		zend_bailout();
 	}
 }
 
