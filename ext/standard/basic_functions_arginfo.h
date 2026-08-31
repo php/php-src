b46e489e9351dd6d76a1aa6a7afa5e3e19e94031
Sepehr <sepehrphpr@gmail.com>
2026-08-31 15:46:50 +0330

    Fix array_str_contains declaration and update stubs

--- ext/standard/basic_functions_arginfo.h
+++ ext/standard/basic_functions_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit basic_functions.stub.php instead.
- * Stub hash: 4feeab72edf6b7440af99a60e1401492828f141e
+ * Stub hash: 60b6f8419e496b944ad567d9d496c01f51f47ec8
  * Has decl header: yes */
 
 #include "zend_attributes.h"
@@ -156,15 +156,16 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_array_walk, 0, 2, IS_TRUE, 0)
 ZEND_END_ARG_INFO()
 
 #define arginfo_array_walk_recursive arginfo_array_walk
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_array_str_contains, 0, 2, IS_ARRAY, 0)
-	ZEND_ARG_TYPE_INFO(0, haystack, IS_ARRAY, 0)
-	ZEND_ARG_TYPE_INFO(0, needle, IS_STRING, 0)
-ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_in_array, 0, 2, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, needle, IS_MIXED, 0)
 	ZEND_ARG_TYPE_INFO(0, haystack, IS_ARRAY, 0)
 	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, strict, _IS_BOOL, 0, "false")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_array_str_contains, 0, 2, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, haystack, IS_ARRAY, 0)
+	ZEND_ARG_TYPE_INFO(0, needle, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_array_search, 0, 2, MAY_BE_LONG|MAY_BE_STRING|MAY_BE_FALSE)
@@ -2403,6 +2404,7 @@ ZEND_FUNCTION(clamp);
 ZEND_FUNCTION(array_walk);
 ZEND_FUNCTION(array_walk_recursive);
 ZEND_FUNCTION(in_array);
+ZEND_FUNCTION(array_str_contains);
 ZEND_FUNCTION(array_search);
 ZEND_FUNCTION(extract);
 ZEND_FUNCTION(compact);
@@ -3000,6 +3002,7 @@ static const zend_function_entry ext_functions[] = {
 	ZEND_FE(array_walk, arginfo_array_walk)
 	ZEND_FE(array_walk_recursive, arginfo_array_walk_recursive)
 	ZEND_RAW_FENTRY("in_array", zif_in_array, arginfo_in_array, ZEND_ACC_COMPILE_TIME_EVAL, frameless_function_infos_in_array, NULL)
+	ZEND_FE(array_str_contains, arginfo_array_str_contains)
 	ZEND_RAW_FENTRY("array_search", zif_array_search, arginfo_array_search, ZEND_ACC_COMPILE_TIME_EVAL, NULL, NULL)
 #if (PHP_VERSION_ID >= 80600)
 	ZEND_RAW_FENTRY("extract", zif_extract, arginfo_extract, ZEND_FENTRY_FLAGS(0, ZEND_ACC2_FORBID_DYN_CALLS), NULL, NULL)
@@ -3573,9 +3576,7 @@ static const zend_function_entry ext_functions[] = {
 	ZEND_FE(sapi_windows_cp_is_utf8, arginfo_sapi_windows_cp_is_utf8)
 	ZEND_FE(sapi_windows_set_ctrl_handler, arginfo_sapi_windows_set_ctrl_handler)
 	ZEND_FE(sapi_windows_generate_ctrl_event, arginfo_sapi_windows_generate_ctrl_event)
-
 #endif
-	ZEND_FE(array_str_contains, arginfo_array_str_contains)
 	ZEND_FE_END
 };
 
