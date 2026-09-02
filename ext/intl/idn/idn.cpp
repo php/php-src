6267d42edc3c6ebc22b0353b3aefacc377a44ae4
lacatoire <la.catoire@gmail.com>
2026-09-02 11:06:37 +0200

    ext/intl: fix the format specifier in the domain length error
    The message concatenated PRId32 as a literal instead of using it as a
    conversion, so it read "must be less than d bytes" and INT32_MAX was
    never printed.

--- ext/intl/idn/idn.cpp
+++ ext/intl/idn/idn.cpp
@@ -122,7 +122,7 @@ static void php_intl_idn_handoff(INTERNAL_FUNCTION_PARAMETERS, int mode)
 		RETURN_THROWS();
 	}
 	if (UNEXPECTED(ZSTR_LEN(domain) > INT32_MAX - 1)) {
-		zend_argument_value_error(1, "must be less than " PRId32 " bytes", INT32_MAX);
+		zend_argument_value_error(1, "must be less than %" PRId32 " bytes", INT32_MAX);
 		RETURN_THROWS();
 	}
 	if (variant != INTL_IDN_VARIANT_UTS46) {
