47a27e71d9562ab746cfe3e2e341b219412f9fad
Matthieu Napoli <matthieu@mnapoli.fr>
2026-09-03 17:03:07 +0200

    Add `--enable-cli-fpm` to link the FPM SAPI into the CLI binary (`php --fpm`)

---
+++ sapi/fpm/config0.m4
@@ -0,0 +1,6 @@
+PHP_ARG_ENABLE([fpm],
+  [for FPM build],
+  [AS_HELP_STRING([--enable-fpm],
+    [Enable building of the fpm SAPI executable])],
+  [no],
+  [no])
--- sapi/fpm/fpm/fpm_main_arginfo.h
+++ sapi/fpm/fpm/fpm_main_arginfo.h
@@ -1,5 +1,5 @@
 /* This is a generated file, edit fpm_main.stub.php instead.
- * Stub hash: b4ac4c0f1d91c354293e21185a2e6d9f99cc9fcc */
+ * Stub hash: 464cd4b4281d68f6368597bd3f890437459e77b7 */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_fastcgi_finish_request, 0, 0, _IS_BOOL, 0)
 ZEND_END_ARG_INFO()
@@ -13,13 +13,13 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_fpm_get_status, 0, 0, MAY_BE_ARR
 ZEND_END_ARG_INFO()
 
 ZEND_FUNCTION(fastcgi_finish_request);
-ZEND_FUNCTION(apache_request_headers);
+ZEND_FUNCTION(fpm_apache_request_headers);
 ZEND_FUNCTION(fpm_get_status);
 
 static const zend_function_entry ext_functions[] = {
 	ZEND_FE(fastcgi_finish_request, arginfo_fastcgi_finish_request)
-	ZEND_FE(apache_request_headers, arginfo_apache_request_headers)
-	ZEND_RAW_FENTRY("getallheaders", zif_apache_request_headers, arginfo_getallheaders, 0, NULL, NULL)
+	ZEND_RAW_FENTRY("apache_request_headers", zif_fpm_apache_request_headers, arginfo_apache_request_headers, 0, NULL, NULL)
+	ZEND_RAW_FENTRY("getallheaders", zif_fpm_apache_request_headers, arginfo_getallheaders, 0, NULL, NULL)
 	ZEND_FE(fpm_get_status, arginfo_fpm_get_status)
 	ZEND_FE_END
 };
