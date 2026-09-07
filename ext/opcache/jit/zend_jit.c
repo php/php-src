28b467b1dbd6d80b895fbd5431734f3e251550d7
Marc <m@pyc.ac>
2026-09-07 14:13:48 +0200

    simplify

--- ext/opcache/jit/zend_jit.c
+++ ext/opcache/jit/zend_jit.c
@@ -3100,30 +3100,10 @@ jit_failure:
 	return FAILURE;
 }
 
-#if defined(__GNUC__) && !defined(__clang__) && ZEND_VM_KIND == ZEND_VM_KIND_TAILCALL
 /* GCC cannot tail-call from a function that uses setjmp. */
 static zend_never_inline void zend_runtime_jit_compile(zend_op_array *op_array)
 {
 	const zend_op *opline = op_array->opcodes;
-#else
-/* Run-time JIT handler */
-#if ZEND_VM_KIND == ZEND_VM_KIND_CALL || ZEND_VM_KIND == ZEND_VM_KIND_TAILCALL
-static ZEND_OPCODE_HANDLER_RET ZEND_OPCODE_HANDLER_CCONV zend_runtime_jit(ZEND_OPCODE_HANDLER_ARGS)
-#else
-static ZEND_OPCODE_HANDLER_RET ZEND_OPCODE_HANDLER_FUNC_CCONV zend_runtime_jit(ZEND_OPCODE_HANDLER_ARGS)
-#endif
-{
-#if GCC_GLOBAL_REGS
-	zend_execute_data *execute_data;
-	zend_op *opline;
-#else
-	const zend_op *orig_opline = opline;
-#endif
-
-	execute_data = EG(current_execute_data);
-	zend_op_array *op_array = &EX(func)->op_array;
-	opline = op_array->opcodes;
-#endif
 	zend_jit_op_array_extension *jit_extension;
 	bool do_bailout = 0;
 
@@ -3161,16 +3141,23 @@ static ZEND_OPCODE_HANDLER_RET ZEND_OPCODE_HANDLER_FUNC_CCONV zend_runtime_jit(Z
 	if (do_bailout) {
 		zend_bailout();
 	}
-
-#if defined(__GNUC__) && !defined(__clang__) && ZEND_VM_KIND == ZEND_VM_KIND_TAILCALL
 }
 
+/* Run-time JIT handler */
+#if ZEND_VM_KIND == ZEND_VM_KIND_CALL || ZEND_VM_KIND == ZEND_VM_KIND_TAILCALL
 static ZEND_OPCODE_HANDLER_RET ZEND_OPCODE_HANDLER_CCONV zend_runtime_jit(ZEND_OPCODE_HANDLER_ARGS)
+#else
+static ZEND_OPCODE_HANDLER_RET ZEND_OPCODE_HANDLER_FUNC_CCONV zend_runtime_jit(ZEND_OPCODE_HANDLER_ARGS)
+#endif
 {
+#if GCC_GLOBAL_REGS
+	zend_execute_data *execute_data;
+#else
 	const zend_op *orig_opline = opline;
+#endif
+
 	execute_data = EG(current_execute_data);
 	zend_runtime_jit_compile(&EX(func)->op_array);
-#endif
 
 	/* JIT-ed code is going to be called by VM */
 #if GCC_GLOBAL_REGS
