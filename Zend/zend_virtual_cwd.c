ab7759262a7dc8f3f3f9f5a86d674e8252ff5454
henderkes <m@pyc.ac>
2026-09-17 22:49:52 +0200

    remove realcache enabled check

--- Zend/zend_virtual_cwd.c
+++ Zend/zend_virtual_cwd.c
@@ -1361,8 +1361,7 @@ CWD_API int virtual_access(const char *pathname, int mode) /* {{{ */
 	int ret;
 
 #ifndef ZEND_WIN32
-	if (pathname[0] == '/' &&
-		(CWDG(realpath_cache_size_limit) || virtual_path_is_direct(pathname))) {
+	if (pathname[0] == '/') {
 		return access(pathname, mode);
 	}
 #endif
@@ -1583,8 +1582,7 @@ CWD_API int virtual_stat(const char *path, zend_stat_t *buf) /* {{{ */
 	int retval;
 
 #ifndef ZEND_WIN32
-	if (path[0] == '/' &&
-		(CWDG(realpath_cache_size_limit) || virtual_path_is_direct(path))) {
+	if (path[0] == '/') {
 		return php_sys_stat(path, buf);
 	}
 #endif
