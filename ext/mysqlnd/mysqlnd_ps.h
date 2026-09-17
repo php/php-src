bd9529d30d4224be41836ac550555092e9299f41
Kamil Tekiela <tekiela246@gmail.com>
2026-09-17 17:41:16 +0100

    Drop _mysqlnd_init_ps_fetch_subsystem (#23727)

--- ext/mysqlnd/mysqlnd_ps.h
+++ ext/mysqlnd/mysqlnd_ps.h
@@ -26,12 +26,11 @@ struct st_mysqlnd_perm_bind {
 	unsigned int php_type;
 };
 
-extern struct st_mysqlnd_perm_bind mysqlnd_ps_fetch_functions[MYSQL_TYPE_LAST + 1];
+extern const struct st_mysqlnd_perm_bind mysqlnd_ps_fetch_functions[MYSQL_TYPE_LAST + 1];
 
 enum_func_status mysqlnd_fetch_stmt_row_cursor(MYSQLND_RES * result, zval **row_data, const unsigned int flags, bool * fetched_anything);
 
 void _mysqlnd_init_ps_subsystem(void);/* This one is private, mysqlnd_library_init() will call it */
-void _mysqlnd_init_ps_fetch_subsystem(void);
 
 void ps_fetch_from_1_to_8_bytes(zval * zv, const MYSQLND_FIELD * const field, const unsigned int pack_len, const zend_uchar ** row, unsigned int byte_count);
 
