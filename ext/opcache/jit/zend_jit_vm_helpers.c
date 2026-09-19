07f36d2cf76cf5e76374097132d8be31e5d2f1eb
yqtian-se <yqtian668@gmail.com>
2026-09-19 14:50:54 +0000

    Use Zend diagnostic macros for global registers

--- ext/opcache/jit/zend_jit_vm_helpers.c
+++ ext/opcache/jit/zend_jit_vm_helpers.c
@@ -32,8 +32,7 @@
 #include "zend_jit_internal.h"
 
 #ifdef HAVE_GCC_GLOBAL_REGS
-# pragma GCC diagnostic push
-# pragma GCC diagnostic ignored "-Wvolatile-register-var"
+ZEND_DIAGNOSTIC_IGNORED_START("-Wvolatile-register-var")
 # if defined(__x86_64__)
 register zend_execute_data* volatile execute_data __asm__("%r14");
 register const zend_op* volatile opline __asm__("%r15");
@@ -44,7 +43,7 @@ register const zend_op* volatile opline __asm__("%edi");
 register zend_execute_data* volatile execute_data __asm__("x27");
 register const zend_op* volatile opline __asm__("x28");
 # endif
-# pragma GCC diagnostic pop
+ZEND_DIAGNOSTIC_IGNORED_END
 #endif
 
 ZEND_OPCODE_HANDLER_RET ZEND_FASTCALL zend_jit_leave_nested_func_helper(uint32_t call_info EXECUTE_DATA_DC)
