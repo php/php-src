0be211a0cf752780881a9611224f8adc516af573
Pierrick Charron <pierrick@php.net>
2026-09-22 04:44:13 -0400

    Update versions for PHP 8.2.34

--- main/php_version.h
+++ main/php_version.h
@@ -3,6 +3,6 @@
 #define PHP_MAJOR_VERSION 8
 #define PHP_MINOR_VERSION 2
 #define PHP_RELEASE_VERSION 34
-#define PHP_EXTRA_VERSION "-dev"
-#define PHP_VERSION "8.2.34-dev"
+#define PHP_EXTRA_VERSION ""
+#define PHP_VERSION "8.2.34"
 #define PHP_VERSION_ID 80234
