3987f2e7f0eb93ecd30e65b243bd612168b634f3
Matteo Beccati <mbeccati@php.net>
2026-09-23 09:22:46 +0000

    Prepare for PHP 8.6.0RC2

--- Zend/zend_modules.h
+++ Zend/zend_modules.h
@@ -30,7 +30,7 @@
 #define ZEND_MODULE_INFO_FUNC_ARGS zend_module_entry *zend_module
 #define ZEND_MODULE_INFO_FUNC_ARGS_PASSTHRU zend_module
 
-#define ZEND_MODULE_API_NO 20250926
+#define ZEND_MODULE_API_NO 20260924
 #ifdef ZTS
 #define USING_ZTS 1
 #else
