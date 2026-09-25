0a24a2c73fae602abcd81e983ccecf49866953b5
Tim Düsterhus <tim@tideways-gmbh.com>
2026-09-25 15:26:51 +0200

    standard: Take `HashTable*` instead of `zval*` in `php_explode()`

--- ext/standard/php_string.h
+++ ext/standard/php_string.h
@@ -50,7 +50,7 @@ PHPAPI zend_string *php_trim(zend_string *str, const char *what, size_t what_len
 PHPAPI size_t php_strip_tags(char *rbuf, size_t len, const char *allow, size_t allow_len);
 PHPAPI size_t php_strip_tags_ex(char *rbuf, size_t len, const char *allow, size_t allow_len, bool allow_tag_spaces);
 PHPAPI void php_implode(const zend_string *delim, HashTable *arr, zval *return_value);
-PHPAPI void php_explode(const zend_string *delim, zend_string *str, zval *return_value, zend_long limit);
+PHPAPI void php_explode(const zend_string *delim, zend_string *str, HashTable *parts, zend_long limit);
 
 PHPAPI size_t php_strspn(const char *s1, const char *s2, const char *s1_end, const char *s2_end);
 PHPAPI size_t php_strcspn(const char *s1, const char *s2, const char *s1_end, const char *s2_end);
