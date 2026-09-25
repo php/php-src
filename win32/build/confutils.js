e658a10f3e49c85f494f13a285f4fa3575cd2097
Shivam Mathur <shivam_jpr@hotmail.com>
2026-09-25 19:57:47 +0530

    Respect requested C standard

--- win32/build/confutils.js
+++ win32/build/confutils.js
@@ -1811,7 +1811,7 @@ function ADD_SOURCES(dir, file_list, target, obj_dir, duplicate_sources)
 					obj = filename.replace(re, ".obj");
 					var c11_flag = VS_TOOLSET && !cxx_mode_targets[target] && /\.c$/i.test(src) ? " /std:c11" : "";
 
-					MFO.WriteLine("\t" + CMD_MOD1 + "$(CC) $(" + flags + ") $(CFLAGS)" + c11_flag + " $(" + bd_flags_name + ") /c " + dir + "\\" + src + " /Fo" + sub_build + d + obj);
+					MFO.WriteLine("\t" + CMD_MOD1 + "$(CC)" + c11_flag + " $(" + flags + ") $(CFLAGS) $(" + bd_flags_name + ") /c " + dir + "\\" + src + " /Fo" + sub_build + d + obj);
 
 					if ("clang" == PHP_ANALYZER) {
 						MFO.WriteLine("\t" + CMD_MOD1 + "\"$(CLANG_CL)\" " + analyzer_base_args + " $(" + flags + "_ANALYZER) $(CFLAGS_ANALYZER) $(" + bd_flags_name + "_ANALYZER) " + dir + "\\" + src);
@@ -1830,7 +1830,7 @@ function ADD_SOURCES(dir, file_list, target, obj_dir, duplicate_sources)
 				}
 
 				var c11_flag = VS_TOOLSET && !cxx_mode_targets[target] && /\.c\s/i.test(src_line) && !/\.cpp\s/i.test(src_line) ? " /std:c11" : "";
-				MFO.WriteLine("\t" + CMD_MOD1 + "$(CC) $(" + flags + ") $(CFLAGS)" + c11_flag + " /Fo" + sub_build + d + " $(" + bd_flags_name + ") /c " + src_line);
+				MFO.WriteLine("\t" + CMD_MOD1 + "$(CC)" + c11_flag + " $(" + flags + ") $(CFLAGS) /Fo" + sub_build + d + " $(" + bd_flags_name + ") /c " + src_line);
 
 				if ("clang" == PHP_ANALYZER) {
 					MFO.WriteLine("\t\"$(CLANG_CL)\" " + analyzer_base_args + " $(" + flags + "_ANALYZER) $(CFLAGS_ANALYZER)  $(" + bd_flags_name + "_ANALYZER) " + src_line);
